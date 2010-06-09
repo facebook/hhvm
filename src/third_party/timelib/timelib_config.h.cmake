564401ae8189fd0cfae4189e6c4ec149ad373816
Scott MacVicar <macvicar@facebook.com>
2010-06-09 15:53:35 -0700

    typo noticed by hui chen

--- src/third_party/timelib/timelib_config.h.cmake
+++ src/third_party/timelib/timelib_config.h.cmake
@@ -5,7 +5,7 @@
 #cmakedefine HAVE_INTTYPES_H 1
 #cmakedefine WORDS_BIGENDIAN 1
 #cmakedefine HAVE_SETLOCALE 1
-#cmakedefine HAVE_LIMITS_H
+#cmakedefine HAVE_LIMITS_H 1
 #cmakedefine HAVE_INT32_T 1
 #cmakedefine HAVE_UINT32_T 1
 #cmakedefine HAVE_STRTOLL 1

18c94d0c361e9b3b6d9dbf61a970ce5d15e2a74e
mwilliams <markw65@facebook.com>
2010-07-05 15:30:40 -0700

    Temporarily back out a test under hphpi
    Summary:
    The test passes under hphpc, but fails under hphpi. Removing for now until
    hphpi can be fixed.
    
    Test Plan:
    test TestCodeRunEval TestInlining
    
    DiffCamp Revision: 129875
    Reviewed By: hzhao
    CC: hphp-diffs@lists, hzhao
    Revert Plan:
    OK

--- src/test/test_code_run.cpp
+++ src/test/test_code_run.cpp
@@ -11842,16 +11842,18 @@ bool TestCodeRun::TestInlining() {
        "  }"
        "var_dump(test(1,2,3));");
 
-  MVCR("<?php "
-       "function g($a) { function t(){}; return $a ? array(1,2,3) : 'foo'; }"
-       "function f($a) { return g($a); }"
-       "function test($a) {"
-       "  return reset((f($a)));"
-       "  }"
-       "var_dump(test(1));"
-       "function &h(&$a) { return $a['foo']; }"
-       "function i($a) { $x = &h($a); $x = 'hello'; return $a; }"
-       "var_dump(i(false));");
+  if (Option::EnableEval < Option::FullEval) {
+    MVCR("<?php "
+         "function g($a) { function t(){}; return $a ? array(1,2,3) : 'foo'; }"
+         "function f($a) { return g($a); }"
+         "function test($a) {"
+         "  return reset((f($a)));"
+         "  }"
+         "var_dump(test(1));"
+         "function &h(&$a) { return $a['foo']; }"
+         "function i($a) { $x = &h($a); $x = 'hello'; return $a; }"
+         "var_dump(i(false));");
+  }
 
   Option::AutoInline = save;
   return true;

2227cda88982a6f185651ce2787019de8f949697
Scott MacVicar <macvicar@facebook.com>
2010-07-11 15:42:12 -0700

    Make sure debug info isn't replaced because of our hardcoded flags, rely on CMake to provide the correct ones

--- CMake/HPHPSetup.cmake
+++ CMake/HPHPSetup.cmake
@@ -2,21 +2,24 @@ OPTION(INFINITE_LOOP_DETECTION "Enable Infinite Loop Detection" ON)
 OPTION(INFINITE_RECURSION_DETECTION "Enable Infinite Recursion Detection" ON)
 OPTION(REQUEST_TIMEOUT_DETECTION "Enable Timeout Detection" ON)
 
+if(NOT CMAKE_BUILD_TYPE)
+	set(CMAKE_BUILD_TYPE "Release")
+endif()
+
 include(HPHPFunctions)
 include(HPHPFindLibs)
 
 add_definitions(-D_GNU_SOURCE -D_REENTRANT=1 -D_PTHREADS=1)
 
-#should wrap this in an if
-add_definitions(-DRELEASE=1)
+if(${CMAKE_BUILD_TYPE} MATCHES "Release")
+	add_definitions(-DRELEASE=1)
+endif()
 
 # eable the OSS options if we have any
 add_definitions(-DHPHP_OSS=1)
 
-set(HPHP_OPT "-O3")
-
-set(CMAKE_C_FLAGS "${HPHP_OPT} -w -fPIC")
-set(CMAKE_CXX_FLAGS "${HPHP_OPT} -fPIC -fno-omit-frame-pointer -ftemplate-depth-60 -Wall -Woverloaded-virtual -Wno-deprecated -Wno-parentheses -Wno-strict-aliasing -Wno-write-strings -Wno-invalid-offsetof")
+set(CMAKE_C_FLAGS "-w -fPIC")
+set(CMAKE_CXX_FLAGS "-fPIC -fno-omit-frame-pointer -ftemplate-depth-60 -Wall -Woverloaded-virtual -Wno-deprecated -Wno-parentheses -Wno-strict-aliasing -Wno-write-strings -Wno-invalid-offsetof")
 
 include_directories(${HPHP_HOME}/src)
 include_directories(${HPHP_HOME}/src/lib/system/gen)

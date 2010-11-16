27afbe78b29fe058b514ef1635355f3700cd4e8e
macvicar <macvicar@facebook.com>
2010-11-16 05:52:39 -0800

    Add the ability for external contributors to add their own credit
    Summary:
    Hyves have been contributing and we're adding their own Copyright at the
    top, it's still licensed under the PHP license and they've signed a CLA.
    
    Test Plan:
    Added new file and run bin/license.php
    
    DiffCamp Revision: 183002
    Reviewed By: hzhao
    CC: hzhao, achao, hphp-diffs@lists
    Revert Plan:
    Ok

--- bin/license.php
+++ bin/license.php
@@ -24,6 +24,9 @@ $excluded = array(
   'third_party/',
 );
 
+$files_external_party = array(
+);
+
 $files_zend = array(
   'runtime/base/zend/',
   'runtime/base/array/zend_array.cpp',
@@ -41,6 +44,9 @@ $generated_files = array(
   'lex.eval_.cpp',
   'hphp.tab.cpp',
 );
+
+$external_licenses = array();
+$external_licenses['hyves'] = 'Copyright (c) 2010 Hyves (http://www.hyves.nl)';
 
 $license_zend = <<<LICENSE
 /*
@@ -98,6 +104,35 @@ $license_php = <<<LICENSE
 */
 LICENSE;
 
+$license_external = <<<LICENSE
+/*
+   +----------------------------------------------------------------------+
+   | HipHop for PHP                                                       |
+   +----------------------------------------------------------------------+
+<external>
+   | Copyright (c) 2010 Facebook, Inc. (http://www.facebook.com)          |
+   | Copyright (c) 1997-2010 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+*/
+LICENSE;
+
+// license length
+$license_line_length = 74;
+
+$built_licenses = array();
+foreach ($external_licenses as $key => $value) {
+  $line = '   | ' . $value;
+  $line .= str_repeat(' ', $license_line_length - strlen($line)) . '|';
+  $built_licenses[$key] = str_replace('<external>', $line, $license_external);
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -130,6 +165,15 @@ foreach ($files as $file) {
     }
     file_put_contents($file, $replaced);
     $contents = $replaced;
+  }
+
+  foreach ($files_external_party as $f => $license) {
+    if (preg_match('/'.preg_quote($f, '/').'/', $file)) {
+      if (isset($built_licenses[$license])) {
+        file_put_contents($file, $built_licenses[$license]."\n".$contents);
+        continue 2;
+      }
+    }
   }
 
   // add zend licese

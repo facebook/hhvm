add89e477f1f20d77f4511e089a359f646904fe4
Lorenzo Castelli <lcastelli@gmail.com>
2011-02-03 11:04:25 +0100

    Add detection of jemalloc in cmake
    This adds detection of jemalloc in cmake. It can be enabled with an
    option, which is left to OFF by default for now.

--- CMake/Options.cmake
+++ CMake/Options.cmake
@@ -17,6 +17,8 @@ option(ENABLE_FULL_SETLINE "Enable full setline function for debugger and code-c
 
 option(WANT_FB_LIBMCC "want FB Memcache" OFF)
 
-option(USE_TCMALLOC "Use tcmalloc" ON)
+option(USE_JEMALLOC "Use jemalloc" OFF)
+
+option(USE_TCMALLOC "Use tcmalloc (if jemalloc is not used)" ON)
 option(USE_GOOGLE_HEAP_PROFILER "Use Google heap profiler" OFF)
 option(USE_GOOGLE_CPU_PROFILER "Use Google cpu profiler" OFF)

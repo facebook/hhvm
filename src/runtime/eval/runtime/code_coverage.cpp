da8a26c5e50346ea13f1a97d22b5aca4d730d8b1
pad <pad@fb.com>
2011-03-31 13:43:42 -0700

    Fix coverage report in hphpi -v Eval.RecordCodeCoverage=1, do not use line1
    Summary:
    See the comment
    
    Test Plan:
    make -j
    $ ./hphpi  -v Eval.RecordCodeCoverage=1 -v
    Eval.CodeCoverageOutputFile=/tmp/cov.log -f ~/cover.php
    $ cat /tmp/cov.log
    {
    "/home/pad/cover.php": [0,3,0,1,6,2,0,0,3,0,0,0,2]
    }
    
    which makes more sense than:
    {
    "/home/pad/cover.php": [0,3,3,2,7,4,2,1,4,4,3,1,3]
    }
    
    Reviewed By: hzhao
    Reviewers: sgrimm, hzhao, myang
    Commenters: myang, sgrimm
    CC: iproctor, sgrimm, hzhao, myang, pad, hphp-diffs@lists
    Revert Plan:
    OK
    
    Differential Revision: 231414

--- src/runtime/eval/runtime/code_coverage.cpp
+++ src/runtime/eval/runtime/code_coverage.cpp
@@ -26,10 +26,68 @@ namespace HPHP { namespace Eval {
 Mutex CodeCoverage::s_mutex;
 CodeCoverage::CodeCoverageMap CodeCoverage::s_hits;
 
+/*
+ * The function below will be called by the interpreter on each
+ * evaluated expression when running hphpi with:
+ *   $ hphpi -v Eval.RecordCodeCoverage=1 <phpfile>
+ * 
+ * The (line0, line1) pair is supposed to represent the start and end
+ * of an evaluated expression. One should normally increment the line
+ * usage counters for all the lines between line0 and line1 but certain
+ * constructs like including a file, eval-ing a string, or even 
+ * executing a for loop do not have a good value for line1.
+ *
+ * Indeed on this toy cover.php file:
+ *
+ *  <?php
+ * 
+ *  echo "here\n";
+ *  for($i = 0; $i < 2; $i++) {
+ *    echo "here $i\n";
+ *  }
+ *  
+ *  echo 1 +
+ *       2 +
+ *       (3 + 4);
+ *  
+ *  eval("echo 'bar\n';");
+ * 
+ * and with this command:
+ *
+ *  $ hphpi -v Eval.RecordCodeCoverage=1 \
+ *          -v Eval.CodeCoverageOutputFile=/tmp/cov.log \
+ *           -f cover.php
+ *
+ * one get this output (with appropriate printf in this file):
+ *
+ *   /home/pad/cover.php, (1, 12)
+ *   /home/pad/cover.php, (3, 3)
+ *   here
+ *   /home/pad/cover.php, (4, 6)
+ *   /home/pad/cover.php, (4, 4)
+ *   /home/pad/cover.php, (5, 5)
+ *   here 0
+ *   /home/pad/cover.php, (4, 4)
+ *   /home/pad/cover.php, (4, 4)
+ *   /home/pad/cover.php, (5, 5)
+ *   here 1
+ *   /home/pad/cover.php, (4, 4)
+ *   /home/pad/cover.php, (4, 4)
+ *   /home/pad/cover.php, (8, 10)
+ *   /home/pad/cover.php, (8, 9)
+ *   /home/pad/cover.php, (8, 10)
+ *   6/home/pad/cover.php, (12, 12)
+ *   /home/pad/cover.php, (12, 12)
+ *   /home/pad/cover.php, (1, 2)
+ *   /home/pad/cover.php, (1, 2)
+ * 
+ * So right now it is just simpler to ignore line1.
+ */
 void CodeCoverage::Record(const char *filename, int line0, int line1) {
   if (!filename || !*filename || line0 <= 0 || line1 <= 0 || line0 > line1) {
     return;
   }
+  Logger::Verbose("%s, (%d, %d)\n", filename, line0, line1);
 
   Lock lock(s_mutex);
 
@@ -37,7 +95,7 @@ void CodeCoverage::Record(const char *filename, int line0, int line1) {
   if (iter == s_hits.end()) {
     vector<int> &lines = s_hits[filename];
     lines.resize(line1 + 1);
-    for (int i = line0; i <= line1; i++) {
+    for (int i = line0; i <= line0 /* should be line1 one day */; i++) {
       lines[i] = 1;
     }
   } else {
@@ -45,7 +103,7 @@ void CodeCoverage::Record(const char *filename, int line0, int line1) {
     if ((int)lines.size() < line1 + 1) {
       lines.resize(line1 + 1);
     }
-    for (int i = line0; i <= line1; i++) {
+    for (int i = line0; i <= line0 /* should be line1 one day */; i++) {
       ++lines[i];
     }
   }

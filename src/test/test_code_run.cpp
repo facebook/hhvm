e41971f0aa36eb01ab6e88ff17e4a093757a99ab
mwilliams <mwilliams@fb.com>
2011-04-09 16:06:31 -0700

    [Fix] Fix crash in unserialize
    Summary:
    Another case of a bad key being passed to lvalAt. This time, its because we
    explicitly tell lvalAt that the key is good, so it doesnt bother to call toKey
    on it. We cant call toKey, because eg "1" should be different from 1 in this
    specific instance - where toKey would treat them as the same thing.
    
    So we need to check explicitly for integer or string keys, throw if not, and
    then tell lvalAt to use the key as is.
    
    Reviewed By: myang
    Reviewers: andrewparoski, myang
    CC: myang, lesha
    Tasks:
    
    Differential Revision: 235453

--- src/test/test_code_run.cpp
+++ src/test/test_code_run.cpp
@@ -11515,6 +11515,11 @@ bool TestCodeRun::TestSerialization() {
 
         "string(24) \"a:1:{i:0;a:1:{i:0;R:2;}}\"\n");
 
+  MVCR("<?php "
+       "var_dump(unserialize('a:1:{s:1:\"1\";s:3:\"foo\";}'));"
+       "var_dump(unserialize('a:1:{d:1;s:3:\"foo\";}'));"
+       "var_dump(unserialize('a:1:{a:1:{i:0;i:1;}}'));");
+
   return true;
 }
 

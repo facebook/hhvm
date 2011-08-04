2965813f74bf12fca01616777d5bad79eace990b
myang <myang@fb.com>
2011-08-04 10:42:52 -0700

    [Perf/HPHPi] Reduce file repository lock contention
    Summary:
    
    Task ID: #
    
    Blame Rev:
    
    Reviewers:
    
    CC:
    
    Test Plan:
    
    Revert Plan:
    
    Tags:
    
    - begin *PUBLIC* platform impact section -
    Bugzilla: #
    - end platform impact -

--- src/runtime/eval/runtime/file_repository.cpp
+++ src/runtime/eval/runtime/file_repository.cpp
@@ -75,7 +75,7 @@ Variant PhpFile::eval(LVariableTable *vars) {
   return true;
 }
 
-Mutex FileRepository::s_lock;
+ReadWriteMutex FileRepository::s_lock;
 hphp_hash_map<string, PhpFileWrapper*, string_hash> FileRepository::s_files;
 hphp_hash_map<string, PhpFile*, string_hash> FileRepository::s_md5Files;
 
@@ -88,7 +88,7 @@ static class FileDumpInitializer {
 bool FileRepository::fileDump(const char *filename) {
   std::ofstream out(filename);
   if (out.fail()) return false;
-  Lock lock(s_lock);
+  ReadLock lock(s_lock);
   out << "s_files: " << s_files.size() << endl;
   for (hphp_hash_map<string, PhpFileWrapper*, string_hash>::const_iterator it =
        s_files.begin(); it != s_files.end(); it++) {
@@ -114,7 +114,7 @@ void FileRepository::onDelete(PhpFile *f) {
 void FileRepository::onZeroRef(PhpFile *f) {
   ASSERT(f->getRef() == 0);
   if (RuntimeOption::SandboxCheckMd5) {
-    Lock lock(s_lock);
+    WriteLock lock(s_lock);
     onDelete(f);
   } else {
     onDelete(f);
@@ -124,7 +124,6 @@ void FileRepository::onZeroRef(PhpFile *f) {
 PhpFile *FileRepository::checkoutFile(const string &rname,
                                       const struct stat &s) {
   PhpFile *ret = NULL;
-  Lock lock(s_lock);
   string name;
 
   if (rname[0] == '/') {
@@ -135,6 +134,19 @@ PhpFile *FileRepository::checkoutFile(const string &rname,
     name = RuntimeOption::SourceRoot + "/" + rname;
   }
 
+  {
+    ReadLock lock(s_lock);
+    hphp_hash_map<string, PhpFileWrapper*, string_hash>::iterator it =
+      s_files.find(name);
+    if (it != s_files.end() && !it->second->isChanged(s)) {
+      // should be the common case after initial startup
+      ret = it->second->getPhpFile();
+      ret->incRef();
+      return ret;
+    }
+  }
+
+  WriteLock lock(s_lock);
   hphp_hash_map<string, PhpFileWrapper*, string_hash>::iterator it =
     s_files.find(name);
   if (it == s_files.end()) {

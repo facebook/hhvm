playnine/hiphop-php

Work-around GCC compiler bug
The same problem described in a previous patch
3610aa4a81be8d770c928aa1685c97ff8b9e97b0 has been observed in the new
ObjectAllocatorInitSetup<T> function and how it calls
set<...>.insert(). Like in the previous case, there are situations
where the caller thinks the insert() function has been applied the
return value optimization, and so it passes as the first parameter the
address of where it wants the return value to be stored. The function
itself is instead not expecting any additional return value parameter,
and thus uses all the parameters incorrectly causing seg faults or
incorrect behaviors. See the description of the other patch for some
more information.

This patch moves the insert function call outside of the function
template, which seems effective at working around the problem.

src/runtime/base/object_data.h
@@ -378,7 +378,7 @@ class ItemSize<UNIT_SIZE> {
 template <typename T>
 void *ObjectAllocatorInitSetup() {
   ThreadLocalSingleton<ObjectAllocator<ItemSize<sizeof(T)>::value> > tls;
-  GetAllocatorInitList().insert((AllocatorThreadLocalInit)(tls.get));
+  AddAllocatorThreadLocalInit((AllocatorThreadLocalInit)(tls.get));
   return (void *)tls.getNoCheck;
 }
 

facebook/hhvm

src/runtime/base/object_data.h
@@ -476,7 +476,7 @@ class ItemSize<UNIT_SIZE> {
 template <typename T>
 void *ObjectAllocatorInitSetup() {
   ThreadLocalSingleton<ObjectAllocator<ItemSize<sizeof(T)>::value> > tls;
-  GetAllocatorInitList().insert((AllocatorThreadLocalInit)(tls.getCheck));
+  AddAllocatorThreadLocalInit((AllocatorThreadLocalInit)(tls.getCheck));
   return (void *)tls.getNoCheck;
 }
 
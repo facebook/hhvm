facebook/hhvm

commit 4a92854c4aa726c54b17a8ba3da3079ea212a6c2

[cleanup] Remove special handling for silence operator
Summary: The code to handle the silence operator was written before
preOutputCPP existed, and aimed to solve a similar problem - where to declare a
variable that could be used anywhere inside an expression. Remove the old code,
and rewrite to work with preOutputCPP instead.
Test Plan: fast_tests slow_tests
Reviewed By: myang
Reviewers: qigao, myang, stephentu
CC: ps, mwilliams, myang
Revert Plan:
Tags:

- begin *PUBLIC* platform impact section -
Bugzilla: #
- end platform impact -

Differential Revision: 296007

src/compiler/expression/unary_op_expression.cpp
@@ -82,16 +82,14 @@ inline void UnaryOpExpression::ctorInit() {
 UnaryOpExpression::UnaryOpExpression
 (EXPRESSION_CONSTRUCTOR_BASE_PARAMETERS, ExpressionPtr exp, int op, bool front)
   : Expression(EXPRESSION_CONSTRUCTOR_BASE_PARAMETER_VALUES),
-    m_exp(exp), m_op(op), m_front(front),
-    m_silencer(-1) {
+    m_exp(exp), m_op(op), m_front(front) {
   ctorInit();
 }
 
 UnaryOpExpression::UnaryOpExpression
 (EXPRESSION_CONSTRUCTOR_PARAMETERS, ExpressionPtr exp, int op, bool front)
   : Expression(EXPRESSION_CONSTRUCTOR_PARAMETER_VALUES(UnaryOpExpression)),
-    m_exp(exp), m_op(op), m_front(front),
-    m_silencer(-1) {
+    m_exp(exp), m_op(op), m_front(front) {
   ctorInit();
 }
 
@@ -182,17 +180,7 @@ void UnaryOpExpression::onParse(AnalysisResultConstPtr ar, FileScopePtr scope) {
 // static analysis functions
 
 void UnaryOpExpression::analyzeProgram(AnalysisResultPtr ar) {
-  if (ar->getPhase() == AnalysisResult::AnalyzeFinal && m_op == '@') {
-    StatementPtr stmt = ar->getStatementForSilencer();
-    ASSERT(stmt);
-    m_silencer = stmt->requireSilencers(1);
-  }
   if (m_exp) m_exp->analyzeProgram(ar);
-  if (ar->getPhase() == AnalysisResult::AnalyzeFinal && m_op == '@') {
-    StatementPtr stmt = ar->getStatementForSilencer();
-    ASSERT(stmt);
-    stmt->endRequireSilencers(m_silencer);
-  }
 }
 
 bool UnaryOpExpression::preCompute(CVarRef value, Variant &result) {
@@ -268,8 +256,7 @@ bool UnaryOpExpression::canonCompare(ExpressionPtr e) const {
     static_pointer_cast<UnaryOpExpression>(e);
 
   return m_op == u->m_op &&
-    m_front == u->m_front &&
-    m_silencer == u->m_silencer;
+    m_front == u->m_front;
 }
 
 ExpressionPtr UnaryOpExpression::preOptimize(AnalysisResultConstPtr ar) {
@@ -650,23 +637,14 @@ bool UnaryOpExpression::preOutputCPP(CodeGenerator &cg, AnalysisResultPtr ar,
 
   if (m_op == '@') {
     if (isUnused()) m_exp->setUnused(true);
-    bool inExpression = cg.inExpression();
-    bool doit = state & FixOrder;
-    if (!doit) {
-      cg.setInExpression(false);
-      if (m_exp->preOutputCPP(cg, ar, state)) {
-        doit = true;
-      }
-      cg.setInExpression(inExpression);
-    }
-    if (doit && inExpression) {
-      cg_printf("%s%d.enable();\n", Option::SilencerPrefix, m_silencer);
+    bool doit = (state & FixOrder) || m_exp->hasEffect();
+    if (doit && cg.inExpression()) {
+      cg.wrapExpressionBegin();
+      std::string tmp = genCPPTemp(cg, ar);
+      cg_printf("Silencer %s(true);\n", tmp.c_str());
       m_exp->preOutputCPP(cg, ar, 0);
-      int s = m_silencer;
-      m_silencer = -1;
-      this->preOutputStash(cg, ar, state | FixOrder);
-      m_silencer = s;
-      cg_printf("%s%d.disable();\n", Option::SilencerPrefix, m_silencer);
+      this->preOutputStash(cg, ar, state | FixOrder | StashAll);
+      cg_printf("%s.disable();\n", tmp.c_str());
     }
     return doit;
   } else if (m_op == T_PRINT && m_exp && !m_exp->hasEffect()) {
@@ -850,11 +828,6 @@ void UnaryOpExpression::outputCPPImpl(CodeGenerator &cg,
         }
         break;
       case '@':
-        if (m_silencer >= 0) {
-          cg_printf("(%s%d.enable(),%s%d.disable(",
-                    Option::SilencerPrefix, m_silencer,
-                    Option::SilencerPrefix, m_silencer);
-        }
         break;
       case T_FILE:
         cg_printf("get_source_filename(\"%s\")", getLocation()->file);
@@ -876,13 +849,8 @@ void UnaryOpExpression::outputCPPImpl(CodeGenerator &cg,
         }
         break;
       case '@':
-        if (m_silencer < 0 && isUnused()) {
+        if (isUnused()) {
           m_exp->outputCPPUnneeded(cg, ar);
-        } else if (!m_exp->hasCPPTemp() && !m_exp->getActualType()) {
-          // Void needs to return something to silenceDec
-          cg_printf("(");
-          m_exp->outputCPP(cg, ar);
-          cg_printf(",null)");
         } else {
           m_exp->outputCPP(cg, ar);
         }
@@ -929,9 +897,6 @@ void UnaryOpExpression::outputCPPImpl(CodeGenerator &cg,
       cg_printf(")");
       break;
     case '@':
-      if (m_silencer >= 0) {
-        cg_printf("))");
-      }
       break;
     default:
       break;

src/compiler/analysis/analysis_result.cpp
@@ -147,37 +147,6 @@ FileScopePtr AnalysisResult::findFileScope(const std::string &name) const {
   return FileScopePtr();
 }
 
-void AnalysisResult::pushStatement(StatementPtr stmt) {
-  m_stmt = stmt;
-  m_stmts.push_back(stmt);
-}
-
-void AnalysisResult::popStatement() {
-  ASSERT(!m_stmts.empty());
-  m_stmts.pop_back();
-  if (m_stmts.empty()) {
-    m_stmt.reset();
-  } else {
-    m_stmt = m_stmts.back();
-  }
-}
-
-StatementPtr AnalysisResult::getStatementForSilencer() const {
-  // Because of how we parse if/else statements, we need
-  // to handle them differently
-  if (m_stmt && m_stmt->is(Statement::KindOfIfBranchStatement)) {
-    if (m_stmts.size() < 3)
-      return StatementPtr();
-    // If the current statement is an IfBranchStatement, we want to
-    // return the enclosing IfStatement. The parser guarantees that
-    // each IfBranchStatement is the grandchild of the enclosing
-    // IfStatement.
-    ASSERT(m_stmts[m_stmts.size()-3]->is(Statement::KindOfIfStatement));
-    return m_stmts[m_stmts.size()-3];
-  }
-  return m_stmt;
-}
-
 FunctionScopePtr AnalysisResult::findFunction(
   const std::string &funcName) const {
   StringToFunctionScopePtrVecMap::const_iterator bit =

src/compiler/analysis/analysis_result.h
@@ -243,15 +243,6 @@ public:
   void addFileScope(FileScopePtr fileScope);
 
   /**
-   * To implement the silence operator correctly, we need to keep trace
-   * of the current statement being parsed.
-   */
-  void pushStatement(StatementPtr stmt);
-  void popStatement();
-  StatementPtr getStatement() const { return m_stmt; }
-  StatementPtr getStatementForSilencer() const;
-
-  /**
    * Declarations
    */
   bool declareFunction(FunctionScopePtr funcScope) const;

src/compiler/expression/expression.cpp
@@ -704,7 +704,10 @@ void Expression::preOutputStash(CodeGenerator &cg, AnalysisResultPtr ar,
                                 int state) {
   if (hasCPPTemp() || isScalar()) return;
   bool fastCast = needsFastCastTemp(ar);
-  if (!isLocalExprAltered() && !hasEffect() && !fastCast) return;
+  if (!isLocalExprAltered() && !hasEffect() &&
+      !fastCast && !(state & StashAll)) {
+    return;
+  }
 
   bool killCast = false;
 

src/compiler/expression/unary_op_expression.h
@@ -64,7 +64,6 @@ protected:
   ExpressionPtr m_exp;
   int m_op;
   bool m_front;
-  int m_silencer;
 
 private:
   bool preCompute(CVarRef value, Variant &result);

src/compiler/statement/block_statement.cpp
@@ -45,7 +45,7 @@ int BlockStatement::getRecursiveCount() const {
 ///////////////////////////////////////////////////////////////////////////////
 // static analysis functions
 
-void BlockStatement::analyzeProgramImpl(AnalysisResultPtr ar) {
+void BlockStatement::analyzeProgram(AnalysisResultPtr ar) {
   if (m_stmts) m_stmts->analyzeProgram(ar);
 }
 

src/compiler/statement/break_statement.cpp
@@ -50,7 +50,7 @@ StatementPtr BreakStatement::clone() {
 ///////////////////////////////////////////////////////////////////////////////
 // static analysis functions
 
-void BreakStatement::analyzeProgramImpl(AnalysisResultPtr ar) {
+void BreakStatement::analyzeProgram(AnalysisResultPtr ar) {
   if (m_exp) m_exp->analyzeProgram(ar);
 }
 

src/compiler/statement/case_statement.cpp
@@ -74,7 +74,7 @@ string CaseStatement::getLiteralString() const {
   return exp->getLiteralString();
 }
 
-void CaseStatement::analyzeProgramImpl(AnalysisResultPtr ar) {
+void CaseStatement::analyzeProgram(AnalysisResultPtr ar) {
   if (m_condition) m_condition->analyzeProgram(ar);
   if (m_stmt) m_stmt->analyzeProgram(ar);
 }

commit ce7bd50b8edf31c3b83965406ca6c849ce9e4abf

Bug in using error-control operator together with constants.
The following code
<?php
   var_dump(@A);
   var_dump(B);
?>
when compiled will result in
HipHop Notice:  Use of undefined constant A - assumed 'A' in _bug2.php on line 3
string(1) "A"
HipHop Notice:  Use of undefined constant B - assumed 'B' in _bug2.php on line 4
string(1) "B"
while only second notice is expected.

The bug was introduced with 4a92854c4aa726c54b17a8ba3da3079ea212a6c2.

The fix makes it always use Silencer for constants with error-control operator.

src/compiler/expression/unary_op_expression.cpp
@@ -53,6 +53,11 @@ inline void UnaryOpExpression::ctorInit() {
       m_exp->clearContext(Expression::NoLValueWrapper);
     }
     break;
+  case '@':
+    if (m_exp->is(Expression::KindOfConstantExpression)) {
+      m_localEffects = IOEffect;
+    }
+    break;
   case T_PRINT:
     m_localEffects = IOEffect;
     break;
@@ -664,7 +669,7 @@ bool UnaryOpExpression::preOutputCPP(CodeGenerator &cg, AnalysisResultPtr ar,
 
   if (m_op == '@') {
     if (isUnused()) m_exp->setUnused(true);
-    bool doit = (state & FixOrder) || m_exp->hasEffect();
+    bool doit = (state & FixOrder) || m_exp->hasEffect() || hasEffect();
     if (doit && cg.inExpression()) {
       cg.wrapExpressionBegin();
       std::string tmp = genCPPTemp(cg, ar);
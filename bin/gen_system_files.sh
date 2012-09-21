1f553118f4037f599e85c4a75352bf3b5e660ea6
Sara Golemon <sgolemon@fb.com>
2012-09-20 17:48:13 -0700

    Add helper script to created generated files

---
+++ bin/gen_system_files.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+check_err()
+{
+  if [ "$1" -ne "0" ]; then
+    echo "ERROR # $1 : $2" 1>&2
+    exit $1
+  fi
+}
+
+[ -z "$HPHP_HOME" ] && check_err 1 "HPHP_HOME environment variable not set"
+[ -z "$USE_HHVM" ] && check_err 1 "USE_HHVM must be set to generate HHVM files"
+
+VERBOSE=1
+
+HPHP=$HPHP_HOME/src/hphp/hphp
+[ ! -x "$HPHP" ] && check_err 1 "$HPHP is not executable"
+
+HHVM=$HPHP_HOME/src/hhvm/hhvm
+[ ! -x "$HHVM" ] && check_err 1 "$HHVM is not executable"
+
+if [ "$1" = "help" ]; then
+  echo "$0 gen        - Build src/system/gen/* files"
+  echo "$0 hhvm       - Build src/system/runtime/ext/*.ext_hhvm.cpp"
+  echo "$0 infotabs   - Build src/system/runtime/ext_hhvm/ext_hhvm_infotabs.cpp"
+  echo "$0 systemlib  - Build bin/systemlib.php"
+  echo "$0 injection  - Add INJECTION macros to extensions"
+  echo "$0 license    - Add license headers to all files"
+  echo ""
+  echo "$0 all  - All of the above in listed order"
+  exit 0
+fi
+
+if [ "$1" = "gen" -o "$1" = "all" ]; then
+  [ $VERBOSE -eq 1 ] && echo "Generating src/system/gen/* files using hphpc"
+  cd $HPHP_HOME/src/system
+  $HPHP --opts=none -t cpp -f sys -o gen \
+        --input-dir . -i `find classes globals -name '*.php'`
+  check_err $? "Failed generating src/system/gen/*"
+fi
+
+if [ "$1" = "hhvm" -o "$1" = "all" ]; then
+  cd $HPHP_HOME/src/runtime/ext
+  SOURCES=`ls ext_*.cpp | grep -v ext_hhvm | cut -d . -f 1` 
+  RUNTIME_BUILD=$HPHP_HOME/src/CMakeFiles/hphp_runtime_static.dir
+  cd $HPHP_HOME/src/runtime/ext_hhvm
+  if [ -z "$SOURCES" -a $VERBOSE -eq 1 ]; then
+    echo "No extensions found while generating *.ext_hhvm.cpp"
+  fi
+  for i in $SOURCES; do
+    [ ! -f "$RUNTIME_BUILD/runtime/ext/$i.cpp.o" ] && \
+      check_err 1 "No object file to generate $i.ext_hhvm.cpp from, did you build first?"
+    [ $VERBOSE -eq 1 ] && echo "Generating src/runtime/ext/$i.ext_hhvm.cpp"
+    $HHVM gen_ext_hhvm.php ../ext/$i.ext_hhvm.cpp . \
+          /dev/null /dev/null "" \
+          $RUNTIME_BUILD/runtime/ext/$i.cpp.o
+    check_err $? "Failed generating src/runtime/ext/$i.ext_hhvm.cpp"
+  done
+fi
+
+if [ "$1" = "infotabs" -o "$1" = "all" ]; then
+  cd $HPHP_HOME/src/runtime/ext_hhvm
+  [ $VERBOSE -eq 1 ] && echo "Generating src/runtime/ext_hhvm/ext_hhvm_infotabs.cpp"
+  $HHVM gen_infotabs.php ext_hhvm_infotabs.cpp . "" "" ""
+  check_err $? "Failed generating src/runtime/ext_hhvm/ext_hhvm_infotabs.cpp"
+fi
+
+if [ "$1" = "systemlib" -o "$1" = "all" ]; then
+  cd $HPHP_HOME
+  [ $VERBOSE -eq 1 ] && echo "Generating bin/systemlib.php"
+  $HHVM src/system/lib/gen_systemlib.php bin/
+  check_err $? "Failed generating bin/systemlib.php"
+fi
+
+if [ "$1" = "injection" -o "$1" = "all" ]; then
+  cd $HPHP_HOME
+  [ $VERBOSE -eq 1 ] && echo "Adding INJECTION macros to extensions"
+  $HHVM $HPHP_HOME/bin/ext_injection.php
+  check_err $? "Failed adding injection macros"
+fi
+
+if [ "$1" = "license" -o "$1" = "all" ]; then
+  cd $HPHP_HOME
+  [ $VERBOSE -eq 1 ] && echo "Updating license headers"
+  # TODO: At the moment, license.php fails on PCRE_ERROR_MATCHLIMIT
+  # Fix that script then change this to detect errors properly
+  $HHVM $HPHP_HOME/bin/license.php 2>&1 | grep -v PCRE_ERROR_MATCHLIMIT
+  #check_err $? "Failed updating license headers"
+fi

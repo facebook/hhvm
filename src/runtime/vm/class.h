7435197c6c7b442438cd9d45cc047bc2641fc7a9
bsimmers <bsimmers@fb.com>
2012-11-09 13:23:45 -0800

    Check jemaloc version in Class::alwaysLowMem
    We're using a feature that's only stable in jemalloc >= 3.2.0

--- src/runtime/vm/class.h
+++ src/runtime/vm/class.h
@@ -19,6 +19,9 @@
 
 #include <bitset>
 #include <tbb/concurrent_hash_map.h>
+#ifdef USE_JEMALLOC
+# include <jemalloc/jemalloc.h>
+#endif
 
 #include <runtime/vm/core_types.h>
 #include <runtime/vm/repo_helpers.h>
@@ -542,7 +545,16 @@ public:
   void atomicRelease();
 
   static bool alwaysLowMem() {
+    // jemalloc 3.2.0 and later support allocating objects in low memory
+    // (addresses that fit in 32 bits), and we take advantage of this in
+    // translated code.
+#if defined(JEMALLOC_VERSION) &&                                        \
+  ((JEMALLOC_VERSION_MAJOR == 3 && JEMALLOC_VERSION_MINOR >= 2) ||      \
+   JEMALLOC_VERSION_MAJOR > 3)
     return use_jemalloc && RuntimeOption::RepoAuthoritative;
+#else
+    return false;
+#endif
   }
   static size_t sizeForNClasses(unsigned nClasses) {
     return offsetof(Class, m_classVec) + (sizeof(Class*) * nClasses);

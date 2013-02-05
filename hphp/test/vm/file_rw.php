453c9938c92bc5b111c5c56d9a1d0c8d09bc737f
mwilliams <mwilliams@fb.com>
2013-02-05 13:52:03 -0800

    Dont leave 1.txt lying around

--- hphp/test/vm/file_rw.php
+++ hphp/test/vm/file_rw.php
@@ -1,7 +1,9 @@
 <?php
-unlink("1.txt");
-$w = fopen("1.txt", "w");
-$r = fopen("1.txt", "r");
+$fname = tempnam(__DIR__, "data");
+unlink("$fname");
+$w = fopen("$fname", "w");
+$r = fopen("$fname", "r");
 printf("Read %d bytes\n", strlen(fread($r, 10)));
 fwrite($w, "hello\n");fclose($w);
 printf("Read %d bytes\n", strlen(fread($r, 10)));
+unlink($fname);

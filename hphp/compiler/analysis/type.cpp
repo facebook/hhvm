755d38157fa3a888b32197dcb2c55726455232cb
aalexandre <aalexandre@fb.com>
2013-02-06 22:40:40 -0800

    Fix serialization test failure introduced by inttypes
    It seems serialization is sensitive to the alias name for int64.

--- hphp/compiler/analysis/type.cpp
+++ hphp/compiler/analysis/type.cpp
@@ -595,7 +595,7 @@ string Type::getCPPDecl(AnalysisResultConstPtr ar,
   switch (m_kindOf) {
     case KindOfBoolean:     return "bool";
     case KindOfInt32:       return "int";
-    case KindOfInt64:       return "int64_t";
+    case KindOfInt64:       return "int64";
     case KindOfDouble:      return "double";
     case KindOfString:      return "String";
     case KindOfArray:       return "Array";

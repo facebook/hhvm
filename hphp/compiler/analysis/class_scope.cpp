7ff582616fdfd166d4ab03293becd249c6da451d
mwilliams <mwilliams@fb.com>
2013-03-01 06:09:43 -0800

    Don't compile systemlib php files
    We don't need them in g_class_map, beacuse hhvm processes
    the php for itself.

--- hphp/compiler/analysis/class_scope.cpp
+++ hphp/compiler/analysis/class_scope.cpp
@@ -356,7 +356,7 @@ void ClassScope::collectMethods(AnalysisResultPtr ar,
           setVolatile();
         }
       }
-    } else {
+    } else if (!Option::SystemGen) {
       Compiler::Error(Compiler::UnknownBaseClass, m_stmt, base);
       if (base == m_parent) {
         ar->declareUnknownClass(m_parent);

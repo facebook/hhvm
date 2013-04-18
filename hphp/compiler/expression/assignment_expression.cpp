8a0dd4fae45eb92ebd404d5a726f7a9cccde403b
mwilliams <mwilliams@fb.com>
2013-04-17 17:08:16 -0700

    Don't fold invalid string operations at compile time
    It turns out the String::rvalAt and String::set don't raise
    warnings on invalid offsets, so check explicitly before using them.

--- hphp/compiler/expression/assignment_expression.cpp
+++ hphp/compiler/expression/assignment_expression.cpp
@@ -249,6 +249,14 @@ ExpressionPtr AssignmentExpression::preOptimize(AnalysisResultConstPtr ar) {
               g_context->setThrowAllErrors(true);
               if (aoff) {
                 if (!aoff->getScalarValue(o)) break;
+                if (v.isString()) {
+                  if (!o.isInteger() ||
+                      o.toInt64Val() < 0 ||
+                      o.toInt64Val() >= v.toCStrRef().size()) {
+                    // warnings should be raised...
+                    break;
+                  }
+                }
                 v.set(o, r);
               } else {
                 v.append(r);

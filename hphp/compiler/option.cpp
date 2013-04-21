1cea03f3f7b26966373424f7d67adf6a83272e6d
Edwin Smith <smith@fb.com>
2013-04-21 10:13:43 -0700

    hphp/src is now hphp/hphp
    That's it

--- hphp/compiler/option.cpp
+++ hphp/compiler/option.cpp
@@ -166,7 +166,7 @@ std::string Option::GetSystemRoot() {
       throw Exception("Environment variable HPHP_HOME is not set.");
     }
     SystemRoot = home;
-    SystemRoot += "/src";
+    SystemRoot += "/hphp";
   }
   return SystemRoot;
 }

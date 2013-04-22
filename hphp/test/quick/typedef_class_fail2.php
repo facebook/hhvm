a2e46efe3816311069b60ca1781f85395a5ac089
Mark Williams <mwilliams@fb.com>
2013-04-22 07:43:07 -0700

    Make sure we raise a warning in repo mode
    Repo mode can skip undefined variable warnings,
    if they're known to be uninit at compile time, and eliminate
    expressions whose only side effect might be to raise a
    diagnostic.

--- hphp/test/quick/typedef_class_fail2.php
+++ hphp/test/quick/typedef_class_fail2.php
@@ -8,6 +8,6 @@ type Foo = MyClass;
 function test(): void {
   // error: making a new type name doesn't make it available outside
   // of type expressions (parameter lists, etc).
-  $x = Foo::ASD;
+  return Foo::ASD;
 }
 test();
--- hphp/test/quick/undefined-variable.php
+++ hphp/test/quick/undefined-variable.php
@@ -1,11 +1,13 @@
 <?php
 function foo() {
-  $a = $b;
+  if (isset($GLOBALS['b'])) $b = 0;
+  return $b;
 }
 foo();
 
 function baz($x) {}
 function bar() {
+  if (isset($GLOBALS['a'])) $a = 0;
   baz($a);
 }
 bar();

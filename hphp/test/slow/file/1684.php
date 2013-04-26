90e7a30404928b707907d932d25a5ae4dd148e74
Paul Tarjan <ptarjan@fb.com>
2013-04-26 03:45:15 -0700

    fix shared test name
    This test was imported WITHOUT hard-coded output. Meaning it was compared to zend. It so happened that I had cleaned my repo after any runs of ##TestExt## so the file they both created (and sometimes didn't clean up) didn't exist. So that is the output I recorded.
    
    @mwilliams do you think you intended for the file to exist? It wasn't in the repo.
    
    With this diff I'm using a unique filename so it doesn't matter if ##TestExt## hasn't cleaned up perfectly.

--- hphp/test/slow/file/1684.php
+++ hphp/test/slow/file/1684.php
@@ -1,3 +1,8 @@
 <?php
 
- var_dump(filetype('test/test_ext_file2.tmp'));var_dump(is_link('test/test_ext_file2.tmp'));$a = lstat('test/test_ext_file2.tmp');var_dump($a['mtime']);
+$file = __DIR__."/../../sample_dir/symlink";
+
+var_dump(filetype($file));
+var_dump(is_link($file));
+$a = lstat($file);
+var_dump($a['mtime'] > 1234567890);

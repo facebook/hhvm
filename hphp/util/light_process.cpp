59beab1596e6f73379e4fd3369e0d681ea9be629
bsimmers <bsimmers@fb.com>
2013-05-02 16:31:26 -0700

    Don't dereference a null array in our SIGCHLD handler
    Sometimes this array is destructed by the time the handler is
    called.

--- hphp/util/light_process.cpp
+++ hphp/util/light_process.cpp
@@ -298,7 +298,7 @@ void LightProcess::SigChldHandler(int sig, siginfo_t* info, void* ctx) {
   }
   pid_t pid = info->si_pid;
   for (int i = 0; i < g_procsCount; ++i) {
-    if (g_procs[i].m_shadowProcess == pid) {
+    if (g_procs && g_procs[i].m_shadowProcess == pid) {
       // The exited process was a light process. Notify the callback, if any.
       if (s_lostChildHandler) {
         s_lostChildHandler(pid);

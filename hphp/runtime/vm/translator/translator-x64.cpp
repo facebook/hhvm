d04d06c7356f7c919f808427e21b87bcedd16ebe
Guilherme Ottoni <ottoni@fb.com>
2013-05-21 21:56:22 -0700

    Increase spill area
    Bump it from 8 to 16 Cells.

--- hphp/runtime/vm/translator/translator-x64.cpp
+++ hphp/runtime/vm/translator/translator-x64.cpp
@@ -2851,7 +2851,7 @@ static_assert(rVmSp == rbx &&
               rVmTl == r12 &&
               rStashedAR == r15,
               "__enterTCHelper needs to be modified to use the correct ABI");
-static_assert(kReservedRSPScratchSpace == 0x100,
+static_assert(kReservedRSPScratchSpace == 0x180,
               "enterTCHelper needs to be updated for changes to "
               "kReservedRSPScratchSpace");
 static_assert(REQ_BIND_CALL == 0x1,

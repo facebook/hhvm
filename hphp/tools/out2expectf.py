87ff33328153c7af783e2f802c7f72cb880ccf8a
Mike Magruder <mikemag@fb.com>
2013-06-04 14:08:13 -0700

    Add support for debuggers and continuations to out2expectf.py
    Added something specific for the debugger to sanitize the relative paths of the debugger tests. Also added sanitization for continuation functions names.

--- hphp/tools/out2expectf.py
+++ hphp/tools/out2expectf.py
@@ -14,6 +14,12 @@ for test in sys.argv[1:]:
 
     data = file(test + '.out').read()
     data = re.sub('/data[^ ]*/hphp', '%s', data)
+    # The debugger prints the path given on the command line, which is often
+    # relative. All such debugger tests live under something/debugger/foo.php.
+    data = re.sub('[^ ]*/debugger(/[^ ]*.php)', r'%s\1', data)
+    # Generator method names are, well, generated!
+    # See ParserBase::getAnonFuncName.
+    data = re.sub(' [0-9]+_[0-9]+\(', ' %d_%d(', data)
     file(test + '.expectf', 'w').write(data)
 
     print ('Copied %s.out to %s.expectf' % (test, test))

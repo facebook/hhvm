ec6f051b0bb88e24626d3cd085a7ed7ee3375b0d
Paul Tarjan <ptarjan@fb.com>
2013-06-05 12:13:55 -0700

    fix bug36258
    Now that contbuild summaries work, it is so much nicer.
    
    I'm trying to use the property in a child. Either a keep the data around or make the parent's protected.

--- hphp/test/ext/test_ext_memcached.cpp
+++ hphp/test/ext/test_ext_memcached.cpp
@@ -2,9 +2,7 @@
    +----------------------------------------------------------------------+
    | HipHop for PHP                                                       |
    +----------------------------------------------------------------------+
-   | Copyright (c) 2010 Hyves (http://www.hyves.nl)                       |
    | Copyright (c) 2010-2013 Facebook, Inc. (http://www.facebook.com)     |
-   | Copyright (c) 1997-2010 The PHP Group                                |
    +----------------------------------------------------------------------+
    | This source file is subject to version 3.01 of the PHP license,      |
    | that is bundled with this package in the file LICENSE, and is        |

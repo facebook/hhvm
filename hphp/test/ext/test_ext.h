74f217adcb114427910d366b081e1a3f0640e8b6
Jordan DeLong <jdelong@fb.com>
2013-06-14 20:57:27 -0700

    Move TestExtBzip2 to a php implementation

--- hphp/test/ext/test_ext.h
+++ hphp/test/ext/test_ext.h
@@ -34,7 +34,6 @@
 #include "hphp/facebook/extensions/string_buffer/test_ext_string_buffer.h"
 #include "hphp/facebook/extensions/tao/test_ext_tao.h"
 #include "hphp/facebook/extensions/urlextraction/test_ext_urlextraction.h"
-#include "hphp/test/ext/test_ext_bzip2.h"
 #include "hphp/test/ext/test_ext_curl.h"
 #include "hphp/test/ext/test_ext_file.h"
 #include "hphp/test/ext/test_ext_hash.h"

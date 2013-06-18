3d3865c34ea246e0b2fbfa73e685881093a87269
Jordan DeLong <jdelong@fb.com>
2013-06-17 22:12:23 -0700

    Port TestExtZlib to php
    Also fixed the memory leak.

--- hphp/test/ext/test_ext.h
+++ hphp/test/ext/test_ext.h
@@ -44,6 +44,5 @@
 #include "hphp/test/ext/test_ext_process.h"
 #include "hphp/test/ext/test_ext_server.h"
 #include "hphp/test/ext/test_ext_session.h"
-#include "hphp/test/ext/test_ext_zlib.h"
 
 #endif // incl_EXT_LIST_TEST_EXT_H_

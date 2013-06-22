7d997163991627e95a67329d39647ac8023a2fb0
Jordan DeLong <jdelong@fb.com>
2013-06-21 22:05:23 -0700

    Beef up type alias mixedcase unit test
    Add a few more cases that weren't tested.

--- hphp/test/quick/typedef_mixed_case.php
+++ hphp/test/quick/typedef_mixed_case.php
@@ -34,6 +34,38 @@ function lots(A $x,
               I $xxxxxxxxx)
 {}
 
+type A_ = a;
+type B_ = b;
+type C_ = c;
+type D_ = d;
+type E_ = e;
+type F_ = f;
+type G_ = g;
+type H_ = h;
+type I_ = i;
+
+function lots2(A_ $x,
+               B_ $xx,
+               C_ $xxx,
+               D_ $xxxx,
+               E_ $xxxxx,
+               F_ $xxxxxx,
+               G_ $xxxxxxx,
+               H_ $xxxxxxxx,
+               I_ $xxxxxxxxx)
+{}
+
+function lots3(a_ $x,
+               b_ $xx,
+               c_ $xxx,
+               d_ $xxxx,
+               e_ $xxxxx,
+               f_ $xxxxxx,
+               g_ $xxxxxxx,
+               h_ $xxxxxxxx,
+               i_ $xxxxxxxxx)
+{}
+
 function main() {
   foo(12, 13);
   bar(1.0);
@@ -48,6 +80,24 @@ function main() {
        1.3,
        1.4,
        1.5);
+  lots2(array(),
+        12,
+        12,
+        false,
+        true,
+        "asd",
+        1.3,
+        1.4,
+        1.5);
+  lots3(array(),
+        12,
+        12,
+        false,
+        true,
+        "asd",
+        1.3,
+        1.4,
+        1.5);
 }
 
 main();

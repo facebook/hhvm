fd94725e578f797bc331de03c99d291b306d3957
Sara Golemon <sgolemon@fb.com>
2013-06-24 19:33:24 -0700

    Get rid of runtime/base/hphp.h
    This is a vestigal limb of HPHPc
    which caused global_variables.cpp to way over-include
    by way of runtime/ext/ext.h

--- hphp/hhvm/global_variables.cpp
+++ hphp/hhvm/global_variables.cpp
@@ -14,7 +14,7 @@
    +----------------------------------------------------------------------+
 */
 
-#include "hphp/runtime/base/hphp.h"
+#include "hphp/runtime/base/base_includes.h"
 
 namespace HPHP {
 

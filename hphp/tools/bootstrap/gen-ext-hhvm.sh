f9da9970d2e6b9a3407d2eaca87f7e400ac85bea
Sara Golemon <sgolemon@fb.com>
2013-07-10 17:51:12 -0700

    Pass OS determination from CMake into gen-ext-hhvm.sh

--- hphp/tools/bootstrap/gen-ext-hhvm.sh
+++ hphp/tools/bootstrap/gen-ext-hhvm.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-# $1 Machine architecture (currently: "x64" or "arm")
-# $2 Object file to read symbols from
-# $3 ext_hhvm.cpp source to generate
-# $4 ext_hhvm.h header to generate
-if [ "$OSTYPE" = "darwin"* ]; then
-  gobjdump --section=".text" --section="LC_SEGMENT..text.hot.built." -t $2 | \
+# $1 Operating System (currently: "linux" or "darwin")
+# $2 Machine architecture (currently: "x64" or "arm")
+# $3 Object file to read symbols from
+# $4 ext_hhvm.cpp source to generate
+# $5 ext_hhvm.h header to generate
+if [ "$1" = "darwin" ]; then
+  gobjdump --section=".text" --section="LC_SEGMENT..text.hot.built." -t $3 | \
       awk '{ if ($2=="g") print $8 }' | \
       $HPHP_HOME/hphp/tools/bootstrap/gen-ext-hhvm \
-      $1 $4 $3 $HPHP_HOME/hphp/system/idl/*.idl.json
+      $2 $5 $4 $HPHP_HOME/hphp/system/idl/*.idl.json
 else
-  readelf -s -W $2 | grep 'FUNC.*GLOBAL' | \
+  readelf -s -W $3 | grep 'FUNC.*GLOBAL' | \
       sed -e 's/^.*DEFAULT[0-9 ]*//' | \
       $HPHP_HOME/hphp/tools/bootstrap/gen-ext-hhvm \
-      $1 $4 $3 $HPHP_HOME/hphp/system/idl/*.idl.json
+      $2 $5 $4 $HPHP_HOME/hphp/system/idl/*.idl.json
 fi

fb9f1405cee1066ae5592b79ceb9b4b0238691aa
Mike Magruder <mikemag@fb.com>
2013-07-25 16:30:47 -0700

    Remove ctrl-c test from debugger server test
    We can never make the ctrl-c portion of the debugger server test 100% reliable. Removing that portion of the test. I'll replace it with something else soon. See task 2656173.

--- hphp/test/server/debugger/tests/runTest1.php
+++ hphp/test/server/debugger/tests/runTest1.php
@@ -9,13 +9,6 @@ function test1Controller($hphpdOutput, $hphpdProcessId, $serverPort) {
   echo "Requesting test1.php\n";
   request($url, 10); // proceed without waiting for a response
 
-  // Send ctrl-c to client, which is waiting for a hung server
-  waitForClientToOutput($hphpdOutput, "Break at cls::pubHardBreak()", $url);
-  waitForClientToOutput($hphpdOutput, "Waiting for server response");
-  sleep(2); // give server a chance to get itself in the enless loop
-  echo "sending SIGINT to the debugger client.\n";
-  posix_kill($hphpdProcessId, SIGINT);
-
   // Let client run until script quits
   waitForClientToOutput($hphpdOutput, "quit");
 }

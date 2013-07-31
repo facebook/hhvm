b0a8dde8de944536b335fb8997aa33176c62bf2f
Eric Caruso <zircean@fb.com>
2013-07-31 01:21:35 -0700

    Add pprof server
    hhvm will now respond to pprof requests. Start up a
    server, curl /hhprof/start to put in a request to profile, then
    fire pprof at /pprof/heap to get a heap profile.
    
    Differential Revision: D896906

--- hphp/runtime/base/program_functions.cpp
+++ hphp/runtime/base/program_functions.cpp
@@ -23,6 +23,7 @@
 #include "hphp/runtime/base/thread_init_fini.h"
 #include "hphp/runtime/base/code_coverage.h"
 #include "hphp/runtime/base/runtime_option.h"
+#include "hphp/runtime/base/pprof_server.h"
 #include "hphp/util/shared_memory_allocator.h"
 #include "hphp/runtime/server/pagelet_server.h"
 #include "hphp/runtime/server/xbox_server.h"
@@ -58,6 +59,7 @@
 #include <boost/program_options/positional_options.hpp>
 #include <boost/program_options/variables_map.hpp>
 #include <boost/program_options/parsers.hpp>
+#include <boost/make_shared.hpp>
 #include <libgen.h>
 #include <oniguruma.h>
 #include <signal.h>
@@ -660,6 +662,11 @@ static int start_server(const std::string &username) {
   // Create the HttpServer before any warmup requests to properly
   // initialize the process
   HttpServer::Server = HttpServerPtr(new HttpServer(sslCTX));
+
+  if (memory_profiling) {
+    Logger::Info("Starting up profiling server");
+    HeapProfileServer::Server = boost::make_shared<HeapProfileServer>();
+  }
 
   // If we have any warmup requests, replay them before listening for
   // real connections

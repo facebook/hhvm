279111962af05bad283ace33d82e04730533ac54
Eric Le Lay <eric.lelay@gmail.com>
2013-08-19 16:11:45 +0200

    reproduce issue stackoverflow

---
+++ hphp/test/slow/class_exists_overflow/ext_class_positive.php
@@ -0,0 +1,14 @@
+<?php
+function autoload($className) { 
+
+    $path = "./class_". str_replace("Core","",$className). ".php";
+
+    if (is_file($path)) {
+        require $path;
+	var_dump(class_exists('test')); 
+    }
+
+}
+spl_autoload_register('autoload');
+$foo = new testCore;
+?>

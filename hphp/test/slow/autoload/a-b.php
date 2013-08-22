72b3cc6629b05f74159100fa97895209648c72e8
Eric Le Lay <eric.lelay@gmail.com>
2013-08-22 14:38:37 +0200

    Create a-b.php

---
+++ hphp/test/slow/autoload/a-b.php
@@ -0,0 +1,19 @@
+<?php
+function autoload($className) { 
+
+    $path = "./class_". str_replace("Core","",$className). ".php";
+
+    if (is_file($path)) {
+        require $path;
+	
+    }
+}
+function __autoload($class_name) {
+    include "./class_".$class_name . '.php';
+}
+$a= new auto;
+var_dump(class_exists('auto')); 
+spl_autoload_register('autoload');
+$b = new testCore;
+var_dump(class_exists('test',false)); 
+?>

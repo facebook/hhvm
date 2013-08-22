aa4ef06107b5735f100b111dd483e122b06ef4e6
Eric Le Lay <eric.lelay@gmail.com>
2013-08-22 14:45:17 +0200

    Create a-in-b.php

---
+++ hphp/test/slow/autoload/a-in-b.php
@@ -0,0 +1,18 @@
+<?php
+function autoload($className) { 
+
+    $path = "./class_". str_replace("Core","",$className). ".php";
+
+    if (is_file($path)) {
+        require $path;
+	$a= new auto;
+	var_dump(class_exists('auto'));
+	var_dump(class_exists('test',false)); 
+    }
+}
+function __autoload($class_name) {
+    include "./class_".$class_name . '.php';
+}
+spl_autoload_register('autoload');
+$b = new testCore;
+?>

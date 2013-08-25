a197544a4c6b977f70717216ec76af8088189b32
Eric Le Lay <eric.lelay@gmail.com>
2013-08-25 15:58:42 +0200

    Create spl_autoload_register_class_private_array_string.php

---
+++ hphp/test/slow/autoload/spl_autoload_register_class_private_array_string.php
@@ -0,0 +1,17 @@
+<?php
+
+    class ClassAutoloader {
+        public function __construct() {
+            spl_autoload_register(array('ClassAutoloader', 'loader'));
+        }
+        private function loader($className) {
+            echo 'Trying to load ', $className, ' via ', __METHOD__, "()\n";
+            include './class_'.str_replace("Core","",$className) . '.php';
+        }
+    }
+
+    $autoloader = new ClassAutoloader();
+
+    $obj = new testCore();
+
+?>

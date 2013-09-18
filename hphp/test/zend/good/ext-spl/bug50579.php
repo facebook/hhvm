3b7f782882ec220aae23de614053566e94a1b083
Guilherme Blanco <guilhermeblanco@hotmail.com>
2013-09-18 09:41:47 -0700

    Implemented SPL RegexIterator
    Draft implementation of SPL RegexIterator
    
    Closes #504
    Closes #1061
    
    Reviewed By: @ptarjan
    
    Differential Revision: D972519
    
    Pulled By: @scannell

--- hphp/test/zend/good/ext-spl/bug50579.php
+++ hphp/test/zend/good/ext-spl/bug50579.php
@@ -3,8 +3,8 @@
 class foo extends ArrayIterator {
 	public function __construct( ) {
 		parent::__construct(array(
-		'test1'=>'test888', 
-		'test2'=>'what?', 
+		'test1'=>'test888',
+		'test2'=>'what?',
 		'test3'=>'test999'));
 	}
 }
@@ -14,7 +14,7 @@ $i->replacement = '[$0]';
 foreach ($i as $name=>$value) {
 	echo $name . '=>' . $value . "\n";
 }
-  
+
 $i->replacement = '$1';
 foreach ($i as $name=>$value) {
 	echo $name . '=>' . $value . "\n";

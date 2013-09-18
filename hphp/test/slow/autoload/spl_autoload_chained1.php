abdf85e13a5d4a6b17bded5a213e45bed07c6cfc
Yuval Hager <yhager@yhager.com>
2013-09-18 15:28:00 -0700

    removed include files so tests would run in RepoAuthoritative mode

--- hphp/test/slow/autoload/spl_autoload_chained1.php
+++ hphp/test/slow/autoload/spl_autoload_chained1.php
@@ -5,7 +5,25 @@ spl_autoload_register('my_autoload');
 function my_autoload($class) {
   var_dump($class);
   $test = class_exists($class);
-  include_once ("spl_autoload_" . strtolower($class) . '.inc');
+  if ($class != 'A')
+    $test = class_exists('A');
+  if ($class == 'A') {
+    class A {
+      public $var = 'class A';
+    };
+    $b = new B();
+  }
+  if ($class == 'B') {
+    class B {
+      public $var = 'class B';
+    };
+    $c = new C();
+  }
+  if ($class == 'C') {
+    class C {
+      public $var = 'class C';
+    };
+  }
 }
 
 $a = new A();
--- hphp/test/slow/autoload/spl_autoload_chained2.php
+++ hphp/test/slow/autoload/spl_autoload_chained2.php
@@ -6,7 +6,23 @@ function my_autoload($class) {
   var_dump($class);
   if ($class == 'A')
     $test = class_exists('C');
-  include_once ("spl_autoload_" . strtolower($class) . '.inc');
+  if ($class == 'A') {
+    class A {
+      public $var = 'class A';
+    };
+    $b = new B();
+  }
+  if ($class == 'B') {
+    class B {
+      public $var = 'class B';
+    };
+    $c = new C();
+  }
+  if ($class == 'C') {
+    class C {
+      public $var = 'class C';
+    };
+  }
 }
 
 $a = new A();

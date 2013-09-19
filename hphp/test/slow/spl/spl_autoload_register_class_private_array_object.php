556e6c8f611e9c0e6841116e80ff43851d6b955e
Eric Le Lay <eric.lelay@gmail.com>
2013-09-19 12:50:41 +0200

    update test for to pass repo mode

--- hphp/test/slow/spl/spl_autoload_register_class_private_array_object.php
+++ hphp/test/slow/spl/spl_autoload_register_class_private_array_object.php
@@ -1,17 +1,18 @@
 <?php
-
+function test(){
+	class testCore {
+	  public function __construct() {var_dump("myClass"); }
+	}
+}
     class ClassAutoloader {
         public function __construct() {
             spl_autoload_register(array($this, 'loader'));
         }
         private function loader($className) {
-            echo 'Trying to load ', $className, ' via ', __METHOD__, "()\n";
-            include dirname(__FILE__).'/class_'.str_replace("Core","",$className) . '.php';
+		if($className == "testCore"){var_dump("myClass expect"); test();}
         }
     }
 
     $autoloader = new ClassAutoloader();
-
     $obj = new testCore();
-
 ?>

3b709c2a3b88baac146e3644d0b58cdebf59c8a2
Eric Le Lay <eric.lelay@gmail.com>
2013-09-26 14:11:02 +0200

    check subvert function depend on context Class
    cf scannel indication:
    It should be transparent but in your implementation, it isn't, even with version #2.
    
    Example: Let's say A::foo and B::private_bar are both registered autoload handlers. If I'm autoloading using A::foo, A::foo would still be able to directly call B::private_bar [not through autoload, as a direct method call] even though it is private because it is registered as an autoload handler.

---
+++ hphp/test/slow/spl/spl_autoload_subvert.php
@@ -0,0 +1,27 @@
+<?php
+    function test(){
+		class testCore {
+		  public function __construct() {var_dump("myClass"); }
+		}
+    }	
+    class A {
+        public function __construct() {var_dump("construct A");
+            spl_autoload_register(array($this, 'foo'));
+        }
+        private function foo($className) {echo 'Class A Trying to load ', $className, ' via ', __METHOD__, "()\n";
+		if($className == "testCore"){test();}
+		B::priv("zen");
+        }
+    }
+    class B {
+        public function __construct() {var_dump("construct B");
+            spl_autoload_register(array($this, 'priv'));
+        }
+        private function priv($className) {
+		echo 'Class B Trying to load ', $className, ' via ', __METHOD__, "()\n";
+        }
+    }
+    $autoloader_A = new A();
+    $autoloader_B = new B();
+    $obj = new testCore();
+?>

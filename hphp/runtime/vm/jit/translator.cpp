7f6e7e9cbe9aa6448b020a1ebbc72a7c4f46f0c6
aravind <aravind@fb.com>
2013-10-14 16:08:15 -0700

    Fixes for PGO mode
    This fixes a couple of asserts I hit running www in DEBUG mode with
    -vEval.JitPGO=1 -vEval.JitRegionSelector=hottrace
    -vEval.JitPGOHotOnly=0.
    
    Reviewed By: @ottoni
    
    Differential Revision: D1016284

--- hphp/runtime/vm/jit/translator.cpp
+++ hphp/runtime/vm/jit/translator.cpp
@@ -1376,7 +1376,7 @@ bool outputIsPredicted(SrcKey startSk,
     assert(iInfo.out == Stack1 || inst.op() == OpSetM);
     auto dt = predictOutputs(startSk, &inst);
     if (dt != KindOfAny) {
-      inst.outPred = Type(dt);
+      inst.outPred = Type(dt, dt == KindOfRef ? KindOfAny : KindOfNone);
     } else {
       doPrediction = false;
     }

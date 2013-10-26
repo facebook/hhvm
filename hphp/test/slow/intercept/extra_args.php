565465d580e5c501ca587410ed99a35f28b96b13
Jordan DeLong <jdelong@fb.com>
2013-10-26 13:44:48 -0700

    Fix slow/intercept tests to pass DynamicInvokeFunctions to compiler
    Modify one of the tests so it fails with the wrong options
    and pass the right ones, add a similar test for static member
    functions.  It appears DynamicInvokeFunction is ignored for member
    functions (but member functions are inspected for a "dyn_" prefix??),
    so I added norepo for the ones that do intercept on member functions.
    
    Reviewed By: @swtaarrs
    
    Differential Revision: D1029398

--- hphp/test/slow/intercept/extra_args.php
+++ hphp/test/slow/intercept/extra_args.php
@@ -2,18 +2,23 @@
 
 function foo() {
  var_dump('failed');
- }
+ return 12;
+}
 
 fb_intercept('foo', 'bar', "hello");
 
-function bar() {
+class lol { public function __destruct() { echo "lol\n"; } }
+function & bar() {
   var_dump(func_get_args());
-  return 25;
+  $x = new lol();
+  return $x;
 }
 
 function main() {
   var_dump(foo(1,2));
+  $l = foo(1,2);
 }
 
 main();
+echo "done\n";
 

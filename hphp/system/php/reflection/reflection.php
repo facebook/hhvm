715d7e00620a9792745b4dcdd6b655dfea05a93a
Paul Tarjan <ptarjan@fb.com>
2013-11-05 01:50:35 -0800

    Make ReflectionParameter not completely wrong
    @dariorussi what is this crazyness? Do you have a rewrite of this class? This is 100% insane.
    
    Reviewed By: @dariorussi
    
    Differential Revision: D1041883

--- hphp/system/php/reflection/reflection.php
+++ hphp/system/php/reflection/reflection.php
@@ -68,9 +68,31 @@ class ReflectionParameter implements Reflector {
    */
   public function __construct($func, $param) {
     if ($func && $param) {
-      $params = $func->getParameters();
-      $this->info = $params[$param]->info;
-      $this->name = $this->info['name'];
+      if (is_string($func)) {
+        $double_colon = strpos($func, "::");
+        if ($double_colon === false) {
+          $params = (new ReflectionFunction($func))->getParameters();
+        } else {
+          $class = substr($func, 0, $double_colon);
+          $method = substr($func, $double_colon + 2);
+          $params = (new ReflectionMethod($class, $method))->getParameters();
+        }
+      } else if (is_array($func)) {
+        $params = (new ReflectionMethod($func[0], $func[1]))->getParameters();
+      } else {
+        throw new Exception(
+          "Invalid function, expected string, got ".gettype($func)
+        );
+      }
+
+      foreach ($params as $p) {
+        if ($p->name == $param) {
+          $this->info = $p->info;
+          $this->name = $p->name;
+          return;
+        }
+      }
+      throw new Exception("No param named $param found");
     }
   }
 

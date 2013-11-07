44191bb097202334431f255915c9e6eec18badaa
rbelenov <roman.belenov@intel.com>
2013-11-07 11:25:30 +0400

    Fixed processing of code generated in stubs area

--- hphp/runtime/vm/jit/vtune-jit.cpp
+++ hphp/runtime/vm/jit/vtune-jit.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <algorithm>
 
 #include "vtune-jit.h"
 #include "jitapi/jitprofiling.h"
@@ -26,42 +27,71 @@ void reportTraceletToVtune(const Unit* unit, const Func* func, const TransRec& t
 
     methodInfo.source_file_name = const_cast<char *>(unit->filepath()->data());
 
+    // aStart field of transRec.bcmapping may point to stubs range, so we need to explicitly
+    // form mappings for main code and stubs
+
+    size_t bcSize = transRec.bcMapping.size();
+    std::vector<LineNumberInfo> mainLineMap, stubsLineMap;
+
+    for(size_t i = 0; i < bcSize; i++)
+    {
+        LineNumberInfo info;
+
+        info.LineNumber = unit->getLineNumber(transRec.bcMapping[i].bcStart);
+
+        if (transRec.bcMapping[i].aStart >= transRec.aStart &&
+            transRec.bcMapping[i].aStart < transRec.aStart + transRec.aLen)
+        {
+            info.Offset = transRec.bcMapping[i].aStart - transRec.aStart;
+            mainLineMap.push_back(info);
+        }
+        else if (transRec.bcMapping[i].aStart >= transRec.astubsStart &&
+            transRec.bcMapping[i].aStart < transRec.astubsStart + transRec.astubsLen)
+        {
+            info.Offset = transRec.bcMapping[i].aStart - transRec.astubsStart;
+            stubsLineMap.push_back(info);
+        }
+
+        info.Offset = transRec.bcMapping[i].astubsStart - transRec.astubsStart;
+        stubsLineMap.push_back(info);
+    }
+
+    auto infoComp = [&](const LineNumberInfo& a, const LineNumberInfo& b) -> bool {
+        return a.Offset < b.Offset;
+    };
+
+    std::sort(mainLineMap.begin(), mainLineMap.end(), infoComp);
+    std::sort(stubsLineMap.begin(), stubsLineMap.end(), infoComp);
+
+    // Note that at this moment LineNumberInfo structures contain pairs of lines and code offset
+    // for the start of the corresponding code, while JIT API treats the offset as the end of
+    // this code (and the start offset is taken from the previous element or is 0); need to
+    // shift the elements. Also, attribute the prologue (code before the first byte in the
+    // mapping) to the first line.
+
+    auto shiftLineMap = [&](std::vector<LineNumberInfo>& lineMap, unsigned regionSize) {
+        if (lineMap.size() > 0)
+        {
+            LineNumberInfo tmpInfo;
+            tmpInfo.Offset = regionSize;
+            tmpInfo.LineNumber = lineMap.back().LineNumber;
+            lineMap.push_back(tmpInfo);
+            for (size_t i = lineMap.size() - 2; i > 0; i--)
+            {
+                lineMap[i].LineNumber = lineMap[i - 1].LineNumber;
+            }
+        }
+    };
+
+    shiftLineMap(mainLineMap, transRec.aLen);
+    shiftLineMap(stubsLineMap, transRec.astubsLen);
+
     // Report main body
 
     methodInfo.method_load_address = transRec.aStart;
     methodInfo.method_size = transRec.aLen;
-
-    std::vector<LineNumberInfo> lineMapping;
-    
-    unsigned bcSize = transRec.bcMapping.size();
-    LineNumberInfo lineInfo;
-
-    // Attribute prologue to first bytecode; note that offset marks the
-    // *end* of corresponding line
-    if (bcSize > 0)
-    {
-        lineInfo.Offset = transRec.bcMapping[0].aStart - transRec.aStart;
-        lineInfo.LineNumber = unit->getLineNumber(transRec.bcMapping[0].bcStart);
-        lineMapping.push_back(lineInfo);
-    }
-    
-    for(unsigned i = 1; i < bcSize; i++)
-    {
-        lineInfo.Offset = transRec.bcMapping[i].aStart - transRec.aStart;
-        lineInfo.LineNumber = unit->getLineNumber(transRec.bcMapping[i - 1].bcStart);
-        lineMapping.push_back(lineInfo);
-    }
-
-    // Report the code until tracelet end to the line corresponding to last bytecode
-    if (bcSize > 0)
-    {
-        lineInfo.Offset = transRec.aLen;
-        lineInfo.LineNumber = unit->getLineNumber(transRec.bcMapping[bcSize - 1].bcStart);
-        lineMapping.push_back(lineInfo);
-    }
-
-    methodInfo.line_number_size = bcSize == 0 ? bcSize : bcSize + 1;
-    methodInfo.line_number_table = &lineMapping[0];
+    methodInfo.line_number_size = mainLineMap.size();
+    methodInfo.line_number_table = &mainLineMap[0];
 
     iJIT_NotifyEvent(iJVM_EVENT_TYPE_METHOD_LOAD_FINISHED, (void *)&methodInfo);
 
@@ -69,30 +99,8 @@ void reportTraceletToVtune(const Unit* unit, const Func* func, const TransRec& t
 
     methodInfo.method_load_address = transRec.astubsStart;
     methodInfo.method_size = transRec.astubsLen;
-    lineMapping.clear();
-
-    if (bcSize > 0)
-    {
-        lineInfo.Offset = transRec.bcMapping[0].astubsStart - transRec.astubsStart;
-        lineInfo.LineNumber = unit->getLineNumber(transRec.bcMapping[0].bcStart);
-        lineMapping.push_back(lineInfo);
-    }
-    
-    for(unsigned i = 1; i < bcSize; i++)
-    {
-        lineInfo.Offset = transRec.bcMapping[i].astubsStart - transRec.astubsStart;
-        lineInfo.LineNumber = unit->getLineNumber(transRec.bcMapping[i - 1].bcStart);
-        lineMapping.push_back(lineInfo);
-    }
-
-    if (bcSize > 0)
-    {
-        lineInfo.Offset = transRec.astubsLen;
-        lineInfo.LineNumber = unit->getLineNumber(transRec.bcMapping[bcSize - 1].bcStart);
-        lineMapping.push_back(lineInfo);
-    }
-
-    methodInfo.line_number_table = &lineMapping[0];
+    methodInfo.line_number_size = stubsLineMap.size();
+    methodInfo.line_number_table = &stubsLineMap[0];
 
     iJIT_NotifyEvent(iJVM_EVENT_TYPE_METHOD_LOAD_FINISHED, (void *)&methodInfo);
 }

a596e67ce070996ab70bc129958f4ed0f7303e53
seanc <seanc@fb.com>
2013-11-08 09:36:52 -0800

    Add libboost-thread to Travis dependencies
    Summary: With the fastcgi implementation we now depend on
    libboost-thread so add it to the list of Travis dependencies.

--- configure_ubuntu_12.04.sh
+++ configure_ubuntu_12.04.sh
@@ -24,7 +24,7 @@ export HPHP_HOME=`/bin/pwd`
 
 # install apt-fast to speedup later dependency installation
 sudo add-apt-repository -y ppa:apt-fast/stable
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-get -y install apt-fast
 
 # install the actual dependencies
@@ -33,8 +33,8 @@ sudo apt-fast -y install git-core cmake g++ libboost1.48-dev libmysqlclient-dev 
   libxml2-dev libmcrypt-dev libicu-dev openssl build-essential binutils-dev \
   libcap-dev libgd2-xpm-dev zlib1g-dev libtbb-dev libonig-dev libpcre3-dev \
   autoconf libtool libcurl4-openssl-dev libboost-regex1.48-dev libboost-system1.48-dev \
-  libboost-program-options1.48-dev libboost-filesystem1.48-dev wget memcached \
-  libreadline-dev libncurses-dev libmemcached-dev libbz2-dev \
+  libboost-program-options1.48-dev libboost-filesystem1.48-dev libboost-thread1.48-dev \
+  wget memcached libreadline-dev libncurses-dev libmemcached-dev libbz2-dev \
   libc-client2007e-dev php5-mcrypt php5-imagick libgoogle-perftools-dev \
   libcloog-ppl0 libelf-dev libdwarf-dev libunwind7-dev subversion \
   python-software-properties &

df859438ee3bd727ca74b70c2cf5efb5482b7b17
Paul Tarjan <ptarjan@fb.com>
2013-11-15 11:05:40 -0800

    update laravel hash
    They took 2 of my fixes upstream.
    
    Reviewed By: @JoelMarcey
    
    Differential Revision: D1058397

--- hphp/test/frameworks/run.php
+++ hphp/test/frameworks/run.php
@@ -1031,7 +1031,7 @@ class Laravel extends Framework {
     return Map {
       "install_root" => __DIR__."/frameworks/laravel",
       "git_path" => "https://github.com/laravel/framework.git",
-      "git_commit" => "6ea8d8b5b3c921e9fe02bfafa44d2601d206ed6e",
+      "git_commit" => "f85efd4d16837d8fcac11aeb5e7d0977d295fb6b",
       "test_file_search_roots" => Set {
         __DIR__."/frameworks/laravel/tests",
       }

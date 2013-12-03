d9b4ee69476aec563221f5e64beb2d373bc0d45c
Andrei Alexandrescu <aalexandre@fb.com>
2013-12-03 10:00:54 -0800

    Fix for code smell reported by Robert Henry
    Robert, an old friend, found a bug in our code.
    
    Reviewed By: @markw65
    
    Differential Revision: D1080348

--- hphp/runtime/ext/ext_memcache.cpp
+++ hphp/runtime/ext/ext_memcache.cpp
@@ -58,7 +58,7 @@ IMPLEMENT_STATIC_REQUEST_LOCAL(MEMCACHEGlobals, s_memcache_globals);
 bool ini_on_update_hash_strategy(const String& value, void *p) {
   if (!strncasecmp(value.data(), "standard", sizeof("standard"))) {
     MEMCACHEG(hash_strategy) = "standard";
-  } else if (!strncasecmp(value.data(), "standard", sizeof("consistent"))) {
+  } else if (!strncasecmp(value.data(), "consistent", sizeof("consistent"))) {
     MEMCACHEG(hash_strategy) = "consistent";
   } else {
     return false;

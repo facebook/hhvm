c3a9cea6408cb8b195e8a8082d2f7e0ebd68a032
Jordan DeLong <jdelong@fb.com>
2014-01-03 19:46:48 -0800

    Fix bad_ini_quotes test
    Oops.
    
    Reviewed By: @ptarjan
    
    Differential Revision: D1116602

--- hphp/test/slow/ext_file/bad_ini_quotes.php
+++ hphp/test/slow/ext_file/bad_ini_quotes.php
@@ -1,8 +1,8 @@
 <?php
 
 function main() {
-  $path = __DIR__.'bad_ini_quotes.ini';
-  $x = file_get_contents('test.ini');
+  $path = __DIR__.'/bad_ini_quotes.ini';
+  $x = file_get_contents($path);
   $y = parse_ini_string($x, true);
   var_dump($y);
 }

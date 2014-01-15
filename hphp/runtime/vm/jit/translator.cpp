eb8f8f3825bc815c6a649b3623d3f7dd147cd3c8
Jordan DeLong <jdelong@fb.com>
2014-01-14 16:27:40 -0800

    Fix several issues with type assert opcodes
    Type assertions were allocating new DynLocations every time
    we encountered them, which breaks getOutputUsage (it uses DynLocation
    pointer equality to track the flow of values).  When encountering
    assertions, it also wasn't doing the optimization that we have in
    applyInputMetaData to eliminate earlier predictions on the same
    location.  Finally, this diff finishes the support for optional object
    types (fixes inlining and object property accesses in these
    situations).
    
    Reviewed By: @swtaarrs
    
    Differential Revision: D1128602

--- hphp/runtime/vm/jit/translator.cpp
+++ hphp/runtime/vm/jit/translator.cpp
@@ -1501,45 +1501,23 @@ void Translator::handleAssertionEffects(Tracelet& t,
   }();
   if (loc.isInvalid()) return;
 
-  auto const rt = [&]() -> folly::Optional<RuntimeType> {
+  auto const assertTy = [&]() -> folly::Optional<RuntimeType> {
     if (ni.op() == Op::AssertObjStk || ni.op() == Op::AssertObjL) {
-      if (isNullableAssertObj(ni)) {
-        /*
-         * To handle these properly, we'd need to actually record a
-         * read of the live type, so it will be guarded on.  (Whether
-         * we can provide any more useful information depends on first
-         * guarding that it's not-null.)
-         *
-         * If you try to just do that right here, guard relaxation
-         * will always throw away the dependency, because no
-         * instructions will have it as an input.
-         *
-         * So for now, we try to make use of this extra info at JIT
-         * time but not for tracelet formation.
-         *
-         * Note, this causes inlining not to work on these functions:
-         * TODO(#3520763).
-         */
-        return folly::none;
-      }
-
       /*
        * Even though the class must be defined at the point of the
-       * AssertObj, we might not have defined it yet in this tracelet,
-       * or it might not be unique.  For now just restrict this to
-       * unique classes (we could also check parent of current
-       * context).
+       * AssertObj (unless it's optional), we might not have defined
+       * it yet in this tracelet, or it might not be unique.  For now
+       * just restrict this to unique classes (we could also check
+       * parent of current context).
        *
-       * There's nothing we can do with the 'exact' bit right now.
-       * We've already bailed if it was nullable above.
+       * There's nothing we can do with the 'exact' bit right now,
+       * since neither analyze() nor the IR typesystem tracks that.
        */
       auto const cls = Unit::lookupUniqueClass(
         ni.m_unit->lookupLitstrId(ni.imm[1].u_SA)
       );
-      if (cls && (cls->attrs() & AttrUnique)) {
-        return RuntimeType{KindOfObject, KindOfNone, cls};
-      }
-      return folly::none;
+      if (!cls || !(cls->attrs() & AttrUnique)) return folly::none;
+      return RuntimeType{KindOfObject, KindOfNone, cls};
     }
 
     switch (static_cast<AssertTOp>(ni.imm[1].u_OA)) {
@@ -1594,43 +1572,107 @@ void Translator::handleAssertionEffects(Tracelet& t,
     }
     not_reached();
   }();
-  if (!rt) return;
+  if (!assertTy) return;
 
-  auto const dl = t.newDynLocation(loc, *rt);
+  FTRACE(1, "examining assertion for {}\n", loc.pretty());
 
-  // No need for m_resolvedDeps---because we're in the bytecode stream
-  // we don't need to tell hhbc-translator about it out of band.
-  auto& curVal = tas.m_currentMap[dl->location];
-  if (curVal && !curVal->rtt.isVagueValue()) {
-    if (curVal->rtt.outerType() != dl->rtt.outerType()) {
+  /*
+   * Nullable object assertions are slightly special: we have extra
+   * information, but it's conditional on previously guarding that the
+   * object wasn't null.  And if it is null in this tracelet, the
+   * assertion should have no effects.
+   *
+   * We need to do a recordRead without setting dontGuard, so we'll
+   * still leave a guard.  (Guard relaxation will remove it if nothing
+   * ends up using it.)
+   *
+   * We also shouldn't try to remove predictions like the code below:
+   * a prediction guard might be the reason we know it's not null
+   * here.
+   */
+  if (isNullableAssertObj(ni)) {
+    auto const dl = tas.recordRead(InputInfo{loc});
+    if (dl->rtt.isNull()) {
+      FTRACE(1, "assertion leaving live KindOfNull alone for ?Obj type\n");
+      return;
+    }
+    if (!dl->isObject()) {
+      // We're about to fail a VerifyParamType or something similar.
+      FTRACE(1, "not adjusting {} since it's not an object\n",
+             dl->pretty());
+      return;
+    }
+    dl->rtt = *assertTy;
+    return;
+  }
+
+  InputInfo ii{loc};
+  ii.dontGuard = true;
+  auto const dl = tas.recordRead(ii, assertTy->outerType());
+
+  if (dl->rtt.outerType() != assertTy->outerType() &&
+      !(dl->rtt.isString() && assertTy->isString())) {
+    if (!dl->rtt.isVagueValue()) {
       /*
-       * The tracked type disagrees with ahead of time analysis.  A
-       * similar case occurs in applyInputMetaData.
+       * The live or tracked type disagrees with ahead of time analysis.
+       * A similar case occurs in applyInputMetaData.
        *
-       * Either static analysis is wrong, this was a mispredicted type
-       * from warmup profiling, or the code is unreachable because we're
-       * about to fatal (e.g. a VerifyParamType is about to throw).
-       *
-       * Punt this opcode to end the trace.
+       * Either static analysis is wrong (bug), this was a mispredicted
+       * type from warmup profiling, or the code is unreachable because
+       * we're about to fatal earlier in this tracelet.  (e.g. a
+       * VerifyParamType is about to throw.)
        */
+      if (tas.m_changeSet.count(dl->location)) {
+        auto const src = findInputSrc(tas.m_t->m_instrStream.last, dl);
+        if (src && src->outputPredicted) {
+          FTRACE(1, "correcting mispredicted type for {}\n", loc.pretty());
+          src->outputPredicted = false;
+          return;
+        }
+      }
       FTRACE(1, "punting for {}\n", loc.pretty());
       punt();
     }
+  } else {
+    /*
+     * Static inference confirmed a type we already have, but if the
+     * type we have came from profiling we want to clear outputPredicted
+     * to avoid unnecessary guards.
+     */
+    if (tas.m_changeSet.count(dl->location)) {
+      auto const src = findInputSrc(tas.m_t->m_instrStream.last, dl);
+      if (src->outputPredicted) src->outputPredicted = false;
+    }
+  }
 
-    auto const isSpecializedObj =
-      rt->outerType() == KindOfObject && rt->valueClass();
-    if (!isSpecializedObj || curVal->rtt.valueClass()) {
-      // Otherwise, we may have more information in the curVal
-      // RuntimeType than would come from the AssertT if we were
-      // tracking a literal value or something.
-      FTRACE(1, "assertion leaving curVal alone {}\n", curVal->pretty());
+  /*
+   * The valueClass fields we track on KindOfObject types basically
+   * all come from static analysis, but they can also come from being
+   * inside of an analyzeCallee situation and knowing more specific
+   * argument types.  It's also possible in principle for a tracelet
+   * to extend through a join point, and thereby propagate a better
+   * type than one of the type assertions we get from static analysis.
+   *
+   * So, sometimes we may know a more derived type here dynamically,
+   * in which case we don't want to modify the RuntimeType and make it
+   * worse.
+   */
+  auto const assertIsSpecializedObj =
+    assertTy->isObject() && assertTy->valueClass() != nullptr;
+  auto const liveIsSpecializedObj =
+    dl->rtt.isObject() && dl->rtt.valueClass() != nullptr;
+  if (assertIsSpecializedObj && liveIsSpecializedObj) {
+    auto const assertCls = assertTy->valueClass();
+    auto const liveCls = dl->rtt.valueClass();
+    if (assertCls != liveCls && !assertCls->classof(liveCls)) {
+      // liveCls must be more specialized than assertCls.
+      FTRACE(1, "assertion leaving curVal alone {}\n", loc.pretty());
       return;
     }
   }
-  FTRACE(1, "assertion effects {} -> {}\n",
-    curVal ? curVal->pretty() : std::string{},
-    dl->pretty());
-  curVal = dl;
+
+  FTRACE(1, "assertion effects {} -> {}\n", loc.pretty(), assertTy->pretty());
+  dl->rtt = *assertTy;
 }
 
 bool Translator::applyInputMetaData(Unit::MetaHandle& metaHand,
@@ -3003,8 +3045,9 @@ void Translator::constrainDep(const DynLocation* loc,
   if (relxType.isEqual(specType)) return; // can't contrain it any further
 
   FTRACE(3, "\nconstraining dep {}\n", loc->pretty());
-  for (NormalizedInstruction* instr = firstInstr; instr; instr = instr->next) {
+  for (auto instr = firstInstr; instr; instr = instr->next) {
     if (instr->noOp) continue;
+
     auto opc = instr->op();
     size_t nInputs = instr->inputs.size();
     for (size_t i = 0; i < nInputs; i++) {

facebook/hhvm

commit 04b77176fe0e17e4885c6465fcd98cccaf50d4a4

Fix sockopen_impl with IPv6 addresses
sockopen_impl requires IPv6 addresses be enclosed in brackets.

Differential Revision: D927309

hphp/runtime/ext/ext_socket.cpp
@@ -19,6 +19,7 @@
 #include "hphp/runtime/base/socket.h"
 #include "hphp/runtime/base/ssl-socket.h"
 #include "hphp/runtime/server/server-stats.h"
+#include "hphp/util/logger.h"
 #include "folly/String.h"
 
 #include <sys/types.h>
@@ -287,22 +288,21 @@ static int php_read(Socket *sock, void *buf, int maxlen, int flags) {
   return n;
 }
 
-static bool create_new_socket(const char *&name, int port, Variant &errnum,
-                              Variant &errstr, Resource &ret, Socket *&sock,
-                              double timeout) {
-  int domain = AF_INET;
+static bool create_new_socket(const Util::HostURL &hosturl,
+                              Variant &errnum, Variant &errstr, Resource &ret,
+                              Socket *&sock, double timeout) {
+  int domain = hosturl.isIPv6() ? AF_INET6 : AF_INET;
   int type = SOCK_STREAM;
-  if (strncmp(name, "udp://", 6) == 0 || strncmp(name, "udg://", 6) == 0) {
+  const std::string scheme = hosturl.getScheme();
+
+  if (scheme == "udp" || scheme == "udg") {
     type = SOCK_DGRAM;
-    name += 6;
-  } else if (strncmp(name, "tcp://", 6) == 0) {
-    name += 6;
-  } else if (strncmp(name, "unix://", 7) == 0) {
+  } else if (scheme == "unix") {
     domain = AF_UNIX;
-    name += 7;
   }
 
-  sock = new Socket(socket(domain, type, 0), domain, name, port, timeout);
+  sock = new Socket(socket(domain, type, 0), domain,
+                    hosturl.getHost().c_str(), hosturl.getPort(), timeout);
   ret = Resource(sock);
   if (!sock->valid()) {
     SOCKET_ERROR(sock, "unable to create socket", errno);
@@ -670,10 +670,16 @@ Variant f_socket_select(VRefParam read, VRefParam write, VRefParam except,
 Variant f_socket_server(CStrRef hostname, int port /* = -1 */,
                         VRefParam errnum /* = null */,
                         VRefParam errstr /* = null */) {
+  Util::HostURL hosturl(static_cast<const std::string>(hostname), port);
+  return socket_server_impl(hosturl, errnum, errstr);
+}
+
+Variant socket_server_impl(const Util::HostURL &hosturl,
+                           VRefParam errnum /* = null */,
+                           VRefParam errstr /* = null */) {
   Resource ret;
   Socket *sock = NULL;
-  const char *name = hostname.data();
-  if (!create_new_socket(name, port, errnum, errstr, ret, sock, 0.0)) {
+  if (!create_new_socket(hosturl, errnum, errstr, ret, sock, 0.0)) {
     return false;
   }
   assert(ret.get() && sock);
@@ -681,7 +687,8 @@ Variant f_socket_server(CStrRef hostname, int port /* = -1 */,
   sockaddr_storage sa_storage;
   struct sockaddr *sa_ptr;
   size_t sa_size;
-  if (!set_sockaddr(sa_storage, sock, name, port, sa_ptr, sa_size)) {
+  if (!set_sockaddr(sa_storage, sock, hosturl.getHost().c_str(),
+                    hosturl.getPort(), sa_ptr, sa_size)) {
     return false;
   }
   if (::bind(sock->fd(), sa_ptr, sa_size) < 0) {
@@ -980,18 +987,15 @@ void f_socket_clear_error(CResRef socket /* = null_object */) {
     sock->setError(0);
   }
 }
-
 ///////////////////////////////////////////////////////////////////////////////
 // fsock: treating sockets as "file"
 
-static Variant sockopen_impl(CStrRef hostname, int port, Variant &errnum,
-                             Variant &errstr, double timeout,
-                             bool persistent) {
+Variant sockopen_impl(const Util::HostURL &hosturl, VRefParam errnum,
+                      VRefParam errstr, double timeout, bool persistent) {
+
   string key;
   if (persistent) {
-    key = hostname.data();
-    key += ":";
-    key += boost::lexical_cast<string>(port);
+    key = hosturl.getHostURL();
     Socket *sock =
       dynamic_cast<Socket*>(g_persistentObjects->get("socket", key.c_str()));
     if (sock) {
@@ -1006,17 +1010,15 @@ static Variant sockopen_impl(CStrRef hostname, int port, Variant &errnum,
   }
 
   Resource ret;
-  const char *name = hostname.data();
   Socket *sock = NULL;
 
   if (timeout <= 0) timeout = RuntimeOption::SocketDefaultTimeout;
   // test if protocol is SSL
-  SSLSocket *sslsock = SSLSocket::Create(name, port, timeout);
+  SSLSocket *sslsock = SSLSocket::Create(hosturl, timeout);
   if (sslsock) {
     sock = sslsock;
     ret = sock;
-  } else if (!create_new_socket(name, port, errnum, errstr,
-                                ret, sock, timeout)) {
+  } else if (!create_new_socket(hosturl, errnum, errstr, ret, sock, timeout)) {
     return false;
   }
   assert(ret.get() && sock);
@@ -1024,13 +1026,15 @@ static Variant sockopen_impl(CStrRef hostname, int port, Variant &errnum,
   sockaddr_storage sa_storage;
   struct sockaddr *sa_ptr;
   size_t sa_size;
-  if (!set_sockaddr(sa_storage, sock, name, port, sa_ptr, sa_size)) {
+  if (!set_sockaddr(sa_storage, sock, hosturl.getHost().c_str(),
+                    hosturl.getPort(), sa_ptr, sa_size)) {
     return false;
   }
 
   int retval;
   int fd = sock->fd();
-  IOStatusHelper io("socket::connect", name, port);
+  IOStatusHelper io("socket::connect",
+                    hosturl.getHostURL().c_str(), hosturl.getPort());
   if (timeout <= 0) {
     retval = connect(fd, sa_ptr, sa_size);
   } else {
@@ -1049,10 +1053,7 @@ static Variant sockopen_impl(CStrRef hostname, int port, Variant &errnum,
           int valopt;
           getsockopt(fd, SOL_SOCKET, SO_ERROR, (void*)(&valopt), &lon);
           if (valopt) {
-            std::string msg = "failed to connect to ";
-            msg += name;
-            msg += ":";
-            msg += boost::lexical_cast<std::string>(port);
+            std::string msg = "failed to connect to " + hosturl.getHostURL();
             SOCKET_ERROR(sock, msg.c_str(), valopt);
             errnum = sock->getError();
             errstr = String(folly::errnoStr(sock->getError()).toStdString());
@@ -1063,10 +1064,7 @@ static Variant sockopen_impl(CStrRef hostname, int port, Variant &errnum,
         } else {
           std::string msg = "timed out after ";
           msg += boost::lexical_cast<std::string>(timeout);
-          msg += " seconds when connecting to ";
-          msg += name;
-          msg += ":";
-          msg += boost::lexical_cast<std::string>(port);
+          msg += " seconds when connecting to " + hosturl.getHostURL();
           SOCKET_ERROR(sock, msg.c_str(), ETIMEDOUT);
           errnum = sock->getError();
           errstr = String(folly::errnoStr(sock->getError()).toStdString());
@@ -1103,7 +1101,8 @@ Variant f_fsockopen(CStrRef hostname, int port /* = -1 */,
                     VRefParam errnum /* = null */,
                     VRefParam errstr /* = null */,
                     double timeout /* = 0.0 */) {
-  return sockopen_impl(hostname, port, errnum, errstr, timeout, false);
+  Util::HostURL hosturl(static_cast<const std::string>(hostname), port);
+  return sockopen_impl(hosturl, errnum, errstr, timeout, false);
 }
 
 Variant f_pfsockopen(CStrRef hostname, int port /* = -1 */,
@@ -1111,7 +1110,8 @@ Variant f_pfsockopen(CStrRef hostname, int port /* = -1 */,
                      VRefParam errstr /* = null */,
                      double timeout /* = 0.0 */) {
   // TODO: persistent socket handling
-  return sockopen_impl(hostname, port, errnum, errstr, timeout, true);
+  Util::HostURL hosturl(static_cast<const std::string>(hostname), port);
+  return sockopen_impl(hosturl, errnum, errstr, timeout, true);
 }
 
 String ipaddr_convert(struct sockaddr *addr, int addrlen) {

hphp/runtime/base/ssl-socket.cpp
@@ -308,27 +308,27 @@ bool SSLSocket::handleError(int64_t nr_bytes, bool is_init) {
 
 ///////////////////////////////////////////////////////////////////////////////
 
-SSLSocket *SSLSocket::Create(const char *&name, int port, double timeout) {
+SSLSocket *SSLSocket::Create(const Util::HostURL &hosturl, double timeout) {
   CryptoMethod method;
-  if (strncmp(name, "ssl://", 6) == 0) {
-    name += 6;
+  const std::string scheme = hosturl.getScheme();
+
+  if (scheme == "ssl") {
     method = CryptoMethod::ClientSSLv23;
-  } else if (strncmp(name, "sslv2://", 8) == 0) {
-    name += 8;
+  } else if (scheme == "sslv2") {
     method = CryptoMethod::ClientSSLv2;
-  } else if (strncmp(name, "sslv3://", 8) == 0) {
-    name += 8;
+  } else if (scheme == "sslv3") {
     method = CryptoMethod::ClientSSLv3;
-  } else if (strncmp(name, "tls://", 6) == 0) {
-    name += 6;
+  } else if (scheme == "tls") {
     method = CryptoMethod::ClientTLS;
   } else {
     return nullptr;
   }
 
-  int domain = AF_INET;
+  int domain = hosturl.isIPv6() ? AF_INET6 : AF_INET;
   int type = SOCK_STREAM;
-  SSLSocket *sock = new SSLSocket(socket(domain, type, 0), domain, name, port);
+  SSLSocket *sock = new SSLSocket(socket(domain, type, 0), domain,
+                                  hosturl.getHost().c_str(),
+                                  hosturl.getPort());
   sock->m_method = method;
   sock->m_connect_timeout = timeout;
   sock->m_enable_on_connect = true;

hphp/runtime/base/ssl-socket.h
@@ -19,6 +19,7 @@
 
 #include "hphp/runtime/base/socket.h"
 #include "hphp/util/lock.h"
+#include "hphp/util/network.h"
 #include "openssl/ssl.h"
 #include "openssl/x509.h"
 #include "openssl/err.h"
@@ -44,7 +45,7 @@ public:
   };
 
   static int GetSSLExDataIndex();
-  static SSLSocket *Create(const char *&name, int port, double timeout);
+  static SSLSocket *Create(const Util::HostURL &hosturl, double timeout);
 
 public:
   SSLSocket();

hphp/runtime/ext/ext_network.h
@@ -20,6 +20,7 @@
 
 #include "hphp/runtime/base/base-includes.h"
 #include "hphp/runtime/ext/ext_stream.h"
+#include "hphp/util/network.h"
 #include <syslog.h>
 
 namespace HPHP {
@@ -53,10 +54,15 @@ bool f_getmxrr(CStrRef hostname, VRefParam mxhosts,
 ///////////////////////////////////////////////////////////////////////////////
 // socket
 
-Variant f_fsockopen(CStrRef hostname, int port = -1, VRefParam errnum = uninit_null(),
+Variant sockopen_impl(const Util::HostURL &hosturl,
+                      VRefParam errnum, VRefParam errstr,
+                      double timeout = 0.0, bool persistent = false);
+Variant f_fsockopen(CStrRef hostname, int port = -1,
+                    VRefParam errnum = uninit_null(),
                     VRefParam errstr = uninit_null(), double timeout = 0.0);
 
-Variant f_pfsockopen(CStrRef hostname, int port = -1, VRefParam errnum = uninit_null(),
+Variant f_pfsockopen(CStrRef hostname, int port = -1,
+                     VRefParam errnum = uninit_null(),
                      VRefParam errstr = uninit_null(), double timeout = 0.0);
 
 Variant f_socket_get_status(CResRef stream);

hphp/runtime/ext/ext_socket.h
@@ -19,6 +19,7 @@
 #define incl_HPHP_EXT_SOCKET_H_
 
 #include "hphp/runtime/base/base-includes.h"
+#include "hphp/util/network.h"
 
 namespace HPHP {
 ///////////////////////////////////////////////////////////////////////////////
@@ -40,8 +41,12 @@ bool f_socket_bind(CResRef socket, CStrRef address, int port = 0);
 bool f_socket_listen(CResRef socket, int backlog = 0);
 Variant f_socket_select(VRefParam read, VRefParam write, VRefParam except,
                         CVarRef vtv_sec, int tv_usec = 0);
-Variant f_socket_server(CStrRef hostname, int port = -1, VRefParam errnum = uninit_null(),
+Variant f_socket_server(CStrRef hostname, int port = -1,
+                        VRefParam errnum = uninit_null(),
                         VRefParam errstr = uninit_null());
+Variant socket_server_impl(const Util::HostURL &hosturl,
+                           VRefParam errnum = uninit_null(),
+                           VRefParam errstr = uninit_null());
 Variant f_socket_accept(CResRef socket);
 Variant f_socket_read(CResRef socket, int length, int type = 0);
 Variant f_socket_write(CResRef socket, CStrRef buffer, int length = 0);

hphp/runtime/ext/ext_stream.cpp
@@ -26,6 +26,7 @@
 #include "hphp/runtime/base/stream-wrapper.h"
 #include "hphp/runtime/base/stream-wrapper-registry.h"
 #include "hphp/runtime/base/user-stream-wrapper.h"
+#include "hphp/util/network.h"
 #include <memory>
 #include <unistd.h>
 #include <fcntl.h>
@@ -316,30 +317,30 @@ bool f_stream_wrapper_unregister(CStrRef protocol) {
 ///////////////////////////////////////////////////////////////////////////////
 // stream socket functions
 
+// Extract host:port pair.
+// 192.168.1.1:80 -> 192.168.1.0 80
+// [2a03:2880::1]:80 -> [2a03:2880::1] 80
 static void parse_host(CStrRef address, String &host, int &port) {
-  int pos = address.find(':');
-  if (pos >= 0) {
-    host = address.substr(0, pos);
-    port = address.substr(pos + 1).toInt16();
-  } else {
-    host = address;
-    port = 0;
-  }
-}
 
-static void parse_socket(CStrRef socket, String &protocol, String &host,
-                         int &port) {
-  String address;
-  int pos = socket.find("://");
-  if (pos >= 0) {
-    protocol = socket.substr(0, pos);
-    address = socket.substr(pos + 3);
+  if (address.find('[') != std::string::npos) {
+    auto epos = address.rfind(']');
+    if (epos != std::string::npos) {
+      auto cpos = address.rfind(':', epos);
+      if (cpos != std::string::npos) {
+        port = address.substr(cpos + 1).toInt16();
+      }
+    }
+    host = address.substr(0, epos + 1);
   } else {
-    protocol = "tcp";
-    address = socket;
+    auto cpos = address.rfind(':');
+    if (cpos != std::string::npos) {
+      host = address.substr(0, cpos);
+      port = address.substr(cpos + 1).toInt16();
+    } else {
+      host = address;
+      port = 0;
+    }
   }
-
-  parse_host(address, host, port);
 }
 
 static Socket *socket_accept_impl(CResRef socket, struct sockaddr *addr,
@@ -439,9 +440,8 @@ Variant f_stream_socket_server(CStrRef local_socket,
                                VRefParam errstr /* = null */,
                                int flags /* = 0 */,
                                CResRef context /* = null_object */) {
-  String protocol, host; int port;
-  parse_socket(local_socket, protocol, host, port);
-  return f_socket_server(protocol + "://" + host, port, errnum, errstr);
+  Util::HostURL hosturl(static_cast<const std::string>(local_socket));
+  return socket_server_impl(hosturl, errnum, errstr);
 }
 
 Variant f_stream_socket_client(CStrRef remote_socket,
@@ -450,9 +450,8 @@ Variant f_stream_socket_client(CStrRef remote_socket,
                                double timeout /* = 0.0 */,
                                int flags /* = 0 */,
                                CResRef context /* = null_object */) {
-  String protocol, host; int port;
-  parse_socket(remote_socket, protocol, host, port);
-  return f_fsockopen(protocol + "://" + host, port, errnum, errstr, timeout);
+  Util::HostURL hosturl(static_cast<const std::string>(remote_socket));
+  return sockopen_impl(hosturl, errnum, errstr, timeout, false);
 }
 
 Variant f_stream_socket_enable_crypto(CResRef stream, bool enable,

hphp/test/slow/ext_network/ext_network.php
@@ -11,6 +11,29 @@ function tryopen($u, $p) {
   return $r;
 }
 
+function tryopen_noport($u) {
+  for ($i = 0; $i < 10; $i++) {
+    $r = fsockopen($u);
+    if ($r) return $r;
+    sleep(1);
+  }
+  return $r;
+}
+
+function get_addresses($host) {
+  $r = array();
+  if (($records = dns_get_record($host))) {
+    foreach ($records as $record) {
+      if (isset($record['ipv6'])) {
+        $r []= '['.$record['ipv6'].']';
+      } else if (isset($record['ip'])) {
+        $r []= $record['ip'];
+      }
+    }
+  }
+  return $r;
+}
+
 var_dump(gethostname() != false);
 
 var_dump(strpos(gethostbyaddr("127.0.0.1"), 'localhost'));
@@ -67,6 +90,52 @@ while ($f && !feof($f)) {
 }
 var_dump(!empty($response));
 
+$addresses = get_addresses('www.facebook.com');
+$schemes = array('ssl://' => 443, 'tcp://' => 80);
+foreach ($addresses as $address) {
+  foreach ($schemes as $scheme => $port) {
+    $f = tryopen($scheme.$address, $port);
+    fwrite($f,
+      "GET / HTTP/1.1\r\n".
+      "Host: www.facebook.com\r\n".
+      "Connection: Close\r\n".
+      "\r\n");
+    $response = '';
+    while ($f && !feof($f)) {
+      $line = fgets($f, 128);
+      $response .= $line;
+    }
+    var_dump(!empty($response));
+
+    # Try appending a port
+    $f = tryopen_noport($scheme.$address.':'.$port);
+    fwrite($f,
+      "GET / HTTP/1.1\r\n".
+      "Host: www.facebook.com\r\n".
+      "Connection: Close\r\n".
+      "\r\n");
+    $response = '';
+    while ($f && !feof($f)) {
+      $line = fgets($f, 128);
+      $response .= $line;
+    }
+    var_dump(!empty($response));
+
+    # php expects anything following the ':' to over-ride the port passed in
+    $f = tryopen($scheme.$address.':'.$port, 12345);
+    fwrite($f,
+      "GET / HTTP/1.1\r\n".
+      "Host: www.facebook.com\r\n".
+      "Connection: Close\r\n".
+      "\r\n");
+    $response = '';
+    while ($f && !feof($f)) {
+      $line = fgets($f, 128);
+      $response .= $line;
+    }
+    var_dump(!empty($response));
+  }
+}
 var_dump(socket_get_status(new stdclass));
 
 $f = tryopen("facebook.com", 80);

commit 37bd7690c702baa9d4bf6e90ca33da97423812f3

Fix ambiguous key in g_persistentObjects for persistent connections
Summary: Persistent connections return the wrong socket due to ambiguous key in g_persistentObjects
Persistent connections currently return a cached connection for (key = "hostname").
Expected behaviour is to return a cached connection for (key = "hostname + port").

As a result, persistent connections write and read from the *wrong* socket if you have multiple connections to the same hostname but different port.

Redis hhvm implementation is affected by this bug (probably other modules too, but not MySQL at least, who uses its own socket cache handler).

This bug has been introduced by commit 04b77176fe0e17e4885c6465fcd98cccaf50d4a4 ("Fix sockopen_impl with IPv6 addresses")

How to reproduce:

  Run two servers:

  Server A: nc -l -p 4545
  Server B: nc -l -p 7878

  <?php
  $A = pfsockopen('127.0.0.1', 4545);
  $B = pfsockopen('127.0.0.1', 7878);

  fwrite($A, 'hello_on_socket_A' . "\n");
  fwrite($B, 'hello_on_socket_B' . "\n");

  fwrite will write both messages on socket $A instead of writing on $A and $B.

  Actual output on server A:
    hello_on_socket_A
    hello_on_socket_B

  Actual output on server B:
    <null>

Expected output:

  On port 4545:
    hello_on_socket_A

  On port 7878:
    hello_on_socket_B

Known workaround (without recompiling hhvm):

The key is derived from Util::HostURL.getHostURL so a workaround is to explicitly pass to HostURL a port, so the used key in g_persistentObjects becomes "127.0.0.1:4545" instead of "127.0.0.1".

  $A = pfsockopen('127.0.0.1:4545', 4545);
  $B = pfsockopen('127.0.0.1:7878', 7878);

Fix:

Always use port in the key used in g_persistentObjects.

Side Effects: none

Revert Plan: simple

hphp/runtime/ext/ext_socket.cpp
@@ -1045,7 +1045,7 @@ Variant sockopen_impl(const Util::HostURL &hosturl, VRefParam errnum,
 
   std::string key;
   if (persistent) {
-    key = hosturl.getHostURL();
+    key = hosturl.getHostURL() + ":" + boost::lexical_cast<std::string>(hosturl.getPort());
     Socket *sock =
       dynamic_cast<Socket*>(g_persistentObjects->get("socket", key.c_str()));
     if (sock) {
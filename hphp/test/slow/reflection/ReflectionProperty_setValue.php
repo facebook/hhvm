80a9ae11b2868eb731c8a61ec3f9fb7e9c05d456
Surupa Biswas <surupab@fb.com>
2014-01-21 11:29:01 -0800

    Warnings for incorrect params passed to ReflectionProperty get/setValue
    Added warnings to match Zend behavior for incorrect number of arguments passed
    and for not passing an object as the first parameter when reflecting on instance properties.
    
    Reviewed By: @ptarjan
    
    Differential Revision: D1135118

--- hphp/test/slow/reflection/ReflectionProperty_setValue.php
+++ hphp/test/slow/reflection/ReflectionProperty_setValue.php
@@ -18,7 +18,15 @@ var_dump($fooRef->getValue());
 
 // Non-static property $bar
 $barRef = new ReflectionProperty('cls', 'bar');
+$barRef->setValue(); //fail
+var_dump($barRef->getValue($obj));
 $barRef->setValue(3); // fail
 var_dump($barRef->getValue($obj));
 $barRef->setValue($obj, 4); // ok
 var_dump($barRef->getValue($obj));
+$barRef->setValue(1, 2); //fail
+var_dump($barRef->getValue($obj));
+var_dump($barRef->getValue()); //fail
+var_dump($barRef->getValue("foo")); //fail
+var_dump($barRef->getValue(1, 2)); //fail
+

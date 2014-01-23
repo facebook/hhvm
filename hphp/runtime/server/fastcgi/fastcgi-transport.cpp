39bc81c7b0e469e2c671ff60c29964845da7d944
Alex Malyshev <alexanderm@fb.com>
2014-01-22 16:21:32 -0800

    Don't use boost::to_upper
    perf is showing that it's calling dynamic_cast, a lot.
    
    Reviewed By: @jdelong
    
    Differential Revision: D1136788

--- hphp/runtime/server/fastcgi/fastcgi-transport.cpp
+++ hphp/runtime/server/fastcgi/fastcgi-transport.cpp
@@ -311,12 +311,16 @@ void FastCGITransport::onHeader(std::unique_ptr<folly::IOBuf> key_chain,
                                 std::unique_ptr<folly::IOBuf> value_chain) {
   Cursor cursor(key_chain.get());
   std::string key = cursor.readFixedString(key_chain->computeChainDataLength());
+
   // HTTP has case insensitive header keys
-  boost::to_upper(key);
+  for (auto& c : key) {
+    c = std::toupper(c);
+  }
+
   cursor = Cursor(value_chain.get());
   std::string value = cursor.readFixedString(
                                value_chain->computeChainDataLength());
-  m_requestHeaders.insert(std::make_pair(key, value));
+  m_requestHeaders.emplace(key, value);
 }
 
 void FastCGITransport::onHeadersComplete() {

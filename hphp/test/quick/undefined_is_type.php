748f484b34708259519fd1e6e2b3a56c2f511397
Jordan DeLong <jdelong@fb.com>
2014-02-06 15:24:42 -0800

    Fix a unit test that relies on warnings a little harder
    D783472 tried to keep this the same in repo and non-repo
    mode, but after adding a TPrim type hhbbc is able to optimize some of
    these IsType's away.
    
    Reviewed By: @elgenie
    
    Differential Revision: D1163177

--- hphp/test/quick/undefined_is_type.php
+++ hphp/test/quick/undefined_is_type.php
@@ -1,7 +1,15 @@
 <?php
 
 function foo() {
+  // Force all the types to be in so these don't get constant folded.
   if (isset($GLOBALS['a'])) $a = 1;
+  if (isset($GLOBALS['b'])) $a = 1.2;
+  if (isset($GLOBALS['c'])) $a = '1';
+  if (isset($GLOBALS['d'])) $a = new stdclass;
+  if (isset($GLOBALS['e'])) $a = array();
+  if (isset($GLOBALS['f'])) $a = false;
+  if (isset($GLOBALS['g'])) $a = null;
+
   echo "set:     ", isset($a)      . "\n";
   echo "nul:     ", is_null($a)    . "\n";
   echo "str:     ", is_string($a)  . "\n";

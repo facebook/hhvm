512b984be1638588ebea0d8d97f721ffbca48c07
Joel Low <joel@joelsplace.sg>
2014-02-08 20:31:37 +0800

    Fix matching output for the class methods unit test.
    The array returnd by getMethods cannot be directly dumped for PDOStatement
    because it is too long for preg_match to match against the expectf.
    
    Fixes #1624.

--- hphp/test/slow/reflection/class_get_methods.php
+++ hphp/test/slow/reflection/class_get_methods.php
@@ -9,6 +9,8 @@ var_dump($reflection->getMethods());
 var_dump(get_class_methods('A'));
 
 $reflection = new ReflectionClass('PDOStatement');
-var_dump($reflection->getMethods());
+$methods = $reflection->getMethods();
+var_dump(array_map(function($x) { return $x->class . '::' . $x->name; },
+  $methods));
 var_dump(get_class_methods('PDOStatement'));
 ?>

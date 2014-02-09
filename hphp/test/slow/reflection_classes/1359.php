4798d16828cf6a87d383015538835748d77eb541
Joel Low <joel@joelsplace.sg>
2014-02-10 00:36:31 +0800

    Revert "Fix case sensitivity for class methods."
    This reverts commit fc525b7573b521c2115323bdaa9e6a1e972f9553.

--- hphp/test/slow/reflection_classes/1359.php
+++ hphp/test/slow/reflection_classes/1359.php
@@ -8,5 +8,5 @@ echo 'fail';
 $ref = new ReflectionClass('C');
 var_dump($ref->hasMethod('mE'));
 var_dump($ref->hasMethod('me'));
-$m = $ref->getMethod('mE');
+$m = $ref->getMethod('me');
 var_dump($m->getName());

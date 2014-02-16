d542badf0aade1829cb63fc5fb62768fc1531575
K Fisher <kf08tm@brocku.ca>
2014-02-16 00:02:52 -0500

    Altered test again (wasn't changing setting)

--- hphp/test/slow/ext_session/session_ini_set_get_test.php
+++ hphp/test/slow/ext_session/session_ini_set_get_test.php
@@ -1,6 +1,6 @@
 <?php
 var_dump(ini_get('session.serialize_handler'));
-var_dump(ini_set('session.serialize_handler','php'));
+var_dump(ini_set('session.serialize_handler','php_binary'));
 var_dump(ini_get('session.serialize_handler'));
 
 var_dump(ini_get('session.gc_maxlifetime'));

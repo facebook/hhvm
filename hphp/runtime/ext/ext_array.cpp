bc7d03a6bbfdbebdc442f17a2aded52c02312aaa
Noam Zilberstein <noamz@seas.upenn.edu>
2014-02-28 18:36:35 +0000

    fixed some array_column() error messages related to type errors

--- hphp/runtime/ext/ext_array.cpp
+++ hphp/runtime/ext/ext_array.cpp
@@ -74,6 +74,24 @@ using HPHP::JIT::EagerCallerFrame;
   ArrNR arrNR_##input(cell_##input->m_data.parr);                 \
   CArrRef arr_##input = arrNR_##input.asArray();
 
+#define getCheckedArrayColumnRet(input, fail)                     \
+  auto const cell_##input = static_cast<CVarRef>(input).asCell(); \
+  if (UNLIKELY(cell_##input->m_type != KindOfArray)) {            \
+    if (UNLIKELY(cell_##input->m_type == KindOfString)) {         \
+      throw_bad_type_exception("array_column() expects parameter" \
+			       " 1 to be array, string given");	  \
+    } else if (UNLIKELY(cell_##input->m_type == KindOfInt64)) {   \
+      throw_bad_type_exception("array_column() expects parameter" \
+			       " 1 to be array, integer given");  \
+    } else {                                                      \
+      throw_expected_array_exception();				  \
+    }                                                             \
+    return fail;                                                  \
+  }                                                               \
+  ArrNR arrNR_##input(cell_##input->m_data.parr);                 \
+  CArrRef arr_##input = arrNR_##input.asArray();
+
+
 #define getCheckedArray(input) getCheckedArrayRet(input, uninit_null())
 
 Variant f_array_change_key_case(CVarRef input, bool upper /* = false */) {
@@ -131,7 +149,7 @@ static inline bool array_column_coerce_key(Variant &key, const char *name) {
     key = key.toString();
     return true;
   } else {
-    raise_warning("The %s key should be either a string or an integer", name);
+    raise_warning("array_column(): The %s key should be either a string or an integer", name);
     return false;
   }
 }
@@ -139,7 +157,7 @@ static inline bool array_column_coerce_key(Variant &key, const char *name) {
 Variant f_array_column(CVarRef input, CVarRef val_key,
                        CVarRef idx_key /* = null_variant */) {
   /* Be strict about array type */
-  getCheckedArrayRet(input, uninit_null());
+  getCheckedArrayColumnRet(input, uninit_null());
   Variant val = val_key, idx = idx_key;
   if (!array_column_coerce_key(val, "column") ||
       !array_column_coerce_key(idx, "index")) {

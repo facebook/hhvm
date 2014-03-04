beee66243f53e11826274230a7e5125c803a3897
Fred Emmott <fredemmott@fb.com>
2014-03-04 11:49:10 -0800

    Make DateTime implement DateTimeInterface (PHP 5.5.0)
    Precursor to implementing DateTimeImmutable, which is used by Twig.
    
    Reviewed By: @ptarjan
    
    Differential Revision: D1201828

---
+++ hphp/system/php/date/datetimeinterface.php
@@ -0,0 +1,16 @@
+<?php
+
+/* Type hints are missing as DateTime is IDL + C++ - with the type hints,
+ * the C++ implementation is considered incompatible.
+ */
+interface DateTimeInterface {
+  public function diff(
+    /* DateTimeInterface */ $datetime2,
+    /* bool */ $absolute /*= false */
+  );
+  public function format(/* string */ $format);
+  public function getOffset();
+  public function getTimestamp();
+  public function getTimezone();
+  public function __wakeup();
+}

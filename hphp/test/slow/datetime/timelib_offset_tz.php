54d2599aa89057ceffa0586370ec867ed4cafd2d
mwilliams <mwilliams@fb.com>
2014-03-12 08:23:48 -0700

    Revert "Fix DateTime->format() flags 'e' and 'T' for offset time zones."
    This reverts commit 15d69c2b684a3b028a47b9dc679f0c99824b9288. It was pointlessly allocating memory that could just have been in stack variables, doing work on every call that was only used in some cases, and leaking the allocated memory.
    
    Reviewed By: @bertmaher
    
    Differential Revision: D1216322

--- hphp/test/slow/datetime/timelib_offset_tz.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dateTime = \DateTime::createFromFormat('U', 1293839999,
-  new \DateTimeZone('GMT'));
-$var1 = $dateTime->format('T');
-$var2 = $dateTime->format('e');
-var_dump($var1);
-var_dump($var2);

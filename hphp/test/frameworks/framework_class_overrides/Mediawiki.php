5c9a68d6312dae49a95a6d6e7ee45610a57cbd58
Fred Emmott <fredemmott@fb.com>
2014-03-18 12:06:34 -0700

    mediawiki at 100%
    - Update our branch to have another test fix (incorrect usage of group Database)
    - Re-record
    - Modify suite.xml instead of trying to pass --excluded-group
    
    Will PR the changes to mediawiki once Travis has HHVM 3, as they'd currently introduce failures.
    
    Reviewed By: @JoelMarcey
    
    Differential Revision: D1226556

--- hphp/test/frameworks/framework_class_overrides/Mediawiki.php
+++ hphp/test/frameworks/framework_class_overrides/Mediawiki.php
@@ -4,13 +4,14 @@ require_once __DIR__.'/../Framework.php';
 class Mediawiki extends Framework {
   public function __construct(string $name) {
     $tc = get_runtime_build().' --php '.Options::$frameworks_root.
-          "/mediawiki-core/tests/phpunit/phpunit.php ".
-          "--exclude-group=Database,Broken ";
+          "/mediawiki-core/tests/phpunit/phpunit.php ";
+
     parent::__construct($name, $tc, null, null, true, TestFindModes::TOKEN);
   }
 
   protected function install(): void {
     parent::install();
+    $this->skipDatabaseTests();
     # Mediawiki has a custom test runner that pulls in parts of PHPUnit
     # from vendor/
     verbose("Installing phpunit 3.7 for Mediawiki.\n", Options::$verbose);
@@ -22,5 +23,23 @@ class Mediawiki extends Framework {
             Options::$verbose);
     $touch_command = "touch ".$this->getInstallRoot()."/LocalSettings.php";
     exec($touch_command);
+  }
+
+  // We don't have a MySQL instance running for our test runs, so Database
+  // tests won't work
+  private function skipDatabaseTests() {
+    verbose("Modifying suite.xml to skip database tests\n", Options::$verbose);
+    $file = Options::$frameworks_root.'/mediawiki-core/tests/phpunit/suite.xml';
+    $doc = new DOMDocument();
+    $doc->load($file);
+
+    $database_node = $doc->createElement('group');
+    $database_node->appendChild($doc->createTextNode('Database'));
+
+    $xpath = new DOMXPath($doc);
+    $exclude = $xpath->evaluate('/phpunit/groups/exclude')->item(0);
+    $exclude->appendChild($database_node);
+
+    file_put_contents($file, $doc->saveXML());
   }
 }

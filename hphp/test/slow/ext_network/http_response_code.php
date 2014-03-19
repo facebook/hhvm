6698889d549e83f5e30cc53040061cd63277e398
Vincent Cheung <vcheung@fb.com>
2014-03-19 11:22:21 -0700

    Allow http_response_code in CLI mode
    - Added variable to remember response code state instead of solely depending on the transport
    
    Closes #1352
    
    Reviewed By: @ptarjan
    
    Differential Revision: D1229093

---
+++ hphp/test/slow/ext_network/http_response_code.php
@@ -0,0 +1,12 @@
+<?php
+var_dump(http_response_code());
+var_dump(http_response_code(0));
+var_dump(http_response_code());
+var_dump(http_response_code(-1));
+var_dump(http_response_code());
+var_dump(http_response_code(295));
+var_dump(http_response_code());
+var_dump(http_response_code(0));
+var_dump(http_response_code());
+var_dump(http_response_code(404));
+var_dump(http_response_code());

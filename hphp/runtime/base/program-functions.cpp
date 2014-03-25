facebook/hhvm

commit d37399da99bc745a70167c7cd999be68b4a99057

Fix hhvm --php -d - bad temporary filename, parsed as hdf
- breaks phpunit runInSeparateProcess
 - '-c' and filename need to be two args - this was interpreted as adding an extra space, leading to file not found
 - temporary ones don't end in .ini

refs facebook/hhvm#1904

Reviewed By: @ptarjan

Differential Revision: D1224939

hphp/util/hdf.cpp
@@ -131,7 +131,7 @@ void Hdf::open(const char *filename) {
 
 void Hdf::append(const char *filename) {
   assert(filename && *filename);
-  if (boost::ends_with(filename, ".ini")) {
+  if (!boost::ends_with(filename, ".hdf")) {
     return;
   }
   CheckNeoError(hdf_read_file(getRaw(), (char*)filename));

hphp/runtime/base/emulate-zend.cpp
@@ -168,9 +168,8 @@ int emulate_zend(int argc, char** argv) {
   }
 
   if (ini_fd != -1) {
-    std::string arg("-c ");
-    arg += ini_path;
-    newargv.push_back(arg);
+    newargv.push_back("-c");
+    newargv.push_back(ini_path);
   }
 
   if (cnt < argc && strcmp(argv[cnt], "--") == 0) cnt++;

hphp/test/ext/config-debugger-server.hdf
@@ -25,7 +25,7 @@ Log {
 
 Sandbox {
   SandboxMode = true
-  ConfFile = sandbox_conf
+  ConfFile = sandbox_conf.hdf
   # only match hphpd.<user>.sth.sth, not any requests
   Pattern = hphpd\.([a-z0-9_-]+)\.[a-z0-9_-]+\.[a-z0-9_-]+
 }

hphp/test/server/debugger/config/debugger-server.hdf
@@ -21,7 +21,7 @@ Eval {
 
 Sandbox {
   SandboxMode = true
-  ConfFile = sandbox_conf
+  ConfFile = sandbox_conf.hdf
   # only match hphpd.<user>.sth.sth, not any requests
   Pattern = hphpd\.([a-z0-9_-]+)\.[a-z0-9_-]+\.[a-z0-9_-]+
 }

hphp/test/server/debugger/tests/test_base.php
@@ -82,7 +82,7 @@ function startServer($serverPort, $adminPort, $debugPort) {
   $useJit = array_key_exists('HHVM_JIT', $_ENV) && $_ENV['HHVM_JIT'] == 1;
   $jitConfig = ' -vEval.Jit='.($useJit ? "true" : "false");
   // To emulate sandbox setup, let Sandbox.Home be '$home'
-  // and user name be 'debugger', so that the server can find the sandbox_conf
+  // and user name be 'debugger', so that the server can find the sandbox_conf.hdf
   // in '$home'.'/debugger'.
   $sandboxHomeConfig = ' -vSandbox.Home='.$home;
 

commit 431fe537d17059baeea148f76d81202790a2ee1f

Parse the config file as .ini unless it is explicitly .hdf
This has the side effect of fixing a silent failure caused by d37399d
when loading a Sandbox HDF file without an extension.

hphp/doc/options.compiled
@@ -656,7 +656,7 @@ a web server, hence eliminating a lot of setups.
     SandboxMode = false
     Pattern = www.[user]-[sandbox].[machine].facebook.com
     Home = /home
-    ConfFile = ~/.hphp
+    ConfFile = ~/.hhvm.hdf
 
     ServerVariables {
       name = value

hphp/runtime/base/program-functions.cpp
@@ -59,10 +59,12 @@
 #include "hphp/runtime/base/stream-wrapper-registry.h"
 #include "hphp/runtime/vm/debug/debug.h"
 
+#include <boost/algorithm/string/predicate.hpp>
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/positional_options.hpp>
 #include <boost/program_options/variables_map.hpp>
 #include <boost/program_options/parsers.hpp>
+
 #include <libgen.h>
 #include <oniguruma.h>
 #include <signal.h>
@@ -1202,11 +1204,15 @@ static int execute_program_impl(int argc, char** argv) {
 
   Hdf config;
   for (auto& c : po.config) {
-    config.append(c);
+    if (boost::ends_with(c, ".hdf")) {
+      config.append(c);
+    }
   }
   RuntimeOption::Load(config, &po.confStrings);
   for (auto& c : po.config) {
-    process_ini_settings(c);
+    if (!boost::ends_with(c, ".hdf")) {
+      process_ini_settings(c);
+    }
   }
 
   vector<string> badnodes;

hphp/util/hdf.cpp
@@ -15,9 +15,6 @@
 */
 
 #include "hphp/util/hdf.h"
-
-#include <boost/algorithm/string/predicate.hpp>
-
 #include "hphp/util/lock.h"
 
 namespace HPHP {
@@ -131,9 +128,6 @@ void Hdf::open(const char *filename) {
 
 void Hdf::append(const char *filename) {
   assert(filename && *filename);
-  if (!boost::ends_with(filename, ".hdf")) {
-    return;
-  }
   CheckNeoError(hdf_read_file(getRaw(), (char*)filename));
 }
 
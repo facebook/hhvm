326c5490f440c9693cef3545afb235e5b3001055
Eugene Letuchy <eletuchy@fb.com>
2014-03-28 13:49:35 -0700

    add a test for interaction of async and finally
    ... to better expose issues with the behavior of the two.
    
    Reviewed By: @jano
    
    Differential Revision: D1246942

---
+++ hphp/test/slow/finally/async.php
@@ -0,0 +1,39 @@
+<?hh
+
+async function genEager($a) {
+  try {
+    echo "eager execution\n";
+    return 10;
+  } finally {
+    echo "finally\n";
+  }
+}
+
+function block() { // simulates blocking I/O
+  return RescheduleWaitHandle::create(1,1);
+};
+
+async function genBlocking($a) {
+  try {
+    echo "before blocking\n";
+    await block();
+    echo "after blocking\n";
+    return 10;
+  } finally {
+    echo "finally\n";
+  }
+}
+
+function main() {
+  // Note: finally should appear in output, but doesn't
+  echo '= This test demonstrates incorrect behavior =', "\n\n";
+
+  echo "* eager async *\n";
+  $result = genEager(42)->join();
+  var_dump($result);
+
+  echo "* blocking async *\n";
+  $result = genBlocking(42)->join();
+  var_dump($result);
+}
+main();

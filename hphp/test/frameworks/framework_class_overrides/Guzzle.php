e56f61b3483ce5a26950129280f6b1efa3ddced0
Fred Emmott <fredemmott@fb.com>
2014-04-08 15:26:42 -0700

    Test runner improvements for guzzle
    - run sequentially - the tests spawn a server on a fixed port which they share.
    - set PHP_BINARY as they're on phpunit 3.x
    
    Reviewed By: @JoelMarcey
    
    Differential Revision: D1265597

--- hphp/test/frameworks/framework_class_overrides/Guzzle.php
+++ hphp/test/frameworks/framework_class_overrides/Guzzle.php
@@ -5,6 +5,10 @@ class Guzzle extends Framework {
   public function __construct(string $name) {
     $tc = get_runtime_build().' '.__DIR__.
       '/../framework_downloads/guzzle/vendor/bin/phpunit';
-    parent::__construct($name, $tc, null, null, true, TestFindModes::TOKEN);
+    // PHPUnit <4.x @runInSeparateProcess fix
+    $env = Map { "PHP_BINARY" =>  get_runtime_build(true) };
+    $parallel = false;
+    parent::__construct($name, $tc, $env,
+                        null, $parallel, TestFindModes::TOKEN);
   }
 }

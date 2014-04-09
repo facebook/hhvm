7bdfc2dc2e7b5e80ee5e335c3506121f090cf058
Sara Golemon <sgolemon@fb.com>
2014-04-09 11:56:06 -0700

    Enable EZC core by default
    To build bundled EZC exts, add the existing ENABLE_ZEND_COMPAT option
    
    cmake -DENABLE_ZEND_COMPAT=On .

--- CMake/ExtZendCompat.cmake
+++ CMake/ExtZendCompat.cmake
@@ -1,4 +1,4 @@
-option(ENABLE_ZEND_COMPAT "Enable Zend source compatibility (beta)" OFF)
+option(ENABLE_ZEND_COMPAT "Enable Zend source compatibility extensions" OFF)
 INCLUDE(CMakeDependentOption)
 CMAKE_DEPENDENT_OPTION(ENABLE_MONGO
   "Enable Mongo extension (requires ENABLE_ZEND_COMPAT)" OFF "ENABLE_ZEND_COMPAT" OFF)
@@ -8,20 +8,20 @@ CMAKE_DEPENDENT_OPTION(ENABLE_EZC_TEST
 set(ZEND_COMPAT_PROJECTS)
 set(ZEND_COMPAT_BUILD_DIRS)
 set(ZEND_COMPAT_LINK_LIBRARIES)
-
-# Look for projects
 set(EZC_DIR "${HPHP_HOME}/hphp/runtime/ext_zend_compat/")
-file(GLOB ezc_projects RELATIVE ${EZC_DIR} "${EZC_DIR}/*")
-foreach(ezc_project ${ezc_projects})
-	get_filename_component(ezc_name ${ezc_project} NAME)
-	if ((NOT ${ezc_name} STREQUAL "php-src") AND
-            (NOT ${ezc_name} STREQUAL "hhvm") AND
-            (IS_DIRECTORY "${EZC_DIR}/${ezc_name}"))
-		list(APPEND ZEND_COMPAT_PROJECTS ${ezc_name})
-	endif()
-endforeach()
 
 if (ENABLE_ZEND_COMPAT)
+	# Look for projects
+	file(GLOB ezc_projects RELATIVE ${EZC_DIR} "${EZC_DIR}/*")
+	foreach(ezc_project ${ezc_projects})
+		get_filename_component(ezc_name ${ezc_project} NAME)
+		if ((NOT ${ezc_name} STREQUAL "php-src") AND
+	            (NOT ${ezc_name} STREQUAL "hhvm") AND
+	            (IS_DIRECTORY "${EZC_DIR}/${ezc_name}"))
+			list(APPEND ZEND_COMPAT_PROJECTS ${ezc_name})
+		endif()
+	endforeach()
+
 	foreach(ezc_project ${ZEND_COMPAT_PROJECTS})
 		if (${ezc_project} STREQUAL "yaml")
 			find_package(LibYaml)
@@ -43,13 +43,11 @@ if (ENABLE_ZEND_COMPAT)
 			list(APPEND ZEND_COMPAT_BUILD_DIRS "${EZC_DIR}/${ezc_project}")
 		endif()
 	endforeach()
-
-	if (ZEND_COMPAT_BUILD_DIRS)
-		list(APPEND ZEND_COMPAT_BUILD_DIRS "${EZC_DIR}/php-src" "${EZC_DIR}/hhvm")
-		include_directories("${EZC_DIR}/php-src")
-		include_directories("${EZC_DIR}/php-src/main")
-		include_directories("${EZC_DIR}/php-src/Zend")
-		include_directories("${EZC_DIR}/php-src/TSRM")
-	endif()
 endif()
+
+list(APPEND ZEND_COMPAT_BUILD_DIRS "${EZC_DIR}/php-src" "${EZC_DIR}/hhvm")
+include_directories("${EZC_DIR}/php-src")
+include_directories("${EZC_DIR}/php-src/main")
+include_directories("${EZC_DIR}/php-src/Zend")
+include_directories("${EZC_DIR}/php-src/TSRM")
 

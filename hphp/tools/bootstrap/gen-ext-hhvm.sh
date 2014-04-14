f80199774bd441b9754a03e0111fed8962004ebe
Daniel Sloof <goapsychadelic@gmail.com>
2014-04-14 20:46:21 +0200

    remove outdated section from gen-ext-hhvm
    Removes an incorrect warning while building.

--- hphp/tools/bootstrap/gen-ext-hhvm.sh
+++ hphp/tools/bootstrap/gen-ext-hhvm.sh
@@ -6,7 +6,7 @@
 # $4 ext_hhvm.cpp source to generate
 # $5 ext_hhvm.h header to generate
 if [ "$1" = "darwin" ]; then
-  gobjdump --section=".text" --section="LC_SEGMENT..text.hot.built." -t $3 | \
+  gobjdump --section=".text" -t $3 | \
       awk '{ if ($2=="g") print $8 }' | \
       $HPHP_HOME/hphp/tools/bootstrap/gen-ext-hhvm \
       $2 $5 $4 $HPHP_HOME/hphp/system/idl/*.idl.json

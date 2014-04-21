e8a6a3bf4b0a665870c2c3f3ae6fc7bbcdcf7669
Joel Low <joel@joelsplace.sg>
2014-04-21 16:06:15 -0700

    Fix potential null pointer dereference.
    Summary:
    Closes #2392
    Closes #2489
    
    Reviewed By: @fredemmott
    
    Differential Revision: D1287004

--- hphp/runtime/ext/pdo_mysql.cpp
+++ hphp/runtime/ext/pdo_mysql.cpp
@@ -469,7 +469,9 @@ int PDOMySqlConnection::handleError(const char *file, int line,
   } else {
     Array info = Array::Create();
     info.append(String(*pdo_err, CopyString));
-    stmt->dbh->fetchErr(stmt, info);
+    if (stmt) {
+      stmt->dbh->fetchErr(stmt, info);
+    }
     throw_pdo_exception((int)einfo->errcode, info, "SQLSTATE[%s] [%d] %s",
                         pdo_err[0], einfo->errcode, einfo->errmsg);
   }

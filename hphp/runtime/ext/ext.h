f1da367af7b20fa8181357813b0242220724e740
Jordan DeLong <jdelong@fb.com>
2014-04-22 15:28:54 -0700

    Revert "Convert ext StringBuffer to HNI"
    Summary: It's looking like a regression in perflab charts (the few
    runs that passed on the diff look red, too...)
    
    Reviewed By: @markw65
    
    Differential Revision: D1290446

--- hphp/runtime/ext/ext.h
+++ hphp/runtime/ext/ext.h
@@ -27,6 +27,7 @@
 #include "hphp/facebook/extensions/fbml/ext_fbml.h"
 #include "hphp/facebook/extensions/obc/ext_obc.h"
 #include "hphp/facebook/extensions/phpmc/ext_phpmc.h"
+#include "hphp/facebook/extensions/string_buffer/ext_string_buffer.h"
 #endif
 
 #include "hphp/runtime/ext/ext_apc.h"

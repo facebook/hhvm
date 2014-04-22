d27456495ba124dfab209e2d4971626fb7370ce8
Joel Beales <joelb@fb.com>
2014-04-22 12:26:20 -0700

    Fix Find Refs For Namespaces
    Summary: Add namespace to the string we're matching for find references. Strip it from the result set if we need to.
    
    I really need to find time to set up unit tests for this feature.
    
    Reviewed By: @jwatzman
    
    Differential Revision: D1287144

--- hphp/hack/src/server/serverFindRefs.ml
+++ hphp/hack/src/server/serverFindRefs.ml
@@ -10,19 +10,28 @@
 
 open Utils
 
+let add_ns name =
+  if name.[0] = '\\' then name else "\\" ^ name
+
+let strip_ns results =
+  List.map begin fun (s, p) -> ((Utils.strip_ns s), p) end results
+
 let search class_names method_name files genv env oc =
   let files_list = SSet.fold (fun x y -> x :: y) files [] in
   (* Get all the references to the provided method name and classes in the files *)
   let res = FindRefsService.find_references genv.ServerEnv.workers class_names
       method_name files_list in
+  let res = strip_ns res in
   Marshal.to_channel oc res []
 
 let search_function function_name genv env oc =
+  let function_name = add_ns function_name in
   let files = FindRefsService.get_dependent_files_function
       genv.ServerEnv.workers function_name in
   search None (Some function_name) files genv env oc
 
 let search_method class_name method_name genv env oc =
+  let class_name = add_ns class_name in
   (* Find all the classes that extend this one *)
   let all_classes = FindRefsService.get_child_classes
       genv.ServerEnv.workers env.ServerEnv.files_info class_name in
@@ -33,6 +42,7 @@ let search_method class_name method_name genv env oc =
   search (Some all_classes) (Some method_name) files genv env oc
 
 let search_class class_name genv env oc =
+  let class_name = add_ns class_name in
   let files = FindRefsService.get_dependent_files
       genv.ServerEnv.workers (SSet.singleton class_name) in
   search (Some (SSet.singleton class_name)) None files genv env oc

03f33c79671fa8e665ceb6ccbcfc886db76b6c6b
Otto Ebeling <otto@fb.com>
2014-04-29 14:02:46 -0700

    backport CVE-2013-4248
    Summary: Backport fix of CVE-2013-4248 from
    http://git.php.net/?p=php-src.git;a=commit;h=2874696a5a8d46639d261571f915c493cd875897
    
    Reviewed By: afrind
    
    Differential Revision: D1281517

--- hphp/runtime/ext/openssl/ext_openssl.cpp
+++ hphp/runtime/ext/openssl/ext_openssl.cpp
@@ -2397,6 +2397,73 @@ static time_t asn1_time_to_time_t(ASN1_UTCTIME *timestr) {
   return ret;
 }
 
+/* Special handling of subjectAltName, see CVE-2013-4073
+ * Christian Heimes
+ */
+
+static int openssl_x509v3_subjectAltName(BIO *bio, X509_EXTENSION *extension)
+{
+  GENERAL_NAMES *names;
+  const X509V3_EXT_METHOD *method = nullptr;
+  long i, length, num;
+  const unsigned char *p;
+
+  method = X509V3_EXT_get(extension);
+  if (method == nullptr) {
+    return -1;
+  }
+
+  p = extension->value->data;
+  length = extension->value->length;
+  if (method->it) {
+    names = (GENERAL_NAMES*)(ASN1_item_d2i(nullptr, &p, length,
+                                          ASN1_ITEM_ptr(method->it)));
+  } else {
+    names = (GENERAL_NAMES*)(method->d2i(nullptr, &p, length));
+  }
+  if (names == nullptr) {
+    return -1;
+  }
+
+  num = sk_GENERAL_NAME_num(names);
+  for (i = 0; i < num; i++) {
+    GENERAL_NAME *name;
+    ASN1_STRING *as;
+    name = sk_GENERAL_NAME_value(names, i);
+    switch (name->type) {
+      case GEN_EMAIL:
+             BIO_puts(bio, "email:");
+             as = name->d.rfc822Name;
+             BIO_write(bio, ASN1_STRING_data(as),
+                       ASN1_STRING_length(as));
+             break;
+      case GEN_DNS:
+             BIO_puts(bio, "DNS:");
+             as = name->d.dNSName;
+             BIO_write(bio, ASN1_STRING_data(as),
+                       ASN1_STRING_length(as));
+             break;
+      case GEN_URI:
+             BIO_puts(bio, "URI:");
+             as = name->d.uniformResourceIdentifier;
+             BIO_write(bio, ASN1_STRING_data(as),
+                       ASN1_STRING_length(as));
+             break;
+      default:
+             /* use builtin print for GEN_OTHERNAME, GEN_X400,
+              * GEN_EDIPARTY, GEN_DIRNAME, GEN_IPADD and GEN_RID
+              */
+             GENERAL_NAME_print(bio, name);
+    }
+    /* trailing ', ' except for last element */
+    if (i < (num - 1)) {
+           BIO_puts(bio, ", ");
+    }
+  }
+  sk_GENERAL_NAME_pop_free(names, GENERAL_NAME_free);
+
+  return 0;
+}
 Variant HHVM_FUNCTION(openssl_x509_parse, const Variant& x509cert,
                                           bool shortnames /* = true */) {
   Resource ocert = Certificate::Get(x509cert);
@@ -2460,10 +2527,12 @@ Variant HHVM_FUNCTION(openssl_x509_parse, const Variant& x509cert,
   {
     Array subitem;
     for (int i = 0; i < X509_get_ext_count(cert); i++) {
+      int nid;
       X509_EXTENSION *extension = X509_get_ext(cert, i);
       char *extname;
       char buf[256];
-      if (OBJ_obj2nid(X509_EXTENSION_get_object(extension)) != NID_undef) {
+      nid = OBJ_obj2nid(X509_EXTENSION_get_object(extension));
+      if (nid != NID_undef) {
         extname = (char*)OBJ_nid2sn(OBJ_obj2nid
                                     (X509_EXTENSION_get_object(extension)));
       } else {
@@ -2472,7 +2541,19 @@ Variant HHVM_FUNCTION(openssl_x509_parse, const Variant& x509cert,
         extname = buf;
       }
       BIO *bio_out = BIO_new(BIO_s_mem());
-      if (X509V3_EXT_print(bio_out, extension, 0, 0)) {
+      if (nid == NID_subject_alt_name) {
+        if (openssl_x509v3_subjectAltName(bio_out, extension) == 0) {
+          BUF_MEM *bio_buf;
+          BIO_get_mem_ptr(bio_out, &bio_buf);
+          subitem.set(String(extname, CopyString),
+                      String((char*)bio_buf->data,
+                             bio_buf->length,
+                             CopyString));
+        } else {
+          BIO_free(bio_out);
+          return false;
+        }
+      } else if (X509V3_EXT_print(bio_out, extension, 0, 0)) {
         BUF_MEM *bio_buf;
         BIO_get_mem_ptr(bio_out, &bio_buf);
         subitem.set(String(extname, CopyString),

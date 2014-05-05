0dfec72120b4a38d82b05d34ae497bd750618671
Tim Starling <tstarling@wikimedia.org>
2014-05-05 16:49:12 +1000

    EZC: allow dynamically loadable Zend compat extensions
    * Use the existing macro ZEND_GET_MODULE() to provide the necessary
      getModule() function.
    * In CMake, add macro HHVM_COMPAT_EXTENSION(), equivalent to
      HHVM_EXTENSION(), which will register a zend-compat extension.
    * Set the source language, so that it's not even necessary to rename the
      files. This makes it trivial to have a dual-targeted source tree.

--- CMake/HPHPIZEFunctions.cmake
+++ CMake/HPHPIZEFunctions.cmake
@@ -12,10 +12,27 @@ function(HHVM_ADD_INCLUDES EXTNAME)
   include_directories(${ARGN})
 endfunction()
 
+# Add an extension
 function(HHVM_EXTENSION EXTNAME)
   add_library(${EXTNAME} SHARED ${ARGN})
   set_target_properties(${EXTNAME} PROPERTIES PREFIX "")
   set_target_properties(${EXTNAME} PROPERTIES SUFFIX ".so")
+endfunction()
+
+# Add an extension that uses the Zend compatibility layer.
+function(HHVM_COMPAT_EXTENSION EXTNAME)
+  HHVM_EXTENSION(${EXTNAME} ${ARGN})
+  # Compile all source files as C++
+  set_source_files_properties(${ARGN} PROPERTIES LANGUAGE "CXX")
+  # Define COMPILE_DL_<EXTNAME> so that ZEND_GET_MODULE() will be invoked
+  string(TOUPPER ${EXTNAME} EXTNAME_UPPER)
+  add_definitions("-DCOMPILE_DL_${EXTNAME_UPPER}")
+  # Add extra include directories
+  set(EZC_DIR "${HPHP_HOME}/hphp/runtime/ext_zend_compat")
+  include_directories("${EZC_DIR}/php-src")
+  include_directories("${EZC_DIR}/php-src/main")
+  include_directories("${EZC_DIR}/php-src/Zend")
+  include_directories("${EZC_DIR}/php-src/TSRM")
 endfunction()
 
 function(HHVM_SYSTEMLIB EXTNAME)

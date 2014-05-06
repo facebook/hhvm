3bdfe10e22c68ece682d916a55d947ff8745be37
Daniel Sloof <goapsychadelic@gmail.com>
2014-05-06 13:39:37 +0200

    fix get_object_vars for simplexml objects
    Just like o_toArray, we need to make o_toIterArray call into our own
    implementation.

--- hphp/runtime/ext/ext_simplexml.h
+++ hphp/runtime/ext/ext_simplexml.h
@@ -93,6 +93,7 @@ class c_SimpleXMLElement :
   static int64_t ToInt64(const ObjectData* obj) noexcept;
   static double  ToDouble(const ObjectData* obj) noexcept;
   static Array   ToArray(const ObjectData* obj);
+  static Array   ToIterArray(const ObjectData* obj);
 
   Resource   document;
   xmlNodePtr node;

e4ea378d91d4f743801b72c0be04564e54cb47b5
Tim Starling <tstarling@wikimedia.org>
2014-05-08 15:58:35 +1000

    Don't require full source tree for building DSOs
    * Install header files
    * Install CMake files needed by hphpize
    * Fix hphpize to look for headers and CMake files in the installed
      directory
    * Rename php_config.h-linux to php_config-linux.h and
      php_config.h-darwin to php_config-darwin.h, so that it will be
      picked up by *.h glob for installation. Also helps with syntax
      highlighting.
    
    Also:
    * Add an install target to the makefile of DSOs. Install to a
      version-dependent directory, similar to what PHP does. I chose
      lib/hphp over lib/hhvm for consistency with include/hphp.

--- hphp/runtime/ext_zend_compat/php-src/main/php_config.h
+++ hphp/runtime/ext_zend_compat/php-src/main/php_config.h
@@ -3,9 +3,9 @@
 #define incl_HPHP_EZC_PHP_CONFIG_H
 
 #ifdef __APPLE__
-# include "php_config.h-darwin"
+# include "php_config-darwin.h"
 #else
-# include "php_config.h-linux"
+# include "php_config-linux.h"
 #endif
 
 #endif

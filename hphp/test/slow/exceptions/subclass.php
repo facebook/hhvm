9c47ea3bb159939b127df298b8334d756668950c
Witchakorn Kamolpornwijit <witchakk@mit.edu>
2014-05-10 19:02:12 -0700

    Make PDOException a subclass of RuntimeException.
    Summary: This matches PHP5 behavior
    
    Closes #2584
    Closes #2583
    
    Reviewed By: @ptarjan
    
    Differential Revision: D1314289
    
    Pulled By: @JoelMarcey

---
+++ hphp/test/slow/exceptions/subclass.php
@@ -0,0 +1,11 @@
+<?php
+$exec_class = ['Exception', 'RuntimeException', 'InvalidArgumentException',
+               'BadMethodCallException', 'OutOfBoundsException',
+               'DOMException', 'PDOException'];
+
+foreach($exec_class as $e1) {
+  foreach($exec_class as $e2) {
+    echo $e1.' '.(is_subclass_of($e1,$e2)?'is':'is not').
+         ' a subclass of '.$e2."\n";
+  }
+}

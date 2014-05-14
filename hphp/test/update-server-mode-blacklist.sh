62619a7710567865e8f6f0afa2a1c16137fe69ef
bsimmers <bsimmers@fb.com>
2014-05-14 10:51:24 -0700

    Fix server-mode tests, add -vEval.JitRequireWriteLease
    Summary: Without the RequireWriteLease option, many of the tests are just
    skipping the JIT entirely because they can't get the write lease. And a recent
    change to how config files are handled was disabling server mode for all tests.
    
    I also changed it to rerun a test in CLI mode if it fails in server mode. I'm
    seeing occasional crashes (real bugs that I have yet to debug) that are
    truncating the output for concurrently running tests without making the curl
    command fail. Re-running the test in CLI mode will eliminate these spurious
    failures, at the expense of masking some tests that are truly broken in server
    mode. The verbose output indicates when this happens to allow some kind of
    visibility into it.
    
    Reviewed By: @markw65
    
    Differential Revision: D1349388

--- hphp/test/update-server-mode-blacklist.sh
+++ hphp/test/update-server-mode-blacklist.sh
@@ -11,13 +11,20 @@ words="stdin STDIN stdout STDOUT stderr STDERR \
     ob_list_handlers memory_get_usage PHP_SELF \
     php://fd/ proc_nice clearstatcache \
     php_sapi_name libxml_disable_entity_loader \
+    headers_sent ob_get_status \
 "
 for word in $words; do
-    echo "Looking for $word"
-    for file in $(find quick slow zend/good -name '*.php' -exec grep -rl -F $word {} +); do
-        if [[ ! -s $file.noserver ]]; then
-            echo $file.noserver
-            echo "Used banned symbol $word" > $file.noserver
-        fi
-    done
+    # Doing this in parallel is racy, but the absolute worst that can happen is
+    # we'll end up with some garbled text inside a .noserver file.
+    (
+        echo "Looking for $word"
+        for file in $(find quick slow zend/good -name '*.php' -exec grep -rl -F $word {} +); do
+            if [[ ! -s $file.noserver ]]; then
+                echo $file.noserver
+                echo "Used banned symbol $word" > $file.noserver
+            fi
+        done
+    ) &
 done
+
+wait

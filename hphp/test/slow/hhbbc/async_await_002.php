d7245e1f6c9d02e88fa4eda8d155a0b1eae402bc
Jordan DeLong <jdelong@fb.com>
2014-05-14 06:46:37 -0700

    Fix a bug in Type::operator== for WaitH types
    Summary: I ran some perflabs in dbgo and they hit an assert type.  The
    problem was with code storing WaitH<Foos> into a local instead of
    awaiting it immediately, then having a control flow join that did the
    await---the merge_into implementation uses operator== and decided they
    were the same since they were all wait handles.
    
    Reviewed By: @edwinsmith
    
    Differential Revision: D1328006

---
+++ hphp/test/slow/hhbbc/async_await_002.php
@@ -0,0 +1,76 @@
+<?hh
+
+async function foo() {
+  // Hide a bool in APC so static analysis can't see it.
+  apc_store('mybool2', true);
+  return apc_fetch('mybool2');
+}
+
+function returningFalse() {
+  // Hide a bool in APC so static analysis can't see it.
+  apc_store('mybool', false);
+  return apc_fetch('mybool');
+}
+
+class :x:frag {}
+class :ui:action-list extends :x:frag {}
+class :ui:form extends :x:frag {}
+
+final class foo {
+  public async function genThings() {
+    if (returningFalse()) {
+      $is_bouncing = await foo();
+      if ($is_bouncing) {
+        $gen = $this->genA();
+      } else {
+        $gen = $this->genB();
+      }
+    } else {
+      $gen = $this->genC();
+    }
+    list($text, $primary, $secondary) = await $gen;
+    return $secondary;
+  }
+
+  private async function genC() {
+    $text = 'a';
+    $primary = 'b';
+    $secondary =
+      <ui:action-list>
+      </ui:action-list>;
+    return tuple($text, $primary, $secondary);
+  }
+
+  private async function genB() {
+    $text = 'asd';
+    $primary = 'asd';
+    if ($major_domain) {
+      $secondary =
+        <ui:action-list>
+        </ui:action-list>;
+    } else {
+      $secondary =
+        <ui:action-list>
+        </ui:action-list>;
+    }
+    return tuple($text, $primary, $secondary);
+  }
+
+  private async function genA() {
+    $form =
+      <ui:form>
+      </ui:form>;
+    return tuple($text, $form, <x:frag />);
+  }
+}
+
+async function go() {
+  $x = new foo;
+  await $x->genThings();
+  await $x->genThings();
+  await $x->genThings();
+  await $x->genThings();
+  await $x->genThings();
+}
+$y = go();
+$y->join();

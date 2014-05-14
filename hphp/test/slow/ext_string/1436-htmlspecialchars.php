0fba281c875cbdb4565ab076ce1ff998e4a9a662
Camillus Gerard Cai <cgcai@qxcg.net>
2014-05-14 16:09:26 -0700

    htmlspecialchars() checks for type of input parameter before executing.
    Reviewed By: @ptarjan
    
    Differential Revision: D1305960
    
    Pulled By: @JoelMarcey

---
+++ hphp/test/slow/ext_string/1436-htmlspecialchars.php
@@ -0,0 +1,8 @@
+<?php
+
+// Github Issue #1436: Return type of htmlspecialchars() when
+// wrong type passed.
+
+$string = array('str'=>'aaa');
+$str = htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
+var_dump($str);

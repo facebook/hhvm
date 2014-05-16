29c07b87ad7a4f16b9eb3fd0cc0d0d777d50cf63
Eugene Letuchy <eletuchy@fb.com>
2014-05-16 13:28:28 -0700

    perf: teach hphpc about second arg to *_exists functions
    Summary: There is a second argument (default true) to class_exists,
     interface_exists, function_exists (HHVM only), defined (HHVM only).
     However, hphpc completely ignored expressions that provided a second
     param, even though the only rewrites it does don't care about whether
     autoload happens or is at all successful.
    
    Reviewed By: mwilliams
    
    Subscribers: hphp-diffslists, ps
    
    Differential Revision: D1334918

--- hphp/test/slow/volatile/1233.php
+++ hphp/test/slow/volatile/1233.php
@@ -9,20 +9,40 @@ var_dump(HH\autoload_set_paths(
   ''
 ));
 function failure($kind, $name) {
-  if ($kind == 'constant' && $name == 'bar') define('bar', 'baz');
+  if ($kind == 'constant' && $name == 'bar') {
+    define('bar', 'baz');
+  }
   var_dump($kind, $name);
 }
+
+function builtin() {
+  var_dump(function_exists('strpos'));
+  if (!function_exists('strpos')) {
+    echo "dead code\n";
+  }
+  var_dump(function_exists('strpos', true));
+  if (!function_exists('strpos', true)) {
+    echo "dead code\n";
+  }
+  var_dump(function_exists('strpos', false));
+  if (!function_exists('strpos', false)) {
+    echo "dead code\n";
+  }
+}
+builtin();
+
 var_dump(function_exists('foo'));
+var_dump(function_exists('foo', true));
+var_dump(function_exists('foo', false));
 var_dump(function_exists('bar', false));
 var_dump(defined('foo'));
 var_dump(defined('bar', false));
 var_dump(constant('foo'));
 var_dump(constant('bar'));
+
 if (0) {
-  function foo() {
-}
-  function foo() {
-}
+  function foo() {}
+  function foo() {}
   define('foo', 0);
   define('bar', 0);
 }

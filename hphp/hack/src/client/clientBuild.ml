649ec799ad061817a4a5aa5850383932fc1e2a3a
Jason Reed <jcreed@fb.com>
2014-05-19 14:13:53 -0700

    Autostart server on hack build, increase timeout
    Reviewed By: @aryx
    
    Differential Revision: D1336574

--- hphp/hack/src/client/clientBuild.ml
+++ hphp/hack/src/client/clientBuild.ml
@@ -10,7 +10,7 @@
 
 open ClientExceptions
 
-let num_build_retries = 60
+let num_build_retries = 120
 
 type env = ServerMsg.build_opts
 
@@ -19,20 +19,26 @@ let rec connect env retries =
     ClientUtils.connect env.ServerMsg.root
   with
   | ClientExceptions.Server_cant_connect ->
-    Printf.printf
-      "Can't connect to the server\n\
-      Try 'hh start your_directory'\n";
-    exit 2
+    Printf.printf "Can't connect to server, retrying.\n%!";
+    if retries > 0
+    then begin
+      Unix.sleep 1;
+      connect env (retries - 1)
+    end
+    else exit 2
   | ClientExceptions.Server_initializing ->
     Printf.printf "Server still initializing.\n%!";
     if retries > 0
-    then (
-      Unix.sleep(1);
+    then begin
+      Unix.sleep 1;
       connect env (retries - 1)
-    )
+    end
     else exit 2
 
 let main env =
+  (* Check if a server is up *)
+  if not (ClientUtils.server_exists env.ServerMsg.root)
+  then ClientStart.start_server env.ServerMsg.root;
   let ic, oc = connect env num_build_retries in
   ServerMsg.cmd_to_channel oc (ServerMsg.BUILD env);
   try

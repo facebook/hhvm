83707867c590564067d4857b8df795e35a923ab8
Fred Emmott <fredemmott@fb.com>
2014-05-23 15:01:46 -0700

    Fix HH\autoload_set_paths with nested maps
    Summary: fixes facebook/hhvm#1259
    
    Reviewed By: @ptarjan
    
    Differential Revision: D1347346

--- hphp/test/slow/autoload/autoload_set_paths_weird.php
+++ hphp/test/slow/autoload/autoload_set_paths_weird.php
@@ -1,15 +1,15 @@
 <?hh
 
-HH\autoload_set_paths(false, '');
-HH\autoload_set_paths(1, '');
+var_dump(HH\autoload_set_paths(false, ''));
+var_dump(HH\autoload_set_paths(1, ''));
 class A {};
-HH\autoload_set_paths(new A, '');
-HH\autoload_set_paths(STDOUT, '');
+var_dump(HH\autoload_set_paths(new A(), ''));
+var_dump(HH\autoload_set_paths(STDOUT, ''));
 
 $map = Map {
   'function' => [],
   'failure' => function ($kind, $name) {
     echo "Autoload $kind $name\n";
 }};
-HH\autoload_set_paths($map, '');
+var_dump(HH\autoload_set_paths($map, ''));
 b();

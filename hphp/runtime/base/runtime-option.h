be654dd35a50b0c5a6918b376b5d168fdd00be25
ptarjan <ptarjan@fb.com>
2014-05-28 09:16:49 -0700

    turn off HHIRInlineFrameOpts
    It was causing lots of crashes due to a race between the JIT and the
    interpreter during warmup. It is fixed in trunk but porting the trunk
    fix over to 3.1 is too much risk.

--- hphp/runtime/base/runtime-option.h
+++ hphp/runtime/base/runtime-option.h
@@ -441,7 +441,7 @@ public:
   F(uint32_t, HHIRAlwaysInlineMaxCost, 10)                              \
   F(uint32_t, HHIRInliningMaxDepth,    4)                               \
   F(uint32_t, HHIRInliningMaxReturnDecRefs, 3)                          \
-  F(bool, HHIRInlineFrameOpts,         true)                            \
+  F(bool, HHIRInlineFrameOpts,         false)                           \
   /* 1 (the default) gives most asserts. 2 adds less commonly           \
    * useful/more expensive asserts. */                                  \
   F(uint32_t, HHIRGenerateAsserts,     debug)                           \

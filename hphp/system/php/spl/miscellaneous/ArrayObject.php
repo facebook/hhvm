3df306b1919bb120a46b15b5648cb7d28ee1492c
Joel Marcey <joelm@fb.com>
2014-06-03 16:55:34 -0700

    Spacing nit in offsetUnset()
    Summary: A groundbreaking space
    
    Reviewed By: @fredemmott
    
    Differential Revision: D1361520

--- hphp/system/php/spl/miscellaneous/ArrayObject.php
+++ hphp/system/php/spl/miscellaneous/ArrayObject.php
@@ -290,7 +290,7 @@ class ArrayObject implements IteratorAggregate, ArrayAccess,
    * @return     mixed   No value is returned.
    */
   public function offsetUnset($index) {
-    if(!$this->offsetExists($index)) {
+    if (!$this->offsetExists($index)) {
       [][$index]; // Try to access wrong key so a Notice is raised
       return;
     }

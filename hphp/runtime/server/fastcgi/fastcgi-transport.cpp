740f281428f362d70b21fa874db316f20a6b925f
Paul Tarjan <ptarjan@fb.com>
2014-06-10 13:48:11 -0700

    remove anything after a ? too
    Summary: Heroku is running into this problem with their current apache rewrite rules
    
    Reviewed By: @JoelMarcey
    
    Differential Revision: D1374358

--- hphp/runtime/server/fastcgi/fastcgi-transport.cpp
+++ hphp/runtime/server/fastcgi/fastcgi-transport.cpp
@@ -434,7 +434,7 @@ void FastCGITransport::onHeadersComplete() {
     m_pathTranslated = getRawHeader(s_scriptFilename);
   }
 
-  // do a check for mod_proxy_cgi and remove the start portion of the string
+  // do a check for mod_proxy_fcgi and remove the extra portions of the string
   const std::string modProxy = "proxy:fcgi://";
   if (m_pathTranslated.find(modProxy) == 0) {
     m_pathTranslated = m_pathTranslated.substr(modProxy.length());
@@ -442,6 +442,11 @@ void FastCGITransport::onHeadersComplete() {
     int slashPos = m_pathTranslated.find('/');
     if (slashPos != String::npos) {
       m_pathTranslated = m_pathTranslated.substr(slashPos);
+    }
+    // remove everything after the first ?
+    int questionPos = m_scriptFilename.find('?');
+    if (questionPos != String::npos) {
+      m_scriptFilename = m_scriptFilename.substr(0, questionPos);
     }
   }
 

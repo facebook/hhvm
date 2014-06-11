ea7c447c00e964f0ae62e0ee637484cd333d4465
Yaacov Akiba Slama <ya@slamail.org>
2014-06-11 12:28:34 +0300

    Use localhost instead of 127.0.0.1 in the test

--- hphp/test/slow/ext_memcache/get_compressed.php
+++ hphp/test/slow/ext_memcache/get_compressed.php
@@ -8,12 +8,12 @@ $tmp_object->int_attr = 123;
 $serialized = serialize($tmp_object);
 $compressed = zlib_encode($serialized, ZLIB_ENCODING_DEFLATE);
 
-$socket = stream_socket_client('127.0.0.1:11211');
+$socket = stream_socket_client('localhost:11211');
 fwrite($socket, "set ". $key . " 3 0 " . strlen($compressed) . "\r\n" . $compressed . "\r\n" );
 fclose($socket);
 
 $memcache = new Memcache;
-$memcache->addServer('127.0.0.1', 11211);
+$memcache->addServer('localhost', 11211);
 $r = $memcache->get($key);
 var_dump($r);
 $memcache->delete($key);

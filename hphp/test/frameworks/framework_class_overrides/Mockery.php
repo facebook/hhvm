80d295e508533d6144226411777c417ea04eefea
Fred Emmott <fredemmott@fb.com>
2014-06-13 14:42:29 -0700

    Get mockery at 100% on FB builds
    Summary: Cause is https://github.com/facebook/hhvm/issues/2937
    
    Reviewed By: @JoelMarcey
    
    Differential Revision: D1382810

--- hphp/test/frameworks/framework_class_overrides/Mockery.php
+++ hphp/test/frameworks/framework_class_overrides/Mockery.php
@@ -6,7 +6,7 @@ class Mockery extends Framework {
     /* Mockery tries stubbing out part of Mongo, which we have
      * in Zend compat, but it's not at a usable state yet.'
      */
-    $tc = get_runtime_build().' -vEval.EnableZendCompat=false'.
+    $tc = get_runtime_build().' -c '.__DIR__.'/Mockery.ini'.
           ' '.__DIR__.'/../vendor/bin/phpunit';
     parent::__construct($name, $tc);
   }

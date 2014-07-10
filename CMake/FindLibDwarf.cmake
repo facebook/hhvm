f420f7ea181daaefbea31be0b028ede3a92ced7a
James Miller <james@aatch.net>
2014-07-09 20:11:36 -0700

    libdwarf, libpam, libelf, bootstrap cmake fixes
    Summary: First: libdwarf. libdwarf seems to have no problem making breaking
    changes and provides no easy way to even test the version you're
    building against. I have added two tests in `FindLibDwarf.cmake` to
    detect a change to function type signature and to detect the removal
    of `dwarf_producer_init_c`. Hopefully this will work on all systems.
    
    The recent change to use more system libraries broke the build of the
    `gen-*` bootstrap utilities, as the targets were not updated to use
    the system `double-conversion` library and instead attempted to use
    the (unbuilt) local one.
    
    Commit 83cbca11c1 made linking to libpam happen only on OSX. This
    breaks the build on ArchLinux and openSUSE at least. I reverted this
    change given that detecting whether or not you need to link to it is
    quite difficult.
    Closes #3134
    
    Reviewed By: @ptarjan
    
    Differential Revision: D1427288
    
    Pulled By: @fredemmott

--- CMake/FindLibDwarf.cmake
+++ CMake/FindLibDwarf.cmake
@@ -52,11 +52,54 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibDwarf DEFAULT_MSG
 
 if (LIBDWARF_LIBRARIES AND LIBDWARF_INCLUDE_DIRS)
   set(CMAKE_REQUIRED_INCLUDES ${LIBDWARF_INCLUDE_DIRS})
-  set(CMAKE_REQUIRED_LIBRARIES ${LIBDWARF_LIBRARIES})
-  include(CheckSymbolExists)
-  CHECK_SYMBOL_EXISTS(dwarf_encode_leb128 "libdwarf.h" LIBDWARF_HAVE_ENCODE_LEB128)
+  set(CMAKE_REQUIRED_LIBRARIES ${LIBDWARF_LIBRARIES} ${LIBELF_LIBRARIES})
+
+  # libdwarf makes breaking changes occasionally and doesn't provide an easy
+  # way to test for them. The following checks should detect the changes and
+  # pass that information on accordingly.
+  INCLUDE(CheckCSourceCompiles)
+  CHECK_C_SOURCE_COMPILES("#include <libdwarf.h>
+  int dwarfCallback(const char * a, int b, Dwarf_Unsigned c,
+  Dwarf_Unsigned d, Dwarf_Unsigned e, Dwarf_Unsigned f,
+  Dwarf_Unsigned * g, Dwarf_Ptr h, int * i) {}
+int main() {
+  dwarf_producer_init_c(0, dwarfCallback, 0, 0, 0, 0);
+  return 0;
+}" DW_INIT_C)
+
+  if (NOT DW_INIT_C)
+	CHECK_C_SOURCE_COMPILES("#include <libdwarf.h>
+  int dwarfCallback(const char * a, int b, Dwarf_Unsigned c,
+  Dwarf_Unsigned d, Dwarf_Unsigned e, Dwarf_Unsigned f,
+  Dwarf_Unsigned * g, Dwarf_Ptr h, int * i) {}
+  int main() {
+	dwarf_producer_init(0, dwarfCallback, 0, 0, 0, 0, 0, 0, 0, 0);
+    return 0;
+  }" DW_INIT)
+	if (DW_INIT)
+	  set(LIBDWARF_CONST_NAME 1)
+	  set(LIBDWARF_USE_INIT_C 0)
+	else()
+	  set(LIBDWARF_CONST_NAME 0)
+	  set(LIBDWARF_USE_INIT_C 1)
+	endif()
+  else()
+	set(LIBDWARF_CONST_NAME 1)
+	set(LIBDWARF_USE_INIT_C 1)
+  endif()
+endif()
+
+if(LIBDWARF_CONST_NAME)
+  message(STATUS "libdwarf uses const char* type")
+else()
+  message(STATUS "libdwarf uses char* type")
+endif()
+if(LIBDWARF_USE_INIT_C)
+  message(STATUS "libdwarf has dwarf_producer_init_c")
+else()
+  message(STATUS "libdwarf does not have dwarf_producer_init_c, using dwarf_producer_init")
 endif()
 
 mark_as_advanced(LIBDW_INCLUDE_DIR DWARF_INCLUDE_DIR)
 mark_as_advanced(LIBDWARF_INCLUDE_DIRS LIBDWARF_LIBRARIES)
-mark_as_advanced(LIBDWARF_HAVE_ENCODE_LEB128)
+mark_as_advanced(LIBDWARF_CONST_NAME LIBDWARF_USE_INIT_C)

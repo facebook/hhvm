73d7d54222a63c1e0fb07a41fe08e89a65c552f8
Craig Carnell <craig_carnell@hotmail.com>
2014-07-14 09:53:13 +0100

    add lz4compress etc. for backwards compatibility for renamed methods

---
+++ hphp/system/php/zlib/ext_zlib.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ *
+ * Provide compatibility for renamed lz4 function -
+ * lz4compress is now lz4_compress
+ * 
+ */
+function lz4compress(string $uncompressed) {
+	trigger_error("lz4compress is now depreciated in favour of lz4_compress", 
+								E_USER_WARNING);
+	lz4_compress($uncompressed);
+}
+
+/**
+ *
+ * Provide compatibility for renamed lz4 function -
+ * lz4hccompress is now lz4_hccompress
+ * 
+ */
+function lz4hccompress(string $uncompressed) {
+	trigger_error("lz4hccompress is now depreciated in favour of lz4_hccompress", 
+								E_USER_WARNING);
+	lz4_hccompress($uncompressed);
+}
+
+/**
+ *
+ * Provide compatibility for renamed lz4 function -
+ * lz4uncompress is now lz4_uncompress
+ * 
+ */
+function lz4uncompress(string $compressed) {
+	trigger_error("lz4uncompress is now depreciated in favour of lz4_uncompress", 
+								E_USER_WARNING);
+	lz4_uncompress($compressed);
+}

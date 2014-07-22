2296ad029bd6c038e3a5b7167bf8d4daca313f1f
Joe Marrama <jmarrama@box.com>
2014-07-22 14:58:04 -0700

    Fix the default ini value for "arg_separator.input" to be "&"
    This fixes #3256

--- hphp/runtime/base/request-injection-data.cpp
+++ hphp/runtime/base/request-injection-data.cpp
@@ -87,6 +87,9 @@ void RequestInjectionData::threadInit() {
                    "arg_separator.output", "&",
                    &m_argSeparatorOutput);
   IniSetting::Bind(IniSetting::CORE, IniSetting::PHP_INI_ALL,
+                   "arg_separator.input", "&",
+                   &m_argSeparatorInput);
+  IniSetting::Bind(IniSetting::CORE, IniSetting::PHP_INI_ALL,
                    "default_charset", RuntimeOption::DefaultCharsetName.c_str(),
                    &m_defaultCharset);
   IniSetting::Bind(IniSetting::CORE, IniSetting::PHP_INI_ALL,

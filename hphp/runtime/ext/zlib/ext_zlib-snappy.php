8df97df1e47f8433012e82eb0a0d36192ceefb85
Craig Carnell <craig_carnell@hotmail.com>
2014-07-23 10:26:00 +0100

    remove ext_zlib-snappy.php from system and adds wrapper functions to runtime

--- hphp/runtime/ext/zlib/ext_zlib-snappy.php
+++ hphp/runtime/ext/zlib/ext_zlib-snappy.php
@@ -22,3 +22,31 @@ function snappy_compress(string $data): mixed;
  */
 <<__Native>>
 function snappy_uncompress(string $data): mixed;
+
+/**
+ *
+ * This is a wrapper function as sncompress is now snappy_compress
+ *
+ * https://github.com/facebook/hhvm/pull/3258 - 23/07/2014
+ *
+ */
+<<__HipHopSpecific>>
+function sncompress(string $data) {
+  trigger_error("sncompress is now depreciated in favour of snappy_compress",
+                 E_USER_WARNING);
+  return snappy_compress($uncompressed);
+}
+
+/**
+ *
+ * This is a wrapper function as snuncompress is now snappy_uncompress
+ *
+ * https://github.com/facebook/hhvm/pull/3258 - 23/07/2014
+ *
+ */
+<<__HipHopSpecific>>
+function snuncompress(string $data) {
+  trigger_error("snuncompress is now depreciated in favour of snappy_uncompress",
+                E_USER_WARNING);
+  return snappy_uncompress($compressed);
+}

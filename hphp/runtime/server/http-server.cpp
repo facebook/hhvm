16f06eedaf5c1bd3dc8d74937b2debff6529479d
Reeze Xia <reeze@php.net>
2014-07-24 23:07:16 +0800

    use shorter variable name

--- hphp/runtime/server/http-server.cpp
+++ hphp/runtime/server/http-server.cpp
@@ -515,10 +515,10 @@ void HttpServer::dropCache() {
 }
 
 void HttpServer::checkMemory() {
-  int64_t used_mem = Process::GetProcessRSS(Process::GetProcessId()) * 1024 * 1024;
+  int64_t used = Process::GetProcessRSS(Process::GetProcessId()) * 1024 * 1024;
   if (RuntimeOption::MaxRSS > 0 && used_mem > RuntimeOption::MaxRSS) {
     Logger::Error("ResourceLimit.MaxRSS %ld reached %ld used, exiting",
-                  RuntimeOption::MaxRSS, used_mem);
+                  RuntimeOption::MaxRSS, used);
     stop();
   }
 }

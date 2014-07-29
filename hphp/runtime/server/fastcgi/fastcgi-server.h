43c050dfb2148ed031201d807d76f04b17254c20
Shivanker Goel <shivanker@fb.com>
2014-07-28 17:41:32 -0700

    Changed signature of onNewConnection in Acceptor to take non-const TransportInfo
    Summary: We need to modify TransportInfo in non open source code for
    integrating SSL Fingerprinting.
    
    Reviewed By: dcsommer
    
    Differential Revision: D1462392

--- hphp/runtime/server/fastcgi/fastcgi-server.h
+++ hphp/runtime/server/fastcgi/fastcgi-server.h
@@ -53,7 +53,7 @@ public:
     apache::thrift::async::TAsyncSocket::UniquePtr sock,
     const apache::thrift::transport::TSocketAddress* peerAddress,
     const std::string& nextProtocolName,
-    const facebook::proxygen::TransportInfo& tinfo) override;
+    facebook::proxygen::TransportInfo& tinfo) override;
   virtual void onConnectionsDrained() override;
 
 private:

8074b4f27eda3e938327424883633b14d3b7f18b
Bert Maher <bertrand@fb.com>
2014-07-30 20:51:35 -0700

    These are flaking out for me locally
    Reviewed By: @swtaarrs
    
    Differential Revision: D1468898

---
+++ hphp/test/zend/flaky/ext/spl/tests/SplFileObject_fgetcsv_basic.php
@@ -0,0 +1,16 @@
+<?php
+$fp = fopen('SplFileObject__fgetcsv1.csv', 'w+');
+fputcsv($fp, array(
+	'field1',
+	'field2',
+	'field3',
+	5
+));
+fclose($fp);
+
+$fo = new SplFileObject('SplFileObject__fgetcsv1.csv');
+var_dump($fo->fgetcsv());
+?>
+<?php
+unlink('SplFileObject__fgetcsv1.csv');
+?>
---
+++ hphp/test/zend/flaky/ext/spl/tests/SplFileObject_fgetcsv_enclosure_error.php
@@ -0,0 +1,16 @@
+<?php
+$fp = fopen('SplFileObject__fgetcsv5.csv', 'w+');
+fputcsv($fp, array(
+	'field1',
+	'field2',
+	'field3',
+	5
+), ',', '"');
+fclose($fp);
+
+$fo = new SplFileObject('SplFileObject__fgetcsv5.csv');
+var_dump($fo->fgetcsv(',', 'invalid'));
+?>
+<?php
+unlink('SplFileObject__fgetcsv5.csv');
+?>

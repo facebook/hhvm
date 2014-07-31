efbb8019463258ce143a2889c267c7d589ecb5eb
bauerj <bauerj@bauerj.eu>
2014-07-31 17:36:46 +0200

    New test case for imagecreatefromwebp

--- hphp/test/slow/ext_gd/imagewebp.php
+++ hphp/test/slow/ext_gd/imagewebp.php
@@ -1,5 +1,5 @@
 <?php
-$filename= 'php.webp';
+$filename= './php.webp';
 
 // Create a blank image and add some text
 $im = imagecreatetruecolor(380, 60);
@@ -8,13 +8,14 @@ $text_color = imagecolorallocate($im, 233, 66, 91);
 imagestring($im, 2, 9, 9,  'I\'ve got 99 problems but WebP ain\'t one.', $text_color);
 
 // Save the image
-imagewebp($im, $filename);
+imagewebp($im, $filename, 99);
 
 imagedestroy($im);
 
-//We could check the MD5-hash here but maybe libvpx will get some
-//improvements in the future that would change the output file.
-echo filesize($filename)."\n";
+// We could check the MD5-hash here but maybe libvpx will get some
+// improvements in the future that would change the output file.
+// We just check if the file is a valid WebP-File and has approximately the right size.
+echo floor(filesize($filename)/100)*100, "\n";
 $fp = fopen($filename, 'r');
 
 $data = fread($fp, 14);   // read 14 Bytes from the WebP-File, that contains the header

facebook/hhvm

commit 91b19180d1c30f587edc3bec06c91164cb6af9a9

Redis fixes and tests
Summary: Fixed:
* `processDoubleResponse()` Redis returns self::TYPE_BULK for double/float
* $cmd should always be lowercase in `client()` for $cmd == 'list' comparison
* `doEval` / `script` should return null on error
* `self::lastError` should be null by default
* Script `$subcmd` parameter should be lowercased for comparison.

Added:
* Tests for client LIST command
* Tests for `incr` `incrBy` `decr` `decrBy` `incrByFloat`
* Missing `processClientListResponse`
* Set `self::lastError` on script error and tests for this
* `script()` returns to be compatible with phpredis and tests for these.
Closes #2603

Reviewed By: @ptarjan

Differential Revision: D1314240

Pulled By: @JoelMarcey

hphp/system/php/redis/Redis.php
@@ -168,6 +168,7 @@ class Redis {
   }
 
   public function client($cmd, $arg = '') {
+    $cmd = strtolower($cmd);
     if (func_num_args() == 2) {
       $this->processCommand('CLIENT', $cmd, $arg);
     } else {
@@ -612,11 +613,12 @@ class Redis {
   }
 
   public function script($subcmd/* ... */) {
-    switch ($subcmd) {
+    switch (strtolower($subcmd)) {
       case 'flush':
       case 'kill':
         $this->processCommand('SCRIPT', $subcmd);
-        return $this->processVariantResponse();
+        $response = $this->processVariantResponse();
+        return ($response !== NULL) ? true : false;
       case 'load':
         if (func_num_args() < 2) {
           return false;
@@ -626,7 +628,8 @@ class Redis {
           return false;
         }
         $this->processCommand('SCRIPT', 'load', $script);
-        return $this->processVariantResponse();
+        $response = $this->processVariantResponse();
+        return ($response !== NULL) ? $response : false;
       case 'exists':
         $args = func_get_args();
         $args[0] = 'EXISTS';
@@ -677,7 +680,7 @@ class Redis {
   }
 
   public function clearLastError() {
-    $this->lastError = '';
+    $this->lastError = null;
     return true;
   }
 
@@ -897,7 +900,7 @@ class Redis {
   protected $retry_interval = 0;
   protected $persistent = false;
   protected $connection = null;
-  protected $lastError = '';
+  protected $lastError = null;
 
   protected $timeout_connect = 0;
   protected $timeout_seconds = 0;
@@ -1133,6 +1136,32 @@ class Redis {
     }
   }
 
+  protected function processClientListResponse() {
+    if ($this->mode !== self::ATOMIC) {
+      $this->multiHandler[] = [ 'cb' => [$this,'processClientListResponse'] ];
+      if (($this->mode === self::MULTI) && !$this->processQueuedResponse()) {
+        return false;
+      }
+      return $this;
+    }
+    $resp = $this->sockReadData($type);
+    if (($type !== self::TYPE_LINE) AND ($type !== self::TYPE_BULK)) {
+      return null;
+    }
+    $ret = [];
+    $pairs = explode(' ', trim($resp));
+    foreach ($pairs as $pair) {
+      $kv = explode('=', $pair, 2);
+      if (count($kv) == 1) {
+        $ret[] = $pair;
+      } else {
+        list($k, $v) = $kv;
+        $ret[$k] = $v;
+      }
+    }
+    return $ret;
+  }
+
   protected function processVariantResponse() {
     if ($this->mode !== self::ATOMIC) {
       $this->multiHandler[] = [ 'cb' => [$this,'processVariantResponse'] ];
@@ -1161,6 +1190,11 @@ class Redis {
         $ret[] = $this->doProcessVariantResponse();
       }
       return $ret;
+    }
+
+    if ($type === self::TYPE_ERR) {
+      $this->lastError = $resp;
+      return null;
     }
 
     return $resp;

hphp/test/slow/ext_redis/list.php.expect
@@ -13,3 +13,5 @@ int(2)
 string(11) "easy as 123"
 string(5) "apple"
 string(11) "easy as 123"
+bool(true)
+bool(true)

hphp/test/slow/ext_redis/evalevalSha.php
@@ -31,8 +31,21 @@ foreach (['_evalSha', 'evalSha', 'evaluateSha'] as $method) {
     echo $method . "\n";
     var_dump($r->$method($sha)); // Return integer -> 42
     // SHA1SUM of with parameters -> OK
-    var_dump($r->evalSha("c686f316aaf1eb01d5a4de1b0b63cd233010e63d",
+    var_dump($r->$method("c686f316aaf1eb01d5a4de1b0b63cd233010e63d",
                          [$key, 'bar'], 1));
+    $r->clearLastError();
     // No SHA1SUM -> NOSCRIPT error
     var_dump($r->$method("ffffffffffffffffffffffffffffffffffffffff"));
+    var_dump(gettype($r->getLastError()));
 }
+
+echo "script\n";
+var_dump($r->script('load','return 42'));
+var_dump($r->script('load','bad script'));
+var_dump($r->script('exists','1fa00e76656cc152ad327c13fe365858fd7be306',
+                    'ffffffffffffffffffffffffffffffffffffffff'));
+var_dump($r->script('flush'));
+var_dump($r->script('kill'));
+var_dump(gettype($r->getLastError()));
+$r->clearLastError();
+var_dump(gettype($r->getLastError()));

hphp/test/slow/ext_redis/evalevalSha.php.expect
@@ -100,12 +100,28 @@ array(4) {
 _evalSha
 int(42)
 string(2) "OK"
-string(45) "NOSCRIPT No matching script. Please use EVAL."
+NULL
+string(6) "string"
 evalSha
 int(42)
 string(2) "OK"
-string(45) "NOSCRIPT No matching script. Please use EVAL."
+NULL
+string(6) "string"
 evaluateSha
 int(42)
 string(2) "OK"
-string(45) "NOSCRIPT No matching script. Please use EVAL."
+NULL
+string(6) "string"
+script
+string(40) "1fa00e76656cc152ad327c13fe365858fd7be306"
+bool(false)
+array(2) {
+  [0]=>
+  int(1)
+  [1]=>
+  int(0)
+}
+bool(true)
+bool(false)
+string(6) "string"
+string(4) "NULL"

hphp/test/slow/ext_redis/incrdecr.php
@@ -1,0 +1,51 @@
+<?php
+
+include(__DIR__ . '/redis.inc');
+
+$r = NewRedisTestInstance();
+$key = GetTestKeyName(__FILE__);
+$r->delete($key);
+echo "incr/decr Atomic\n";
+var_dump($r->incr($key));
+var_dump($r->incr($key, 2));
+var_dump($r->incrBy($key, 3));
+var_dump($r->decr($key));
+var_dump($r->decr($key, 2));
+var_dump($r->decrBy($key, 3));
+echo "incr/decr Multi\n";
+var_dump(
+  $r->multi()
+    ->incr($key)
+    ->incr($key, 2)
+    ->incrBy($key, 3)
+    ->decr($key)
+    ->decr($key, 2)
+    ->decrBy($key, 3)
+    ->exec()
+);
+echo "incr/decr Pipeline\n";
+var_dump(
+  $r->pipeline()
+    ->incr($key)
+    ->incr($key, 2)
+    ->incrBy($key, 3)
+    ->decr($key)
+    ->decr($key, 2)
+    ->decrBy($key, 3)
+    ->exec()
+);
+
+$r->delete($key);
+echo "incrByFloat Atomic\n";
+var_dump($r->incrByFloat($key, 1.5));
+var_dump($r->incrByFloat($key, -1.5));
+echo "incrByFloat Multi\n";
+var_dump($r->multi()
+  ->incrByFloat($key, 1.5)
+  ->incrByFloat($key, -1.5)
+  ->exec());
+echo "incrByFloat Pipeline\n";
+var_dump($r->pipeline()
+  ->incrByFloat($key, 1.5)
+  ->incrByFloat($key, -1.5)
+  ->exec());

hphp/test/slow/ext_redis/incrdecr.php.expect
@@ -1,0 +1,54 @@
+incr/decr Atomic
+int(1)
+int(3)
+int(6)
+int(5)
+int(3)
+int(0)
+incr/decr Multi
+array(6) {
+  [0]=>
+  int(1)
+  [1]=>
+  int(3)
+  [2]=>
+  int(6)
+  [3]=>
+  int(5)
+  [4]=>
+  int(3)
+  [5]=>
+  int(0)
+}
+incr/decr Pipeline
+array(6) {
+  [0]=>
+  int(1)
+  [1]=>
+  int(3)
+  [2]=>
+  int(6)
+  [3]=>
+  int(5)
+  [4]=>
+  int(3)
+  [5]=>
+  int(0)
+}
+incrByFloat Atomic
+float(1.5)
+float(0)
+incrByFloat Multi
+array(2) {
+  [0]=>
+  float(1.5)
+  [1]=>
+  float(0)
+}
+incrByFloat Pipeline
+array(2) {
+  [0]=>
+  float(1.5)
+  [1]=>
+  float(0)
+}

hphp/test/slow/ext_redis/info.php
@@ -8,3 +8,6 @@ var_dump(count($info) > 35);
 var_dump($info['tcp_port'] == REDIS_PORT);
 $r->client('setname', 'hhvm-redis-client');
 var_dump($r->client('getname'));
+var_dump(count($r->client('LIST')) >= 1); // at least one connection
+$r2 = NewRedisTestInstance();
+var_dump(count($r->client('LIST')) >= 2); // at least two connections

commit c852c5ce5f0a32d0a8bb222d49f33ace9ecdaebf

Fixes for Redis extension and associated test suite
* Fix a couple of tests that were broken by 91b1918 adding two lines to the
  wrong expect file (list.php.expect instead of info.php.expect).
* Fixed hphp/test/slow/ext_redis/watch.php.expect to expect the correct
  behavior. (Tested by verifying with Zend + phpredis.)
* Make the key prefix set by the test suite include the md5 of the file path
  plus UNIX timestamp representing the test suite initialization. This makes
  the tests not break when the path exceeds the redis key length limit and it
  makes repeated test runs not conflict with one another.
* Make Redis::sRandMember apply the key prefix (if one exists).
* Make Redis::get() and Redis::h{m}get() return false rather than null for
  missing keys or fields, matching the behavior of phpredis.

Minimal test case for the false / null behavior:

    <?php
    $redis = new Redis;
    $redis->connect("localhost");
    var_dump($redis->get("nonexistent-key"));
    $redis->hMset('ice_cream', array('flavor' => 'vanilla'));
    var_dump($redis->hMget('ice_cream', array('flavor', 'nonexistent-field')));

Output with phpredis:

    bool(false)
    array(2) {
      ["flavor"]=>
      string(7) "vanilla"
      ["nonexistent-field"]=>
      bool(false)
    }

Output with HHVM:

    NULL
    array(2) {
      ["flavor"]=>
      string(7) "vanilla"
      ["nonexistent-field"]=>
      NULL
    }

Fixes https://bugzilla.wikimedia.org/show_bug.cgi?id=68841

hphp/system/php/redis/Redis.php
@@ -369,7 +369,7 @@ class Redis {
   /* Sets ---------------------------------------------------------------- */
 
   public function sRandMember($key, $count = null) {
-    $args = [$key];
+    $args = [$this->prefix($key)];
     if ($count !== null) {
        $args[] = $count;
     }
@@ -1220,8 +1220,11 @@ class Redis {
   protected function processSerializedResponse() {
     if ($this->mode === self::ATOMIC) {
       $resp = $this->sockReadData($type);
+      if ($resp === null) {
+        return false;
+      }
       return (($type === self::TYPE_LINE) OR ($type === self::TYPE_BULK))
-             ? $this->unserialize($resp) : null;
+             ? $this->unserialize($resp) : false;
     }
     $this->multiHandler[] = [ 'cb' => [$this,'processSerializedResponse'] ];
     if (($this->mode === self::MULTI) && !$this->processQueuedResponse()) {
@@ -1307,7 +1310,7 @@ class Redis {
       if ($unser AND (($lineNo % $unser) == 0)) {
         $val = $this->unserialize($val);
       }
-      $ret[] = $val;
+      $ret[] = $val !== null ? $val : false;
     }
     return $ret;
   }
@@ -1370,7 +1373,7 @@ class Redis {
       if ($unser_val) {
         $val = $this->unserialize($val);
       }
-      $ret[$key] = $val;
+      $ret[$key] = $val !== null ? $val : false;
     }
     return $ret;
   }

hphp/test/slow/ext_redis/hmsetget.php.expect
@@ -27,7 +27,7 @@ array(3) {
 string(3) "bar"
 string(5) "boom1"
 string(5) "boom2"
-NULL
+bool(false)
 array(4) {
   ["foo"]=>
   string(3) "bar"
@@ -36,5 +36,5 @@ array(4) {
   ["baz2"]=>
   string(5) "boom2"
   ["nofield"]=>
-  NULL
+  bool(false)
 }

hphp/test/slow/ext_redis/hsetget.php.expect
@@ -20,4 +20,4 @@ array(2) {
 }
 string(3) "bar"
 string(4) "boom"
-NULL
+bool(false)

hphp/test/slow/ext_redis/info.php.expect
@@ -4,3 +4,5 @@ bool(true)
 bool(true)
 bool(true)
 string(17) "hhvm-redis-client"
+bool(true)
+bool(true)

hphp/test/slow/ext_redis/list.php.expect
@@ -13,5 +13,3 @@ int(2)
 string(11) "easy as 123"
 string(5) "apple"
 string(11) "easy as 123"
-bool(true)
-bool(true)

hphp/test/slow/ext_redis/redis.inc
@@ -21,7 +21,7 @@ function NewRedisTestInstance($status = false) {
 }
 
 function GetTestKeyName($test, $rand = false) {
-  $name = 'REDIS_TEST:' . preg_replace('/[^a-zA-Z0-9]/', '-', $test);
-  if ($rand) $name .= ':' . rand(0,1000000);
-  return $name;
+  static $ts;
+  $ts = $ts ?: time();
+  return 'REDIS_TEST:' . md5( $test . $ts . ( $rand ? '' : rand(0,1000000) ) );
 }

hphp/test/slow/ext_redis/setExtended.php.expect
@@ -5,11 +5,11 @@ bool(false)
 bool(true)
 bool(false)
 bool(true)
-NULL
+bool(false)
 bool(true)
 bool(true)
-NULL
+bool(false)
 bool(true)
 string(2) "10"
-NULL
+bool(false)
 Redis Exception: Invalid set options: nx and xx may not be specified at the same time

hphp/test/slow/ext_redis/srandmember.php.expect
@@ -2,6 +2,6 @@ string(3) "bar"
 array(0) {
 }
 array(1) {
-  [0] =>
+  [0]=>
   string(3) "bar"
 }

hphp/test/slow/ext_redis/watch.php
@@ -16,7 +16,7 @@ var_dump($r1->set($key1, $value)); //true
 var_dump($r1->watch($key1)); //true
 $checkValue = $r1->get($key1);
 var_dump($checkValue); //6379
-if ($checkValue !== null && $checkValue == $value) {
+if ($checkValue !== false && $checkValue == $value) {
   var_dump($r1->multi()->del($key1)->exec()); //[1]
 }
 
@@ -25,6 +25,6 @@ var_dump($r1->watch($key1));
 $checkValue = $r1->get($key1);
 var_dump($r2->set($key1, 'different value'));
 var_dump($checkValue);
-if ($checkValue !== null && $checkValue == $value) {
+if ($checkValue !== false && $checkValue == $value) {
   var_dump($r1->multi()->del($key1)->exec());
 }

hphp/test/slow/ext_redis/watch.php.expect
@@ -11,5 +11,5 @@ bool(true)
 string(4) "6379"
 array(1) {
   [0]=>
-  int(0)
+  int(1)
 }
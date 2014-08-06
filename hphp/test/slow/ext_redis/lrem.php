b29887fd11335fae7156188205da1f5d1758c28e
Robert R. Henry <rrh@newrelic.com>
2014-08-05 22:58:52 -0700

    Rename var_Dump to var_dump to fix a stupid replicated typo. Shorten commentary on one line to bring < 80 chars.

--- hphp/test/slow/ext_redis/lrem.php
+++ hphp/test/slow/ext_redis/lrem.php
@@ -11,18 +11,18 @@ $r->rPush($key, "A");
 $r->rPush($key, "B");
 $r->rPush($key, "C");
 
-var_Dump($r->lGet($key, 0));   // A
-var_Dump($r->lGet($key, 1));   // B
-var_Dump($r->lGet($key, 2));   // C
-var_Dump($r->lGet($key, -1));  // C
+var_dump($r->lGet($key, 0));   // A
+var_dump($r->lGet($key, 1));   // B
+var_dump($r->lGet($key, 2));   // C
+var_dump($r->lGet($key, -1));  // C
 
-var_Dump($r->lIndex($key, 0));   // A
-var_Dump($r->lIndex($key, 1));   // B
-var_Dump($r->lIndex($key, 2));   // C
+var_dump($r->lIndex($key, 0));   // A
+var_dump($r->lIndex($key, 1));   // B
+var_dump($r->lIndex($key, 2));   // C
 
-var_Dump($r->lRemove($key, "B", 1));  // 1 item requested, 1 removed
-var_Dump($r->lRemove($key, "B", 1));  // 1 item requested, 0 removed (it's already gone)
+var_dump($r->lRemove($key, "B", 1));  // 1 item req, 1 rm'ed
+var_dump($r->lRemove($key, "B", 1));  // 1 item req, 0 rm'ed (it's already gone)
 
-var_Dump($r->lIndex($key, 0));   // A
-var_Dump($r->lIndex($key, 1));   // C
+var_dump($r->lIndex($key, 0));   // A
+var_dump($r->lIndex($key, 1));   // C
 

facebook/hhvm

commit b53a42309061876adf275c82ff87df3981fe1bf2

Cygwin/Windows cmake build fixes
Summary: Alterations to cmake files to make building with cmake successful with cygwin, contains both generic "windows' fixes and cygwin specific ones

Reviewed By: @sgolemon

Differential Revision: D1420857

CMake/HPHPSetup.cmake
@@ -26,6 +26,10 @@ if (APPLE)
 elseif (IS_AARCH64)
   set(HHVM_ANCHOR_SYMS
     -Wl,--whole-archive ${HHVM_WHOLE_ARCHIVE_LIBRARIES} -Wl,--no-whole-archive)
+elseif(CYGWIN)
+  set(ENABLE_FASTCGI 0)
+  set(HHVM_ANCHOR_SYMS
+  -Wl,--whole-archive ${HHVM_WHOLE_ARCHIVE_LIBRARIES} -Wl,--no-whole-archive)
 else()
   set(ENABLE_FASTCGI 1)
   set(HHVM_ANCHOR_SYMS
@@ -76,10 +80,37 @@ include(HPHPCompiler)
 include(HPHPFunctions)
 include(HPHPFindLibs)
 
-add_definitions(-D_REENTRANT=1 -D_PTHREADS=1 -D__STDC_FORMAT_MACROS -DFOLLY_HAVE_WEAK_SYMBOLS=1)
+# check for weak symbols
+INCLUDE(CheckCSourceCompiles) 
+CHECK_C_SOURCE_COMPILES("
+    extern \"C\" void configure_link_extern_weak_test() __attribute__((weak));
+    int main(int argc, char** argv) {
+        return configure_link_extern_weak_test == nullptr;
+    }
+"
+    FOLLY_HAVE_WEAK_SYMBOLS
+)
+
+if(FOLLY_HAVE_WEAK_SYMBOLS)
+  add_definitions(-DFOLLY_HAVE_WEAK_SYMBOLS=1)
+else()
+  add_definitions(-DFOLLY_HAVE_WEAK_SYMBOLS=0)
+endif()
+
+add_definitions(-D_REENTRANT=1 -D_PTHREADS=1 -D__STDC_FORMAT_MACROS)
 
 if (LINUX)
   add_definitions(-D_GNU_SOURCE)
+endif()
+
+# cygwin headers are easily confused
+if(CYGWIN)
+  add_definitions(-D_GLIBCXX_USE_C99=1)
+endif()
+
+if(MSVC OR CYGWIN OR MINGW)
+  add_definitions(-DNOUSER=1 -DGLOG_NO_ABBREVIATED_SEVERITIES)
+  add_definitions(-DWIN32_LEAN_AND_MEAN)
 endif()
 
 if(${CMAKE_BUILD_TYPE} MATCHES "Release")
@@ -111,7 +142,7 @@ if(ENABLE_FULL_SETLINE)
   add_definitions(-DENABLE_FULL_SETLINE=1)
 endif()
 
-if(APPLE OR FREEBSD)
+if(APPLE OR FREEBSD OR CYGWIN OR MSVC OR MINGW)
   add_definitions(-DSKIP_USER_CHANGE=1)
 endif()
 

.gitignore
@@ -83,3 +83,7 @@ CPackSourceConfig.cmake
 .ninja_deps
 .ninja_log
 *.ninja
+
+# windows files
+*.exe
+*.rc

CMake/FindLibDwarf.cmake
@@ -23,6 +23,7 @@ find_path (DWARF_INCLUDE_DIR
     /usr/include
     /usr/include/libdwarf
     /usr/local/include
+    /usr/local/include/libdwarf
     /opt/local/include
     /sw/include
     ENV CPATH) # PATH and INCLUDE will also work
@@ -33,7 +34,7 @@ endif ()
 
 find_library (LIBDWARF_LIBRARIES
   NAMES
-    dwarf
+    dwarf libdwarf
   PATHS
     /usr/lib
     /usr/local/lib

CMake/HPHPFunctions.cmake
@@ -96,19 +96,30 @@ macro(MYSQL_SOCKET_SEARCH)
 endmacro()
 
 function(append_systemlib TARGET SOURCE SECTNAME)
-  if (APPLE)
-    set(${TARGET}_SLIBS ${${TARGET}_SLIBS} -Wl,-sectcreate,__text,${SECTNAME},${SOURCE} PARENT_SCOPE)
+  if(CYGWIN OR MSVC OR MINGW)
+    # for each library append the following line to embed.rc
+    # $sectionname RCDATA "$source"
+    add_custom_command(TARGET generate_rc
+      COMMAND echo "${SECTNAME} RCDATA \"${SOURCE}\"" >> embed.rc
+      COMMENT "Adding ${SOURCE} as ${SECTNAME} to embed.rc"
+      VERBATIM)
   else()
-    set(${TARGET}_SLIBS ${${TARGET}_SLIBS} "--add-section" "${SECTNAME}=${SOURCE}" PARENT_SCOPE)
+    if (APPLE)
+      set(${TARGET}_SLIBS ${${TARGET}_SLIBS} -Wl,-sectcreate,__text,${SECTNAME},${SOURCE} PARENT_SCOPE)
+    else()
+      set(${TARGET}_SLIBS ${${TARGET}_SLIBS} "--add-section" "${SECTNAME}=${SOURCE}" PARENT_SCOPE)
+    endif()
+    # Add the systemlib file to the "LINK_DEPENDS" for the systemlib, this will cause it
+    # to be relinked and the systemlib re-embedded
+    set_property(TARGET ${TARGET} APPEND PROPERTY LINK_DEPENDS ${SOURCE})
   endif()
-  # Add the systemlib file to the "LINK_DEPENDS" for the systemlib, this will cause it
-  # to be relinked and the systemlib re-embedded
-  set_property(TARGET ${TARGET} APPEND PROPERTY LINK_DEPENDS ${SOURCE})
 endfunction(append_systemlib)
 
 function(embed_systemlibs TARGET DEST)
   if (APPLE)
     target_link_libraries(${TARGET} ${${TARGET}_SLIBS})
+  elseif(CYGWIN OR MSVC OR MINGW)
+    # Do nothing, resource compiler will do the work on embed.rc
   else()
     add_custom_command(TARGET ${TARGET} POST_BUILD
       COMMAND "objcopy"

hphp/hhvm/CMakeLists.txt
@@ -2,10 +2,41 @@ set(CXX_SOURCES)
 auto_sources(files "*.cpp" "")
 list(APPEND CXX_SOURCES ${files})
 
+# Windows targets use a generate rc file for embedding libraries
+if(CYGWIN OR MSVC OR MING)
+  add_custom_command(
+    OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/generate.rc
+    COMMAND echo \"// THIS IS A GENERATED FILE \" > ${CMAKE_CURRENT_SOURCE_DIR}/embed.rc
+    DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/embed.rc
+    COMMENT "generating embed.rc")
+
+  # this is a cmake trick to allow items added later to depend properly
+  add_custom_target(generate_rc
+    DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/generate.rc
+    COMMENT "Generating RC file")
+
+    list(APPEND CXX_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/embed.rc)
+    set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES
+      ${CMAKE_CURRENT_SOURCE_DIR}/embed.rc)
+endif()
+
 add_executable(hhvm ${CXX_SOURCES})
 target_link_libraries(hhvm ${HHVM_LINK_LIBRARIES})
+
+# cygwin has an issue with linking to crypt and intl
+# this is the only way the redirection stuff works
+if(CYGWIN)
+  target_link_libraries(hhvm crypt)
+  target_link_libraries(hhvm intl)
+endif()
+
 embed_all_systemlibs(hhvm "${CMAKE_CURRENT_BINARY_DIR}/.." "${CMAKE_CURRENT_BINARY_DIR}/hhvm")
-add_dependencies(hhvm systemlib)
+if(CYGWIN OR MSVC OR MINGW)
+  add_dependencies(hhvm generate_rc)
+else()
+  add_dependencies(hhvm systemlib)
+endif()
+
 if (ENABLE_COTIRE)
   cotire(hhvm)
 endif()

hphp/runtime/ext_hhvm/CMakeLists.txt
@@ -14,6 +14,10 @@ if (APPLE)
   set(EXT_HHVM_OS "darwin")
 endif()
 
+if (CYGWIN)
+  set(EXT_HHVM_OS "cygwin")
+endif()
+
 macro(EXT_HHVM_FILE SOURCES HEADERS REL EHHVM_OS EHHVM_ARCH SOURCE_REL
       BUILD_TARGET)
   set(SOURCE_ABS "${CMAKE_CURRENT_SOURCE_DIR}/${SOURCE_REL}")

hphp/tools/bootstrap/.gitignore
@@ -2,3 +2,4 @@ Makefile
 gen-ext-hhvm
 gen-infotabs
 gen-class-map
+*.exe

hphp/tools/bootstrap/gen-ext-hhvm.sh
@@ -12,6 +12,11 @@ if [ "$1" = "darwin" ]; then
       awk '{ if ($2=="g") print $8 }' | \
       $GEN_EXT_HHVM \
       $2 $5 $4 $DIR/../../system/idl/*.idl.json
+elif [ "$1" = "cygwin" ]; then
+  objdump --section=".text" -t $3 | \
+      awk '{ if(NF > 1) print $NF }' | \
+      $GEN_EXT_HHVM \
+      $2 $5 $4 $DIR/../../system/idl/*.idl.json
 else
   readelf -s -W $3 | grep 'FUNC.*GLOBAL' | \
       sed -e 's/^.*DEFAULT[0-9 ]*//' | \

commit dc9853c5fe279351443920769fcf37d3c6d842a8

Fix regression on OS X introduced by b53a42309061876adf275c82ff87df3981fe1bf2
OS X has some issues with __attribute__((weak)), see http://glandium.org/blog/?p=2764 for more information.

CMake/HPHPSetup.cmake
@@ -88,15 +88,24 @@ include(HPHPCompiler)
 include(HPHPFunctions)
 include(HPHPFindLibs)
 
-# check for weak symbols
-CHECK_CXX_SOURCE_COMPILES("
-    extern \"C\" void configure_link_extern_weak_test() __attribute__((weak));
-    int main(int argc, char** argv) {
-        return configure_link_extern_weak_test == nullptr;
-    }
-"
-    FOLLY_HAVE_WEAK_SYMBOLS
-)
+# Weak linking on Linux, Windows, and OS X all work somewhat differently. The following test
+# works well on Linux and Windows, but fails for annoying reasons on OS X, and even works
+# differently on different releases of OS X, cf. http://glandium.org/blog/?p=2764. Getting
+# the test to work properly on OS X would require an APPLE check anyways, so just hardcode
+# OS X as "we know weak linking works".
+if(APPLE)
+  set(FOLLY_HAVE_WEAK_SYMBOLS 1)
+else()
+  # check for weak symbols
+  CHECK_CXX_SOURCE_COMPILES("
+      extern \"C\" void configure_link_extern_weak_test() __attribute__((weak));
+      int main(int argc, char** argv) {
+          return configure_link_extern_weak_test == nullptr;
+      }
+  "
+      FOLLY_HAVE_WEAK_SYMBOLS
+  )
+endif()
 
 if(FOLLY_HAVE_WEAK_SYMBOLS)
   add_definitions(-DFOLLY_HAVE_WEAK_SYMBOLS=1)
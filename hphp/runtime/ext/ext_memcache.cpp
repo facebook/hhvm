eb57215d7b867b3a411300f7586040161c2d8d3b
porsihe <porsihe@gmail.com>
2014-08-12 18:46:15 +0800

    Update ext_memcache.cpp

--- hphp/runtime/ext/ext_memcache.cpp
+++ hphp/runtime/ext/ext_memcache.cpp
@@ -81,9 +81,9 @@ static bool ini_on_update_hash_strategy(const std::string& value) {
 
 static bool ini_on_update_hash_function(const std::string& value) {
   if (!strncasecmp(value.data(), "crc32", sizeof("crc32"))) {
-    MEMCACHEG(hash_strategy) = "crc32";
+    MEMCACHEG(hash_function) = "crc32";
   } else if (!strncasecmp(value.data(), "fnv", sizeof("fnv"))) {
-    MEMCACHEG(hash_strategy) = "fnv";
+    MEMCACHEG(hash_function) = "fnv";
   }
   return false;
 }

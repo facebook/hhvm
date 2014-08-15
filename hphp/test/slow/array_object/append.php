be5c755780e52193e00d55cca9475381b3913c35
aude <aude.wiki@gmail.com>
2014-08-15 15:07:14 -0700

    ArrayObject::append() should delegate to ArrayObject::offsetSet()
    Summary: Match php5 implementation by delegating rather than direct manipulation of internal state.
    Fixes issue #3403.
    Closes https://github.com/facebook/hhvm/pull/3404
    
    Reviewed By: @fredemmott
    
    Differential Revision: D1486300
    
    Pulled By: svcscm

--- hphp/test/slow/array_object/append.php
+++ hphp/test/slow/array_object/append.php
@@ -1,6 +1,21 @@
 <?php
 
+class ExtendedArrayObject extends ArrayObject {
+  public function offsetSet($key, $value) {
+    $key = 'q1';
+    parent::offsetSet($key, $value);
+  }
+}
+
 $arrayobj = new ArrayObject(array('first','second','third'));
 $arrayobj->append('fourth');
 $arrayobj->append(array('five', 'six'));
+var_dump($arrayobj);
+
+$arrayobj = new ExtendedArrayObject(array('y'));
+$arrayobj->append('x');
+var_dump($arrayobj);
+
+$arrayobj = new ExtendedArrayObject(array('q2' => 'y'));
+$arrayobj->append('z');
 var_dump($arrayobj);

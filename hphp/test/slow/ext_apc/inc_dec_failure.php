cc78da37effcd7751c25038229f1a52eeed207ab
Tim Siebels <tim_siebels_aurich@yahoo.de>
2014-08-18 17:27:50 -0700

    Don't apc_inc/dec on non-numerics
    Summary: Closes https://github.com/facebook/hhvm/pull/3419
    
    Reviewed By: @ptarjan
    
    Differential Revision: D1488804

--- hphp/test/slow/ext_apc/inc_dec_failure.php
+++ hphp/test/slow/ext_apc/inc_dec_failure.php
@@ -2,3 +2,13 @@
 apc_delete('test');
 var_dump(apc_inc('test'));
 var_dump(apc_dec('test'));
+
+apc_store('x', 'x');
+
+var_dump(apc_inc('x'));
+var_dump(apc_dec('x'));
+
+apc_store('numeric_str', '1');
+
+var_dump(apc_inc('numeric_str'));
+var_dump(apc_dec('numeric_str'));

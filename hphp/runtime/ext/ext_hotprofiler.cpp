e12893a8c33f44950cfb27656d84c8a1ca14eb80
Robert R. Henry <rrh@newrelic.com>
2014-08-19 21:59:28 -0700

    Add support for registering and using an externally supplied Profiler, as for example from a 3rd party DSO.
    While I'm at it, plumb in instantiation of the pre-existing experimental
    MemoProfiler.
    
    This PR replaces https://github.com/facebook/hhvm/pull/3082. This PR
    is simpler than that proposed before, since the old work is supplanted
    in large part by recent work for debugger hooks.  There were too many
    git branch conflicts to warrant preserving the commentary of 3082,
    most of which is no longer relevant.

--- hphp/runtime/ext/ext_hotprofiler.cpp
+++ hphp/runtime/ext/ext_hotprofiler.cpp
@@ -1312,11 +1312,20 @@ bool ProfilerFactory::start(ProfilerKind kind,
   case ProfilerKind::XDebug:
     m_profiler = new XDebugProfiler();
     break;
+  case ProfilerKind::External:
+    if (m_external_profiler) {
+      m_profiler = m_external_profiler;
+    } else {
+      throw_invalid_argument(
+        "ProfilerFactory::setExternalProfiler() not yet called");
+      /* Control reaches here if !RuntimeOption::ThrowInvalidArguments. */
+    }
+    break;
   default:
     throw_invalid_argument("level: %d", kind);
     return false;
   }
-  if (m_profiler->m_successful) {
+  if (m_profiler && m_profiler->m_successful) {
     // This will be disabled automatically when the thread completes the request
     HPHP::EventHook::Enable();
     ThreadInfo::s_threadInfo->m_profiler = m_profiler;
@@ -1435,6 +1444,17 @@ void f_xhprof_enable(int flags/* = 0 */,
 
   if (flags & XhpTrace) {
     s_profiler_factory->start(ProfilerKind::Trace, flags);
+  } else if (flags & Memo) {
+    flags = 0;  /* flags are not used by MemoProfiler::MemoProfiler */
+    s_profiler_factory->start(ProfilerKind::Memo, flags);
+  } else if (flags & External) {
+    flags = TrackBuiltins | TrackCPU | TrackMemory;
+    for (ArrayIter iter(args); iter; ++iter) {
+      if (iter.first().toInt32() == 0) {
+         flags = iter.second().toInt32();
+      }
+    }
+    s_profiler_factory->start(ProfilerKind::External, flags);
   } else {
     s_profiler_factory->start(ProfilerKind::Hierarchical, flags);
   }

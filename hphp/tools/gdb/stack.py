916fa003a71064469cf4ca7b0de2332b7a2a71f3
Max Wang <mwang@fb.com>
2014-08-21 08:38:25 -0700

    Fix up walkstk command
    Summary: Two fixes to `walkstk`:
    
    - If HPHP::mcg is corrupted, use a simple heuristic (is the
      corresponding native frame a normal, nameless frame?) to decide if
      we're in the TC.  This complicates the logic a tad but makes the
      command more reliable.
    
    - Don't omit the initial frame.
    
    Reviewed By: @markw65
    
    Differential Revision: D1511373

--- hphp/tools/gdb/stack.py
+++ hphp/tools/gdb/stack.py
@@ -24,6 +24,7 @@ Filename and line number information is only included for C++ functions.
     def __init__(self):
         super(WalkstkCommand, self).__init__('walkstk', gdb.COMMAND_STACK)
 
+
     def print_frame(self, i, sp, rip, func, loc):
         out = '#%-2d %s @ 0x%x: %s' % (i, str(sp), int(str(rip)), func)
 
@@ -59,19 +60,40 @@ Filename and line number information is only included for C++ functions.
         if len(argv) == 2:
             rip = argv[1].cast(rip_type)
 
-        native_frame = gdb.newest_frame()
-
-        mcg = V('HPHP::JIT::mcg')
-        tc_base = mcg['code']['m_base']
-        tc_end = tc_base + mcg['code']['m_codeSize']
+        try:
+            mcg = V('HPHP::JIT::mcg')
+            tc_base = mcg['code']['m_base']
+            tc_end = tc_base + mcg['code']['m_codeSize']
+        except:
+            mcg = None
 
         i = 0
+        native_frame = gdb.newest_frame()
+        skip_tc = False  # Only used when we can't find HPHP::mcg.
+
+        # Munge `sp' so that it looks like the stack pointer that would point
+        # to it if we had another frame---this lets us promote the "increment"
+        # to the beginning of the loop, so that we don't miss the final frame.
+        sp = (sp, rip)
 
         while sp:
+            rip = sp[1]
+            sp = sp[0].cast(sp_type)
+
             func = '<unknown>'
             loc = None
 
-            in_tc = rip >= tc_base and rip < tc_end
+            if mcg is not None:
+                in_tc = rip >= tc_base and rip < tc_end
+            elif not skip_tc:
+                # TC frames look like unnamed normal native frames.
+                next_frame = native_frame.older()
+                in_tc = (next_frame is not None and
+                         next_frame.name() is None and
+                         next_frame.type() == gdb.NORMAL_FRAME)
+            else:
+                in_tc = False
+                skip_tc = False
 
             # Try to get the PHP function name from the ActRec at %sp if we're
             # executing in the TC.
@@ -85,7 +107,9 @@ Filename and line number information is only included for C++ functions.
                         func = '[PHP] ' + name + '()'
                     else:
                         func = '[PHP] <psuedomain>'
-                except: pass
+                except:
+                    if mcg is None:
+                        skip_tc = True
 
             # Pop native frames until we find our current %sp.
             else:
@@ -105,8 +129,5 @@ Filename and line number information is only included for C++ functions.
                 loc = native_frame.find_sal()
 
             self.print_frame(i + 1 if in_tc else i, sp, rip, func, loc)
-
-            rip = sp[1]
-            sp = sp[0].cast(sp_type)
 
 WalkstkCommand()

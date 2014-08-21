9044aa563af419829713209761ed41487a759d0f
maide <ian@maide.ca>
2014-08-21 13:09:24 -0700

    Fix Freetype to look for ft2build.h first
    Summary: In the previous configuration, it would find freetype2 for me, but then fail to #include freetype.h through the macro header include.  In this configuration, it finds ft2build.h goes along happily.
    Closes https://github.com/facebook/hhvm/pull/3532
    
    Reviewed By: @ptarjan, @sgolemon
    
    Differential Revision: D1508208

--- CMake/FindFreetype.cmake
+++ CMake/FindFreetype.cmake
@@ -2,23 +2,23 @@
 find_package(PkgConfig)
 pkg_check_modules(PC_FREETYPE QUIET freetype2)
 
-find_path(FREETYPE_INCLUDE_DIRS
-          NAMES freetype/config/ftheader.h freetype2/config/ftheader.h
+find_path(FREETYPE_INCLUDE_DIR_FT2BUILD
+          NAMES ft2build.h
           HINTS ${PC_FREETYPE_INCLUDEDIR} ${PC_FREETYPE_INCLUDE_DIRS}
           PATH_SUFFIXES freetype2)
-if(FREETYPE_INCLUDE_DIRS AND
-    NOT EXISTS "${FREETYPE_INCLUDE_DIRS}/freetype/config/ftheader.h")
-  add_definitions(-DFREETYPE_PATH_FREETYPE2)
-endif()
-
-if(NOT FREETYPE_INCLUDE_DIRS)
- find_path(FREETYPE_INCLUDE_DIR_FT2BUILD NAMES ft2build.h
-   HINTS ${PC_FREETYPE_INCLUDEDIR} ${PC_FREETYPE_INCLUDE_DIRS}
-   PATH_SUFFIXES freetype2)
- if(FREETYPE_INCLUDE_DIR_FT2BUILD)
+if(FREETYPE_INCLUDE_DIR_FT2BUILD AND
+   NOT EXISTS "${FREETYPE_INCLUDE_DIR_FT2BUILD}/freetype2/freetype/config/ftheader.h")
   add_definitions(-DHAVE_FT2BUILD)
   set(FREETYPE_INCLUDE_DIRS ${FREETYPE_INCLUDE_DIR_FT2BUILD})
- endif()
+else()
+  find_path(FREETYPE_INCLUDE_DIRS
+            NAMES freetype/config/ftheader.h freetype2/config/ftheader.h
+            HINTS ${PC_FREETYPE_INCLUDEDIR} ${PC_FREETYPE_INCLUDE_DIRS}
+            PATH_SUFFIXES freetype2)
+  if(FREETYPE_INCLUDE_DIRS AND
+     NOT EXISTS "${FREETYPE_INCLUDE_DIRS}/freetype/config/ftheader.h")
+    add_definitions(-DFREETYPE_PATH_FREETYPE2)
+  endif()
 endif()
 
 find_library(FREETYPE_LIBRARIES NAMES freetype)

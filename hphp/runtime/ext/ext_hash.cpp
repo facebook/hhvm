040dd1786a4438aab3e96125cfaa293111863661
Owen Yamauchi <oyamauchi@fb.com>
2014-09-05 03:41:53 -0700

    Fix potential security leak in HashContext
    Summary: CVE-2014-6229
    
    This is not a NUL-terminated string, it's a fixed-size block of data.
    The risks were key truncation (if there happens to be a NUL byte in the
    key) or over-reading (which would be information leakage).
    
    Reviewed By: @ptarjan
    
    Differential Revision: D1533546

--- hphp/runtime/ext/ext_hash.cpp
+++ hphp/runtime/ext/ext_hash.cpp
@@ -152,7 +152,12 @@ public:
     context = malloc(ops->context_size);
     ops->hash_copy(context, ctx->context);
     options = ctx->options;
-    key = ctx->key ? strdup(ctx->key) : nullptr;
+    if (ctx->key) {
+      key = static_cast<char*>(malloc(ops->block_size));
+      memcpy(key, ctx->key, ops->block_size);
+    } else {
+      key = nullptr;
+    }
   }
 
   ~HashContext() {

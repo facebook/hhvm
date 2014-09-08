faa3417785dc252f359053fd754a2f82d42a22d0
Edwin Smith <smith@fb.com>
2014-09-08 13:33:19 -0700

    Reduce NumSingleJitRequests from 20 to 5
    Summary: Tweaks to warmup to reduce thread count.
    
    Reviewed By: @markw65
    
    Differential Revision: D1542901

--- hphp/runtime/base/runtime-option.h
+++ hphp/runtime/base/runtime-option.h
@@ -427,7 +427,7 @@ public:
   F(bool, JitTypePrediction,           true)                            \
   F(int32_t, JitStressTypePredPercent, 0)                               \
   F(uint32_t, JitProfileInterpRequests, kDefaultProfileInterpRequests)  \
-  F(uint32_t, NumSingleJitRequests,    20)                              \
+  F(uint32_t, NumSingleJitRequests,    5)                              \
   F(uint32_t, JitProfileRequests,      kDefaultProfileRequests)         \
   F(bool, JitProfileRecord,            false)                           \
   F(uint32_t, GdbSyncChunks,           128)                             \

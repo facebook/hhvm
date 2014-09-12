9d2369b5dd01b88d5ee9b01672948c186ff5723c
Kristaps Kaupe <kristaps.k@intelligentsystems.lv>
2014-09-13 02:23:05 +0300

    More correct test case version, closes connections

--- hphp/test/slow/session/session_start_undefined_scheme.php
+++ hphp/test/slow/session/session_start_undefined_scheme.php
@@ -1,8 +1,12 @@
 <?php
 // find out TCP port that nobody listens on
 for ($port = 1; $port <= 65535; $port++) {
-    if (@fsockopen('127.0.0.1', $port) === false) {
+    $fp = @fsockopen('127.0.0.1', $port);
+    if ($fp === false) {
         break;
+    }
+    else {
+        fclose($fp);
     }
 }
 // now test redis session handler on that port

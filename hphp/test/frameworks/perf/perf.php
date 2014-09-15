4c096d55d7b0199bd40a90aa8f1eaba3c78b0880
Fred Emmott <fredemmott@fb.com>
2014-09-15 11:12:07 -0700

    Spew nginx stats too
    Summary: We're sending *much* fewer bytes than php-ng. Turns out this is because we gzip-compress everything by default.
    
    Confirmed with:
    
      curl http://devbig076.prn2.facebook.com:8090/ | wc -c # gets me the same number of bytes as php-ng
      curl -H 'Accept-Encoding: gzip' http://devbig076.prn2.facebook.com:8090/ | wc -c # gets me the lower number
    
    Reviewed By: @ptarjan
    
    Differential Revision: D1552801

--- hphp/test/frameworks/perf/perf.php
+++ hphp/test/frameworks/perf/perf.php
@@ -71,6 +71,9 @@ function run_benchmark(
   print_progress('Enabling engine stats collection');
   $php_engine->enableStats();
 
+  print_progress('Clearing nginx access.log');
+  $nginx->clearAccessLog();
+
   print_progress('Running Siege for benchmark');
   $siege = new Siege($options, $target, RequestModes::BENCHMARK);
   $siege->start();
@@ -80,6 +83,7 @@ function run_benchmark(
   print_progress('Collecting results');
   $php_engine_stats = $php_engine->collectStats();
   $siege_stats = $siege->collectStats();
+  $nginx_stats = $nginx->collectStats();
 
   $combined_stats = Map { };
   foreach ($php_engine_stats as $page => $stats) {
@@ -92,7 +96,15 @@ function run_benchmark(
       $combined_stats[$page] = $stats;
     }
   }
+  foreach ($nginx_stats as $page => $stats) {
+    if ($combined_stats->containsKey($page)) {
+      $combined_stats[$page]->setAll($stats);
+    } else {
+      $combined_stats[$page] = $stats;
+    }
+  }
 
+  ksort($combined_stats);
   print(json_encode($combined_stats, JSON_PRETTY_PRINT)."\n");
 
   print_progress('All done');

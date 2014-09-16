32c9f1d27c74b4b99b7847c8d07979a0d745662e
Nigel Chan <nchan@fb.com>
2014-09-16 08:37:33 -0700

    Convert filter to HNI
    Summary: fixes #3705
    
    Reviewed By: @ptarjan
    
    Differential Revision: D1545181

---
+++ hphp/runtime/ext/filter/ext_filter.php
@@ -0,0 +1,34 @@
+<?hh
+
+/**
+ * Returns a list of all supported filters
+ *
+ * @return mixed - Returns an array of names of all supported filters, empty
+ *   array if there are no such filters. Indexes of this array are not filter
+ *   IDs, they can be obtained with filter_id() from a name instead.
+ *
+ */
+<<__Native>>
+function filter_list(): mixed;
+
+/**
+ * Returns the filter ID belonging to a named filter
+ *
+ * @param mixed $filtername - Name of a filter to get.
+ *
+ * @return mixed - ID of a filter on success or FALSE if filter doesn't exist.
+ *
+ */
+<<__Native>>
+function filter_id(mixed $filtername): mixed;
+
+/**
+ * Filters a variable with a specified filter
+ *
+ * @return mixed - Returns the filtered data, or FALSE if the filter fails.
+ *
+ */
+<<__Native>>
+function filter_var(mixed $variable,
+                    int $filter = 516,
+                    mixed $options = array()): mixed;

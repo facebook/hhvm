22ff23e5722ba18a9cfff68ac4efb959e82117dd
Fred Emmott <fredemmott@fb.com>
2014-09-17 09:33:24 -0700

    Fix bogus EOF in bzip2
    Summary: Check for EOF the way PHP-NG's bz2 extension does.
    
    The C library is:
     - returning fewer bytes than requested
     - setting BZ_STREAM_END (if I use bz2_bzRead() instead of bz2_bzread()...
       ya rly...
    
    When there's still data left. A read after the C library claims 'EOF' works fine.
    
    I'm finding this by tar jcvf'ing hphp/test/frameworks/vendor/ and trying to extract with PharData (or __SystemLib\TarArchiveHandler) - but I can't get a more isolated repro case. I suspect there's multiple bzip2 streams in the file, but catting together multiple .bz2 files doesn't create this problem. I've also tried bunzip2 + bzip2ing it, and it still has the problem, so it's not something weird about how tar is bzipping.
    
    Reviewed By: @ptarjan
    
    Differential Revision: D1560065

--- hphp/runtime/ext/bz2/bz2-file.cpp
+++ hphp/runtime/ext/bz2/bz2-file.cpp
@@ -86,10 +86,18 @@ bool BZ2File::flush() {
 }
 
 int64_t BZ2File::readImpl(char * buf, int64_t length) {
+  if (length == 0) {
+    return 0;
+  }
   assert(m_bzFile);
   int len = BZ2_bzread(m_bzFile, buf, length);
-  if (len < length)
+  /* Sometimes libbz2 will return fewer bytes than requested, and set bzerror
+   * to BZ_STREAM_END, but it's not actually EOF, and you can keep reading from
+   * the file - so, only set EOF after a failed read. This matches PHP5.
+   */
+  if (len == 0) {
     m_eof = true;
+  }
   return len;
 }
 

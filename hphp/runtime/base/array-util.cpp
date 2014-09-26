fe70efe4efa8318536ea1d4940362e83e641d905
bill fumerola <bill@fb.com>
2014-09-25 17:57:22 -0700

    ArrayUtil::RegularSortUnique(): pre-allocate array based on unique count
    Reviewed By: @elgenie
    
    Differential Revision: D1580109

--- hphp/runtime/base/array-util.cpp
+++ hphp/runtime/base/array-util.cpp
@@ -501,6 +501,7 @@ Variant ArrayUtil::RegularSortUnique(const Array& input) {
   std::vector<int> indices;
   Array::SortImpl(indices, input, opaque, Array::SortRegularAscending, false);
 
+  int duplicates_count = 0;
   std::vector<bool> duplicates(indices.size(), false);
   int lastIdx = indices[0];
   Variant last = input->getValue(opaque.positions[lastIdx]);
@@ -508,6 +509,7 @@ Variant ArrayUtil::RegularSortUnique(const Array& input) {
     int currentIdx = indices[i];
     Variant current = input->getValue(opaque.positions[currentIdx]);
     if (equal(current, last)) {
+      ++duplicates_count;
       if (currentIdx > lastIdx) {
         duplicates[currentIdx] = true;
         continue;
@@ -518,12 +520,12 @@ Variant ArrayUtil::RegularSortUnique(const Array& input) {
     last = current;
   }
 
-  Array ret = Array::Create();
+  ArrayInit ret(indices.size() - duplicates_count, ArrayInit::Map{});
   int i = 0;
   for (ArrayIter iter(input); iter; ++iter, ++i) {
     if (!duplicates[i]) ret.set(iter.first(), iter.secondRef());
   }
-  return ret;
+  return ret.toVariant();
 }
 
 ///////////////////////////////////////////////////////////////////////////////

fa18346f135168a7ba225d3fd25b9358495a763c
Reeze Xia <reeze@php.net>
2014-10-03 01:35:09 +0800

    Use space instead of tab

--- hphp/test/slow/error_handler/recoverable.php
+++ hphp/test/slow/error_handler/recoverable.php
@@ -1,7 +1,7 @@
 <?php
 
 set_error_handler(function($errno, $errmsg) {
-	var_dump($errno == E_RECOVERABLE_ERROR);
+  var_dump($errno == E_RECOVERABLE_ERROR);
 }, E_RECOVERABLE_ERROR);
 
 $closure = function() {};

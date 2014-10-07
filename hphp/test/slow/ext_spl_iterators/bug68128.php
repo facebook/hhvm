a1a4a92d7052c6554ebb082a988731a344efcaf9
Tim Siebels <tim_siebels_aurich@yahoo.de>
2014-10-07 19:21:22 +0200

    Fix lint

--- hphp/test/slow/ext_spl_iterators/bug68128.php
+++ hphp/test/slow/ext_spl_iterators/bug68128.php
@@ -4,25 +4,24 @@ $array = new ArrayIterator(array('a', array('b', 'c')));
 $regex = new RegexIterator($array, '/Array/');
 
 foreach ($regex as $match) {
-    var_dump($match);
+  var_dump($match); // We should never get here
 }
 
-$rArrayIterator = new RecursiveArrayIterator(array('test1', array('tet3', 'test4', 'test5')));
+$rArrayIterator = new RecursiveArrayIterator(
+                    array('test1', array('tet3', 'test4', 'test5'))
+                  );
 $rRegexIterator = new RecursiveRegexIterator($rArrayIterator, '/^test/',
-    RecursiveRegexIterator::ALL_MATCHES);
+                    RecursiveRegexIterator::ALL_MATCHES);
 
 foreach ($rRegexIterator as $key1 => $value1) {
-
-    if ($rRegexIterator->hasChildren()) {
-
-        // print all children
-        echo "Children: ";
-        foreach ($rRegexIterator->getChildren() as $key => $value) {
-            echo $value . " ";
-        }
-        echo "\n";
-    } else {
-        echo "No children\n";
+  if ($rRegexIterator->hasChildren()) {
+    // print all children
+    echo "Children: ";
+    foreach ($rRegexIterator->getChildren() as $key => $value) {
+      echo $value . " ";
     }
-
+    echo "\n";
+  } else {
+    echo "No children\n";
+  }
 }

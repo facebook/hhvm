f6333b69772dd235116bef118ad232255c510092
Nik Everett <neverett@wikimedia.org>
2014-10-15 17:31:29 -0700

    Build against bundled sqlite is 3.8
    Running hhvm against sqlite 3.8 fails with:
    'COMMIT;' --> (5) cannot commit transaction - SQL statements in progress
    
    Without this patch the cmake scripts will use the bundled copy of sqlite
    if 3.8.4 <= version < 3.8.6.  With this patch the range is now
    3.8.0 <= version.
    
    Cleans up the output CMake spits out when it 3.8 to make it more clear.
    
    Closes #3994

--- CMake/FindLibSQLite.cmake
+++ CMake/FindLibSQLite.cmake
@@ -8,25 +8,24 @@
 find_package(PkgConfig)
 pkg_check_modules(PC_SQLITE3 QUIET sqlite3)
 
-if (PC_SQLITE3_FOUND AND (PC_SQLITE3_VERSION VERSION_LESS "3.8.6" AND
-                          NOT (PC_SQLITE3_VERSION VERSION_LESS "3.8.4")))
+if (PC_SQLITE3_FOUND AND NOT (PC_SQLITE3_VERSION VERSION_LESS "3.8.0"))
   set(LIBSQLITE3_FOUND 0)
   set(LIBSQLITE3_INCLUDE_DIR "LIBSQLITE3_INCLUDE_DIR-NOTFOUND")
   set(LIBSQLITE3_LIBRARY "LIBSQLITE3_LIBRARY-NOTFOUND")
+  message(STATUS "Found non-working version of libsqlite3")
 else()
   find_path(LIBSQLITE3_INCLUDE_DIR
             NAMES sqlite3.h
             HINTS ${PC_SQLITE3_INCLUDE_DIRS})
 
-    find_library(LIBSQLITE3_LIBRARY
-                 NAMES sqlite3)
-endif (PC_SQLITE3_FOUND AND (PC_SQLITE3_VERSION VERSION_LESS "3.8.6" AND
-                             NOT (PC_SQLITE3_VERSION VERSION_LESS "3.8.4")))
+  find_library(LIBSQLITE3_LIBRARY
+            NAMES sqlite3)
+  include(FindPackageHandleStandardArgs)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(LIBSQLITE3 DEFAULT_MSG
+                                    LIBSQLITE3_LIBRARY
+                                    LIBSQLITE3_INCLUDE_DIR)
 
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(
-    LIBSQLITE3 DEFAULT_MSG
-    LIBSQLITE3_LIBRARY LIBSQLITE3_INCLUDE_DIR)
+endif (PC_SQLITE3_FOUND AND NOT (PC_SQLITE3_VERSION VERSION_LESS "3.8.0"))
 
 if (NOT LIBSQLITE3_FOUND)
   message(STATUS "Using third-party bundled libsqlite3")

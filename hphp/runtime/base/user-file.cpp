f7f156ad33a2dee81f88a62ff3b8592893ba18da
Michael Moravec <mail@majkl578.cz>
2014-10-22 04:55:06 +0200

    use constant instead of plain number (f_flock is even more cryptic)

--- hphp/runtime/base/user-file.cpp
+++ hphp/runtime/base/user-file.cpp
@@ -33,6 +33,11 @@ namespace HPHP {
 
 ///////////////////////////////////////////////////////////////////////////////
 
+#define PHP_LOCK_SH 1
+#define PHP_LOCK_EX 2
+#define PHP_LOCK_UN 3
+#define PHP_LOCK_NB 4
+
 StaticString s_stream_open("stream_open");
 StaticString s_stream_close("stream_close");
 StaticString s_stream_read("stream_read");
@@ -289,12 +294,12 @@ bool UserFile::truncate(int64_t size) {
 bool UserFile::lock(int operation, bool &wouldBlock) {
   int64_t op = 0;
   if (operation & LOCK_NB) {
-    op |= 4; // PHP LOCK_NB = 4
+    op |= PHP_LOCK_NB;
   }
   switch (operation & ~LOCK_NB) {
-    case LOCK_SH: op |= 1; break; // PHP LOCK_SH = 1
-    case LOCK_EX: op |= 2; break; // PHP LOCK_EX = 2
-    case LOCK_UN: op |= 3; break; // PHP LOCK_UN = 3
+    case LOCK_SH: op |= PHP_LOCK_SH; break;
+    case LOCK_EX: op |= PHP_LOCK_EX; break;
+    case LOCK_UN: op |= PHP_LOCK_UN; break;
   }
 
   // bool stream_lock(int $operation)

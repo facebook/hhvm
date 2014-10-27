1be5f29dd4f63dafda0bac332d31bac09df42a1b
mwilliams <mwilliams@fb.com>
2014-10-27 13:44:00 -0700

    Refactor debugger tests
    Summary: We had some debugger tests that were setup to run in
    server mode. This was a superset of what was required to run
    server mode tests, so I refactored it, and created a small
    stub for server tests.
    
    Reviewed By: @ptarjan, @bertmaher
    
    Differential Revision: D1644913
    
    Signature: t1:1644913:1414610120:a48a422643821e3b4fcbd39fa3f9c2bad8f88721

---
+++ hphp/test/server/fastcgi/server_root/subdoc/subdir/test.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "DocumentRoot  : ${_SERVER['DOCUMENT_ROOT']}\n";
+echo "ScriptFileName: ${_SERVER['SCRIPT_FILENAME']}\n";
+echo "ScriptName    : ${_SERVER['SCRIPT_NAME']}\n";
---
+++ hphp/test/server/http/tests/runTest1.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('test_base.inc');
+
+function test1Controller($serverPort) {
+  $requests = array(
+    "test_get.php?name=Foo",
+    "test_get.php?name=Bar",
+  );
+  foreach ($requests as $request) {
+    echo "Requesting '$request'\n";
+    var_dump(request(php_uname('n'), $serverPort, $request));
+  }
+}
+
+runTest('test1', "test1Controller");

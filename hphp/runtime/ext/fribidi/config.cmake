82917ef103d91cec9661800fab7a075dba50d68b
Matt Crinklaw-Vogt <mlaw@fb.com>
2014-11-02 10:12:07 -0800

    Fribidi extension for HHVM
    Summary: Kind of a noob to this stuff but this is a straight port of pecl-fribidi.  I have some concerns over how the strings are being managed...  for a PHP extension you can specify whether or not the string being returned should be copied.  Not sure how to do that for HHVM.
    
    Reviewed By: @ptarjan, @behnam
    
    Differential Revision: D1632647

---
+++ hphp/runtime/ext/fribidi/config.cmake
@@ -0,0 +1,8 @@
+find_package(fribidi 0.19.6)
+
+if (FRIBIDI_LIBRARY)
+  HHVM_EXTENSION(fribidi ext_fribidi.cpp)
+  HHVM_ADD_INCLUDES(fribidi ${FRIBIDI_INCLUDE_DIR})
+  HHVM_LINK_LIBRARIES(fribidi ${FRIBIDI_LIBRARY})
+  HHVM_SYSTEMLIB(fribidi ext_fribidi.php)
+endif()
---
+++ hphp/test/slow/ext_fribidi/004.php
@@ -0,0 +1,13 @@
+<?php
+  error_reporting (E_ALL);
+
+  $a = fribidi_log2vis(null,  FRIBIDI_AUTO, FRIBIDI_CHARSET_CAP_RTL);
+  $b = fribidi_log2vis(false, FRIBIDI_AUTO, FRIBIDI_CHARSET_CAP_RTL);
+  $c = fribidi_log2vis(true,  FRIBIDI_AUTO, FRIBIDI_CHARSET_CAP_RTL);
+  $d = fribidi_log2vis(0,     FRIBIDI_AUTO, FRIBIDI_CHARSET_CAP_RTL);
+  $e = fribidi_log2vis(1,     FRIBIDI_AUTO, FRIBIDI_CHARSET_CAP_RTL);
+  $f = fribidi_log2vis('',    FRIBIDI_AUTO, FRIBIDI_CHARSET_CAP_RTL);
+  $g = fribidi_log2vis("",    FRIBIDI_AUTO, FRIBIDI_CHARSET_CAP_RTL);
+
+  var_dump(array($a, $b, $c, $d, $e, $f, $g));
+?>

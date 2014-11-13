1db0f56455b1177f8305d91529a41abdc13a6c8b
Kaiwen Xu <kevin@kevxu.net>
2014-11-12 23:10:14 -0500

    Warning when file_put_contents on non-regular file with exclusive lock.
    In HHVM_FUNCTION(file_put_contents), raise warning if attempting to add an
    exclusive lock on non-regular file, in order to match PHP5's behavior.
    
    Passing vfsstream's filePutContentsLockShouldReportError test case.

--- hphp/test/slow/ext_file/file_put_contents.php
+++ hphp/test/slow/ext_file/file_put_contents.php
@@ -6,3 +6,10 @@ file_put_contents($tempfile, 'testing file_put_contents');
 var_dump(file_get_contents($tempfile));
 
 unlink($tempfile);
+
+$tempfile2 = tempnam('/tmp', 'vmextfiletest');
+
+file_put_contents('File://' . $tempfile2, 'testing file_put_contents with File:// url and LOCK_EX', LOCK_EX);
+var_dump(file_get_contents($tempfile2));
+
+unlink($tempfile2);

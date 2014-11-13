df013de4dee44afd3228fa17013bc1cb0470b9a4
Paul Cavallaro <ptc@fb.com>
2014-11-13 08:03:44 -0800

    Add per kind ArrayFunc counters for ArrayTracer
    Summary: Adds an array of std::atomic<uint64_t> to keep count of ArrayFunc per ArrayKind
    
    Reviewed By: @edwinsmith
    
    Differential Revision: D1623929

--- hphp/test/slow/array_tracer/array_tracer.php
+++ hphp/test/slow/array_tracer/array_tracer.php
@@ -19,17 +19,27 @@ function harness($times) {
 }
 
 function test() {
-  echo "Creating about 400 PackedVectors\n";
+  echo "Creating about 400 PackedVectors via Appending\n";
   harness(200);
   $dump = hphp_array_tracer_dump();
   $numPacked = $dump["PackedVector"];
+  $numPackedAppend = $dump["Array Func Calls For PackedKind"]["Append"];
+  $numEmptyAppend = $dump["Array Func Calls For EmptyKind"]["Append"];
 
   echo "Creating another 400 PackedVectors\n";
   harness(200);
   $dump = hphp_array_tracer_dump();
   $newNumPacked = $dump["PackedVector"];
+  $newNumPackedAppend = $dump["Array Func Calls For PackedKind"]["Append"];
+  $newNumEmptyAppend = $dump["Array Func Calls For EmptyKind"]["Append"];
+
   echo "Verifying we created an additional 400 PackedVectors\n";
   var_dump($newNumPacked - $numPacked);
+  echo "Verifying we performed an additional (200*5) = 1000 Appends "
+     . "on Packed\n";
+  var_dump($newNumPackedAppend - $numPackedAppend);
+  echo "Verifying we performed an additional (200*1) = 200 Appends on Empty\n";
+  var_dump($newNumEmptyAppend - $numEmptyAppend);
 }
 
 test();

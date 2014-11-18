47c180c33df3d86ee080fbbe3424e9d4d3a3dba7
Mark Hahnenberg <mhahnenberg@fb.com>
2014-11-18 09:45:25 -0800

    Use stderr for non-vital info when running benchy
    Summary: Basically everything that benchy prints other than the final
    table of results should be piped to stderr so that it can be filtered
    out when running benchy as part of other scripts.
    
    Reviewed By: @edwinsmith
    
    Differential Revision: D1682346
    
    Signature: t1:1682346:1416057424:7217a7242372904227108b26734423cd0292567f

---
+++ hphp/tools/benchy/benchy_utils.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""Miscellaneous convenience utilities.
+
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+import shlex
+import subprocess
+import sys
+
+def run_command(cmd, verbose=False, env=None, stdout=None, stderr=None):
+    """Runs a command and checks the return code for errors. If stdout is not
+    specified, redirects stdout of command to stderr.
+
+    """
+    cmd = shlex.split(cmd.encode('utf8'))
+    try:
+        saved_stdout = sys.stdout
+        sys.stdout = sys.stderr
+        if verbose:
+            print(cmd)
+        subprocess.check_call(
+            cmd,
+            env=env,
+            stdout=(stdout or sys.stdout),
+            stderr=(stderr or sys.stderr))
+    finally:
+        sys.stdout = saved_stdout

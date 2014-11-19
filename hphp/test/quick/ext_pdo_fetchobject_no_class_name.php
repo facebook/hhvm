870c8ede3ae2fdbea9725afaf681fd98d785bf8d
Yaacov Akiba Slama <ya@slamail.org>
2014-11-19 11:07:09 +0200

    Use sys_get_temp_dir() instead of hardcoded directory

--- hphp/test/quick/ext_pdo_fetchobject_no_class_name.php
+++ hphp/test/quick/ext_pdo_fetchobject_no_class_name.php
@@ -1,5 +1,5 @@
 <?php
-$tmp_sqllite = tempnam('/tmp', 'vmpdotest');
+$tmp_sqllite = tempnam(sys_get_temp_dir(), 'vmpdotest');
 $source = "sqlite:$tmp_sqllite";
 $db = new PDO($source);
 $rows = $db->query('SELECT LENGTH("123456") as col;')->fetchObject();

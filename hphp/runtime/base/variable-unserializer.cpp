a76b39e13c6f87d2073c89ec8a828dee697e74f4
Bert Maher <bertrand@fb.com>
2014-12-08 21:50:00 -0800

    Use a custom implementation of strtoll for unserialize
    Summary: We can do better than the standard libc strtoll for unserialize, both
    because we can avoid the plt, and because unserialize doesn't need all the edge
    conditions (overflow doesn't need to be defined in any particular way, there's
    no need AFAIK to handle leading + or whitespace).
    
    Reviewed By: @ottoni
    
    Differential Revision: D1722763
    
    Signature: t1:1722763:1417817958:a1b96780879ce4400b30fe39b21523b171ba5bd9

--- hphp/runtime/base/variable-unserializer.cpp
+++ hphp/runtime/base/variable-unserializer.cpp
@@ -41,12 +41,28 @@ Variant VariableUnserializer::unserializeKey() {
   return v;
 }
 
+static std::pair<int64_t,const char*> hh_strtoll_base10(const char* p) {
+  int64_t x = 0;
+  bool neg = false;
+  if (*p == '-') {
+    neg = true;
+    ++p;
+  }
+  while (*p >= '0' && *p <= '9') {
+    x = (x * 10) + ('0' - *p);
+    ++p;
+  }
+  if (!neg) {
+    x = -x;
+  }
+  return std::pair<int64_t,const char*>(x, p);
+}
+
 int64_t VariableUnserializer::readInt() {
   check();
-  char *newBuf;
-  int64_t r = strtoll(m_buf, &newBuf, 10);
-  m_buf = newBuf;
-  return r;
+  auto r = hh_strtoll_base10(m_buf);
+  m_buf = r.second;
+  return r.first;
 }
 
 double VariableUnserializer::readDouble() {

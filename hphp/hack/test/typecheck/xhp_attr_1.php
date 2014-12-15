735f05d1baaa6e24c1d6086f434b0ddc98217a7c
Drew Paroski <andrewparoski@fb.com>
2014-12-14 19:17:50 -0800

    Fix lexer rules for XHP identifiers
    Summary: This fixes how the typechecker's lexer deals with XHP identifiers to match
    HHVM. Most notably, XHP identifiers are not allowed to end with a hyphen or
    a colon.
    
    Reviewed By: @int3
    
    Differential Revision: D1739223
    
    Signature: t1:1739223:1418616156:87575bcda7d550bec63584c14b373549fe5c3831

--- hphp/hack/test/typecheck/xhp_attr_1.php
+++ hphp/hack/test/typecheck/xhp_attr_1.php
@@ -1,12 +1,18 @@
 <?hh // strict
 class :blah {}
 class GenericClass<T> {}
+class SomeClass {
+  public function bar(): float { return 3.14; }
+}
 class :foo {
   attribute
     // Using array without type parameters is okay for now
     array bar-baz = array(),
     // Using GenericClass without type parameters is okay for now
     GenericClass yo @required,
+    // Check to make sure that "->:" parses correctly when chained
+    // together in a larger expression.
+    SomeClass some-obj @required,
     // An XHP class name can be used as a type hint; marking it
     // with "@required" so the typechecker knows it can't be null
     :blah fizz-buzz @required,
@@ -28,4 +34,7 @@ function test2(:foo $obj): ?:blah {
 }
 function test3(:foo $obj): Stringish {
   return $obj->:string:attr-might-be:stringish;
+}
+function test4(:foo $obj): float {
+  return $obj->:some-obj->bar();
 }

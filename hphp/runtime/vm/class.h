daec34f790030ba69c606b9e2bd3ab7534aa3215
Tim Siebels <tim_siebels_aurich@yahoo.de>
2014-12-16 15:09:26 +0100

    Fatal if a class implements IteratorAggregate and Iterator
    It doesn't make sense to implement both IteratorAggregate and Iterator.
    Which of those two are supposed to be iterated? PHP fatals on this, we should too.
    
    Part of #3536
    This might cause issue for people using `extends PDOStatement implements IteratorAggregate`
    But since it wouldn't do what you expect it to do anyway (See #3536 why PdoStatement is broken), we should tell them
    
    Test Plan:
    `test/run test/slow/ext_class`

--- hphp/runtime/vm/class.h
+++ hphp/runtime/vm/class.h
@@ -937,6 +937,7 @@ private:
                      const StringData* newMethName);
 
   void checkInterfaceMethods();
+  void checkInterfaceConstraints();
   void methodOverrideCheck(const Func* parentMethod, const Func* method);
 
   static bool compatibleTraitPropInit(TypedValue& tv1, TypedValue& tv2);

310b2fda394ce724b308947112344d1b06b0797e
Tim Siebels <tim_siebels_aurich@yahoo.de>
2014-12-17 17:34:45 +0100

    change test

--- hphp/test/slow/ext_spl_iterators/follow_symlink.php
+++ hphp/test/slow/ext_spl_iterators/follow_symlink.php
@@ -1,16 +1,19 @@
 <?php
+$p = '/tmp/recursivedirectoryiterator_followsymlink_test/';
 
-mkdir("test");
-mkdir("test/dir1");
-touch("test/dir1/file1.txt");
-mkdir("test/dir2");
-touch("test/dir2/file2.txt");
-symlink("../dir1", "test/dir2/dir1");
+mkdir($p);
+mkdir($p . "test");
+mkdir($p . "test/dir1");
+touch($p . "test/dir1/file1.txt");
+mkdir($p . "test/dir2");
+touch($p . "test/dir2/file2.txt");
+symlink($p . "../dir1", $p . "test/dir2/dir1");
 
 // Don't follow
-$iterator = new RecursiveDirectoryIterator('test/dir2', 
+$iterator = new RecursiveDirectoryIterator($p . 'test/dir2', 
               FilesystemIterator::SKIP_DOTS);
-$iterator = new RecursiveIteratorIterator($iterator, RecursiveIteratorIterator::SELF_FIRST);
+$iterator = new RecursiveIteratorIterator($iterator,
+              RecursiveIteratorIterator::SELF_FIRST);
 
 echo "DONT FOLLOW SYMLINKS", PHP_EOL;
 
@@ -20,11 +23,12 @@ foreach ($iterator as $path => $dir) {
 
 // Follow
 $iterator = new RecursiveDirectoryIterator(
-              'test/dir2',
+              $p . 'test/dir2',
               FilesystemIterator::SKIP_DOTS | 
               FilesystemIterator::FOLLOW_SYMLINKS
             );
-$iterator = new RecursiveIteratorIterator($iterator, RecursiveIteratorIterator::SELF_FIRST);
+$iterator = new RecursiveIteratorIterator($iterator,
+              RecursiveIteratorIterator::SELF_FIRST);
 
 echo "FOLLOW SYMLINKS", PHP_EOL;
 
@@ -34,10 +38,11 @@ foreach ($iterator as $path => $dir) {
 
 
 // Clean up
-unlink("test/dir2/dir1");
-unlink("test/dir2/file2.txt");
-unlink("test/dir1/file1.txt");
-rmdir("test/dir2");
-rmdir("test/dir1");
-rmdir("test");
+unlink($p . "test/dir2/dir1");
+unlink($p . "test/dir2/file2.txt");
+unlink($p . "test/dir1/file1.txt");
+rmdir($p . "test/dir2");
+rmdir($p . "test/dir1");
+rmdir($p . "test");
+rmdir($p);
 

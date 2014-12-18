125fcbe8084da107239f6badaf1a2f7f0763a2c5
Matt Clarke-Lauer <mcl@fb.com>
2014-12-18 14:10:32 -0800

    reorder shm_put_var operations
    Summary: Reordering the shm_put_var operation to serialize the object before
    writing to memory aviods a segfault and possible arbitrary memory write.
    Previously the shared memory location was found before the object was
    serialized and it is possible to detach the shm location during serialization.
    This would have shm_put_data write to memory using a shm handle that doesn't
    exist.
    
    CVE-2010-1861
    
    Reviewed By: @markw65
    
    Differential Revision: D1694899

--- hphp/runtime/ext/ext_ipc.cpp
+++ hphp/runtime/ext/ext_ipc.cpp
@@ -742,6 +742,9 @@ bool f_shm_has_var(int64_t shm_identifier, int64_t variable_key) {
 
 bool f_shm_put_var(int64_t shm_identifier, int64_t variable_key,
                    const Variant& variable) {
+  /* setup string-variable and serialize */
+  String serialized = HHVM_FN(serialize)(variable);
+
   Lock lock(g_shm_mutex);
   std::set<sysvshm_shm*>::iterator iter =
     g_shms.find((sysvshm_shm*)shm_identifier);
@@ -750,11 +753,8 @@ bool f_shm_put_var(int64_t shm_identifier, int64_t variable_key,
                   shm_identifier);
     return false;
   }
+
   sysvshm_shm *shm_list_ptr = *iter;
-
-  /* setup string-variable and serialize */
-  String serialized = HHVM_FN(serialize)(variable);
-
   /* insert serialized variable into shared memory */
   int ret = put_shm_data(shm_list_ptr->ptr, variable_key,
                          (char*)serialized.data(), serialized.size());

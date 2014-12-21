99ff4a38f621b95f4f2dcdebe7f84f4965913ad2
Jordan DeLong <jdelong@fb.com>
2014-12-21 12:41:53 -0800

    Do some cleanup in irgen-ret.cpp
    Summary: Split things into subroutines a little bit.  This diff shouldn't have
    any functional changes (separating one into another diff).
    
    Reviewed By: @alexmalyshev, @jano
    
    Differential Revision: D1753393

--- hphp/runtime/vm/jit/irgen-ret.cpp
+++ hphp/runtime/vm/jit/irgen-ret.cpp
@@ -43,23 +43,8 @@ void retSurpriseCheck(HTS& env, SSATmp* frame, SSATmp* retVal) {
   );
 }
 
-void implRet(HTS& env, Type type) {
-  auto const func = curFunc(env);
-  if (func->attrs() & AttrMayUseVV) {
-    // Note: this has to be the first thing, because we cannot bail after
-    //       we start decRefing locs because then there'll be no corresponding
-    //       bytecode boundaries until the end of RetC
-    gen(env, ReleaseVVOrExit, makeExitSlow(env), fp(env));
-  }
-
-  // Pop the return value. Since it will be teleported to its place in memory,
-  // we don't care about the type.
-  auto retVal = pop(env, type, func->isGenerator() ? DataTypeSpecific
-                                                   : DataTypeGeneric);
-
-  // Free local variables.  We do the decrefs inline if there are less
-  // refcounted locals than a threshold.
-  auto const localCount = func->numLocals();
+void freeLocalsAndThis(HTS& env) {
+  auto const localCount = curFunc(env)->numLocals();
   auto const shouldFreeInline = mcg->useLLVM() || [&]() -> bool {
     auto const count = mcg->numTranslations(
       env.irb->unit().context().srcKey());
@@ -82,95 +67,111 @@ void implRet(HTS& env, Type type) {
     gen(env, GenericRetDecRefs, fp(env));
   }
 
-  // Free $this.
-  if (func->mayHaveThis()) {
-    gen(env, DecRefThis, fp(env));
-  }
+  if (curFunc(env)->mayHaveThis()) gen(env, DecRefThis, fp(env));
+}
 
-  retSurpriseCheck(env, fp(env), retVal);
-
-  // In async function, wrap the return value into succeeded StaticWaitHandle.
-  if (!resumed(env) && func->isAsyncFunction()) {
-    retVal = gen(env, CreateSSWH, retVal);
-  }
-
-  SSATmp* stack;
-  SSATmp* resumableObj = nullptr;
-  int32_t spOffset;
-  if (!resumed(env)) {
-    // Store the return value.
-    gen(env, StRetVal, fp(env), retVal);
-
-    // Free ActRec.
-    stack = gen(env, RetAdjustStack, fp(env));
-    spOffset = 0;
-  } else if (func->isAsyncFunction()) {
-    // Load the parent chain.
-    auto parentChain = gen(env, LdAsyncArParentChain, fp(env));
-
-    // Mark the async function as succeeded.
-    gen(env, StAsyncArSucceeded, fp(env));
-
-    // Store the return value.
-    gen(env, StAsyncArResult, fp(env), retVal);
-
-    // Unblock parents.
-    gen(env, ABCUnblock, parentChain);
-
-    // Sync SP.
-    spillStack(env);
-    stack = sp(env);
-    spOffset = offsetFromSP(env, 0);
-
-    // Get the AsyncFunctionWaitHandle.
-    resumableObj = gen(env, LdResumableArObj, fp(env));
-  } else if (func->isNonAsyncGenerator()) {
-    // Clear generator's key and value.
-    auto const oldKey = gen(env, LdContArKey, Type::Cell, fp(env));
-    gen(env, StContArKey, fp(env), cns(env, Type::InitNull));
-    gen(env, DecRef, oldKey);
-
-    auto const oldValue = gen(env, LdContArValue, Type::Cell, fp(env));
-    gen(env, StContArValue, fp(env), cns(env, Type::InitNull));
-    gen(env, DecRef, oldValue);
-
-    // Mark generator as finished.
-    gen(env,
-        StContArState,
-        GeneratorState { BaseGenerator::State::Done },
-        fp(env));
-
-    // Push return value of next()/send()/raise().
-    push(env, cns(env, Type::InitNull));
-
-    // Sync SP.
-    spillStack(env);
-    stack = sp(env);
-    spOffset = offsetFromSP(env, 0);
-  } else {
-    not_reached();
-  }
-
-  // Grab caller info from ActRec.
+void normalReturn(HTS& env, SSATmp* retval) {
+  gen(env, StRetVal, fp(env), retval);
+  gen(env, RetAdjustStack, fp(env));
   auto const retAddr = gen(env, LdRetAddr, fp(env));
-  auto const frame = gen(env, FreeActRec, fp(env));
+  gen(env, FreeActRec, fp(env));
+  gen(env, RetCtrl, RetCtrlData { 0, false }, sp(env), fp(env), retAddr);
+}
 
-  // Drop reference to this resumable. The reference to the object storing
-  // the frame is implicitly owned by the execution. TakeRef is used to inform
-  // the refcount optimizer about this fact.
-  if (resumableObj != nullptr) {
-    gen(env, TakeRef, resumableObj);
-    gen(env, DecRef, resumableObj);
+void asyncFunctionReturn(HTS& env, SSATmp* retval) {
+  if (!resumed(env)) {
+    // Return from an eagerly-executed async function: wrap the return value in
+    // a StaticWaitHandle object and return that normally.
+    auto const wrapped = gen(env, CreateSSWH, retval);
+    normalReturn(env, wrapped);
+    return;
   }
+
+  auto parentChain = gen(env, LdAsyncArParentChain, fp(env));
+  gen(env, StAsyncArSucceeded, fp(env));
+  gen(env, StAsyncArResult, fp(env), retval);
+  gen(env, ABCUnblock, parentChain);
+
+  spillStack(env);
+
+  // Must load this before FreeActRec, which adjusts fp(env).
+  auto const resumableObj = gen(env, LdResumableArObj, fp(env));
+
+  auto const retAddr = gen(env, LdRetAddr, fp(env));
+  gen(env, FreeActRec, fp(env));
+
+  // Decref the AsyncFunctionWaitHandle.  The TakeRef informs refcount-opts
+  // that we're going to consume the reference.
+  gen(env, TakeRef, resumableObj);
+  gen(env, DecRef, resumableObj);
 
   gen(
     env,
     RetCtrl,
-    RetCtrlData { spOffset, false },
-    stack,
-    frame,
+    RetCtrlData { offsetFromSP(env, 0), false },
+    sp(env),
+    fp(env),
     retAddr
   );
+}
+
+void generatorReturn(HTS& env, SSATmp* retval) {
+  // Clear generator's key and value.
+  auto const oldKey = gen(env, LdContArKey, Type::Cell, fp(env));
+  gen(env, StContArKey, fp(env), cns(env, Type::InitNull));
+  gen(env, DecRef, oldKey);
+
+  auto const oldValue = gen(env, LdContArValue, Type::Cell, fp(env));
+  gen(env, StContArValue, fp(env), cns(env, Type::InitNull));
+  gen(env, DecRef, oldValue);
+
+  gen(env,
+      StContArState,
+      GeneratorState { BaseGenerator::State::Done },
+      fp(env));
+
+  // Push return value of next()/send()/raise().
+  push(env, cns(env, Type::InitNull));
+
+  spillStack(env);
+  auto const retAddr = gen(env, LdRetAddr, fp(env));
+  gen(env, FreeActRec, fp(env));
+
+  gen(
+    env,
+    RetCtrl,
+    RetCtrlData { offsetFromSP(env, 0), false },
+    sp(env),
+    fp(env),
+    retAddr
+  );
+}
+
+void implRet(HTS& env, Type type) {
+  if (curFunc(env)->attrs() & AttrMayUseVV) {
+    // Note: this has to be the first thing, because we cannot bail after
+    //       we start decRefing locs because then there'll be no corresponding
+    //       bytecode boundaries until the end of RetC
+    gen(env, ReleaseVVOrExit, makeExitSlow(env), fp(env));
+  }
+
+  // Pop the return value. Since it will be teleported to its place in memory,
+  // we don't care about the type.
+  auto const retval = pop(env, type,
+    curFunc(env)->isGenerator() ? DataTypeSpecific
+                                : DataTypeGeneric);
+
+  freeLocalsAndThis(env);
+  retSurpriseCheck(env, fp(env), retval);
+
+  if (curFunc(env)->isAsyncFunction()) {
+    return asyncFunctionReturn(env, retval);
+  }
+  if (resumed(env)) {
+    assert(curFunc(env)->isNonAsyncGenerator());
+    return generatorReturn(env, retval);
+  }
+  return normalReturn(env, retval);
 }
 
 //////////////////////////////////////////////////////////////////////

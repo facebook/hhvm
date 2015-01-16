e0cee620ffc78b2e70cb361b56650c89e3bf70ef
Josh Watzman <jwatzman@fb.com>
2015-01-16 11:24:00 -0800

    Fix memcached test externally
    Summary: The version of this const depends on your version of
    libmemcached.
    
    {sync, type="parent", child="external", childrevid="31713", childdiffid="165159", parentdiffid="6185912"}
    
    Reviewed By: @JoelMarcey
    
    Differential Revision: D1787202
    
    Signature: t1:1787202:1421434918:73caeece39e6e3912db804ad9195942c59fa4b2a

--- hphp/test/slow/ext_memcached/constants.php
+++ hphp/test/slow/ext_memcached/constants.php
@@ -2,6 +2,5 @@
 $refl = new ReflectionClass("Memcached");
 $constants = $refl->getConstants();
 var_dump($constants["RES_SERVER_MARKED_DEAD"]);
-var_dump($constants["RES_SERVER_TEMPORARILY_DISABLED"]);
 var_dump($constants["OPT_SORT_HOSTS"]);
 var_dump($constants["OPT_VERIFY_KEY"]);

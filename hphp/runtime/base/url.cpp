f70c84af143a193e441f165d7331f21b73473044
Matt Eagle <93eagle93@gmail.com>
2015-01-25 20:48:18 -0500

    made the url pasing faster and updated license headers

--- hphp/runtime/base/url.cpp
+++ hphp/runtime/base/url.cpp
@@ -2,7 +2,7 @@
    +----------------------------------------------------------------------+
    | HipHop for PHP                                                       |
    +----------------------------------------------------------------------+
-   | Copyright (c) 2010-2014 Facebook, Inc. (http://www.facebook.com)     |
+   | Copyright (c) 2010-2015 Facebook, Inc. (http://www.facebook.com)     |
    +----------------------------------------------------------------------+
    | This source file is subject to version 3.01 of the PHP license,      |
    | that is bundled with this package in the file LICENSE, and is        |
@@ -23,19 +23,20 @@ namespace URL {
 
 const char *getServerObject(const char* url) {
   assert(url);
-  int strip = 0;
+  const char *p;
   if (strncmp(url, "http://", 7) == 0) {
-    strip = 7;
+    p = strchr(url + 7, '/');
   } else if (strncmp(url, "https://", 8) == 0) {
-    strip = 8;
+    p = strchr(url + 8, '/');
+  } else {
+    p = strchr(url, '/');
   }
-  const char *p = strchr(url + strip, '/');
+  
   if (p) {
     while (*(p + 1) == '/') p++;
     return p;
   }
-  if (strip == 0) return url;
-  return "";
+  return url;
 }
 
 std::string getCommand(const char* serverObject) {

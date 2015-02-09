8b31896921c72ecf53cd3dc829c8cafddb16f1c7
Thomas Dudziak <tomdzk@fb.com>
2015-02-08 20:06:23 -0800

    Fix hhvm number formatter fa_IR tests
    Summary: This fixes the number formatter tests for fa_IR for multiple
    ICU versions which was a problem in the OSS version (see
    https://github.com/facebook/hhvm/commit/2c0b77ece17b4a7516eebcb628bf04e1f649c902)
    
    Reviewed By: @fredemmott
    
    Differential Revision: D1791162

--- hphp/test/slow/ext_icu/icu_latest/NumberFormatter_format_string_fa_IR.php
+++ hphp/test/slow/ext_icu/icu_latest/NumberFormatter_format_string_fa_IR.php
@@ -1,13 +1,11 @@
 <?php
-$formatter = NumberFormatter::create("en_US", NumberFormatter::DEFAULT_STYLE);
+$formatter = NumberFormatter::create("fa_IR", NumberFormatter::DEFAULT_STYLE);
 $formatter->setAttribute(NumberFormatter::FRACTION_DIGITS, 2);
-
 echo $formatter->format(1.23);
 echo "\n";
 echo $formatter->format("10.345");
 echo "\n";
 
-$formatter = NumberFormatter::create("en_US", NumberFormatter::DEFAULT_STYLE);
 $formatter->setAttribute(NumberFormatter::FRACTION_DIGITS, 0);
 $formatter->setAttribute(NumberFormatter::ROUNDING_MODE,
                          NumberFormatter::ROUND_HALFUP);

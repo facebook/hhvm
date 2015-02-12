cfce920aa7df23ba7faf851eace4a0a9f0819545
sherlockhua <sherlockhua@163.com>
2015-02-12 14:11:26 +0800

    fix test case

--- hphp/test/slow/ini/ini_parse_memory_leak.php
+++ hphp/test/slow/ini/ini_parse_memory_leak.php
@@ -1,34 +1,32 @@
 <?php
 
 error_reporting(0);
-$total_run = 100000;
-
-function empty_func()
-{
-
-}
 
 function leak_test()
 {
-    global $total_run;
-    $count = $total_run;
-    while ($count) {
-        $count--;
+    $a = memory_get_usage(true);
+    parse_ini_file('./ini_parse_memory_leak_test.ini', true);
+    $b = memory_get_usage(true);
+    $diff = $b - $a;
+
+    $c = memory_get_usage(true);
+    for ($i = 0; $i < 10000; $i++) {
         parse_ini_file('./ini_parse_memory_leak_test.ini', true);
+    }
+    $d = memory_get_usage(true);
+
+    $total=$d - $c;
+    $count = 0;
+    if ($diff > 0) {
+        $count = $total/$diff;
+    }
+
+    if ($count <= 3) {
+        echo "OK\n";
+    } else {
+        echo "diff:".$diff.",total:".$total."\n"; 
+        echo "count:".$count."\n";
     }
 }
 
-$a = memory_get_usage(true);
-empty_func();
-$b = memory_get_usage(true);
 leak_test();
-$c = memory_get_usage(true);
-
-$diff1 = $b - $a;
-$diff2 = $c - $b;
-
-if ($diff2 - $diff1 < 81920) {
-    echo "OK";
-} else {
-    echo "ini parse memory leak, total:".$diff1;
-}

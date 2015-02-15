6f99b08d25dca290c05e0b9297971ce300a8ec31
sherlockhua <sherlockhua@163.com>
2015-02-15 11:03:26 +0800

    fix test base for leak detected

--- hphp/test/slow/ini/ini_parse_memory_leak.php
+++ hphp/test/slow/ini/ini_parse_memory_leak.php
@@ -1,31 +1,32 @@
 <?php
 
 error_reporting(0);
+function empty_func()
+{
+
+}
 
 function leak_test()
 {
     $a = memory_get_usage(true);
-    parse_ini_file('./ini_parse_memory_leak_test.ini', true);
+    for ($i = 0; $i < 100000; $i++) {
+        parse_ini_file('/tmp/ini_parse_memory_leak_test.ini', true);
+    }
     $b = memory_get_usage(true);
-    $diff = $b - $a;
+    $first_leak = $b - $c;
+
+    for ($i = 0; $i < 200000; $i++) {
+        parse_ini_file('/tmp/ini_parse_memory_leak_test.ini', true);
+    }
 
     $c = memory_get_usage(true);
-    for ($i = 0; $i < 10000; $i++) {
-        parse_ini_file('./ini_parse_memory_leak_test.ini', true);
-    }
-    $d = memory_get_usage(true);
+    $second_leak = $c - $b;
 
-    $total=$d - $c;
-    $count = 0;
-    if ($diff > 0) {
-        $count = $total/$diff;
-    }
-
-    if ($count <= 3) {
-        echo "OK\n";
+    $count = $second_leak / $first_leak;
+    if ($count <= 1) {
+        echo "OK";
     } else {
-        echo "diff:".$diff.",total:".$total."\n"; 
-        echo "count:".$count."\n";
+        echo "leak first:".$first_leak.", leak second:".$second_leak.", times:".$count."\n";
     }
 }
 

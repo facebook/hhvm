ed427f348141f0fa2ad73c54faa940eba6dec355
Gustavo Frederico Temple Pedrosa <gustavo.pedrosa@eldorado.org.br>
2015-02-16 19:26:30 +0000

    Adds PowerPC64 arch-specific code
    Adding the PowerPC64 arch-specific code,
    equivalent of x64 and AARCH64.

--- hphp/util/atomic.h
+++ hphp/util/atomic.h
@@ -43,7 +43,7 @@ inline void assert_address_is_atomically_accessible(T* address) {
   assert(((uintptr_t(address) + sizeof(T) - 1) & ~63ul) ==
          ( uintptr_t(address)                  & ~63ul) &&
         "Atomically accessed addresses may not span cache lines");
-#elif __AARCH64EL__
+#elif __AARCH64EL__ || __powerpc64__
   // N-byte accesses must be N-byte aligned
   assert((uintptr_t(address) & (sizeof(T) - 1)) == 0);
 #else

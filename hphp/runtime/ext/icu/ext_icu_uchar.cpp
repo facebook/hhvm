7642688cfc0d9868eb45ee40b54d664a0787b3ea
Sara Golemon <sgolemon@fb.com>
2015-02-24 14:00:45 -0800

    Add missing constants to IntlChar extension
    Summary: FOLD_CASE_EXCLUDE_SPECIAL_I and FOLD_CASE_DEFAULT
    
    Reviewed By: @JoelMarcey
    
    Differential Revision: D1862250

--- hphp/runtime/ext/icu/ext_icu_uchar.cpp
+++ hphp/runtime/ext/icu/ext_icu_uchar.cpp
@@ -355,6 +355,8 @@ void IntlExtension::initUChar() {
                                (s_IntlChar.get(), makeStaticString(name), val);
   IC_CONSTL("CODEPOINT_MIN", UCHAR_MIN_VALUE)
   IC_CONSTL("CODEPOINT_MAX", UCHAR_MAX_VALUE)
+  IC_CONSTL("FOLD_CASE_DEFAULT", U_FOLD_CASE_DEFAULT)
+  IC_CONSTL("FOLD_CASE_EXCLUDE_SPECIAL_I", U_FOLD_CASE_EXCLUDE_SPECIAL_I)
 
   /* All enums used by the uchar APIs.  There are a LOT of them,
    * so they're separated out into include files,

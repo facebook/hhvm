91cf450b8ec25b849274fe19e1e66dd5380e4ea6
James Titcumb <james@asgrim.com>
2015-03-03 11:52:51 -0800

    Set the memory limit default to 16GiB
    Summary: It was suggested in #4725 that the memory limit was set to something a little more sensible than the current default of 9223372036854775807. This change sets the default memory limit to 1073741824 bytes (1GiB). Not sure if the test here is overkill, it is just testing a default after all...?
    
    Potential risk - if users of HHVM rely on the extremely high default to "ignore" the memory limit, this could potentially cause breakages? I'm not sure what a sensible default might be (I guess, it is mostly subjective).
    Closes https://github.com/facebook/hhvm/pull/4760
    
    Reviewed By: @sgolemon
    
    Differential Revision: D1821432

--- hphp/runtime/base/runtime-option.cpp
+++ hphp/runtime/base/runtime-option.cpp
@@ -1101,8 +1101,7 @@ void RuntimeOption::Load(IniSetting::Map& ini, Hdf& config,
     Config::Bind(PspTimeoutSeconds, ini, server["PspTimeoutSeconds"], 0);
     Config::Bind(PspCpuTimeoutSeconds, ini, server["PspCpuTimeoutSeconds"], 0);
     Config::Bind(ServerMemoryHeadRoom, ini, server["MemoryHeadRoom"], 0);
-    Config::Bind(RequestMemoryMaxBytes, ini, server["RequestMemoryMaxBytes"],
-                       std::numeric_limits<int64_t>::max());
+    Config::Bind(RequestMemoryMaxBytes, ini, server["RequestMemoryMaxBytes"], (16l << 30)); // 16GiB
     Config::Bind(ResponseQueueCount, ini, server["ResponseQueueCount"], 0);
     if (ResponseQueueCount <= 0) {
       ResponseQueueCount = ServerThreadCount / 10;

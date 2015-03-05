1dca85d5f62309829de07e1f0f037f9207137978
Owen Yamauchi <oyamauchi@fb.com>
2015-03-04 20:31:19 -0800

    Fix remove_soft_types for return types
    Summary: The regex we were using for parsing return type failures out of error logs was
    wrong. It wasn't filtering out the word "function" or "method", or the word
    "async", so it would end up looking for a function named "function f" instead
    of just "f".
    
    Reviewed By: @jwatzman
    
    Differential Revision: D1885574

--- hphp/hack/tools/remove_soft_types/test.php
+++ hphp/hack/tools/remove_soft_types/test.php
@@ -22,3 +22,27 @@ C::f(null);
 g(null);
 $f = <foo:bar:hello-world>toto</foo:bar:hello-world>;
 $f->i("titi");
+
+function one(): @int {
+  return 'nope';
+}
+
+async function two(): @Awaitable<int> {
+  return 'nope';
+}
+
+class D {
+  public function one(): @int {
+    return 'nope';
+  }
+
+  public async function two(): @Awaitable<int> {
+    return 'nope';
+  }
+}
+
+one();
+two()->getWaitHandle()->join();
+$d = new D();
+$d->one();
+$d->two()->getWaitHandle()->join();

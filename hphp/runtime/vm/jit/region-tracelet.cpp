a80bc23d9beac8b33291bca98b9d558e55e73f63
Guilherme Ottoni <ottoni@fb.com>
2015-03-12 19:08:07 -0700

    Fix detection of "main exit" blocks
    Summary: I added assertions that there's always a single block found as the
    main exit, which exposed a few issues fixed here.
    
    1. Blocks should be treated as the exit even if they have a taken edge to a catch block.
    
    2. We now also use the block hint's to determine whether a block is the main exit.  If there are multiple exit blocks, we take the non-Unlikely one.  There should be only one such block (except for Await).
    
    Reviewed By: @binliu19
    
    Differential Revision: D1896478

--- hphp/runtime/vm/jit/region-tracelet.cpp
+++ hphp/runtime/vm/jit/region-tracelet.cpp
@@ -253,14 +253,10 @@ RegionDescPtr RegionFormer::go() {
       break;
     }
 
-    if (!curIRBlock->empty()) {
-      auto& lastInst = curIRBlock->back();
-      if (lastInst.isTerminal() &&
-          (lastInst.taken() == nullptr || lastInst.taken()->isCatch())) {
-        FTRACE(1, "selectTracelet: tracelet broken due to terminal/non-jumpy "
-               "IR instruction: {}\n", lastInst.toString());
-        break;
-      }
+    if (curIRBlock->isExitNoThrow()) {
+      FTRACE(1, "selectTracelet: tracelet broken due to exiting IR instruction:"
+             "{}\n", curIRBlock->back());
+      break;
     }
 
     if (isFCallStar(m_inst.op())) m_arStates.back().pop();

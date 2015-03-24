424ee7de455e79934fcf09d7e617918372d3ec50
sherlockhua <sherlockhua@163.com>
2015-03-24 15:08:04 +0800

    fix testcase for ini parse

--- hphp/test/slow/ini/ini_parse_memory_leak.php
+++ hphp/test/slow/ini/ini_parse_memory_leak.php
@@ -1,29 +1,38 @@
 <?php
 
-error_reporting(0);
+//error_reporting(0);
 
-function leak_test()
+function leak_test($output)
 {
     $a = memory_get_usage(true);
-    for ($i = 0; $i < 100000; $i++) {
+    for ($i = 0; $i < 1000; $i++) {
         parse_ini_file('./ini_parse_memory_leak_test.ini', true);
     }
     $b = memory_get_usage(true);
     $first_leak = $b - $a;
 
-    for ($i = 0; $i < 200000; $i++) {
+    for ($i = 0; $i < 2000; $i++) {
         parse_ini_file('./ini_parse_memory_leak_test.ini', true);
     }
 
     $c = memory_get_usage(true);
-    $second_leak = $c - $b;
+    $second_leak = $c - $a;
 
-    $count = $second_leak / $first_leak;
-    if ($count <= 1 || $second_leak < 10*1024*1024) {
+    if ($output === false) {
+        return;
+    }
+
+    $count = 0;
+    if ($first_leak > 0) {
+        $count = $second_leak / $first_leak;
+    }
+
+    if ($count <= 1.25) {
         echo "OK";
     } else {
         echo "leak first:".$first_leak.", leak second:".$second_leak.", times:".$count."\n";
     }
 }
 
-leak_test();
+leak_test(false);
+leak_test(true);

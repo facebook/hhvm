0a202453bc68c994112fe5113375f53f21e9e538
Keyur Govande <keyur@php.net>
2015-04-08 01:07:22 +0000

    Fix ZendSorting performance.
    Details: The old implementation use zend-collator which does a lot of useless
    conversions to UTF-16 back to UTF-8 and comparisons using ICU, all of which
    gets really expensive.
    The new implementation ports the existing Zend functionality to HHVM and
    relies on regular string/numeric comparison functions where possible. The
    performance is an order of magnitude faster.

---
+++ hphp/test/slow/array/zend_sort_locale.php
@@ -0,0 +1,17 @@
+<?php
+setlocale(LC_ALL, 'fr_FR.ISO8859-1', 'fr_FR');
+$table = array("AB" => "Alberta",
+"BC" => "Colombie-Britannique",
+"MB" => "Manitoba",
+"NB" => "Nouveau-Brunswick",
+"NL" => "Terre-Neuve-et-Labrador",
+"NS" => "Nouvelle-�cosse",
+"ON" => "Ontario",
+"PE" => "�le-du-Prince-�douard",
+"QC" => "Qu�bec",
+"SK" => "Saskatchewan",
+"NT" => "Territoires du Nord-Ouest",
+"NU" => "Nunavut",
+"YT" => "Territoire du Yukon");
+asort($table, SORT_LOCALE_STRING);
+var_dump($table);

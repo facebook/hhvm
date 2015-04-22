211ee22e2269398d408afc8ca376c29d57c702e1
Bill Nell <bnell@fb.com>
2015-04-22 09:46:41 -0700

    Make getTyped return a SmartPtr. Add deprecation message.
    Summary: Make getTyped return a SmartPtr.  Add deprecation message.
    
    I'm not sure about the deprecated attribute (and message).  It didn't throw any warnings in my sandbox when I tried to use getTyped.  I assume we haven't enabled these yet.
    
    Reviewed By: @edwinsmith
    
    Differential Revision: D1898356

--- hphp/runtime/base/type-object.h
+++ hphp/runtime/base/type-object.h
@@ -112,7 +112,8 @@ public:
    * o.getTyped<T>(true,  true) -> dyn_cast_or_null<T>(o)
    */
   template<typename T>
-  T *getTyped(bool nullOkay = false, bool badTypeOkay = false) const {
+  [[deprecated("Please use one of the cast family of functions instead.")]]
+  SmartPtr<T> getTyped(bool nullOkay = false, bool badTypeOkay = false) const {
     static_assert(std::is_base_of<ObjectData, T>::value, "");
 
     ObjectData *cur = get();
@@ -129,7 +130,7 @@ public:
       return nullptr;
     }
 
-    return static_cast<T*>(cur);
+    return SmartPtr<T>(static_cast<T*>(cur));
   }
 
   template<typename T>

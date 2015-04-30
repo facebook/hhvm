075bec3e24b8911e8cd438c5ccbba7341ebd5b13
Thomas Colomb <thomas.colomb@dailymotion.com>
2015-04-30 08:27:05 +0000

    fix lint problems

--- hphp/test/slow/ext_date/json_decode.php
+++ hphp/test/slow/ext_date/json_decode.php
@@ -1,4 +1,5 @@
 <?php
 
-$dateTimeImmutable = new DateTimeImmutable('2015-05-03', new DateTimeZone('Arctic/Longyearbyen'));
+$dateTimeZone = new DateTimeZone('Arctic/Longyearbyen');
+$dateTimeImmutable = new DateTimeImmutable('2015-05-03', $dateTimeZone);
 var_dump(json_encode($dateTimeImmutable));

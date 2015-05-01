58f9085bc8e7c1a2f8a37a058c565e62cbf9d631
Joel Marcey <joelm@fb.com>
2015-04-30 20:30:43 -0700

    Fix test-context.h test
    Summary: I think two commits (D2033896 and D2021087) landed close to each other and there was a clobber. Trunk
    is failing build with this test as is now.
    
    ```
    hphp/runtime/test/test-context.h:29:33: error: too many initializers for 'HPHP::jit::TransContext'
    ```
    
    Reviewed By: @markw65
    
    Differential Revision: D2038466

--- hphp/runtime/test/test-context.h
+++ hphp/runtime/test/test-context.h
@@ -26,7 +26,7 @@ namespace HPHP { namespace jit {
 // A TransContext for use in tests.
 const auto test_context =
   TransContext { kInvalidTransID, 0, FPInvOffset{0}, false, 0,
-                 nullptr, false };
+                 nullptr };
 
 //////////////////////////////////////////////////////////////////////
 

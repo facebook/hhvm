2e0fcb32e91392e636ad2c0f3544002f4805f428
thbourlove <thbourlove@gmail.com>
2015-05-05 08:59:34 +0800

    test with random port

--- hphp/test/slow/ext_socket/client_float_timeout_open_port.php
+++ hphp/test/slow/ext_socket/client_float_timeout_open_port.php
@@ -1,5 +1,16 @@
 <?php
-$port = 62534;
+function random_free_port() {
+  for ($i = 0; $i < 100; $i++) {
+    $port = rand(50000, 65000);
+    if ($socket = @socket_create_listen($port)) {
+      socket_close($socket);
+      return $port;
+    }
+  }
+  return 0;
+}
+
+$port = random_free_port();
 $time = microtime(true);
 @stream_socket_client("tcp://172.0.0.1:$port", $errno, $errstr, 0.001);
 $elapsed = microtime(true) - $time;

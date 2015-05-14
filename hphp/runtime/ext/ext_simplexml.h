a581e312aa5c4a2bf0fa3eece583dfe1f48dbbff
matthias <matthias@icontroller.be>
2015-05-15 00:00:20 +0200

    Added SimpleXMLIterator

--- hphp/runtime/ext/ext_simplexml.h
+++ hphp/runtime/ext/ext_simplexml.h
@@ -61,7 +61,7 @@ struct c_SimpleXMLElement : SimpleXMLElementBase {
   void sweep();
 
   explicit c_SimpleXMLElement(Class* cls = c_SimpleXMLElement::classof());
-  public: ~c_SimpleXMLElement();
+  public: virtual ~c_SimpleXMLElement();
   public: void t___construct(const String& data, int64_t options = 0,
                              bool data_is_url = false, const String& ns = "",
                              bool is_prefix = false);
@@ -69,7 +69,7 @@ struct c_SimpleXMLElement : SimpleXMLElementBase {
   public: Variant t_offsetget(const Variant& index);
   public: void t_offsetset(const Variant& index, const Variant& newvalue);
   public: void t_offsetunset(const Variant& index);
-  public: Variant t_getiterator();
+  public: Variant t_getiteratorfortraversable();
   public: int64_t t_count();
   public: Variant t_xpath(const String& path);
   public: bool t_registerxpathnamespace(const String& prefix, const String& ns);
@@ -143,6 +143,26 @@ class c_SimpleXMLElementIterator : public ExtObjectData {
 
 public:
   SmartPtr<c_SimpleXMLElement> sxe;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// class SimpleXMLElementIterator
+
+class c_SimpleXMLIterator : public c_SimpleXMLElement {
+ public:
+  DECLARE_CLASS_NO_SWEEP(SimpleXMLIterator)
+
+  c_SimpleXMLIterator(Class* cls = c_SimpleXMLIterator::classof())
+    : c_SimpleXMLElement(cls)
+  {}
+  ~c_SimpleXMLIterator() {}
+  Variant t_current();
+  Variant t_key();
+  Variant t_next();
+  Variant t_rewind();
+  Variant t_valid();
+  bool t_haschildren();
+  Object t_getchildren();
 };
 
 ///////////////////////////////////////////////////////////////////////////////
--- hphp/test/quick/builtin_extension_SimpleXMLElement.php
+++ hphp/test/quick/builtin_extension_SimpleXMLElement.php
@@ -6,3 +6,22 @@ class A_SimpleXMLElement extends SimpleXMLElement {
   public $___x;
 }
 test("SimpleXMLElement");
+
+
+function basicXML() {
+  return
+'<root>
+  <a><a1>a1</a1></a>
+  <b>b</b>
+  <c>c1</c>
+</root>';
+}
+
+function testIteration() {
+  $s = new SimpleXMLElement(basicXMl());
+  foreach ($s as $i => $e) {
+    echo 'Of basic XML structure the ';
+    echo ($i+1) . "th nodes contents is: $e\n";
+  }
+}
+testIteration();

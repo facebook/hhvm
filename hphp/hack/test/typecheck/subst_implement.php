55b4dcc9b20b113f83b4fa34a4773384f51cb189
Brian Rogoff <bpr@fb.com>
2015-05-21 08:55:25 -0700

    Add check and test case for interface attempting to implement
    Summary: Added check in parser to prevent interface with 'implements'. Fixes T7028878
    
    Reviewed By: @int3
    
    Differential Revision: D2086918

--- hphp/hack/test/typecheck/subst_implement.php
+++ hphp/hack/test/typecheck/subst_implement.php
@@ -1,17 +1,9 @@
 <?hh // strict
-/**
- * Copyright (c) 2014, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the "hack" directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- *
- */
 
 interface A {}
+
 interface B extends A {}
-interface Beep<T, T2 as T> {
-}
-interface Boop implements Beep<A, B> {
-}
+
+interface Beep<T, T2 as T> {}
+
+interface Boop extends Beep<A, B> {}

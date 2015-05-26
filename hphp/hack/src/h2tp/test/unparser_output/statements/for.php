3f5d90d9ec799ba27df10edade6603bc9e06238b
Jez Ng <jezng@fb.com>
2015-05-26 11:17:49 -0700

    Format: Improve handling of multi-line for loops
    Summary: * Avoids adding a trailing comma to multi-line for loop conditions,
      which is a syntax error
    * Much nicer indentation overall (see h2tp tests for old ugliness)
    
    Closes https://github.com/facebook/hhvm/issues/5390
    
    Reviewed By: @fredemmott
    
    Differential Revision: D2098291

--- hphp/hack/src/h2tp/test/unparser_output/statements/for.php
+++ hphp/hack/src/h2tp/test/unparser_output/statements/for.php
@@ -8,10 +8,10 @@ class Foo {
     for (; $y < 3; $y++) {
       echo ("fi\n");
     }
-    for ($i = 1, $j = 1; ($i++) < 3, ($i + $j) < 7;
-    ++$i,
-    ++$j,
-    print("fo\n")
+    for (
+      $i = 1, $j = 1;
+      ($i++) < 3, ($i + $j) < 7;
+      ++$i, ++$j, print("fo\n")
     ) {
     }
   }

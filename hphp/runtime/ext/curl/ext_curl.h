8b28dd1bce9e812bfdeb0ca13b507042baf6d7c8
Jim Radford <radford@blackbean.org>
2015-06-01 12:51:40 -0700

    Add curl_strerror
    Summary: Closes https://github.com/facebook/hhvm/pull/5431
    
    Reviewed By: @jwatzman
    
    Differential Revision: D2114777

--- hphp/runtime/ext/curl/ext_curl.h
+++ hphp/runtime/ext/curl/ext_curl.h
@@ -278,6 +278,7 @@ Variant HHVM_FUNCTION(curl_exec, const Resource& ch);
 Variant HHVM_FUNCTION(curl_getinfo, const Resource& ch, int opt = 0);
 Variant HHVM_FUNCTION(curl_errno, const Resource& ch);
 Variant HHVM_FUNCTION(curl_error, const Resource& ch);
+String HHVM_FUNCTION(curl_strerror, int code);
 Variant HHVM_FUNCTION(curl_close, const Resource& ch);
 void HHVM_FUNCTION(curl_reset, const Resource& ch);
 Resource HHVM_FUNCTION(curl_multi_init);

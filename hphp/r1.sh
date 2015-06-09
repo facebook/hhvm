3adea853016986a3ee4f6f7f00e34d1aa4757deb
Robert R. Henry <rrh@newrelic.com>
2015-06-09 13:51:05 -0700

    Remove rogue files.

--- hphp/r1.sh
+++
@@ -1,7 +0,0 @@
-test/run --threads 8 \
-'test/server/fastcgi/tests/global_variables_server.php' \
-'test/slow/datetime/timezone_identifiers_list_per_country.php' \
-'test/slow/ext_ezc_test/caught-exception-recursive.php' \
-'test/slow/ext_ezc_test/ezc-throw.php' \
-'test/slow/ext_ezc_test/wrapped-exception.php' \
-'test/slow/ext_memcache/session_support.php' \

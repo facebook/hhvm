6757b991e860e997a35777242f3fe22ef742b13a
Josh Watzman <jwatzman@fb.com>
2015-06-12 14:16:43 -0700

    Backport INI patch properly

--- hphp/runtime/base/runtime-option.cpp
+++ hphp/runtime/base/runtime-option.cpp
@@ -962,6 +962,12 @@ void RuntimeOption::Load(IniSetting::Map& ini, Hdf& config,
     Config::Bind(TimeoutsUseWallTime, ini, eval["TimeoutsUseWallTime"], true);
     Config::Bind(CheckFlushOnUserClose, ini, eval["CheckFlushOnUserClose"],
                  true);
+    // Enables the hotfixing of a bug that occurred with D1797805 where
+    // per request user settings (like upload_max_filesize) were not able to be
+    // accessed on a server request any longer. The longer term fix is in review
+    // D2099778, but we want that to simmer in Master for a while and we need
+    // a hotfix for our current 3.6 LTS (Github Issue #4993)
+    Config::Bind(EnableZendIniCompat, ini, eval["EnableZendIniCompat"], true);
 
     if (EnableHipHopSyntax) {
       // If EnableHipHopSyntax is true, it forces EnableXHP to true
@@ -1530,13 +1536,6 @@ void RuntimeOption::Load(IniSetting::Map& ini, Hdf& config,
 
   refineStaticStringTableSize();
 
-  // Enables the hotfixing of a bug that occurred with D1797805 where
-  // per request user settings (like upload_max_filesize) were not able to be
-  // accessed on a server request any longer. The longer term fix is in review
-  // D2099778, but we want that to simmer in Master for a while and we need
-  // a hotfix for our current 3.6 LTS (Github Issue #4993)
-  Config::Bind(EnableZendIniCompat, ini, config, "Eval.EnableZendIniCompat",
-               true);
   // Language and Misc Configuration Options
   IniSetting::Bind(IniSetting::CORE, IniSetting::PHP_INI_ONLY, "expose_php",
                    &RuntimeOption::ExposeHPHP);

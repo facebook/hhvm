fbf9ffbafe2c361a66d9e4018d594ea769881d5d
mwilliams <mwilliams@fb.com>
2015-06-22 20:14:01 -0700

    Fix func_*_args funcs in non-global pseudomains
    Summary: PHP5 warns for any func_*_args call in a
    pseudomain, regardless of whether its in global scope
    or not. We were only warning in global scope. This
    makes us match, and also makes the test cheaper.
    
    Reviewed By: @paulbiss
    
    Differential Revision: D2181342

--- hphp/runtime/ext/std/ext_std_function.cpp
+++ hphp/runtime/ext/std/ext_std_function.cpp
@@ -108,8 +108,7 @@ static Variant func_get_arg_impl(int arg_num) {
   if (ar == nullptr) {
     return false;
   }
-  if ((ar->func()->attrs() & AttrMayUseVV) &&
-      ar->hasVarEnv() && ar->getVarEnv()->isGlobalScope()) {
+  if (ar->func()->isPseudoMain()) {
     raise_warning(
       "func_get_arg():  Called from the global scope - no function context"
     );
@@ -189,8 +188,7 @@ Array hhvm_get_frame_args(const ActRec* ar, int offset) {
 #define FUNC_GET_ARGS_IMPL(offset) do {                                        \
   EagerCallerFrame cf;                                                         \
   ActRec* ar = cf.actRecForArgs();                                             \
-  if (ar && (ar->func()->attrs() & AttrMayUseVV) &&                            \
-      ar->hasVarEnv() && ar->getVarEnv()->isGlobalScope()) {                   \
+  if (!ar || ar->func()->isPseudoMain()) {                                     \
     raise_warning(                                                             \
       "func_get_args():  Called from the global scope - no function context"   \
     );                                                                         \
@@ -224,8 +222,7 @@ static int64_t func_num_args_impl() {
   if (ar == nullptr) {
     return -1;
   }
-  if ((ar->func()->attrs() & AttrMayUseVV) &&
-      ar->hasVarEnv() && ar->getVarEnv()->isGlobalScope()) {
+  if (ar->func()->isPseudoMain()) {
     raise_warning(
       "func_num_args():  Called from the global scope - no function context"
     );

fdd32b806ab1b2949e8e11fa22500894bc32fa65
Kasper Kopec <kasper@fb.com>
2015-06-26 23:01:36 -0700

    Avoid infinite loop on recursive types resulting from unifying T and ?T
    Summary: I've spent some time trying to avoid this case completely (i.e making T and ?T always unify to ?T), but I couldn't get it to work and I'm not convinced if it's possible to do in general case, so I did the same approach that we already have for Tunresolved.
    
    Reviewed By: @int3
    
    Differential Revision: D2197408

---
+++ hphp/hack/test/typecheck/recursive_optional.php
@@ -0,0 +1,19 @@
+<?hh
+
+function compact<T>(Vector<?T> $vector): Vector<T> {
+  //UNSAFE
+}
+
+function test(bool $b): ?string {
+
+  $nullable = Vector {null}; // Vector([?T]])
+  $non_nullable = compact($nullable); // Vector([T])
+
+  if ($b) {
+    $res = $nullable;
+  } else {
+    $res = $non_nullable;
+  }
+  // integrating if branches unifies ?T with T, resulting in recursive type
+  return $res[0]; // Toption(Toption(Toption(Toption(Toption(...)))))
+}

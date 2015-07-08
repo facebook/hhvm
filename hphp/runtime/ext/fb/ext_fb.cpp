edf90978c2a2653eda151b736b1d80f99020709d
Orvid King <blah38621@gmail.com>
2015-07-08 12:39:58 -0700

    Removed the unneeded mysql includes in ext_fb
    Summary: ext_fb has no business, nor need, to include the mysql headers or extension, and, as ext_fb is, indirectly required for HHVM to compile properly, it's not possible to build without mysql with these includes present.
    
    By removing these unneeded includes, we move closer to being able to compile HHVM without mysql. There are still a couple other places in HHVM that do require mysql, but I intend to deal with those in separate PRs.
    Closes https://github.com/facebook/hhvm/pull/5573
    
    Reviewed By: @paulbiss
    
    Differential Revision: D2214904

--- hphp/runtime/ext/fb/ext_fb.cpp
+++ hphp/runtime/ext/fb/ext_fb.cpp
@@ -45,8 +45,6 @@
 #include "hphp/runtime/base/thread-info.h"
 #include "hphp/runtime/ext/std/ext_std_function.h"
 #include "hphp/runtime/ext/fb/FBSerialize.h"
-#include "hphp/runtime/ext/mysql/ext_mysql.h"
-#include "hphp/runtime/ext/mysql/mysql_common.h"
 #include "hphp/runtime/ext/fb/VariantController.h"
 #include "hphp/runtime/vm/unwind.h"
 

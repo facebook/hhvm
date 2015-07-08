93945a270bfc6d48ee24c225cb78775231790d5c
Orvid King <blah38621@gmail.com>
2015-07-08 15:49:30 -0700

    Add a HHVM_REMOVE_MATCHES_FROM_LISTS CMake function
    Summary: There are numerous places in the source tree (mostly in the third-party repo) that loop trhough the files returned by `auto_sources` to remove files matching certain patterns, usually tests. The single loop on it's own is fine, but, when you start adding header files to it, as is needed to be useful in Visual Studio, it starts to become unmaintainable. To counter that, HHVM_REMOVE_MATCHES_FROM_LISTS is a single function that allows you to pass in the names of the lists, the matches to remove, and the matches to ignore.
    This also replaces a couple of places using a basic foreach loop with a call to this function instead.
    Closes https://github.com/facebook/hhvm/pull/5581
    
    Reviewed By: @sgolemon
    
    Differential Revision: D2216856

--- CMake/HPHPFunctions.cmake
+++ CMake/HPHPFunctions.cmake
@@ -210,3 +210,67 @@ macro(HHVM_EXT_OPTION EXTNAME PACKAGENAME)
     find_package(${PACKAGENAME} REQUIRED)
   endif()
 endmacro()
+
+# Remove all files matching a set of patterns, and,
+# optionally, not matching a second set of patterns,
+# from a set of lists.
+#
+# Example:
+# This will remove all files in the CPP_SOURCES list
+# matching "/test/" or "Test.cpp$", but not matching
+# "BobTest.cpp$".
+# HHVM_REMOVE_MATCHES_FROM_LISTS(CPP_SOURCES MATCHES "/test/" "Test.cpp$" IGNORE_MATCHES "BobTest.cpp$")
+# 
+# Parameters:
+# 
+# [...]:
+# The names of the lists to remove matches from.
+#
+# [MATCHES ...]:
+# The matches to remove from the lists.
+#
+# [IGNORE_MATCHES ...]:
+# The matches not to remove, even if they match
+# the main set of matches to remove.
+function(HHVM_REMOVE_MATCHES_FROM_LISTS)
+  set(LISTS_TO_SEARCH)
+  set(MATCHES_TO_REMOVE)
+  set(MATCHES_TO_IGNORE)
+  set(argumentState 0)
+  foreach (arg ${ARGN})
+    if ("x${arg}" STREQUAL "xMATCHES")
+      set(argumentState 1)
+    elseif ("x${arg}" STREQUAL "xIGNORE_MATCHES")
+      set(argumentState 2)
+    elseif (argumentState EQUAL 0)
+      list(APPEND LISTS_TO_SEARCH ${arg})
+    elseif (argumentState EQUAL 1)
+      list(APPEND MATCHES_TO_REMOVE ${arg})
+    elseif (argumentState EQUAL 2)
+      list(APPEND MATCHES_TO_IGNORE ${arg})
+    else()
+      message(FATAL_ERROR "Unknown argument state!")
+    endif()
+  endforeach()
+  
+  foreach (theList ${LISTS_TO_SEARCH})
+    foreach (entry ${${theList}})
+      foreach (match ${MATCHES_TO_REMOVE})
+        if (${entry} MATCHES ${match})
+          set(SHOULD_IGNORE OFF)
+          foreach (ign ${MATCHES_TO_IGNORE})
+            if (${entry} MATCHES ${ign})
+              set(SHOULD_IGNORE ON)
+              break()
+            endif()
+          endforeach()
+          
+          if (NOT SHOULD_IGNORE)
+            list(REMOVE_ITEM ${theList} ${entry})
+          endif()
+        endif()
+      endforeach()
+    endforeach()
+    set(${theList} ${${theList}} PARENT_SCOPE)
+  endforeach()
+endfunction()

e3338449ca018661c0e28a9ae5475227f8866f2a
Peter Marshall <p.s.marshall0@gmail.com>
2015-07-08 14:08:37 -0400

    Unconditionally set the MRB

--- hphp/runtime/vm/jit/code-gen-helpers-x64.cpp
+++ hphp/runtime/vm/jit/code-gen-helpers-x64.cpp
@@ -82,14 +82,10 @@ void emitIncRef(Vout& v, Vreg base) {
   v << inclm{base[FAST_REFCOUNT_OFFSET], sf};
   emitAssertFlagsNonNegative(v, sf);
 
-  // if the refcount is greater than 1, set the mrb
-  auto const sf2 = v.makeReg();
-  v << cmplim{1, base[FAST_REFCOUNT_OFFSET], sf2};
-  ifThen(v, CC_NLE, sf2, [&](Vout& v) {
-    // TODO don't hardcode this
-    // TODO orb instruction
-    v << orwim{1 << 2, base[HeaderKindOffset + 1], v.makeReg()};
-  });
+  // set the mrb
+  // TODO don't hardcode this
+  // TODO orb instruction
+  v << orwim{1 << 2, base[HeaderKindOffset + 1], v.makeReg()};
 
 }
 

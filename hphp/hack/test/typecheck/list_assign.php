fbeab6da7bb25fdb28f363712c534777fb4e04cb
Dwayne Reeves <dreeves@fb.com>
2015-07-08 17:42:50 -0700

    Support list assignment for Tabstract of a vector
    Summary: If a Vector or a Pair is wrapped in a ##Tabstract## (either a generic, newtype,
    or type constant), then the assignment will fail even though we allow this for
    the Vector and Pair type.
    
    This fixes that case.
    
    Reviewed By: @jwatzman
    
    Differential Revision: D2226799

---
+++ hphp/hack/test/typecheck/list_assign.php
@@ -0,0 +1,13 @@
+<?hh // strict
+
+function test<T as Vector<int>>(Vector<int> $vec, T $list): (int, int) {
+  list($x, $y) = $vec;
+  hh_show($x);
+  hh_show($y);
+
+  list($i1, $i2) = $list;
+  hh_show($i1);
+  hh_show($i2);
+
+  return tuple($i1, $i2);
+}

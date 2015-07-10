fe1cdbfe117b4e8bd73be810e7ee251ff263bfb2
Dwayne Reeves <dreeves@fb.com>
2015-07-09 19:35:38 -0700

    Create stable expression dependent types for local variables
    Summary: One final piece of analysis I want to add for tracking late bound types is for
    locals. The goal of the analysis is:
    
    1) Expression dependent types generated from a local variable should be stable
    if the local has not be assigned.
    
    2) After assigning local ##$foo## to local ##$bar##, ##$bar## should generate
    the same dependent type as ##$foo##
    
    The easiest way to achieve this was to add an additional value we track with
    locals called an 'expression id'. An expression id is generated each time a
    local variable is assigned a new value. If it is assigned a local variable,
    then the expression id is transferred from one local to another, otherwise a
    new expression id is generated.
    
    I've written out in great detail what happens in the test cases I've added
    
    Reviewed By: @jwatzman
    
    Differential Revision: D2212586

---
+++ hphp/hack/test/typecheck/tconst/lvar_expr_dependent4.php
@@ -0,0 +1,29 @@
+<?hh // strict
+
+// Test of expression dependent type of local variable for foreach
+
+abstract class C {
+  abstract const type T as num;
+
+  abstract public function get(): this::T;
+  abstract public function get_a_C(): C;
+
+  public function test(C $k, C $v, KeyedTraversable<C, C> $map): void {
+    // New stable id
+    hh_show($k->get());
+    hh_show($k->get());
+    // New stable id
+    hh_show($v->get());
+    hh_show($v->get());
+
+    foreach ($map as $k => $v) {
+      // New ids within the loop
+      hh_show($k->get());
+      hh_show($v->get());
+    }
+
+    // New ids outside of loop
+    hh_show($k->get());
+    hh_show($v->get());
+  }
+}

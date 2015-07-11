e9eebb939e3825e503903d9939c69d504f74ee91
Dwayne Reeves <dreeves@fb.com>
2015-07-11 12:02:13 -0700

    Fix infinite recursion in Typing_utils.simplified_uerror
    Summary: Had a bug where we would continually try to simplify an unification error
    produced by an unconstraint expression dependent type. When checking if we
    should simplify I need to make sure its not only a expression dependent type
    but also wraps some base type.
    
    Reviewed By: @int3
    
    Differential Revision: D2235706

---
+++ hphp/hack/test/typecheck/tconst/simplify_error.php
@@ -0,0 +1,14 @@
+<?hh // strict
+
+abstract class P<T> {
+  abstract protected function foo(): T;
+}
+
+abstract class C extends P<this::T> {
+  abstract const type T;
+  // Silence the Naming phase errors so we see the
+  // unification error
+  /* HH_IGNORE_ERROR[2002] */
+  /* HH_IGNORE_ERROR[2049] */
+  abstract protected function foo(): T;
+}

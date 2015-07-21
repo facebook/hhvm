05ccc3983e69db7f529085ddeecd209693da2218
Michael Sullivan <msullivan@fb.com>
2015-07-21 14:01:41 -0700

    Add support for nontrivial property initializers in experimental emitter
    Summary: Add support for nontrivial property initializers in experimental emitter. This includes support for emitting the literal format as well as generating a 86pinit method to initialize non-constant properties.
    
    Depends on D2238047.
    
    Reviewed By: @jwatzman
    
    Differential Revision: D2262003

---
+++ hphp/hack/test/emitter/props.php
@@ -0,0 +1,29 @@
+<?hh // strict
+
+class C {
+  public int $arithmetic = 100+100;
+  public int $intvar = 100;
+  public array<int> $arrayvar1 = array(100);
+  public array<array<int>> $arrayvar2 = array(array(100), array(200));
+  public array<int> $arrayvar3 = array();
+  public string $strvar1 = 'hello\n';
+  public string $strvar2 = "hello\n";
+  public bool $boolvar1 = true;
+  public bool $boolvar2 = false;
+  public float $floatvar = 3.1415;
+  public ?bool $nullvar = null;
+}
+
+function test(): void {
+  $x = new C();
+  var_dump($x->arithmetic);
+  var_dump($x->intvar);
+  var_dump($x->arrayvar1);
+  var_dump($x->arrayvar2);
+  var_dump($x->strvar1);
+  var_dump($x->strvar2);
+  var_dump($x->boolvar1);
+  var_dump($x->boolvar2);
+  var_dump($x->floatvar);
+  var_dump($x->nullvar);
+}

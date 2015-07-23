506d7801bb04534c8b8ea4eadd33157621d89b16
Michael Sullivan <msullivan@fb.com>
2015-07-23 11:44:24 -0700

    Implement await/async support in experimental emitter
    Summary: Implement await/async support in the experimental emitter. Await is sort of weird since it doesn't preserve the stack state, so when we are doing "... = await ..." we actually have to evaluate the RHS first, save it off into a variable, then evaluate the LHS so we can assign to it.
    
    Depends on D2262380.
    
    Reviewed By: @jano
    
    Differential Revision: D2262456

---
+++ hphp/hack/test/emitter/async.php
@@ -0,0 +1,68 @@
+<?hh // strict
+
+function f(int $x): int {
+  echo "f(",$x,")\n";
+  return $x;
+}
+
+async function genNum(int $x): Awaitable<int> {
+  if ($x == 1) {
+    await wait();
+  }
+  return $x;
+}
+
+async function genNum2(int $x, int $y): Awaitable<int> {
+  return $x+$y;
+}
+
+async function foo0(): Awaitable<int> {
+  return await genNum(0);
+}
+
+async function foo1(): Awaitable<int> {
+  $x = await genNum(0);
+  $y = await genNum(1);
+  return $x + $y;
+}
+
+async function foo2(): Awaitable<int> {
+  list($x, $y) = await gena_(array(genNum(f(1)), genNum(f(2))));
+  return $x + $y;
+}
+
+async function foo3(): Awaitable<array<int>> {
+  $nus = array();
+  $nus[f(0)] = await genNum2(f(1), f(2));
+  return $nus;
+}
+
+function prep<T>(Awaitable<T> $aw): T {
+  /* HH_FIXME[4053]: WaitHandle missing join in hh_single_type_check */
+  return $aw->getWaitHandle()->join();
+}
+
+function gena_<Tk, Tv>(
+  KeyedTraversable<Tk, Awaitable<Tv>> $awaitables
+  ): Awaitable<array<Tv>> {
+  $wait_handles = array();
+  foreach ($awaitables as $index => $awaitable) {
+    $wait_handles[$index] = $awaitable->getWaitHandle();
+  }
+  /* HH_FIXME[2049] */
+  /* HH_FIXME[4026] */
+  return HH\GenArrayWaitHandle::create($wait_handles);
+}
+
+function wait(): Awaitable<void> {
+  /* HH_FIXME[2049] */
+  /* HH_FIXME[4026] */
+  return HH\RescheduleWaitHandle::create(0, 0);
+}
+
+function test(): void {
+  var_dump(prep(foo0()));
+  var_dump(prep(foo1()));
+  var_dump(prep(foo2()));
+  var_dump(prep(foo3()));
+}

06ae27be0478c94a610be6afd230a16a58c02167
Michael Sullivan <msullivan@fb.com>
2015-07-24 12:05:46 -0700

    Make the experimental emitter a standalone binary
    Summary: Make the experimental emitter be a standalone binary. This
    gives us a way to run the emitter that is disconnected from
    hh_single_type_check's roll as a test harness. hh_emitter does not run
    typechecking and ignores naming errors so that it can be used to emit
    files that would not pass checks when considered in isolation (that
    is, ~all files in a real web project).
    
    Reviewed By: @int3
    
    Differential Revision: D2275707

--- hphp/hack/test/emitter/emitter-test.sh
+++ hphp/hack/test/emitter/emitter-test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 TEST="$1"
-EMITTER="$2"
-HHVM="$3"
+CHECKER="$2"
+EMITTER="$3"
+HHVM="$4"
 
 HHVM_CMD="$HHVM -vRepo.Local.Mode=-- -vRepo.Central.Path=verify.hhbc"
 
@@ -12,6 +13,7 @@ EXPECTED="$TEST.exp"
 # First, compute the expected output using regular HHVM execution
 (cat "$TEST"; echo "test();") | $HHVM_CMD /dev/stdin > "$EXPECTED"
 
-# Bytecode compile with the emitter and run the generated assembly
-"$EMITTER" --emit "$TEST" > "$HHAS" &&
+# Typecheck, compile with the emitter and run the generated assembly
+"$CHECKER" "$TEST" > /dev/null &&
+"$EMITTER" --test "$TEST" > "$HHAS" &&
   $HHVM_CMD -vEval.AllowHhas=1 "$HHAS"

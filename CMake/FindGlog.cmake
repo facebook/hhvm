fd6b3a5522198fbf87dc2d226ee24be5bbd6041c
Elan Ruusamäe <glen@delfi.ee>
2015-07-26 14:38:14 +0300

    Update FindGlog.cmake

--- CMake/FindGlog.cmake
+++ CMake/FindGlog.cmake
@@ -1,7 +1,4 @@
 #
-# $Id$
-#
-# - Find libglog
 # Find libglog
 #
 #  LIBGLOG_INCLUDE_DIR - where to find glog/logging.h, etc.
@@ -18,7 +15,7 @@ FIND_PATH(LIBGLOG_INCLUDE_DIR glog/logging.h)
 
 FIND_LIBRARY(LIBGLOG_LIBRARY glog)
 
-# handle the QUIETLY and REQUIRED arguments and set Libmemcached_FOUND to TRUE if
+# handle the QUIETLY and REQUIRED arguments and set LIBGLOG_FOUND to TRUE if
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(LIBGLOG DEFAULT_MSG LIBGLOG_LIBRARY LIBGLOG_INCLUDE_DIR)

e455d51999ae1bb123486e7056c4b0933b7d6f70
Dwayne Reeves <dreeves@fb.com>
2015-07-28 17:32:02 -0700

    Properly handle the 'this' type in type constant constraints
    Summary: For type constants whose as constraints contained a `this` type we were not
    properly resolving the 'this' type to an expression dependent type. We did this
    for the `this` type in the assigned type of a type constant, so I moved the
    logic outside the pattern match so it applies to all cases.
    
    Reviewed By: @jwatzman
    
    Differential Revision: D2286736

---
+++ hphp/hack/test/typecheck/tconst/this_in_as_constraint.php
@@ -0,0 +1,12 @@
+<?hh // strict
+
+abstract class C {
+  abstract const type T1;
+  abstract const type T2 as this::T1;
+
+  abstract public function getT2(): this::T2;
+
+  public function getT1(): this::T1 {
+    return $this->getT2();
+  }
+}

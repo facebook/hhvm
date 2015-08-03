5c8b939de55340602cd8c03dce0147525f152353
Michael Sullivan <msullivan@fb.com>
2015-08-03 16:27:04 -0700

    Implement ::class
    Summary: Implement support for ::class in hh_emitter.
    
    Reviewed By: @int3
    
    Differential Revision: D2303236

---
+++ hphp/hack/test/emitter/colcol_class.php
@@ -0,0 +1,22 @@
+<?hh // strict
+
+class A {
+  public static function static_class(): void {
+    echo static::class, "\n";
+  }
+}
+
+class B extends A {
+  public static function stuff(): void {
+    echo self::class, "\n";
+    echo parent::class, "\n";
+  }
+}
+
+function test(): void {
+  echo A::class, "\n";
+  echo B::class, "\n";
+  A::static_class();
+  B::static_class();
+  B::stuff();
+}

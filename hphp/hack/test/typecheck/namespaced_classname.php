d9ea337d9319676d8400641fe244c4262894e4e9
Fred Emmott <fredemmott@fb.com>
2015-08-05 15:59:19 -0700

    Auto-import classname<T> into namespaces
    Summary: also, add runtime tests for both namespaced and non-namespaced versions
    
    Reviewed By: @jwatzman
    
    Differential Revision: D2311514

---
+++ hphp/hack/test/typecheck/namespaced_classname.php
@@ -0,0 +1,18 @@
+<?hh // strict
+
+namespace foo\bar;
+
+<<__ConsistentConstruct>>
+class Herp {}
+
+class Derp extends Herp {}
+
+function foo<T as Herp>(classname<T> $class): T {
+  return new $class();
+}
+
+function bar(Vector<string> $foo): void {}
+
+function main(): void {
+  foo(Derp::class);
+}
---
+++ hphp/test/slow/variable_class_name/hack_classname_t_ns.php
@@ -0,0 +1,19 @@
+<?hh
+
+namespace foo\bar;
+
+<<__ConsistentConstruct>>
+class Herp {
+}
+
+class Derp extends Herp { }
+
+function foo<T as Herp>(classname<T> $class): T { return new $class(); }
+
+function bar(Vector<string> $foo): void {}
+
+function main(): void {
+  var_dump(foo(Derp::class));
+}
+
+main();

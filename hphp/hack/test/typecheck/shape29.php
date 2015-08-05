33b00ca6b582a43c006fd9fdad475d3f9df4e09a
Xiao Shi <xshi@fb.com>
2015-08-04 21:19:55 -0700

    get rid of trailing comma in hh_show() for shapes
    Summary: also sort the fields by the field names
    
    Reviewed By: @int3
    
    Differential Revision: D2312090

---
+++ hphp/hack/test/typecheck/shape29.php
@@ -0,0 +1,8 @@
+<?hh // strict
+
+function test(): void {
+  $x = shape('foo' => 1, 'bar' => true, 'xyz' => 3.14, 'baz' => 'abc');
+  $y = shape('bar' => true, 'baz' => 'abc', 'foo' => 1, 'xyz' => 3.14);
+  hh_show($x);
+  hh_show($y);
+}

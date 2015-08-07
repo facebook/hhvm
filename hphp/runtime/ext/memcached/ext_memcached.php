3ca108ce1265db397b8fa77a90ba466341d7f552
Thomas Colomb <thomas.colomb@dailymotion.com>
2015-08-07 13:22:48 +0000

    Implement Memcached::isPristine and Memcached::isPersistent functions.

--- hphp/runtime/ext/memcached/ext_memcached.php
+++ hphp/runtime/ext/memcached/ext_memcached.php
@@ -508,6 +508,24 @@ class Memcached {
                                  int $expiry = 0): mixed;
 
   /**
+   * Check if the instance was recently created
+   *
+   * @return bool - Returns the true if instance is recently created,
+   * false otherwise.
+   */
+  <<__Native>>
+  public function isPersistent(): bool;
+
+  /**
+   * Check if a persitent connection to memcache is being used.
+   *
+   * @return bool - Returns true if Memcache instance uses a persistent
+   * connection, false otherwise.
+   */
+  <<__Native>>
+  public function isPristine(): bool;
+
+  /**
    * Prepend data to an existing item
    *
    * @param string $key - The key of the item to prepend the data to.

70606e2ecb0b8041dd648379f43c7e3ef32f26bc
Sara Golemon <sgolemon@fb.com>
2015-08-07 16:07:33 -0700

    Stop using ActRec in DomDocument
    Summary: This took some refactoring because DomDocument::load...()
    depend on different behavior when they're called statically
    and we made the decision to not support that in normal HNI methods
    (hence the use of ActRec here).
    
    I'm moving the patently ridiculous `if (!$this)` logic into PHP
    where it's "properly" supported by the VM, and unifying the helpers
    down to just XML versus HTML, with isFile as a flag.
    
    I also disabled a dumb test which doesn't test anything useful.
    
    Reviewed By: @paulbiss
    
    Differential Revision: D2320987

--- hphp/test/slow/dom_document/load_static.php
+++ hphp/test/slow/dom_document/load_static.php
@@ -3,8 +3,5 @@
 var_dump(get_class(DOMDocument::loadXML('<html/>')));
 var_dump(get_class(DOMDocument::loadHTML('<html/>')));
 
-var_dump(DOMDocument::loadXML('<html/>', 1, 2));
-var_dump(DOMDocument::loadHTML('<html/>', 1, 2));
-
 var_dump(get_class(DOMDocument::loadXML('<html/>', LIBXML_NOWARNING + LIBXML_NOERROR)));
 var_dump(get_class(DOMDocument::loadHTML('<html/>', LIBXML_NOWARNING + LIBXML_NOERROR)));

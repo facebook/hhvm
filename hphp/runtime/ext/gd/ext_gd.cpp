2da81c1440fe0c59736cf325715b41b30d38c72e
Thomas Colomb <thomas.colomb@dailymotion.com>
2015-08-08 11:50:38 +0000

    Fix indentation

--- hphp/runtime/ext/gd/ext_gd.cpp
+++ hphp/runtime/ext/gd/ext_gd.cpp
@@ -3174,29 +3174,25 @@ Variant HHVM_FUNCTION(imagecrop, const Resource& image, const Array& rect) {
   if (!im) return false;
   if (rect.exists(s_x)) {
     gdrect.x = rect[s_x].toInt64();
-  }
-  else {
+  } else {
     raise_warning("imagecrop(): Missing x position");
     return false;
   }
   if (rect.exists(s_y)) {
     gdrect.y = rect[s_y].toInt64();
-  }
-  else {
+  } else {
     raise_warning("imagecrop(): Missing y position");
     return false;
   }
   if (rect.exists(s_width)) {
     gdrect.width = rect[s_width].toInt64();
-  }
-  else {
+  } else {
     raise_warning("imagecrop(): Missing width position");
     return false;
   }
   if (rect.exists(s_height)) {
     gdrect.height = rect[s_height].toInt64();
-  }
-  else {
+  } else {
     raise_warning("imagecrop(): Missing height position");
     return false;
   }

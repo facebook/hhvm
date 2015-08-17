049529ea4588381b76fb76b115e50e0aa9091778
Orvid King <blah38621@gmail.com>
2015-08-16 19:12:48 -0500

    Use alloca in ext_hash
    Rather than a C99 stack allocated array, which MSVC doesn't support.

--- hphp/runtime/ext/hash/ext_hash.cpp
+++ hphp/runtime/ext/hash/ext_hash.cpp
@@ -155,7 +155,8 @@ struct HashContext : SweepableResourceData {
     /* Just in case the algo has internally allocated resources */
     if (context) {
       assert(ops->digest_size >= 0);
-      unsigned char dummy[ops->digest_size];
+      unsigned char* dummy = (unsigned char*)alloca(
+        sizeof(unsigned char) * ops->digest_size);
       ops->hash_final(dummy, context);
       free(context);
     }

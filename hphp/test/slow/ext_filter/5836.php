facebook/hhvm

commit 2f48df45d331cfd51b586a274963eb21389925bf

Don't fallback to string filter when invalid filter is passed to filter_var
Summary: For some reason hhvm was falling back to string filter for when a filter isn't found when php simply returns false.

Fixes #2739

Reviewed By: @ptarjan

Differential Revision: D1346745

hphp/system/php/filter/filter_var_array.php
@@ -3,15 +3,20 @@
 function _filter_var_array_single($value, $filter, $options = array()) {
   $ret = filter_var($value, (int) $filter, $options);
 
+  // Retry with default filter if failed.
+  if ($ret === false) {
+    $ret = filter_var($value, FILTER_DEFAULT, $options);
+  }
+
   $flags = isset($options['flags']) ? $options['flags'] : 0;
   if ($flags & FILTER_FORCE_ARRAY && !is_array($ret)) {
-    $ret = array($ret);
+    return array($ret);
   }
   if ($flags & FILTER_REQUIRE_SCALAR && is_array($ret)) {
-    $ret = false;
+    return false;
   }
   if ($flags & FILTER_REQUIRE_ARRAY && is_null($ret)) {
-    $ret = array();
+    return array();
   }
 
   return $ret;
@@ -47,7 +52,6 @@ function _filter_var_array_single($value, $filter, $options = array()) {
    *                     the variable is not set.
    */
 function filter_var_array($data, $definition = null, $add_empty = true) {
-
   if (!is_array($data)) {
     trigger_error('filter_var_array() expects parameter 1 to be array, '.
       gettype($data).' given', E_USER_WARNING);

hphp/runtime/ext/filter/ext_filter.cpp
@@ -248,29 +248,26 @@ static Variant fail(bool return_null, const Variant& options) {
   return false;
 }
 
-static filter_list_entry php_find_filter(uint64_t id) {
+static const filter_list_entry* php_find_filter(uint64_t id) {
   int i, size = sizeof(filter_list) / sizeof(filter_list_entry);
 
   for (i = 0; i < size; ++i) {
     if (filter_list[i].id == id) {
-      return filter_list[i];
+      return &filter_list[i];
     }
   }
-  /* Fallback to "string" filter */
-  for (i = 0; i < size; ++i) {
-    if (filter_list[i].id == k_FILTER_DEFAULT) {
-      return filter_list[i];
-    }
-  }
-  // never hit
-  return filter_list[0];
+
+  return nullptr;
 }
 
 #define FAIL_IF(x) do { if (x) return false; } while (0)
 
 static bool filter_var(Variant& ret, const Variant& variable, int64_t filter,
                        const Variant& options) {
-  filter_list_entry filter_func = php_find_filter(filter);
+  const filter_list_entry* filter_func = php_find_filter(filter);
+  if (!filter_func) {
+    return false;
+  }
 
   int64_t flags;
   Variant option_array;
@@ -284,7 +281,7 @@ static bool filter_var(Variant& ret, const Variant& variable, int64_t filter,
 
   FAIL_IF(variable.isObject() && !variable.getObjectData()->hasToString());
 
-  ret = filter_func.function(variable.toString(), flags, option_array);
+  ret = filter_func->function(variable.toString(), flags, option_array);
   if (option_array.isArray() && option_array.toArray().exists(s_default) &&
       ((flags & k_FILTER_NULL_ON_FAILURE && ret.isNull()) ||
        (!(flags & k_FILTER_NULL_ON_FAILURE) && ret.isBoolean() &&

hphp/test/slow/ext_filter/filter_var_url.php
@@ -1,0 +1,2 @@
+<?php
+var_dump(filter_var('www.localtest.com', FILTER_VALIDATE_IP | FILTER_FLAG_IPV6));

hphp/test/slow/ext_filter/filter_var_url.php.expect
@@ -1,0 +1,1 @@
+bool(false)

commit f2fb726fef23c082c220779d2831ec50bc0abf99

filter_var_array: do not fall back to FILTER_DEFAULT
Summary: Fallback to `FILTER_DEFAULT` for failed validations of individual
elements was introduced in 2f48df4 while fixing a deeper bug where HHVM
always fell back to FILTER_DEFAULT when any `filter_var()` validation
failed. This fallback behavior is not compatible with the PHP5 engine which
applies no special result processing for failed validations.

Replaces #5837
Fixes #5836
Closes https://github.com/facebook/hhvm/pull/5939

Reviewed By: @sgolemon

Differential Revision: D2352105

hphp/system/php/filter/filter_var_array.php
@@ -1,12 +1,18 @@
 <?php
+function _filter_var_array_is_valid_filter($filter) {
+  static $ids = null;
+  if ($ids === null) {
+    // A bit painful in php, exposing the IDs might be better if this is hot
+    $ids = array_fill_keys(array_map('filter_id', filter_list()), null);
+  }
+  return array_key_exists($filter, $ids);
+}
 
 function _filter_var_array_single($value, $filter, $options = array()) {
-  $ret = filter_var($value, (int) $filter, $options);
-
-  // Retry with default filter if failed.
-  if ($ret === false) {
-    $ret = filter_var($value, FILTER_DEFAULT, $options);
+  if (!_filter_var_array_is_valid_filter($filter)) {
+    $filter = FILTER_DEFAULT;
   }
+  $ret = filter_var($value, (int) $filter, $options);
 
   $flags = isset($options['flags']) ? $options['flags'] : 0;
   if ($flags & FILTER_FORCE_ARRAY && !is_array($ret)) {
@@ -63,9 +69,7 @@ function filter_var_array($data, $definition = null, $add_empty = true) {
     if ($definition === null) {
       $default_filter = FILTER_DEFAULT;
     } else if (is_int($definition)) {
-      // A bit painful in php, exposing the IDs might be better if this is hot
-      $ids = array_fill_keys(array_map('filter_id', filter_list()), null);
-      if (!array_key_exists($definition, $ids)) {
+      if (!_filter_var_array_is_valid_filter($definition)) {
         return false;
       }
       $default_filter = $definition;

hphp/test/slow/ext_filter/5836.php
@@ -1,0 +1,25 @@
+<?php
+$data = array(
+  'true'     => '1',
+  'false'    => '0',
+  'badbool'  => 'xyzzy',
+  'bademail' => 'foo',
+  'badfloat' => 'idkfa',
+  'badint'   => '42.42',
+  'badip'    => '256.0.0.1',
+);
+
+$args = array(
+  'true'    => FILTER_VALIDATE_BOOLEAN,
+  'false'   => FILTER_VALIDATE_BOOLEAN,
+  'badbool' => array(
+    'filter' => FILTER_VALIDATE_BOOLEAN,
+    'flags'  => FILTER_NULL_ON_FAILURE,
+  ),
+  'bademail' => FILTER_VALIDATE_EMAIL,
+  'badfloat' => FILTER_VALIDATE_FLOAT,
+  'badint'   => FILTER_VALIDATE_INT,
+  'badip'    => FILTER_VALIDATE_IP,
+);
+
+var_dump(filter_var_array($data, $args));

hphp/test/slow/ext_filter/5836.php.expect
@@ -1,0 +1,16 @@
+array(7) {
+  ["true"]=>
+  bool(true)
+  ["false"]=>
+  bool(false)
+  ["badbool"]=>
+  NULL
+  ["bademail"]=>
+  bool(false)
+  ["badfloat"]=>
+  bool(false)
+  ["badint"]=>
+  bool(false)
+  ["badip"]=>
+  bool(false)
+}
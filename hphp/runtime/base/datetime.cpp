7fd9ee6d3676f2c9968288c2c1c25505f3f6cba0
Tim Siebels <tim_siebels_aurich@yahoo.de>
2015-08-27 21:47:59 +0200

    Change quick test, fix lint, return false on error

--- hphp/runtime/base/datetime.cpp
+++ hphp/runtime/base/datetime.cpp
@@ -440,19 +440,23 @@ void DateTime::setTimezone(req::ptr<TimeZone> timezone) {
   }
 }
 
-void DateTime::modify(const String& diff) {
+bool DateTime::modify(const String& diff) {
   timelib_error_container* error = nullptr;
   timelib_time *tmp_time = timelib_strtotime((char*)diff.data(), diff.size(),
                                              &error, TimeZone::GetDatabase(),
                                              TimeZone::GetTimeZoneInfoRaw);
   if (error && error->error_count > 0) {
-    raise_warning("DateTime::modify(): Failed to parse time string (%s) at position %d (%c): %s",
+    raise_warning("DateTime::modify(): Failed to parse time string (%s)"
+                  " at position %d (%c): %s",
       diff.c_str(), error->error_messages[0].position,
       error->error_messages[0].character, error->error_messages[0].message
     );
+    timelib_time_dtor(tmp_time);
+    return false;
   }
   internalModify(tmp_time);
   timelib_time_dtor(tmp_time);
+  return true;
 }
 
 void DateTime::internalModify(timelib_time *t) {
--- hphp/test/slow/ext_datetime/datetime_modify_error.php
+++ hphp/test/slow/ext_datetime/datetime_modify_error.php
@@ -2,5 +2,5 @@
 date_default_timezone_set("Europe/Berlin");
 
 $datetime = new DateTime('2015-01-08');
-$datetime->modify('invalid string');
+var_dump($datetime->modify('invalid string'));
 echo $datetime->format('Y-m-d');

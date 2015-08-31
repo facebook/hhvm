f3fb1085330e20e2a4ead36cb2bcfbf30d65b761
Sara Golemon <sgolemon@fb.com>
2015-08-31 09:41:34 -0700

    Add SHA-3 hashing algorithms
    Summary: sha3-224. sha3-256, sha3-384, and sha3-512
    based on spec and reference implementation.
    
    Reviewed By: @JoelMarcey
    
    Differential Revision: D2381820

---
+++ hphp/test/slow/hash/hash_keccak.php
@@ -0,0 +1,21 @@
+<?php
+
+// Test vectors taken from a combination of NIST FIPS-202,
+// Wikipedia reference vectors,
+// and output from reference implementation
+
+$subjects = [
+  '',
+  'a',
+  'The quick brown fox jumps over the lazy dog',
+  'The quick brown fox jumps over the lazy dog.',
+  str_repeat('a', 257),
+  str_repeat("\xA3", 200),
+];
+
+foreach ($subjects as $subject) {
+  echo '== ', urlencode($subject), " ==\n";
+  foreach ([224, 256, 384, 512] as $bits) {
+    echo $bits, ': ', hash("sha3-$bits", $subject), "\n";
+  }
+}

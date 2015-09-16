085535c71bd4b1f4bcddfc10aa039d600a445f03
Joe Marrama <jmarrama@box.com>
2015-09-16 12:23:24 -0700

    Add more tests to ensure OOMing works in the shutdown handler

--- hphp/test/slow/memory/shutdown_handler_can_still_oom.php
+++ hphp/test/slow/memory/shutdown_handler_can_still_oom.php
@@ -4,8 +4,7 @@ ini_set('memory_limit', '5M');
 
 register_shutdown_function(function () {
     print "IN SHUTDOWN".PHP_EOL;
-    $y = str_repeat('x', 1024 * 64);
-    echo strlen($y).PHP_EOL;
+    $y = str_repeat('x', 1024 * 1024 * 20);
 });
 
 function foo() {

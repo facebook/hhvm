419720f84212d68cd855151810c6ea8b90134be5
Alexander Chow <achow@fb.com>
2015-09-15 18:53:05 -0700

    Inherited const satisfies abstract const declaration
    Summary: Inherited const satisfies abstract const declaration
    
    Reviewed By: @dlreeves
    
    Differential Revision: D2445970

---
+++ hphp/hack/test/typecheck/inherited_const_satisfies_abstract_declaration.php
@@ -0,0 +1,14 @@
+<?hh // strict
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+interface I {
+  abstract const int THE_CONST;
+}
+
+class ActualClass {
+  const int THE_CONST = 0;
+}
+
+class FinalActualClass extends ActualClass implements I {
+  public function __construct() {}
+}

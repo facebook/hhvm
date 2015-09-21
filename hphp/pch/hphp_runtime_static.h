6d1c9d2489db8e80183324402119cb2ace6c4cbb
Orvid King <blah38621@gmail.com>
2015-09-21 15:59:26 -0700

    Add a few more includes to the PCH for MSVC
    Summary: Adding these makes compiles quite a bit faster.
    Closes https://github.com/facebook/hhvm/pull/6234
    
    Reviewed By: @paulbiss
    
    Differential Revision: D2437490

--- hphp/pch/hphp_runtime_static.h
+++ hphp/pch/hphp_runtime_static.h
@@ -4,3 +4,11 @@
 #include <folly/FilePortability.h>
 #include <folly/Portability.h>
 #include <folly/SocketPortability.h>
+
+#include "hphp/runtime/base/array-init.h"
+#include "hphp/runtime/base/builtin-functions.h"
+#include "hphp/runtime/base/config.h"
+#include "hphp/runtime/base/execution-context.h"
+#include "hphp/runtime/base/ini-setting.h"
+#include "hphp/runtime/base/program-functions.h"
+#include "hphp/runtime/base/runtime-option.h"

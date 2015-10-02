e0cb6b3f60e34afde1fdeaf4e7af918864026918
Mark Williams <mwilliams@fb.com>
2015-10-02 08:28:03 -0700

    Fix a couple of oom tests in repo mode
    Summary: They didn't oom in repo mode because we optimize the str_repeat to a
    literal string.
    
    Reviewed By: @alexmalyshev
    
    Differential Revision: D2500677

--- hphp/test/slow/memory/oom_does_not_oom_shutdown_handler.php
+++ hphp/test/slow/memory/oom_does_not_oom_shutdown_handler.php
@@ -1,15 +1,18 @@
 <?php
 
 ini_set('memory_limit', '5M');
+$rep = 'x';
 
 register_shutdown_function(function () {
+    global $rep;
     print "IN SHUTDOWN".PHP_EOL;
-    $y = str_repeat('x', 1024 * 64);
+    $y = str_repeat($rep, 1024 * 64);
     echo strlen($y).PHP_EOL;
 });
 
 function foo() {
-  $x = str_repeat('x', 1024 * 1024 * 20);
+  global $rep;
+  $x = str_repeat($rep, 1024 * 1024 * 20);
 }
 
 foo();
--- hphp/test/slow/memory/shutdown_handler_can_still_oom.php
+++ hphp/test/slow/memory/shutdown_handler_can_still_oom.php
@@ -1,14 +1,17 @@
 <?php
 
 ini_set('memory_limit', '5M');
+$rep = 'x';
 
 register_shutdown_function(function () {
+    global $rep;
     print "IN SHUTDOWN".PHP_EOL;
-    $y = str_repeat('x', 1024 * 1024 * 20);
+    $y = str_repeat($rep, 1024 * 1024 * 20);
 });
 
 function foo() {
-  $x = str_repeat('x', 1024 * 1024 * 20);
+  global $rep;
+  $x = str_repeat($rep, 1024 * 1024 * 20);
 }
 
 foo();

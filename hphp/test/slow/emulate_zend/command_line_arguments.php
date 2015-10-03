c72a4db7a3b3750670e2f7f5e5762a14f8bf3538
Andrew Peabody <apeabody@gmail.com>
2015-10-02 21:24:49 -0400

    Revised tests per comments

--- hphp/test/slow/emulate_zend/command_line_arguments.php
+++ hphp/test/slow/emulate_zend/command_line_arguments.php
@@ -10,6 +10,10 @@ $cmd = PHP_BINARY.' --php -n < '.SCRIPT_NAME;
 var_dump($cmd);
 system($cmd);
 
-$cmd = PHP_BINARY.' --php --define date.timezone=America/New_York '.SCRIPT_NAME;
+$cmd = PHP_BINARY.' --php --define date.timezone=America/New_York -r "var_dump((string) (new DateTime())->format(\'e\'));"';
+var_dump($cmd);
+system($cmd);
+
+$cmd = PHP_BINARY.' --php --define date.timezone=America/Los_Angeles -r "var_dump((string) (new DateTime())->format(\'e\'));"';
 var_dump($cmd);
 system($cmd);

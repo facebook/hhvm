09729fb5991dd3dcd02154f9d2a0efe49ddad7fa
Tim Siebels <tim_siebels_aurich@yahoo.de>
2015-10-05 22:59:55 +0200

    I can't copy & paste tests :(

--- hphp/test/slow/ext_filter/array.php
+++ hphp/test/slow/ext_filter/array.php
@@ -1,5 +1,7 @@
 <?php
 // based on zend/ext/filter/010.php
 
-var_dump(filter_var(array(array()), FILTER_VALIDATE_FLOAT, FILTER_REQUIRE_ARRAY));
+var_dump(
+  filter_var(array(array()), FILTER_VALIDATE_FLOAT, FILTER_REQUIRE_ARRAY)
+);
 

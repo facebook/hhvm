d590fe9470a9ade42d10da45e0da65fd79b81e6c
apaloma <drunkenf00l@hotmail.com>
2015-10-16 14:42:06 -0700

    Add tests for SSL/TLS servers

--- hphp/test/slow/ext_socket/persistent_socket.php
+++ hphp/test/slow/ext_socket/persistent_socket.php
@@ -1,23 +1,75 @@
 <?php
-$port = 0;
-$server = null;
-while (!$server) {
-  $port = rand(50000, 65535);
-  $server = @stream_socket_server(
-    "tcp://127.0.0.1:$port",
-    $errno,
-    $errstr,
-    STREAM_SERVER_BIND|STREAM_SERVER_LISTEN
-  );
+
+$pemfile = tempnam('/tmp', 'sslservertest');
+
+$certdata = array('countryName' => 'US',
+                  'stateOrProvinceName' => 'California',
+                  'localityName' => 'Menlo Park',
+                  'organizationName' => 'Test Corp',
+                  'organizationalUnitName' => 'Test Team',
+                  'commonName' => 'Foo Bar',
+                  'emailAddress' => 'foo@bar.com');
+
+// Generate the certificate
+$pkey = openssl_pkey_new();
+$cert = openssl_csr_new($certdata, $pkey);
+$cert = openssl_csr_sign($cert, null, $pkey, 1);
+
+// Generate and save the PEM file
+$pem_passphrase = 'testing';
+$pem = array();
+openssl_x509_export($cert, $pem[0]);
+openssl_pkey_export($pkey, $pem[1], $pem_passphrase);
+if (file_put_contents($pemfile, implode($pem)) === false) {
+  echo "Error writing PEM file.\n";
+  die;
 }
 
-$pid = pcntl_fork();
-if( $pid ) {
-  test_server();
-  pcntl_wait($status);
-} else {
-  test_client();
+$context = stream_context_create();
+
+stream_context_set_option($context, 'ssl', 'local_cert', $pemfile);
+stream_context_set_option($context, 'ssl', 'passphrase', $pem_passphrase);
+stream_context_set_option($context, 'ssl', 'allow_self_signed', true);
+stream_context_set_option($context, 'ssl', 'verify_peer', false);
+
+$schemes = array("tcp", "tls", "ssl");
+$contexts = array(null, $context, $context);
+$servers = array();
+$ports = array();
+foreach ($schemes as $i => $scheme) {
+  $server = null;
+  $port = 0;
+  while (!$server) {
+    $port = rand(50000, 65535);
+    $server = @stream_socket_server(
+      "$scheme://127.0.0.1:$port",
+      $errno,
+      $errstr,
+      STREAM_SERVER_BIND|STREAM_SERVER_LISTEN,
+      $contexts[$i]
+    );
+  }
+  $servers[] = $server;
+  $ports[] = $port;
 }
+
+foreach ($schemes as $i => $scheme) {
+  $server = $servers[$i];
+  $port = $ports[$i];
+
+  $pid = pcntl_fork();
+  if( $pid ) {
+    echo "Testing $scheme://\n";
+    test_server($server);
+    pcntl_wait($status);
+    exit;
+  } else {
+    test_client($scheme, $port);
+  }
+}
+
+unlink($pemfile);
+
 
 function read_all_data( $conn, $bytes ) {
   $all_data = '';
@@ -33,9 +85,7 @@ function read_all_data( $conn, $bytes ) {
   return $bytes == 0 ? $all_data : false;
 }
 
-function test_server() {
-  global $server;
-
+function test_server($server) {
   // The server only accepts once, but the client will call
   // stream_socket_client multiple times with the persistent flag.
   if( $conn = stream_socket_accept($server) ) {
@@ -57,21 +107,27 @@ function test_server() {
   fclose($server);
 }
 
-function test_client() {
-  do_request();
-  do_request();
-  do_request();
+function test_client($scheme, $port) {
+  $clientcontext = null;
+  if ($scheme != "tcp") {
+    $clientcontext = stream_context_create();
+    stream_context_set_option($clientcontext, 'ssl', 'allow_self_signed', true);
+    stream_context_set_option($clientcontext, 'ssl', 'verify_peer', false);
+  }
+
+  do_request($scheme, $port, $clientcontext);
+  do_request($scheme, $port, $clientcontext);
+  do_request($scheme, $port, $clientcontext);
 }
 
-function do_request() {
-  global $port;
-
+function do_request($scheme, $port, $context) {
   $client = stream_socket_client(
-    "tcp://127.0.0.1:$port",
+    "$scheme://127.0.0.1:$port",
     $errno,
     $errstr,
     1.0,
-    STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT
+    STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT,
+    $context
   );
 
   if( $client === FALSE ) {
@@ -89,5 +145,6 @@ function do_request() {
   }
 
   echo "Client received response: $data\n";
+
   return true;
 }

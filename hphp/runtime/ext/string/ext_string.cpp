7cc39aa57af8b8e20a71c0e0291369a8f805efcf
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-10-20 07:46:25 -0700

    fix segfault when input shorter than all patterns
    Summary: When the input string is shorter than the shortest input
    pattern the last position to visit is set to -1. Patch
    short circuits and returns input string when all patterns
    are longer than the input string.
    Closes https://github.com/facebook/hhvm/pull/6071
    
    Reviewed By: jwatzman
    
    Differential Revision: D2367627
    
    fb-gh-sync-id: 704ab038e68145916460c890bf6522dd62910af8

--- hphp/runtime/ext/string/ext_string.cpp
+++ hphp/runtime/ext/string/ext_string.cpp
@@ -1744,12 +1744,13 @@ String HHVM_FUNCTION(sha1,
 #define HASH_TAB_MASK ((uint16_t)(HASH_TAB_SIZE - 1))
 
 struct PatAndRepl {
-  const String  pat;
-  const String  repl;
+  const std::string pat;
+  const std::string repl;
 
   uint16_t hash(int start, int len) const;
 
-  PatAndRepl(const String& pat, const String& repl) : pat(pat), repl(repl) { }
+  PatAndRepl(const String& pat, const String& repl)
+  : pat(pat.data(), pat.size()), repl(repl.data(), repl.size()) { }
 };
 
 using ShiftTab   = std::array<size_t, SHIFT_TAB_SIZE>;
@@ -1775,7 +1776,7 @@ public:
   : m(minLen), B(MIN(m,2)), Bp(MIN(m,2)),
     valid(initPatterns(arr)) { }
 
-  Variant translate(String source);
+  Variant translate(String source) const;
 };
 
 static inline uint16_t strtr_hash(const char *str, int len) {
@@ -1837,6 +1838,8 @@ bool WuManberReplacement::initPatterns(const Array& arr) {
     patterns.emplace_back(pattern, iter.second().toString());
   }
 
+  initTables();
+
   return true;
 }
 
@@ -1893,7 +1896,7 @@ void WuManberReplacement::initTables() {
   }
 }
 
-Variant WuManberReplacement::translate(String source) {
+Variant WuManberReplacement::translate(String source) const {
   size_t  pos      = 0,
           nextwpos = 0,
           lastpos  = source.size() - m;
@@ -1902,8 +1905,9 @@ Variant WuManberReplacement::translate(String source) {
     return false;
   }
 
-  if (prefix.size() == 0) {
-    initTables();
+  // all patterns are longer than the source
+  if (m > source.size()) {
+    return source;
   }
 
   StringBuffer  result(source.size());
@@ -1925,7 +1929,7 @@ Variant WuManberReplacement::translate(String source) {
           continue;
         }
 
-        PatAndRepl *pnr = &patterns[i];
+        const PatAndRepl *pnr = &patterns[i];
         if (pnr->pat.size() > source.size() - pos ||
             memcmp(pnr->pat.data(), source.data() + pos,
                    pnr->pat.size()) != 0) {
@@ -2018,7 +2022,7 @@ Variant strtr_fast(const String& str, const Array& arr,
 
 static constexpr int kBitsPerQword = CHAR_BIT * sizeof(uint64_t);
 
-using WuManberPtr   = std::shared_ptr<WuManberReplacement>;
+using WuManberPtr   = std::shared_ptr<const WuManberReplacement>;
 using WuManberCache = ConcurrentLRUCache<int64_t, WuManberPtr>;
 static WuManberCache wuManberCache(10);
 
@@ -2061,7 +2065,7 @@ Variant HHVM_FUNCTION(strtr,
   }
 
   if (arr.size() < 1000) {
-    WuManberReplacement replacer(arr, minlen);
+    const WuManberReplacement replacer(arr, minlen);
     return replacer.translate(str);
   }
 

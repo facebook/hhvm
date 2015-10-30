0a446558fbc4d643a3b56b3715e80c5f789ffd7d
Kasper Kopec <kasper@fb.com>
2015-10-30 13:31:30 -0700

    Remove Typing_utils.grow_shape
    Summary: This function mostly duplicates the work of Typing_arrays.update_array_type. The main differences are:
    
    - grow_shape runs only on local variables on the left hand side of assignment expression, while update_array_type can be called on nested array types in lvalue expressions
    - grow_shape deleted the previous type (this is sound, because of the previous point), while update_array_type doesn't, allowing it to grow
    - grow_shape infinite loops on recursive variables, update_array_type doesn't ;)
    
    I replace one with the other by adding a boolean lvar_assignment parameter and doing the right thing based on this.
    
    Reviewed By: dlreeves
    
    Differential Revision: D2588218
    
    fb-gh-sync-id: d2352e1930010e10329f2197a5fe6ab9f3555ba3

--- hphp/hack/test/coverage/akshape.php
+++ hphp/hack/test/coverage/akshape.php
@@ -6,16 +6,15 @@ function string_key(): string {
 
 function test() {
   $a = array();
-  $a[string_key()] = 4; // $a is Tarraykind AKempty, which we consider
-  // partially checked
+  $a[string_key()] = 4; // $a is updated to AKmap, checked
   $a; // $a is a <string, int> array - checked
 
   $b = array();
-  $b[] = 4; // partially checked
+  $b[] = 4; // still AKempty, partially checked
   $b; // array<int> - checked
 
   $c = array();
-  $c['a'] = 4; // partially checked (AKempty)
+  $c['a'] = 4; // checked (AKmap)
   $c['b'] = no_type(); // checked
   $c; // partially checked
 }

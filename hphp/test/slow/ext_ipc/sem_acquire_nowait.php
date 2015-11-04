97ba67938f06db90e25b342e13e78dcda9d19d72
Tim Siebels <tim_siebels_aurich@yahoo.de>
2015-11-04 21:27:00 +0100

    Fix tab indent from zend test

--- hphp/test/slow/ext_ipc/sem_acquire_nowait.php
+++ hphp/test/slow/ext_ipc/sem_acquire_nowait.php
@@ -3,83 +3,83 @@
 // TODO: remove this test after the next import of zend test, containing 
 // this test.
 
-$SEMKEY	= ftok(__FILE__, 'P');  //  Semaphore key
+$SEMKEY  = ftok(__FILE__, 'P');  //  Semaphore key
 
 $pid = pcntl_fork();
 
 if ($pid) {
   echo "Parent.\n";
 
-	pcntl_signal(SIGCHLD, SIG_IGN);
+  pcntl_signal(SIGCHLD, SIG_IGN);
 
-	// Get semaphore
-	$sem_id = sem_get($SEMKEY, 1);
-	if ($sem_id === FALSE) {
-		echo "P: fail to get semaphore";
-		exit;
-	}
-	echo "P: got semaphore $sem_id.\n";
+  // Get semaphore
+  $sem_id = sem_get($SEMKEY, 1);
+  if ($sem_id === FALSE) {
+    echo "P: fail to get semaphore";
+    exit;
+  }
+  echo "P: got semaphore $sem_id.\n";
 
-	register_shutdown_function(function () use ($sem_id) {
-		echo "P: cleanup.\n";
-		sem_remove($sem_id);
-	});
+  register_shutdown_function(function () use ($sem_id) {
+    echo "P: cleanup.\n";
+    sem_remove($sem_id);
+  });
   
-	// Acquire semaphore
-	if (! sem_acquire($sem_id)) {
-		echo "P: fail to acquire semaphore $sem_id.\n";
-		sem_remove($sem_id);
-		exit;
-	}
-	echo "P: success acquire semaphore $sem_id.\n";
+  // Acquire semaphore
+  if (! sem_acquire($sem_id)) {
+    echo "P: fail to acquire semaphore $sem_id.\n";
+    sem_remove($sem_id);
+    exit;
+  }
+  echo "P: success acquire semaphore $sem_id.\n";
 
-	usleep(20000);
+  usleep(20000);
 
-	echo "P: releases.\n";
-	sem_release($sem_id);
+  echo "P: releases.\n";
+  sem_release($sem_id);
 
-	usleep(5000);
+  usleep(5000);
 
-	// Acquire semaphore
-	if (! sem_acquire($sem_id)) {
-		echo "P: fail to acquire semaphore $sem_id.\n";
-		sem_remove($sem_id);
-		exit;
-	}
-	echo "P: success acquire semaphore $sem_id.\n";
+  // Acquire semaphore
+  if (! sem_acquire($sem_id)) {
+    echo "P: fail to acquire semaphore $sem_id.\n";
+    sem_remove($sem_id);
+    exit;
+  }
+  echo "P: success acquire semaphore $sem_id.\n";
 
-	$status = null;
-	pcntl_waitpid($pid, $status);
+  $status = null;
+  pcntl_waitpid($pid, $status);
 
 } else {
-	usleep(10000);
-	echo "Child.\n";
+  usleep(10000);
+  echo "Child.\n";
 
-	// Get semaphore
-	$sem_id = sem_get($SEMKEY, 1);
-	if ($sem_id === FALSE) {
-		echo "C: fail to get semaphore";
-		exit;
-	}
-	echo "C: got semaphore $sem_id.\n";
+  // Get semaphore
+  $sem_id = sem_get($SEMKEY, 1);
+  if ($sem_id === FALSE) {
+    echo "C: fail to get semaphore";
+    exit;
+  }
+  echo "C: got semaphore $sem_id.\n";
   
-	// Acquire semaphore
-	if (! sem_acquire($sem_id)) {
-		echo "C: fail to acquire semaphore $sem_id.\n";
-		exit;
-	}
-	echo "C: success acquire semaphore $sem_id.\n";
+  // Acquire semaphore
+  if (! sem_acquire($sem_id)) {
+    echo "C: fail to acquire semaphore $sem_id.\n";
+    exit;
+  }
+  echo "C: success acquire semaphore $sem_id.\n";
 
-	echo "C: releases.\n";
-	sem_release($sem_id);
+  echo "C: releases.\n";
+  sem_release($sem_id);
 
-	usleep(10000);
+  usleep(10000);
 
-	// Acquire semaphore
+  // Acquire semaphore
   if (! sem_acquire($sem_id, true)) {
-		echo "C: fail to acquire semaphore $sem_id.\n";
-		exit;
-	}
-	echo "C: success acquire semaphore $sem_id.\n";
+    echo "C: fail to acquire semaphore $sem_id.\n";
+    exit;
+  }
+  echo "C: success acquire semaphore $sem_id.\n";
 }
 

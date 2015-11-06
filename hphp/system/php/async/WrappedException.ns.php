0f1fbe65f4cbe1ff192f6137cf3416d2c388154c
Fred Emmott <fredemmott@fb.com>
2015-11-06 13:06:38 -0800

    Re-import asio-utilities
    Summary: These were split out to https://github.com/hhvm/asio-utilities because
    the API was unstable. Given these have been stable since late February, bring
    them back into HHVM (they never made an HHVM release, this shouldn't be a problem).
    
    Unit tests are from the original implementation, though the matrix test has been
    substantially modified when re-writing.
    
    Tests resurrected from https://github.com/facebook/hhvm/commit/145512b64ccfdf4b850df562a79c6ca6ff2b64a5
    
    Reviewed By: sgolemon
    
    Differential Revision: D2623989
    
    fb-gh-sync-id: 67acadfd1c269ca6a98cc17408ac37b49a330fcb

---
+++ hphp/system/php/async/WrappedException.ns.php
@@ -0,0 +1,29 @@
+<?hh // strict
+
+namespace HH\Asio {
+
+/**
+ * Represents a result of failed operation.
+ */
+final class WrappedException<Te as \Exception,Tr>
+  implements ResultOrExceptionWrapper<Tr> {
+  public function __construct(private Te $exception) {}
+
+  public function isSucceeded(): bool {
+    return false;
+  }
+
+  public function isFailed(): bool {
+    return true;
+  }
+
+  public function getResult(): Tr {
+    throw $this->exception;
+  }
+
+  public function getException(): Te {
+    return $this->exception;
+  }
+}
+
+} // namespace HH\Asio

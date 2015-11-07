e6e5c0836d462c99ac9fc80a7a1f16fb36b7767f
Kasper Kopec <kasper@fb.com>
2015-11-06 16:21:09 -0800

    Allow Shapes::idx(...) default to be the subtype of field type
    Summary: Looks like currently it must be equal or supertype - changing it to mimic the behaviour of Indexish idx(...) - we allow any type, and reflect it in returning Tunresolved.
    
    Reviewed By: dlreeves
    
    Differential Revision: D2627515
    
    fb-gh-sync-id: 78ad60c67de794e55bfd9855fb9a7796ac2d0d87

--- hphp/hack/test/typecheck/shape_idx3.php
+++ hphp/hack/test/typecheck/shape_idx3.php
@@ -1,10 +1,22 @@
 <?hh // strict
 
-type s = shape('x' => int);
+type my_shapeA = shape('x' => ?arraykey);
+type my_shapeB = shape('x' => ?int);
 
-/**
- * Default has the wrong type
- */
-function test(s $s, string $default): void {
-  Shapes::idx($s, 'x', $default);
+function test(
+  my_shapeA $s1,
+  my_shapeB $s2,
+  array<string, arraykey> $a1,
+  array<string, int> $a2,
+  arraykey $a,
+  int $b,
+): int {
+  hh_show(idx($a1, 'x', $b));
+  hh_show(idx($a2, 'x', $a));
+  hh_show(Shapes::idx($s1, 'x', $b));
+  hh_show(Shapes::idx($s2, 'x', $a));
+
+  // Using "wrong" default type will be reported when trying to use the result
+  // in incompatible way
+  return Shapes::idx($s2, 'x', $a);
 }

d821c0e18472c179b8ffade15135688a89b1c3af
Josh Watzman <jwatzman@fb.com>
2015-11-11 09:50:11 -0800

    Support "use function" and "use const"
    Summary: Our namespace logic supported this improperly; `use X` is only
    for classes, and you have to specify for functions and constants.
    
    Closes https://github.com/facebook/hhvm/issues/6081
    
    Reviewed By: dabek
    
    Differential Revision: D2632961
    
    fb-gh-sync-id: a0db1f860e8d85e347cf7667fcae69fddbce838c

---
+++ hphp/hack/test/typecheck/namespace_use_function_const.php
@@ -0,0 +1,19 @@
+<?hh // strict
+
+namespace N {
+  function f(): void {
+    echo "hi\n";
+  }
+
+  const int C = 1;
+}
+
+namespace N2 {
+  use function N\f;
+  use const N\C as D;
+
+  function f2(): int {
+    f();
+    return D;
+  }
+}

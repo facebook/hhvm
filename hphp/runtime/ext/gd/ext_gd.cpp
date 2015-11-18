7c23c510f30e027f14594adcbfeacb69beb87bff
Thomas Colomb <thomas.colomb@dailymotion.com>
2015-11-18 10:33:50 +0000

    fix identation

--- hphp/runtime/ext/gd/ext_gd.cpp
+++ hphp/runtime/ext/gd/ext_gd.cpp
@@ -3981,8 +3981,7 @@ Variant HHVM_FUNCTION(imagecolorallocate,
 Variant HHVM_FUNCTION(imagepalettecopy,
     const Resource& dst,
     const Resource& src) {
-  gdImagePtr dstim = cast<Image>(dst)->get()
-  ;
+  gdImagePtr dstim = cast<Image>(dst)->get();
   gdImagePtr srcim = cast<Image>(src)->get();
   if (!dstim || !srcim)
     return false;

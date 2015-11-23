07d64c1a42431efd0029dd0307c6f55fba8af82e
Alan Frindell <afrind@fb.com>
2015-11-23 13:38:01 -0800

    Remove priority argument from HTTPUpstreamSession::newTransaction
    Summary: The priority gets set on the message, so create the transaction with default priority and update it when we send the request headers.
    
    Reviewed By: ranjeeth
    
    Differential Revision: D2413641
    
    fb-gh-sync-id: 6ea2d5f3dd7e29824fb57308ecfba739378ad096

--- hphp/runtime/server/proxygen/proxygen-transport.cpp
+++ hphp/runtime/server/proxygen/proxygen-transport.cpp
@@ -66,16 +66,14 @@ class PushTxnHandler : public proxygen::HTTPPushTransactionHandler {
 
  public:
   PushTxnHandler(uint64_t pushId,
-                 const std::shared_ptr<ProxygenTransport>& transport,
-                 uint8_t priority)
+                 const std::shared_ptr<ProxygenTransport>& transport)
       : m_pushId(pushId),
         m_transport(transport) {}
 
   proxygen::HTTPTransaction *getOrCreateTransaction(
     proxygen::HTTPTransaction *clientTxn, bool newPushOk) {
     if (!m_pushTxn && newPushOk) {
-      m_pushTxn = clientTxn->newPushedTransaction(
-        this, proxygen::http2::DefaultPriority);
+      m_pushTxn = clientTxn->newPushedTransaction(this);
     }
     return m_pushTxn;
   }
@@ -610,12 +608,12 @@ int64_t ProxygenTransport::pushResource(const char *host, const char *path,
   }
 
   int64_t pushId = m_nextPushId++;
-  PushTxnHandler *handler = new PushTxnHandler(pushId, shared_from_this(),
-                                               priority);
+  PushTxnHandler *handler = new PushTxnHandler(pushId, shared_from_this());
   HTTPMessage& pushMsg = handler->getPushMessage();
   pushMsg.setURL(path);
   pushMsg.setIsChunked(true); // implicitly chunked
   pushMsg.setSecure(true); // should we allow setting scheme?
+  pushMsg.setPriority(priority);
 
   for (ArrayIter iter(headers); iter; ++iter) {
     Variant key = iter.first();

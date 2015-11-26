999d941bc5275c3a6bacb8f6277d278e2acdfeec
Greg Bentley <gregory.d.bentley@intel.com>
2015-11-25 20:06:15 -0800

    Match PHP's behavior during the daylight saving switch-over hour.
    Summary: This resolve #3651
    Closes https://github.com/facebook/hhvm/pull/6521
    
    Reviewed By: sgolemon, JoelMarcey
    
    Differential Revision: D2637247
    
    Pulled By: JoelMarcey
    
    fb-gh-sync-id: 079407552d1aa2e5ac6744302799f4e9a506c5fa

---
+++ hphp/test/slow/ext_datetime/dst_change.php
@@ -0,0 +1,13 @@
+<?php
+
+$datetimes = [
+    '2014-10-05T01:59:59',
+    '2014-10-05T02:00:00',
+    '2014-10-05T02:59:59',
+    '2014-10-05T03:00:00'
+];
+
+foreach ($datetimes as $datetime) {
+    $dt = new DateTime($datetime, new DateTimeZone('Australia/Sydney'));
+    echo $dt->format(DateTime::ISO8601) . PHP_EOL;
+}

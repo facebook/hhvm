e61e070339c99633b72e3e0d39c294797e8f2515
Orvid King <blah38621@gmail.com>
2015-11-25 20:39:36 -0800

    Eliminate the cs_GMP_FUNC_NAME_* globals
    Summary: They are pointlessly long, and, although they may be there in an attempt to prevent typos, even include a typo, as `cs_GMP_FUNC_NAME_GMP_AND` is set to `"gmp_add"`.
    This just does a regex replace of `cs_GMP_FUNC_NAME_([A-Z_]+)` with `"\L\1"`. (For those who don't know, `\L` converts to lowercase in, at the very least, the regex engine used by Notepad++)
    
    This also inlines a couple of the errors that are used in only one place, and are shorter than the name of the global.
    This also removes `cs_GMP_FAILED_TO_ALTER_BIT`, which was never referenced anywhere.
    Closes https://github.com/facebook/hhvm/pull/6586
    
    Reviewed By: sgolemon, JoelMarcey
    
    Differential Revision: D2695888
    
    Pulled By: JoelMarcey
    
    fb-gh-sync-id: 27aef55bf51b8243635122c0a5ff6d47174da5ec

--- hphp/runtime/ext/gmp/ext_gmp.h
+++ hphp/runtime/ext/gmp/ext_gmp.h
@@ -82,60 +82,10 @@ const char* const cs_GMP_INVALID_ROOT_MUST_BE_POSITIVE =
   "%s(): The root must be positive";
 const char* const cs_GMP_INVALID_ROUNDING_MODE =
   "%s(): Invalid rounding mode";
-const char* const cs_GMP_FAILED_TO_ALTER_BIT =
-  "%s(): Failed to alter bit";
 const char* const cs_GMP_INVALID_STARTING_INDEX_IS_NEGATIVE =
   "%s(): Starting index must be greater than or equal to zero";
-const char* const cs_GMP_COULD_NOT_UNSERIALIZE_NUMBER =
-  "Could not unserialize number";
-const char* const cs_GMP_COULD_NOT_UNSERIALIZE_PROPERTIES =
-  "Could not unserialize properties";
 const char* const cs_GMP_ERROR_EVEN_ROOT_NEGATIVE_NUMBER =
   "%s(): Can't take even root of negative number";
-
-// Function name strings
-const char* const cs_GMP_FUNC_NAME_GMP_ABS            = "gmp_abs";
-const char* const cs_GMP_FUNC_NAME_GMP_ADD            = "gmp_add";
-const char* const cs_GMP_FUNC_NAME_GMP_AND            = "gmp_add";
-const char* const cs_GMP_FUNC_NAME_GMP_CLRBIT         = "gmp_clrbit";
-const char* const cs_GMP_FUNC_NAME_GMP_CMP            = "gmp_cmp";
-const char* const cs_GMP_FUNC_NAME_GMP_COM            = "gmp_com";
-const char* const cs_GMP_FUNC_NAME_GMP_DIV_Q          = "gmp_div_q";
-const char* const cs_GMP_FUNC_NAME_GMP_DIV_R          = "gmp_div_r";
-const char* const cs_GMP_FUNC_NAME_GMP_DIV_QR         = "gmp_div_qr";
-const char* const cs_GMP_FUNC_NAME_GMP_DIVEXACT       = "gmp_divexact";
-const char* const cs_GMP_FUNC_NAME_GMP_FACT           = "gmp_fact";
-const char* const cs_GMP_FUNC_NAME_GMP_GCD            = "gmp_gcd";
-const char* const cs_GMP_FUNC_NAME_GMP_GCDEXCT        = "gmp_gcdexct";
-const char* const cs_GMP_FUNC_NAME_GMP_HAMDIST        = "gmp_hamdist";
-const char* const cs_GMP_FUNC_NAME_GMP_INIT           = "gmp_init";
-const char* const cs_GMP_FUNC_NAME_GMP_INTVAL         = "gmp_intval";
-const char* const cs_GMP_FUNC_NAME_GMP_INVERT         = "gmp_invert";
-const char* const cs_GMP_FUNC_NAME_GMP_JACOBI         = "gmp_jacobi";
-const char* const cs_GMP_FUNC_NAME_GMP_LEGENDRE       = "gmp_legendre";
-const char* const cs_GMP_FUNC_NAME_GMP_MOD            = "gmp_mod";
-const char* const cs_GMP_FUNC_NAME_GMP_MUL            = "gmp_mul";
-const char* const cs_GMP_FUNC_NAME_GMP_NEG            = "gmp_neg";
-const char* const cs_GMP_FUNC_NAME_GMP_NEXTPRIME      = "gmp_nextprime";
-const char* const cs_GMP_FUNC_NAME_GMP_OR             = "gmp_or";
-const char* const cs_GMP_FUNC_NAME_GMP_PERFECT_SQUARE = "gmp_perfect_square";
-const char* const cs_GMP_FUNC_NAME_GMP_POPCOUNT       = "gmp_popcount";
-const char* const cs_GMP_FUNC_NAME_GMP_POW            = "gmp_pow";
-const char* const cs_GMP_FUNC_NAME_GMP_POWM           = "gmp_powm";
-const char* const cs_GMP_FUNC_NAME_GMP_PROB_PRIME     = "gmp_prob_prime";
-const char* const cs_GMP_FUNC_NAME_GMP_RANDOM         = "gmp_random";
-const char* const cs_GMP_FUNC_NAME_GMP_ROOT           = "gmp_root";
-const char* const cs_GMP_FUNC_NAME_GMP_ROOTREM        = "gmp_rootrem";
-const char* const cs_GMP_FUNC_NAME_GMP_SCAN0          = "gmp_scan0";
-const char* const cs_GMP_FUNC_NAME_GMP_SCAN1          = "gmp_scan1";
-const char* const cs_GMP_FUNC_NAME_GMP_SETBIT         = "gmp_setbit";
-const char* const cs_GMP_FUNC_NAME_GMP_SIGN           = "gmp_sign";
-const char* const cs_GMP_FUNC_NAME_GMP_SQRT           = "gmp_sqrt";
-const char* const cs_GMP_FUNC_NAME_GMP_SQRTREM        = "gmp_sqrtrem";
-const char* const cs_GMP_FUNC_NAME_GMP_STRVAL         = "gmp_strval";
-const char* const cs_GMP_FUNC_NAME_GMP_SUB            = "gmp_sub";
-const char* const cs_GMP_FUNC_NAME_GMP_TESTBIT        = "gmp_testbit";
-const char* const cs_GMP_FUNC_NAME_GMP_XOR            = "gmp_xor";
 
 ///////////////////////////////////////////////////////////////////////////////
 // classes

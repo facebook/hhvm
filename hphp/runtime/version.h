d5f9c7bfac4aac0b69c9f05b38150af23feea113
Paul Bissonnette <paulbiss@fb.com>
2016-02-11 14:11:31 -0800

    Targeting 3.13.0
    Summary: Branch is cut, packages for 3.12.0 will roll today
    
    Reviewed By: fredemmott
    
    Differential Revision: D2928762
    
    fb-gh-sync-id: 8ce52b520b5b1bee8f531466ed31dcf5645a0c25
    shipit-source-id: 8ce52b520b5b1bee8f531466ed31dcf5645a0c25

--- hphp/runtime/version.h
+++ hphp/runtime/version.h
@@ -11,7 +11,7 @@
  */
 #ifndef HHVM_VERSION_OVERRIDE
 # define HHVM_VERSION_MAJOR 3
-# define HHVM_VERSION_MINOR 12
+# define HHVM_VERSION_MINOR 13
 # define HHVM_VERSION_PATCH 0
 # define HHVM_VERSION_SUFFIX "-dev"
 #endif

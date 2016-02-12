5dd2528ba5662fe31d322266d72b57f051b0f090
Christopher Dykes <cdykes@fb.com>
2016-02-12 08:35:26 -0800

    Refactor out the spot that directly divides by zero
    Summary: MSVC produces a warning complaining about the "potential" divide by 0. This may also not be fully portable to other architectures, so only allow it under x86_64 for now.
    
    Reviewed By: markw65
    
    Differential Revision: D2901176
    
    fb-gh-sync-id: 4e4515aa0bf47341205cb5a189348fd1d07615f5
    shipit-source-id: 4e4515aa0bf47341205cb5a189348fd1d07615f5

--- hphp/runtime/base/tv-arith.cpp
+++ hphp/runtime/base/tv-arith.cpp
@@ -172,8 +172,25 @@ struct Div {
     if (UNLIKELY(u == 0)) {
       raise_warning(Strings::DIVISION_BY_ZERO);
       if (RuntimeOption::PHP7_IntSemantics) {
-        // PHP7 uses the IEEE definition (+/- INF and NAN).
-        return make_dbl(t / 0.0);
+        // PHP 7 requires IEEE compliance (+/- INF and NAN) with the result
+        // of dividing a value by zero. MSVC warns about the direct division
+        // by zero, and the literal division may not be portable to all
+        // platforms, so abstract the division out so that we can both keep
+        // MSVC quiet, and also handle platforms that don't have the same
+        // semantics as x86_64.
+        //
+        // This has to be factored out like this in order for MSVC to actually
+        // disable the warning, as MSVC only allows this warning to be disabled
+        // at function boundaries. Disabling it for a single line in a function
+        // is impossible; it must be disabled for the entire function.
+        FOLLY_PUSH_WARNING
+        FOLLY_MSVC_DISABLE_WARNING(4723)
+        return make_dbl([](int64_t tVal) {
+          auto v = tVal / 0.0;
+          assert(std::isnan(v) || std::isinf(v));
+          return v;
+        }(t));
+        FOLLY_POP_WARNING
       } else {
         return make_tv<KindOfBoolean>(false);
       }

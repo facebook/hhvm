ed708c365f04047d4d832a25c80594d1775c2ead
Kasper Kopec <kasper@fb.com>
2016-02-16 12:51:14 -0800

    Tests for HH_json string escaping and unescaping being reverse of each other
    Summary: Automatic version of test plan from {D2892906}
    
    Reviewed By: int3
    
    Differential Revision: D2931339
    
    fb-gh-sync-id: fa4a788fc367299ee5559edab24e7ace13e7d9ef
    shipit-source-id: fa4a788fc367299ee5559edab24e7ace13e7d9ef

---
+++ hphp/hack/test/utils/hh_json_test.ml
@@ -0,0 +1,36 @@
+(**
+ * Copyright (c) 2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the "hack" directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ *)
+
+open Core
+
+let test_escape_unescape_data = [
+  "newline\n";
+  "\"quoted string\"";
+  "tab\t";
+  "carriage return\r";
+  "backslash\\";
+  "magic char" ^ (String.make 1 (Char.chr 8));
+]
+
+let test_escape_unescape () =
+  List.for_all test_escape_unescape_data begin fun s ->
+    let json = Hh_json.JSON_String s in
+    let encoded = Hh_json.json_to_string json in
+    let decoded = Hh_json.json_of_string encoded in
+    let result = Hh_json.get_string_exn decoded in
+    (result = s)
+  end
+
+let tests = [
+  "test_escape_unescape", test_escape_unescape;
+]
+
+let () =
+  Unit_test.run_all tests

b0fb33239ac4c71c6abcf981ff25d78a434d9daa
Paul Bissonnette <paulbiss@fb.com>
2016-02-29 15:37:35 -0800

    Tests for DictArray
    Summary:Breaking this out into a separate diff as it makes the stack more
    reviewable, and because I'd like the hack team to have a look at the tests
    to make sure that the behavior is as expected for this array kind.
    
    See: http://hhvm.com/blog/10649/improving-arrays-in-hack
    Related to https://github.com/facebook/hhvm/issues/6452
    
    Reviewed By: swtaarrs
    
    Differential Revision: D2933196
    
    fb-gh-sync-id: d40f34439364a388a045708952f5e6d96c94d1d8
    shipit-source-id: d40f34439364a388a045708952f5e6d96c94d1d8

---
+++ hphp/test/slow/dict/dict-convert.php
@@ -0,0 +1,14 @@
+<?hh
+
+function main() {
+  $d = dict[1 => "apple", "1" => "orange", 2 => "tomato", "2" => "potato"];
+  $m = array(1 => "orange", "one" => "tomato", "red" => "potato");
+  $p = array('a', 'b', 'c');
+  $e = array();
+  var_dump(dict($d));
+  var_dump(dict($m));
+  var_dump(dict($p));
+  var_dump(dict($e));
+}
+
+main();

c1b414e85f9ab358845bdf26435a5800cf522ff4
Mark Williams <mwilliams@fb.com>
2016-03-04 08:41:28 -0800

    Fix edge case for max/min of int vs dbl
    Summary:min(x, y) should return x if x==y, but in the jit, if x and y were
    ints or doubles, it returned y (this is only visible if one is an int
    and the other is a double).
    
    Reviewed By: paulbiss
    
    Differential Revision: D3001634
    
    fb-gh-sync-id: ce37d6e58cc12929ce793d76d73e9186f1cd1fec
    shipit-source-id: ce37d6e58cc12929ce793d76d73e9186f1cd1fec

--- hphp/runtime/vm/jit/irgen-builtin.cpp
+++ hphp/runtime/vm/jit/irgen-builtin.cpp
@@ -404,11 +404,11 @@ SSATmp* minmax(IRGS& env, const bool is_max) {
     [&] (Block* taken) {
       SSATmp* cmp;
       if (ty1 <= TInt && ty2 <= TInt) {
-        cmp = gen(env, is_max ? GteInt : LteInt, val1, val2);
+        cmp = gen(env, is_max ? GtInt : LtInt, val1, val2);
       } else {
         auto conv1 = (ty1 <= TDbl) ? val1 : gen(env, ConvIntToDbl, val1);
         auto conv2 = (ty2 <= TDbl) ? val2 : gen(env, ConvIntToDbl, val2);
-        cmp = gen(env, is_max ? GteDbl : LteDbl, conv1, conv2);
+        cmp = gen(env, is_max ? GtDbl : LtDbl, conv1, conv2);
       }
       gen(env, JmpZero, taken, cmp);
     },
---
+++ hphp/test/slow/arithmetic/maxmin.php
@@ -0,0 +1,8 @@
+<?php
+
+function main($a, $b) {
+  var_dump(min($a, $b), max($a, $b));
+  }
+
+main(1.0, 1);
+main(1, 1.0);

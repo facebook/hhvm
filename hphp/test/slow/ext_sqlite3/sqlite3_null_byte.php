e40139d5de95f2b4c6362d1ae956fc1f3b75600c
Alex Malyshev <alexanderm@fb.com>
2016-03-07 16:49:26 -0800

    Sqlite3 test wasn't deleting a temp file
    Summary: Got tired of seeing hphp/example.db
    
    Reviewed By: paulbiss
    
    Differential Revision: D3020286
    
    fb-gh-sync-id: 95323492e442208f0c3015844dfc14af60dbf243
    shipit-source-id: 95323492e442208f0c3015844dfc14af60dbf243

--- hphp/test/slow/ext_sqlite3/sqlite3_null_byte.php
+++ hphp/test/slow/ext_sqlite3/sqlite3_null_byte.php
@@ -2,7 +2,11 @@
 
 $file = '/etc/passwd'.chr(0).'asdf';
 
-$sql3 = new SQLite3('example.db');
+$db = __DIR__ . DIRECTORY_SEPARATOR . 'example.db';
+
+set_error_handler(function() use ($db) { unlink($db); return false; });
+
+$sql3 = new SQLite3($db);
 var_dump($sql3->loadExtension($file));
 
 $sql3 = new SQlite3($file);

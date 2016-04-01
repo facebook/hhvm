79e548a0a401a92fcaa192e814ef5e4770f969dc
Jez Ng <jezng@fb.com>
2016-04-01 11:58:57 -0700

    Add test for incremental updates on method signature change
    Summary: This demonstrates that we are redeclaring files unnecessarily
    
    Reviewed By: dlreeves
    
    Differential Revision: D3120306
    
    fb-gh-sync-id: 0bf5480be5447c7e095a712c54ef6de7c7a7ebe2
    fbshipit-source-id: 0bf5480be5447c7e095a712c54ef6de7c7a7ebe2

--- hphp/hack/test/integration/hierarchy_tests.py
+++ hphp/hack/test/integration/hierarchy_tests.py
@@ -30,3 +30,32 @@ class HierarchyTests(object):
             '    inherited from File "{root}bar.php", line 3, characters 7-9: Bar',
             ],
             options=['--inheritance-ancestors', 'Baz'])
+
+    def test_method_signature_change(self):
+        with open(os.path.join(self.repo_dir, 'qux.php'), 'w') as f:
+            f.write('''<?hh
+class Qux {
+  public function f() {
+    $x = new Foo();
+    $x->f();
+  }
+}
+''')
+        self.write_load_config('qux.php')
+        self.check_cmd(['No errors!'])
+        debug_sub = self.subscribe_debug()
+        with open(os.path.join(self.repo_dir, 'foo.php'), 'w') as f:
+            f.write('''<?hh
+class Foo {
+  public function f(): void {}
+  public final function g() {}
+}
+''')
+        msgs = debug_sub.get_incremental_logs()
+        self.assertEqual(set(msgs['to_redecl_phase1']['files']),
+                set(['foo.php']))
+        # FIXME: redeclaring qux.php is unnecessary
+        self.assertEqual(set(msgs['to_redecl_phase2']['files']),
+                set(['foo.php', 'bar.php', 'baz.php', 'qux.php']))
+        self.assertEqual(set(msgs['to_recheck']['files']),
+                set(['foo.php', 'bar.php', 'baz.php', 'qux.php']))

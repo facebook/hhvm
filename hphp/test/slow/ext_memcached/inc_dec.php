a71d7405551bd3d6ee0face46a4accce4c5e8e16
Adrián del Campo <adelcampo@tuenti.com>
2016-04-19 17:00:00 +0200

    Added new test cases for increment / decrement

--- hphp/test/slow/ext_memcached/inc_dec.php
+++ hphp/test/slow/ext_memcached/inc_dec.php
@@ -8,3 +8,19 @@ $mc->increment(key, 3);
 var_dump($mc->get( key));
 $mc->decrement(key, 1);
 var_dump($mc->get(key));
+
+//increment with initial value only works with binary protocol
+const non_existant_key = 'incr_decr_test_fail';
+
+$mc2 = new Memcached;
+$mc2->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
+$mc2->addServer('127.0.0.1', 11211);
+
+var_dump($mc2->increment(non_existant_key, 3));
+var_dump($mc2->get_multi(array(non_existant_key)));
+var_dump($mc2->decrement(non_existant_key, 1));
+var_dump($mc2->get_multi(array(non_existant_key)));
+
+$mc2->increment(non_existant_key, 3, 1);
+$result = $mc2->get_multi(array(non_existant_key));
+var_dump($result[non_existant_key]);

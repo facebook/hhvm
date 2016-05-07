47829bec2e8ccbbac1694d346fdb63009be916aa
Jez Ng <jezng@fb.com>
2016-05-07 12:49:34 -0700

    Remove dependency of ai/ on Nast heap
    Reviewed By: hermanventer
    
    Differential Revision: D3051880
    
    fb-gh-sync-id: 699c409c7484db2b2efe776362fec9a0fde0ae30
    fbshipit-source-id: 699c409c7484db2b2efe776362fec9a0fde0ae30

---
+++ hphp/hack/src/naming/naming_heap_api.ml
@@ -0,0 +1,30 @@
+(**
+ * Copyright (c) 2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the "hack" directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ *)
+
+let get_class id =
+  match Naming_heap.TypeIdHeap.get id with
+  | None
+  | Some (_, `Typedef) -> None
+  | Some (p, `Class) ->
+    let fn = Pos.filename p in
+    match Parser_heap.find_class_in_file fn id with
+    | None -> None
+    | Some class_ ->
+      Some (Naming.class_ TypecheckerOptions.permissive class_)
+
+let get_fun id =
+  match Naming_heap.FunPosHeap.get id with
+  | None -> None
+  | Some p ->
+    let fn = Pos.filename p in
+    match Parser_heap.find_fun_in_file fn id with
+    | None -> None
+    | Some fun_ ->
+      Some (Naming.fun_ TypecheckerOptions.permissive fun_)

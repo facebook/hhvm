d55deda9dd290afcc11c48335802d7a3cc4fb9ca
lpathy <lpathy@cavium.com>
2016-05-11 14:11:11 -0700

    Added support for ld1, st1 and index fmov instructions
    Summary: Closes https://github.com/facebook/hhvm/pull/7016
    
    Reviewed By: Orvid
    
    Differential Revision: D3221842
    
    fbshipit-source-id: 8b6b7a6bd183112e9a5a3fbb95e4b2c401bdc282

--- hphp/vixl/a64/assembler-a64.h
+++ hphp/vixl/a64/assembler-a64.h
@@ -103,6 +103,11 @@ class CPURegister {
     return size_ == 64;
   }
 
+  bool Is128Bits() const {
+    assert(IsValid());
+    return size_ == 128;
+  }
+
   bool IsValid() const {
     return IsValidRegister() || IsValidFPRegister();
   }
@@ -212,10 +217,16 @@ class FPRegister : public CPURegister {
   static const FPRegister dregisters[];
 };
 
+
+// For SIMD instructions
+typedef FPRegister VRegister;
+
+
 // No*Reg is used to indicate an unused argument, or an error case. Note that
 // these all compare equal (using the Is() method). The Register and FPRegister
 // variants are provided for convenience.
 const Register NoReg;
+const VRegister NoVReg;
 const FPRegister NoFPReg;
 const CPURegister NoCPUReg;
 
@@ -619,6 +630,10 @@ class Assembler {
     assert(Label::kEndOfChain == 0);
     return UpdateAndGetByteOffsetTo(label) >> kInstructionSizeLog2;
   }
+
+  HPHP::CodeBlock& code() const { return cb_; }
+
+  HPHP::jit::TCA base() const { return cb_.base(); }
 
   HPHP::jit::TCA frontier() const {
     return cb_.frontier();
@@ -1107,6 +1122,12 @@ class Assembler {
   // Store exclusive register.
   void stxr(const Register& rs, const Register& rt, const MemOperand& dst);
 
+  // One-element structure load to one register.
+  void ld1(const VRegister& vt, const MemOperand& src);
+
+  // One-element structure store from one register.
+  void st1(const VRegister& vt, const MemOperand& src);
+
   // Move instructions. The default shift of -1 indicates that the move
   // instruction will calculate an appropriate 16-bit immediate and left shift
   // that is equal to the 64-bit immediate argument. If an explicit left shift
@@ -1173,6 +1194,12 @@ class Assembler {
 
   // Move FP register to FP register.
   void fmov(FPRegister fd, FPRegister fn);
+
+  // Move register to FP register 1
+  void fmov(const FPRegister& fd, int index, const Register& rn);
+
+  // Move FP register 1 to register
+  void fmov(const Register& rd, const FPRegister& fn, int index);
 
   // FP add.
   void fadd(const FPRegister& fd, const FPRegister& fn, const FPRegister& fm);
@@ -1314,6 +1341,12 @@ class Assembler {
   static Instr Rm(CPURegister rm) {
     assert(rm.code() != kSPRegInternalCode);
     return rm.code() << Rm_offset;
+  }
+
+  static Instr RmNot31(CPURegister rm) {
+    assert(rm.code() != kSPRegInternalCode);
+    assert(!rm.IsZero());
+    return Rm(rm);
   }
 
   static Instr Ra(CPURegister ra) {
@@ -1569,6 +1602,12 @@ class Assembler {
     return scale << FPScale_offset;
   }
 
+  // Instruction bits for vector format in load and store operations.
+  static Instr LSVFormat(VRegister vd) {
+    // Note: vasm opcodes need only 2 lanes (64b each)
+    return LS_NEON_2D;
+  }
+
   // Size of the code generated in bytes
   uint64_t SizeOfCodeGenerated() const {
     assert(cb_.available() > 0);
@@ -1626,6 +1665,11 @@ class Assembler {
   void LoadStore(const CPURegister& rt,
                  const MemOperand& addr,
                  LoadStoreOp op);
+  void LoadStoreStruct(const VRegister& vt,
+                       const MemOperand& addr,
+                       NEONLoadStoreMultiStructOp op);
+  Instr LoadStoreStructAddrModeField(const MemOperand& addr);
+
   static bool IsImmLSUnscaled(ptrdiff_t offset);
   static bool IsImmLSScaled(ptrdiff_t offset, LSDataSize size);
 

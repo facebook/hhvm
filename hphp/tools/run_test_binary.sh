fc0942f0b8782a1fe1b838ecbd7bea649f2f7d9f
Alex Malyshev <alexanderm@fb.com>
2016-05-17 11:01:53 -0700

    Reverted commit D3299771
    Summary:
    It uses hphp/tools/run_test_binary.sh, which wasn't finding //hphp/test/ext:test
    correctly.
    
    Reviewed By: markw65
    
    Differential Revision: D3299771
    
    fbshipit-source-id: 0b1b616a275bd8cb72a1d22b4f26f98f78389541

--- hphp/tools/run_test_binary.sh
+++ hphp/tools/run_test_binary.sh
@@ -5,15 +5,5 @@
 # ./run_test_ext.sh
 #
 
-unset CDPATH
-
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-
-# Tools don't agree on whether to pass relative or absolute paths, so accept
-# both.
-TEST_BIN=$1
-if [ ! -x "${TEST_BIN}" ]; then
-  TEST_BIN="${DIR}/../../$1"
-fi
-
-cd $DIR/../ && "${TEST_BIN}" "$2" "$3" "$4"
+cd $DIR/../ && $DIR/../../$1 "$2" "$3" "$4"

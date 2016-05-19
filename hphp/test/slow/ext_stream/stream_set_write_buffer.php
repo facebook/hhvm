93e17cd015f47e43e3b4d115aa67a9dbf58883ad
Andrew Peabody <apeabody@gmail.com>
2016-05-19 10:57:19 -0700

    Re-implement stream_set_write_buffer()
    Summary:
    stream_set_write_buffer() re-implemented in-line with the PHP spec (https://secure.php.net/manual/en/function.stream-set-read-buffer.php) for file streams. If a buffer size of 0 is provided then file operations will be unbuffered. Returns 0 on success, or another number on failure.  See related #7063
    
    Adds additional cases to the slow stream_set_write_buffer test.
    Closes https://github.com/facebook/hhvm/pull/7073
    
    Reviewed By: Orvid
    
    Differential Revision: D3317301
    
    fbshipit-source-id: 1fe7561e5689ddb2db34a589e19bccb1f6654586

--- hphp/test/slow/ext_stream/stream_set_write_buffer.php
+++ hphp/test/slow/ext_stream/stream_set_write_buffer.php
@@ -8,3 +8,10 @@ $descriptorspec = array(
 
 $process = proc_open('echo', $descriptorspec, $io);
 var_dump(stream_set_write_buffer($io[0], 0));
+
+
+$fd = fopen(__DIR__.'/stream_set_write_buffer.php.sample', 'rb');
+var_dump(stream_set_write_buffer($fd, 0));
+var_dump(stream_set_write_buffer($fd, 4096));
+var_dump(trim(fgets($fd)));
+fclose($fd);

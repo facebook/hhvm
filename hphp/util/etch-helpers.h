85effca5f75d98f91766b8effa0974602f57ba47
Leonardo Bianconi <leonardo.bianconi@eldorado.org.br>
2016-05-20 17:12:49 +0000

    Removing some useless code
    Removing changes of etch-helpers.h.

--- hphp/util/etch-helpers.h
+++ hphp/util/etch-helpers.h
@@ -49,41 +49,6 @@
 #define ETCH_GET_ARG6     /* not used */
 #define ETCH_RET1         %rax
 
-#elif defined(__powerpc64__)
-#define CFI(x)            .cfi_##x
-#define CFI2(x, y)        .cfi_##x y
-#define CFI3C(x, y, z)    .cfi_##x y##, z
-#define ETCH_ALIGN16      .align 16
-#define ETCH_ALIGN8       .align 8
-#define ETCH_ALIGN4       .align 4
-#define ETCH_SECTION(x)   .section .text.x,"ax"
-#define ETCH_SIZE(x)      .size x, .-x
-#define ETCH_NAME(x)      x
-#define ETCH_LABEL(x)     .L##x
-#define ETCH_TYPE(x, y)   .type x, y
-#define ETCH_NAME_REL(x)  $ x
-#define ETCH_ARG1         %r3
-#define ETCH_ARG2         %r4
-#define ETCH_ARG3         %r5
-#define ETCH_ARG4         %r6
-#define ETCH_ARG5         %r7
-#define ETCH_ARG6         %r8
-#define ETCH_RET1         %r3
-/* VM Registers must match definition on abi-ppc64.h */
-#define ETCH_V_ONE        1
-#define ETCH_R_ONE        %r28
-#define ETCH_VMFP         %r31
-#define ETCH_VMSP         %r29
-#define ETCH_VMTL         %r30
-#define ETCH_SP           %r27
-/* Service Request Registers must match svcreq_args list on abi-ppc64.cpp */
-#define ETCH_SVCREQ_REQ   %r3
-#define ETCH_SVCREQ_STUB  %r8
-#define ETCH_SVCREQ_ARG1  %r4
-#define ETCH_SVCREQ_ARG2  %r5
-#define ETCH_SVCREQ_ARG3  %r6
-#define ETCH_SVCREQ_ARG4  %r7
-
 #else /* Other x86 (e.g. linux) */
 #define CFI(x)            .cfi_##x
 #define CFI2(x, y)        .cfi_##x y

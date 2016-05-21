79b7c35e28e6e4524c1aca7e47428e0e14d51f52
Andrew Peabody <apeabody@gmail.com>
2016-05-20 23:07:37 -0400

    Adds ZipArchive::setCompressionName() & setCompressionIndex() & tests

---
+++ hphp/test/slow/ext_zlib/ziparchive_setcompression.php
@@ -0,0 +1,14 @@
+<?php
+$str = 'temp';
+$dir = tempnam(sys_get_temp_dir(), __FILE__);
+unlink($dir);
+mkdir($dir);
+$archive = new ZipArchive();
+$archive->open("$dir/comptest.zip", ZipArchive::CREATE);
+$archive->addFromString("A.txt", $str);
+$archive->addFromString("B.txt", $str);
+var_dump($archive->setCompressionIndex(0, ZIPArchive::CM_STORE));
+var_dump($archive->setCompressionName("B.txt", ZIPArchive::CM_STORE));
+$archive->close();
+unlink("$dir/comptest.zip");
+rmdir($dir);

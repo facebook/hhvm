f67f1de63d159872f59bd919fccb75ab015a9aa5
Igor Nunes <igor.nunes@eldorado.org.br>
2016-05-25 16:44:04 -0300

    Use specific emitTLSAddr for powerpc64
    For ppc64, there is no fs register. To refer to a thread content,
    the ppc64-abi uses a displacement relative to r13 (thread pointer).

--- hphp/runtime/vm/jit/code-gen-tls-inl.h
+++ hphp/runtime/vm/jit/code-gen-tls-inl.h
@@ -33,8 +33,10 @@ namespace HPHP { namespace jit {
 
 template<typename T>
 inline Vptr emitTLSAddr(Vout& v, TLSDatum<T> datum) {
-  if (arch() != Arch::X64 && arch() != Arch::ARM) not_implemented();
-  return x64::detail::emitTLSAddr(v, datum);
+  if (arch() == Arch::X64 || arch() == Arch::ARM)
+    return x64::detail::emitTLSAddr(v, datum);
+  else if (arch() == Arch::PPC64) return ppc64::detail::emitTLSAddr(v, datum);
+  else not_implemented();
 }
 
 ///////////////////////////////////////////////////////////////////////////////

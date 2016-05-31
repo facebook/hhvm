acf2bfe7b7d03d85affdaa4b91c8ee96a1d265e0
Leonardo Bianconi <leonardo.bianconi@eldorado.org.br>
2016-05-31 11:25:15 -0700

    Port bitops.h to PPC64
    Summary:
    Bit search function implemented in asm for PPC64. Added a variable initialization as well, to avoid warning.
    Closes https://github.com/facebook/hhvm/pull/7095
    
    Reviewed By: Orvid
    
    Differential Revision: D3341239
    
    fbshipit-source-id: c82563f1e3cd864cb26dfb0b5b713c911c1d4bde

--- hphp/util/bitops.h
+++ hphp/util/bitops.h
@@ -29,7 +29,7 @@ namespace HPHP {
 // unoffset bit position in their reference param.
 template<typename I64>
 inline bool ffs64(I64 input, I64 &out) {
-  bool retval;
+  bool retval = false;
 #if defined(__x86_64__)
   asm volatile (
     "bsfq  %2, %1\n\t"   // bit scan forward
@@ -47,6 +47,21 @@ inline bool ffs64(I64 input, I64 &out) {
     "=r"(retval), "=r"(out), "+r"(input):
     :
     "cc"
+  );
+#elif defined(__powerpc64__)
+  // In PowerPC 64, bit 0 is the most significant
+  asm volatile (
+    "neg    %0, %2\n\t"     // 2-complement of input, using retval as temp
+    "and    %0, %2, %0\n\t"
+    "cntlzd %1, %0\n\t"     // count leading zeros (starting from index 0)
+    "cmpdi  %1, 64\n\t"
+    "li     %0, 1\n\t"      // using retval as temp
+    "iseleq %0, 0, %0\n\t"  // (input == 0) ? 0 : 1
+    "neg    %1, %1\n\t"
+    "addi   %1, %1, 63\n\t":// 63 - amount of leading zeros -> position in LSB
+    "+r"(retval), "=r"(out):// +r else %0 and %2 will be the same register
+    "r"(input):
+    "cr0"
   );
 #else
   out = folly::findFirstSet(input);
@@ -77,6 +92,19 @@ inline bool fls64(I64 input, I64 &out) {
     "=r"(retval), "=r"(out):
     "r"(input):
     "cc"
+  );
+#elif defined(__powerpc64__)
+  // In PowerPC 64, bit 0 is the most significant
+  asm volatile (
+    "cntlzd %1, %2\n\t"     // count leading zeros (starting from index 0)
+    "cmpdi  %1, 64\n\t"
+    "li     %0, 1\n\t"      // using retval as temp
+    "iseleq %0, 0, %0\n\t"  // (input == 0) ? 0 : 1
+    "neg    %1, %1\n\t"
+    "addi   %1, %1, 63\n\t":// 63 - amount of leading zeros -> position in LSB
+    "=r"(retval), "=r"(out):
+    "r"(input):
+    "cr0"
   );
 #else
   out = folly::findLastSet(input) - 1;

e217770b457adb252fd90a0c1cb7c1a629c85a00
Keith Adams <kma@slack-corp.com>
2016-06-14 12:43:13 -0700

    Fix redis scan
    Summary:
    Sorry for the churn, Orvid. This is the descendant of https://github.com/facebook/hhvm/pull/6889.
    Closes https://github.com/facebook/hhvm/pull/7154
    
    Differential Revision: D3425764
    
    fbshipit-source-id: 0d83f905161d20075c18df948119e52e7a84b3d8

--- hphp/test/slow/ext_redis/scan.php
+++ hphp/test/slow/ext_redis/scan.php
@@ -3,17 +3,58 @@
 include (__DIR__ . '/redis.inc');
 
 $r = NewRedisTestInstance();
-$r->setOption(Redis::OPT_PREFIX, GetTestKeyName(__FILE__) . ':');
-$r->delete('scan');
-
-$r->mset(array('key:one' => 'one', 'key:two' => 'two',
+global $prefix;
+$prefix = GetTestKeyName(__FILE__) . ':';
+$r->setOption(Redis::OPT_PREFIX, $prefix);
+$r->setOption(Redis::OPT_SCAN, Redis::SCAN_RETRY);
+$ret = $r->delete('scan');
+$ret = $r->mset(array('key:one' => 'one', 'key:two' => 'two',
                'key:three' => 'three','key:four' => 'four'));
 
-var_dump($r->scan(0));
-var_dump($r->scan(0, 'key:t*'));
-var_dump($r->scan(0, 'nokey:t*'));
+/*
+ * The PHP5 extension we're patterning after doesn't try to turn
+ * OPT_PREFIX into a pattern when doing scan(), so scan returns
+ * global state. Filter it ourselves.
+ */
+function performScan($fn){
+	$cursor = null;
+	$returns = array();
+	while (($retval = $fn($cursor))){
+		foreach ($retval as $key){
+			global $prefix;
+			if (strstr($key, $prefix) !== false){
+				$returns []= substr($key, strlen($prefix));
+			}
+		}
+	}
+	// Normalize return order
+	sort($returns);
+	return $returns;
+}
 
-$r->delete('key:one');
-$r->delete('key:two');
-$r->delete('key:three');
-$r->delete('key:four');
+try {
+	$ret = performScan(function(&$cursor) use($r){
+		// Scan without patterns.
+		return $r->scan($cursor);
+	});
+	var_dump($ret);
+
+	$ret = performScan(function(&$cursor) use($r){
+		// catch key:two and key:three
+		global $prefix;
+		return $r->scan($cursor, $prefix . 'key:t*');
+	});
+	var_dump($ret);
+
+	$ret = performScan(function(&$cursor) use($r){
+		// nothing.
+		global $prefix;
+		return $r->scan($cursor, $prefix . 'nokey:t*');
+	});
+	var_dump($ret);
+} finally {
+	$r->delete('key:one');
+	$r->delete('key:two');
+	$r->delete('key:three');
+	$r->delete('key:four');
+}

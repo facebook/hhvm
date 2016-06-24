706841c7d266f5bd35aa2245020a022d268672ca
Christopher Dykes <cdykes@fb.com>
2016-06-24 10:42:03 -0700

    Allow 86ctors to be hot
    Summary: This assertion had a flaw where-by if running in JIT mode and a base class has been defined and its constructor has been determined to be hot, and then another (new) class was loaded that extends the base class, and neither define a constructor, this assertion would fail because the 86ctor had been marked as Hot.
    
    Reviewed By: paulbiss
    
    Differential Revision: D3477612
    
    fbshipit-source-id: 3f9cd0f8110d52bb351fcdd850caf2441e1d8ec6

--- hphp/runtime/vm/class.cpp
+++ hphp/runtime/vm/class.cpp
@@ -1268,7 +1268,7 @@ void Class::setSpecial() {
   // Use 86ctor(), since no program-supplied constructor exists
   m_ctor = findSpecialMethod(this, s_86ctor.get());
   assert(m_ctor && "class had no user-defined constructor or 86ctor");
-  assert((m_ctor->attrs() & ~(AttrBuiltin|AttrAbstract|
+  assert((m_ctor->attrs() & ~(AttrBuiltin|AttrAbstract|AttrHot|
                               AttrInterceptable|AttrMayUseVV)) ==
          (AttrPublic|AttrNoInjection|AttrPhpLeafFn));
 }

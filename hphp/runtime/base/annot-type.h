ebeb832b708bc1efa1ea01602c624203e2bce60c
Rick Lavoie <rlavoie@fb.com>
2016-07-06 10:44:20 -0700

    Fix typo bug in annotCompat()
    Summary:
    When KindOfPersistentArray was added, a change to add the usage of isArrayType()
    in annotCompat() apparently went wrong. Instead of isArrayType(dt),
    isArrayType(KindOfArray) was added instead, which of course is always true. As a
    result, the compiler optimizes away the expression entirely. Fix it to be
    isArrayType(dt).
    
    Fover ok:
    
    I noticed this as part of the Hack array changes and fixed it in that
    stack. However, fixing this might be part of the perf regression, so I decided
    to split out on its own. This bug has been present since November. I'm very
    surprised it doesn't seem to actually break anything.
    
    Reviewed By: markw65
    
    Differential Revision: D3521845
    
    fbshipit-source-id: 8126c2687f4614d05c9ae38c146b886f39de9e21

--- hphp/runtime/base/annot-type.h
+++ hphp/runtime/base/annot-type.h
@@ -168,7 +168,7 @@ annotCompat(DataType dt, AnnotType at, const StringData* annotClsName) {
     case AnnotMetaType::Callable:
       // For "callable", if `dt' is not string/array/object we know
       // it's not compatible, otherwise more checks are required
-      return (isStringType(dt) || isArrayType(KindOfArray) ||
+      return (isStringType(dt) || isArrayType(dt) ||
               dt == KindOfObject)
         ? AnnotAction::CallableCheck : AnnotAction::Fail;
     case AnnotMetaType::Dict:

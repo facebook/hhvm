d239f8660e7c3bff35d619a500c63f4a1f10eae5
Christopher Dykes <cdykes@fb.com>
2016-07-12 10:13:50 -0700

    Re-enable the rvalue form of throw_object
    Summary: But expand the scope of when it's disabled to be all of 4.8, rather than just 4.8.1.
    
    Reviewed By: paulbiss
    
    Differential Revision: D3545160
    
    fbshipit-source-id: bdb2934ce1525eac51d11f22166c26b0e4f682bf

--- hphp/runtime/base/builtin-functions.cpp
+++ hphp/runtime/base/builtin-functions.cpp
@@ -624,6 +624,12 @@ void throw_object(const Object& e) {
   throw req::root<Object>(e);
 }
 
+#if ((__GNUC__ != 4) || (__GNUC_MINOR__ != 8))
+void throw_object(Object&& e) {
+  throw req::root<Object>(std::move(e));
+}
+#endif
+
 /*
  * This function is used when another thread is segfaulting---we just
  * want to wait forever to give it a chance to write a stacktrace file

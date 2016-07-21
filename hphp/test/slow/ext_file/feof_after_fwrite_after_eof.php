f2f147e5a36339475a845ea9dde1e9b64edf8b5c
Andrew Peabody <apeabody@gmail.com>
2016-07-21 10:34:43 -0700

    FIX: behavior of TempFile's (php://{memory,temp}) fseek(), ftruncate(), & ftell()
    Summary:
    RE-DO for #7139
    
    NOTE: I'm uncertain why StatCache::lstat doesn't return during ~3 of the ext_oauth tests resulting in an empty string being passed back from the http_server function (perhaps HHVM is somehow prematurely cleaning up the disk based temp file but the file handler still works?), however providing a fall back to fstat() appears to address the issue.  This hybrid model is still probably preferable to a pure fstat() implementation given how infrequently it appears to need to fall back (none of the ext_file tests).  If someone has more insight into StatCache I'd appreciate it - I'm open to all ideas as normal.
    
    NOTE: HHVM implements php://memory and php://temp as the TempFile class
    
    - correct return of fseek() past eof for TempFile to int(-1) Closes #6916
    - now adjusts position for ftruncate() for TempFile Closes #6830
    - correct return of ftell() past eof for TempFile streams to bool(false) https://3v4l.org/MTMtH
    - enable correct behavior of SEEK_END for fseek() for TempFile
    Closes https://github.com/facebook/hhvm/pull/7186
    
    Reviewed By: Orvid
    
    Differential Revision: D3492057
    
    Pulled By: Orvid
    
    fbshipit-source-id: 82745c56e694b254271a18d12c682e190b0c8323

--- hphp/test/slow/ext_file/feof_after_fwrite_after_eof.php
+++ hphp/test/slow/ext_file/feof_after_fwrite_after_eof.php
@@ -9,12 +9,19 @@ function main() {
   foreach ($inputs as $input) {
     printf("---%s---\n", $input);
     $f = fopen($input, 'r+');
+    var_dump(fseek($f, 5, SEEK_SET));
+    var_dump(ftell($f));
     fread($f, 1);
     var_dump(feof($f));
     fwrite($f, 'foo');
     var_dump(feof($f));
-    fseek($f, 0, SEEK_CUR);
+    fseek($f, 0, SEEK_SET);
+    fwrite($f, "1234567890");
     var_dump(feof($f));
+    ftruncate($f, 0);
+    fwrite($f, "abcdefghij");
+    rewind($f);
+    var_dump(fread($f, 10));
   }
 }
 

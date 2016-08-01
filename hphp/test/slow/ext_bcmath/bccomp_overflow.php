a7cc2482597e99f04d1940299742f894d49e84d0
mwilliams@fb.com <mwilliams@fb.com>
2016-08-01 15:13:10 -0700

    Fix buffer overrun due to integer overflow in bcmath
    Summary: scale gets passed around as an int inside the library.
    
    Reviewed By: mxw
    
    Differential Revision: D3624520
    
    fbshipit-source-id: d39927413cec24fda2e475a296ad5d9019ccef0a

---
+++ hphp/test/slow/ext_bcmath/bccomp_overflow.php
@@ -0,0 +1,7 @@
+<?php
+
+$intMaxPre = PHP_INT_MAX - 1;
+$stringNormal = 'abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+$number_bccomp_10 = bccomp("2015.5", $stringNormal, $intMaxPre);
+var_dump($number_bccomp_10);

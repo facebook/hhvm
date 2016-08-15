2a25526b52a95b0cd7e2277d90910fd5d47beb54
Kasper Kopec <kasper@fb.com>
2016-08-15 16:49:51 -0700

    Injectable version of ClientProvider to use in tests
    Summary:
    TestClientProvider is an implementation of ClientProvider_sig.S that doesn't use file descriptors, but always returns "no clients".
    
    I use alexchow dependency injection method to decide which module version to use based on a boolean flag in config file.
    
    Reviewed By: alexchow
    
    Differential Revision: D3700159
    
    fbshipit-source-id: 3a3aa3bc91e61070df72253252f2b4f7b263a791

---
+++ hphp/hack/src/server/testClientProvider.ml
@@ -0,0 +1,25 @@
+type t = unit
+type client = unit
+
+let provider_from_file_descriptor _ = ()
+let client_from_channel_pair _ = ()
+
+let sleep_and_check _ _ = false, false
+
+let not_implemented () = failwith "not implemented"
+
+let accept_client _ = not_implemented ()
+
+let say_hello _ = not_implemented ()
+
+let read_connection_type _ = not_implemented ()
+
+let send_response_to_client _ _ = not_implemented ()
+
+let read_client_msg _ = not_implemented ()
+
+let get_channels _ =  not_implemented ()
+
+let is_persistent _ _ = not_implemented ()
+
+let shutdown_client _ = not_implemented ()

59607de05849e8b98a2d060b8a9b36290ca32e12
Philip Hölzenspies <drphil@fb.com>
2016-09-20 11:51:52 -0700

    Bugfix; arguments to rewriters flipped
    Summary: Looking into the rewriter for parse trees from the full fidelity parser, I noticed that the arguments for the helper function in `rewrite_post` and `rewrite_pre` were in the wrong order. This bug wasn't surfaced yet, because nobody uses these functions (yet). (Also, corrected indent that was off.)
    
    Reviewed By: ericlippert, andrewjkennedy
    
    Differential Revision: D3886402
    
    fbshipit-source-id: 6b32a0c3e4c45f5e5dc648af68abb10ef2cf6619

--- hphp/hack/src/full_fidelity/full_fidelity_rewriter.ml
+++ hphp/hack/src/full_fidelity/full_fidelity_rewriter.ml
@@ -17,48 +17,48 @@ end
 
 module WithSyntax(Syntax: RewritableType) = struct
 
-(* The rewriter takes a function f that is [node] -> node -> (node, bool),
-   where the bool indicates whether something changed. The rewriter
-   itself returns (node, bool).  The list of nodes passed in to f
-   is the parent chain of the node. First the children are rewritten,
-   then f is called on the rewritten node. *)
+  (* The rewriter takes a function f that is [node] -> node -> (node, bool),
+     where the bool indicates whether something changed. The rewriter
+     itself returns (node, bool).  The list of nodes passed in to f
+     is the parent chain of the node. First the children are rewritten,
+     then f is called on the rewritten node. *)
 
-let parented_rewrite_post f node =
-  let rec aux parents f node =
-    let folder (acc, changed) child =
-      let (new_child, child_changed) = aux (node :: parents) f child in
-      (new_child :: acc, changed || child_changed) in
-    let cs = Syntax.children node in
-    let (new_children, child_changed) =
-      List.fold_left folder ([], false) cs in
-    let with_new_children =
-      if child_changed then
-        Syntax.from_children (Syntax.kind node) (List.rev new_children)
-      else
-        node in
-    let (new_node, node_changed) = f parents with_new_children in
-    (new_node, node_changed || child_changed) in
-  aux [] f node
+  let parented_rewrite_post f node =
+    let rec aux parents f node =
+      let folder (acc, changed) child =
+        let (new_child, child_changed) = aux (node :: parents) f child in
+        (new_child :: acc, changed || child_changed) in
+      let cs = Syntax.children node in
+      let (new_children, child_changed) =
+        List.fold_left folder ([], false) cs in
+      let with_new_children =
+        if child_changed then
+          Syntax.from_children (Syntax.kind node) (List.rev new_children)
+        else
+          node in
+      let (new_node, node_changed) = f parents with_new_children in
+      (new_node, node_changed || child_changed) in
+    aux [] f node
 
-(* As above, but the rewrite happens to the node first and then
-   recurses on the children. *)
+  (* As above, but the rewrite happens to the node first and then
+     recurses on the children. *)
 
-let parented_rewrite_pre f node =
-  let rec aux parents f node =
-    let folder (acc, changed) child =
-      let (new_child, child_changed) = aux (node :: parents) f child in
-      (new_child :: acc, changed || child_changed) in
-    let (new_node, node_changed) = f parents node in
-    let cs = Syntax.children new_node in
-    let (new_children, child_changed) =
-      List.fold_left folder ([], false) cs in
-    let with_new_children =
-      if child_changed then
-        Syntax.from_children (Syntax.kind new_node) (List.rev new_children)
-      else
-        new_node in
-    (with_new_children, node_changed || child_changed) in
-  aux [] f node
+  let parented_rewrite_pre f node =
+    let rec aux parents f node =
+      let folder (acc, changed) child =
+        let (new_child, child_changed) = aux (node :: parents) f child in
+        (new_child :: acc, changed || child_changed) in
+      let (new_node, node_changed) = f parents node in
+      let cs = Syntax.children new_node in
+      let (new_children, child_changed) =
+        List.fold_left folder ([], false) cs in
+      let with_new_children =
+        if child_changed then
+          Syntax.from_children (Syntax.kind new_node) (List.rev new_children)
+        else
+          new_node in
+      (with_new_children, node_changed || child_changed) in
+    aux [] f node
 
   (* These are simpler versions of the rewriter, for cases where the caller
      does not need the parent context.
@@ -66,11 +66,11 @@ let parented_rewrite_pre f node =
      the bool indicates whether something changed.  *)
 
   let rewrite_post f node =
-    let g node parents = f node in
+    let g parents node = f node in
     parented_rewrite_post g node
 
   let rewrite_pre f node =
-    let g node parents = f node in
+    let g parents node = f node in
     parented_rewrite_pre g node
 
 end

b4ebf1051bf3a3aeccc13532824b1d79fa8e081b
Kasper Kopec <kasper@fb.com>
2016-09-20 23:06:09 -0700

    Add a missing test exercising to_recheck2 set in ServerTypeCheck
    Summary:
    As mentioned in {D3891404}, I was surprised that when I erase the contents of this set, all of our tests still pass.
    
    I though the purpose of this set is to capture the errors caused transitively through inheritance and typing usage (e.g Bar uses FooChild::f() which is inherited from Foo::f(), and we changed the method in Foo). But it looks like in cases like this one, there is a direct typing dependency between Foo and Bar (though that might be something that wasn't the case in the past).
    
    After some very tiresome bisecting over real diffs in www, one case I found out where it is necessary, is when Foo didn't exist in the first place, and we are only adding it. This is the test case that I added.
    
    The problem then was making this visible user error - the errors were correct, because FooChild ended up being redeclared due to "always redecl things that failed decl before" rule. Failing decl was less common in the past, when we had assume_php = true and less strict files - now I had to resort to HH_FIXME's to make it show.
    
    I'll work on IDE/lazy check version of this rule in next diff.
    
    Reviewed By: oulgen
    
    Differential Revision: D3898080
    
    fbshipit-source-id: dcdbacb91142edbde2c7aa6d1a405fe81f40b051

---
+++ hphp/hack/test/integration_ml/test_added_parent.ml
@@ -0,0 +1,79 @@
+(**
+ * Copyright (c) 2016, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the "hack" directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ *)
+
+open Integration_test_base_types
+
+module Test = Integration_test_base
+
+let foo_name = "foo.php"
+let foo_contents =
+"<?hh // strict
+
+class Foo {
+  public function f() : string {
+    // UNSAFE
+  }
+}
+"
+
+let foo_child_name = "foo_child.php"
+let foo_child_contents =
+"<?hh // strict
+
+/* HH_FIXME[2049] */
+/* HH_FIXME[4123] */
+class FooChild extends Foo {}
+"
+
+let bar_name = "bar.php"
+let bar_contents =
+"<?hh // strict
+
+function take_int(int $x) : void {}
+
+function test(FooChild $foo_child) : void {
+  take_int($foo_child->f());
+}
+"
+
+let bar_errors =
+"File \"/bar.php\", line 6, characters 12-26:
+Invalid argument (Typing[4110])
+File \"/bar.php\", line 3, characters 19-21:
+This is an int
+File \"/foo.php\", line 4, characters 25-30:
+It is incompatible with a string
+"
+
+let get_errors env = Errors.get_error_list env.ServerEnv.errorl
+
+let assert_no_errors env =
+  if get_errors env <> [] then Test.fail "Expected to have no errors"
+
+let () =
+  let env = Test.setup_server () in
+  let env = Test.setup_disk env [
+    foo_name, "";
+    foo_child_name, foo_child_contents;
+    bar_name, bar_contents;
+  ] in
+
+  (* We need to suppress all the errors (see HH_FIXMEs above), otherwise the
+   * logic that always rechecks the files with errors kicks in and does the
+   * same job as phase2 fanout. We want to test the latter one in this test. *)
+  assert_no_errors env;
+
+  (* restore parent, but with a mismatching return type of f() *)
+  let env, _ = Test.(run_loop_once env { default_loop_input with
+    disk_changes = [
+      foo_name, foo_contents;
+    ]
+  }) in
+  Test.assertSingleError bar_errors (get_errors env)

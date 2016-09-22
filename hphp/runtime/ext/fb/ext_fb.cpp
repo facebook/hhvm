b554d09c4fe60142eee0bd5223bdee5143a799e7
Christopher Dykes <cdykes@fb.com>
2016-09-22 12:44:06 -0700

    Move FBSerialize.h to a subdirectory
    Summary: Because it's in the way where it is.
    
    Reviewed By: aorenste
    
    Differential Revision: D3895765
    
    fbshipit-source-id: c24f5622b4190528f6d70bb81faf56ae018e3bd2

--- hphp/runtime/ext/fb/ext_fb.cpp
+++ hphp/runtime/ext/fb/ext_fb.cpp
@@ -45,7 +45,7 @@
 #include "hphp/runtime/base/string-util.h"
 #include "hphp/runtime/base/thread-info.h"
 #include "hphp/runtime/ext/std/ext_std_function.h"
-#include "hphp/runtime/ext/fb/FBSerialize.h"
+#include "hphp/runtime/ext/fb/FBSerialize/FBSerialize.h"
 #include "hphp/runtime/ext/fb/VariantController.h"
 #include "hphp/runtime/vm/unwind.h"
 

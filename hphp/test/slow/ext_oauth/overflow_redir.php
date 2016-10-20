3179871b68237df2a755b8d66234858392b773e2
Neal Poole <neal@fb.com>
2016-10-20 13:09:17 -0700

    Make hphp/test/slow/ext_oauth/overflow_redir.php agnostic to the order of HTTP headers
    Summary: http://git.php.net/?p=pecl/web_services/oauth.git;a=blob;f=tests/overflow_redir.phpt;h=aa3e9411e760368a0f22da4bfb73db70c110c154;hb=refs/heads/master is outputting two HTTP requests as a string. Our tests will fail here if the order of the headers ever changes (ie: because of a change in libcurl behavior, like http://git.php.net/?p=pecl/web_services/oauth.git;a=commitdiff;h=9fab60303cdae63bfe488d5154ebbc8b8761923a). So lets rewrite the test to make them ignore the header order by sorting in a predictable way.
    
    Reviewed By: markw65
    
    Differential Revision: D4052126
    
    fbshipit-source-id: 5bab7143b63982c89d5c4b23ca3d27552b3dadb3

--- hphp/test/slow/ext_oauth/overflow_redir.php
+++ hphp/test/slow/ext_oauth/overflow_redir.php
@@ -6,18 +6,30 @@ $x->setRequestEngine(OAUTH_REQENGINE_CURL);
 
 $port = random_free_port();
 $pid = http_server("tcp://127.0.0.1:$port", array(
-	"HTTP/1.0 302 Found\r\nLocation: http://127.0.0.1:$port/" . str_repeat('a', 512) . "bbb\r\n\r\n",
-	"HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 40\r\n\r\noauth_token=1234&oauth_token_secret=4567",
+  "HTTP/1.0 302 Found\r\nLocation: http://127.0.0.1:$port/" . str_repeat('a', 512) . "bbb\r\n\r\n",
+  "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 40\r\n\r\noauth_token=1234&oauth_token_secret=4567",
 ), $output);
 
 try {
-	$x->setAuthType(OAUTH_AUTH_TYPE_AUTHORIZATION);
-	var_dump($x->getRequestToken("http://127.0.0.1:$port/test", null, 'GET'));
+  $x->setAuthType(OAUTH_AUTH_TYPE_AUTHORIZATION);
+  var_dump($x->getRequestToken("http://127.0.0.1:$port/test", null, 'GET'));
 } catch (Exception $e) {
-	var_dump($x->debugInfo);
+  var_dump($x->debugInfo);
 }
 fseek($output, 0, SEEK_SET);
-var_dump(stream_get_contents($output));
+
+// Split up the requests, make sure we sort the headers
+// since we need to compare output but we don't care about
+// the exact ordering.
+$requests = explode("\r\n\r\n", trim(stream_get_contents($output)));
+foreach ($requests as $cur_request) {
+  $request_headers = explode("\r\n", $cur_request);
+  var_dump(array_shift($request_headers));
+  sort($request_headers);
+  foreach ($request_headers as $cur_header) {
+    var_dump($cur_header);
+  }
+}
 
 http_server_kill($pid);
 

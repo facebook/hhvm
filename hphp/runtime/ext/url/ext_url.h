055c3ddb59923e941d66c5dd8567e237613578a6
Eugen Wissner <belka@caraus.de>
2016-10-24 12:00:54 -0700

    Fix http_build_query's numeric_prefix for php7
    Summary:
    Closes #7431.
    Closes https://github.com/facebook/hhvm/pull/7437
    
    Differential Revision: D4067798
    
    Pulled By: aorenste
    
    fbshipit-source-id: 227d09815d8a15ac7a4dd2bd4de535cf24128801

--- hphp/runtime/ext/url/ext_url.h
+++ hphp/runtime/ext/url/ext_url.h
@@ -43,7 +43,7 @@ Array HHVM_FUNCTION(get_meta_tags, const String& filename,
                                    bool use_include_path /* = false */);
 
 Variant HHVM_FUNCTION(http_build_query, const Variant& formdata,
-                           const String& numeric_prefix /* = null_string */,
+                           const Variant& numeric_prefix /* = null_string */,
                            const String& arg_separator /* = null_string */,
                            int enc_type = k_PHP_QUERY_RFC1738);
 Variant HHVM_FUNCTION(parse_url, const String& url,
---
+++ hphp/test/slow/ext_url/http_build_query_numeric_prefix.php
@@ -0,0 +1,14 @@
+<?php
+$query = array(
+    0                    => "4.6.1",
+    'php'                => "7.0.99-hhvm",
+    'locale'             => "de_DE",
+    'mysql'              => "5.5.5",
+    'local_package'      => "de_DE",
+    'blogs'              => 1,
+    'users'              => 5,
+    8                    => 0,
+    'initial_db_version' => 27916,
+);
+var_dump(http_build_query($query, null, "&"));
+var_dump(http_build_query($query, 52, "&"));

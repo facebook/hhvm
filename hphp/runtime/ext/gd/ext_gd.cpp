8cc9ec69da0f878cb2be4c3096e8264436441ce4
Aaron Orenstein <aorenste@fb.com>
2016-10-25 10:10:40 -0700

    Fix getimagesize returning FALSE on valid jpg
    Summary:
    getimagesize() is rather strict about the length of the marker payload data,
    and fails if there are extraneous bytes before the next marker. Only a very
    special case reported in bug #13213 is catered to.
    
    libjpeg is rather resilient to such corrupted JPEG files, and raises a
    recoverable error in this case. Other image processors also accept such
    JPEG files, so we adapt getimagesize() to skip (but warn about) such
    extraneous bytes.
    
    (This is the same fix as PHP commit https://github.com/php/php-src/commit/82df4e263886a0da21a00c98189649287666ba5c
    by Christoph M. Becker <cmbecker69@gmx.de> fixing PHP [bug 72278](https://bugs.php.net/bug.php?id=72278).)
    Closes https://github.com/facebook/hhvm/pull/7435
    Github Author: =?UTF-8?q?Bartosz=20Dziewo=C5=84ski?= <matma.rex@gmail.com>
    
    Github PR Sync: {sync, type="child", parent="github", parentrepo="facebook/hhvm", parentprnum="7435", parentprfbid="321128794922734"}
    
    Test Plan: Imported from GitHub, without a `Test Plan:` line.
    
    Reviewers: michaelofarrell, svcscm
    
    Reviewed By: michaelofarrell, svcscm
    
    Subscribers: svcscm, hphp-diffs@, cdykes, michaelofarrell
    
    Differential Revision: https://phabricator.intern.facebook.com/D4062300
    
    Signature: t1:4062300:1477408360:e8b99a85453d10f2262584e0afb3139f914eaaf5

--- hphp/runtime/ext/gd/ext_gd.cpp
+++ hphp/runtime/ext/gd/ext_gd.cpp
@@ -726,47 +726,35 @@ static unsigned short php_read2(const req::ptr<File>& stream) {
 
 static unsigned int php_next_marker(const req::ptr<File>& file,
                                     int last_marker,
-                                    int comment_correction,
                                     int ff_read) {
   int a=0, marker;
 
   // get marker byte, swallowing possible padding
-  if (last_marker==M_COM && comment_correction) {
-    // some software does not count the length bytes of COM section
-    // one company doing so is very much envolved in JPEG... so we accept too
-    // by the way: some of those companies changed their code now...
-    comment_correction = 2;
-  } else {
-    last_marker = 0;
-    comment_correction = 0;
+  if (!ff_read) {
+    size_t extraneous = 0;
+
+    while ((marker = file->getc()) != 0xff) {
+      if (marker == EOF) {
+        return M_EOI;/* we hit EOF */
+      }
+      extraneous++;
+    }
+    if (extraneous) {
+      raise_warning("corrupt JPEG data: %zu extraneous bytes before marker",
+                    extraneous);
+    }
   }
-  if (ff_read) {
-    a = 1; /* already read 0xff in filetype detection */
-  }
+  a = 1;
   do {
     if ((marker = file->getc()) == EOF)
     {
       return M_EOI;/* we hit EOF */
-    }
-    if (last_marker==M_COM && comment_correction>0)
-    {
-      if (marker != 0xFF)
-      {
-        marker = 0xff;
-        comment_correction--;
-      } else {
-        last_marker = M_PSEUDO; /* stop skipping non 0xff for M_COM */
-      }
     }
     ++a;
   } while (marker == 0xff);
   if (a < 2)
   {
     return M_EOI; /* at least one 0xff is needed before marker code */
-  }
-  if ( last_marker==M_COM && comment_correction)
-  {
-    return M_EOI; /* ah illegal: char after COM section not 0xFF */
   }
   return (unsigned int)marker;
 }
@@ -816,7 +804,7 @@ struct gfxinfo *php_handle_jpeg(const req::ptr<File>& file, Array& info) {
   unsigned short length, ff_read=1;
 
   for (;;) {
-    marker = php_next_marker(file, marker, 1, ff_read);
+    marker = php_next_marker(file, marker, ff_read);
     ff_read = 0;
     switch (marker) {
     case M_SOF0:

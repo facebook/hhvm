d119df120c454b186e23b602a094ed1204ed9983
Mark Williams <mwilliams@fb.com>
2016-11-01 10:07:36 -0700

    Fix tier matching
    Summary:
    Given 0 for backtrack and recursion limits, pcre8.32 seemed to do
    the right thing. For 8.38, we're getting inconsistent results -
    regexes match on one machine, but not another.
    
    Using zero was never intentional, so lets just set them to the global
    defaults instead.
    
    Reviewed By: mxw
    
    Differential Revision: D4111207
    
    fbshipit-source-id: 61009906545c5a26e871d68d93db8465b5e5d774

--- hphp/runtime/base/preg.cpp
+++ hphp/runtime/base/preg.cpp
@@ -65,6 +65,10 @@ using jit::TCA;
 
 PCREglobals::PCREglobals() {
   jit_stack = pcre_jit_stack_alloc(32768, 524288);
+  // Set these to handle uses of pcre prior to PcreExtension::threadInit
+  // In particular, for matching tier overrides during RuntimeOption::Load
+  preg_backtrace_limit = RuntimeOption::PregBacktraceLimit;
+  preg_recursion_limit = RuntimeOption::PregRecursionLimit;
 }
 
 PCREglobals::~PCREglobals() {

d0a1a07a6bd79b35f4ca5e769a2eafd073f301ab
James Wu <jjwu@fb.com>
2016-12-02 15:14:18 -0800

    Add logging for which file went missing on a file heap stale
    Summary: The information isn't useful if file_heap_stale is working as intended, but it's good in case we have an unintended crash
    
    Reviewed By: alexchow
    
    Differential Revision: D4268468
    
    fbshipit-source-id: e75bdce2a43f20165dda93c097ead15d677b62b7

--- hphp/hack/src/naming/namingGlobal.ml
+++ hphp/hack/src/naming/namingGlobal.ml
@@ -59,6 +59,9 @@ module GEnv = struct
         by adding more files to the unprocessed/partially-processed set in
         the previous loop.
       *)
+      let fn = FileInfo.get_pos_filename pos in
+      Hh_logger.log "File missing: %s" (Relative_path.to_absolute fn);
+      Hh_logger.log "Name missing: %s" (name);
       raise File_heap.File_heap_stale
 
   let type_pos popt name = match TypeIdHeap.get name with

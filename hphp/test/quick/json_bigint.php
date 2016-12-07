626ecb4c38e8acbb52e43d043aeb26069c873bb3
Daniel Andersson <koda@fb.com>
2016-12-07 10:28:42 -0800

    add unit test for json parser bigint bug
    Summary: Add unit test for issue proactively found using the verifier in D4277127.
    
    Reviewed By: markw65
    
    Differential Revision: D4287837
    
    fbshipit-source-id: 92a4fa5f218b1a517cc2aa0d83fc66fdfc2d766d

---
+++ hphp/test/quick/json_bigint.php
@@ -0,0 +1,5 @@
+<?php
+var_dump(json_decode("8012345678901234567", true, 512, JSON_FB_LOOSE));
+var_dump(json_decode("9012345678901234567", true, 512, JSON_FB_LOOSE));
+var_dump(json_decode("80123456789012345678", true, 512, JSON_FB_LOOSE));
+var_dump(json_decode("90123456789012345678", true, 512, JSON_FB_LOOSE));

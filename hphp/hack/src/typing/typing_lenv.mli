f9894b16bcd9991906ace9a6f87cc8a3e540fd45
Oguz Ulgen <oulgen@fb.com>
2016-12-14 13:17:27 -0800

    Fix OSS - Ocaml doesn't allow module name aliasing in mli files
    Summary: Fix OSS - Ocaml doesn't allow module name aliasing in mli files
    
    Reviewed By: dabek
    
    Differential Revision: D4328671
    
    fbshipit-source-id: 8ecb3b2a065eac243d60b5c395196bcd207fb76b

--- hphp/hack/src/typing/typing_lenv.mli
+++ hphp/hack/src/typing/typing_lenv.mli
@@ -8,17 +8,28 @@
  *
  *)
 
-module Env = Typing_env
-
 (*****************************************************************************)
 (* Functions dealing with old style local environment *)
 (*****************************************************************************)
 
-val intersect_fake : Env.fake_members -> Env.fake_members -> Env.fake_members
+val intersect_fake :
+  Typing_env.fake_members ->
+  Typing_env.fake_members -> Typing_env.fake_members
 val intersect :
-  Env.env -> Env.local_env -> Env.local_env -> Env.local_env -> Env.env
-val integrate : Env.env -> Env.local_env -> Env.local_env -> Env.env
+  Typing_env.env ->
+  Typing_env.local_env ->
+  Typing_env.local_env ->
+  Typing_env.local_env -> Typing_env.env
+val integrate :
+  Typing_env.env ->
+  Typing_env.local_env ->
+  Typing_env.local_env -> Typing_env.env
 val intersect_list :
-  Env.env -> Env.local_env -> (bool * Env.local_env) list -> Env.env
-val fully_integrate : Env.env -> Env.local_env -> Env.env
-val env_with_empty_fakes : Env.env -> Env.env
+  Typing_env.env ->
+  Typing_env.local_env ->
+  (bool * Typing_env.local_env) list -> Typing_env.env
+val fully_integrate :
+  Typing_env.env ->
+  Typing_env.local_env -> Typing_env.env
+val env_with_empty_fakes :
+  Typing_env.env -> Typing_env.env

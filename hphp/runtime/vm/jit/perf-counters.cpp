00a807c5e69eb06eeae230772ad6b5612833923d
Daniel Andersson <koda@fb.com>
2016-12-14 14:48:58 -0800

    avoid repeated lookup of perf counter names
    Summary:
    Every call to getPerfCounters was looking up all counter
    names in the static string table. Replace this with one-time
    initialization at startup.
    
    Reviewed By: swtaarrs
    
    Differential Revision: D4209213
    
    fbshipit-source-id: 11850d91c7021b88959e3725cb1aa31a645470c0

--- hphp/runtime/vm/jit/perf-counters.cpp
+++ hphp/runtime/vm/jit/perf-counters.cpp
@@ -46,6 +46,11 @@ const char* const kPerfCounterNames[] = {
   TRANS_PERF_COUNTERS
 };
 #undef TPC
+#define TPC(n) StaticString("jit_" #n),
+const StaticString s_PerfCounterNames[tpc_num_counters] = {
+  TRANS_PERF_COUNTERS
+};
+#undef TPC
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -54,8 +59,7 @@ void getPerfCounters(Array& ret) {
     // Until perflab can automatically scale the values we give it to an
     // appropriate range, we have to fudge these numbers so they look more like
     // reasonable hardware counter values.
-    ret.set(String::FromCStr(kPerfCounterNames[i]),
-            tl_perf_counters[i] * 1000);
+    ret.set(s_PerfCounterNames[i], tl_perf_counters[i] * 1000);
   }
 
   for (auto const& pair : Timer::Counters()) {

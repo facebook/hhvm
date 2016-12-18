2d18b3efa128957971061289af0706d5b33e1523
Aaron Orenstein <aorenste@fb.com>
2016-12-18 10:02:40 -0800

    Check depth values in json_decode
    Summary: Over-large depth values can cause bad things to happen, so don't allow them.
    
    Reviewed By: markw65
    
    Differential Revision: D4091055
    
    fbshipit-source-id: 74e1fd1bebbd71ba379bb7f53f9d43c249901ac1

--- hphp/runtime/ext/json/ext_json.cpp
+++ hphp/runtime/ext/json/ext_json.cpp
@@ -135,6 +135,10 @@ Variant HHVM_FUNCTION(json_decode, const String& json, bool assoc /* = false */,
     json_set_last_error_code(json_error_codes::JSON_ERROR_DEPTH);
     return init_null();
   }
+  if (depth < 0 || depth > INT_MAX) {
+    json_set_last_error_code(json_error_codes::JSON_ERROR_DEPTH);
+    return init_null();
+  }
 
   const int64_t supported_options =
     k_JSON_FB_LOOSE |

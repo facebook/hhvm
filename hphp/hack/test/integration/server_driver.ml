5308b73fe08557a202c0ffa2d77ea2ca03ea7e7a
Alexander Chow <achow@fb.com>
2016-12-20 13:49:05 -0800

    Turntable integration test
    Summary: Turntable integration test
    
    Reviewed By: dabek
    
    Differential Revision: D4324090
    
    fbshipit-source-id: 8501322d6aa618d966d2bd34819049d3d7f77f4c

--- hphp/hack/test/integration/server_driver.ml
+++ hphp/hack/test/integration/server_driver.ml
@@ -48,28 +48,58 @@ module Args = struct
 
 end
 
+type response_matcher =
+  | StringMatcher of string
+  | RegexMatcher of string
+
+let describe_matcher = function
+  | StringMatcher s -> "StringMatcher: " ^ s
+  | RegexMatcher s -> "RegexMatcher: " ^ s
+
+let matches matcher actual = match matcher with
+  | StringMatcher s -> String.equal s actual
+  | RegexMatcher s ->
+    let re = Str.regexp s in
+    try (Str.search_forward re actual 0) >= 0 with
+    | Not_found -> false
+
 (** Mostly a convenient GADT to ADT adaptor, because lists cannot be
  * polymorphic. Also groups together RPC commands with its expected
  * responses. *)
 type command_response =
   (** A pair of the cmd and the expected result as a string. *)
-  | RpcCommand : ('a SCT.t * string) -> command_response
+  | RpcCommand : ('a SCT.t * response_matcher) -> command_response
 
 let cmd_responses_case_0 args = [
-  (RpcCommand ((SCT.STATUS), ""));
+  (RpcCommand ((SCT.STATUS), StringMatcher ""));
   (RpcCommand ((SCT.INFER_TYPE
     (ServerUtils.FileName
       ((Path.concat (Args.root args) "foo_1.php") |> Path.to_string), 4, 20 )),
-      "line 3, character 23 - line 3, character 25, int"));
+      StringMatcher "line 3, character 23 - line 3, character 25, int"));
+  ]
+
+let cmd_responses_case_1 args = [
+  (RpcCommand (
+    (SCT.OPEN_FILE (
+      ((Path.concat (Args.root args) "foo_1.php") |> Path.to_string),
+    (* "foo_1.php", *)
+    {|<?hh
+        function f() {
+            return g() + $missing_var;
+        }
+    |}
+    )), StringMatcher ""));
+   (RpcCommand ((SCT.STATUS), RegexMatcher "^Undefined variable.*"));
   ]
 
 exception Invalid_test_case
 
 let get_test_case args = match args.Args.test_case with
   | 0 -> cmd_responses_case_0 args
+  | 1 -> cmd_responses_case_1 args
   | _ -> raise Invalid_test_case
 
-exception Response_doesnt_match_expected of string * string
+exception Response_doesnt_match_expected of string * response_matcher
 
 (** We make a string representation of the response, then compare that to
  * the expected. It also allows us to print a useful error message
@@ -81,7 +111,7 @@ let response_to_string : type a. a SCT.t -> a -> string = begin
   fun cmd response -> match cmd, response with
     | SCT.STATUS, (_, errors) ->
       let errors = List.map (fun e -> Errors.to_string e) errors in
-      let errors = List.fold_left (fun acc e -> acc ^ "\n" ^ e) "" errors in
+      let errors = String.concat "\n" errors in
       errors
     | SCT.INFER_TYPE _, response ->
       let to_string = function
@@ -93,14 +123,20 @@ let response_to_string : type a. a SCT.t -> a -> string = begin
       in
       let response = to_string response in
       response
+    | SCT.EDIT_FILE _, () -> ""
+    | SCT.OPEN_FILE _, () -> ""
     | _, _ -> "Some other response"
   end
+
+let rec sleep_and_wait () =
+  let _, _, _ = Unix.select [] [] [] 999999.999 in
+  sleep_and_wait ()
 
 let send_command_and_read_response conn cmd_response = match cmd_response with
   | RpcCommand (cmd, expected) ->
     let response = ClientIde.rpc conn cmd in
     let response = response_to_string cmd response in
-    let is_equal = response = expected in
+    let is_equal = matches expected response in
     if is_equal then
       ()
     else
@@ -108,13 +144,18 @@ let send_command_and_read_response conn cmd_response = match cmd_response with
 
 let () =
   Daemon.check_entry_point (); (* this call might not return *)
+  Sys_utils.set_signal Sys.sigint (Sys.Signal_handle (fun _ ->
+    exit 0));
   let args = Args.parse () in
   HackEventLogger.client_init @@ Args.root args;
   let conn = ClientIde.connect_persistent { ClientIde.root = Args.root args }
     ~retries:800 in
   let test_case = get_test_case args in
-  try List.iter (send_command_and_read_response conn) test_case with
+  let () = try List.iter (send_command_and_read_response conn) test_case with
   | Response_doesnt_match_expected (actual, expected) ->
     Printf.eprintf "Failed case %d\n%!" args.Args.test_case;
-    Printf.eprintf "Expected: %s. Found: %s" expected actual;
+    Printf.eprintf "Expected: %s. Found: %s" (describe_matcher expected) actual;
     exit 1
+  in
+  Printf.eprintf "Finished\n%!";
+  sleep_and_wait ()
--- hphp/hack/test/integration/test_hh_record.py
+++ hphp/hack/test/integration/test_hh_record.py
@@ -10,11 +10,14 @@ import signal
 import sys
 import time
 
-from hh_paths import hh_client, recorder_cat, server_driver
+from hh_paths import hh_client, recorder_cat, server_driver, turntable
 
 
 def boxed_string(content):
-    return "\n============\n" + content + "\n===========\n"
+    return (
+        "\n============================\n" +
+        content +
+        "\n============================\n")
 
 # Use the MiniStateTestDriver to set up the custom saved mini state
 # Note: We don't use this driver to drive any of the CommonTests since
@@ -35,6 +38,15 @@ start_with_recorder_on = true
 
 class HhRecordTests(HhRecordTestDriver, unittest.TestCase):
     template_repo = 'hphp/hack/test/integration/data/simple_repo'
+
+    def stop_server(self):
+        logs = self.get_server_logs()
+        (_, _, retcode) = self.proc_call([
+            hh_client,
+            'stop',
+            self.repo_dir
+        ])
+        self.assertEqual(retcode, 0, "See also server logs: " + boxed_string(logs))
 
     def assert_match_regexes(self, doc, regexes, msg):
         for regex in regexes:
@@ -78,7 +90,12 @@ class HhRecordTests(HhRecordTestDriver, unittest.TestCase):
             '(Loaded_saved_state /tmp/.........../foo with 0 dirtied files)',
             "See also recorder daemon logs:" + boxed_string(recorder_log))
 
-    def test_server_driver_case_zero(self):
+    # Start up a hack server and run `server_driver --test-case <case_num>` on it.
+    # Returns the subprocess, path to the recording file, and recorder logs
+    # This doesn't kill the server_driver process, so its persistent connection
+    # with the Hack server is intentionally left alive maintaining the server's
+    # IDE state.
+    def run_server_driver_case(self, case_num):
         self.write_load_config()
         self.run_check()
         logs = self.get_server_logs()
@@ -89,15 +106,54 @@ class HhRecordTests(HhRecordTestDriver, unittest.TestCase):
             logs)
         self.assertIsNotNone(recording_matcher)
         self.assertIn('recorder_out', recording_matcher.group(1))
-        (_, err, retcode) = self.proc_call([
+        self.assertIn('recorder_log', recording_matcher.group(2))
+        proc = self.proc_create([
             server_driver,
-            '0',
+            '--test-case',
+            str(case_num),
             self.repo_dir
-        ])
-        self.assertEqual(
-            retcode,
-            0,
-            "See also server_driver stderr:" + boxed_string(err))
+        ], {})
+        finished = proc.stderr.readline()
+        if finished.strip() != "Finished":
+            proc.send_signal(signal.SIGINT)
+            (driver_out, driver_err) = proc.communicate(None)
+            driver_err = finished + driver_err
+            self.assertEqual(
+                finished.strip(),
+                "Finished",
+                "See also server_driver stderr: " + boxed_string(driver_err))
+        with open(recording_matcher.group(2)) as f:
+            recorder_log = f.read()
+        # Running a new server instance after shutting down the current one
+        # will overwrite the recording's, So we want to return the real
+        # absolute path here.
+        recording = os.path.realpath(recording_matcher.group(1))
+        return proc, recording, recorder_log
+
+    # Run the entire record on the repo using the turntable.
+    # Keeps the turntable spinning even after reaching the end of the cecord
+    # so that the persisent connection is maintained (thus returns that
+    # still-running process).
+    def spin_record(self, recording):
+        proc = self.proc_create([
+            turntable,
+            '--recording',
+            recording,
+            self.repo_dir
+        ], {})
+        for line in proc.stderr:
+            if line.strip() == "Played back an event":
+                continue
+            elif line.strip() == "End of recording...waiting for termination":
+                break
+            else:
+                self.fail("Read unexpected line: " + line)
+        else:
+            self.fail("Read no lines from turntable stderr")
+        return proc
+
+    def test_server_driver_case_zero(self):
+        server_driver, recording, recorder_log = self.run_server_driver_case(0)
         (_, _, retcode) = self.proc_call([
             hh_client,
             'stop',
@@ -109,10 +165,8 @@ class HhRecordTests(HhRecordTestDriver, unittest.TestCase):
         time.sleep(4)
         out, err, retcode = self.proc_call([
             recorder_cat,
-            recording_matcher.group(1)
+            recording
         ])
-        with open(recording_matcher.group(2)) as f:
-            recorder_log = f.read()
         self.assertEqual(
             retcode,
             0,
@@ -125,3 +179,25 @@ class HhRecordTests(HhRecordTestDriver, unittest.TestCase):
                 '(HandleServerCommand INFER_TYPE)'
             ],
             "See also recorder daemon logs:" + boxed_string(recorder_log))
+
+    # Creates a recoridng using server_driver case 1, then spins the recording
+    # on a new Hack instance with the turntable
+    def test_server_driver_case_one_and_turntable(self):
+        server_driver, recording, recorder_log = self.run_server_driver_case(1)
+        self.check_cmd(["{root}foo_1.php:3:26,37: "
+                        "Undefined variable: $missing_var (Naming[2050])"])
+        # Stop the server and the server_driver before spinning up a new server
+        # on which we will playback the recording.
+        self.stop_server()
+        server_driver.send_signal(signal.SIGINT)
+        server_driver.wait()
+        # We can't wait for grandchild pids, i.e. the recorder daemon,
+        # so just give it a little time.
+        time.sleep(1)
+        self.check_cmd(["No errors!"])
+        turntable_proc = self.spin_record(recording)
+        # Before spinning the record, we get no errors. Afterwards, we get
+        # the errors introduced by the record.
+        self.check_cmd(["{root}foo_1.php:3:26,37: Undefined "
+                        "variable: $missing_var (Naming[2050])"])
+        turntable_proc.send_signal(signal.SIGINT)

3959c3bf8ade4963541256cc87810760a83c0165
Eric Lippert <ericlippert@fb.com>
2017-01-09 11:27:32 -0800

    Add a test for XHP bodies that contain PHP comments
    Summary: We should not treat # // /* as comments inside an XHP body.  This tests that change made in a previous diff.
    
    Reviewed By: KendallHopkins
    
    Differential Revision: D4379682
    
    fbshipit-source-id: e25c8e06c7602d1edea13ecae0ee2822175f9667

--- hphp/hack/test/full_fidelity/cases/test_xhp.php
+++ hphp/hack/test/full_fidelity/cases/test_xhp.php
@@ -1,7 +1,7 @@
 <?hh
 function foo() {
-  return <foo bar = "blah"><!--comment
-  comment--><abc/>body{$x}
+  return <foo bar = "blah">#<!--comment
+  comment--><abc/>body{$x}#
   body</foo>;
 }
 class :c {

16653985ee21e42caf8c2f0bfa5bf80f1525e577
Kasper Kopec <kasper@fb.com>
2017-01-17 10:04:43 -0800

    Prioritize persistent client requests over command line ones
    Summary: I didn't put much attention to the use case of using editor with persistent connection and command line client at the same time, hoping that no one will need to do it - alas, the bugs on the editor side (t14993171) make some people revert to typing in editor and looking at the errors in command line (hopefully only for now...). But then they are getting unexpected results, since the edits from editor end up being interleaved with their command line requests. The intuition that people seem to have is "when I save the file and type hh ., I will  get all of the errors reflecting the saved file state". Making sure that we finish all the persistent connection edits first ensures this.
    
    Reviewed By: alexchow
    
    Differential Revision: D4417020
    
    fbshipit-source-id: 4f9cc769060d7ce769313a4ff451b85abf2cd9a7

--- hphp/hack/src/server/serverClientProvider.ml
+++ hphp/hack/src/server/serverClientProvider.ml
@@ -47,7 +47,9 @@ let sleep_and_check in_fd persistent_client_opt =
   in
   let ready_fd_l, _, _ = Unix.select l [] [] (0.1) in
   match ready_fd_l with
-    | [_; _] -> accept_client_opt in_fd, true
+    | [_; _] ->
+        (* Prioritize persistent client requests over command line ones *)
+        None, true
     | [fd] when fd = in_fd -> accept_client_opt in_fd, false
     | [fd] when fd <> in_fd -> None, true
     | _ -> None, false

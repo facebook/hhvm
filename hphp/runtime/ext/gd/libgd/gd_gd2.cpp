6dfc326f1d51e9f88865d053c4ec72f0b4c57580
Aaron Orenstein <aorenste@fb.com>
2017-01-27 08:54:45 -0800

    imagecreatefromgd2() may leak memory
    Summary: As-per title.
    
    Reviewed By: aorenste
    
    Differential Revision: D4429928
    
    fbshipit-source-id: b0b50b507fa0a98a5469568b32ad4d0bb547fdbd

--- hphp/runtime/ext/gd/libgd/gd_gd2.cpp
+++ hphp/runtime/ext/gd/libgd/gd_gd2.cpp
@@ -173,34 +173,37 @@ fail1:
 
 static gdImagePtr _gd2CreateFromFile (gdIOCtxPtr in, int *sx, int *sy, int *cs, int *vers, int *fmt, int *ncx, int *ncy, t_chunk_info ** cidx)
 {
-	gdImagePtr im;
+  gdImagePtr im;
 
-	if (_gd2GetHeader (in, sx, sy, cs, vers, fmt, ncx, ncy, cidx) != 1) {
-		GD2_DBG(php_gd_error("Bad GD2 header"));
-		goto fail1;
-	}
+  if (_gd2GetHeader (in, sx, sy, cs, vers, fmt, ncx, ncy, cidx) != 1) {
+    GD2_DBG(php_gd_error("Bad GD2 header"));
+    goto fail1;
+  }
 
-	if (gd2_truecolor(*fmt)) {
-		im = gdImageCreateTrueColor(*sx, *sy);
-	} else {
-		im = gdImageCreate(*sx, *sy);
-	}
-	if (im == NULL) {
-		GD2_DBG(php_gd_error("Could not create gdImage"));
-		goto fail1;
-	}
+  if (gd2_truecolor(*fmt)) {
+    im = gdImageCreateTrueColor(*sx, *sy);
+  } else {
+    im = gdImageCreate(*sx, *sy);
+  }
+  if (im == NULL) {
+    GD2_DBG(php_gd_error("Could not create gdImage"));
+    goto fail2;
+  }
 
-	if (!_gdGetColors(in, im, (*vers) == 2)) {
-		GD2_DBG(php_gd_error("Could not read color palette"));
-		goto fail2;
-	}
-	GD2_DBG(php_gd_error("Image palette completed: %d colours", im->colorsTotal));
+  if (!_gdGetColors(in, im, (*vers) == 2)) {
+    GD2_DBG(php_gd_error("Could not read color palette"));
+    goto fail3;
+  }
+  GD2_DBG(php_gd_error("Image palette completed: %d colours", im->colorsTotal));
 
-	return im;
+  return im;
+
+fail3:
+  gdImageDestroy(im);
+  return 0;
 
 fail2:
-	gdImageDestroy(im);
-	return 0;
+  gdFree(*cidx);
 
 fail1:
 	return 0;

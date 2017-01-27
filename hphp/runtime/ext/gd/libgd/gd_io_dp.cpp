3c3986d492d7b2740c591047b9e99749d99d4c80
Aaron Orenstein <aorenste@fb.com>
2017-01-27 08:54:45 -0800

    Prevent Stack Buffer Overflow in GD dynamicGetbuf
    Summary: As titled.
    
    Reviewed By: aorenste
    
    Differential Revision: D4430333
    
    fbshipit-source-id: 1e440c574cc49ccbe5d85eee076421d0fe3a654b

--- hphp/runtime/ext/gd/libgd/gd_io_dp.cpp
+++ hphp/runtime/ext/gd/libgd/gd_io_dp.cpp
@@ -226,27 +226,27 @@ static void dynamicPutchar (struct gdIOCtx *ctx, int a)
 
 static int dynamicGetbuf (gdIOCtxPtr ctx, void *buf, int len)
 {
-	int rlen, remain;
-	dpIOCtxPtr dctx;
-	dynamicPtr *dp;
+  int rlen, remain;
+  dpIOCtxPtr dctx;
+  dynamicPtr *dp;
 
-	dctx = (dpIOCtxPtr) ctx;
-	dp = dctx->dp;
+  dctx = (dpIOCtxPtr) ctx;
+  dp = dctx->dp;
 
-	remain = dp->logicalSize - dp->pos;
-	if (remain >= len) {
-		rlen = len;
-	} else {
-		if (remain == 0) {
-			return EOF;
-		}
-		rlen = remain;
-	}
+  remain = dp->logicalSize - dp->pos;
+  if (remain >= len) {
+    rlen = len;
+  } else {
+    if (remain <= 0) {
+      return EOF;
+    }
+    rlen = remain;
+  }
 
-	memcpy(buf, (void *) ((char *) dp->data + dp->pos), rlen);
-	dp->pos += rlen;
+  memcpy(buf, (void *) ((char *) dp->data + dp->pos), rlen);
+  dp->pos += rlen;
 
-	return rlen;
+  return rlen;
 }
 
 static int dynamicGetchar (gdIOCtxPtr ctx)

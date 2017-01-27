ecda5ef04221c1dc9f9ce7e4049d096a0e75b39a
Aaron Orenstein <aorenste@fb.com>
2017-01-27 08:54:45 -0800

    imagefilltoborder stackoverflow on truecolor images
    Summary: This prevents it.
    
    Reviewed By: aorenste
    
    Differential Revision: D4430237
    
    fbshipit-source-id: d366fc75c61e8d552495d0b8567856146ea6a6ea

--- hphp/runtime/ext/gd/libgd/gd.cpp
+++ hphp/runtime/ext/gd/libgd/gd.cpp
@@ -1770,84 +1770,84 @@ void gdImageFilledArc (gdImagePtr im, int cx, int cy, int w, int h, int s, int e
 
 void gdImageFillToBorder (gdImagePtr im, int x, int y, int border, int color)
 {
-	int lastBorder;
-	/* Seek left */
-	int leftLimit = -1, rightLimit;
-	int i, restoreAlphaBlending = 0;
+  int lastBorder;
+  /* Seek left */
+  int leftLimit = -1, rightLimit;
+  int i, restoreAlphaBlending = 0;
 
-	if (border < 0) {
-		/* Refuse to fill to a non-solid border */
-		return;
-	}
+  if (border < 0 || color < 0) {
+    /* Refuse to fill to a non-solid border */
+    return;
+  }
 
-	restoreAlphaBlending = im->alphaBlendingFlag;
-	im->alphaBlendingFlag = 0;
+  restoreAlphaBlending = im->alphaBlendingFlag;
+  im->alphaBlendingFlag = 0;
 
-	if (x >= im->sx) {
-		x = im->sx - 1;
-	} else if (x < 0) {
-		x = 0;
-	}
-	if (y >= im->sy) {
-		y = im->sy - 1;
-	} else if (y < 0) {
-		y = 0;
-	}
+  if (x >= im->sx) {
+    x = im->sx - 1;
+  } else if (x < 0) {
+    x = 0;
+  }
+  if (y >= im->sy) {
+    y = im->sy - 1;
+  } else if (y < 0) {
+    y = 0;
+  }
 
-	for (i = x; i >= 0; i--) {
-		if (gdImageGetPixel(im, i, y) == border) {
-			break;
-		}
-		gdImageSetPixel(im, i, y, color);
-		leftLimit = i;
-	}
-	if (leftLimit == -1) {
-		im->alphaBlendingFlag = restoreAlphaBlending;
-		return;
-	}
-	/* Seek right */
-	rightLimit = x;
-	for (i = (x + 1); i < im->sx; i++) {
-		if (gdImageGetPixel(im, i, y) == border) {
-			break;
-		}
-		gdImageSetPixel(im, i, y, color);
-		rightLimit = i;
-	}
-	/* Look at lines above and below and start paints */
-	/* Above */
-	if (y > 0) {
-		lastBorder = 1;
-		for (i = leftLimit; i <= rightLimit; i++) {
-			int c = gdImageGetPixel(im, i, y - 1);
-			if (lastBorder) {
-				if ((c != border) && (c != color)) {
-					gdImageFillToBorder(im, i, y - 1, border, color);
-					lastBorder = 0;
-				}
-			} else if ((c == border) || (c == color)) {
-				lastBorder = 1;
-			}
-		}
-	}
+  for (i = x; i >= 0; i--) {
+    if (gdImageGetPixel(im, i, y) == border) {
+      break;
+    }
+    gdImageSetPixel(im, i, y, color);
+    leftLimit = i;
+  }
+  if (leftLimit == -1) {
+    im->alphaBlendingFlag = restoreAlphaBlending;
+    return;
+  }
+  /* Seek right */
+  rightLimit = x;
+  for (i = (x + 1); i < im->sx; i++) {
+    if (gdImageGetPixel(im, i, y) == border) {
+      break;
+    }
+    gdImageSetPixel(im, i, y, color);
+    rightLimit = i;
+  }
+  /* Look at lines above and below and start paints */
+  /* Above */
+  if (y > 0) {
+    lastBorder = 1;
+    for (i = leftLimit; i <= rightLimit; i++) {
+      int c = gdImageGetPixel(im, i, y - 1);
+      if (lastBorder) {
+        if ((c != border) && (c != color)) {
+          gdImageFillToBorder(im, i, y - 1, border, color);
+          lastBorder = 0;
+        }
+      } else if ((c == border) || (c == color)) {
+        lastBorder = 1;
+      }
+    }
+  }
 
-	/* Below */
-	if (y < ((im->sy) - 1)) {
-		lastBorder = 1;
-		for (i = leftLimit; i <= rightLimit; i++) {
-			int c = gdImageGetPixel(im, i, y + 1);
+  /* Below */
+  if (y < ((im->sy) - 1)) {
+    lastBorder = 1;
+    for (i = leftLimit; i <= rightLimit; i++) {
+      int c = gdImageGetPixel(im, i, y + 1);
 
-			if (lastBorder) {
-				if ((c != border) && (c != color)) {
-					gdImageFillToBorder(im, i, y + 1, border, color);
-					lastBorder = 0;
-				}
-			} else if ((c == border) || (c == color)) {
-				lastBorder = 1;
-			}
-		}
-	}
-	im->alphaBlendingFlag = restoreAlphaBlending;
+      if (lastBorder) {
+        if ((c != border) && (c != color)) {
+          gdImageFillToBorder(im, i, y + 1, border, color);
+          lastBorder = 0;
+        }
+      } else if ((c == border) || (c == color)) {
+        lastBorder = 1;
+      }
+    }
+  }
+  im->alphaBlendingFlag = restoreAlphaBlending;
 }
 
 /*

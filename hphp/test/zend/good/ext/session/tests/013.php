85a730f96a2b741dbbf358f61316e27a0f3104e2
Christoph Muellner <christoph.muellner@theobroma-systems.com>
2017-01-31 16:10:08 +0100

    test: Make session IDs unique.
    The session ID is used to create a filename of a new created file in /tmp.
    If that is not unique, warning of the following form can be observed:
    
    Warning: open(/tmp/sess_abtest, O_RDWR) failed: Permission denied (13)
    
    This patch make the session ID unique, by calling uniqid() with the
    defined string.
    
    Signed-off-by: Christoph Muellner <christoph.muellner@theobroma-systems.com>

--- hphp/test/zend/good/ext/session/tests/013.php
+++ hphp/test/zend/good/ext/session/tests/013.php
@@ -1,10 +1,10 @@
 <?php
 error_reporting(E_ALL);
 
-session_id("abtest");
+session_id(uniqid("abtest"));
 session_start();
 session_destroy();
-session_id("abtest2");
+session_id(uniqid("abtest2"));
 session_start();
 session_destroy();
 

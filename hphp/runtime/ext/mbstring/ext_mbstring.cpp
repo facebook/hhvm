ed7eaea8651eef9a7a4ead321c5e556e7931a98c
Aaron Orenstein <aorenste@fb.com>
2017-02-10 08:42:29 -0800

    Fail gracefully in mbstring when the output strings are too long
    Summary: As titled.
    
    Reviewed By: aorenste
    
    Differential Revision: D4430443
    
    fbshipit-source-id: 02d270a846bdb53d23d7aeb9c52e1452a21317cb

--- hphp/runtime/ext/mbstring/ext_mbstring.cpp
+++ hphp/runtime/ext/mbstring/ext_mbstring.cpp
@@ -1324,6 +1324,10 @@ Variant HHVM_FUNCTION(mb_convert_kana,
 
   ret = mbfl_ja_jp_hantozen(&string, &result, opt);
   if (ret != nullptr) {
+    if (ret->len > StringData::MaxSize) {
+      raise_warning("String too long, max is %d", StringData::MaxSize);
+      return false;
+    }
     return String(reinterpret_cast<char*>(ret->val), ret->len, AttachString);
   }
   return false;
@@ -1550,6 +1554,10 @@ static Variant php_mb_numericentity_exec(const String& str,
   ret = mbfl_html_numeric_entity(&string, &result, iconvmap, mapsize, type);
   free(iconvmap);
   if (ret != nullptr) {
+    if (ret->len > StringData::MaxSize) {
+      raise_warning("String too long, max is %d", StringData::MaxSize);
+      return false;
+    }
     return String(reinterpret_cast<char*>(ret->val), ret->len, AttachString);
   }
   return false;
@@ -1688,6 +1696,10 @@ Variant HHVM_FUNCTION(mb_encode_mimeheader,
   ret = mbfl_mime_header_encode(&string, &result, charsetenc, transenc,
                                 linefeed.data(), indent);
   if (ret != nullptr) {
+    if (ret->len > StringData::MaxSize) {
+      raise_warning("String too long, max is %d", StringData::MaxSize);
+      return false;
+    }
     return String(reinterpret_cast<char*>(ret->val), ret->len, AttachString);
   }
   return false;

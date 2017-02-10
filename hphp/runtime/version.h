c0d0aef3e594ca007b58f0ff74c14071444cf46e
Aaron Orenstein <aorenste@fb.com>
2017-02-10 11:02:00 -0800

    Releasing 3.12.14

--- hphp/runtime/version.h
+++ hphp/runtime/version.h
@@ -13,7 +13,7 @@
 # define HHVM_VERSION_MAJOR 3
 # define HHVM_VERSION_MINOR 12
 # define HHVM_VERSION_PATCH 14
-# define HHVM_VERSION_SUFFIX "-dev"
+# define HHVM_VERSION_SUFFIX ""
 #endif
 
 /* HHVM_VERSION_ID minus the patch number

af6d3e25681bcc60400e4b9ea3f44af99076ea73
Christopher Dykes <cdykes@fb.com>
2017-02-28 08:41:11 -0800

    don't assume POST data is NUL-terminated
    Summary: Use actual POST data size when parsing it, rather than assuming it's always NUL-terminated. Verify in DEBUG mode that there are no embedded NULs.
    
    Reviewed By: markw65
    
    Differential Revision: D4537912
    
    fbshipit-source-id: 0ede9788d75ab290521d311a3ad5501bc95b5337

--- hphp/runtime/server/transport.cpp
+++ hphp/runtime/server/transport.cpp
@@ -194,9 +194,10 @@ void Transport::parsePostParams() {
     size_t size;
     const char *data = (const char *)getPostData(size);
     if (data && *data && size) {
-      // Post data may be binary, but if parsePostParams() is called, it is
-      // correct to handle it as a null-terminated string
-      m_postData = strdup(data);
+      // Post data may be binary, but if parsePostParams() is called, any
+      // wellformed data cannot have embedded NULs. If it does, we simply
+      // truncate it.
+      m_postData = strndup(data, size);
       parseQuery(m_postData, m_postParams);
     }
     m_postDataParsed = true;

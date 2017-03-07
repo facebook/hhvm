f619c01807143d01980e035eb29e66c64ddb10fd
Norbyte <infernorb@gmail.com>
2017-03-07 22:14:51 +0100

    Fix possible self-assignment

--- hphp/runtime/ext/pgsql/pq.h
+++ hphp/runtime/ext/pgsql/pq.h
@@ -39,9 +39,11 @@ struct Result {
   }
 
   Result& operator=(Result&& other) {
-    clear();
-    m_res = other.m_res;
-    other.m_res = nullptr;
+    if (this != &other) {
+      clear();
+      m_res = other.m_res;
+      other.m_res = nullptr;
+    }
     return *this;
   }
 

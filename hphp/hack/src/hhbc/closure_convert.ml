819bb4567b6095dfcc672b593b6faac31f596dc7
Andrew Kennedy <akenn@fb.com>
2017-03-22 13:04:35 -0700

    Codegen: fix order and numbering of closure classes
    Summary: We now match HHVM in the naming and ordering of closure classes in the output.
    
    Reviewed By: oulgen
    
    Differential Revision: D4745849
    
    fbshipit-source-id: 5d3f4e49e64369c157d00a93f6185f83ac113531

--- hphp/hack/src/hhbc/closure_convert.ml
+++ hphp/hack/src/hhbc/closure_convert.ml
@@ -17,39 +17,44 @@ let strip_dollar id =
   String.sub id 1 (String.length id - 1)
 
 type env = {
-  (* Name of enclosing function in form class::method *)
-  outer_function_name : string;
-  (* Are we in a static or top-level function? *)
+  (* Prefix used to name closure classes in form
+   *   Closure$ (top-level statements)
+   *   Closure$fun (top-level function called fun)
+   *   Closure$cls::meth (method)
+   *)
+  prefix : string;
+  (* Are we in a static or top-level function or statement?
+   *)
   outer_is_static : bool;
   (* Number of closures created in the current function *)
   per_function_count : int;
-  (* Total number of closures created *)
-  total_count : int;
-  (* Closure class number, used in CreateCl instruction *)
-  class_num : int;
-  (* The closure classes themselves *)
-  closures : class_ list;
   (* Free variables computed so far *)
   captured_vars : ULS.t;
   (* Variables that are *defined* in current scope (e.g. lambda parameters)
    * and so should not be added to capture set
    *)
   defined_vars : SSet.t;
+  (* Total number of closures created *)
+  total_count : int;
+  (* Closure class number, used in CreateCl instruction *)
+  class_num : int;
+  (* The closure classes themselves *)
+  closures : class_ list;
 }
 
-let initial_env initial_class_num : env =
+let initial_env initial_class_num =
 {
-  outer_function_name = "";
+  prefix = "Closure$";
   outer_is_static = true;
   per_function_count = 0;
+  captured_vars = ULS.empty;
+  defined_vars = SSet.empty;
   total_count = 0;
   class_num = initial_class_num;
   closures = [];
-  captured_vars = ULS.empty;
-  defined_vars = SSet.empty;
 }
 
-(* Add a variable to the free variables *)
+(* Add a variable to the captured variables *)
 let add_var env var =
   (* If it's bound as a parameter or definite assignment, don't add it *)
   if SSet.mem var env.defined_vars
@@ -71,34 +76,34 @@ let enter_lambda env fd =
 let add_defined_var env var =
   { env with defined_vars = SSet.add var env.defined_vars }
 
-(* Set the function name, upon entering a function (top-level or in a class) *)
-let set_function env is_static name =
+(* Set the prefix name, upon entering a top-level function *)
+let set_function env fd =
   { env with
-    outer_function_name = name;
-    outer_is_static = is_static;
+    prefix = "Closure$" ^ Utils.strip_ns (snd fd.f_name);
+    outer_is_static = true;
+    per_function_count = 0; }
+
+(* Set the prefix name, upon entering a top-level function *)
+let set_method env cd md =
+  { env with
+    prefix = "Closure$" ^
+      Utils.strip_ns (snd cd.c_name) ^
+      "::" ^
+      Utils.strip_ns (snd md.m_name);
+    outer_is_static = List.mem md.m_kind Static;
     per_function_count = 0; }
 
 (* Retrieve the closure classes in the order that they were generated *)
 let get_closure_classes env =
   List.rev env.closures
 
-let make_closure_name env =
-  let prefix = "Closure$" in
+let make_closure_name total_count env =
   let name = if env.per_function_count = 1
-             then prefix ^ env.outer_function_name
-             else prefix ^ env.outer_function_name ^
-               "#" ^ string_of_int env.per_function_count in
-  name ^ ";" ^ string_of_int (env.total_count + 1)
+               then env.prefix
+               else env.prefix ^ "#" ^ string_of_int env.per_function_count in
+  name ^ ";" ^ string_of_int (total_count + 1)
 
-(* Bump the counts; these are used only to construct class names *)
-let inc_counts env =
-  { env with
-    per_function_count = env.per_function_count + 1;
-    total_count = env.total_count + 1; }
-
-let add_closure p env fd body =
-  let class_num = env.class_num in
-  let env = inc_counts env in
+let make_closure p total_count class_num env fd body =
   let unique_var_names = ULS.items env.captured_vars in
   let md = {
     m_kind = [Public] @ (if env.outer_is_static then [Static] else []);
@@ -107,10 +112,7 @@ let add_closure p env fd body =
     m_name = (fst fd.f_name, "__invoke");
     m_params = fd.f_params;
     m_body = body;
-    m_user_attributes =
-      (*{ ua_name = (p, "no_override"); ua_params = [] } ::
-      { ua_name = (p, "unique"); ua_params = [] } :: *)
-      fd.f_user_attributes;
+    m_user_attributes = fd.f_user_attributes;
     m_ret = fd.f_ret;
     m_ret_by_ref = fd.f_ret_by_ref;
     m_fun_kind = fd.f_fun_kind;
@@ -124,7 +126,7 @@ let add_closure p env fd body =
     c_final = false;
     c_kind = Cnormal;
     c_is_xhp = false;
-    c_name = (p, make_closure_name env);
+    c_name = (p, make_closure_name total_count env);
     c_tparams = [];
     c_extends = [(p, Happly((p, "Closure"), []))];
     c_implements = [];
@@ -136,9 +138,7 @@ let add_closure p env fd body =
   let inline_fundef =
     { fd with f_body = [];
               f_name = (p, string_of_int class_num) } in
-  let env = { env with class_num = env.class_num + 1; } in
-  let env = { env with closures = cd :: env.closures } in
-  env, inline_fundef, unique_var_names
+  inline_fundef, unique_var_names, cd
 
 let rec convert_expr env (p, expr_ as expr) =
   match expr_ with
@@ -249,9 +249,18 @@ and convert_lambda env p fd use_vars_opt =
   (* Remember the current capture and defined set across the lambda *)
   let captured_vars = env.captured_vars in
   let defined_vars = env.defined_vars in
+  let total_count = env.total_count + 1 in
+  let class_num = env.class_num in
+  let closures_until_now = env.closures in
+  let env = { env with
+    total_count = total_count;
+    closures = [];
+    class_num = env.class_num + 1 } in
   let env = enter_lambda env fd in
   let env, block = convert_block env fd.f_body in
-  let env, inline_fundef, lambda_vars = add_closure p env fd block in
+  let env = { env with per_function_count = env.per_function_count + 1 } in
+  let inline_fundef, lambda_vars, cd =
+    make_closure p total_count class_num env fd block in
   (* Restore capture and defined set *)
   let env = { env with captured_vars = captured_vars;
                        defined_vars = defined_vars } in
@@ -265,6 +274,7 @@ and convert_lambda env p fd use_vars_opt =
       List.map use_vars (fun ((_, var), _ref) -> var), use_vars in
   (* Add lambda captured vars to current captured vars *)
   let env = List.fold_left lambda_vars ~init:env ~f:add_var in
+  let env = { env with closures = env.closures @ cd :: closures_until_now } in
   env, (p, Efun (inline_fundef, use_vars))
 
 and convert_exprs env el =
@@ -379,22 +389,18 @@ and convert_afield env afield =
     env, AFkvalue (e1, e2)
 
 let convert_fun env fd =
-  let function_name = Utils.strip_ns (snd fd.f_name) in
-  let env = set_function env true function_name in
+  let env = set_function env fd in
   let env, block = convert_block env fd.f_body in
   env, { fd with f_body = block }
 
 let rec convert_class env cd =
-  let class_name = Utils.strip_ns (snd cd.c_name) in
-  let env, c_body = List.map_env env cd.c_body (convert_class_elt class_name) in
+  let env, c_body = List.map_env env cd.c_body (convert_class_elt cd) in
   env, { cd with c_body = c_body }
 
-and convert_class_elt class_name env ce =
+and convert_class_elt cd env ce =
   match ce with
   | Method md ->
-    let method_name = Utils.strip_ns (snd md.m_name) in
-    let env = set_function env (List.mem md.m_kind Static)
-      (class_name ^ "::" ^ method_name) in
+    let env = set_method env cd md in
     let env, block = convert_block env md.m_body in
     env, Method { md with m_body = block }
 

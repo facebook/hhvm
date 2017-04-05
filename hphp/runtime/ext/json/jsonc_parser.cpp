facebook/hhvm

commit f7a1dae3d90179c10c03e749875aa8a6a5015605

Fix json c parser for overflowing ints (they become floats). See D4359593
Reviewed By: Orvid

Differential Revision: D4654138

fbshipit-source-id: ae967bc6507ffd5c2e3feee69a1423380342daa9

hphp/runtime/ext/json/jsonc_parser.cpp
@@ -28,6 +28,8 @@
 #include <json/json.h>
 #endif
 
+#include <folly/CppAttributes.h>
+
 #include "hphp/runtime/ext/collections/ext_collections-map.h"
 #include "hphp/runtime/ext/collections/ext_collections-vector.h"
 #include "hphp/runtime/ext/json/JSON_parser.h"
@@ -170,6 +172,13 @@ Variant json_object_to_variant(json_object *new_obj, const bool assoc,
 
     type = json_object_get_type(new_obj);
     switch (type) {
+    case json_type_int:
+        i64 = json_object_get_int64(new_obj);
+        if (!(i64==INT64_MAX || i64==INT64_MIN)) {
+          return Variant(i64);
+        }
+        FOLLY_FALLTRHOUGH
+
     case json_type_double:
         return Variant(json_object_get_double(new_obj));
 
@@ -177,13 +186,6 @@ Variant json_object_to_variant(json_object *new_obj, const bool assoc,
         return Variant(String(json_object_get_string(new_obj),
                               json_object_get_string_len(new_obj),
                               CopyString));
-
-    case json_type_int:
-        i64 = json_object_get_int64(new_obj);
-        if (i64==INT64_MAX || i64==INT64_MIN) {
-            // php notice: integer overflow detected
-        }
-        return Variant(i64);
 
     case json_type_boolean:
         if (json_object_get_boolean(new_obj)) {

commit 0a2fcb8608878a16e71a4e6644a3f2182f624d12

Fix typo in json_object_to_variant()
Summary:
Breaks compilation with -DUSE_JSONC=ON, introduced with f7a1dae3d90179c10c03e749875aa8a6a5015605
Closes https://github.com/facebook/hhvm/pull/7766

Reviewed By: Orvid

Differential Revision: D4835709

Pulled By: mofarrell

fbshipit-source-id: 6797aa78bf8e32f33b9c8cf6e3f425366f57043e

hphp/runtime/ext/json/jsonc_parser.cpp
@@ -177,7 +177,7 @@ Variant json_object_to_variant(json_object *new_obj, const bool assoc,
         if (!(i64==INT64_MAX || i64==INT64_MIN)) {
           return Variant(i64);
         }
-        FOLLY_FALLTRHOUGH
+        FOLLY_FALLTHROUGH
 
     case json_type_double:
         return Variant(json_object_get_double(new_obj));
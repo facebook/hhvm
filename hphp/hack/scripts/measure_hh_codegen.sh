27cb84f4d1b120f7027465f27cd0dcd53bc4d59a
Jarred Bonaparte <jarred.bonaparte@oculus.com>
2017-04-11 20:19:26 -0700

    Saving file diffs to everstore for meansure_hh_codegen
    Summary: Writing pure diffs to a file and storing that file in everstore so changes can be investigated
    
    Reviewed By: oulgen
    
    Differential Revision: D4841846
    
    fbshipit-source-id: af4c9615e6ab89b07ac6683b9161b0ae96eaf2a2

--- hphp/hack/scripts/measure_hh_codegen.sh
+++ hphp/hack/scripts/measure_hh_codegen.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script compares codegen of hh against hhvm on some set of php files
 # it does so by generating HHAS files with both hh and hhvm and comparing the
@@ -6,6 +6,11 @@
 #
 # arguments: $1 directory containing php files (it is searched recursively)
 # outputs: logs to scuba table hh_codegen_progress
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
 
 if [ -z "$1" ]; then
   echo 'ERROR: expected argument'
@@ -21,6 +26,7 @@ fi
 HHVM='/usr/local/hphpi/bin/hhvm'
 PHP_FILES=$(find "$1"/ -name '*.php')
 BUCK='tools/build/buck/bin/buck'
+DIFF_TMP=$(mktemp /tmp/diff_hh_codegen.XXXXXX)
 
 NUM_LINES_HHVM=0
 NUM_LINES_HH=0
@@ -33,9 +39,32 @@ for FILE in $PHP_FILES; do
   NUM_LINES_HHVM=$((NUM_LINES_HHVM + $(wc -l < "$HHVM_TMP")))
   NUM_LINES_HH=$((NUM_LINES_HH + $(wc -l < "$HH_TMP")))
   NUM_LINES_COMMON=$((NUM_LINES_COMMON + $(/bin/diff -bBd --unchanged-group-format=\'%=\' --old-group-format=\'\' --new-group-format=\'\' --changed-group-format=\'\' "$HHVM_TMP" "$HH_TMP" | wc -l)))
+  { echo "*******************************************************";
+    echo "Diff details for $FILE";
+    IFS=''
+    /bin/diff -bBd -y "$HHVM_TMP" "$HH_TMP" | expand | sed -e 's/\\n/\\\\n/g' | while read -r line; do
+      OPERATOR="${line:62:1}"
+      if [ "$OPERATOR" == "<" ]
+      then
+        COLOR=${RED}
+      elif [ "$OPERATOR" == ">" ]
+      then
+        COLOR=${GREEN}
+      elif [ "$OPERATOR" == "|" ]
+      then
+        COLOR=${YELLOW}
+      else
+        COLOR=${NC}
+      fi
+      echo -e "${COLOR}${line}${NC}"
+    done;
+    unset IFS
+  } >> "$DIFF_TMP"
   rm "$HHVM_TMP"
   rm "$HH_TMP"
 done
 
 # log to scuba
-$BUCK run //hphp/hack/scripts:log_hh_codegen_results_to_scuba -- "$NUM_LINES_HHVM" "$NUM_LINES_HH" "$NUM_LINES_COMMON" "$1"
+$BUCK run //hphp/hack/scripts:log_hh_codegen_results_to_scuba -- "$NUM_LINES_HHVM" "$NUM_LINES_HH" "$NUM_LINES_COMMON" "$1" "$DIFF_TMP"
+
+rm "$DIFF_TMP"

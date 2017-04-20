34093925e96773b925fbbe254fc7bcd4e9aa95ac
Michael Tingley <tingley@fb.com>
2017-04-20 15:01:48 -0700

    Implement a testing framework for coroutine lowering and stubs for coroutine lowering
    Summary:
    Introduces a test framework and basic test file for coroutines.
    
    The test framework does the following:
    
      1. From a filename, parses a `SyntaxTree`.
      2. Lowers coroutines in the `SyntaxTree`, using `Coroutine_lowerer.lower_coroutines`.
      3. Optionally formats the output. The output is/will be generated code. It doesn't have to look pretty, but it would be useful if it was more legible to humans. This formatting is enabled by default in tests, but the script can be run directly with formatting disabled. It's generally a good idea to run with formatting enabled, as it ensures that the generated `SyntaxTree` is at least comprehensible to the formatting utilities, which is a good indicator of correctness.
    
    In the future, I would also like to support type checking in the test. This is listed as a TODO, currently.
    
    Fwiw, `coroutine_lowerer.ml` now exists. It simply returns what it's given. Descendent diffs will add functionality here.
    
    Reviewed By: ericlippert
    
    Differential Revision: D4916036
    
    fbshipit-source-id: c799a17088bcbe160ace47fcb76ba959837eddb4

---
+++ hphp/hack/src/parser/coroutine/coroutine_lowerer.mli
@@ -0,0 +1,23 @@
+(**
+ * Copyright (c) 2016, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the "hack" directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *)
+
+module SyntaxTree = Full_fidelity_syntax_tree
+
+(**
+ * Transforms a full-fidelity syntax tree to generate the required constructs
+ * for coroutines. The resulting full-fidelity syntax tree should contain the
+ * required state machines for coroutine functions, coroutine functions
+ * themselves should be rewritten into stubs that wrap calls into the coroutine
+ * state machines, and so forth.
+ *
+ * The coroutine architecture used by Hack is inspired by the implementation in
+ * Kotlin. You can read more about Kotlin's design and implementation details at
+ * https://github.com/Kotlin/kotlin-coroutines/blob/master/kotlin-coroutines-informal.md.
+ *)
+val lower_coroutines : SyntaxTree.t -> SyntaxTree.t

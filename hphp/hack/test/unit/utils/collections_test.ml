0ac23f8c5657bde52750be1434471c7f5ac9cdab
Nat Mote <nmote@fb.com>
2017-04-24 15:41:17 -0700

    Add combine parameter to MyMap.union
    Summary:
    This brings the interface more in line with the `union` function introduced in
    OCaml 4.03.0, without forcing me to change all callsites to provide this
    argument.
    
    Reviewed By: gabelevi
    
    Differential Revision: D4926155
    
    fbshipit-source-id: 97ed3d08d4c6d28e46233d3237d1b8cae5ed34f3

--- hphp/hack/test/unit/utils/collections_test.ml
+++ hphp/hack/test/unit/utils/collections_test.ml
@@ -10,9 +10,21 @@ let map_of_elements = List.fold_left
 let test_myMap_union () =
   let map1 = map_of_elements [(1, 2); (3, 4)] in
   let map2 = map_of_elements [(1, 10); (5, 6)] in
-  let union = IntMap.union map1 map2 in
-  let expected = map_of_elements [(1, 2); (3, 4); (5, 6)] in
-  if not (IntMap.equal union expected) then failwith "Maps not equal";
+  let () =
+    let union = IntMap.union map1 map2 in
+    let expected = map_of_elements [(1, 2); (3, 4); (5, 6)] in
+    if not (IntMap.equal union expected) then failwith "Maps not equal"
+  in
+  let () =
+    let union = IntMap.union ~combine:(fun _ _ snd -> Some snd) map1 map2 in
+    let expected = map_of_elements [(1, 10); (3, 4); (5, 6)] in
+    if not (IntMap.equal union expected) then failwith "Maps not equal"
+  in
+  let () =
+    let union = IntMap.union ~combine:(fun _ _ _ -> None) map1 map2 in
+    let expected = map_of_elements [(3, 4); (5, 6)] in
+    if not (IntMap.equal union expected) then failwith "Maps not equal"
+  in
   true
 
 let tests = [

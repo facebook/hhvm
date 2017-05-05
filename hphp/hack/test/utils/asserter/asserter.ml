29ddaa741e443e6506f72d8d623777f65030b18c
Alexander Chow <achow@fb.com>
2017-05-04 21:40:12 -0700

    A Mock watchman module that is injectable
    Summary:
    The Informant is difficult to test. I want to avoid making it a functor
    (and proliferating functorization throughout the entire codebase which
    will get us to "constructor hell"). Which is what the injector is
    there to solve. Here I make an injected
    Watchman module that is Mocked. See in-line comments for more info.
    
    Differential Revision: D4933141
    
    fbshipit-source-id: 6c75dc6e409970ac2a67a17c3cc3cec4c7d812bd

--- hphp/hack/test/utils/asserter/asserter.ml
+++ hphp/hack/test/utils/asserter/asserter.ml
@@ -27,7 +27,18 @@ module Bool_comparator = struct
     | false, false -> true
     | true, false -> false
     | false, true -> false
-end
+end;;
+
+
+module Hh_json_json_comparator = struct
+  type t = Hh_json.json
+  let to_string v = Hh_json.json_to_string v
+  let is_equal exp actual =
+    (** Shortcut by comparing the canonical string representation. *)
+    let exp = Hh_json.json_to_string exp in
+    let actual = Hh_json.json_to_string actual in
+    String.equal exp actual
+end;;
 
 
 module Recorder_event_comparator = struct
@@ -100,7 +111,34 @@ module Pattern_comparator(Substitutions : Pattern_substitutions) = struct
 end;;
 
 
+(** Useful for abstracting on option types. *)
+module Make_option_comparator (Comp : Comparator)
+  : Comparator with type t = (Comp.t option) = struct
+    type t = Comp.t option
+
+    let to_string v =
+      match v with
+      | None ->
+        "None"
+      | Some v ->
+        Comp.to_string v
+
+    let is_equal exp actual =
+      match exp, actual with
+      | None, None ->
+        true
+      | None, Some _
+      | Some _, None ->
+        false
+      | Some exp, Some actual ->
+        Comp.is_equal exp actual
+
+end;;
+
+
 module Make_asserter (Comp : Comparator) = struct
+
+  module Comp_option = Make_option_comparator(Comp)
 
   let assert_equals exp actual failure_msg =
     if Comp.is_equal exp actual then
@@ -127,11 +165,30 @@ module Make_asserter (Comp : Comparator) = struct
       let () = Printf.eprintf "%s" failure_msg in
       assert false
 
+  let assert_option_equals exp actual failure_msg =
+    match exp, actual with
+    | None, None ->
+      ()
+    | None, Some v ->
+      Printf.eprintf
+        "assert_option_equals failed. Expected None but got Some(%s)"
+        (Comp.to_string v)
+    | Some v, None ->
+      Printf.eprintf
+        "assert_option_equals failed. Expected Some(%s) but got None"
+        (Comp.to_string v)
+    | Some exp, Some actual ->
+      assert_equals exp actual failure_msg
+
 end;;
 
 
+module Hh_json_json_option_comparator =
+  Make_option_comparator (Hh_json_json_comparator);;
+
 module String_asserter = Make_asserter (String_comparator);;
 module Bool_asserter = Make_asserter (Bool_comparator);;
+module Hh_json_json_asserter = Make_asserter (Hh_json_json_comparator);;
 module Int_asserter = Make_asserter (Int_comparator);;
 module Process_status_asserter = Make_asserter (Process_status_comparator);;
 module Recorder_event_asserter = Make_asserter (Recorder_event_comparator);;
---
+++ hphp/hack/test/watchman/watchman_mock_test.ml
@@ -0,0 +1,91 @@
+module Watchman_changes_comparator = struct
+  type t = Watchman.changes
+  let to_string changes =
+    let open Watchman in
+    match changes with
+    | Watchman_unavailable ->
+      "Watchman_unavailable"
+    | Watchman_pushed (State_enter (name, json)) ->
+      Printf.sprintf "State_enter(%s, %s)"
+        name
+        (Asserter.Hh_json_json_option_comparator.to_string json)
+    | Watchman_pushed (State_leave (name, json)) ->
+      Printf.sprintf "State_leave(%s, %s)"
+        name
+        (Asserter.Hh_json_json_option_comparator.to_string json)
+    | Watchman_pushed (Files_changed s) ->
+      Printf.sprintf
+        "Watchman_push files [%s]"
+        (String.concat ", " @@ SSet.elements s)
+    | Watchman_synchronous s ->
+      Printf.sprintf
+        "Watchman_synchronous [%s]"
+        (String.concat ", " @@ SSet.elements s)
+
+    let pushed_is_equal exp actual =
+      let open Watchman in
+      match exp, actual with
+        | Files_changed exp, Files_changed actual ->
+          SSet.equal exp actual
+        | State_enter (state_exp, json_exp),
+          State_enter (state_actual, json_actual)
+        | State_leave (state_exp, json_exp),
+          State_leave (state_actual, json_actual) ->
+            let json_equals = Asserter.Hh_json_json_option_comparator.is_equal
+              json_exp json_actual in
+            (String.equal state_exp state_actual) &&
+            (json_equals)
+        | _, _ ->
+          false
+
+    let is_equal exp actual =
+      let open Watchman in
+      match exp, actual with
+      | Watchman_unavailable, Watchman_unavailable ->
+        true
+      | Watchman_pushed exp, Watchman_pushed actual ->
+        pushed_is_equal exp actual
+      | Watchman_synchronous exp, Watchman_synchronous actual ->
+        SSet.equal exp actual
+      | _ ->
+        false
+
+end;;
+
+
+module Watchman_changes_asserter =
+  Asserter.Make_asserter (Watchman_changes_comparator);;
+
+
+(**
+ * Tests for Watchman.Mocking module.
+ *
+ * Tests require Watchman_Mock to be injected.
+ *)
+
+let test_mock_basic () =
+  Watchman.Mocking.init_returns None;
+  let result = Watchman.init Watchman.Testing.test_settings in
+  let result = Option.map result ~f:Watchman.Mocking.print_env in
+  Asserter.String_asserter.assert_option_equals None result "init_returns";
+  Watchman.Mocking.init_returns (Some "hello");
+  let result = Watchman.init Watchman.Testing.test_settings in
+  let result = Option.map result ~f:Watchman.Mocking.print_env in
+  Asserter.String_asserter.assert_option_equals
+    (Some "hello") result "init_returns";
+  let expected_changes = Watchman.Watchman_synchronous
+    (SSet.of_list ["some_file.php"]) in
+  Watchman.Mocking.get_changes_returns expected_changes;
+  let _, actual_changes = Watchman.get_changes
+    (Watchman.Watchman_alive Watchman.Testing.test_env) in
+  Watchman_changes_asserter.assert_equals
+    expected_changes actual_changes "get_changes_returns";
+  true
+
+let tests =
+  [
+    "test_mock_basic", test_mock_basic;
+  ]
+
+let () =
+  Unit_test.run_all tests

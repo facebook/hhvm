caa8d779f10b6e630dfc97c705843a732f02efd5
Oguz Ulgen <oulgen@fb.com>
2017-05-15 19:28:42 -0700

    Clean up the entry point to semdiff
    Summary: Clean up the entry point to semdiff
    
    Reviewed By: hubyrod
    
    Differential Revision: D5064789
    
    fbshipit-source-id: ba5dd36cc4305aa1f9149b816f442a5f9ebc1304

--- hphp/hack/src/hhbc/semdiff/semdiff.ml
+++ hphp/hack/src/hhbc/semdiff/semdiff.ml
@@ -10,44 +10,65 @@
 open Hhas_parser
 open Diff
 
-let program_parser = program Hhas_lexer.read
-let _ = print_endline "Hhas differencing tool"
-let _ = if Array.length Sys.argv != 3 then
-        (print_endline "Usage: semdiff file1 file2"; exit 1)
-        else ()
+type options = {
+  files : string * string;
+}
 
-let channel1 = open_in Sys.argv.(1) (* TODO: error handling *)
-let channel2 = open_in Sys.argv.(2)
+let die str =
+  let oc = stderr in
+  output_string oc str;
+  close_out oc;
+  exit 2
 
-let lexer1 = Lexing.from_channel channel1
-let (data_decls1, prog1) = try program_parser lexer1
-            with Parsing.Parse_error -> (
-              print_endline "Parsing of file1 failed";
-              raise Parsing.Parse_error
-              )
-let _ = close_in channel1
+let parse_options () =
+  let purpose = "Hhas differencing tool" in
+  let usage =
+    Printf.sprintf "%s\nUsage: %s file1 file2\n" purpose Sys.argv.(0)
+  in
+  if Array.length Sys.argv <> 3
+  then die usage
+  else {files = (Sys.argv.(1), Sys.argv.(2));}
 
-(* copy paste... *)
-let lexer2 = Lexing.from_channel channel2
-let (data_decls2, prog2) = try program_parser lexer2
-            with Parsing.Parse_error -> (
-              print_endline "Parsing of file2 failed";
-              raise Parsing.Parse_error
-              )
-let _ = close_in channel2
+let parse_file program_parser filename =
+  let channel = open_in filename in (* TODO: error handling *)
 
-(* This is an absolutely foul, and hopefully temporary, hack to get around
-the fact that hhbc_hhas now uses a global ref to generate adata indices on the
-fly, by looking up the data. That means you can't have two programs around
-at once and print their instructions correctly *)
-let _ = Diff.data_decls_ref1 := data_decls1
+  let lexer = Lexing.from_channel channel in
+  let (data_decls, prog) =
+    try program_parser lexer
+      with Parsing.Parse_error -> (
+        print_endline "Parsing of file failed";
+        raise Parsing.Parse_error
+        )
+  in
+  close_in channel;
+  (data_decls, prog)
 
-let _ = Diff.data_decls_ref2 := data_decls2
+let run options =
+  let program_parser = program Hhas_lexer.read in
+  let (data_decls1, prog1) = parse_file program_parser (fst options.files) in
+  let (data_decls2, prog2) = parse_file program_parser (snd options.files) in
 
-let foo () =
- let (d,(s,e)) = program_comparer.comparer prog1 prog2 in
+  (* This is an absolutely foul, and hopefully temporary, hack to get around
+  the fact that hhbc_hhas now uses a global ref to generate adata indices on the
+  fly, by looking up the data. That means you can't have two programs around
+  at once and print their instructions correctly *)
+  Diff.data_decls_ref1 := data_decls1;
+  Diff.data_decls_ref2 := data_decls2;
+
+  let (d,(s,e)) = program_comparer.comparer prog1 prog2 in
   (Printf.printf "distance = %d\nsize = %d\nsimilarity =%.2f%%\nedits=\n%s"
-          d s (100.0 *. (1.0 -. float_of_int d /. float_of_int (s+1))) e;
-   print_endline defaultstring) (* make sure the colours are back to normal *)
+         d s (100.0 *. (1.0 -. float_of_int d /. float_of_int (s+1))) e;
+  print_endline defaultstring) (* make sure the colors are back to normal *)
 
-let _ = foo ()
+(* command line driver *)
+let _ =
+  if ! Sys.interactive
+  then ()
+  else
+    (* On windows, setting 'binary mode' avoids to output CRLF on
+       stdout.  The 'text mode' would not hurt the user in general, but
+       it breaks the testsuite where the output is compared to the
+       expected one (i.e. in given file without CRLF). *)
+    set_binary_mode_out stdout true;
+    let options = parse_options () in
+    Unix.handle_unix_error run options

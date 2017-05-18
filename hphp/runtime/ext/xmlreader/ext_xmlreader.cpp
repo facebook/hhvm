facebook/hhvm

commit 9d2be6c30b5b6dadf414692d0e7fbab5f9105b5f

FIX: segfault in bzip2 when running ::sweep followed by destructor
Summary:
-fix bzip's closeImpl() for cases when run when file is already closed
-fix XMLReader's close() pointer handling during sweep
-add tests to confirm absence of segfault

Closes #7075
Closes https://github.com/facebook/hhvm/pull/7114

Reviewed By: Orvid

Differential Revision: D3366389

fbshipit-source-id: e39ce7f4a627a75fec51d604f8e55e6314e8963b

hphp/runtime/ext/xmlreader/ext_xmlreader.cpp
@@ -85,11 +85,14 @@ XMLReader::~XMLReader() {
   close();
 }
 
+void XMLReader::sweep() {
+  close();
+}
+
 void XMLReader::close() {
   SYNC_VM_REGS_SCOPED();
   if (m_stream) {
     m_stream->close();
-    m_stream = nullptr;
   }
   if (m_ptr) {
     xmlFreeTextReader(m_ptr);

hphp/runtime/ext/bz2/bz2-file.cpp
@@ -32,14 +32,11 @@ BZ2File::BZ2File(req::ptr<PlainFile>&& innerFile)
 }
 
 BZ2File::~BZ2File() {
-  if (m_bzFile)
-    closeImpl();
+  closeImpl();
 }
 
 void BZ2File::sweep() {
-  if (m_bzFile) {
-    closeImpl();
-  }
+  closeImpl();
   File::sweep();
 }
 
@@ -107,15 +104,18 @@ int64_t BZ2File::writeImpl(const char * buf, int64_t length) {
 }
 
 bool BZ2File::closeImpl() {
-  assert(m_bzFile);
-  bool ret = true;
-  BZ2_bzclose(m_bzFile);
-  m_bzFile = nullptr;
-  setIsClosed(true);
-  m_innerFile->close();
+  if (!isClosed()) {
+    if (m_bzFile) {
+      BZ2_bzclose(m_bzFile);
+      m_bzFile = nullptr;
+    }
+    setIsClosed(true);
+    if (m_innerFile) {
+      m_innerFile->close();
+    }
+  }
   File::closeImpl();
-  setEof(false);
-  return ret;
+  return true;
 }
 
 bool BZ2File::eof() {

hphp/runtime/ext/xmlreader/ext_xmlreader.h
@@ -36,6 +36,7 @@ namespace HPHP {
 struct XMLReader {
   XMLReader();
   ~XMLReader();
+  void sweep();
   void close();
   String read_string_func(xmlreader_read_char_t internal_function);
   bool bool_func_no_arg(xmlreader_read_int_t internal_function);

hphp/test/slow/ext_bzip2/ext_zip2_close.php
@@ -1,0 +1,7 @@
+<?php
+
+ini_set( 'display_errors', 'stderr' );
+$reader = new XMLReader();
+$url = "compress.bzip2://".dirname(__FILE__)."/book.xml.bz2";
+var_dump($reader->open( $url, null ));
+var_dump($reader->read());

hphp/test/slow/ext_bzip2/ext_zip2_close.php.expect
@@ -1,0 +1,2 @@
+bool(true)
+bool(true)

commit 296c14df8ce3961404714f720c427fa941254a9a

Fix regression in XMLReader
9d2be6c30b5b6dadf414692d0e7fbab5f9105b5f introduced a regression by
removing the line to reset m_stream.

This results in segfaults when running the mediawiki test suite (or
mediawiki in general): https://github.com/facebook/hhvm/issues/7779

This patch has been running on Mediawiki servers and is confirmed to
fix this regression.

hphp/runtime/ext/xmlreader/ext_xmlreader.cpp
@@ -93,6 +93,7 @@ void XMLReader::close() {
   SYNC_VM_REGS_SCOPED();
   if (m_stream) {
     m_stream->close();
+    m_stream = nullptr;
   }
   if (m_ptr) {
     xmlFreeTextReader(m_ptr);
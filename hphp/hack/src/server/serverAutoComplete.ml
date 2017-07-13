1e3a2c6f84e9cbd63b515a9679da663eecd5c6b6
Phillip Shih <philshih@fb.com>
2017-07-13 13:52:03 -0700

    Adding types to function arguments in serverAutoComplete.ml
    Summary: It took me a while to figure out exactly what tcopt was (command + click wasn't working properly/is really slow), so adding these in to save other people some time.
    
    Reviewed By: dabek
    
    Differential Revision: D5408167
    
    fbshipit-source-id: f41ad3bc60e676d2708d7cb1a43aab9e7754767a

--- hphp/hack/src/server/serverAutoComplete.ml
+++ hphp/hack/src/server/serverAutoComplete.ml
@@ -13,13 +13,15 @@
 (* Code for auto-completion *)
 (*****************************************************************************)
 
-let get_results tcopt _ file_info =
+let get_results (tcopt:TypecheckerOptions.t) _ (file_info:FileInfo.t)
+  : AutocompleteService.complete_autocomplete_result list =
   let {
     FileInfo.n_funs = content_funs; n_classes = content_classes; _
   } = FileInfo.simplify file_info in
   AutocompleteService.get_results tcopt content_funs content_classes
 
-let auto_complete tcopt content =
+let auto_complete (tcopt:TypecheckerOptions.t) (content:string)
+  : AutocompleteService.complete_autocomplete_result list =
   AutocompleteService.attach_hooks();
   let result =
     ServerIdeUtils.declare_and_check content ~f:(get_results tcopt) tcopt in

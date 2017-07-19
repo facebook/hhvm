8c7c12a697114e8f47a2f93b8de206208c0a348d
Elena Polozova <elenapolozova@fb.com>
2017-07-19 15:14:42 -0700

    Improving internal error message to streamline debugging
    Summary: I extended these functions a bit while debugging an error, and their augmented functionality really helped me, so I think they'd be good to keep around.
    
    Differential Revision: D5445762
    
    fbshipit-source-id: f286facd3fa623e29927015d1a5d2c39cc94f32c

--- hphp/hack/src/parser/full_fidelity_parser_context.ml
+++ hphp/hack/src/parser/full_fidelity_parser_context.ml
@@ -25,6 +25,9 @@ module Scope = struct
     type t = TokenKind.t
     let compare = Pervasives.compare
     end)
+  let to_string scope =
+    let append elt acc = acc ^ "'" ^ (TokenKind.to_string elt) ^ "', " in
+    fold append scope ">> "
 end
 
 type t = Scope.t list
@@ -54,8 +57,16 @@ let pop_scope context token_kind_list =
   | current :: others
     when Scope.equal current (Scope.of_list token_kind_list) -> others
   | [ ] when Core.List.is_empty token_kind_list -> [ ]
-  | _ -> failwith ("Error: attempted to pop a list of token kinds that " ^
-    "wasn't on top of the context.")
+  (* Failure conditions *)
+  | current :: others ->
+    let failure_str = Printf.sprintf
+    ("Error: attempted to pop a list of token kinds that wasn't on top of " ^^
+    "the context. Tried to pop: %sbut the first scope on the stack was " ^^
+    "actually: %s") (Scope.to_string (Scope.of_list token_kind_list))
+    (Scope.to_string current) in
+    failwith (failure_str)
+  | [ ] -> failwith ("Error: tried to pop a list of token kinds off of an " ^
+    "empty context.")
 
 let print_expected context =
   Printf.printf "There are %d scopes on the stack. %!" (Core.List.length context);

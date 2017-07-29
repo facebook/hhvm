07f5607dc323bd0354818ff97a6879af2b7ebacf
Haoyang Qian <haoyangqian@fb.com>
2017-07-29 11:21:55 -0700

    add type check for parameter default value
    Summary: add type check for parameter default value type and the provided hint type, pass some tests which failed before.
    
    Reviewed By: paulbiss
    
    Differential Revision: D5467508
    
    fbshipit-source-id: f344e4e5c222609dd29bc5a97be753d880565d3f

--- hphp/hack/src/hhbc/emit_param.ml
+++ hphp/hack/src/hhbc/emit_param.ml
@@ -41,6 +41,93 @@ object(_)
   method on_Markup _ parent _ _ = parent
 end
 
+let get_hint_display_name hint =
+  Option.map hint (fun h ->
+    match h with
+    |  "HH\\bool"                ->   "bool"
+    |  "array"                   ->   "array"
+    |  "HH\\varray"              ->   "HH\\varray"
+    |  "HH\\darray"              ->   "HH\\darray"
+    |  "HH\\varray_or_darray"    ->   "HH\\varray_or_darray"
+    |  "HH\\int"                 ->   "int"
+    |  "HH\\num"                 ->   "num"
+    |  "HH\\arraykey"            ->   "arraykey"
+    |  "HH\\float"               ->   "float"
+    |  "HH\\string"              ->   "string"
+    |   _                        ->   "class" )
+
+
+(* By now only check default type for bool, array, int, float and string.
+   Return None when hint_type and default_value matches (in hh mode,
+   "class" type matches anything). If not, return default_value type string
+   for printing fatal parse error *)
+let match_default_and_hint is_hh_file hint_type param_expr =
+  let accept_any = match hint_type with
+                  | "class" -> is_hh_file
+                  | _       -> false
+  in
+  if accept_any
+  then None
+  else match param_expr with
+      | Some (_, (A.True | A.False)) ->
+        begin match hint_type with
+               | "bool"   -> None
+               | _        -> Some "Boolean"
+        end
+      | Some (_, A.Array _ ) ->
+        begin match hint_type with
+             | "array"
+             | "HH\\varray"
+             | "HH\\darray"
+             | "HH\\varray_or_darray" -> None
+             | _        -> Some "Array"
+        end
+      | Some (_, A.Int _ )  ->
+        begin match hint_type with
+             | "int"
+             | "num"
+             | "arraykey"
+             | "float"  -> None
+             | _        -> Some "Int64"
+        end
+      | Some (_, A.Float _ ) ->
+        begin match hint_type with
+             | "float"
+             | "num"    -> None
+             | _        -> Some "Double"
+        end
+      | Some (_, A.String _ ) ->
+        begin match hint_type with
+             | "string"
+             | "arraykey" -> None
+             | _          -> Some "String"
+        end
+
+      | _  -> None
+
+(* Return None if it passes type check, otherwise return error msg *)
+let default_type_check param_name param_type_info param_expr =
+  let hint =
+    Option.bind param_type_info Hhas_type_info.user_type
+  in
+  let hint_type = get_hint_display_name hint in
+  (* If matches, return None, otherwise return default_type *)
+  let default_type = Option.bind hint_type (function hint_type ->
+      match_default_and_hint
+      (Emit_env.is_hh_file () || Hhbc_options.enable_hiphop_syntax !Hhbc_options.compiler_options)
+      hint_type param_expr)
+  in
+  let param_true_name = Hhbc_string_utils.Locals.strip_dollar param_name
+  in
+    Option.bind default_type (function t ->
+      match hint_type with
+      |  Some "class" ->  Some ("Default value for parameter "
+      ^ param_true_name ^ " with a class type hint can only be NULL")
+      |  Some h       ->  Some ("Default value for parameter "
+      ^ param_true_name ^ " with type " ^ t ^
+      " needs to have the same type as the type hint " ^ h)
+      |  _            ->  None )
+
 let from_ast ~tparams ~namespace ~generate_defaults ~scope p =
   let param_name = snd p.A.param_id in
   let param_is_variadic = p.Ast.param_is_variadic in
@@ -57,6 +144,13 @@ let from_ast ~tparams ~namespace ~generate_defaults ~scope p =
   let param_type_info = Option.map param_hint
     Emit_type_hint.(hint_to_type_info
       ~kind:Param ~skipawaitable:false ~nullable ~namespace ~tparams) in
+  (* Do the type check for default value type and hint type *)
+  let _ =
+    if not nullable then
+    match (default_type_check param_name param_type_info p.A.param_expr) with
+    | None    -> ()
+    | Some s  -> Emit_fatal.raise_fatal_parse s
+  in
   let param_expr =
     Option.map p.Ast.param_expr ~f:(resolver_visitor#on_expr scope)
   in

c4029b549fade5a2dc5276f133861042bcd6f2a2
Varun Ramesh <varunramesh@fb.com>
2017-08-22 13:34:53 -0700

    Don't throw away fall-through cases in switch statements (Fixed)
    Reviewed By: michaeltingley
    
    Differential Revision: D5674117
    
    fbshipit-source-id: 06b97f79ffa173091e30996da0c99fa30831ac9a

---
+++ hphp/hack/test/tast/switch_fallthrough.php
@@ -0,0 +1,38 @@
+<?hh // strict
+
+function test(): void {
+  // $x is an int
+  $x = 0;
+  hh_show($x);
+  switch ($x) {
+    case 0:
+      // This is a noop fallthrough so we should not do an intersect
+    case 1:
+      // $x should be int, that isn't wrapped in an unresolved
+      hh_show($x);
+      // $x is a string
+      $x = '';
+      hh_show($x);
+      // FALLTHROUGH
+    case 3:
+      // $x should be string & int
+      hh_show($x);
+      $x = true;
+      // $x should be string & int & bool
+      hh_show($x);
+      // FALLTHROUGH
+    case 4:
+      // $x should be int & string
+      hh_show($x);
+      break;
+    case 4:
+      // $x should be int
+      hh_show($x);
+      // $x should be float
+      $x = 1.0;
+      hh_show($x);
+      break;
+  }
+  // $x should be int & string & float & bool
+  hh_show($x);
+}
---
+++ hphp/hack/test/typecheck/case_null.php
@@ -0,0 +1,25 @@
+<?hh // strict
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+enum Enum : int {
+  A = 0;
+  B = 1;
+  C = 2;
+  D = 3;
+}
+
+function f(Enum $e): int {
+  $x = null;
+  switch ($e) {
+    case Enum::A:
+      $x = 1;
+      break;
+    case Enum::B:
+    case Enum::C:
+      $x = 2;
+      break;
+    case Enum::D:
+      return 3;
+  }
+  return $x;
+}

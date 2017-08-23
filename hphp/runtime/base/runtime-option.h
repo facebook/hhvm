d79b9672604c465879df5ab6373fc79246eabc99
Guilherme Ottoni <ottoni@fb.com>
2017-08-22 19:47:55 -0700

    Tune inlining cost
    Reviewed By: paulbiss
    
    Differential Revision: D5679641
    
    fbshipit-source-id: 5192b550805790e8ee77f249bd93a2834f58a07d

--- hphp/runtime/base/runtime-option.h
+++ hphp/runtime/base/runtime-option.h
@@ -578,7 +578,7 @@ struct RuntimeOption {
   F(bool, HHIRGenOpts,                 true)                            \
   F(bool, HHIRRefcountOpts,            true)                            \
   F(bool, HHIREnableGenTimeInlining,   true)                            \
-  F(uint32_t, HHIRInliningMaxVasmCost, 370)                             \
+  F(uint32_t, HHIRInliningMaxVasmCost, 175)                             \
   F(uint32_t, HHIRInliningMaxReturnDecRefs, 12)                         \
   F(uint32_t, HHIRInliningMaxReturnLocals, 20)                          \
   F(bool, HHIRInlineFrameOpts,         true)                            \

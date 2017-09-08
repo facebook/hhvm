d84b3e0e037dedbd3ef8a544accc90a9d67af527
Varun Ramesh <varunramesh@fb.com>
2017-09-08 00:53:36 -0700

    Make Typing_check_utils return a TAST
    Summary: This makes `Typing_check_utils` return a TAST, which is an intermediate step in making the TAST available during hh_server type-checking.
    
    Reviewed By: jamesjwu
    
    Differential Revision: D5719341
    
    fbshipit-source-id: c14d46c0d0e923673ae9bea1943d70cbfa079cfb

--- hphp/hack/src/typing/typing_check_utils.ml
+++ hphp/hack/src/typing/typing_check_utils.ml
@@ -10,30 +10,30 @@
 
 open Core
 
+let check_defs_tast tcopt fn {FileInfo.funs; classes; typedefs; consts; _} =
+  let open Option in
+  let result, tast, _ = (Errors.do_ (fun () ->
+    let fs = List.map funs begin fun (_, x) ->
+      Typing_check_service.type_fun tcopt fn x
+      >>| (fun (f, env) -> Tast.Fun f, env)
+    end in
+    let cs = List.map classes begin fun (_, x) ->
+      Typing_check_service.type_class tcopt fn x
+      >>| (fun (c, env) -> Tast.Class c, env)
+    end in
+    let ts = List.map typedefs begin fun (_, x) ->
+      Typing_check_service.check_typedef tcopt fn x
+      >>| (fun (td, env) -> Tast.Typedef td, env)
+    end in
+    let gcs = List.map consts begin fun (_, x) ->
+      Typing_check_service.check_const tcopt fn x
+      >>| (fun (c, env) -> Tast.Constant c, env)
+    end in
+    (fs @ cs @ ts @ gcs)
+  )) in
+  result, tast
+
 (*****************************************************************************)
 (* Usually used when we want to run typing hooks *)
 (*****************************************************************************)
-let check_defs tcopt fn {FileInfo.funs; classes; typedefs; consts; _} =
-  let result, _, _ = (Errors.do_ (fun () ->
-    ignore(
-      List.map funs begin fun (_, x) ->
-        Typing_check_service.type_fun tcopt fn x
-      end
-    );
-    ignore(
-      List.map classes begin fun (_, x) ->
-        Typing_check_service.type_class tcopt fn x;
-      end
-    );
-    ignore(
-      List.map typedefs begin fun (_, x) ->
-        Typing_check_service.check_typedef tcopt fn x
-      end
-    );
-    ignore(
-      List.map consts begin fun (_, x) ->
-        Typing_check_service.check_const tcopt fn x
-      end
-    );
-  )) in
-  result
+let check_defs tcopt fn fi = fst (check_defs_tast tcopt fn fi)

b8c9827c51ad08d4d6b324324b070b88eb6a749b
Lucian Wischik <ljw@fb.com>
2017-10-09 14:11:48 -0700

    some more tests for go-to-def
    Summary:
    These tests embody the current behavior of go-to-def for constructors.
    
    (The behavior should probably ultimately change, but I wanted to at least have the current behavior in test form).
    
    Differential Revision: D6004773
    
    fbshipit-source-id: 16623759982c00510c06d2479854d200d8501918

--- hphp/hack/test/integration/data/lsp_exchanges/definition.php
+++ hphp/hack/test/integration/data/lsp_exchanges/definition.php
@@ -7,3 +7,29 @@ function a_definition(): int {
 function b_definition(): int {
   return 42;
 }
+
+class BB {
+  public function __construct(int $i) {}
+}
+
+class CC extends BB {
+}
+
+class DD extends CC {
+}
+
+class EE extends DD {
+  public function __construct() {
+    parent::__construct(1);
+  }
+}
+
+class FF {}
+
+function test(): void {
+  $bb = new BB(1); // should go to B::__construct
+  $cc = new CC(1); // should offer choice B::__construct or C
+  $dd = new DD(1); // should offer choice B::__construct or D
+  $ee = new EE(); // should go to E::__construct
+  $ff = new FF(); // should go to F
+}

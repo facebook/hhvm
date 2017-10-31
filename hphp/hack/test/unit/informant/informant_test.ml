366d90ee75f72e3a5e830ef8719525b25f6fa5ae
Alexander Chow <achow@fb.com>
2017-10-31 16:04:22 -0700

    Refactor Informant test tools
    Summary:
    Refactor out Informant test tools so we can reuse in other unit tests.
    
    This also fixes the messed up revision numbering.
    
    Reviewed By: dabek
    
    Differential Revision: D6115088
    
    fbshipit-source-id: a54f8fa7eacb6892146b957b68ecc394def5040f

--- hphp/hack/test/unit/informant/informant_test.ml
+++ hphp/hack/test/unit/informant/informant_test.ml
@@ -26,47 +26,7 @@ module WEWClient = WatchmanEventWatcherClient
 module WEWConfig = WatchmanEventWatcherConfig
 
 module Tools = struct
-  let hg_rev_1 = "abc"
-  let hg_rev_2 = "def"
-  let hg_rev_3 = "ghi"
-  let hg_rev_4 = "jkl"
-  let svn_1 = "1"
-  let svn_2 = "5"
-  let svn_3 = "200" (** is a significant distance from the above. *)
-  let svn_4 = "230"
-
-  type state_transition =
-    | State_leave
-    | State_enter
-    | Changed_merge_base
-
-  let set_hg_to_svn_map ?delay_rev_3 () =
-    Hg.Mocking.closest_svn_ancestor_bind_value hg_rev_1
-      @@ Future.of_value svn_1;
-    Hg.Mocking.closest_svn_ancestor_bind_value hg_rev_2
-      @@ Future.of_value svn_2;
-    Hg.Mocking.closest_svn_ancestor_bind_value hg_rev_3
-      begin match delay_rev_3 with
-        | None -> Future.of_value svn_3
-        | Some i -> Future.delayed_value ~delays:i svn_3
-      end;
-    Hg.Mocking.closest_svn_ancestor_bind_value hg_rev_4
-      @@ Future.of_value svn_4
-
-  let set_next_watchman_state_transition move hg_rev =
-    let open Hh_json in
-    let json = JSON_Object
-      [("rev", JSON_String hg_rev)] in
-    let move = match move with
-    | State_leave ->
-      Watchman.State_leave ("hg.update", (Some json))
-    | State_enter ->
-      Watchman.State_enter ("hg.update", (Some json))
-    | Changed_merge_base ->
-      Watchman.Changed_merge_base (hg_rev, SSet.empty)
-    in
-    Watchman.Mocking.get_changes_returns
-      (Watchman.Watchman_pushed move)
+  include Informant_test_tools
 
   (** Test the given transition to an hg_rev and assert the expected report. *)
   let test_transition informant transition hg_rev server_status
@@ -77,41 +37,6 @@ module Tools = struct
         informant server_status in
       Report_asserter.assert_equals expected_report report
         assert_msg
-
-  (** Create an entry in XDB table for a saved state.
-   *
-   * stat_svn_rev: The revision on which this saved state was created.
-   * for_svn_rev: The svn_rev lookup in "Xdb.find_nearest" that will locate
-   *              this result.
-   *)
-  let set_xdb ~state_svn_rev ~for_svn_rev ~everstore_handle ~tiny  =
-    let hh_version = Build_id.build_revision in
-    let hg_hash = match state_svn_rev with
-      | 1 -> hg_rev_1
-      | 5 -> hg_rev_2
-      | 200 -> hg_rev_3
-      | 230 -> hg_rev_4
-      | _ ->
-        Printf.eprintf "Error: Invalid svn_rev number\n";
-        assert false
-    in
-    let hhconfig_hash, _config = Config_file.parse "/tmp/.hhconfig" in
-    match hhconfig_hash with
-    | None ->
-      Printf.eprintf "Error: Failed to get hash of config file. Cannot continue.\n";
-      assert false
-    | Some hhconfig_hash ->
-      let result = {
-        Xdb.svn_rev = state_svn_rev;
-        hg_hash;
-        everstore_handle;
-        hh_version;
-        hhconfig_hash;
-      } in
-      let result = Future.of_value [result] in
-      Xdb.Mocking.find_nearest_returns ~db:Xdb.hack_db_name
-        ~db_table:Xdb.mini_saved_states_table
-        ~svn_rev:for_svn_rev ~hh_version ~hhconfig_hash ~tiny result
 end;;
 
 
@@ -148,21 +73,21 @@ let test_informant_restarts_significant_move temp_dir =
 
   (** Move base revisions insignificant distance away. *)
   Tools.test_transition
-    informant Tools.State_enter Tools.hg_rev_2
+    informant Tools.State_enter Tools.hg_rev_5
     Informant_sig.Server_alive Informant_sig.Move_along
     "state enter insignificant distance";
   Tools.test_transition
-    informant Tools.State_leave Tools.hg_rev_2
+    informant Tools.State_leave Tools.hg_rev_5
     Informant_sig.Server_alive Informant_sig.Move_along
     "state leave insignificant distance";
 
   (** Move significant distance. *)
   Tools.test_transition
-    informant Tools.State_enter Tools.hg_rev_3
+    informant Tools.State_enter Tools.hg_rev_200
     Informant_sig.Server_alive Informant_sig.Move_along
     "state enter significant distance";
   Tools.test_transition
-    informant Tools.State_leave Tools.hg_rev_3
+    informant Tools.State_leave Tools.hg_rev_200
     Informant_sig.Server_alive Informant_sig.Move_along
     "state leave significant distance";
   let expected_state_target = {
@@ -170,46 +95,46 @@ let test_informant_restarts_significant_move temp_dir =
     target_svn_rev = 200;
   } in
   Tools.test_transition
-    informant Tools.Changed_merge_base Tools.hg_rev_3
+    informant Tools.Changed_merge_base Tools.hg_rev_200
     Informant_sig.Server_alive
     (Informant_sig.Restart_server (Some expected_state_target))
     "Move forward significant distance";
 
   (** Informant now sitting at revision 200. Moving to 230 no restart. *)
   Tools.test_transition
-    informant Tools.State_enter Tools.hg_rev_4
+    informant Tools.State_enter Tools.hg_rev_230
     Informant_sig.Server_alive Informant_sig.Move_along
     "state enter insignificant distance";
   Tools.test_transition
-    informant Tools.State_leave Tools.hg_rev_4
+    informant Tools.State_leave Tools.hg_rev_230
     Informant_sig.Server_alive Informant_sig.Move_along
     "state leave insignificant distance";
   Tools.test_transition
-    informant Tools.Changed_merge_base Tools.hg_rev_4
+    informant Tools.Changed_merge_base Tools.hg_rev_230
     Informant_sig.Server_alive Informant_sig.Move_along
     "Changed merge base insignificant distance";
 
   (** Moving back to 200 no restart. *)
   Tools.test_transition
-    informant Tools.State_enter Tools.hg_rev_3
+    informant Tools.State_enter Tools.hg_rev_200
     Informant_sig.Server_alive Informant_sig.Move_along
     "state enter insignificant distance";
   Tools.test_transition
-    informant Tools.State_leave Tools.hg_rev_3
+    informant Tools.State_leave Tools.hg_rev_200
     Informant_sig.Server_alive Informant_sig.Move_along
     "state leave insignificant distance";
   Tools.test_transition
-    informant Tools.Changed_merge_base Tools.hg_rev_3
+    informant Tools.Changed_merge_base Tools.hg_rev_200
     Informant_sig.Server_alive Informant_sig.Move_along
     "Changed_merge_base insignificant distance";
 
-  (** Moving back to SVN rev 5 (hg_rev_2) restarts. *)
+  (** Moving back to SVN rev 5 (hg_rev_5) restarts. *)
   Tools.test_transition
-    informant Tools.State_enter Tools.hg_rev_2
+    informant Tools.State_enter Tools.hg_rev_5
     Informant_sig.Server_alive Informant_sig.Move_along
     "state enter significant distance";
   Tools.test_transition
-    informant Tools.State_leave Tools.hg_rev_2
+    informant Tools.State_leave Tools.hg_rev_5
     Informant_sig.Server_alive Informant_sig.Move_along
     "state leave significant distance";
   let expected_state_target = {
@@ -217,7 +142,7 @@ let test_informant_restarts_significant_move temp_dir =
     target_svn_rev = 5;
   } in
   Tools.test_transition
-    informant Tools.Changed_merge_base Tools.hg_rev_2
+    informant Tools.Changed_merge_base Tools.hg_rev_5
     Informant_sig.Server_alive
     (Informant_sig.Restart_server (Some expected_state_target))
     "Move back significant distance";
@@ -232,7 +157,7 @@ let test_informant_restarts_significant_move_delayed temp_dir =
    * Informant.report call. This occurs because Revision_tracker.report
    * calls itself recursively if something arrived on the Watchman
    * subscription. *)
-  Tools.set_hg_to_svn_map ~delay_rev_3:8 ();
+  Tools.set_hg_to_svn_map ~delay_rev_200:8 ();
   Watchman.Mocking.init_returns @@ Some "test_mock_basic";
   Hg.Mocking.current_working_copy_base_rev_returns
     (Future.of_value Tools.svn_1);
@@ -254,18 +179,18 @@ let test_informant_restarts_significant_move_delayed temp_dir =
   (** Start with a significant Changed Merge Base to a revision that's mapped
    * SVN revision is delayed by a few cycles. *)
   Tools.test_transition
-    informant Tools.Changed_merge_base Tools.hg_rev_3
+    informant Tools.Changed_merge_base Tools.hg_rev_200
     Informant_sig.Server_alive Informant_sig.Move_along
     "Changed_merge_base significant distance, but delayed should not restart";
 
   (** Informant now sitting at revision 200, but informant doesn't know it yet
    * because of the delayed computation. we nudge it twice. *)
   Tools.test_transition
-    informant Tools.State_enter Tools.hg_rev_4
+    informant Tools.State_enter Tools.hg_rev_230
     Informant_sig.Server_alive Informant_sig.Move_along
     "Nudge delayed future with State_enter";
   Tools.test_transition
-    informant Tools.State_leave Tools.hg_rev_4
+    informant Tools.State_leave Tools.hg_rev_230
     Informant_sig.Server_alive Informant_sig.Move_along
     "Nudge delayed future with State_leave";
 
@@ -273,13 +198,13 @@ let test_informant_restarts_significant_move_delayed temp_dir =
    * the actual SVN rev for 200. So the Restart_server from the
    * Changed_merge_base above shows up now. *)
   Tools.test_transition
-    informant Tools.State_enter Tools.hg_rev_3
+    informant Tools.State_enter Tools.hg_rev_200
     Informant_sig.Server_alive (Informant_sig.Restart_server None)
     "Trigger last delayed value for prior Changed_merge_base svn rev mapping";
   true
 
 (** This test is similar to the above (but shorter) except the
- * we are going to svn_4 which has no entry in the XDB table,
+ * we are going to svn_230 which has no entry in the XDB table,
  * and thus we get no restart report.
  *)
 let test_informant_no_saved_state_no_restart temp_dir =
@@ -304,7 +229,7 @@ let test_informant_no_saved_state_no_restart temp_dir =
 
   (** There's no saved state for rev 4, so we don't restart. *)
   Tools.test_transition
-    informant Tools.Changed_merge_base Tools.hg_rev_4
+    informant Tools.Changed_merge_base Tools.hg_rev_230
     Informant_sig.Server_alive Informant_sig.Move_along
     "Significantly changed merge base, but no saved state for this rev";
     true
@@ -319,7 +244,7 @@ let test_informant_xdb_saved_state_too_far temp_dir =
   Tools.set_hg_to_svn_map ();
   Watchman.Mocking.init_returns @@ Some "test_mock_basic";
   Hg.Mocking.current_working_copy_base_rev_returns
-    (Future.of_value Tools.svn_2);
+    (Future.of_value Tools.svn_5);
   Watchman.Mocking.get_changes_returns
     (Watchman.Watchman_pushed (Watchman.Files_changed SSet.empty));
   let informant = HhMonitorInformant.init {
@@ -338,7 +263,7 @@ let test_informant_xdb_saved_state_too_far temp_dir =
   Tools.set_xdb ~state_svn_rev:1 ~for_svn_rev:200
     ~everstore_handle:"Fake everstore handle for svn rev 1" ~tiny:false;
   Tools.test_transition
-    informant Tools.Changed_merge_base Tools.hg_rev_3
+    informant Tools.Changed_merge_base Tools.hg_rev_200
     Informant_sig.Server_alive Informant_sig.Move_along
     "Significantly changed merge base, saved state for this rev is too distant";
     true
---
+++ hphp/hack/test/unit/informant/informant_test_tools.ml
@@ -0,0 +1,76 @@
+let hg_rev_1 = "abc"
+let hg_rev_5 = "def"
+let hg_rev_200 = "ghi"
+let hg_rev_230 = "jkl"
+let svn_1 = "1"
+let svn_5 = "5"
+let svn_200 = "200" (** is a significant distance from the above. *)
+let svn_230 = "230"
+
+type state_transition =
+  | State_leave
+  | State_enter
+  | Changed_merge_base
+
+let set_hg_to_svn_map ?delay_rev_200 () =
+  Hg.Mocking.closest_svn_ancestor_bind_value hg_rev_1
+    @@ Future.of_value svn_1;
+  Hg.Mocking.closest_svn_ancestor_bind_value hg_rev_5
+    @@ Future.of_value svn_5;
+  Hg.Mocking.closest_svn_ancestor_bind_value hg_rev_200
+    begin match delay_rev_200 with
+      | None -> Future.of_value svn_200
+      | Some i -> Future.delayed_value ~delays:i svn_200
+    end;
+  Hg.Mocking.closest_svn_ancestor_bind_value hg_rev_230
+    @@ Future.of_value svn_230
+
+let set_next_watchman_state_transition move hg_rev =
+  let open Hh_json in
+  let json = JSON_Object
+    [("rev", JSON_String hg_rev)] in
+  let move = match move with
+  | State_leave ->
+    Watchman.State_leave ("hg.update", (Some json))
+  | State_enter ->
+    Watchman.State_enter ("hg.update", (Some json))
+  | Changed_merge_base ->
+    Watchman.Changed_merge_base (hg_rev, SSet.empty)
+  in
+  Watchman.Mocking.get_changes_returns
+    (Watchman.Watchman_pushed move)
+
+(** Create an entry in XDB table for a saved state.
+ *
+ * stat_svn_rev: The revision on which this saved state was created.
+ * for_svn_rev: The svn_rev lookup in "Xdb.find_nearest" that will locate
+ *              this result.
+ *)
+let set_xdb ~state_svn_rev ~for_svn_rev ~everstore_handle ~tiny  =
+  let hh_version = Build_id.build_revision in
+  let hg_hash = match state_svn_rev with
+    | 1 -> hg_rev_1
+    | 5 -> hg_rev_5
+    | 200 -> hg_rev_200
+    | 230 -> hg_rev_230
+    | _ ->
+      Printf.eprintf "Error: Invalid svn_rev number\n";
+      assert false
+  in
+  let hhconfig_hash, _config = Config_file.parse "/tmp/.hhconfig" in
+  match hhconfig_hash with
+  | None ->
+    Printf.eprintf "Error: Failed to get hash of config file. Cannot continue.\n";
+    assert false
+  | Some hhconfig_hash ->
+    let result = {
+      Xdb.svn_rev = state_svn_rev;
+      hg_hash;
+      everstore_handle;
+      hh_version;
+      hhconfig_hash;
+    } in
+    let result = Future.of_value [result] in
+    Xdb.Mocking.find_nearest_returns ~db:Xdb.hack_db_name
+      ~db_table:Xdb.mini_saved_states_table
+      ~svn_rev:for_svn_rev ~hh_version ~hhconfig_hash ~tiny result

5c015262e124ad3eca3e71a21737adc792919ae4
Andrew Kennedy <akenn@fb.com>
2017-11-08 10:21:38 -0800

    Fix subtyping for undefined parent classes
    fixes facebook/hhvm#8049
    
    Summary:
    In non-strict files, it's possible to write
    ```
    class C extends B { }
    ```
    even if `B` does not exist. In strict files, subtyping used to "tolerate" missing parents when checking subtypes. For example, this used to be accepted:
    ```
    interface I { }
    function foo(C $x):I { return $x; }
    ```
    Let's now be more strict.
    
    Differential Revision: D6272684
    
    fbshipit-source-id: 435749a969dee715e839b2164f05a0cb25335100

---
+++ hphp/hack/test/typecheck/subtype_parent_undefined.php
@@ -0,0 +1,11 @@
+<?hh
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+interface I {}
+
+// Weirdly, if B is undefined, we used to get no error in foo
+class C extends B {}
+
+function foo(C $x): I {
+  return $x;
+}

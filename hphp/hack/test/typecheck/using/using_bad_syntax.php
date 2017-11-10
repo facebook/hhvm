39860b34d115b81396dd7c4c2212dd1293e7b7e9
Andrew Kennedy <akenn@fb.com>
2017-11-10 15:20:34 -0800

    Hack legacy parser: reject function-scoped using statement in block scope
    Summary:
    Reject use of function-scoped `using` inside a block e.g.
    ```
    function foo():void {
      if ($b) {
        using ($x = new Handle());
        // more statements
      }
    }
    
    Reviewed By: KendallHopkins
    
    Differential Revision: D6296643
    
    fbshipit-source-id: dc31a983755fa239d0bacd5b1a702a7213ebf09e

---
+++ hphp/hack/test/typecheck/using/using_bad_syntax.php
@@ -0,0 +1,17 @@
+<?hh // strict
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+class Handle implements IDisposable {
+  public function __dispose():void { }
+  public function foo():void { }
+}
+
+function testit(bool $b):void {
+  // This is legal
+  using ($x = new Handle());
+
+  // This is not
+  if ($b) {
+    using ($y = new Handle());
+  }
+}
---
+++ hphp/hack/test/typecheck/using/using_bad_syntax_3.php
@@ -0,0 +1,21 @@
+<?hh // strict
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+class Handle implements IDisposable {
+  public function __dispose():void { }
+  public function foo():void { }
+}
+class ScopeGuard implements IDisposable {
+  public function __construct((function():void) $f) { }
+  public function __dispose():void { }
+}
+
+function testit():void {
+  // This is legal
+  using ($x = new Handle());
+
+  using ($w = new Handle()) {
+    // This is not
+    using ($q = new ScopeGuard(() ==> {}));
+  }
+}

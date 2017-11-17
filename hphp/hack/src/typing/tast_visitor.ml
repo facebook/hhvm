b0e5e651cd4f23198bc45542260a03c5443d7132
Jake Bailey <jakebailey@fb.com>
2017-11-17 10:52:55 -0800

    Add TAST visitor
    Summary: The TAST contains EnvAnnotations which need to be handled whenever a visitor descends into an annotated node. Instead of duplicating this handling in every TAST visitor, we provide this base visitor class.
    
    Reviewed By: andrewjkennedy
    
    Differential Revision: D6313494
    
    fbshipit-source-id: 24bd11b5bdbff01eb85118b5f67738cd82e644e7

---
+++ hphp/hack/src/typing/tast_visitor.ml
@@ -0,0 +1,42 @@
+(**
+ * Copyright (c) 2017, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the "hack" directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ *)
+
+class virtual ['a] visitor = object (self)
+  inherit ['a] Tast.Visitor.visitor as super
+
+  val mutable saved_env = None;
+
+  method private saved_env =
+    match saved_env with
+    | Some saved_env -> saved_env
+    | None -> failwith "No containing node had an env annotation"
+
+  method private with_env env f =
+    let prev_env = saved_env in
+    saved_env <- Some env;
+    let result = f () in
+    saved_env <- prev_env;
+    result
+
+  method! on_fun_ acc x =
+    self#with_env x.Tast.f_annotation (fun () -> super#on_fun_ acc x)
+
+  method! on_class_ acc x =
+    self#with_env x.Tast.c_annotation (fun () -> super#on_class_ acc x)
+
+  method! on_method_ acc x =
+    self#with_env x.Tast.m_annotation (fun () -> super#on_method_ acc x)
+
+  method! on_typedef acc x =
+    self#with_env x.Tast.t_annotation (fun () -> super#on_typedef acc x)
+
+  method! on_gconst acc x =
+    self#with_env x.Tast.cst_annotation (fun () -> super#on_gconst acc x)
+end

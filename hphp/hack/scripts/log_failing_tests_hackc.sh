e5c922a8d132b90371fc5bf2c46af0f631315362
Hugo Venturini <hgo@fb.com>
2017-11-22 10:18:15 -0800

    change path to avoid failures on sandcastle"
    Summary:
    sandcastle fails when scheduling a job via chronos. Could be because of paths. Let's try and learn.
    
    (Note: this ignores all push blocking failures!)
    
    Reviewed By: oulgen
    
    Differential Revision: D6397565
    
    fbshipit-source-id: 22c508964726f74d57171d5c95be484dc156743c

--- hphp/hack/scripts/log_failing_tests_hackc.sh
+++ hphp/hack/scripts/log_failing_tests_hackc.sh
@@ -10,10 +10,10 @@ set -e
 #$ scribe_cat perfpipe_hackc_hhvm_tests "{\"int\": {\"time\": $(date +'%s')}}"
 
 TABLE_NAME="perfpipe_hackc_hhvm_tests"
-FAIL_COMPARE_QUICK=$(grep -c '^' ~/fbsource/fbcode/hphp/test/hhcodegen_failing_tests_quick)
-FAIL_COMPARE_SLOW=$(grep -c '^' ~/fbsource/fbcode/hphp/test/hhcodegen_failing_tests_slow)
-FAIL_RUN_QUICK=$(grep -c '^' ~/fbsource/fbcode/hphp/test/hackc_failing_tests_quick)
-FAIL_RUN_SLOW=$(grep -c '^' ~/fbsource/fbcode/hphp/test/hackc_failing_tests_slow)
+FAIL_COMPARE_QUICK=$(grep -c '^' hphp/test/hhcodegen_failing_tests_quick)
+FAIL_COMPARE_SLOW=$(grep -c '^' hphp/test/hhcodegen_failing_tests_slow)
+FAIL_RUN_QUICK=$(grep -c '^' hphp/test/hackc_failing_tests_quick)
+FAIL_RUN_SLOW=$(grep -c '^' hphp/test/hackc_failing_tests_slow)
 
 SAMPLE_COMPARE_QUICK="{ \"int\": { \"time\": $(date +'%s'), \"fail\": $FAIL_COMPARE_QUICK }, \"normal\": {\"type\": \"compare_quick\"}}"
 SAMPLE_COMPARE_SLOW="{ \"int\": { \"time\": $(date +'%s'), \"fail\": $FAIL_COMPARE_SLOW }, \"normal\": {\"type\": \"compare_slow\"}}"

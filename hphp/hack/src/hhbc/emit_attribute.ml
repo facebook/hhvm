2d11e9db30756dac0947329ea66e53dda0cadbc7
Andrew Kennedy <akenn@fb.com>
2017-11-22 03:46:36 -0800

    Position information in attribute error
    Summary: As it says on the tin
    
    Reviewed By: hubyrod
    
    Differential Revision: D6384794
    
    fbshipit-source-id: f8f466f8b5a15ac1d95355cbc3ebdd81bf46ece7

--- hphp/hack/src/hhbc/emit_attribute.ml
+++ hphp/hack/src/hhbc/emit_attribute.ml
@@ -11,31 +11,26 @@
 open Hh_core
 open Emit_expression
 
-let ast_attribute_name: A.user_attribute -> Litstr.id =
-  fun ast_attr ->
-  Litstr.to_string @@ snd ast_attr.Ast.ua_name
-
-let from_attribute_base namespace attribute_name arguments =
+let from_attribute_base namespace attribute_id arguments =
   try
     let attribute_arguments =
       Ast_constant_folder.literals_from_exprs_with_index namespace arguments
     in
-    Hhas_attribute.make attribute_name attribute_arguments
+    Hhas_attribute.make (snd attribute_id) attribute_arguments
   with Ast_constant_folder.UserDefinedConstant ->
-    Emit_fatal.raise_fatal_parse Pos.none
+    Emit_fatal.raise_fatal_parse (fst attribute_id)
       "User-defined constants are not allowed in user attribute expressions"
 
 let from_ast : Namespace_env.env -> A.user_attribute -> Hhas_attribute.t =
   fun namespace ast_attr ->
-  let attribute_name = ast_attribute_name ast_attr in
-  from_attribute_base namespace attribute_name ast_attr.A.ua_params
+  from_attribute_base namespace ast_attr.A.ua_name ast_attr.A.ua_params
 
 let from_asts namespace ast_attributes =
   List.map ast_attributes (from_ast namespace)
 
 let ast_is_memoize : A.user_attribute -> bool =
   fun ast_attr ->
-  "__Memoize" = (ast_attribute_name ast_attr)
+  snd ast_attr.A.ua_name = Naming_special_names.UserAttributes.uaMemoize
 
 let ast_any_is_memoize : A.user_attribute list -> bool =
   fun ast_attrs ->

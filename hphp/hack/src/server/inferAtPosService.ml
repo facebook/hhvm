225e055e732720c72acba88113c4262e258f9494
Jake Bailey <jakebailey@fb.com>
2017-12-06 14:39:16 -0800

    Remove dead code in InferAtPosService
    Summary: This code became dead in {D6313608}. This diff just cleans it up.
    
    Reviewed By: arxanas
    
    Differential Revision: D6501712
    
    fbshipit-source-id: 42f3ec36141226383ad29ec580edbaff243ba8fe

--- hphp/hack/src/server/inferAtPosService.ml
+++ hphp/hack/src/server/inferAtPosService.ml
@@ -19,26 +19,3 @@ let infer_result_to_ide_response typename =
     Infer_type_response { type_string = None; type_json = None }
   | Some (str, _json) ->
     Infer_type_response { type_string = Some str; type_json = None }
-
-(* Remember (when we care) the type found at a position *)
-let save_infer result_ty target_line target_column ty pos env =
-  if Pos.inside pos target_line target_column && !result_ty = None
-  then begin
-    let ty_string = Typing_print.full_strip_ns env ty in
-    let ty_with_constraints_string =
-      match Typing_print.constraints_for_type env ty with
-      | None -> ty_string
-      | Some s -> ty_string ^ "\n" ^ s in
-    result_ty := Some (ty_with_constraints_string,
-      Hh_json.json_to_string (Typing_print.to_json env ty))
-  end
-
-let attach_hooks line column =
-  let result_ty = ref None in
-  let get_result () = !result_ty  in
-  Typing_hooks.attach_infer_ty_hook
-    (save_infer result_ty line column);
-  get_result
-
-let detach_hooks () =
-  Typing_hooks.remove_all_hooks ()

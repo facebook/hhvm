ddd9d07718bfef753271d322e52ff199ab539af4
Fred Emmott <fe@fb.com>
2018-01-08 21:33:27 -0800

    [Security] Prevent illegal write/read access caused by gdImageAALine overflow
    Summary: As titled.
    
    Reviewed By: markw65
    
    Differential Revision: D4430384
    
    fbshipit-source-id: 93e28287f936e2748f3806fa85818ba78b56db48

---
+++ hphp/test/zend/good/ext/gd/tests/bug72482.php
@@ -0,0 +1,8 @@
+<?php
+$img = imagecreatetruecolor(13, 1007);
+imageantialias($img, true);
+imageline($img, 0, 0, 1073745919, 1073745919, 4096);
+
+$img = imagecreatetruecolor(100, 100);
+imageantialias($img, true);
+imageline($img, 1094795585, 0, 2147483647, 255, 0xff);

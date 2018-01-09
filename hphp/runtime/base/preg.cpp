98c905308ebd7ec7b63f762a37911b7ffd19eac9
Jan Oravec <jan@fb.com>
2018-01-09 09:40:45 -0800

    [Security] preg_quote() does not quote # control character
    Summary: Applies the patch from PHP bug #75355.
    
    Reviewed By: alexeyt
    
    Differential Revision: D6681530
    
    fbshipit-source-id: 312bccd8929271edc848a3fa386b3688ea838352

--- hphp/runtime/base/preg.cpp
+++ hphp/runtime/base/preg.cpp
@@ -2014,6 +2014,7 @@ String preg_quote(const String& str,
     case '[': case '^':  case ']': case '$': case '(':
     case ')': case '{':  case '}': case '=': case '!':
     case '>': case '<':  case '|': case ':': case '-':
+    case '#':
       *q++ = '\\';
       *q++ = c;
       break;

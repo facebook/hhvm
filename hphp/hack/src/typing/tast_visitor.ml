b50db0ea43523d9fff0d48222aed49fe72384b5e
Jake Bailey <jakebailey@fb.com>
2018-01-12 13:42:26 -0800

    Add Tast iter, map, and endo visitors
    Summary: Add saved_env-aware iter, map, and endo visitors to Tast_visitor.
    
    Reviewed By: arxanas
    
    Differential Revision: D6481753
    
    fbshipit-source-id: bb172ad55acdf6c2698c7bf9f0b35bab33af96b9

--- hphp/hack/src/typing/tast_visitor.ml
+++ hphp/hack/src/typing/tast_visitor.ml
@@ -44,6 +44,27 @@ let gconst_env cst =
 let method_env env m =
   Tast_expand.restore_saved_env env m.m_annotation
 
+class virtual ['self] iter = object (self : 'self)
+  inherit [_] Tast.iter as super
+
+  (* Entry point *)
+  method go program = self#on_list (fun () -> self#go_def) () program
+
+  method go_def def =
+    match def with
+    | Fun x -> self#go_fun_ x
+    | Class x -> self#go_class_ x
+    | Typedef x -> self#go_typedef x
+    | Constant x -> self#go_gconst x
+
+  method go_fun_    x = self#on_fun_ (fun_env x) x
+  method go_class_  x = self#on_class_ (class_env x) x
+  method go_typedef x = self#on_typedef (typedef_env x) x
+  method go_gconst  x = self#on_gconst (gconst_env x) x
+
+  method! on_method_ env x = super#on_method_ (method_env env x) x
+end
+
 class virtual ['self] reduce = object (self : 'self)
   inherit [_] Tast.reduce as super
 
@@ -56,6 +77,56 @@ class virtual ['self] reduce = object (self : 'self)
     | Class x -> self#go_class_ x
     | Typedef x -> self#go_typedef x
     | Constant x -> self#go_gconst x
+
+  method go_fun_    x = self#on_fun_ (fun_env x) x
+  method go_class_  x = self#on_class_ (class_env x) x
+  method go_typedef x = self#on_typedef (typedef_env x) x
+  method go_gconst  x = self#on_gconst (gconst_env x) x
+
+  method! on_method_ env x = super#on_method_ (method_env env x) x
+end
+
+class virtual ['self] map = object (self : 'self)
+  inherit [_] Tast.map as super
+
+  (* Entry point *)
+  method go program = self#on_list (fun () -> self#go_def) () program
+
+  method go_def def =
+    match def with
+    | Fun x -> Fun (self#go_fun_ x)
+    | Class x -> Class (self#go_class_ x)
+    | Typedef x -> Typedef (self#go_typedef x)
+    | Constant x -> Constant (self#go_gconst x)
+
+  method go_fun_    x = self#on_fun_ (fun_env x) x
+  method go_class_  x = self#on_class_ (class_env x) x
+  method go_typedef x = self#on_typedef (typedef_env x) x
+  method go_gconst  x = self#on_gconst (gconst_env x) x
+
+  method! on_method_ env x = super#on_method_ (method_env env x) x
+end
+
+class virtual ['self] endo = object (self : 'self)
+  inherit [_] Tast.endo as super
+
+  (* Entry point *)
+  method go program = self#on_list (fun () -> self#go_def) () program
+
+  method go_def def =
+    match def with
+    | Fun x ->
+      let y = self#go_fun_ x in
+      if x == y then def else Fun y
+    | Class x ->
+      let y = self#go_class_ x in
+      if x == y then def else Class y
+    | Typedef x ->
+      let y = self#go_typedef x in
+      if x == y then def else Typedef y
+    | Constant x ->
+      let y = self#go_gconst x in
+      if x == y then def else Constant y
 
   method go_fun_    x = self#on_fun_ (fun_env x) x
   method go_class_  x = self#on_class_ (class_env x) x

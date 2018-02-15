ca4363144d6c42076c61e5ad68ed772e3b2a7863
Philip Hölzenspies <drphil@fb.com>
2018-02-15 10:35:39 -0800

    Expose more of the parser configuration through the CLI of `hh_parse`
    Summary:
    jamesjwu asked to make parsing more debuggable. We've grown an extensive web of
    configurable options for parsing. This diff exposes command line options to set
    or unset the boolean choices of that configuration in `hh_parse`.
    
    NOTE: It also renames the option `--hhvm_compat_mode` to `--hhvm-compat-mode`
    for consistency of naming. Sorry for churn, users thereof.
    
    Reviewed By: jamesjwu
    
    Differential Revision: D7000494
    
    fbshipit-source-id: 64b713d201d4e9a43c044d9402dd5a7934b2a6c1

--- hphp/hack/src/full_fidelity_parse.ml
+++ hphp/hack/src/full_fidelity_parse.ml
@@ -39,6 +39,7 @@ module FullFidelityParseArgs = struct
 
   type t =
   {
+    (* Output options *)
     full_fidelity_json : bool;
     full_fidelity_text_json : bool;
     full_fidelity_errors : bool;
@@ -51,7 +52,18 @@ module FullFidelityParseArgs = struct
     pretty_print : bool;
     schema: bool;
     show_file_name : bool;
+    (* Configuring the parser *)
     hhvm_compat_mode : bool;
+    is_hh_file : bool;
+    codegen : bool;
+    php5_compat_mode : bool;
+    elaborate_namespaces : bool;
+    include_line_comments : bool;
+    keep_errors : bool;
+    quick_mode : bool;
+    lower_coroutines : bool;
+    enable_hh_syntax : bool;
+    (* Defining the input *)
     files : string list
   }
 
@@ -68,6 +80,15 @@ module FullFidelityParseArgs = struct
     pretty_print
     schema
     hhvm_compat_mode
+    is_hh_file
+    codegen
+    php5_compat_mode
+    elaborate_namespaces
+    include_line_comments
+    keep_errors
+    quick_mode
+    lower_coroutines
+    enable_hh_syntax
     show_file_name
     files = {
     full_fidelity_json;
@@ -82,6 +103,15 @@ module FullFidelityParseArgs = struct
     pretty_print;
     schema;
     hhvm_compat_mode;
+    is_hh_file;
+    codegen;
+    php5_compat_mode;
+    elaborate_namespaces;
+    include_line_comments;
+    keep_errors;
+    quick_mode;
+    lower_coroutines;
+    enable_hh_syntax;
     show_file_name;
     files }
 
@@ -111,7 +141,15 @@ module FullFidelityParseArgs = struct
     let schema = ref false in
     let set_schema () = schema := true in
     let hhvm_compat_mode = ref false in
-    let set_hhvm_compat_mode () = hhvm_compat_mode := true in
+    let is_hh_file = ref false in
+    let codegen = ref false in
+    let php5_compat_mode = ref false in
+    let elaborate_namespaces = ref true in
+    let include_line_comments = ref false in
+    let keep_errors = ref true in
+    let quick_mode = ref false in
+    let lower_coroutines = ref true in
+    let enable_hh_syntax = ref false in
     let show_file_name = ref false in
     let set_show_file_name () = show_file_name := true in
     let files = ref [] in
@@ -153,9 +191,60 @@ No errors are filtered out.";
       "--schema",
         Arg.Unit set_schema,
         "Displays the parser version and schema of nodes.";
-      "--hhvm_compat_mode",
-        Arg.Unit set_hhvm_compat_mode,
+      "--hhvm-compat-mode",
+        Arg.Set hhvm_compat_mode,
         "Enforce HHVM compatibility (especially in what errors to ignore)";
+      "--no-hhvm-compat-mode",
+        Arg.Clear hhvm_compat_mode,
+        "Do not enforce HHVM compatibility (especially in what errors to ignore)";
+      "--is-hh-file",
+        Arg.Set is_hh_file,
+        "Set the is_hh_file option for the parser.";
+      "--no-is-hh-file",
+        Arg.Clear is_hh_file,
+        "Unset the is_hh_file option for the parser.";
+      "--codegen",
+        Arg.Set codegen,
+        "Set the codegen option for the parser.";
+      "--no-codegen",
+        Arg.Clear codegen,
+        "Unset the codegen option for the parser.";
+      "--php5-compat-mode",
+        Arg.Set php5_compat_mode,
+        "Set the php5_compat_mode option for the parser.";
+      "--no-php5-compat-mode",
+        Arg.Clear php5_compat_mode,
+        "Unset the php5_compat_mode option for the parser.";
+      "--elaborate-namespaces",
+        Arg.Set elaborate_namespaces,
+        "Set the elaborate_namespaces option for the parser.";
+      "--no-elaborate-namespaces",
+        Arg.Clear elaborate_namespaces,
+        "Unset the elaborate_namespaces option for the parser.";
+      "--include-line-comments",
+        Arg.Set include_line_comments,
+        "Set the include_line_comments option for the parser.";
+      "--no-include-line-comments",
+        Arg.Clear include_line_comments,
+        "Unset the include_line_comments option for the parser.";
+      "--keep-errors",
+        Arg.Set keep_errors,
+        "Set the keep_errors option for the parser.";
+      "--no-keep-errors",
+        Arg.Clear keep_errors,
+        "Unset the keep_errors option for the parser.";
+      "--quick-mode",
+        Arg.Set quick_mode,
+        "Set the quick_mode option for the parser.";
+      "--no-quick-mode",
+        Arg.Clear quick_mode,
+        "Unset the quick_mode option for the parser.";
+      "--lower-coroutines",
+        Arg.Set lower_coroutines,
+        "Set the lower_coroutines option for the parser.";
+      "--no-lower-coroutines",
+        Arg.Clear lower_coroutines,
+        "Unset the lower_coroutines option for the parser.";
       "--show-file-name",
         Arg.Unit set_show_file_name,
         "Displays the file name.";
@@ -174,6 +263,15 @@ No errors are filtered out.";
       !pretty_print
       !schema
       !hhvm_compat_mode
+      !is_hh_file
+      !codegen
+      !php5_compat_mode
+      !elaborate_namespaces
+      !include_line_comments
+      !keep_errors
+      !quick_mode
+      !lower_coroutines
+      !enable_hh_syntax
       !show_file_name
       (List.rev !files)
 end
@@ -238,7 +336,20 @@ let handle_existing_file args filename =
   end;
   if args.full_fidelity_ast_s_expr then begin
     let module Lowerer = Full_fidelity_ast in
-    let env = Lowerer.make_env file in
+    let env =
+      Lowerer.make_env
+        ~hhvm_compat_mode:args.hhvm_compat_mode
+        ~codegen:args.codegen
+        ~php5_compat_mode:args.php5_compat_mode
+        ~elaborate_namespaces:args.elaborate_namespaces
+        ~include_line_comments:args.include_line_comments
+        ~keep_errors:args.keep_errors
+        ~quick_mode:args.quick_mode
+        ~lower_coroutines:args.lower_coroutines
+        ~enable_hh_syntax:args.enable_hh_syntax
+        ~is_hh_file:args.is_hh_file
+        file
+    in
     let res = Lowerer.from_file env in
     let str = Debug.dump_ast @@ Ast.AProgram res.Lowerer.ast in
     Printf.printf "%s" str

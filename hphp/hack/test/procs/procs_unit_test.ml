b5db4855c1b1cfcfef9e7431bf37195f5e949147
Alexander Chow <achow@fb.com>
2018-03-16 00:21:06 -0700

    MultiWorker coalesces worker failures and raises bundled exception.
    Summary:
    Add support to MultiWorker.call for a coalesced failure of workers.
    
    Right now, when OOM-killer sigkills one worker process we sometimes
    don't see it. Instead, it leaves SharedMem in a corrupted state and we
    see some other worker fail for reasons unrelated to sigkill.
    
    But we want to actually detect that OOM-killer's sigkill.
    
    This lets us do that.
    
    When a worker failure is encountered, we continue looking through the remaining
    workers looking for more failures, coalesce them all together, and throw a
    single unified exception for all of the failed workers.
    
    Reviewed By: dabek
    
    Differential Revision: D7210619
    
    fbshipit-source-id: d853e06278156c6f810d1a8ad2457ecd006a2f1e

--- hphp/hack/test/procs/procs_unit_test.ml
+++ hphp/hack/test/procs/procs_unit_test.ml
@@ -56,11 +56,83 @@ let multi_worker_of_n_buckets workers () =
   Printf.printf "Got %d\n" result;
   result = expected
 
+let multi_worker_one_worker_throws _workers () =
+  (** When a worker fails, the rest in the same MultiWorker call can't
+   * be reused right now. So let's use fresh workers for this unit test
+   * instead of corrupted the shared workers. *)
+  let workers = make_workers 10 in
+  let split ~bucket = bucket + 1 in
+  let open Bucket in
+  let do_work _ bucket =
+    if (bucket.work = 5) then
+      (** Bucket number 5 exits abnormally. *)
+      exit 3
+    else
+      bucket.work
+  in
+  try begin
+    let _result =
+      MultiWorker.call (Some workers)
+        ~job:do_work
+        ~merge:(+)
+        ~neutral:0
+        ~next:(make_n_buckets ~buckets:20 ~split)
+    in
+    false
+  end with
+  | MultiThreadedCall.Coalesced_failures [ Unix.WEXITED 3 ] ->
+    true
+
+let multi_worker_with_failure_handler _workers () =
+  (** Spawn new workers since some will be failing and the rest non-reusable. *)
+  let workers = make_workers 10 in
+  let split ~bucket =
+    (** We want the first 5 buckets to all have exited while the MultiWorker result
+     * is being collected. But that's racy. Since the work is fanned out
+     * statically, we can ensure the first 5 have all "failed completely" by
+     * sleeping when handing out the 6th job. *)
+    if bucket = 5 then
+      let () = Unix.sleep 1 in
+      bucket + 1
+    else
+      bucket + 1 in
+  let open Bucket in
+  let do_work _ _ =
+    (** Each bucket exits abnormally with exit code 3*)
+    exit 3
+  in
+  try
+    let _ = MultiWorker.call
+      (Some workers)
+      ~job:do_work
+      ~merge:(+)
+      ~neutral:0
+      ~next:(make_n_buckets ~buckets:10 ~split)
+    in
+    Printf.eprintf "Expected MultiWorker.call to throw, but it didn't!\n";
+    false
+  with
+  | MultiThreadedCall.Coalesced_failures failures ->
+    (** Every single bucket failed, and we should have processed all of them since
+     * we have 10 worker processes. *)
+    assert ((List.length failures) = 5);
+    let sum = List.fold_left (
+      fun acc e -> match e with
+      | Unix.WEXITED 3 ->
+        acc + 3
+      | _ ->
+        failwith "Unexpected worker exit") 0 failures
+    in
+    (** Sum of all exit codes (each exit 3) is 15. *)
+    sum = 15
+
 let tests =
   [
     "multi_worker_list", multi_worker_list;
     "multi_worker_bucket", multi_worker_bucket;
     "multi_worker_of_n_buckets", multi_worker_of_n_buckets;
+    "multi_worker_one_worker_throws", multi_worker_one_worker_throws;
+    "multi_worker_with_failure_handler", multi_worker_with_failure_handler;
   ]
 
 let () =

facebook/hhvm

commit afa3f0811556226a202b724aab8d41c94ae55675

use THP for Bump2MMapper and use it in ManagedArena, take 2
Summary: Previous effort in shipping this was reverted because of a bug in when MALLOC_CONF changes the default narenas.  So reship this after the bug fix.

Reviewed By: interwq

Differential Revision: D7261457

fbshipit-source-id: a4e9a9e9abb5b5cc98f76bbb68c341258659700d

hphp/util/alloc.cpp
@@ -216,6 +216,8 @@ unsigned high_huge1g_arena_real = 0;
 // arena when it is disabled, so that we know when to reenable it.
 std::atomic_uint g_high1GRecentlyFreed;
 
+alloc::Bump2MMapper* low_2m_mapper = nullptr;
+
 // Customized hooks to use 1g pages for jemalloc metadata.
 static extent_hooks_t huge_page_metadata_hooks;
 static extent_alloc_t* orig_alloc = nullptr;
@@ -364,65 +366,64 @@ static void set_arena_retain_grow_limit(unsigned id) {
   }
 }
 
-/*
- * Get `pages` (at most 2) 1G huge pages and map to the low memory that grows
- * down from 4G.  We can do either one (3G-4G) or two pages (2G-4G).
- */
-void setup_low_1g_arena(int pages) {
-  if (pages <= 0) return;
-  if (pages > 2) pages = 2;             // At most 2 1G pages in low memory
+using namespace alloc;
+static BumpMapper* getHugeMapperWithFallback(unsigned n1GPages,
+                                             bool use2MFallback,
+                                             short nextNode) {
 #ifdef HAVE_NUMA
   const int max_node = numa_max_node();
 #else
   constexpr int max_node = 0;
 #endif
-  using namespace alloc;
   BumpMapper* mapper = nullptr;
+  BumpMapper* fallback = nullptr;
   if (max_node < 1) {
     // We either don't have libnuma, or run on a single-socket CPU.  In either
     // case, no need to worry about NUMA.
-    mapper = new Bump1GMapper(pages);
-    // mapper->append(new Bump4KMapper);
+    fallback = new Bump4KMapper;
+    if (n1GPages) {
+      mapper = new Bump1GMapper(n1GPages);
+    } else if (use2MFallback) {
+      mapper = new Bump2MMapper;
+    }
   } else {
-    mapper = new Bump1GMapper(pages, numa_node_set,
-                              0 /* starting from Node 0 */);
-    // mapper->append(new Bump4KMapper(numa_node_set));
+    fallback = new Bump4KMapper(numa_node_set);
+    if (n1GPages) {
+      mapper = new Bump1GMapper(n1GPages, numa_node_set, nextNode);
+    } else {
+      mapper = new Bump2MMapper(numa_node_set);
+    }
+  }
+  if (mapper) {
+    mapper->append(fallback);
+  } else {
+    mapper = fallback;
+  }
+  return mapper;
+}
+
+void setup_low_1g_arena(unsigned n1GPages) {
+  auto mapper = getHugeMapperWithFallback(n1GPages, true, 0);
+  if (n1GPages == 0) {
+    // If we are using 2M pages, save this so we can change how many 2M huge
+    // pages to use.
+    low_2m_mapper = dynamic_cast<Bump2MMapper*>(mapper);
   }
   auto ma =
     LowHugeArena::CreateAt(&g_lowHugeArena,
-                           size1g * 4, size1g * pages, false, mapper);
+                           kLowArenaMaxAddr, kLowArenaMaxCap, false, mapper);
   set_arena_retain_grow_limit(ma->id());
   low_huge1g_arena = low_huge1g_arena_real = ma->id();
 }
 
-/*
- * Get `pages` 1G huge pages with explicit NUMA balancing.
- */
-void setup_high_1g_arena(int pages) {
-  if (pages <= 0) return;
-  // We don't need/want a crazy number of pages here.
-  if (pages > 12) pages = 12;
-#ifdef HAVE_NUMA
-  const int max_node = numa_max_node();
-#else
-  constexpr int max_node = 0;
-#endif
+void setup_high_1g_arena(unsigned n1GPages) {
+  // If we use 1G huge pages on NUMA servers, start grabbing 1G huge pages from
+  // a node different from the one for low arena.
+  auto mapper = getHugeMapperWithFallback(n1GPages, false,
+                                          num_numa_nodes() / 2 + 1);
   using namespace alloc;
-  BumpMapper* mapper;
-  if (max_node < 1) {
-    // We either don't have libnuma, or run on a single-node system.  In
-    // either case, no need to worry about NUMA.
-    mapper = new Bump1GMapper(pages);
-    // mapper->append(new Bump4KMapper);
-  } else {
-    // start grabbing 1G huge pages from a node different from the one for low
-    // arena.
-    mapper = new Bump1GMapper(pages, numa_node_set,
-                              max_node / 2 + 1);
-    // mapper->append(new Bump4KMapper(numa_node_set));
-  }
   auto ma = HighArena::CreateAt(&g_highArena,
-                                kHigh1GArenaMaxAddr, size1g * pages,
+                                kHighArenaMaxAddr, kHighArenaMaxCap,
                                 false, mapper);
   set_arena_retain_grow_limit(ma->id());
   high_huge1g_arena = high_huge1g_arena_real = ma->id();
@@ -559,19 +560,31 @@ struct JEMallocInitializer {
     assert((uintptr_t(sbrk(0)) & kHugePageMask) == 0);
     highest_lowmall_addr = (char*)sbrk(0) - 1;
 
-#if defined USE_JEMALLOC_EXTENT_HOOKS
+#ifdef USE_JEMALLOC_EXTENT_HOOKS
     // Number of 1G huge pages for data in low memeory
-    int low_1g_pages = 0;
+    unsigned low_1g_pages = 0;
     if (char* buffer = getenv("HHVM_LOW_1G_PAGE")) {
-      sscanf(buffer, "%d", &low_1g_pages);
+      if (!sscanf(buffer, "%u", &low_1g_pages)) {
+        fprintf(stderr,
+                "Bad environment variable HHVM_LOW_1G_PAGE: %s\n", buffer);
+        abort();
+      }
     }
     // Number of 1G pages for shared data not in low memory (e.g., APC)
-    int high_1g_pages = 0;
+    unsigned high_1g_pages = 0;
     if (char* buffer = getenv("HHVM_HIGH_1G_PAGE")) {
-      sscanf(buffer, "%d", &high_1g_pages);
+      if (!sscanf(buffer, "%u", &high_1g_pages)) {
+        fprintf(stderr,
+                "Bad environment variable HHVM_HIGH_1G_PAGE: %s\n", buffer);
+        abort();
+      }
     }
 
-    if (low_1g_pages > 0 || high_1g_pages > 0) {
+    HugePageInfo info = get_huge1g_info();
+    unsigned remaining = static_cast<unsigned>(info.nr_hugepages);
+    if (remaining == 0) {
+      low_1g_pages = high_1g_pages = 0;
+    } else if (low_1g_pages > 0 || high_1g_pages > 0) {
       KernelVersion version;
       if (version.m_major < 3 ||
           (version.m_major == 3 && version.m_minor < 9)) {
@@ -580,11 +593,10 @@ struct JEMallocInitializer {
       }
     }
 
-    HugePageInfo info = get_huge1g_info();
-    int remaining = info.nr_hugepages;
-    if (remaining == 0) return;         // no pages reverved
-
-    // Do some allocation between low and high 1G arenas
+    // Do some allocation between low and high 1G arenas.  We use at most 2 1G
+    // pages for the low 1G arena; usually 1 is good enough.
+    auto const origLow1G = low_1g_pages;
+    auto const origHigh1G = high_1g_pages;
     if (low_1g_pages > 0) {
       if (low_1g_pages > 2) {
         low_1g_pages = 2;
@@ -592,16 +604,25 @@ struct JEMallocInitializer {
       if (low_1g_pages + high_1g_pages > remaining) {
         low_1g_pages = 1;
       }
+      assert(remaining >= low_1g_pages);
       remaining -= low_1g_pages;
-      setup_low_1g_arena(low_1g_pages);
     }
+    if (origLow1G) {
+      fprintf(stderr,
+              "using %u (specified %u) 1G huge pages for low arena\n",
+              low_1g_pages, origLow1G);
+    }
+    setup_low_1g_arena(low_1g_pages);
 
     if (high_1g_pages > remaining) {
       high_1g_pages = remaining;
     }
-    if (high_1g_pages > 0) {
-      setup_high_1g_arena(high_1g_pages);
+    if (origHigh1G) {
+      fprintf(stderr,
+              "using %u (specified %u) 1G huge pages for high arena\n",
+              high_1g_pages, origHigh1G);
     }
+    setup_high_1g_arena(high_1g_pages);
 #endif
   }
 };
@@ -705,6 +726,18 @@ void* malloc_huge1g_impl(size_t size) {
 void low_malloc_skip_huge(void* start, void* end) {}
 
 #endif // USE_JEMALLOC
+
+void low_malloc_huge_pages(int pages) {
+#ifdef USE_JEMALLOC
+  if (pages <= 0) return;
+  low_huge_pages = pages;
+#ifdef USE_JEMALLOC_EXTENT_HOOKS
+  if (low_2m_mapper) {
+    low_2m_mapper->setMaxPages(pages);
+  }
+#endif
+#endif
+}
 
 int mallctlCall(const char* cmd, bool errOk) {
   // Use <unsigned> rather than <void> to avoid sizeof(void).

hphp/util/alloc.h
@@ -181,8 +181,14 @@ extern unsigned low_huge1g_arena;
 extern unsigned low_huge1g_arena_real;
 extern unsigned high_huge1g_arena;
 extern unsigned high_huge1g_arena_real;
-// Address range in the high1g arena is less than 16G.
-constexpr uintptr_t kHigh1GArenaMaxAddr = 16ull << 30;
+
+// Address ranges for the managed arenas.  Low arena is in [3G, 4G), and high
+// arena in [4G, 32G) at most.  Both grows down and can be smaller.  But things
+// won't work well if either overflows.
+constexpr uintptr_t kLowArenaMaxAddr = 4ull << 30;
+constexpr uintptr_t kHighArenaMaxAddr = 32ull << 30;
+constexpr size_t kLowArenaMaxCap = 2ull << 30;
+constexpr size_t kHighArenaMaxCap = kHighArenaMaxAddr - kLowArenaMaxAddr;
 
 // Explicit per-thread tcache for the huge arenas.
 extern __thread int high_huge1g_tcache;
@@ -234,12 +240,7 @@ inline void low_free(void* ptr) {
 #endif
 }
 
-inline void low_malloc_huge_pages(int pages) {
-#ifdef USE_JEMALLOC
-  low_huge_pages = pages;
-#endif
-}
-
+void low_malloc_huge_pages(int pages);
 void low_malloc_skip_huge(void* start, void* end);
 
 inline void* low_malloc_data(size_t size) {
@@ -272,7 +273,7 @@ inline void free_huge(void* ptr) {
 #ifndef USE_JEMALLOC_EXTENT_HOOKS
   free(ptr);
 #else
-  if (LIKELY(reinterpret_cast<uintptr_t>(ptr) < kHigh1GArenaMaxAddr)) {
+  if (LIKELY(reinterpret_cast<uintptr_t>(ptr) < kHighArenaMaxAddr)) {
     if (ptr) dallocx(ptr, dallocx_huge1g_flags());
   } else {
     // Not from the high 1G arena

hphp/util/bump-mapper.cpp
@@ -18,6 +18,7 @@
 
 #include "hphp/util/assertions.h"
 #include "hphp/util/hugetlb.h"
+#include "hphp/util/maphuge.h"
 #include "hphp/util/numa.h"
 
 #include <folly/portability/SysMman.h>
@@ -95,56 +96,9 @@ bool Bump1GMapper::addMappingImpl(BumpAllocState& state, size_t newSize) {
   return false;
 }
 
-bool Bump2MMapper::addMappingImpl(BumpAllocState& state, size_t newSize) {
-  // Check quota and alignment before trying to map.
-  if (m_currNumPages >= m_maxNumPages) {
-    m_failed = true;
-    return false;
-  }
-  uintptr_t currFrontier = state.frontier();
-  if (currFrontier % size2m != 0) return false;
-
-#ifdef HAVE_NUMA
-  assert((m_interleaveMask & ~numa_node_set) == 0);
-  const unsigned allowedRetries = __builtin_popcount(m_interleaveMask);
-#else
-  constexpr unsigned allowedRetries = 0; // No retry when there is no NUMA limit
-#endif
-  // Try to map 2M pages until we have enough capacity for the requested size,
-  // or until we run out of allowed quota, or until we have failed to obtain 2M
-  // huge pages on all the allowed NUMA nodes.
-  unsigned failCount = 0;
-  while (state.m_currCapacity < newSize) {
-    currFrontier = state.m_base - state.m_currCapacity;
-    assert(currFrontier % size2m == 0);
-    void* newPageStart = reinterpret_cast<void*>(currFrontier - size2m);
-    int currNode = -1;
-#ifdef HAVE_NUMA
-    if (m_interleaveMask) {
-      // Advance to the next allowed node.
-      do {
-        currNode = m_nextNode;
-        m_nextNode = (currNode + 1) & numa_node_mask;
-      } while (!((1u << currNode) & m_interleaveMask));
-    }
-#endif
-    if (mmap_2m(newPageStart, PROT_READ | PROT_WRITE, currNode)) {
-      state.m_currCapacity += size2m;
-      if (++m_currNumPages >= m_maxNumPages) {
-        m_failed = true;
-        return state.m_currCapacity >= newSize;
-      }
-      failCount = 0;
-    } else if (++failCount >= allowedRetries) {
-      // Failed on all allowed nodes.
-      return false;
-    }
-  }
-  return true;
-}
+constexpr size_t Bump4KMapper::kChunkSize;
 
 bool Bump4KMapper::addMappingImpl(BumpAllocState& state, size_t newSize) {
-  constexpr size_t kChunkSize = size2m * 4;
   auto const currFrontier = state.m_base - state.m_currCapacity;
   if (currFrontier % size4k != 0) return false;
   void* newPageStart = reinterpret_cast<void*>(currFrontier - kChunkSize);
@@ -167,6 +121,28 @@ bool Bump4KMapper::addMappingImpl(BumpAllocState& state, size_t newSize) {
           &mask, 32 /* max node */, 0 /* flag */);
   }
 #endif
+  return true;
+}
+
+bool Bump2MMapper::addMappingImpl(BumpAllocState& state, size_t newSize) {
+  // Check quota and alignment before trying to map.  Note that m_maxNumPages
+  // may be initialized later for this mapper, so don't fail permanently before
+  // it is initialized.
+  if (m_currNumPages >= m_maxNumPages) {
+    if (m_maxNumPages) {
+      m_failed = true;
+    }
+    return false;
+  }
+  auto newPages = state.m_base - state.m_currCapacity;
+  // Add some 4K pages before madvise()
+  if (!Bump4KMapper::addMappingImpl(state, newSize)) {
+    return false;
+  }
+  auto const hugeSize = std::min(Bump4KMapper::kChunkSize,
+                                 size2m * (m_maxNumPages - m_currNumPages));
+  assert(newPages % size2m == 0);
+  hintHuge(reinterpret_cast<void*>(newPages), hugeSize);
   return true;
 }
 

hphp/util/bump-mapper.h
@@ -91,7 +91,7 @@ struct BumpMapper {
   // If the mapper doesn't use huge pages, it can choose not to look at it in
   // `addMappingImpl()`, and that's why we don't check m_currNumPages <
   // m_maxNumPages in `addMapping()`.
-  const uint32_t m_maxNumPages{0};
+  uint32_t m_maxNumPages{0};
   uint32_t m_currNumPages{0};
   // NUMA interleave masks for the newly added memory.  0 indicates no-NUMA.
   uint32_t m_interleaveMask{0};
@@ -114,20 +114,26 @@ struct Bump1GMapper : public BumpMapper {
   bool addMappingImpl(BumpAllocState& state, size_t newSize) override;
 };
 
-// Add 2M huge pages
-struct Bump2MMapper : public BumpMapper {
-  template<typename... Args>
-  explicit Bump2MMapper(Args&&... args)
-    : BumpMapper(std::forward<Args>(args)...) {}
- protected:
-  bool addMappingImpl(BumpAllocState& state, size_t newSize) override;
-};
-
 struct Bump4KMapper : public BumpMapper {
   // Only the NUMA mask is relevant.  `m_maxNumPages` is ignored for this
   // mapper.
   explicit Bump4KMapper(uint32_t numaMask = 0)
     : BumpMapper(0, numaMask) {}
+ protected:
+  bool addMappingImpl(BumpAllocState& state, size_t newSize) override;
+  static constexpr size_t kChunkSize = 8u << 20;
+};
+
+// Bump4KMapper with transparent huge pages.
+struct Bump2MMapper : public Bump4KMapper {
+  template<typename... Args>
+  explicit Bump2MMapper(uint32_t numaMask = 0)
+    : Bump4KMapper(numaMask) {}
+
+  inline void setMaxPages(uint32_t max2MPages) {
+    m_maxNumPages = max2MPages;
+  }
+
  protected:
   bool addMappingImpl(BumpAllocState& state, size_t newSize) override;
 };

hphp/util/maphuge.cpp
@@ -17,31 +17,13 @@
 #include "hphp/util/maphuge.h"
 
 #include "hphp/util/hugetlb.h"
-#include "hphp/util/kernel-version.h"
 
-#include <folly/portability/SysMman.h>
 #include <folly/portability/Unistd.h>
 
+#include <errno.h>
+#include <stdint.h>
+
 namespace HPHP {
-
-void hintHuge(void* mem, size_t length) {
-#ifdef MADV_HUGEPAGE
-  if (hugePagesSupported()) {
-    madvise(mem, length, MADV_HUGEPAGE);
-  }
-#endif
-}
-
-bool hugePagesSupported() {
-#ifdef MADV_HUGEPAGE
-  static KernelVersion kv;
-  // This kernel fixed a panic when using MADV_HUGEPAGE.
-  static KernelVersion minKv("3.2.28-72_fbk12");
-  return KernelVersion::cmp(kv, minKv);
-#else
-  return false;
-#endif
-}
 
 void hintHugeDeleteData(char* mem, size_t length, int prot, bool shared) {
 #ifdef __linux__

hphp/util/maphuge.h
@@ -16,11 +16,28 @@
 #ifndef incl_HPHP_MAPHUGE_H_
 #define incl_HPHP_MAPHUGE_H_
 
-#include <sys/types.h>
+#include <folly/portability/SysMman.h>
 
 namespace HPHP {
-void hintHuge(void* mem, size_t length);
-bool hugePagesSupported();
+
+inline constexpr bool hugePagesSupported() {
+#ifdef MADV_HUGEPAGE
+  // Kernels earlier than 3.2.28 may have bugs dealing with MADV_HUGEPAGE.  The
+  // bug should've been fixed in production kernels now, so we no longer do
+  // run-time kernel version checks.
+  return true;
+#else
+  return false;
+#endif
+}
+
+inline void hintHuge(void* mem, size_t length) {
+#ifdef MADV_HUGEPAGE
+  if (hugePagesSupported()) {
+    madvise(mem, length, MADV_HUGEPAGE);
+  }
+#endif
+}
 
 // Will delete whatever data on the pages when remapping.  Caller is responsible
 // to copy the data back if desired.

commit 68b4cbb60fc48b1c57713daf381f309748aff108

Fix compilation error (numa_node_set undefined) for non-NUMA builds
Summary:
Fixes compilation error for non-NUMA builds which was introduced with afa3f0811556226a202b724aab8d41c94ae55675.
Closes https://github.com/facebook/hhvm/pull/8170

Differential Revision: D7534916

Pulled By: fredemmott

fbshipit-source-id: 2a8f878b4284753fdf0c6f3f08a4f5bac21ae943

hphp/util/alloc.cpp
@@ -373,6 +373,7 @@ static BumpMapper* getHugeMapperWithFallback(unsigned n1GPages,
     } else if (use2MFallback) {
       mapper = new Bump2MMapper;
     }
+#ifdef HAVE_NUMA
   } else {
     fallback = new Bump4KMapper(numa_node_set);
     if (n1GPages) {
@@ -380,6 +381,7 @@ static BumpMapper* getHugeMapperWithFallback(unsigned n1GPages,
     } else {
       mapper = new Bump2MMapper(numa_node_set);
     }
+#endif
   }
   if (mapper) {
     mapper->append(fallback);
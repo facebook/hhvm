9c92ae7f0e544847eda178caba3ea5106ee1809d
Kunal Mehta <kunalm@fb.com>
2018-05-08 16:46:53 -0700

    Support Hack arrays in is-expressions
    Summary:
    This diff was primarily to add tests for Hack arrays, but it exposed a bug as well:
    
    Because the user may not provide type parameters at all, we must proactively truncate and pad the list inside `isexpr_generate_fresh_tparams`, which actually brings back the code removed in the previous diff.
    
    Reviewed By: oulgen
    
    Differential Revision: D7898034
    
    fbshipit-source-id: 7db64088b00ec6d66b54b41f0e5c411d119e3174

--- hphp/hack/src/typing/typing.ml
+++ hphp/hack/src/typing/typing.ml
@@ -5816,12 +5816,21 @@ and safe_instanceof env p class_name class_info ivar_pos ivar_ty obj_ty =
   env, obj_ty
 
 and isexpr_generate_fresh_tparams env class_info reason hint_tyl =
-  let replace_wildcard env hint_ty tp =
+  let tparams_len = List.length class_info.tc_tparams in
+  let hint_tyl = List.take hint_tyl tparams_len in
+  let pad_len = tparams_len - (List.length hint_tyl) in
+  let hint_tyl =
+    List.map hint_tyl (fun x -> Some x) @ (List.init pad_len (fun _ -> None)) in
+  let replace_wildcard env hint_ty ((_, (_, tparam_name), _) as tp) =
     match hint_ty with
-      | _, Tabstract (AKgeneric name, _)
+      | Some (_, Tabstract (AKgeneric name, _))
         when Env.is_fresh_generic_parameter name ->
         env, (Some (tp, name), (reason, Tabstract (AKgeneric name, None)))
-      | _ -> env, (None, hint_ty)
+      | Some ty ->
+        env, (None, ty)
+      | None ->
+        let env, new_name = Env.add_fresh_generic_parameter env tparam_name in
+        env, (Some (tp, new_name), (reason, Tabstract (AKgeneric new_name, None)))
   in
   let env, tparams_and_tyl = List.map2_env env hint_tyl class_info.tc_tparams
     ~f:replace_wildcard in

bd029f1f9231fdbcaff1ebd3b9d761cbcc0268eb
Greg Nisbet <gregorynisbet@fb.com>
2018-05-15 10:03:39 -0700

    tests for hh_mem_status
    Summary:
    test `hh_mem_status` to show that removed and absent keys are distinguished.
    
    `move`-ing is equivalent to removing the key and reinserting it, except that it's more efficient.
    
    Reviewed By: alexchow, dabek
    
    Differential Revision: D7784851
    
    fbshipit-source-id: 83aedf1559cbd808c82ce47d0fb645e79a7f957c

--- hphp/hack/test/unit/heap/test_hashtbl.ml
+++ hphp/hack/test/unit/heap/test_hashtbl.ml
@@ -12,6 +12,7 @@ type key = Digest.t
 
 external hh_add    : key -> string -> unit = "hh_add"
 external hh_mem    : key -> bool           = "hh_mem"
+external hh_mem_status : key -> int        = "hh_mem_status"
 external hh_remove : key -> unit           = "hh_remove"
 external hh_move   : key -> key -> unit    = "hh_move"
 external hh_get    : key -> string         = "hh_get_and_deserialize"
@@ -29,6 +30,7 @@ let to_key = Digest.string
 
 let add key value = hh_add (to_key key) value
 let mem key = hh_mem (to_key key)
+let get_status key = hh_mem_status (to_key key)
 let remove key = hh_remove (to_key key)
 let move k1 k2 = hh_move (to_key k1) (to_key k2)
 let get key = hh_get (to_key key)
@@ -75,6 +77,14 @@ let expect_not_mem key =
   expect ~msg:(Printf.sprintf "Expected key '%s' to not be in hashtable" key)
   @@ not (mem key)
 
+let expect_absent key =
+  expect ~msg:(Printf.sprintf "Expected key '%s' to be absent from hashtable" key)
+  @@ (get_status key = -1)
+
+let expect_removed key =
+  expect ~msg:(Printf.sprintf "Expected key '%s' to be removed from hashtable" key)
+  @@ (get_status key = -2)
+
 let expect_get key expected =
   let value = get key in
   expect ~msg:(
@@ -99,6 +109,7 @@ let expect_aggressive_collect expected =
 let test_ops () =
   expect_stats ~nonempty:0 ~used:0;
   expect_not_mem "0";
+  expect_absent "0";
 
   add "0" "";
   expect_stats ~nonempty:1 ~used:1;
@@ -107,11 +118,13 @@ let test_ops () =
   move "0" "1";
   expect_stats ~nonempty:2 ~used:1;
   expect_not_mem "0";
+  expect_removed "0";
   expect_mem "1";
 
   remove "1";
   expect_stats ~nonempty:2 ~used:0;
-  expect_not_mem "1"
+  expect_not_mem "1";
+  expect_removed "1"
 
 let test_hashtbl_full_hh_add () =
   expect_stats ~nonempty:0 ~used:0;
@@ -162,6 +175,7 @@ let test_no_overwrite () =
   remove "0";
   expect_stats ~nonempty:1 ~used:0;
   expect_not_mem "0";
+  expect_removed "0";
 
   add "0" "Bar";
   expect_stats ~nonempty:1 ~used:1;
@@ -182,6 +196,7 @@ let test_reuse_slots () =
    *)
   remove "1";
   expect_not_mem "1";
+  expect_removed "1";
   expect_stats ~nonempty:2 ~used:1;
   add "1" "Foo";
   expect_mem "1";
@@ -191,9 +206,11 @@ let test_reuse_slots () =
   (* If we move to a previously used slot, nonempty slots stays the same *)
   remove "1";
   expect_not_mem "1";
+  expect_removed "1";
   expect_stats ~nonempty:2 ~used:1;
   move "0" "1";
   expect_not_mem "0";
+  expect_removed "0";
   expect_mem "1";
   expect_get "1" "0";
   expect_stats ~nonempty:2 ~used:1;
@@ -201,6 +218,7 @@ let test_reuse_slots () =
   (* Moving to a brand new key will increase number of nonempty slots *)
   move "1" "2";
   expect_not_mem "1";
+  expect_removed "1";
   expect_mem "2";
   expect_get "2" "0";
   expect_stats ~nonempty:3 ~used:1

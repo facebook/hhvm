4bd7a420395b3b422744fd4d7166ae4f0f33c4d4
James Wu <jjwu@fb.com>
2018-05-31 12:48:18 -0700

    Use linear search for line breaks
    Summary: We use a binary search to look for line break offsets, but its much more likely we look for line breaks earlier than later. Thus, to abuse this locality, we use a linear search that starts from the previously found offset each time, which should improve perf significantly.
    
    Reviewed By: pittsw
    
    Differential Revision: D8148739
    
    fbshipit-source-id: b420557dc1faa7739ba1648a662afdc32b74f133

--- hphp/hack/test/utils/pos/line_break_map_test.ml
+++ hphp/hack/test/utils/pos/line_break_map_test.ml
@@ -18,17 +18,13 @@ let () =
   let breaks = 4 in (* Better to count this in the string *)
   let len = String.length str in
   let m = make str in
-  assert
-    (try (ignore (offset_to_position m (-1)); false) with
-    | Invalid_argument _ -> true
-    );
-  let cyclic_index = true in
+
   let rec range n k = if k > n then [] else k :: range n (k+1) in
   List.iter (fun offset ->
       let ch = try String.get str offset with _ -> '_' in
-      let (line, column) = offset_to_position ~cyclic_index m offset in
-      let line_start = offset_to_line_start_offset ~cyclic_index m offset in
-      let regen_offset = position_to_offset ~cyclic_index m (line, column) in
+      let (line, column) = offset_to_position m offset in
+      let line_start = offset_to_line_start_offset m offset in
+      let regen_offset = position_to_offset m (line, column) in
       Printf.eprintf
         " >> %2d -> (%2d,%2d) `%d -> %2d  %C\n"
         offset
@@ -40,4 +36,4 @@ let () =
       assert (offset <  0 || offset = regen_offset);
       assert (column = offset - line_start + 1);
       assert (regen_offset >= 0 && regen_offset < len || line = breaks + 2);
-    ) (-2 * len :: range (String.length str + 3) (-3))
+    ) (range (String.length str) (0))

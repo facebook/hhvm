ffed3b6545b591d793bd939f42a6cea66fd3ef36
Edwin Smith <smith@fb.com>
2018-07-02 08:40:41 -0700

    Use vector_set for ExecutionContext::m_liveBCObjs
    Summary:
    req::vector_set supports the while (!c.empty()) c.erase(c.begin());
    idiom being used by ExecutionContext::destructObjects()
    efficiently, so switch to that instead of req::set (which
    is a sorted tree with many small nodes).
    
    Reviewed By: markw65
    
    Differential Revision: D8397712
    
    fbshipit-source-id: 2610956220f5eb97ed2cceed45e1af8bdf41b8b0

--- hphp/runtime/base/execution-context.h
+++ hphp/runtime/base/execution-context.h
@@ -560,7 +560,7 @@ private:
   const VirtualHost* m_vhost;
 public:
   DebuggerSettings debuggerSettings;
-  req::set<ObjectData*> m_liveBCObjs; // objects with destructors
+  req::vector_set<ObjectData*> m_liveBCObjs; // objects with destructors
 private:
   size_t m_apcMemSize{0};
   std::vector<APCHandle*> m_apcHandles; // gets moved to treadmill

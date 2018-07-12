e5bf541ee8b7bc9c8e62a07687051d2a527441fb
Fred Emmott <fe@fb.com>
2018-07-12 10:29:18 -0700

    Call evthread_use_pthreads() on startup
    libevent is not thread-safe without this. I think we're lucky on Linux,
    but on Mac, this causes:
    
    - `[err] evmap.c:779: Assertion fdinfo->idxplus1 == i + 1 failed in
    event_changelist_remove_all_`
    - https://github.com/facebook/hhvm/issues/8230

--- hphp/runtime/base/program-functions.cpp
+++ hphp/runtime/base/program-functions.cpp
@@ -119,6 +119,8 @@
 #include <boost/program_options/variables_map.hpp>
 #include <boost/filesystem.hpp>
 
+#include <event2/thread.h>
+
 #include <oniguruma.h>
 // Onigurama defines UChar to unsigned char, but ICU4C defines it to signed
 // 16-bit int. This is supposed to be resolved by ONIG_ESCAPE_UCHAR_COLLISION,
@@ -1745,6 +1747,7 @@ static int execute_program_impl(int argc, char** argv) {
 
   // we need to initialize pcre cache table very early
   pcre_init();
+  evthread_use_pthreads();
 
   tl_heap.getCheck();
   if (RuntimeOption::ServerExecutionMode()) {

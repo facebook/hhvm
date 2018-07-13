46e5dff56d0d9c0dd296b9f36615e1756bc07c07
Fred Emmott <fe@fb.com>
2018-07-13 08:46:36 -0700

    Call evthread_use_pthreads() on startup (#8258)
    Summary:
    libevent2 is not thread-safe without this.
    
    - `[err] evmap.c:779: Assertion fdinfo->idxplus1 == i + 1 failed in
    event_changelist_remove_all_`
    - https://github.com/facebook/hhvm/issues/8230
    Pull Request resolved: https://github.com/facebook/hhvm/pull/8258
    
    Reviewed By: fugalh
    
    Differential Revision: D8822279
    
    Pulled By: fredemmott
    
    fbshipit-source-id: 77365718fb4defee5c07eb9eb7a1da728cc01946

--- hphp/runtime/base/program-functions.cpp
+++ hphp/runtime/base/program-functions.cpp
@@ -116,6 +116,11 @@
 #include <boost/program_options/variables_map.hpp>
 #include <boost/filesystem.hpp>
 
+#ifndef FACEBOOK
+// Needed on libevent2
+#include <event2/thread.h>
+#endif
+
 #include <oniguruma.h>
 // Onigurama defines UChar to unsigned char, but ICU4C defines it to signed
 // 16-bit int. This is supposed to be resolved by ONIG_ESCAPE_UCHAR_COLLISION,
@@ -1646,8 +1651,13 @@ static int execute_program_impl(int argc, char** argv) {
   // forget the source for systemlib.php unless we are debugging
   if (po.mode != "debug") SystemLib::s_source = "";
 
-  // we need to initialize pcre cache table very early
+  // we need to to initialize these very early
   pcre_init();
+  // this is needed for libevent2 to be thread-safe, which backs Hack ASIO.
+  #ifndef FACEBOOK
+  // FB uses a custom libevent 1
+  evthread_use_pthreads();
+  #endif
 
 #ifdef ENABLE_ZEND_COMPAT
   //

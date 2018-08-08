c16f0b8833d426e738ba56f6e4fc573079672196
Fred Emmott <fe@fb.com>
2018-08-08 15:51:36 -0700

    Set empty captures to empty string for regexp HSL
    - added `PREG_FB__PRIVATE__HSL_IMPL`
    - intentionally not added it to the HHIs to discourage direct use. The
    HSL will require FIXMEs until the direct native impl is done.

--- hphp/test/slow/ext_hsl/ext_regex.php
+++ hphp/test/slow/ext_hsl/ext_regex.php
@@ -7,3 +7,22 @@ function f(Regex\Pattern $pattern): void {
 }
 
 f("hello\n");
+
+$patterns = vec[
+  tuple('/foo(bar)?/', 'foo'),
+  tuple('/foo(?<bar>bar)?/', 'foo'),
+  tuple('/foo(?<bar>bar)?(baz)?/', 'foo'),
+  tuple('/foo(?<bar>bar)?(baz)?/', 'foobar'),
+  tuple('/foo(?<bar>bar)?(baz)?/', 'foobaz'),
+  tuple('/foo(?<bar>bar)?(baz)?/', 'foobarbaz'),
+];
+
+foreach ($patterns as list($pattern, $text)) {
+  $matches = [];
+  preg_match($pattern, $text, &$matches);
+  printf("---\nRE: %s\nIn: %s\nPHP:\n", $pattern, $text);
+  var_dump($matches);
+  preg_match($pattern, $text, &$matches, PREG_FB__PRIVATE__HSL_IMPL);
+  printf("HSL:\n", $pattern, $text);
+  var_dump($matches);
+}

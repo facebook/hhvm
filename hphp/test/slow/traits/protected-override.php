1694dff8740f1a45d988dd5e2d6e03535783e072
Mark Williams <mwilliams@fb.com>
2018-08-15 08:55:35 -0700

    Fix a bug overriding protected properties from flattened traits
    Summary:
    When a trait property overrides a base class property, it's required
    to match exactly, but otherwise has no effect; when a normal property
    does so, it updates the defining class, which can affect the
    accessibility of protected properties.
    
    Reviewed By: ricklavoie, alexeyt
    
    Differential Revision: D9328606
    
    fbshipit-source-id: 1d04d8d0a50ee1a7337d9119b84dbf260174a652

---
+++ hphp/test/slow/traits/protected-override.php
@@ -0,0 +1,25 @@
+<?hh
+
+trait T {
+  protected $params;
+};
+
+class B {
+  protected $params;
+};
+
+class B1 extends B {
+  use T;
+}
+
+class B2 extends B {
+  use T;
+}
+
+class C extends B1 {
+  function __construct($b2) {
+    $this->params = $b2->params;
+  }
+}
+
+var_dump(new C(new B2));

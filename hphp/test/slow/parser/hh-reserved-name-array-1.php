488c33c7c96a4a588e138301d1bf6d7e564b9a60
Alexey Toptygin <alexeyt@fb.com>
2018-08-31 23:31:40 -0700

    Fix forward tests broken by D9302107.
    Summary: Undo __EntryPoint-ification of 2 tests where it was not appropriate. Also fix their test expectations that got stepped on by a bad merge. Take this opportunity to simplify them from expectregex to expectf, since we don't need to support the hphpc output any more.
    
    Reviewed By: paulbiss
    
    Differential Revision: D9625121
    
    fbshipit-source-id: bee79d13e70363fdc8fd66bf2eeea0c0ac853f3b

--- hphp/test/slow/parser/hh-reserved-name-array-1.php
+++ hphp/test/slow/parser/hh-reserved-name-array-1.php
@@ -1,9 +1,6 @@
 <?hh
 
-<<__EntryPoint>>
-function main_hh_reserved_name_array_1() {
 if (false) {
   class Array {}
 }
 echo "Done\n";
-}
--- hphp/test/slow/parser/php-reserved-name-array-1.php
+++ hphp/test/slow/parser/php-reserved-name-array-1.php
@@ -1,9 +1,6 @@
 <?php
 
-<<__EntryPoint>>
-function main_php_reserved_name_array_1() {
 if (false) {
   class Array {}
 }
 echo "Done\n";
-}

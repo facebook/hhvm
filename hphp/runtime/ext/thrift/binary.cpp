132457932cfee5f1eb1f7468c9045f4fe2516b07
Joseph Griego <jgriego@fb.com>
2018-09-11 07:43:49 -0700

    Suppress Hack arr notices in thrift binary deserialization
    Summary:
    binary_serialize constructs a PHP array temporarily when it serializes
    a thrift map--for Hack `Map`s with intish keys, this raises a notice
    
    Reviewed By: ricklavoie
    
    Differential Revision: D9759729
    
    fbshipit-source-id: f289f5870930b34349f2f60332453bb6df4a0df0

--- hphp/runtime/ext/thrift/binary.cpp
+++ hphp/runtime/ext/thrift/binary.cpp
@@ -696,6 +696,8 @@ void HHVM_FUNCTION(thrift_protocol_write_binary,
 
   const Object& obj_request_struct = request_struct;
 
+  SuppressHackArrCompatNotices suppressNotices;
+
   Variant spec(get_tspec(obj_request_struct->getVMClass()));
   binary_serialize_spec(obj_request_struct, transport, spec.toArray());
 

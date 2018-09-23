38aef93b8b54ee09bc41e5fa46f8a5095d4d5c07
Calvin Fernandez <cfernand@fb.com>
2018-09-22 23:46:37 -0700

    Remove js_env
    Reviewed By: oulgen
    
    Differential Revision: D9950029
    
    fbshipit-source-id: b7c0fb1bdf090591a2437f09f92f9e1759ca7b5d

--- hphp/hack/src/hhbc/hhbc_options.ml
+++ hphp/hack/src/hhbc/hhbc_options.ml
@@ -47,7 +47,6 @@ type t = {
   option_enable_reified_generics          : bool;
   option_enable_intrinsics_extension      : bool;
   option_enable_hhjs                      : bool;
-  option_hhjs_env_import_path             : string;
   option_phpism_undefined_const_as_string : bool;
   option_phpism_undefined_const_fallback  : bool;
 }
@@ -90,7 +89,6 @@ let default = {
   option_enable_reified_generics = false;
   option_enable_intrinsics_extension = false;
   option_enable_hhjs = false;
-  option_hhjs_env_import_path = "";
   option_phpism_undefined_const_as_string = true;
   option_phpism_undefined_const_fallback = true;
 }
@@ -130,7 +128,6 @@ let disable_return_by_reference o = o.option_disable_return_by_reference
 let enable_reified_generics o = o.option_enable_reified_generics
 let enable_intrinsics_extension o = o.option_enable_intrinsics_extension
 let enable_hhjs o = o.option_enable_hhjs
-let hhjs_env_import_path o = o.option_hhjs_env_import_path
 let phpism_undefined_const_as_string o = o.option_phpism_undefined_const_as_string
 let phpism_undefined_const_fallback o = o.option_phpism_undefined_const_fallback
 let to_string o =
@@ -178,7 +175,6 @@ let to_string o =
     ; Printf.sprintf "disable_return_by_reference: %B" @@ disable_return_by_reference o
     ; Printf.sprintf "enable_intrinsics_extension: %B" @@ enable_intrinsics_extension o
     ; Printf.sprintf "enable_hhjs: %B" @@ enable_hhjs o
-    ; Printf.sprintf "hhjs_env_import_path: %s" @@ hhjs_env_import_path o
     ; Printf.sprintf "phpism_undefined_const_as_string: %B" @@ phpism_undefined_const_as_string o
     ; Printf.sprintf "phpism_undefined_const_fallback: %B" @@ phpism_undefined_const_fallback o
     ]
@@ -257,8 +253,6 @@ let set_option options name value =
     { options with option_disable_return_by_reference = as_bool value}
   | "eval.enablehhjs" ->
     { options with option_enable_hhjs = as_bool value }
-  | "eval.hhjsenvimportpath" ->
-    { options with option_hhjs_env_import_path = value }
   | "hack.lang.phpism.undefinedconstasstring" ->
     { options with option_phpism_undefined_const_as_string = as_bool value}
   | "hack.lang.phpism.undefinedconstfallback" ->
@@ -385,10 +379,8 @@ let value_setters = [
      fun opts v -> { opts with option_enable_intrinsics_extension = (v = 1) });
   (set_value "hhvm.disable_return_by_reference" get_value_from_config_int @@
      fun opts v -> { opts with option_disable_return_by_reference = (v = 1)});
-   (set_value "hhvm.enable_hhjs" get_value_from_config_int @@
+  (set_value "hhvm.enable_hhjs" get_value_from_config_int @@
      fun opts v -> { opts with option_enable_hhjs = (v = 1) });
-   (set_value "hhvm.hhjs_env_import_path" get_value_from_config_string @@
-     fun opts v -> { opts with option_hhjs_env_import_path = v });
   (set_value "hhvm.hack.lang.phpism.undefined_const_as_string" get_value_from_config_int @@
      fun opts v -> { opts with option_phpism_undefined_const_as_string = (v = 1)});
   (set_value "hhvm.hack.lang.phpism.undefined_const_fallback" get_value_from_config_int @@
--- hphp/runtime/base/runtime-option.h
+++ hphp/runtime/base/runtime-option.h
@@ -591,7 +591,6 @@ struct RuntimeOption {
   F(bool, LogThreadCreateBacktraces,   false)                           \
   F(bool, FailJitPrologs,              false)                           \
   F(bool, EnableHHJS,                  false)                           \
-  F(string, HHJSEnvImportPath,         "")                              \
   F(bool, UseHHBBC,                    !getenv("HHVM_DISABLE_HHBBC"))   \
   /* Generate warning of side effect of the pseudomain is called by     \
      top-level code.*/                                                  \

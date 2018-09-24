20f7ad9c61cc096aecfc517ce79cb902c8c7c85d
James Wu <jjwu@fb.com>
2018-09-24 09:11:09 -0700

    Extending linearization with other inheritance
    Summary:
    This adds all the other forms of inheritance to linearization.
    
    The order of linearization for a class C is as follows:
    
    1. C
    2. C's traits
    3. C's implemented interfaces
    4. C's requirements
    5. C's xhp attribute uses(if any)
    6. C's parent classes
    
    Note that at each step we depth first search, so C's trait's parents appear earlier in the linearization that C's parent.
    
    Reviewed By: kmeht
    
    Differential Revision: D9724698
    
    fbshipit-source-id: 624c067709da1dc780652d140bb2c16e5a4b76e2

--- hphp/hack/src/decl/decl_linearize.ml
+++ hphp/hack/src/decl/decl_linearize.ml
@@ -26,6 +26,11 @@ let from_class (env : Decl_env.env) (hint : Nast.hint) : result =
   let _, class_name, _ = Decl_utils.unwrap_class_hint hint in
   get_linearization env class_name
 
+
+let from_list (env : Decl_env.env) (l : Nast.hint list) (acc : result) : result =
+  List.fold_left l ~init:acc
+    ~f:(fun acc hint -> add_linearization acc (from_class env hint))
+
 let from_parent (env : Decl_env.env) (c : Nast.class_) (acc : result) : result =
   let extends =
     (* In an abstract class or a trait, we assume the interfaces
@@ -37,12 +42,22 @@ let from_parent (env : Decl_env.env) (c : Nast.class_) (acc : result) : result =
       | Ast.Ctrait -> c.c_implements @ c.c_extends @ c.c_req_implements
       | _ -> c.c_extends
   in
-  List.fold_left extends ~init:acc
-    ~f:(fun acc hint -> add_linearization acc (from_class env hint))
+  from_list env extends acc
 
 (* Linearize a class declaration given its nast *)
 let linearize (env : Decl_env.env) (c : Nast.class_) : result =
   let child = snd c.c_name in
   let acc = add_linearization [] [child] in
+  (* Add traits in backwards order *)
+  let acc = from_list env (List.rev c.c_uses) acc in
+  (* Add interfaces(interfaces can define constants)
+  TODO(jjwu): implemented interfaces are *only* important for constants and
+  otherwise don't need to take up so much space in the linearization.
+  Can we get rid of this somehow? *)
+  let acc = from_list env c.c_implements acc in
+  let acc = from_list env c.c_req_implements acc in (* Same with req_implements *)
+  (* Add requirements *)
+  let acc = from_list env c.c_req_extends acc in
+  let acc = from_list env c.c_xhp_attr_uses acc in
   let result = from_parent env c acc in
   List.rev result

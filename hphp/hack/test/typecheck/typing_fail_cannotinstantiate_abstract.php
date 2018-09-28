651f5d6700889958ac588f19a3835bcd5e9a8b46
Leo Osvald <leoo@fb.com>
2018-09-28 12:42:11 -0700

    Disallow uninstantiable types in shape field's return position
    Summary:
    - call check_instantiability on each shape field type
    - add a test that previously failed
    - add a regression test (for future-proofness)
    
    Reviewed By: jamesjwu
    
    Differential Revision: D10056126
    
    fbshipit-source-id: 79f69cb3f635f7daca41f20847c7ddeb7110e202

---
+++ hphp/hack/test/typecheck/typing_fail_cannotinstantiate_abstract.php
@@ -0,0 +1,16 @@
+<?hh // strict
+/**
+ * Copyright (c) 2018, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the "hack" directory of this source tree.
+ *
+ */
+
+abstract final class CannotBeInstantiated {}
+class TestFoo {
+
+  // this type can never be satisfied
+  public function foo(CannotBeInstantiated $c): void {}
+}
---
+++ hphp/hack/test/typecheck/typing_fail_uninstantiable_in_shape_return.php
@@ -0,0 +1,17 @@
+<?hh // strict
+/**
+ * Copyright (c) 2018, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the "hack" directory of this source tree.
+ *
+ */
+
+abstract final class CannotBeInstantiated {}
+class TestFoo {
+
+  const type TData = shape('text' => CannotBeInstantiated);
+  // this type can never be satisfied
+  public function foo(this::TData $c): void {}
+}

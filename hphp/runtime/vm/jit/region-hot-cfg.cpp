01df3669f6cefd010e1da60b20e9ea8977f9f6ba
Michael O'Farrell <michaelofarrell@fb.com>
2018-10-12 21:02:32 -0700

    Back out "add logLowPriPerfWarning when hit the per-region bytecode limit"
    Summary: Original commit changeset: 853cba23d967
    
    Reviewed By: ricklavoie
    
    Differential Revision: D10368632
    
    fbshipit-source-id: e7b8fe2bc9c57ec07fb8f29341d2148054e7b507

--- hphp/runtime/vm/jit/region-hot-cfg.cpp
+++ hphp/runtime/vm/jit/region-hot-cfg.cpp
@@ -19,7 +19,6 @@
 
 #include "hphp/util/trace.h"
 
-#include "hphp/runtime/base/perf-warning.h"
 #include "hphp/runtime/vm/jit/normalized-instruction.h"
 #include "hphp/runtime/vm/jit/prof-data.h"
 #include "hphp/runtime/vm/jit/region-prune-arcs.h"
@@ -171,17 +170,6 @@ private:
     auto tidInstrs = tidRegion->instrSize();
     if (tidInstrs > m_numBCInstrs) {
       ITRACE(5, "- visit: skipping {} due to region size\n", tid);
-      if (!m_inlining) {
-        logLowPriPerfWarning(
-          "selectHotCFG",
-          [&](StructuredLogEntry& cols) {
-            cols.setInt("maxBCInstrSize", m_numBCInstrs);
-            cols.setInt("tidRegionInstrSize", tidInstrs);
-            auto sd = m_region->start().func()->fullName();
-            cols.setStr("funcName", sd->data());
-          }
-        );
-      }
       return;
     }
 

0fca10acfa507b8f562bded57a2b004eed32bf75
Rick Lavoie <rlavoie@fb.com>
2018-10-15 22:36:36 -0700

    Improve vasm DCE
    Summary:
    Add special handling for copyargs and phijmp/phidef to the vasm dead code
    elimination pass. If these are treated like normal instructions, we'll only
    dead-code eliminate them if all of their dests are dead. If we recognize and
    handle them as a special case, we can do better. We can propagate the liveness
    of each individual dest through to its specific individual source. Moreover, if
    that dest is dead, we can remove it from the copy individually. For phis, if all
    the dests are removed, we can lower the phijmp/phidef pairs into just a jmp
    (which may allow for further simplification).
    
    Also simplify the pass a bit. It was keeping live-in sets for each block, but
    this is unnecessary. Whether a particular variable is dead is flow insensitive
    (because we're in SSA form), so we can use a single bitset for the entire unit.
    
    Reviewed By: markw65
    
    Differential Revision: D10305856
    
    fbshipit-source-id: cc70c00a83a1176224779aa55a3c9442730717f8

--- hphp/runtime/vm/jit/vasm-dead.cpp
+++ hphp/runtime/vm/jit/vasm-dead.cpp
@@ -25,6 +25,7 @@
 #include "hphp/runtime/vm/jit/vasm-visit.h"
 
 #include <boost/dynamic_bitset.hpp>
+#include <boost/range/adaptor/reversed.hpp>
 
 #include <algorithm>
 
@@ -33,8 +34,8 @@ TRACE_SET_MOD(vasm);
 namespace HPHP { namespace jit {
 
 namespace {
-typedef boost::dynamic_bitset<> LiveSet;
-bool effectful(Vinstr& inst) {
+
+bool effectful(const Vinstr& inst) {
   switch (inst.op) {
     case Vinstr::absdbl:
     case Vinstr::addl:
@@ -294,11 +295,14 @@ bool effectful(Vinstr& inst) {
   }
   always_assert(false);
 }
+
 }
 
 // Remove dead instructions by doing a traditional liveness analysis.
-// instructions that mutate memory, physical registers, or status flags
-// are considered useful. All branches are considered useful.
+// instructions that mutate memory, physical registers, or status flags are
+// considered useful. All branches are considered useful. We give copyargs and
+// phijmp/defs special treatment to allow for liveness to propagate for each
+// individual src/dst pair.
 //
 // Given SSA, there's a faster sparse version of this algorithm that marks
 // useful instructions in one pass, then transitively marks pure instructions
@@ -312,57 +316,193 @@ bool effectful(Vinstr& inst) {
 // the nearest useful post-dominator.
 void removeDeadCode(Vunit& unit) {
   Timer timer(Timer::vasm_dce);
-  auto blocks = sortBlocks(unit);
-  jit::vector<LiveSet> livein(unit.blocks.size());
-  LiveSet live(unit.next_vr);
 
-  auto pass = [&](bool mutate) {
-    bool changed = false;
-    for (auto blockIt = blocks.end(); blockIt != blocks.begin();) {
-      auto b = *--blockIt;
-      auto& block = unit.blocks[b];
-      live.reset();
-      for (auto s : succs(block)) {
-        if (!livein[s].empty()) {
-          live |= livein[s];
-        }
-      }
-      for (auto i = block.code.end(); i != block.code.begin();) {
-        auto& inst = *--i;
-        auto useful = effectful(inst);
-        visitDefs(unit, inst, [&](Vreg r) {
-          if (r.isPhys() || live.test(r)) {
-            useful = true;
-            live.reset(r);
+  assertx(check(unit));
+
+  auto const blocks = sortBlocks(unit);
+
+  boost::dynamic_bitset<> live{unit.next_vr};
+  auto const isLive = [&] (Vreg r) { return r.isPhys() || live[r]; };
+
+  auto const isLiveInst = [&] (const Vinstr& inst) {
+    if (effectful(inst)) return true;
+    auto anyLive = false;
+    visitDefs(
+      unit, inst,
+      [&] (Vreg r) { anyLive |= isLive(r); }
+    );
+    return anyLive;
+  };
+
+  // Lazily load preds as needed
+  folly::Optional<PredVector> preds;
+  auto const getPreds = [&] () -> const PredVector& {
+    if (preds) return *preds;
+    preds = computePreds(unit);
+    return *preds;
+  };
+
+  auto const analyze = [&] {
+    auto changed = false;
+    for (auto const b : boost::adaptors::reverse(blocks)) {
+      auto const& block = unit.blocks[b];
+      for (auto const& inst : boost::adaptors::reverse(block.code)) {
+        switch (inst.op) {
+          case Vinstr::copyargs: {
+            // A copyarg's source is live if only its corresponding dest is.
+            auto const& s = unit.tuples[inst.copyargs_.s];
+            auto const& d = unit.tuples[inst.copyargs_.d];
+            assertx(s.size() == d.size());
+            for (size_t i = 0; i < s.size(); ++i) {
+              if (!isLive(d[i]) || isLive(s[i])) continue;
+              live[s[i]] = true;
+              changed = true;
+            }
+            break;
           }
-        });
-        if (useful) {
-          visitUses(unit, inst, [&](Vreg r) {
-            live.set(r);
-          });
-        } else if (mutate) {
-          inst = nop{};
-          changed = true;
+          case Vinstr::phijmp: {
+            // A phijmp's source is live if the corresponding dest in the phidef
+            // is.
+            auto const& s = unit.tuples[inst.phijmp_.uses];
+            auto const& phidef = unit.blocks[inst.phijmp_.target].code.front();
+            assertx(phidef.op == Vinstr::phidef);
+            auto const& d = unit.tuples[phidef.phidef_.defs];
+            assertx(d.size() == s.size());
+            for (size_t i = 0; i < d.size(); ++i) {
+              if (!isLive(d[i]) || isLive(s[i])) continue;
+              live[s[i]] = true;
+              changed = true;
+            }
+            break;
+          }
+          case Vinstr::phidef: // Processed as part of phijmp above
+            break;
+          default:
+            // Otherwise the sources are only live if all of the dests are.
+            if (!isLiveInst(inst)) break;
+            visitUses(
+              unit, inst,
+              [&] (Vreg r) {
+                if (isLive(r)) return;
+                live[r] = true;
+                changed = true;
+              }
+            );
+            break;
         }
       }
-      if (mutate) {
-        assertx(live == livein[b]);
-      } else {
-        if (live != livein[b]) {
-          livein[b] = live;
-          changed = true;
+    }
+
+    return changed;
+  };
+
+  auto const remove = [&] {
+    auto changed = false;
+    for (auto const b : blocks) {
+      auto& block = unit.blocks[b];
+      for (auto& inst : block.code) {
+        switch (inst.op) {
+          case Vinstr::copyargs: {
+            // Remove dead source/dest pairs from copyargs.
+            auto const& s = unit.tuples[inst.copyargs_.s];
+            auto const& d = unit.tuples[inst.copyargs_.d];
+            assertx(s.size() == d.size());
+            if (!d.empty() && std::all_of(d.begin(), d.end(), isLive)) break;
+
+            VregList newSrcs;
+            VregList newDsts;
+            for (size_t i = 0; i < s.size(); ++i) {
+              if (!isLive(d[i])) continue;
+              assertx(isLive(s[i]));
+              newSrcs.emplace_back(s[i]);
+              newDsts.emplace_back(d[i]);
+            }
+
+            // If no pairs remain, turn it into a nop. If there's a single pair
+            // left, a copy. Otherwise just shrink the argument tuples.
+            assertx(newSrcs.size() == newDsts.size());
+            if (newDsts.empty()) {
+              inst = nop{};
+            } else if (newDsts.size() == 1) {
+              inst.op = Vinstr::copy;
+              inst.copy_ = copy{newSrcs[0], newDsts[0]};
+            } else {
+              inst.copyargs_.s = unit.makeTuple(std::move(newSrcs));
+              inst.copyargs_.d = unit.makeTuple(std::move(newDsts));
+            }
+            changed = true;
+            break;
+          }
+          case Vinstr::phidef: {
+            // Remove dead source/dest pairs from phidef/phijmp.
+            auto d = &unit.tuples[inst.phidef_.defs];
+            if (!d->empty() && std::all_of(d->begin(), d->end(), isLive)) break;
+
+            // First remove dead sources from the phijmps in all the
+            // predecessors.
+            for (auto const pred : getPreds()[b]) {
+              auto& phijmp = unit.blocks[pred].code.back();
+              assertx(phijmp.op == Vinstr::phijmp);
+              auto const& s = unit.tuples[phijmp.phijmp_.uses];
+              assertx(s.size() == d->size());
+
+              VregList newSrcs;
+              for (size_t i = 0; i < d->size(); ++i) {
+                if (!isLive((*d)[i])) continue;
+                assertx(isLive(s[i]));
+                newSrcs.emplace_back(s[i]);
+              }
+
+              // If there's no sources left, just leave a jmp. Otherwise shrink
+              // the argument tuple.
+              if (newSrcs.empty()) {
+                assertx(phijmp.phijmp_.target == b);
+                phijmp.op = Vinstr::jmp;
+                phijmp.jmp_.target = b;
+              } else {
+                phijmp.phijmp_.uses = unit.makeTuple(std::move(newSrcs));
+                // makeTuple may have invalidated d
+                d = &unit.tuples[inst.phidef_.defs];
+              }
+            }
+
+            // Now likewise shrink the dests in the phidef
+            VregList newDsts;
+            for (size_t i = 0; i < d->size(); ++i) {
+              if (!isLive((*d)[i])) continue;
+              newDsts.emplace_back((*d)[i]);
+            }
+
+            if (newDsts.empty()) {
+              inst = nop{};
+            } else {
+              inst.phidef_.defs = unit.makeTuple(std::move(newDsts));
+            }
+
+            changed = true;
+            break;
+          }
+          case Vinstr::phijmp: // Processed as part of the phidef above.
+            break;
+          default:
+            // Otherwise change the instruction into a nop if all of its dests
+            // are dead.
+            if (isLiveInst(inst)) continue;
+            changed = true;
+            inst = nop{};
+            break;
         }
       }
     }
     return changed;
   };
 
-  // analyze until livein reaches a fixed point
-  while (pass(false)) {}
-  auto const changed = pass(true);
+  while (analyze()) {}
+  auto const changed = remove();
   removeTrivialNops(unit);
   if (changed) {
     printUnit(kVasmDCELevel, "after vasm-dead", unit);
+    assertx(check(unit));
   }
 }
 

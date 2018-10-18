f2bb2c989d27414bbc6b35fb1381ba6017d66234
Catherine Gasnier <catg@fb.com>
2018-10-18 05:01:10 -0700

    splitting lint.ml
    Summary: This file might grow a lot as errors are added, so let's move out the important parts so we can find them more easily.
    
    Reviewed By: Wilfred
    
    Differential Revision: D10334730
    
    fbshipit-source-id: ecbe7011fccf99ce0296c22424c7855e7b9b3ea6

---
+++ hphp/hack/src/utils/lint/lint_codes.ml
@@ -0,0 +1,17 @@
+(**
+ * Copyright (c) 2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the "hack" directory of this source tree.
+ *
+ *)
+
+let lowercase_constant                    = 5001 (* DONT MODIFY!!!! *)
+let use_collection_literal                = 5002 (* DONT MODIFY!!!! *)
+let static_string                         = 5003 (* DONT MODIFY!!!! *)
+let shape_idx_required_field              = 5005 (* DONT MODIFY!!!! *)
+
+(* Values 5501 - 5999 are reserved for FB-internal use *)
+
+(* EXTEND HERE WITH NEW VALUES IF NEEDED *)

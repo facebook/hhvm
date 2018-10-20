62d5ab29984d8e8f2a49566be5698241a10f3c37
Jake Bailey (Hacklang) <jakebailey@fb.com>
2018-10-19 19:37:39 -0700

    Visit shape keys in AAST visitors
    Summary:
    It looks like visitation of shape keys was overlooked in D6481759--these base visitors currently only visit ShapeMap values. After this change, we will visit both keys and values.
    
    The `endo` visitor is meant to be equivalent to `map`, but should avoid allocating new nodes when unnecessary. We don't use `endo` visitors much, and the existing implementation already reallocated unconditionally, so I haven't taken the time to write a proper `endo` implementation and called out the problem with a FIXME instead.
    
    Reviewed By: arxanas
    
    Differential Revision: D10470531
    
    fbshipit-source-id: 596cdaab71b46abb361eb85f6aef48a43f22e45e

--- hphp/hack/src/annotated_ast/aast_defs_visitors_ancestors.ml
+++ hphp/hack/src/annotated_ast/aast_defs_visitors_ancestors.ml
@@ -12,35 +12,52 @@ module SM = Ast.ShapeMap
 class ['self] iter_defs_base = object (self : 'self)
   inherit [_] Ast.iter_defs
   method private on_shape_map
-    : 'env 'a . ('env -> 'a -> unit) -> 'env -> 'a SM.t -> unit
+    : 'a . ('env -> 'a -> unit) -> 'env -> 'a SM.t -> unit
     = fun f env x -> SM.iter (self#on_shape_map_entry f env) x
   method private on_shape_map_entry
-    : 'env 'a . ('env -> 'a -> unit) -> 'env -> SM.key -> 'a -> unit
-    = fun f env _key data -> f env data
+    : 'a . ('env -> 'a -> unit) -> 'env -> SM.key -> 'a -> unit
+    = fun f env key data ->
+      self#on_shape_field_name env key;
+      f env data
 end
 
 class virtual ['self] reduce_defs_base = object (self : 'self)
   inherit [_] Ast.reduce_defs
   method private on_shape_map
-    : 'env 'a . ('env -> 'a -> 'acc) -> 'env -> 'a SM.t -> 'acc
+    : 'a . ('env -> 'a -> 'acc) -> 'env -> 'a SM.t -> 'acc
     = fun f env x ->
       SM.fold (fun k d acc -> self#plus acc (self#on_shape_map_entry f env k d))
         x self#zero
   method private on_shape_map_entry
-    : 'env 'a . ('env -> 'a -> 'acc) -> 'env -> SM.key -> 'a -> 'acc
-    = fun f env _key data -> f env data
+    : 'a . ('env -> 'a -> 'acc) -> 'env -> SM.key -> 'a -> 'acc
+    = fun f env key data ->
+      self#plus (self#on_shape_field_name env key) (f env data)
 end
 
-class ['self] map_defs_base = object (_self : 'self)
+class ['self] map_defs_base = object (self : 'self)
   inherit [_] Ast.map_defs
   method private on_shape_map
-    : 'env 'a 'b . ('env -> 'a -> 'b) -> 'env -> 'a SM.t -> 'b SM.t
-    = fun f env -> SM.map (f env)
+    : 'a 'b . ('env -> 'a -> 'b) -> 'env -> 'a SM.t -> 'b SM.t
+    = fun f env x ->
+      let map_entry key data acc =
+        let key = self#on_shape_field_name env key in
+        let data = f env data in
+        SM.add key data acc
+      in
+      SM.fold map_entry x SM.empty
 end
 
-class ['self] endo_defs_base = object (_self : 'self)
+class ['self] endo_defs_base = object (self : 'self)
   inherit [_] Ast.endo_defs
   method private on_shape_map
-    : 'env 'a . ('env -> 'a -> 'a) -> 'env -> 'a SM.t -> 'a SM.t
-    = fun f env -> SM.map (f env)
+    : 'a 'b . ('env -> 'a -> 'b) -> 'env -> 'a SM.t -> 'b SM.t
+    = fun f env x ->
+      (* FIXME: Should be possible to write a true (more efficient) endo
+         implementation rather than copying map *)
+      let map_entry key data acc =
+        let key = self#on_shape_field_name env key in
+        let data = f env data in
+        SM.add key data acc
+      in
+      SM.fold map_entry x SM.empty
 end

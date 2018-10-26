4881190ed514ba7a32f23ec786e0bdafd1950266
Waleed Khan <waleedk@fb.com>
2018-10-26 15:20:14 -0700

    Fix `inout` parameters for lambdas
    Summary: Previously, these would reverse the resulting `inout` parameters such that if you passed in 1, 2, 3, they would be reassigned to 3, 2, 1.
    
    Reviewed By: alexeyt, jamesjwu
    
    Differential Revision: D10846752
    
    fbshipit-source-id: 28fdf531fce2e84ed49b50a2803a334f990e771f

---
+++ hphp/test/quick/inout-closure.php
@@ -0,0 +1,15 @@
+<?hh
+$closure = (inout $a, inout $b, inout $c) ==> {
+  print("In closure: $a, $b, $c\n");
+};
+
+$a = 'hello';
+$b = 'world';
+$c = 8;
+
+$closure(
+  inout $a,
+  inout $b,
+  inout $c,
+);
+print("After closure: $a, $b, $c\n");

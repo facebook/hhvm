521af6f24d5d553a0edaf03a9ebac28d58121b9b
Kunal Mehta <kunalm@fb.com>
2018-10-29 11:20:19 -0700

    Fix preg_match for HSL flag + offset capture
    Summary: For the HSL, we added a flag that fixed a bug in PHP's preg library in which ummatched groups would sometimes be omitted from the result. However, we didn't take the `PREG_OFFSET_CAPTURE` flag into account, which adds arrays instead of strings.
    
    Reviewed By: fredemmott, alexeyt
    
    Differential Revision: D12677349
    
    fbshipit-source-id: b51c072824dbcd351d236f31517ca7de3f046974

--- hphp/test/slow/ext_hsl/ext_regex.php
+++ hphp/test/slow/ext_hsl/ext_regex.php
@@ -2,13 +2,7 @@
 
 use namespace HH\Lib\Regex;
 
-function f(Regex\Pattern $pattern): void {
-  echo($pattern);
-}
-
-f("hello\n");
-
-$patterns = vec[
+const PATTERNS = vec[
   tuple('/foo(bar)?/', 'foo'),
   tuple('/foo(?<bar>bar)?/', 'foo'),
   tuple('/foo(?<bar>bar)?(baz)?/', 'foo'),
@@ -17,12 +11,22 @@ $patterns = vec[
   tuple('/foo(?<bar>bar)?(baz)?/', 'foobarbaz'),
 ];
 
-foreach ($patterns as list($pattern, $text)) {
-  $matches = [];
-  preg_match($pattern, $text, &$matches);
-  printf("---\nRE: %s\nIn: %s\nPHP:\n", $pattern, $text);
-  var_dump($matches);
-  preg_match($pattern, $text, &$matches, PREG_FB__PRIVATE__HSL_IMPL);
-  printf("HSL:\n", $pattern, $text);
-  var_dump($matches);
+function test(int $flags): void {
+  foreach (PATTERNS as list($pattern, $text)) {
+    $matches = [];
+    preg_match($pattern, $text, &$matches, $flags);
+    printf("---\nRE: %s\nIn: %s\nPHP:\n", $pattern, $text);
+    var_dump($matches);
+    preg_match($pattern, $text, &$matches, PREG_FB__PRIVATE__HSL_IMPL | $flags);
+    printf("HSL:\n", $pattern, $text);
+    var_dump($matches);
+  }
+}
+
+<<__EntryPoint>>
+function main(): void {
+  printf("Matches Only:\n");
+  test(0);
+  printf("Matches with Offsets:\n");
+  test(PREG_OFFSET_CAPTURE);
 }

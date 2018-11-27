cdfe901964306e907c01c70643719890a81d0d5e
Andrew Kennedy <akenn@fb.com>
2018-11-27 14:48:04 -0800

    Don't add redundant bounds to tvenv
    Summary: As with `tpenv`, it's sensible to avoid adding redundant upper and lower bounds, by passing the `intersect` and `union` parameters into `add_tyvar_upper_bound` etc.
    
    Reviewed By: manzyuk
    
    Differential Revision: D13213945
    
    fbshipit-source-id: 60a6121367812dfa57197850b7a7c05b9d943138

--- hphp/hack/src/typing/typing_subtype.ml
+++ hphp/hack/src/typing/typing_subtype.ml
@@ -1550,7 +1550,7 @@ and props_to_env env remain props =
       [Log_type ("var", ty'); Log_type ("ty", ty)])]));
     let tyl = Typing_set.elements (Env.get_tyvar_lower_bounds env var) in
     let env = List.fold_left ~f:(fun env ty_sub -> sub_type env ty_sub ty) ~init:env tyl in
-    props_to_env (Env.add_tyvar_upper_bound env var ty) remain props
+    props_to_env (Env.add_tyvar_upper_bound ~intersect:(try_intersect env) env var ty) remain props
   | TL.IsSubtype (ty, ((r, Tvar var) as ty')) :: props ->
     (* Add a new lower bound ty on var. Apply transitivity of sutyping, so if we
      * already have var <: tyl then check that for each ty_super in tyl we
@@ -1562,7 +1562,7 @@ and props_to_env env remain props =
       [Log_type ("var", ty'); Log_type ("ty", ty)])]));
     let tyl = Typing_set.elements (Env.get_tyvar_upper_bounds env var) in
     let env = List.fold_left ~f:(fun env ty_super -> sub_type env ty ty_super) ~init:env tyl in
-    props_to_env (Env.add_tyvar_lower_bound env var ty) remain props
+    props_to_env (Env.add_tyvar_lower_bound ~union:(try_union env) env var ty) remain props
   | TL.Conj props' :: props ->
     props_to_env env remain (props' @ props)
   | prop :: props ->
@@ -1853,7 +1853,7 @@ and try_intersect env ty tyl =
  * 3. It can be assumed that the original list contains no redundancy.
  * TODO: there are many more unions to implement yet.
  *)
-let rec try_union env ty tyl =
+and try_union env ty tyl =
   match tyl with
   | [] -> [ty]
   | ty'::tyl' ->

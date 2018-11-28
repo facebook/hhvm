84f7ce4722f2ede87675e750b20b78749c165658
Catherine Gasnier <catg@fb.com>
2018-11-28 09:28:00 -0800

    link to built lz4 and sqlite3 (#8390)
    Summary:
    Pull Request resolved: https://github.com/facebook/hhvm/pull/8390
    
    This should fix external linux builds.
    
    Make dune build with the libraries built elsewhere with CMake if there is no system version or if the system version is not new enough.
    
    Fixes facebook/hhvm#8386
    
    Differential Revision: D13234118
    
    fbshipit-source-id: 8fbb32617dcc9d877e5cd2ab611ae2201e264645

--- hphp/hack/oss_build_quick_test.sh
+++ hphp/hack/oss_build_quick_test.sh
@@ -4,6 +4,6 @@ HACKDIR=$(realpath $(dirname "$0"))
 SKIP_OPAM=${SKIP_OPAM:-0}
 
 cmake "$HACKDIR" -Wno-dev -DOCAML_EXECUTABLE="$(command -v ocaml)" \
-  -DSKIP_OPAM="${SKIP_OPAM}"
+  -DSKIP_OPAM="${SKIP_OPAM}" -DUSE_GLOBAL_LIBS=1
 cd "$HACKDIR" && make hack
 hg rev "$HACKDIR/src/hhi/.merlin" "$HACKDIR/src/ppx/.merlin"

062c45a1ef454e5f8a1cf463e1e162a4f1318c5d
Sasha Manzyuk <manzyuk@fb.com>
2018-12-05 10:33:57 -0800

    Add another test case under typecheck/new_inference/falsepos
    Summary: T25936918 contains an example that fails to type-check with the legacy type-checker even though the code follows Hack's type rules.  I thought this example would require search when solving constraints but apparently it doesn't!  Sticking `hh_show_env()` confirms that the types are resolved correctly.
    
    Reviewed By: andrewjkennedy
    
    Differential Revision: D13341992
    
    fbshipit-source-id: bedd6e8528dd57c7730211e1455465ed6ae82a0c

---
+++ hphp/hack/test/typecheck/new_inference/falsepos/t25936918.php
@@ -0,0 +1,14 @@
+<?hh // strict
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+function f<Tk>(Vector<?Tk> $x): Vector<Tk> {
+  return Vector {};
+}
+
+function test1(Vector<?int> $x): Vector<?int> {
+  return f($x);
+}
+
+function test2(Vector<?int> $x): Vector<int> {
+  return f($x);
+}

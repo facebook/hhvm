9f9e7b2d073e90e155f6fe0e7aa3abdeacf7e19f
Andrew Kennedy <akenn@fb.com>
2018-12-06 06:37:24 -0800

    Another test for constraint based inference
    Summary: This test file passes the current type-checker: it should not, as it demonstrates an unsoundness. (Test was devised during development of D13277972). Fortunately, constraint-based type inference rejects it.
    
    Reviewed By: Wilfred
    
    Differential Revision: D13339781
    
    fbshipit-source-id: f3cdd1f3cfd3d75471f6609236d100e77df5b632

---
+++ hphp/hack/test/typecheck/new_inference/typehole/merge_tvars2.php
@@ -0,0 +1,20 @@
+<?hh // strict
+
+class A<+T1, -T2 as T1> {
+  private ?T2 $contents;
+  public function __construct() {}
+  public function put(T2 $x): void { $this->contents = $x; }
+  public function get(): ?T1 {
+    return $this->contents;
+  }
+}
+
+function test(): void {
+  $x = new A(); // x : A<v1, v2>, -v2 <: +v1
+  $x->put(0); // int <: -v2
+  take_string_opt($x->get()); // +v1 <: string -> should error
+}
+
+function take_string_opt(?string $x): void {
+  var_dump($x);
+}

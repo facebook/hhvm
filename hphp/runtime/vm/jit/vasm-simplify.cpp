facebook/hhvm

commit c02ec574cdae149eed5d1795b2349428ecac37c2

combine two leas where the second one uses the result of the first one
Summary:
combine 2 leas of the form
lea disp1(%reg1), %reg2
lea disp2(%reg2), %reg3
into lea disp1+disp2(%reg1), %reg3
where there is no other use of %reg2.
This works both for vregs and Physregs.

Reviewed By: markw65

Differential Revision: D13015606

fbshipit-source-id: 12c3ee203da68d5fbf59aeae21b984fa48fdf1d8

hphp/runtime/vm/jit/vasm-simplify.cpp
@@ -660,6 +660,63 @@ bool simplify(Env& env, const addq& vadd, Vlabel b, size_t i) {
   return false;
 }
 
+// find two lea that add an immediate to a register and fold into a
+// single operation.  given this lea, look for a subsequent lea
+// within the same block whose base reg is this lea's dest reg,
+// and it is the only use of this lea's dest reg.
+//
+// first, try to simplify lea (%r1), %r2 into copy %r1,%r2
+bool simplify(Env& env, const lea& vlea, Vlabel b, size_t i) {
+  if (vlea.s.disp == 0 && !vlea.s.index.isValid()) {
+    env.unit.blocks[b].code[i] = copy{ vlea.s.base, vlea.d };
+    return true;
+  }
+  auto xinst = env.unit.blocks[b].code[i];
+  bool found_second = false;
+  size_t x;
+  int disp2;
+  if (vlea.d.isPhys()) {
+    bool found_interf = false;
+    for (x = i+1; x < env.unit.blocks[b].code.size(); ++x) {
+      xinst = env.unit.blocks[b].code[x];
+      if (xinst.op == Vinstr::lea && xinst.lea_.s.base == vlea.d &&
+          xinst.lea_.d == vlea.d && !(xinst.lea_.s.index.isValid())) {
+        found_second = true;
+        disp2 = xinst.lea_.s.disp;
+        break;
+      } else {
+        visitUses(env.unit, xinst,
+            [&] (Vreg r) { if (r == vlea.d) found_interf = true; });
+        if (found_interf) return false;
+        visitDefs(env.unit, xinst,
+            [&] (Vreg r) { if (r == vlea.d) found_interf = true; });
+        if (found_interf) return false;
+      }
+    }
+  } else {
+    if (env.use_counts[vlea.d] == 1) {
+      for (x = i+1 ; x < env.unit.blocks[b].code.size(); ++x) {
+        xinst = env.unit.blocks[b].code[x];
+        if ((xinst.op == Vinstr::lea) && (xinst.lea_.s.base == vlea.d) &&
+              !xinst.lea_.s.index.isValid()) {
+          found_second = true;
+          disp2 = xinst.lea_.s.disp;
+          break;
+        }
+      }
+    }
+  }
+  if (found_second &&
+      deltaFits((int64_t)(vlea.s.disp) + (int64_t)(disp2), sz::dword)) {
+     (void) simplify_impl(env, b, i,
+         lea { vlea.s+disp2, (xinst).lea_.d });
+     // update uses and delete the inst
+     (void) simplify_impl(env, b, x, [&] (Vout& v) { return 1; });
+     return true;
+  }
+  return false;
+}
+
 // remove compares with unused results. This overlaps with removeDeadCode,
 // but does it earlier.
 template <typename Cmp>

commit 69f3b813a1241bde39116597fed4987012465021

predicate test to avoid issues on aarch64 (#8398)
Summary:
This commit https://github.com/facebook/hhvm/commit/c02ec574cdae149eed5d1795b2349428ecac37c2
broke the regression suite on aarch64 hosts.  The first transformation removes a form expected by
the service requests code.  An internal assert correctly detected this condition.

/data2/swalk/play.120518a/hhvm/hphp/runtime/vm/jit/service-requests.cpp:280: HPHP::jit::FPInvOffset HPHP::jit::svcreq::extract_spoff(HPHP::jit::TCA): assertion `false && "Expected an instruction that offsets SP"' failed.
Core dumped: Aborted

A full MOP was run.  The results reflected the new baseline as documented in issue #8396.

This fixes issue #8397
Pull Request resolved: https://github.com/facebook/hhvm/pull/8398

Differential Revision: D13362114

Pulled By: fredemmott

fbshipit-source-id: ce4b1cfb1f7b94c01355e7b983a47fccee3b7a48

hphp/runtime/vm/jit/vasm-simplify.cpp
@@ -667,7 +667,8 @@ bool simplify(Env& env, const addq& vadd, Vlabel b, size_t i) {
 //
 // first, try to simplify lea (%r1), %r2 into copy %r1,%r2
 bool simplify(Env& env, const lea& vlea, Vlabel b, size_t i) {
-  if (vlea.s.disp == 0 && !vlea.s.index.isValid()) {
+  if (vlea.s.disp == 0 && !vlea.s.index.isValid() &&
+    arch() != Arch::ARM) {
     env.unit.blocks[b].code[i] = copy{ vlea.s.base, vlea.d };
     return true;
   }
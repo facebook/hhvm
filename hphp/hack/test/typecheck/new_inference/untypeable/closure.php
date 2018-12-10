58c734790142550679eee99a075e162f5d0827b5
Sasha Manzyuk <manzyuk@fb.com>
2018-12-10 02:29:34 -0800

    Add an example of safe but untypeable code
    Summary: Add an example of safe but untypeable code that is accidentally accepted by the legacy type-checker but is correctly rejected by the constraint-based inference.
    
    Reviewed By: andrewjkennedy
    
    Differential Revision: D13379104
    
    fbshipit-source-id: 9caaca138ec24ba8cba6930b43446f91567d833a

---
+++ hphp/hack/test/typecheck/new_inference/untypeable/closure.php
@@ -0,0 +1,26 @@
+<?hh // strict
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+class Ref<T> {
+  private ?T $value = null;
+  public function get(T $default): T {
+    return $this->value ?? $default;
+  }
+  public function set(T $value): void {
+    $this->value = $value;
+  }
+}
+
+function getter<T>(Ref<T> $r): (function(T): T) {
+  return $x ==> $r->get($x);
+}
+
+function expect_string(string $_): void {}
+
+function test(int $a, string $b): void {
+  $r = new Ref();
+  $f = getter($r);
+  $c = $f($b);
+  $r->set($a);
+  expect_string($c);
+}

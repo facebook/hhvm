53cfbf17efa204eb99f2caec1bb2e87448707a38
Steve Walk <steve.walk@cavium.com>
2018-12-12 08:07:53 -0800

    revise callstubs defs for aarch64 target

--- hphp/runtime/vm/jit/reg-alloc.cpp
+++ hphp/runtime/vm/jit/reg-alloc.cpp
@@ -233,8 +233,10 @@ void getEffects(const Abi& abi, const Vinstr& i,
 
     case Vinstr::callstub:
       defs =
-        (abi.all() - (abi.calleeSaved | rvmfp()))
-        | jit::abi(CodeKind::CrossTrace).unreserved();
+        (abi.all() - (abi.calleeSaved | rvmfp()));
+      if (arch() != Arch::ARM) {
+        defs |= jit::abi(CodeKind::CrossTrace).unreserved();
+      }
       break;
 
     case Vinstr::callfaststub:

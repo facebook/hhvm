412f21b0da5b8777fa1bd787f3902386d7e23034
Rick Lavoie <rlavoie@fb.com>
2018-12-17 23:04:42 -0800

    Implement constant materialization in vasm-graph-color
    Summary:
    Implement the materialization of constants in vasm-graph-color. That is, turning
    uses of Vregs representing constants into normal Vregs defined by a ldimm*
    instruction. While this can be done very trivially by inserting the constants at
    the beginning of the unit, instead use dataflow to compute a more optimal
    placement. This finishes the "preparation" stage of the algorithm.
    
    Reviewed By: mxw
    
    Differential Revision: D13311853
    
    fbshipit-source-id: e32157d65636b9f633d809f634f6257ee91a3e71

--- hphp/runtime/vm/jit/vasm-graph-color.cpp
+++ hphp/runtime/vm/jit/vasm-graph-color.cpp
@@ -23,6 +23,8 @@
 
 #include "hphp/ppc64-asm/asm-ppc64.h"
 
+#include "hphp/util/dataflow-worklist.h"
+
 TRACE_SET_MOD(vasm_graph_color);
 
 namespace HPHP { namespace jit {
@@ -284,6 +286,495 @@ State make_state(Vunit& unit, const Abi& abi) {
 // Pre-allocation preparation
 
 /*
+ * Constant placement
+ *
+ * Vregs representing constants need to be materialized and placed in the unit
+ * before register allocation. Each constant needs to be materialized before its
+ * first usage. The easiest thing would be to just place constants in the entry
+ * block, but that's not optimal.
+ *
+ * Ideally a constant should be (1) placed as close to possible to its uses (to
+ * minimize register pressure), (2) as few times as possible (to minimize code
+ * size), and should not be (3) placed on a path where it may not be used (to
+ * minimize useless work).
+ *
+ * (1) can be dealt with by starting at the entry block and then sinking the
+ * definitions as far as possible using standard dataflow. This will prevent us
+ * from sinking constant definitions inside loops, which is desirable, and
+ * satisfies (3), but tends to violate (2). With no other constraints, the
+ * definitions will tend to be sunk to immediately before all their usages,
+ * leading to code duplication.
+ *
+ * To prevent this (and satisfy (2)), we first calculate "sink stops". These
+ * indicate that at a particular block, a particular Vreg (representing the
+ * constant) should not be sunk further.
+ *
+ * The sink stops for a constant are the post-dominator frontier of all usages
+ * of that constant. That is, the blocks where the constant is used on all
+ * successors from that block (or in the block itself), but where the
+ * predecessor is not a sink stop for that constant. This captures (2) and (3)
+ * as we will not place the constant where there's a path where it might be
+ * used, but if the constant is used on all paths, we won't duplicate it along
+ * those paths.
+ *
+ * Once sink stops are calculated, we start at the entry block and attempt to
+ * sink each constant as far as possible. We use typical partial redundancy
+ * dataflow for this and stop sinking at a sink stop, or if we encounter a use
+ * of that constant. In addition, we use liveness of the constants to avoid
+ * sinking down paths where the constant isn't used at all.
+ *
+ * Once we stop sinking, we insert the appropriate "ldimm" vasm instruction
+ * (writing to its associated Vreg) at that point to materialize the constant. A
+ * given constant (which has a single Vreg) may be placed at multiple points,
+ * which means that Vreg now has multiple definitions. This is fine, as the unit
+ * will be rewritten into SSA form anyways, which will fix this.
+ */
+
+struct PlaceConstantsBlockInfo {
+  VregSet stopSink;
+  VregSet liveIn;
+};
+
+// Calculate sink stop and liveness information for each constant for every
+// block, returning an empty vector if there's no constants.
+jit::vector<PlaceConstantsBlockInfo>
+compute_place_constants_block_info(const State& state) {
+  auto const& unit = state.unit;
+  auto const& rpo = state.rpo;
+  auto const& rpoOrder = state.rpoOrder;
+  auto const& preds = state.preds;
+
+  if (unit.regToConst.empty()) return {};
+
+  /* Per-block dataflow state:
+   *
+   * We keep track of two things for dataflow:
+   *
+   * definitelyUsed - True if a register is used in the block, or if
+   *                  definitelyUsed for all successors.
+   *
+   * nearestUse - This block if the register is used by this block, or if
+   *              there's different blocks in the uses state among the
+   *              successors. Otherwise its the same as all the successors' uses
+   *              block.
+   */
+  struct BlockState {
+    // Mapping of a constant to its nearest usage (or union of multiple usages)
+    // at this block
+    jit::fast_map<Vreg, Vlabel> nearestUse;
+    // Set of constants which are used along all paths from this block
+    VregSet definitelyUsed;
+    bool operator==(const BlockState& o) const {
+      return std::tie(nearestUse, definitelyUsed) ==
+        std::tie(o.nearestUse, o.definitelyUsed);
+    }
+    bool operator!=(const BlockState& o) const { return !(*this == o); }
+  };
+
+  // Dataflow bock state. As an optimization, we store the state in a copy_ptr,
+  // which lets us share it between blocks cheaply. We use an empty copy_ptr to
+  // represent the empty state.
+  jit::vector<copy_ptr<BlockState>> blockIn(unit.blocks.size());
+
+  // Mapping of blocks to the constants used in that block
+  jit::vector<VregSet> blockUses(unit.blocks.size());
+
+  dataflow_worklist<size_t, std::less<size_t>> worklist(rpo.size());
+
+  boost::dynamic_bitset<> visited(unit.blocks.size());
+  visited.flip();
+
+  // Populate the constant usages for each block. Since this is a backwards
+  // dataflow, start the worklist with successor-less blocks (the exits).
+  auto foundUse = false;
+  for (size_t i = 0; i < rpo.size(); ++i) {
+    auto const b = rpo[i];
+    auto const& block = unit.blocks[b];
+    auto& uses = blockUses[b];
+
+    for (auto const& inst : block.code) {
+      visitUses(
+        unit,
+        inst,
+        [&] (Vreg r) { if (unit.regToConst.count(r)) uses.add(r); }
+      );
+
+      // A constant should never have a definition (before we place them).
+      if (debug) {
+        visitDefs(
+          unit,
+          inst,
+          [&] (Vreg r) { always_assert(!unit.regToConst.count(r)); }
+        );
+      }
+    }
+
+    visited[b] = false;
+    if (!foundUse && uses.any()) foundUse = true;
+    if (succs(block).size() == 0) worklist.push(i);
+  }
+
+  // The unit has constants but they're never actually used. Bail out.
+  if (!foundUse) return {};
+
+  while (!worklist.empty()) {
+    auto const b = rpo[worklist.pop()];
+    auto const& block = unit.blocks[b];
+
+    auto const successors = succs(block);
+
+    // All successors are identical if there's no adjacent successors which are
+    // different.
+    auto const allSuccsIdentical =
+      std::adjacent_find(
+        successors.begin(),
+        successors.end(),
+        [&] (Vlabel s1, Vlabel s2) {
+          // NB: We're checking if the successors are different, so the return
+          // values are inverted.
+          if (!visited[s1] || !visited[s2]) return true;
+          auto const& succState1 = blockIn[s1];
+          auto const& succState2 = blockIn[s2];
+          // Short-cut if they're the same pointer
+          if (succState1.get() == succState2.get()) return false;
+          if (!succState1 || !succState2) return true;
+          return *succState1 != *succState2;
+        }
+      ) == successors.end();
+
+    // Schedule predecessors if the state has changed. Return true if the state
+    // changed, false otherwise.
+    auto const schedulePreds = [&] (const BlockState* newState) {
+      auto const& oldState = blockIn[b];
+      assertx(!oldState || newState);
+      if (!visited[b] || (!oldState && newState) ||
+          (oldState.get() != newState && *oldState != *newState)) {
+        for (auto const pred : preds[b]) worklist.push(rpoOrder[pred]);
+        return true;
+      }
+      return false;
+    };
+
+    auto const& uses = blockUses[b];
+    if (allSuccsIdentical) {
+      // All the successors have identical state. We can just cheaply copy one
+      // of the successors state for our own (because they're in copy_ptrs).
+      assertx(successors.empty() || visited[successors[0]]);
+
+      // Copy arbitrary successor
+      auto newState = decltype(blockIn)::value_type{};
+      if (!successors.empty()) newState = blockIn[successors[0]];
+
+      if (uses.any()) {
+        // If this block has any usages, we have to mutate the state (and make a
+        // deep copy). Since this block uses the constant, that constant is
+        // definitely used at this point.
+        if (!newState) newState.emplace();
+        auto mutated = newState.mutate();
+        mutated->definitelyUsed |= uses;
+        uses.forEach(
+          [&] (Vreg r) { mutated->nearestUse.insert_or_assign(r, b); }
+        );
+      }
+      // Update this block's state and schedule any predecessors
+      if (schedulePreds(newState.get())) blockIn[b] = newState;
+    } else {
+      // Successors have differing states. We can't copy it cheaply but need to
+      // compute the union among them.
+      assertx(!successors.empty());
+
+      BlockState newState;
+      auto first = true;
+      for (auto const& succ : successors) {
+        if (!visited[succ]) continue;
+
+        auto const& succState = blockIn[succ];
+        if (!succState) {
+          // Successor doesn't use anything, so nothing from the successors is
+          // definitely used.
+          newState.definitelyUsed.reset();
+          first = false;
+          continue;
+        }
+        // Merge together usages from this successor. If we already have a usage
+        // for this constant and that usage isn't the same block, we're at a
+        // join point between distinct usages. Treat the current block as the
+        // usage point.
+        for (auto const& p : succState->nearestUse) {
+          auto const result = newState.nearestUse.emplace(p.first, p.second);
+          if (!result.second && result.first->second != p.second) {
+            result.first->second = b;
+          }
+        }
+        // Constants are definitely used only if they're definitely used by all
+        // of their successors.
+        if (first) {
+          newState.definitelyUsed = succState->definitelyUsed;
+          first = false;
+        } else {
+          newState.definitelyUsed &= succState->definitelyUsed;
+        }
+      }
+      assertx(!first);
+
+      // If this block uses a constant, that constant is definitely used at this
+      // point.
+      newState.definitelyUsed |= uses;
+      uses.forEach(
+        [&] (Vreg r) { newState.nearestUse.insert_or_assign(r, b); }
+      );
+
+      // Update this block's state and schedule any predecessors
+      if (schedulePreds(&newState)) blockIn[b].emplace(std::move(newState));
+    }
+
+    visited[b] = true;
+  };
+
+  always_assert(visited.all());
+
+  jit::vector<PlaceConstantsBlockInfo> outInfo(unit.blocks.size());
+  for (auto const b : rpo) {
+    auto const& state = blockIn[b];
+    if (!state) continue;
+
+    auto& out = outInfo[b];
+
+    for (auto const& p : state->nearestUse) {
+      // A constant is live if there's any usage from this block.
+      out.liveIn.add(p.first);
+      // We want to stop sinking at this block only if its definitely used from
+      // this point, and the nearest usage is this block. The latter condition
+      // implies that either the constant is used in this block, or there's at
+      // least two distinct use sites from this block. Since the constant is
+      // definitely used, its not profitable to sink any farther.
+      if (state->definitelyUsed[p.first] && p.second == b) {
+        out.stopSink.add(p.first);
+      }
+    }
+  }
+
+  return outInfo;
+}
+
+// Materialize constants by placing instructions to create them at the
+// appropriate points. The set of registers used is returned.
+VregSet place_constants(State& state) {
+  auto const blockInfo = compute_place_constants_block_info(state);
+  if (blockInfo.empty()) return {};
+
+  auto& unit = state.unit;
+  assertx(blockInfo.size() == unit.blocks.size());
+
+  auto const& rpo = state.rpo;
+  auto const& rpoOrder = state.rpoOrder;
+  auto const& preds = state.preds;
+
+  // The set of constants in each block which can be sunk to the beginning of
+  // that block.
+  jit::vector<VregSet> inStates(unit.blocks.size());
+  // The set of constants in each block which can be sunk to the end of that
+  // block.
+  jit::vector<VregSet> outStates(unit.blocks.size());
+  boost::dynamic_bitset<> visited(unit.blocks.size());
+
+  dataflow_worklist<size_t> worklist(rpo.size());
+
+  assertx(!rpo.empty() && rpo[0] == unit.entry);
+  worklist.push(0);
+
+  /*
+   * Dataflow:
+   *
+   * IN(B) = intersection of OUT(P) for each P in PREDS(B)
+   *
+   * (It is safe to sink a constant to the beginning of B if it can be sunk to
+   * the end of every predecessor).
+   *
+   * OUT(B) = (IN(B) - STOP(B)) & LIVE(B)
+   *
+   * (It is safe to sink a constant to the end of B if it can be sunk to the
+   * beginning of B, does not have a stop sink, and is live from B.
+   *
+   * PLACE(B) = (IN(B) & LIVE(B) & STOP(B)) | (OUT(B) - (intersection of IN(S)
+   *             for each S in SUCCS(B)))
+   *
+   * (A constant should be placed in B if it can be sunk to the beginning of B,
+   * is live in B, and is stopped in B. It should also be placed in B if it can
+   * be sunk to the end of B, and can not be sunk to the beginning of all of B's
+   * successors).
+   */
+  do {
+    auto const b = rpo[worklist.pop()];
+    auto const& info = blockInfo[b];
+    auto const& predList = preds[b];
+    auto& in = inStates[b];
+    auto& out = outStates[b];
+
+    if (b == unit.entry) {
+      assertx(predList.empty());
+      assertx(!visited[b]);
+      assertx(in.none());
+      assertx(out.none());
+      // A constant can be trivially "sunk" to the beginning of the entry block
+      // if its live.
+      in = info.liveIn;
+    } else {
+      assertx(!predList.empty());
+
+      // Find all the constants which can be sunk to the beginning of this
+      // block. A constant can be sunk to the beginning of this block if it can
+      // be sunk to the end of all of its predecessors.
+
+      in.reset();
+      // Find the first visited predecessor. There has to be at least one
+      // because we're going in RPO order.
+      auto predIt = std::find_if(
+        predList.begin(),
+        predList.end(),
+        [&] (Vlabel p) { return visited[p]; }
+      );
+      assertx(predIt != predList.end());
+
+      in = outStates[*predIt];
+      for (++predIt; predIt != predList.end(); ++predIt) {
+        if (visited[*predIt]) in &= outStates[*predIt];
+      }
+    }
+
+    // From the constants which can be sunk to the beginning of the block, find
+    // the subset which can be sunk to the end. A constant can be sunk through
+    // the block if there's not a sink stop and if its still live in the block.
+    auto newOut = in;
+    newOut -= info.stopSink;
+    newOut &= info.liveIn;
+
+    // Schedule successors if the out state has changed.
+    if (!visited[b] || newOut != out) {
+      out = std::move(newOut);
+      for (auto const succ : succs(unit.blocks[b])) {
+        worklist.push(rpoOrder[succ]);
+      }
+    }
+
+    visited[b] = true;
+  } while (!worklist.empty());
+
+  VregSet allPlaced;
+  VregSet placeThisInst;
+  for (auto const b : rpo) {
+    auto place = [&] {
+      // Find the constants which should be materialized in this block. The
+      // constant should be materialized if it can be sunk to the beginning of
+      // the block, is live in that block, and we have a sink stop.
+      auto const& info = blockInfo[b];
+      auto place = inStates[b];
+      place &= info.liveIn;
+      place &= info.stopSink;
+
+      auto const& succList = succs(unit.blocks[b]);
+      assertx(!succList.empty() || outStates[b].none());
+      if (!succList.empty()) {
+        // If a constant could be sunk to the end of the block, but not sunk to
+        // the beginning of all of its predecessors, we also need to place it
+        // here.
+        auto sunk = inStates[succList[0]];
+        for (size_t i = 1; i < succList.size(); ++i) {
+          sunk &= inStates[succList[i]];
+        }
+        auto didntSink = outStates[b];
+        didntSink -= sunk;
+        place |= didntSink;
+      }
+      return place;
+    }();
+    allPlaced |= place;
+
+    auto block = &unit.blocks[b];
+    assertx(!block->code.empty());
+
+    // Find the last index in the block where its not legal to insert the
+    // ldimm. IE, avoid the special block ending instructions which we cannot
+    // insert anything after.
+    auto stopIdx = [&]{
+      auto const valid = [] (Vinstr::Opcode op) {
+        return op != Vinstr::unwind &&
+               op != Vinstr::syncpoint &&
+               op != Vinstr::nothrow &&
+               op != Vinstr::fallthru;
+      };
+      auto i = block->code.size();
+      while (i > 0 && !valid(block->code[i-1].op)) --i;
+      return i;
+    }();
+
+    // Walk through this block, inserting ldimms immediately before a constant's
+    // use for the constants we decided to materialize here.
+    for (size_t i = 0; i < stopIdx; ++i) {
+      if (place.none()) break;
+
+      placeThisInst.reset();
+      if (i == stopIdx - 1) {
+        // We reached the end of the block. Any constants remaining are ones we
+        // couldn't sink to all predecessors. Materialize them here at the end.
+        placeThisInst |= place;
+      } else {
+        visitUses(
+          unit,
+          block->code[i],
+          [&] (Vreg r) { if (place[r]) placeThisInst.add(r); }
+        );
+      }
+
+      if (placeThisInst.none()) continue;
+
+      auto const materialize = [&] (Vout& v, Vreg r) {
+        auto const it = unit.regToConst.find(r);
+        assertx(it != unit.regToConst.end());
+        auto const& vconst = it->second;
+        // TODO (T37584483): Undef constants can be dealt with better here.
+        switch (vconst.kind) {
+          case Vconst::Quad:
+          case Vconst::Double:
+            v << ldimmq{uint64_t(vconst.val), r};
+            break;
+          case Vconst::Long:
+            v << ldimml{int32_t(vconst.val), r};
+            break;
+          case Vconst::Byte:
+            v << ldimmb{uint8_t(vconst.val), r};
+            break;
+        }
+        ++i;
+        ++stopIdx;
+      };
+
+      vmodify(
+        unit, b, i,
+        [&] (Vout& v) {
+          placeThisInst.forEach([&] (Vreg r) { materialize(v, r); });
+          return 0;
+        }
+      );
+      block = &unit.blocks[b];
+      place -= placeThisInst;
+    }
+
+    assertx(place.none());
+  }
+
+  // Constant registers no longer exist!
+  unit.regToConst.clear();
+  unit.constToReg.clear();
+
+  return allPlaced;
+}
+
+//////////////////////////////////////////////////////////////////////
+
+/*
+ * Pseudo conversion
+ *
  * Vasm instructions may have RegSets or implicit register effects (which aren't
  * reflected in the instruction's operands). Its not possible to store arbitrary
  * Vregs in these, but we need to put the program into SSA form (which will
@@ -596,6 +1087,10 @@ void prepare_unit(State& state) {
    */
   pseudo_convert(state);
 
+  // Materialize constants (this has to be done after pseudo_convert because it
+  // breaks SSA form).
+  auto toSSA = place_constants(state);
+
   VregSet unreserved{state.gpUnreserved | state.simdUnreserved};
   // Ensure that the physical registers have a definition (required for the SSA
   // restoration algorithm).
@@ -606,8 +1101,9 @@ void prepare_unit(State& state) {
       return 0;
     }
   );
+  toSSA |= unreserved;
 
-  auto const mappings = restoreSSA(state.unit, unreserved, state.rpo);
+  auto const mappings = restoreSSA(state.unit, toSSA, state.rpo);
   for (auto const& map : mappings) {
     if (!map.second.isPhys()) continue;
     // Remember what physical register these new Vregs were

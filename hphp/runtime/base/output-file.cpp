08e1f6a8cd526f7bc892206d7fde2d3351878e47
Joseph Griego <jgriego@fb.com>
2019-01-08 14:05:43 -0800

    [CVE-2019-3557] Fix OOB read in readRecord on BZ2Files/OutputFiles
    Summary:
    These File subclasses return -1 on read errors which is not what is
    expected for readImpl--this made File::readRecord behave unusually if the read
    fails, causing it to read (size_t)(-1) bytes from its stream buffer; which,
    unsurprisingly produces a out-of-bounds heap read.
    
    Reviewed By: markw65
    
    Differential Revision: D13601735

--- hphp/runtime/base/output-file.cpp
+++ hphp/runtime/base/output-file.cpp
@@ -66,7 +66,7 @@ bool OutputFile::closeImpl() {
 
 int64_t OutputFile::readImpl(char* /*buffer*/, int64_t /*length*/) {
   raise_warning("cannot read from a php://output stream");
-  return -1;
+  return 0;
 }
 
 int OutputFile::getc() {

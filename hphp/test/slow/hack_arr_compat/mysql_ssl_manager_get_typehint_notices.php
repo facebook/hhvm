b538ebb2685f4b9d43f984401340cd10c69bea78
Kevin Viratyosin <viratyosin@fb.com>
2019-01-11 13:06:39 -0800

    Change argument 1 typehint of mysql_ssl_manager_get to darray<string, mixed>
    Summary: As titled. The plain `array` typehint was resulting in notices when `vEval.HackArrCompatTypeHintNotices=true`
    
    Reviewed By: billf
    
    Differential Revision: D13600019
    
    fbshipit-source-id: a0e754464aa6ff616dce344277935d20c536795b

---
+++ hphp/test/slow/hack_arr_compat/mysql_ssl_manager_get_typehint_notices.php
@@ -0,0 +1,12 @@
+<?hh // strict
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+<<__EntryPoint>>
+function main_mysql_ssl_manager_get_typehint_notices() {
+  mysql_ssl_manager_get(
+    darray[
+      'endpoint_sys_tier' => 'dbs.60006',
+      'db_user' => 'scriptro:sys.unittestdb',
+    ],
+  );
+}

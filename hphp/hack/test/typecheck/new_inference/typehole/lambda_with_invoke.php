244e6cb6e44ff91fb564d830e89d5a22739eeab2
Andrew Kennedy <akenn@fb.com>
2019-01-25 23:56:14 -0800

    New inference: more tests
    Summary: Another example of unsoundness that is fixed by the new inference algorithm.
    
    Reviewed By: kmeht
    
    Differential Revision: D13819397
    
    fbshipit-source-id: 44928fcfea9b71aef052a598294374f70b85a146

---
+++ hphp/hack/test/typecheck/new_inference/typehole/lambda_with_invoke.php
@@ -0,0 +1,22 @@
+<?hh // strict
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+function add<T>(vec<Func<T>> $rules, T $val):void {
+  $rules[0]->apply($val);
+}
+
+abstract class Func<-T> {
+  abstract public function apply(T $x):void;
+}
+
+final class WrappedLambda<T> extends Func<T>
+{
+  public function __construct(private (function(T): void) $func) { }
+  public function apply(T $x): void {
+    $f = $this->func;
+    ($f)($x);
+  }
+}
+function testit():void {
+  add(vec[new WrappedLambda($m ==> $m->rubbish())], 4);
+}

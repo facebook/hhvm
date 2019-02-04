1ab8f7b6a14b31731667a02544d2e26daa2a3679
Maggie Moss <maggiemoss@fb.com>
2019-02-04 10:33:47 -0800

    Default compact() to a warning
    Summary: This sets the default to the warning, and is the first step to removing `compact()` completely.
    
    Reviewed By: billf
    
    Differential Revision: D13779854
    
    fbshipit-source-id: c20e12e9f6f30817d4294b5f0307a8a7ddadbf62

--- hphp/runtime/base/runtime-option.cpp
+++ hphp/runtime/base/runtime-option.cpp
@@ -696,7 +696,7 @@ int64_t RuntimeOption::HeapResetCountMultiple = 2;
 int64_t RuntimeOption::HeapLowWaterMark = 16;
 int64_t RuntimeOption::HeapHighWaterMark = 1024;
 
-uint64_t RuntimeOption::DisableCompact = 0;
+uint64_t RuntimeOption::DisableCompact = 1;
 uint64_t RuntimeOption::DisableExtract = 0;
 uint64_t RuntimeOption::DisableForwardStaticCall = 0;
 uint64_t RuntimeOption::DisableForwardStaticCallArray = 0;

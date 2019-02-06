aeb5001c285d26d48eb426f9bcac08de442f7917
Julia Pitts (they/she) <jupi@fb.com>
2019-02-05 18:06:57 -0800

    Fix issue where `assert_option_equals` wasn't properly throwing.
    Summary: One of the test functions we use, `Asserter.Make_asserter.assert_option_equals`, only throws an exception when both cases are `Some` but have different contents.  In order to be a proper assert method, it must also throw when one input is `None` and the other is `Some`.
    
    Reviewed By: arxanas
    
    Differential Revision: D13965239
    
    fbshipit-source-id: 5182bc4f3bb9f929a05f355c27618d6270955312

--- hphp/hack/test/utils/asserter/asserter.ml
+++ hphp/hack/test/utils/asserter/asserter.ml
@@ -163,11 +163,13 @@ module Make_asserter (Comp : Comparator) = struct
     | None, Some v ->
       Printf.eprintf
         "assert_option_equals failed. Expected None but got Some(%s)"
-        (Comp.to_string v)
+        (Comp.to_string v);
+      assert false
     | Some v, None ->
       Printf.eprintf
         "assert_option_equals failed. Expected Some(%s) but got None"
-        (Comp.to_string v)
+        (Comp.to_string v);
+      assert false
     | Some exp, Some actual ->
       assert_equals exp actual failure_msg
 

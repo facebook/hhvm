8ab11f06e9666ce70885f0217f06c5243abe1479
Vincent Siles <vsiles@fb.com>
2019-02-15 00:45:11 -0800

    Update dune/ocamlbuild so that hphp/hack is the new project root
    Summary:
    test and hhi files are in hphp/hack, so it's going to be easier to support
    tests with the dune build if the dune-project's root is hphp/hack too
    
    Reviewed By: CatherineGasnier
    
    Differential Revision: D14041738
    
    fbshipit-source-id: b7a64098f05b00d685e4918d47dff337fef1b48e

--- hphp/hack/opam_setup.sh
+++ hphp/hack/opam_setup.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 set -euf
 OCAML_PREFIX=$(dirname "$1")
-SRC=$2
+ROOT="$2"
+SRC="$ROOT/src"
 export PATH="$OCAML_PREFIX:$PATH"
-export OPAMROOT="$SRC/_build/.opam"
+export OPAMROOT="$ROOT/_build/.opam"
 mkdir -p "$OPAMROOT"
 export OPAMYES="1"
 MINI_TARBALL="$SRC/facebook/opam-mini-repo/opam-mini-repository.tar.gz"
-MINI_REPO="$SRC/_build/opam-mini-repository"
+MINI_REPO="$ROOT/_build/opam-mini-repository"
 
 if [ -f "$MINI_TARBALL" ]
 then
 	rm -rf "$MINI_REPO" ||:
-	tar xzf "$MINI_TARBALL" -C "$SRC/_build"
+	tar xzf "$MINI_TARBALL" -C "$ROOT/_build"
 	opam init offline_clone "$MINI_REPO" --no-setup
 else
 	opam init --no-setup

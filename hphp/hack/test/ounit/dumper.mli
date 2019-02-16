5c6a96f9ee32a516b05b86e94e4d0ac8ca14479c
Kasper Kopec <kasper@fb.com>
2019-02-15 18:18:11 -0800

    Deduplicate OUnit occurences in codebase
    Summary: `src/facebook/build/utils/oUnit.ml` and `test/ounit/oUnit.ml` are mostly identical. The first one is used in more places than the second one, so changing everyone to use it (while also moving it to more logical directory).
    
    Reviewed By: arxanas
    
    Differential Revision: D13741818
    
    fbshipit-source-id: 69ea626a526dde99237557eb417ffcb102915611

---
+++ hphp/hack/test/ounit/dumper.mli
@@ -0,0 +1,6 @@
+(* Dump an OCaml value into a printable string.
+ * By Richard W.M. Jones (rich@annexia.org).
+ * dumper.mli 1.1 2005/02/03 23:07:47 rich Exp
+ *)
+
+val dump : 'a -> string
--- hphp/hack/test/ounit/oUnit.ml
+++ hphp/hack/test/ounit/oUnit.ml
@@ -32,12 +32,12 @@ the use or other dealings in the software.
 
 let bracket set_up f tear_down () =
   let fixture = set_up () in
-    try
-      f fixture;
-      tear_down fixture
-    with e ->
-      tear_down fixture;
-      raise e
+  try
+    f fixture;
+    tear_down fixture
+  with e ->
+    tear_down fixture;
+    raise e
 
 exception Skip of string
 let skip_if b msg =
@@ -58,16 +58,19 @@ let assert_string str =
   if not (str = "") then assert_failure str
 
 let assert_equal ?(cmp = ( = )) ?printer ?msg expected actual  =
-  let get_error_string _ =
-    match printer, msg with
-      | None, None ->
-          (Format.sprintf "expected and actual result not equal")
-      | None, Some s ->
-          (Format.sprintf "%s\nnot equal" s)
-      | Some p, None -> (Format.sprintf "expected: %s but got: %s"
-         (p expected) (p actual))
-      | Some p, Some s -> (Format.sprintf "%s\nexpected: %s but got: %s"
-           s (p expected) (p actual))
+  (* pad: better to use dump by default *)
+  let p = Dumper.dump in
+
+  let get_error_string _ = match printer, msg with
+    | None, None ->
+      Format.sprintf "expected: %s but got: %s" (p expected) (p actual)
+    | None, Some s ->
+      Format.sprintf "%s\nnot equal, expected: %s but got: %s"
+        s (p expected) (p actual)
+    | Some p, None ->
+      Format.sprintf "expected: %s but got: %s" (p expected) (p actual)
+    | Some p, Some s ->
+      Format.sprintf "%s\nexpected: %s but got: %s" s (p expected) (p actual)
   in
   if not (cmp expected actual) then
     assert_failure (get_error_string ())
@@ -76,22 +79,20 @@ let raises f =
   try
     f ();
     None
-  with e ->
-    Some e
+  with
+    e -> Some e
 
 let assert_raises ?msg exn (f: unit -> 'a) =
   let pexn = Printexc.to_string in
   let get_error_string _ =
-    let str = Format.sprintf
-      "expected exception %s, but no exception was raised." (pexn exn)
-    in
-      match msg with
-        None -> assert_failure str
-      | Some s -> assert_failure (Format.sprintf "%s\n%s" s str)
+    let str = Format.sprintf "expected exception %s, but no exception was raised." (pexn exn) in
+    match msg with
+    | None -> assert_failure str
+    | Some s -> assert_failure (Format.sprintf "%s\n%s" s str)
   in
   match raises f with
-      None -> assert_failure (get_error_string ())
-    | Some e -> assert_equal ?msg ~printer:pexn exn e
+  | None -> assert_failure (get_error_string ())
+  | Some e -> assert_equal ?msg ~printer:pexn exn e
 
 (* Compare floats up to a given relative error *)
 let cmp_float ?(epsilon = 0.00001) a b =
@@ -106,7 +107,7 @@ type test_fun = unit -> unit
 
 (* The type of tests *)
 type test =
-    TestCase of test_fun
+  | TestCase of test_fun
   | TestList of test list
   | TestLabel of string * test
 
@@ -118,33 +119,35 @@ let (>:::) s l = TestLabel(s, TestList(l)) (* infix *)
 (* Utility function to manipulate test *)
 let rec test_decorate g tst =
   match tst with
-    | TestCase f ->
-        TestCase (g f)
-    | TestList tst_lst ->
-        TestList (List.map (test_decorate g) tst_lst)
-    | TestLabel (str, tst) ->
-        TestLabel (str, test_decorate g tst)
+  | TestCase f ->
+    TestCase (g f)
+  | TestList tst_lst ->
+    TestList (List.map (test_decorate g) tst_lst)
+  | TestLabel (str, tst) ->
+    TestLabel (str, test_decorate g tst)
 
 (* Return the number of available tests *)
 let rec test_case_count test =
   match test with
-      TestCase _ -> 1
-    | TestLabel (_, t) -> test_case_count t
-    | TestList l -> List.fold_left (fun c t -> c + test_case_count t) 0 l
+  | TestCase _ -> 1
+  | TestLabel (_, t) -> test_case_count t
+  | TestList l -> List.fold_left (fun c t -> c + test_case_count t) 0 l
 
 type node = ListItem of int | Label of string
 type path = node list
 
 let string_of_node node =
   match node with
-      ListItem n -> (string_of_int n)
-    | Label s -> s
+  | ListItem n -> (string_of_int n)
+  | Label s -> s
 
 let string_of_path path =
   List.fold_left
-    (fun a l -> if a = "" then l else l ^ ":" ^ a)
-    ""
-    (List.map string_of_node path)
+    (fun a l ->
+       if a = "" then
+         l
+       else
+         l ^ ":" ^ a) "" (List.map string_of_node path)
 
 (* Some helper function, they are generally applicable *)
 (* Applies function f in turn to each element in list. Function f takes
@@ -152,16 +155,16 @@ let string_of_path path =
 let mapi f l =
   let rec rmapi cnt l =
     match l with
-        [] -> []
-      | h::t -> (f h cnt)::(rmapi (cnt + 1) t)
+    | [] -> []
+    | h::t -> (f h cnt)::(rmapi (cnt + 1) t)
   in
   rmapi 0 l
 
 let fold_lefti f accu l =
   let rec rfold_lefti cnt accup l =
     match l with
-        [] -> accup
-      | h::t -> rfold_lefti (cnt + 1) (f accup h cnt) t
+    | [] -> accup
+    | h::t -> rfold_lefti (cnt + 1) (f accup h cnt) t
   in
   rfold_lefti 0 accu l
 
@@ -171,10 +174,10 @@ let fold_lefti f accu l =
 let test_case_paths test =
   let rec tcps path test =
     match test with
-        TestCase _ -> [path]
-      | TestList tests ->
-        List.concat (mapi (fun t i -> tcps ((ListItem i)::path) t) tests)
-      | TestLabel (l, t) -> tcps ((Label l)::path) t
+    | TestCase _ -> [path]
+    | TestList tests ->
+      List.concat (mapi (fun t i -> tcps ((ListItem i)::path) t) tests)
+    | TestLabel (l, t) -> tcps ((Label l)::path) t
   in
   tcps [] test
 
@@ -194,7 +197,7 @@ let test_filter only test =
          let nacc =
            f i acc e
          in
-           (i + 1), nacc
+         (i + 1), nacc
       )
       acc
       lst
@@ -207,39 +210,39 @@ let test_filter only test =
     else
       (
         match tst with
-          | TestCase _ ->
-              None
-          | TestList tst_lst ->
-              let (_, ntst_lst) =
-                foldi
-                  (fun i ntst_lst tst ->
-                     let nntst_lst =
-                       match filter_test ((ListItem i) :: path) tst with
-                         | Some tst ->
-                             tst :: ntst_lst
-                         | None ->
-                             ntst_lst
-                     in
-                       nntst_lst
-                  )
-                  (0, [])
-                  tst_lst
-              in
-                if ntst_lst = [] then
-                  None
-                else
-                  Some (TestList ntst_lst)
-          | TestLabel (lbl, tst) ->
-              let ntst =
-                filter_test
-                  ((Label lbl) :: path)
-                  tst
-              in
-                match ntst with
-                  | Some tst ->
-                      Some (TestLabel (lbl, tst))
-                  | None ->
-                      None
+        | TestCase _ ->
+          None
+        | TestList tst_lst ->
+          let (_, ntst_lst) =
+            foldi
+              (fun i ntst_lst tst ->
+                 let nntst_lst =
+                   match filter_test ((ListItem i) :: path) tst with
+                   | Some tst ->
+                     tst :: ntst_lst
+                   | None ->
+                     ntst_lst
+                 in
+                 nntst_lst
+              )
+              (0, [])
+              tst_lst
+          in
+          if ntst_lst = [] then
+            None
+          else
+            Some (TestList ntst_lst)
+        | TestLabel (lbl, tst) ->
+          let ntst =
+            filter_test
+              ((Label lbl) :: path)
+              tst
+          in
+          match ntst with
+          | Some tst ->
+            Some (TestLabel (lbl, tst))
+          | None ->
+            None
       )
   in
   filter_test [] test
@@ -247,48 +250,48 @@ let test_filter only test =
 
 (* The possible test results *)
 type test_result =
-    RSuccess of path
+  | RSuccess of path
   | RFailure of path * string
   | RError of path * string
   | RSkip of path * string
   | RTodo of path * string
 
 let is_success = function
-    RSuccess _  -> true
+  | RSuccess _  -> true
   | RFailure _ | RError _  | RSkip _ | RTodo _ -> false
 
 let is_failure = function
-    RFailure _ -> true
+  | RFailure _ -> true
   | RSuccess _ | RError _  | RSkip _ | RTodo _ -> false
 
 let is_error = function
-    RError _ -> true
+  | RError _ -> true
   | RSuccess _ | RFailure _ | RSkip _ | RTodo _ -> false
 
 let is_skip = function
-    RSkip _ -> true
+  | RSkip _ -> true
   | RSuccess _ | RFailure _ | RError _  | RTodo _ -> false
 
 let is_todo = function
-    RTodo _ -> true
+  | RTodo _ -> true
   | RSuccess _ | RFailure _ | RError _  | RSkip _ -> false
 
 let result_flavour = function
-    RError _ -> "Error"
+  | RError _ -> "Error"
   | RFailure _ -> "Failure"
   | RSuccess _ -> "Success"
   | RSkip _ -> "Skip"
   | RTodo _ -> "Todo"
 
 let result_path = function
-    RSuccess path
+  | RSuccess path
   | RError (path, _)
   | RFailure (path, _)
   | RSkip (path, _)
   | RTodo (path, _) -> path
 
 let result_msg = function
-    RSuccess _ -> "Success"
+  | RSuccess _ -> "Success"
   | RError (_, msg)
   | RFailure (_, msg)
   | RSkip (_, msg)
@@ -297,16 +300,16 @@ let result_msg = function
 (* Returns true if the result list contains successes only *)
 let rec was_successful results =
   match results with
-      [] -> true
-    | RSuccess _::t
-    | RSkip _::t -> was_successful t
-    | RFailure _::_
-    | RError _::_
-    | RTodo _::_ -> false
+  | [] -> true
+  | RSuccess _::t
+  | RSkip _::t -> was_successful t
+  | RFailure _::_
+  | RError _::_
+  | RTodo _::_ -> false
 
 (* Events which can happen during testing *)
 type test_event =
-    EStart of path
+  | EStart of path
   | EEnd of path
   | EResult of test_result
 
@@ -317,26 +320,27 @@ let perform_test report test =
       f ();
       RSuccess path
     with
-        Failure s -> RFailure (path, s)
-      | Skip s -> RSkip (path, s)
-      | Todo s -> RTodo (path, s)
-      | s -> RError (path, (Printexc.to_string s ^ " " ^
-                               Printexc.get_backtrace ()))
+    | Failure s -> RFailure (path, s)
+    | Skip s -> RSkip (path, s)
+    | Todo s -> RTodo (path, s)
+    | s ->
+      let stack = Printexc.get_backtrace () in
+      RError (path, (Printexc.to_string s ^ " " ^ stack))
   in
   let rec run_test path results test =
     match test with
-      TestCase(f) ->
-        report (EStart path);
-        let result = run_test_case f path in
-        report (EResult result);
-        report (EEnd path);
-        result::results
-      | TestList (tests) ->
-        fold_lefti
-          (fun results t cnt -> run_test ((ListItem cnt)::path) results t)
+    | TestCase(f) ->
+      report (EStart path);
+      let result = run_test_case f path in
+      report (EResult result);
+      report (EEnd path);
+      result::results
+    | TestList (tests) ->
+      fold_lefti
+        (fun results t cnt -> run_test ((ListItem cnt)::path) results t)
         results tests
-      | TestLabel (label, t) ->
-        run_test ((Label label)::path) results t
+    | TestLabel (label, t) ->
+      run_test ((Label label)::path) results t
   in
   run_test [] [] test
 
@@ -354,25 +358,32 @@ let run_test_tt ?(verbose=false) test =
   let separator2 =
     "----------------------------------------------------------------------" in
   let string_of_result = function
-      RSuccess _ -> if verbose then "ok\n" else "."
-    | RFailure (_, _) -> if verbose then "FAIL\n" else "F"
-    | RError (_, _) -> if verbose then "ERROR\n" else "E"
-    | RSkip (_, _) -> if verbose then "SKIP\n" else "S"
-    | RTodo (_, _) -> if verbose then "TODO\n" else "T"
+    | RSuccess _ ->
+      if verbose then "ok\n" else "."
+    | RFailure (_, _) ->
+      if verbose then "FAIL\n" else "F"
+    | RError (_, _) ->
+      if verbose then "ERROR\n" else "E"
+    | RSkip (_, _) ->
+      if verbose then "SKIP\n" else "S"
+    | RTodo (_, _) ->
+      if verbose then "TODO\n" else "T"
   in
   let report_event = function
-      EStart p -> if verbose then printf "%s ... " (string_of_path p)
+    | EStart p ->
+      if verbose then printf "%s ... " (string_of_path p)
     | EEnd _ -> ()
-    | EResult result -> printf "%s@?" (string_of_result result);
+    | EResult result ->
+      printf "%s@?" (string_of_result result);
   in
   let print_result_list results =
     List.iter
       (fun result -> printf "%s\n%s: %s\n\n%s\n%s\n"
-         separator1
-         (result_flavour result)
-         (string_of_path (result_path result))
-         (result_msg result)
-         separator2)
+          separator1
+          (result_flavour result)
+          (string_of_path (result_path result))
+          (result_msg result)
+          separator2)
       results
   in
 
@@ -383,69 +394,65 @@ let run_test_tt ?(verbose=false) test =
   let skips = List.filter is_skip results in
   let todos = List.filter is_todo results in
 
-    if not verbose then printf "\n";
+  if not verbose then printf "\n";
 
-    (* Print test report *)
-    print_result_list errors;
-    print_result_list failures;
-    printf "Ran: %d tests in: %.2f seconds.\n"
-      (List.length results) running_time;
+  (* Print test report *)
+  print_result_list errors;
+  print_result_list failures;
+  printf "Ran: %d tests in: %.2f seconds.\n"
+    (List.length results) running_time;
 
-    (* Print final verdict *)
-    if was_successful results then
-      (
-        if skips = [] then
-          printf "OK"
-        else
-          printf "OK: Cases: %d Skip: %d\n"
-            (test_case_count test) (List.length skips)
-      )
-    else
-      printf
-        "FAILED: Cases: %d Tried: %d Errors: %d Failures: %d Skip:%d Todo:%d\n"
-          (test_case_count test)
-          (List.length results)
-          (List.length errors)
-          (List.length failures)
-          (List.length skips)
-          (List.length todos);
+  (* Print final verdict *)
+  if was_successful results then
+    (
+      if skips = [] then
+        printf "OK"
+      else
+        printf "OK: Cases: %d Skip: %d\n"
+          (test_case_count test) (List.length skips)
+    )
+  else
+    printf "FAILED: Cases: %d Tried: %d Errors: %d Failures: %d Skip:%d Todo:%d\n"
+      (test_case_count test) (List.length results)
+      (List.length errors) (List.length failures)
+      (List.length skips) (List.length todos);
 
-    (* Return the results possibly for further processing *)
-    results
+  (* Return the results possibly for further processing *)
+  results
 
 (* Call this one from you test suites *)
 let run_test_tt_main suite =
   let verbose = ref false in
   let only_test = ref [] in
 
-    Arg.parse
-      (Arg.align
-         [("-verbose", Arg.Set verbose, " Run the test in verbose mode.");
-          ("-only-test", Arg.String (fun str -> only_test := str :: !only_test),
-           "path Run only the selected test");
-         ]
-      )
-      (fun x -> raise (Arg.Bad ("Bad argument : " ^ x)))
-      ("usage: " ^ Sys.argv.(0) ^ " [-verbose] [-only-test path]*");
+  Arg.parse
+    (Arg.align
+       [("-verbose", Arg.Set verbose, " Run the test in verbose mode.");
+        ("-only-test", Arg.String (fun str -> only_test := str :: !only_test),
+         "path Run only the selected test");
+       ]
+    )
+    (fun x -> raise (Arg.Bad ("Bad argument : " ^ x)))
+    ("usage: " ^ Sys.argv.(0) ^ " [-verbose] [-only-test path]*");
 
-    let nsuite =
-      if !only_test = [] then
-        (
-          suite
-        )
-      else
-        (
-          match test_filter !only_test suite with
-            | Some tst ->
-                tst
-            | None ->
-                failwith ("Filtering test "^
-                          (String.concat ", " !only_test)^
-                          " lead to no test")
-        )
-    in
-    let result = run_test_tt ~verbose:!verbose nsuite in
-    if not (was_successful result) then
-      exit 1
+  let nsuite =
+    if !only_test = [] then
+      (
+        suite
+      )
     else
-      result
+      (
+        match test_filter !only_test suite with
+        | Some tst ->
+          tst
+        | None ->
+          failwith ("Filtering test "^
+              (String.concat ", " !only_test)^
+              " lead to no test")
+      )
+  in
+  let result = run_test_tt ~verbose:!verbose nsuite in
+  if not (was_successful result) then
+    exit 1
+  else
+    result

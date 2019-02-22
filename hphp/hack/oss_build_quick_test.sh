01d4c3157efe18f82fed750808ce9c0f52ed712d
Vincent Siles <vsiles@fb.com>
2019-02-22 09:28:02 -0800

    Update dune build to use third-party-buck instead of system lib
    Summary:
    Current dune build relies on pkg-config and system libs for lz4 and sqlite3.
    This patch instruments cmake scripts to thread the correct information to
    dune. In case the libs are not found in third-party-buck, the system will
    fallback to pkg-config.
    
    Reviewed By: fredemmott
    
    Differential Revision: D14103613
    
    fbshipit-source-id: 8cdb38c2ba197ec5c95ca21aefef8784f79df102

--- hphp/hack/oss_build_quick_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-HACKDIR="$(realpath "$(dirname "$0")")"
-SKIP_OPAM=${SKIP_OPAM:-0}
-
-cmake "$HACKDIR" -Wno-dev -DOCAML_EXECUTABLE="$(command -v ocaml)" \
-  -DSKIP_OPAM="${SKIP_OPAM}" -DWITH_DUNE=1
-cd "$HACKDIR" && make hack_dune
-hg rev "$HACKDIR/src/hhi/.merlin" "$HACKDIR/src/ppx/.merlin"

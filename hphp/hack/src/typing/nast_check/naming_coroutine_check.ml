cd5ac3fa90310694983b65ed3ba147f22347e118
Maggie Moss <maggiemoss@fb.com>
2019-03-01 16:20:26 -0800

    Move return in gen checks to nast_check
    Summary:
    - Move return in gen check to the corresponding nast_check .
    - Removed now unused flag from the nastCheck env
    - Refactor how this check keeps track of types of functions so moving over more coroutine checks will be more straightforward.
    
    Reviewed By: kmeht
    
    Differential Revision: D14285412
    
    fbshipit-source-id: 16703a5695e534584dc479c331cab00be7e7a93e

--- hphp/hack/src/typing/nast_check/naming_coroutine_check.ml
+++ hphp/hack/src/typing/nast_check/naming_coroutine_check.ml
@@ -13,56 +13,56 @@ open Nast
 module SN = Naming_special_names
 
 type ctx = {
-  is_coroutine: bool;
-  is_async: bool;
-  is_final: bool;
+  fun_kind: Ast.fun_kind;
+  is_finally: bool;
 }
 
-let set_ctx ctx f =
-  match f.f_fun_kind with
-  | Ast.FCoroutine ->
-    { ctx with is_coroutine = true }
-  | Ast.FSync
-  | Ast.FAsync
-  | Ast.FGenerator
-  | Ast.FAsyncGenerator ->
-    { ctx with is_async = true }
+let set_fun_kind ctx f =
+  { ctx with fun_kind = f.f_fun_kind }
 
-let default_ctx = {
-  is_coroutine = false;
-  is_async = false;
-  is_final = false;
-}
+let is_coroutine ctx =
+  ctx.fun_kind = Ast.FCoroutine
+
+let is_generator ctx =
+  ctx.fun_kind = Ast.FGenerator || ctx.fun_kind = Ast.FAsyncGenerator
+let default_ctx =
+  {
+    is_finally = false;
+    fun_kind = Ast.FSync;
+  }
 
 let visitor = object(self)
   inherit [ctx] Nast_visitor.iter_with_state as super
 
   method! on_fun_ (env, ctx) f =
-    super#on_fun_ (env, set_ctx ctx f) f
+    super#on_fun_ (env, set_fun_kind ctx f) f
 
   method! on_expr (env, ctx) (p, e) =
-    match e with
+    begin match e with
     | Yield _
     | Yield_break
     | Yield_from _ ->
-      if ctx.is_coroutine then Errors.yield_in_coroutine p
+      if is_coroutine ctx then Errors.yield_in_coroutine p
     | Await _ ->
-      if ctx.is_coroutine then Errors.await_in_coroutine p
+      if is_coroutine ctx then Errors.await_in_coroutine p
     | Suspend _ ->
-      if ctx.is_coroutine && ctx.is_final then Errors.suspend_in_finally p;
-      if ctx.is_async && not ctx.is_coroutine then Errors.suspend_outside_of_coroutine p
-    | _ -> ();
+      if not (is_coroutine ctx) then Errors.suspend_outside_of_coroutine p
+      else if ctx.is_finally then Errors.suspend_in_finally p
+    | _ -> ()
+    end;
     super#on_expr (env, ctx) (p, e)
 
   method! on_stmt (env, ctx) s =
-    if ctx.is_coroutine then
     begin match s with
-    | Using { us_has_await = has_await; us_expr = e;  _ } ->
+    | Using { us_has_await = has_await; us_expr = e;  _ } when is_coroutine ctx ->
       if has_await then Errors.await_in_coroutine (fst e)
-    | Foreach (_, (Await_as_v (p, _) | Await_as_kv (p, _, _)), _) ->
-      Errors.await_in_coroutine p
-    | Try (_, _, fb) ->
-      self#on_block (env, { ctx with is_final = true }) fb
+    | Foreach (_, (Await_as_v (p, _) | Await_as_kv (p, _, _)), _)
+      when is_coroutine ctx ->
+        Errors.await_in_coroutine p
+    | Try (_, _, fb) when is_coroutine ctx ->
+      self#on_block (env, { ctx with is_finally = true }) fb
+    | Return (p, Some _) when is_generator ctx ->
+      Errors.return_in_gen p
     | _ -> ()
     end;
     super#on_stmt (env, ctx) s
@@ -72,10 +72,6 @@ end
 let handler = object
   inherit Nast_visitor.handler_base
 
-  method! at_expr env e = visitor#on_expr (env, default_ctx) e
-
-  method! at_stmt env s = visitor#on_stmt (env, default_ctx) s
-
-  method! at_fun_ env f = visitor#on_fun_ (env, set_ctx default_ctx f) f
+  method! at_fun_ env f = visitor#on_fun_ (env, set_fun_kind default_ctx f) f
 
 end

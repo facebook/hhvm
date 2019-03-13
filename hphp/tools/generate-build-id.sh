9c5f94d07a9585016cccaaf98a6dd600fe40a737
Fred Emmott <fe@fb.com>
2019-03-13 10:35:06 -0700

    Replace `openssl sha -sha1` with `openssl dgst -sha1`
    Summary: `sha -sha1` is non-portable, and does not exist on modern macs.
    
    Reviewed By: alexeyt, jjergus
    
    Differential Revision: D14428137
    
    fbshipit-source-id: f415e0fb07d43262a2756b352993d19b66066ba9

--- hphp/tools/generate-build-id.sh
+++ hphp/tools/generate-build-id.sh
@@ -11,7 +11,7 @@ set -e
 cd "$FBCODE_DIR" || exit 2;
 
 function digest() {
-  openssl sha -sha1 "$@" | cut -f2 -d' '
+  openssl dgst -sha1 "$@" | cut -f2 -d' '
 }
 
 # if we're under source control,

d9691728c910a027358e47a8e9e291536930ee48
Evgeny Fiksman <efiksman@fb.com>
2019-03-19 07:49:48 -0700

    Remove reference to PHAR extension files
    Summary: This diff to complete D14482678, as those files were missed when splitting D14418046.
    
    Reviewed By: alexeyt
    
    Differential Revision: D14515120
    
    fbshipit-source-id: 5e5e1604a1aa4b66687a79f1a2ddac9a8b7026cf

--- hphp/test/slow/archive_handler/tar_archive.php
+++
@@ -1,8 +0,0 @@
-<?hh
-
-
-<<__EntryPoint>>
-function main_tar_archive() {
-$handler = new \__SystemLib\TarArchiveHandler(__DIR__ . '/tar_archive.tar');
-var_dump($handler->getEntriesMap()->keys());
-}

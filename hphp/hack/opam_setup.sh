5877e8071c2d5e2c182eadaa45db42dc57823ca3
Vincent Siles <vsiles@fb.com>
2019-03-28 10:16:11 +0100

    Fix opam2/arm64 build
    The current ocaml used to build hhvm is from opam2, which is lacking
    the patches/ocaml.patch patch. This PR updates the opam switch creation
    so that ocaml is patched before it is built.
    
    The old ocaml is still present to have a simple PR. Next PR will remove
    the old ocaml from third-party and the toplevel ocaml-patch

--- hphp/hack/opam_setup.sh
+++ hphp/hack/opam_setup.sh
@@ -37,6 +37,8 @@ opam_require_version_2 () {
 opam_switch_create_if_needed () {
     local name=$1
     local switch=$2
+    local source_dir="$OPAMROOT/custom-sources"
+    local ocaml_dir="$source_dir/$switch"
     local switch_exists=no
     for installed_switch in $(opam switch list --short); do
         if [ "$installed_switch" == "$name" ]; then
@@ -45,7 +47,16 @@ opam_switch_create_if_needed () {
         fi
     done
     if [ "$switch_exists" = "no" ]; then
-        opam switch create "$name" "$switch"
+        # Creates an empty switch so we can fetch ocaml source code
+        # and patch it for arm64 builds
+        opam switch create --empty "$name"
+        mkdir -p "$source_dir"
+        opam source "$switch" --dir "$ocaml_dir"
+        pushd "$ocaml_dir"
+        patch -p1 < "$ROOT/ocaml.patch"
+        popd
+        opam pin add ocaml-variants.4.05.0+arm64-patch "$ocaml_dir"
+        eval $(opam env)
     fi
 }
 
@@ -67,7 +78,7 @@ then
   tar xzf "$MINI_TARBALL" -C "$ROOT/_build"
   opam init --disable-sandboxing --reinit offline_clone "$MINI_REPO" --no-setup --bare
 else
-  opam init --disable-sandboxing --reinit --no-setup
+  opam init --disable-sandboxing --reinit --no-setup --bare
 fi
 
 opam_switch_create_if_needed "$HACK_OPAM_NAME" "$HACK_OPAM_SWITCH"

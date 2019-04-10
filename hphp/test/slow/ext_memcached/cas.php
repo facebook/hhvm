b76dbb6a0a1e3d0dd458806fef1b68e513f21e38
Bill Fumerola <bill@fb.com>
2019-04-10 09:36:50 -0700

    fix by-ref issue in ext_memcached test
    Reviewed By: fredemmott
    
    Differential Revision: D14815091
    
    fbshipit-source-id: 42d36b6d980518932a37873ad7cdd9df7f8efe9e

--- hphp/test/slow/ext_memcached/cas.php
+++ hphp/test/slow/ext_memcached/cas.php
@@ -5,8 +5,8 @@ $memc->addServer('localhost', '11211');
 
 $key = 'cas_test';
 var_dump($memc->set($key, 10, 60));
-
-var_dump($memc->get($key, null, $cas));
+$cas = null;
+var_dump($memc->get($key, null, &$cas));
 
 var_dump(is_double($cas));
 var_dump($memc->cas($cas, $key, 11, 60));

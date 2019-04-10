ffee43eed4f459d78b3d6e7e19595aafbb4ef738
Vincent Siles <vsiles@fb.com>
2019-04-10 00:54:34 -0700

    Getting rid of CMake for internal build
    Summary:
    For FB only build, we don't need the full generic CMake support. Getting
    rid of it to have cleaner scripts. This patch introduce a dune wrapper
    to have more fine grain control over the build. The next patch will clean
    CMake from things FB related and remove the old script.
    
    This is just a smart wrapper around dune that sends all of the command line
    infos to dune, in the correct context. Please refer to dune's documentation
    if you have specific question (or ask the ocaml group / myself).
    
    Reviewed By: CatherineGasnier
    
    Differential Revision: D14831600
    
    fbshipit-source-id: 80fa15f69419ed14377285ee015278649241b8b9

--- hphp/hack/opam_setup.sh
+++ hphp/hack/opam_setup.sh
@@ -10,28 +10,12 @@ export OPAMROOT="$ROOT/_build/.opam"
 mkdir -p "$OPAMROOT"
 export OPAMYES="1"
 
+# shellcheck disable=SC1090
+source "$ROOT/opam_helpers.sh"
+
 # Shamelessly copied from
 # https://github.com/facebook/infer/blob/master/scripts/opam_utils.sh
 # Many thanks to the infer team :D
-
-opam_require_version_2 () {
-    local status=0
-    local version=0
-    { version=$(opam --version 2>/dev/null); status=$?; }
-    if [ "$status" != 0 ]; then
-        printf '*** ERROR: `opam --version` failed, please install opam version 2\n' >&2
-        env >&2
-        exit 1
-    fi
-    case $version in
-        2*) ;;
-        *)
-            printf '*** ERROR: opam version "%s" is not supported, please install opam version 2\n' "$version" >&2
-            printf '*** NOTE: opam is "%s"\n' "$(command -v opam)" >&2
-            env >&2
-            exit 1
-    esac
-}
 
 # assumes opam is available and initialized
 opam_switch_create_if_needed () {

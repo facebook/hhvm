c9bd663aaf3b554fba9b145795b1db8c8a45afcc
Jake Bailey (Hacklang) <jakebailey@fb.com>
2019-04-10 20:40:45 -0700

    Fix review.sh
    Summary:
    Looks like the change to review.sh in {D14854837} had an error--the variable `$SRC` is not defined. This causes review.sh to fail to copy the actual output into the expected output file when requested--instead, it copies the actual output into a file named ".exp".
    
    I think the intent of the change in D14854837 was to always copy into the file with the EXP_EXT extension rather than FALLBACK_EXP_EXT. If that's the behavior we want, then all we need to do then is replace `$SRC` with the actual filename, `$f`.
    
    Reviewed By: kmeht
    
    Differential Revision: D14883378
    
    fbshipit-source-id: 99ec1af742bb6291d74ff0f04f4069f75ad23db5

--- hphp/hack/test/review.sh
+++ hphp/hack/test/review.sh
@@ -63,7 +63,7 @@ for f in "$@"; do
   fi
   echo ""
   if [ "$REPLY" = "y" ] && [ "$NO_COPY" = false ]; then
-    cp "$OUT" "$SRC$EXP_EXT"
+    cp "$OUT" "$f$EXP_EXT"
   elif [ "$REPLY" = "q" ]; then
     exit 0
   fi

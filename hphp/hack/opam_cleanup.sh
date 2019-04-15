3df54e253da467d7c188a2de43e18fb98ba6c5bf
Catherine Gasnier <catg@fb.com>
2019-04-15 04:07:37 -0700

    make opam_cleanup.sh work from any directory
    Reviewed By: vsiles
    
    Differential Revision: D14933006
    
    fbshipit-source-id: 32652b74b4535b2f32a0e9a0584afd3cbe6cfffd

--- hphp/hack/opam_cleanup.sh
+++ hphp/hack/opam_cleanup.sh
@@ -6,13 +6,15 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the "hack" directory of this source tree.
 
+HACK_DIR="$(realpath "$(dirname "$0")")"
+
 # cleanup OSS locations
-if [ -d _build ]; then
-  rm -rf _build/opam
+if [ -d "${HACK_DIR}/_build" ]; then
+  rm -rf "${HACK_DIR}/_build/opam"
 fi
 
 # cleanup FB locations
-if [ -d facebook ]; then
-  rm -rf facebook/opam
-  rm -rf facebook/opam2-mini-repository
+if [ -d "${HACK_DIR}/facebook" ]; then
+  rm -rf "${HACK_DIR}/facebook/opam"
+  rm -rf "${HACK_DIR}/facebook/opam2-mini-repository"
 fi

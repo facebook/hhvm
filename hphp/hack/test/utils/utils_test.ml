8398a84718e13a70bde012009017f976826f399e
Ted Spence <tspence@fb.com>
2019-05-01 09:42:13 -0700

    Method to split a name into namespace\name
    Summary:
    Small method that splits a name into a namespace plus a name.
    
    Examples:
    
    `HH\Lib\Str\Format` -> `HH\Lib\Str\` * `Format`
    `NameWithoutANamespace` -> `\` * `NameWithoutANamespace`
    `HH\Lib\Str\` -> `HH\Lib\Str\` * (empty)
    
    Reviewed By: arxanas
    
    Differential Revision: D15140475
    
    fbshipit-source-id: af84c8d0f5ffb10528665df7892ac37d166700de

---
+++ hphp/hack/test/utils/utils_test.ml
@@ -0,0 +1,22 @@
+open Asserter
+
+let test_ns_split name assert_left assert_right =
+  let (left, right) = Utils.split_ns_from_name name in
+  String_asserter.assert_equals left assert_left "Namespace is wrong";
+  String_asserter.assert_equals right assert_right "Namespace is wrong"
+
+let test_namespace_splitter () =
+  test_ns_split "HH\\Lib\\Str\\Format"
+    "HH\\Lib\\Str\\" "Format";
+  test_ns_split "NameWithoutANamespace"
+    "\\" "NameWithoutANamespace";
+  test_ns_split "HH\\Lib\\Str\\"
+    "HH\\Lib\\Str\\" "";
+  test_ns_split "\\HH\\Lib\\Hellothisisafunction"
+    "\\HH\\Lib\\" "Hellothisisafunction";
+  true
+
+let () =
+  Unit_test.run_all [
+    ("test ability to split namespaces", test_namespace_splitter);
+  ]

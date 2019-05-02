b798fe1ea5fd80f700d5f656570ae70c219e8a7c
Andrew Kennedy <akenn@fb.com>
2019-05-02 01:56:00 -0700

    New inference: add performance regression test
    Summary: D15149670 fixed a performance regression. This diff adds a directory and test for that regression, making use of the `--timeout` feature to check that the time taken to check a file does not exceed 1 second.
    
    Reviewed By: CatherineGasnier
    
    Differential Revision: D15148182
    
    fbshipit-source-id: 88268346d531836a31040670d09df148d1d030cb

---
+++ hphp/hack/test/typecheck/perf/vec_update.php
@@ -0,0 +1,31 @@
+<?hh // strict
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+function test2(int $id): void {
+  $res = Map {};
+  $res[$id] = vec[1];
+  $res[$id][0] += 0; // + 3 tyvars
+  $res[$id][1] += 0; // + 6 tyvars
+  $res[$id][2] += 0; // + 9 tyvars...
+  $res[$id][3] += 0;
+  $res[$id][4] += 0;
+  $res[$id][5] += 0;
+  $res[$id][6] += 0;
+  $res[$id][7] += 0;
+  $res[$id][8] += 0;
+  $res[$id][9] += 0;
+  $res[$id][10] += 0;
+  $res[$id][11] += 0;
+  $res[$id][12] += 0;
+  $res[$id][13] += 0;
+  $res[$id][14] += 0;
+  $res[$id][15] += 0;
+  $res[$id][16] += 0;
+  $res[$id][17] += 0;
+  $res[$id][18] += 0;
+  $res[$id][19] += 0;
+  $res[$id][20] += 0;
+  $res[$id][21] += 0;
+  $res[$id][22] += 0;
+//  hh_show_env();
+}

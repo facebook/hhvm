38ca8eb733061df1a5d7c170515fbefd72691f53
Ted Spence <tspence@fb.com>
2019-05-09 14:44:38 -0700

    Make index builder work in dune
    Summary:
    I discovered during testing of the "generate sqlite on demand" diff (D15251808) that dune was broken.
    
    It takes a lot of changes to make that work, so let's get started.
    
    * Add a stub file for ApiExporter.
    * Add a dune build configuration file for index_builder.
    * Move SearchUtils into a separate folder, so that it can have its own dune file.
    * Add dune file and targets for SearchUtils.
    * Update all Buck targets with the new name for SearchUtils.
    
    Reviewed By: pittsw
    
    Differential Revision: D15262236
    
    fbshipit-source-id: 01b50291007994fd1896db6bbfaa1fcc908c3550

---
+++ hphp/hack/src/facts/symbols/apiExporter.ml
@@ -0,0 +1,13 @@
+(**
+ * Copyright (c) 2019, Facebook, Inc.
+ * All rights reserved.
+ *
+*)
+
+(* Implement this stub to enable code information to be sent to an API *)
+let send_to_api
+    (_files: string list)
+    (_service: string)
+    (_repo_name: string)
+    (_globalrev: string): unit =
+  ()

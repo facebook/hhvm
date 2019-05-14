61cd9f763c76e3f50bd36632c7a0acdfe78a5fd1
Waleed Khan <waleedk@fb.com>
2019-05-14 11:22:00 -0700

    Format `test_fresh_init.py`
    Summary: Formatting changes only.
    
    Differential Revision: D15324499
    
    fbshipit-source-id: 69bc8a5dcedca43c4414b0ea63c8a8b4c8465e4c

--- hphp/hack/test/integration/test_fresh_init.py
+++ hphp/hack/test/integration/test_fresh_init.py
@@ -1,43 +1,44 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-import common_tests
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import os
 import time
 import unittest
 
+import common_tests
 from hh_paths import hh_client
 
-class FreshInitTestDriver(common_tests.CommonTestDriver):
 
+class FreshInitTestDriver(common_tests.CommonTestDriver):
     def write_load_config(self, use_saved_state=False):
         # Fresh init tests don't care about which files changed, so we can
         # just use the default .hhconfig in the template repo
         pass
 
     def check_cmd(
-            self,
-            expected_output,
-            stdin=None,
-            options=(),
-            retries=3,
-            assert_loaded_saved_state=False
+        self,
+        expected_output,
+        stdin=None,
+        options=(),
+        retries=3,
+        assert_loaded_saved_state=False,
     ):
         time.sleep(2)  # wait for Hack to catch up with file system changes
 
         root = self.repo_dir + os.path.sep
-        (output, err, retcode) = self.proc_call([
-            hh_client,
-            'check',
-            '--retries',
-            '60',
-            '--no-load',
-            '--error-format',
-            'raw',
-            self.repo_dir
-            ] + list(map(lambda x: x.format(root=root), options)),
-            stdin=stdin)
+        (output, err, retcode) = self.proc_call(
+            [
+                hh_client,
+                "check",
+                "--retries",
+                "60",
+                "--no-load",
+                "--error-format",
+                "raw",
+                self.repo_dir,
+            ]
+            + list(map(lambda x: x.format(root=root), options)),
+            stdin=stdin,
+        )
 
         if retcode == 6 and retries > 0:
             # this sometimes happens and retrying helps
@@ -46,8 +47,8 @@ class FreshInitTestDriver(common_tests.CommonTestDriver):
 
         if expected_output is not None:
             self.assertCountEqual(
-                map(lambda x: x.format(root=root), expected_output),
-                output.splitlines())
+                map(lambda x: x.format(root=root), expected_output), output.splitlines()
+            )
         return err
 
     def assertEqualString(self, first, second, msg=None):
@@ -56,12 +57,11 @@ class FreshInitTestDriver(common_tests.CommonTestDriver):
         self.assertEqual(first, second, msg)
 
 
-class TestFreshInit(common_tests.CommonTests, FreshInitTestDriver,
-        unittest.TestCase):
-
+class TestFreshInit(common_tests.CommonTests, FreshInitTestDriver, unittest.TestCase):
     def test_remove_dead_fixmes(self):
-        with open(os.path.join(self.repo_dir, 'foo_4.php'), 'w') as f:
-            f.write("""<?hh // strict
+        with open(os.path.join(self.repo_dir, "foo_4.php"), "w") as f:
+            f.write(
+                """<?hh // strict
                 function foo(?string $s): void {
                   /* HH_FIXME[4010] We can delete this one */
                   /* HH_FIXME[4089] We need to keep this one */
@@ -76,17 +76,17 @@ class TestFreshInit(common_tests.CommonTests, FreshInitTestDriver,
                   /* HH_FIXME[4098] We can delete this one */
                   print "done\n";
                 }
-            """)
+            """
+            )
 
-        self.start_hh_server(changed_files=['foo_4.php'], args=["--no-load"])
-        self.check_cmd(
-            expected_output=None,
-            options=['--remove-dead-fixmes'],
-        )
+        self.start_hh_server(changed_files=["foo_4.php"], args=["--no-load"])
+        self.check_cmd(expected_output=None, options=["--remove-dead-fixmes"])
 
-        with open(os.path.join(self.repo_dir, 'foo_4.php')) as f:
+        with open(os.path.join(self.repo_dir, "foo_4.php")) as f:
             out = f.read()
-            self.assertEqual(out, """<?hh // strict
+            self.assertEqual(
+                out,
+                """<?hh // strict
                 function foo(?string $s): void {
                   /* HH_FIXME[4110] Keep errors discovered by new_inference */
                   if ($s) {
@@ -96,4 +96,5 @@ class TestFreshInit(common_tests.CommonTests, FreshInitTestDriver,
                   }
                   print "done\n";
                 }
-            """)
+            """,
+            )

fcae27e7fd021ad3758e626ffbba67b13e5ac0e2
Vassil Mladenov <vmladenov@fb.com>
2019-05-17 11:09:40 -0700

    Add an interface for Typing_phase
    Summary:
    This diff adds a .mli file for `Typing_phase`. This allows us to change the implementation of Typing_phase and identify the parts of the codebase that use it. This is useful for separating external calls to Typing_phase and internal mutually recursive calls. Having this barrier allows you to quickly find all references to a `Typing_phase` function outside Typing_phase itself.
    
    It also means that consumers of localization don't have to worry about the trusted-ness of their types, as Typing_phase can handle it as needed. For example, I found that `localize_hint` (`hint -> locl ty`), while looking similar to other chains of `Decl_hint.hint` (`hint -> decl ty`) and `localize` (`decl ty -> locl ty`), is used for *is* and *as* expressions where the type is required to be trusted, because it is what is passed to the runtime.
    
    Reviewed By: Matt-Schellhas
    
    Differential Revision: D15245956
    
    fbshipit-source-id: 19509eca250f5094db3f02e1fd6ed5b1bef0bec0

---
+++ hphp/hack/src/typing/typing_phase.mli
@@ -0,0 +1,86 @@
+[@@@warning "-33"]
+open Core_kernel
+open Common
+[@@@warning "+33"]
+open Typing_defs
+
+module Env = Typing_env
+
+type method_instantiation =
+{
+  use_pos: Pos.t;
+  use_name: string;
+  explicit_tparams: Nast.hint list;
+}
+
+type env = expand_env
+
+val env_with_self:
+  Env.env ->
+  expand_env
+val localize_with_self:
+  Env.env ->
+  decl ty ->
+  Env.env * locl ty
+val localize_with_env:
+  ety_env:expand_env ->
+  Env.env ->
+  decl ty ->
+  Env.env * (expand_env * locl ty)
+val localize:
+  ety_env:expand_env ->
+  Env.env ->
+  decl ty ->
+  Env.env * locl ty
+val localize_ft:
+  ?instantiation:method_instantiation ->
+  ety_env:expand_env ->
+  Env.env ->
+  decl fun_type ->
+  Env.env * locl fun_type
+val localize_hint_with_self:
+  Env.env ->
+  Nast.hint ->
+  Env.env * locl ty
+val localize_hint:
+  ety_env:expand_env ->
+  Env.env ->
+  Nast.hint ->
+  Env.env * locl ty
+val localize_generic_parameters_with_bounds:
+  ety_env:expand_env ->
+  Env.env ->
+  Nast.tparam list ->
+  Env.env * (locl ty * Ast.constraint_kind * locl ty) list
+val localize_where_constraints:
+  ety_env:expand_env ->
+  Env.env ->
+  Nast.where_constraint list ->
+  Env.env
+val localize_with_dty_validator:
+  Env.env ->
+  decl ty ->
+  (decl ty -> unit) ->
+  Env.env * locl ty
+val sub_type_decl:
+  Env.env ->
+  decl ty ->
+  decl ty ->
+  unit
+val unify_decl:
+  Env.env ->
+  decl ty ->
+  decl ty ->
+  unit
+val check_tparams_constraints:
+  use_pos:Pos.t ->
+  ety_env:expand_env ->
+  Env.env ->
+  decl tparam list ->
+  Env.env
+val decl:
+  decl ty ->
+  phase_ty
+val locl:
+  locl ty ->
+  phase_ty

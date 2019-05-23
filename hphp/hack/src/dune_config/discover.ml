9d1cc0c9cb3995bdc86e036e55f4001342e053c4
Vincent Siles <vsiles@fb.com>
2019-05-23 00:21:49 -0700

    make buck call dune
    Summary:
    This is an experimental in order to replace `ocaml_library` and `ocaml_binary`
    by calls to dune, so that buck relies on dune to build ocaml code.
    This is a first draft, mostly for discussion.
    
    Current ups and down
    - (down) instruct dune to build everything in buck-out rather than locally. At the
      moment one must run dune clean when running buck clean.
    - (down)hhjs is not built yet with dune so I make my tests using typecheck
    - (up): from a clean state, 'normal' buck builds hh_single_type_check
      in 3m1.777s. With the new rule, it only takes 1m20.265s
    - (up): once both rules are build, if I `touch
      hack/src/utils/string_utils.ml{,i}`, the 'normal' buck rebuilds tons of
      things in 2m29.717s, while dune rebuilds nothing in 0m0.848s
    - (up) since we are using buck, it should work nicely with tests / sandcastle
    
    Reviewed By: 2BitSalute
    
    Differential Revision: D15316669
    
    fbshipit-source-id: c33eb66abba3b3da7cf0be2d09b888cab2af9f1d

--- hphp/hack/src/dune_config/discover.ml
+++ hphp/hack/src/dune_config/discover.ml
@@ -3,35 +3,11 @@ https://jbuilder.readthedocs.io/en/latest/configurator.html *)
 
 module C = Configurator.V1
 
-let find_hphp_parent dir =
-  let path = String.split_on_char '/' dir in
-  let rec reconstruct_till_hphp path acc =
-    match path with
-    | [] -> acc
-    | "hphp" :: path ->
-      if List.mem "hphp" path
-      then reconstruct_till_hphp path (acc ^ "hphp/")
-      else acc
-    | dir :: path ->
-      let acc = acc ^ dir ^ "/" in
-      reconstruct_till_hphp path acc in
-  reconstruct_till_hphp path ""
-
-let find_sqlite () =
+let () =
+  C.main ~name:"hphpdir" (fun (_c : C.t) ->
   let split = (fun s -> if s = "" then [] else String.split_on_char ' ' s) in
   let rpath = match Sys.getenv "FB_LD_OPTS" with
     | t -> let flags = split t in
       List.fold_left (fun acc x -> "-ccopt" :: x :: acc ) [] flags
     | exception Not_found -> [] in
-  C.Flags.write_sexp "ld-opts.sexp" rpath
-
-let () =
-  C.main ~name:"hphpdir" (fun (_c : C.t) ->
-    let source_root = match Sys.getenv "CMAKE_SOURCE_DIR" with
-    | t -> t^"/"
-    | exception Not_found ->
-      let workingdir = Sys.getcwd () in
-      find_hphp_parent workingdir
-    in
-    find_sqlite ();
-    C.Flags.write_lines "hphp_parent" [ source_root ])
+  C.Flags.write_sexp "ld-opts.sexp" rpath)

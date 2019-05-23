578eed5efbb0420860512846d57d117420d4be78
Vassil Mladenov <vmladenov@fb.com>
2019-05-23 08:22:26 -0700

    Allow <<__AtMostRxAsFunc>> to be used with like functions
    Summary: Function types in parameter and return positions get pessimized into like functions because they are not enforced, but the check for `<<__AtMostRxAsFunc>>` only looks at the locl ty and expects a raw function type. This allows the attribute on an inferred like function type.
    
    Reviewed By: vladima
    
    Differential Revision: D15442539
    
    fbshipit-source-id: c25dfb71065946a271e9f118751cef38ecc8e027

--- hphp/hack/src/typing/typing_reactivity.ml
+++ hphp/hack/src/typing/typing_reactivity.ml
@@ -268,7 +268,9 @@ let disallow_atmost_rx_as_rxfunc_on_non_functions env param param_ty =
     then Errors.missing_annotation_for_atmost_rx_as_rxfunc_parameter param.Nast.param_pos
     else match TU.non_null env param.Nast.param_pos param_ty with
     (* if parameter has <<__AtMostRxAsFunc>> annotation then:
-       - parameter should be typed as function *)
+       - parameter should be typed as function or a like function *)
+    | _, (_, Tunion [(_, Tfun _); (_, Tdynamic)])
+    | _, (_, Tunion [(_, Tdynamic); (_, Tfun _)])
     | _, (_, Tfun _) -> ()
     | _ ->
       Errors.invalid_type_for_atmost_rx_as_rxfunc_parameter

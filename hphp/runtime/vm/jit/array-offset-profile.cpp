32b28f1fe6d1e6f69e03721a3c69ee601d01479b
Shaunak Kishore <kshaunak@fb.com>
2019-06-01 04:44:32 -0700

    Factor out common profiling code
    Summary:
    This diff pulls out the basic "first n distinct elements" profiling logic into FrequencyProfile and uses it in ArrayOffsetProfile and CallTargetProfile.
    
    It also simplifies this structure a little bit. In the old implementation, we need to check whether the profile is initialized (i.e. has processed its first input) because the struct starts out 0-initialized in RDS. Rather than doing that, I've opted to make FrequencyProfile use 0s as its "unoccupied entry" value and to add 1 to the offsets being profiled in ArrayOffsetProfile and the funcIDs in CallTargetProfile.
    
    This diff fixes a bug in ArrayOffsetProfile that fails to correctly merge two profiles. This bug is asymptomatic, because when the profile hits, it tends to be *very* (>90%) concentrated on a single offset.
    
    Reviewed By: mofarrell
    
    Differential Revision: D15502373
    
    fbshipit-source-id: d6aac1548f9ff1180031ad863626476f4778cfe0

--- hphp/runtime/vm/jit/array-offset-profile.cpp
+++ hphp/runtime/vm/jit/array-offset-profile.cpp
@@ -36,146 +36,61 @@ namespace HPHP { namespace jit {
 
 ///////////////////////////////////////////////////////////////////////////////
 
-std::string ArrayOffsetProfile::toString() const {
-  if (!m_init) return std::string("uninitialized");
-  std::ostringstream out;
-  for (auto const& line : m_hits) {
-    out << folly::format("{}:{},", line.pos, line.count);
-  }
-  out << folly::format("untracked:{}", m_untracked);
-  return out.str();
+namespace {
+
+template<typename ArrType>
+int32_t strPos(const ArrType* arr, const StringData* sd) {
+  auto const pos = arr->find(sd, sd->hash());
+  return validPos(pos) && arr->data()[pos].strKey() == sd ? pos : -1;
 }
+
+}  // namespace
+
+///////////////////////////////////////////////////////////////////////////////
 
 folly::Optional<uint32_t>
 ArrayOffsetProfile::choose() const {
-  Line hottest;
-  uint32_t total = 0;
-
-  if (!m_init) return folly::none;
-
-  for (auto i = 0; i < kNumTrackedSamples; ++i) {
-    auto const& line = m_hits[i];
-    if (!validPos(line.pos)) break;
-
-    if (line.count > hottest.count) hottest = line;
-    total += line.count;
-  }
-  total += m_untracked;
-
-  auto const bound = total * RuntimeOption::EvalHHIRMixedArrayProfileThreshold;
-  return hottest.count >= bound
-    ? folly::make_optional(safe_cast<uint32_t>(hottest.pos))
+  auto const entry = m_profile.choose();
+  auto const bound = RuntimeOption::EvalHHIRMixedArrayProfileThreshold;
+  return entry != nullptr && entry->count >= bound * m_profile.total()
+    ? folly::make_optional(safe_cast<uint32_t>(entry->value))
     : folly::none;
 }
 
-bool ArrayOffsetProfile::update(int32_t pos, uint32_t count) {
-  if (!m_init) init();
-
-  if (!validPos(pos)) {
-    m_untracked += count;
-    return false;
-  }
-
-  for (auto i = 0; i < kNumTrackedSamples; ++i) {
-    auto& line = m_hits[i];
-
-    if (line.pos == pos) {
-      line.count += count;
-      return true;
-    }
-    if (line.pos == -1) {
-      line.pos = pos;
-      line.count = count;
-      return true;
-    }
-  }
-
-  m_untracked += count;
-  return false;
-}
-
 void ArrayOffsetProfile::update(const ArrayData* ad, int64_t i, bool cowCheck) {
-  // We shouldn't count accesses that would fail to be detected by the offset
-  // optimization.
-  if (cowCheck && ad->cowCheck()) {
-    update(-1, 1);
-    return;
-  }
+  // We can only optimize int accesses that don't result in a COW.
   auto h = hash_int64(i);
   auto const pos =
+    cowCheck && ad->cowCheck() ? -1 :
     ad->hasMixedLayout() ? MixedArray::asMixed(ad)->find(i, h) :
     ad->isKeyset() ? SetArray::asSet(ad)->find(i, h) :
     -1;
-  update(pos, 1);
+  // Ensure that the position is either valid or the profile's missing value.
+  assertx(validPos(pos) || pos == -1);
+  m_profile.update(pos, 1);
 }
 
 void ArrayOffsetProfile::update(const ArrayData* ad, const StringData* sd,
                                 bool cowCheck) {
-  // We shouldn't count accesses that would fail to be detected by the offset
-  // optimization.  These include cases that require a COW, and cases where we
-  // need to walk the string.
-  if (cowCheck && ad->cowCheck()) {
-    update(-1, 1);
-    return;
-  }
-  auto pos = -1;
-  if (ad->hasMixedLayout()) {
-    pos = MixedArray::asMixed(ad)->find(sd, sd->hash());
-    if (pos != -1 && MixedArray::asMixed(ad)->data()[pos].strKey() != sd) {
-      pos = -1;
-    }
-  } else if (ad->isKeyset()) {
-    pos = SetArray::asSet(ad)->find(sd, sd->hash());
-    if (pos != -1 && SetArray::asSet(ad)->data()[pos].strKey() != sd) {
-      pos = -1;
-    }
-  }
-  update(pos, 1);
+  // We can only optimize str accesses that don't result in a COW, and that
+  // can match the key to the element by pointer equality (checked in strPos).
+  auto const pos =
+    cowCheck && ad->cowCheck() ? -1 :
+    ad->hasMixedLayout() ? strPos(MixedArray::asMixed(ad), sd) :
+    ad->isKeyset() ? strPos(SetArray::asSet(ad), sd) :
+    -1;
+  // Ensure that the position is either valid or the profile's missing value.
+  assertx(validPos(pos) || pos == -1);
+  m_profile.update(pos, 1);
 }
 
 void ArrayOffsetProfile::reduce(ArrayOffsetProfile& l,
                                 const ArrayOffsetProfile& r) {
-  if (!r.m_init) return;
-
-  Line scratch[2 * kNumTrackedSamples];
-  auto n = uint32_t{0};
-
-  for (auto i = 0; i < kNumTrackedSamples; ++i) {
-    auto const& rline = r.m_hits[i];
-    if (!validPos(rline.pos)) break;
-
-    // Update `l'.  If `l' can't record the update, save it to scratch.
-    if (!l.update(rline.pos, rline.count)) {
-      scratch[n++] = rline;
-    }
-  }
-  l.m_untracked += r.m_untracked;
-
-  if (n == 0) return;
-  assertx(n <= kNumTrackedSamples);
-
-  // Sort the combined samples, then copy the top hits back into `l'.
-  std::memcpy(&scratch[n], &l.m_hits[0],
-              kNumTrackedSamples * sizeof(Line));
-  std::sort(&scratch[0], &scratch[n + kNumTrackedSamples],
-            [] (Line a, Line b) { return a.count > b.count; });
-  std::memcpy(l.m_hits, scratch, kNumTrackedSamples);
-
-  // Add the hits in the discarded tail to m_untracked.
-  for (auto i = kNumTrackedSamples; i < n + kNumTrackedSamples; ++i) {
-    auto const& line = scratch[i];
-    if (!validPos(line.pos)) break;
-
-    l.m_untracked += line.count;
-  }
+  l.m_profile.reduce(r.m_profile);
 }
 
-void ArrayOffsetProfile::init() {
-  assertx(!m_init);
-  for (auto i = 0; i < kNumTrackedSamples; ++i) {
-    m_hits[i] = Line{};
-  }
-  m_init = true;
+std::string ArrayOffsetProfile::toString() const {
+  return m_profile.toString();
 }
 
 ///////////////////////////////////////////////////////////////////////////////

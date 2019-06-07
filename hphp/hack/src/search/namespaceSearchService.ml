756e6f2ad1c8a740de90ab919c63dd23a2dab925
Ted Spence <tspence@fb.com>
2019-06-07 06:49:27 -0700

    Add support for namespace aliases
    Summary:
    The namespace map system should support aliases that are configurable through .hhconfig.
    
    An aliased namespace such as "Str\" might be a shortcut for "HH\Lib\Str\".
    
    This is represented as a "shortcut" within the tree structure of namespaces.
    
    This feature is not yet enabled for usage; that will follow later in this diffstack.
    
    Reviewed By: pittsw
    
    Differential Revision: D15696171
    
    fbshipit-source-id: c6d1eb36278a9fa949b1b78c3ca51a3751cb3a6f

--- hphp/hack/src/search/namespaceSearchService.ml
+++ hphp/hack/src/search/namespaceSearchService.ml
@@ -53,6 +53,29 @@ let register_namespace (namespace: string): unit =
   ) in
   ()
 
+(* Find the namespace that matches this prefix *)
+let find_exact_match (namespace: string): nss_node =
+
+  (* If we're at the root namespace the answer is easy *)
+  if namespace = "" || namespace = "\\" then begin
+    root_namespace
+  end else begin
+    Hh_logger.log "Called find_exact_match [%s]" namespace;
+    let elem_list = String.split_on_char '\\' namespace in
+    Core_kernel.List.fold elem_list ~init:root_namespace
+      ~f:(fun current_node leaf_name ->
+
+      (* Check to see if this leaf exists within the current node *)
+      match Hashtbl.find_opt current_node.nss_children leaf_name with
+      | Some matching_leaf -> matching_leaf;
+      | None ->
+        Hh_logger.log "Failed to find [%s] in namespace tree [%s]"
+          leaf_name current_node.nss_full_namespace;
+        current_node
+    )
+  end
+
+
 (* Find all namespaces that match this prefix *)
 let find_matching_namespaces (query_text: string): si_results =
   let elem_list = String.split_on_char '\\' query_text in
@@ -86,3 +109,26 @@ let find_matching_namespaces (query_text: string): si_results =
 
   (* Now take those matches and return them as a list *)
   !matches
+
+
+(*
+ * Register a namespace alias, represented as a shortcut in the tree.
+ * The alias will share the hashtbl of its target, so future namespaces
+ * should appear in both places.
+ *)
+let register_alias (alias: string) (target_ns: string): unit =
+
+  (* First find the target and make sure there's only one *)
+  let target = find_exact_match target_ns in
+
+  (* Now assert that the alias cannot have a backslash in it *)
+  let (source_ns, name) = Utils.split_ns_from_name alias in
+  let source = find_exact_match source_ns in
+
+  (* Register this alias at the root *)
+  let new_node = {
+    nss_name = name;
+    nss_full_namespace = target.nss_full_namespace;
+    nss_children = target.nss_children;
+  } in
+  Hashtbl.add source.nss_children name new_node;

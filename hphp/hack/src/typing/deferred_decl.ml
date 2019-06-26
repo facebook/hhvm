3d7dbb7c9f19d987ebcfd63c78fca9412a4d736f
Tatiana Racheva <tatianaracheva@fb.com>
2019-06-25 20:54:02 -0700

    `Deferred_decl` module: Storage for dependencies and dependents
    Summary:
    Today, when we type check file `A.php`, we declare dependencies as we encounter them. This leads to bad type checking times on files that depend on a lot of classes. This is relevant in the case of generated files, for example.
    
    The idea of this diff stack is to defer declaring dependencies and discard type errors for `A.php` if its dependencies weren't already available.
    
    =====
    
    The `Deferred_decl` module provides global storage for files whose declaration should be deferred.
    
    Differential Revision: D15862879
    
    fbshipit-source-id: 9204d16c3887e879788b3bd500b646cdb1d8ebea

---
+++ hphp/hack/src/typing/deferred_decl.ml
@@ -0,0 +1,32 @@
+(**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the "hack" directory of this source tree.
+ *
+ *)
+
+open Core_kernel
+
+exception Defer of Relative_path.t
+
+type deferment = Relative_path.t
+type deferments_t = Relative_path.Set.t
+
+let (deferments: deferments_t ref) = ref Relative_path.Set.empty
+let (counter: int ref) = ref 0
+
+let should_defer ~(d: Relative_path.t) ~(threshold: int): unit =
+  counter := !counter + 1;
+  if threshold < !counter then
+    raise (Defer d)
+
+let add ~(d:deferment): unit =
+  deferments := Relative_path.Set.add !deferments d
+
+let reset () =
+  deferments := Relative_path.Set.empty;
+  counter := 0
+
+let get ~(f:(deferment -> 'a)): 'a list =
+  Relative_path.Set.fold !deferments ~init:[] ~f:(fun d l -> (f d) :: l)
--- hphp/hack/test/unit/server_tests.ml
+++ hphp/hack/test/unit/server_tests.ml
@@ -25,7 +25,73 @@ let test_dmesg_parser () =
   ] in
   Sys_utils.find_oom_in_dmesg_output test_process_data.ServerProcess.pid "hh_server" input
 
+let ensure_count (count: int): unit =
+  let deferred = Deferred_decl.get ~f:(fun d -> d) in
+  Asserter.Int_asserter.assert_equals
+    count
+    (List.length deferred)
+    "The number of deferred items should match the expected value"
+
+let test_deferred_decl_add () =
+  Deferred_decl.reset ();
+  ensure_count 0;
+
+  Deferred_decl.add (Relative_path.create Relative_path.Dummy "foo");
+  ensure_count 1;
+
+  Deferred_decl.add (Relative_path.create Relative_path.Dummy "foo");
+  ensure_count 1;
+
+  Deferred_decl.add (Relative_path.create Relative_path.Dummy "bar");
+  ensure_count 2;
+
+  Deferred_decl.reset ();
+  ensure_count 0;
+
+  true
+
+let ensure_threshold ~(threshold: int) ~(limit: int) ~(expected: int): unit =
+  Deferred_decl.reset ();
+  ensure_count 0;
+
+  let deferred_count = ref 0 in
+  for i = 1 to limit do
+    let path = Printf.sprintf "foo-%d" i in
+    let relative_path = Relative_path.create Relative_path.Dummy path in
+    try
+      Deferred_decl.should_defer ~d:relative_path ~threshold
+    with Deferred_decl.Defer d ->
+      Asserter.Bool_asserter.assert_equals
+        (i >= threshold)
+        true
+        (Printf.sprintf "We should have reached the threshold %d, i=%d" threshold i);
+      Asserter.String_asserter.assert_equals
+        (Relative_path.suffix d)
+        path
+        "The deferred path should be the last one we saw";
+      deferred_count := !deferred_count + 1;
+  done;
+
+  Asserter.Int_asserter.assert_equals
+    expected
+    !deferred_count
+    (Printf.sprintf
+      "Unexpected deferred count; threshold: %d; limit: %d; expected: %d"
+      threshold
+      limit
+      expected)
+
+let test_deferred_decl_should_defer () =
+  ensure_threshold ~threshold:0 ~limit:1 ~expected: 1;
+  ensure_threshold ~threshold:1 ~limit:2 ~expected: 1;
+  ensure_threshold ~threshold:2 ~limit:1 ~expected: 0;
+  ensure_threshold ~threshold:1 ~limit:5 ~expected: 4;
+
+  true
+
 let tests = [
+  "test_deferred_decl_add", test_deferred_decl_add;
+  "test_deferred_decl_should_defer", test_deferred_decl_should_defer;
   "test_dmesg_parser", test_dmesg_parser;
 ]
 

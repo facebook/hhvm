2313620c1661181d0e088ddac02277457cbfd405
Jeff Hemphill <jhemphill@fb.com>
2019-07-02 20:11:39 -0700

    Separate SQLite code from Repo code
    Summary:
    My native autoloader stores data in a SQLite database for fast cold starts and CLI standalone performance. HHVM already uses SQLite to cache bytecode. These SQLite DBs are the `.hhbc` and `.hhbbc` files managed by the `Repo` class. I originally tried storing my autoloader information in the `.hhbc` DB, but this led to terrible performance. SQLite only allows one thread or process to write to a DB at a given time, and the autoloader and unit emitter fought with each other for the ability to write to the DB.
    
    The `Repo` class provides a fairly clean, opinionated, RAII-style wrapper around SQLite. I really liked that, so here I've abstracted the bits that aren't specific to `.hhbc` into a new `SQLite` class. In theory, we can make the `Repo` class depend on this `SQLite` class to reduce the duplication, but I'm more interested in just using this class to power the native autoloader for the time being.
    
    Reviewed By: paulbiss
    
    Differential Revision: D15562623
    
    fbshipit-source-id: c53e9a1a186c354d0d24cec7f5fd5d3026e377ae

---
+++ hphp/util/sqlite-wrapper-helpers.h
@@ -0,0 +1,194 @@
+/*
+   +----------------------------------------------------------------------+
+   | HipHop for PHP                                                       |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2010-present Facebook, Inc. (http://www.facebook.com)  |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+*/
+
+#pragma once
+
+#include <stdexcept>
+#include <string>
+
+#include <folly/Range.h>
+
+struct sqlite3_stmt;
+
+namespace HPHP {
+
+struct SQLite;
+struct SQLiteExc;
+struct SQLiteQuery;
+struct SQLiteStmt;
+struct SQLiteTxn;
+
+//==============================================================================
+
+/**
+ * RAII wrapper around a SQLite transaction.
+ *
+ * Rolls the transaction back when destroyed unless commit() is called first.
+ */
+struct SQLiteTxn {
+  explicit SQLiteTxn(SQLite& db);
+  SQLiteTxn(SQLiteTxn&&) noexcept;
+  SQLiteTxn& operator=(SQLiteTxn&&) noexcept;
+  ~SQLiteTxn();
+
+  /**
+   * Return a query object to run the given statement.
+   *
+   * Only one query object may exist for any given statement at any given time.
+   */
+  SQLiteQuery query(SQLiteStmt& stmt) noexcept;
+
+  /**
+   * Run a one-off query without storing the prepared statement or
+   * returning results.
+   */
+  void exec(folly::StringPiece sql);  // throws(SQLiteExc)
+
+  /**
+   * Complete the transaction.
+   *
+   * If you don't call this method, all modifications made to the
+   * database while this transaction was alive will be rolled back
+   * when this object is destroyed.
+   */
+  void commit();  // throws(SQLiteExc)
+
+ private:
+  SQLiteTxn() = delete;
+  SQLiteTxn(const SQLiteTxn&) = delete;
+  SQLiteTxn& operator=(const SQLiteTxn&) = delete;
+  void* operator new(size_t) = delete;
+
+  SQLite* m_db;
+  bool m_pending = true;
+};
+
+//==============================================================================
+
+/**
+ * RAII wrapper around a sqlite3_stmt statement object.
+ */
+struct SQLiteStmt {
+  SQLiteStmt(SQLiteStmt&&) noexcept;
+  SQLiteStmt& operator=(SQLiteStmt&&) noexcept;
+  ~SQLiteStmt();
+
+  /**
+   * Return the SQL string used to prepare this statement.
+   */
+  folly::StringPiece sql() const noexcept;
+
+ private:
+  friend struct SQLite;
+  friend struct SQLiteQuery;
+  friend struct SQLiteTxn;
+
+  SQLiteStmt() = delete;
+  SQLiteStmt(const SQLiteStmt&) = delete;
+  SQLiteStmt& operator=(const SQLiteStmt&) = delete;
+  void* operator new(size_t) = delete;
+
+  SQLiteStmt(SQLite& db, folly::StringPiece sql);  // throws(SQLiteExc)
+
+  /**
+   * Return a query object to run this statement.
+   *
+   * Only one query object may exist at a time.
+   */
+  SQLiteQuery query() noexcept;
+
+  /**
+   * Reinitialize this statement to run a new query.
+   *
+   * Called by SQLiteQuery's destructor.
+   */
+  void reset() noexcept;
+
+  sqlite3_stmt* m_stmt = nullptr;
+  bool m_queryExists = false;
+};
+
+//==============================================================================
+
+/**
+ * Represents a single query run on a sqlite3_stmt.
+ *
+ * Resets the sqlite3_stmt when destroyed.
+ */
+struct SQLiteQuery {
+  SQLiteQuery(SQLiteQuery&&) noexcept;
+  SQLiteQuery& operator=(SQLiteQuery&&) noexcept;
+  ~SQLiteQuery();
+
+  bool row() const noexcept { return m_row; }
+
+  bool done() const noexcept { return m_done; }
+
+  folly::StringPiece sql() const noexcept;
+
+  void step();  // throws(SQLiteExc)
+
+  void bindBlob(const char* paramName, const void* blob, int size,
+                bool isStatic = false) noexcept;
+  void bindText(const char* paramName, const char* text, int size,
+                bool isStatic = false) noexcept;
+  void bindString(const char* paramName, const folly::StringPiece s) noexcept;
+  void bindDouble(const char* paramName, double val) noexcept;
+  void bindInt(const char* paramName, int val) noexcept;
+  void bindBool(const char* paramName, bool b) noexcept;
+  void bindInt64(const char* paramName, int64_t val) noexcept;
+  void bindNull(const char* paramName) noexcept;
+
+  // Get the column value as the named type. If the value cannot be converted
+  // into the named type then an error is thrown.
+  bool getBool(int iCol);                                   // throws(SQLiteExc)
+  int getInt(int iCol);                                     // throws(SQLiteExc)
+  int64_t getInt64(int iCol);                               // throws(SQLiteExc)
+  double getDouble(int iCol);                               // throws(SQLiteExc)
+  void getBlob(int iCol, const void*& blob, size_t& size);  // throws(SQLiteExc)
+  const folly::StringPiece getString(int iCol);             // throws(SQLiteExc)
+
+ private:
+  friend struct SQLite;
+  friend struct SQLiteStmt;
+  friend struct SQLiteTxn;
+
+  SQLiteQuery() = delete;
+  SQLiteQuery(SQLiteQuery&) = delete;
+  SQLiteQuery& operator=(SQLiteQuery&) = delete;
+  void* operator new(size_t) = delete;
+
+  explicit SQLiteQuery(SQLiteStmt& stmt);
+
+  SQLiteStmt* m_stmt;
+  bool m_row = false;
+  bool m_done = false;
+};
+
+//==============================================================================
+
+/**
+ * Exception encapsulating a SQLite error
+ */
+struct SQLiteExc : std::runtime_error::runtime_error {
+  SQLiteExc(int code, std::string sql);
+  SQLiteExc() = delete;
+
+  int m_code;
+  std::string m_sql;
+};
+
+}  // namespace HPHP
---
+++ hphp/util/sqlite-wrapper.h
@@ -0,0 +1,113 @@
+/*
+   +----------------------------------------------------------------------+
+   | HipHop for PHP                                                       |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2010-present Facebook, Inc. (http://www.facebook.com)  |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+*/
+
+#pragma once
+
+#include <string>
+
+#include "hphp/util/sqlite-wrapper-helpers.h"
+
+struct sqlite3;
+
+namespace HPHP {
+
+/**
+ * RAII wrapper around a sqlite3 database connection.
+ *
+ * Use with:
+ *
+ *   SQLite& db = SQLite::get(":memory:");
+ *
+ *   SQLiteTxn txn = db.begin();
+ *   txn.exec("CREATE TABLE foo (bar)");
+ *
+ *   SQLiteStmt insertStmt = db.prepare("INSERT INTO foo VALUES (@v)");
+ *   for (auto i = 0; i < 10; i++) {
+ *     SQLiteQuery query = db.query(insertStmt);
+ *
+ *     query.bindInt("@v", i);
+ *     query.step();
+ *   }
+ *
+ *   SQLiteStmt selectStmt = db.prepare("SELECT bar FROM foo;");
+ *   SQLiteQuery query = txn.query(selectStmt);
+ *   for (; !query.done(); query.step()) {
+ *     std::cout << query.getInt(0) << std::endl;
+ *   }
+ *
+ */
+struct SQLite {
+  SQLite(SQLite&&) noexcept;
+  SQLite& operator=(SQLite&&) noexcept;
+  ~SQLite();
+
+  /**
+   * Return a new SQLite connection, creating the DB file if necessary.
+   *
+   * path is the location of the DB in your filesystem, or ":memory:"
+   * if you want to store data in memory instead.
+   */
+  static SQLite connect(const folly::StringPiece path);
+
+  /**
+   * Compile the given SQL query into a statement object which can run and rerun
+   * the query.
+   */
+  SQLiteStmt prepare(const folly::StringPiece sql);  // throws(SQLiteExc)
+
+  /**
+   * Begin a SQLite transaction to run queries within.
+   */
+  SQLiteTxn begin();
+
+  /**
+   * Sleep for a time up to the given maximum when the DB is locked.
+   *
+   * Setting a number equal to 0 or less than zero will disable the
+   * busy timeout.
+   */
+  void setBusyTimeout(int ms) noexcept;
+
+  /**
+   * Return the most recent error message from SQLite.
+   */
+  std::string errMsg() const noexcept;
+
+ private:
+  friend struct SQLiteStmt;
+  friend struct SQLiteTxn;
+
+  explicit SQLite(sqlite3* dbc);
+
+  SQLite() = delete;
+  SQLite(const SQLite&) = delete;
+  SQLite& operator=(const SQLite&) = delete;
+
+  void txPush();  // throws(SQLiteExc)
+  void txPop();   // throws(SQLiteExc)
+  void rollback() noexcept;
+  void commit();  // throws(SQLiteExc)
+
+  sqlite3* m_dbc = nullptr;
+  unsigned m_txDepth = 0;   // Transaction nesting depth.
+  bool m_rollback = false;  // If true, rollback rather than commit.
+
+  SQLiteStmt m_beginStmt;
+  SQLiteStmt m_rollbackStmt;
+  SQLiteStmt m_commitStmt;
+};
+
+}  // namespace HPHP

200a1ca3d0eb95f01ab4025ad718bb46a27d50f6
Max Wang <mwang@fb.com>
2019-07-03 19:24:52 -0700

    Add a logging Trace category
    Reviewed By: mofarrell
    
    Differential Revision: D15571309
    
    fbshipit-source-id: f316069b6c3fe30cc0366a750453590b0ac568ff

--- hphp/util/trace.h
+++ hphp/util/trace.h
@@ -151,6 +151,7 @@ namespace Trace {
       TM(jittime)       \
       TM(layout)        \
       TM(libxml)        \
+      TM(logging)       \
       TM(mcg)           \
       TM(mcgstats)      \
       TM(minstr)        \

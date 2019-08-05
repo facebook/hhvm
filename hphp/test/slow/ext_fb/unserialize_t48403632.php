945f43706e9f9c3dec7dfb70ab9af7ee9f7a55eb
Rick Lavoie <rlavoie@fb.com>
2019-08-05 15:58:13 -0700

    Fix CHECK_ENOUGH in fb_unserialize
    Summary:
    CHECK_ENOUGH was casting things which might be int64_t to
    int.
    
    Reviewed By: mofarrell
    
    Differential Revision: D16651507
    
    fbshipit-source-id: ec8972258d1608666cede4e4e0454103472caf1f

---
+++ hphp/test/slow/ext_fb/unserialize_t48403632.php
@@ -0,0 +1,11 @@
+<?hh
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+
+<<__EntryPoint>>
+function main() {
+  $success = null;
+  var_dump(
+    fb_unserialize("\xf9\xf2\x01\x00\x00\x00\x00\x00\x00\x01a", inout $success)
+  );
+  var_dump($success);
+}

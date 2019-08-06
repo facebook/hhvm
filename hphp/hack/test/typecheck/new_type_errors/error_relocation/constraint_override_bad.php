57bf7b5c8953ca64ee6fd14412c9258ae6558870
Matt Schellhas <rms@fb.com>
2019-08-06 16:21:07 -0700

    A representative subset of errors that currently appear on declarations, but maybe shouldn't
    Summary:
    Currently many of our errors appear on class/interface/trait declarations, rather than the members that cause them. This made it easier to suppress bad classes in the past, but now that hack is more mature it's making it harder to categorize and programmatically solve the remaining issues.
    
    This adds some tests so we can try to split that up confidently.
    
    Reviewed By: DavidSnider
    
    Differential Revision: D16612542
    
    fbshipit-source-id: 29022008d1900323aa717e68eb6ce7551583268f

---
+++ hphp/hack/test/typecheck/new_type_errors/error_relocation/constraint_override_bad.php
@@ -0,0 +1,27 @@
+<?hh // strict
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+class B {
+  public function foo<T super C>(): T {
+    return new C();
+  }
+}
+class C extends B {
+  public function foo<T super B>(): T {
+    return new B();
+  }
+  public function bar(): void {
+    echo 'bar';
+  }
+}
+
+function CallOnB(B $b): void {
+  $x = $b->foo();
+  $x->bar();
+}
+function TestIt(): void {
+  $c = new C();
+  CallOnB($c);
+}
+/* HH_FIXME[1002] */
+TestIt();
---
+++ hphp/hack/test/typecheck/new_type_errors/error_relocation/where_override_bad.php
@@ -0,0 +1,35 @@
+<?hh // strict
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+class Base { }
+class Derived extends Base {
+  public function bar(): void { echo 'bar'; }
+}
+
+class MyList<T> {
+  public function foo(T $x):void where T as Base { }
+  public function boo(T $x):void where T as Derived { }
+}
+
+// This is bad: we've strengthened a constraint
+class List4<T> extends MyList<T> {
+  public function foo(T $x):void where T as Derived {
+    $x->bar();
+  }
+  public function boo(T $x):void {
+  }
+}
+
+
+function BreakIt(MyList<Base> $l):void {
+  $l->foo(new Base());
+}
+
+
+function TestIt():void {
+  $x = new List4();
+  BreakIt($x);
+}
+
+/* HH_FIXME[1002] */
+TestIt();

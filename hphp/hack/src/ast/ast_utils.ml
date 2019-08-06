01f823512b0fae69b75e827cc7144e7a5825c11d
Arun Kumar <arkumar@fb.com>
2019-08-06 04:35:40 -0700

    Remove unused functions in Ast_utils
    Reviewed By: shiqicao
    
    Differential Revision: D16642916
    
    fbshipit-source-id: 67eb7f644862bf146e16c4e94e33d191c9bfde5d

--- hphp/hack/src/ast/ast_utils.ml
+++ hphp/hack/src/ast/ast_utils.ml
@@ -10,37 +10,6 @@
 open Core_kernel
 open Ast
 
-(* Given a Ast.program, give me the list of entities it defines *)
-let get_defs ast =
-  (* fold_right traverses the file from top to bottom, and as such gives nicer
-   * error messages than fold_left. E.g. in the case where a function is
-   * declared twice in the same file, the error will say that the declaration
-   * with the larger line number is a duplicate. *)
-  let rec get_defs ast acc =
-    List.fold_right
-      ast
-      ~init:acc
-      ~f:(fun def ((acc1, acc2, acc3, acc4) as acc) ->
-        match def with
-        | Ast.Fun f ->
-          (FileInfo.pos_full f.Ast.f_name :: acc1, acc2, acc3, acc4)
-        | Ast.Class c ->
-          (acc1, FileInfo.pos_full c.Ast.c_name :: acc2, acc3, acc4)
-        | Ast.Typedef t ->
-          (acc1, acc2, FileInfo.pos_full t.Ast.t_id :: acc3, acc4)
-        | Ast.Constant cst ->
-          (acc1, acc2, acc3, FileInfo.pos_full cst.Ast.cst_name :: acc4)
-        | Ast.Namespace (_, defs) -> get_defs defs acc
-        | Ast.NamespaceUse _
-        | Ast.SetNamespaceEnv _ ->
-          acc
-        (* toplevel statements are ignored *)
-        | Ast.FileAttributes _
-        | Ast.Stmt _ ->
-          acc)
-  in
-  get_defs ast ([], [], [], [])
-
 (* Utility functions for getting all nodes of a particular type *)
 class ast_get_defs_visitor =
   object
@@ -48,51 +17,6 @@ class ast_get_defs_visitor =
 
     method! on_def acc def = def :: acc
   end
-
-type ignore_attribute_env = { ignored_attributes: string list }
-
-let ast_deregister_attributes_mapper =
-  object (self)
-    inherit [_] Ast.endo as super
-
-    method ignored_attr env l =
-      List.exists l (fun attr ->
-          List.mem env.ignored_attributes (snd attr.ua_name) ~equal:( = ))
-
-    (* Filter all functions and classes with the user attributes banned *)
-    method! on_program env toplevels =
-      let toplevels =
-        List.filter toplevels (fun toplevel ->
-            match toplevel with
-            | Fun f when self#ignored_attr env f.f_user_attributes -> false
-            | Class c when self#ignored_attr env c.c_user_attributes -> false
-            | _ -> true)
-      in
-      super#on_program env toplevels
-
-    method! on_class_ env this =
-      (* Filter out class elements which are methods with wrong attributes *)
-      let body = this.c_body in
-      let body =
-        List.filter body (fun elt ->
-            match elt with
-            | Method m when self#ignored_attr env m.m_user_attributes -> false
-            | ClassVars cv when self#ignored_attr env cv.cv_user_attributes ->
-              false
-            | _ -> true)
-      in
-      let this = { this with c_body = body } in
-      super#on_class_ env this
-  end
-
-let deregister_ignored_attributes ast =
-  let env =
-    {
-      (* For now, only ignore the __PHPStdLib *)
-      ignored_attributes = [Naming_special_names.UserAttributes.uaPHPStdLib];
-    }
-  in
-  ast_deregister_attributes_mapper#on_program env ast
 
 let get_def_nodes ast = List.rev ((new ast_get_defs_visitor)#on_program [] ast)
 

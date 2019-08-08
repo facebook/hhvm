5f9fb490502cd8f2d81314033ce70f3e1c925601
Young Kim <youngkim@fb.com>
2019-08-08 12:00:28 -0700

    Add handling of Bucket.wait in shared_lru
    Summary:
    Clients of `shared_lru` can now return a `Bucket.wait` state if they want to wait for all pending jobs to complete.
    
    Due to this change, some of the logic in `shared_lru.ml` was able to be simplified:
    
    - `fork_and_start_workers` only starts a single worker, and was renamed appropriately.
    - `monitor_workers_h` is responsible for starting all workers, and workers do not have to be "pre-initialized" with work beforehand.
    
    I don't think that this refactor will have an effect on perf/memory usage, but I'm running tests in comparison with the current implementation, and against `merge-base`.
    
    Update: [4 reps](https://fburl.com/everpaste/7v11nqqo) finished, and it looks like this implementation saves memory (and might be better in terms of performance as well, more reps are necessary) – but it does not appear to cause any regressions!
    
    Reviewed By: 2BitSalute
    
    Differential Revision: D16634639
    
    fbshipit-source-id: 76caa3715bd46e5f21fb165395805f0413590b3a

--- hphp/hack/src/typing/typing_lru_check_service.ml
+++ hphp/hack/src/typing/typing_lru_check_service.ml
@@ -11,6 +11,13 @@ open Core_kernel
 module Bucket = Hack_bucket
 open Typing_check_service
 
+type job_state = {
+  files_to_process: file_computation list;
+  errors_acc: Errors.t;
+  num_files_checked: int;
+  pending_jobs: int;
+}
+
 (* Helper function to process a single [file_computation] *)
 let process_file_computation
     ~(dynamic_view_files: Relative_path.Set.t)
@@ -38,15 +45,18 @@ let process_in_parallel
   ServerProgress.send_percentage_progress_to_monitor
     "typechecking" 0 files_initial_count "files";
 
-  let next ~(batch_size: int) (files_to_process, errors_acc, num_files) =
-    let files_to_process, next_input =
-      match files_to_process with
-      | [] -> [], Hack_bucket.Done
-      | _ ->
-        let batch, remaining = List.split_n files_to_process batch_size in
-        remaining, Hack_bucket.Job batch
+  let next ~(batch_size: int) job_state =
+    let files_to_process, next_input, pending_jobs =
+      match (job_state.files_to_process, job_state.pending_jobs) with
+      | ([], 0) -> ([], Hack_bucket.Done, 0)
+      | ([], n) -> ([], Hack_bucket.Wait, n)
+      | (files, n) ->
+        let batch, remaining = List.split_n files batch_size in
+        (remaining, Hack_bucket.Job batch, n + 1)
     in
-    (files_to_process, errors_acc, num_files), next_input
+    let job_state =
+      {job_state with files_to_process; pending_jobs} in
+    job_state, next_input
   in
 
   let job (fc_lst: file_computation list) =
@@ -73,7 +83,7 @@ let process_in_parallel
         in
         (* Errors.merge is a List.rev_append, so put the [acc] second *)
         let acc = Errors.merge new_errors acc in
-        (* Check if we should exit due to memory pressure *)
+        (* TODO: Check if we should exit due to memory pressure *)
         job_helper ~fc_lst:fc_tl ~acc
     in
     (* Process the files! *)
@@ -88,14 +98,18 @@ let process_in_parallel
     errors, num_files_checked
   in
 
-  let callback
-      (fnl, errors_acc, files_count_acc)
-      (errors, num_files) =
+  let callback job_state (errors, num_files) =
+    let { errors_acc; num_files_checked; pending_jobs; _ } = job_state in
+    let total_files_checked = num_files_checked + num_files in
     ServerProgress.send_percentage_progress_to_monitor
-      "typechecking" (files_count_acc + num_files) files_initial_count "files";
+      "typechecking" total_files_checked files_initial_count "files";
     (* Errors.merge is a List.rev_append, so put the [acc] second *)
     let errors_acc = Errors.merge errors errors_acc in
-    fnl, errors_acc, (files_count_acc + num_files)
+    { job_state with
+      errors_acc;
+      num_files_checked=total_files_checked;
+      pending_jobs=(pending_jobs - 1);
+    }
   in
 
   let max_size = Bucket.max_size () in
@@ -106,9 +120,15 @@ let process_in_parallel
   in
 
   (* Start shared_lru workers. *)
-  let _files_left, errors ,_num_files_checked = Shared_lru.run
+  let initial_state = {
+    files_to_process=fnl;
+    errors_acc=Errors.empty;
+    num_files_checked=0;
+    pending_jobs=0;
+  } in
+  let finished_state = Shared_lru.run
     ~host_env:lru_host_env
-    ~initial_env:(fnl, Errors.empty, 0)
+    ~initial_env:initial_state
     ~job
     ~callback
     ~next:(next ~batch_size)
@@ -116,8 +136,7 @@ let process_in_parallel
 
   let env = interrupt.MultiThreadedCall.env in
   TypeCheckStore.clear();
-  errors, env, []
-
+  finished_state.errors_acc, env, []
 
 (* Disclaimer: does not actually go with interrupt yet, although it will
  * in a future version. The function is named the same as the one in

a9a9675ddedec3320bbf567866030b233be8f4f5
Kasper Kopec <kasper@fb.com>
2019-08-14 16:59:01 -0700

    Add a repro case for mysterious format crash
    Summary: I want to ask some people for help, and giving them an already committed ready to go repro will increase chances of getting such help.
    
    Differential Revision: D16815764
    
    fbshipit-source-id: 2d59daa76be9ee6e1e5ca50ed3adc3d20bb3145a

---
+++ hphp/hack/test/rust/ocaml_rust_crash.rs
@@ -0,0 +1,12 @@
+#[macro_use]
+extern crate ocaml;
+
+caml!(rust_crash() {
+    let _four = format!("{}", 4);
+    ocaml::Value::unit()
+});
+
+caml!(rust_no_crash() {
+    let _also_four = format!("{}", 4.to_string());
+    ocaml::Value::unit()
+});

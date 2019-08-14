ba993795d12cfbc88392c4a9b682dd26ce594636
Leo Osvald <leoo@fb.com>
2019-08-13 22:39:14 -0700

    Build and link Rust code with dune
    Summary:
    Call Cargo from dune and link real Rust code as static libraries,
    removing the need for stubs (except in OSS build for now).
    
    Reviewed By: dabek
    
    Differential Revision: D16657596
    
    fbshipit-source-id: 5a9c610f8895e744007ca80d8c54c80e86ea5b76

---
+++ hphp/hack/scripts/build_rust_to_ocaml.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+hack_root="${HACKDIR}"
+
+if [ -z "$HACKDIR" ] || ! [ -e "$HACKDIR/src/facebook/dune" ]; then
+   echo >&2 "ERROR: only stubs for Rust FFI are supported in OSS build"
+   exit 1
+fi
+
+if (( "$#" < 2 )); then
+   echo "Usage: CARGO_BIN=path/to/cargo-bin $0 PACKAGE_NAME LIB_NAME"
+   exit 2
+fi
+pkg="$1"
+lib="$2"
+shift 2
+
+profile=debug; profile_flags=
+if [ -z ${HACKDEBUG+1} ]; then
+  profile=release; profile_flags="--release"
+fi
+( # add CARGO_BIN to PATH so that rustc and other tools can be invoked
+  [[ -n "$CARGO_BIN" ]] && PATH="$CARGO_BIN:$PATH";
+  # note: --manifest-path doesn't work with custom toolchain, so do cd
+  cd "$hack_root" && \
+  cargo build --frozen --package "$pkg" $profile_flags "$@"
+) && cp "$hack_root/target/$profile/lib$lib.a" "lib${lib}_stubs.a"

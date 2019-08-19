66a7a6ce8bab9476ab1cda55809b5dfa6d6ff153
Pierre Oechsel <poechsel@fb.com>
2019-08-19 03:53:38 -0700

    Refactor of LambdaParameterType in ServerRewriteLambdaParameters
    Summary: Refactor of LambdaParameterType in a new file, CodemodTypePrinter
    
    Reviewed By: CatherineGasnier
    
    Differential Revision: D16645984
    
    fbshipit-source-id: 9261a2949f4bdd9aa60ba1d5e7a1e881981ecd76

--- hphp/hack/src/server/serverRewriteLambdaParameters.ml
+++ hphp/hack/src/server/serverRewriteLambdaParameters.ml
@@ -15,93 +15,17 @@ module PositionedTree = Full_fidelity_syntax_tree
   .WithSyntax(Full_fidelity_positioned_syntax)
 
 open Syntax
+let is_not_acceptable ty =
+  let finder = object
+    inherit [_] Type_visitor.type_visitor
+    method! on_tprim acc _ = function
+    | Aast.Tnull | Aast.Tvoid | Aast.Tresource | Aast.Tnoreturn -> true
+    | _ -> acc
+  end
+  in finder#on_type false ty
 
-module LambdaParameterType : sig
-  val print : Typing_defs.locl Typing_defs.ty -> string option
-end = struct
-  open Typing_defs
-
-  exception Non_denotable
-
-  let print_tprim =
-    let open Aast in
-    function
-    | Tbool -> "bool"
-    | Tint -> "int"
-    | Tfloat -> "float"
-    | Tnum -> "num"
-    | Tstring -> "string"
-    | Tarraykey -> "arraykey"
-    | Tnull | Tvoid | Tresource | Tnoreturn -> raise Non_denotable
-
-  let rec print_ty_exn ty =
-    match snd ty with
-    | Tprim p -> print_tprim p
-    | Tany | Terr | Tvar _ | Tabstract ((AKdependent _ | AKgeneric _), _)
-    | Tanon _ | Tunion _ | Tintersection _ | Tobject | Tarraykind (AKany | AKempty) ->
-      raise Non_denotable
-    | Tnonnull -> "nonnull"
-    | Tdynamic -> "dynamic"
-    | Toption (_, Tnonnull) -> "mixed"
-    | Toption ty -> "?" ^ print_ty_exn ty
-    | Tfun ft ->
-      let params = List.map ft.ft_params ~f:print_fun_param_exn in
-      let params = match ft.ft_arity with
-        | Fstandard _ -> params
-        | Fellipsis _ -> raise Non_denotable
-        | Fvariadic (_, p) -> params @ [print_ty_exn p.fp_type.et_type ^ "..."] in
-      Printf.sprintf "(function(%s): %s)"
-        (String.concat ~sep:", " params) (print_ty_exn ft.ft_ret.et_type)
-    | Ttuple tyl ->
-      "(" ^ print_tyl_exn tyl ^ ")"
-    | Tdestructure tyl ->
-      "list(" ^ print_tyl_exn tyl ^ ")"
-    | Tshape (shape_kind, fdm) ->
-      let fields =
-        List.map (Nast.ShapeMap.elements fdm) ~f:print_shape_field_exn in
-      let fields = match shape_kind with
-        | Closed_shape -> fields
-        | Open_shape -> fields @ ["..."] in
-      Printf.sprintf "shape(%s)" (String.concat ~sep:", " fields)
-    | Tabstract (AKnewtype (name, []), _) -> Utils.strip_ns name
-    | Tabstract (AKnewtype (name, tyl), _) ->
-      Utils.strip_ns name ^ "<" ^ print_tyl_exn tyl ^ ">"
-    | Tclass ((_, name), _, []) -> Utils.strip_ns name
-    | Tclass ((_, name), _, tyl) ->
-      Utils.strip_ns name ^ "<" ^ print_tyl_exn tyl ^ ">"
-    | Tarraykind (AKvarray ty) ->
-      Printf.sprintf "varray<%s>" (print_ty_exn ty)
-    | Tarraykind (AKvarray_or_darray ty) ->
-      Printf.sprintf "varray_or_darray<%s>" (print_ty_exn ty)
-    | Tarraykind (AKvec ty) ->
-      Printf.sprintf "array<%s>" (print_ty_exn ty)
-    | Tarraykind (AKdarray (ty1, ty2)) ->
-      Printf.sprintf "darray<%s, %s>" (print_ty_exn ty1) (print_ty_exn ty2)
-    | Tarraykind (AKmap (ty1, ty2)) ->
-      Printf.sprintf "array<%s, %s>" (print_ty_exn ty1) (print_ty_exn ty2)
-
-  and print_tyl_exn tyl =
-    String.concat ~sep:", " (List.map tyl ~f:print_ty_exn)
-
-  and print_fun_param_exn param =
-    match param.fp_kind with
-    | FPinout -> "inout " ^ print_ty_exn param.fp_type.et_type
-    | _ -> print_ty_exn param.fp_type.et_type
-
-  and print_shape_field_exn (name, {sft_optional; sft_ty; _}) =
-    Printf.sprintf "%s%s => %s"
-      (if sft_optional then "?" else "")
-      (print_shape_field_name name)
-      (print_ty_exn sft_ty)
-
-  and print_shape_field_name name =
-    let s = Typing_env.get_shape_field_name name in
-    match name with
-    | Ast_defs.SFlit_str _ -> "'" ^ s ^ "'"
-    | _ -> s
-
-  let print ty = try Some (print_ty_exn ty) with Non_denotable -> None
-end
+let print_ty ty =
+  if is_not_acceptable ty then None else CodemodTypePrinter.print ty
 
 let get_first_suggested_type_as_string file type_map node =
   let open Option.Monad_infix in
@@ -115,7 +39,7 @@ let get_first_suggested_type_as_string file type_map node =
       let env, ty = Tast_env.expand_type env ty in
       begin match ty with
       | Typing_reason.Rsolve_fail _, ty_ ->
-        begin match LambdaParameterType.print ty with
+        begin match print_ty ty with
         | Some type_str -> Some type_str
         | None ->
           Hh_logger.log "%s failed to rewrite lambda parameter %s: the suggested type %s is non-denotable"

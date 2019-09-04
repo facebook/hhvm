ac218381d4ccace03e28ae7db88a76c436c3a386
Steve Cao <shiqicao@fb.com>
2019-09-04 12:56:17 -0700

    move lower_pass to different folder
    Summary: as title
    
    Reviewed By: losvald
    
    Differential Revision: D17144063
    
    fbshipit-source-id: 1638ba46305c57d43629282067421c41bf87855f

--- hphp/hack/test/rust/run_lowerer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PREFIX="$(realpath ~/fbsource/fbcode)"
-buck run  @mode/dev //hphp/hack/test/rust:rust_ocaml -- --lower > /tmp/lowerer.out
-grep ":EQUAL:" /tmp/lowerer.out | sed "s|$PREFIX||" |  sort > ~/fbsource/fbcode/hphp/hack/test/rust/lowerer_pass
-hg diff ~/fbsource/fbcode/hphp/hack/test/rust/lowerer_pass --rev .~1

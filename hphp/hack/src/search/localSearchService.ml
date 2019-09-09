0d7e09777255a2603d1cd2a63f63e7eff6494c3b
Ted Spence <tspence@fb.com>
2019-09-09 09:48:01 -0700

    Enable synthetic tests with fake "root" folders to work
    Summary:
    As part of the project to gradually phase out the old "Trie"-based autocomplete, we'll switch our test suite to use LocalIndex.
    
    However, as it turns out, some tests will fake the notion of editing a file and use a phony root value.  Let's allow those tests to proceed even if they are using LocalIndex.
    
    Basically, the tests work as follows:
    * Set the root folder to "/"
    * Send phony "file edited" messages to hh
    * Expect that files edited via this phony process work correctly
    
    Reviewed By: 2BitSalute
    
    Differential Revision: D17228758
    
    fbshipit-source-id: 29c7c82d3784f91319dc3d70b8dced2d825a36a8

--- hphp/hack/src/search/localSearchService.ml
+++ hphp/hack/src/search/localSearchService.ml
@@ -109,28 +109,24 @@ let update_file
     si_env =
   let _ = info in
   let tombstone = get_tombstone path in
-  let full_filename = Relative_path.to_absolute path in
   let filepath = Relative_path.suffix path in
-  if Sys.file_exists full_filename then
-    let contents = IndexBuilder.parse_one_file ~path in
-    let contents =
+  let contents =
+    let full_filename = Relative_path.to_absolute path in
+    if Sys.file_exists full_filename then
+      let contents = IndexBuilder.parse_one_file ~path in
       if List.length contents = 0 then
         convert_fileinfo_to_contents ~info ~filepath
       else
         contents
-    in
-    {
-      sienv with
-      lss_fullitems =
-        Relative_path.Map.add sienv.lss_fullitems ~key:path ~data:contents;
-      lss_tombstones = Tombstone_set.add sienv.lss_tombstones tombstone;
-    }
-  else (
-    Hh_logger.log
-      "Symbol Index: Attempt to update a file that does not exist [%s]"
-      full_filename;
-    sienv
-  )
+    else
+      convert_fileinfo_to_contents ~info ~filepath
+  in
+  {
+    sienv with
+    lss_fullitems =
+      Relative_path.Map.add sienv.lss_fullitems ~key:path ~data:contents;
+    lss_tombstones = Tombstone_set.add sienv.lss_tombstones tombstone;
+  }
 
 (* Remove files from local when they are deleted *)
 let remove_file ~(sienv : si_env) ~(path : Relative_path.t) : si_env =

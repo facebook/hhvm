e5fdcd60ce2e9efb5e1405cbb68513805c8f3aad
Mack Grzybek <grzybek@fb.com>
2019-09-10 10:54:27 -0700

    Test the output of ini_set for 'memory_limit'
    Summary:
    Per documentation, `set_ini` should return old value of setting or FALSE if failure.
    Let's ensure we test the behavior for `memory_limit` key.
    
    Reviewed By: edwinsmith
    
    Differential Revision: D17159016
    
    fbshipit-source-id: 8ee08628166714d45a7b09900db257148d5a7d0d

--- hphp/test/quick/ini_get_set_memory_limit.php
+++ hphp/test/quick/ini_get_set_memory_limit.php
@@ -2,15 +2,15 @@
 <<__EntryPoint>> function main(): void {
 var_dump( ini_get('memory_limit'));
 
-ini_set('memory_limit', '128M');
+var_dump(ini_set('memory_limit', '128M'));
 var_dump( ini_get('memory_limit'));
 
-ini_set('memory_limit', '128G');
+var_dump(ini_set('memory_limit', '128G'));
 var_dump( ini_get('memory_limit'));
 
-ini_set('memory_limit', '17000K');
+var_dump(ini_set('memory_limit', '17000K'));
 var_dump( ini_get('memory_limit'));
 
-ini_set('memory_limit', '136314880');
+var_dump(ini_set('memory_limit', '136314880'));
 var_dump( ini_get('memory_limit'));
 }

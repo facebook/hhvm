14f8c975f0bb1dc2cb538a7a3206c036e1107fd3
Ted Spence <tspence@fb.com>
2019-09-16 14:14:22 -0700

    Make test spot check for specific items
    Summary:
    As discovered by Catherine Gasnier the test for hh_global_index_builder is brittle.  It checks SHA1SUM as well as word count/line count/etc.
    
    I had originally endorsed this test but I had underestimated how often those numbers would change.
    
    Let's convert this test to verify by using a "No smaller than X lines" metric, as well as a spot-check system to assume that the software is able to find some results from both the repo in question as well as the HHI builtins.
    
    This should be less brittle.
    
    Reviewed By: jewelpit
    
    Differential Revision: D17400301
    
    fbshipit-source-id: 064f567a492a8b43f69e7a6c7953d431522e24d8

--- hphp/hack/test/integration/test_global_index_builder.sh
+++ hphp/hack/test/integration/test_global_index_builder.sh
@@ -7,26 +7,52 @@ NAME=autocomplete
 TEMP_REPO="$(mktemp -d)"
 TEMP_FILE="${TEMP_REPO}/${NAME}.txt"
 touch "${TEMP_FILE}"
+echo "CHECKING ${TEMP_REPO} ${TEMP_FILE}"
 
+# Build a global index in text
 if ! "${CMD}" --text "${TEMP_FILE}" "${REPO}"; then
   echo "FAILURE: ${CMD} failed to run"
   exit 1
 fi
 
-TEMP_WC="${TEMP_REPO}/${NAME}.wc.txt"
-wc "${TEMP_FILE}" | cut -d " " --fields=3,5,6 > "${TEMP_WC}"
-if ! diff "${TEMP_WC}" "${REPO}/${NAME}.wc.txt"; then
-  echo "FAILURE: different line, word, or byte counts:"
-  cat "${TEMP_WC}"
+# Ensure that we have at least 5,300 lines - new HHIs may be added over time so
+# don't throw a failure if the line count increases
+LINE_COUNT=$(wc "${TEMP_FILE}" | cut -d " " --fields=3)
+if [ "${LINE_COUNT}" -gt 5300 ]; then
+  echo "Line count is good"
+else
+  echo "FAILURE: Line count should be greater than 5300, since"
+  echo "         HHI builtins are included in this task."
+  echo "         If the file has shrunk in size, the global"
+  echo "         index builder is probably not catching HHIs."
   exit 1
 fi
 
-TEMP_SHA1SUM="${TEMP_REPO}/${NAME}.sha1sum.txt"
-sha1sum "${TEMP_FILE}" | cut -d " " -f1 > "${TEMP_SHA1SUM}"
-if ! diff "${TEMP_SHA1SUM}" "${REPO}/${NAME}.sha1sum.txt"; then
-  echo "FAILURE: different SHA-1 checksum:"
-  cat "${TEMP_SHA1SUM}"
-  exit 1
-fi
 
+function spot_check() {
+  expected=$1
+  matches=0
+  matches=$(grep "${expected}" "${TEMP_REPO}/autocomplete.txt" -c)
+  if [ "${matches}" -ne 1 ]; then
+    echo "FAILURE: Expected to find the symbol:"
+    echo "         [${expected}]"
+    echo "         Please check to see if this symbol still exists in"
+    echo "         //hphp/hack/test/integration/data/simple_repo"
+    echo "         If it does not, please comment out this test.  If"
+    echo "         the symbol still exists, then hh_global_index_builder"
+    echo "         may be failing."
+    exit 1
+  else
+    echo "Searching for [${expected}] found [${matches}] matches"
+  fi
+}
+
+
+# Run some spot tests to ensure certain things made it into the file
+spot_check "Aaaaaaaaaaa_class 1 acid  acnew  actype"
+spot_check "Derp\\\\Lib\\\\Herp\\\\f"
+spot_check "HH\\\\vec"
+spot_check "__CLASS__ 9 acid"
+
+# Clean up
 rm -rf "${TEMP_REPO}"

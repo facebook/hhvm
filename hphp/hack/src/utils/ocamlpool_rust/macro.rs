9b55d1c032c9d65150370d2264b0fdb9432504c8
Steve Cao <shiqicao@fb.com>
2019-10-04 18:42:26 -0700

    Ocamlvalue macro supports `Box<(...)>`
    Summary:
    To deduce `aast` enum varaiant size, `Expr_::Foo(X, Y, Z)` will be rewritten to `Expr_::Foo(Box<(X, Y, Z)>)`. However, this rewritten variant actually doesn't have the same memory structure assuming `Ocamlvalue` macro is used. `Ocamlvalue` macro will convert `Foo(Box<(X, Y, Z)>)` to `Foo of (x * y)` which is different than `Foo of x * y`.
    
    This diff updated `Ocamlvalue` macro to always treat `Expr::Foo(Box<(X, Y, Z)>)` as `Expr::Foo(X, Y, Z)`. This change assumes there is no `Foo of (x * y)` in Ocaml, otherwise it will be incorrectly mapped to `Foo of x * y`.
    
    There is actually one case, `Stmt::AwaitAll`, and the outer tuple can be removed(D17683906).
    
    ```
    Awaitall of
      (
        (lid option * ('ex, 'fb, 'en, 'hi) expr) list
        *
        ('ex, 'fb, 'en, 'hi) block
      )
    ```
    
    Differential Revision: D17696666
    
    fbshipit-source-id: 9cb7ee54a53b5c1ee1bc966cdc2ccfe7b3d23ebf

--- hphp/hack/src/utils/ocamlpool_rust/macro.rs
+++ hphp/hack/src/utils/ocamlpool_rust/macro.rs
@@ -6,9 +6,29 @@
 //
 
 use quote::quote;
+use syn::{Type, TypePath};
 use synstructure::decl_derive;
 
 decl_derive!([Ocamlvalue] => derive_ocamlvalue);
+
+fn get_boxed_tuple_len(bi: &synstructure::BindingInfo) -> usize {
+    if let Type::Path(TypePath { path, .. }) = &bi.ast().ty {
+        if let Some(path_seg) = path.segments.first() {
+            if path_seg.ident == "Box" {
+                if let syn::PathArguments::AngleBracketed(args) = &path_seg.arguments {
+                    if let Some(syn::GenericArgument::Type(Type::Tuple(syn::TypeTuple {
+                        elems,
+                        ..
+                    }))) = args.args.first()
+                    {
+                        return elems.len();
+                    }
+                }
+            }
+        }
+    }
+    0
+}
 
 fn derive_ocamlvalue(mut s: synstructure::Structure) -> proc_macro2::TokenStream {
     s.add_bounds(synstructure::AddBounds::Generics);
@@ -62,20 +82,38 @@ fn derive_ocamlvalue(mut s: synstructure::Structure) -> proc_macro2::TokenStream
             let mut variant_with_param_tag: u8 = 0;
             let mut variant_with_no_param_tag: u8 = 0;
             for v in s.variants().iter() {
-                let has_param = v.bindings().iter().count() > 0;
+                let binding_len = v.bindings().len();
                 let p = v.pat();
-                if has_param {
-                    let list = v.bindings().iter().fold(quote! {}, |acc, bi| {
-                        quote! {#acc #bi.ocamlvalue() , }
-                    });
-                    pattern_acc = quote! {
-                        #pattern_acc #p => {
-                            ocamlpool_rust::utils::caml_block(
-                                #variant_with_param_tag,
-                                &[ #list ]
-                            )
+                if binding_len > 0 {
+                    let first_boxed_tuple_len = get_boxed_tuple_len(&v.bindings()[0]);
+                    if binding_len == 1 && first_boxed_tuple_len > 0 {
+                        let bi = &v.bindings()[0];
+                        let mut list = quote! {};
+                        for i in 0..first_boxed_tuple_len {
+                            let idx = syn::Index::from(i);
+                            list = quote! {#list #bi.#idx.ocamlvalue() , }
                         }
-                    };
+                        pattern_acc = quote! {
+                            #pattern_acc #p => {
+                                ocamlpool_rust::utils::caml_block(
+                                    #variant_with_param_tag,
+                                    &[ #list ]
+                                )
+                            }
+                        }
+                    } else {
+                        let list = v.bindings().iter().fold(quote! {}, |acc, bi| {
+                            quote! {#acc #bi.ocamlvalue() , }
+                        });
+                        pattern_acc = quote! {
+                            #pattern_acc #p => {
+                                ocamlpool_rust::utils::caml_block(
+                                    #variant_with_param_tag,
+                                    &[ #list ]
+                                )
+                            }
+                        }
+                    }
                     variant_with_param_tag += 1;
                 } else {
                     pattern_acc = quote! {

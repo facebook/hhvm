dac92471b59ff8017d48d3700a5ec776ab88ea90
Leo Osvald <leoo@fb.com>
2019-10-07 14:20:48 -0700

    Port hhas_type_info and hhas_type_constraint
    Summary:
    Most of the type constraint flags is implemented for free
    by using `bitflags` crate; since the create provides a nice
    to-string implementation, I only needed to replace " | " separator
    with " " to satisfy the only caller of OCaml's:
    
      List.map ~f:Hhas_type_constraint.string_of_flag flags
    
    *Note*: I bundled together Constraint & Info because they're very small,
    don't depend on anything else and are mostly used together.
    
    Reviewed By: shiqicao
    
    Differential Revision: D17794082
    
    fbshipit-source-id: e844c86d0ca732e77c570bbc78eb51d922a50ddd

---
+++ hphp/hack/src/hhbc/hhas_type.rs
@@ -0,0 +1,61 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the "hack" directory of this source tree.
+
+extern crate bitflags;
+
+/// Type info has additional optional user type *)
+#[allow(dead_code)]
+pub struct Info {
+    pub user_type: Option<String>,
+    pub type_constraint: constraint::Type,
+}
+
+#[allow(dead_code)]
+pub mod constraint {
+
+    use bitflags::bitflags;
+
+    pub struct Type {
+        pub name: Option<String>,
+        pub flags: Flags,
+    }
+
+    bitflags! {
+        pub struct Flags: u8 {
+            const NULLABLE =         0b0000_0001;
+            const HH_TYPE =          0b0000_0010;
+            const EXTENDED_HINT =    0b0000_0100;
+            const TYPE_VAR =         0b0000_1000;
+            const SOFT =             0b0001_0000;
+            const TYPE_CONSTANT =    0b0010_0000;
+            const DISPLAY_NULLABLE = 0b0100_0000;
+        }
+    }
+
+    /// Implicitly provides a to_string() method consistent with the one
+    /// used in hhbc_hhas: i.e., lowercased flag names separated by space.
+    impl std::fmt::Display for Flags {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let set_names: String = format!("{:?}", self)
+                .split(" | ")
+                .map(|s| s.to_lowercase())
+                .collect::<Vec<_>>()
+                .join(" ");
+            write!(f, "{}", set_names.to_string())
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn test_constraint_flags_to_string_called_by_hhbc_hhas() {
+        use crate::constraint::Flags;
+        let typevar_and_soft = Flags::TYPE_VAR | Flags::SOFT;
+        assert_eq!("type_var soft", typevar_and_soft.to_string());
+        assert_eq!("hh_type", Flags::HH_TYPE.to_string());
+    }
+}

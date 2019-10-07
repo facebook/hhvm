2d8e8c99244968d3e3b3263f87d5bab09814356d
Ted Spence <tspence@fb.com>
2019-10-07 15:53:31 -0700

    Reduce size of autocomplete text responses
    Summary:
    Our autocomplete results contain a lot of text.  Some of that text isn't particularly useful.
    
    One problem is that, if we get autocomplete results via a global symbol table, we don't get the filename / line / column number.  Instead, we simply transmit blank details because they aren't necessary.
    
    However, that information pollutes the JSON and makes it harder to write integration tests that look sensible, because matching against "line = 0" doesn't seem appropriate.
    
    This diff eliminates unnecessary data in the JSON to make our tests easier to validate.
    
    Reviewed By: arxanas
    
    Differential Revision: D17767672
    
    fbshipit-source-id: 1fde9dd914b434398dd64cc10513533b01c079ab

--- hphp/hack/src/client/clientLsp.ml
+++ hphp/hack/src/client/clientLsp.ml
@@ -1433,22 +1433,31 @@ let make_ide_completion_response
             | Some base_class -> Hh_json.JSON_String base_class
             | None -> Hh_json.JSON_Null
           in
-          Some
-            (Hh_json.JSON_Object
-               [
-                 (* Fullname is needed for namespaces.  We often trim namespaces to make
-                  * the results more readable, such as showing "ad__breaks" instead of
-                  * "Thrift\Packages\cf\ad__breaks".
-                  *)
+          (* If we do not have a correct file position, skip sending that data *)
+          if Int.equal line 0 && Int.equal start 0 then
+            Some
+              (Hh_json.JSON_Object
+                 [
                    ("fullname", Hh_json.JSON_String completion.res_fullname);
-                 (* Filename/line/char/base_class are used to handle class methods.
-                  * We could unify this with fullname in the future.
-                  *)
-                   ("filename", Hh_json.JSON_String filename);
-                 ("line", Hh_json.int_ line);
-                 ("char", Hh_json.int_ start);
-                 ("base_class", base_class);
-               ])
+                   ("base_class", base_class);
+                 ])
+          else
+            Some
+              (Hh_json.JSON_Object
+                 [
+                   (* Fullname is needed for namespaces.  We often trim namespaces to make
+                    * the results more readable, such as showing "ad__breaks" instead of
+                    * "Thrift\Packages\cf\ad__breaks".
+                    *)
+                     ("fullname", Hh_json.JSON_String completion.res_fullname);
+                   (* Filename/line/char/base_class are used to handle class methods.
+                    * We could unify this with fullname in the future.
+                    *)
+                     ("filename", Hh_json.JSON_String filename);
+                   ("line", Hh_json.int_ line);
+                   ("char", Hh_json.int_ start);
+                   ("base_class", base_class);
+                 ])
         in
         {
           label =

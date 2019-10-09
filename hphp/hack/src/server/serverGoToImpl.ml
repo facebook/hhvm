36de8c0e59c0f4dc8e2e5c11ffc5e271ba7aab07
Yael Goldin <ygoldin@fb.com>
2019-10-09 10:37:15 -0700

    go_to_implementation works in parallel
    Summary: Update `go_to_implementation` functionality to search for the class/trait/interface implementations in parallel. Also add more tests
    
    Reviewed By: arxanas
    
    Differential Revision: D17817965
    
    fbshipit-source-id: ffbaeed0d65a54cb1f8782ffa7869d8de47bea91

--- hphp/hack/src/server/serverGoToImpl.ml
+++ hphp/hack/src/server/serverGoToImpl.ml
@@ -12,30 +12,41 @@ open Reordered_argument_collections
 open ServerCommandTypes.Find_refs
 open ServerCommandTypes.Done_or_retry
 
-let convert_classes_to_positions (child_classes : SSet.t) :
+let find_positions_of_classes
+    (acc : (string * Pos.t) list) (child_classes : string list) :
     (string * Pos.t) list =
-  SSet.elements child_classes
-  |> List.map ~f:(fun child_class ->
-         match Naming_table.Types.get_pos child_class with
-         | None ->
-           failwith ("Could not find definition of child class: " ^ child_class)
-         | Some (FileInfo.Full pos, _type) -> (child_class, pos)
-         | Some (FileInfo.File (FileInfo.Class, path), _type) ->
-           (match Ast_provider.find_class_in_file path child_class with
-           | None ->
-             failwith
-               (Printf.sprintf
-                  "Could not find class %s in %s"
-                  child_class
-                  (Relative_path.to_absolute path))
-           | Some { Aast.c_span; _ } -> (child_class, c_span))
-         | Some
-             FileInfo.(File ((Fun | RecordDef | Typedef | Const), _path), _type)
-           ->
-           failwith
-             (Printf.sprintf
-                "Information for class %s was returned as not a class"
-                child_class))
+  acc
+  @ List.map child_classes ~f:(fun child_class ->
+        match Naming_table.Types.get_pos child_class with
+        | None ->
+          failwith ("Could not find definition of child class: " ^ child_class)
+        | Some (FileInfo.Full pos, _type) -> (child_class, pos)
+        | Some (FileInfo.File (FileInfo.Class, path), _type) ->
+          (match Ast_provider.find_class_in_file path child_class with
+          | None ->
+            failwith
+              (Printf.sprintf
+                 "Could not find class %s in %s"
+                 child_class
+                 (Relative_path.to_absolute path))
+          | Some { Aast.c_span; _ } -> (child_class, c_span))
+        | Some
+            FileInfo.(File ((Fun | RecordDef | Typedef | Const), _path), _type)
+          ->
+          failwith
+            (Printf.sprintf
+               "Information for class %s was returned as not a class"
+               child_class))
+
+let parallel_find_positions_of_classes
+    (child_classes : string list) (workers : MultiWorker.worker list option) :
+    (string * Pos.t) list =
+  MultiWorker.call
+    workers
+    ~job:find_positions_of_classes
+    ~neutral:[]
+    ~merge:List.append
+    ~next:(MultiWorker.next workers child_classes)
 
 let search_class class_name genv env =
   let class_name = ServerFindRefs.add_ns class_name in
@@ -51,8 +62,12 @@ let search_class class_name genv env =
   in
   let child_classes =
     FindRefsService.find_child_classes class_name env.naming_table files
+    |> SSet.elements
   in
-  convert_classes_to_positions child_classes
+  if List.length child_classes < 10 then
+    find_positions_of_classes [] child_classes
+  else
+    parallel_find_positions_of_classes child_classes genv.workers
 
 let go action genv env =
   match action with

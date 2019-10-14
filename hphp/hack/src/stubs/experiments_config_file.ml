5e9945588273d6b23b344b095c4e6f3ec271018a
Tatiana Racheva <tatianaracheva@fb.com>
2019-10-14 12:21:28 -0700

    Experiment modules
    Summary: A simple module to get experimental settings.
    
    Differential Revision: D17652563
    
    fbshipit-source-id: 441f263bbefc658b3e4ef1ceb2b93ea0ce4df256

---
+++ hphp/hack/src/stubs/experiments_config_file.ml
@@ -0,0 +1,11 @@
+(*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the "hack" directory of this source tree.
+ *
+ *)
+
+let update ~(file : string) : (string, string) result =
+  ignore file;
+  Ok "Experiments config update: nothing to do"

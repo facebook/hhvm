af227285c07ef274fe81341e48a5407bc3635d05
Leo Osvald <leoo@fb.com>
2019-10-18 15:51:26 -0700

    Use Dependency Injection for global emit_* state
    Summary:
    OCaml conveniently avoids *circularly* depending on higher-level modules
    (`emit_*`) by having each `emit_*` use **its own** global mutable state.
    
    However, porting this directly to Rust would require mutexes or `unsafe`,
    which impacts performance and/or safety and it is an anti-pattern.
    
    Achieve a similar behavior safely via dynamic dispatch through `Any`,
    and let each stateful `emit_*` crate attach its own hidden state
    to the emitter by implementing a one-off trait via a convenience macro
    (this is required due to the orphan rule for traits in Rust):
    
        struct FooState { ... }  // this is crate-private
        env::lazy_emit_state!(foo_state, FooState, FooState::init);
    
    This essentially provides a dependency injection mechanism,
    since emitter env does not need to know nor initialize any states.
    Instead, each stateful crate that already depends on emitter/env
    and has access to `e: &mut Emitter` will simply do:
    
        e.emit_state_mut()
    
    *Note*: I made a design decision to panic if `DynState` slot is overtaken
    (i.e., two crates using the same `foo_state` field,  as this is more fool proof
    than silently stomping over a state from the other crate by re-initializing.
    
    Reviewed By: shiqicao
    
    Differential Revision: D17974004
    
    fbshipit-source-id: aa59c5487343cf818e76c16ba6aaafd6d5288f97

---
+++ hphp/hack/src/hhbc/emit_adata.rs
@@ -0,0 +1,34 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the "hack" directory of this source tree.
+
+use env::emitter::Emitter;
+
+struct State {
+    // TODO(hrust)
+}
+
+impl State {
+    fn init() -> Box<dyn std::any::Any> {
+        Box::new(State {})
+    }
+}
+env::lazy_emit_state!(adata_state, State, State::init);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // verify it compiles (no test attribute)
+    #[allow(dead_code)]
+    fn ref_state_from_emiter(e: &Emitter) {
+        let _: &State = e.emit_state();
+    }
+
+    // verify it compiles (no test attribute)
+    #[allow(dead_code)]
+    fn mut_state_from_emiter(e: &mut Emitter) {
+        let _: &mut State = e.emit_state_mut();
+    }
+}
--- hphp/hack/src/hhbc/emitter.rs
+++ hphp/hack/src/hhbc/emitter.rs
@@ -21,6 +21,16 @@ pub struct Emitter {
     label_gen: label::Gen,
     local_gen: local::Gen,
     iterator: Iter,
+
+    // dynamic states are exposed because another crate
+    // needs to inject the lazy set/get into these slots,
+    // accessed via `emit_state()` on Emitter that
+    // auto-implemented via the `lazy_emit_state!` macro
+    // (each crate such as emit_XYZ only access one of these)
+    pub adata_state: DynState,
+    pub expression_state: DynState,
+    pub statement_state: DynState,
+    pub symbol_refs_state: DynState,
 }
 
 impl Emitter {
@@ -73,5 +83,62 @@ impl Context for Emitter {
     }
     fn systemlib(&self) -> bool {
         self.systemlib
+    }
+}
+
+use std::any::Any;
+
+/// Injects stateful type to emitter without adding dependency to another crate
+/// Example:
+/// ```
+/// struct State {}
+/// impl State {
+///     fn init() -> Box<dyn std::any::Any> {
+///         Box::new(State {})
+///     }
+/// }
+/// env::lazy_emit_state!(adata_state, State, State::init);
+/// ```
+/// now the crate can call `emit_state_mut` (or `emit_state`),
+/// which converts to the create-private type `&mut State` (or `State`).
+#[macro_export]
+macro_rules! lazy_emit_state {
+    ($field: ident, $type: ty, $init: expr) => {
+        // Note: if multiple decls or name clashes, do one of:
+        // - add explicit name(s) as macro parameter(s)
+        // - use crate paste/mashup to create unique trait/method names
+        trait LazyState<T> {
+            fn emit_state(&self) -> &T;
+            fn emit_state_mut(&mut self) -> &mut T;
+        }
+        impl LazyState<$type> for Emitter {
+            fn emit_state(&self) -> &$type {
+                self.$field
+                    .as_ref()
+                    .expect(concat!("uninit'd ", module_path!(), " state"))
+                    .downcast_ref::<$type>()
+                    .expect(concat!("expected ", module_path!(), " state"))
+            }
+
+            fn emit_state_mut(&mut self) -> &mut $type {
+                self.$field
+                    .get_or_init($init)
+                    .downcast_mut::<$type>()
+                    .expect(concat!("expected ", module_path!(), " state"))
+            }
+        }
+    };
+}
+
+/// The plumbing to make `lazy_emit_state!` macro work,
+/// which gives user-friendly access to mutable crate-provided type
+#[derive(Debug, Default)]
+pub struct DynState(Option<Box<dyn Any>>);
+impl DynState {
+    pub fn get_or_init(&mut self, init: fn() -> Box<dyn Any>) -> &mut Box<dyn Any> {
+        self.0.get_or_insert_with(init)
+    }
+    pub fn as_ref(&self) -> Option<&Box<dyn Any>> {
+        self.0.as_ref()
     }
 }

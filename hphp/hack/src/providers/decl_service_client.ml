fe2c3e56904cf0f0728a0374d838ac449d8fbf7f
Lucian Wischik <ljw@fb.com>
2019-10-18 03:01:21 -0700

    init decl-service connection
    Summary:
    The previous diff made each worker invoke a not-yet-implemented "decl_service.init" function. This diff implements it.
    
    Its job is for the worker to set up a communication pipe to the decl service.
    
    Reviewed By: 2BitSalute
    
    Differential Revision: D17969606
    
    fbshipit-source-id: 67da3945778fc803c6c6108b837e78de113a146b

--- hphp/hack/src/providers/decl_service_client.ml
+++ hphp/hack/src/providers/decl_service_client.ml
@@ -10,13 +10,23 @@ type sharedmem_base_address
 
 type t = { rpc_get_gconst: string -> (string, Marshal_tools.error) result }
 
-let _rpc_get_gconst
+let rpc_get_gconst
     (_fd : Unix.file_descr) (_base : sharedmem_base_address) (name : string) :
     (string, Marshal_tools.error) result =
   Error (Marshal_tools.Rpc_malformed ("not implemented", Utils.Callstack name))
 
-let init (decl_sock_file : string) (_base : sharedmem_base_address) :
+let init (decl_sock_file : string) (base : sharedmem_base_address) :
     (t, Marshal_tools.error) result =
-  Error
-    (Marshal_tools.Rpc_malformed
-       ("not implemented", Utils.Callstack decl_sock_file))
+  let sockaddr = Unix.ADDR_UNIX decl_sock_file in
+  let connect_res =
+    try Ok (Timeout.open_connection sockaddr) with
+    | Unix.Unix_error (Unix.ECONNREFUSED, _, _) as e ->
+      Error (Marshal_tools.Rpc_absent (Exception.wrap e))
+    | e -> Error (Marshal_tools.Rpc_disconnected (Exception.wrap e))
+  in
+  match connect_res with
+  | Error error -> Error error
+  | Ok (ic, _oc) ->
+    (* ic and oc have the same underlying Unix.file_descr *)
+    let fd = Timeout.descr_of_in_channel ic in
+    Ok { rpc_get_gconst = rpc_get_gconst fd base }

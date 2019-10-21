3a9ff9b62c7431b7f0309ac8fab4d89a1141fda0
Ted Spence <tspence@fb.com>
2019-10-21 12:10:51 -0700

    Back out "[hack] auto autocomplete should add open and close parens for function calls"
    Summary:
    The diff that added parens correctly in tests doesn't work quite right when executing directly in a language server protocol environment.
    
    This will need more consideration before we ship a solution for adding parens correctly.
    
    Reviewed By: jewelpit
    
    Differential Revision: D18041977
    
    fbshipit-source-id: 6db62ed0bba6ff132b2187f07f5d9ef5018114b3

--- hphp/hack/src/server/autocompleteService.ml
+++ hphp/hack/src/server/autocompleteService.ml
@@ -306,22 +306,16 @@ let resolve_ty
   (*   <class1 :attr="a"  -- do strip it                                      *)
   (* The logic is thorny here because we're relying upon regexes to figure    *)
   (* out the context. Once we switch autocomplete to FFP, it'll be cleaner.   *)
-  let (name, fullname) =
+  let name =
     match (x.kind_, autocomplete_context) with
     | ( SearchUtils.SI_Property,
         { AutocompleteTypes.is_instance_member = false; _ } ) ->
-      let n = lstrip x.name ":" in
-      (n, n)
+      lstrip x.name ":"
     | (SearchUtils.SI_XHP, { AutocompleteTypes.is_xhp_classname = true; _ })
     | (SearchUtils.SI_Class, { AutocompleteTypes.is_xhp_classname = true; _ })
       ->
-      let n = lstrip x.name ":" in
-      (n, n)
-    | (SearchUtils.SI_Function, _)
-    | (SearchUtils.SI_ClassMethod, _)
-    | (SearchUtils.SI_Constructor, _) ->
-      (x.name ^ "()", x.name)
-    | _ -> (x.name, x.name)
+      lstrip x.name ":"
+    | _ -> x.name
   in
   let pos =
     match x.ty with
@@ -334,7 +328,7 @@ let resolve_ty
     res_base_class = x.base_class;
     res_ty = get_desc_string_for env x.ty x.kind_;
     res_name = name;
-    res_fullname = fullname;
+    res_fullname = name;
     res_kind = x.kind_;
     func_details = get_func_details_for env x.ty;
     ranking_details = None;

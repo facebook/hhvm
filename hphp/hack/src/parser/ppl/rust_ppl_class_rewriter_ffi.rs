c2b7733aca8be84ab25b6f99d8d75dfacdcd6013
Jake Bailey (Hacklang) <jakebailey@fb.com>
2019-10-25 09:06:52 -0700

    Use ocamlrep in PPL class rewriter ffi
    Summary:
    Now that ocamlrep can allocate via ocamlpool, replacing the Ocamlvalue trait with OcamlRep will allow us to use types which implement OcamlRep in Rust binaries. This was difficult before because implementations of Ocamlvalue linked against the OCaml runtime.
    
    This diff uses ocamlrep in the PPL class rewriter FFI.
    
    I did not use the `ocaml_ffi_no_panic!` macro here (`ocamlrep_ocamlpool`'s equivalent to the `caml!` macro) because the result is converted to OCaml via `ToOcaml` rather than `OcamlRep`. We use `OcamlRep` here only for the conversion of the source text argument.
    
    Reviewed By: jewelpit
    
    Differential Revision: D18066126
    
    fbshipit-source-id: b721687dea627e34b95328a3967a7ed127dcbe58

--- hphp/hack/src/parser/ppl/rust_ppl_class_rewriter_ffi.rs
+++ hphp/hack/src/parser/ppl/rust_ppl_class_rewriter_ffi.rs
@@ -5,9 +5,7 @@
 
 use std::rc::Rc;
 
-use ocaml::caml;
-use ocamlpool_rust::utils::{block_field, str_field};
-use oxidized::relative_path::RelativePath;
+use ocamlrep::OcamlRep;
 use parser_rust::{
     parser::Parser, parser_env::ParserEnv, smart_constructors::NoState,
     smart_constructors::WithKind, source_text::SourceText,
@@ -24,33 +22,31 @@ extern "C" {
     fn ocamlpool_leave();
 }
 
-caml!(parse_and_rewrite_ppl_classes, |ocaml_source_text|, <res>, {
-
-    let ocaml_source_text_value = ocaml_source_text.0;
-
-    let relative_path_raw = block_field(&ocaml_source_text, 0);
-    let relative_path = RelativePath::from_ocamlvalue(&relative_path_raw);
-    let content = str_field(&ocaml_source_text, 2);
-    let source_text = SourceText::make_with_raw(&relative_path, &content.data(), ocaml_source_text_value);
+#[no_mangle]
+pub extern "C" fn parse_and_rewrite_ppl_classes(ocaml_source_text: usize) -> usize {
+    let source_text = unsafe { SourceText::from_ocaml(ocaml_source_text).unwrap() };
 
     let env = ParserEnv {
-        is_experimental_mode : false,
-        hhvm_compat_mode : false,
-        php5_compat_mode : false,
-        codegen : false,
-        allow_new_attribute_syntax : false,
+        is_experimental_mode: false,
+        hhvm_compat_mode: false,
+        php5_compat_mode: false,
+        codegen: false,
+        allow_new_attribute_syntax: false,
     };
 
     let mut parser = PositionedSyntaxParser::make(&source_text, env);
     let root = parser.parse_script(None);
 
-    let context = SerializationContext::new(ocaml_source_text_value);
+    let context = SerializationContext::new(ocaml_source_text);
 
-    let mut editable_root = EditablePositionedSyntax::from_positioned_syntax(&root, &Rc::new(source_text));
+    let mut editable_root =
+        EditablePositionedSyntax::from_positioned_syntax(&root, &Rc::new(source_text));
     PplClassRewriter::rewrite_ppl_classes(&mut editable_root);
 
-    ocamlpool_enter();
-    let ocaml_root = editable_root.to_ocaml(&context);
-    ocamlpool_leave();
-    res = ocaml::Value::new(ocaml_root);
-} -> res);
+    unsafe {
+        ocamlpool_enter();
+        let ocaml_root = editable_root.to_ocaml(&context);
+        ocamlpool_leave();
+        ocaml_root
+    }
+}

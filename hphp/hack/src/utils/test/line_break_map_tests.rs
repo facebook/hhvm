c5717fd286a527e5d13dd78b8453f6a8996d7200
Jake Bailey (Hacklang) <jakebailey@fb.com>
2019-10-26 17:20:58 -0700

    Use ocamlrep in line break map tests
    Summary:
    We currently have four methods of converting between Rust and OCaml values (that I know of):
    
    - the `ocaml` crate, which provides a few limited wrappers around OCaml runtime allocation functions. Since it calls into the OCaml runtime, the garbage collector may run, and we must be careful to keep values rooted while using it.
    - `ToOcaml`, which was implemented for passing the CST from the Rust parser to OCaml. It passes around a SerializationContext containing a pointer to the OCaml source text, since we chose not to write those pointers into positioned tokens and trivia on the Rust side, but those fields still exist on the OCaml side. It is backed by `ocamlpool`, a library which allows us to allocate values on the OCaml heap without running the garbage collector (so that we need not root our values).
    - `OcamlRep`, which was originally conceived as a means of allocating OCaml values into Rust-managed memory (coupled with generation of implementations using a procedural macro). It is now (after {D17864960}) a generic interface for allocating OCaml values and deserializing OCaml values into Rust values.
    - `Ocamlvalue`, which used a similar approach to OcamlRep (i.e., a trait and a procedural macro) to allocate values using `ocamlpool`.
    
    Now that `ocamlrep` can allocate values using `ocamlpool` (after {D17864957}), I think it would be nice to bring this number from four down to three. Replacing the Ocamlvalue trait with OcamlRep will also allow us to use types which implement OcamlRep in Rust binaries. This was difficult before because implementations of Ocamlvalue linked against the OCaml runtime (via `ocamlpool`).
    
    This diff replaces Ocamlvalue with Ocamlrep in the line break map tests.
    
    Reviewed By: shiqicao
    
    Differential Revision: D18076618
    
    fbshipit-source-id: c782efb2474590d139ef5fcc51aa0666fca45dec

--- hphp/hack/src/utils/test/line_break_map_tests.rs
+++ hphp/hack/src/utils/test/line_break_map_tests.rs
@@ -4,79 +4,41 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
 
-extern "C" {
-    fn ocamlpool_enter();
-    fn ocamlpool_leave();
-}
-
 use line_break_map::LineBreakMap;
-use ocaml::caml;
-use ocamlpool_rust::ocamlvalue::*;
-use ocamlpool_rust::utils::*;
+use ocamlrep_ocamlpool::ocaml_ffi;
 
-caml!(offset_to_file_pos_triple, |ocaml_string, ocaml_offset|, <r>, {
-    ocamlpool_enter();
-    let os = ocaml::Str::from(ocaml_string);
-    let string = os.data();
-    let offset = ocaml_to_isize(ocaml_offset.0);
-    let line_break_map = LineBreakMap::new(string);
-    r = ocaml::Value::new(
-        line_break_map.offset_to_file_pos_triple_original(offset).ocamlvalue()
-    );
-    ocamlpool_leave();
-} -> r);
+ocaml_ffi! {
+    fn offset_to_file_pos_triple(string: String, offset: isize) -> (isize, isize, isize) {
+        let line_break_map = LineBreakMap::new(string.as_bytes());
+        line_break_map.offset_to_file_pos_triple_original(offset)
+    }
 
-caml!(offset_to_file_pos_triple_with_cursor, |ocaml_string, ocaml_cursor_offset, ocaml_offset|, <r>, {
-    ocamlpool_enter();
-    let os = ocaml::Str::from(ocaml_string);
-    let string = os.data();
-    let offset = ocaml_to_isize(ocaml_offset.0);
-    let cursor_offset = ocaml_to_isize(ocaml_cursor_offset.0);
-    let line_break_map = LineBreakMap::new(string);
-    line_break_map.offset_to_file_pos_triple_original(cursor_offset);
-    r = ocaml::Value::new(
-        line_break_map.offset_to_file_pos_triple_original(offset).ocamlvalue()
-    );
-    ocamlpool_leave();
-} -> r);
+    fn offset_to_file_pos_triple_with_cursor(
+        string: String,
+        cursor_offset: isize,
+        offset: isize,
+    ) -> (isize, isize, isize) {
+        let line_break_map = LineBreakMap::new(string.as_bytes());
+        line_break_map.offset_to_file_pos_triple_original(cursor_offset);
+        line_break_map.offset_to_file_pos_triple_original(offset)
+    }
 
-caml!(offset_to_line_start_offset, |ocaml_string, ocaml_offset|, <r>, {
-    ocamlpool_enter();
-    let os = ocaml::Str::from(ocaml_string);
-    let string = os.data();
-    let offset = ocaml_to_isize(ocaml_offset.0);
-    let line_break_map = LineBreakMap::new(string);
-    r = ocaml::Value::new(
-        line_break_map.offset_to_line_start_offset(offset).ocamlvalue()
-    );
-    ocamlpool_leave();
-} -> r);
+    fn offset_to_line_start_offset(string: String, offset: isize) -> isize {
+        let line_break_map = LineBreakMap::new(string.as_bytes());
+        line_break_map.offset_to_line_start_offset(offset)
+    }
 
-caml!(offset_to_position, |ocaml_string, ocaml_offset|, <r>, {
-    ocamlpool_enter();
-    let os = ocaml::Str::from(ocaml_string);
-    let string = os.data();
-    let offset = ocaml_to_isize(ocaml_offset.0);
-    let line_break_map = LineBreakMap::new(string);
-    r = ocaml::Value::new(
-        line_break_map.offset_to_position(offset).ocamlvalue()
-    );
-    ocamlpool_leave();
-} -> r);
+    fn offset_to_position(string: String, offset: isize) -> (isize, isize) {
+        let line_break_map = LineBreakMap::new(string.as_bytes());
+        line_break_map.offset_to_position(offset)
+    }
 
-caml!(position_to_offset, |ocaml_string, ocaml_existing, ocaml_i, ocaml_j|, <r>, {
-    ocamlpool_enter();
-    let os = ocaml::Str::from(ocaml_string);
-    let string = os.data();
-    let existing = ocaml::Value::from(ocaml_existing).0 == ocaml::value::TRUE.0;
-    let i = ocaml_to_isize(ocaml_i.0);
-    let j = ocaml_to_isize(ocaml_j.0);
-    let line_break_map = LineBreakMap::new(string);
-    let result = line_break_map.position_to_offset(existing, i, j);
-    let pair = match result {
-        Ok(p) => (p, true),
-        Err(_) => (0, false),
-    };
-    r = ocaml::Value::new(pair.ocamlvalue());
-    ocamlpool_leave();
-} -> r);
+    fn position_to_offset(string: String, existing: bool, i: isize, j: isize) -> (isize, bool) {
+        let line_break_map = LineBreakMap::new(string.as_bytes());
+        let result = line_break_map.position_to_offset(existing, i, j);
+        match result {
+            Ok(p) => (p, true),
+            Err(_) => (0, false),
+        }
+    }
+}

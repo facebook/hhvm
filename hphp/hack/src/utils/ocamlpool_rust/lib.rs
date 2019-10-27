cf3eff027f41718c12011fa39a0203d9559da81c
Jake Bailey (Hacklang) <jakebailey@fb.com>
2019-10-26 17:20:58 -0700

    Remove Ocamlvalue
    Summary:
    We currently have four methods of converting between Rust and OCaml values (that I know of):
    
    - the `ocaml` crate, which provides a few limited wrappers around OCaml runtime allocation functions. Since it calls into the OCaml runtime, the garbage collector may run, and we must be careful to keep values rooted while using it.
    - `ToOcaml`, which was implemented for passing the CST from the Rust parser to OCaml. It passes around a SerializationContext containing a pointer to the OCaml source text, since we chose not to write those pointers into positioned tokens and trivia on the Rust side, but those fields still exist on the OCaml side. It is backed by `ocamlpool`, a library which allows us to allocate values on the OCaml heap without running the garbage collector (so that we need not root our values).
    - `OcamlRep`, which was originally conceived as a means of allocating OCaml values into Rust-managed memory (coupled with generation of implementations using a procedural macro). It is now (after {D17864960}) a generic interface for allocating OCaml values and deserializing OCaml values into Rust values.
    - `Ocamlvalue`, which used a similar approach to OcamlRep (i.e., a trait and a procedural macro) to allocate values using `ocamlpool`.
    
    Now that `ocamlrep` can allocate values using `ocamlpool` (after {D17864957}), I think it would be nice to bring this number from four down to three. Replacing the Ocamlvalue trait with OcamlRep will also allow us to use types which implement OcamlRep in Rust binaries. This was difficult before because implementations of Ocamlvalue linked against the OCaml runtime (via `ocamlpool`).
    
    This stack has removed all uses of the Ocamlvalue trait, so this diff removes it.
    
    Reviewed By: losvald
    
    Differential Revision: D18076619
    
    fbshipit-source-id: ee3241a6b3b0e63806c23243e03283dc08a9d3a4

--- hphp/hack/src/utils/ocamlpool_rust/lib.rs
+++ hphp/hack/src/utils/ocamlpool_rust/lib.rs
@@ -4,39 +4,4 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
 
-pub mod ocamlvalue;
 pub mod utils;
-
-#[macro_export]
-macro_rules! caml_raise {
-    ($name:ident, |$($param:ident),*|, <$($local:ident),*>, $code:block -> $retval:ident) => {
-        ocaml::caml!($name, |$($param),*|, <caml_raise_ret, $($local),*>, {
-            let result = std::panic::catch_unwind(
-                || {
-                    $code;
-                    return $retval;
-                }
-            );
-            match result {
-                Ok (value) => {
-                    caml_raise_ret = value;
-                },
-                Err (err) => {
-                    ocamlpool_leave();
-                    let msg: &str;
-                    if let Some (str) = err.downcast_ref::<&str>() {
-                        msg = str;
-                    } else if let Some (string) = err.downcast_ref::<String>() {
-                        msg = &string[..];
-                    } else {
-                        msg = "Unknown panic type, only support string type.";
-                    }
-                    ocaml::runtime::raise_with_string(
-                        &ocaml::named_value("rust exception").unwrap(),
-                        msg,
-                    );
-                },
-            };
-        } -> caml_raise_ret);
-    };
-}

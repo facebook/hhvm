567aec4c2d780c2be95129279e5b1564be297522
Fred Emmott <fe@fb.com>
2019-10-29 08:52:14 -0700

    update version.h
    Summary: `hphp/facebook/update_version_header.sh`
    
    fbshipit-source-id: df352bca5a49b1bab8484f3bca0b1eea82e604d5

--- hphp/runtime/version.h
+++ hphp/runtime/version.h
@@ -11,7 +11,7 @@
  */
 #ifndef HHVM_VERSION_OVERRIDE
 # define HHVM_VERSION_MAJOR 4
-# define HHVM_VERSION_MINOR 29
+# define HHVM_VERSION_MINOR 30
 # define HHVM_VERSION_PATCH 0
 # define HHVM_VERSION_SUFFIX "-dev"
 #endif

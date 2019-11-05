76f74f0221d73a8492565a99a70eab8b61820c0e
Tatiana Racheva <tatianaracheva@fb.com>
2019-11-05 12:43:16 -0800

    Call direct decl parser on successful name lookup
    Summary:
    If the decl for a given symbol name is not in the cache, look up the path from the naming table, then parse using the direct decl parser.
    
    The current code assumes that we're working with consts.
    
    Reviewed By: ljw1004
    
    Differential Revision: D18313300
    
    fbshipit-source-id: 87b07377afaea96f453942d950ff21fdb411e06e

--- hphp/hack/src/name_index/sqlite/names.rs
+++ hphp/hack/src/name_index/sqlite/names.rs
@@ -8,7 +8,7 @@ use crate::file_infos::*;
 
 use oxidized::relative_path::RelativePath;
 use rusqlite::{Connection, OpenFlags};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::sync::{Arc, Mutex};
 
 // TODO: file_infos is not used yet
@@ -17,10 +17,12 @@ use std::sync::{Arc, Mutex};
 pub struct Names {
     consts: ConstsTable,
     file_infos: FileInfoTable,
+    path: PathBuf,
 }
 
 impl Names {
     pub fn new(path: &Path) -> Self {
+        // TODO: check if the path exists, and create an in-memory representation if it doesn't
         let connection = Arc::new(Mutex::new(
             Connection::open_with_flags(path, OpenFlags::SQLITE_OPEN_READ_ONLY).unwrap(),
         ));
@@ -28,6 +30,7 @@ impl Names {
         Names {
             consts: ConstsTable::new(connection.clone()),
             file_infos: FileInfoTable::new(connection.clone()),
+            path: path.to_path_buf(),
         }
     }
 

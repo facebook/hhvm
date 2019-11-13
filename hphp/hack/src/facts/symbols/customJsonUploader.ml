86cd0bf888e68abcc177de6d8aab424d74012e82
Yael Goldin <ygoldin@fb.com>
2019-11-13 12:43:41 -0800

    provide the option to print file status
    Summary: Don't print the status of every glean file upload in building the dependency graph
    
    Differential Revision: D18460275
    
    fbshipit-source-id: 0c694b70e849b1d1f76f7dbc77cbef65a671c9ea

--- hphp/hack/src/facts/symbols/customJsonUploader.ml
+++ hphp/hack/src/facts/symbols/customJsonUploader.ml
@@ -8,9 +8,11 @@
  *)
 
 (* Export all files matching this prefix to a custom symbol service.
- * Parallelize the execution if workers are provided. *)
+ * Parallelize the execution if workers are provided.
+ * Print the status of uploading each file by default *)
 let send_to_custom_writer
     ?(_workers : MultiWorker.worker list option = None)
+    ?(_print_file_status = true)
     (_files : string list)
     (_service : string)
     (_repo_name : string)

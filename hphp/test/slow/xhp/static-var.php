ef2fd2ebf4d1f8dac56b51d51291f5873741dfcd
Ján <jjergus@fb.com>
2019-11-14 11:15:17 -0800

    add test for new XHP bug
    Summary:
    This test demonstrates what's probably a new XHP bug. The test succeeds on the previous nightly HHVM build but fails on the latest:
    
    ```
    FAILED: test/slow/xhp/static-var.php
    001+ Fatal error: Class undefined: \:fb:thing in /home/jjergus/work/code/hhvm/hphp/test/slow/xhp/static-var.php on line 11
    001- int(42)
    002- int(45)
    003- int(45)
    004- int(48)
    ```
    
    It must be caused by one of the recent commits: https://github.com/facebook/hhvm/commits/master?after=f88acfe5fc92d4c07b7c2148deec311b5044224c+0
    
    I don't know enough about HackC/HHVM internals to determine which commit caused it, can you help me triage?
    
    Reviewed By: kmeht
    
    Differential Revision: D18455008
    
    fbshipit-source-id: b343fa5f54b2fad1c4d2f24f11487044607c9802

---
+++ hphp/test/slow/xhp/static-var.php
@@ -0,0 +1,23 @@
+<?hh
+
+// references a static variable of an XHP class in various contexts
+
+class :fb:thing {
+  public static int $x = 42;
+}
+
+<<__EntryPoint>>
+function main_class_name() {
+  var_dump(:fb:thing::$x);
+  :fb:thing::$x = :fb:thing::$x + 1;
+  ++:fb:thing::$x;
+  :fb:thing::$x++;
+  var_dump(:fb:thing::$x);
+
+  $classname = :fb:thing::class;
+  var_dump($classname::$x);
+  $classname::$x = $classname::$x + 1;
+  ++$classname::$x;
+  $classname::$x++;
+  var_dump($classname::$x);
+}

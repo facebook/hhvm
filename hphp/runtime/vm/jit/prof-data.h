1c2210d92b46a7812ceddc7b9ad0d6de72ad865e
Guilherme Ottoni <ottoni@fb.com>
2019-12-05 16:28:09 -0800

    Move ProfCounters into a separate file
    Reviewed By: paulbiss
    
    Differential Revision: D18826977
    
    fbshipit-source-id: 67d4a8dcdb66292292097fd897e62ed265cfa6ba

--- hphp/runtime/vm/jit/prof-data.h
+++ hphp/runtime/vm/jit/prof-data.h
@@ -26,6 +26,7 @@
 #include "hphp/runtime/vm/srckey.h"
 #include "hphp/runtime/vm/treadmill.h"
 
+#include "hphp/runtime/vm/jit/prof-counters.h"
 #include "hphp/runtime/vm/jit/region-selection.h"
 #include "hphp/runtime/vm/jit/translator.h"
 #include "hphp/runtime/vm/jit/types.h"
@@ -77,61 +78,6 @@ const ProfData* globalProfData();
 void discardProfData();
 
 ////////////////////////////////////////////////////////////////////////////////
-
-/**
- * A simple class of a growable number of profiling counters with fixed
- * addresses, suitable for being incremented from the TC.
- */
-template<typename T>
-struct ProfCounters {
-  explicit ProfCounters(T initVal)
-    : m_initVal(initVal)
-  {}
-
-  ProfCounters(ProfCounters&&) = default;
-  ProfCounters& operator=(ProfCounters&&) = default;
-
-  ProfCounters(const ProfCounters&) = delete;
-  ProfCounters& operator=(const ProfCounters&) = delete;
-
-  T get(uint32_t id) const {
-    assertx(kCountersPerChunk % 11);
-    return id / kCountersPerChunk >= m_chunks.size()
-      ? m_initVal
-      : m_chunks[id / kCountersPerChunk][(id * 11) % kCountersPerChunk];
-  }
-
-  T* getAddr(uint32_t id) {
-    // allocate a new chunk of counters if necessary
-    while (id >= m_chunks.size() * kCountersPerChunk) {
-      uint32_t size = sizeof(T) * kCountersPerChunk;
-      auto const chunk = new T[size];
-      std::fill_n(chunk, kCountersPerChunk, m_initVal);
-      m_chunks.emplace_back(chunk);
-    }
-    assertx(id / kCountersPerChunk < m_chunks.size());
-    assertx(kCountersPerChunk % 11);
-    return &(m_chunks[id / kCountersPerChunk][(id * 11) % kCountersPerChunk]);
-  }
-
-  T getDefault() const { return m_initVal; }
-
-  void resetAllCounters(T value) {
-    // We need to set m_initVal so that method transCounter() works, and also so
-    // that newly created counters start with `value'.
-    m_initVal = value;
-    // Reset all counters already created.
-    for (auto& chunk : m_chunks) {
-      std::fill_n(chunk.get(), kCountersPerChunk, value);
-    }
-  }
-
-private:
-  static const uint32_t kCountersPerChunk = 2 * 1024 * 1024 / sizeof(T);
-
-  T m_initVal;
-  std::vector<std::unique_ptr<T[]>> m_chunks;
-};
 
 /**
  * A profiling record kept for each translation in JitPGO mode.

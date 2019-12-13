8a3a599557266554861679fe82e2b8d2bbe1b031
Waleed Khan <waleedk@fb.com>
2019-12-12 17:48:11 -0800

    Create `hh_lite`
    Summary:
    This is a helper executable which loads a saved-state, parses any changes since the saved-state, then typechecks the given files and prints their errors.
    
    It's suitable to pipe the output from `hh_fanout` directly into `hh_lite`. It's also quite usable for interactive debugging.
    
    Reviewed By: ljw1004
    
    Differential Revision: D18731972
    
    fbshipit-source-id: b9da091ba0880c187a01c4393e746585144e3b99

--- hphp/hack/src/utils/wwwroot.mli
+++ hphp/hack/src/utils/wwwroot.mli
@@ -7,12 +7,6 @@
  *
  *)
 
-(* This `.mli` file was generated automatically. It may include extra
-definitions that should not actually be exposed to the caller. If you notice
-that this interface file is a poor interface, please take a few minutes to
-clean it up manually, and then delete this comment once the interface is in
-shape. *)
-
 val is_www_directory : ?config:string -> Path.t -> bool
 
 val assert_www_directory : ?config:string -> Path.t -> unit

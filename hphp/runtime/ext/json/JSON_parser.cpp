62e9fc8465453ab2807c5a7293733e7f1f86d0a1
Mark Williams <mwilliams@fb.com>
2020-01-28 16:31:57 -0800

    Fix a buffer-overrun in SimpleParser
    Summary: In the failure case, we might have already consumed the entire string.
    
    Reviewed By: binliu19, ottoni
    
    Differential Revision: D19610775

--- hphp/runtime/ext/json/JSON_parser.cpp
+++ hphp/runtime/ext/json/JSON_parser.cpp
@@ -342,7 +342,7 @@ struct SimpleParser {
                        JSONContainerType container_type, bool is_tsimplejson) {
     SimpleParser parser(inp, length, buf, container_type, is_tsimplejson);
     bool ok = parser.parseValue();
-    parser.skipSpace();
+    if (ok) parser.skipSpace();
     if (!ok || parser.p != inp + length) {
       // Unsupported, malformed, or trailing garbage. Release entire stack.
       tvDecRefRange(buf, parser.top);

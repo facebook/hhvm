292b9952b6825dfddcdfc1d1c7199c8e9b071033
Katie Ots <kjo@fb.com>
2020-02-14 02:58:05 -0800

    Use Thrift decoder in symbol info write test
    Summary:
    Change the symbol info write test to check the JSON produced against a
    thrift decoder, rather than manually adding and checking JSON fields.
    
    Reviewed By: simonmar
    
    Differential Revision: D19791548
    
    fbshipit-source-id: a03d7ff9772913079da5ec3a3d2f2beeec277402

--- hphp/hack/test/integration/test_write_symbol_info.py
+++ hphp/hack/test/integration/test_write_symbol_info.py
@@ -3,12 +3,22 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import json
 import os
-from typing import Any, ClassVar, Dict, List, Optional
+from typing import ClassVar, Dict, List, Optional, Type
 
 import test_case
-import utils
 from common_tests import CommonTestDriver
+from glean.schema.hack.types import (
+    ClassDeclaration,
+    ClassDefinition,
+    DeclarationLocation,
+    FileXRefs,
+    InterfaceDeclaration,
+    InterfaceDefinition,
+    TraitDeclaration,
+    TraitDefinition,
+)
 from hh_paths import hh_server
+from thrift.py3 import Protocol, Struct, deserialize
 
 
 class WriteSymbolInfoTests(test_case.TestCase[CommonTestDriver]):
@@ -44,71 +54,54 @@ max_workers = 2
         if test_driver is not None:
             cls.write_repo = os.path.join(test_driver.repo_dir, "symbol_info_test_dir")
 
-        # hardcoded map of valid keys
-        cls.valid_keys = {
-            "hack.ClassDeclaration.1": ["name"],
-            "hack.ClassDefinition.1": [
-                "declaration",
-                "is_abstract",
-                "is_final",
-                "members",
-            ],
-            "hack.DeclarationLocation.1": ["declaration", "file", "span"],
-            "hack.FileXRefs.1": ["file", "xrefs"],
-            "hack.InterfaceDeclaration.1": ["name"],
-            "hack.InterfaceDefinition.1": ["declaration"],
-            "hack.TraitDeclaration.1": ["name"],
-            "hack.TraitDefinition.1": ["declaration"],
-            "name": ["key"],
-            "declaration": ["container", int],
-            "container": ["class_", "interface_", "trait"],
-            "id": [int],
-            "key": [str],
-        }
-
-    def verify_all_json(self) -> None:
+    def verify_json(self) -> None:
         for filename in os.listdir(self.write_repo):
-            if not filename.endswith(".json"):  # not a json for some reason - ignore
-                continue
+            self.assertTrue(
+                filename.endswith(".json"), "All output files are in JSON format"
+            )
 
             with open(os.path.join(self.write_repo, filename)) as file:
-                json_obj = json.load(file)
-                self.verify_json_array(json_obj)
+                json_predicate_list = json.load(file)
+                for pred_obj in json_predicate_list:
+                    self.assertTrue(
+                        "predicate" in pred_obj and "facts" in pred_obj,
+                        "JSON predicate has correct form",
+                    )
 
-    def verify_json_array(self, json_array: List[utils.Json]) -> None:
-        # starts off as json array
-        all_preds = [
-            "hack.ClassDeclaration.1",
-            "hack.ClassDefinition.1",
-            "hack.DeclarationLocation.1",
-            "hack.FileXRefs.1",
-            "hack.InterfaceDeclaration.1",
-            "hack.InterfaceDefinition.1",
-            "hack.TraitDeclaration.1",
-            "hack.TraitDefinition.1",
-        ]
+                    fact_type = self.predicate_name_to_type(pred_obj["predicate"])
+                    if fact_type is None:
+                        self.fail(
+                            "Could not find matching Thrift definition for {}".format(
+                                pred_obj["predicate"]
+                            )
+                        )
 
-        for json_obj in json_array:
-            self.assertIn("predicate", json_obj, "Object is predicate")
-            self.assertIn(json_obj["predicate"], all_preds, "Predicate is valid")
+                    for fact in pred_obj["facts"]:
+                        try:
+                            deserialize(
+                                fact_type,
+                                json.dumps(fact).encode("UTF-8"),
+                                protocol=Protocol.JSON,
+                            )
+                        except Exception as e:
+                            self.fail(
+                                "Could not deserialize {} fact JSON: {}\nDeserialization error: {}".format(
+                                    fact_type, fact, e
+                                )
+                            )
 
-            valid_keys = self.valid_keys[json_obj["predicate"]]
-            for json_fact in json_obj["facts"]:
-                self.verify_json(json_fact, valid_keys)
-
-    def verify_json(self, json_obj: Dict[str, Any], valid_keys: List[object]) -> None:
-        for key, element in json_obj.items():
-            # if element is not an object, just check that type(element)
-            # in valid_keys[key]
-            if key in self.valid_keys and type(element) in self.valid_keys[key]:
-                continue
-
-            if key == "key":
-                self.verify_json(element, valid_keys)
-            else:
-                self.assertIn(key, valid_keys, "Object key is valid")
-                if key in self.valid_keys:
-                    self.verify_json(element, self.valid_keys[key])
+    def predicate_name_to_type(self, predicate_name: str) -> Optional[Type[Struct]]:
+        predicate_dict = {
+            "hack.FileXRefs.1": FileXRefs,
+            "hack.ClassDefinition.1": ClassDefinition,
+            "hack.TraitDefinition.1": TraitDefinition,
+            "hack.InterfaceDefinition.1": InterfaceDefinition,
+            "hack.DeclarationLocation.1": DeclarationLocation,
+            "hack.ClassDeclaration.1": ClassDeclaration,
+            "hack.TraitDeclaration.1": TraitDeclaration,
+            "hack.InterfaceDeclaration.1": InterfaceDeclaration,
+        }
+        return predicate_dict.get(predicate_name)
 
     def start_hh_server(
         self,
@@ -132,4 +125,4 @@ max_workers = 2
         args: Optional[List[str]] = None
         args = ["--write-symbol-info", self.write_repo]
         self.start_hh_server(args=args)
-        self.verify_all_json()
+        self.verify_json()

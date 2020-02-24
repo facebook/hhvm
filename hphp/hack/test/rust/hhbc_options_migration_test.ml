84a4a85322901a5fa260b95253952d577cc8a97f
Leo Osvald <leoo@fb.com>
2020-02-24 12:48:18 -0800

    HHBC options Rust-OCaml parity tests for overrides
    Summary:
    This adds **end-to-end** coverage for the emitter options FFI by
    setting each option to a non-default value in two ways:
    - via JSON key (`{ ..., "JSON_KEY": {"global_value": VAL} }`)
    - via CLI argument (`-vNAME=VAL`)
    
    and asserting that both Rust and OCaml produce the same result.
    
    Reviewed By: dabek
    
    Differential Revision: D20044858
    
    fbshipit-source-id: c0919656ccfb9c6fff62b11f4c3ee2b3f484f47d

--- hphp/hack/test/rust/hhbc_options_migration_test.ml
+++ hphp/hack/test/rust/hhbc_options_migration_test.ml
@@ -2,13 +2,14 @@ open Core_kernel
 open OUnit2
 
 (* Useful debugging, since no ppx printer for Hhbc_options.t *)
-let _assert_opts_equal caml rust =
+let assert_opts_equal caml rust =
   assert_equal
     Hhbc_options.(constant_folding caml)
     Hhbc_options.(constant_folding rust);
   assert_equal
     Hhbc_options.(optimize_null_checks caml)
     Hhbc_options.(optimize_null_checks rust);
+  (* Note: this is unsettable by either JSON or CLI interface (i.e., const.) *)
   assert_equal
     Hhbc_options.(max_array_elem_size_on_the_stack caml)
     Hhbc_options.(max_array_elem_size_on_the_stack rust);
@@ -137,6 +138,9 @@ let _assert_opts_equal caml rust =
   assert_equal
     Hhbc_options.(disable_xhp_element_mangling caml)
     Hhbc_options.(disable_xhp_element_mangling rust);
+  assert_equal
+    Hhbc_options.(enable_xhp_class_modifier caml)
+    Hhbc_options.(enable_xhp_class_modifier rust);
   ()
 
 let json_override_2bools =
@@ -273,8 +277,336 @@ let test_json_configs_stackable _ =
 let test_no_overrides _ =
   let caml_opts = caml_from_configs ~jsons:[] ~args:[] in
   let rust_opts = Options_ffi.from_configs ~jsons:[] ~args:[] in
-  _assert_opts_equal caml_opts rust_opts;
+  assert_opts_equal caml_opts rust_opts;
   ()
+
+let test_all_overrides_json_only _ =
+  let json =
+    "{
+  \"doc_root\": { \"global_value\": \"some/path\" },
+  \"eval.disassembler_source_mapping\": {
+    \"global_value\": 1
+  },
+  \"hack.compiler.constant_folding\": {
+    \"global_value\": 0
+  },
+  \"hack.compiler.optimize_null_checks\": {
+    \"global_value\": 1
+  },
+  \"hhvm.aliased_namespaces\": {
+    \"global_value\": {
+      \"bar\": \"baz\",
+      \"foo\": \"moo\"
+    }
+  },
+  \"hhvm.array_provenance\": {
+    \"global_value\": true
+  },
+  \"hhvm.dynamic_invoke_functions\": {
+    \"global_value\": [\"f\", \"g\"]
+  },
+  \"hhvm.emit_cls_meth_pointers\": {
+    \"global_value\": false
+  },
+  \"hhvm.emit_func_pointers\": {
+    \"global_value\": false
+  },
+  \"hhvm.emit_generics_ub\": {
+    \"global_value\": true
+  },
+  \"hhvm.emit_inst_meth_pointers\": {
+    \"global_value\": false
+  },
+  \"hhvm.emit_meth_caller_func_pointers\": {
+    \"global_value\": false
+  },
+  \"hhvm.enable_intrinsics_extension\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.abstract_static_props\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.allow_new_attribute_syntax\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.check_int_overflow\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.const_default_func_args\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.const_static_props\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.disable_legacy_attribute_syntax\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.disable_legacy_soft_typehints\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.disable_lval_as_an_expression\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.disable_unset_class_const\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.disable_xhp_element_mangling\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.disallow_func_ptrs_in_constants\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.enable_class_level_where_clauses\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.enable_coroutines\": {
+    \"global_value\": false
+  },
+  \"hhvm.hack.lang.enable_first_class_function_pointers\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.enable_pocket_universes\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.enable_xhp_class_modifier\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.hacksperimental\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.phpism.disallow_execution_operator\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.phpism.disable_nontoplevel_declarations\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.phpism.disable_static_closures\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack.lang.phpism.disable_halt_compiler\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack_arr_compat_notices\": {
+    \"global_value\": true
+  },
+  \"hhvm.hack_arr_dv_arrs\": {
+    \"global_value\": true
+  },
+  \"hhvm.include_roots\": {
+    \"global_value\": {\"key\": \"val\"}
+  },
+  \"hhvm.jit_enable_rename_function\": {
+    \"global_value\": true
+  },
+  \"hhvm.log_extern_compiler_perf\": {
+    \"global_value\": true
+  },
+  \"hhvm.php7.ltr_assign\": {
+    \"global_value\": true
+  },
+  \"hhvm.php7.uvs\": {
+    \"global_value\": true
+  },
+  \"hhvm.rx_is_enabled\": {
+    \"global_value\": true
+  },
+  \"hhvm.server.include_search_paths\": {
+    \"global_value\": [\"path1\", \"path2\"]
+  },
+  \"hhvm.widen_is_array\": {
+    \"global_value\": true
+  }
+}
+"
+  in
+  let caml_opts =
+    caml_from_configs ~jsons:[Some (Hh_json.json_of_string json)] ~args:[]
+  in
+  let rust_opts = Options_ffi.from_configs ~jsons:[json] ~args:[] in
+  assert_opts_equal caml_opts rust_opts
+
+module CliArgOverrides = struct
+  (* Note: in lexicographical order by JSON key, for easier visual match *)
+
+  (* let doc_root = "UNSUPPORTED BY CLI" *)
+
+  let eval'disassembler_source_mapping'alias1 =
+    "-veval.dissasemblersourcemapping=1"
+
+  let eval'disassembler_source_mapping'alias2 =
+    "-vhack.compiler.sourcemapping=1"
+
+  (* Note: only supported by CLI (not as JSON) *)
+  let hack'compiler'relabel = "-vhack.compiler.relabel=false"
+
+  (* Note: lines generated (except string literals) from the above by:
+     grep -o '^  \\"\([^"]*\)\\"' THIS_FILE | sed -e 's:[\\" ]::g' \
+       -e "s:\\.:':g" -e 's/.*/  let & = "-vTODO=TODO"\n/'
+  *)
+  let hack'compiler'constant_folding = "-vhack.compiler.constantfolding=false"
+
+  let hack'compiler'optimize_null_checks =
+    "-vhack.compiler.optimizenullcheck=true"
+
+  (* let hhvm'aliased_namespaces = "UNSUPPORTED BY CLI" *)
+
+  let hhvm'array_provenance = "-vhhvm.array_provenance=true"
+
+  (* let hhvm'dynamic_invoke_functions = "UNSUPPORTED BY CLI" *)
+
+  let hhvm'emit_cls_meth_pointers = "-vhhvm.emit_cls_meth_pointers=0"
+
+  let hhvm'emit_func_pointers = "-vhhvm.emit_func_pointers=0"
+
+  let hhvm'emit_generics_ub = "-vhhvm.emit_generics_ub=2"
+
+  let hhvm'emit_inst_meth_pointers = "-vhhvm.emit_inst_meth_pointers=0"
+
+  let hhvm'emit_meth_caller_func_pointers =
+    "-vhhvm.emit_meth_caller_func_pointers=0"
+
+  let hhvm'enable_intrinsics_extension = "-veval.enableintrinsicsextension=true"
+
+  let hhvm'hack'lang'abstract_static_props =
+    "-vhhvm.lang.abstractstaticprops=true"
+
+  let hhvm'hack'lang'allow_new_attribute_syntax =
+    "-vhhvm.lang.allow_new_attribute_syntax=true"
+
+  let hhvm'hack'lang'check_int_overflow =
+    "-vhhvm.hack.lang.check_int_overflow=2"
+
+  let hhvm'hack'lang'const_default_func_args =
+    "-vhhvm.lang.constdefaultfuncargs=true"
+
+  let hhvm'hack'lang'const_static_props = "-vhvm.lang.conststaticprops=true"
+
+  let hhvm'hack'lang'disable_legacy_attribute_syntax =
+    "-vhhvm.lang.disable_legacy_attribute_syntax=true"
+
+  let hhvm'hack'lang'disable_legacy_soft_typehints =
+    "-vhhvm.lang.disable_legacy_soft_typehints=true"
+
+  let hhvm'hack'lang'disable_lval_as_an_expression =
+    "-vhack.lang.disable_lval_as_an_expression=true"
+
+  let hhvm'hack'lang'disable_unset_class_const =
+    "-vhhvm.lang.disableunsetclassconst=true"
+
+  let hhvm'hack'lang'disable_xhp_element_mangling =
+    "-vhhvm.hack.lang.disable_xhp_element_mangling=true"
+
+  let hhvm'hack'lang'disallow_func_ptrs_in_constants =
+    "-vhhvm.lang.disallow_func_ptrs_in_constants=true"
+
+  let hhvm'hack'lang'enable_class_level_where_clauses =
+    "-vhhvm.lang.enable_class_level_where_clauses=true"
+
+  let hhvm'hack'lang'enable_coroutines = "-vhack.lang.enablecoroutines=false"
+
+  let hhvm'hack'lang'enable_first_class_function_pointers =
+    "-vhhvm.hack.lang.enable_first_class_function_pointers=2"
+
+  let hhvm'hack'lang'enable_pocket_universes =
+    "-vhack.lang.enablepocketuniverses=true"
+
+  let hhvm'hack'lang'enable_xhp_class_modifier =
+    "-vhhvm.hack.lang.enable_xhp_class_modifier=true"
+
+  let hhvm'hack'lang'hacksperimental = "-vhack.lang.hacksperimental=true"
+
+  let hhvm'hack'lang'phpism'disallow_execution_operator =
+    "-vhack.lang.phpism.disallowexecutionoperator=true"
+
+  let hhvm'hack'lang'phpism'disable_nontoplevel_declarations =
+    "-vhack.lang.phpism.disablenontopleveldeclarations=true"
+
+  let hhvm'hack'lang'phpism'disable_static_closures =
+    "-vhack.lang.phpism.disablestaticclosures=true"
+
+  let hhvm'hack'lang'phpism'disable_halt_compiler =
+    "-vhhvm.lang.phpism.disablehaltcompiler=true"
+
+  let hhvm'hack_arr_compat_notices = "-veval.hackarrcompatnotices=true"
+
+  let hhvm'hack_arr_dv_arrs = "-veval.hackarrdvarrs=true"
+
+  (* let hhvm'include_roots = "UNSUPPORTED BY CLI" *)
+
+  let hhvm'jit_enable_rename_function = "-veval.jitenablerenamefunction=true"
+
+  let hhvm'log_extern_compiler_perf = "-veval.logexterncompilerperf=true"
+
+  let hhvm'php7'ltr_assign = "-vhhvm.php7.ltr_assign=true"
+
+  let hhvm'php7'uvs = "-vhhvm.php7.ltr_assign=true"
+
+  let hhvm'rx_is_enabled = "-vhhvm.rx_is_enabled=2"
+
+  (* let hhvm'server'include_search_paths = "UNSUPPORTED BY CLI" *)
+
+  let hhvm'widen_is_array = "-vhhvm.widen_is_array=true"
+end
+
+let test_all_overrides_cli_only _ =
+  let open CliArgOverrides in
+  let args =
+    [
+      eval'disassembler_source_mapping'alias1;
+      eval'disassembler_source_mapping'alias2;
+      hack'compiler'relabel;
+      (* Note: generated from the above by:
+       grep -o '^  \\"\([^"]*\)\\"' THIS_FILE | sed -e 's:[\\" ]::g' \
+         -e "s:\\.:':g" -e 's/.*/  &;/'
+    *)
+      (* doc_root; *)
+      hack'compiler'constant_folding;
+      hack'compiler'optimize_null_checks;
+      (* hhvm'aliased_namespaces; *)
+      hhvm'array_provenance;
+      (* hhvm'dynamic_invoke_functions; *)
+      hhvm'emit_cls_meth_pointers;
+      hhvm'emit_func_pointers;
+      hhvm'emit_generics_ub;
+      hhvm'emit_inst_meth_pointers;
+      hhvm'emit_meth_caller_func_pointers;
+      hhvm'enable_intrinsics_extension;
+      hhvm'hack'lang'abstract_static_props;
+      hhvm'hack'lang'allow_new_attribute_syntax;
+      hhvm'hack'lang'check_int_overflow;
+      hhvm'hack'lang'const_default_func_args;
+      hhvm'hack'lang'const_static_props;
+      hhvm'hack'lang'disable_legacy_attribute_syntax;
+      hhvm'hack'lang'disable_legacy_soft_typehints;
+      hhvm'hack'lang'disable_lval_as_an_expression;
+      hhvm'hack'lang'disable_unset_class_const;
+      hhvm'hack'lang'disable_xhp_element_mangling;
+      hhvm'hack'lang'disallow_func_ptrs_in_constants;
+      hhvm'hack'lang'enable_class_level_where_clauses;
+      hhvm'hack'lang'enable_coroutines;
+      hhvm'hack'lang'enable_first_class_function_pointers;
+      hhvm'hack'lang'enable_pocket_universes;
+      hhvm'hack'lang'enable_xhp_class_modifier;
+      hhvm'hack'lang'hacksperimental;
+      hhvm'hack'lang'phpism'disallow_execution_operator;
+      hhvm'hack'lang'phpism'disable_nontoplevel_declarations;
+      hhvm'hack'lang'phpism'disable_static_closures;
+      hhvm'hack'lang'phpism'disable_halt_compiler;
+      hhvm'hack_arr_compat_notices;
+      hhvm'hack_arr_dv_arrs;
+      (* hhvm'include_roots; *)
+      hhvm'jit_enable_rename_function;
+      hhvm'log_extern_compiler_perf;
+      hhvm'php7'ltr_assign;
+      hhvm'php7'uvs;
+      hhvm'rx_is_enabled;
+      (* hhvm'server'include_search_paths; *)
+      hhvm'widen_is_array;
+    ]
+  in
+  let caml_opts = caml_from_configs ~jsons:[] ~args in
+  let rust_opts = Options_ffi.from_configs ~jsons:[] ~args in
+  assert_opts_equal caml_opts rust_opts
 
 let () =
   "hhbc_options_migration"
@@ -290,5 +622,7 @@ let () =
          >:: test_aliased_namespaces_empty_configs_to_json_ffi;
          "test_json_configs_stackable" >:: test_json_configs_stackable;
          "test_no_overrides" >:: test_no_overrides;
+         "test_all_overrides_json_only" >:: test_all_overrides_json_only;
+         "test_all_overrides_cli_only" >:: test_all_overrides_cli_only;
        ]
   |> run_test_tt_main

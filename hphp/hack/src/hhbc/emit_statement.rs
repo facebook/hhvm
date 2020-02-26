beb7b2d478c5153f484fcc797015bb454cfe41dc
Millie Chen <milliechen@fb.com>
2020-02-26 10:50:24 -0800

    Port emit_statement: emit_do
    Summary: As titled
    
    Reviewed By: shiqicao
    
    Differential Revision: D20110842
    
    fbshipit-source-id: 384e830de2658e11d7fab45ba1e1b3703caa2881

--- hphp/hack/src/hhbc/emit_statement.rs
+++ hphp/hack/src/hhbc/emit_statement.rs
@@ -289,7 +289,12 @@ pub fn emit_stmt(e: &mut Emitter, env: &mut Env, stmt: &tast::Stmt) -> Result {
         a::Stmt_::Using(_) => unimplemented!("TODO(hrust)"),
         a::Stmt_::Break => Ok(emit_break(e, env, pos)),
         a::Stmt_::Continue => Ok(emit_continue(e, env, pos)),
-        a::Stmt_::Do(_) => unimplemented!("TODO(hrust)"),
+        a::Stmt_::Do(x) => emit_do(
+            e,
+            env,
+            &tast::Stmt(pos.clone(), tast::Stmt_::mk_block(x.0.clone())),
+            &x.1,
+        ),
         a::Stmt_::For(x) => emit_for(e, env, pos, &x.0, &x.1, &x.2, &x.3),
         a::Stmt_::Throw(_) => unimplemented!("TODO(hrust)"),
         a::Stmt_::Try(_) => unimplemented!("TODO(hrust)"),
@@ -313,6 +318,30 @@ fn emit_stmts(e: &mut Emitter, env: &Env, stl: &[tast::Stmt]) -> Result {
             .map(|s| emit_stmt(e, &mut new_env, s))
             .collect::<Result<Vec<_>>>()?,
     ))
+}
+
+fn emit_stmt_wrapper(env: &mut Env, emitter: &mut Emitter, stmt: &tast::Stmt) -> Result {
+    emit_stmt(emitter, env, stmt)
+}
+
+fn emit_do(e: &mut Emitter, env: &mut Env, body: &tast::Stmt, cond: &tast::Expr) -> Result {
+    let break_label = e.label_gen_mut().next_regular();
+    let cont_label = e.label_gen_mut().next_regular();
+    let start_label = e.label_gen_mut().next_regular();
+    Ok(InstrSeq::gather(vec![
+        InstrSeq::make_label(start_label.clone()),
+        env.do_in_loop_body(
+            e,
+            break_label.clone(),
+            cont_label.clone(),
+            None,
+            body,
+            emit_stmt_wrapper,
+        )?,
+        InstrSeq::make_label(cont_label),
+        emit_expr::emit_jmpnz(e, env, cond, &start_label)?.instrs,
+        InstrSeq::make_label(break_label),
+    ]))
 }
 
 fn emit_for(
@@ -395,9 +424,6 @@ fn emit_for(
             }),
             None => final_(emitter, env, pos, jmpz, label, cond),
         }
-    };
-    fn emit_stmt_wrapper(env: &mut Env, emitter: &mut Emitter, stmt: &tast::Stmt) -> Result {
-        emit_stmt(emitter, env, stmt)
     };
     // TODO: this is bizarre codegen for a "for" loop.
     //  This should be codegen'd as

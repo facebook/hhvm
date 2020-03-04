3fab1eb55c1247fce359f37bde834df21f7c9e21
Jake Bailey (Hacklang) <jakebailey@fb.com>
2020-03-04 11:02:22 -0800

    Only format manual sections in partially-generated files
    Summary: This is a long-standing feature request which has been blocked on a solution for line-based formatting-suppression. Now that we have the infrastructure for suppressing formatting within ranges of lines in place (after {D20124500}), it is easy to implement suppression of formatting outside of manual sections in partially-generated files.
    
    Reviewed By: arxanas
    
    Differential Revision: D20169730
    
    fbshipit-source-id: acd159705b84121d4029f41c110667c53b2d4ade

--- hphp/hack/src/hackfmt/libhackfmt.ml
+++ hphp/hack/src/hackfmt/libhackfmt.ml
@@ -125,44 +125,106 @@ let format_node ?config ?(indent = 0) node =
   |> Chunk_builder.build
   |> Line_splitter.solve env ~source_text
 
-(** Return the start and end offsets of every fixme comment, sorted by order of
-    appearance in the file. *)
-let get_fixme_offsets tree : Interval.t list =
+let begin_manual_section_regexp =
+  Str.regexp "/\\* BEGIN MANUAL SECTION [^*]*\\*/"
+
+let end_manual_section_tag = "/* END MANUAL SECTION */"
+
+(** Return the start and end offsets of every fixme comment and every manual
+    section (of partially-generated files), sorted by order of appearance in the
+    file. *)
+let get_fixme_ranges_and_manual_sections tree :
+    Interval.t list * Interval.t list =
   let module Syntax = Full_fidelity_positioned_syntax in
   let module Trivia = Full_fidelity_positioned_trivia in
   let module Token = Full_fidelity_positioned_token in
   let trivium_range trivium =
     (Trivia.start_offset trivium, Trivia.end_offset trivium)
   in
-  let add_fixme_range acc trivium =
+  let add_fixme_range fixmes trivium =
     match Trivia.kind trivium with
     | Full_fidelity_trivia_kind.(FixMe | IgnoreError) ->
-      trivium_range trivium :: acc
-    | _ -> acc
+      trivium_range trivium :: fixmes
+    | _ -> fixmes
   in
-  let add_fixme_ranges acc trivia =
-    List.fold (List.rev trivia) ~init:acc ~f:add_fixme_range
+  let add_fixme_ranges fixmes trivia =
+    List.fold (List.rev trivia) ~init:fixmes ~f:add_fixme_range
+  in
+  let is_begin_tag text =
+    try
+      let (_ : int) = Str.search_forward begin_manual_section_regexp text 0 in
+      true
+    with Caml.Not_found -> false
+  in
+  let is_end_tag text = text = end_manual_section_tag in
+  let add_manual_tag manual_tags trivium =
+    match Trivia.kind trivium with
+    | Full_fidelity_trivia_kind.DelimitedComment
+      when is_begin_tag (Trivia.text trivium) ->
+      `Begin (Trivia.end_offset trivium) :: manual_tags
+    | Full_fidelity_trivia_kind.DelimitedComment
+      when is_end_tag (Trivia.text trivium) ->
+      `End (Trivia.start_offset trivium) :: manual_tags
+    | _ -> manual_tags
+  in
+  let add_manual_tags manual_tags trivia =
+    List.fold (List.rev trivia) ~init:manual_tags ~f:add_manual_tag
   in
   let rec aux acc node =
     match Syntax.get_token node with
     | None -> List.fold (List.rev (Syntax.children node)) ~init:acc ~f:aux
     | Some t ->
-      let acc = add_fixme_ranges acc (Token.trailing t) in
-      let acc = add_fixme_ranges acc (Token.leading t) in
-      acc
+      let (fixmes, manual_tags) = acc in
+      let fixmes = add_fixme_ranges fixmes (Token.trailing t) in
+      let fixmes = add_fixme_ranges fixmes (Token.leading t) in
+      let manual_tags = add_manual_tags manual_tags (Token.trailing t) in
+      let manual_tags = add_manual_tags manual_tags (Token.leading t) in
+      (fixmes, manual_tags)
   in
-  aux [] (SyntaxTree.root tree)
+  let (fixmes, manual_tags) = aux ([], []) (SyntaxTree.root tree) in
+  let (manual_sections, _) =
+    List.fold
+      manual_tags
+      ~init:([], None)
+      ~f:(fun (manual_sections, section_start) tag ->
+        match (section_start, tag) with
+        | (None, `Begin start_offset) -> (manual_sections, Some start_offset)
+        | (Some start_offset, `End end_offset) ->
+          ((start_offset, end_offset) :: manual_sections, None)
+        (* Malformed: multiple BEGIN tags in a row. Ignore the earlier. *)
+        | (Some _, `Begin start_offset) -> (manual_sections, Some start_offset)
+        (* Malformed: an END tag with no BEGIN. Ignore it. *)
+        | (None, `End _) -> (manual_sections, section_start))
+  in
+  (fixmes, List.rev manual_sections)
 
 let get_suppressed_formatting_ranges line_boundaries tree =
   let source_text = SyntaxTree.text tree in
+  let (fixme_ranges, manual_sections) =
+    get_fixme_ranges_and_manual_sections tree
+  in
+  let expand_to_line_boundaries =
+    expand_to_line_boundaries ~ranges:line_boundaries source_text
+  in
   (* Expand each range to contain the entire line the fixme is on, then add one
      to the range end and expand again to include the next line, since the fixme
      suppresses errors on both the line in which it appears and the line
      following it. *)
-  get_fixme_offsets tree
-  |> List.map ~f:(expand_to_line_boundaries ~ranges:line_boundaries source_text)
-  |> List.map ~f:(fun (st, ed) -> (st, ed + 1))
-  |> List.map ~f:(expand_to_line_boundaries ~ranges:line_boundaries source_text)
+  let fixme_ranges =
+    fixme_ranges
+    |> List.map ~f:expand_to_line_boundaries
+    |> List.map ~f:(fun (st, ed) -> (st, ed + 1))
+    |> List.map ~f:expand_to_line_boundaries
+  in
+  let generated_sections =
+    if List.is_empty manual_sections then
+      []
+    else
+      let whole_file = [(0, String.length (SourceText.text source_text))] in
+      Interval.diff_sorted_lists whole_file manual_sections
+      |> List.map ~f:expand_to_line_boundaries
+  in
+  List.merge fixme_ranges generated_sections ~compare:Interval.compare
   |> Interval.union_consecutive_overlapping
 
 let text_with_formatted_ranges

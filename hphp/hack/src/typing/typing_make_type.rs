84a5ef69cda6a8509898c5c6b735b3862f2feb70
Andrew Kennedy <akenn@fb.com>
2020-03-05 00:28:49 -0800

    Extend TypeBuilder with classes and well-known types
    Summary: Add classes to `Typing_make_type`. Need well-known classes as taken from `Naming_special_names`. As mentioned in the comments, we probably need to construct a "symbol builder" with support for these built-in names. For now, they're constructed in the type builder itself.
    
    Differential Revision: D20248663
    
    fbshipit-source-id: 46d0f66f4a2953c6496627c3ee1c3021480cc296

--- hphp/hack/src/typing/typing_make_type.rs
+++ hphp/hack/src/typing/typing_make_type.rs
@@ -3,15 +3,69 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
 
-use oxidized::{aast_defs::*, ident, typing_defs::*, typing_reason::*};
+use naming_special_names_rust::{classes, collections};
+use oxidized::{aast_defs::*, ast_defs::*, ident, typing_defs::*, typing_reason::*};
 
 // Struct off which we call type builder methods
 // This gives us the option to keep some state here e.g. for hash consing
-pub struct TypeBuilder {}
+pub struct TypeBuilder {
+    // Well-known identifiers
+    // We could initialize the positions to their actual definition site in hhi
+    // This makes more sense than inheriting the position from the use
+    // But for now, let's just put none as the position
+    // TODO: we might consider creating a SymbolBuilder type
+    id_traversable: Id,
+    id_keyed_traversable: Id,
+    id_keyed_container: Id,
+    id_awaitable: Id,
+    id_generator: Id,
+    id_async_generator: Id,
+    id_async_iterator: Id,
+    id_async_keyed_iterator: Id,
+    id_pair: Id,
+    id_dict: Id,
+    id_keyset: Id,
+    id_vec: Id,
+    id_container: Id,
+    id_throwable: Id,
+    id_datetime: Id,
+    id_datetime_immutable: Id,
+    id_const_vector: Id,
+    id_const_collection: Id,
+    id_collection: Id,
+}
+
+// Copying of string is bad
+// Perhaps best is to actually *resolve* well-known classes
+// as though they appear in the source, so we get the position
+// from the definition e.g. in hhi file.
+fn mk_special_id(name: &str) -> Id {
+    Id(Pos::make_none(), name.to_string())
+}
 
 impl TypeBuilder {
     pub fn new() -> Self {
-        TypeBuilder {}
+        TypeBuilder {
+            id_traversable: mk_special_id(collections::TRAVERSABLE),
+            id_keyed_traversable: mk_special_id(collections::KEYED_TRAVERSABLE),
+            id_keyed_container: mk_special_id(collections::KEYED_CONTAINER),
+            id_awaitable: mk_special_id(classes::AWAITABLE),
+            id_generator: mk_special_id(classes::GENERATOR),
+            id_async_generator: mk_special_id(classes::ASYNC_GENERATOR),
+            id_async_iterator: mk_special_id(classes::ASYNC_ITERATOR),
+            id_async_keyed_iterator: mk_special_id(classes::ASYNC_KEYED_ITERATOR),
+            id_pair: mk_special_id(collections::PAIR),
+            id_dict: mk_special_id(collections::DICT),
+            id_keyset: mk_special_id(collections::KEYSET),
+            id_vec: mk_special_id(collections::VEC),
+            id_container: mk_special_id(collections::CONTAINER),
+            id_throwable: mk_special_id(classes::THROWABLE),
+            id_datetime: mk_special_id(classes::DATE_TIME),
+            id_datetime_immutable: mk_special_id(classes::DATE_TIME_IMMUTABLE),
+            id_const_vector: mk_special_id(collections::CONST_VECTOR),
+            id_const_collection: mk_special_id(collections::CONST_COLLECTION),
+            id_collection: mk_special_id(collections::COLLECTION),
+        }
     }
     // All type construction should go through here
     pub fn mk(&self, reason: Reason, ty_: Ty_) -> Ty {
@@ -19,6 +73,86 @@ impl TypeBuilder {
     }
     pub fn prim(&self, reason: Reason, kind: Tprim) -> Ty {
         self.mk(reason, Ty_::Tprim(kind))
+    }
+    pub fn class(&self, reason: Reason, name: Sid, tys: Vec<Ty>) -> Ty {
+        self.mk(reason, Ty_::Tclass(name, Exact::Nonexact, tys))
+    }
+    pub fn traversable(&self, reason: Reason, ty: Ty) -> Ty {
+        self.class(reason.clone(), self.id_traversable.clone(), vec![ty])
+    }
+    pub fn keyed_traversable(&self, reason: Reason, kty: Ty, vty: Ty) -> Ty {
+        self.class(
+            reason.clone(),
+            self.id_keyed_traversable.clone(),
+            vec![kty, vty],
+        )
+    }
+    pub fn keyed_container(&self, reason: Reason, kty: Ty, vty: Ty) -> Ty {
+        self.class(
+            reason.clone(),
+            self.id_keyed_container.clone(),
+            vec![kty, vty],
+        )
+    }
+    pub fn awaitable(&self, reason: Reason, ty: Ty) -> Ty {
+        self.class(reason.clone(), self.id_awaitable.clone(), vec![ty])
+    }
+    pub fn generator(&self, reason: Reason, key: Ty, value: Ty, send: Ty) -> Ty {
+        self.class(
+            reason.clone(),
+            self.id_generator.clone(),
+            vec![key, value, send],
+        )
+    }
+    pub fn async_generator(&self, reason: Reason, key: Ty, value: Ty, send: Ty) -> Ty {
+        self.class(
+            reason.clone(),
+            self.id_async_generator.clone(),
+            vec![key, value, send],
+        )
+    }
+    pub fn async_iterator(&self, reason: Reason, ty: Ty) -> Ty {
+        self.class(reason.clone(), self.id_async_iterator.clone(), vec![ty])
+    }
+    pub fn async_keyed_iterator(&self, reason: Reason, kty: Ty, vty: Ty) -> Ty {
+        self.class(
+            reason.clone(),
+            self.id_async_keyed_iterator.clone(),
+            vec![kty, vty],
+        )
+    }
+    pub fn pair(&self, reason: Reason, ty1: Ty, ty2: Ty) -> Ty {
+        self.class(reason.clone(), self.id_pair.clone(), vec![ty1, ty2])
+    }
+    pub fn dict(&self, reason: Reason, kty: Ty, vty: Ty) -> Ty {
+        self.class(reason.clone(), self.id_dict.clone(), vec![kty, vty])
+    }
+    pub fn keyset(&self, reason: Reason, ty: Ty) -> Ty {
+        self.class(reason.clone(), self.id_keyset.clone(), vec![ty])
+    }
+    pub fn vec(&self, reason: Reason, ty: Ty) -> Ty {
+        self.class(reason.clone(), self.id_vec.clone(), vec![ty])
+    }
+    pub fn container(&self, reason: Reason, ty: Ty) -> Ty {
+        self.class(reason.clone(), self.id_container.clone(), vec![ty])
+    }
+    pub fn const_vector(&self, reason: Reason, ty: Ty) -> Ty {
+        self.class(reason.clone(), self.id_const_vector.clone(), vec![ty])
+    }
+    pub fn const_collection(&self, reason: Reason, ty: Ty) -> Ty {
+        self.class(reason.clone(), self.id_const_collection.clone(), vec![ty])
+    }
+    pub fn collection(&self, reason: Reason, ty: Ty) -> Ty {
+        self.class(reason.clone(), self.id_collection.clone(), vec![ty])
+    }
+    pub fn throwable(&self, reason: Reason) -> Ty {
+        self.class(reason.clone(), self.id_throwable.clone(), vec![])
+    }
+    pub fn datetime(&self, reason: Reason) -> Ty {
+        self.class(reason.clone(), self.id_datetime.clone(), vec![])
+    }
+    pub fn datetime_immutable(&self, reason: Reason) -> Ty {
+        self.class(reason.clone(), self.id_datetime_immutable.clone(), vec![])
     }
     pub fn int(&self, reason: Reason) -> Ty {
         self.prim(reason, Tprim::Tint)

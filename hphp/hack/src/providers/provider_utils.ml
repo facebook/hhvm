c21bc7584610ca277fc1ce4a69574dd7bf8db8ed
Lucian Wischik <ljw@fb.com>
2020-03-14 14:10:03 -0700

    exn guarantee for respect_but_quarantine
    Summary:
    Previously this function didn't have clean exception semantics. In particular, if an exception arose while entering quarantine, then it would fail to leave the quarantine cleanly.
    
    It's a bug (fixed next in the stack) that we failed to enter the quarantine cleanly. But nevertheless this function should be fixed.
    
    Note that `Utils.with_context ~enter ~exit ~f` will decline to 'exit' if 'enter' raised an exception. That was what bit us. I've switched to `Utils.try_finally` which is less subtle.
    
    I also added code to leave quarantine cleanly in the case where we only partially succeeded to enter quarantine.
    
    Reviewed By: arxanas
    
    Differential Revision: D20450313
    
    fbshipit-source-id: b3c120e067f6f7e206f8909060c5fd4c1ded1258

--- hphp/hack/src/providers/provider_utils.ml
+++ hphp/hack/src/providers/provider_utils.ml
@@ -16,72 +16,79 @@ let ctx_from_server_env (env : ServerEnv.env) : Provider_context.t =
     entries = Relative_path.Map.empty;
   }
 
+(** This function will (1) enter quarantine, (2) respect ctx by updating
+naming table for all files in ctx, (3) do the callback "f", (4) leave
+quarantine. If an exception arises during step (1,2,3) then nevertheless
+we guarantee that quarantine is safely left. If an exception arises during
+step (4) then we'll raise an exception but the program state has become unstable...
+*)
 let respect_but_quarantine_unsaved_changes
     ~(ctx : Provider_context.t) ~(f : unit -> 'a) : 'a =
-  let make_then_revert_local_changes f () =
-    Utils.with_context
-      ~enter:(fun () ->
-        Provider_context.set_is_quarantined_internal ();
-
-        Errors.set_allow_errors_in_default_path true;
-        SharedMem.allow_hashtable_writes_by_current_process false;
-
-        Ast_provider.local_changes_push_stack ();
-        Decl_provider.local_changes_push_stack ctx;
-        File_provider.local_changes_push_stack ();
-        Fixme_provider.local_changes_push_stack ();
-
-        Ide_parser_cache.activate ();
-
-        Naming_provider.push_local_changes ctx)
-      ~exit:(fun () ->
-        Errors.set_allow_errors_in_default_path false;
-        SharedMem.allow_hashtable_writes_by_current_process true;
-
-        Ast_provider.local_changes_pop_stack ();
-        Decl_provider.local_changes_pop_stack ctx;
-        File_provider.local_changes_pop_stack ();
-        Fixme_provider.local_changes_pop_stack ();
-
-        Ide_parser_cache.deactivate ();
-
-        Naming_provider.pop_local_changes ctx;
-
-        SharedMem.invalidate_caches ();
-
-        Provider_context.unset_is_quarantined_internal ())
-      ~do_:f
+  let ast_pushed = ref false in
+  let decl_pushed = ref false in
+  let file_pushed = ref false in
+  let fixme_pushed = ref false in
+  let naming_pushed = ref false in
+  let quarantine_set = ref false in
+  let enter_quarantine_exn () =
+    Ast_provider.local_changes_push_stack ();
+    ast_pushed := true;
+    Decl_provider.local_changes_push_stack ctx;
+    decl_pushed := true;
+    File_provider.local_changes_push_stack ();
+    file_pushed := true;
+    Fixme_provider.local_changes_push_stack ();
+    fixme_pushed := true;
+    Naming_provider.push_local_changes ctx;
+    naming_pushed := true;
+    Ide_parser_cache.activate ();
+    Errors.set_allow_errors_in_default_path true;
+    SharedMem.allow_hashtable_writes_by_current_process false;
+    Provider_context.set_is_quarantined_internal ();
+    quarantine_set := true;
+    ()
+  in
+  let leave_quarantine_exn () =
+    if !quarantine_set then Provider_context.unset_is_quarantined_internal ();
+    SharedMem.allow_hashtable_writes_by_current_process true;
+    Errors.set_allow_errors_in_default_path false;
+    Ide_parser_cache.deactivate ();
+    if !naming_pushed then Naming_provider.pop_local_changes ctx;
+    if !fixme_pushed then Fixme_provider.local_changes_pop_stack ();
+    if !file_pushed then File_provider.local_changes_pop_stack ();
+    if !decl_pushed then Decl_provider.local_changes_pop_stack ctx;
+    if !ast_pushed then Ast_provider.local_changes_pop_stack ();
+    SharedMem.invalidate_caches ();
+    ()
+  in
+  let respect_local_changes_exn () =
+    Relative_path.Map.iter ctx.Provider_context.entries ~f:(fun _path entry ->
+        let ast = Ast_provider.compute_ast ctx entry in
+        let (funs, classes, record_defs, typedefs, consts) =
+          Nast.get_defs ast
+        in
+        (* Update the positions of the symbols present in the AST by redeclaring
+      them. Note that this doesn't handle *removing* any entries from the
+      naming table if they've disappeared since the last time we updated the
+      naming table. *)
+        let get_names ids = List.map ~f:snd ids |> SSet.of_list in
+        Naming_global.remove_decls
+          ~ctx
+          ~funs:(get_names funs)
+          ~classes:(get_names classes)
+          ~record_defs:(get_names record_defs)
+          ~typedefs:(get_names typedefs)
+          ~consts:(get_names consts);
+        Naming_global.make_env ctx ~funs ~classes ~record_defs ~typedefs ~consts);
+    ()
   in
   let (_errors, result) =
-    Errors.do_
-    @@ make_then_revert_local_changes (fun () ->
-           Relative_path.Map.iter
-             ctx.Provider_context.entries
-             ~f:(fun _path entry ->
-               let ast = Ast_provider.compute_ast ctx entry in
-               let (funs, classes, record_defs, typedefs, consts) =
-                 Nast.get_defs ast
-               in
-               (* Update the positions of the symbols present in the AST by redeclaring
-               them. Note that this doesn't handle *removing* any entries from the
-               naming table if they've disappeared since the last time we updated the
-               naming table. *)
-               let get_names ids = List.map ~f:snd ids |> SSet.of_list in
-               Naming_global.remove_decls
-                 ~ctx
-                 ~funs:(get_names funs)
-                 ~classes:(get_names classes)
-                 ~record_defs:(get_names record_defs)
-                 ~typedefs:(get_names typedefs)
-                 ~consts:(get_names consts);
-               Naming_global.make_env
-                 ctx
-                 ~funs
-                 ~classes
-                 ~record_defs
-                 ~typedefs
-                 ~consts);
-
-           f ())
+    Errors.do_ (fun () ->
+        Utils.try_finally
+          ~f:(fun () ->
+            enter_quarantine_exn ();
+            respect_local_changes_exn ();
+            f ())
+          ~finally:leave_quarantine_exn)
   in
   result

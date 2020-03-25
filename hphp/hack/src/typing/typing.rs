e373149ee44002b82c198fb83d8d491e32de6232
Catherine Gasnier <catg@fb.com>
2020-03-25 06:50:42 -0700

    reorder functions in typing.rs
    Summary:
    Now that we have a language where we can define functions in any order, let's order them in a more natural way, starting with the entry point.
    That way a reader who want to start exploring typing.rs can simply start at the top of the file.
    
    Reviewed By: losvald
    
    Differential Revision: D20620476
    
    fbshipit-source-id: c509978407b77450c5a1fe4e0e8410749fd43d0b

--- hphp/hack/src/typing/typing.rs
+++ hphp/hack/src/typing/typing.rs
@@ -13,6 +13,118 @@ use oxidized::{ast, pos::Pos};
 use typing_defs_rust::{tast, FuncBodyAnn, SavedEnv, Ty, Ty_};
 use typing_env_rust::Env;
 
+pub fn program<'a>(ast: &ast::Program, env: &mut Env<'a>) -> tast::Program<'a> {
+    ast.iter().filter_map(|x| def(x, env)).collect()
+}
+
+fn def<'a>(def: &ast::Def, env: &mut Env<'a>) -> Option<tast::Def<'a>> {
+    match def {
+        ast::Def::Fun(x) => Some(tast::Def::mk_fun(fun(x, env))),
+        ast::Def::Stmt(x) => Some(tast::Def::mk_stmt(stmt(x, env))),
+        _ => unimplemented!(),
+    }
+}
+
+fn fun<'a>(f: &ast::Fun_, env: &mut Env<'a>) -> tast::Fun_<'a> {
+    let ast = f.body.ast.iter().map(|x| stmt(x, env)).collect();
+
+    // We put empty vec below for all of those, since real conversion is unimplemented
+    assert!(f.tparams.is_empty());
+    assert!(f.params.is_empty());
+    assert!(f.user_attributes.is_empty());
+    assert!(f.file_attributes.is_empty());
+
+    let ret = tast::TypeHint(env.builder().null(env.builder().mk_rnone()), None);
+
+    tast::Fun_ {
+        span: f.span.clone(),
+        annotation: SavedEnv,
+        mode: f.mode,
+        ret,
+        name: f.name.clone(),
+        tparams: vec![],
+        where_constraints: f.where_constraints.clone(),
+        body: tast::FuncBody {
+            ast,
+            annotation: FuncBodyAnn,
+        },
+        fun_kind: f.fun_kind,
+        variadic: tast::FunVariadicity::FVnonVariadic,
+        params: vec![],
+        user_attributes: vec![],
+        file_attributes: vec![],
+        external: f.external,
+        namespace: f.namespace.clone(),
+        doc_comment: f.doc_comment.clone(),
+        static_: f.static_,
+    }
+}
+
+fn stmt<'a>(ast::Stmt(pos, s): &ast::Stmt, env: &mut Env<'a>) -> tast::Stmt<'a> {
+    tast::Stmt(pos.clone(), stmt_(s, env))
+}
+
+fn stmt_<'a>(s: &ast::Stmt_, env: &mut Env<'a>) -> tast::Stmt_<'a> {
+    match s {
+        ast::Stmt_::Noop => tast::Stmt_::Noop,
+        ast::Stmt_::Markup(x) => markup(&x.0, &x.1),
+        ast::Stmt_::Expr(x) => tast::Stmt_::mk_expr(expr(x, env)),
+        x => unimplemented!("{:#?}", x),
+    }
+}
+
+fn markup<'a>(s: &ast::Pstring, e: &Option<ast::Expr>) -> tast::Stmt_<'a> {
+    match e {
+        None => tast::Stmt_::mk_markup(s.clone(), None),
+        x => unimplemented!("{:#?}", x),
+    }
+}
+
+fn expr<'a>(ast::Expr(pos, e): &ast::Expr, env: &mut Env<'a>) -> tast::Expr<'a> {
+    let (ty, e) = match e {
+        ast::Expr_::Call(x) => {
+            // TODO(hrust) pseudo functions, might_throw
+            let (call_type, e, explicit_targs, el, unpacked_element) = &**x;
+            let in_suspend = false;
+            check_call(
+                env,
+                pos,
+                call_type,
+                e,
+                explicit_targs,
+                el,
+                unpacked_element,
+                in_suspend,
+            )
+        }
+        x => unimplemented!("{:#?}", x),
+    };
+    ast::Expr((pos.clone(), ty), e)
+}
+
+fn check_call<'a>(
+    env: &mut Env<'a>,
+    pos: &Pos,
+    call_type: &ast::CallType,
+    e: &ast::Expr,
+    explicit_targs: &Vec<ast::Targ>,
+    el: &Vec<ast::Expr>,
+    unpacked_element: &Option<ast::Expr>,
+    in_suspend: bool,
+) -> (Ty<'a>, tast::Expr_<'a>) {
+    dispatch_call(
+        env,
+        pos,
+        call_type,
+        e,
+        explicit_targs,
+        el,
+        unpacked_element,
+        in_suspend,
+    )
+    // TODO(hrust) forget_fake_members
+}
+
 fn dispatch_call<'a>(
     env: &mut Env<'a>,
     pos: &Pos,
@@ -81,116 +193,4 @@ fn call<'a>(
         }
         x => unimplemented!("{:#?}", x),
     }
-}
-
-fn expr<'a>(ast::Expr(pos, e): &ast::Expr, env: &mut Env<'a>) -> tast::Expr<'a> {
-    let (ty, e) = match e {
-        ast::Expr_::Call(x) => {
-            // TODO(hrust) pseudo functions, might_throw
-            let (call_type, e, explicit_targs, el, unpacked_element) = &**x;
-            let in_suspend = false;
-            check_call(
-                env,
-                pos,
-                call_type,
-                e,
-                explicit_targs,
-                el,
-                unpacked_element,
-                in_suspend,
-            )
-        }
-        x => unimplemented!("{:#?}", x),
-    };
-    ast::Expr((pos.clone(), ty), e)
-}
-
-fn check_call<'a>(
-    env: &mut Env<'a>,
-    pos: &Pos,
-    call_type: &ast::CallType,
-    e: &ast::Expr,
-    explicit_targs: &Vec<ast::Targ>,
-    el: &Vec<ast::Expr>,
-    unpacked_element: &Option<ast::Expr>,
-    in_suspend: bool,
-) -> (Ty<'a>, tast::Expr_<'a>) {
-    dispatch_call(
-        env,
-        pos,
-        call_type,
-        e,
-        explicit_targs,
-        el,
-        unpacked_element,
-        in_suspend,
-    )
-    // TODO(hrust) forget_fake_members
-}
-
-fn markup<'a>(s: &ast::Pstring, e: &Option<ast::Expr>) -> tast::Stmt_<'a> {
-    match e {
-        None => tast::Stmt_::mk_markup(s.clone(), None),
-        x => unimplemented!("{:#?}", x),
-    }
-}
-
-fn stmt_<'a>(s: &ast::Stmt_, env: &mut Env<'a>) -> tast::Stmt_<'a> {
-    match s {
-        ast::Stmt_::Noop => tast::Stmt_::Noop,
-        ast::Stmt_::Markup(x) => markup(&x.0, &x.1),
-        ast::Stmt_::Expr(x) => tast::Stmt_::mk_expr(expr(x, env)),
-        x => unimplemented!("{:#?}", x),
-    }
-}
-
-fn stmt<'a>(ast::Stmt(pos, s): &ast::Stmt, env: &mut Env<'a>) -> tast::Stmt<'a> {
-    tast::Stmt(pos.clone(), stmt_(s, env))
-}
-
-fn fun<'a>(f: &ast::Fun_, env: &mut Env<'a>) -> tast::Fun_<'a> {
-    let ast = f.body.ast.iter().map(|x| stmt(x, env)).collect();
-
-    // We put empty vec below for all of those, since real conversion is unimplemented
-    assert!(f.tparams.is_empty());
-    assert!(f.params.is_empty());
-    assert!(f.user_attributes.is_empty());
-    assert!(f.file_attributes.is_empty());
-
-    let ret = tast::TypeHint(env.builder().null(env.builder().mk_rnone()), None);
-
-    tast::Fun_ {
-        span: f.span.clone(),
-        annotation: SavedEnv,
-        mode: f.mode,
-        ret,
-        name: f.name.clone(),
-        tparams: vec![],
-        where_constraints: f.where_constraints.clone(),
-        body: tast::FuncBody {
-            ast,
-            annotation: FuncBodyAnn,
-        },
-        fun_kind: f.fun_kind,
-        variadic: tast::FunVariadicity::FVnonVariadic,
-        params: vec![],
-        user_attributes: vec![],
-        file_attributes: vec![],
-        external: f.external,
-        namespace: f.namespace.clone(),
-        doc_comment: f.doc_comment.clone(),
-        static_: f.static_,
-    }
-}
-
-fn def<'a>(def: &ast::Def, env: &mut Env<'a>) -> Option<tast::Def<'a>> {
-    match def {
-        ast::Def::Fun(x) => Some(tast::Def::mk_fun(fun(x, env))),
-        ast::Def::Stmt(x) => Some(tast::Def::mk_stmt(stmt(x, env))),
-        _ => unimplemented!(),
-    }
-}
-
-pub fn program<'a>(ast: &ast::Program, env: &mut Env<'a>) -> tast::Program<'a> {
-    ast.iter().filter_map(|x| def(x, env)).collect()
 }

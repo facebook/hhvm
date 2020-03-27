1dfe594d0cdedbdac684efacd7e6e16e98fd7ed6
Bill Fumerola <bill@fb.com>
2020-03-26 21:52:40 -0700

    fail fast in systemlib_dump.sh
    Reviewed By: leikahing
    
    Differential Revision: D20560327
    
    fbshipit-source-id: 6d49aaab43275e6b36cbd5e93b8339e1d636b166

--- hphp/tools/systemlib_dump.sh
+++ hphp/tools/systemlib_dump.sh
@@ -1,25 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-OBJDUMP=`which objdump`
+OBJDUMP=$(command -v objdump)
 if [ ! -x "${OBJDUMP}" ]; then
-  OBJDUMP=`which gobjdump`
+  OBJDUMP=$(command -v gobjdump)
 fi
 
-if [ ! -x "${OBJDUMP}" ]; then
-  echo "objdump utility not found" 2>&1
+function die {
+  echo "$@" 2>&1
   exit 1
+}
+
+[[ -x "${OBJDUMP}" ]] || die "cannot locate objdump: ${OBJDUMP}"
+
+[[ -n "$1" ]] || die "Usage: $0 <hhvm binary> [section name]"
+[[ -r "$1" ]] || die "hhvm binary ${1} not readable"
+
+SECTNAME=systemlib
+if [ -n "$2" ]; then
+  DIRNAME=$(dirname "$(readlink -e "${BASH_SOURCE[0]}")")
+  SECTNAME=$("${DIRNAME}/section_name.sh" "$2")
 fi
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 <hhvm binary> [section name]" 2>&1
-  exit 1
-fi
-
-if [ -z "$2" ]; then
-  SECTNAME=systemlib
-else
-  DIRNAME=`dirname $0`
-  SECTNAME=`${DIRNAME}/section_name.sh $2`
-fi
-
-${OBJDUMP} -s -j ${SECTNAME} $1
+${OBJDUMP} -s -j "${SECTNAME}" "$1"

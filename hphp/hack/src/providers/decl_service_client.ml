a4f9bb9942a9ce9d1079db32dd87cb05068ed540
Jake Bailey (Hacklang) <jakebailey@fb.com>
2020-03-31 16:41:51 -0700

    Add client-side decl cache
    Summary:
    Avoid a round-trip to the decl service for any previously-requested decl.
    
    Since decls are now cloned into a worker's OCaml heap, it is no longer necessary to pin decl handles in the decl service (for longer than until the next request). Therefore, this diff pins only the last-requested decl (I am hoping this will make it easier for me to trigger cache compaction and exercise the slab-rebase callback). We can investigate whether more aggressive pinning results in perf impacts or prevents cache compaction once we have an MVP (i.e., investigate whether we could use the pin-everything-until-hh_worker-dies strategy if we got rid of polymorphic operations).
    
    Reviewed By: Wilfred
    
    Differential Revision: D20503264
    
    fbshipit-source-id: 8783452bdedfbdbcadff38fa15303bce46577ace

--- hphp/hack/src/providers/decl_service_client.ml
+++ hphp/hack/src/providers/decl_service_client.ml
@@ -8,7 +8,24 @@
 
 open Core_kernel
 
-type t = Decl_ipc_ffi_externs.decl_client
+type t = {
+  client: Decl_ipc_ffi_externs.decl_client;
+  fun_cache: Typing_defs.fun_elt option String.Table.t;
+  class_cache: Shallow_decl_defs.shallow_class option String.Table.t;
+  typedef_cache: Typing_defs.typedef_type option String.Table.t;
+  record_cache: Typing_defs.record_def_type option String.Table.t;
+  gconst_cache: Typing_defs.decl_ty option String.Table.t;
+}
+
+let from_raw_client (client : Decl_ipc_ffi_externs.decl_client) : t =
+  {
+    client;
+    fun_cache = String.Table.create ();
+    class_cache = String.Table.create ();
+    typedef_cache = String.Table.create ();
+    record_cache = String.Table.create ();
+    gconst_cache = String.Table.create ();
+  }
 
 (* HACK: The decl service just stores the decl (rather than a decl option),
    so it either responds with a pointer to the decl_ty (when present) or the
@@ -23,28 +40,49 @@ let pointer_to_option (ptr : 'a) (caller : string) : 'a option =
   else
     Some ptr
 
-let rpc_get_fun (client : Decl_ipc_ffi_externs.decl_client) (name : string) :
-    Typing_defs.fun_elt option =
-  let ty = Decl_ipc_ffi_externs.get_decl client FileInfo.Fun name in
-  pointer_to_option ty "rpc_get_fun"
+let rpc_get_fun (t : t) (name : string) : Typing_defs.fun_elt option =
+  match String.Table.find t.fun_cache name with
+  | Some opt -> opt
+  | None ->
+    let ptr = Decl_ipc_ffi_externs.get_decl t.client FileInfo.Fun name in
+    let fun_elt_opt = pointer_to_option ptr "rpc_get_fun" in
+    String.Table.add_exn t.fun_cache name fun_elt_opt;
+    fun_elt_opt
 
-let rpc_get_class (client : Decl_ipc_ffi_externs.decl_client) (name : string) :
+let rpc_get_class (t : t) (name : string) :
     Shallow_decl_defs.shallow_class option =
-  let ty = Decl_ipc_ffi_externs.get_decl client FileInfo.Class name in
-  pointer_to_option ty "rpc_get_class"
+  match String.Table.find t.class_cache name with
+  | Some opt -> opt
+  | None ->
+    let ptr = Decl_ipc_ffi_externs.get_decl t.client FileInfo.Class name in
+    let class_decl_opt = pointer_to_option ptr "rpc_get_class" in
+    String.Table.add_exn t.class_cache name class_decl_opt;
+    class_decl_opt
 
-let rpc_get_typedef (client : Decl_ipc_ffi_externs.decl_client) (name : string)
-    : Typing_defs.typedef_type option =
-  let ty = Decl_ipc_ffi_externs.get_decl client FileInfo.Typedef name in
-  pointer_to_option ty "rpc_get_typedef"
+let rpc_get_typedef (t : t) (name : string) : Typing_defs.typedef_type option =
+  match String.Table.find t.typedef_cache name with
+  | Some opt -> opt
+  | None ->
+    let ptr = Decl_ipc_ffi_externs.get_decl t.client FileInfo.Typedef name in
+    let typedef_decl_opt = pointer_to_option ptr "rpc_get_typedef" in
+    String.Table.add_exn t.typedef_cache name typedef_decl_opt;
+    typedef_decl_opt
 
-let rpc_get_record_def
-    (client : Decl_ipc_ffi_externs.decl_client) (name : string) :
+let rpc_get_record_def (t : t) (name : string) :
     Typing_defs.record_def_type option =
-  let ty = Decl_ipc_ffi_externs.get_decl client FileInfo.RecordDef name in
-  pointer_to_option ty "rpc_get_record_def"
+  match String.Table.find t.record_cache name with
+  | Some opt -> opt
+  | None ->
+    let ptr = Decl_ipc_ffi_externs.get_decl t.client FileInfo.RecordDef name in
+    let record_decl_opt = pointer_to_option ptr "rpc_get_record_def" in
+    String.Table.add_exn t.record_cache name record_decl_opt;
+    record_decl_opt
 
-let rpc_get_gconst (client : Decl_ipc_ffi_externs.decl_client) (name : string) :
-    Typing_defs.decl_ty option =
-  let ty = Decl_ipc_ffi_externs.get_decl client FileInfo.Const name in
-  pointer_to_option ty "rpc_get_gconst"
+let rpc_get_gconst (t : t) (name : string) : Typing_defs.decl_ty option =
+  match String.Table.find t.gconst_cache name with
+  | Some opt -> opt
+  | None ->
+    let ptr = Decl_ipc_ffi_externs.get_decl t.client FileInfo.Const name in
+    let gconst_ty_opt = pointer_to_option ptr "rpc_get_gconst" in
+    String.Table.add_exn t.gconst_cache name gconst_ty_opt;
+    gconst_ty_opt
--- hphp/hack/src/providers/decl_service_client.mli
+++ hphp/hack/src/providers/decl_service_client.mli
@@ -10,6 +10,8 @@
 for decls and get back answers. Often these requests will block upon IO. *)
 type t
 
+val from_raw_client : Decl_ipc_ffi_externs.decl_client -> t
+
 val rpc_get_fun : t -> string -> Typing_defs.fun_elt option
 
 val rpc_get_class : t -> string -> Shallow_decl_defs.shallow_class option

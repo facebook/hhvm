ccfa436e9531a3033d35b67e19acf5a0fde8e311
Andrew Kennedy <akenn@fb.com>
2020-04-01 15:39:48 -0700

    Fix infinite loop in is_enforceable
    Summary:
    Direct cycles in type definitions such as
    ```
    type T = T;
    ```
    or
    ```
    type T = T2;
    type T2 = T;
    ```
    on their own correctly produce a "Cyclic typedef" error, but cause the type checker to go into an infinite loop if the type alias appears in a function signature e.g.
    ```
    function foo(T $x):void { }
    ```
    This is due to the lack of a "visited" check in `Typing_enforceability.is_enforceable`.
    
    For future, it seems to me that a more maintainable solution would be to mark the type *definition* as bad, and thereby avoid repeated checks for cycles in other parts of the type checker.
    
    Reviewed By: Wilfred
    
    Differential Revision: D20763755
    
    fbshipit-source-id: 56d9c55529b24760eb79ea92bfdfc909831433f3

--- hphp/hack/src/typing/typing_enforceability.ml
+++ hphp/hack/src/typing/typing_enforceability.ml
@@ -18,88 +18,99 @@ let wrap_like ty =
   let r = Typing_reason.Renforceable (get_pos ty) in
   MakeType.like r ty
 
-let rec is_enforceable (env : env) (ty : decl_ty) =
-  match get_node ty with
-  | Tthis -> false
-  | Tapply ((_, name), _) when Env.is_enum env name -> false
-  | Tapply ((_, name), _) when Env.is_typedef env name ->
-    begin
-      match Env.get_typedef env name with
-      | Some { td_vis = Aast.Transparent; td_tparams; td_type; _ } ->
-        (* So that the check does not collide with reified generics *)
-        let env = Env.add_generic_parameters env td_tparams in
-        is_enforceable env td_type
-      | _ -> false
-    end
-  | Tapply ((_, name), tyl) ->
-    begin
-      match Env.get_class env name with
-      | Some tc ->
-        let tparams = Cls.tparams tc in
-        begin
-          match tyl with
-          | [] -> true
-          | targs ->
-            List.Or_unequal_lengths.(
-              begin
-                match
-                  List.fold2 ~init:true targs tparams ~f:(fun acc targ tparam ->
-                      match get_node targ with
-                      | Tdynamic
-                      (* We accept the inner type being dynamic regardless of reification *)
-                      | Tlike _ ->
-                        acc
-                      | _ ->
-                        (match tparam.tp_reified with
-                        | Aast.Erased -> false
-                        | Aast.SoftReified -> false
-                        | Aast.Reified -> is_enforceable env targ && acc))
-                with
-                | Ok new_acc -> new_acc
-                | Unequal_lengths -> true
-              end)
-        end
-      | None -> true
-    end
-  | Tgeneric name ->
-    begin
-      match (Env.get_reified env name, Env.get_enforceable env name) with
-      | (Aast.Erased, _) -> false
-      | (Aast.SoftReified, _) -> false
-      | (Aast.Reified, false) -> false
-      | (Aast.Reified, true) -> true
-    end
-  | Taccess _ -> false
-  | Tlike _ -> false
-  | Tarray (None, None) -> true
-  | Tarray _ -> false
-  | Tprim prim ->
-    begin
-      match prim with
-      | Aast.Tvoid
-      | Aast.Tnoreturn ->
-        false
-      | _ -> true
-    end
-  | Tany _ -> true
-  | Terr -> true
-  | Tnonnull -> true
-  | Tdynamic -> true
-  | Tfun _ -> false
-  | Ttuple _ -> false
-  | Tunion [] -> true
-  | Tunion _ -> false
-  | Tintersection _ -> false
-  | Tshape _ -> false
-  | Tmixed -> true
-  | Tvar _ -> false
-  | Tdarray _ -> false
-  | Tvarray _ -> false
-  (* With no parameters, we enforce varray_or_darray just like array *)
-  | Tvarray_or_darray (_, ty) -> is_any ty
-  | Toption ty -> is_enforceable env ty
-  (* TODO(T36532263) make sure that this is what we want *)
-  | Tpu_access _ -> false
+let is_enforceable (env : env) (ty : decl_ty) =
+  let rec is_enforceable env visited ty =
+    match get_node ty with
+    | Tthis -> false
+    | Tapply ((_, name), _) when Env.is_enum env name -> false
+    | Tapply ((_, name), _) when Env.is_typedef env name ->
+      (* Cyclic type definition error will be produced elsewhere *)
+      (not (SSet.mem name visited))
+      &&
+      begin
+        match Env.get_typedef env name with
+        | Some { td_vis = Aast.Transparent; td_tparams; td_type; _ } ->
+          (* So that the check does not collide with reified generics *)
+          let env = Env.add_generic_parameters env td_tparams in
+          is_enforceable env (SSet.add name visited) td_type
+        | _ -> false
+      end
+    | Tapply ((_, name), tyl) ->
+      begin
+        match Env.get_class env name with
+        | Some tc ->
+          let tparams = Cls.tparams tc in
+          begin
+            match tyl with
+            | [] -> true
+            | targs ->
+              List.Or_unequal_lengths.(
+                begin
+                  match
+                    List.fold2
+                      ~init:true
+                      targs
+                      tparams
+                      ~f:(fun acc targ tparam ->
+                        match get_node targ with
+                        | Tdynamic
+                        (* We accept the inner type being dynamic regardless of reification *)
+                        | Tlike _ ->
+                          acc
+                        | _ ->
+                          (match tparam.tp_reified with
+                          | Aast.Erased -> false
+                          | Aast.SoftReified -> false
+                          | Aast.Reified ->
+                            is_enforceable env visited targ && acc))
+                  with
+                  | Ok new_acc -> new_acc
+                  | Unequal_lengths -> true
+                end)
+          end
+        | None -> true
+      end
+    | Tgeneric name ->
+      begin
+        match (Env.get_reified env name, Env.get_enforceable env name) with
+        | (Aast.Erased, _) -> false
+        | (Aast.SoftReified, _) -> false
+        | (Aast.Reified, false) -> false
+        | (Aast.Reified, true) -> true
+      end
+    | Taccess _ -> false
+    | Tlike _ -> false
+    | Tarray (None, None) -> true
+    | Tarray _ -> false
+    | Tprim prim ->
+      begin
+        match prim with
+        | Aast.Tvoid
+        | Aast.Tnoreturn ->
+          false
+        | _ -> true
+      end
+    | Tany _ -> true
+    | Terr -> true
+    | Tnonnull -> true
+    | Tdynamic -> true
+    | Tfun _ -> false
+    | Ttuple _ -> false
+    | Tunion [] -> true
+    | Tunion _ -> false
+    | Tintersection _ -> false
+    | Tshape _ -> false
+    | Tmixed -> true
+    | Tvar _ -> false
+    | Tdarray _ -> false
+    | Tvarray _ -> false
+    (* With no parameters, we enforce varray_or_darray just like array *)
+    | Tvarray_or_darray (_, ty) -> is_any ty
+    | Toption ty -> is_enforceable env visited ty
+    (* TODO(T36532263) make sure that this is what we want *)
+    | Tpu_access _ -> false
+  in
+  is_enforceable env SSet.empty ty
 
 let make_locl_like_type env ty =
   if env.Typing_env_types.pessimize then

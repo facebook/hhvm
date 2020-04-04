959b3b8ff4494165b9b4bed0fbaad76ecf22ca89
Lucian Wischik <ljw@fb.com>
2020-04-04 07:53:18 -0700

    Shallow_classes_provider.push local invalidation is already done by caller
    Summary:
    Shallow_classes_provider.push_local is only ever invoked by quarantine. Quarantine already invalidates shallow decl caches for all ctx entries. Therefore there's no need for push_local to do the same thing itself.
    
    This is part of my project to make all the 'provider.push_local' functions uniform, and only affect sharedmem.
    
    Reviewed By: arxanas
    
    Differential Revision: D20834227
    
    fbshipit-source-id: 5d918872c51eba078df2f873eac8b463d0d9d3e0

--- hphp/hack/src/providers/shallow_classes_provider.ml
+++ hphp/hack/src/providers/shallow_classes_provider.ml
@@ -96,39 +96,16 @@ let remove_batch (ctx : Provider_context.t) (names : SSet.t) : unit =
   | Provider_backend.Decl_service _ ->
     failwith "remove_batch not implemented for Decl_service"
 
-let invalidate_context_decls_for_local_backend
-    (shallow_decl_cache : Provider_backend.Shallow_decl_cache.t)
-    (entries : Provider_context.entry Relative_path.Map.t) : unit =
-  Relative_path.Map.iter entries ~f:(fun _path entry ->
-      match entry.Provider_context.parser_return with
-      | None -> () (* hasn't been parsed, hence nothing to invalidate *)
-      | Some { Parser_return.ast; _ } ->
-        let (_funs, classes, _record_defs, _typedefs, _gconsts) =
-          Nast.get_defs ast
-        in
-        List.iter classes ~f:(fun (_, class_name) ->
-            Provider_backend.Shallow_decl_cache.remove
-              shallow_decl_cache
-              ~key:
-                (Provider_backend.Shallow_decl_cache_entry.Shallow_class_decl
-                   class_name)))
-
 let push_local_changes (ctx : Provider_context.t) : unit =
   match Provider_context.get_backend ctx with
   | Provider_backend.Shared_memory -> Shallow_classes_heap.push_local_changes ()
-  | Provider_backend.Local_memory { Provider_backend.shallow_decl_cache; _ } ->
-    invalidate_context_decls_for_local_backend
-      shallow_decl_cache
-      (Provider_context.get_entries ctx)
+  | Provider_backend.Local_memory _ -> ()
   | Provider_backend.Decl_service _ ->
     failwith "push_local_changes not implemented for Decl_service"
 
 let pop_local_changes (ctx : Provider_context.t) : unit =
   match Provider_context.get_backend ctx with
   | Provider_backend.Shared_memory -> Shallow_classes_heap.pop_local_changes ()
-  | Provider_backend.Local_memory { Provider_backend.shallow_decl_cache; _ } ->
-    invalidate_context_decls_for_local_backend
-      shallow_decl_cache
-      (Provider_context.get_entries ctx)
+  | Provider_backend.Local_memory _ -> ()
   | Provider_backend.Decl_service _ ->
     failwith "pop_local_changes not implemented for Decl_service"

e25ffcd9c1f492f078267a5c6e2b79f76cb71e96
jjergus <jjergus@fb.com>
2020-04-28 14:42:16 -0700

    security patch

---
+++ hphp/test/slow/sodium/generichash_init.php
@@ -0,0 +1,9 @@
+<?hh
+
+<<__EntryPoint>>
+function main() {
+  $key =
+    hex2bin('36be2998c85757e98c1abf3687c8db3a849a393701c05454023d9aba1096fd47');
+  $y = sodium_crypto_generichash_init($key, 64);
+  var_dump(bin2hex($y));
+}

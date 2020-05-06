906519163ac714fc53cec7c01ecded089f3bbe82
Alexey Toptygin <alexeyt@fb.com>
2020-05-06 10:43:09 -0700

    Fix copy pasta in spec test.
    Summary: Fix copy pasta in hphp/test/slow/spec/tests/types/resource/resources.php so that it's testing everything it intended.
    
    Reviewed By: oulgen
    
    Differential Revision: D21401708
    
    fbshipit-source-id: 785798eb0b9cb6822f38d214d64aad078df5dda1

--- hphp/test/slow/spec/tests/types/resource/resources.php
+++ hphp/test/slow/spec/tests/types/resource/resources.php
@@ -13,10 +13,10 @@ var_dump(is_resource(STDIN));
 var_dump(get_resource_type(STDIN));
 
 var_dump(STDOUT);
-var_dump(is_resource(STDIN));
-var_dump(get_resource_type(STDIN));
+var_dump(is_resource(STDOUT));
+var_dump(get_resource_type(STDOUT));
 
 var_dump(STDERR);
-var_dump(is_resource(STDIN));
-var_dump(get_resource_type(STDIN));
+var_dump(is_resource(STDERR));
+var_dump(get_resource_type(STDERR));
 }

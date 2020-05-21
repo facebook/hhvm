aac56128a287175e94f81a4b3a52d83b41930964
Oguz Ulgen <oulgen@fb.com>
2020-05-20 18:28:56 -0700

    Minor cleanup in profiledArrayAccess
    Reviewed By: markw65
    
    Differential Revision: D21668918
    
    fbshipit-source-id: 4010a421fa0988158c525046f6cf742d99a05ca7

--- hphp/runtime/vm/jit/irgen-minstr.h
+++ hphp/runtime/vm/jit/irgen-minstr.h
@@ -110,89 +110,86 @@ SSATmp* profiledArrayAccess(IRGS& env, SSATmp* arr, SSATmp* key, MOpMode mode,
                                         : ProfileMixedArrayAccess;
     auto const data = ArrayAccessProfileData { profile.handle(), cow_check };
     gen(env, op, data, arr, key);
-    return generic(key, SizeHintData{});
   }
+  if (!profile.optimizing()) return generic(key, SizeHintData{});
 
-  if (profile.optimizing()) {
-    auto const data = profile.data();
-    auto const result = data.choose();
-    logArrayAccessProfile(env, arr, key, mode, data);
+  auto const data = profile.data();
+  auto const result = data.choose();
+  logArrayAccessProfile(env, arr, key, mode, data);
 
-    FTRACE_MOD(Trace::idx, 1, "{}\nArrayAccessProfile: {}\n",
-               env.irb->curMarker().show(), data.toString());
+  FTRACE_MOD(Trace::idx, 1, "{}\nArrayAccessProfile: {}\n",
+             env.irb->curMarker().show(), data.toString());
 
-    using Action = ArrayAccessProfile::Action;
-    auto const missingCond = [&] (Action action, auto f) {
-      assertx(action != Action::None);
-      return cond(env,
-        f,
-        [&] {
-          return missing(key);
-        },
-        [&] {
-          hint(env, Block::Hint::Unlikely);
-          if (action == Action::Cold) return generic(key, result.size_hint);
-          gen(env, Jmp, makeExitSlow(env));
-          return cns(env, TBottom);
-        }
-      );
-    };
-
-    if (!is_define && result.empty != Action::None) {
-      return missingCond(result.empty, [&] (Block* taken) {
-        auto const count = [&] {
-          if (is_dict) return gen(env, CountDict, arr);
-          if (is_keyset) return gen(env, CountKeyset, arr);
-          return gen(env, CountArrayFast, arr);
-        }();
-        gen(env, JmpNZero, taken, count);
-      });
-    }
-    if (!is_define && !is_keyset && result.missing != Action::None) {
-      return missingCond(result.missing, [&] (Block* taken) {
-        // According to the profiling, the key is mostly a TStaticStr.
-        // If if the JIT doesn't know that statically, lets check for it.
-        auto const skey = key->isA(TStaticStr) ? key :
-          gen(env, CheckType, TStaticStr, taken, key);
-        gen(env, CheckMissingKeyInArrLike, taken, arr, skey);
-        auto const t = is_dict ? TStaticDict :
-                       is_keyset ? TStaticKeyset : TStaticArr;
-        gen(env, AssertType, t, arr);
-      });
-    }
-    auto const offset_action = result.offset.first;
-    if (offset_action == Action::None) return generic(key, result.size_hint);
-
-    return cond(
-      env,
-      [&] (Block* taken) {
-        auto const marr = [&](){
-          if (is_dict || is_keyset) return arr;
-          return gen(env, CheckType, TMixedArr, taken, arr);
-        }();
-
-        auto const op = is_dict ? CheckDictOffset
-                                : is_keyset ? CheckKeysetOffset
-                                            : CheckMixedArrayOffset;
-        gen(env, op, IndexData { result.offset.second }, taken, marr, key);
-        if (cow_check) gen(env, CheckArrayCOW, taken, marr);
-        return marr;
-      },
-      [&] (SSATmp* tmp) {
-        return direct(tmp, key, cns(env, result.offset.second));
+  using Action = ArrayAccessProfile::Action;
+  auto const missingCond = [&] (Action action, auto f) {
+    assertx(action != Action::None);
+    return cond(env,
+      f,
+      [&] {
+        return missing(key);
       },
       [&] {
         hint(env, Block::Hint::Unlikely);
-        // NOTE: We could pass result.size_hint here, but that's the size hint
-        // for the overall distribution, not the conditional distribution when
-        // the likely offset profile misses. We pass a default profile instead.
-        if (offset_action == Action::Cold) return generic(key, SizeHintData{});
+        if (action == Action::Cold) return generic(key, result.size_hint);
         gen(env, Jmp, makeExitSlow(env));
         return cns(env, TBottom);
       }
     );
+  };
+
+  if (!is_define && result.empty != Action::None) {
+    return missingCond(result.empty, [&] (Block* taken) {
+      auto const count = [&] {
+        if (is_dict) return gen(env, CountDict, arr);
+        if (is_keyset) return gen(env, CountKeyset, arr);
+        return gen(env, CountArrayFast, arr);
+      }();
+      gen(env, JmpNZero, taken, count);
+    });
   }
-  return generic(key, SizeHintData{});
+  if (!is_define && !is_keyset && result.missing != Action::None) {
+    return missingCond(result.missing, [&] (Block* taken) {
+      // According to the profiling, the key is mostly a TStaticStr.
+      // If if the JIT doesn't know that statically, lets check for it.
+      auto const skey = key->isA(TStaticStr) ? key :
+        gen(env, CheckType, TStaticStr, taken, key);
+      gen(env, CheckMissingKeyInArrLike, taken, arr, skey);
+      auto const t = is_dict ? TStaticDict :
+                      is_keyset ? TStaticKeyset : TStaticArr;
+      gen(env, AssertType, t, arr);
+    });
+  }
+  auto const offset_action = result.offset.first;
+  if (offset_action == Action::None) return generic(key, result.size_hint);
+
+  return cond(
+    env,
+    [&] (Block* taken) {
+      auto const marr = [&](){
+        if (is_dict || is_keyset) return arr;
+        return gen(env, CheckType, TMixedArr, taken, arr);
+      }();
+
+      auto const op = is_dict ? CheckDictOffset
+                              : is_keyset ? CheckKeysetOffset
+                                          : CheckMixedArrayOffset;
+      gen(env, op, IndexData { result.offset.second }, taken, marr, key);
+      if (cow_check) gen(env, CheckArrayCOW, taken, marr);
+      return marr;
+    },
+    [&] (SSATmp* tmp) {
+      return direct(tmp, key, cns(env, result.offset.second));
+    },
+    [&] {
+      hint(env, Block::Hint::Unlikely);
+      // NOTE: We could pass result.size_hint here, but that's the size hint
+      // for the overall distribution, not the conditional distribution when
+      // the likely offset profile misses. We pass a default profile instead.
+      if (offset_action == Action::Cold) return generic(key, SizeHintData{});
+      gen(env, Jmp, makeExitSlow(env));
+      return cns(env, TBottom);
+    }
+  );
 }
 
 ///////////////////////////////////////////////////////////////////////////////

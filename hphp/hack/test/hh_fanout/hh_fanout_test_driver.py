00c775243759525b0dc649cb502a3a2d31a9ef15
Waleed Khan <waleedk@fb.com>
2020-06-03 12:26:02 -0700

    Use DSL for test cases
    Summary: I want to add the ability to calculate either fanout or typechecking errors in a test case. It's getting unwieldy to use a test repository without a description of what test we should be running. This commit introduces a simple DSL to write tests. (We'll be adding a `calculate-errors` command in the next commit.)
    
    Reviewed By: 2BitSalute
    
    Differential Revision: D21754107
    
    fbshipit-source-id: deff63756d4d577a5b6d48d38ff528735203912e

--- hphp/hack/test/hh_fanout/hh_fanout_test_driver.py
+++ hphp/hack/test/hh_fanout/hh_fanout_test_driver.py
@@ -5,8 +5,8 @@
 The fanout service answers the question "given these changed symbols, which
 files need to be re-typechecked"?
 
-The test driver verifies that the list of files to re-typecheck is the expected
-list.
+The test cases are in files called `test.txt` in the subdirectories of this
+directory. They are written in a small DSL (detailed later in this file).
 
 TODO: Additionally, we perform the sanity check that the list of typechecking
 errors produced from a full typecheck is the same as for an incremental
@@ -29,6 +29,7 @@ from typing import Dict, List, Optional, cast
 DEBUGGING = False
 
 Path = str
+Cursor = str
 
 
 def log(message: str) -> None:
@@ -43,7 +44,7 @@ def copy(source: Path, dest: Path) -> None:
 
 def exec(args: List[str]) -> str:
     log(f"Running: {' '.join(args)}")
-    result = subprocess.check_output(args)
+    result = subprocess.check_output(args, stderr=subprocess.DEVNULL)
     return result.decode()
 
 
@@ -133,7 +134,7 @@ def sanitize_hh_fanout_result(env: Env, result: Dict[str, object]) -> None:
             env, relevant_dep_edge["dependent_path"]
         )
 
-    cursor = cast(str, result["cursor"])
+    cursor = cast(Cursor, result["cursor"])
     delimiter = ","
     assert delimiter in cursor
     parts = cursor.split(delimiter)
@@ -144,21 +145,24 @@ def sanitize_hh_fanout_result(env: Env, result: Dict[str, object]) -> None:
         result["telemetry"] = "<telemetry-redacted-for-test>"
 
 
-def run_test(
+def run_fanout_test(
     env: Env,
     saved_state_info: SavedStateInfo,
-    test_path: Path,
+    args: List[Path],
     changed_files: List[Path],
-) -> None:
+    cursor: Optional[Cursor],
+) -> Cursor:
     result = run_hh_fanout(
         env=env,
         saved_state_info=saved_state_info,
         changed_files=changed_files,
-        args=[test_path],
-        cursor=None,
+        args=args,
+        cursor=cursor,
     )
+    cursor = cast(Cursor, result["cursor"])
     sanitize_hh_fanout_result(env, result)
     pprint.pprint(result)
+    return cursor
 
 
 def main() -> None:
@@ -168,13 +172,11 @@ def main() -> None:
     parser.add_argument("--hh-server", type=os.path.abspath)
 
     args = parser.parse_args()
+    source_dir = os.path.dirname(args.test_path)
     with tempfile.TemporaryDirectory() as work_dir:
-        test_path = os.path.join(work_dir, os.path.relpath(args.test_path, os.getcwd()))
         with open(os.path.join(work_dir, ".hhconfig"), "w") as f:
             f.write("")
-        for source_file in glob.glob(
-            os.path.join(os.path.dirname(args.test_path), "*.php")
-        ):
+        for source_file in glob.glob(os.path.join(source_dir, "*.php")):
             copy(source_file, work_dir)
 
         env = Env(
@@ -184,30 +186,62 @@ def main() -> None:
         )
         saved_state_info = generate_saved_state(env, work_dir)
 
-        i = 0
-        while True:
-            i += 1
-            new_files = glob.glob(
-                os.path.join(os.path.dirname(args.test_path), f"*.new{i}")
-            )
-            if not new_files and i > 1:
-                break
+        with open(args.test_path) as f:
+            test_commands = f.readlines()
 
-            changed_files = []
-            for source_file in new_files:
-                source_file = os.path.relpath(source_file, os.getcwd())
-                (target_filename, _) = os.path.splitext(source_file)
-                target_path = os.path.join(work_dir, target_filename)
-                changed_files.append(target_path)
-                copy(source_file, target_path)
+        # The test proceeds through a series of timestamps. The first timestamp
+        # is 0, which corresponds to the initial state of the repository,
+        # copied from the test source directory. A saved-state is generated
+        # using that set of files, and fed into `hh_fanout` commands.
+        #
+        # When we move to the next timestamp `N`, we copy any files matching
+        # the pattern `*.php.newN` into the working directory (simulating a
+        # file change on disk).
+        timestamp = 0
+        is_first = True
+        changed_files: List[Path] = []
+        cursor: Optional[Cursor] = None
+        for test_command in test_commands:
+            test_command = test_command.strip()
+            if test_command.startswith("#") or test_command == "":
+                continue
+            [command, *command_args] = test_command.split()
 
-            print(f"Change set #{i}")
-            run_test(
-                env=env,
-                saved_state_info=saved_state_info,
-                test_path=test_path,
-                changed_files=changed_files,
-            )
+            # `advance-time` advances the timestamp to the next value, and
+            # copies any files for that timestamp into the test working
+            # directory.
+            if command == "advance-time":
+                timestamp += 1
+                new_files = glob.glob(os.path.join(source_dir, f"*.new{timestamp}"))
+                changed_files = []
+                for source_file in new_files:
+                    source_file = os.path.relpath(source_file, os.getcwd())
+                    (target_filename, _) = os.path.splitext(source_file)
+                    target_path = os.path.join(work_dir, target_filename)
+                    changed_files.append(target_path)
+                    copy(source_file, target_path)
+
+            # `calculate-fanout` calculates the fanout for the arguments and
+            # prints it to the test output for comparison against the `.exp`
+            # file.
+            elif command == "calculate-fanout":
+                if is_first:
+                    is_first = False
+                else:
+                    print()
+                print(f"Fanout calculation at time #{timestamp}")
+
+                cursor = run_fanout_test(
+                    env=env,
+                    saved_state_info=saved_state_info,
+                    args=command_args,
+                    changed_files=changed_files,
+                    cursor=cursor,
+                )
+                changed_files = []
+
+            else:
+                raise ValueError(f"Unrecognized test command: {command}")
 
         if DEBUGGING:
             debug_dir = "/tmp/hh_fanout_debug"

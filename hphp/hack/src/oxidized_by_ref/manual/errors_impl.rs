060c10f7085e61cfa43ebdbe16b15035ab97fed5
David Tolnay <dtolnay@fb.com>
2020-06-04 12:18:22 -0700

    rustfmt: Use use_try_shorthand default
    Summary:
    I observed that for whatever reason our setting of `use_try_shorthand = true` in rustfmt.toml was causing entire functions to not get processed by rustfmt. Even files that contain neither `try` nor `?`. Remove it and reformat fbsource.
    
    Documentation of that config:
    
    - https://github.com/rust-lang/rustfmt/blob/master/Configurations.md#use_try_shorthand
    
    We don't particularly care about the value anymore because nobody writes `r#try!(...)` in 2018 edition code.
    
    Minimized:
    
    ```
    fn f() {
        g(
            )
            // ...
            .h
    }
    ```
    
    This function gets formatted only if use_try_shorthand is not set.
    
    The bug is fixed in the rustfmt 2.0 release candidate.
    
    Reviewed By: jsgf
    
    Differential Revision: D21878162
    
    fbshipit-source-id: b028673c0eb703984d24bf0d2983453fc2a8c212

--- hphp/hack/src/oxidized_by_ref/manual/errors_impl.rs
+++ hphp/hack/src/oxidized_by_ref/manual/errors_impl.rs
@@ -38,7 +38,8 @@ impl<P: Ord> Ord for Error_<'_, P> {
         let (self_pos, self_msg) = self_first;
         let (other_pos, other_msg) = other_first;
         // The primary sort order is the position of the first message.
-        self_pos.cmp(other_pos)
+        self_pos
+            .cmp(other_pos)
             // If the positions are the same, sort by error code.
             .then((*self_code as isize).cmp(&(*other_code as isize)))
             // If the error codes are the same, sort by message text.
@@ -46,7 +47,12 @@ impl<P: Ord> Ord for Error_<'_, P> {
             // If the first message text is the same, compare the rest of the
             // messages (which contain further explanation for the error
             // reported in the first message).
-            .then(self_messages.iter().skip(1).cmp(other_messages.iter().skip(1)))
+            .then(
+                self_messages
+                    .iter()
+                    .skip(1)
+                    .cmp(other_messages.iter().skip(1)),
+            )
     }
 }
 

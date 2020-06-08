a8569adad0db669eceb81590f3b225bdd50e5ae3
jjergus <jjergus@fb.com>
2020-06-08 10:05:23 -0700

    Releasing 4.61.0

--- hphp/runtime/version.h
+++ hphp/runtime/version.h
@@ -13,7 +13,7 @@
 # define HHVM_VERSION_MAJOR 4
 # define HHVM_VERSION_MINOR 61
 # define HHVM_VERSION_PATCH 0
-# define HHVM_VERSION_SUFFIX "-dev"
+# define HHVM_VERSION_SUFFIX ""
 #endif
 
 /* HHVM_VERSION_ID minus the patch number

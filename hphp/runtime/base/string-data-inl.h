eb6a1c2e710e67b52ae356f13d75ed0e96e9bc34
Oguz Ulgen <oulgen@fb.com>
2020-06-24 10:37:56 -0700

    Check for pointer equality in isame
    Summary: Simple optimization
    
    Reviewed By: ricklavoie
    
    Differential Revision: D22163375
    
    fbshipit-source-id: f76d36caf8aaedacbed8325c95d64cf10cdf04ac

--- hphp/runtime/base/string-data-inl.h
+++ hphp/runtime/base/string-data-inl.h
@@ -167,6 +167,7 @@ inline bool StringData::same(const StringData* s) const {
 
 inline bool StringData::isame(const StringData* s) const {
   assertx(s);
+  if (this == s) return true;
   if (m_len != s->m_len) return false;
   return bstrcaseeq(data(), s->data(), m_len);
 }

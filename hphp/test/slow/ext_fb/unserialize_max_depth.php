8f6709471fc65f0eecdbfef8ae1ce679c9ae5fd8
jjergus <jjergus@fb.com>
2020-06-29 11:36:00 -0700

    CVE-2020-1898
    Don't allow for unlimited nesting in FBUnserialize or fb_compact_unserialize

---
+++ hphp/test/slow/ext_fb/unserialize_max_depth.php
@@ -0,0 +1,53 @@
+<?hh
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+
+function make_fb_serialize_struct($size) {
+  return str_repeat("\x0a\x02\x01", $size) . "\x02\x01" . str_repeat("\x01", $size);
+}
+
+function make_fb_serialize_vector($size) {
+  return str_repeat("\x12", $size) . str_repeat("\x01", $size);
+}
+
+function make_fb_serialize_list($size) {
+  return str_repeat("\x13\x02\x01", $size) . "\x02\x01" . str_repeat("\x01", $size);
+}
+
+function make_fb_cs_list_map($size) {
+  return str_repeat("\xfa\xfd", $size) . str_repeat("\xfc", $size);
+}
+
+function make_fb_cs_map($size) {
+  return str_repeat("\xfb\x01", $size) . "\x01" . str_repeat("\xfc", $size);
+}
+
+function make_fb_cs_vector($size) {
+  return str_repeat("\xfe", $size) . str_repeat("\xfc", $size);
+}
+
+function test($serialized) {
+  $ret = null;
+  var_dump(
+    fb_unserialize(
+      $serialized,
+      inout $ret,
+      FB_SERIALIZE_HACK_ARRAYS
+    )
+  );
+  var_dump($ret);
+}
+
+function tests($size) {
+  test(make_fb_serialize_struct($size));
+  test(make_fb_serialize_vector($size));
+  test(make_fb_serialize_list($size));
+  test(make_fb_cs_list_map($size));
+  test(make_fb_cs_map($size));
+  test(make_fb_cs_vector($size));
+}
+
+<<__EntryPoint>>
+function main() {
+  tests(10);
+  tests(1026);
+}

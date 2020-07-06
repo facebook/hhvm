3fb49d6418a494d27d2ebafbb58a06a426736675
Jake Bailey (Hacklang) <jakebailey@fb.com>
2020-07-06 14:25:36 -0700

    Populate naming cache based on decl results and vice versa
    Summary: If we get a decl from a decl request, we can determine what file it is in, so we can populate the naming cache and avoid an IPC round-trip. Likewise, if we perform a naming request and learn that there is *no* decl, we can save that information in the decl cache and avoid future round-trips.
    
    Reviewed By: Wilfred
    
    Differential Revision: D20527978
    
    fbshipit-source-id: eff923f2560fe6197227b489412581ef8ab777ee

--- hphp/hack/src/providers/decl_service_client.ml
+++ hphp/hack/src/providers/decl_service_client.ml
@@ -43,6 +43,11 @@ let pointer_to_option (ptr : 'a) : 'a option =
   else
     Some ptr
 
+(* When writing information from a decl request into a naming cache or vice
+   versa, we don't care whether an entry already exists, so we ignore the
+   returned value (which indicates whether the added entry was a duplicate).  *)
+let add_to_cache cache key value = ignore (String.Table.add cache key value)
+
 let rpc_get_fun (t : t) (name : string) : Typing_defs.fun_elt option =
   match String.Table.find t.fun_cache name with
   | Some opt -> opt
@@ -50,6 +55,11 @@ let rpc_get_fun (t : t) (name : string) : Typing_defs.fun_elt option =
     let ptr = Decl_ipc_ffi_externs.get_decl t.client FileInfo.Fun name in
     let fun_elt_opt = pointer_to_option ptr in
     String.Table.add_exn t.fun_cache name fun_elt_opt;
+    let path_opt =
+      Option.map fun_elt_opt ~f:(fun fun_elt ->
+          Pos.filename fun_elt.Typing_defs.fe_pos)
+    in
+    add_to_cache t.fun_path_cache name path_opt;
     fun_elt_opt
 
 let rpc_get_class (t : t) (name : string) :
@@ -60,6 +70,11 @@ let rpc_get_class (t : t) (name : string) :
     let ptr = Decl_ipc_ffi_externs.get_decl t.client FileInfo.Class name in
     let class_decl_opt = pointer_to_option ptr in
     String.Table.add_exn t.class_cache name class_decl_opt;
+    let path_and_kind_opt =
+      Option.map class_decl_opt ~f:(fun sc ->
+          (Pos.filename (fst sc.Shallow_decl_defs.sc_name), Naming_types.TClass))
+    in
+    add_to_cache t.type_path_and_kind_cache name path_and_kind_opt;
     class_decl_opt
 
 let rpc_get_typedef (t : t) (name : string) : Typing_defs.typedef_type option =
@@ -69,6 +84,11 @@ let rpc_get_typedef (t : t) (name : string) : Typing_defs.typedef_type option =
     let ptr = Decl_ipc_ffi_externs.get_decl t.client FileInfo.Typedef name in
     let typedef_decl_opt = pointer_to_option ptr in
     String.Table.add_exn t.typedef_cache name typedef_decl_opt;
+    let path_and_kind_opt =
+      Option.map typedef_decl_opt ~f:(fun td ->
+          (Pos.filename td.Typing_defs.td_pos, Naming_types.TTypedef))
+    in
+    add_to_cache t.type_path_and_kind_cache name path_and_kind_opt;
     typedef_decl_opt
 
 let rpc_get_record_def (t : t) (name : string) :
@@ -79,6 +99,11 @@ let rpc_get_record_def (t : t) (name : string) :
     let ptr = Decl_ipc_ffi_externs.get_decl t.client FileInfo.RecordDef name in
     let record_decl_opt = pointer_to_option ptr in
     String.Table.add_exn t.record_cache name record_decl_opt;
+    let path_and_kind_opt =
+      Option.map record_decl_opt ~f:(fun rdt ->
+          (Pos.filename rdt.Typing_defs.rdt_pos, Naming_types.TRecordDef))
+    in
+    add_to_cache t.type_path_and_kind_cache name path_and_kind_opt;
     record_decl_opt
 
 let rpc_get_gconst (t : t) (name : string) : Typing_defs.decl_ty option =
@@ -88,6 +113,11 @@ let rpc_get_gconst (t : t) (name : string) : Typing_defs.decl_ty option =
     let ptr = Decl_ipc_ffi_externs.get_decl t.client FileInfo.Const name in
     let gconst_ty_opt = pointer_to_option ptr in
     String.Table.add_exn t.gconst_cache name gconst_ty_opt;
+    let path_opt =
+      Option.map gconst_ty_opt ~f:(fun ty ->
+          Pos.filename (Typing_defs.get_pos ty))
+    in
+    add_to_cache t.gconst_path_cache name path_opt;
     gconst_ty_opt
 
 let rpc_get_gconst_path (t : t) (name : string) : Relative_path.t option =
@@ -96,6 +126,7 @@ let rpc_get_gconst_path (t : t) (name : string) : Relative_path.t option =
   | None ->
     let opt = Decl_ipc_ffi_externs.get_const_path t.client name in
     String.Table.add_exn t.gconst_path_cache name opt;
+    if Option.is_none opt then add_to_cache t.gconst_cache name None;
     opt
 
 let rpc_get_fun_path (t : t) (name : string) : Relative_path.t option =
@@ -104,6 +135,7 @@ let rpc_get_fun_path (t : t) (name : string) : Relative_path.t option =
   | None ->
     let opt = Decl_ipc_ffi_externs.get_fun_path t.client name in
     String.Table.add_exn t.fun_path_cache name opt;
+    if Option.is_none opt then add_to_cache t.fun_cache name None;
     opt
 
 let rpc_get_type_path_and_kind (t : t) (name : string) :
@@ -113,4 +145,12 @@ let rpc_get_type_path_and_kind (t : t) (name : string) :
   | None ->
     let opt = Decl_ipc_ffi_externs.get_type_path_and_kind t.client name in
     String.Table.add_exn t.type_path_and_kind_cache name opt;
+    let kind_opt = Option.map opt snd in
+    let ( <> ) a b = not (Option.equal Naming_types.equal_kind_of_type a b) in
+    if kind_opt <> Some Naming_types.TClass then
+      add_to_cache t.class_cache name None;
+    if kind_opt <> Some Naming_types.TTypedef then
+      add_to_cache t.typedef_cache name None;
+    if kind_opt <> Some Naming_types.TRecordDef then
+      add_to_cache t.record_cache name None;
     opt

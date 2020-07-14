f89648ca199edac3215579b5345f3254c8b06d53
Emil Hesslow <emil@fb.com>
2020-07-13 17:36:51 -0700

    Fix more top level tests
    Reviewed By: oulgen, jano
    
    Differential Revision: D22480515
    
    fbshipit-source-id: 5b1b3f78c5d7c0f41c48442257a79368ac7b664d

--- hphp/test/slow/array_provenance/trait-flattening.php
+++ hphp/test/slow/array_provenance/trait-flattening.php
@@ -1,13 +1,10 @@
 <?hh
 
-include dirname(__FILE__) . "/trait-flattening.inc";
-
-class foo {
-  use baz;
-}
-
 <<__EntryPoint>>
 function main() {
+  include __DIR__."/trait-flattening.inc";
+  include __DIR__."/trait-flattening-class.inc";
+
   $x = new foo;
   var_dump(HH\get_provenance($x->frob()));
   var_dump(HH\get_provenance($x->fizaz()));

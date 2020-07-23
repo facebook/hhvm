f9cc41330549072e552ed5979c5fa8c00f5286e5
Shaunak Kishore <kshaunak@fb.com>
2020-07-22 22:56:28 -0700

    Ban many SPL iterators in the runtime
    Summary: Ban all methods and iterator classes removed from HHI in D22665716 (https://github.com/facebook/hhvm/commit/b364679a05fed8c19965fa032c61beeff8f27050).
    
    Reviewed By: ricklavoie
    
    Differential Revision: D22657529
    
    fbshipit-source-id: 0de0bd05962a181cbd93c578a42f06dfa1d826c7

--- hphp/system/php/spl/iterators/ParentIterator.php
+++
@@ -1,9 +0,0 @@
-<?hh // partial
-
-class ParentIterator extends RecursiveFilterIterator
- implements RecursiveIterator, OuterIterator {
-
-  public function accept() {
-    return $this->getInnerIterator()->hasChildren();
-  }
-}

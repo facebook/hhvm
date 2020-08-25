fb2d9c89a80e6ee85c0b65c92980e23b2661c2a7
Jake Bailey (Hacklang) <jakebailey@fb.com>
2020-08-25 11:20:15 -0700

    Remove oxidized_by_rc
    Summary: This was used for typechecker port experiments, but since those have been removed, it's no longer needed.
    
    Differential Revision: D23320609
    
    fbshipit-source-id: 7bc15bb2437e97078696657a93548845fe7c209b

--- hphp/hack/src/oxidized_by_rc/manual/i_map.rs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Facebook, Inc. and its affiliates.
-//
-// This source code is licensed under the MIT license found in the
-// LICENSE file in the "hack" directory of this source tree.
-
-pub type IMap<T> = std::collections::BTreeMap<isize, T>;

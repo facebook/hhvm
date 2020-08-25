2e62b4e7cdd1623df129dac60fa14b50f71e7dfc
Steve Cao <shiqicao@fb.com>
2020-08-25 04:45:16 -0700

    remove unused ppl_check
    Reviewed By: dabek
    
    Differential Revision: D23278794
    
    fbshipit-source-id: 81ffdf923addfa652348ed7b8b405280b92747c4

--- hphp/hack/src/typing/tast_check/tast_check.ml
+++ hphp/hack/src/typing/tast_check/tast_check.ml
@@ -25,7 +25,6 @@ let visitor ctx =
         String_cast_check.handler;
         Tautology_check.handler;
         Enforceable_hint_check.handler;
-        Ppl_check.handler;
         Redundant_nullsafe_check.handler;
         Const_write_check.handler;
         Switch_check.handler (fun t ->

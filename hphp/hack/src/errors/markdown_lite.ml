b42304d66cb34f66fd306bed8fbbcec11cafbef2
Michael Christensen <michristensen@fb.com>
2020-09-11 09:49:42 -0700

    Allow nested markdown delimiters in error messages
    Summary:
    This adds limited support for nested Markdown delimiters in error messages, e.g.
    ```
    `camel~~C~~ase`
    ```
    renders is underlined with 'C' highlighted in color. Here's an actual example:
    
    {F333221853}
    
    This diff's approach is to accumulate a list of delimiters for each character in a message, recursively trying parse the contents of valid sections. This is not an attempt to cover all the possible edge cases of a full-fledged markdown parser (e.g. there are a lot of rules just related to nesting in the Commonmark [spec](https://spec.commonmark.org/0.29/#emphasis-and-strong-emphasis)), but basic things like the above example should work.
    
    Reviewed By: Wilfred
    
    Differential Revision: D23621091
    
    fbshipit-source-id: c0578c577cc780a8ccf9c5234a567119e14c8ac9

--- hphp/hack/src/errors/markdown_lite.ml
+++ hphp/hack/src/errors/markdown_lite.ml
@@ -7,15 +7,28 @@
  *)
 open Hh_prelude
 
-type parse_state = string * string
-
-type parser = parse_state -> parse_state option
-
 type delimiter =
   | Backtick
   | Asterisk
   | DoubleAsterisk
   | DoubleTilde
+
+module DelimiterKind = struct
+  type t = delimiter
+
+  let compare = compare
+end
+
+module DelimiterSet = Caml.Set.Make (DelimiterKind)
+
+type tagged_char = DelimiterSet.t * char
+
+type parse_state = {
+  parsed: tagged_char list;
+  remaining: string;
+}
+
+type parser = parse_state -> parse_state option
 
 let delimiter_to_string = function
   | Backtick -> "`"
@@ -34,72 +47,61 @@ let first_and_tail_exn s =
     *foo bar* for italics
     **foo bar** for bold
     `foo bar` for code (underlines text)
-    ~~foo bar~~ for highlighting the text in red
+    ~~foo bar~~ for highlighting the text (in red by default)
+
+   The `color` parameter is only used on characters
+   tagged with DoubleTilde.
 *)
-let format_markdown_section
-    delimiter ?(add_bold = false) ?(color = Tty.Red) section =
-  let style =
-    match delimiter with
-    | Backtick ->
-      if add_bold then
-        Tty.BoldUnderline Tty.Default
+let format_markdown
+    ?(add_bold = false) ?(color = Tty.Red) (parsed : tagged_char list) =
+  let format delimiters s : string =
+    let styles =
+      if DelimiterSet.mem Backtick delimiters then
+        [`Underline]
       else
-        Tty.Underline Tty.Default
-    | Asterisk ->
-      if add_bold then
-        Tty.BoldItalics Tty.Default
+        []
+    in
+    let styles =
+      (* Second condition: if we're supposed to bold the message anyway
+        (e.g. in the main message), then add italics to it (and add bold later below) *)
+      if
+        DelimiterSet.mem Asterisk delimiters
+        || (DelimiterSet.mem DoubleAsterisk delimiters && add_bold)
+      then
+        `Italics :: styles
       else
-        Tty.Italics Tty.Default
-    | DoubleAsterisk ->
-      (* If we're in the main message which will be entirely bolded,
-        then bold italicize it instead *)
-      if add_bold then
-        Tty.BoldItalics Tty.Default
+        styles
+    in
+    let styles =
+      if DelimiterSet.mem DoubleAsterisk delimiters || add_bold then
+        `Bold :: styles
       else
-        Tty.Bold Tty.Default
-    | DoubleTilde ->
-      if add_bold then
-        Tty.Bold color
+        styles
+    in
+    let color =
+      if DelimiterSet.mem DoubleTilde delimiters then
+        color
       else
-        Tty.Normal color
+        Tty.Default
+    in
+    if List.is_empty styles then
+      s
+    else
+      Tty.apply_color_from_style (Tty.style_num_from_list color styles) s
   in
-  Tty.apply_color style section
+  List.group parsed ~break:(fun (set1, _) (set2, _) ->
+      not (DelimiterSet.equal set1 set2))
+  |> List.map ~f:(fun tagged_chars ->
+         let (delimiters, _) = List.hd_exn tagged_chars in
+         let s = List.map tagged_chars ~f:snd |> String.of_char_list in
+         format delimiters s)
+  |> String.concat
 
 let eat_prefix (prefix : string) (state : parse_state) : parse_state option =
-  let (parsed, rest) = state in
+  let { parsed; remaining } = state in
   Option.Monad_infix.(
-    String.chop_prefix rest prefix >>= fun s -> Some (parsed, s))
-
-let rec parse_and_format_until
-    (delimiter : delimiter)
-    ?(add_bold = false)
-    ?(color = Tty.Red)
-    (state : parse_state) : parse_state option =
-  let is_valid_section section =
-    (* Example: "*foo bar*" is a valid section, while
-       "**", "* foo bar*", "*foo bar *", and "* foo bar *" are not.
-       The absence of a leading space is checked before entering this function. *)
-    String.length section > 0 && not (String.is_suffix ~suffix:" " section)
-  in
-  match eat_prefix (delimiter_to_string delimiter) state with
-  | Some (parsed, rest) when is_valid_section parsed ->
-    (* Matched a delimiter over a non-empty section *)
-    Some (format_markdown_section delimiter ~add_bold ~color parsed, rest)
-  | _ ->
-    let (parsed, rest) = state in
-    (match rest with
-    | "" ->
-      (* Nothing left to try *)
-      None
-    | _ ->
-      let (first, tail) = first_and_tail_exn rest in
-      (* Haven't matched a delimiter yet (or did, but the section
-         wasn't valid), and we still have text we can try *)
-      parse_and_format_until
-        delimiter
-        ~add_bold
-        ~color
-        (parsed ^ String.of_char first, tail))
+    String.chop_prefix remaining prefix >>= fun remaining ->
+    Some { parsed; remaining })
 
 (* Success if p s fails, returning original s *)
 let fail (p : parser) (s : parse_state) =
@@ -107,48 +109,73 @@ let fail (p : parser) (s : parse_state) =
   | None -> Some s
   | Some _ -> None
 
-let parse_and_format_section
-    ~(delimiter : delimiter)
-    ~(add_bold : bool)
-    ~(color : Tty.raw_color)
-    (s : parse_state) =
-  Option.Monad_infix.(
-    eat_prefix (delimiter_to_string delimiter) s
-    >>= (eat_prefix " " |> fail)
-    >>= parse_and_format_until delimiter ~add_bold ~color)
-
 (* Try first parser; if it fails, try second with original state *)
 let ( |? ) (p1 : parser) (p2 : parser) (s : parse_state) =
   match p1 s with
   | None -> p2 s
   | Some s -> Some s
 
-let parse_single_char ~add_bold (parsed, rest) =
-  let formatter c =
-    if add_bold then
-      Tty.apply_color (Tty.Bold Tty.Default) c
-    else
-      c
-  in
-  let (first, tail) = first_and_tail_exn rest in
-  let c = String.of_char first |> formatter in
-  Some (parsed ^ c, tail)
+let parse_single_char { parsed; remaining } =
+  let (first, remaining) = first_and_tail_exn remaining in
+  Some { parsed = parsed @ [(DelimiterSet.empty, first)]; remaining }
 
-let rec render ?(add_bold = false) ?(color = Tty.Red) (msg : string) =
+let rec parse_and_format_until (delimiter : delimiter) (state : parse_state) :
+    parse_state option =
+  let is_valid_section section =
+    (* Example: "*foo bar*" is a valid section, while
+       "**", "* foo bar*", "*foo bar *", and "* foo bar *" are not.
+       The absence of a leading space is checked before entering this function. *)
+    List.length section > 0
+    && not (Char.equal ' ' (List.last_exn section |> snd))
+  in
+  match eat_prefix (delimiter_to_string delimiter) state with
+  | Some { parsed; remaining } when is_valid_section parsed ->
+    (* Matched a delimiter over a non-empty section;
+       now try and see if there are nested delimited sections
+       in that larger section. *)
+    let parsed =
+      parse_markdown (List.map ~f:snd parsed |> String.of_char_list)
+      |> List.map ~f:(fun (delims, c) -> (DelimiterSet.add delimiter delims, c))
+    in
+    Some { parsed; remaining }
+  | _ ->
+    let { parsed; remaining } = state in
+    (match remaining with
+    | "" ->
+      (* Nothing left to try *)
+      None
+    | _ ->
+      let (first, remaining) = first_and_tail_exn remaining in
+      (* Haven't matched a delimiter yet (or did, but the section
+         wasn't valid), and we still have text we can try *)
+      parse_and_format_until
+        delimiter
+        { parsed = parsed @ [(DelimiterSet.empty, first)]; remaining })
+
+and parse_and_format_section ~(delimiter : delimiter) (s : parse_state) =
+  Option.Monad_infix.(
+    eat_prefix (delimiter_to_string delimiter) s
+    >>= (eat_prefix " " |> fail)
+    >>= parse_and_format_until delimiter)
+
+and parse_markdown (msg : string) : tagged_char list =
   if String.is_empty msg then
-    ""
+    []
   else
     let parse =
-      parse_and_format_section ~delimiter:DoubleAsterisk ~add_bold ~color
-      |? parse_and_format_section ~delimiter:DoubleTilde ~add_bold ~color
-      |? parse_and_format_section ~delimiter:Asterisk ~add_bold ~color
-      |? parse_and_format_section ~delimiter:Backtick ~add_bold ~color
-      |? parse_single_char ~add_bold
+      parse_and_format_section ~delimiter:DoubleAsterisk
+      |? parse_and_format_section ~delimiter:DoubleTilde
+      |? parse_and_format_section ~delimiter:Asterisk
+      |? parse_and_format_section ~delimiter:Backtick
+      |? parse_single_char
     in
-    match parse ("", msg) with
+    match parse { parsed = []; remaining = msg } with
     | None ->
       failwith "Impossible: unable to parse a single character in error message"
-    | Some (parsed, rest) -> parsed ^ render ~add_bold ~color rest
+    | Some { parsed; remaining } -> parsed @ parse_markdown remaining
+
+let render ?(add_bold = false) ?(color = Tty.Red) (msg : string) =
+  parse_markdown msg |> format_markdown ~add_bold ~color
 
 let md_codify s = "`" ^ s ^ "`"
 

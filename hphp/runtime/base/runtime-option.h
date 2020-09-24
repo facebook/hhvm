f81043cad593e6369080b7717998a899464d3a59
Shaunak Kishore <kshaunak@fb.com>
2020-09-24 11:58:02 -0700

    Clean up some relaxation options
    Summary: These options were used to relax DataTypeCountness, which is now gone. They weren't set elsewhere.
    
    Reviewed By: ottoni
    
    Differential Revision: D23904883
    
    fbshipit-source-id: 63f0ceef9ae0d3ba69a740a03e3d72355f351f5a

--- hphp/runtime/base/runtime-option.h
+++ hphp/runtime/base/runtime-option.h
@@ -931,8 +931,6 @@ struct RuntimeOption {
   F(double,   JitPGOMinArcProbability, 0.0)                             \
   F(uint32_t, JitPGOMaxFuncSizeDupBody, 80)                             \
   F(uint32_t, JitPGORelaxPercent,      100)                             \
-  F(uint32_t, JitPGORelaxUncountedToGenPercent, 20)                     \
-  F(uint32_t, JitPGORelaxCountedToGenPercent, 75)                       \
   F(double,   JitPGOCalledFuncCheckThreshold, 50)                       \
   F(double,   JitPGOCalledFuncExitThreshold,  99.9)                     \
   F(bool,     JitPGODumpCallGraph,     false)                           \

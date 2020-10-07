109cdaed501ee9b8f46873d5fb873f48a18d972e
Oguz Ulgen <oulgen@fb.com>
2020-10-07 11:28:10 -0700

    Turn on RaiseOnCaseInsensitiveLookup again
    Differential Revision: D24167287
    
    fbshipit-source-id: 22c4e0bdfa1c0943780de2aa5b681c7857126abc

--- hphp/runtime/base/runtime-option.h
+++ hphp/runtime/base/runtime-option.h
@@ -728,7 +728,7 @@ struct RuntimeOption {
   F(bool, HHBBCTestCompression,        false)                           \
   F(bool, EnablePerRepoOptions,        true)                            \
   F(bool, CachePerRepoOptionsPath,     true)                            \
-  F(bool, RaiseOnCaseInsensitiveLookup,false)                           \
+  F(bool, RaiseOnCaseInsensitiveLookup,true)                            \
   /* ThrowOnNonExhaustiveSwitch
    * Generates warnings when switch statements are non exhaustive.
    *  0 - Nothing

f91624a9b43cecebd730373f0bfbe391369603e7
Leo Osvald <leoo@fb.com>
2020-10-07 12:22:15 -0700

    Add tests for closures with and without annotations
    Reviewed By: vassilmladenov
    
    Differential Revision: D24040794
    
    fbshipit-source-id: a27d7ba96d2fea7c15f59b1779c42969cb578dc6

---
+++ hphp/hack/test/typecheck/coeffects/closures_explicit.php
@@ -0,0 +1,50 @@
+<?hh
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+
+<<file: __EnableUnstableFeatures('coeffects_provisional')>>
+
+function rx_context()[rx]: void {
+  $more_permissive = ()[rx, non_det] ==> {
+    rx_context(); // ok
+  };
+
+  $less_permissive = ()[pure] ==> {
+    $more_permissive(); // error (missing non_det)
+    rx_context(); // error (pure </: rx) FIXME(coeffects)
+  };
+
+  $equally_permissive = ()[rx] ==> {
+    $less_permissive(); // ok (rx <: pure)
+    rx_context(); // ok
+    $more_permissive(); // error (missing non_det)
+  };
+}
+
+function nondeterministic_context()[non_det]: void {
+  // the type-checker shouldn't close over the non_det capability
+  ()[rx] ==> nondeterministic_context(); // error FIXME(coeffects)
+}
+
+function nesting_test()[]: void {
+  $rx_lambda = ()[rx] ==> {};
+  $least_permissive = ()[] ==> {};
+  $nondeterministic = ()[non_det] ==> {};
+
+  ()[non_det] ==> {
+    $call_lp = ()[] ==> $least_permissive();
+    $call_rx = ()[rx] ==> {
+      $rx_lambda(); // ok FIXME(coeffects)
+      $least_permissive(); // ok
+      $nondeterministic(); // error FIXME(coeffects)
+    };
+    ()[] ==> {
+      $rx_lambda(); // error
+      $call_rx(); // error (verify inner lambda is typed properly)
+
+      $least_permissive(); // ok
+      $call_lp(); // ok (verify inner lambda is typed properly)
+
+      $nondeterministic(); // error
+    };
+  };
+}
---
+++ hphp/hack/test/typecheck/coeffects/closures_implicit.php
@@ -0,0 +1,36 @@
+<?hh
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+
+<<file: __EnableUnstableFeatures('coeffects_provisional')>>
+
+function default_context()[non_rx]: void {}
+
+function rx_context()[rx]: void {
+  // verify the lambda inherits capability rx from the outer function
+  $rx_lambda = () ==> rx_context(); // ok
+  $rx_lambda2 = () ==> default_context(); // error
+}
+
+function implicit_context(): void {
+  // the lambda should have defaults capability
+  $default_lambda = () ==> {
+    implicit_context(); // ok
+    default_context(); // ok
+    rx_context(); // ok (Rx <: Defaults)
+  };
+
+  $rx_lambda = ()[rx] ==> {};
+  $default_lambda = () ==> $rx_lambda(); // ok
+}
+
+function nondeterministic_context()[non_det]: void {
+  // the lambda should be typed as having non_det capability, not defaults
+  () ==> rx_context(); // error
+
+  ()[rx] ==> {
+    // the type-checker shouldn't close over the non_det capability
+    () ==> nondeterministic_context(); // error FIXME(coeffects)
+
+    () ==> rx_context(); // ok (since rx is in the enclosing scope / inherited)
+  };
+}

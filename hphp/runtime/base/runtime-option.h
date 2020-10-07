ba6fbe68bcac89d1096c986b3dd7a9e05c7a7f55
Shaunak Kishore <kshaunak@fb.com>
2020-10-07 09:03:21 -0700

    Revert D24101351: Turn on raising notice on case insensitive FSM lookup
    Differential Revision:
    D24101351 (https://github.com/facebook/hhvm/commit/0fb0e1c373168d09b45f3469493e5574b43154ed)
    
    Original commit changeset: 171da8766781
    
    fbshipit-source-id: 502d49aa8d596bc7cc7efdd2f1af7b3b7d8ce39a

--- hphp/runtime/base/runtime-option.h
+++ hphp/runtime/base/runtime-option.h
@@ -728,7 +728,7 @@ struct RuntimeOption {
   F(bool, HHBBCTestCompression,        false)                           \
   F(bool, EnablePerRepoOptions,        true)                            \
   F(bool, CachePerRepoOptionsPath,     true)                            \
-  F(bool, RaiseOnCaseInsensitiveLookup,true)                            \
+  F(bool, RaiseOnCaseInsensitiveLookup,false)                           \
   /* ThrowOnNonExhaustiveSwitch
    * Generates warnings when switch statements are non exhaustive.
    *  0 - Nothing

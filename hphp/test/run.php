7952b781ce6047bfa1fa5eea3318c56d81fe65cc
Kevin Viratyosin <viratyosin@fb.com>
2020-10-08 11:09:45 -0700

    Avoid provenance messages in hphp/test/run.php
    Summary: When provenance is turned on for cli mode for a devserver (D24167464), the test runner itself is subject to this config and was spewing notices due to the serialization of varrays in `Queue::sendMessage`. These serialized message array is just list-destructured so we can avoid the notices by switching to vecs.
    
    Differential Revision: D24194460
    
    fbshipit-source-id: 2f5ebd22fccc61767b8feffe25b9f5ffb322b843

--- hphp/test/run.php
+++ hphp/test/run.php
@@ -1618,7 +1618,7 @@ class Status {
                              'start_time' => $stime,
                              'end_time' => $etime,
                              'time' => $time];
-    self::send(self::MSG_TEST_PASS, varray[$test, $time, $stime, $etime]);
+    self::send(self::MSG_TEST_PASS, vec[$test, $time, $stime, $etime]);
   }
 
   public static function skip($test, $reason, $time, $stime, $etime) {
@@ -1634,7 +1634,7 @@ class Status {
       'time' => $time,
     ];
     self::send(self::MSG_TEST_SKIP,
-               varray[$test, $reason, $time, $stime, $etime]);
+               vec[$test, $reason, $time, $stime, $etime]);
   }
 
   public static function fail($test, $time, $stime, $etime, $diff) {
@@ -1646,7 +1646,7 @@ class Status {
       'end_time' => $etime,
       'time' => $time
     ];
-    self::send(self::MSG_TEST_FAIL, varray[$test, $time, $stime, $etime]);
+    self::send(self::MSG_TEST_FAIL, vec[$test, $time, $stime, $etime]);
   }
 
   public static function handle_message($type, $message) {

66446e16140f1cd5aadc13674d49b754b3e8141a
Daniel Neiter <dneiter@fb.com>
2020-10-11 12:21:40 -0700

    Support %{date} placeholder in ShardSalt for config overrides
    Summary:
    We have a use-case, where we would like to periodically rotate salt for partial rollout in order to get coverage of on scripts, which only run on specific hosts.
    
    Supporting %{date} takes care of it for use. This ensures, that
    1. subsequent re-runs of the script within short time window use the same salt, which is helpful for debugging issues
    2. salt is rotated daily
    
    I somewhat arbitrary chose YYYY-mm-dd format for the date.
    
    Choice of %{date} placeholder style is consistent with other placeholders, already supported in other parts of config.hdf.
    
    Reviewed By: ricklavoie
    
    Differential Revision: D24238592
    
    fbshipit-source-id: 0569c0aee223fd3556d7cc14dfd68f53d6b3a6cc

--- hphp/runtime/base/runtime-option.cpp
+++ hphp/runtime/base/runtime-option.cpp
@@ -34,6 +34,7 @@
 #include "hphp/runtime/base/request-info.h"
 #include "hphp/runtime/base/static-string-table.h"
 #include "hphp/runtime/base/tv-refcount.h"
+#include "hphp/runtime/base/zend-string.h"
 #include "hphp/runtime/base/zend-url.h"
 #include "hphp/runtime/ext/extension-registry.h"
 #include "hphp/runtime/server/access-log.h"
@@ -1235,6 +1236,16 @@ static void normalizePath(std::string &path) {
   }
 }
 
+static String todayDate() {
+  time_t rawtime;
+  struct tm timeinfo;
+  char buf[256];
+  time(&rawtime);
+  localtime_r(&rawtime, &timeinfo);
+  strftime(buf, sizeof(buf), "%Y-%m-%d", &timeinfo);
+  return buf;
+}
+
 static bool matchShard(
   const std::string& hostname,
   const IniSetting::Map& ini, Hdf hdfPattern,
@@ -1253,7 +1264,9 @@ static bool matchShard(
 
   auto input = hostname;
   if (hdfPattern.exists("ShardSalt")) {
-    input += Config::GetString(ini, hdfPattern, "ShardSalt", "", false);
+    auto salt = Config::GetString(ini, hdfPattern, "ShardSalt", "", false);
+    salt = string_replace(salt, "%{date}", todayDate()).toCppString();
+    input += salt;
   }
 
   auto const md5 = Md5Digest(input.data(), input.size());

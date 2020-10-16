c0ca279ac14decb5cb3a0d833364ce644bac1511
Vincent Siles <vsiles@fb.com>
2020-10-16 07:51:10 -0700

    Adding some runtime tests for the EDT project
    Summary:
    Experimental project: Enum Dependent Types. This test checks that
    - the `enum class` syntax is supported
    - `getValues` behaves as expected (with and without `extends`)
    - `Elt::name()` returns the right string
    
    Reviewed By: shayne-fletcher
    
    Differential Revision: D24302805
    
    fbshipit-source-id: 5de546f6362466b65130240ba6557b45788a615a

---
+++ hphp/test/slow/enum_class/enum_class09.php
@@ -0,0 +1,55 @@
+<?hh
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+
+<<file: __EnableUnstableFeatures('enum_class')>>
+
+interface ExBox {}
+
+class Box<T> implements ExBox {
+  public function __construct(public T $data) {}
+}
+
+class IBox extends Box<int> {
+  public function add(int $x) : void { $this->data = $this->data + $x; }
+}
+
+enum class E : ExBox {
+  A<Box<string>>(new Box('zuck'));
+  B<IBox>(new IBox(42));
+}
+
+enum class F : ExBox extends E {
+  C<Box<num>>(new Box(3.14));
+}
+
+<<__EntryPoint>>
+function main(): void {
+  echo E::A->unwrap()->data;
+  echo "\n";
+
+  echo "Testing E's getValues()\n";
+  foreach (E::getValues() as $key => $value) {
+    echo "$key = ";
+    $box = $value->unwrap() as Box<_>;
+    echo $box->data;
+    echo "\n";
+  }
+
+  echo "Testing F\n";
+  echo F::A->unwrap()->data;
+  echo "\n";
+  echo F::C->unwrap()->data;
+  echo "\n";
+  echo "Testing F's getValues()\n";
+  foreach (F::getValues() as $key => $value) {
+    echo "$key = ";
+    $box = $value->unwrap() as Box<_>;
+    echo $box->data;
+    echo "\n";
+  }
+
+  echo "Testing ->name()\n";
+  echo "F::C->name() = ";
+  echo F::C->name();
+  echo "\n";
+}

d787bc8e705c39f56fad273e5c5b54d428dd9663
Thomas Jiang <thomasjiang@fb.com>
2020-10-23 12:29:02 -0700

    Script to update definitions in test files
    Summary:
    I don't want to re-update all of the definitions in all of the test files manually.
    
    So I wrote a very hacky python script that does it for me.
    
    All you need to do is update `expr_tree_defs.php` and then:
    
    ```
    cd ~/fbsource/fbcode/hphp/hack/test/typecheck/expression_trees && python ~/fbsource/fbcode/hphp/hack/test/typecheck/expression_trees/write_defs.py
    ```
    
    We don't have to commit this. But in case you want to use it, here's a little record of it.
    
    Reviewed By: Wilfred
    
    Differential Revision: D24466756
    
    fbshipit-source-id: 41d4eebdd3f03ab6e719a522a4c7adff50ca8c25

---
+++ hphp/hack/test/typecheck/expression_trees/write_defs.py
@@ -0,0 +1,44 @@
+import os
+
+
+def main():
+    # Load the definitions from expr_tree_defs.php
+    def_file = "expr_tree_defs.php"
+    def_file_handle = open(def_file, "r")
+    def_file_read = def_file_handle.readlines()
+    defs = ""
+    copy = False
+    for line in def_file_read:
+        if line.find("BEGIN DEFS") != -1:
+            copy = True
+        if copy:
+            defs += line
+        if line.find("END DEFS") != -1:
+            copy = False
+    def_file_handle.close()
+
+    # Go through the remaining files in expression_trees/
+    curr_dir = os.getcwd()
+    for entry in os.listdir(curr_dir):
+        if (entry.endswith(".php")) and entry != def_file:
+            # Rewrite the file
+            entry_handle = open(entry, "r")
+            entry_read = entry_handle.readlines()
+            new_entry = ""
+            copy = True
+            for line in entry_read:
+                if line.find("BEGIN DEFS") != -1:
+                    copy = False
+                    new_entry += defs
+                if copy:
+                    new_entry += line
+                if line.find("END DEFS") != -1:
+                    copy = True
+            entry_handle.close()
+            entry_handle = open(entry, "w")
+            entry_handle.write(new_entry)
+            entry_handle.close()
+
+
+if __name__ == "__main__":
+    main()

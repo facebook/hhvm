5770d24f1fd6086151d120283a6033dbe82053b4
Michael Colavita <mcolavita@fb.com>
2020-11-02 09:21:31 -0800

    Fix BespokeGet memory effects
    Summary: BespokeGet cannot be marked as a PureLoad because it can potentially branch. In the future, we plan to make BespokeGet a PureLoad and factor the branching behavior into separate IR ops. For now, this fixes a series of irgen problems.
    
    Differential Revision: D24677146
    
    fbshipit-source-id: 0cd2178381f8ac2f1cd76b058ebba01644f22b82

--- hphp/runtime/vm/jit/memory-effects.cpp
+++ hphp/runtime/vm/jit/memory-effects.cpp
@@ -1068,14 +1068,16 @@ MemEffects memory_effects_impl(const IRInstruction& inst) {
   }
 
   case BespokeGet: {
+    // TODO(mcolavita): This should be PureLoad once it no longer branches
     auto const base = inst.src(0);
     auto const key  = inst.src(1);
     if (key->isA(TInt)) {
-      return PureLoad {
-        key->hasConstVal() ? AElemI { base, key->intVal() } : AElemIAny
-      };
+      return may_load_store(
+        key->hasConstVal() ? AElemI { base, key->intVal() } : AElemIAny,
+        AEmpty
+      );
     } else {
-      return PureLoad { AElemAny };
+      return may_load_store(AElemAny, AEmpty);
     }
   }
 

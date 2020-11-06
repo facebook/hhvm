80fcc7cc88070f8c3cc2fd7bfc66e8166434f722
Ben Harrison <bnh@fb.com>
2020-11-06 13:20:18 -0800

    Repair the "mysql_escape_string" test.
    Reviewed By: ricklavoie
    
    Differential Revision: D24678282
    
    fbshipit-source-id: 85519a0809c2df965cd206441ed838706fd8830e

--- hphp/test/zend/good/ext/mysql/tests/mysql_escape_string.php
+++ hphp/test/zend/good/ext/mysql/tests/mysql_escape_string.php
@@ -1,13 +1,7 @@
 <?hh
 
-include "connect.inc";
-
 <<__EntryPoint>>
 function main_entry(): void {
-
-  $tmp    = NULL;
-  $link   = NULL;
-
   try { mysql_escape_string(); } catch (Exception $e) { echo "\n".'Warning: '.$e->getMessage().' in '.__FILE__.' on line '.__LINE__."\n"; }
 
   var_dump(@mysql_escape_string("Am I a unicode string in PHP 6?"));
@@ -18,5 +12,5 @@ function main_entry(): void {
   var_dump(@mysql_escape_string("\r"));
   var_dump(@mysql_escape_string("foo" . chr(0) . "bar"));
 
-  print "done!";
+  print "done!\n";
 }

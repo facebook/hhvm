975b5c86d0e6012d926b6a9fdc7f96ee29e70d03
Steve Cao <shiqicao@fb.com>
2020-11-16 16:04:22 -0800

    remove decl_list
    Differential Revision: D24986208
    
    fbshipit-source-id: 38bc31ac42780d977ebc34829cc94734a41b7f4e

--- hphp/hack/src/decl/direct_decl_parser.ml
+++ hphp/hack/src/decl/direct_decl_parser.ml
@@ -7,14 +7,6 @@
 
 type decls = (string * Shallow_decl_defs.decl) list [@@deriving show]
 
-type decl_lists = {
-  dl_classes: (string * Shallow_decl_defs.shallow_class) list;
-  dl_funs: (string * Typing_defs.fun_elt) list;
-  dl_typedefs: (string * Typing_defs.typedef_type) list;
-  dl_consts: (string * Typing_defs.const_decl) list;
-  dl_records: (string * Typing_defs.record_def_type) list;
-}
-
 type ns_map = (string * string) list
 
 external parse_decls_and_mode_ffi :
--- hphp/hack/src/decl/direct_decl_parser.mli
+++ hphp/hack/src/decl/direct_decl_parser.mli
@@ -8,14 +8,6 @@
 
 type decls = (string * Shallow_decl_defs.decl) list [@@deriving show]
 
-type decl_lists = {
-  dl_classes: (string * Shallow_decl_defs.shallow_class) list;
-  dl_funs: (string * Typing_defs.fun_elt) list;
-  dl_typedefs: (string * Typing_defs.typedef_type) list;
-  dl_consts: (string * Typing_defs.const_decl) list;
-  dl_records: (string * Typing_defs.record_def_type) list;
-}
-
 type ns_map = (string * string) list
 
 val parse_decls_ffi : Relative_path.t -> string -> ns_map -> decls

da1cf6a2281efa75e7d80dc0026d21831900aff9
Catherine Gasnier <catg@fb.com>
2020-12-01 02:08:44 -0800

    add Decl_hint interface
    Reviewed By: vsiles
    
    Differential Revision: D25218987
    
    fbshipit-source-id: e3ae4c08d304c0aef7199ac99254033ba0aec041

---
+++ hphp/hack/src/decl/decl_hint.mli
@@ -0,0 +1,15 @@
+(*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the "hack" directory of this source tree.
+ *
+ *)
+
+val hint : Decl_env.env -> Aast.hint -> Typing_defs.decl_ty
+
+val aast_tparam_to_decl_tparam :
+  Decl_env.env -> Nast.tparam -> Typing_defs.decl_ty Typing_defs.tparam
+
+val aast_user_attribute_to_decl_user_attribute :
+  Nast.user_attribute -> Typing_defs.user_attribute

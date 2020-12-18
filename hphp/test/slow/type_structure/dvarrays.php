2a061be48024d78b7c6838a4110340a002ff7c79
Shaunak Kishore <kshaunak@fb.com>
2020-12-18 09:15:51 -0800

    Add unit test for typehint reflection
    Summary:
    We've been saying all along that typehints accessed via type_structure or ReflectionClass won't change with The Big Switch, but I realized that I don't know of a test for that. Here it is.
    
    We don't need to explicitly enable HADVAs for it - we have a separate HADVAs contbuild.
    
    Reviewed By: arnabde03
    
    Differential Revision: D25638669
    
    fbshipit-source-id: f2d9ed09f999ba099ae8a4c2dc52deffd97f5e18

---
+++ hphp/test/slow/type_structure/dvarrays.php
@@ -0,0 +1,40 @@
+<?hh
+
+class C {
+  const type D_ = darray<string, bool>;
+  const type V_ = varray<int>;
+  const type DV = varray_or_darray<float>;
+  const type SH = shape('a' => int, 'b' => string);
+  const type TU = (int, string);
+
+  public function d_(): darray<string, bool> { return darray[]; }
+  public function v_(): varray<int> { return varray[]; }
+  public function dv(): varray_or_darray<float> { return varray[]; }
+  public function sh(): shape(?'a' => int, ?'b' => string) { return shape(); }
+  public function tu(): (int, string) { return tuple(17, 'x'); }
+}
+
+function show($x) {
+  print(json_encode($x, JSON_FB_FORCE_HACK_ARRAYS)."\n");
+}
+
+<<__EntryPoint>>
+function main() {
+  print("type_structure:\n");
+  show(type_structure(C::class, 'D_'));
+  show(type_structure(C::class, 'V_'));
+  show(type_structure(C::class, 'DV'));
+  show(type_structure(C::class, 'SH'));
+  show(type_structure(C::class, 'TU'));
+
+  $cls = new ReflectionClass('C');
+  print("\nReflection: Methods:\n");
+  foreach ($cls->getMethods() as $method) {
+    print($method->getName().': '.$method->getReturnTypeText()."\n");
+  }
+  print("\nReflection: TypeConstants:\n");
+  foreach ($cls->getTypeConstants() as $cns) {
+    print($cns->getName().': '.$cns->getAssignedTypeText()." =>  ");
+    show($cns->getTypeStructure());
+  }
+}

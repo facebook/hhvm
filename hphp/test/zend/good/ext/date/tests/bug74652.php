21ee3f8e3fe2eb274f575c3817d6a95b823fe378
Patrick Liu <patrickliu@fb.com>
2021-01-08 16:34:41 -0800

    bump up parser version from v1 to v2
    Summary:
    Only bump up parser version from v1 to v2. No change on tzdata and we still use 2020a tzdata. parser version change of timelib was done in D25191333
    
    Let us roll out timelib update in 3 phases:
    
    phase 1: coordinate with www to run some test for D25338654. The diff it does not include all the changes to make timelib up to date with the github source repo. It only includes the minimal changes required to change parser version from v1 to v2. After www tests pass, we can land D25338654.
    
    phase 2: Make timelib entirely up to date with the source repo. The diff is D25352772.
    
    phase 3: update the tzdata.
    
    Why we have phase 1?
    -> Phase 2 is still not completed and phase 1 is done. We can check in phase 1 first. This allows us to use any new tzdata releases from IANA directly. And in the meantime, we can develop phase 2.
    
    If we do not have phase 1, phase 2 is blocker for phase 3:
    phase 2 -> phase 3
    
    Now we have phase 1, phase 2 is not a blocker:
    phase 1 -> phase 3
    
    Implementation work in phase 1 has been completed. I have tested it using admon, adEventJoiner, adFinder canaries, and HACK unit tests.
    phase 2 requires longer period for development. Without phase 1, we cannot use the newly released tzdata from IANA until phase 2 is done.
    
    Reviewed By: jaimedp
    
    Differential Revision: D25338654
    
    fbshipit-source-id: c8609f69afbf117177ec65eaf43ef987939091c9

---
+++ hphp/test/zend/good/ext/date/tests/bug74652.php
@@ -0,0 +1,19 @@
+<?hh
+<<__EntryPoint>> function main(): void {
+date_default_timezone_set('UTC');
+$formats = varray[
+    '2017-03-25 10:52:09',
+    '2017-03-25 10:52',
+    '2017-03-25 10am',
+    '2017-03-25',
+    '2017-03',
+    '2017.042',
+    '2017043',
+];
+
+foreach ( $formats as $format )
+{
+    $dt = new DateTimeImmutable( $format );
+    echo $dt->format( 'Y-m-d H:i:s' ), "\n";
+}
+}

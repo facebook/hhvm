3e06bd3884385d363415796745353d83fbef4125
Mistral Orhan Jean-Pierre Contrastin <mojpc2@fb.com>
2021-01-20 05:41:55 -0800

    Move hh rage implementation
    Summary: `hh rage` is not meaningful in the open source build, so it is now a no-op.
    
    Reviewed By: ljw1004
    
    Differential Revision: D25945475
    
    fbshipit-source-id: 905abc0f2f7bf5f4c1001052fb483f35ca513fbf

---
+++ hphp/hack/src/stubs/clientRage.ml
@@ -0,0 +1,16 @@
+(*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the "hack" directory of this source tree.
+ *
+ *)
+
+type env = {
+  root: Path.t;
+  from: string;
+  rageid: string option;
+  desc: string;
+}
+
+let main _ = Exit.exit Exit_status.No_error

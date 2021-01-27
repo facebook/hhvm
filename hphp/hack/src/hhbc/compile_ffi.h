c236cb7210709cd74924e98bee00569f5b63e463
Shayne Fletcher <shaynefletcher@fb.com>
2021-01-27 08:06:57 -0800

    Implement a Rust FFI for embedding bytecode generation in C++
    Summary:
    Bytecode generation entry point `compile_ffi::compile_from_text_cpp_ffi(...)` and a driver to exercise it:  `hh_single_compile_cpp`.
    
    What we get here is a C FFI for generating HHAS from source text. We can tweak the interface as we need to going forward but this will do for a first version and I think has roughly feature parity with the OCaml FFI:
    - `compile_ffi.rs` defines the function;
    - `compile_ffi.h` defines the C interface;
    - `hh_single_compile_cpp.cpp` provides a C++ CLI that calls the function.
    
    Reviewed By: shiqicao
    
    Differential Revision: D25979967
    
    fbshipit-source-id: 4b46f9af23c61150dda6c33f9fa14e2c455c54c2

---
+++ hphp/hack/src/hhbc/compile_ffi.h
@@ -0,0 +1,54 @@
+/* Copyright (c) 2021, Facebook, Inc.
+ All rights reserved.
+
+ This source code is licensed under the MIT license found in the
+ LICENSE file in the "hack" directory of this source tree.
+*/
+#if !defined(COMPILE_FFI_H)
+#  define COMPILE_FFI_H
+
+#  include<stdint.h>
+#  include<stddef.h>
+
+enum env_flags {
+    IS_SYSTEMLIB=1 << 0
+  , IS_EVALED=1 << 1
+  , FOR_DEBUGGER_EVAL=1 << 2
+  , DUMP_SYMBOL_REFS=1 << 3
+  , DISABLE_TOPLEVEL_ENUMERATION=1 << 4
+};
+
+struct environment {
+  char const* filepath;
+  char const* const* config_jsons;
+  size_t num_config_jsons;
+  char const* const* config_list;
+  size_t num_config_list;
+  uint8_t flags;
+};
+
+struct output_config {
+  bool include_header;
+  char const* output_file;
+};
+
+struct buf_t {
+  char* buf;
+  int buf_siz;
+};
+
+#  if defined(__cplusplus)
+extern "C" {
+#  endif /*defined(__cplusplus)*/
+char const* compile_from_text_cpp_ffi(
+       environment const* env
+     , char const* source_text
+     , output_config const* config
+     , buf_t* error_buf );
+
+void compile_free_string_cpp_ffi(char const*);
+#  if defined(__cplusplus)
+}
+#  endif /*defined(__cplusplus)*/
+
+#endif/*!defined(COMPILE_FFI_H)*/
--- hphp/hack/src/hhbc/compile_ffi.rs
+++ hphp/hack/src/hhbc/compile_ffi.rs
@@ -5,7 +5,7 @@
 // LICENSE file in the "hack" directory of this source tree.
 
 use compile_rust as compile;
-use ocamlrep::FromOcamlRep;
+use ocamlrep::{rc::RcOc, FromOcamlRep};
 use ocamlrep_derive::FromOcamlRep;
 use ocamlrep_ocamlpool::to_ocaml;
 use oxidized::relative_path::RelativePath;
@@ -16,8 +16,11 @@ use anyhow::{anyhow, Result};
 use serde_json::{map::Map, value::Value};
 use std::io::Write;
 
+use libc::{c_char, c_int};
+use log::warn;
+
 #[derive(Debug, FromOcamlRep)]
-struct RustOutputConfig {
+pub struct RustOutputConfig {
     include_header: bool,
     output_file: Option<String>,
 }
@@ -35,6 +38,175 @@ impl<'content> FromOcamlRep for OcamlStr<'content> {
         Ok(Self(unsafe {
             std::mem::transmute(ocamlrep::bytes_from_ocamlrep(value)?)
         }))
+    }
+}
+
+// C `char*` conversions.
+mod cstr {
+    use libc::{c_char, c_int};
+
+    // `char*` to `&[u8]`.
+    pub fn to_u8<'a>(s: *const c_char) -> &'a [u8] {
+        unsafe { std::slice::from_raw_parts(s as *const u8, libc::strlen(s)) }
+    }
+    // `char*` to `&mut [u8]`.
+    pub fn to_mut_u8<'a>(buf: *mut c_char, buf_len: c_int) -> &'a mut [u8] {
+        unsafe { std::slice::from_raw_parts_mut(buf as *mut u8, buf_len as usize) }
+    }
+    // `char*` to `&str`.
+    pub fn to_str<'a>(s: *const c_char) -> &'a str {
+        unsafe { std::str::from_utf8_unchecked(to_u8(s)) }
+    }
+    // `char**` to `Vec<&str>`.
+    pub fn to_vec<'a>(cstrs: *const *const c_char, num_cstrs: usize) -> std::vec::Vec<&'a str> {
+        unsafe {
+            std::slice::from_raw_parts(cstrs, num_cstrs)
+                .iter()
+                .map(|&s| to_str(s))
+                .collect()
+        }
+    }
+}
+
+#[repr(C)]
+struct COutputConfig {
+    include_header: bool,
+    output_file: *const c_char,
+}
+impl std::convert::From<&COutputConfig> for RustOutputConfig {
+    fn from(output_config: &COutputConfig) -> RustOutputConfig {
+        RustOutputConfig {
+            include_header: output_config.include_header,
+            output_file: if output_config.output_file.is_null() {
+                None
+            } else {
+                Some(String::from(cstr::to_str(output_config.output_file)))
+            },
+        }
+    }
+}
+
+#[repr(C)]
+struct CEnv {
+    filepath: *const c_char,
+    config_jsons: *const *const c_char,
+    num_config_jsons: usize,
+    config_list: *const *const c_char,
+    num_config_list: usize,
+    flags: u8,
+}
+impl<'a> std::convert::From<&CEnv> for compile::Env<&'a str> {
+    fn from(env: &CEnv) -> compile::Env<&'a str> {
+        compile::Env {
+            filepath: RelativePath::make(
+                oxidized::relative_path::Prefix::Dummy,
+                std::path::PathBuf::from(cstr::to_str(env.filepath)),
+            ),
+            config_jsons: cstr::to_vec(env.config_jsons, env.num_config_jsons),
+            config_list: cstr::to_vec(env.config_list, env.num_config_list),
+            flags: compile::EnvFlags::from_bits(env.flags).unwrap(),
+        }
+    }
+}
+
+#[repr(C)]
+pub struct CBuf {
+    buf: *mut c_char,
+    buf_len: c_int,
+}
+
+// Utility for raw pointer conversions.
+fn from_ptr<'a, T: 'a, U, F: FnOnce(&'a T) -> U>(p: usize, f: F) -> std::option::Option<U> {
+    match p {
+        0 => None,
+        _ => Some(f(unsafe { &*(p as *const T) })),
+    }
+}
+
+// Return a string to Rust.
+#[no_mangle]
+extern "C" fn compile_free_string_cpp_ffi(s: *mut c_char) {
+    let _ = unsafe { std::ffi::CString::from_raw(s) };
+}
+
+// Compile to HHAS from source text.
+#[no_mangle]
+extern "C" fn compile_from_text_cpp_ffi(
+    env: usize,
+    source_text: *const c_char,
+    output_cfg: usize,
+    err_buf: usize,
+) -> *const c_char {
+    let err_buf: &CBuf = from_ptr(err_buf, std::convert::identity).unwrap();
+    let buf_len: c_int = err_buf.buf_len;
+    let buf: &mut [u8] = cstr::to_mut_u8(err_buf.buf, buf_len);
+    let _output_config: RustOutputConfig = from_ptr(output_cfg, RustOutputConfig::from).unwrap();
+    let text: &[u8] = cstr::to_u8(source_text);
+
+    let job_builder = move || {
+        move |stack_limit: &StackLimit, _nomain_stack_size: Option<usize>| {
+            let env = from_ptr(env, compile::Env::<&str>::from).unwrap();
+            let source_text = SourceText::make(RcOc::new(env.filepath.clone()), text);
+            let mut w = String::new();
+            match compile::from_text_(&env, stack_limit, &mut w, source_text) {
+                Ok(_) => {
+                    //print_output(w, output_config, &env.filepath, profile)?;
+                    Ok(w)
+                }
+                Err(e) => Err(anyhow!("{}", e)),
+            }
+        }
+    };
+    // Assume peak is 2.5x of stack. This is initial estimation, need
+    // to be improved later.
+    let stack_slack = |stack_size| stack_size * 6 / 10;
+    let on_retry = &mut |stack_size_tried: usize| {
+        // Not always printing warning here because this would fail
+        // some HHVM tests.
+        if atty::is(atty::Stream::Stderr) || std::env::var_os("HH_TEST_MODE").is_some() {
+            let env = from_ptr(env, compile::Env::<&str>::from).unwrap();
+            eprintln!(
+                "[hrust] warning: compile_from_text_ffi exceeded stack of {} KiB on: {}",
+                (stack_size_tried - stack_slack(stack_size_tried)) / KI,
+                env.filepath.path_str(),
+            );
+        }
+    };
+    let job = stack_limit::retry::Job {
+        nonmain_stack_min: 13 * MI,
+        // TODO(hrust) aast_parser_ffi only requies 1 * GI, it's like
+        // rust compiler produce inconsistent binary.
+        nonmain_stack_max: Some(7 * GI),
+        ..Default::default()
+    };
+
+    let r: Result<String, String> = job
+        .with_elastic_stack(job_builder, on_retry, stack_slack)
+        .map_err(|e| format!("{}", e))
+        .expect("compile_ffi: compile_from_text_cpp_ffi: retry failed")
+        .map_err(|e| e.to_string());
+    match r {
+        Ok(out) => {
+            let cs = std::ffi::CString::new(out)
+                .expect("compile_ffi: compile_from_text_cpp_ffi: String::new failed");
+            cs.into_raw() as *const c_char
+        }
+        Err(e) => {
+            if e.len() >= buf.len() {
+                warn!("Provided error buffer too is too small.");
+                warn!(
+                    "Expected at least {} bytes but got {}",
+                    e.len() + 1,
+                    buf.len()
+                );
+            } else {
+                unsafe {
+                    std::ptr::copy_nonoverlapping(e.as_ptr(), buf.as_mut_ptr(), e.len())
+                };
+                buf[e.len()] = 0;
+            }
+            std::ptr::null()
+        }
     }
 }
 

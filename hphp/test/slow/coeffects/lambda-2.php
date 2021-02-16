45d46dddbdd424d9f1c8521fb134d3c5517d1fbc
Andre Nash <alnash@fb.com>
2021-02-16 14:34:53 -0800

    Add more complicated unit tests for lambda coeffects
    Summary:
    The previous diff added a very simple test because warning/enforcement is disabled by default. This diff was supposed to be included but it was yanked out because the tests randomly failed.
    
    It wasn't random - it failed in repo mode and passed otherwise. The two tests that failed were the first two tests of this patch.
    
    It appears that because the function body for `regular` was empty, hhbbc optimized away the function calls. This was the exact issue with the test (the first two expectations were never met).
    
    This hacks in a way to avoid this by happening - by adding a function with a side-effect that cannot be optimized.
    
    Reviewed By: oulgen
    
    Differential Revision: D26384027
    
    fbshipit-source-id: 2e26f46e86c939f6524dffe547f28b73114b5f49

---
+++ hphp/test/slow/coeffects/lambda-2.php
@@ -0,0 +1,41 @@
+<?hh
+
+<<__NEVER_INLINE>> function regular() { } // defaults
+
+<<__NEVER_INLINE>> function regular_writeprops()[write_props] { }
+
+<<__NEVER_INLINE>> function f($f)[rx] { $f(); }
+
+function test(int $times): void {
+  switch ($times) {
+    case 0:
+      // here, the failure is in the lambda - it is `pure` and `pure` cannot
+      // call defaults
+      f(()[] ==> { regular(); });
+      break;
+    case 1:
+      // here, f is `rx` and the lambda is also `rx`, but this fails because
+      // the lambda can't call `defaults`
+      f(()[rx] ==> { regular(); });
+      break;
+    case 2:
+      // f is `rx` and the lambda inherits coeffects from `test` and is
+      // therefore `defaults`, so when f (`rx`) attempts to call
+      // `defaults`, it raises a violation
+      f(() ==> { regular(); });
+      break;
+    case 3:
+      // f is `rx`, lambda is `write_props`, so f cannot call lambda
+      f(()[write_props] ==> { regular_writeprops(); });
+      break;
+    default:
+      break;
+  }
+}
+
+<<__EntryPoint>>
+function main() {
+  for ($ctr=0; $ctr < 4; ++$ctr) {
+    test($ctr);
+  }
+}

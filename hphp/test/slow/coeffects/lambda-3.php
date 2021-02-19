0f2148da0db3b533f70b7c07667804791592a443
Oguz Ulgen <oulgen@fb.com>
2021-02-19 14:30:36 -0800

    Add coeffect tests for lambda violation
    Summary: more test
    
    Reviewed By: jano
    
    Differential Revision: D26518060
    
    fbshipit-source-id: 647c73b327d977472bf8dd83e11bdbdd3a905d42

---
+++ hphp/test/slow/coeffects/lambda-3.php
@@ -0,0 +1,25 @@
+<?hh
+
+function rx_local($f) {
+  $f();
+}
+
+function pure($f)[] {
+  $f();
+};
+
+<<__EntryPoint>>
+function main() {
+  $f1 = () ==> { echo "in lambda\n"; };
+  $f2 = ()[] ==> { echo "in lambda\n"; };
+  $f3 = ()[rx] ==> { echo "in lambda\n"; };
+  $f4 = ()[rx_local] ==> { echo "in lambda\n"; };
+  pure($f1);
+  pure($f2);
+  pure($f3);
+  pure($f4);
+  rx_local($f1);
+  rx_local($f2);
+  rx_local($f3);
+  rx_local($f4);
+}

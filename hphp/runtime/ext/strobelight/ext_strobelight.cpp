bb8e8b4d13bf7590cbe4d28ff0a5acee04b2d9bf
Francis Ricci <fjricci@fb.com>
2021-02-22 12:03:32 -0800

    Factor out variable logging in Strobelight extension
    Summary: This consolidates some duplicated code.
    
    Reviewed By: binliu19
    
    Differential Revision: D26514264
    
    fbshipit-source-id: 2658ab2275592134f5097110ea1526457b00ab5f

--- hphp/runtime/ext/strobelight/ext_strobelight.cpp
+++ hphp/runtime/ext/strobelight/ext_strobelight.cpp
@@ -90,7 +90,6 @@ bool logToUSDT(const Array& bt) {
   IterateVNoInc(
     bt.get(),
     [&](TypedValue tv) -> bool {
-
       if (i >= strobelight::kMaxStackframes) {
         return true;
       }
@@ -105,41 +104,22 @@ bool logToUSDT(const Array& bt) {
         frame->line = val(line).num;
       }
 
-      auto const file_name = bt_frame->get(s_file.get());
-      if (file_name.is_init()) {
-        assertx(isStringType(type(file_name)));
-        strncpy(frame->file_name,
-                val(file_name).pstr->data(),
-                std::min<int64_t>(
-                  val(file_name).pstr->size(),
-                  strobelight::kFileNameMax
-                ));
-        frame->file_name[strobelight::kFileNameMax - 1] = '\0';
-      }
+      auto addString = [bt_frame](const StaticString& var,
+                                  char* dest,
+                                  int32_t maxLen) {
+        auto const value = bt_frame->get(var.get());
+        if (value.is_init()) {
+          assertx(isStringType(type(value)));
+          strncpy(dest,
+                  val(value).pstr->data(),
+                  std::min<int64_t>(val(value).pstr->size(), maxLen));
+          dest[maxLen - 1] = '\0';
+        }
+      };
 
-      auto const class_name = bt_frame->get(s_class.get());
-      if (class_name.is_init()) {
-        assertx(isStringType(type(class_name)));
-        strncpy(frame->class_name,
-                val(class_name).pstr->data(),
-                std::min<int64_t>(
-                  val(class_name).pstr->size(),
-                  strobelight::kClassNameMax
-                ));
-        frame->class_name[strobelight::kClassNameMax - 1] = '\0';
-      }
-
-      auto const function_name = bt_frame->get(s_function.get());
-      if (function_name.is_init()) {
-        assertx(isStringType(type(function_name)));
-        strncpy(frame->function,
-                val(function_name).pstr->data(),
-                std::min<int64_t>(
-                  val(function_name).pstr->size(),
-                  strobelight::kFunctionMax
-                ));
-        frame->function[strobelight::kFunctionMax - 1] = '\0';
-      }
+      addString(s_file, frame->file_name, strobelight::kFileNameMax);
+      addString(s_class, frame->class_name, strobelight::kClassNameMax);
+      addString(s_function, frame->function, strobelight::kFunctionMax);
 
       i++;
       return false;

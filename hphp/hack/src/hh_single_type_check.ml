29fd9cdd43f6ad2b00b214ab5dcac12ed8e2e573
Jake Bailey (Hacklang) <jakebailey@fb.com>
2021-03-01 12:49:50 -0800

    Add --get-member mode to hh_single_type_check
    Summary: I've been using this to diagnose problems with shallow class decl in WWW where I'd like to know more details about specific class members (in particular, where they were inherited from). It seems like it might be generally useful, so perhaps we should land it.
    
    Reviewed By: vsiles
    
    Differential Revision: D26677922
    
    fbshipit-source-id: db060fd318c6710644394ffceaf63480c9d8e71a

--- hphp/hack/src/hh_single_type_check.ml
+++ hphp/hack/src/hh_single_type_check.ml
@@ -13,6 +13,7 @@ open String_utils
 open Sys_utils
 open Typing_env_types
 module Inf = Typing_inference_env
+module Cls = Decl_provider.Class
 
 module StringAnnotation = struct
   type t = string
@@ -43,6 +44,7 @@ type mode =
   | Dump_deps
   | Identify_symbol of int * int
   | Find_local of int * int
+  | Get_member of string
   | Outline
   | Dump_nast
   | Dump_stripped_tast
@@ -446,6 +448,12 @@ let parse_options () =
       ( "--unsafe-rx",
         Arg.Unit (set_bool unsafe_rx),
         " Disables reactivity related errors" );
+      ( "--get-member",
+        Arg.String
+          (fun class_and_member_id ->
+            set_mode (Get_member class_and_member_id) ()),
+        " Given ClassName::MemberName, fetch the decl of members with that name and print them."
+      );
       ( "--mro",
         Arg.Unit (set_mode Linearization),
         " Grabs the linearization of all classes in a file." );
@@ -664,9 +672,10 @@ let parse_options () =
   let options = Arg.align ~limit:25 options in
   Arg.parse options (fun fn -> fn_ref := fn :: !fn_ref) usage;
   let fns =
-    match !fn_ref with
-    | [] -> die usage
-    | x -> x
+    match (!fn_ref, !mode) with
+    | ([], Get_member _) -> []
+    | ([], _) -> die usage
+    | (x, _) -> x
   in
   let is_ifc_mode =
     match !mode with
@@ -1984,6 +1993,116 @@ let handle_mode
     print_errors_if_present parse_errors;
     let filename = expect_single_file () in
     test_shallow_class_diff ctx filename
+  | Get_member class_and_member_id ->
+    let (cid, mid) =
+      match Str.split (Str.regexp "::") class_and_member_id with
+      | [cid; mid] -> (cid, mid)
+      | _ ->
+        failwith
+          (Printf.sprintf "Invalid --get-member ID: %S" class_and_member_id)
+    in
+    let cid = Utils.add_ns cid in
+    (match Decl_provider.get_class ctx cid with
+    | None -> Printf.printf "No class named %s\n" cid
+    | Some cls ->
+      let ty_to_string ty =
+        let env = Typing_env.empty ctx Relative_path.default ~droot:None in
+        Typing_print.full_strip_ns_decl env ty
+      in
+      let print_class_element member_type get mid =
+        match get cls mid with
+        | None -> ()
+        | Some ce ->
+          let abstract =
+            if Typing_defs.get_ce_abstract ce then
+              "abstract "
+            else
+              ""
+          in
+          let origin = ce.Typing_defs.ce_origin in
+          let from =
+            if String.equal origin cid then
+              ""
+            else
+              Printf.sprintf " from %s" (Utils.strip_ns origin)
+          in
+          Printf.printf
+            "  %s%s%s: %s\n"
+            abstract
+            member_type
+            from
+            (ty_to_string (Lazy.force ce.Typing_defs.ce_type))
+      in
+      Printf.printf "%s::%s\n" cid mid;
+      print_class_element "method" Cls.get_method mid;
+      print_class_element "static method" Cls.get_smethod mid;
+      print_class_element "property" Cls.get_prop mid;
+      print_class_element "static property" Cls.get_sprop mid;
+      print_class_element "static property" Cls.get_sprop ("$" ^ mid);
+      (match Cls.get_const cls mid with
+      | None -> ()
+      | Some cc ->
+        let abstract =
+          if cc.Typing_defs.cc_abstract then
+            "abstract "
+          else
+            ""
+        in
+        let origin = cc.Typing_defs.cc_origin in
+        let from =
+          if String.equal origin cid then
+            ""
+          else
+            Printf.sprintf " from %s" (Utils.strip_ns origin)
+        in
+        let ty = ty_to_string cc.Typing_defs.cc_type in
+        Printf.printf "  %sconst%s: %s\n" abstract from ty);
+      (match Cls.get_typeconst cls mid with
+      | None -> ()
+      | Some ttc ->
+        let origin = ttc.Typing_defs.ttc_origin in
+        let from =
+          if String.equal origin cid then
+            ""
+          else
+            Printf.sprintf " from %s" (Utils.strip_ns origin)
+        in
+        let ty =
+          match ttc.Typing_defs.ttc_abstract with
+          | Typing_defs.TCConcrete ->
+            (match ttc.Typing_defs.ttc_type with
+            | None -> "= None"
+            | Some ty -> "= " ^ ty_to_string ty)
+          | Typing_defs.TCAbstract default ->
+            String.concat
+              ~sep:" "
+              (List.filter_map
+                 [
+                   Option.map ttc.Typing_defs.ttc_as_constraint ~f:(fun ty ->
+                       "as " ^ ty_to_string ty);
+                   Option.map default ~f:(fun ty -> "= " ^ ty_to_string ty);
+                 ]
+                 ~f:(fun x -> x))
+          | Typing_defs.TCPartiallyAbstract ->
+            String.concat
+              ~sep:" "
+              (List.filter_map
+                 [
+                   Option.map ttc.Typing_defs.ttc_as_constraint ~f:(fun ty ->
+                       "as " ^ ty_to_string ty);
+                   Option.map ttc.Typing_defs.ttc_type ~f:(fun ty ->
+                       "= " ^ ty_to_string ty);
+                 ]
+                 ~f:(fun x -> x))
+        in
+        let abstract =
+          match ttc.Typing_defs.ttc_abstract with
+          | Typing_defs.TCConcrete -> ""
+          | Typing_defs.TCAbstract _ -> "abstract "
+          | Typing_defs.TCPartiallyAbstract -> "partially abstract "
+        in
+        Printf.printf "  %stypeconst%s: %s %s\n" abstract from mid ty);
+      ())
   | Linearization ->
     if not (List.is_empty parse_errors) then (
       print_error error_format (List.hd_exn parse_errors);

0d1927a98088ad1f6de8a76014eec8f7a0d18382
Vincent Siles <vsiles@fb.com>
2021-03-02 07:37:00 -0800

    Factor out part of ppx_gen_hhi
    Summary:
    In order to migrate away from Migrate_parsetree, this diff extracts the common
    code used by buck and dune version of ppx_gen_hhi into ppx_gen_hhi_common and
    clean up the buck ppx file.
    
    Note that the dune ppx doesn't use yet the common code, and will do in the
    next commit
    
    Reviewed By: madgen
    
    Differential Revision: D26637948
    
    fbshipit-source-id: 2dc2bbf110f3662df6d94a26cfb06457ea312941

--- hphp/hack/src/ppx/ppx_gen_hhi_direct.ml
+++ hphp/hack/src/ppx/ppx_gen_hhi_direct.ml
@@ -7,4 +7,41 @@
  *
  *)
 
-let () = Ppx_gen_hhi.register ()
+open Asttypes
+open Parsetree
+open Ast_mapper
+open Ast_helper
+
+(* Turn the (name, contents) list into a PPX ast (string * string) array
+ * expression *)
+let contents hhi_dir =
+  Ppx_gen_hhi_common.get_hhis hhi_dir
+  |> List.map (fun (name, contents) ->
+         Exp.tuple
+           [
+             Exp.constant (Const.string name);
+             Exp.constant (Const.string contents);
+           ])
+  |> Exp.array
+
+(* Whenever we see [%hhi_contents], replace it with all of the hhis *)
+let ppx_gen_hhi_mapper hhi_dir =
+  {
+    default_mapper with
+    expr =
+      (fun mapper expr ->
+        match expr with
+        | {
+         pexp_desc = Pexp_extension ({ txt = "hhi_contents"; _ }, PStr []);
+         _;
+        } ->
+          contents hhi_dir
+        | other -> default_mapper.expr mapper other);
+  }
+
+let register () =
+  register "ppx_gen_hhi" (fun _argv ->
+      let hhi_dir = Sys.argv.(1) in
+      ppx_gen_hhi_mapper hhi_dir)
+
+let () = register ()

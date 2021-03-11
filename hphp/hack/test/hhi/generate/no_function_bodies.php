42b841771aa7200c52e12c524f03731799ba003b
Fred Emmott <fe@fb.com>
2021-03-11 14:17:35 -0600

    Add `hh_parse --generate-hhi`
    Summary:
    
    Starting to work on having HSL truly built-in; we want to keep it typechecked, but we don't want
    the typechecker to always typecheck the implementation. If we generate HHIs, we can remove
    the HH_FIXMEs around the PHP builtin calls, as we will no longer need the body to typecheck when using
    the deregister_phpstdlib option.
    
    ocamlformat and buck support todo before commit
    
    Test plan:
    
    ```
    dune runtest test/hhi/generate
    ```

---
+++ hphp/hack/test/hhi/generate/no_function_bodies.php
@@ -0,0 +1,20 @@
+<?hh // strict
+
+namespace GeneratedHHITest;
+
+/** Top level function doc block */
+function top_level_function(): void {
+  foo();
+}
+
+class Foo {
+  /** public method doc block */
+  public function publicMethod(): void {
+    bar();
+  }
+
+  /** public static method doc block */
+  public static function publicStaticMethod(): void {
+    bar();
+  }
+}

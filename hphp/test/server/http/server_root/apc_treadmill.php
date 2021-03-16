75fd9eae136682b81c339d3f3b8503910e12e363
Bin Liu <binliu@fb.com>
2021-03-15 17:35:09 -0700

    test to apc treadmill in server mode
    Summary: This adds a server test that reproduces a long-standing issue with APC corruption.
    
    Reviewed By: jano
    
    Differential Revision: D27040537
    
    fbshipit-source-id: 2cc252d7220889b64c5e81a6625529df2e1d6080

---
+++ hphp/test/server/http/server_root/apc_treadmill.php
@@ -0,0 +1,13 @@
+<?hh
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+
+<<__EntryPoint>>
+function test_treadmill() {
+  $v = keyset['a'.mt_rand() % 8];
+  $key = "apc.key";
+  apc_store($key, $v);
+  $succ = false;
+  $v = apc_fetch($key, inout $succ);
+  apc_add($key, $v);
+  echo 'see if it crashes after this request finishes';
+}

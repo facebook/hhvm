27ef2a91d34a433eb71743d03d925b0ead6b3f87
Steve Cao <shiqicao@fb.com>
2021-03-18 11:27:09 -0700

    port emit_function
    Reviewed By: shayne-fletcher
    
    Differential Revision: D27155480
    
    fbshipit-source-id: e6fba08670b820dd775163554230947abd960055

---
+++ hphp/hack/src/hhbc/hhbc_by_ref/emit_function.rs
@@ -0,0 +1,199 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the "hack" directory of this source tree.
+
+mod emit_memoize_function;
+
+use hhbc_by_ref_ast_scope::{self as ast_scope, Scope, ScopeItem};
+use hhbc_by_ref_emit_attribute as emit_attribute;
+use hhbc_by_ref_emit_body::{self as emit_body};
+use hhbc_by_ref_emit_memoize_helpers as emit_memoize_helpers;
+use hhbc_by_ref_env::emitter::Emitter;
+use hhbc_by_ref_hhas_attribute::{self as hhas_attribute, HhasAttribute};
+use hhbc_by_ref_hhas_coeffects::HhasCoeffects;
+use hhbc_by_ref_hhas_function::{self as hhas_function, HhasFunction};
+use hhbc_by_ref_hhas_pos::Span;
+use hhbc_by_ref_hhbc_id::{self as hhbc_id, Id};
+use hhbc_by_ref_instruction_sequence::{instr, Result};
+use hhbc_by_ref_options::HhvmFlags;
+use naming_special_names_rust::user_attributes as ua;
+use ocamlrep::rc::RcOc;
+use oxidized::{ast as tast, ast_defs};
+
+use itertools::Either;
+
+pub fn emit_function<'a, 'arena>(
+    alloc: &'arena bumpalo::Bump,
+    e: &mut Emitter<'arena>,
+    f: &'a tast::Fun_,
+) -> Result<Vec<HhasFunction<'a, 'arena>>> {
+    use ast_defs::FunKind;
+    use hhas_function::Flags;
+    let mut original_id = hhbc_id::function::Type::from_ast_name(alloc, &f.name.1);
+    let mut flags = Flags::empty();
+    flags.set(
+        hhas_function::Flags::ASYNC,
+        matches!(f.fun_kind, FunKind::FAsync | FunKind::FAsyncGenerator),
+    );
+
+    let mut attrs: Vec<HhasAttribute<'arena>> =
+        emit_attribute::from_asts(alloc, e, &f.namespace, &f.user_attributes)?;
+    attrs.extend(emit_attribute::add_reified_attribute(&f.tparams));
+    let memoized = attrs.iter().any(|a| ua::is_memoized(&a.name));
+    flags.set(Flags::MEMOIZE_IMPL, memoized);
+    flags.set(Flags::NO_INJECTION, hhas_attribute::is_no_injection(&attrs));
+
+    let renamed_id = {
+        if memoized {
+            original_id.add_suffix(alloc, emit_memoize_helpers::MEMOIZE_SUFFIX);
+        }
+        original_id
+    };
+    flags.set(
+        Flags::INTERCEPTABLE,
+        emit_memoize_function::is_interceptable(e.options()),
+    );
+    let is_meth_caller = f.name.1.starts_with("\\MethCaller$");
+    let call_context = if is_meth_caller {
+        match &f.user_attributes[..] {
+            [tast::UserAttribute {
+                name: ast_defs::Id(_, ref s),
+                params,
+            }] if s == "__MethCaller" => match &params[..] {
+                [tast::Expr(_, tast::Expr_::String(ref ctx))] if !ctx.is_empty() => Some(
+                    hhbc_id::class::Type::from_ast_name(
+                        alloc,
+                        // FIXME: This is not safe--string literals are binary strings.
+                        // There's no guarantee that they're valid UTF-8.
+                        unsafe { std::str::from_utf8_unchecked(ctx.as_slice()) },
+                    )
+                    .to_raw_string()
+                    .into(),
+                ),
+                _ => None,
+            },
+            _ => None,
+        }
+    } else {
+        None
+    };
+    let is_debug_main = match f.user_attributes.as_slice() {
+        [tast::UserAttribute { name, params }]
+            if name.1 == "__DebuggerMain" && params.is_empty() =>
+        {
+            true
+        }
+        _ => false,
+    };
+    let mut scope = Scope::toplevel();
+    if !is_debug_main {
+        scope.push_item(ScopeItem::Function(ast_scope::Fun::new_ref(&f)));
+    }
+
+    let coeffects = HhasCoeffects::from_ast(&f.ctxs, &f.params);
+    let (ast_body, rx_body) = {
+        if !coeffects.is_any_rx() {
+            (&f.body.ast, false)
+        } else {
+            match hhbc_by_ref_hhas_coeffects::halves_of_is_enabled_body(&f.body) {
+                Some((enabled_body, disabled_body)) => {
+                    if e.options().hhvm.flags.contains(HhvmFlags::RX_IS_ENABLED) {
+                        (enabled_body, true)
+                    } else {
+                        flags.insert(hhas_function::Flags::RX_DISABLED);
+                        (disabled_body, false)
+                    }
+                }
+                None => (&f.body.ast, true),
+            }
+        }
+    };
+
+    let deprecation_info = hhas_attribute::deprecation_info(attrs.iter());
+    let (body, is_gen, is_pair_gen) = {
+        let deprecation_info = if memoized { None } else { deprecation_info };
+        let native = attrs.iter().any(|a| ua::is_native(&a.name));
+        use emit_body::{Args as EmitBodyArgs, Flags as EmitBodyFlags};
+        let mut body_flags = EmitBodyFlags::empty();
+        body_flags.set(
+            EmitBodyFlags::ASYNC,
+            flags.contains(hhas_function::Flags::ASYNC),
+        );
+        body_flags.set(EmitBodyFlags::RX_BODY, rx_body);
+        body_flags.set(EmitBodyFlags::NATIVE, native);
+        body_flags.set(EmitBodyFlags::MEMOIZE, memoized);
+        body_flags.set(
+            EmitBodyFlags::SKIP_AWAITABLE,
+            f.fun_kind == ast_defs::FunKind::FAsync,
+        );
+        body_flags.set(
+            EmitBodyFlags::HAS_COEFFECT_RULES,
+            coeffects.has_coeffect_rules(),
+        );
+
+        emit_body::emit_body(
+            alloc,
+            e,
+            RcOc::clone(&f.namespace),
+            Either::Right(ast_body),
+            instr::null(alloc),
+            scope,
+            EmitBodyArgs {
+                flags: body_flags,
+                deprecation_info: &deprecation_info,
+                default_dropthrough: None,
+                doc_comment: f.doc_comment.clone(),
+                pos: &f.span,
+                ret: f.ret.1.as_ref(),
+                ast_params: &f.params,
+                call_context,
+                immediate_tparams: &f.tparams,
+                class_tparam_names: &[],
+            },
+        )?
+    };
+    flags.set(hhas_function::Flags::GENERATOR, is_gen);
+    flags.set(hhas_function::Flags::PAIR_GENERATOR, is_pair_gen);
+    let memoize_wrapper = if memoized {
+        Some(emit_memoize_function::emit_wrapper_function(
+            alloc,
+            e,
+            original_id,
+            &renamed_id,
+            &deprecation_info,
+            &f,
+        )?)
+    } else {
+        None
+    };
+    //let name: String = renamed_id.into();
+    let normal_function = HhasFunction {
+        attributes: attrs,
+        name: (alloc, renamed_id.to_raw_string()).into(),
+        span: Span::from_pos(&f.span),
+        coeffects,
+        body,
+        flags,
+    };
+
+    Ok(if let Some(memoize_wrapper) = memoize_wrapper {
+        vec![normal_function, memoize_wrapper]
+    } else {
+        vec![normal_function]
+    })
+}
+
+pub fn emit_functions_from_program<'a, 'arena>(
+    alloc: &'arena bumpalo::Bump,
+    e: &mut Emitter<'arena>,
+    tast: &'a [tast::Def],
+) -> Result<Vec<HhasFunction<'a, 'arena>>> {
+    Ok(tast
+        .iter()
+        .filter_map(|d| d.as_fun().map(|f| emit_function(alloc, e, f)))
+        .collect::<Result<Vec<Vec<_>>>>()?
+        .into_iter()
+        .flatten()
+        .collect::<Vec<_>>())
+}
---
+++ hphp/hack/src/hhbc/hhbc_by_ref/emit_memoize_function.rs
@@ -0,0 +1,356 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the "hack" directory of this source tree.
+use hhbc_by_ref_ast_scope::{self as ast_scope, Scope, ScopeItem};
+use hhbc_by_ref_emit_attribute as emit_attribute;
+use hhbc_by_ref_emit_body as emit_body;
+use hhbc_by_ref_emit_memoize_helpers as emit_memoize_helpers;
+use hhbc_by_ref_emit_param as emit_param;
+use hhbc_by_ref_emit_pos::emit_pos_then;
+use hhbc_by_ref_env::{emitter::Emitter, Env};
+use hhbc_by_ref_hhas_body::HhasBody;
+use hhbc_by_ref_hhas_coeffects::HhasCoeffects;
+use hhbc_by_ref_hhas_function::{Flags as HhasFunctionFlags, HhasFunction};
+use hhbc_by_ref_hhas_param::HhasParam;
+use hhbc_by_ref_hhas_pos::Span;
+use hhbc_by_ref_hhas_type::Info as HhasTypeInfo;
+use hhbc_by_ref_hhbc_ast::{FcallArgs, FcallFlags};
+use hhbc_by_ref_hhbc_id::function;
+use hhbc_by_ref_hhbc_string_utils::{coeffects, reified};
+use hhbc_by_ref_instruction_sequence::{instr, InstrSeq, Result};
+use hhbc_by_ref_options::{HhvmFlags, Options, RepoFlags};
+use hhbc_by_ref_runtime::TypedValue;
+use ocamlrep::rc::RcOc;
+use oxidized::{ast as T, pos::Pos};
+
+pub(crate) fn is_interceptable(opts: &Options) -> bool {
+    opts.hhvm
+        .flags
+        .contains(HhvmFlags::JIT_ENABLE_RENAME_FUNCTION)
+        && !opts.repo_flags.contains(RepoFlags::AUTHORITATIVE)
+}
+
+pub(crate) fn emit_wrapper_function<'a, 'arena>(
+    alloc: &'arena bumpalo::Bump,
+    emitter: &mut Emitter<'arena>,
+    original_id: function::Type<'arena>,
+    renamed_id: &function::Type<'arena>,
+    deprecation_info: &Option<&[TypedValue<'arena>]>,
+    f: &'a T::Fun_,
+) -> Result<HhasFunction<'a, 'arena>> {
+    emit_memoize_helpers::check_memoize_possible(&(f.name).0, &f.params, false)?;
+    let scope = Scope {
+        items: vec![ScopeItem::Function(ast_scope::Fun::new_ref(f))],
+    };
+    let mut tparams = scope
+        .get_tparams()
+        .iter()
+        .map(|tp| tp.name.1.as_str())
+        .collect::<Vec<_>>();
+    let params = emit_param::from_asts(
+        alloc,
+        emitter,
+        &mut tparams,
+        &f.namespace,
+        true,
+        &scope,
+        &f.params,
+    )?;
+    let mut attributes =
+        emit_attribute::from_asts(alloc, emitter, &f.namespace, &f.user_attributes)?;
+    attributes.extend(emit_attribute::add_reified_attribute(&f.tparams));
+    let return_type_info = emit_body::emit_return_type_info(
+        alloc,
+        &tparams,
+        f.fun_kind.is_fasync(), /* skip_awaitable */
+        f.ret.1.as_ref(),
+    )?;
+    let is_reified = f
+        .tparams
+        .iter()
+        .any(|tp| tp.reified.is_reified() || tp.reified.is_soft_reified());
+    let mut env = Env::default(alloc, RcOc::clone(&f.namespace)).with_scope(scope);
+    let body_instrs = make_memoize_function_code(
+        alloc,
+        emitter,
+        &mut env,
+        &f.span,
+        *deprecation_info,
+        &params,
+        &f.params,
+        *renamed_id,
+        f.fun_kind.is_fasync(),
+        is_reified,
+    )?;
+    let coeffects = HhasCoeffects::from_ast(&f.ctxs, &f.params);
+    let has_coeffect_rules = coeffects.has_coeffect_rules();
+    env.with_rx_body(coeffects.is_any_rx_or_pure());
+    let body = make_wrapper_body(
+        alloc,
+        emitter,
+        env,
+        return_type_info,
+        params,
+        body_instrs,
+        is_reified,
+        has_coeffect_rules,
+    )?;
+
+    let mut flags = HhasFunctionFlags::empty();
+    flags.set(HhasFunctionFlags::ASYNC, f.fun_kind.is_fasync());
+    let is_interceptable = is_interceptable(emitter.options());
+    flags.set(HhasFunctionFlags::INTERCEPTABLE, is_interceptable);
+
+    Ok(HhasFunction {
+        attributes,
+        name: original_id,
+        body,
+        span: Span::from_pos(&f.span),
+        coeffects,
+        flags,
+    })
+}
+
+fn make_memoize_function_code<'a, 'arena>(
+    alloc: &'arena bumpalo::Bump,
+    e: &mut Emitter<'arena>,
+    env: &mut Env<'a, 'arena>,
+    pos: &Pos,
+    deprecation_info: Option<&[TypedValue<'arena>]>,
+    hhas_params: &[HhasParam<'arena>],
+    ast_params: &[T::FunParam],
+    renamed_id: function::Type<'arena>,
+    is_async: bool,
+    is_reified: bool,
+) -> Result<InstrSeq<'arena>> {
+    let fun = if hhas_params.is_empty() && !is_reified {
+        make_memoize_function_no_params_code(alloc, e, env, deprecation_info, renamed_id, is_async)
+    } else {
+        make_memoize_function_with_params_code(
+            alloc,
+            e,
+            env,
+            pos,
+            deprecation_info,
+            hhas_params,
+            ast_params,
+            renamed_id,
+            is_async,
+            is_reified,
+        )
+    }?;
+    Ok(emit_pos_then(alloc, pos, fun))
+}
+
+fn make_memoize_function_with_params_code<'a, 'arena>(
+    alloc: &'arena bumpalo::Bump,
+    e: &mut Emitter<'arena>,
+    env: &mut Env<'a, 'arena>,
+    pos: &Pos,
+    deprecation_info: Option<&[TypedValue<'arena>]>,
+    hhas_params: &[HhasParam<'arena>],
+    ast_params: &[T::FunParam],
+    renamed_id: function::Type<'arena>,
+    is_async: bool,
+    is_reified: bool,
+) -> Result<InstrSeq<'arena>> {
+    let param_count = hhas_params.len();
+    let notfound = e.label_gen_mut().next_regular(alloc);
+    let suspended_get = e.label_gen_mut().next_regular(alloc);
+    let eager_set = e.label_gen_mut().next_regular(alloc);
+    // The local that contains the reified generics is the first non parameter local,
+    // so the first local is parameter count + 1 when there are reified = generics
+    let add_refied = usize::from(is_reified);
+    let first_local = hhbc_by_ref_local::Type::Unnamed(param_count + add_refied);
+    let deprecation_body =
+        emit_body::emit_deprecation_info(alloc, &env.scope, deprecation_info, e.systemlib())?;
+    let (begin_label, default_value_setters) =
+        // Default value setters belong in the wrapper method not in the original method
+        emit_param::emit_param_default_value_setter(e, env, pos, hhas_params)?;
+    let fcall_args = {
+        let mut fcall_flags = FcallFlags::default();
+        fcall_flags.set(FcallFlags::HAS_GENERICS, is_reified);
+        FcallArgs::new(
+            alloc,
+            fcall_flags,
+            1,
+            &[],
+            if is_async { Some(eager_set) } else { None },
+            param_count,
+            None,
+        )
+    };
+    let (reified_get, reified_memokeym) = if !is_reified {
+        (instr::empty(alloc), instr::empty(alloc))
+    } else {
+        (
+            instr::cgetl(
+                alloc,
+                hhbc_by_ref_local::Type::Named(reified::GENERICS_LOCAL_NAME),
+            ),
+            InstrSeq::gather(
+                alloc,
+                emit_memoize_helpers::get_memo_key_list(
+                    alloc,
+                    param_count,
+                    param_count + add_refied,
+                    reified::GENERICS_LOCAL_NAME,
+                ),
+            ),
+        )
+    };
+    let param_count = (param_count + add_refied) as isize;
+    Ok(InstrSeq::gather(
+        alloc,
+        vec![
+            begin_label,
+            emit_body::emit_method_prolog(e, env, pos, hhas_params, ast_params, &[])?,
+            deprecation_body,
+            emit_memoize_helpers::param_code_sets(alloc, hhas_params, param_count as usize),
+            reified_memokeym,
+            if is_async {
+                InstrSeq::gather(
+                    alloc,
+                    vec![
+                        instr::memoget_eager(
+                            alloc,
+                            notfound,
+                            suspended_get,
+                            Some((first_local, param_count)),
+                        ),
+                        instr::retc(alloc),
+                        instr::label(alloc, suspended_get),
+                        instr::retc_suspended(alloc),
+                    ],
+                )
+            } else {
+                InstrSeq::gather(
+                    alloc,
+                    vec![
+                        instr::memoget(alloc, notfound, Some((first_local, param_count))),
+                        instr::retc(alloc),
+                    ],
+                )
+            },
+            instr::label(alloc, notfound),
+            instr::nulluninit(alloc),
+            instr::nulluninit(alloc),
+            emit_memoize_helpers::param_code_gets(alloc, hhas_params),
+            reified_get,
+            instr::fcallfuncd(alloc, fcall_args, renamed_id),
+            instr::memoset(alloc, Some((first_local, param_count))),
+            if is_async {
+                InstrSeq::gather(
+                    alloc,
+                    vec![
+                        instr::retc_suspended(alloc),
+                        instr::label(alloc, eager_set),
+                        instr::memoset_eager(alloc, Some((first_local, param_count))),
+                        instr::retc(alloc),
+                    ],
+                )
+            } else {
+                InstrSeq::gather(alloc, vec![instr::retc(alloc)])
+            },
+            default_value_setters,
+        ],
+    ))
+}
+
+fn make_memoize_function_no_params_code<'a, 'arena>(
+    alloc: &'arena bumpalo::Bump,
+    e: &mut Emitter<'arena>,
+    env: &mut Env<'a, 'arena>,
+    deprecation_info: Option<&[TypedValue<'arena>]>,
+    renamed_id: function::Type<'arena>,
+    is_async: bool,
+) -> Result<InstrSeq<'arena>> {
+    let notfound = e.label_gen_mut().next_regular(alloc);
+    let suspended_get = e.label_gen_mut().next_regular(alloc);
+    let eager_set = e.label_gen_mut().next_regular(alloc);
+    let deprecation_body =
+        emit_body::emit_deprecation_info(alloc, &env.scope, deprecation_info, e.systemlib())?;
+    let fcall_args = FcallArgs::new(
+        alloc,
+        FcallFlags::default(),
+        1,
+        &[],
+        if is_async { Some(eager_set) } else { None },
+        0,
+        None,
+    );
+    Ok(InstrSeq::gather(
+        alloc,
+        vec![
+            deprecation_body,
+            if is_async {
+                InstrSeq::gather(
+                    alloc,
+                    vec![
+                        instr::memoget_eager(alloc, notfound, suspended_get, None),
+                        instr::retc(alloc),
+                        instr::label(alloc, suspended_get),
+                        instr::retc_suspended(alloc),
+                    ],
+                )
+            } else {
+                InstrSeq::gather(
+                    alloc,
+                    vec![instr::memoget(alloc, notfound, None), instr::retc(alloc)],
+                )
+            },
+            instr::label(alloc, notfound),
+            instr::nulluninit(alloc),
+            instr::nulluninit(alloc),
+            instr::fcallfuncd(alloc, fcall_args, renamed_id),
+            instr::memoset(alloc, None),
+            if is_async {
+                InstrSeq::gather(
+                    alloc,
+                    vec![
+                        instr::retc_suspended(alloc),
+                        instr::label(alloc, eager_set),
+                        instr::memoset_eager(alloc, None),
+                        instr::retc(alloc),
+                    ],
+                )
+            } else {
+                instr::retc(alloc)
+            },
+        ],
+    ))
+}
+
+fn make_wrapper_body<'a, 'arena>(
+    alloc: &'arena bumpalo::Bump,
+    emitter: &mut Emitter<'arena>,
+    env: Env<'a, 'arena>,
+    return_type_info: HhasTypeInfo,
+    params: Vec<HhasParam<'arena>>,
+    body_instrs: InstrSeq<'arena>,
+    is_reified: bool,
+    has_coeffect_rules: bool,
+) -> Result<HhasBody<'a, 'arena>> {
+    let mut decl_vars = vec![];
+    if is_reified {
+        decl_vars.push(reified::GENERICS_LOCAL_NAME.into());
+    }
+    if has_coeffect_rules {
+        decl_vars.push(coeffects::LOCAL_NAME.into());
+    }
+    emit_body::make_body(
+        alloc,
+        emitter,
+        body_instrs,
+        decl_vars,
+        true,   /* is_memoize_wrapper */
+        false,  /* is_memoize_wrapper_lsb */
+        vec![], /* upper_bounds */
+        vec![], /* shadowed_tparams */
+        params,
+        Some(return_type_info),
+        None, /* doc comment */
+        Some(env.clone()),
+    )
+}

1dcebf0922232a8f725b481c9ee08e62aacd157e
Kasper Kopec <kasper@fb.com>
2021-03-18 14:08:12 -0700

    One more case showing difference in handling type const inheritance between shallow and folded decl
    Summary:
    As in title - I have one more idea I want to try for those, but if that doesn't work we will probably have to HH_FIXME those as part of shallow decl rollout.
    
    This might be the same as {D16388119 (https://github.com/facebook/hhvm/commit/bcc584add0f5cca7838c5106bdeaa4111ef7508c)}, but I think presence of `Tany` make it work a bit different, so just to be sure committing as test case for future fixes.
    
    Reviewed By: vassilmladenov
    
    Differential Revision: D27135292
    
    fbshipit-source-id: dce6e74013da6cec9a284832fd77ad4d28dfd403

---
+++ hphp/hack/test/typecheck/typeconsts/tany_ambiguous.php
@@ -0,0 +1,41 @@
+<?hh // strict
+
+type ANY<T> = T;
+/* HH_FIXME[4101] trick to create Tany type */
+type TANY = ANY;
+
+abstract class Base {
+  const type TData as TANY = TANY;
+
+  final private function __construct(protected this::TData $data) {}
+
+  final protected function getData(): this::TData {
+    return $this->data;
+  }
+}
+
+abstract class Middle extends Base {
+  const type TData as string = string;
+}
+
+trait ChildTrait1 {
+  require extends Middle;
+}
+
+trait ChildTrait2 {
+  require extends Base;
+}
+
+final class Child extends Middle {
+  // in folded decl, this::TData comes from Base OR Middle, depending on order of used traits below
+  // `check_ambiguous_inheritance` aims to force explicit redeclaration in such case,
+  // but it's not triggered in presence of Tany (which is both subtype and supertype of string)
+
+  // in shallow decl, we always deterministically choose TData = string
+  use ChildTrait1;
+  use ChildTrait2;
+
+  public function test(): int {
+    return $this->getData();
+  }
+}

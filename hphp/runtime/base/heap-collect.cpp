fb231899e6794e586c787160ea5542d8c98fc5a5
Bin Liu <binliu@fb.com>
2021-03-24 14:30:44 -0700

    make it possible to turn off GC in the global option
    Summary: When the global option is off, even manual gc won't run. But a request really wants to run gc, it can still be turned on for the request using ini settings.
    
    Reviewed By: ricklavoie
    
    Differential Revision: D27279716
    
    fbshipit-source-id: ca42f8023be351879c4c13d7c9af7fe64a5f5511

--- hphp/runtime/base/heap-collect.cpp
+++ hphp/runtime/base/heap-collect.cpp
@@ -62,8 +62,7 @@ constexpr auto MaxMark = GCBits(3);
  * Eval.EnableGC - Default value of the per-request MemoryManager::m_gc_enabled
  * flag. This flag can be dynamically set/cleared by PHP via
  * ini_set("zend.enable_gc"). In turn, m_gc_enabled enables automatic background
- * garbage collection. Regardless of its value, PHP can call gc_collect_cycles()
- * for manual gc.
+ * garbage collection. If not enabled, gc_collect_cycles() won't run.
  *
  * Eval.EagerGC - If set, trigger collection after every allocation, in debug
  * builds. Has no effect in opt builds or when m_gc_enabled == false.
@@ -707,6 +706,7 @@ void MemoryManager::updateNextGc() {
 }
 
 void MemoryManager::collect(const char* phase) {
+  if (!isGCEnabled()) return;
   if (empty()) return;
   rl_gcdata->t_req_age = cpu_ns()/1000 - m_req_start_micros;
   rl_gcdata->t_trigger = m_nextGC;

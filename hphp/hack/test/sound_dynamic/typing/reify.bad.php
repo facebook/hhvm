ff5bf972d6114f6610bdd635f4a9c620cc135345
Scott Owens <sowens@fb.com>
2021-04-07 07:12:04 -0700

    Add simple tests for reified generics
    Summary: Check that reified generics and dynamic aren't doing anything funny with enforceability to allow a C<dynamic> to be passed into a C<int> (where C's generic is reified), because that would be bound to fail at run time.
    
    Reviewed By: vsiles
    
    Differential Revision: D27618614
    
    fbshipit-source-id: 24120b64198bf7a9bb9f3dd4d6d085bec4ea34d7

---
+++ hphp/hack/test/sound_dynamic/typing/reify.bad.php
@@ -0,0 +1,14 @@
+<?hh
+
+class C<reify T> {}
+
+function expect_Cdyn(C<dynamic> $c) : void { }
+
+function expect_Cint(C<int> $c) : void { }
+
+function test(C<dynamic> $cd, C<int> $ci) : void {
+  expect_Cdyn($ci);
+  expect_Cint($cd);
+  expect_Cdyn(new C<int>());
+  expect_Cint(new C<dynamic>());
+}
---
+++ hphp/hack/test/sound_dynamic/typing/reify.good.php
@@ -0,0 +1,16 @@
+<?hh
+
+<<__SoundDynamicCallable>>
+class C<reify T> {}
+
+function expect_dynamic(dynamic $d) : void {}
+
+function expect_Cint(C<int> $c) : void {}
+
+function expect_Cdyn(C<dynamic> $c) : void {}
+
+function test(dynamic $d, C<int> $c) : void {
+  expect_dynamic($c);
+  expect_Cint($d);
+  expect_Cdyn($d);
+}

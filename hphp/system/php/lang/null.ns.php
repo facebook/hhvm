a58bc50d99369bea54851bb0d10df28356be340a
Oguz Ulgen <oulgen@fb.com>
2021-04-09 15:18:32 -0700

    Make 86null pure
    Summary: Created from Diffusion's 'Open in Editor' feature.
    
    Reviewed By: viratyosin
    
    Differential Revision: D27680887
    
    fbshipit-source-id: b92d5785551b251730b0799ac05d6d9e4a739b7e

--- hphp/system/php/lang/null.ns.php
+++ hphp/system/php/lang/null.ns.php
@@ -1,15 +1,13 @@
-<?hh // partial
+<?hh
 
 namespace __SystemLib {
 
 /* Special function used by nullsafe FPushObjMethodD.
  */
-<<__Pure>>
-function __86null() {}
+function __86null()[]: void {}
 
 /* Special function used by nullsafe 86reifiedinit.
  */
-<<__Pure>>
-function __86single_arg_null($_) {}
+function __86single_arg_null($_)[]: void {}
 
 }

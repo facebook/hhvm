e45ec866736a42dfec1273127fb3bc3702859dc3
jjergus <jjergus@fb.com>
2021-04-14 13:51:39 -0700

    security fixes

--- hphp/runtime/ext/mysql/mysql_common.cpp
+++ hphp/runtime/ext/mysql/mysql_common.cpp
@@ -614,9 +614,21 @@ Variant php_mysql_do_connect_with_ssl(
     const Variant& sslContextProvider /* = null */) {
 #ifdef ENABLE_ASYNC_MYSQL
   std::shared_ptr<SSLOptionsProviderBase> ssl_provider;
+
   if (!sslContextProvider.isNull()) {
+    auto ctx = sslContextProvider.toObject();
+    if (!ctx.instanceof(MySSLContextProvider::s_className)) {
+       SystemLib::throwInvalidArgumentExceptionObject(
+         folly::sformat(
+           "Invalid argument. Expected {}, received {}",
+           MySSLContextProvider::s_className,
+           ctx->getClassName().c_str()
+         )
+       );
+    }
+
     auto* obj =
-        Native::data<HPHP::MySSLContextProvider>(sslContextProvider.toObject());
+        Native::data<HPHP::MySSLContextProvider>(ctx);
     ssl_provider = obj->getSSLProvider();
   }
 #endif

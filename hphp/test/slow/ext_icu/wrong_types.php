85cfafeab2529ba7abf643b47e0b7b44dc6f1f8b
jjergus <jjergus@fb.com>
2021-04-14 13:51:50 -0700

    security fixes

---
+++ hphp/test/slow/ext_icu/wrong_types.php
@@ -0,0 +1,15 @@
+<?hh
+
+class MyFakeCalendar0 {}
+class MyFakeCalendar1 {}
+class MyFakeCalendar2 {}
+class MyFakeCalendar3 {}
+
+<<__EntryPoint>>
+function main() {
+  $calendar = IntlCalendar::createInstance('UTC');
+  var_dump($calendar->after(new MyFakeCalendar0()));
+  var_dump($calendar->before(new MyFakeCalendar1()));
+  var_dump($calendar->equals(new MyFakeCalendar2()));
+  var_dump($calendar->isEquivalentTo(new MyFakeCalendar3()));
+}

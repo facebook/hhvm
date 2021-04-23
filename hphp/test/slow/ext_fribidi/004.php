fd2a0fb1d211e8348ccfa1f6e86d54968232e6eb
Alexey Toptygin <alexeyt@fb.com>
2021-04-22 18:28:25 -0700

    Fix skipifs and broken tests of the fribidi extension
    Summary:
    hphp/test/slow/ext_fribidi/config.runif had top-level code. Fix it by converting to a config.runif
    
    All the .expect files in this directory needed to be adjusted for HAM unification.
    
    hphp/test/slow/ext_fribidi/002.php.expect needed substantial changes, but as it notes on line 41 the output there is subject to change (presumably with library version updates).
    
    hphp/test/slow/ext_fribidi/004.php needed a partial rewrite. It appears to have been testing passing invalid arguments to fribidi_log2vis and to have been assuming that it would just get a result back. When the argument's type is wrong we raise a fatal, so the test can't continue to work as is. Narrow it down to just testing the empty string input, we don't need to test the argument type enforcement machinery here.
    
    Differential Revision: D27938046
    
    fbshipit-source-id: 614366d6639fe34605701266ebee913132bdc067

--- hphp/test/slow/ext_fribidi/004.php
+++ hphp/test/slow/ext_fribidi/004.php
@@ -1,13 +1,7 @@
-<?hh <<__EntryPoint>> function main(): void {
-error_reporting (E_ALL);
+<?hh
 
-$a = fribidi_log2vis(null,  FRIBIDI_AUTO, FRIBIDI_CHARSET_CAP_RTL);
-$b = fribidi_log2vis(false, FRIBIDI_AUTO, FRIBIDI_CHARSET_CAP_RTL);
-$c = fribidi_log2vis(true,  FRIBIDI_AUTO, FRIBIDI_CHARSET_CAP_RTL);
-$d = fribidi_log2vis(0,     FRIBIDI_AUTO, FRIBIDI_CHARSET_CAP_RTL);
-$e = fribidi_log2vis(1,     FRIBIDI_AUTO, FRIBIDI_CHARSET_CAP_RTL);
-$f = fribidi_log2vis('',    FRIBIDI_AUTO, FRIBIDI_CHARSET_CAP_RTL);
-$g = fribidi_log2vis("",    FRIBIDI_AUTO, FRIBIDI_CHARSET_CAP_RTL);
+<<__EntryPoint>> function main(): void {
+  error_reporting (E_ALL);
 
-var_dump(varray[$a, $b, $c, $d, $e, $f, $g]);
+  var_dump(fribidi_log2vis('', FRIBIDI_AUTO, FRIBIDI_CHARSET_CAP_RTL));
 }

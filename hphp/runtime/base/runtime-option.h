c14f2215c8202b6458a4fc957849ed5e3dad4072
Michael Colavita <mcolavita@fb.com>
2021-05-04 10:02:43 -0700

    Bump BespokeStructDictMaxNumKeys
    Summary: Increase maximum numbber of StructDict keys to 250.
    
    Differential Revision: D28164340
    
    fbshipit-source-id: c94dce74fe742cb5ee17fed34a420ce045f2a979

--- hphp/runtime/base/runtime-option.h
+++ hphp/runtime/base/runtime-option.h
@@ -1125,7 +1125,7 @@ struct RuntimeOption {
   /* Should we use structs? If so, how big can they get? Due to how we  \
    * represent structs, we can't make any with more than 255 keys. */   \
   F(bool, EmitBespokeStructDicts, true)                                 \
-  F(uint8_t, BespokeStructDictMaxNumKeys, 64)                           \
+  F(uint8_t, BespokeStructDictMaxNumKeys, 250)                          \
   F(double, BespokeStructDictKeyCoverageThreshold, 95.0)                \
   F(bool, EmitAPCBespokeArrays, true)                                   \
   /* Should we use monotypes? */                                        \

03cd2cf7bcb72e01aa2908ca69fbee2e7b86a1fa
Steve Cao <shiqicao@fb.com>
2021-05-07 20:40:26 -0700

    impl DeserializeInArena for tuples and Option
    Summary: Tuples and `Option` needs to implement `DeserializeInArena`, so `Foo(Option<&T>)` and `(&T, &S)` can be deserialize with arena.
    
    Reviewed By: dtolnay
    
    Differential Revision: D28090255
    
    fbshipit-source-id: 1f99938fcd2d50d1d3aac9bbb346025bfb278bf2

--- hphp/hack/src/utils/arena_deserializer/impls.rs
+++ hphp/hack/src/utils/arena_deserializer/impls.rs
@@ -32,29 +32,28 @@ macro_rules! deserialize_without_arena {
                     $crate::serde::Deserialize::deserialize(deserializer)
                 }
             }
+
+            impl<'arena> $crate::DeserializeInArena<'arena> for &'arena $ty
+            {
+                fn deserialize_in_arena<D>(arena: &'arena Bump, deserializer: D) -> Result<Self, D::Error>
+                where
+                    D: $crate::serde::Deserializer<'arena>,
+                {
+                    let value = <$ty>::deserialize(deserializer)?;
+                    Ok(arena.alloc(value))
+                }
+            }
         )+
     };
 }
 
 deserialize_without_arena! {
+    (),
     bool,
     i8, i16, i32, i64, isize,
     u8, u16, u32, u64, usize,
     f32, f64,
     char, String,
-}
-
-impl<'arena, T> DeserializeInArena<'arena> for &'arena T
-where
-    T: Deserialize<'arena>,
-{
-    fn deserialize_in_arena<D>(arena: &'arena Bump, deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'arena>,
-    {
-        let value = T::deserialize(deserializer)?;
-        Ok(arena.alloc(value))
-    }
 }
 
 impl<'arena> DeserializeInArena<'arena> for &'arena str {
@@ -126,4 +125,131 @@ where
             marker: PhantomData,
         })
     }
+}
+
+impl<'arena, T> DeserializeInArena<'arena> for Option<T>
+where
+    T: DeserializeInArena<'arena>,
+{
+    fn deserialize_in_arena<D>(arena: &'arena Bump, deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'arena>,
+    {
+        struct OptionVisitor<'arena, T> {
+            arena: &'arena Bump,
+            marker: PhantomData<fn() -> T>,
+        }
+
+        impl<'arena, T: DeserializeInArena<'arena>> Visitor<'arena> for OptionVisitor<'arena, T> {
+            type Value = Option<T>;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("option")
+            }
+
+            #[inline]
+            fn visit_unit<E>(self) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                Ok(None)
+            }
+
+            #[inline]
+            fn visit_none<E>(self) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                Ok(None)
+            }
+
+            #[inline]
+            fn visit_some<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+            where
+                D: Deserializer<'arena>,
+            {
+                T::deserialize_in_arena(self.arena, deserializer).map(Some)
+            }
+        }
+
+        deserializer.deserialize_option(OptionVisitor {
+            arena,
+            marker: PhantomData,
+        })
+    }
+}
+
+macro_rules! tuple_impls {
+    ($($len:tt => ($($n:tt $name:ident)+))+) => {
+        $(
+            impl<'arena, $($name: DeserializeInArena<'arena>),+> DeserializeInArena<'arena> for ($($name,)+) {
+                #[inline]
+                fn deserialize_in_arena<D>(_arena: &'arena Bump, deserializer: D) -> Result<Self, D::Error>
+                where
+                    D: Deserializer<'arena>,
+                {
+                    struct TupleVisitor<$($name,)+> {
+                        marker: PhantomData<($($name,)+)>,
+                    }
+
+                    impl<'arena, $($name: DeserializeInArena<'arena>),+> Visitor<'arena> for TupleVisitor<$($name,)+> {
+                        type Value = ($($name,)+);
+
+                        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                            formatter.write_str(concat!("a tuple of size ", $len))
+                        }
+
+                        #[inline]
+                        #[allow(non_snake_case)]
+                        fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+                        where
+                            A: SeqAccess<'arena>,
+                        {
+                            $(
+                                let seed = ArenaSeed::new();
+                                let $name = match seq.next_element_seed(seed)? {
+                                    Some(value) => value,
+                                    None => return Err(serde::de::Error::invalid_length($n, &self)),
+                                };
+                            )+
+
+                            Ok(($($name,)+))
+                        }
+                    }
+
+                    deserializer.deserialize_tuple($len, TupleVisitor { marker: PhantomData })
+                }
+            }
+
+            impl<'arena, $($name: DeserializeInArena<'arena>),+> DeserializeInArena<'arena> for &'arena ($($name,)+) {
+                #[inline]
+                fn deserialize_in_arena<D>(arena: &'arena Bump, deserializer: D) -> Result<Self, D::Error>
+                where
+                    D: Deserializer<'arena>,
+                {
+                    let v = <($($name,)+)>::deserialize_in_arena(arena, deserializer)?;
+                    Ok(arena.alloc(v))
+                }
+            }
+        )+
+    }
+}
+
+tuple_impls! {
+    1  => (0 T0)
+    2  => (0 T0 1 T1)
+    3  => (0 T0 1 T1 2 T2)
+    4  => (0 T0 1 T1 2 T2 3 T3)
+    5  => (0 T0 1 T1 2 T2 3 T3 4 T4)
+    6  => (0 T0 1 T1 2 T2 3 T3 4 T4 5 T5)
+    7  => (0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6)
+    8  => (0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7)
+    9  => (0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7 8 T8)
+    10 => (0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7 8 T8 9 T9)
+    11 => (0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7 8 T8 9 T9 10 T10)
+    12 => (0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7 8 T8 9 T9 10 T10 11 T11)
+    13 => (0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7 8 T8 9 T9 10 T10 11 T11 12 T12)
+    14 => (0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7 8 T8 9 T9 10 T10 11 T11 12 T12 13 T13)
+    15 => (0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7 8 T8 9 T9 10 T10 11 T11 12 T12 13 T13 14 T14)
+    16 => (0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7 8 T8 9 T9 10 T10 11 T11 12 T12 13 T13 14 T14 15 T15)
 }
--- hphp/hack/src/utils/test/arena_deserializer/lib.rs
+++ hphp/hack/src/utils/test/arena_deserializer/lib.rs
@@ -32,9 +32,21 @@ fn example() {
         #[serde(deserialize_with = "arena_deserializer::arena")]
         List(&'a [&'a Num<'a>]),
         #[serde(deserialize_with = "arena_deserializer::arena")]
+        ListValue(&'a [Num<'a>]),
+        #[serde(deserialize_with = "arena_deserializer::arena")]
         Str(&'a str),
         String(String),
+        #[serde(deserialize_with = "arena_deserializer::arena")]
+        OptRef(Option<&'a isize>),
+        #[serde(deserialize_with = "arena_deserializer::arena")]
+        Opt(Option<isize>),
+        #[serde(deserialize_with = "arena_deserializer::arena")]
+        PairRef(&'a (bool, &'a isize)),
+        #[serde(deserialize_with = "arena_deserializer::arena")]
+        Pair((&'a Num<'a>, &'a isize)),
     }
+
+    deserialize_without_arena!(Num<'arena>, I);
 
     let arena = Bump::new();
 
@@ -54,5 +66,27 @@ fn example() {
     let n2 = Num::Succ(&n1);
     let ls = vec![&n1, &n2];
     let x = Num::List(&ls);
+    round_trip(x, &arena);
+
+    let i = I(1);
+    let n1 = Num::Base(&i);
+    let n2 = Num::Succ(&n1);
+    let n1 = Num::Base(&i);
+    let ls = vec![n1, n2];
+    let x = Num::ListValue(&ls);
+    round_trip(x, &arena);
+
+    let i: isize = 3;
+    let x = Num::OptRef(Some(&i));
+    round_trip(x, &arena);
+
+    let i: isize = 3;
+    let x = Num::Opt(Some(i));
+    round_trip(x, &arena);
+
+    let i: isize = 1;
+    let n = Num::Opt(Some(3));
+    let p = (&n, &i);
+    let x = Num::Pair(p);
     round_trip(x, &arena);
 }

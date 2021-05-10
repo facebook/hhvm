2a31e2bfa1e8a48eca1669e55938a9ed11c5a7d8
Vincent Siles <vsiles@fb.com>
2021-05-10 04:55:54 -0700

    Require __Atom attribute if parent has it
    Summary:
    Much like `__Override`, we require all child class that overrides a method
    with `__Atom` to specify it explicitly. See the new test file
    `attribute.lint.php` for examples of missing attributes.
    
    This is done using an ocaml lint
    
    Reviewed By: Wilfred
    
    Differential Revision: D27998364
    
    fbshipit-source-id: 90bfae9124f0a0db90825f139b50e2c7f5d72882

---
+++ hphp/hack/test/enum_atom/typing/attribute.lint.php
@@ -0,0 +1,59 @@
+<?hh
+<<file:__EnableUnstableFeatures('enum_atom')>>
+
+enum class E : int {
+  int A = 42;
+}
+
+interface I {
+  public function f(<<__Atom>>HH\MemberOf<E, int> $x): int;
+}
+
+class C implements I {
+  public function f(HH\MemberOf<E, int> $x): int {
+    return $x;
+  }
+}
+
+class D {
+  public function f(<<__Atom>>HH\MemberOf<E, int> $x): int {
+    return $x;
+  }
+}
+
+class F extends D {
+  <<__Override>>
+  public function f(HH\MemberOf<E, int> $x): int {
+    return $x;
+  }
+}
+
+trait T {
+  public function f(HH\MemberOf<E, int> $x): int {
+    return $x;
+  }
+}
+
+class X implements I {
+  use T;
+}
+
+class Y extends D {
+  use T;
+}
+
+trait T0 {
+  require implements I;
+
+  public function f(HH\MemberOf<E, int> $x): int {
+    return $x;
+  }
+}
+
+trait T1 {
+  require extends D;
+
+  public function f(HH\MemberOf<E, int> $x): int {
+    return $x;
+  }
+}

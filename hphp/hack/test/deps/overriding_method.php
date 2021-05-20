596d0d6c3b6ab8ca28020937df465d2c8bc67f0e
Xiangyu Guo <lambda@fb.com>
2021-05-20 14:27:05 -0700

    Unit test for overriding a parent method in child class.
    Summary: Adding unit test to the dependency graph tool, so that we can cover overriding method use cases.
    
    Reviewed By: jamesjwu
    
    Differential Revision: D28556775
    
    fbshipit-source-id: 9ce45c6b61d9d1370e2341684cf2aa62016e3e1a

---
+++ hphp/hack/test/deps/overriding_method.php
@@ -0,0 +1,22 @@
+<?hh
+
+class B {
+    public function foo(): int {
+        return 0;
+    }
+}
+
+class C extends B {
+    <<__Override>>
+    public function foo(): int {
+        return 1;
+    }
+}
+
+function f1(): int {
+    return 0;
+}
+
+function f2(): int {
+    return f1();
+}

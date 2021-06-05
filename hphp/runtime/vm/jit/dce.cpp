25d8341f9750a8825dcdb7762d0bc9843814520f
Oguz Ulgen <oulgen@fb.com>
2021-06-04 21:12:27 -0700

    Optimize ConcatStrStr pairs to ConcatStr3
    Summary:
    When we see a pattern where
    ```
    t1 = ConcatStrStr a b
    t2 = ConcatStrStr t1 c
    ```
    we can convert this into
    ```
    t2 = ConcatStr3 a b c
    ```
    if all uses of `t1` are
    * `t2 = ConcatStrStr t1 c`
    * `Decref t1`
    * `StStk` in catch traces
    
    Note: `StStk` in catch traces comes from bringing the unwinder to a consistent state so that it properly unwind the stack.
    
    Reviewed By: ottoni
    
    Differential Revision: D28293952
    
    fbshipit-source-id: 216c47d2400af57bb89fd8d9df1acdc357b00a90

--- hphp/runtime/vm/jit/dce.cpp
+++ hphp/runtime/vm/jit/dce.cpp
@@ -989,13 +989,119 @@ void optimizeCatchBlocks(const BlockList& blocks,
                          IRUnit& unit,
                          const UseCounts& uses,
                          jit::fast_map<IRInstruction*, TrackedInstr>& rcInsts) {
-
+  FTRACE(1, "OptimizeCatchBlocks:vvvvvvvvvvvvvvvvvvvv\n");
+  SCOPE_EXIT { FTRACE(1, "OptimizeCatchBlocks:^^^^^^^^^^^^^^^^^^^^\n"); };
   for (auto block : blocks) {
     if (block->back().is(EndCatch) &&
         block->back().extra<EndCatch>()->mode !=
           EndCatchData::CatchMode::SideExit &&
         block->front().is(BeginCatch)) {
       processCatchBlock(unit, state, block, uses, rcInsts);
+    }
+  }
+}
+
+void optimizeConcats(jit::vector<IRInstruction*>& concats,
+                     DceState& state,
+                     IRUnit& unit,
+                     UseCounts& uses,
+                     jit::fast_map<IRInstruction*, TrackedInstr>& rcInsts) {
+  FTRACE(1, "OptimizeConcats:vvvvvvvvvvvvvvvvvvvv\n");
+  SCOPE_EXIT { FTRACE(1, "OptimizeConcats:^^^^^^^^^^^^^^^^^^^^\n"); };
+  auto const incref = [&] (auto inst, auto src) {
+    auto const blk = inst->block();
+    auto const ins = unit.gen(IncRef, inst->bcctx(), src);
+    blk->insert(blk->iteratorTo(inst), ins);
+    state[ins].setLive();
+    ++uses[src];
+    FTRACE(3, "Adding {}\n", ins->toString());
+  };
+  auto const decref = [&] (auto inst, auto src) {
+    auto const blk = inst->block();
+    auto const ins = unit.gen(DecRef, inst->bcctx(), DecRefData{}, src);
+    blk->insert(blk->iteratorTo(inst), ins);
+    state[ins].setLive();
+    ++uses[src];
+    FTRACE(3, "Adding {}\n", ins->toString());
+  };
+  auto const combine = [&] (auto inst, auto inst_prev,
+                            auto src1, auto src2, auto src3) {
+    /*
+     * ~~ Converting ~~
+     * t1 = ConcatStrStr a b (implicit Decref a)
+     * DecRef b
+     * t2 = ConcatStrStr t1 c (implicit Decref t1)
+     * DecRef c
+     *
+     * to
+     *
+     * IncRef a
+     * IncRef b
+     * t1 = ConcatStrStr a b (implicit Decref a)
+     * DecRef b
+     * t2 = ConcatStr3 a b c (implicit Decref a)
+     * DecRef b
+     * DecRef c
+
+     * ~~ Converting ~~
+     * t1 = ConcatStrStr a b (implicit Decref a)
+     * DecRef b
+     * t2 = ConcatStrStr c t1 (implicit Decref c)
+     * DecRef t1
+     *
+     * to
+     *
+     * IncRef a
+     * IncRef b
+     * t1 = ConcatStrStr a b (implicit Decref a)
+     * DecRef b
+     * t2 = ConcatStr3 c a b (implicit Decref c)
+     * DecRef a
+     * DecRef b
+     * DecRef t1
+     *
+     * We need to make sure refcounts are correct. We do this by increffing
+     * the sources of the first ConcatStrStr and then decreffing them after
+     * ConcatStr3 unless ConcatStr3 already consumes the reference
+     *
+     * Note that later stages of DCE will kill the extra ConcatStrStr and the
+     * refcounting.
+     */
+    assertx(inst_prev->is(ConcatStrStr));
+    assertx(inst->is(ConcatStrStr));
+    if (uses[inst_prev->dst()] == 1 + rcInsts[inst_prev].decs.size() +
+                                      rcInsts[inst_prev].stores.size()) {
+      FTRACE(3, "Combining {} into {}",
+             inst_prev->toString(), inst->toString());
+      auto next = inst->next();
+      unit.replace(inst, ConcatStr3, inst->taken(), src1, src2, src3);
+      inst->setNext(next);
+      FTRACE(3, " and got {}\n", inst->toString());
+      state[inst].setLive();
+      --uses[inst_prev->dst()];
+      ++uses[inst_prev->src(0)];
+      ++uses[inst_prev->src(1)];
+      // Incref the first source since the first ConcatStrStr controls
+      // its refcount
+      incref(inst_prev, inst_prev->src(0));
+      incref(inst_prev, inst_prev->src(1));
+      // ConcatStr3 ends the blocks, so insert the decrefs to the next block
+      assertx(inst->next() && !inst->next()->empty());
+      decref(&inst->next()->front(), src2);
+      if (src3 == inst_prev->src(1)) decref(&inst->next()->front(), src3);
+    }
+  };
+
+  for (auto& inst : concats) {
+    if (!inst->is(ConcatStrStr)) continue;
+    auto const src1 = inst->src(0);
+    auto const src2 = inst->src(1);
+    if (src1->inst()->is(ConcatStrStr)) {
+      combine(inst, src1->inst(),
+              src1->inst()->src(0), src1->inst()->src(1), src2);
+    } else if (src2->inst()->is(ConcatStrStr)) {
+      combine(inst, src2->inst(),
+              src1, src2->inst()->src(0), src2->inst()->src(1));
     }
   }
 }
@@ -1100,12 +1206,14 @@ void fullDCE(IRUnit& unit) {
 
   UseCounts uses(unit, 0);
   jit::fast_map<IRInstruction*, TrackedInstr> rcInsts;
+  jit::vector<IRInstruction*> concats;
 
   // process the worklist
   while (!wl.empty()) {
     auto* inst = wl.back();
     wl.pop_back();
     for (uint32_t ix = 0; ix < inst->numSrcs(); ++ix) {
+      if (inst->is(ConcatStrStr)) concats.emplace_back(inst);
       auto const src = inst->src(ix);
       IRInstruction* srcInst = src->inst();
       if (srcInst->op() == DefConst) continue;
@@ -1128,6 +1236,7 @@ void fullDCE(IRUnit& unit) {
   }
 
   optimizeCatchBlocks(blocks, state, unit, uses, rcInsts);
+  optimizeConcats(concats, state, unit, uses, rcInsts);
 
   // If every use of a dce-able PRc instruction is a DecRef or PureStore based
   // on its dst, then we can kill it, and DecRef any of its consumesReference

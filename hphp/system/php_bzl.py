6ffdafae98f7be12e165e5822adeaa10fb62df90
Stiopa Koltsov <nga@fb.com>
2021-06-08 20:32:49 -0700

    hphp buck: replace open call with generated .bzl file
    Summary:
    Buck Starlark does not provide `open` function to read `php.txt` in Buck files.
    
    This diff instead generates `php.bzl` file from `php.txt`.
    
    Buck test added to guarantee that `php.bzl` is in sync with `php.txt`.
    
    This diff is added to be able to convert hphp to Starlark (following diff D28953252), which is is part of an effort to switch fbcode Buck setup from Python DSL to Starlark.
    
    Reviewed By: DrMarcII
    
    Differential Revision: D28953253
    
    fbshipit-source-id: 3ab151c38b006af3f432d27314cfe264cd2a9700

---
+++ hphp/system/php_bzl.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+# Utility to generate or verify `php.bzl` from `php.txt`
+
+import sys
+import os.path
+
+def _read_file_and_gen_bzl(path: str) -> str:
+    r = ""
+    r += f"# This file is {'@'}generated from `php.txt`.\n"
+    r += "# Do not edit manually, run `python3 hphp/system/php_bzl.py` instead.\n"
+    r += "SYSTEMLIB_SRCS = [\n"
+    with open(path) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line or line.startswith("#"):
+                continue
+            r += f"    \"{line}\",\n"
+    r += "]\n"
+    return r
+
+def _generate():
+    php_txt = os.path.dirname(__file__) + "/php.txt"
+    php_bzl = os.path.dirname(__file__) + "/php.bzl"
+    contents = _read_file_and_gen_bzl(php_txt)
+    with open(php_bzl, mode="w") as f:
+        f.write(contents)
+
+def _verify(php_txt: str, php_bzl: str):
+    with open(php_bzl, mode="rb") as f:
+        expected = f.read()
+    assert isinstance(expected, bytes)
+    actual = _read_file_and_gen_bzl(php_txt)
+    actual = bytes(actual, "utf-8")
+    assert expected == actual, "file need to be regenerated"
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        # This is to be invoked manually when `php.txt` changes
+        _generate()
+    elif sys.argv[1] == "verify":
+        # This is invoked as Buck test
+        [php_txt, php_bzl] = sys.argv[2:]
+        _verify(php_txt, php_bzl)
+    else:
+        raise Exception("unknown mode")

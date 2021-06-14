31ba20f45b4bff4583f380b101eba192d3874c08
Katy Voor <voork@fb.com>
2021-06-14 12:42:47 -0700

    Null Coalesce Assignment Exception Tests
    Summary:
    We'd like to make `NullCoalesce` another SetOpOp when the rhs is side-effect free. This results in different exceptions thrown due to moving from 2 member op chains (MOpMode::Warn + MOpMode::Define) to just a member op chain in MOpMode::Define.
    
    There are no tests stressing the exceptions thrown by null coalesce assignment operator. Adding here to ensure future diffs do not change behavior and to note the current behavior.
    
    In particular, one thing that may need to change for the optimization to work, is for const properties to throw, when they are used on the lhs of a `??=`. Currently, when using a class const on the lhs, HackC throws a fatal (even for `??=` when it may _not_ write):
    ```
    Fatal error: Array-like class consts are not valid lvalues
    ```
    
    But const props have different behavior for `??=` and `=`. This will need to change for the optimization to work, as we do not know at the prop dim `d` whether we write or not, and therefore can't throw "Mutate const prop". We then lose this information when we move to the elem op.
    
    ```
    class A {
      <<__Const>> public dict<int, int> $d = dict[1 => 10];
    }
    
    $a = new A();
    $a->d[1] ??= 10; //ok
    $a->d[2] = 10; // fatal: Mutate const prop.
    
    ```
    
    Reviewed By: oulgen
    
    Differential Revision: D28983860
    
    fbshipit-source-id: e700d2c427aeb642b6f4f87cade963ea7950a806

---
+++ hphp/test/slow/null_coalesce_assignment/null_coalesce_assignment_exception.php
@@ -0,0 +1,123 @@
+<?hh
+
+class A {
+  <<__Const>> public int $i = 10;
+  <<__Const>> public dict<int, int> $d = dict[1 => 10];
+  <<__Const>> public dict<int, dict<int, int>> $dd = dict[1 => dict[2 => 3]];
+}
+
+<<__EntryPoint>>
+function main() {
+  echo "-------- Dict ---------------------------------------------------"."\n";
+  $d = dict[];
+  try {
+    $d['a'][0] ??= 10;
+  } catch (OutOfBoundsException $e) {
+    var_dump($e->getMessage());
+  }
+  try {
+    $d[1][0] ??= 10;
+  } catch (OutOfBoundsException $e) {
+    var_dump($e->getMessage());
+  }
+
+  echo "--------- Vec --------------------------------------------------"."\n";
+  $v = vec[];
+  try {
+    $v[1][0] ??= 10;
+  } catch (OutOfBoundsException $e) {
+    var_dump($e->getMessage());
+  }
+
+  echo "-------- Keyset ------------------------------------------------"."\n";
+  $k = keyset[];
+  $k ??= 10;
+  try {
+    $k[1] ??= 10;
+  } catch (InvalidOperationException $e) {
+    var_dump($e->getMessage());
+  }
+  try {
+    $k[2][0] ??= 10;
+  } catch (OutOfBoundsException $e) {
+    var_dump($e->getMessage());
+  }
+  try {
+    $k[2][0] = 10;
+  } catch (InvalidOperationException $e) {
+    var_dump($e->getMessage());
+  }
+
+  echo "-------- BaseL -------------------------------------------------"."\n";
+  try {
+    $a['a'] ??= 10;
+  } catch (UndefinedVariableException $e) {
+    var_dump($e->getMessage());
+  }
+  try {
+    $a['a'] = 10;
+  } catch (OutOfBoundsException $e) {
+    var_dump($e->getMessage());
+  }
+
+  echo "------- MutateConstProp ----------------------------------------"."\n";
+  // Exists
+  $a = new A();
+  $a->i ??= 10;
+  try {
+    $a->i = 10;
+  } catch (InvalidOperationException $e) {
+    var_dump($e->getMessage());
+  }
+
+  // Dim
+  try {
+    $a->d[2] ??= 10;
+  } catch (InvalidOperationException $e) {
+    var_dump($e->getMessage());
+  }
+  try {
+    $a->d[2] = 10;
+  } catch (InvalidOperationException $e) {
+    var_dump($e->getMessage());
+  }
+
+  // prop, short-circuit ok
+  $a->d[1] ??= 10;
+  try {
+    $a->d[1] = 10;
+  } catch (InvalidOperationException $e) {
+    var_dump($e->getMessage());
+  }
+
+  // elem, short-circuit ok
+  $a->dd[1][2] ??= 10;
+  try {
+    $a->dd[1][2] = 10;
+  } catch (InvalidOperationException $e) {
+    var_dump($e->getMessage());
+  }
+
+  echo "------ Dim property does not exist -----------------------------"."\n";
+  try {
+    $a = new A();
+    $a->g->a ??= 10;
+  } catch (UndefinedPropertyException $e) {
+    var_dump($e->getMessage());
+  }
+  try {
+    $a = new A();
+    $a->g->a = 10;
+    // throws generic exception
+  } catch (Exception $e) {
+    var_dump($e->getMessage());
+  }
+
+  echo "----- Dim MW ---------------------------------------------------"."\n";
+  // Dim when mcode is MW
+  try {
+    $d[][] = 10;
+  } catch (InvalidOperationException $e) {
+    var_dump($e->getMessage());
+  }
+}
---
+++ hphp/test/slow/null_coalesce_assignment/null_coalesce_assignment_exception1.php
@@ -0,0 +1,7 @@
+<?hh
+
+<<__EntryPoint>>
+function main() {
+  $d = dict[];
+  $d[][] ??= 10;
+}

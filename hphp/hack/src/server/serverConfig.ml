6c938b572829a7fa2236bcec171f3be58af893d2
Sankha Narayan Guria <ngsankha@fb.com>
2021-06-16 14:22:59 -0700

    Add enable sealed subclasses option to serverConfig
    Summary: Allows `enable_sealed_subclasses = true` to be set in .hhconfig
    
    Reviewed By: vassilmladenov
    
    Differential Revision: D29137266
    
    fbshipit-source-id: ee6776824130a4ccfb50d3b44f6dcb62b6a046cf

--- hphp/hack/src/server/serverConfig.ml
+++ hphp/hack/src/server/serverConfig.ml
@@ -455,6 +455,8 @@ let load ~silent config_filename options : t * ServerLocalConfig.t =
       ?tco_require_extends_implements_ancestors:
         (bool_opt "require_extends_implements_ancestors" config)
       ?tco_strict_value_equality:(bool_opt "strict_value_equality" config)
+      ?tco_enforce_sealed_subclasses:
+        (bool_opt "enable_sealed_subclasses" config)
       ()
   in
   Errors.allowed_fixme_codes_strict :=

6b68f082c6bfedfe63582d6a7874e28ea6304277
David Snider <dizzy@fb.com>
2021-06-22 11:52:27 -0700

    Switch Coercions test refactor
    Summary: as requested by alexeyt
    
    Differential Revision: D29068595
    
    fbshipit-source-id: d1a8990874e787330511fdef42bc9739ebed3312

---
+++ hphp/test/slow/implicit_conversions/switch/int_literal.php
@@ -0,0 +1,13 @@
+<?hh
+
+<<__EntryPoint>>
+function main() {
+  switch (30) {
+    case 30.0:
+      echo "case 30.0\n";
+      break;
+    case 30:
+      echo "case 30\n";
+      break;
+  }
+}
--- hphp/test/slow/implicit_conversions/switch/this.php
+++ hphp/test/slow/implicit_conversions/switch/this.php
@@ -2,15 +2,6 @@
 
 <<__EntryPoint>>
 function main() {
-  switch (30) {
-    case 30.0:
-      echo "case 30.0\n";
-      break;
-    case 30:
-      echo "case 30\n";
-      break;
-  }
-
   $x = new X();
   $x->bar(new stdClass);
   $x->bar($x);

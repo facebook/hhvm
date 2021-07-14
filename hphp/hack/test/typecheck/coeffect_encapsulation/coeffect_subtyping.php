1fcf789e8f20c5c5c9952f0913a0c7abb1ed32b5
Sankha Narayan Guria <ngsankha@fb.com>
2021-07-14 12:51:14 -0700

    tests for coeffect subtyping and lambdas
    Summary:
    Adds tests to check for the 2 scenarios:
    
    1. User-defined coeffects subtype correctly
    2. A lambda when defined gets the correct user defined coeffect
    
    Reviewed By: vassilmladenov
    
    Differential Revision: D29688558
    
    fbshipit-source-id: b7253d4949d31f4c7ee6af1d73fb4ed36108e346

---
+++ hphp/hack/test/typecheck/coeffect_encapsulation/coeffect_subtyping.php
@@ -0,0 +1,26 @@
+//// f.php
+<?hh
+<<file:__EnableUnstableFeatures('context_alias_declaration')>>
+
+newtype First as [] = [\HH\Contexts\defaults];
+
+//// g.php
+<?hh
+<<file:__EnableUnstableFeatures('context_alias_declaration')>>
+
+newtype Second as [\First] = [\HH\Contexts\defaults, \First];
+
+function int_op((function (int)[\First]: int) $f, int $i)[\First]: void {
+  $result = $f($i);
+}
+
+//// h.php
+<?hh
+
+// client code
+<<__EntryPoint>>
+function main()[\Second]: void {
+  int_op((int $inp) ==> {
+    return $inp + 4;
+  }, 5);
+}
---
+++ hphp/hack/test/typecheck/coeffect_encapsulation/lambda_inherits_coeffect.php
@@ -0,0 +1,26 @@
+//// f.php
+<?hh
+<<file:__EnableUnstableFeatures('context_alias_declaration')>>
+
+newtype Foo as [] = [\HH\Contexts\defaults];
+
+function enter_foo(
+  (function ()[\Foo]: void) $f
+)[\Foo]: void {
+  echo "Hello I can secretly do IO";
+  $f();
+}
+
+//// g.php
+<?hh
+<<file:__EnableUnstableFeatures('context_alias_declaration')>>
+
+// client code
+<<__EntryPoint>>
+function main()[\Foo]: void {
+  enter_foo(
+    () ==> {
+      echo "break purity"; // illegal!
+    }
+  );
+}

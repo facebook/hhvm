4422db66e250aa6c214a7bf3ef401bc7b2d1141b
Vassil Mladenov <vmladenov@fb.com>
2021-07-16 18:20:06 -0700

    Move defaults for class constants back to Migration
    Summary:
    This is a revert of
    - {D29447426 (https://github.com/facebook/hhvm/commit/eced89b3c3038ada28bdb2a3146891a5b3dba764)}
    - {D29475990 (https://github.com/facebook/hhvm/commit/805c544ba5394540591c5706a32e9735c39ffbf3)}
    
    The issue with constant initializers raised in the latter is resolved by {D29619639 (https://github.com/facebook/hhvm/commit/40584aca54e13db34a8197972c102d74ce881846)}, so the feature can be promoted to Migration.
    
    Reviewed By: oulgen
    
    Differential Revision: D29740876
    
    fbshipit-source-id: cf70720d5bd2597ef3d4f4539c73559921f3e811

--- hphp/hack/src/parser/rust_parser_errors.rs
+++ hphp/hack/src/parser/rust_parser_errors.rs
@@ -114,7 +114,7 @@ impl UnstableFeatures {
             UnstableFeatures::Readonly => Preview,
             UnstableFeatures::Modules => Unstable,
             UnstableFeatures::ContextAliasDeclaration => Unstable,
-            UnstableFeatures::ClassConstDefault => Unstable,
+            UnstableFeatures::ClassConstDefault => Migration,
         }
     }
 }

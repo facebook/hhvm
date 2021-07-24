0998e6938250cbb0ac5f97214d006c0df8d67b06
Jake Bailey (Hacklang) <jakebailey@fb.com>
2021-07-24 11:51:16 -0700

    Port Hhi module to Rust
    Summary:
    Port [the module which writes hhi files to disk](https://www.internalfb.com/code/fbsource/%5B374c32eb2dd3%5D/fbcode/hphp/hack/src/hhi/hhi.ml) to Rust, so that the decl service can write them to a tmpdir on startup instead of having the worker-prototype process do so.
    
    The Hhi module uses [a ppx transform](https://www.internalfb.com/code/fbsource/%5B374c32eb2dd3%5D/fbcode/hphp/hack/src/ppx/ppx_gen_hhi_common.ml?lines=94) to embed the contents of the hhi files [directly into the source of the Hhi module](https://www.internalfb.com/code/fbsource/%5B374c32eb2dd3%5D/fbcode/hphp/hack/src/hhi/hhi.ml?lines=13). I've done the same thing here by writing a Buck custom rule which generates a Rust file containing the hhi contents as a static const.
    
    Reviewed By: bobrenjc93
    
    Differential Revision: D29889674
    
    fbshipit-source-id: ad31fc4ea325c339b41dad4bdfa1d97d77922a1d

---
+++ hphp/hack/src/hhi/rust/gen_hhi_contents.rs
@@ -0,0 +1,75 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the "hack" directory of this source tree.
+
+use std::io::Write;
+use std::path::{Path, PathBuf};
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(no_version)] // don't consult CARGO_PKG_VERSION (buck doesn't set it)
+struct Options {
+    /// Directory containing subdirectories which should be merged into the
+    /// final hhi directory.
+    #[structopt(long, parse(from_os_str))]
+    hhi_dir: PathBuf,
+
+    /// The directory containing this stamp is the directory to search for HHIs
+    /// generated from the HSL. These will be placed in the final hhi directory
+    /// under a subdirectory named "hsl_generated".
+    #[structopt(long, parse(from_os_str))]
+    hsl_stamp: PathBuf,
+}
+
+fn main() {
+    let opts = Options::from_args();
+    let hsl_dir = opts.hsl_stamp.parent().unwrap();
+    let out_dir = std::env::var("OUT").unwrap();
+
+    let mut hhi_contents = vec![];
+
+    for entry in std::fs::read_dir(&opts.hhi_dir).unwrap() {
+        let hhi_dir = entry.unwrap().path();
+        hhi_contents.extend(get_hhis_in_dir(&hhi_dir));
+    }
+    hhi_contents.extend(
+        get_hhis_in_dir(&hsl_dir)
+            .map(|(path, contents)| (PathBuf::from("hsl_generated").join(path), contents)),
+    );
+
+    let out_filename = PathBuf::from(out_dir).join("lib.rs");
+    write_hhi_contents_file(&out_filename, &hhi_contents).unwrap();
+}
+
+fn get_hhis_in_dir(root: &Path) -> impl Iterator<Item = (PathBuf, String)> + '_ {
+    walkdir::WalkDir::new(root)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.file_type().is_file())
+        .filter(|e| e.path().extension().and_then(|s| s.to_str()) == Some("hhi"))
+        .map(move |e| {
+            let contents = std::fs::read_to_string(e.path()).unwrap();
+            let relative_path = e.path().strip_prefix(root).unwrap().to_owned();
+            (relative_path, contents)
+        })
+}
+
+fn write_hhi_contents_file(
+    out_filename: &Path,
+    hhi_contents: &[(PathBuf, String)],
+) -> std::io::Result<()> {
+    let mut out_file = std::fs::File::create(&out_filename)?;
+    writeln!(out_file, "pub const HHI_CONTENTS: &[(&str, &str)] = &[")?;
+    for (path, contents) in hhi_contents {
+        writeln!(
+            out_file,
+            "    (\"{}\", r###\"{}\"###),",
+            path.display(),
+            contents
+        )?;
+    }
+    writeln!(out_file, "];")?;
+    Ok(())
+}

e60ff516fc21773cfc9919134f8b5e95ad9674a9
David Snider <dizzy@fb.com>
2021-07-26 17:48:29 -0700

    turn on comparison coercion notices by default
    Differential Revision: D29625443
    
    fbshipit-source-id: ba7e1e9b849667b8188ac75e1467980513e68cf0

--- hphp/runtime/base/runtime-option.h
+++ hphp/runtime/base/runtime-option.h
@@ -1391,7 +1391,7 @@ struct RuntimeOption {
   /* 0 nothing, 1 notice, 2 error */                                    \
   F(int32_t, NoticeOnCoerceForMath, 1)                                  \
   /* 0 nothing, 1 notice, 2 error */                                    \
-  F(int32_t, NoticeOnCoerceForCmp, 0)                                   \
+  F(int32_t, NoticeOnCoerceForCmp, 1)                                   \
   /* 0 nothing, 1 notice, 2 error, 3 behaviour change */                \
   F(int32_t, NoticeOnCoerceForEq, 0)                                    \
   F(string, TaoMigrationOverride, std::string(""))                      \

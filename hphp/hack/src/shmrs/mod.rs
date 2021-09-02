9c58d48892e73e06cc550815d8b3076f66545401
Henri Verroken <hverr@fb.com>
2021-09-02 00:45:30 -0700

    Implement a concurrent hash map using sharding
    Summary:
    Implement a concurrent hash map that lives in shared memory and uses lock-protected shard.
    
    Note that the initialization code is somewhat ugly. But I don't particularly mind it. It tries to be idiomatic unsafe Rust code (if there is such a thing) using `MaybeUninit` where necessary.
    
    Reviewed By: mjhostet
    
    Differential Revision: D30069844
    
    fbshipit-source-id: 63d1639535f9bf9175e214519716486132738e34

--- hphp/hack/src/shmrs/mod.rs
+++ hphp/hack/src/shmrs/mod.rs
@@ -3,9 +3,11 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
 #![feature(maybe_uninit_ref)]
+#![feature(maybe_uninit_array_assume_init)]
 #![feature(allocator_api)]
 
 /// A Rust library for interacting with shared memory.
+pub mod chashmap;
 pub mod error;
 pub mod filealloc;
 pub mod hashmap;

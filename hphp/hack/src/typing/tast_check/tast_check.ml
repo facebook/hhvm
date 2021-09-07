b4cba2799e48eeea29d5141df008bf64a7ff22ef
Henri Verroken <hverr@fb.com>
2021-09-07 02:14:30 -0700

    Make more hierarchy checks skippable
    Summary: Make more hierarchy checks skippable
    
    Reviewed By: andrewjkennedy
    
    Differential Revision: D30632443
    
    fbshipit-source-id: 9a88c794129570ae492ca5bc6d45697da670d9b3

--- hphp/hack/src/typing/tast_check/tast_check.ml
+++ hphp/hack/src/typing/tast_check/tast_check.ml
@@ -27,49 +27,67 @@ let visitor ctx =
     | Some level when level > 0 -> [Bad_type_logger.handler]
     | _ -> []
   in
+  let skip_hierarchy_checks =
+    TypecheckerOptions.skip_hierarchy_checks (Provider_context.get_tcopt ctx)
+  in
   Tast_visitor.iter_with
     (handlers
     @ irregular_handlers
-    @ [
-        Shape_field_check.handler;
-        String_cast_check.handler;
-        Tautology_check.handler;
-        Enforceable_hint_check.handler;
-        Const_write_check.handler;
-        Switch_check.handler (fun t ->
-            if TypecheckerOptions.disallow_scrutinee_case_value_type_mismatch t
-            then
-              Errors.invalid_switch_case_value_type
-            else
-              fun _ _ _ ->
-            ());
-        Void_return_check.handler;
-        Rvalue_check.handler;
-        Callconv_check.handler;
-        Xhp_check.handler;
-        Discarded_awaitable_check.handler;
-        Invalid_index_check.handler;
-        Pseudofunctions_check.handler;
-        Reified_check.handler;
-        Instantiability_check.handler;
-        Static_memoized_check.handler;
-        Abstract_class_check.handler;
-        Class_parent_check.handler;
-        Method_type_param_check.handler;
-        Obj_get_check.handler;
-        This_hint_check.handler;
-        Unresolved_type_variable_check.handler;
-        Invalid_arraykey_constraint_check.handler;
-        Type_const_check.handler;
-        Static_method_generics_check.handler;
-        Class_inherited_member_case_check.handler;
-        Ifc_tast_check.handler;
-        Readonly_check.handler;
-        Meth_caller_check.handler;
-        Expression_tree_check.handler;
-        Class_const_origin_check.handler;
-        Enum_classes_check.handler;
-      ])
+    @ List.filter_map
+        ~f:Fn.id
+        [
+          Some Shape_field_check.handler;
+          Some String_cast_check.handler;
+          Some Tautology_check.handler;
+          Some Enforceable_hint_check.handler;
+          Some Const_write_check.handler;
+          Some
+            (Switch_check.handler (fun t ->
+                 if
+                   TypecheckerOptions
+                   .disallow_scrutinee_case_value_type_mismatch
+                     t
+                 then
+                   Errors.invalid_switch_case_value_type
+                 else
+                   fun _ _ _ ->
+                 ()));
+          Some Void_return_check.handler;
+          Some Rvalue_check.handler;
+          Some Callconv_check.handler;
+          Some Xhp_check.handler;
+          Some Discarded_awaitable_check.handler;
+          Some Invalid_index_check.handler;
+          Some Pseudofunctions_check.handler;
+          Some Reified_check.handler;
+          Some Instantiability_check.handler;
+          Some Static_memoized_check.handler;
+          Some Abstract_class_check.handler;
+          (if skip_hierarchy_checks then
+            None
+          else
+            Some Class_parent_check.handler);
+          Some Method_type_param_check.handler;
+          Some Obj_get_check.handler;
+          Some This_hint_check.handler;
+          Some Unresolved_type_variable_check.handler;
+          Some Invalid_arraykey_constraint_check.handler;
+          Some Type_const_check.handler;
+          Some Static_method_generics_check.handler;
+          (if skip_hierarchy_checks then
+            None
+          else
+            Some Class_inherited_member_case_check.handler);
+          Some Ifc_tast_check.handler;
+          Some Readonly_check.handler;
+          Some Meth_caller_check.handler;
+          Some Expression_tree_check.handler;
+          (if skip_hierarchy_checks then
+            None
+          else
+            Some Class_const_origin_check.handler);
+          Some Enum_classes_check.handler;
+        ])
 
 let program ctx = (visitor ctx)#go ctx
 

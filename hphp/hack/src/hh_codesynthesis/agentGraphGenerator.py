ed3edbd5032e680284fd45ab493f98ed42cb4f9b
XiangyuGuo <tobielf@gmail.com>
2021-09-27 15:31:21 -0700

    Add comment and a set of command line options.

--- hphp/hack/src/hh_codesynthesis/agentGraphGenerator.py
+++ hphp/hack/src/hh_codesynthesis/agentGraphGenerator.py
@@ -5,7 +5,12 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the "hack" directory of this source tree.
 #
-#
+# This library will construct a logic program using the given parameter,
+# invoke Clingo to solve the logic program to produce an agent graph,
+# and we create multiple agents in parallel to produce valid codebase.
+import argparse
+import sys
+
 from typing import List, Optional, Set, Union
 
 import clingo
@@ -13,7 +18,10 @@ from clingo.symbol import Number, Symbol
 
 
 class AgentGraphClingoContext:
-    """ """
+    """Context class interact with Python and Clingo.
+    We can customize each value, range, and function to experiment on
+    different settings. Refer to the test case `test_small_agent_graph`
+    for more detail usages."""
 
     number_of_leaves = 30
     number_of_infra_agents = 20
@@ -65,6 +73,8 @@ class AgentGraphClingoContext:
         tgt_agent_number = target_agent.number
         src_level_number = source_level.number
         tgt_level_number = target_level.number
+        # Since clingo doesn't support Boolean return, we are returning
+        # Number(1) for true, and Number(0) for false.
         if (
             src_agent_number < tgt_agent_number + 200
             and tgt_agent_number < src_agent_number
@@ -79,6 +89,8 @@ class AgentGraphClingoContext:
     ) -> Symbol:
         src_agent_number = source_agent.number
         tgt_agent_number = target_agent.number
+        # Since clingo doesn't support Boolean return, we are returning
+        # Number(1) for true, and Number(0) for false.
         if tgt_agent_number * 3 + 200 < src_agent_number:
             return Number(1)
         else:
@@ -86,6 +98,13 @@ class AgentGraphClingoContext:
 
 
 class AgentGraphGenerator:
+    """A generator that could produce an agent graph. We are providing an
+    evaluation function along with an actual generate function. So that
+    the user can do a dry run to evaluate the quality of actual graph.
+    The user must specify which on_model is going to use."""
+
+    _raw_model = ""
+
     def __init__(self) -> None:
         self.infra_agents: List[int] = []
         self.product_agents: List[int] = []
@@ -132,16 +151,22 @@ class AgentGraphGenerator:
         similar_agents = 0
         checked_agents = set()
         similar_relations = []
-        for key, value in enumerate(self.edges):
-            for number, agent in enumerate(self.edges[key + 1 :]):
-                if value == agent and len(value) != 0 and number not in checked_agents:
+        for outer_agent, outer_dependents in enumerate(self.edges):
+            for inner_agent, inner_dependents in enumerate(
+                self.edges[outer_agent + 1 :]
+            ):
+                if (
+                    outer_dependents == inner_dependents
+                    and len(outer_dependents) != 0
+                    and inner_agent not in checked_agents
+                ):
                     similar_agents += 1
-                    checked_agents.add(number)
-                    if agent not in similar_relations:
-                        similar_relations.append(agent)
+                    checked_agents.add(inner_agent)
+                    if inner_dependents not in similar_relations:
+                        similar_relations.append(inner_dependents)
 
         print("There are {0} similar agents.".format(similar_agents))
-        print(similar_relations)
+        print("Common dependents set is {0}".format(similar_relations))
 
     def generate_raw(self, m: clingo.Model) -> None:
         self._raw_model = m.__str__()
@@ -150,6 +175,8 @@ class AgentGraphGenerator:
         raise RuntimeError("Must specify a valid method.")
 
 
+# Generate a set of agents using given distribution. The distribution has the
+# critical path requirement (levels) as well as how many roots are in the graph.
 def generating_agent_distribution(agent_distribution: List[int]) -> List[str]:
     if not all(i > 0 for i in agent_distribution):
         raise RuntimeError("Agent distribution must have all positive integers.")
@@ -164,6 +191,7 @@ def generating_agent_distribution(agent_distribution: List[int]) -> List[str]:
     ]
 
 
+# Take in an agent distribution and a generator to create an agent graph.
 def generating_an_agent_graph(
     agent_distribution: List[int], generator: AgentGraphGenerator
 ) -> None:
@@ -187,27 +215,52 @@ def generating_an_agent_graph(
             raise RuntimeError("Unsatisfiable.")
 
 
-if __name__ == "__main__":
+def main() -> int:
     agent_graph = AgentGraphGenerator()
+
     agent_graph.on_model = agent_graph.generate_raw
 
-    scale = 1
+    # Parse the arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--agents", nargs="+", type=int)
 
-    agent_distribution = [5, 10, 10, 10, 10, 10, 10, 15, 20]
-    agent_distribution = [2, 4, 4]
-    agent_distribution = [x * scale for x in agent_distribution]
+    parser.add_argument("--number_of_infra_agents", type=int)
+    parser.add_argument("--number_of_product_agents", type=int)
+    parser.add_argument("--number_of_leaves", type=int)
 
-    AgentGraphClingoContext.number_of_infra_agents = 2 * scale
-    AgentGraphClingoContext.number_of_product_agents = 6 * scale
-    AgentGraphClingoContext.number_of_leaves = 5
-    AgentGraphClingoContext.infra_agent_profile = {
-        "in_degree": [0, 10],
-        "out_degree": [1, 10],
-    }
-    AgentGraphClingoContext.product_agent_profile = {
-        "in_degree": [1, 10],
-        "out_degree": [0, 5],
-    }
+    parser.add_argument("--infra_agent_indegrees", nargs=2, type=int)
+    parser.add_argument("--infra_agent_outdegrees", nargs=2, type=int)
+    parser.add_argument("--product_agent_indegrees", nargs=2, type=int)
+    parser.add_argument("--product_agent_outdegrees", nargs=2, type=int)
 
+    parser.add_argument('--evaluate', action=argparse.BooleanOptionalAction)
+
+    args: argparse.Namespace = parser.parse_args()
+
+    agent_distribution = args.agents or [5, 10, 10, 10, 10, 10, 10, 15, 20]
+    AgentGraphClingoContext.number_of_infra_agents = args.number_of_infra_agents or 20
+    AgentGraphClingoContext.number_of_product_agents = (
+        args.number_of_product_agents or 60
+    )
+    AgentGraphClingoContext.number_of_leaves = args.number_of_leaves or 30
+    AgentGraphClingoContext.infra_agent_profile[
+        "in_degree"
+    ] = args.infra_agent_indegrees or [0, 10]
+    AgentGraphClingoContext.infra_agent_profile[
+        "out_degree"
+    ] = args.infra_agent_outdegrees or [5, 100]
+    AgentGraphClingoContext.product_agent_profile[
+        "in_degree"
+    ] = args.product_agent_indegrees or [5, 20]
+    AgentGraphClingoContext.product_agent_profile[
+        "out_degree"
+    ] = args.product_agent_outdegrees or [0, 5]
+
+    if args.evaluate:
+        agent_graph.on_model = agent_graph.evaluate
     generating_an_agent_graph(agent_distribution, agent_graph)
     print(agent_graph._raw_model)
+
+
+if __name__ == "__main__":
+    sys.exit(main())

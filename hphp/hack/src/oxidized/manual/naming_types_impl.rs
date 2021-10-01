3b54a6f401774f4c659d2c1ba901b63980baeb77
Lucian Wischik <ljw@fb.com>
2021-09-30 21:20:38 -0700

    converge Naming_types 2/2
    Summary:
    This diff completes the switchover for all places to use the same numeric values for naming-sqlite, defined in a single place in the source code.
    
    (The weird thing is that we store structured Naming_types.name_kind in sqlite, but the numeric values are defined for the flat FileInfo.name_type.)
    
    Differential Revision: D31251399
    
    fbshipit-source-id: fdf81bfc4ff59c54def6eb51143e724038aa787e

--- hphp/hack/src/oxidized/manual/naming_types_impl.rs
+++ hphp/hack/src/oxidized/manual/naming_types_impl.rs
@@ -5,6 +5,7 @@
 
 use std::convert::TryFrom;
 
+use crate::file_info::NameType;
 use crate::gen::naming_types::{KindOfType, NameKind};
 
 impl TryFrom<i64> for NameKind {
@@ -12,13 +13,20 @@ impl TryFrom<i64> for NameKind {
 
     fn try_from(kind: i64) -> Result<Self, String> {
         // Must be kept in sync with naming_types.ml and namekind_to_i64
-        match kind {
-            0 => Ok(NameKind::TypeKind(KindOfType::TClass)),
-            1 => Ok(NameKind::TypeKind(KindOfType::TTypedef)),
-            2 => Ok(NameKind::TypeKind(KindOfType::TRecordDef)),
-            3 => Ok(NameKind::FunKind),
-            4 => Ok(NameKind::ConstKind),
-            _ => Err(format!("Out of range for KindOfType: {}", kind)),
+        // name_type.ml is where the numerical values are defined, and NameType in rust is
+        // generated from it, and all other places including namkind_to_i64 just use it.
+        if kind == (NameType::Class as i64) {
+            Ok(NameKind::TypeKind(KindOfType::TClass))
+        } else if kind == (NameType::Typedef as i64) {
+            Ok(NameKind::TypeKind(KindOfType::TTypedef))
+        } else if kind == (NameType::RecordDef as i64) {
+            Ok(NameKind::TypeKind(KindOfType::TRecordDef))
+        } else if kind == (NameType::Fun as i64) {
+            Ok(NameKind::FunKind)
+        } else if kind == (NameType::Const as i64) {
+            Ok(NameKind::ConstKind)
+        } else {
+            Err("oops".to_owned())
         }
     }
 }

7d086f36c3ea3b011b7fdb76718a217b13f1b11e
Katy Voor <voork@fb.com>
2021-10-01 16:18:43 -0700

    Convert the decl entrypoint to cxx [1/2] (#8903)
    Summary:
    Pull Request resolved: https://github.com/facebook/hhvm/pull/8903
    
    - Convert the decl entrypoint to cxx
    - Pass around boxes instead of pointers
    - Note: `DeclParserOptions` is returned in a Box because "returning opaque Rust type by value is not supported".
    
    Next diff will be adding back the Decl Providers
    
    Reviewed By: shayne-fletcher
    
    Differential Revision: D31292529
    
    fbshipit-source-id: d59737834496f48d47dd4a9085034d08ca6a04be

--- hphp/runtime/vm/hhvm_decl_provider.h
+++ hphp/runtime/vm/hhvm_decl_provider.h
@@ -16,7 +16,7 @@
 
 #pragma once
 
-#include "hphp/hack/src/decl/cpp_ffi/decl_ffi_types.h"
+#include "hphp/hack/src/decl/ffi_bridge/rust_decl_ffi_bridge.rs"
 #include "hphp/runtime/base/autoload-map.h"
 #include "hphp/runtime/base/type-string.h"
 
@@ -32,11 +32,11 @@ struct HhvmDeclProvider {
   HhvmDeclProvider(HhvmDeclProvider const&) = delete;
   HhvmDeclProvider& operator=(HhvmDeclProvider const&) = delete;
 
-  hackc::decl::decls const* getDecl(HPHP::AutoloadMap::KindOf kind, char const* symbol);
+  Decls const* getDecl(HPHP::AutoloadMap::KindOf kind, char const* symbol);
 
  private:
-  std::map<std::string, std::pair<hackc::decl::decl_result, hackc::decl::bump_allocator const*>> m_cache;
+  std::map<std::string, std::pair<DeclResult, ::rust::Box<Bump>>> m_cache;
 };
 
-hackc::decl::decls const* hhvm_decl_provider_get_decl(void* provider, char const* symbol);
+Decls const* hhvm_decl_provider_get_decl(void* provider, char const* symbol);
 }

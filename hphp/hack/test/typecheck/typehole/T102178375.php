b08431b2ab43c72a3dec41ed99a93ebd12f9725c
Scott Owens <sowens@fb.com>
2021-10-04 03:29:09 -0700

    Test case for type hole
    Summary: Type hole from a bad interaction with refinement and `dynamic`.
    
    Reviewed By: vsiles
    
    Differential Revision: D31341261
    
    fbshipit-source-id: 9f8e53f23044716f28dc9a01a10296767d6a08af

---
+++ hphp/hack/test/typecheck/typehole/T102178375.php
@@ -0,0 +1,24 @@
+<?hh
+class C<T> {}
+class S<T> extends C<T> {
+  public function __construct(private T $x) {}
+  public function get() : T { return $this->x; }
+}
+
+function f(bool $b, dynamic $d, C<int> $c) : vec<int> {
+  if ($b) {
+    $x = $d;
+  } else {
+    $x = $c;
+  }
+  $x as S<_>;
+  return vec[$x->get()];
+}
+
+function expect_int(int $i) : void {}
+
+<<__EntryPoint>>
+function main() : void {
+  $vi = f(true, new S<string>("1"), new C());
+  expect_int($vi[0]);
+}

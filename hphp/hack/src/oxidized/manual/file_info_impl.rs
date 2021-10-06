66981dcca6221a5bf9c5d4aeb4440bcc6f3bb844
Lucian Wischik <ljw@fb.com>
2021-10-06 10:53:21 -0700

    FILENAME 4/4 - naming_sqlite.rs uses NameType not NameKind
    Summary:
    My goal is to have a clean implementation of the FILENAME action all the way from Naming_provider through to decl-service-worker.
    
    There are two types in ocaml, `Naming_types.name_kind` which has structure and `FileInfo.name_type` which doesn't. The ocaml codebase uses both, mostly through historical legacy of code which (pointlessly) uses `Naming_types.kind_of_type`.
    
    The decl service doesn't need that legacy. I've switched over to using `file_info::NameType` everywhere in the decl service. Indeed, the only consumer of it was Naming_provider.ml, which always wanted a NameType, so all the intermediate uses of NameKind were just a waste.
    
    I also moved `impl FromSql for SqliteNameKind` in naming_sqlite/datatypes.rs into `impl FromSql for NameType` in oxidized/file_info_impl.rs. It feels a little bit awkward that a really tiny atomic type like file_info should depend upon rusqlite, but only a little bit.
    
    Differential Revision: D31326773
    
    fbshipit-source-id: 1acd6394f10dd11a4e19179be2a0373cac8ece8a

--- hphp/hack/src/oxidized/manual/file_info_impl.rs
+++ hphp/hack/src/oxidized/manual/file_info_impl.rs
@@ -3,11 +3,13 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
 
-use std::convert::{From, TryFrom};
+use std::convert::From;
 
 use crate::gen::file_info::Mode;
 use crate::gen::file_info::NameType;
 use crate::gen::naming_types::KindOfType;
+use rusqlite::types::FromSql;
+use rusqlite::types::{FromSqlError, FromSqlResult, ValueRef};
 
 impl Mode {
     pub fn from_string(s: &str) -> Option<Self> {
@@ -19,27 +21,35 @@ impl Mode {
     }
 }
 
-impl TryFrom<u32> for NameType {
-    type Error = String;
-
-    fn try_from(kind: u32) -> Result<Self, String> {
-        match kind {
-            0 => Ok(NameType::Fun),
-            1 => Ok(NameType::Class),
-            2 => Ok(NameType::RecordDef),
-            3 => Ok(NameType::Typedef),
-            4 => Ok(NameType::Const),
-            _ => Err(format!("Out of range for NameType: {}", kind)),
-        }
-    }
-}
-
 impl From<KindOfType> for NameType {
     fn from(kind: KindOfType) -> Self {
         match kind {
             KindOfType::TClass => NameType::Class,
             KindOfType::TTypedef => NameType::Typedef,
             KindOfType::TRecordDef => NameType::RecordDef,
+        }
+    }
+}
+
+impl FromSql for NameType {
+    fn column_result(value: ValueRef<'_>) -> FromSqlResult<Self> {
+        match value {
+            ValueRef::Integer(i) => {
+                if i == NameType::Fun as i64 {
+                    Ok(NameType::Fun)
+                } else if i == NameType::Const as i64 {
+                    Ok(NameType::Const)
+                } else if i == NameType::Class as i64 {
+                    Ok(NameType::Class)
+                } else if i == NameType::Typedef as i64 {
+                    Ok(NameType::Typedef)
+                } else if i == NameType::RecordDef as i64 {
+                    Ok(NameType::RecordDef)
+                } else {
+                    Err(FromSqlError::OutOfRange(i))
+                }
+            }
+            _ => Err(FromSqlError::InvalidType),
         }
     }
 }

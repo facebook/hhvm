37455ff3d56581fb6b6d1279075b3b148d2162e8
Bob Ren <bobren@fb.com>
2021-10-18 14:41:51 -0700

    Only invalidate shallow classes in parsed instead of whole transitive dependency
    Summary:
    Addresses comments in D31722829 (https://github.com/facebook/hhvm/commit/32cf6d56d16a23707ef7d146baaa670d0a4cf23f). Copying here for ease
    
    > I think we need a more narrowly targeted fix here! Deleting large swaths of the shallow classes heap will help cover up bugs where we write the wrong decl to shmem, but it'll blow up fanouts for future incremental checks in shallow-fanout mode (since we won't have old decls to compare against). That's a pretty serious negative effect (which won't be exposed by mass-replay, since it only arises with repeated incremental checks)!
    
    > We're likely to recompute the shallow decls of the changed classes before the next incremental check, since those are definitely scheduled for typechecking (we may not get the chance, though, since our typecheck could be interrupted). But the files containing the descendant classes may not be scheduled for rechecking at all, so we may not recompute their decls before the next incremental check.
    
    Differential Revision: D31740682
    
    fbshipit-source-id: 2c238f5b4d900b1ab60a91592fa8e04ae590f5f6

--- hphp/hack/src/decl/decl_redecl_service.ml
+++ hphp/hack/src/decl/decl_redecl_service.ml
@@ -567,9 +567,21 @@ let invalidate_folded_classes_for_shallow_fanout
   in
   Decl_class_elements.remove_old_all (get_elems invalidated ~old:true);
   Decl_class_elements.remove_all (get_elems invalidated ~old:false);
-  Shallow_classes_provider.remove_batch ctx invalidated;
   Decl_heap.Classes.remove_old_batch invalidated;
   Decl_heap.Classes.remove_batch invalidated;
+  SharedMem.GC.collect `gentle;
+  ()
+
+let invalidate_shallow_classes_for_shallow_fanout
+    ctx
+    {
+      FileInfo.n_funs = _;
+      n_classes;
+      n_record_defs = _;
+      n_types = _;
+      n_consts = _;
+    } =
+  Shallow_classes_provider.remove_batch ctx n_classes;
   SharedMem.GC.collect `gentle;
   ()
 
@@ -649,6 +661,8 @@ let redo_type_decl
         ~bucket_size
         ~get_classes_in_file:get_classes
         changed';
+
+      invalidate_shallow_classes_for_shallow_fanout ctx all_defs;
 
       let changed = DepSet.union changed changed' in
       let to_recheck = DepSet.union to_recheck needs_recheck in

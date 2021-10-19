95494e5b61ae6dbc873ee3f28cd2f56b9b00dc3e
Vincent Siles <vsiles@fb.com>
2021-10-19 00:06:53 -0700

    fix/restore sync between ocaml and rust parser options
    Summary:
    D31603441 (https://github.com/facebook/hhvm/commit/a4656f2431b5a5f0fdb3caa30b034ca9d2c9dc79) (and probably one before that) broke the synchronization between
    ocaml and rust parser options encoding.
    
    I resync'ed the numbers and added the missing options
    
    Reviewed By: Wilfred
    
    Differential Revision: D31726345
    
    fbshipit-source-id: 611b4d348d4956ad58f057ce8d02ae33c324a247

--- hphp/hack/src/parser/rust_parser_errors_ffi.rs
+++ hphp/hack/src/parser/rust_parser_errors_ffi.rs
@@ -24,6 +24,7 @@ unsafe fn parser_options_from_ocaml_only_for_parser_errors(
 ) -> (ParserOptions, (bool, bool, bool, bool)) {
     let ocaml_opts = ocaml_opts.as_usize() as *const usize;
 
+    // Keep in sync with src/options/parserOptions.ml
     let hhvm_compat_mode = bool::from_ocaml(*ocaml_opts.add(0)).unwrap();
     let hhi_mode = bool::from_ocaml(*ocaml_opts.add(1)).unwrap();
     let codegen = bool::from_ocaml(*ocaml_opts.add(2)).unwrap();
@@ -45,8 +46,11 @@ unsafe fn parser_options_from_ocaml_only_for_parser_errors(
     let po_const_default_lambda_args = bool::from_ocaml(*ocaml_opts.add(16)).unwrap();
     let po_disable_array_typehint = bool::from_ocaml(*ocaml_opts.add(17)).unwrap();
     let po_allow_unstable_features = bool::from_ocaml(*ocaml_opts.add(18)).unwrap();
-    let po_disallow_fun_and_cls_meth_pseudo_funcs = bool::from_ocaml(*ocaml_opts.add(20)).unwrap();
-    let po_disallow_inst_meth = bool::from_ocaml(*ocaml_opts.add(22)).unwrap();
+    let po_disallow_fun_and_cls_meth_pseudo_funcs = bool::from_ocaml(*ocaml_opts.add(19)).unwrap();
+    let po_interpret_soft_types_as_like_types = bool::from_ocaml(*ocaml_opts.add(20)).unwrap();
+    let po_disallow_inst_meth = bool::from_ocaml(*ocaml_opts.add(21)).unwrap();
+    let po_enable_readonly_in_emitter = bool::from_ocaml(*ocaml_opts.add(22)).unwrap();
+    let po_escape_brace = bool::from_ocaml(*ocaml_opts.add(23)).unwrap();
     let tco_enable_systemlib_annotations = bool::from_ocaml(*ocaml_opts.add(24)).unwrap();
 
     parser_options.po_disable_lval_as_an_expression = po_disable_lval_as_an_expression;
@@ -54,7 +58,6 @@ unsafe fn parser_options_from_ocaml_only_for_parser_errors(
     parser_options.tco_const_static_props = tco_const_static_props;
     parser_options.po_disable_legacy_attribute_syntax = po_disable_legacy_attribute_syntax;
     parser_options.po_const_default_func_args = po_const_default_func_args;
-    parser_options.po_const_default_lambda_args = po_const_default_lambda_args;
     parser_options.po_abstract_static_props = po_abstract_static_props;
     parser_options.po_disallow_func_ptrs_in_constants = po_disallow_func_ptrs_in_constants;
     parser_options.po_enable_xhp_class_modifier = po_enable_xhp_class_modifier;
@@ -63,11 +66,15 @@ unsafe fn parser_options_from_ocaml_only_for_parser_errors(
     parser_options.po_enable_enum_classes = po_enable_enum_classes;
     parser_options.po_disable_modes = po_disable_modes;
     parser_options.po_disable_array = po_disable_array;
+    parser_options.po_const_default_lambda_args = po_const_default_lambda_args;
     parser_options.po_disable_array_typehint = po_disable_array_typehint;
     parser_options.po_allow_unstable_features = po_allow_unstable_features;
     parser_options.po_disallow_fun_and_cls_meth_pseudo_funcs =
         po_disallow_fun_and_cls_meth_pseudo_funcs;
+    parser_options.po_interpret_soft_types_as_like_types = po_interpret_soft_types_as_like_types;
     parser_options.po_disallow_inst_meth = po_disallow_inst_meth;
+    parser_options.po_enable_readonly_in_emitter = po_enable_readonly_in_emitter;
+    parser_options.po_escape_brace = po_escape_brace;
     parser_options.tco_enable_systemlib_annotations = tco_enable_systemlib_annotations;
     (
         parser_options,

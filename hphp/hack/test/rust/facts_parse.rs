d3d6a1ad2181452cffcc49c7d7dd54806dafffc6
Edwin Smith <smith@fb.com>
2021-10-20 08:19:27 -0700

    use -Drust-2018-idioms outside hack/src (14/N)
    Summary:
    These include RUST_FLAGS from hack/src/defs.bzl so they need to
    be migrated too.
    
    Reviewed By: shayne-fletcher
    
    Differential Revision: D31729489
    
    fbshipit-source-id: e0c03ff41d30ed7b365bc6ec48122e938aef3fa8

--- hphp/hack/test/rust/facts_parse.rs
+++ hphp/hack/test/rust/facts_parse.rs
@@ -4,8 +4,6 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.;
 
-extern crate clap;
-
 use std::fs;
 use std::path::PathBuf;
 

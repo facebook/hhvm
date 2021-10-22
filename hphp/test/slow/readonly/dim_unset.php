b27ad5d5ea2308a8181174875d779d0ed16651a2
Katy Voor <voork@fb.com>
2021-10-21 23:11:33 -0700

    Convert hhas test to hack test
    Summary: New verification constraint showed this hhas lagging behind. Lets just convert.
    
    Reviewed By: ricklavoie
    
    Differential Revision: D31846957
    
    fbshipit-source-id: 0f996760be9394ea93de2441693eb7499531c729

---
+++ hphp/test/slow/readonly/dim_unset.php
@@ -0,0 +1,23 @@
+<?hh
+
+class Foo2 {
+  public int $x;
+  public function __construct(num $x = 0) {
+    $this->x = $x;
+  }
+}
+
+class Foo1 {
+  public vec<Foo2> $f2;
+  public readonly vec<Foo2> $ro_f2;
+}
+
+<<__EntryPoint>>
+function test(): void {
+  $t = new Foo1();
+  $t1 = new Foo1();
+  $t->f2 = vec[new Foo2()];
+  unset($t->f2[0]->x);
+  $t1->ro_f2 = vec[new Foo2()];
+  unset($t1->ro_f2[0]->x);
+}

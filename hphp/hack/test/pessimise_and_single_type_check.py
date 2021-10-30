ab432d8cf57467bd7df6edbd981e49441deb3869
Francesco Zappa Nardelli <fzn@fb.com>
2021-10-29 20:53:54 -0700

    pessimise hhi passed via --extra-builtin when running the pessimisation test suite
    Summary:
    The `typecheck` test suite in some cases passes ad-hoc hhi files to hh_single_type_check via the `--extra-builtin` option.  This diff ensures that when running the pessimisation test suite the hhi passed via the `--extra-builtin` option are themselves pessimised.
    
    In detail, whenever an option `--extra-builtin path/file.hhi` is encountered, `file.hhi` is pessimised in `path/pessimised_hhi/file.hhi` and the option rewritten into `--extra-builtin path/pessimised_hhi/file.hhi`.
    
    Reviewed By: vassilmladenov
    
    Differential Revision: D31957407
    
    fbshipit-source-id: e880e397427a5dbca3bb88e3d84f589a297c9988

--- hphp/hack/test/pessimise_and_single_type_check.py
+++ hphp/hack/test/pessimise_and_single_type_check.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 # pyre-strict
 
+import os
 import subprocess
 import sys
 from typing import List
+
+
+def arg_extract(args: List[str], arg: str) -> str:
+    index = args.index(arg)
+    value = args.pop(index + 1)
+    del args[index]
+    return value
+
 
 if __name__ == "__main__":
     # Parsing the arguments is not straightforward as pessimise_and_single_type_check
@@ -17,18 +26,9 @@ if __name__ == "__main__":
     # 4. the remaining arguments are the flags to be passed to hh_single_type_checks
 
     arguments: List[str] = sys.argv
-    hh_stc_path_index: int = arguments.index("--hh-stc-path")
-    hh_stc_path: str = arguments.pop(hh_stc_path_index + 1)
-    del arguments[hh_stc_path_index]
-
-    hh_pessimisation_path_index: int = arguments.index("--hh-pessimisation-path")
-    hh_pessimisation_path: str = arguments.pop(hh_pessimisation_path_index + 1)
-    del arguments[hh_pessimisation_path_index]
-
-    pessimised_hhi_path_index: int = arguments.index("--pessimised-hhi-path")
-    pessimised_hhi_path: str = arguments.pop(pessimised_hhi_path_index + 1)
-    del arguments[pessimised_hhi_path_index]
-
+    hh_stc_path: str = arg_extract(arguments, "--hh-stc-path")
+    hh_pessimisation_path: str = arg_extract(arguments, "--hh-pessimisation-path")
+    pessimised_hhi_path: str = arg_extract(arguments, "--pessimised-hhi-path")
     del arguments[0]
 
     hh_stc_arguments: List[str] = []
@@ -45,12 +45,49 @@ if __name__ == "__main__":
         cmd: List[str] = [hh_pessimisation_path, "--file=" + file, "--update"]
         subprocess.run(cmd)
 
+    # identify the extra builtins used
+    extra_builtins: List[str] = []
+    while True:
+        try:
+            extra_builtins.append(arg_extract(hh_stc_arguments, "--extra-builtin"))
+        except ValueError:
+            break
+
+    # pessimise the extra builtins and build the cli options
+    # - if extra_builtin refer to <path>/<file>, the pessimised extra builtin is
+    #   generated inside <path>/pessimised_hhi/<file>
+    # - to avoid a race condition, the hhi is first pessimised into <file>.pess.<PID>,
+    #   and then moved to <path>/pessimised_hhi/<file>
+    extra_builtins_opts: List[str] = []
+    for file in extra_builtins:
+        (file_dir, file_name) = os.path.split(file)
+        unique_output_file = file + ".pess." + str(os.getpid())
+        pessimised_file_dir = os.path.join(file_dir, "pessimised_hhi")
+        pessimised_file = os.path.join(pessimised_file_dir, file_name)
+
+        if not (os.path.exists(pessimised_file)):
+            cmd: List[str] = [
+                hh_pessimisation_path,
+                "--file",
+                file,
+                "--output-file",
+                unique_output_file,
+            ]
+            subprocess.run(cmd)
+            os.makedirs(pessimised_file_dir, exist_ok=True)
+            # this is expected to be atomic
+            os.rename(unique_output_file, pessimised_file)
+
+        extra_builtins_opts.append("--extra-builtin")
+        extra_builtins_opts.append(pessimised_file)
+
     # pessimised tests have extension file+".pess"
     # invoke hh_single_type_check on the pessimised tests
     pessimised_files: List[str] = list(map(lambda file: file + ".pess", files))
     cmd: List[str] = (
         [hh_stc_path]
         + hh_stc_arguments
+        + extra_builtins_opts
         + ["--enable-sound-dynamic-type", "--like-type-hints", "--pessimise-builtins"]
         + ["--custom-hhi-path", pessimised_hhi_path]
         + pessimised_files

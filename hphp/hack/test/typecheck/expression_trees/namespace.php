0b8c7a0a8bc70afd075e6eaaccffb47d1767326d
Thomas Jiang <thomasjiang@fb.com>
2021-11-24 16:47:56 -0800

    Fix namespace test
    Reviewed By: Wilfred
    
    Differential Revision: D32608199
    
    fbshipit-source-id: 03c93aa72a08d5dd791b4754a9206f72c2e23a20

--- hphp/hack/test/typecheck/expression_trees/namespace.php
+++ hphp/hack/test/typecheck/expression_trees/namespace.php
@@ -3,17 +3,39 @@
 <<file:__EnableUnstableFeatures('expression_trees')>>
 
 namespace Bar {
-  class CodeChild extends \Code {}
+
+class MyCode {
+  const type TAst = mixed;
+
+  public static function makeTree<<<__Explicit>> TInfer>(
+    ?\ExprPos $pos,
+    shape(
+      'splices' => dict<string, mixed>,
+      'functions' => vec<mixed>,
+      'static_methods' => vec<mixed>,
+    ) $metadata,
+    (function(MyCode): MyCode::TAst) $ast,
+  ): \ExprTree<MyCode, MyCode::TAst, TInfer> {
+    throw new \Exception();
+  }
+
+  // Virtual types (These do not have to be implemented)
+  public static function intType(): \ExampleInt {
+    throw new \Exception();
+  }
+
+  // Desugared nodes (These should be implemented)
+  public function visitInt(?\ExprPos $_, int $_): MyCode::TAst {
+    throw new \Exception();
+  }
 }
+
+} // namespace Bar
 
 namespace {
 
 function test(): void {
-    Bar\CodeChild`1`;
+    Bar\MyCode`1`;
 }
 
-
-
-
-
-}
+} // namespace

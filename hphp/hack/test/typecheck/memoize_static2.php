6f1fe1d9f63501badcfdabc8e0fbc64660b3fe40
Andrew Kennedy <akenn@fb.com>
2021-12-15 11:49:59 -0800

    Eliminate FIXME from test
    Summary: No need for a FIXME in this test.
    
    Differential Revision: D33122195
    
    fbshipit-source-id: 248bfce1124fe43c5f30be8be0c32ab0472a8d7e

--- hphp/hack/test/typecheck/memoize_static2.php
+++ hphp/hack/test/typecheck/memoize_static2.php
@@ -1,8 +1,10 @@
 <?hh // strict
-/* HH_IGNORE_ERROR[4054]: Ignore the not implementing IMemoizeParam error */
 final class FinalC implements IMemoizeParam {
   <<__Memoize>>
   public static function get(): this {
     return new static();
+  }
+  public function getInstanceKey():string {
+    return "";
   }
 }

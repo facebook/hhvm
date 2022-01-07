d13e8254dc88b3acbe975f525363d840de0110ee
Fred Emmott <fe@fb.com>
2022-01-07 15:05:45 -0600

    Targetting 4.102.7

--- hphp/runtime/version.h
+++ hphp/runtime/version.h
@@ -11,8 +11,8 @@
 #ifndef HHVM_VERSION_OVERRIDE
 # define HHVM_VERSION_MAJOR 4
 # define HHVM_VERSION_MINOR 102
-# define HHVM_VERSION_PATCH 6
-# define HHVM_VERSION_SUFFIX ""
+# define HHVM_VERSION_PATCH 7
+# define HHVM_VERSION_SUFFIX "-dev"
 #endif
 
 /* HHVM_VERSION_ID minus the patch number

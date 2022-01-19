bffaa524aa3aa80dc2d1368187cc3ffb84a2e2c6
Henri Verroken <hverr@fb.com>
2022-01-19 15:05:27 -0800

    Fix tests for buck2
    Summary:
    Use proper paths to hh_single_type_check and the prototype's binaries for `//hphp/hack/src/rupro:test`
    
    I don't take the approach of using command line arguments to specify the location of the binaries because I want
    
    ```
    buck run //hphp/hack/src/rupro:test
    ```
    
    to work out of the box.
    
    Reviewed By: shayne-fletcher
    
    Differential Revision: D33657182
    
    fbshipit-source-id: e6154f9674433e535816399ef6dc41791234b9c8

--- hphp/hack/src/rupro/test.sh
+++ hphp/hack/src/rupro/test.sh
@@ -7,28 +7,25 @@ function usage {
   echo "  buck run //hphp/hack/src/rupro:test -- [--vim] <FILEPATH>.php"
 }
 
-if [[ -z "$BUCK_DEFAULT_RUNTIME_RESOURCES" || \
-      -z "$BUCK_PROJECT_ROOT" ]]; then
+if [[ -z "$BUCK_DEFAULT_RUNTIME_RESOURCES" ]]; then
   usage >&2
   echo "fatal: use buck" >&2
   exit 1
 fi
-#BUCKOUT=$(echo "$BUCK_DEFAULT_RUNTIME_RESOURCES" | sed -E "s|(^.+buck-out).+$|\1|g")
-#FBCODE=$(dirname "$BUCKOUT")
-#HACK="$FBCODE/hphp/hack"
-
-# Extract Buck mode
-mode=$(grep -oP 'fbcode/buck-out/\K[^/]+' <<< "$BUCK_PROJECT_ROOT")
-if [[ "$mode" != dev && "$mode" != opt ]]; then
-  echo "fatal: unknown mode '$mode'" >&2
-  exit 1
-fi
 
 # Locate binaries
-HSTC_CAML="$BUCK_PROJECT_ROOT/buck-out/$mode/bin/hphp/hack/src/hh_single_type_check/hh_single_type_check.opt"
-HSTC_RUST="$BUCK_PROJECT_ROOT/buck-out/$mode/bin/hphp/hack/src/rupro/stc/stc.opt"
-[ -f "$HSTC_CAML" ] || { echo "fatal: ocaml hstc not found at $HSTC_CAML" >&2; exit 1; }
-[ -f "$HSTC_RUST" ] || { echo "fatal: rust hstc not found at $HSTC_RUST" >&2; exit 1; }
+# In Buck v1 the BUCK_DEFAULT_RUNTIME_RESOURCES is target relative whereas
+# in Buck v2 it is project (fbsource) relative. So for Buck v1, we have to
+# "dirname" until we find the executables
+RESOURCES_DIR="$BUCK_DEFAULT_RUNTIME_RESOURCES"
+while true; do
+  HSTC_CAML="$RESOURCES_DIR/hphp/hack/src/hh_single_type_check"
+  [ -f "$HSTC_CAML" ] && break
+  RESOURCES_DIR="$(dirname "$RESOURCES_DIR")"
+done
+HSTC_RUST="$RESOURCES_DIR/hphp/hack/src/rupro/stc"
+[ -f "$HSTC_CAML" ] || { echo "fatal: ocaml hstc not found anywhere around $BUCK_DEFAULT_RUNTIME_RESOURCES" >&2; exit 1; }
+[ -f "$HSTC_RUST" ] || { echo "fatal: rust hstc not found anywhere around $BUCK_DEFAULT_RUNTIME_RESOURCES" >&2; exit 1; }
 
 f=""
 vim=false

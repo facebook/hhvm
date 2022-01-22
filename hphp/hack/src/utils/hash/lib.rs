09c11199ea1dfc3f1094e24e5d9d78145c62c4b0
Edwin Smith <smith@fb.com>
2022-01-21 16:38:59 -0800

    add Hasher typedefs in hack/src/utils/hash
    Summary:
    Some trivial cleanup in hack/src/utils/hash. This allows clients to
    use the curated Hasher and BuildHasher types without caring which
    hash algorithm was actually chosen.
    
    Reviewed By: aorenste
    
    Differential Revision: D33715256
    
    fbshipit-source-id: 91f1642d7800194475905460e39b4357231da118

--- hphp/hack/src/utils/hash/lib.rs
+++ hphp/hack/src/utils/hash/lib.rs
@@ -1,8 +1,8 @@
 // Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
+pub type Hasher = rustc_hash::FxHasher;
+pub type BuildHasher = std::hash::BuildHasherDefault<Hasher>;
 pub type HashMap<K, V> = rustc_hash::FxHashMap<K, V>;
 pub type HashSet<K> = rustc_hash::FxHashSet<K>;
-pub type IndexMap<K, V> =
-    indexmap::map::IndexMap<K, V, std::hash::BuildHasherDefault<rustc_hash::FxHasher>>;
-pub type IndexSet<K> =
-    indexmap::set::IndexSet<K, std::hash::BuildHasherDefault<rustc_hash::FxHasher>>;
+pub type IndexMap<K, V> = indexmap::map::IndexMap<K, V, BuildHasher>;
+pub type IndexSet<K> = indexmap::set::IndexSet<K, BuildHasher>;

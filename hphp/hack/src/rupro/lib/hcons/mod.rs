745a1884bb4de1bca4300078bf1873a35c454610
Jake Bailey (Hacklang) <jakebailey@fb.com>
2022-01-25 16:14:05 -0800

    Remove tag in Hc
    Summary:
    Each hash-consed value is associated with a unique tag, so that we can test for equality between values just by comparing the tags. This strategy is described in [this paper](https://www.lri.fr/~filliatr/ftp/publis/hash-consing2.pdf) and implemented in [this Rust library](https://docs.rs/hashconsing/1.3.0/hashconsing/index.html), which may have inspired our own hcons implementation. The paper notes that a tag must be used rather than pointer values because of the garbage collector. While the GC shouldn't be a problem for testing equality (there will still be only one copy of each hash-consed value, and all pointers to it will be equal to one another), the paper describes building balanced-tree datastructures using tags as the key. Using pointers as keys for those data structures wouldn't be suitable, since a hashconsed value may be moved after it is inserted into the container.
    
    This isn't a concern for us: we don't currently have data structures for hash-consed terms which key off the tag or pointer-value, and even if we did, there is no GC to move our ref-counted values. I think we could just use pointer-values rather than tags. This should help performance: when performing an equality test, there's no need to dereference the pointer to get the tag.
    
    Additionally, since there's no longer a need for an HcImpl struct to store the tag, we can more easily hash-cons unsized types like `str`. A later diff in the stack switches the representation of `Symbol` from `Hc<Box<str>>` to `Hc<str>` now that this is possible.
    
    Differential Revision: D33698453
    
    fbshipit-source-id: 7817151c45489cb19bbb78b8d32972ae5245a65f

--- hphp/hack/src/rupro/lib/hcons/mod.rs
+++ hphp/hack/src/rupro/lib/hcons/mod.rs
@@ -3,57 +3,16 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
 
-use std::borrow::ToOwned;
 use std::collections::{hash_map::Entry, HashMap};
 use std::fmt;
 use std::hash::{Hash, Hasher};
-use std::ops::{Deref, DerefMut};
+use std::ops::Deref;
 use std::rc::{Rc, Weak};
-use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::Mutex;
-
-struct HcImpl<T> {
-    node: T,
-    tag: u64,
-}
-
-impl<T: fmt::Debug> fmt::Debug for HcImpl<T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        self.node.fmt(f)
-    }
-}
-
-impl<T> Deref for HcImpl<T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        &self.node
-    }
-}
-
-impl<T> DerefMut for HcImpl<T> {
-    fn deref_mut(&mut self) -> &mut T {
-        &mut self.node
-    }
-}
-
-impl<T> Eq for HcImpl<T> {}
-
-impl<T> Hash for HcImpl<T> {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.tag.hash(state);
-    }
-}
-
-impl<T> PartialEq for HcImpl<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.tag == other.tag
-    }
-}
 
 /// A hash-consed pointer.
 #[derive(Clone)]
-pub struct Hc<T>(Rc<HcImpl<T>>);
+pub struct Hc<T>(Rc<T>);
 
 impl<T: fmt::Debug> fmt::Debug for Hc<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
@@ -71,7 +30,7 @@ impl<T> Deref for Hc<T> {
 
 impl<T> Eq for Hc<T> {}
 
-impl<T> Hash for Hc<T> {
+impl<T: Hash> Hash for Hc<T> {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.0.hash(state);
     }
@@ -79,7 +38,7 @@ impl<T> Hash for Hc<T> {
 
 impl<T> PartialEq for Hc<T> {
     fn eq(&self, other: &Self) -> bool {
-        self.0 == other.0
+        std::ptr::eq(self.0.as_ref(), other.0.as_ref())
     }
 }
 
@@ -144,36 +103,29 @@ impl AsRef<std::ffi::OsStr> for Hc<Box<str>> {
 
 #[derive(Debug)]
 pub struct Conser<T> {
-    next_tag: AtomicU64,
-    table: Mutex<HashMap<T, Weak<HcImpl<T>>>>,
+    table: Mutex<HashMap<T, Weak<T>>>,
 }
 
 impl<T: Eq + Hash + Clone> Conser<T> {
     pub fn new() -> Self {
         Conser {
-            next_tag: AtomicU64::new(0),
             table: Mutex::new(HashMap::new()),
         }
     }
 
     pub fn mk(&self, x: T) -> Hc<T> {
-        let make_rc = |x: &T| {
-            let node = x.to_owned();
-            let tag = self.next_tag.fetch_add(1, Ordering::Relaxed);
-            Rc::new(HcImpl { node, tag })
-        };
         let mut table = self.table.lock().unwrap();
         let rc = match table.entry(x) {
             Entry::Occupied(mut o) => match o.get().upgrade() {
                 Some(rc) => rc,
                 None => {
-                    let rc = make_rc(o.key());
+                    let rc = Rc::new(o.key().to_owned());
                     o.insert(Rc::downgrade(&rc));
                     rc
                 }
             },
             Entry::Vacant(v) => {
-                let rc = make_rc(v.key());
+                let rc = Rc::new(v.key().to_owned());
                 v.insert(Rc::downgrade(&rc));
                 rc
             }

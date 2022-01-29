8c8f1bad16e5b485fd94311df7031327bfcfb0b7
Edwin Smith <smith@fb.com>
2022-01-29 04:04:47 -0800

    Migrate hhvm symlink rules
    Summary:
    * Converted to buck_genrule - all command args are explicit
    * rule output is a directory, and shell command must mkdir
    * need `realpath` on input hhvm path, for buck2
    * force local execution to avoid shipping the hhvm binary in/out of RE just to make links.
    
    Reviewed By: ricklavoie
    
    Differential Revision: D33622405
    
    fbshipit-source-id: eee1c920ff83a864dea58a7ff13f300489c2674a

--- hphp/hhvm/symlinks.sh
+++ hphp/hhvm/symlinks.sh
@@ -1,12 +1,9 @@
 #/bin/sh
 
-if echo $1 | grep -q -e 'install_dir=' -e 'fbcode_dir=' ; then
-  # Skip --install_dir and --fbcode_dir.
-  shift 2
-fi
+HHVM_BIN=$(realpath "$1")
+OUT=$2
 
-HHVM_BIN=$1
-
-ln -sf "${HHVM_BIN}" "${INSTALL_DIR}/hphp"
-ln -sf "${HHVM_BIN}" "${INSTALL_DIR}/php"
-ln -sf "${HHVM_BIN}" "${INSTALL_DIR}/hhbbc"
+mkdir -p "${OUT}"
+ln -sf "${HHVM_BIN}" "${OUT}/hphp"
+ln -sf "${HHVM_BIN}" "${OUT}/php"
+ln -sf "${HHVM_BIN}" "${OUT}/hhbbc"

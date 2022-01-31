a4753646d8c5caa30fa1fc846e1c36c2da2994c0
Fred Emmott <fe@fb.com>
2022-01-31 18:49:10 +0000

    Add script to automate first-party tag changes

---
+++ third-party/update-first-party.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Update the pinned versions of first-party dependencies
+#
+# It is likely that patches will need updating - see README.md
+set -e
+OLD="$1"
+NEW="$2"
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage: $0 OLD NEW"
+  echo " OLD and NEW should be of the form YYYY.MM.DD.xx (usually 00)"
+  exit 0
+fi
+
+sed -i "s/$OLD/$NEW/g" */CMakeLists.txt
+
+for MATCH in $(grep --only-matching -P  "https://github.com/.+$NEW.+\.gz" */CMakeLists.txt); do
+  CMAKE_FILE="${MATCH%%:*}"
+  URL="${MATCH#*:}"
+
+  FILE="$(mktemp)"
+  wget -O "$FILE" "$URL"
+  HASH="$(openssl dgst -sha256 "$FILE" | awk '{print $NF}')"
+  rm "$FILE"
+  sed -i 's/SHA[0-9]\+=[0-9a-z]\+/SHA256='$HASH'/' "$CMAKE_FILE"
+done

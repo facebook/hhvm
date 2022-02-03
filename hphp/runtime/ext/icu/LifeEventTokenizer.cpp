9e2b06bc3583544bd069d03536f01f4afe2cd8fd
Oguz Ulgen <oulgen@fb.com>
2022-02-02 22:59:15 -0800

    Fix uft8 problems in c++20
    Summary: UTF8 strings are not very well supported in C++20, lets cast them to char arrays.
    
    Reviewed By: ottoni
    
    Differential Revision: D33944955
    
    fbshipit-source-id: dfba53bb9b28366694ecf1a131828e9bb0e9f898

--- hphp/runtime/ext/icu/LifeEventTokenizer.cpp
+++ hphp/runtime/ext/icu/LifeEventTokenizer.cpp
@@ -24,7 +24,7 @@ namespace HPHP {
 
 
 // Rules for ICU's RuleBasedBreakIterator class.
-const char* strRules = u8"\n\
+const char* strRules = (const char *)u8"\n\
 !!chain;\n\
 $CR           = [\\p{Word_Break = CR}];\n\
 $LF           = [\\p{Word_Break = LF}];\n\

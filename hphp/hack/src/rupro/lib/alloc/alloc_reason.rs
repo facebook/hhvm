fcea0b24f5d95305234020c7cb7602f69631a0f4
Jake Bailey (Hacklang) <jakebailey@fb.com>
2022-02-03 14:20:06 -0800

    Construct most Reason variants
    Summary: Add most of the Reason variants. The ones covered in this version of the diff include all of the variants which occur in decls, so this diff is sufficient to hash-cons every decl in WWW.
    
    Reviewed By: edwinsmith
    
    Differential Revision: D33955284
    
    fbshipit-source-id: fb8b6853126153048f1dfecdf1ea5ec05145a5a4

--- hphp/hack/src/rupro/lib/alloc/alloc_reason.rs
+++ hphp/hack/src/rupro/lib/alloc/alloc_reason.rs
@@ -16,7 +16,102 @@ impl<R: Reason> Allocator<R> {
                 OR::Rnone => RI::Rnone,
                 OR::Rwitness(pos) => RI::Rwitness(self.pos_from_decl(pos)),
                 OR::RwitnessFromDecl(pos) => RI::RwitnessFromDecl(self.pos_from_decl(pos)),
+                OR::RidxVector(pos) => RI::RidxVector(self.pos_from_decl(pos)),
+                OR::RidxVectorFromDecl(pos) => RI::RidxVectorFromDecl(self.pos_from_decl(pos)),
+                OR::Rforeach(pos) => RI::Rforeach(self.pos_from_decl(pos)),
+                OR::Rasyncforeach(pos) => RI::Rasyncforeach(self.pos_from_decl(pos)),
+                OR::Rarith(pos) => RI::Rarith(self.pos_from_decl(pos)),
+                OR::RarithRet(pos) => RI::RarithRet(self.pos_from_decl(pos)),
+                OR::RarithRetInt(pos) => RI::RarithRetInt(self.pos_from_decl(pos)),
+                OR::RarithDynamic(pos) => RI::RarithDynamic(self.pos_from_decl(pos)),
+                OR::RbitwiseDynamic(pos) => RI::RbitwiseDynamic(self.pos_from_decl(pos)),
+                OR::RincdecDynamic(pos) => RI::RincdecDynamic(self.pos_from_decl(pos)),
+                OR::Rcomp(pos) => RI::Rcomp(self.pos_from_decl(pos)),
+                OR::RconcatRet(pos) => RI::RconcatRet(self.pos_from_decl(pos)),
+                OR::RlogicRet(pos) => RI::RlogicRet(self.pos_from_decl(pos)),
+                OR::Rbitwise(pos) => RI::Rbitwise(self.pos_from_decl(pos)),
+                OR::RbitwiseRet(pos) => RI::RbitwiseRet(self.pos_from_decl(pos)),
+                OR::RnoReturn(pos) => RI::RnoReturn(self.pos_from_decl(pos)),
+                OR::RnoReturnAsync(pos) => RI::RnoReturnAsync(self.pos_from_decl(pos)),
+                OR::RretFunKind(&(pos, fun_kind)) => {
+                    RI::RretFunKind(self.pos_from_decl(pos), fun_kind)
+                }
+                OR::RretFunKindFromDecl(&(pos, fun_kind)) => {
+                    RI::RretFunKindFromDecl(self.pos_from_decl(pos), fun_kind)
+                }
                 OR::Rhint(pos) => RI::Rhint(self.pos_from_decl(pos)),
+                OR::Rthrow(pos) => RI::Rthrow(self.pos_from_decl(pos)),
+                OR::Rplaceholder(pos) => RI::Rplaceholder(self.pos_from_decl(pos)),
+                OR::RretDiv(pos) => RI::RretDiv(self.pos_from_decl(pos)),
+                OR::RyieldGen(pos) => RI::RyieldGen(self.pos_from_decl(pos)),
+                OR::RyieldAsyncgen(pos) => RI::RyieldAsyncgen(self.pos_from_decl(pos)),
+                OR::RyieldAsyncnull(pos) => RI::RyieldAsyncnull(self.pos_from_decl(pos)),
+                OR::RyieldSend(pos) => RI::RyieldSend(self.pos_from_decl(pos)),
+                OR::RclassClass(&(pos, s)) => {
+                    RI::RclassClass(self.pos_from_decl(pos), self.symbol(s))
+                }
+                OR::RunknownClass(pos) => RI::RunknownClass(self.pos_from_decl(pos)),
+                OR::RvarParam(pos) => RI::RvarParam(self.pos_from_decl(pos)),
+                OR::RvarParamFromDecl(pos) => RI::RvarParamFromDecl(self.pos_from_decl(pos)),
+                OR::RunpackParam(&(pos1, pos2, i)) => {
+                    RI::RunpackParam(self.pos_from_decl(pos1), self.pos_from_decl(pos2), i)
+                }
+                OR::RinoutParam(pos) => RI::RinoutParam(self.pos_from_decl(pos)),
+                OR::RnullsafeOp(pos) => RI::RnullsafeOp(self.pos_from_decl(pos)),
+                OR::RtconstNoCstr(&pos_id) => RI::RtconstNoCstr(self.pos_id_from_decl(pos_id)),
+                OR::Rpredicated(&(pos, s)) => {
+                    RI::Rpredicated(self.pos_from_decl(pos), self.symbol(s))
+                }
+                OR::Ris(pos) => RI::Ris(self.pos_from_decl(pos)),
+                OR::Ras(pos) => RI::Ras(self.pos_from_decl(pos)),
+                OR::RvarrayOrDarrayKey(pos) => RI::RvarrayOrDarrayKey(self.pos_from_decl(pos)),
+                OR::RvecOrDictKey(pos) => RI::RvecOrDictKey(self.pos_from_decl(pos)),
+                OR::Rusing(pos) => RI::Rusing(self.pos_from_decl(pos)),
+                OR::RdynamicProp(pos) => RI::RdynamicProp(self.pos_from_decl(pos)),
+                OR::RdynamicCall(pos) => RI::RdynamicCall(self.pos_from_decl(pos)),
+                OR::RdynamicConstruct(pos) => RI::RdynamicConstruct(self.pos_from_decl(pos)),
+                OR::RidxDict(pos) => RI::RidxDict(self.pos_from_decl(pos)),
+                OR::RsetElement(pos) => RI::RsetElement(self.pos_from_decl(pos)),
+                OR::RmissingOptionalField(&(pos, s)) => {
+                    RI::RmissingOptionalField(self.pos_from_decl(pos), self.symbol(s))
+                }
+                OR::RunsetField(&(pos, s)) => {
+                    RI::RunsetField(self.pos_from_decl(pos), self.symbol(s))
+                }
+                OR::Rregex(pos) => RI::Rregex(self.pos_from_decl(pos)),
+                OR::RimplicitUpperBound(&(pos, s)) => {
+                    RI::RimplicitUpperBound(self.pos_from_decl(pos), self.symbol(s))
+                }
+                OR::RtypeVariable(pos) => RI::RtypeVariable(self.pos_from_decl(pos)),
+                OR::RtypeVariableGenerics(&(pos, s1, s2)) => RI::RtypeVariableGenerics(
+                    self.pos_from_decl(pos),
+                    self.symbol(s1),
+                    self.symbol(s2),
+                ),
+                OR::RglobalTypeVariableGenerics(&(pos, s1, s2)) => RI::RglobalTypeVariableGenerics(
+                    self.pos_from_decl(pos),
+                    self.symbol(s1),
+                    self.symbol(s2),
+                ),
+                OR::RsolveFail(pos) => RI::RsolveFail(self.pos_from_decl(pos)),
+                OR::RcstrOnGenerics(&(pos, pos_id)) => {
+                    RI::RcstrOnGenerics(self.pos_from_decl(pos), self.pos_id_from_decl(pos_id))
+                }
+                OR::Rshape(&(pos, s)) => RI::Rshape(self.pos_from_decl(pos), self.symbol(s)),
+                OR::Renforceable(pos) => RI::Renforceable(self.pos_from_decl(pos)),
+                OR::Rdestructure(pos) => RI::Rdestructure(self.pos_from_decl(pos)),
+                OR::RkeyValueCollectionKey(pos) => {
+                    RI::RkeyValueCollectionKey(self.pos_from_decl(pos))
+                }
+                OR::RglobalClassProp(pos) => RI::RglobalClassProp(self.pos_from_decl(pos)),
+                OR::RglobalFunParam(pos) => RI::RglobalFunParam(self.pos_from_decl(pos)),
+                OR::RglobalFunRet(pos) => RI::RglobalFunRet(self.pos_from_decl(pos)),
+                OR::Rsplice(pos) => RI::Rsplice(self.pos_from_decl(pos)),
+                OR::RetBoolean(pos) => RI::RetBoolean(self.pos_from_decl(pos)),
+                OR::RdefaultCapability(pos) => RI::RdefaultCapability(self.pos_from_decl(pos)),
+                OR::RconcatOperand(pos) => RI::RconcatOperand(self.pos_from_decl(pos)),
+                OR::RinterpOperand(pos) => RI::RinterpOperand(self.pos_from_decl(pos)),
+                OR::RsupportDynamicType(pos) => RI::RsupportDynamicType(self.pos_from_decl(pos)),
                 r => unimplemented!("reason: {:?}", r),
             }
         })
--- hphp/hack/src/rupro/lib/reason.rs
+++ hphp/hack/src/rupro/lib/reason.rs
@@ -4,10 +4,12 @@
 // LICENSE file in the "hack" directory of this source tree.
 
 use ocamlrep::{Allocator, OpaqueValue, ToOcamlRep};
-use pos::{BPos, NPos, Pos};
+use pos::{BPos, NPos, Pos, Positioned, Symbol};
 use std::hash::Hash;
 
 use crate::walker::Walker;
+
+pub use oxidized::typing_reason::{ArgPosition, BlameSource};
 
 pub trait Reason:
     Eq + Hash + Clone + ToOcamlRep + Walker<Self> + std::fmt::Debug + Send + Sync + 'static
@@ -25,11 +27,115 @@ pub trait Reason:
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub enum ReasonImpl<P: Pos> {
+pub struct Blame<P>(pub P, pub BlameSource);
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum ExprDepTypeReason {
+    ERexpr(isize),
+    ERstatic,
+    ERclass(Symbol),
+    ERparent(Symbol),
+    ERself(Symbol),
+    ERpu(Symbol),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum ReasonImpl<P> {
     Rnone,
     Rwitness(P),
     RwitnessFromDecl(P),
+    /// Used as an index into a vector-like array or string.
+    /// Position of indexing, reason for the indexed type
+    Ridx(P, BReason),
+    RidxVector(P),
+    /// Used as an index, in the Vector case
+    RidxVectorFromDecl(P),
+    /// Because it is iterated in a foreach loop
+    Rforeach(P),
+    /// Because it is iterated "await as" in foreach
+    Rasyncforeach(P),
+    Rarith(P),
+    RarithRet(P),
+    /// pos, arg float typing reason, arg position
+    RarithRetFloat(P, BReason, oxidized::typing_reason::ArgPosition),
+    /// pos, arg num typing reason, arg position
+    RarithRetNum(P, BReason, oxidized::typing_reason::ArgPosition),
+    RarithRetInt(P),
+    RarithDynamic(P),
+    RbitwiseDynamic(P),
+    RincdecDynamic(P),
+    Rcomp(P),
+    RconcatRet(P),
+    RlogicRet(P),
+    Rbitwise(P),
+    RbitwiseRet(P),
+    RnoReturn(P),
+    RnoReturnAsync(P),
+    RretFunKind(P, oxidized::ast_defs::FunKind),
+    RretFunKindFromDecl(P, oxidized::ast_defs::FunKind),
     Rhint(P),
+    Rthrow(P),
+    Rplaceholder(P),
+    RretDiv(P),
+    RyieldGen(P),
+    RyieldAsyncgen(P),
+    RyieldAsyncnull(P),
+    RyieldSend(P),
+    RlostInfo(Symbol, BReason, Blame<P>),
+    Rformat(P, Symbol, BReason),
+    RclassClass(P, Symbol),
+    RunknownClass(P),
+    RvarParam(P),
+    RvarParamFromDecl(P),
+    /// splat pos, fun def pos, number of args before splat
+    RunpackParam(P, P, isize),
+    RinoutParam(P),
+    Rinstantiate(BReason, Symbol, BReason),
+    Rtypeconst(BReason, Positioned<Symbol, P>, Symbol, BReason),
+    RtypeAccess(BReason, Vec<(BReason, Symbol)>),
+    RexprDepType(BReason, P, ExprDepTypeReason),
+    /// ?-> operator is used
+    RnullsafeOp(P),
+    RtconstNoCstr(Positioned<Symbol, P>),
+    Rpredicated(P, Symbol),
+    Ris(P),
+    Ras(P),
+    RvarrayOrDarrayKey(P),
+    RvecOrDictKey(P),
+    Rusing(P),
+    RdynamicProp(P),
+    RdynamicCall(P),
+    RdynamicConstruct(P),
+    RidxDict(P),
+    RsetElement(P),
+    RmissingOptionalField(P, Symbol),
+    RunsetField(P, Symbol),
+    RcontravariantGeneric(BReason, Symbol),
+    RinvariantGeneric(BReason, Symbol),
+    Rregex(P),
+    RimplicitUpperBound(P, Symbol),
+    RtypeVariable(P),
+    RtypeVariableGenerics(P, Symbol, Symbol),
+    RglobalTypeVariableGenerics(P, Symbol, Symbol),
+    RsolveFail(P),
+    RcstrOnGenerics(P, Positioned<Symbol, P>),
+    RlambdaParam(P, BReason),
+    Rshape(P, Symbol),
+    Renforceable(P),
+    Rdestructure(P),
+    RkeyValueCollectionKey(P),
+    RglobalClassProp(P),
+    RglobalFunParam(P),
+    RglobalFunRet(P),
+    Rsplice(P),
+    RetBoolean(P),
+    RdefaultCapability(P),
+    RconcatOperand(P),
+    RinterpOperand(P),
+    RdynamicCoercion(BReason),
+    RsupportDynamicType(P),
+    RdynamicPartialEnforcement(P, Symbol, BReason),
+    RrigidTvarEscape(P, Symbol, Symbol, BReason),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -48,6 +154,7 @@ impl Reason for BReason {
         match &*self.0 {
             Rnone => unimplemented!(),
             Rwitness(p) | RwitnessFromDecl(p) | Rhint(p) => p,
+            r => unimplemented!("BReason::pos: {:?}", r),
         }
     }
 

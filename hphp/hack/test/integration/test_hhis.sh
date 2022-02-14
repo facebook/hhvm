2f62d0a9099e0ce17c656a60d8b499c21f2e1b17
Lucian Wischik <ljw@fb.com>
2022-02-14 12:07:31 -0800

    defend against buck2 "Text file is busy"
    Summary:
    See https://fb.workplace.com/groups/buck2users/posts/3078672432389117/
    
    Buck2 is currently vulnerable to "Text file is busy" which usually means that a process still has an FD with a write handle open on the executable, at the time we try to invoke it. T107518211 has plans to defend against it.
    
    In the meantime we're getting several test failures in buck2 because of it.
    
    What other people do is they catch "Text file is busy" (manifest as exit code 126, "command is found but not executable"), and in response they wait and then retry. But I don't know. Implementing retry logic feels like a pain, and every retry loop indeed also needs the ability to give up after some number of retries. Therefore: what I've done in this diff is only catch the problem in the cases where it has manifest so far (rather than a cross-cutting solution), and I've hard-coded in whatever number of retries involved the least code disruption (1 and 0, in the two cases I'm looking at).
    
    Reviewed By: shayne-fletcher
    
    Differential Revision: D34190504
    
    fbshipit-source-id: b9ebd95f8c7872c2f81c0885c66aca29f6002c71

--- hphp/hack/test/integration/test_hhis.sh
+++ hphp/hack/test/integration/test_hhis.sh
@@ -10,5 +10,14 @@ cat > "${REPO}/.hhconfig" <<EOF
 unsafe_rx=false
 EOF
 export HH_TEST_MODE=1  # avoid writing a bunch of telemetry
+set +e
 "${HH_SERVER}" --check "${REPO}" --config max_workers=2
+code=$?
+if [[ "$code" == 126 ]]; then
+  # 126 means "not executable", typically in buck2 from "Text file is busy" because there are still open FD write handles to the binary
+  # Ugly workaround for now: https://www.internalfb.com/intern/qa/312685/text-file-is-busy---test-is-run-before-fclose-on-e
+  # This workaround should be removed once T107518211 is closed.
+  sleep 20
+  "${HH_SERVER}" --check "${REPO}" --config max_workers=2
+fi
 rm -rf "${REPO}"

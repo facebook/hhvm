97a3cdd0104dfdc8599737ddcc1f914dad22ac6c
Josh Soref <jsoref@users.noreply.github.com>
2022-02-14 22:43:04 -0500

    spelling: acquire
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- hphp/vixl/a64/assembler-a64.cc
+++ hphp/vixl/a64/assembler-a64.cc
@@ -1143,7 +1143,7 @@ void Assembler::ldr(const FPRegister& ft, double imm) {
 
 void Assembler::ldaddal(const Register& rs, const Register& rt, const MemOperand& src) {
   assert(src.IsImmediateOffset() && (src.offset() == 0));
-  // aquire/release semantics
+  // acquire/release semantics
   uint32_t op = rt.Is64Bits() ? LSELD_ADD_alx : LSELD_ADD_alw;
   Emit(op | Rs(rs) | Rt(rt) | RnSP(src.base()));
 }

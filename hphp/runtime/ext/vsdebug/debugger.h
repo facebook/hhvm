5e4dd59433a555808e8380b53fe5367b3423df7f
Josh Soref <jsoref@users.noreply.github.com>
2022-02-14 22:43:11 -0500

    spelling: failure
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- hphp/runtime/ext/vsdebug/debugger.h
+++ hphp/runtime/ext/vsdebug/debugger.h
@@ -534,7 +534,7 @@ private:
     int line
   );
 
-  // Reports faiure to process a message from the debugger client to the
+  // Reports failure to process a message from the debugger client to the
   // front-end.
   void reportClientMessageError(
     folly::dynamic& clientMsg,

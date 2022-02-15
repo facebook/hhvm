94a97f1a03cfecc8e374135c8615c12f40d2bb37
Josh Soref <jsoref@users.noreply.github.com>
2022-02-14 22:43:18 -0500

    spelling: resolve
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- hphp/runtime/ext/extension-registry.cpp
+++ hphp/runtime/ext/extension-registry.cpp
@@ -380,7 +380,7 @@ static void sortDependencies() {
 
   if (UNLIKELY(!unresolved.empty())) {
     // Alerts user to circular dependency in extensions
-    // e.g. Unable to resovle dependencies for extension(s):
+    // e.g. Unable to resolve dependencies for extension(s):
     //         A(depends: B) B(depends: C) C(depends: A)
 
     std::stringstream ss;

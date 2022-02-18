7bd1acb099206d127b89a869049e7d284d95419c
Melanie Chen <mchen110@fb.com>
2022-02-18 09:35:34 -0800

    Turn on runtime enforcement for reading globals needs to be enclosed in readonly
    Summary:
    Same story as in D32708938 (https://github.com/facebook/hhvm/commit/7c3c0f3ae7daf24967915054b83e9fc1f0b959d7) - don't want to enforce in the runtime if
    an error has been committed in www between hack release and runtime release.
    Turned on the enforcement of the behavior in the typechecker in D32855117 (https://github.com/facebook/hhvm/commit/167ae5ec97f12c98173c481813131f8e2a84210a)
    and now that those changes are released in
    https://fb.workplace.com/groups/hacklang.release/permalink/1169654543570793/,
    we can enforce in the runtime as well.
    
    Reviewed By: oulgen
    
    Differential Revision: D33949450
    
    fbshipit-source-id: 53dabed6d3b0230045f05c8b93de8b9ac2cea22b

--- hphp/test/slow/implicit-context/memo-cls-2.php
+++ hphp/test/slow/implicit-context/memo-cls-2.php
@@ -3,7 +3,7 @@
 class Foo {
   static Foo $x;
   <<__PolicyShardedMemoize>>
-  function memo<reify T>($a, $b)[zoned] {
+  readonly function memo<reify T>($a, $b)[zoned] {
     $hash = quoted_printable_encode(
       HH\ImplicitContext\_Private\get_implicit_context_memo_key()
     );
@@ -13,22 +13,22 @@ class Foo {
 }
 
 function g()[zoned] {
-  Foo::$x->memo<int>(1, 2);
-  Foo::$x->memo<int>(1, 3);
-  Foo::$x->memo<string>(1, 2);
-  Foo::$x->memo<string>(1, 3);
+  (readonly Foo::$x)->memo<int>(1, 2);
+  (readonly Foo::$x)->memo<int>(1, 3);
+  (readonly Foo::$x)->memo<string>(1, 2);
+  (readonly Foo::$x)->memo<string>(1, 3);
 }
 
 function f()[zoned] {
-  Foo::$x->memo<int>(1, 2);
-  Foo::$x->memo<int>(1, 3);
-  Foo::$x->memo<string>(1, 2);
-  Foo::$x->memo<string>(1, 3);
+  (readonly Foo::$x)->memo<int>(1, 2);
+  (readonly Foo::$x)->memo<int>(1, 3);
+  (readonly Foo::$x)->memo<string>(1, 2);
+  (readonly Foo::$x)->memo<string>(1, 3);
   ClassContext2::start(new B, g<>);
-  Foo::$x->memo<int>(1, 2);
-  Foo::$x->memo<int>(1, 3);
-  Foo::$x->memo<string>(1, 2);
-  Foo::$x->memo<string>(1, 3);
+  (readonly Foo::$x)->memo<int>(1, 2);
+  (readonly Foo::$x)->memo<int>(1, 3);
+  (readonly Foo::$x)->memo<string>(1, 2);
+  (readonly Foo::$x)->memo<string>(1, 3);
 }
 
 <<__EntryPoint>>

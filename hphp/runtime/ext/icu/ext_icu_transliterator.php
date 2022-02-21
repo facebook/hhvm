a0fb0690cac42d557c164488edc680ce8e9cc92b
Josh Soref <jsoref@users.noreply.github.com>
2022-02-21 17:54:39 -0500

    spelling: success
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- hphp/runtime/ext/icu/ext_icu_transliterator.php
+++ hphp/runtime/ext/icu/ext_icu_transliterator.php
@@ -109,7 +109,7 @@ class Transliterator {
    * Get transliterator IDs
    *
    * @return array - An array of registered transliterator IDs on
-   *   success, .
+   *   success.
    */
   <<__Native>>
   public static function listIDs(): mixed;
@@ -121,7 +121,7 @@ class Transliterator {
    * @param int $start -
    * @param int $end -
    *
-   * @return string - The transfomed string on success, .
+   * @return string - The transfomed string on success.
    */
   <<__Native>>
   public function transliterate(string $subject,
@@ -206,7 +206,7 @@ function transliterator_list_ids(): varray {
  * @param int $start -
  * @param int $end -
  *
- * @return  - The transfomed string on success, .
+ * @return  - The transfomed string on success.
  */
 function transliterator_transliterate(mixed $tOrId,
                                       string $subject,

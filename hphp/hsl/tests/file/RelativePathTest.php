962970fef3aefb42b9cdad951492fd45a44a4fb5
Yang, Bo <pop.atry@gmail.com>
2022-02-23 13:50:41 -0800

    Add _File\relative_path (#179)
    Summary:
    This PR adds the `relative_path` function, which will be used in https://github.com/hhvm/hsl/issues/178
    
    X-link: https://github.com/hhvm/hsl/pull/179
    
    Reviewed By: fredemmott
    
    Differential Revision: D34152355
    
    Pulled By: Atry
    
    fbshipit-source-id: 37b89c720f51ed8f45765b01b680ac83b01fa425

---
+++ hphp/hsl/tests/file/RelativePathTest.php
@@ -0,0 +1,43 @@
+<?hh // strict
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the MIT license found in the
+ *  LICENSE file in the hphp/hsl/ subdirectory of this source tree.
+ *
+ */
+
+
+use function HH\__Private\MiniTest\expect;
+use type HH\__Private\MiniTest\HackTest;
+use namespace HH\Lib\_Private\_File;
+final class RelativePathTest extends HackTest {
+
+  public async function testBasic(): Awaitable<void> {
+    $real_path = _File\relative_path(
+      'baz',
+      'foo/bar',
+    );
+    expect($real_path)->toEqual('foo/bar/baz');
+  }
+
+  public async function testDot(): Awaitable<void> {
+    $real_path = \realpath(_File\relative_path(
+      './DictSelectTest.php',
+      _File\relative_path('../dict', __DIR__),
+    ));
+    expect($real_path)->toContainSubstring('/tests/dict/DictSelectTest.php');
+  }
+
+  public async function testSlash(): Awaitable<void> {
+    $path = _File\relative_path('/bin/sh', 'path/to/whatever');
+    expect($path)->toEqual('/bin/sh');
+  }
+
+  public async function testNull(): Awaitable<void> {
+    $path = _File\relative_path('path/to/file', null);
+    expect($path)->toEqual('path/to/file');
+  }
+
+}

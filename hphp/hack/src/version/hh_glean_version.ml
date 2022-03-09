0e97dba034e210fa2a45972839e9a5883f8552ca
Philippe Bidinger <pbidinger@fb.com>
2022-03-09 07:02:08 -0800

    update schema version
    Summary:
    This should have been done in D34751840
    
    This sets the schema_version of the db to be produced. It should be 8.
    
    Reviewed By: lazamar
    
    Differential Revision: D34751840
    
    fbshipit-source-id: 1286063f67d06723adfcba55de6800399759da1b

--- hphp/hack/src/version/hh_glean_version.ml
+++ hphp/hack/src/version/hh_glean_version.ml
@@ -8,7 +8,7 @@
 
 (** This is the version of the Glean "all" schema to be used for
     code indexation *)
-let schema_version = 7
+let schema_version = 8
 
 (** This is the version of the Glean "hack" schema to be used for
     code indexation *)

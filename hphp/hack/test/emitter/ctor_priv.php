3bdc5f507ecdeda09e922c150e9738f4eb5a2805
Alexey Toptygin <alexeyt@fb.com>
2022-03-14 19:09:06 -0700

    Delete dead test code in hphp/hack/test/emitter/
    Summary: I couldn't find any references to these files so I tried deleting them. Nothing broke, so I conclude they were dead.
    
    Reviewed By: aorenste
    
    Differential Revision: D34784141
    
    fbshipit-source-id: 493dbbe8f9300a6cdf6b98dee72f6377f97e7d94

--- hphp/hack/test/emitter/ctor_priv.php
+++
@@ -1,8 +0,0 @@
-<?hh // strict
-
-class Foo {
-  // We shouldn't crash hhvm when we have a private ctor...
-  private function __construct() { }
-}
-
-function test(): void {}
--- hphp/hack/test/emitter/superglobals.php
+++
@@ -1,8 +0,0 @@
-<?hh
-
-function test(): void {
-  $GLOBALS['hi'] = 10;
-  var_dump($GLOBALS['hi']);
-  var_dump($GLOBALS["hi"]);
-  var_dump($_ENV['HOME']);
-}

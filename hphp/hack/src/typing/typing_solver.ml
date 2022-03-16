e46f5851eb425355f138be310da45ae4e1b98097
Andrew Kennedy <akenn@fb.com>
2022-03-16 02:19:44 -0700

    Allow type projection on tyvar in dynamic mode
    Summary: Currently we reject type projections on type variables that have no lower bounds, at the variable solving stage. Unfortunately this results in a lot of errors in dynamic mode checking (see test), because often we can infer nothing useful about a type variable that participates in a type projection. It's not clear to me why we need to reject type variables so late - aside from type constants, we can solve type variables however we wish, in accordance with bounds - but during dynamic checking mode it seems benign. We should revisit this.
    
    Reviewed By: francesco-zappa-nardelli
    
    Differential Revision: D34898380
    
    fbshipit-source-id: c3f62417d286f57071d2b23bfa3ddd198069a4a0

--- hphp/hack/src/typing/typing_solver.ml
+++ hphp/hack/src/typing/typing_solver.ml
@@ -522,9 +522,21 @@ let solve_tyvar_wrt_variance env r var =
     | (true, false)
     | (false, false) ->
       (* As in Local Type Inference by Pierce & Turner, if type variable does
-       * not appear at all, or only appears covariantly, solve to lower bound
+       * not appear at all, or only appears covariantly, solve to lower bound.
+       * If there are no lower bounds, and we've got type constant projections
+       * on the type variable, then solve to the upper bound anyway, hoping that
+       * this resolves the projections. Only do in dynamic checking mode,
+       * as otherwise this introduces a type soundness issue, for reasons
+       * that I don't understand. (See T78983983)
        *)
-      bind_to_lower_bound ~freshen:false env r var lower_bounds
+      if
+        Typing_env_types.(env.in_support_dynamic_type_method_check)
+        && ITySet.is_empty lower_bounds
+        && not (SMap.is_empty (Env.get_tyvar_type_consts env var))
+      then
+        bind_to_upper_bound env r var upper_bounds
+      else
+        bind_to_lower_bound ~freshen:false env r var lower_bounds
     | (false, true) ->
       (* As in Local Type Inference by Pierce & Turner, if type variable
        * appears only contravariantly, solve to upper bound

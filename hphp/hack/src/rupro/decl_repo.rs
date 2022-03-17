3806c3c5d64ee60893387f31449c19564d3b49fc
Jake Bailey (Hacklang) <jakebailey@fb.com>
2022-03-16 17:12:43 -0700

    Extend decl_repo to support folding decls
    Summary: Write HHI files to a tmpdir (to allow folding classes in WWW that extend builtins), allow configuring the number of threads in rayon's threadpool (to allow running in single-threaded mode), and provide a --fold flag which computes folded decls after completing the shallow-decl parse. Use the FileSummary type from our naming table crate to allow iterating over class names without having to re-decl-parse each file.
    
    Differential Revision: D34941655
    
    fbshipit-source-id: c8273c356ac550579bf9f7c81ef2b13cc5f552b7

--- hphp/hack/src/rupro/decl_repo.rs
+++ hphp/hack/src/rupro/decl_repo.rs
@@ -7,7 +7,8 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
 
-use std::path::PathBuf;
+use std::collections::BTreeMap;
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
 use indicatif::ParallelProgressIterator;
@@ -15,21 +16,37 @@ use jwalk::WalkDir;
 use rayon::prelude::*;
 use structopt::StructOpt;
 
-use hackrs::decl_parser::DeclParser;
-use hackrs::reason::{BReason, NReason, Reason};
-use hackrs::shallow_decl_provider::ShallowDeclCache;
+use hackrs::{
+    decl_parser::DeclParser,
+    folded_decl_provider::{FoldedDeclProvider, LazyFoldedDeclProvider},
+    reason::{BReason, NReason, Reason},
+    shallow_decl_provider::{EagerShallowDeclProvider, ShallowDeclCache},
+};
 use hackrs_test_utils::cache::NonEvictingCache;
 use hackrs_test_utils::serde_cache::{Compression, SerializingCache};
-use pos::{Prefix, RelativePath, RelativePathCtx};
+use names::FileSummary;
+use pos::{Prefix, RelativePath, RelativePathCtx, TypeName};
 
 #[derive(StructOpt, Debug)]
 struct CliOptions {
     /// The repository root (where .hhconfig is), e.g., ~/www
     root: PathBuf,
 
+    /// Set the number of threads to use for parsing and folding.
+    ///
+    /// If omitted or set to 0, uses the rayon default (the value of the
+    /// `RAYON_NUM_THREADS` environment variable if set, or the number of
+    /// logical CPUs otherwise).
+    #[structopt(long)]
+    num_threads: Option<usize>,
+
     /// Allocate decls with positions instead of allocating position-free decls.
     #[structopt(long)]
     with_pos: bool,
+
+    /// In addition to parsing shallow decls, compute folded class decls.
+    #[structopt(long)]
+    fold: bool,
 
     /// Store decls in a data store which serializes and compresses them.
     #[structopt(long)]
@@ -43,26 +60,34 @@ struct CliOptions {
 fn main() {
     let opts = CliOptions::from_args();
 
+    if let Some(num_threads) = opts.num_threads {
+        rayon::ThreadPoolBuilder::new()
+            .num_threads(num_threads)
+            .build_global()
+            .unwrap();
+    }
+
+    let hhi_root = tempdir::TempDir::new("rupro_decl_repo_hhi").unwrap();
+    hhi::write_hhi_files(hhi_root.path()).unwrap();
+
     let path_ctx = Arc::new(RelativePathCtx {
         root: opts.root.clone(),
-        hhi: PathBuf::new(),
+        hhi: hhi_root.path().into(),
         dummy: PathBuf::new(),
         tmp: PathBuf::new(),
     });
 
-    let (filenames, time_taken) = time(|| {
-        WalkDir::new(opts.root.clone())
-            .into_iter()
-            .filter_map(|e| e.ok())
-            .filter(|e| !e.file_type().is_dir() && find_utils::is_hack(&e.path()))
-            .map(|e| {
-                let path = e.path();
-                match path.strip_prefix(&path_ctx.root) {
-                    Ok(suffix) => RelativePath::new(Prefix::Root, suffix),
-                    Err(..) => RelativePath::new(Prefix::Dummy, &path),
-                }
-            })
-            .collect::<Vec<RelativePath>>()
+    let mut filenames: Vec<RelativePath> = find_hack_files(&path_ctx.hhi)
+        .map(|path| RelativePath::new(Prefix::Hhi, path.strip_prefix(&path_ctx.hhi).unwrap()))
+        .collect();
+
+    let ((), time_taken) = time(|| {
+        filenames.extend(find_hack_files(&opts.root).map(|path| {
+            match path.strip_prefix(&path_ctx.root) {
+                Ok(suffix) => RelativePath::new(Prefix::Root, suffix),
+                Err(..) => RelativePath::new(Prefix::Dummy, &path),
+            }
+        }))
     });
     println!(
         "Collected {} filenames in {:?}",
@@ -71,15 +96,76 @@ fn main() {
     );
 
     if opts.with_pos {
-        parse_repo::<BReason>(&opts, path_ctx, &filenames);
+        decl_repo::<BReason>(&opts, path_ctx, filenames);
     } else {
-        parse_repo::<NReason>(&opts, path_ctx, &filenames);
+        decl_repo::<NReason>(&opts, path_ctx, filenames);
     }
 }
 
-fn parse_repo<R: Reason>(opts: &CliOptions, ctx: Arc<RelativePathCtx>, filenames: &[RelativePath]) {
-    let decl_parser = DeclParser::<R>::new(ctx);
-    let shallow_decl_cache = if opts.serialize {
+fn decl_repo<R: Reason>(
+    opts: &CliOptions,
+    ctx: Arc<RelativePathCtx>,
+    filenames: Vec<RelativePath>,
+) {
+    let shallow_decl_cache = make_shallow_cache::<R>(opts);
+    let (summaries, time_taken) = time(|| parse(ctx, &shallow_decl_cache, filenames));
+    println!("Parsed repo in {:?}", time_taken);
+    print_rss();
+
+    let folded_decl_provider = make_folded_provider(opts, shallow_decl_cache);
+    if opts.fold {
+        let ((), time_taken) = time(|| fold(&folded_decl_provider, summaries));
+        println!("Folded repo in {:?}", time_taken);
+        print_rss();
+    }
+
+    // exit without running destructors (our cache is huge and full of Arcs)
+    std::process::exit(0);
+}
+
+fn parse<R: Reason>(
+    ctx: Arc<RelativePathCtx>,
+    shallow_decl_cache: &ShallowDeclCache<R>,
+    filenames: Vec<RelativePath>,
+) -> BTreeMap<RelativePath, FileSummary> {
+    let decl_parser = DeclParser::new(ctx);
+    let len = filenames.len();
+    filenames
+        .into_par_iter()
+        .progress_count(len as u64)
+        .map(|path| {
+            let (decls, summary) = decl_parser.parse_and_summarize(path).unwrap();
+            shallow_decl_cache.add_decls(decls);
+            (path, summary)
+        })
+        .collect()
+}
+
+fn fold<R: Reason>(
+    provider: &impl FoldedDeclProvider<R>,
+    summaries: BTreeMap<RelativePath, FileSummary>,
+) {
+    let len = summaries.len();
+    summaries
+        .into_par_iter()
+        .progress_count(len as u64)
+        .for_each(|(filename, summary)| {
+            for (class, _hash) in summary.classes() {
+                let class = TypeName::new(class);
+                provider
+                    .get_class(class.into(), class)
+                    .unwrap_or_else(|e| {
+                        panic!("failed to fold class {} in {:?}: {:?}", class, filename, e)
+                    })
+                    .unwrap_or_else(|| {
+                        panic!("failed to look up class {} in {:?}", class, filename)
+                    });
+            }
+        })
+}
+
+fn make_shallow_cache<R: Reason>(opts: &CliOptions) -> ShallowDeclCache<R> {
+    if opts.serialize {
         ShallowDeclCache::new(
             Arc::new(SerializingCache::with_compression(opts.compression)), // types
             Box::new(SerializingCache::with_compression(opts.compression)), // funs
@@ -96,24 +182,42 @@ fn parse_repo<R: Reason>(opts: &CliOptions, ctx: Arc<RelativePathCtx>, filenames
             Box::new(NonEvictingCache::default()),
             Box::new(NonEvictingCache::default()),
         )
-    };
-    let ((), time_taken) = time(|| {
-        filenames
-            .par_iter()
-            .progress_count(filenames.len() as u64)
-            .for_each(|&path| shallow_decl_cache.add_decls(decl_parser.parse(path).unwrap()))
-    });
-    println!("Parsed {} files in {:?}", filenames.len(), time_taken);
+    }
+}
 
+fn make_folded_provider<R: Reason>(
+    opts: &CliOptions,
+    shallow_decl_cache: ShallowDeclCache<R>,
+) -> impl FoldedDeclProvider<R> {
+    LazyFoldedDeclProvider::new(
+        Default::default(),
+        if opts.serialize {
+            Arc::new(SerializingCache::with_compression(opts.compression))
+        } else {
+            Arc::new(NonEvictingCache::default())
+        },
+        hackrs::special_names::SpecialNames::new(),
+        Arc::new(EagerShallowDeclProvider::new(Arc::new(shallow_decl_cache))),
+        Arc::new(hackrs_test_utils::registrar::DependencyGraph::new()),
+    )
+}
+
+fn find_hack_files(path: impl AsRef<Path>) -> impl Iterator<Item = PathBuf> {
+    WalkDir::new(path)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| !e.file_type().is_dir())
+        .map(|e| e.path())
+        .filter(|path| find_utils::is_hack(path))
+}
+
+fn print_rss() {
     let me = procfs::process::Process::myself().unwrap();
     let page_size = procfs::page_size().unwrap();
     println!(
         "RSS: {:.3}GiB",
         (me.stat.rss * page_size) as f64 / 1024.0 / 1024.0 / 1024.0
     );
-
-    // exit without running destructors (our cache is huge and full of Arcs)
-    std::process::exit(0);
 }
 
 fn time<T>(f: impl FnOnce() -> T) -> (T, std::time::Duration) {

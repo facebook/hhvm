8633427edb36ae0c06d7e53f0a906e92ba7d93f0
Josh Soref <jsoref@users.noreply.github.com>
2022-03-23 22:55:20 -0400

    spelling: ocaml
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- hphp/hack/src/shmffi/shmffi.rs
+++ hphp/hack/src/shmffi/shmffi.rs
@@ -558,7 +558,7 @@ pub extern "C" fn shmffi_get_and_deserialize(hash: u64) -> usize {
                     // Safety: we are not holding on to unrooted OCaml values.
                     //
                     // This value itself is unrooted, but we are not calling into
-                    // the OCalm runtime after this. The option that will be allocated
+                    // the OCaml runtime after this. The option that will be allocated
                     // later is allocated via ocamlpool, which cannot trigger the GC.
                     let deserialized_value = unsafe { heap_value.to_ocaml_value() };
 

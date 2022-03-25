bfc68e9958cc33106a18e950f03f1c1b31584dba
Hunter Goldstein <hgoldstein@fb.com>
2022-03-25 14:31:09 -0700

    call `Typing_check_job.check_module` in Typing_check_utils.type_file
    Summary:
    This is a utility that's meant to hook into `hh_single_type_check`, `hh --lint`, and `ServerCoverageMetric`. We have, yet again, fallen victim to projecting off of `FileInfo.t` rather than destructuring against it, meaning we have forgotten to process modules.
    
    Let's pattern match on `FileInfo.t` (plus lift out the `List.map ~f:snd |> List.filter_map ~f` pattern into a helper).
    
    Reviewed By: jamesjwu
    
    Differential Revision: D35033667
    
    fbshipit-source-id: 4497be806780af9a265ccadd5260fd03a4bba0a8

--- hphp/hack/src/typing/typing_check_utils.ml
+++ hphp/hack/src/typing/typing_check_utils.ml
@@ -10,32 +10,34 @@
 open Hh_prelude
 
 let type_file_with_global_tvenvs
-    tcopt fn { FileInfo.funs; classes; typedefs; consts; _ } =
+    tcopt
+    fn
+    {
+      FileInfo.funs;
+      classes;
+      typedefs;
+      consts;
+      modules;
+      hash = _;
+      file_mode = _;
+      comments = _;
+    } =
   let snd (_, x, _) = x in
   let (errors, (tast, global_tvenvs)) =
     Errors.do_with_context fn Errors.Typing (fun () ->
+        let check ~f defs =
+          List.map defs ~f:snd |> List.filter_map ~f:(fun x -> f tcopt fn x)
+        in
         let (fs, f_global_tvenvs) =
-          List.map funs ~f:snd
-          |> List.filter_map ~f:(fun x -> Typing_check_job.type_fun tcopt fn x)
-          |> List.unzip
+          check ~f:Typing_check_job.type_fun funs |> List.unzip
         in
         let (cs, c_global_tvenvs) =
-          List.map classes ~f:snd
-          |> List.filter_map ~f:(fun x ->
-                 Typing_check_job.type_class tcopt fn x)
-          |> List.unzip
+          check ~f:Typing_check_job.type_class classes |> List.unzip
         in
-        let ts =
-          List.map typedefs ~f:snd
-          |> List.filter_map ~f:(fun x ->
-                 Typing_check_job.check_typedef tcopt fn x)
-        in
-        let gcs =
-          List.map consts ~f:snd
-          |> List.filter_map ~f:(fun x ->
-                 Typing_check_job.check_const tcopt fn x)
-        in
-        ( fs @ cs @ ts @ gcs,
+        let ts = check ~f:Typing_check_job.check_typedef typedefs in
+        let gcs = check ~f:Typing_check_job.check_const consts in
+        let mds = check ~f:Typing_check_job.check_module modules in
+        ( fs @ cs @ ts @ gcs @ mds,
           lazy (List.concat (f_global_tvenvs :: c_global_tvenvs)) ))
   in
   (tast, global_tvenvs, errors)

28bfbce2bce65cb942e3a95a778694c90cbda349
Jake Bailey (Hacklang) <jakebailey@fb.com>
2022-03-28 18:20:22 -0700

    Add --naming-table support to decl_repo
    Summary: Add the option to do parsing lazily during folding by using a precomputed naming table on disk (as D34402402 (https://github.com/facebook/hhvm/commit/2d3ac5e83553b93921b854f59ae2ff90e2c9559d) did for `decl_file`).
    
    Reviewed By: shayne-fletcher
    
    Differential Revision: D35063498
    
    fbshipit-source-id: d245e43588b970504ff1334ec5bc075cd63c3ed2

--- hphp/hack/src/rupro/decl_repo.rs
+++ hphp/hack/src/rupro/decl_repo.rs
@@ -7,7 +7,6 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
 
-use std::collections::BTreeMap;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
@@ -15,16 +14,16 @@ use indicatif::ParallelProgressIterator;
 use jwalk::WalkDir;
 use rayon::prelude::*;
 use structopt::StructOpt;
+use tempdir::TempDir;
 
 use hackrs::{
     decl_parser::DeclParser,
-    folded_decl_provider::{FoldedDeclProvider, LazyFoldedDeclProvider},
+    folded_decl_provider::FoldedDeclProvider,
     reason::{BReason, NReason, Reason},
-    shallow_decl_provider::{EagerShallowDeclProvider, ShallowDeclCache},
+    shallow_decl_provider::ShallowDeclCache,
 };
 use hackrs_test_utils::cache::NonEvictingCache;
 use hackrs_test_utils::serde_cache::{Compression, SerializingCache};
-use names::FileSummary;
 use pos::{Prefix, RelativePath, RelativePathCtx, TypeName};
 
 #[derive(StructOpt, Debug)]
@@ -39,6 +38,10 @@ struct CliOptions {
     /// logical CPUs otherwise).
     #[structopt(long)]
     num_threads: Option<usize>,
+
+    /// Path to a SQLite naming table (allowing parsing to be done lazily instead of up-front).
+    #[structopt(long)]
+    naming_table: Option<PathBuf>,
 
     /// Allocate decls with positions instead of allocating position-free decls.
     #[structopt(long)]
@@ -67,7 +70,7 @@ fn main() {
             .unwrap();
     }
 
-    let hhi_root = tempdir::TempDir::new("rupro_decl_repo_hhi").unwrap();
+    let hhi_root = TempDir::new("rupro_decl_repo_hhi").unwrap();
     hhi::write_hhi_files(hhi_root.path()).unwrap();
 
     let path_ctx = Arc::new(RelativePathCtx {
@@ -77,90 +80,120 @@ fn main() {
         tmp: PathBuf::new(),
     });
 
-    let mut filenames: Vec<RelativePath> = find_hack_files(&path_ctx.hhi)
-        .map(|path| RelativePath::new(Prefix::Hhi, path.strip_prefix(&path_ctx.hhi).unwrap()))
-        .collect();
-
-    let ((), time_taken) = time(|| {
-        filenames.extend(find_hack_files(&opts.root).map(|path| {
-            match path.strip_prefix(&path_ctx.root) {
-                Ok(suffix) => RelativePath::new(Prefix::Root, suffix),
-                Err(..) => RelativePath::new(Prefix::Dummy, &path),
-            }
-        }))
-    });
-    println!(
-        "Collected {} filenames in {:?}",
-        filenames.len(),
-        time_taken
-    );
-
     if opts.with_pos {
-        decl_repo::<BReason>(&opts, path_ctx, filenames);
+        decl_repo::<BReason>(&opts, path_ctx, hhi_root);
     } else {
-        decl_repo::<NReason>(&opts, path_ctx, filenames);
+        decl_repo::<NReason>(&opts, path_ctx, hhi_root);
     }
 }
 
-fn decl_repo<R: Reason>(
-    opts: &CliOptions,
-    ctx: Arc<RelativePathCtx>,
-    filenames: Vec<RelativePath>,
-) {
-    let shallow_decl_cache = make_shallow_cache::<R>(opts);
-    let (summaries, time_taken) = time(|| parse(ctx, &shallow_decl_cache, filenames));
-    println!("Parsed repo in {:?}", time_taken);
-    print_rss();
+fn decl_repo<R: Reason>(opts: &CliOptions, ctx: Arc<RelativePathCtx>, hhi_root: TempDir) {
+    let names = collect_file_or_class_names(opts, &ctx);
 
-    let folded_decl_provider = make_folded_provider(opts, shallow_decl_cache);
+    let parser = DeclParser::new(ctx);
+    let shallow_decl_cache = make_shallow_cache::<R>(opts);
+    let classes = match names {
+        Names::Classnames(classes) => classes,
+        Names::Filenames(filenames) => {
+            let (classes, time_taken) =
+                time(|| parse(parser.clone(), &shallow_decl_cache, filenames));
+            println!("Parsed repo in {:?}", time_taken);
+            print_rss();
+            classes
+        }
+    };
+
+    let folded_decl_provider = make_folded_provider(opts, shallow_decl_cache, parser);
     if opts.fold {
-        let ((), time_taken) = time(|| fold(&folded_decl_provider, summaries));
+        let ((), time_taken) = time(|| fold(&folded_decl_provider, classes));
         println!("Folded repo in {:?}", time_taken);
         print_rss();
     }
 
-    // exit without running destructors (our cache is huge and full of Arcs)
+    // Avoid running the decl provider's destructor or destructors for hcons
+    // tables, since our caches are huge and full of Arcs which reference one
+    // another. Do destroy the hhi_root, so the tempdir gets cleaned up.
+    drop(hhi_root);
     std::process::exit(0);
 }
 
+enum Names {
+    Filenames(Vec<RelativePath>),
+    Classnames(Vec<TypeName>),
+}
+
+fn collect_file_or_class_names(opts: &CliOptions, ctx: &RelativePathCtx) -> Names {
+    if let Some(naming_table) = &opts.naming_table {
+        let (classes, time_taken) = time(|| {
+            let conn = rusqlite::Connection::open(naming_table).unwrap();
+            let mut stmt = conn
+                .prepare("select classes from naming_file_info")
+                .unwrap();
+            let mut rows = stmt.query(rusqlite::params![]).unwrap();
+            let mut classes = vec![];
+            while let Some(row) = rows.next().unwrap() {
+                let row: Option<String> = row.get(0).unwrap();
+                if let Some(row) = row {
+                    classes.extend(row.split('|').filter(|s| !s.is_empty()).map(TypeName::from));
+                }
+            }
+            classes
+        });
+        println!("Queried all classes from naming table in {:?}", time_taken);
+        Names::Classnames(classes)
+    } else {
+        let mut filenames: Vec<RelativePath> = find_hack_files(&ctx.hhi)
+            .map(|path| RelativePath::new(Prefix::Hhi, path.strip_prefix(&ctx.hhi).unwrap()))
+            .collect();
+
+        let ((), time_taken) = time(|| {
+            filenames.extend(find_hack_files(&opts.root).map(|path| {
+                match path.strip_prefix(&ctx.root) {
+                    Ok(suffix) => RelativePath::new(Prefix::Root, suffix),
+                    Err(..) => RelativePath::new(Prefix::Dummy, &path),
+                }
+            }))
+        });
+        println!(
+            "Collected {} filenames in {:?}",
+            filenames.len(),
+            time_taken
+        );
+        Names::Filenames(filenames)
+    }
+}
+
 fn parse<R: Reason>(
-    ctx: Arc<RelativePathCtx>,
+    decl_parser: DeclParser<R>,
     shallow_decl_cache: &ShallowDeclCache<R>,
     filenames: Vec<RelativePath>,
-) -> BTreeMap<RelativePath, FileSummary> {
-    let decl_parser = DeclParser::new(ctx);
+) -> Vec<TypeName> {
     let len = filenames.len();
     filenames
         .into_par_iter()
         .progress_count(len as u64)
-        .map(|path| {
+        .flat_map_iter(|path| {
             let (decls, summary) = decl_parser.parse_and_summarize(path).unwrap();
             shallow_decl_cache.add_decls(decls);
-            (path, summary)
+            summary
+                .classes()
+                .map(|(class, _hash)| TypeName::new(class))
+                .collect::<Vec<_>>()
+                .into_iter()
         })
         .collect()
 }
 
-fn fold<R: Reason>(
-    provider: &impl FoldedDeclProvider<R>,
-    summaries: BTreeMap<RelativePath, FileSummary>,
-) {
-    let len = summaries.len();
-    summaries
+fn fold<R: Reason>(provider: &impl FoldedDeclProvider<R>, classes: Vec<TypeName>) {
+    let len = classes.len();
+    classes
         .into_par_iter()
         .progress_count(len as u64)
-        .for_each(|(filename, summary)| {
-            for (class, _hash) in summary.classes() {
-                let class = TypeName::new(class);
-                provider
-                    .get_class(class.into(), class)
-                    .unwrap_or_else(|e| {
-                        panic!("failed to fold class {} in {:?}: {:?}", class, filename, e)
-                    })
-                    .unwrap_or_else(|| {
-                        panic!("failed to look up class {} in {:?}", class, filename)
-                    });
-            }
+        .for_each(|class| {
+            provider
+                .get_class(class.into(), class)
+                .unwrap_or_else(|e| panic!("failed to fold class {}: {:?}", class, e))
+                .unwrap_or_else(|| panic!("failed to look up class {}", class));
         })
 }
 
@@ -190,15 +223,30 @@ fn make_shallow_cache<R: Reason>(opts: &CliOptions) -> ShallowDeclCache<R> {
 fn make_folded_provider<R: Reason>(
     opts: &CliOptions,
     shallow_decl_cache: ShallowDeclCache<R>,
+    decl_parser: DeclParser<R>,
 ) -> impl FoldedDeclProvider<R> {
-    LazyFoldedDeclProvider::new(
+    hackrs::folded_decl_provider::LazyFoldedDeclProvider::new(
         Default::default(),
         if opts.serialize {
             Arc::new(SerializingCache::with_compression(opts.compression))
         } else {
             Arc::new(NonEvictingCache::default())
         },
-        Arc::new(EagerShallowDeclProvider::new(Arc::new(shallow_decl_cache))),
+        if let Some(naming_table) = &opts.naming_table {
+            let naming_table =
+                hackrs::naming_provider::SqliteNamingTable::new(naming_table).unwrap();
+            Arc::new(hackrs::shallow_decl_provider::LazyShallowDeclProvider::new(
+                Arc::new(shallow_decl_cache),
+                Arc::new(naming_table),
+                decl_parser,
+            ))
+        } else {
+            Arc::new(
+                hackrs::shallow_decl_provider::EagerShallowDeclProvider::new(Arc::new(
+                    shallow_decl_cache,
+                )),
+            )
+        },
         Arc::new(hackrs_test_utils::registrar::DependencyGraph::new()),
     )
 }

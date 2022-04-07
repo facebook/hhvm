37bbcf3653a26c0fa9d8b1c4b60fdd0ed52f553e
Aaron Orenstein <aorenste@fb.com>
2022-04-07 09:42:05 -0700

    Fix hhbc-ast.h generation to include the macro-generated Opcode enum
    Summary:
    The Opcode enum is filled in by a macro which is invisible to the ffi_cbindgen program.  So instead we have to use dump-opcodes to emit the output of the macro and pass that file to ffi_cbindgen instead.
    
    dump-opcodes tried to be clever and pipe its output through rustfmt - unfortunately since it just calls "rustfmt" directly it didn't handle the case where our build machines didn't have rustfmt in the path.
    
    While I was adding command-line option support to dump-opcodes anyway I added an `--output` flag.
    
    Reviewed By: kavoor
    
    Differential Revision: D35453949
    
    fbshipit-source-id: 7f5206f57143358dcd099b08b492a1812556df7d

--- hphp/hack/src/hackc/hhbc/dump_opcodes.rs
+++ hphp/hack/src/hackc/hhbc/dump_opcodes.rs
@@ -7,34 +7,64 @@ use anyhow::Result;
 use hhbc_gen::OpcodeData;
 use quote::quote;
 use std::{
+    fs::File,
     io::Write,
+    path::PathBuf,
     process::{Command, Stdio},
 };
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(no_version)]
+struct Opts {
+    #[structopt(short = "o", long = "out")]
+    output: Option<PathBuf>,
+
+    #[structopt(long)]
+    no_format: bool,
+}
 
 fn main() -> Result<()> {
+    let opts = Opts::from_args();
+
     let mut opcode_data: Vec<OpcodeData> = hhbc_gen::opcode_data().to_vec();
     opcode_data.sort_by(|a: &OpcodeData, b: &OpcodeData| a.name.cmp(b.name));
 
-    let opcodes = emit_opcodes::emit_opcodes(
-        quote!(
-            enum Opcodes<'arena> {}
-        ),
-        &opcode_data,
-    )?;
+    let input = quote!(
+        #[emit_opcodes_macro::emit_opcodes]
+        #[derive(Clone, Debug, Targets)]
+        #[repr(C)]
+        pub enum Opcode<'arena> {
+            // This is filled in by the emit_opcodes macro.  It can be printed using the
+            // "//hphp/hack/src/hackc/hhbc:dump-opcodes" binary.
+        }
+    );
 
-    let targets = emit_opcodes::emit_impl_targets(
-        quote!(
-            enum Opcodes<'arena> {}
-        ),
-        &opcode_data,
-    )?;
+    let opcodes = emit_opcodes::emit_opcodes(input.clone(), &opcode_data)?;
+    let targets = emit_opcodes::emit_impl_targets(input, &opcode_data)?;
 
     let output = format!("{}\n\n{}", opcodes, targets);
 
-    let mut child = Command::new("rustfmt")
-        .args(["--emit", "stdout"])
-        .stdin(Stdio::piped())
-        .spawn()?;
+    if opts.no_format {
+        if let Some(out) = opts.output.as_ref() {
+            std::fs::write(out, output)?;
+        } else {
+            println!("{}", output);
+        }
+        return Ok(());
+    }
+
+    let mut child = Command::new("rustfmt");
+    child.args(["--emit", "stdout"]);
+    child.stdin(Stdio::piped());
+
+    if let Some(out) = opts.output.as_ref() {
+        use std::os::unix::io::{FromRawFd, IntoRawFd};
+        let file = File::create(out).expect("couldn't create output file");
+        child.stdout(unsafe { Stdio::from_raw_fd(file.into_raw_fd()) });
+    }
+
+    let mut child = child.spawn()?;
     child.stdin.as_mut().unwrap().write_all(output.as_bytes())?;
     child.wait()?;
 

0429eafb45b83e2098f9a154ac7bac1c61e8098e
Edwin Smith <smith@fb.com>
2022-04-08 11:43:12 -0700

    reap get_unique_id_for_scope()
    Summary: Dead code
    
    Reviewed By: aorenste
    
    Differential Revision: D35408245
    
    fbshipit-source-id: f3358c7f1b80342841a0673271f387f7e48eb480

--- hphp/hack/src/hackc/utils/unique_id_builder.rs
+++ hphp/hack/src/hackc/utils/unique_id_builder.rs
@@ -3,8 +3,6 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
 
-use ast_scope::{Scope, ScopeItem};
-
 pub type SMap<T> = std::collections::BTreeMap<String, T>;
 pub type SSet = std::collections::BTreeSet<String>;
 
@@ -18,19 +16,4 @@ pub fn get_unique_id_for_method(cls_name: &str, md_name: &str) -> String {
 
 pub fn get_unique_id_for_function(fun_name: &str) -> String {
     format!("|{}", fun_name)
-}
-
-pub fn get_unique_id_for_scope(scope: &Scope<'_, '_>) -> String {
-    match scope.items.as_slice() {
-        [] => get_unique_id_for_main(),
-        [.., ScopeItem::Class(cls), ScopeItem::Method(md)]
-        | [
-            ..,
-            ScopeItem::Class(cls),
-            ScopeItem::Method(md),
-            ScopeItem::Lambda(_),
-        ] => get_unique_id_for_method(cls.get_name_str(), md.get_name_str()),
-        [.., ScopeItem::Function(fun)] => get_unique_id_for_function(fun.get_name_str()),
-        _ => panic!("unexpected scope shape"),
-    }
 }

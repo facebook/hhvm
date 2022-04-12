19847cfcc71a9ab430b983f62a59633c87399f51
Michael O'Farrell <michaelofarrell@fb.com>
2022-04-12 05:26:43 -0700

    Use an explicit stack for PostorderSort
    Summary:
    PostorderSort makes use of the call stack for its DFS walk of the unit.  Create
    a heap allocated stack for the walk so that we don't blow the stack, and get
    the appropriate max size of 8 bytes per block.  Rather than incur resizes this
    allocates enough space for all blocks to be on the stack simultaneously.
    
    Reviewed By: ricklavoie
    
    Differential Revision: D35561888
    
    fbshipit-source-id: e48781a6739a89fc58198439a08552c8fa434463

--- hphp/runtime/vm/jit/cfg-inl.h
+++ hphp/runtime/vm/jit/cfg-inl.h
@@ -30,22 +30,39 @@ namespace detail {
 template<class Visitor>
 struct PostorderSort {
   PostorderSort(Visitor& visitor, unsigned num_blocks)
-    : m_visited(num_blocks), m_visitor(visitor)
-  {}
+    : m_visited(num_blocks), m_complete(num_blocks), m_visitor(visitor)
+  {
+    m_stack.reserve(num_blocks);
+  }
 
   void walk(Block* block) {
-    if (m_visited.test(block->id())) return;
-    m_visited.set(block->id());
-    // Valid blocks can't be empty, but we can see empty blocks here
-    // when trying to print a unit before it's finished for debugging.
-    if (block->empty()) return;
-    if (auto const t = block->taken()) walk(t);
-    if (auto const n = block->next()) walk(n);
-    m_visitor(block);
+    auto const enqueue = [this] (Block* b) {
+      if (m_visited.test(b->id())) return;
+      m_stack.emplace_back(b);
+    };
+    enqueue(block);
+    while (!m_stack.empty()) {
+      block = m_stack.back();
+      if (!m_visited.test(block->id())) {
+        m_visited.set(block->id());
+        if (auto const n = block->next()) enqueue(n);
+        if (auto const t = block->taken()) enqueue(t);
+      } else {
+        m_stack.pop_back();
+        // Valid blocks can't be empty, but we can see empty blocks here when
+        // trying to print a unit before it's finished for debugging.
+        if (!m_complete.test(block->id()) && !block->empty()) {
+          m_complete.set(block->id());
+          m_visitor(block);
+        }
+      }
+    }
   }
 
 private:
   boost::dynamic_bitset<> m_visited;
+  boost::dynamic_bitset<> m_complete;
+  jit::vector<Block*> m_stack;
   Visitor& m_visitor;
 };
 

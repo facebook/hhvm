cefe1fb9ead4ee5a3bb6b884d943296d4ba6821a
Jeff Hemphill <jhemphill@fb.com>
2022-04-14 22:10:05 -0700

    Convert `watchman.socket.default` into a template string with an expandable "%{user}" tag
    Summary:
    We use Watchman to track the files and file changes on a given repository. To communicate with the correct Watchman server, we first check which user owns the repo we're watching, then we resolve that username to a socket path.
    
    Today, we "resolve that username to a socket path" by finding the user's homedir, finding a `.hphp` file in the homedir, and finding a `watchman.socket.default` option in the `.hphp` file. This is kind of a roundabout way to do it. Also, not all users have homedirs with `.hphp` files. Fortunately, our watchman sockets are all in paths of the form `/var/facebook/watchman/$USER-state/sock`. So instead of going through homedirs, we can just write a template string: `"/var/facebook/watchman/%{user}-state/sock"`. Then we can expand `%{user}` to the correct username.
    
    Reviewed By: periodic1236
    
    Differential Revision: D35412413
    
    fbshipit-source-id: e65f01c693db26cf2b695454a172b3b2fadf6c69

--- hphp/runtime/base/watchman-connection.cpp
+++ hphp/runtime/base/watchman-connection.cpp
@@ -14,15 +14,21 @@
    +----------------------------------------------------------------------+
 */
 
+#include <exception>
+#include <string>
+#include <string_view>
+
 #include "hphp/runtime/base/watchman-connection.h"
 
 #include "hphp/runtime/base/config.h"
 #include "hphp/runtime/base/ini-setting.h"
 #include "hphp/runtime/base/runtime-option.h"
+#include "hphp/util/optional.h"
 #include "hphp/util/trace.h"
 #include "hphp/util/user-info.h"
 
 #include <folly/concurrency/ConcurrentHashMap.h>
+#include <folly/Format.h>
 #include <folly/Singleton.h>
 
 TRACE_SET_MOD(watchman);
@@ -31,22 +37,62 @@ namespace HPHP {
 
 namespace {
 
+constexpr std::string_view kUserTemplateTag = "%{user}";
+
+/**
+ * Resolve the given template to return a Watchman socket owned by the given
+ * user.
+ *
+ * Watchman sockets are often scoped by the user who runs the server process.
+ * If Wez has a Watchman server, maybe we can communicate with Wez's server by
+ * going to `/var/watchman/wez-state/sock`. The user may pass a template
+ * string like `/var/watchman/%{user}-state/sock`. This function resolves the
+ * template tag `"%{user}"` into "wez".
+ *
+ * We're resolving a UID into a Linux username on the machine. Linux usernames
+ * already have to play nicely with the directory system. You can't have a user
+ * named `"../"`, or they wouldn't be able to have a homedir.
+ */
+std::string format_watchman_socket(
+    std::string_view watchmanSocketTemplate, Optional<std::string_view> user) {
+  std::string watchmanSocket{watchmanSocketTemplate};
+  size_t userTagPos = watchmanSocket.find(kUserTemplateTag);
+  if (userTagPos == std::string::npos) {
+    return watchmanSocket;
+  }
+
+  if (!user) {
+    throw std::runtime_error{folly::sformat(
+      "We could not find a valid Unix username to expand {} to a path",
+      watchmanSocket)};
+  }
+
+  // Replace each occurrence of "%{user}" with the username we found
+  assertx(user->find(kUserTemplateTag) == std::string_view::npos);
+  do {
+    watchmanSocket.replace(userTagPos, kUserTemplateTag.size(), *user);
+    userTagPos = watchmanSocket.find(kUserTemplateTag);
+  } while (userTagPos != std::string::npos);
+
+  return watchmanSocket;
+}
+
 /**
  * Discover who owns the given repo and return the Watchman socket
  * corresponding to that user.
  */
 Optional<std::string> find_user_socket(const folly::fs::path& repoRoot) {
-  auto def = [] () -> Optional<std::string> {
-    FTRACE(3, "No per-user watchman socket found.\n");
+  auto def = [] (Optional<std::string_view> user) -> Optional<std::string> {
+    FTRACE(3, "Using watchman.socket.default = {}.\n", RO::WatchmanDefaultSocket);
     if (RO::WatchmanDefaultSocket.empty()) return {};
-    return RO::WatchmanDefaultSocket;
+    return format_watchman_socket(RO::WatchmanDefaultSocket, user);
   };
   int repoRootFD = ::open(repoRoot.native().c_str(), O_DIRECTORY | O_RDONLY);
-  if (repoRootFD == -1) return def();
+  if (repoRootFD == -1) return def(std::nullopt);
   SCOPE_EXIT { ::close(repoRootFD); };
 
   struct ::stat hstat {};
-  if (::fstat(repoRootFD, &hstat) != 0) return def();
+  if (::fstat(repoRootFD, &hstat) != 0) return def(std::nullopt);
 
   // The repo is owned by root, so use a special root socket
   if (hstat.st_uid == 0) {
@@ -55,8 +101,13 @@ Optional<std::string> find_user_socket(const folly::fs::path& repoRoot) {
         3,
         "{} is owned by root, looking for socket at {}\n",
         repoRoot.native(),
-        rootSock.empty() ? rootSock : "<none>");
-    return rootSock.empty() ? def() : rootSock;
+        rootSock.empty() ? "<none>" : rootSock);
+    if (rootSock.empty()) {
+      std::string root{"root"};
+      return def(root);
+    } else {
+      return rootSock;
+    }
   }
 
   // Find the `watchman.socket` setting in the repo owner's
@@ -65,16 +116,18 @@ Optional<std::string> find_user_socket(const folly::fs::path& repoRoot) {
   UserInfo info{hstat.st_uid};
   auto user = std::string{info.pw->pw_name};
   auto homePath = RO::GetHomePath(user);
-  if (!homePath) return def();
+  if (!homePath) return def(user);
 
   IniSetting::Map ini = IniSetting::Map::object;
   Hdf config;
 
   auto confFileName = (*homePath) / RO::SandboxConfFile;
-  if (!RO::ReadPerUserSettings(confFileName, ini, config)) return def();
+  if (!RO::ReadPerUserSettings(confFileName, ini, config)) {
+    return def(user);
+  }
 
   auto sock = Config::GetString(ini, config, "watchman.socket.default");
-  return sock.empty() ? def() : sock;
+  return sock.empty() ? def(user) : sock;
 }
 
 using WatchmanCache = folly::ConcurrentHashMap<

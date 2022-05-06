e5f9f888551ee857e5d9fed7c1adec65d15b3c61
Mistral Orhan Jean-Pierre Contrastin <mojpc2@fb.com>
2022-05-06 10:11:21 -0700

    Use suggestive grammar for logging functions
    Summary: Pluralises some logging function names in line with their arguments.
    
    Reviewed By: enetsee, hgoldstein
    
    Differential Revision: D36097257
    
    fbshipit-source-id: 85e865f91f2446226fe5fc56c8c303a3553009b5

--- hphp/hack/src/typing/tast_check/shape_analysis_logger.ml
+++ hphp/hack/src/typing/tast_check/shape_analysis_logger.ml
@@ -11,7 +11,7 @@ module A = Aast
 module SA = Shape_analysis
 module Env = Tast_env
 
-let log_result_locally typing_env id results : unit =
+let log_results_locally typing_env id results : unit =
   let log_shape result =
     Format.sprintf "%s: %s\n" id (SA.show_shape_result typing_env result)
     |> Out_channel.output_string !Typing_log.out_channel;
@@ -19,18 +19,18 @@ let log_result_locally typing_env id results : unit =
   in
   List.iter results ~f:log_shape
 
-let log_result typing_env id results : unit =
+let log_results typing_env id results : unit =
   let shape_analysis_log_level =
     Typing_env.get_tcopt typing_env
     |> TypecheckerOptions.log_levels
     |> SMap.find_opt "shape_analysis"
   in
   match shape_analysis_log_level with
-  | Some 1 -> log_result_locally typing_env id results
+  | Some 1 -> log_results_locally typing_env id results
   | Some 2 -> Shape_analysis_scuba.log_results_remotely typing_env id results
   | _ -> ()
 
-let compute_result tast_env params body =
+let compute_results tast_env params body =
   let typing_env = Tast_env.tast_env_as_typing_env tast_env in
   try
     SA.callable tast_env params body
@@ -51,7 +51,7 @@ let handler =
       let typing_env = Tast_env.tast_env_as_typing_env tast_env in
       let handle_method A.{ m_body; m_name = (_, method_name); m_params; _ } =
         let id = class_name ^ "::" ^ method_name in
-        compute_result tast_env m_params m_body |> log_result typing_env id
+        compute_results tast_env m_params m_body |> log_results typing_env id
       in
       if should_not_skip tast_env then List.iter ~f:handle_method c_methods
 
@@ -59,5 +59,5 @@ let handler =
       let A.{ f_body; f_name = (_, id); f_params; _ } = fd.A.fd_fun in
       if should_not_skip tast_env then
         let typing_env = Tast_env.tast_env_as_typing_env tast_env in
-        compute_result tast_env f_params f_body |> log_result typing_env id
+        compute_results tast_env f_params f_body |> log_results typing_env id
   end

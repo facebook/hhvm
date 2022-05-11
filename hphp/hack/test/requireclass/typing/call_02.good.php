0e4d8adf9a9d1504744a9c860e00147c0da4f189
Francesco Zappa Nardelli <fzn@fb.com>
2022-05-11 03:39:27 -0700

    a more Hack tests
    Summary: A few more Hack tests for the `require class` experimental feature.
    
    Reviewed By: vsiles
    
    Differential Revision: D36307465
    
    fbshipit-source-id: 3ad365ef48acc3ca017cd8205e8bf198e8731bc2

---
+++ hphp/hack/test/requireclass/typing/call_02.good.php
@@ -0,0 +1,20 @@
+<?hh
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the "hack" directory of this source tree.
+
+<<file:__EnableUnstableFeatures('require_class')>>
+
+trait T {
+  require class C;
+
+  public function foo(): void { }
+}
+
+class C {
+  use T;
+
+  public function bar(): void { $this->foo(); }
+
+}
---
+++ hphp/hack/test/requireclass/typing/call_03.good.php
@@ -0,0 +1,21 @@
+<?hh
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the "hack" directory of this source tree.
+
+<<file:__EnableUnstableFeatures('require_class')>>
+
+trait T {
+  require class C;
+
+  public function foo(): void { $this->bar(); }
+}
+
+class D {
+  public function bar(): void {}
+}
+
+class C extends D {
+  use T;
+}

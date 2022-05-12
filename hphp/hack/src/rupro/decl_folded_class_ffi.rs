d2036150836c9ba5a88af82d0624ef9a1478fb77
Thomas Jiang <thomasjiang@fb.com>
2022-05-12 15:22:28 -0700

    Add ffi function to shallow decl and fold a partition of a directory
    Summary:
    Previous, I attempted to fold www without ooming by writing the computed folded decls to disk to avoid storing them all in memory. This worked on my devserver... But it doesn't work on sandcastle. Even though the requests don't always OOM - the sandcastle workers die.
    
    Perhaps this is because the machines that have enough RAM to hold the shallow decls in memory and www don't have any hard drive space and instead are all flash. At any rate, after fumbling with that approach for a while, instead, the idea is to partition the classes in www and fold over them in segments - hoping that this goes under the OOM bar on these XLARGE sandcastle machines.
    
    This diff adds the rust logic that does this. Instead of writing stuff to disk (which was very slow), just keep everything in memory and pass it back from rust to ocaml.
    
    Reviewed By: shayne-fletcher
    
    Differential Revision: D36335637
    
    fbshipit-source-id: 2e5a56db53e6edb85bc8e27012e1320f1e3c6600

--- hphp/hack/src/rupro/decl_folded_class_ffi.rs
+++ hphp/hack/src/rupro/decl_folded_class_ffi.rs
@@ -4,16 +4,30 @@
 
 use hackrs::decl_parser::DeclParser;
 use hackrs::folded_decl_provider::FoldedDeclProvider;
-use hackrs_test_utils::cache::make_shallow_decl_cache;
+use hackrs_test_utils::cache::{make_shallow_decl_cache, populate_shallow_decl_cache};
 use hackrs_test_utils::serde_cache::CacheOpts;
+use hackrs_test_utils::serde_cache::Compression;
+use indicatif::ParallelProgressIterator;
+use jwalk::WalkDir;
 use ocamlrep_ocamlpool::{ocaml_ffi_with_arena, Bump};
 use oxidized_by_ref::{decl_defs::DeclClassType, parser_options::ParserOptions};
-use pos::{RelativePath, RelativePathCtx, ToOxidized, TypeName};
+use pos::{Prefix, RelativePath, RelativePathCtx, ToOxidized, TypeName};
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use std::cmp;
 use std::collections::BTreeMap;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use ty::decl::{folded::FoldedClass, shallow};
 use ty::reason::BReason;
+
+fn find_hack_files(path: impl AsRef<Path>) -> impl Iterator<Item = PathBuf> {
+    WalkDir::new(path)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| !e.file_type().is_dir())
+        .map(|e| e.path())
+        .filter(|path| find_utils::is_hack(path))
+}
 
 ocaml_ffi_with_arena! {
     fn fold_classes_in_files_ffi<'a>(
@@ -77,5 +91,84 @@ ocaml_ffi_with_arena! {
     fn show_decl_class_type_ffi<'a>(arena: &'a Bump, decl: &'a DeclClassType<'a>) -> String {
         let decl = <FoldedClass<BReason>>::from(decl);
         format!("{:#?}", decl)
+    }
+
+
+    // Due to memory constraints when folding over a large directory, it may be necessary to
+    // fold over www in parts. This is achieved by finding the shallow decls of all of the files
+    // in the directory but then only folding over a portion of the classes in those files
+    // num_partitions controls the numbers of partitions (>= 1) the classes are divided into
+    // and partition_index controls which partition is being folded
+    // Returns a SMap from class_name to folded_decl
+    fn partition_and_fold_dir_ffi<'a>(
+        arena: &'a Bump,
+        www_root: &'a Path,
+        opts: &'a ParserOptions<'a>,
+        num_partitions: &'a usize,
+        partition_index: &'a usize,
+    ) -> BTreeMap<String, DeclClassType<'a>> {
+        // Collect hhi files
+        let hhi_root = tempdir::TempDir::new("rupro_decl_repo_hhi").unwrap();
+        hhi::write_hhi_files(hhi_root.path()).unwrap();
+        let hhi_root_path: PathBuf = hhi_root.path().into();
+        let mut filenames: Vec<RelativePath> = find_hack_files(&hhi_root_path)
+            .map(|path| RelativePath::new(Prefix::Hhi, path.strip_prefix(&hhi_root_path).unwrap()))
+            .collect();
+        // Collect www files
+        filenames.extend(
+            find_hack_files(&www_root).map(|path| match path.strip_prefix(&www_root) {
+                Ok(suffix) => RelativePath::new(Prefix::Root, suffix),
+                Err(..) => RelativePath::new(Prefix::Dummy, &path),
+            }),
+        );
+
+        let path_ctx = Arc::new(RelativePathCtx {
+            root: www_root.into(),
+            hhi: hhi_root_path,
+            ..Default::default()
+        });
+
+        // Parse and gather shallow decls
+        let file_provider: Arc<dyn file_provider::FileProvider> = Arc::new(file_provider::PlainFileProvider::new(Arc::clone(&path_ctx)));
+        let decl_parser: DeclParser<BReason> = DeclParser::with_options(file_provider, opts);
+        let shallow_decl_cache = make_shallow_decl_cache(CacheOpts::Serialized(Compression::default()));
+        let mut classes =
+            populate_shallow_decl_cache(&shallow_decl_cache, decl_parser.clone(), &filenames);
+        classes.sort();
+        let folded_decl_provider = hackrs_test_utils::decl_provider::make_folded_decl_provider(
+            CacheOpts::Serialized(Compression::default()),
+            None,
+            shallow_decl_cache,
+            decl_parser,
+        );
+
+        let len = classes.len();
+        // Add 1 to size to ensure that we only have num_partitions
+        let size_of_slices = (len / num_partitions) + 1;
+        // Account for edge case where partition_index * size goes out of bounds of the array
+        let start_index = cmp::min(len, partition_index * size_of_slices);
+        // end_index is exclusive, so no need to say len - 1
+        let end_index = cmp::min((partition_index + 1) * size_of_slices, len);
+        // If start_index = end_index, this will be empty vec
+        let s: Vec<(String, Arc<FoldedClass<BReason>>)> = (&classes[start_index..end_index])
+            .into_par_iter()
+            .progress_count(len.try_into().unwrap())
+            .map(|class| {
+                let folded_decl = folded_decl_provider
+                    .get_class((*class).into(), *class)
+                    .expect("failed to fold class")
+                    .expect("failed to look up class");
+                (class.as_str().into(), folded_decl)
+            })
+            .collect();
+        s.into_iter()
+            .map(|(name, fc)| (name, fc.to_oxidized(arena)))
+            .collect()
+    }
+
+    fn decls_equal_ffi<'a>(arena: &'a Bump, ocaml_decl: &'a DeclClassType<'a>, rust_decl: &'a DeclClassType<'a>) -> bool {
+        let ocaml_decl = <FoldedClass<BReason>>::from(ocaml_decl);
+        let rust_decl = <FoldedClass<BReason>>::from(rust_decl);
+        ocaml_decl == rust_decl
     }
 }

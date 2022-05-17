5fe056edfbb230c342e444af55163790cba74b17
Jake Bailey <jakebailey@fb.com>
2022-05-17 15:53:11 -0700

    Connect Db_path_provider to Rust_provider_backend
    Reviewed By: shayne-fletcher
    
    Differential Revision: D36307337
    
    fbshipit-source-id: 1c4f225ccf0b4534f9b342345913914c1758b0bc

--- hphp/hack/src/providers/db_path_provider.ml
+++ hphp/hack/src/providers/db_path_provider.ml
@@ -8,7 +8,7 @@
 
 open Hh_prelude
 
-(** This sharedmem is used only for the Shared_memory backend *)
+(** This sharedmem is used only for the Shared_memory and Analysis backends *)
 module Shared_db_settings =
   SharedMem.Heap
     (SharedMem.ImmediateBackend (SharedMem.NonEvictable)) (StringKey)
@@ -33,10 +33,14 @@ let get_naming_db_path (backend : Provider_backend.t) :
       | Provider_backend.Shared_memory ),
       `Shmem_cached_path path_opt ) ->
     path_opt
-  | ( ( Provider_backend.Analysis | Provider_backend.Rust_provider_backend _
-      | Provider_backend.Shared_memory ),
+  | ( (Provider_backend.Analysis | Provider_backend.Shared_memory),
       `Shmem_not_yet_cached_path ) ->
     let path_opt = Shared_db_settings.get "database_path" in
+    naming_db_path_cache := `Shmem_cached_path path_opt;
+    path_opt
+  | (Provider_backend.Rust_provider_backend backend, `Shmem_not_yet_cached_path)
+    ->
+    let path_opt = Rust_provider_backend.Naming.get_db_path backend in
     naming_db_path_cache := `Shmem_cached_path path_opt;
     path_opt
   | (Provider_backend.Local_memory { Provider_backend.naming_db_path_ref; _ }, _)
@@ -50,10 +54,14 @@ let set_naming_db_path
     (naming_db_path : Naming_sqlite.db_path option) : unit =
   match backend with
   | Provider_backend.Analysis
-  | Provider_backend.Rust_provider_backend _
   | Provider_backend.Shared_memory ->
     Shared_db_settings.remove_batch (SSet.singleton "database_path");
     Option.iter naming_db_path ~f:(Shared_db_settings.add "database_path");
+    naming_db_path_cache := `Shmem_cached_path naming_db_path
+  | Provider_backend.Rust_provider_backend backend ->
+    Option.iter
+      naming_db_path
+      ~f:(Rust_provider_backend.Naming.set_db_path backend);
     naming_db_path_cache := `Shmem_cached_path naming_db_path
   | Provider_backend.Local_memory { Provider_backend.naming_db_path_ref; _ } ->
     naming_db_path_ref := naming_db_path

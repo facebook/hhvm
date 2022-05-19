4e7165ff6a6455c7a6851ebb99aed6eef3f3d858
Elton Leander Pinto <eltonpinto@fb.com>
2022-05-19 14:57:12 -0700

    Add test for global write check (mutation inside function call)
    Summary:
    Test that checks if the global write check detects writes to global variables
    passed as arguments to a function call.
    
    The checker doesn't detect the write to the global variable `$bar` is mutated inside the `plus_one` function, `$fun` anonymous function and `Test::plus_two` method, as can be seen in the output. This test case, therefore,
    demonstrates a false negative.
    
    We will need to update the expected output when this case is handled correctly.
    
    Differential Revision: D36519873
    
    fbshipit-source-id: 242750aacee0f2ad91c0d6a73eb6cb8e22bf597b

---
+++ hphp/hack/test/typecheck/global_write_check/test_function_call.php
@@ -0,0 +1,35 @@
+<?hh //strict
+
+class Bar {
+  public int $prop = 0;
+}
+
+class Foo {
+  <<__LateInit>> public static Bar $bar;
+}
+
+function plus_one(Bar $b): void {
+  $b->prop++; // A global variable is written
+}
+
+class Test {
+  public function plus_two(Bar $b): void {
+    $b->prop += 2; // A global variable is written
+  }
+
+  public function test_method_call(): void {
+    Foo::$bar = new Bar(); // A global variable is written
+
+    // Test function call
+    plus_one(Foo::$bar); // A global variable is passed to a function call
+
+    // Test anonymous function call
+    $fun = (Bar $b): void ==> {
+      $b->prop = 2; // A global variable is written
+    };
+    $fun(Foo::$bar); // A global variable is passed to an anonymous function
+
+    // Test method call
+    $this->plus_two(Foo::$bar); // A global variable is passed to a method
+  }
+}

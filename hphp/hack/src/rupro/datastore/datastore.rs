75951f6ba80b6553a22a34520be8418f9955d95d
Jake Bailey <jakebailey@fb.com>
2022-05-18 17:37:45 -0700

    Move ChangesStore into submodule
    Summary: In preparation for adding more store helpers like ChangesStore, move it into a submodule.
    
    Reviewed By: shayne-fletcher
    
    Differential Revision: D36465270
    
    fbshipit-source-id: 0cc110d1a53d63f0c96a93d9c3366812d85087ec

--- hphp/hack/src/rupro/datastore/datastore.rs
+++ hphp/hack/src/rupro/datastore/datastore.rs
@@ -3,10 +3,11 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
 
-use dashmap::DashMap;
-use parking_lot::RwLock;
+mod changes_store;
+
 use std::fmt::Debug;
-use std::hash::Hash;
+
+pub use changes_store::ChangesStore;
 
 /// A threadsafe datastore, intended for global decl storage. The key type is
 /// intended to be a `Symbol` or tuple of `Symbol`s, and the value type is
@@ -22,71 +23,4 @@ pub trait Store<K: Copy, V>: Debug + Send + Sync {
 pub trait LocalStore<K: Copy, V>: Debug {
     fn get(&self, key: K) -> Option<V>;
     fn insert(&mut self, key: K, val: V);
-}
-
-/// A stack of temporary changes on top of a fallback data store.
-pub struct ChangesStore<K, V, F> {
-    stack: RwLock<Vec<DashMap<K, Option<V>>>>,
-    fallback: F,
-}
-
-impl<K: Copy + Hash + Eq, V: Clone, F: Store<K, V>> ChangesStore<K, V, F> {
-    pub fn new(fallback: F) -> Self {
-        Self {
-            stack: RwLock::new(vec![Default::default()]),
-            fallback,
-        }
-    }
-
-    pub fn get(&self, key: K) -> Option<V> {
-        for store in self.stack.read().iter() {
-            if let Some(val_opt) = store.get(&key) {
-                return val_opt.clone();
-            }
-        }
-        self.fallback.get(key)
-    }
-
-    pub fn insert(&self, key: K, val: V) {
-        let stack = self.stack.read();
-        let store = stack.last().expect("empty stack");
-        store.insert(key, Some(val));
-    }
-
-    pub fn push_local_changes(&self) {
-        self.stack.write().push(Default::default())
-    }
-
-    pub fn pop_local_changes(&self) {
-        self.stack.write().pop();
-    }
-
-    pub fn remove_batch<I: Iterator<Item = K>>(&self, keys: I) {
-        let stack = self.stack.read();
-        let store = stack.last().expect("empty stack");
-        for key in keys {
-            store.insert(key, None);
-        }
-    }
-}
-
-impl<K, V, F> Store<K, V> for ChangesStore<K, V, F>
-where
-    K: Copy + Hash + Eq + Send + Sync,
-    V: Clone + Send + Sync,
-    F: Store<K, V>,
-{
-    fn get(&self, key: K) -> Option<V> {
-        ChangesStore::get(self, key)
-    }
-
-    fn insert(&self, key: K, val: V) {
-        ChangesStore::insert(self, key, val)
-    }
-}
-
-impl<K: Copy, V, F> std::fmt::Debug for ChangesStore<K, V, F> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("ChangesStore").finish()
-    }
 }

25385a8d45b77e0f257804814c0afb366702f9b7
Oguz Ulgen <oulgen@fb.com>
2022-05-21 14:06:04 -0700

    Improve module reflection unit test
    Reviewed By: jamesjwu
    
    Differential Revision: D36556566
    
    fbshipit-source-id: 77a449598704801d40f5f7abaf55c871f336e2a7

--- hphp/test/slow/modules/reflection-2.php
+++ hphp/test/slow/modules/reflection-2.php
@@ -4,17 +4,21 @@
 
 <<Ok, Yes>>
 new module A {}
-new module B {}
 
 <<__EntryPoint>>
 function main() {
+  include "reflection-2.inc";
+
+  // Same file
   $m = new ReflectionModule('A');
   var_dump($m->getName());
   var_dump($m->getAttributes());
 
+  // Different file
   $m = new ReflectionModule('B');
   var_dump($m->getName());
   var_dump($m->getAttributes());
 
+  // Doesn't exist
   $m = new ReflectionModule('C');
 }

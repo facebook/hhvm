3bfa7dfb26b9c55fd50a3e083d75d2036155f20e
Hunter Goldstein <hgoldstein@fb.com>
2022-06-02 13:13:28 -0700

    Typecheck a bunch of Iterators
    Summary:
    The `Iterator` hierarchy is a big chunk of systemlib, let's get this well typed.
    
    There are a couple cases where the methods we *expect* to be part of a given interface are instead defined in children (according to HHIs). I've left task stubs where such happens.
    
    Reviewed By: edwinsmith
    
    Differential Revision: D36718489
    
    fbshipit-source-id: a42618abc1df84d5eaa67315ed227ca1db082826

--- hphp/system/php/lang/AsyncKeyedIterator.ns.php
+++ hphp/system/php/lang/AsyncKeyedIterator.ns.php
@@ -2,6 +2,7 @@
 
 namespace HH {
 
-interface AsyncKeyedIterator extends \HH\AsyncIterator {}
-
+interface AsyncKeyedIterator<+Tk, +Tv> extends AsyncIterator<Tv> {}
+  // TODO(T121800572) We should add in the methods defined in the version of
+  // this class in `hphp/hack/hhi/interfaces.hhi`
 }
--- hphp/system/php/lang/KeyedIterator.ns.php
+++ hphp/system/php/lang/KeyedIterator.ns.php
@@ -2,7 +2,9 @@
 
 namespace HH {
 
-interface KeyedIterator extends \HH\Iterator, \HH\KeyedTraversable {}
-
+interface KeyedIterator<+Tk, +Tv> extends Iterator<Tv>, KeyedTraversable<Tk, Tv> {
+  // TODO(T121800572) We should add in the methods defined in the version of
+  // this class in `hphp/hack/hhi/interfaces.hhi`
 }
 
+}

26617c88ca35c5e078c3aef12c061d7996925375
Edwin Smith <smith@fb.com>
2022-06-10 07:21:49 -0700

    driveby: fix arc rust-check lint
    Summary:
    private struct TestContext hackrs_test.rs is only used by tests in submodules,
    so mark it as `#![cfg(test)]` so arc rust-check doesn't (correctly) complain about
    dead code.
    
    While at it, fix a nit in names::DeclSummary.
    
    Differential Revision: D37003990
    
    fbshipit-source-id: b5c71c07324395bbef52e2310ba69f399d4a11e4

--- hphp/hack/src/rupro/hackrs_test/hackrs_test.rs
+++ hphp/hack/src/rupro/hackrs_test/hackrs_test.rs
@@ -2,6 +2,7 @@
 //
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
+#![cfg(test)]
 
 use anyhow::Result;
 use datastore::NonEvictingStore;

9b0dec14aacee4be7cf71b9ddf1701aad02d90ca
Millie Chen <milliechen@fb.com>
2022-06-20 17:49:48 -0700

    Minor code cleanup
    Summary: Re-reading this file I find some parts slightly difficult to reason about. Adding some style fixes and comments to clean up the logic a little bit.
    
    Reviewed By: bobrenjc93
    
    Differential Revision: D37292673
    
    fbshipit-source-id: 31e882bad4c8803b83e72db06f91448bc10a1a5b

--- hphp/hack/src/server/serverGenRemoteDecls.ml
+++ hphp/hack/src/server/serverGenRemoteDecls.ml
@@ -22,19 +22,12 @@ let ( >>= ) res f =
       | Ok r -> f r
       | Error e -> Future.Promise.return (Error e))
 
-let ( >|= ) = Future.Promise.bind
-
 let return_ok x = Future.Promise.return (Ok x)
 
 let return_err x = Future.Promise.return (Error x)
 
-let get_name_and_decl_hashes_from_decls decls : (string * Int64.t) list =
-  List.filter_map decls ~f:(fun (name, decl, decl_hash) ->
-      match decl with
-      | Shallow_decl_defs.Class _ -> Some (name, decl_hash)
-      | _ -> None)
-
-let get_hh_version ~(repo : Path.t) : (string, string) result Future.Promise.t =
+let get_hh_config_version ~(repo : Path.t) :
+    (string, string) result Future.Promise.t =
   let hhconfig_path =
     Path.to_string
       (Path.concat repo Config_file.file_path_relative_to_repo_root)
@@ -56,7 +49,7 @@ let get_hh_version ~(repo : Path.t) : (string, string) result Future.Promise.t =
       let version = "v" ^ String_utils.lstrip version "^" in
       return_ok version
   end
-  >>= fun hh_version -> return_ok hh_version
+  >>= fun hh_config_version -> return_ok hh_config_version
 
 let go
     (env : ServerEnv.env)
@@ -64,15 +57,15 @@ let go
     (workers : MultiWorker.worker list option) : unit =
   let ctx = Provider_utils.ctx_from_server_env env in
   let repo = Wwwroot.get None in
-  let hh_version =
-    match Future.get @@ get_hh_version ~repo with
+  let hh_config_version =
+    match Future.get @@ get_hh_config_version ~repo with
     | Ok (Ok result) -> result
     | Ok (Error e) -> failwith (Printf.sprintf "%s" e)
     | Error e -> failwith (Printf.sprintf "%s" (Future.error_to_string e))
   in
   let cmd =
     "manifold mkdirs hack_decl_prefetching/tree/prefetch/"
-    ^ hh_version
+    ^ hh_config_version
     ^ "/shallow_decls"
   in
   ignore (Sys.command cmd);
@@ -86,45 +79,36 @@ let go
 
   let job (acc : (string * string) list) (fnl : Relative_path.t list) :
       (string * string) list =
-    let acc =
-      List.fold_left
-        ~init:acc
-        ~f:(fun acc fn ->
-          Hh_logger.log
-            "Saving decls for prefetching: %s"
-            (Relative_path.suffix fn);
-          match Direct_decl_utils.direct_decl_parse ctx fn with
-          | None -> acc
-          | Some parsed_file ->
-            let decls = parsed_file.Direct_decl_parser.pfh_decls in
-            Direct_decl_utils.cache_decls ctx fn decls;
-            let names_and_decl_hashes =
-              get_name_and_decl_hashes_from_decls decls
-            in
-            List.fold_left
-              ~init:acc
-              ~f:(fun acc (name, decl_hash) ->
-                let shallow_decl_opt = Shallow_classes_provider.get ctx name in
-                if Option.is_some shallow_decl_opt then
-                  let shallow_decl = Option.value_exn shallow_decl_opt in
-                  let shallow_decls_in_file = SMap.empty in
-                  let shallow_decls_in_file =
-                    SMap.add name shallow_decl shallow_decls_in_file
-                  in
-                  List.rev_append
-                    acc
-                    [
-                      ( Int64.to_string decl_hash,
-                        Marshal.to_string shallow_decls_in_file [] );
-                    ]
-                else
-                  acc)
-              names_and_decl_hashes)
-        fnl
-    in
-    acc
+    List.fold_left
+      ~init:acc
+      ~f:(fun acc fn ->
+        Hh_logger.log
+          "Saving decls for prefetching: %s"
+          (Relative_path.suffix fn);
+        match Direct_decl_utils.direct_decl_parse ctx fn with
+        | None -> acc
+        | Some parsed_file ->
+          let class_decls =
+            List.filter_map
+              parsed_file.Direct_decl_parser.pfh_decls
+              ~f:(function
+                | (_, Shallow_decl_defs.Class _, _) as decl -> Some decl
+                | _ -> None)
+          in
+          let pf_hash_and_marshalled_declmap =
+            List.map class_decls ~f:(fun (name, decl, decl_hash) ->
+                let decl_hash_64 = Int64.to_string decl_hash in
+                let symbol_to_shallow_decl = SMap.singleton name decl in
+                let marshalled_symbol_to_shallow_decl =
+                  Marshal.to_string symbol_to_shallow_decl []
+                in
+                (decl_hash_64, marshalled_symbol_to_shallow_decl))
+          in
+          acc @ pf_hash_and_marshalled_declmap)
+      fnl
   in
 
+  (* results is a list of (decl_hash, marshalled { symbol: decl }) pairs *)
   let results =
     MultiWorker.call
       workers
@@ -134,6 +118,6 @@ let go
       ~next:get_next
   in
 
-  let _ = Remote_old_decls_ffi.put_decls hh_version results in
+  let _ = Remote_old_decls_ffi.put_decls hh_config_version results in
   Hh_logger.log "Processed %d decls" (List.length results);
   ()

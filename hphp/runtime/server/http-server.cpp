2d459ea9b8bfcb41b4c5cfb32155337d8e765793
Bin Liu <binliu@fb.com>
2022-06-23 11:23:50 -0700

    add stat counter to indicate whether server is about to stop
    Differential Revision: D37305475
    
    fbshipit-source-id: efe451627e004a1a12fd6e381dd2c81cc7180572

--- hphp/runtime/server/http-server.cpp
+++ hphp/runtime/server/http-server.cpp
@@ -194,13 +194,12 @@ HttpServer::HttpServer() {
       counters["queued_requests"] = queued_requests;
       counters["queued_requests_high"] =
         queued_requests > RuntimeOption::ServerHighQueueingThreshold;
-
-
       auto const sat_requests = getSatelliteRequestCount();
       counters["satellite_inflight_requests"] = sat_requests.first;
       counters["satellite_queued_requests"] = sat_requests.second;
       auto const uptime = f_server_uptime();
       counters["uptime"] = uptime;
+      counters["stopping_soon"] = f_server_is_prepared_to_stop();
 
       // Temporary counter that is available only during a short uptime window.
       if (uptime > RO::EvalMemTrackStart && uptime < RO::EvalMemTrackEnd) {

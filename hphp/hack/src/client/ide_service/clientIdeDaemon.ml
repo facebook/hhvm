951251ec566646ba3b778f7bed889d5e4bc6273e
Hunter Goldstein <hgoldstein@fb.com>
2022-06-23 09:42:08 -0700

    Add more detail to `Outfd_write_error `
    Reviewed By: nt591
    
    Differential Revision: D37223592
    
    fbshipit-source-id: f37413e2197a353e276d69897cf65737f9e85160

--- hphp/hack/src/client/ide_service/clientIdeDaemon.ml
+++ hphp/hack/src/client/ide_service/clientIdeDaemon.ml
@@ -29,11 +29,11 @@ type message =
 
 type message_queue = message Lwt_message_queue.t
 
-exception Outfd_write_error
+exception Outfd_write_error of string * string
 
 let is_outfd_write_error (exn : Exception.t) : bool =
   match Exception.unwrap exn with
-  | Outfd_write_error -> true
+  | Outfd_write_error _ -> true
   | _ -> false
 
 (** istate, "initialized state", is the state the daemon after it has
@@ -221,7 +221,8 @@ let write_message
     let%lwt (_ : int) = Marshal_tools_lwt.to_fd_with_preamble out_fd message in
     Lwt.return_unit
   with
-  | Unix.Unix_error (Unix.EPIPE, _, _) -> raise Outfd_write_error
+  | Unix.Unix_error (Unix.EPIPE, fn, param) ->
+    raise @@ Outfd_write_error (fn, param)
 
 let load_saved_state
     (ctx : Provider_context.t)

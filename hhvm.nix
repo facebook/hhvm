c29961c9b6ead52020138cbbfc7e7846e81f6329
Yang, Bo <atry@fb.com>
2022-07-06 12:54:26 -0700

    Use system default stdenv

--- hhvm.nix
+++ hhvm.nix
@@ -13,7 +13,6 @@
 , fmt
 , freetype
 , fribidi
-, gcc10Stdenv
 , gd
 , gdb
 , gettext
@@ -70,12 +69,7 @@
 , zstd
 }:
 let
-  # Use gcc10 in Linux because fbthrift is not compatible with gcc 11+.
-  # For macOS, use the the default compiler, which should be clang.
-  hhvmStdenv =
-    if hostPlatform.isLinux
-    then gcc10Stdenv
-    else stdenv;
+  hhvmStdenv = stdenv;
   versionParts =
     builtins.match
       ''

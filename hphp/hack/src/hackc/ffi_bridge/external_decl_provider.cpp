e84e3b947f186caf52cd0eb7ecbb1cb164c28985
Edwin Smith <smith@fb.com>
2022-07-15 08:49:14 -0700

    remove debugging cruft in provide_type_or_alias()
    Summary: This was leftover from debugging the pointer punning bug fixed by D37754247 (https://github.com/facebook/hhvm/commit/49cc341bfad70ef2b98e6dcccbd1a3983db5a7c9).
    
    Reviewed By: aorenste
    
    Differential Revision: D37882710
    
    fbshipit-source-id: d1f94139aab233c61a0fc9a5598d349580102257

--- hphp/hack/src/hackc/ffi_bridge/external_decl_provider.cpp
+++ hphp/hack/src/hackc/ffi_bridge/external_decl_provider.cpp
@@ -6,17 +6,13 @@
 
 #include "hphp/hack/src/hackc/ffi_bridge/decl_provider.h"
 
-static bool recurse;
-
 extern "C" {
 // Called from hackc/decl_provider/external.rs
-__attribute__((noinline)) ExternalDeclProviderResult provide_type_or_alias(
+ExternalDeclProviderResult provide_type_or_alias(
     const void* provider,
     const char* symbol,
     size_t symbol_len,
     uint64_t depth) noexcept {
-  if (recurse)
-    provide_type_or_alias(provider, symbol, symbol_len, depth);
   return ((DeclProvider*)provider)
       ->getType(std::string_view(symbol, symbol_len), depth);
 }

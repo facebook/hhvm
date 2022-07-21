8f8e87cdee1ed38a91bc6402334059a0522b40a8
Selina Kim <selinakim@fb.com>
2022-07-21 16:35:45 -0700

    Call functions more easily
    Summary: Added returns, function call arguments, and ids to Walker. This enables us to analyze function calls with arguments more easily. In the future, we can write tests for interprocedural analysis more easily as well.
    
    Differential Revision: D38023769
    
    fbshipit-source-id: e1448c457cf726c138e830f727e80ee303b89d55

---
+++ hphp/hack/test/refactor_sd/simplify_constraints/call_arguments2.php
@@ -0,0 +1,25 @@
+<?hh
+
+<<__SupportDynamicType>>
+function f(): int {
+  return 1;
+}
+
+<<__SupportDynamicType>>
+function g(int $i): void {}
+
+function h(): void {
+  $x = (g<> upcast dynamic);
+  $y = f<>;
+  $x($y());
+}
+
+function e(): void {
+  $x = (g<> upcast dynamic);
+  $y = (f<> upcast dynamic);
+  $x($y());
+}
+
+function d(): void {
+  (g<> upcast dynamic)((f<> upcast dynamic)());
+}

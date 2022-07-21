d8ae4416233696c89430fc64f84277c15b6952f6
Michael Roberts <mbr@fb.com>
2022-07-21 14:08:56 -0700

    Split get_def_setup
    Summary: get_def_setup sets up a new context and entry, then an ast and a get_def. But serverPrepareCallHierarchy really only needs the second half done. This refactor is to allow that.
    
    Reviewed By: zhyty
    
    Differential Revision: D38047728
    
    fbshipit-source-id: 163e829e3bfe82f5760590a9c5643db931acd54e

--- hphp/hack/src/server/serverDepsUtil.ml
+++ hphp/hack/src/server/serverDepsUtil.ml
@@ -45,15 +45,17 @@ let body_symbols
       in
       List.filter declarations ~f:pos_filter)
 
+let get_ast_getdef (ctx : Provider_context.t) (entry : Provider_context.entry) =
+  let ast =
+    Ast_provider.compute_ast ~popt:(Provider_context.get_popt ctx) ~entry
+  in
+  let get_def = ServerSymbolDefinition.go ctx (Some ast) in
+  (ast, get_def)
+
 let get_def_setup (acc_ctx_in : Provider_context.t) (file : string) =
   let path = Relative_path.create_detect_prefix file in
   let (acc_ctx_out, entry) =
     Provider_context.add_entry_if_missing ~ctx:acc_ctx_in ~path
   in
-  let ast =
-    Ast_provider.compute_ast
-      ~popt:(Provider_context.get_popt acc_ctx_out)
-      ~entry
-  in
-  let get_def = ServerSymbolDefinition.go acc_ctx_out (Some ast) in
+  let (ast, get_def) = get_ast_getdef acc_ctx_out entry in
   (acc_ctx_out, entry, ast, get_def)

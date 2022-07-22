9a17bc8e5a5f1238a23f5fe3c0f0a6a04c65e54b
Alexey Toptygin <alexeyt@fb.com>
2022-07-22 14:28:33 -0700

    Delete dead String::rval
    Reviewed By: edwinsmith
    
    Differential Revision: D38069052
    
    fbshipit-source-id: 47345f44f14a17b082b3f3cb556bffc924fc2fdb

--- hphp/runtime/base/type-string.h
+++ hphp/runtime/base/type-string.h
@@ -379,28 +379,6 @@ public:
   int compare(const String& v2) const;
 
   /**
-   * Offset
-   */
-  String rval(bool    key) const { return rvalImpl(key ? 1 : 0);}
-  String rval(char    key) const { return rvalImpl(key);}
-  String rval(short   key) const { return rvalImpl(key);}
-  String rval(int     key) const { return rvalImpl(key);}
-  String rval(int64_t key) const { return rvalImpl(key);}
-  String rval(double  key) const { return rvalImpl((int64_t)key);}
-  String rval(const char* key) const {
-    return rvalImpl(String(key).toInt32());
-  }
-  String rval(const StringData *key) const {
-    not_reached();
-    return rvalImpl(key ? key->toInt32() : 0);
-  }
-  String rval(const String& key) const { return rvalImpl(key.toInt32());}
-
-  String rval(const Array& key) const = delete;
-  String rval(const Object& key) const = delete;
-  String rval(const Variant& key) const = delete;
-
-  /**
    * Returns one character at specified position.
    */
   char charAt(int pos) const;
@@ -440,13 +418,6 @@ public:
   }
 
  private:
-  String rvalImpl(int key) const {
-    if (m_str) {
-      return String{m_str->getChar(key)};
-    }
-    return String();
-  }
-
   static void compileTimeAssertions() {
     static_assert(sizeof(String) == sizeof(req::ptr<StringData>), "");
   }

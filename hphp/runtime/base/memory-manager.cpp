2f6086c559bd3e70211482924c5980e1b4a06601
Alexey Toptygin <alexeyt@fb.com>
2022-07-22 01:37:52 -0700

    Don't uselessly pass slab pointer to splitTail/storeTail
    Summary: Helper functions splitTail and storeTail in memory-manager.cpp both take a Slab* but do nothing at all with it. Remove the useless args and stop needlessly computing the Slab* just to pass it in to these helpers.
    
    Reviewed By: ricklavoie
    
    Differential Revision: D38042512
    
    fbshipit-source-id: 55b0c33a9467d92f46df2a82d0550e86f5bf49d8

--- hphp/runtime/base/memory-manager.cpp
+++ hphp/runtime/base/memory-manager.cpp
@@ -660,8 +660,7 @@ alignas(64) const uint8_t kContigIndexTab[] = {
  * Store slab tail bytes (if any) in freelists.
  */
 inline
-void storeTail(FreelistArray& freelists, void* tail, size_t tailBytes,
-               Slab* slab) {
+void storeTail(FreelistArray& freelists, void* tail, size_t tailBytes) {
   void* rem = tail;
   for (auto remBytes = tailBytes; remBytes > 0;) {
     auto fragBytes = remBytes;
@@ -686,8 +685,7 @@ void storeTail(FreelistArray& freelists, void* tail, size_t tailBytes,
  */
 inline
 void splitTail(FreelistArray& freelists, void* tail, size_t tailBytes,
-               size_t split_bytes, size_t splitUsable, size_t index,
-               Slab* slab) {
+               size_t split_bytes, size_t splitUsable, size_t index) {
   assertx(tailBytes >= kSmallSizeAlign);
   assertx((tailBytes & kSmallSizeAlignMask) == 0);
   assertx((splitUsable & kSmallSizeAlignMask) == 0);
@@ -708,14 +706,14 @@ void splitTail(FreelistArray& freelists, void* tail, size_t tailBytes,
 
   auto remBytes = tailBytes - split_bytes;
   assertx(uintptr_t(rem) + remBytes == uintptr_t(tail) + tailBytes);
-  storeTail(freelists, rem, remBytes, slab);
+  storeTail(freelists, rem, remBytes);
 }
 }
 
 void MemoryManager::freeOveralloc(void* base, size_t bytes) {
   if (!bytes) return;
   m_stats.mm_udebt += bytes;
-  storeTail(m_freelists, base, bytes, Slab::fromPtr(base));
+  storeTail(m_freelists, base, bytes);
 }
 
 /*
@@ -725,8 +723,7 @@ void MemoryManager::freeOveralloc(void* base, size_t bytes) {
 NEVER_INLINE void* MemoryManager::newSlab(size_t nbytes) {
   refreshStats();
   if (m_front < m_limit) {
-    storeTail(m_freelists, m_front, (char*)m_limit - (char*)m_front,
-              Slab::fromPtr(m_front));
+    storeTail(m_freelists, m_front, (char*)m_limit - (char*)m_front);
   }
   auto mem = m_heap.allocSlab(m_stats);
   always_assert(reinterpret_cast<uintptr_t>(mem) % kSlabAlign == 0);
@@ -753,10 +750,8 @@ inline void* MemoryManager::slabAlloc(size_t nbytes, size_t index) {
 
   auto ptr = m_front;
   auto next = (void*)(uintptr_t(ptr) + nbytes);
-  Slab* slab;
   if (uintptr_t(next) <= uintptr_t(m_limit)) {
     m_front = next;
-    slab = Slab::fromPtr(ptr);
   } else {
     if (UNLIKELY(index >= kNumSmallSizes) || UNLIKELY(m_bypassSlabAlloc)) {
       // Stats correction; mallocBigSize() updates m_stats. Add to mm_udebt
@@ -766,7 +761,6 @@ inline void* MemoryManager::slabAlloc(size_t nbytes, size_t index) {
       return mallocBigSize(nbytes);
     }
     ptr = newSlab(nbytes); // sets start bit at ptr
-    slab = Slab::fromPtr(ptr);
   }
   // Preallocate more of the same in order to amortize entry into this method.
   auto split_bytes = kContigTab[index] - nbytes;
@@ -777,7 +771,7 @@ inline void* MemoryManager::slabAlloc(size_t nbytes, size_t index) {
   if (split_bytes > 0) {
     auto tail = m_front;
     m_front = (void*)(uintptr_t(tail) + split_bytes);
-    splitTail(m_freelists, tail, split_bytes, split_bytes, nbytes, index, slab);
+    splitTail(m_freelists, tail, split_bytes, split_bytes, nbytes, index);
   }
   FTRACE(4, "slabAlloc({}, {}) --> ptr={}, m_front={}, m_limit={}\n", nbytes,
             index, ptr, m_front, m_limit);
@@ -813,7 +807,7 @@ void* MemoryManager::mallocSmallSizeSlow(size_t nbytes, size_t index) {
                 sizeIndex2Size(i), p);
       // Split tail into preallocations and store them back into freelists.
       splitTail(m_freelists, (char*)p + nbytes, sizeIndex2Size(i) - nbytes,
-                kContigTab[index] - nbytes, nbytes, index, Slab::fromPtr(p));
+                kContigTab[index] - nbytes, nbytes, index);
       return p;
     }
   }

a5028c85da5fc8cd00e014d4f07d6f31709395f1
Paul Saab <ps@fb.com>
2022-07-27 11:17:19 -0700

    Provide aarch64 definitions for HASHTBL_MEM_ADDR & HASHTBL_MEM_SIZE
    Summary: Provide aarch64 definitions for HASHTBL_MEM_ADDR & HASHTBL_MEM_SIZE
    
    Differential Revision: D38209372
    
    fbshipit-source-id: 02330b28b71b6395b9373c3db3e3a77e51b955a7

--- hphp/hack/src/heap/hh_shared.c
+++ hphp/hack/src/heap/hh_shared.c
@@ -280,9 +280,11 @@ typedef enum {
 #  define SHARED_MEM_INIT ((char *) 0x48047e00000ll)
 #elif defined __aarch64__
 /* CentOS 7.3.1611 kernel does not support a full 48-bit VA space, so choose a
- * value low enough that the 21 GB's mmapped in do not interfere with anything
+ * value low enough that the 100 GB's mmapped in do not interfere with anything
  * growing down from the top. 1 << 36 works. */
 #  define SHARED_MEM_INIT ((char *) 0x1000000000ll)
+#  define SHARDED_HASHTBL_MEM_ADDR ((char *) 0x2000000000ll)
+#  define SHARDED_HASHTBL_MEM_SIZE ((size_t)100 * 1024 * 1024 * 1024)
 #else
 #  define SHARED_MEM_INIT ((char *) 0x500000000000ll)
 #  define SHARDED_HASHTBL_MEM_ADDR ((char *) 0x510000000000ll)

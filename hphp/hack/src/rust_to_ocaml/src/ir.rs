b6c1d21fc9c344edf3fae69f1af241f2c2813cbf
Jake Bailey <jakebailey@fb.com>
2022-08-05 10:14:56 -0700

    Move IR Display impl into submodule
    Summary: Move all these Display impls into a separate file.
    
    Reviewed By: edwinsmith
    
    Differential Revision: D38442263
    
    fbshipit-source-id: 2a2e9649a452d7b60f2d41c5e9f70b5ced7e37a8

--- hphp/hack/src/rust_to_ocaml/src/ir.rs
+++ hphp/hack/src/rust_to_ocaml/src/ir.rs
@@ -3,57 +3,13 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
 
-use convert_case::Case;
-use convert_case::Casing;
+mod display;
+
 use derive_more::Display;
 use hash::IndexMap;
 
 pub struct File {
     pub defs: IndexMap<TypeName, Def>,
-}
-
-impl std::fmt::Display for File {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for (name, def) in self.defs.iter() {
-            match def {
-                Def::Alias { doc, tparams, ty } => {
-                    write_toplevel_doc_comment(f, doc)?;
-                    write!(f, "type ")?;
-                    write_type_parameters(f, tparams)?;
-                    writeln!(f, "{name} = {ty}")?
-                }
-                Def::Record {
-                    doc,
-                    tparams,
-                    fields,
-                } => {
-                    write_toplevel_doc_comment(f, doc)?;
-                    write!(f, "type ")?;
-                    write_type_parameters(f, tparams)?;
-                    writeln!(f, "{name} = {{")?;
-                    for field in fields {
-                        writeln!(f, "  {field}")?;
-                    }
-                    writeln!(f, "}}")?;
-                }
-                Def::Variant {
-                    doc,
-                    tparams,
-                    variants,
-                } => {
-                    write_toplevel_doc_comment(f, doc)?;
-                    write!(f, "type ")?;
-                    write_type_parameters(f, tparams)?;
-                    writeln!(f, "{name} =")?;
-                    for variant in variants {
-                        writeln!(f, "  | {variant}")?;
-                    }
-                }
-            }
-            write!(f, "\n")?;
-        }
-        Ok(())
-    }
 }
 
 pub enum Def {
@@ -80,44 +36,9 @@ pub struct Variant {
     pub doc: Vec<String>,
 }
 
-impl std::fmt::Display for Variant {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let Self { name, fields, doc } = self;
-        write!(f, "{name}")?;
-        if let Some(fields) = fields {
-            write!(f, " of {fields}")?;
-        }
-        write_field_or_variant_doc_comment(f, doc)?;
-        Ok(())
-    }
-}
-
 pub enum VariantFields {
     Unnamed(Vec<Type>),
     Named(Vec<Field>),
-}
-
-impl std::fmt::Display for VariantFields {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Unnamed(fields) => {
-                let mut iter = fields.iter();
-                let ty = iter.next().expect("empty VariantFields::Unnamed");
-                ty.fmt(f)?;
-                for ty in iter {
-                    write!(f, " * {ty}")?;
-                }
-                Ok(())
-            }
-            Self::Named(fields) => {
-                writeln!(f, "{{")?;
-                for field in fields {
-                    writeln!(f, "    {field}")?;
-                }
-                write!(f, "}}")
-            }
-        }
-    }
 }
 
 pub struct Field {
@@ -126,27 +47,9 @@ pub struct Field {
     pub doc: Vec<String>,
 }
 
-impl std::fmt::Display for Field {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let Self { name, ty, doc } = self;
-        write!(f, "{name}: {ty};")?;
-        write_field_or_variant_doc_comment(f, doc)?;
-        Ok(())
-    }
-}
-
 pub enum Type {
     Path(TypePath),
     Tuple(TypeTuple),
-}
-
-impl std::fmt::Display for Type {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Type::Path(ty) => ty.fmt(f),
-            Type::Tuple(ty) => ty.fmt(f),
-        }
-    }
 }
 
 pub struct TypePath {
@@ -165,41 +68,8 @@ impl TypePath {
     }
 }
 
-impl std::fmt::Display for TypePath {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self.targs.as_slice() {
-            [] => {}
-            [targ] => write!(f, "{} ", targ)?,
-            [first, rest @ ..] => {
-                write!(f, "({}", first)?;
-                for targ in rest {
-                    write!(f, ", {}", targ)?;
-                }
-                write!(f, ") ")?;
-            }
-        }
-        for module in self.modules.iter() {
-            write!(f, "{}.", module.to_case(Case::UpperCamel))?;
-        }
-        write!(f, "{}", self.ty.to_case(Case::Snake))
-    }
-}
-
 pub struct TypeTuple {
     pub elems: Vec<Type>,
-}
-
-impl std::fmt::Display for TypeTuple {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "(")?;
-        let mut elems = self.elems.iter();
-        let elem = elems.next().expect("empty TypeTuple");
-        write!(f, "{elem}")?;
-        for elem in elems {
-            write!(f, " * {elem}")?;
-        }
-        write!(f, ")")
-    }
 }
 
 #[derive(Clone, Hash, PartialEq, Eq, Display)]
@@ -210,45 +80,3 @@ pub struct FieldName(pub String);
 
 #[derive(Clone, Hash, PartialEq, Eq, Display)]
 pub struct VariantName(pub String);
-
-fn write_toplevel_doc_comment(
-    f: &mut std::fmt::Formatter<'_>,
-    doc: &Vec<String>,
-) -> std::fmt::Result {
-    if doc.is_empty() {
-        return Ok(());
-    }
-    write!(f, "(**{}", doc.join("\n *"))?;
-    if doc.len() == 1 {
-        if !doc[0].contains('\n') {
-            write!(f, " ")?;
-        }
-    } else {
-        write!(f, "\n ")?;
-    }
-    write!(f, "*)\n")?;
-    Ok(())
-}
-
-fn write_field_or_variant_doc_comment(
-    f: &mut std::fmt::Formatter<'_>,
-    doc: &Vec<String>,
-) -> std::fmt::Result {
-    if doc.is_empty() {
-        return Ok(());
-    }
-    let joined = doc.join("\n       *");
-    write!(f, "(**{}", joined)?;
-    if !joined.ends_with(' ') {
-        write!(f, " ")?;
-    }
-    write!(f, "*)\n")?;
-    Ok(())
-}
-
-fn write_type_parameters(f: &mut std::fmt::Formatter<'_>, tparams: &[String]) -> std::fmt::Result {
-    for tparam in tparams.iter() {
-        write!(f, "'{} ", tparam.to_case(Case::Snake))?;
-    }
-    Ok(())
-}
---
+++ hphp/hack/src/rust_to_ocaml/src/ir/display.rs
@@ -0,0 +1,183 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the "hack" directory of this source tree.
+
+use crate::ir;
+use convert_case::Case;
+use convert_case::Casing;
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::fmt::Result;
+
+impl Display for ir::File {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        for (name, def) in self.defs.iter() {
+            match def {
+                ir::Def::Alias { doc, tparams, ty } => {
+                    write_toplevel_doc_comment(f, doc)?;
+                    write!(f, "type ")?;
+                    write_type_parameters(f, tparams)?;
+                    writeln!(f, "{name} = {ty}")?
+                }
+                ir::Def::Record {
+                    doc,
+                    tparams,
+                    fields,
+                } => {
+                    write_toplevel_doc_comment(f, doc)?;
+                    write!(f, "type ")?;
+                    write_type_parameters(f, tparams)?;
+                    writeln!(f, "{name} = {{")?;
+                    for field in fields {
+                        writeln!(f, "  {field}")?;
+                    }
+                    writeln!(f, "}}")?;
+                }
+                ir::Def::Variant {
+                    doc,
+                    tparams,
+                    variants,
+                } => {
+                    write_toplevel_doc_comment(f, doc)?;
+                    write!(f, "type ")?;
+                    write_type_parameters(f, tparams)?;
+                    writeln!(f, "{name} =")?;
+                    for variant in variants {
+                        writeln!(f, "  | {variant}")?;
+                    }
+                }
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
+
+impl Display for ir::Variant {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        let Self { name, fields, doc } = self;
+        write!(f, "{name}")?;
+        if let Some(fields) = fields {
+            write!(f, " of {fields}")?;
+        }
+        write_field_or_variant_doc_comment(f, doc)?;
+        Ok(())
+    }
+}
+
+impl Display for ir::VariantFields {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            Self::Unnamed(fields) => {
+                let mut iter = fields.iter();
+                let ty = iter.next().expect("empty VariantFields::Unnamed");
+                ty.fmt(f)?;
+                for ty in iter {
+                    write!(f, " * {ty}")?;
+                }
+                Ok(())
+            }
+            Self::Named(fields) => {
+                writeln!(f, "{{")?;
+                for field in fields {
+                    writeln!(f, "    {field}")?;
+                }
+                write!(f, "}}")
+            }
+        }
+    }
+}
+
+impl Display for ir::Field {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        let Self { name, ty, doc } = self;
+        write!(f, "{name}: {ty};")?;
+        write_field_or_variant_doc_comment(f, doc)?;
+        Ok(())
+    }
+}
+
+impl Display for ir::Type {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            Self::Path(ty) => ty.fmt(f),
+            Self::Tuple(ty) => ty.fmt(f),
+        }
+    }
+}
+
+impl Display for ir::TypePath {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self.targs.as_slice() {
+            [] => {}
+            [targ] => write!(f, "{} ", targ)?,
+            [first, rest @ ..] => {
+                write!(f, "({}", first)?;
+                for targ in rest {
+                    write!(f, ", {}", targ)?;
+                }
+                write!(f, ") ")?;
+            }
+        }
+        for module in self.modules.iter() {
+            write!(f, "{}.", module.to_case(Case::UpperCamel))?;
+        }
+        write!(f, "{}", self.ty.to_case(Case::Snake))
+    }
+}
+
+impl Display for ir::TypeTuple {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "(")?;
+        let mut elems = self.elems.iter();
+        let elem = elems.next().expect("empty TypeTuple");
+        write!(f, "{elem}")?;
+        for elem in elems {
+            write!(f, " * {elem}")?;
+        }
+        write!(f, ")")
+    }
+}
+
+fn write_toplevel_doc_comment(
+    f: &mut std::fmt::Formatter<'_>,
+    doc: &Vec<String>,
+) -> std::fmt::Result {
+    if doc.is_empty() {
+        return Ok(());
+    }
+    write!(f, "(**{}", doc.join("\n *"))?;
+    if doc.len() == 1 {
+        if !doc[0].contains('\n') {
+            write!(f, " ")?;
+        }
+    } else {
+        write!(f, "\n ")?;
+    }
+    writeln!(f, "*)")?;
+    Ok(())
+}
+
+fn write_field_or_variant_doc_comment(
+    f: &mut std::fmt::Formatter<'_>,
+    doc: &Vec<String>,
+) -> std::fmt::Result {
+    if doc.is_empty() {
+        return Ok(());
+    }
+    let joined = doc.join("\n       *");
+    write!(f, "(**{}", joined)?;
+    if !joined.ends_with(' ') {
+        write!(f, " ")?;
+    }
+    writeln!(f, "*)")?;
+    Ok(())
+}
+
+fn write_type_parameters(f: &mut std::fmt::Formatter<'_>, tparams: &[String]) -> std::fmt::Result {
+    for tparam in tparams.iter() {
+        write!(f, "'{} ", tparam.to_case(Case::Snake))?;
+    }
+    Ok(())
+}

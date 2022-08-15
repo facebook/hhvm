d31e57454e182a538a001a9ebd1e83592897850f
Edwin Smith <smith@fb.com>
2022-08-15 09:45:47 -0700

    Support for decl-directed bytecode in whole-program builds
    Summary:
    With Eval.EnableDecl==true, hackc may request type, func, constant, or module
    decls from other files, while it is compiling bytecode for a given file. Whole program
    builds are now distributed, and hphpc compiles source code to bytecode remotely
    in groups of ~500 files at a time.
    
    To make this work, bytecode compilation (aka "parsing") is now iterative, by making
    use of a new BatchDeclProvider which accumulates a list of requested decls it did
    not have. This list of missing decls drives the iteration in steps:
    1. initially, each source file's list of decls is empty.
    2. after compiling, if any missing decls turn out to be available in the global UnitIndex,
    add them to the source file's list and try to compile that group of files again.
    3. Stop iterating when no additional decls are discovered. This doesn't mean the
    missing symbol list was empty, it just means those symbols are permanently missing.
    
    In practice, hackc currently only requests decls it sees in the original source file, so
    this loop terminates after two iterations. In the future, hackc may request transitive
    decls (e.g. fetch A, then A's parents, and so on), causing more iteration.
    
    As an optimization, if EnableDecls==false, IndexJob no longer stores any decls in
    external storage, since nothing will fetch them.
    
    Differential Revision: D37280035
    
    fbshipit-source-id: 67a4aac29c0fe2aa19eec87161755b30f1c8debb

--- hphp/compiler/package.h
+++ hphp/compiler/package.h
@@ -38,31 +38,7 @@
 namespace HPHP {
 ///////////////////////////////////////////////////////////////////////////////
 
-struct UnitIndex final {
-  using IMap = folly_concurrent_hash_map_simd<
-    const StringData*,
-    const StringData*,
-    string_data_hash,
-    string_data_isame
-  >;
-  using Map = folly_concurrent_hash_map_simd<
-    const StringData*,
-    const StringData*
-  >;
-
-  IMap types;
-  IMap funcs;
-  Map constants;
-
-  // TODO: add Map<filename, decls_ref>
-  // TODO: will need modules for DeclProvider but not for parseOnDemand.
-
-  void clear() {
-    types.clear();
-    funcs.clear();
-    constants.clear();
-  }
-};
+struct UnitIndex;
 
 struct Package {
   Package(const std::string& root,
@@ -96,8 +72,8 @@ struct Package {
       Config c;
       #define R(Opt) c.Opt = RO::Opt;
       UNITCACHEFLAGS()
-        #undef R
-        c.EvalAbortBuildOnCompilerError = RO::EvalAbortBuildOnCompilerError;
+      #undef R
+      c.EvalAbortBuildOnCompilerError = RO::EvalAbortBuildOnCompilerError;
       c.EvalAbortBuildOnVerifyError = RO::EvalAbortBuildOnVerifyError;
       c.IncludeRoots = RO::IncludeRoots;
       c.coeffects = CoeffectsConfig::exportForParse();
@@ -107,8 +83,8 @@ struct Package {
     void apply() const {
       #define R(Opt) RO::Opt = Opt;
       UNITCACHEFLAGS()
-        #undef R
-        RO::EvalAbortBuildOnCompilerError = EvalAbortBuildOnCompilerError;
+      #undef R
+      RO::EvalAbortBuildOnCompilerError = EvalAbortBuildOnCompilerError;
       RO::EvalAbortBuildOnVerifyError = EvalAbortBuildOnVerifyError;
       RO::IncludeRoots = IncludeRoots;
       CoeffectsConfig::importForParse(coeffects);
@@ -117,8 +93,8 @@ struct Package {
     template <typename SerDe> void serde(SerDe& sd) {
       #define R(Opt) sd(Opt);
       UNITCACHEFLAGS()
-        #undef R
-        sd(EvalAbortBuildOnCompilerError)
+      #undef R
+      sd(EvalAbortBuildOnCompilerError)
         (EvalAbortBuildOnVerifyError)
         (IncludeRoots)
         (coeffects);
@@ -152,21 +128,34 @@ struct Package {
     }
   };
 
-  // Index information collected during parsing, used to construct
-  // an autoload index for parse-on-demand.
-  struct IndexMeta {
+  struct DeclNames {
     std::vector<const StringData*> types;
     std::vector<const StringData*> funcs;
     std::vector<const StringData*> constants;
+    std::vector<const StringData*> modules;
 
+    bool empty() const {
+      return types.empty() && funcs.empty() && constants.empty() &&
+        modules.empty();
+    }
+
+    template <typename SerDe> void serde(SerDe& sd) {
+      sd(types)
+        (funcs)
+        (constants)
+        (modules);
+    }
+  };
+
+  // Index information collected during parsing, used to construct
+  // an autoload index for parse-on-demand.
+  struct IndexMeta: DeclNames {
     // If not empty, indexing resulted in an ICE or had parse errors.
     std::string error;
 
     template<typename SerDe> void serde(SerDe& sd) {
-      sd(types)
-        (funcs)
-        (constants)
-        (error);
+      DeclNames::serde(sd);
+      sd(error);
     }
   };
 
@@ -174,9 +163,13 @@ struct Package {
   // drive on-demand parsing as well as toplevel decl names (from UEs)
   // for use later by HHBBC's whole-program Index.
   struct ParseMeta {
-    // Symbols present in the unit. This will be used to find new files
-    // for parse on-demand.
+    // Unresolved symbols required by hackc before bytecode generation.
+    DeclNames m_missing;
+
+    // Unresolved symbols needed by the Unit at runtime but not before
+    // bytecode generation. Used to find new files for parse on-demand.
     SymbolRefs m_symbol_refs;
+
     // If not empty, parsing resulted in an ICE and configuration
     // indicated that this should be fatal.
     std::string m_abort;
@@ -205,14 +198,29 @@ struct Package {
     const StringData* m_filepath{nullptr};
 
     template <typename SerDe> void serde(SerDe& sd) {
-      sd(m_symbol_refs)
+      sd(m_missing)
+        (m_symbol_refs)
         (m_abort)
         (m_definitions)
         (m_filepath);
     }
   };
 
-  using IndexCallback = std::function<void(std::string&&, IndexMeta&&)>;
+  // Serialized decls for a single file, along with the IndexMeta
+  // enumerating the symbols defined in the file.
+  struct UnitDecls {
+    DeclNames symbols;
+    std::string decls;
+
+    template <typename SerDe> void serde(SerDe& sd) {
+      sd(symbols)
+        (decls);
+    }
+  };
+
+  using IndexCallback = std::function<
+    void(std::string&&, IndexMeta&&, extern_worker::Ref<UnitDecls>&&)
+  >;
   using IndexMetaVec = std::vector<IndexMeta>;
   coro::Task<bool> index(const IndexCallback&);
 
@@ -220,10 +228,11 @@ struct Package {
   using FileMetaVec = std::vector<FileMeta>;
   using ParseMetaVec = std::vector<ParseMeta>;
   using FileData = std::tuple<extern_worker::Ref<std::string>,
-                              extern_worker::Ref<RepoOptionsFlags>>;
+                              extern_worker::Ref<RepoOptionsFlags>,
+                              std::vector<extern_worker::Ref<UnitDecls>>>;
 
   using LocalCallback = std::function<coro::Task<void>(UEVec)>;
-  using ParseCallback = std::function<coro::Task<ParseMetaVec>(
+  using ParseCallback = std::function<coro::Task<std::pair<bool,ParseMetaVec>>(
     const extern_worker::Ref<Config>&,
     extern_worker::Ref<FileMetaVec>,
     std::vector<FileData>,
@@ -239,7 +248,8 @@ struct Package {
   static IndexMetaVec indexFini();
   static ParseMetaVec parseFini();
   static UnitEmitterSerdeWrapper parseRun(const std::string&,
-                                          const RepoOptionsFlags&);
+                                          const RepoOptionsFlags&,
+                                          const std::vector<UnitDecls>&);
 private:
 
   struct FileAndSize {
@@ -280,6 +290,8 @@ private:
   coro::Task<FileAndSizeVec> parseGroups(Groups, const UnitIndex&);
   coro::Task<FileAndSizeVec> parseGroup(Group, const UnitIndex&);
 
+  void resolveDecls(const UnitIndex&, const FileMetaVec&,
+      const std::vector<ParseMeta>&, std::vector<FileData>&, size_t attempts);
   void resolveOnDemand(FileAndSizeVec&, const SymbolRefs&, const UnitIndex&,
       bool report = false);
 
@@ -312,6 +324,53 @@ private:
 
   // Content-store for options: Map<hash(options), options>
   extern_worker::RefCache<SHA1, RepoOptionsFlags> m_repoOptions;
+};
+
+struct UnitIndex final {
+  struct Locations {
+    Locations(std::string&& rpath,
+              extern_worker::Ref<Package::UnitDecls>&& ref)
+      : rpath(std::move(rpath)), declsRef(std::move(ref))
+    {}
+
+    // Relative path to source file in local filesystem
+    std::string rpath;
+
+    // handle to serialized decls in extern blob store
+    extern_worker::Ref<Package::UnitDecls> declsRef;
+  };
+
+  using IMap = folly_concurrent_hash_map_simd<
+    const StringData*, std::shared_ptr<Locations>,
+    string_data_hash,
+    string_data_isame
+  >;
+  using Map = folly_concurrent_hash_map_simd<
+    const StringData*, std::shared_ptr<Locations>
+  >;
+
+  IMap types;
+  IMap funcs;
+  Map constants;
+  Map modules;
+
+  bool containsAnyMissing(const Package::ParseMetaVec& parseMetas) const {
+    auto any = [&](auto const& names, auto const& map) -> bool {
+      for (auto name : names) {
+        if (map.find(name) != map.end()) return true;
+      }
+      return false;
+    };
+    for (auto const& p : parseMetas) {
+      if (any(p.m_missing.types, types) ||
+          any(p.m_missing.funcs, funcs) ||
+          any(p.m_missing.constants, constants) ||
+          any(p.m_missing.modules, modules)) {
+        return true;
+      }
+    }
+    return false;
+  }
 };
 
 ///////////////////////////////////////////////////////////////////////////////

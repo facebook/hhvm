45ba2d1a9c1b7c3a3cd77944fb131b894cb0a7ab
Shayne Fletcher <shaynefletcher@fb.com>
2022-08-18 14:42:29 -0700

    initial commit
    Summary: setup for new crate "ocamlrep_marshal"
    
    Differential Revision: D38835789
    
    fbshipit-source-id: b5e7eecb5987f357120b8a910ff8ebee19018b67

---
+++ hphp/hack/src/ocamlrep_marshal/ocamlrep_marshal.rs
@@ -0,0 +1,8 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the "hack" directory of this source tree.
+
+mod r#extern; // c.f. https://github.com/ocaml/ocaml/blob/trunk/runtime/extern.c
+mod intern; // c.f. https://github.com/ocaml/ocaml/blob/trunk/runtime/intern.c
+mod intext; // c.f. https://github.com/ocaml/ocaml/blob/trunk/runtime/caml/intext.h

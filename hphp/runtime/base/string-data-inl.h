5feb43927b1b54d67c0ba1891feb85826da7a94d
Sally Zhou <sallyz@fb.com>
2022-08-19 09:46:17 -0700

    remove pointer comparison inside StringData::same
    Summary: Learned that `same` is used generally when strings have already been checked to not be static, so adding another static check will make this method slower. There shouldn't be any behaviour changes.
    
    Reviewed By: arnabde03
    
    Differential Revision: D38281824
    
    fbshipit-source-id: 020bddc830a0c8ba1028651e963d96f446a90b3d

--- hphp/runtime/base/string-data-inl.h
+++ hphp/runtime/base/string-data-inl.h
@@ -155,7 +155,6 @@ inline strhash_t StringData::hashStatic() const {
 
 inline bool StringData::same(const StringData* s) const {
   assertx(s);
-  if (isStatic() && s->isStatic()) return this == s;
   if (m_len != s->m_len) return false;
   // The underlying buffer and its length are 8-byte aligned, ensured by
   // StringData layout, req::malloc, or malloc. So compare words.

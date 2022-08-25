faa9b06d162e7bcc49e9cbbb448cba4386284034
Ami Oka <aoka@fb.com>
2022-08-25 10:55:10 -0700

    Add benchmark for ApplyPatchToSerializedData
    Summary:
    Added benchmark to test the efficiency of ApplyPatchToSerializedData comparing to original approach (deserialize the entire object, apply patch, and serialize again).
    There are 3 cases; 1st (`patch_few_small_fields`) has a large bool list field and patch doesn't contain it. 2nd (`patch_large_fields`) has a large bool list field and patch contains it. 3rd (`patch_all_small_fields`) has a lot of fields and patch works with all the fields. We can see that this new approach significantly improves the 1st case as it doesn't need to deserialize the list. 2nd case has little change. It doubled the runtime for the 3rd case as it has overhead of constructing mask for each field and checking the maskdata. `_without_partial_deser` is original approach and `with_partial_deser` is this new appraoch.
    Result:
    ```
    ============================================================================
    ApplyPatchToSerializedDataBenchmark.cpp         relative  time/iter   iters/s
    ============================================================================
    patch_few_small_fields_without_partial_deser               14.47ms     69.11
    patch_few_small_fields_with_partial_deser                 111.65us     8.96K
    patch_large_fields_without_partial_deser                   16.42ms     60.91
    patch_large_fields_with_partial_deser                      16.15ms     61.92
    patch_all_small_fields_without_partial_deser              117.62ms      8.50
    patch_all_small_fields_with_partial_deser                 279.99ms      3.57
    ```
    
    Reviewed By: Mizuchi
    
    Differential Revision: D38923408
    
    fbshipit-source-id: f38edcbf3d8a4f523e8d7637c898e55c029a9ad5

---
+++ third-party/thrift/src/thrift/lib/cpp2/protocol/ApplyPatchToSerializedDataBenchmark.cpp
@@ -0,0 +1,163 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <folly/Benchmark.h>
+#include <folly/init/Init.h>
+#include <thrift/lib/cpp2/protocol/CompactProtocol.h>
+#include <thrift/lib/cpp2/protocol/Object.h>
+#include <thrift/lib/cpp2/protocol/Patch.h>
+#include <thrift/lib/cpp2/protocol/Serializer.h>
+#include <thrift/lib/thrift/gen-cpp2/patch_types.h>
+
+using namespace folly;
+using namespace std;
+
+namespace apache::thrift::protocol {
+
+// Testcase 1 uses an object with a large bool list and patch doesn't contain
+// the field.
+Object getObject1(int n) {
+  Object obj;
+  std::vector<Value> boolList;
+  while (n--) {
+    boolList.push_back(asValueStruct<type::bool_t>(n % 2));
+  }
+  obj[FieldId{1}].listValue_ref() = boolList;
+  obj[FieldId{2}].i32Value_ref() = 5;
+  return obj;
+}
+
+Object getPatchObj1() {
+  Value intPatch;
+  intPatch.objectValue_ref()
+      .ensure()[FieldId{static_cast<int16_t>(op::PatchOp::Assign)}] =
+      asValueStruct<type::i32_t>(1);
+  Value objPatch;
+  objPatch.objectValue_ref().ensure()[FieldId{2}] = intPatch;
+  Object patchObj;
+  patchObj[FieldId{static_cast<int16_t>(op::PatchOp::Patch)}] = objPatch;
+  return patchObj;
+}
+
+// Testcase 2 uses an object with a large bool list and patch contains
+// the field.
+Object getObject2(int n) {
+  Object obj;
+  std::vector<Value> boolList;
+  while (n--) {
+    boolList.push_back(asValueStruct<type::bool_t>(n % 2));
+  }
+  obj[FieldId{1}].listValue_ref() = boolList;
+  obj[FieldId{2}].i32Value_ref() = 5;
+  return obj;
+}
+
+Object getPatchObj2() {
+  Value intPatch;
+  intPatch.objectValue_ref()
+      .ensure()[FieldId{static_cast<int16_t>(op::PatchOp::Assign)}] =
+      asValueStruct<type::i32_t>(1);
+  Value listPatch;
+  listPatch.objectValue_ref()
+      .ensure()[FieldId{static_cast<int16_t>(op::PatchOp::Put)}]
+      .listValue_ref() = std::vector<Value>{asValueStruct<type::bool_t>(true)};
+  Value objPatch;
+  objPatch.objectValue_ref().ensure()[FieldId{1}] = listPatch;
+  objPatch.objectValue_ref().ensure()[FieldId{2}] = intPatch;
+  Object patchObj;
+  patchObj[FieldId{static_cast<int16_t>(op::PatchOp::Patch)}] = objPatch;
+  return patchObj;
+}
+
+// Testcase 3 uses an object with many fields and patch contain all the fields.
+Object getObject3(int n) {
+  Object obj;
+  while (n--) {
+    obj[static_cast<FieldId>(n)].i32Value_ref() = n;
+  }
+  return obj;
+}
+
+Object getPatchObj3(int n) {
+  Value intPatch;
+  intPatch.objectValue_ref()
+      .ensure()[FieldId{static_cast<int16_t>(op::PatchOp::Add)}] =
+      asValueStruct<type::i32_t>(1);
+  Value objPatch;
+  while (n--) {
+    objPatch.objectValue_ref().ensure()[static_cast<FieldId>(n)] = intPatch;
+  }
+  Object patchObj;
+  patchObj[FieldId{static_cast<int16_t>(op::PatchOp::Patch)}] = objPatch;
+  return patchObj;
+}
+
+std::unique_ptr<folly::IOBuf> serialized1, serialized2, serialized3;
+Object patchObj1, patchObj2, patchObj3;
+
+void init(int n) {
+  serialized1 =
+      serializeObject<apache::thrift::CompactProtocolWriter>(getObject1(n));
+  patchObj1 = getPatchObj1();
+  serialized2 =
+      serializeObject<apache::thrift::CompactProtocolWriter>(getObject2(n));
+  patchObj2 = getPatchObj2();
+  serialized3 =
+      serializeObject<apache::thrift::CompactProtocolWriter>(getObject3(n));
+  patchObj3 = getPatchObj3(n);
+}
+
+void runOriginalApproach(auto& serialized, const Object& patchObj) {
+  Value value;
+  value.objectValue_ref() = parseObject<CompactProtocolReader>(*serialized);
+  protocol::applyPatch(patchObj, value);
+  protocol::serializeObject<CompactProtocolWriter>(value.as_object());
+}
+
+BENCHMARK(patch_few_small_fields_without_partial_deser) {
+  runOriginalApproach(serialized1, patchObj1);
+}
+
+BENCHMARK(patch_few_small_fields_with_partial_deser) {
+  applyPatchToSerializedData<type::StandardProtocol::Compact>(
+      patchObj1, *serialized1);
+}
+
+BENCHMARK(patch_large_fields_without_partial_deser) {
+  runOriginalApproach(serialized2, patchObj2);
+}
+
+BENCHMARK(patch_large_fields_with_partial_deser) {
+  applyPatchToSerializedData<type::StandardProtocol::Compact>(
+      patchObj2, *serialized2);
+}
+
+BENCHMARK(patch_all_small_fields_without_partial_deser) {
+  runOriginalApproach(serialized3, patchObj3);
+}
+
+BENCHMARK(patch_all_small_fields_with_partial_deser) {
+  applyPatchToSerializedData<type::StandardProtocol::Compact>(
+      patchObj3, *serialized3);
+}
+} // namespace apache::thrift::protocol
+
+int main(int argc, char** argv) {
+  folly::init(&argc, &argv);
+  apache::thrift::protocol::init(10000);
+  runBenchmarks();
+  return 0;
+}

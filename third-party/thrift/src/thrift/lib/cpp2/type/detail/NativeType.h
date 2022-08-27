3bc8e13dbf1f517abef96ba59197d164e7e922d7
Alfred Fuller <afuller@fb.com>
2022-08-26 18:24:49 -0700

    Minor clean up for NativeType details
    Summary: springcleaning
    
    Reviewed By: thedavekwon
    
    Differential Revision: D39042896
    
    fbshipit-source-id: 30492000eb7684de0c038796a411e2bb8d1a9cf2

--- third-party/thrift/src/thrift/lib/cpp2/type/detail/NativeType.h
+++ third-party/thrift/src/thrift/lib/cpp2/type/detail/NativeType.h
@@ -16,9 +16,11 @@
 
 #pragma once
 
+#include <cstdint>
 #include <map>
 #include <set>
 #include <string>
+#include <type_traits>
 #include <vector>
 
 #include <folly/Traits.h>
@@ -26,6 +28,7 @@
 #include <thrift/lib/cpp2/Adapt.h>
 #include <thrift/lib/cpp2/Thrift.h>
 #include <thrift/lib/cpp2/type/BaseType.h>
+#include <thrift/lib/cpp2/type/Tag.h>
 #include <thrift/lib/cpp2/type/ThriftType.h>
 
 namespace apache {
@@ -33,160 +36,161 @@ namespace thrift {
 namespace type {
 namespace detail {
 
-// All the native_types for the given tag.
+// All the NativeTypes for the given tag.
 template <typename Tag>
-struct native_types {
+struct NativeTypes {
   // No types to declare for non concrete types.
   static_assert(is_abstract_v<Tag>, "");
 };
+
+// Infers an appropriate type tag for given non-adapted native type.
+template <typename T, typename = void>
+struct InferTag;
 
 // Resolves the concrete template type when paramaterizing the given template,
 // with the standard types of the give Tags.
 template <template <typename...> class TemplateT, typename... Tags>
 using standard_template_t =
-    TemplateT<typename native_types<Tags>::standard_type...>;
+    TemplateT<typename NativeTypes<Tags>::standard_type...>;
 
 // Resolves the concrete template type when paramaterizing the given template,
 // with the native types of the give Tags.
 template <template <typename...> class TemplateT, typename... Tags>
-using native_template_t =
-    TemplateT<typename native_types<Tags>::native_type...>;
+using native_template_t = TemplateT<typename NativeTypes<Tags>::native_type...>;
 template <template <typename...> class TemplateT, typename... Tags>
 struct native_template {
   using type = native_template_t<TemplateT, Tags...>;
 };
 
-// The (mixin) native_types all concrete types (_t suffix) define.
+// The types all concrete types (_t suffix) define.
 //
 // All concrete types have an associated set of standard types and a native type
 // (which by default is just the first standard type).
 template <typename StandardType, typename NativeType = StandardType>
-struct concrete_type {
+struct ConcreteType {
   using standard_type = StandardType;
   using native_type = NativeType;
 };
-struct nonconcrete_type {};
+struct AbstractType {};
 
 template <template <typename...> class T, typename ValTag>
 using parameterized_type = folly::conditional_t<
     is_concrete_v<ValTag>,
-    concrete_type<standard_template_t<T, ValTag>, native_template_t<T, ValTag>>,
-    nonconcrete_type>;
+    ConcreteType<standard_template_t<T, ValTag>, native_template_t<T, ValTag>>,
+    AbstractType>;
 
 template <template <typename...> class T, typename KeyTag, typename ValTag>
 using parameterized_kv_type = folly::conditional_t<
     is_concrete_v<KeyTag> && is_concrete_v<ValTag>,
-    concrete_type<
+    ConcreteType<
         standard_template_t<T, KeyTag, ValTag>,
         native_template_t<T, KeyTag, ValTag>>,
-    nonconcrete_type>;
+    AbstractType>;
 
 template <>
-struct native_types<void_t> : concrete_type<void> {};
+struct NativeTypes<void_t> : ConcreteType<void> {};
 
 // The native types for all primitive types.
 template <>
-struct native_types<bool_t> : concrete_type<bool> {};
+struct NativeTypes<bool_t> : ConcreteType<bool> {};
 template <>
-struct native_types<byte_t> : concrete_type<int8_t> {};
+struct NativeTypes<byte_t> : ConcreteType<int8_t> {};
 template <>
-struct native_types<i16_t> : concrete_type<int16_t> {};
+struct NativeTypes<i16_t> : ConcreteType<int16_t> {};
 template <>
-struct native_types<i32_t> : concrete_type<int32_t> {};
+struct NativeTypes<i32_t> : ConcreteType<int32_t> {};
 template <>
-struct native_types<i64_t> : concrete_type<int64_t> {};
+struct NativeTypes<i64_t> : ConcreteType<int64_t> {};
 template <>
-struct native_types<float_t> : concrete_type<float> {};
+struct NativeTypes<float_t> : ConcreteType<float> {};
 template <>
-struct native_types<double_t> : concrete_type<double> {};
+struct NativeTypes<double_t> : ConcreteType<double> {};
 template <>
-struct native_types<string_t> : concrete_type<std::string> {};
+struct NativeTypes<string_t> : ConcreteType<std::string> {};
 template <>
-struct native_types<binary_t> : concrete_type<std::string> {};
+struct NativeTypes<binary_t> : ConcreteType<std::string> {};
 
 // Traits for enums.
 template <typename E>
-struct native_types<enum_t<E>> : concrete_type<E> {};
+struct NativeTypes<enum_t<E>> : ConcreteType<E> {};
 
 // Traits for structs.
 template <typename T>
-struct native_types<struct_t<T>> : concrete_type<T> {};
+struct NativeTypes<struct_t<T>> : ConcreteType<T> {};
 
 // Traits for unions.
 template <typename T>
-struct native_types<union_t<T>> : concrete_type<T> {};
+struct NativeTypes<union_t<T>> : ConcreteType<T> {};
 
 // Traits for excpetions.
 template <typename T>
-struct native_types<exception_t<T>> : concrete_type<T> {};
+struct NativeTypes<exception_t<T>> : ConcreteType<T> {};
 
 // Traits for lists.
 template <typename ValTag>
-struct native_types<type::list<ValTag>>
+struct NativeTypes<type::list<ValTag>>
     : parameterized_type<std::vector, ValTag> {};
 
 // Traits for sets.
 template <typename KeyTag>
-struct native_types<set<KeyTag>> : parameterized_type<std::set, KeyTag> {};
+struct NativeTypes<set<KeyTag>> : parameterized_type<std::set, KeyTag> {};
 
 // Traits for maps.
 template <typename KeyTag, typename ValTag>
-struct native_types<map<KeyTag, ValTag>>
+struct NativeTypes<map<KeyTag, ValTag>>
     : parameterized_kv_type<std::map, KeyTag, ValTag> {};
 
 // Traits for adapted types.
 //
 // Adapted types are concrete and have an adapted native_type.
 template <typename Adapter, typename Tag>
-struct native_types<adapted<Adapter, Tag>>
-    : concrete_type<
-          typename native_types<Tag>::standard_type,
+struct NativeTypes<adapted<Adapter, Tag>>
+    : ConcreteType<
+          typename NativeTypes<Tag>::standard_type,
           adapt_detail::
-              adapted_t<Adapter, typename native_types<Tag>::native_type>> {};
+              adapted_t<Adapter, typename NativeTypes<Tag>::native_type>> {};
 
 // Traits for cpp_type types.
 //
 // cpp_type types are concrete and have special native_type.
 template <typename T, typename Tag>
-struct native_types<cpp_type<T, Tag>>
-    : concrete_type<typename native_types<Tag>::standard_type, T> {};
+struct NativeTypes<cpp_type<T, Tag>>
+    : ConcreteType<typename NativeTypes<Tag>::standard_type, T> {};
 
 // Traits for field type tag.
 //
 // TODO(dokwon): Remove this after field_t migration.
 template <typename Tag, FieldId Id>
-struct native_types<field_t<Id, Tag>> : native_types<Tag> {};
+struct NativeTypes<field_t<Id, Tag>> : NativeTypes<Tag> {};
 
 template <typename Tag, typename Context>
-struct native_types<field<Tag, Context>> : native_types<Tag> {};
+struct NativeTypes<field<Tag, Context>> : NativeTypes<Tag> {};
 
 // Traits for field adapted types.
 //
 // Field adapted types are concrete and have an adapted native_type.
 template <typename Adapter, typename Tag, typename Struct, int16_t FieldId>
-struct native_types<field<adapted<Adapter, Tag>, FieldContext<Struct, FieldId>>>
-    : concrete_type<
-          typename native_types<Tag>::standard_type,
+struct NativeTypes<field<adapted<Adapter, Tag>, FieldContext<Struct, FieldId>>>
+    : ConcreteType<
+          typename NativeTypes<Tag>::standard_type,
           adapt_detail::adapted_field_t<
               Adapter,
               FieldId,
-              typename native_types<Tag>::native_type,
+              typename NativeTypes<Tag>::native_type,
               Struct>> {};
 
-// Infers an appropiate type tag for given native type.
-template <typename T, typename = void>
-struct InferTag;
-
+// All generated structured types.
 template <typename T>
-struct InferTag<T, std::enable_if_t<is_thrift_class_v<T>>> {
-  static_assert(is_thrift_class_v<T>, "");
-  using type = folly::conditional_t<
-      is_thrift_union_v<T>,
-      type::union_t<T>,
-      folly::conditional_t<
-          is_thrift_exception_v<T>,
-          type::exception_t<T>,
-          type::struct_t<T>>>;
+struct InferTag<T, std::enable_if_t<is_thrift_union_v<T>>> {
+  using type = type::union_t<T>;
+};
+template <typename T>
+struct InferTag<T, std::enable_if_t<is_thrift_exception_v<T>>> {
+  using type = type::exception_t<T>;
+};
+template <typename T>
+struct InferTag<T, std::enable_if_t<is_thrift_struct_v<T>>> {
+  using type = type::struct_t<T>;
 };
 
 } // namespace detail

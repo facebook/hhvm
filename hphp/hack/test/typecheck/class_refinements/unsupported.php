833d59bbdafb161589ac80e5184cade7be1b1d0d
Leo Osvald <leoo@fb.com>
2022-08-29 13:12:07 -0700

    Write more test cases [1/?]: class generic + covariance
    Summary:
    Write tests for type refinements that could replace the projection of the following form:
    ```
    class Outer<TBox as Box> { ...
      function f<T0>(
        Covariant<TBox> $cov_box,
      ): T0 where TBox::T = T0
    }
    ```
    where `Box` has a type constant `T`. The above form is unsound, and outright banned in Scala 3 (DOT calculus).
    Instead, *a sound* alternative in Scala syntax would be:
    ```
    class Outer[T]
      def f[T0, TBox0 <: Box with { type T = T0 } & TBox](
          cov_t: Covariant[TBox0]
      ): T0
    ```
    which *can* be supported in Hack even without intersection types (`&` ) via multiple `as` in where-clause:
    
    ```
    class Outer<TBox as Box> {
      function genericSubOuterAndRefinement<T0, TBox0>(
        Covariant<TBox0> $cov_box,
      ): T0
      where
        TBox0 as Box with { type T = T0 },
        TBox0 as TBox; // equivalent to: ... & TBox
    }
    ```
    
    **This diff adds test coverage for this and a few similar cases.**
    
    Reviewed By: mpu
    
    Differential Revision: D39092141
    
    fbshipit-source-id: 5b6e82c5a5e314899619c86fb2dda79d13d7655f

--- hphp/hack/test/typecheck/class_refinements/unsupported.php
+++ hphp/hack/test/typecheck/class_refinements/unsupported.php
@@ -15,3 +15,19 @@ function unsupported_bounds<T>(Box with {type T as T} $b): T {
 function unsupported_root<Ta as Box, T>(Ta with {type T = T} $b): T {
   while (true) {}
 }
+
+interface Covariant<+T> { public function unwrap(): T; }
+
+abstract class UnsupportedClassGenericRefinement<TBox as Box> {
+  public static function subOuterRefinement<T0, TBox0>(
+    Covariant<TBox0> $cov_box,
+  ): T0 where TBox0 as TBox with { type T = T0 } { // unsupported in Hack
+    return $cov_box->unwrap()->get(); // ERROR: `TBox0` doesn't have `get`
+  } // NOTE: ^ works in Scala, but we disallow `with` on generics by design
+
+  public static function eqOuterRefinement<T0, TBox0>(
+    Covariant<TBox0> $cov_box,
+  ): T0 where TBox0 = TBox with { type T = T0 } { // unsupported in Hack
+    return $cov_box->unwrap()->get(); // ERROR: `TBox0` doesn't have `get`
+  } // NOTE: ^ works in Scala, but we disallow `with` on generics by design
+}

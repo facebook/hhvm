5b9b8ea0b2c80d716111d1db0a1e203c3f1b1881
Guilherme Ottoni <ottoni@fb.com>
2022-09-08 03:22:27 -0700

    Fix assert in StringData::hashStatic()
    Summary:
    The computed hash may be 0. This happens for the empty string when the C++,
    murmur hash implementation is used.
    
    Reviewed By: ricklavoie
    
    Differential Revision: D39326971
    
    fbshipit-source-id: adc1a13e287d740782370074a20a62954b4af714

--- hphp/runtime/base/string-data-inl.h
+++ hphp/runtime/base/string-data-inl.h
@@ -149,7 +149,7 @@ inline strhash_t StringData::hash() const {
 inline strhash_t StringData::hashStatic() const {
   assertx(isStatic());
   const strhash_t h = m_hash & STRHASH_MASK;
-  assertx(h);
+  assertx(h >= 0);
   return h;
 }
 

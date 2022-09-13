122f4194a2ddd67fa995a500627b1fecb7409f2d
Jake Bailey <jakebailey@fb.com>
2022-09-13 10:48:11 -0700

    Propagate io errors up to entrypoint
    Summary: Instead of unwrapping on every write, propagate io errors up to the caller.
    
    Reviewed By: shayne-fletcher
    
    Differential Revision: D39450951
    
    fbshipit-source-id: 26061bc37d9e941089e120d7afadc7a36de5ac80

--- hphp/hack/src/ocamlrep_marshal/ser.rs
+++ hphp/hack/src/ocamlrep_marshal/ser.rs
@@ -1,6 +1,7 @@
 // Initially generatd by c2rust of 'extern.c' at revision:
 // `f14c8ff3f8a164685bc24184fba84904391e378e`.
 
+use std::io;
 use std::io::Read;
 use std::io::Seek;
 use std::io::Write;
@@ -139,18 +140,18 @@ fn bitvect_set(bv: &mut [usize], i: usize) {
 // Conversion to big-endian
 
 #[inline]
-fn store16(dst: &mut impl Write, n: i16) {
-    dst.write_all(&n.to_be_bytes()).unwrap()
+fn store16(dst: &mut impl Write, n: i16) -> io::Result<()> {
+    dst.write_all(&n.to_be_bytes())
 }
 
 #[inline]
-fn store32(dst: &mut impl Write, n: i32) {
-    dst.write_all(&n.to_be_bytes()).unwrap()
+fn store32(dst: &mut impl Write, n: i32) -> io::Result<()> {
+    dst.write_all(&n.to_be_bytes())
 }
 
 #[inline]
-fn store64(dst: &mut impl Write, n: i64) {
-    dst.write_all(&n.to_be_bytes()).unwrap()
+fn store64(dst: &mut impl Write, n: i64) -> io::Result<()> {
+    dst.write_all(&n.to_be_bytes())
 }
 
 #[repr(C)]
@@ -318,18 +319,18 @@ impl<'a, W: Write> State<'a, W> {
     // Write characters, integers, and blocks in the output buffer
 
     #[inline]
-    fn write(&mut self, c: u8) {
+    fn write(&mut self, c: u8) -> io::Result<()> {
         self.output_len += 1;
-        self.output.write_all(&[c]).unwrap();
+        self.output.write_all(&[c])
     }
 
-    fn writeblock(&mut self, data: &[u8]) {
+    fn writeblock(&mut self, data: &[u8]) -> io::Result<()> {
         self.output_len += data.len();
-        self.output.write_all(data).unwrap();
+        self.output.write_all(data)
     }
 
     #[inline]
-    fn writeblock_float8(&mut self, data: &[f64]) {
+    fn writeblock_float8(&mut self, data: &[f64]) -> io::Result<()> {
         if ARCH_FLOAT_ENDIANNESS == 0x01234567 || ARCH_FLOAT_ENDIANNESS == 0x76543210 {
             // SAFETY: `data.as_ptr()` will be valid for reads of `data.len() *
             // size_of::<f64>()` bytes
@@ -338,73 +339,73 @@ impl<'a, W: Write> State<'a, W> {
                     data.as_ptr() as *const u8,
                     data.len() * std::mem::size_of::<f64>(),
                 )
-            });
+            })
         } else {
             unimplemented!()
         }
     }
 
-    fn writecode8(&mut self, code: u8, val: i8) {
+    fn writecode8(&mut self, code: u8, val: i8) -> io::Result<()> {
         self.output_len += 2;
-        self.output.write_all(&[code, val as u8]).unwrap();
+        self.output.write_all(&[code, val as u8])
     }
 
-    fn writecode16(&mut self, code: u8, val: i16) {
+    fn writecode16(&mut self, code: u8, val: i16) -> io::Result<()> {
         self.output_len += 3;
-        self.output.write_all(&[code]).unwrap();
-        store16(&mut self.output, val);
+        self.output.write_all(&[code])?;
+        store16(&mut self.output, val)
     }
 
-    fn writecode32(&mut self, code: u8, val: i32) {
+    fn writecode32(&mut self, code: u8, val: i32) -> io::Result<()> {
         self.output_len += 5;
-        self.output.write_all(&[code]).unwrap();
-        store32(&mut self.output, val);
+        self.output.write_all(&[code])?;
+        store32(&mut self.output, val)
     }
 
-    fn writecode64(&mut self, code: u8, val: i64) {
+    fn writecode64(&mut self, code: u8, val: i64) -> io::Result<()> {
         self.output_len += 9;
-        self.output.write_all(&[code]).unwrap();
-        store64(&mut self.output, val);
+        self.output.write_all(&[code])?;
+        store64(&mut self.output, val)
     }
 
     /// Marshaling integers
     #[inline]
-    fn extern_int(&mut self, n: isize) {
+    fn extern_int(&mut self, n: isize) -> io::Result<()> {
         if (0..0x40).contains(&n) {
-            self.write(PREFIX_SMALL_INT + n as u8);
+            self.write(PREFIX_SMALL_INT + n as u8)
         } else if (-(1 << 7)..(1 << 7)).contains(&n) {
-            self.writecode8(CODE_INT8, n as i8);
+            self.writecode8(CODE_INT8, n as i8)
         } else if (-(1 << 15)..(1 << 15)).contains(&n) {
-            self.writecode16(CODE_INT16, n as i16);
+            self.writecode16(CODE_INT16, n as i16)
         } else if !(-(1 << 30)..(1 << 30)).contains(&n) {
             if self.flags.contains(ExternFlags::COMPAT_32) {
                 self.failwith("output_value: integer cannot be read back on 32-bit platform");
             }
-            self.writecode64(CODE_INT64, n as i64);
+            self.writecode64(CODE_INT64, n as i64)
         } else {
-            self.writecode32(CODE_INT32, n as i32);
-        };
+            self.writecode32(CODE_INT32, n as i32)
+        }
     }
 
     /// Marshaling references to previously-marshaled blocks
     #[inline]
-    fn extern_shared_reference(&mut self, d: usize) {
+    fn extern_shared_reference(&mut self, d: usize) -> io::Result<()> {
         if d < 0x100 {
-            self.writecode8(CODE_SHARED8, d as i8);
+            self.writecode8(CODE_SHARED8, d as i8)
         } else if d < 0x10000 {
-            self.writecode16(CODE_SHARED16, d as i16);
+            self.writecode16(CODE_SHARED16, d as i16)
         } else if d >= 1 << 32 {
-            self.writecode64(CODE_SHARED64, d as i64);
+            self.writecode64(CODE_SHARED64, d as i64)
         } else {
-            self.writecode32(CODE_SHARED32, d as i32);
-        };
+            self.writecode32(CODE_SHARED32, d as i32)
+        }
     }
 
     /// Marshaling block headers
     #[inline]
-    fn extern_header(&mut self, sz: usize, tag: u8) {
+    fn extern_header(&mut self, sz: usize, tag: u8) -> io::Result<()> {
         if tag < 16 && sz < 8 {
-            self.write(PREFIX_SMALL_BLOCK + tag + ((sz as u8) << 4));
+            self.write(PREFIX_SMALL_BLOCK + tag + ((sz as u8) << 4))
         } else {
             // Note: ocaml-14.4.0 uses `Caml_white` (`0 << 8`)
             // ('caml/runtime/gc.h'). That's why we use this here, so that we
@@ -422,61 +423,61 @@ impl<'a, W: Write> State<'a, W> {
                 self.failwith("output_value: array cannot be read back on 32-bit platform");
             }
             if hd < 1 << 32 {
-                self.writecode32(CODE_BLOCK32, hd as i32);
+                self.writecode32(CODE_BLOCK32, hd as i32)
             } else {
-                self.writecode64(CODE_BLOCK64, hd as i64);
+                self.writecode64(CODE_BLOCK64, hd as i64)
             }
-        };
+        }
     }
 
     #[inline]
-    fn extern_string(&mut self, bytes: &'a [u8]) {
+    fn extern_string(&mut self, bytes: &'a [u8]) -> io::Result<()> {
         let len = bytes.len();
         if len < 0x20 {
-            self.write(PREFIX_SMALL_STRING + len as u8);
+            self.write(PREFIX_SMALL_STRING + len as u8)?;
         } else if len < 0x100 {
-            self.writecode8(CODE_STRING8, len as i8);
+            self.writecode8(CODE_STRING8, len as i8)?;
         } else {
             if len > 0xFFFFFB && self.flags.contains(ExternFlags::COMPAT_32) {
                 self.failwith("output_value: string cannot be read back on 32-bit platform");
             }
             if len < 1 << 32 {
-                self.writecode32(CODE_STRING32, len as i32);
+                self.writecode32(CODE_STRING32, len as i32)?;
             } else {
-                self.writecode64(CODE_STRING64, len as i64);
+                self.writecode64(CODE_STRING64, len as i64)?;
             }
         }
-        self.writeblock(bytes);
+        self.writeblock(bytes)
     }
 
     /// Marshaling FP numbers
     #[inline]
-    fn extern_double(&mut self, v: f64) {
-        self.write(CODE_DOUBLE_NATIVE);
-        self.writeblock_float8(&[v]);
+    fn extern_double(&mut self, v: f64) -> io::Result<()> {
+        self.write(CODE_DOUBLE_NATIVE)?;
+        self.writeblock_float8(&[v])
     }
 
     /// Marshaling FP arrays
     #[inline]
-    fn extern_double_array(&mut self, slice: &[f64]) {
+    fn extern_double_array(&mut self, slice: &[f64]) -> io::Result<()> {
         let nfloats = slice.len();
         if nfloats < 0x100 {
-            self.writecode8(CODE_DOUBLE_ARRAY8_NATIVE, nfloats as i8);
+            self.writecode8(CODE_DOUBLE_ARRAY8_NATIVE, nfloats as i8)?;
         } else {
             if nfloats > 0x1FFFFF && self.flags.contains(ExternFlags::COMPAT_32) {
                 self.failwith("output_value: float array cannot be read back on 32-bit platform");
             }
             if nfloats < 1 << 32 {
-                self.writecode32(CODE_DOUBLE_ARRAY32_NATIVE, nfloats as i32);
+                self.writecode32(CODE_DOUBLE_ARRAY32_NATIVE, nfloats as i32)?;
             } else {
-                self.writecode64(CODE_DOUBLE_ARRAY64_NATIVE, nfloats as i64);
+                self.writecode64(CODE_DOUBLE_ARRAY64_NATIVE, nfloats as i64)?;
             }
         }
-        self.writeblock_float8(slice);
+        self.writeblock_float8(slice)
     }
 
     /// Marshal the given value in the output buffer
-    fn extern_rec(&mut self, mut v: Value<'a>) {
+    fn extern_rec(&mut self, mut v: Value<'a>) -> io::Result<()> {
         let mut goto_next_item: bool;
 
         let mut h: usize = 0;
@@ -486,7 +487,7 @@ impl<'a, W: Write> State<'a, W> {
 
         loop {
             if v.is_immediate() {
-                self.extern_int(v.as_int().unwrap());
+                self.extern_int(v.as_int().unwrap())?;
             } else {
                 let b = v.as_block().unwrap();
                 let tag = b.tag();
@@ -509,12 +510,12 @@ impl<'a, W: Write> State<'a, W> {
                 // Atoms are treated specially for two reasons: they are not allocated
                 // in the externed block, and they are automatically shared.
                 if sz == 0 {
-                    self.extern_header(0, tag);
+                    self.extern_header(0, tag)?;
                 } else {
                     // Check if object already seen
                     if !self.flags.contains(ExternFlags::NO_SHARING) {
                         if self.lookup_position(v, &mut pos, &mut h) {
-                            self.extern_shared_reference(self.obj_counter - pos);
+                            self.extern_shared_reference(self.obj_counter - pos)?;
                             goto_next_item = true;
                         } else {
                             goto_next_item = false;
@@ -528,20 +529,20 @@ impl<'a, W: Write> State<'a, W> {
                             ocamlrep::STRING_TAG => {
                                 let bytes = v.as_byte_string().unwrap();
                                 let len: usize = bytes.len();
-                                self.extern_string(bytes);
+                                self.extern_string(bytes)?;
                                 self.size_32 += 1 + (len + 4) / 4;
                                 self.size_64 += 1 + (len + 8) / 8;
                                 self.record_location(v, h);
                             }
                             ocamlrep::DOUBLE_TAG => {
-                                self.extern_double(v.as_float().unwrap());
+                                self.extern_double(v.as_float().unwrap())?;
                                 self.size_32 += 1 + 2;
                                 self.size_64 += 1 + 1;
                                 self.record_location(v, h);
                             }
                             ocamlrep::DOUBLE_ARRAY_TAG => {
                                 let slice = v.as_double_array().unwrap();
-                                self.extern_double_array(slice);
+                                self.extern_double_array(slice)?;
                                 let nfloats = slice.len();
                                 self.size_32 += 1 + nfloats * 2;
                                 self.size_64 += 1 + nfloats;
@@ -563,7 +564,7 @@ impl<'a, W: Write> State<'a, W> {
                                 "output_value: marshaling of closures not implemented",
                             ),
                             _ => {
-                                self.extern_header(sz, tag);
+                                self.extern_header(sz, tag)?;
                                 self.size_32 += 1 + sz;
                                 self.size_64 += 1 + sz;
                                 self.record_location(v, h);
@@ -595,7 +596,7 @@ impl<'a, W: Write> State<'a, W> {
                 }
             } else {
                 // We are done.
-                return;
+                return Ok(());
             }
         }
         // Never reached as function leaves with return
@@ -607,14 +608,14 @@ impl<'a, W: Write> State<'a, W> {
         flags: ExternFlags,
         mut header: &mut [u8],  // out
         header_len: &mut usize, // out
-    ) -> usize {
+    ) -> io::Result<usize> {
         // Initializations
         self.flags = flags;
         self.obj_counter = 0;
         self.size_32 = 0;
         self.size_64 = 0;
         // Marshal the object
-        self.extern_rec(v);
+        self.extern_rec(v)?;
         // Write the header
         let res_len = self.output_len;
         if res_len >= (1 << 32) || self.size_32 >= (1 << 32) || self.size_64 >= (1 << 32) {
@@ -623,22 +624,23 @@ impl<'a, W: Write> State<'a, W> {
             if self.flags.contains(ExternFlags::COMPAT_32) {
                 self.failwith("output_value: object too big to be read back on 32-bit platform");
             }
-            store32(&mut header, MAGIC_NUMBER_BIG as i32);
-            store32(&mut header, 0);
-            store64(&mut header, res_len as i64);
-            store64(&mut header, self.obj_counter as i64);
-            store64(&mut header, self.size_64 as i64);
+            store32(&mut header, MAGIC_NUMBER_BIG as i32)?;
+            store32(&mut header, 0)?;
+            store64(&mut header, res_len as i64)?;
+            store64(&mut header, self.obj_counter as i64)?;
+            store64(&mut header, self.size_64 as i64)?;
             *header_len = 32;
-            return res_len;
+            Ok(res_len)
+        } else {
+            // Use the small header format
+            store32(&mut header, MAGIC_NUMBER_SMALL as i32)?;
+            store32(&mut header, res_len as i32)?;
+            store32(&mut header, self.obj_counter as i32)?;
+            store32(&mut header, self.size_32 as i32)?;
+            store32(&mut header, self.size_64 as i32)?;
+            *header_len = 20;
+            Ok(res_len)
         }
-        // Use the small header format
-        store32(&mut header, MAGIC_NUMBER_SMALL as i32);
-        store32(&mut header, res_len as i32);
-        store32(&mut header, self.obj_counter as i32);
-        store32(&mut header, self.size_32 as i32);
-        store32(&mut header, self.size_64 as i32);
-        *header_len = 20;
-        res_len
     }
 }
 
@@ -646,15 +648,15 @@ pub fn output_value<W: Read + Write + Seek>(
     w: &mut W,
     v: Value<'_>,
     flags: ExternFlags,
-) -> std::io::Result<()> {
-    let mut header: [u8; 32] = [0; 32];
+) -> io::Result<()> {
+    let mut header = [0; 32];
     let mut header_len = 0;
     // At this point we don't know the size of the header.
     // Guess that it is small, and fix up later if not.
     w.seek(std::io::SeekFrom::Start(0))?;
     w.write_all(&[0; 20])?;
     let mut s = State::new(&mut *w);
-    s.extern_value(v, flags, &mut header, &mut header_len);
+    s.extern_value(v, flags, &mut header, &mut header_len)?;
     drop(s);
     w.flush()?;
     if header_len != 20 {

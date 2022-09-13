2c8efa370dc6c0cac12f9d3c3835233cfaf679a8
Jacob Stein <jacobstein@fb.com>
2022-09-13 11:19:26 -0700

    Remove extra headers to improve build speed (#9186)
    Summary: Pull Request resolved: https://github.com/facebook/hhvm/pull/9186
    
    Reviewed By: lenar-f
    
    Differential Revision: D39187718
    
    fbshipit-source-id: e62dc389f7309c592b8828fa47d221ccda4234e6

--- third-party/mcrouter/src/mcrouter/lib/carbon/NoopAdditionalLogger.h
+++ third-party/mcrouter/src/mcrouter/lib/carbon/NoopAdditionalLogger.h
@@ -5,8 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#include "mcrouter/lib/carbon/ExternalCarbonConnectionStats.h"
-
 #pragma once
 
 // Forward declaration
@@ -65,23 +63,6 @@ class NoopNoBeforeAdditionalLogger {
       const facebook::memcache::mcrouter::RequestClass& /* unused */,
       const carbon::Result& /* unused */,
       const int64_t /* unused */) const {
-    return false;
-  }
-};
-
-class NoopExternalConnectionAdditionalLogger {
- public:
-  explicit NoopExternalConnectionAdditionalLogger(
-      carbon::ExternalCarbonConnectionLoggerOptions&) {}
-
-  void log(carbon::ExternalCarbonConnectionStats&) {}
-
-  bool shouldLog() const {
-    return false;
-  }
-
-  void setEnabledStatus(bool /* unused */) {}
-  bool getEnabledStatus() {
     return false;
   }
 };

1e9f8f93d1a8a252ca9e8009e718f639b43000a1
Yang, Bo <pop.atry@gmail.com>
2022-09-13 18:17:53 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -32,8 +32,8 @@
           };
           packages.default = packages.hhvm;
 
-          checks.full = pkgs.runCommand
-            "hhvm-full-test"
+          checks.all = pkgs.runCommand
+            "hhvm-all-test"
             {
               buildInputs = pkgs.lib.optionals pkgs.hostPlatform.isMacOS [
                 # `system_cmds` provides `sysctl`, which is used in hphp/test/run.php on macOS
@@ -43,7 +43,7 @@
             ''
               set -ex
               cd ${./.}
-              HHVM_BIN="${packages.hhvm}/bin/hhvm" "${packages.hhvm}/bin/hhvm" hphp/test/run.php full
+              HHVM_BIN="${packages.hhvm}/bin/hhvm" "${packages.hhvm}/bin/hhvm" hphp/test/run.php all
               mkdir $out
             '';
 

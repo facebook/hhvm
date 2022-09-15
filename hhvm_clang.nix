fc1c7c670d35aaa0ac1d399bab202af4f8a08284
Yang, Bo <atry@fb.com>
2022-09-15 03:27:48 +0000

    Workaround for dtoa.0.3.2

--- hhvm_clang.nix
+++ hhvm_clang.nix
@@ -5,4 +5,9 @@ lastModifiedDate:
 let
   common = callPackage ./common.nix { } lastModifiedDate;
 in
-llvmPackages_12.libcxxStdenv.mkDerivation common
+llvmPackages_12.libcxxStdenv.mkDerivation (common // {
+  NIX_CFLAGS_COMPILE = common.NIX_CFLAGS_COMPILE ++ [
+    # Workaround for dtoa.0.3.2
+    "-Wno-error=unused-command-line-argument"
+  ];
+})

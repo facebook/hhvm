59bb684e6c317193be58779321c4181781132435
Yang, Bo <atry@fb.com>
2022-09-16 13:09:39 -0700

    Switch to LLVM 13

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
             lastModifiedDate = self.lastModifiedDate;
           };
           packages.hhvm_clang = packages.hhvm.override {
-            stdenv = pkgs.llvmPackages_12.libcxxStdenv;
+            stdenv = pkgs.llvmPackages_13.libcxxStdenv;
           };
           packages.default = packages.hhvm;
 

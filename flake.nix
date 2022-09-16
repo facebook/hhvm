29af81af63c951afd76084c97e8b5656ab1036e3
Bo Yang <atry@fb.com>
2022-09-16 12:48:54 -0700

    Add a VS Code workspace for building HHVM with Clang (#9198)
    Summary:
    This PR adds the Clang development environment for VS Code, including:
    1.  The Nix Flake development shell `devShells.clang` is added to `flake.nix`.
    2. The Nix 1.x  development shell file `shell_clang.nix` is added as a wrapper of `devShells.clang`
    3. The VS Code workspace file `clang.code-workspace` which uses `shell_clang.nix` to set up environment variables
    
    Pull Request resolved: https://github.com/facebook/hhvm/pull/9198
    
    Test Plan:
    In a GitHub Codespace:
    
    - Run `nix develop "git+file://$(pwd)?submodules=1&shallow=1#clang"` to enter the Nix Flake development shell
    - Run `nix-shell shell_clang.nix` to enter the Nix 1.x development shell.
    - Open `clang.code-workspace`, and launch a CMake build, then the build should be configured to use clang and libc++
    
    Reviewed By: alexeyt
    
    Differential Revision: D39555209
    
    Pulled By: Atry
    
    fbshipit-source-id: dd0566f35c8c2f09f399b241377a097d1b5ad161

--- flake.nix
+++ flake.nix
@@ -25,28 +25,34 @@
               "libdwarf-20210528"
             ];
           };
-        in
-        rec {
-          packages.hhvm = pkgs.callPackage ./hhvm.nix {
-            lastModifiedDate = self.lastModifiedDate;
-          };
-          packages.default = packages.hhvm;
-
-          devShells.default =
-            pkgs.mkShell
+          devShellForPackage = hhvm:
+            pkgs.mkShell.override
+              { stdenv = hhvm.stdenv; }
               {
                 inputsFrom = [
-                  packages.hhvm
+                  hhvm
                 ];
                 packages = [
                   pkgs.rnix-lsp
                   pkgs.fpm
                   pkgs.rpm
                 ];
-                inherit (packages.hhvm)
+                inherit (hhvm)
                   NIX_CFLAGS_COMPILE
                   CMAKE_INIT_CACHE;
               };
+        in
+        rec {
+          packages.hhvm = pkgs.callPackage ./hhvm.nix {
+            lastModifiedDate = self.lastModifiedDate;
+          };
+          packages.hhvm_clang = packages.hhvm.override {
+            stdenv = pkgs.llvmPackages_12.libcxxStdenv;
+          };
+          packages.default = packages.hhvm;
+
+          devShells.clang = devShellForPackage packages.hhvm_clang;
+          devShells.default = devShellForPackage packages.hhvm;
 
           ${if pkgs.hostPlatform.isLinux then "bundlers" else null} =
             let

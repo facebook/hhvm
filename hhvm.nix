4f4bc50042673694de99f858f105891910c685b4
Yang, Bo <atry@fb.com>
2022-09-16 02:51:15 +0000

    Forward stdenv to C++ libraries

--- hhvm.nix
+++ hhvm.nix
@@ -16,6 +16,7 @@
 , gd
 , gdb
 , gettext
+, gflags
 , git
 , glog
 , gmp
@@ -116,25 +117,27 @@ stdenv.mkDerivation rec {
     ];
   buildInputs =
     [
-      boost
+      (boost.override { inherit stdenv; })
       brotli
       bzip2
       curl
-      double-conversion
+      (double-conversion.override { inherit stdenv; })
       editline
       expat
-      fmt
+      (fmt.override { inherit stdenv; })
       freetype
       fribidi
       gd
       gdb
       gettext
       git
+      # TODO: (glog.override { inherit stdenv; })
       glog
       gmp
+      # TODO: (gperf.override {inherit stdenv; })
       gperf
       gperftools
-      icu
+      (icu.override { inherit stdenv; })
       imagemagick6
       jemalloc
       libdwarf
@@ -161,7 +164,7 @@ stdenv.mkDerivation rec {
       re2
       re2c
       sqlite
-      tbb
+      (tbb.override { inherit stdenv; })
       tzdata
       unzip
       zlib

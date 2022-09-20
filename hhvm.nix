7adea8fa9a78a684f90f663924136e87546aae9d
Yang, Bo <pop.atry@gmail.com>
2022-09-20 03:02:55 +0000

    remove gccgit

--- hhvm.nix
+++ hhvm.nix
@@ -33,7 +33,6 @@
 , libedit
 , libelf
 , libevent
-, libgccjit
 , libkrb5
 , libmcrypt
 , libmemcached
@@ -153,7 +152,6 @@ stdenv.mkDerivation rec {
       libedit
       libelf
       libevent
-      libgccjit
       libkrb5
       libmcrypt
       libmemcached

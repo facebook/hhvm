c0732634dd5f1737c900530c9b98eaf6f389be9c
Alan Frindell <afrind@meta.com>
2022-09-20 18:47:07 -0700

    Elide empty trailers
    Summary: sendTrailers({}) converts to sendEOM().  There's no point in sending an empty trailer block, and it might confuse the receiver.
    
    Reviewed By: hanidamlaj, mjoras
    
    Differential Revision: D39664211
    
    fbshipit-source-id: 5f434fdc51dde77c2f101e771d86872a9fae2dc2

--- third-party/proxygen/src/proxygen/lib/http/codec/test/HTTP2CodecTest.cpp
+++ third-party/proxygen/src/proxygen/lib/http/codec/test/HTTP2CodecTest.cpp
@@ -2441,6 +2441,39 @@ TEST_F(HTTP2CodecTest, TrailersReply) {
 #endif
 }
 
+TEST_F(HTTP2CodecTest, TrailersReplyEmpty) {
+  SetUpUpstreamTest();
+  HTTPMessage resp;
+  resp.setStatusCode(200);
+  resp.setStatusMessage("nifty-nice");
+  resp.getHeaders().add(HTTP_HEADER_CONTENT_TYPE, "x-coolio");
+  downstreamCodec_.generateHeader(output_, 1, resp);
+
+  string data("abcde");
+  auto buf = folly::IOBuf::copyBuffer(data.data(), data.length());
+  downstreamCodec_.generateBody(
+      output_, 1, std::move(buf), HTTPCodec::NoPadding, false);
+
+  HTTPHeaders trailers;
+  auto trailerSz = downstreamCodec_.generateTrailers(output_, 1, trailers);
+  // Just a frame header (DATA + END_STREAM).
+  EXPECT_EQ(trailerSz, 9);
+
+  parseUpstream();
+
+  callbacks_.expectMessage(true, 2, 200);
+  EXPECT_EQ(callbacks_.bodyCalls, 1);
+  EXPECT_EQ(callbacks_.bodyLength, 5);
+  const auto& headers = callbacks_.msg->getHeaders();
+  EXPECT_TRUE(callbacks_.msg->getHeaders().exists(HTTP_HEADER_DATE));
+  EXPECT_EQ("x-coolio", headers.getSingleOrEmpty(HTTP_HEADER_CONTENT_TYPE));
+  EXPECT_EQ(0, callbacks_.trailers);
+  EXPECT_EQ(nullptr, callbacks_.msg->getTrailers());
+#ifndef NDEBUG
+  EXPECT_EQ(upstreamCodec_.getReceivedFrameCount(), 4);
+#endif
+}
+
 TEST_F(HTTP2CodecTest, TrailersReplyWithNoData) {
   SetUpUpstreamTest();
   HTTPMessage resp;

ea1094908c257bb1a2fa9f0a7c8d47357406bef0
TJ Yin <ytj@fb.com>
2022-09-21 18:47:21 -0700

    Fix cpp_type type tag when including another thrift file with different namespace
    Summary:
    Assume we have the following thrift file
    
    ```
    // foo.thrift
    namespace cpp2 foo.bar
    typedef i32 (cpp.type = "SomeInt", cpp.indirection) MyInt
    ```
    
    ```
    include "foo.thrift"
    
    namespace cpp2 foo.baz
    struct Foo { 1: foo.MyInt field; }
    ```
    
    The generated type tag for `Foo.field` will literally be
    
    ```
    ::apache::thrift::type::adapted<::apache::thrift::IndirectionAdapter<SomeInt>, ::apache::thrift::type::i32_t>
    ```
    
    This is wrong since `SomeInt` doesn't exist in `foo::baz` namespace.
    
    This diff changed type tag to
    
    ```
    ::apache::thrift::type::adapted<::apache::thrift::IndirectionAdapter<::foo::bar::MyInt>, ::apache::thrift::type::i32_t>
    ```
    
    Which doesn't have this issue.
    
    Reviewed By: iahs
    
    Differential Revision: D39678235
    
    fbshipit-source-id: 5f26a766fbbfebad4bd91d8a38a04c3fea1edcd6

---
+++ third-party/thrift/src/thrift/test/reflection/include_indirection_type.thrift
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+include "thrift/test/reflection/fatal_reflection_indirection.thrift"
+
+package "apache.org/thrift/reflection"
+
+struct struct_with_included_indirections {
+  1: i32 real;
+  2: fatal_reflection_indirection.FakeI32 fake;
+  3: fatal_reflection_indirection.HasANumber number;
+  4: fatal_reflection_indirection.HasAResult result;
+
+  // XXX: string type can not be included with cpp.indirection
+  5: string (
+    cpp.type = '::reflection_indirection::CppHasAPhrase',
+    cpp.indirection,
+  ) phrase;
+}

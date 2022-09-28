6e49f8d67631659e685883027a0fecb2412d455f
Miljan Markovic <mmarkovic@fb.com>
2022-09-28 14:15:24 -0700

    Add more tests for type alias expansion.
    Summary: Add more type alias expansion tests in preparation for changes to how type alias expansion is implemented.
    
    Reviewed By: Wilfred
    
    Differential Revision: D39691773
    
    fbshipit-source-id: 5ea24191d78daf47df5af3a04178d7ff6ed0ab80

---
+++ hphp/hack/test/typecheck/typedef_complex_substitution.php
@@ -0,0 +1,19 @@
+<?hh // strict
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the "hack" directory of this source tree.
+ *
+ *
+ */
+
+type A<Ta,Tb> = dict<Ta, vec<dict<Tb, B<Ta>>>>;
+type B<T> = dict<T, vec<T>>;
+
+function expect(dict<int, vec<dict<string, dict<int, vec<int>>>>> $x): void {}
+
+function testit(A<int, string> $a): void {
+  expect($a);
+}
---
+++ hphp/hack/test/typecheck/typedef_hidden_union.php
@@ -0,0 +1,18 @@
+<?hh // strict
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the "hack" directory of this source tree.
+ *
+ *
+ */
+
+type A<T> = vec<?T>;
+
+function expect(vec<?int> $x): void { }
+
+function testit(A<?int> $x): void {
+  expect($x);
+}

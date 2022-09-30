bb62faf77abf2cde60f0c04a9947f5f6e9a24a74
Logan Evans <lpe@meta.com>
2022-09-30 15:02:47 -0700

    Add the boxed_ptr<T> class
    Summary:
    boxed_ptr<T> provides copy-on-write behavior for a value of type T. This
    wrapper can only be used for copyable types. Trivial types and types such as
    st::string that are already boxed are also disabled.
    
    This class is intended to replace boxed_value_ptr<T> once code generation is
    able to recognize when not to use boxed_ptr<T>.
    
    Reviewed By: Alfus
    
    Differential Revision: D39805636
    
    fbshipit-source-id: 162a968a3ba97918026f89f98291ae1917a285b7

--- third-party/thrift/src/thrift/lib/cpp2/BoxedValuePtr.h
+++ third-party/thrift/src/thrift/lib/cpp2/BoxedValuePtr.h
@@ -17,13 +17,17 @@
 #pragma once
 
 #include <memory>
+#include <type_traits>
 
 #include <folly/CPortability.h>
+#include <thrift/lib/cpp2/Thrift.h>
+#include <thrift/lib/cpp2/type/AlignedPtr.h>
 
 namespace apache {
 namespace thrift {
 namespace detail {
 
+// boxed_value_ptr will soon be replaced with boxed_ptr (defined below).
 template <typename T>
 class boxed_value_ptr {
  public:
@@ -107,7 +111,6 @@ class boxed_value_ptr {
     if (ptr_ == nullptr) {
       ptr_ = std::make_unique<T>();
     }
-
     *ptr_ = std::forward<U>(value);
     return *this;
   }
@@ -122,6 +125,180 @@ class boxed_value_ptr {
       ptr_ = std::make_unique<T>(std::forward<Args>(args)...);
     }
     return *ptr_;
+  }
+};
+
+template <typename T>
+class boxed_ptr {
+ public:
+  static_assert(
+      std::is_constructible<T, T&>::value ||
+          std::is_constructible<T, const T&>::value,
+      "Boxed type must be copy constructible");
+
+  using element_type = T;
+
+  FOLLY_ERASE constexpr boxed_ptr() noexcept = default;
+
+  FOLLY_ERASE constexpr boxed_ptr(const boxed_ptr& other) {
+    if (other.getMode() == Mode::MutOwned) {
+      ptr_ = other.copy();
+    } else {
+      ptr_ = other.ptr_;
+    }
+  }
+
+  FOLLY_ERASE constexpr boxed_ptr(boxed_ptr&& other) noexcept : boxed_ptr() {
+    std::swap(ptr_, other.ptr_);
+  }
+
+  FOLLY_ERASE constexpr boxed_ptr(const T* p) noexcept {
+    // The const-ness of the pointer will be tracked using the tag
+    // Mode::ConstUnowned. The only mutable accessor method is
+    // boxed_ptr<T>::mut(), which will copy *p in order to return a
+    // mutable pointer.
+    ptr_.set(
+        const_cast<T*>(p), static_cast<std::uintptr_t>(Mode::ConstUnowned));
+  }
+
+  FOLLY_ERASE ~boxed_ptr() noexcept { destroy(); }
+
+  FOLLY_ERASE constexpr const T& operator*() const noexcept {
+    return *ptr_.get();
+  }
+
+  FOLLY_ERASE constexpr const T* operator->() const noexcept {
+    return ptr_.get();
+  }
+
+  FOLLY_ERASE constexpr explicit operator bool() const noexcept {
+    return bool(ptr_.get());
+  }
+
+  FOLLY_ERASE constexpr boxed_ptr& operator=(boxed_ptr&& other) noexcept {
+    std::swap(ptr_, other.ptr_);
+    return *this;
+  }
+
+  FOLLY_ERASE
+  constexpr boxed_ptr& operator=(const boxed_ptr& other) {
+    boxed_ptr<T> tmp{other};
+    std::swap(ptr_, tmp.ptr_);
+    return *this;
+  }
+
+  FOLLY_ERASE
+  constexpr boxed_ptr& operator=(const T* p) {
+    boxed_ptr<T> tmp{p};
+    std::swap(ptr_, tmp.ptr_);
+    return *this;
+  }
+
+  FOLLY_ERASE constexpr T* mut() noexcept {
+    if (getMode() == Mode::ConstUnowned) {
+      ptr_ = copy();
+    }
+    return ptr_.get();
+  }
+
+  FOLLY_ERASE constexpr void reset(T* p = nullptr) noexcept {
+    destroy();
+    ptr_.set(p, static_cast<std::uintptr_t>(Mode::MutOwned));
+  }
+
+ private:
+  static constexpr int kModeBits = 3;
+  using aligned_pointer_type =
+      type::detail::AlignedPtr<T, kModeBits, kModeBits>;
+
+  enum Mode : std::uintptr_t {
+    MutOwned = 0, // Unique ownership
+    ConstUnowned = 1,
+  };
+
+  friend void swap(boxed_ptr& lhs, boxed_ptr& rhs) noexcept {
+    using std::swap;
+    swap(lhs.ptr_, rhs.ptr_);
+  }
+
+  friend constexpr bool operator==(
+      const boxed_ptr& lhs, const boxed_ptr& rhs) noexcept {
+    return lhs.ptr_.get() == rhs.ptr_.get();
+  }
+  friend constexpr bool operator==(
+      std::nullptr_t, const boxed_ptr& rhs) noexcept {
+    return nullptr == rhs.ptr_.get();
+  }
+  friend constexpr bool operator==(
+      const boxed_ptr& lhs, std::nullptr_t) noexcept {
+    return lhs.ptr_.get() == nullptr;
+  }
+  friend constexpr bool operator!=(
+      const boxed_ptr& lhs, const boxed_ptr& rhs) noexcept {
+    return !(lhs == rhs);
+  }
+  friend constexpr bool operator!=(
+      const boxed_ptr& lhs, std::nullptr_t) noexcept {
+    return lhs.ptr_.get() != nullptr;
+  }
+  friend constexpr bool operator!=(
+      std::nullptr_t, const boxed_ptr& rhs) noexcept {
+    return nullptr != rhs.ptr_.get();
+  }
+
+  Mode getMode() const { return static_cast<Mode>(ptr_.getTag()); }
+
+  void destroy() {
+    if (getMode() == Mode::MutOwned) {
+      delete ptr_.get();
+    }
+    ptr_.clear();
+  }
+
+  aligned_pointer_type copy() const {
+    // This leaves the tag region cleared indicating the new value is
+    // Mode::MutOwned.
+    aligned_pointer_type ptr =
+        ptr_.get() == nullptr ? nullptr : new T(*ptr_.get());
+    assert(ptr.getTag() == Mode::MutOwned);
+    return ptr;
+  }
+
+  aligned_pointer_type ptr_;
+
+ public:
+  // TODO(afuller): This implicitly inplace implicit constructor
+  // should be removed as it lets T intercept any constructor call, which is
+  // extremely bug prone.
+  template <typename... Args, typename = decltype(T(std::declval<Args>()...))>
+  FOLLY_ERASE boxed_ptr(Args&&... args)
+      : ptr_(new T(std::forward<Args>(args)...), Mode::MutOwned) {}
+
+  // TODO(afuller): This implicit creation and value assignment should be
+  // removed as it lets T intercept assignment, which is extremely bug prone.
+  //
+  // TODO(lpe): Replace this with make_boxed<T>.
+  template <typename U>
+  FOLLY_ERASE std::enable_if_t<std::is_assignable<T&, U&&>::value, boxed_ptr&>
+  operator=(U&& value) {
+    if (ptr_.get()) {
+      *ptr_.get() = std::forward<U>(value);
+    } else {
+      ptr_.set(new T(std::forward<U>(value)), Mode::MutOwned);
+    }
+    return *this;
+  }
+
+  // TODO(afuller): This is not a typical smart pointer feature, and
+  // likely should only exist on value-semantic types like std::optional.
+  template <typename... Args>
+  FOLLY_ERASE T& emplace(Args&&... args) {
+    if (ptr_.get()) {
+      *ptr_.get() = T(std::forward<Args>(args)...);
+    } else {
+      ptr_.set(new T(std::forward<Args>(args)...), Mode::MutOwned);
+    }
+    return *ptr_.get();
   }
 };
 
--- third-party/thrift/src/thrift/lib/cpp2/test/BoxedValuePtrTest.cpp
+++ third-party/thrift/src/thrift/lib/cpp2/test/BoxedValuePtrTest.cpp
@@ -19,73 +19,109 @@
 
 using namespace apache::thrift::detail;
 
-TEST(BoxedValuePtrTest, DefaultConstructor) {
-  boxed_value_ptr<int> a;
+struct TestStruct {
+  TestStruct() : a(0) {}
+  TestStruct(int val) : a(val) {}
+  TestStruct(const TestStruct& other) : a(other.a) {}
+
+  TestStruct& operator=(const TestStruct& other) {
+    TestStruct tmp{other};
+    std::swap(a, tmp.a);
+    return *this;
+  }
+
+  TestStruct& operator=(int val) {
+    TestStruct tmp{val};
+    std::swap(a, tmp.a);
+    return *this;
+  }
+
+  int a;
+};
+
+constexpr bool operator==(const TestStruct& lhs, int rhs) {
+  return lhs.a == rhs;
+}
+
+constexpr bool operator==(int lhs, const TestStruct& rhs) {
+  return lhs == rhs.a;
+}
+
+template <typename T>
+class BoxedTest : public ::testing::Test {};
+
+using BoxedPtrTypes =
+    ::testing::Types<boxed_value_ptr<TestStruct>, boxed_ptr<TestStruct>>;
+
+TYPED_TEST_SUITE(BoxedTest, BoxedPtrTypes);
+
+TYPED_TEST(BoxedTest, DefaultConstructor) {
+  TypeParam a;
   EXPECT_FALSE(static_cast<bool>(a));
 }
 
-TEST(BoxedValuePtrTest, Constructor) {
-  boxed_value_ptr<int> a(5);
+TYPED_TEST(BoxedTest, Constructor) {
+  TypeParam a(5);
   EXPECT_EQ(*a, 5);
 }
 
-TEST(BoxedValuePtrTest, CopyConstructor) {
-  boxed_value_ptr<int> a(5);
-  boxed_value_ptr<int> b(a);
+TYPED_TEST(BoxedTest, CopyConstructor) {
+  TypeParam a(5);
+  TypeParam b(a);
   EXPECT_EQ(*b, 5);
   EXPECT_EQ(*a, 5);
 }
 
-TEST(BoxedValuePtrTest, CopyAssignment) {
-  boxed_value_ptr<int> a(5);
-  boxed_value_ptr<int> b;
+TYPED_TEST(BoxedTest, CopyAssignment) {
+  TypeParam a(5);
+  TypeParam b;
   b = a;
   EXPECT_EQ(*b, 5);
   EXPECT_EQ(*a, 5);
 }
 
-TEST(BoxedValuePtrTest, MoveConstructor) {
-  boxed_value_ptr<int> a(5);
-  boxed_value_ptr<int> b(std::move(a));
+TYPED_TEST(BoxedTest, MoveConstructor) {
+  TypeParam a(5);
+  TypeParam b(std::move(a));
   EXPECT_EQ(*b, 5);
 }
 
-TEST(BoxedValuePtrTest, MoveAssignment) {
-  boxed_value_ptr<int> a(5);
-  boxed_value_ptr<int> b;
+TYPED_TEST(BoxedTest, MoveAssignment) {
+  TypeParam a(5);
+  TypeParam b;
   b = std::move(a);
   EXPECT_EQ(*b, 5);
 }
 
-TEST(BoxedValuePtrTest, EmptyAssignment) {
-  boxed_value_ptr<int> a;
-  boxed_value_ptr<int> b(5);
+TYPED_TEST(BoxedTest, EmptyAssignment) {
+  TypeParam a;
+  TypeParam b(5);
   EXPECT_EQ(*b, 5);
   b = a;
   EXPECT_FALSE(static_cast<bool>(b));
 }
 
-TEST(BoxedValuePtrTest, Emplace) {
-  boxed_value_ptr<int> a;
+TYPED_TEST(BoxedTest, Emplace) {
+  TypeParam a;
   a.emplace(5);
   EXPECT_EQ(*a, 5);
   a.emplace(7);
   EXPECT_EQ(*a, 7);
 }
 
-TEST(LazyPtrRest, Reset) {
-  boxed_value_ptr<int> a(6);
+TYPED_TEST(BoxedTest, Reset) {
+  TypeParam a(6);
   a.reset();
   EXPECT_FALSE(static_cast<bool>(a));
 }
 
-TEST(BoxedValuePtrTest, Assignment) {
-  boxed_value_ptr<int> a;
+TYPED_TEST(BoxedTest, Assignment) {
+  TypeParam a;
   a = 6;
   EXPECT_EQ(*a, 6);
 }
 
-TEST(BoxedValuePtrTest, MoveOnlyType) {
+TYPED_TEST(BoxedTest, MoveOnlyType) {
   boxed_value_ptr<std::unique_ptr<int>> a;
   a = std::make_unique<int>(5);
   EXPECT_EQ(**a, 5);
@@ -93,17 +129,17 @@ TEST(BoxedValuePtrTest, MoveOnlyType) {
   EXPECT_EQ(**b, 5);
 }
 
-TEST(BoxedValuePtrTest, Swap) {
-  boxed_value_ptr<int> a(5);
-  boxed_value_ptr<int> b(7);
+TYPED_TEST(BoxedTest, Swap) {
+  TypeParam a(5);
+  TypeParam b(7);
   std::swap(a, b);
   EXPECT_EQ(*a, 7);
   EXPECT_EQ(*b, 5);
 }
 
-TEST(BoxedValuePtrTest, Equal) {
-  boxed_value_ptr<int> a;
-  boxed_value_ptr<int> b;
+TYPED_TEST(BoxedTest, Equal) {
+  TypeParam a;
+  TypeParam b;
   EXPECT_TRUE(a == b);
   a = 5;
   EXPECT_FALSE(a == b);
@@ -111,4 +147,66 @@ TEST(BoxedValuePtrTest, Equal) {
   EXPECT_FALSE(a == b);
   b = 7;
   EXPECT_FALSE(a == b);
+}
+
+class LifecycleTester {
+ public:
+  using __fbthrift_cpp2_type = std::true_type;
+
+  LifecycleTester() : isACopy_(false), hasBeenCopied_(false) {}
+  explicit LifecycleTester(int) : isACopy_(false), hasBeenCopied_(false) {}
+
+  // NOLINTNEXTLINE(facebook-hte-NonConstCopyContructor)
+  LifecycleTester(LifecycleTester& other)
+      : isACopy_(true), hasBeenCopied_(false) {
+    other.hasBeenCopied_ = true;
+  }
+
+  LifecycleTester& operator=(LifecycleTester& other) {
+    LifecycleTester tmp{other};
+    std::swap(isACopy_, tmp.isACopy_);
+    std::swap(hasBeenCopied_, tmp.hasBeenCopied_);
+    other.hasBeenCopied_ = true;
+    return *this;
+  }
+
+  bool isACopy() const { return isACopy_; }
+
+  bool hasBeenCopied() const { return hasBeenCopied_; }
+
+ private:
+  bool isACopy_;
+  bool hasBeenCopied_;
+};
+
+TEST(LifecycleTest, ConstUnownedCopy) {
+  LifecycleTester lct;
+
+  boxed_ptr<LifecycleTester> boxed1 = &lct;
+  EXPECT_FALSE(boxed1->isACopy());
+  EXPECT_FALSE(boxed1->hasBeenCopied());
+
+  boxed_ptr<LifecycleTester> boxed2 = boxed1;
+  EXPECT_FALSE(boxed1->hasBeenCopied());
+  EXPECT_FALSE(boxed2->isACopy());
+
+  EXPECT_FALSE(lct.hasBeenCopied());
+  EXPECT_EQ(boxed1, boxed2);
+}
+
+TEST(LifecycleTest, MutOwnedRefCopy) {
+  LifecycleTester lct;
+
+  boxed_ptr<LifecycleTester> boxed{&lct};
+  EXPECT_FALSE(boxed->isACopy());
+  EXPECT_TRUE(boxed.mut()->isACopy()); // This mutable ref creates a copy.
+  EXPECT_TRUE(lct.hasBeenCopied());
+}
+
+TEST(LifecycleTest, copyOfMutableRefCreatesCopy) {
+  // Creating the object this way starts with a MutOwned state.
+  boxed_ptr<LifecycleTester> boxed1{0};
+  EXPECT_FALSE(boxed1->hasBeenCopied());
+  auto boxed2 = boxed1;
+  EXPECT_TRUE(boxed1->hasBeenCopied());
 }

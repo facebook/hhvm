81a9747ebcc626f6e7cf364ab47ec0aec7566e68
Aaron Orenstein <aorenste@meta.com>
2022-10-13 08:43:04 -0700

    process stuff
    Summary: To use the runtime options we link against the Process stuff in HHVM.  When building in a non-HHVM context there's just too much stuff in there that we don't need - so convert the class into a namespace (that's really the way it was used anyway) and move the GetHostName() and GetCPUModel() functions into their own files.
    
    Reviewed By: edwinsmith
    
    Differential Revision: D39948455
    
    fbshipit-source-id: 63b451ab5e7b25c844bb88fd759f9fbc30008b6d

--- hphp/util/process.cpp
+++ hphp/util/process.cpp
@@ -14,6 +14,8 @@
    +----------------------------------------------------------------------+
 */
 #include "hphp/util/process.h"
+#include "hphp/util/process-cpu.h"
+#include "hphp/util/process-host.h"
 
 #include <sys/types.h>
 #include <stdlib.h>
@@ -261,61 +263,6 @@ bool Process::GetMemoryInfo(MemInfo& info, bool checkCgroup2) {
   return false;
 }
 
-int Process::GetCPUCount() {
-  return sysconf(_SC_NPROCESSORS_ONLN);
-}
-
-#ifdef __x86_64__
-static __inline void do_cpuid(u_int ax, u_int *p) {
-  asm volatile ("cpuid"
-                : "=a" (p[0]), "=b" (p[1]), "=c" (p[2]), "=d" (p[3])
-                : "0" (ax));
-}
-#elif defined(_M_X64)
-#include <intrin.h>
-static ALWAYS_INLINE void do_cpuid(int func, uint32_t* p) {
-  __cpuid((int*)p, func);
-}
-#endif
-
-std::string Process::GetCPUModel() {
-#if defined(__x86_64__) || defined(_M_X64)
-  uint32_t regs[4];
-  do_cpuid(0, regs);
-
-  const int vendor_size = sizeof(regs[1])*3;
-  std::swap(regs[2], regs[3]);
-  uint32_t cpu_exthigh = 0;
-  if (memcmp(regs + 1, "GenuineIntel", vendor_size) == 0 ||
-      memcmp(regs + 1, "AuthenticAMD", vendor_size) == 0) {
-    do_cpuid(0x80000000, regs);
-    cpu_exthigh = regs[0];
-  }
-
-  char cpu_brand[3 * sizeof(regs) + 1];
-  char *brand = cpu_brand;
-  if (cpu_exthigh >= 0x80000004) {
-    for (u_int i = 0x80000002; i < 0x80000005; i++) {
-      do_cpuid(i, regs);
-      memcpy(brand, regs, sizeof(regs));
-      brand += sizeof(regs);
-    }
-  }
-  *brand = '\0';
-  assert(brand - cpu_brand < sizeof(cpu_brand));
-  return cpu_brand;
-
-#else
-  // On non-x64, fall back to calling uname
-  std::string model = "Unknown ";
-  struct utsname uname_buf;
-  uname(&uname_buf);
-  model.append(uname_buf.machine);
-  return model;
-
-#endif  // __x86_64__
-}
-
 ///////////////////////////////////////////////////////////////////////////////
 
 std::string Process::GetAppName() {
@@ -324,14 +271,6 @@ std::string Process::GetAppName() {
     progname = "unknown program";
   }
   return progname;
-}
-
-std::string Process::GetHostName() {
-  char hostbuf[128];
-  hostbuf[0] = '\0'; // for cleaner valgrind output when gethostname() fails
-  gethostname(hostbuf, sizeof(hostbuf));
-  hostbuf[sizeof(hostbuf) - 1] = '\0';
-  return hostbuf;
 }
 
 std::string Process::GetCurrentUser() {

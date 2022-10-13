8a21ccbfbc7d0482b0389081be11698c156051f4
David Tolnay <dtolnay@meta.com>
2022-10-13 09:04:45 -0700

    Suppress all clippy lints in generated code
    Summary: When writing a code generator, it tends to be more relevant that the code generator is easily understandable and maintainable, than that the generated code is maximally idiomatic. In the clippy warnings shown in the test plan, yes we could make the Thrift templates more complex to sidestep those lints, but that wouldn't be a good tradeoff.
    
    Reviewed By: zertosh
    
    Differential Revision: D40314859
    
    fbshipit-source-id: 45e1a2b2e00689942ce7bd677a7b9a87f8f21a74

--- third-party/thrift/src/thrift/compiler/test/fixtures/inheritance/gen-rust/lib.rs
+++ third-party/thrift/src/thrift/compiler/test/fixtures/inheritance/gen-rust/lib.rs
@@ -2,7 +2,7 @@
 // This file is probably not the place you want to edit!
 
 #![recursion_limit = "100000000"]
-#![allow(non_camel_case_types, non_snake_case, non_upper_case_globals, unused_crate_dependencies, clippy::type_complexity)]
+#![allow(non_camel_case_types, non_snake_case, non_upper_case_globals, unused_crate_dependencies, clippy::all)]
 
 pub use self::errors::*;
 

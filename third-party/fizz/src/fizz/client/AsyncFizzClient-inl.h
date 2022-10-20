b32027e72cd811ec6ab497fc0bb5f58572f4615d
Nick Richardson <richardsonnick@meta.com>
2022-10-20 11:09:49 -0700

    Remove error prone secretAvailable USDT
    Summary:
    Temporarily removes usdt probe that causes error with OSS ld. This works on our lld linker.
    
    https://github.com/facebookincubator/fizz/issues/84
    
    Reviewed By: mingtaoy
    
    Differential Revision: D40523842
    
    fbshipit-source-id: 8b914caa53350e3560c6990dd47d0876319cf274

--- third-party/fizz/src/fizz/client/AsyncFizzClient-inl.h
+++ third-party/fizz/src/fizz/client/AsyncFizzClient-inl.h
@@ -609,6 +609,7 @@ template <typename SM>
 void AsyncFizzClientT<SM>::ActionMoveVisitor::operator()(
     SecretAvailable& secret) {
   client_.secretAvailable(secret.secret);
+#if 0
   FOLLY_SDT(
       fizz,
       fizz_secret_available,
@@ -616,6 +617,7 @@ void AsyncFizzClientT<SM>::ActionMoveVisitor::operator()(
       secret.secret.secret.data(),
       secret.secret.type,
       client_.getClientRandom()->data());
+#endif
 }
 
 template <typename SM>

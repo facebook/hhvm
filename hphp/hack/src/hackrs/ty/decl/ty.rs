8d06d696a416ce3d9fdb3cabc2c0eaa3211e6637
Edwin Smith <smith@meta.com>
2022-10-21 12:52:41 -0700

    Use hackrs Visitor to scrape symbol names from decls
    Summary: This way is less fragile. Also, collect function, constant, and module name references.
    
    Differential Revision: D40559399
    
    fbshipit-source-id: 920d9588137e30df63583dc737a600e07ec16fef

--- hphp/hack/src/hackrs/ty/decl/ty.rs
+++ hphp/hack/src/hackrs/ty/decl/ty.rs
@@ -68,6 +68,11 @@ pub enum XhpEnumValue {
     XEVString(Symbol),
 }
 
+walkable!(XhpEnumValue => {
+    Self::XEVInt(i) => [i],
+    Self::XEVString(s) => [s],
+});
+
 #[derive(Clone, Debug, Eq, EqModuloPos, Hash, PartialEq, Serialize, Deserialize)]
 #[derive(ToOcamlRep, FromOcamlRep)]
 pub enum CeVisibility {
@@ -76,6 +81,13 @@ pub enum CeVisibility {
     Protected(TypeName),
     Internal(ModuleName),
 }
+
+walkable!(CeVisibility => {
+    Self::Public => [],
+    Self::Private(t) => [t],
+    Self::Protected(t) => [t],
+    Self::Internal(m) => [m],
+});
 
 #[derive(Clone, Debug, Eq, EqModuloPos, Hash, PartialEq, Serialize, Deserialize)]
 #[derive(ToOcamlRep, FromOcamlRep)]
@@ -130,6 +142,8 @@ pub struct UserAttribute<P> {
     pub name: Positioned<TypeName, P>,
     pub classname_params: Box<[TypeName]>,
 }
+
+walkable!(impl<R: Reason> for UserAttribute<R::Pos> => [name, classname_params]);
 
 #[derive(Clone, Debug, Eq, EqModuloPos, Hash, PartialEq, Serialize, Deserialize)]
 #[derive(ToOcamlRep, FromOcamlRep)]
@@ -410,7 +424,6 @@ pub struct ClassRefinement<TY> {
     pub types: BTreeMap<TypeConstName, ClassTypeRefinement<TY>>,
 }
 
-walkable!(TypeConstName); // To walk the typeconsts BTreeMap
 walkable!(impl<R: Reason> for ClassRefinement<Ty<R>> => [types]);
 
 /// Type constant refinements

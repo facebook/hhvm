63f057454b3c63e2b574abdc34ce8acd43f57c6d
Yang, Bo <atry@meta.com>
2022-10-24 17:37:21 -0700

    Set proper file permission for /nix/var/sccache

--- hhvm.nix
+++ hhvm.nix
@@ -57,7 +57,7 @@
 , re2
 , re2c
 , rustChannelOf
-, setup-compiler-cache ? "/nix/var/cache/setup-compiler-cache.sh"
+, setup-compiler-cache ? "/nix/var/sccache/setup-compiler-cache.sh"
 , stdenv
 , sqlite
 , tbb

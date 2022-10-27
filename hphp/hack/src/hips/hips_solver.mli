ffdee450b9b4f368e9358ce5cbf8abbd70a37668
Max Heiber <mheiber@meta.com>
2022-10-27 09:20:29 -0700

    smaller API surface
    Summary:
    The HIPS `.mli` exposed functions it didn't need to expose.
    
    Keep the API small so it's easier to understand and evolve.
    
    Reviewed By: madgen
    
    Differential Revision: D40298296
    
    fbshipit-source-id: 0573672896dfb958d54f83ec1a711182a9b2624b

--- hphp/hack/src/hips/hips_solver.mli
+++ hphp/hack/src/hips/hips_solver.mli
@@ -11,36 +11,16 @@ open Hips_types
 (** Generates an inter-procedural constraint solver from domain-specific
     intra-procedural data. *)
 module Inter (I : Intra) : sig
-  (** Inter-procedural constraint type *)
-  type inter_constraint = I.inter_constraint
-
-  (** Intra-procedural constraint type *)
-  type intra_constraint = I.intra_constraint
-
-  (** Union of inter- and intra-procedural constraint types *)
-  type any_constraint = I.any_constraint
-
   (** Output type of analyse: applying substitution repeatedly either
       converges, or diverges *)
   type solution =
-    | Divergent of any_constraint list SMap.t
-    | Convergent of any_constraint list SMap.t
-
-  (** Verifies whether two dictionaries with constraint lists as values are
-      equivalent *)
-  val equiv : any_constraint list SMap.t -> any_constraint list SMap.t -> bool
-
-  (** Substitutes the inter-procedural constraints of the second argument with
-      respect to constraints in the first argument *)
-  val substitute :
-    base_constraint_map:any_constraint list SMap.t ->
-    any_constraint list SMap.t ->
-    any_constraint list SMap.t
+    | Divergent of I.any_constraint list SMap.t
+    | Convergent of I.any_constraint list SMap.t
 
   (** The call "analyse base" repeatedly applies constraint substitution with
       respect to "base" postcomposed with constraint deduction, beginning with
       "base" itself. It either finds a fixpoint, in which case it outputs
       "Convergent fp", or terminates early, in which case it outputs
       "Divergent p". *)
-  val analyse : any_constraint list SMap.t -> solution
+  val analyse : I.any_constraint list SMap.t -> solution
 end

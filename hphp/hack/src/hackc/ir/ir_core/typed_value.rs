245b0775a8197e184a0c97734f319e26e15a9786
Aaron Orenstein <aorenste@meta.com>
2022-10-31 07:44:48 -0700

    Fix TypedValue PartialEq
    Summary:
    I had thought that Index(Set|Map)::<PartialEq>::eq() was implemented to respect insertion order but it turns out I was wrong.
    
    This implements PartialEq for TypedValue so that Keyset and Dict comparisons respect insertion order.
    
    Reviewed By: edwinsmith
    
    Differential Revision: D40824891
    
    fbshipit-source-id: c55751d80e3aebbbd7dd6918b879eb8a6c5722f2

--- hphp/hack/src/hackc/ir/ir_core/typed_value.rs
+++ hphp/hack/src/hackc/ir/ir_core/typed_value.rs
@@ -9,7 +9,7 @@ pub use hhbc::FloatBits;
 
 use crate::UnitBytesId;
 
-#[derive(Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub enum TypedValue {
     Uninit,
     Int(i64),
@@ -19,41 +19,8 @@ pub enum TypedValue {
     LazyClass(UnitBytesId),
     Null,
     Vec(Vec<TypedValue>),
-    Keyset(IndexSet<ArrayKey>),
-    Dict(IndexMap<ArrayKey, TypedValue>),
-}
-
-#[allow(clippy::derive_hash_xor_eq)]
-impl std::hash::Hash for TypedValue {
-    fn hash<H: std::hash::Hasher>(&self, h: &mut H) {
-        use std::hash::Hash;
-        Hash::hash(&std::mem::discriminant(self), h);
-        match self {
-            TypedValue::Uninit => {}
-            TypedValue::Int(i) => i.hash(h),
-            TypedValue::Bool(b) => b.hash(h),
-            TypedValue::Float(fb) => fb.hash(h),
-            TypedValue::String(id) | TypedValue::LazyClass(id) => id.hash(h),
-            TypedValue::Null => {}
-            TypedValue::Vec(c) => c.hash(h),
-            TypedValue::Keyset(c) => {
-                // IndexSet doesn't implement Hash so we need to provide our
-                // own. It needs to match IndexSet::eq() which uses insertion
-                // ordering so we'll do the same.
-                for v in c.iter() {
-                    v.hash(h);
-                }
-            }
-            TypedValue::Dict(c) => {
-                // IndexMap doesn't implement Hash so we need to provide our
-                // own. It needs to match IndexSet::eq() which uses insertion
-                // ordering so we'll do the same.
-                for e in c.iter() {
-                    e.hash(h);
-                }
-            }
-        }
-    }
+    Keyset(KeysetValue),
+    Dict(DictValue),
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
@@ -61,4 +28,178 @@ pub enum ArrayKey {
     Int(i64),
     String(UnitBytesId),
     LazyClass(UnitBytesId),
+}
+
+/// A wrapper around IndexSet<ArrayKey> which includes key ordering for
+/// comparisons and provides hash.
+#[derive(Clone, Debug, Default)]
+pub struct KeysetValue(pub IndexSet<ArrayKey>);
+
+impl std::ops::Deref for KeysetValue {
+    type Target = IndexSet<ArrayKey>;
+
+    fn deref(&self) -> &IndexSet<ArrayKey> {
+        &self.0
+    }
+}
+
+impl std::cmp::PartialEq for KeysetValue {
+    fn eq(&self, other: &KeysetValue) -> bool {
+        // IndexSet implements PartialEq but it ignores insertion order,
+        // which we care about.
+        self.len() == other.len() && self.iter().zip(other.iter()).all(|(a, b)| a == b)
+    }
+}
+
+impl std::cmp::Eq for KeysetValue {}
+
+impl std::hash::Hash for KeysetValue {
+    fn hash<H: std::hash::Hasher>(&self, h: &mut H) {
+        // IndexSet doesn't implement Hash so we need to provide our
+        // own. It needs to match TypedValue::eq() which uses insertion
+        // ordering so we'll do the same.
+        for v in self.iter() {
+            v.hash(h);
+        }
+    }
+}
+
+impl FromIterator<ArrayKey> for KeysetValue {
+    fn from_iter<T>(iterator: T) -> Self
+    where
+        T: IntoIterator<Item = ArrayKey>,
+    {
+        Self(IndexSet::from_iter(iterator))
+    }
+}
+
+/// A wrapper around IndexMap<ArrayKey, TypedValue> which includes key ordering
+/// for comparisons and provides hash.
+#[derive(Clone, Debug, Default)]
+pub struct DictValue(pub IndexMap<ArrayKey, TypedValue>);
+
+impl std::ops::Deref for DictValue {
+    type Target = IndexMap<ArrayKey, TypedValue>;
+
+    fn deref(&self) -> &IndexMap<ArrayKey, TypedValue> {
+        &self.0
+    }
+}
+
+impl std::cmp::PartialEq for DictValue {
+    fn eq(&self, other: &DictValue) -> bool {
+        // IndexMap implements PartialEq but it ignores insertion order,
+        // which we care about.
+        self.len() == other.len() && self.iter().zip(other.iter()).all(|(a, b)| a == b)
+    }
+}
+
+impl std::cmp::Eq for DictValue {}
+
+impl std::hash::Hash for DictValue {
+    fn hash<H: std::hash::Hasher>(&self, h: &mut H) {
+        // IndexMap doesn't implement Hash so we need to provide our
+        // own. It needs to match TypedValue::eq() which uses insertion
+        // ordering so we'll do the same.
+        for e in self.iter() {
+            e.hash(h);
+        }
+    }
+}
+
+impl FromIterator<(ArrayKey, TypedValue)> for DictValue {
+    fn from_iter<T>(iterator: T) -> Self
+    where
+        T: IntoIterator<Item = (ArrayKey, TypedValue)>,
+    {
+        Self(IndexMap::from_iter(iterator))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::hash::BuildHasher;
+
+    use super::*;
+    use crate::StringInterner;
+
+    #[test]
+    fn test1() {
+        let a = TypedValue::Keyset(
+            [ArrayKey::Int(1), ArrayKey::Int(2), ArrayKey::Int(3)]
+                .into_iter()
+                .collect(),
+        );
+        let b = TypedValue::Keyset(
+            [ArrayKey::Int(3), ArrayKey::Int(2), ArrayKey::Int(1)]
+                .into_iter()
+                .collect(),
+        );
+        assert_eq!(a, a);
+        assert_ne!(a, b);
+        let h = hash::BuildHasher::default();
+        assert_ne!(h.hash_one(a), h.hash_one(b));
+    }
+
+    #[test]
+    fn test2() {
+        let a = TypedValue::Dict(
+            [
+                (ArrayKey::Int(1), TypedValue::Int(10)),
+                (ArrayKey::Int(2), TypedValue::Int(11)),
+                (ArrayKey::Int(3), TypedValue::Int(12)),
+            ]
+            .into_iter()
+            .collect(),
+        );
+        let b = TypedValue::Dict(
+            [
+                (ArrayKey::Int(3), TypedValue::Int(12)),
+                (ArrayKey::Int(2), TypedValue::Int(11)),
+                (ArrayKey::Int(1), TypedValue::Int(10)),
+            ]
+            .into_iter()
+            .collect(),
+        );
+        assert_eq!(a, a);
+        assert_ne!(a, b);
+        let h = hash::BuildHasher::default();
+        assert_ne!(h.hash_one(a), h.hash_one(b));
+    }
+
+    #[test]
+    fn test3() {
+        let mut strings = StringInterner::default();
+        let a = TypedValue::Dict(
+            [
+                (
+                    ArrayKey::String(strings.intern_str("E")),
+                    TypedValue::Int(1),
+                ),
+                (
+                    ArrayKey::String(strings.intern_str("F")),
+                    TypedValue::Int(1),
+                ),
+                (
+                    ArrayKey::String(strings.intern_str("G")),
+                    TypedValue::Int(1),
+                ),
+                (
+                    ArrayKey::String(strings.intern_str("H")),
+                    TypedValue::Int(1),
+                ),
+                (
+                    ArrayKey::String(strings.intern_str("I")),
+                    TypedValue::Int(1),
+                ),
+            ]
+            .into_iter()
+            .collect(),
+        );
+        let b = TypedValue::Dict(Default::default());
+        assert_eq!(a, a);
+        assert_ne!(a, b);
+        let h = hash::BuildHasher::default();
+        assert_ne!(h.hash_one(a), h.hash_one(b));
+    }
 }

7c1a2e6cbd3513807844f4b09793653327382830
Bahadir Danisik <bahadird@meta.com>
2022-11-07 13:41:59 -0800

    Rocket client fix
    Summary:
    Following issues are fixed in rsocket implementation.
    1. Client timeout and queue timeout settings are send to the server with RpcOption
    2. Internal server errors are handled in java rocket client. Currently we read the incoming compact serialized ResponseRpcError struct as string, this is raised as a TApplicationException with UNKNOWN status.
    
    Reviewed By: robertroeser
    
    Differential Revision: D41084038
    
    fbshipit-source-id: 2072b167a6ce1e4d30c532266fd56bcc784b86ab

--- third-party/thrift/src/thrift/lib/java/runtime/src/main/java/com/facebook/thrift/rsocket/client/RSocketRpcClient.java
+++ third-party/thrift/src/thrift/lib/java/runtime/src/main/java/com/facebook/thrift/rsocket/client/RSocketRpcClient.java
@@ -32,17 +32,24 @@ import com.facebook.thrift.protocol.TProtocolType;
 import com.facebook.thrift.util.RpcClientUtils;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.Unpooled;
 import io.rsocket.Payload;
 import io.rsocket.RSocket;
+import io.rsocket.RSocketErrorException;
+import io.rsocket.frame.ErrorFrameCodec;
 import io.rsocket.util.ByteBufPayload;
 import java.util.Optional;
 import java.util.function.Function;
 import org.apache.thrift.ClientPushMetadata;
 import org.apache.thrift.ProtocolId;
+import org.apache.thrift.RequestRpcMetadata;
+import org.apache.thrift.ResponseRpcError;
+import org.apache.thrift.ResponseRpcErrorCode;
 import org.apache.thrift.ResponseRpcMetadata;
 import org.apache.thrift.StreamPayloadMetadata;
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TStruct;
+import org.apache.thrift.transport.TTransportException;
 import org.reactivestreams.Publisher;
 import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
@@ -69,6 +76,27 @@ public final class RSocketRpcClient implements RpcClient {
     rsocket.dispose();
   }
 
+  private boolean hasTimeoutSettings(RpcOptions options) {
+    if (options == null) {
+      return false;
+    }
+    return options.getClientTimeoutMs() != null || options.getQueueTimeoutMs() != null;
+  }
+
+  private RequestRpcMetadata createMetadataWithTimeouts(
+      RequestRpcMetadata metadata, RpcOptions options) {
+    int clientTimeoutMs = options.getClientTimeoutMs() == null ? 0 : options.getClientTimeoutMs();
+    int queueTimeoutMs = options.getQueueTimeoutMs() == null ? 0 : options.getQueueTimeoutMs();
+    RequestRpcMetadata.Builder builder = new RequestRpcMetadata.Builder(metadata);
+    if (clientTimeoutMs != 0) {
+      builder.setClientTimeoutMs(clientTimeoutMs);
+    }
+    if (queueTimeoutMs != 0) {
+      builder.setQueueTimeoutMs(queueTimeoutMs);
+    }
+    return builder.build();
+  }
+
   @Override
   public <T> Mono<ClientResponsePayload<T>> singleRequestSingleResponse(
       ClientRequestPayload<T> payload, RpcOptions options) {
@@ -77,16 +105,61 @@ public final class RSocketRpcClient implements RpcClient {
       final TProtocolType protocolType = TProtocolType.fromProtocolId(protocol);
 
       return rsocket
-          .requestResponse(clientRequestPayloadToRSocketPayload(payload, protocolType))
+          .requestResponse(clientRequestPayloadToRSocketPayload(payload, protocolType, options))
+          .map(response -> rsocketPayloadToClientResponsePayload(payload, response, protocolType))
           .onErrorResume(
-              t ->
-                  Mono.just(
-                      ByteBufPayload.create(
-                          getExceptionString(t, payload.getRequestRpcMetadata().getName()))))
-          .map(response -> rsocketPayloadToClientResponsePayload(payload, response, protocolType));
+              t -> {
+                if (isInternalError(t)) {
+                  return Mono.just(getErrorFrame(t));
+                }
+                return Mono.just(
+                    rsocketPayloadToClientResponsePayload(
+                        payload,
+                        ByteBufPayload.create(
+                            getExceptionString(t, payload.getRequestRpcMetadata().getName())),
+                        protocolType));
+              });
     } catch (Throwable t) {
       return Mono.error(t);
     }
+  }
+
+  private boolean isInternalError(Throwable t) {
+    if (t instanceof RSocketErrorException) {
+      int code = ((RSocketErrorException) t).errorCode();
+
+      if (code >= ErrorFrameCodec.REJECTED && code <= ErrorFrameCodec.INVALID) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private int getType(ResponseRpcErrorCode code) {
+    switch (code) {
+      case TASK_EXPIRED:
+        return TTransportException.TIMED_OUT;
+      default:
+        return TTransportException.UNKNOWN;
+    }
+  }
+
+  /**
+   * INVALID, CANCELLED and REJECTED rsocket error codes are defined as internal server error in
+   * rocket protocol. Payload will be compact protocol serialized ResponseRpcError struct.
+   *
+   * @param t Throwable contains rsocket error code
+   * @return ClientResponsePayload
+   * @param <T>
+   */
+  private <T> ClientResponsePayload<T> getErrorFrame(Throwable t) {
+    byte[] bytes = t.getMessage().getBytes();
+    ByteBuf byteBuf = Unpooled.wrappedBuffer(bytes);
+    ByteBufTProtocol protocol = TProtocolType.TCompact.apply(byteBuf);
+    ResponseRpcError err = ResponseRpcError.read0(protocol);
+    return ClientResponsePayload.createException(
+        new TTransportException(getType(err.getCode()), t.getMessage()), null, null, false);
   }
 
   @Override
@@ -96,7 +169,8 @@ public final class RSocketRpcClient implements RpcClient {
       final ProtocolId protocol = payload.getRequestRpcMetadata().getProtocol();
       final TProtocolType protocolType = TProtocolType.fromProtocolId(protocol);
 
-      return rsocket.fireAndForget(clientRequestPayloadToRSocketPayload(payload, protocolType));
+      return rsocket.fireAndForget(
+          clientRequestPayloadToRSocketPayload(payload, protocolType, options));
     } catch (Throwable t) {
       return Mono.error(t);
     }
@@ -110,7 +184,7 @@ public final class RSocketRpcClient implements RpcClient {
       final TProtocolType protocolType = TProtocolType.fromProtocolId(protocol);
 
       return rsocket
-          .requestStream(clientRequestPayloadToRSocketPayload(payload, protocolType))
+          .requestStream(clientRequestPayloadToRSocketPayload(payload, protocolType, options))
           .onErrorResume(
               t ->
                   Flux.just(
@@ -136,7 +210,7 @@ public final class RSocketRpcClient implements RpcClient {
                   final TProtocolType protocolType = TProtocolType.fromProtocolId(protocol);
 
                   final Flux<Payload> payloadFlux =
-                      flux.map(t -> clientRequestPayloadToRSocketPayload(t, protocolType));
+                      flux.map(t -> clientRequestPayloadToRSocketPayload(t, protocolType, options));
 
                   Flux<Payload> requestChannel = rsocket.requestChannel(payloadFlux);
 
@@ -170,7 +244,7 @@ public final class RSocketRpcClient implements RpcClient {
   }
 
   private <T> Payload clientRequestPayloadToRSocketPayload(
-      ClientRequestPayload<T> payload, TProtocolType protocolType) {
+      ClientRequestPayload<T> payload, TProtocolType protocolType, RpcOptions options) {
     ByteBuf data = null;
     ByteBuf metadata = null;
     try {
@@ -187,7 +261,12 @@ public final class RSocketRpcClient implements RpcClient {
       in.writeStructEnd();
 
       final ByteBufTProtocol metadataProtocol = TProtocolType.TCompact.apply(metadata);
-      payload.getRequestRpcMetadata().write0(metadataProtocol);
+      if (hasTimeoutSettings(options)) {
+        createMetadataWithTimeouts(payload.getRequestRpcMetadata(), options)
+            .write0(metadataProtocol);
+      } else {
+        payload.getRequestRpcMetadata().write0(metadataProtocol);
+      }
 
       return createPayload(alloc, payload.getRequestRpcMetadata().getCompression(), data, metadata);
 

7eaba813828045d46efb3773888cd7a5677ea3e2
Michael Thomas <mjt@meta.com>
2022-11-08 03:52:51 -0800

    Add tests for miscellaneous validation performed during hint elaboration
    Summary:
    Adds tests for miscellaneous validation performed during hint elaboration, includin:
    - invalid xhp classnames
    - invalid use of intenal classes (for enum classes)
    - function parameter repetition
    
    Reviewed By: andrewjkennedy
    
    Differential Revision: D40893528
    
    fbshipit-source-id: 63c90c853045209d62f4b6fbb0943bce7d26fd11

---
+++ hphp/hack/test/nast/invalid_enum_class.php
@@ -0,0 +1,7 @@
+<?hh
+
+function invalid_enum_class_1(\HH\BuiltinEnum<int> $x): void {}
+
+function invalid_enum_class_2(\HH\BuiltinEnumClass<int> $x): void {}
+
+function invalid_enum_class_3(\HH\BuiltinAbstractEnumClass $x): void {}
---
+++ hphp/hack/test/nast/invalid_xhp.php
@@ -0,0 +1,7 @@
+<?hh
+
+function invalid_xhp_1(Xhp::T $x): void {}
+
+function invalid_xhp_2(:Xhp::T $x): void {}
+
+function invalid_xhp_3(XHP::T $x): void {}

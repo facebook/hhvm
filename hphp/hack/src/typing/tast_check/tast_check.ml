e06fc42d8fd1cb514f89298cf03b96cb026012b6
Mistral Contrastin <mojpc2@meta.com>
2022-11-11 03:15:22 -0800

    Refactor TAST check registration
    Summary:
    TAST check registration logic was a bit messy. I did some behaviour preserving refactoring.
    
    List of changes:
    - Take logger handlers out of the local definition
    - Introduce a conditional adder for hierarchy checks to reduce `if-then-else` clutter
    - Take out global read/write enabling logic and make the conditionals more straightforward
    
    Reviewed By: andrewjkennedy
    
    Differential Revision: D41182791
    
    fbshipit-source-id: 32d243054fea7f5c976b04870d930c52c655a5ff

--- hphp/hack/src/typing/tast_check/tast_check.ml
+++ hphp/hack/src/typing/tast_check/tast_check.ml
@@ -13,55 +13,59 @@ open Hh_prelude
 
 [@@@warning "+33"]
 
+(* Handlers that are enabled through 'log_levels' configuration. *)
+let logger_handlers ctx =
+  let tco = Provider_context.get_tcopt ctx in
+  let log_levels = TypecheckerOptions.log_levels tco in
+  let add_handler handlers (key, handler) =
+    match SMap.find_opt key log_levels with
+    | Some level when level > 0 -> handler :: handlers
+    | _ -> handlers
+  in
+  let key_handler_pairs =
+    [
+      ("tany", Bad_type_logger.handler);
+      ("shape_analysis", Shape_analysis_logger.handler);
+    ]
+  in
+  List.fold ~init:[] ~f:add_handler key_handler_pairs
+
+(* Global access check is turned on only if certain files or functions are
+   enabled for checking global writes or global reads. *)
+let is_global_access_check_enabled ctx =
+  let open TypecheckerOptions in
+  let tcopt = Provider_context.get_tcopt ctx in
+  let check_write = global_access_check_on_write tcopt in
+  let check_read = global_access_check_on_read tcopt in
+  let files_enabled =
+    not @@ List.is_empty (global_access_check_files_enabled tcopt)
+  in
+  let function_enabled =
+    not @@ SSet.is_empty (global_access_check_functions_enabled tcopt)
+  in
+  (check_write || check_read) && (files_enabled || function_enabled)
+
 let visitor ctx =
-  let makers =
-    [Xhp_required_check.make_handler; Redundant_generics_check.make_handler]
-  in
-  let handlers = List.map makers ~f:(( |> ) ctx) |> List.filter_opt in
-  (* Handlers in effect only when a typechecker option is set and is not part
-     of standard typechecker operation. *)
-  let irregular_handlers =
-    let tco = Provider_context.get_tcopt ctx in
-    let log_levels = TypecheckerOptions.log_levels tco in
-    let add_handler handlers (key, handler) =
-      match SMap.find_opt key log_levels with
-      | Some level when level > 0 -> handler :: handlers
-      | _ -> handlers
-    in
-    let key_handler_pairs =
-      [
-        ("tany", Bad_type_logger.handler);
-        ("shape_analysis", Shape_analysis_logger.handler);
-      ]
-    in
-    List.fold ~init:[] ~f:add_handler key_handler_pairs
-  in
+  (* Handlers that are not TAST checks to produce errors, but are used for
+     telemetry that processes TASTs. *)
+  let irregular_handlers = logger_handlers ctx in
   let skip_hierarchy_checks =
     TypecheckerOptions.skip_hierarchy_checks (Provider_context.get_tcopt ctx)
   in
-  (* Global access check is turned on only if certain files or functions are enabled
-     for checking global writes or global reads. *)
-  let is_global_access_check_enabled =
-    let tcopt = Provider_context.get_tcopt ctx in
-    let check_write = TypecheckerOptions.global_access_check_on_write tcopt in
-    let check_read = TypecheckerOptions.global_access_check_on_read tcopt in
-    let files_enabled =
-      List.length (TypecheckerOptions.global_access_check_files_enabled tcopt)
-      > 0
-    in
-    let function_enabled =
-      SSet.cardinal
-        (TypecheckerOptions.global_access_check_functions_enabled tcopt)
-      > 0
-    in
-    (check_write || check_read) && (files_enabled || function_enabled)
+  let hierarchy_check handler =
+    if skip_hierarchy_checks then
+      None
+    else
+      Some handler
   in
+  let is_global_access_check_enabled = is_global_access_check_enabled ctx in
   let handlers =
-    handlers
-    @ irregular_handlers
+    irregular_handlers
     @ List.filter_map
         ~f:Fn.id
         [
+          Xhp_required_check.make_handler ctx;
+          Redundant_generics_check.make_handler ctx;
           Some Shape_field_check.handler;
           Some String_cast_check.handler;
           Some Tautology_check.handler;
@@ -79,37 +83,25 @@ let visitor ctx =
           Some Instantiability_check.handler;
           Some Static_memoized_check.handler;
           Some Abstract_class_check.handler;
-          (if skip_hierarchy_checks then
-            None
-          else
-            Some Class_parent_check.handler);
+          hierarchy_check Class_parent_check.handler;
           Some Method_type_param_check.handler;
           Some Obj_get_check.handler;
           Some This_hint_check.handler;
           Some Unresolved_type_variable_check.handler;
           Some Type_const_check.handler;
           Some Static_method_generics_check.handler;
-          (if skip_hierarchy_checks then
-            None
-          else
-            Some Class_inherited_member_case_check.handler);
+          hierarchy_check Class_inherited_member_case_check.handler;
           Some Ifc_tast_check.handler;
           Some Readonly_check.handler;
           Some Meth_caller_check.handler;
           Some Expression_tree_check.handler;
-          (if skip_hierarchy_checks then
-            None
-          else
-            Some Class_const_origin_check.handler);
+          hierarchy_check Class_const_origin_check.handler;
           Some Enum_classes_check.handler;
           (if is_global_access_check_enabled then
             Some Global_access_check.handler
           else
             None);
-          (if skip_hierarchy_checks then
-            None
-          else
-            Some Enum_check.handler);
+          hierarchy_check Enum_check.handler;
         ]
   in
   let handlers =

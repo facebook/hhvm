655caf1846ff7eedad38cbccb91d60c8f8731109
Sai Nagabhairava <sainagabhairava@meta.com>
2023-01-03 20:20:12 -0800

    Propagate ThriftFrameworkMetadata
    Summary: Propagate ThriftFrameworkMetadata through Cpp2ConnContext.
    
    Reviewed By: iahs
    
    Differential Revision: D41723176
    
    fbshipit-source-id: 4003db43bbff5f00d9292c5a98011ce363c42741

--- third-party/thrift/src/thrift/lib/cpp2/server/Cpp2ConnContext.h
+++ third-party/thrift/src/thrift/lib/cpp2/server/Cpp2ConnContext.h
@@ -572,6 +572,12 @@ class Cpp2RequestContext : public apache::thrift::server::TConnectionContext {
 
   int64_t getInteractionId() { return interactionId_; }
 
+  void setFrameworkMetadata(folly::IOBuf frameworkMetadata) {
+    frameworkMetadata_ = std::move(frameworkMetadata);
+  }
+
+  folly::IOBuf& getFrameworkMetadata() { return frameworkMetadata_; }
+
   void setInteractionCreate(InteractionCreate interactionCreate) {
     interactionCreate_ = std::move(interactionCreate);
   }
@@ -617,6 +623,7 @@ class Cpp2RequestContext : public apache::thrift::server::TConnectionContext {
   TilePtr tile_;
   concurrency::ThreadManager::ExecutionScope executionScope_{
       concurrency::PRIORITY::NORMAL};
+  folly::IOBuf frameworkMetadata_;
 };
 
 } // namespace thrift

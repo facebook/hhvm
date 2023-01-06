e642cde713bf7f6f7966f80b4b7b729dd51e35a7
Stuart Clark <stuclar@meta.com>
2023-01-06 05:45:08 -0800

    Fix RootRoute::doRoute
    Summary: As above
    
    Differential Revision: D42381064
    
    fbshipit-source-id: c3eb02cd0b2a7121bbfeec96859ee58a894d21a5

--- third-party/mcrouter/src/mcrouter/routes/RootRoute.h
+++ third-party/mcrouter/src/mcrouter/routes/RootRoute.h
@@ -153,7 +153,8 @@ class RootRoute {
     }
     if (!rh.empty()) {
       auto reqCopy = std::make_shared<const Request>(req);
-      for (const auto& r : rh) {
+      for (size_t i = 1, e = rh.size(); i < e; ++i) {
+        auto r = rh[i];
         folly::fibers::addTask([r, reqCopy]() { r->route(*reqCopy); });
       }
       return rh[0]->route(req);

c8e467f29c41a982300a19969c109d3503009391
Julien Chaffraix <jchaffraix@slack-corp.com>
2023-01-13 10:04:36 -0800

    Add a GitHub Action to build Ubuntu 20.04 (#9306)
    Summary:
    This PR ports the scripts in
    github.com/hhvm/packaging over to
    GitHub Actions so we can build the HHVM
    package from GitHub.
    
    We run the build script in a container as
    the "build-depends` Debian package triggers
    some conflict with the packages included
    in the default GitHub image.
    
    The only modification to the packaging
    scripts are in `make-debianish-package`:
    1. The `$IS_NIGHTLY` checks were made consistent as it caused compilation errors.
    2. We now fetch the full repository instead of using a source archive.
    3. [minor] Simplified the `BUILD_DEPS_{OLD|NEW}` that was unnecessary.
    4. [minor] Several variables are derived from the enviroment (`$DISTRO_ROOT`, `$OUT`, ...) instead
    of being hardcoded.
    
    This PR doesn't compile as the default GitHub runners only have 7 Gb of memory and thus run out of memory: https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners#supported-runners-and-hardware-resources But I have tested the steps in isolation so it should work with larger runners.
    
    Pull Request resolved: https://github.com/facebook/hhvm/pull/9306
    
    Reviewed By: viratyosin
    
    Differential Revision: D42141191
    
    Pulled By: Wilfred
    
    fbshipit-source-id: 05b9381aaf5ee0d7ab8017e3f80be87f2e6bb90c

---
+++ ci/skeleton/usr/share/hhvm/uninstall_fastcgi.sh
@@ -0,0 +1,219 @@
+#!/bin/bash
+
+apache_check_installed() {
+	echo "Checking if Apache is installed"
+	if [ \( ! -d /etc/apache2/mods-enabled \) -o \( ! -d /etc/apache2/mods-available \) ]
+	then
+		echo "WARNING: Couldn't find Apache2 configuration paths, not configuring"
+		return 1
+	fi
+	echo "Detected Apache installation"
+	return 0
+}
+
+apache_check_custom() {
+	echo "Looking for custom proxy configuration"
+	if [ ! -z "$(grep 9000 -R /etc/apache2/mods-enabled | grep ProxyPass)" ]
+	then
+		echo "WARNING: Clashing existing configuration detected, not configuring"
+		return 0
+	fi
+	echo "No custom proxy configuration found"
+	return 1
+}
+
+apache_check_module() {
+	echo "Checking for ${2} ${1}.${3}"
+	if [ ! -f "/etc/apache2/mods-${2}/${1}.${3}" ]
+	then
+		echo "Not found"
+		return 1
+	fi
+	echo "Found, checking for loading directives"
+	if [ "$3" == "load" ]
+	then
+		result=$(grep -E "^[^#]*LoadModule\\s*${1}_module" "/etc/apache2/mods-${2}/${1}.${3}")
+ 		if [ -z "$result" ]
+		then
+			"Loading directive not found"
+			return 1
+		fi
+	fi
+	echo "Detected ${2} ${1}.${3} configuration, setting up integration"
+	return 0
+}
+
+apache_force_enable_module() {
+	echo "Force enabling module ${1}.${2}"
+	if [ ! -f "/etc/apache2/mods-available/${1}.${2}" ]
+	then
+		echo "WARNING: Unsupported ${1}, not configuring"
+		return 1
+	fi
+	echo "Available module found"
+	rm -f "/etc/apache2/mods-enabled/${1}.${2}"
+	echo "Removed possible duplicates"
+	apache_enable_module $1 $2
+	echo "Completed force enabling"
+	return 0
+}
+
+apache_enable_module() {
+	echo "Enabling module ${1}.${2}"
+	if [ ! -f "/etc/apache2/mods-available/${1}.${2}" ]
+	then
+		echo "WARNING: Could not enable ${1}, not configuring"
+		return 1
+	fi
+	echo "Found available module"
+	if [ -f "/etc/apache2/mods-enabled/${1}.${2}" ]
+	then
+		echo "Module already enabled"
+		return 0
+	fi
+	echo "Creating a symlink"
+	ln -s "../mods-available/${1}.${2}" "/etc/apache2/mods-enabled/${1}.${2}"
+	echo "Finished creating a symlink"
+	return 0
+}
+
+apache_disable_module() {
+	echo "Disabling a module ${1}.${2}"
+	rm -f "/etc/apache2/mods-enabled/${1}.${2}"
+	echo "Finished disabling a module"
+}
+
+apache_restart() {
+	echo "Restarting apache"
+	if [ ! -f "/etc/init.d/apache2" ]
+	then
+		echo "Apache init.d script not detected"
+		return 0
+	fi
+	result=$(/etc/init.d/apache2 status | grep 'is running')
+	if [ ! -z "$result" ]
+	then
+		echo "Apache is running, restarting"
+		/etc/init.d/apache2 restart
+		echo "Finished restarting"
+	fi
+	echo "Finished restarting apache"
+	return 0
+}
+
+#!/bin/bash
+
+apache_prerm() {
+	if ! apache_check_installed
+	then
+		return 0
+	fi
+
+	names="proxy_fcgi fastcgi fcgid"
+
+	for name in $names
+	do
+		apache_disable_module "hhvm_${name}" "conf"
+	done
+
+	apache_restart
+	return 0
+}
+
+apache_prerm
+
+#!/bin/bash
+
+insert_line() {
+	file=`mktemp`
+	sed -E "s/^(\\s*)${2}$/&\\n\\1${3}/" $1 > $file
+	cat $file > $1
+}
+
+remove_line() {
+	file=`mktemp`
+	sed -E "/^.*${2}.*$/d" $1 > $file
+	cat $file > $1
+}
+
+check_line() {
+	if [ -z "$(grep $2 $1)" ]
+	then
+		return 0
+	fi
+	return 1
+}
+
+nginx_check_installed() {
+	echo "Checking if Nginx is installed"
+	if [ -d "/etc/nginx/conf.d/" ]
+	then
+		echo "Detected Nginx installation"
+		return 0
+	fi
+	echo "Nginx not found"
+	return 1
+}
+
+nginx_check_custom() {
+	echo "Checking for custom Nginx configuration"
+	if [ ! \( -z "$(grep -E "^[^#]*(fastcgi|hhvm.conf)" "/etc/nginx/nginx.conf")" \) \
+	  -o ! \( -z "$(grep -ER "^[^#]*(fastcgi|hhvm.conf)" "/etc/nginx/conf.d/")" \) \
+          -o ! \( -z "$(grep -ER "^[^#]*(fastcgi|hhvm.conf)" "/etc/nginx/sites-enabled/")" \) ]
+	then
+		echo "WARNING: Detected clashing configuration, not configuring"
+		return 0
+	fi
+	return 1
+}
+
+nginx_enable_module() {
+	echo "Enabling hhvm Nginx module"
+	insert_line "/etc/nginx/sites-enabled/default" 'server_name.*$' 'include hhvm.conf;'
+	echo "Finished enabling module"
+}
+
+nginx_disable_module() {
+	echo "Disabling hhvm Nginx module"
+	remove_line "/etc/nginx/sites-enabled/default" "hhvm.conf"
+	echo "Finished disabling module"
+}
+
+nginx_restart() {
+	echo "Restarting Nginx"
+	if [ ! -f "/etc/init.d/nginx" ]
+	then
+		echo "Nginx init.d script not found"
+		return 0
+	fi
+	result=$(/etc/init.d/nginx status | grep 'is running')
+	if [ ! -z "$result" ]
+	then
+		echo "Nginx is running, restarting"
+		/etc/init.d/nginx stop
+		/etc/init.d/nginx start
+		echo "Restarted nginx"
+	fi
+	echo "Finished restarting Nginx"
+}
+
+#!/bin/bash
+
+nginx_prerm() {
+	if ! nginx_check_installed
+	then
+		return 0
+	fi
+	nginx_disable_module
+}
+
+nginx_prerm
+
+#!/bin/bash
+
+set -e 
+
+if [ -f /etc/init.d/hhvm ]
+then
+	/etc/init.d/hhvm stop
+fi

ab37bf5f97b3493e1b81e22ad2eeaf4ae1078efb
Edwin Smith <smith@meta.com>
2023-01-14 13:26:50 -0800

    Use correct case for type_structure_non_existant_class
    Summary:
    In certain error conditions, type_structure builtin functions can return
    type structures with a sentinel type named
    `HH\__internal\\type_structure_non_existant_class`.
    
    When doing so, let's use the correct namespace case.
    
    Reviewed By: ricklavoie
    
    Differential Revision: D42519486
    
    fbshipit-source-id: 79f49d424cc8da9468e0387b4fa3d2216b397014

--- hphp/runtime/base/type-structure-helpers-defs.h
+++ hphp/runtime/base/type-structure-helpers-defs.h
@@ -47,7 +47,7 @@ const StaticString s_typevars("typevars");
 const StaticString s_typevar_types("typevar_types");
 const StaticString s_hh_this("HH\\this");
 const StaticString s_type_structure_non_existant_class(
-  "hh\\__internal\\type_structure_non_existant_class");
+  "HH\\__internal\\type_structure_non_existant_class");
 
 // Fixed error messages
 const StaticString s_reified_type_must_be_ts(

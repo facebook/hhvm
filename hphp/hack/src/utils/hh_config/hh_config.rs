5912301201824968102e2a8f698847dd1510758f
Edwin Smith <smith@meta.com>
2023-01-17 11:19:24 -0800

    parse more .hhconfig options
    Summary:
    hh_config.rs is lagging behind serverConfig.ml; this catches it up for a few
    new options that have been added to .hhconfig recently.
    
    Reviewed By: nt591
    
    Differential Revision: D42548246
    
    fbshipit-source-id: d100e270293279649255c4be75a03a6025cfd85c

--- hphp/hack/src/utils/hh_config/hh_config.rs
+++ hphp/hack/src/utils/hh_config/hh_config.rs
@@ -157,6 +157,18 @@ impl HhConfig {
                 "log_levels" => {
                     go.log_levels = parse_json(&value);
                 }
+                "allow_all_locations_for_type_constant_in_enum_class" => {
+                    go.tco_allow_all_locations_for_type_constant_in_enum_class = parse_json(&value);
+                }
+                "allowed_locations_for_type_constant_in_enum_class" => {
+                    go.tco_allowed_locations_for_type_constant_in_enum_class = parse_svec(&value);
+                }
+                "const_default_func_args" => {
+                    go.po_const_default_func_args = parse_json(&value);
+                }
+                "const_default_lambda_args" => {
+                    go.po_const_default_lambda_args = parse_json(&value);
+                }
                 _ => c.unknown.push((key, value)),
             }
         }

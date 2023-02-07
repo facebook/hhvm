e2975df20ccb5c52469191d563a6d8212f04cd51
Shayne Fletcher <shaynefletcher@meta.com>
2023-02-06 18:12:49 -0800

    make elab_one_file a library call
    Summary: make `elab` a package and `elab::elab_one_file` a library call. in this way it can be called from `#[test]` blocks.
    
    Reviewed By: rjbailey
    
    Differential Revision: D43047859
    
    fbshipit-source-id: 11813c346804ca4759e9e145588db2b726f896f2

---
+++ hphp/hack/src/elab/elab/elab.rs
@@ -0,0 +1,7 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the "hack" directory of this source tree.
+
+mod elab_file;
+pub use elab_file::elab_one_file;
--- hphp/hack/src/elab/hackc_elab.rs
+++ hphp/hack/src/elab/hackc_elab.rs
@@ -1,6 +1,5 @@
 // HACKC_ELAB_LOG=info buck2 run fbcode//hphp/hack/src/elab:hackc-elab -- files $HOME/fbsource/fbcode/hphp/hack/test/typecheck/tuple_hints.php $HOME/fbsource/fbcode/hphp/hack/test/typecheck/num_type_hint.php
 
-//use log::info;
 use std::path::PathBuf;
 
 use anyhow::Result;
@@ -48,17 +47,9 @@ pub struct FileOpts {
 mod elab_files {
     use std::path::Path;
 
-    use aast_parser::AastParser;
-    use anyhow::anyhow;
     use anyhow::Result;
     use clap::Parser;
-    use log::info;
-    use ocamlrep::rc::RcOc;
-    use parser_core_types::indexed_source_text::IndexedSourceText;
-    use parser_core_types::source_text::SourceText;
     use rayon::prelude::*;
-    use relative_path::Prefix;
-    use relative_path::RelativePath;
 
     use super::FileOpts;
 
@@ -66,6 +57,17 @@ mod elab_files {
     pub struct Opts {
         #[clap(flatten)]
         pub files: FileOpts,
+    }
+
+    #[derive(Clone, Default)]
+    struct Ctx {}
+    struct Err {}
+
+    #[derive(Default)]
+    struct NoPass {}
+    impl transform::Pass for NoPass {
+        type Ctx = Ctx;
+        type Err = Err;
     }
 
     pub fn run(opts: &Opts) -> Result<()> {
@@ -80,64 +82,12 @@ mod elab_files {
             .collect()
     }
 
-    #[derive(Clone, Default)]
-    struct Ctx {}
+    pub fn elab_one_file(path: &Path, _: &Opts) -> Result<()> {
+        let top_down = NoPass::default();
+        let bottom_up = NoPass::default();
+        let diff = elab::elab_one_file(path, &top_down, &bottom_up)?;
+        println!("diff ({path:#?}):\n{diff}");
 
-    struct Err {}
-
-    #[derive(Default)]
-    struct NoPass {}
-    impl transform::Pass for NoPass {
-        type Ctx = Ctx;
-        type Err = Err;
-    }
-
-    fn elab_one_file(path: &Path, _opts: &Opts) -> Result<()> {
-        let content = std::fs::read(path)?;
-        let filepath = RelativePath::make(Prefix::Dummy, path.to_path_buf());
-        let filename = path.file_name().unwrap().to_str().unwrap();
-        info!("proceeding to elaborate {filename:#?}");
-
-        let source_text = SourceText::make(RcOc::new(filepath), &content);
-        let indexed_source_text = IndexedSourceText::new(source_text);
-        let env = aast_parser::rust_aast_parser_types::Env::default();
-        match AastParser::from_text(&env, &indexed_source_text) {
-            Err(error) => Err(anyhow!("parse failure {path:#?}: {error:#?}"))?,
-            Ok(mut parse_result) => {
-                info!("parsing {filename:#?}");
-                let program = &mut parse_result.aast;
-                let before = format!("{program:#?}");
-
-                info!("calling 'tranform_ty_program' on {filename:#?}");
-                let mut ctx = Ctx::default();
-                let mut errs = Vec::default();
-                let top_down = NoPass::default();
-                let bottom_up = NoPass::default();
-                transform::transform_ty_program(
-                    program, &mut ctx, &mut errs, &top_down, &bottom_up,
-                );
-                if !errs.is_empty() {
-                    println!(
-                        "{} errors encountered elaborating {filename:#?}",
-                        errs.len()
-                    );
-                }
-                let after = format!("{program:#?}");
-
-                info!("calculating diff for {filename:#?}");
-                let diff = similar::TextDiff::from_lines(&before, &after)
-                    .unified_diff()
-                    .context_radius(10)
-                    .header("before", "after")
-                    .to_string();
-                if diff.is_empty() {
-                    info!("calling 'transform_ty_program' on {filename:#?} had no effect");
-                }
-
-                println!("diff ({path:#?}):\n{diff}");
-
-                Ok(())
-            }
-        }
+        Ok(())
     }
 }

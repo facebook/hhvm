8ac98e8a2149e01e3ef417fe54ab90f620ad3b86
Julien Chaffraix <jchaffraix@slack-corp.com>
2023-02-16 10:04:04 -0800

    ZipArchive::getFromIndex: return a nullable string (#9318)
    Summary:
    This updates this function to return null instead of false on error. This allows typing the return as `?string` instead of `mixed`.
    
    Also fixed a return mismatch between builtins_zip.hhi and the extension that was reported by some Slack engineer.
    
    Pull Request resolved: https://github.com/facebook/hhvm/pull/9318
    
    Reviewed By: jthemphill
    
    Differential Revision: D42806636
    
    Pulled By: Wilfred
    
    fbshipit-source-id: 05e09e4af8260d4a6881aaf5fe27df15ba0cb5d1

---
+++ hphp/test/slow/ext_zip/can_manipulate_with_index.php
@@ -0,0 +1,31 @@
+<?hh
+
+// Create test ZIP file
+<<__EntryPoint>>
+function main_write_after_read_works() {
+  $tempfile = __SystemLib\hphp_test_tmppath('hello.zip');
+  $zip_setup = new ZipArchive();
+  $zip_setup->open($tempfile, ZipArchive::OVERWRITE | ZipArchive::CREATE);
+  $zip_setup->addFromString('hello.txt', 'Value here');
+  $zip_setup->addFromString('hello2.txt', 'Other value here');
+  $zip_setup->close();
+
+  // Load the file fresh to modify it
+  $zip = new ZipArchive();
+  $zip->open($tempfile);
+
+  // Get the 2 strings.
+  echo 'First section: ', $zip->getFromIndex(0), "\n";
+  echo 'Second section: ', $zip->getFromIndex(1), "\n";
+  echo 'Third section: ', var_dump($zip->getFromIndex(2));
+
+  // Remove first.
+  echo "\nRemoving first section\n";
+  $zip->deleteIndex(0);
+  echo 'First section: ', var_dump($zip->getFromIndex(0));
+  echo 'Second section: ', $zip->getFromIndex(1), "\n";
+  echo 'Third section: ', var_dump($zip->getFromIndex(2));
+
+  $zip->close();
+  unlink($tempfile);
+}

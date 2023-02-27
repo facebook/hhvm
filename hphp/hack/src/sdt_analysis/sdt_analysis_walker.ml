d838b05fe5765f89a68c8fb0b893b70b643ea57c
Max Heiber <mheiber@meta.com>
2023-02-27 07:20:07 -0800

    Ignore hhis
    Summary:
    Ignore hhis in the walker, similar to what is done in the visitor in shape_analysis_logger
    
    https://www.internalfb.com/code/fbsource/[0de753940ca898fd9f0aaabbfc066b556c5faa1b]/fbcode/hphp/hack/src/typing/tast_check/shape_analysis_logger.ml?lines=205
    
    Reviewed By: madgen
    
    Differential Revision: D43515068
    
    fbshipit-source-id: 74cca07ab0bc2023935c8d032d3b3bc36179501f

--- hphp/hack/src/sdt_analysis/sdt_analysis_walker.ml
+++ hphp/hack/src/sdt_analysis/sdt_analysis_walker.ml
@@ -37,109 +37,116 @@ let remove_supportdyn_from_ty ty =
   | T.Tnewtype (c, [ty], _) when String.equal c SN.Classes.cSupportDyn -> ty
   | _ -> ty
 
+let should_visit = Fn.non Tast_env.is_hhi
+
 let tast_handler writer =
   object
     inherit Tast_visitor.handler_base
 
     method! at_expr env (_, e_pos, e_) =
-      let decorated_constraint ~origin =
-        DecoratedConstraint.
-          { origin; hack_pos = e_pos; constraint_ = Constraint.NeedsSDT }
-      in
-      let id_constraint_pairs =
-        match e_ with
-        | A.Call ((base_ty, _, base_exp), _tel, el, _unpacked_el) ->
-          let doesnt_subtype (fp, (_, (arg_ty, _, _))) =
-            not @@ Tast_env.is_sub_type env arg_ty fp.T.fp_type.T.et_type
-          in
-          let constraints_of_class_sid sid =
-            let id = H.Class sid in
-            let ty = remove_supportdyn_from_ty base_ty in
-            match T.get_node ty with
-            | T.Tfun ft ->
-              let param_arg_pairs =
-                let open List.Or_unequal_lengths in
-                match List.zip ft.T.ft_params el with
-                | Ok pairs -> pairs
-                | Unequal_lengths -> []
-              in
-              if List.exists ~f:doesnt_subtype param_arg_pairs then
-                let constraint_ = decorated_constraint ~origin:__LINE__ in
-                [(id, constraint_)]
-              else
-                []
-            | _ -> []
-          in
-          begin
-            match base_exp with
-            | A.Id (_, sid) -> constraints_of_class_sid sid
-            | A.Obj_get ((receiver_ty, _, _), _, _, _) -> begin
-              match T.get_node receiver_ty with
-              | T.Tclass ((_, sid), _, _) -> constraints_of_class_sid sid
+      if should_visit env then
+        let decorated_constraint ~origin =
+          DecoratedConstraint.
+            { origin; hack_pos = e_pos; constraint_ = Constraint.NeedsSDT }
+        in
+        let id_constraint_pairs =
+          match e_ with
+          | A.Call ((base_ty, _, base_exp), _tel, el, _unpacked_el) ->
+            let doesnt_subtype (fp, (_, (arg_ty, _, _))) =
+              not @@ Tast_env.is_sub_type env arg_ty fp.T.fp_type.T.et_type
+            in
+            let constraints_of_class_sid sid =
+              let id = H.Class sid in
+              let ty = remove_supportdyn_from_ty base_ty in
+              match T.get_node ty with
+              | T.Tfun ft ->
+                let param_arg_pairs =
+                  let open List.Or_unequal_lengths in
+                  match List.zip ft.T.ft_params el with
+                  | Ok pairs -> pairs
+                  | Unequal_lengths -> []
+                in
+                if List.exists ~f:doesnt_subtype param_arg_pairs then
+                  let constraint_ = decorated_constraint ~origin:__LINE__ in
+                  [(id, constraint_)]
+                else
+                  []
+              | _ -> []
+            in
+            begin
+              match base_exp with
+              | A.Id (_, sid) -> constraints_of_class_sid sid
+              | A.Obj_get ((receiver_ty, _, _), _, _, _) -> begin
+                match T.get_node receiver_ty with
+                | T.Tclass ((_, sid), _, _) -> constraints_of_class_sid sid
+                | _ -> []
+              end
+              | A.Class_const ((_, _, A.CI (_, sid)), _) ->
+                constraints_of_class_sid sid
               | _ -> []
             end
-            | A.Class_const ((_, _, A.CI (_, sid)), _) ->
-              constraints_of_class_sid sid
-            | _ -> []
-          end
-        | _ -> []
-      in
-      id_constraint_pairs
-      |> List.iter ~f:(fun (id, constraint_) ->
-             H.Write.add_intra writer id constraint_)
+          | _ -> []
+        in
+        id_constraint_pairs
+        |> List.iter ~f:(fun (id, constraint_) ->
+               H.Write.add_intra writer id constraint_)
 
     method! at_fun_def env A.{ fd_name = (_, sid); fd_fun; _ } : unit =
-      let id = H.Function sid in
-      H.Write.add_id writer id;
-      let env = remove_supportdyn_of_mixed_upper_bound_from_tparams env in
-      let decorated_constraint ~origin =
-        let hack_pos = fd_fun.A.f_span in
-        DecoratedConstraint.
-          { origin; hack_pos; constraint_ = Constraint.NeedsSDT }
-      in
-      let from_dynamically_callable =
-        let is_dynamically_callable =
-          List.exists ~f:(fun attr ->
-              String.equal
-                (snd attr.A.ua_name)
-                SN.UserAttributes.uaDynamicallyCallable)
+      if should_visit env then (
+        let id = H.Function sid in
+        H.Write.add_id writer id;
+        let env = remove_supportdyn_of_mixed_upper_bound_from_tparams env in
+        let decorated_constraint ~origin =
+          let hack_pos = fd_fun.A.f_span in
+          DecoratedConstraint.
+            { origin; hack_pos; constraint_ = Constraint.NeedsSDT }
         in
-        if is_dynamically_callable fd_fun.A.f_user_attributes then
-          let constraint_ = decorated_constraint ~origin:__LINE__ in
-          [(id, constraint_)]
-        else
-          []
-      in
-      let from_signature_check =
-        let is_supportdyn_of_mixed ty =
-          Tast_env.is_sub_type env ty supportdyn_of_mixed
+        let from_dynamically_callable =
+          let is_dynamically_callable =
+            List.exists ~f:(fun attr ->
+                String.equal
+                  (snd attr.A.ua_name)
+                  SN.UserAttributes.uaDynamicallyCallable)
+          in
+          if is_dynamically_callable fd_fun.A.f_user_attributes then
+            let constraint_ = decorated_constraint ~origin:__LINE__ in
+            [(id, constraint_)]
+          else
+            []
         in
-        let fails_formation =
-          List.exists fd_fun.A.f_params ~f:(fun param ->
-              not @@ is_supportdyn_of_mixed @@ fst param.A.param_type_hint)
-          || (not @@ is_supportdyn_of_mixed @@ fst fd_fun.A.f_ret)
+        let from_signature_check =
+          let is_supportdyn_of_mixed ty =
+            Tast_env.is_sub_type env ty supportdyn_of_mixed
+          in
+          let fails_formation =
+            List.exists fd_fun.A.f_params ~f:(fun param ->
+                not @@ is_supportdyn_of_mixed @@ fst param.A.param_type_hint)
+            || (not @@ is_supportdyn_of_mixed @@ fst fd_fun.A.f_ret)
+          in
+          if fails_formation then
+            let constraint_ = decorated_constraint ~origin:__LINE__ in
+            [(id, constraint_)]
+          else
+            []
         in
-        if fails_formation then
-          let constraint_ = decorated_constraint ~origin:__LINE__ in
-          [(id, constraint_)]
-        else
-          []
-      in
 
-      from_dynamically_callable @ from_signature_check
-      |> List.iter ~f:(fun (id, constraint_) ->
-             H.Write.add_intra writer id constraint_)
+        from_dynamically_callable @ from_signature_check
+        |> List.iter ~f:(fun (id, constraint_) ->
+               H.Write.add_intra writer id constraint_)
+      )
 
-    method! at_class_ _ A.{ c_name = (_, sid); c_extends; _ } =
-      let id = H.Class sid in
-      H.Write.add_id writer id;
+    method! at_class_ env A.{ c_name = (_, sid); c_extends; _ } =
+      if should_visit env then (
+        let id = H.Class sid in
+        H.Write.add_id writer id;
 
-      let at_extends (_, hint_) =
-        match hint_ with
-        | A.Happly ((_, parent_sid), _hints) ->
-          let parent_id = H.Class parent_sid in
-          H.Write.add_inter writer id (H.ClassExtends parent_id)
-        | _ -> ()
-      in
-      c_extends |> List.iter ~f:at_extends
+        let at_extends (_, hint_) =
+          match hint_ with
+          | A.Happly ((_, parent_sid), _hints) ->
+            let parent_id = H.Class parent_sid in
+            H.Write.add_inter writer id (H.ClassExtends parent_id)
+          | _ -> ()
+        in
+        c_extends |> List.iter ~f:at_extends
+      )
   end

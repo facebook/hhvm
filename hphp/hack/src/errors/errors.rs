6803a3499afe2e157b9ba98531ab08e3aed3f693
Edwin Smith <smith@meta.com>
2023-02-28 06:20:16 -0800

    move FmtPlain to its own module
    Summary: Since we have four renderers (plus json), let's separate concerns a bit.
    
    Reviewed By: nt591
    
    Differential Revision: D43498114
    
    fbshipit-source-id: 7dde4814f1512243f0885ce077906d91b77c5881

--- hphp/hack/src/errors/errors.rs
+++ hphp/hack/src/errors/errors.rs
@@ -2,7 +2,9 @@
 //
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
+mod fmt_plain;
 mod user_error;
 
+pub use fmt_plain::*;
 pub use oxidized::errors::*;
 pub use user_error::*;
--- hphp/hack/src/errors/user_error.rs
+++ hphp/hack/src/errors/user_error.rs
@@ -3,7 +3,6 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
 use oxidized::message::Message;
-use oxidized::pos_or_decl::PosOrDecl;
 use oxidized::user_error::UserError;
 use rc_pos::Pos;
 use relative_path::RelativePathCtx;
@@ -34,50 +33,4 @@ fn msg_json(
         "end": ecol,
         "code": code,
     })
-}
-
-/// A `std::fmt::Display` implementation that displays the error in the
-/// Errors.Plain format produced by OCaml Errors.to_string.
-pub struct FmtPlain<'a>(pub &'a UserError<Pos, PosOrDecl>, pub &'a RelativePathCtx);
-
-impl<'a> std::fmt::Display for FmtPlain<'a> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let Self(
-            UserError {
-                code,
-                claim: Message(pos, msg),
-                reasons,
-                quickfixes: _,
-                is_fixmed: _,
-            },
-            ctx,
-        ) = self;
-        let code = FmtErrorCode(*code);
-        write!(f, "{}\n{} ({})", pos.absolute(ctx), msg, code)?;
-        for Message(pos, msg) in reasons {
-            write!(f, "\n  {}\n  {}", pos.absolute(ctx), msg)?;
-        }
-        Ok(())
-    }
-}
-
-struct FmtErrorCode(ErrorCode);
-
-impl std::fmt::Display for FmtErrorCode {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let Self(code) = self;
-        write!(f, "{}[{:04}]", error_kind(*code), code)
-    }
-}
-
-fn error_kind(error_code: ErrorCode) -> &'static str {
-    match error_code / 1000 {
-        1 => "Parsing",
-        2 => "Naming",
-        3 => "NastCheck",
-        4 => "Typing",
-        5 => "Lint",
-        8 => "Init",
-        _ => "Other",
-    }
 }

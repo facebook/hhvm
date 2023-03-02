1ff8ca712e113453c3389d117618866869cf629f
Hunter Goldstein <hgoldstein@meta.com>
2023-03-02 09:41:32 -0800

    Use native autoloading in `hphp/test/quick/autoload6.php`
    Reviewed By: jthemphill
    
    Differential Revision: D43479499
    
    fbshipit-source-id: 43b0470e7ab2300693f817436c47c7ff784eed83

--- hphp/test/quick/autoload6.d/autoload6.php
+++ hphp/test/quick/autoload6.d/autoload6.php
@@ -3,13 +3,5 @@
 class C { function __toString() { return 'I'; } }
 
 <<__EntryPoint>> function main(): void {
-  HH\autoload_set_paths(
-    dict[
-      'class' => dict[
-        'i' => 'autoload6-2.inc',
-      ],
-    ],
-    __DIR__.'/',
-  );
   var_dump(interface_exists(new C));
 }

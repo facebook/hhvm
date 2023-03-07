4327878973d86563fb84b0ac8bc6fcf2319ec130
Wilfred Hughes <wilfred@meta.com>
2023-03-06 16:48:30 -0800

    Releasing 4.172.2

--- hphp/runtime/version.h
+++ hphp/runtime/version.h
@@ -12,7 +12,7 @@
 # define HHVM_VERSION_MAJOR 4
 # define HHVM_VERSION_MINOR 172
 # define HHVM_VERSION_PATCH 2
-# define HHVM_VERSION_SUFFIX "-dev"
+# define HHVM_VERSION_SUFFIX ""
 #endif
 
 /* HHVM_VERSION_ID minus the patch number

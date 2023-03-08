a72286619d0d4492c5b01347fca03367f96fc31b
Edwin Smith <smith@meta.com>
2023-03-08 04:21:02 -0800

    migrate test/slow/modules to native facts
    Summary: Tests cannot call autoload_set_paths() any more.
    
    Reviewed By: oulgen
    
    Differential Revision: D43838717
    
    fbshipit-source-id: 9da14f07fd038c3b47ba01b5027a8475b12ea004

--- hphp/test/slow/modules/autoload-1.php
+++ hphp/test/slow/modules/autoload-1.php
@@ -2,17 +2,11 @@
 
 <<__EntryPoint>>
 function main() {
-  HH\autoload_set_paths(
-    dict[
-      'module' => darray[
-        'a' => 'autoload-1.inc',
-        'b' => 'autoload-1.inc'
-      ],
-      'failure' => (...$args) ==> var_dump($args),
-    ],
-    __DIR__.'/'
-  );
   $m = new ReflectionModule('a');
   var_dump($m->getName());
-  try { new ReflectionModule('b'); } catch (Exception $e) { var_dump($e->getMessage()); }
+  try {
+    new ReflectionModule('b');
+  } catch (Exception $e) {
+    var_dump($e->getMessage());
+  }
 }
--- hphp/test/slow/modules/module-exists-1.php
+++ hphp/test/slow/modules/module-exists-1.php
@@ -2,16 +2,6 @@
 
 <<__EntryPoint>>
 function main() {
-  HH\autoload_set_paths(
-    dict[
-      'module' => darray[
-        'a' => 'autoload-1.inc',
-        'b' => 'autoload-1.inc'
-      ],
-      'failure' => (...$args) ==> var_dump($args),
-    ],
-    __DIR__.'/'
-  );
   include "module.inc";
   var_dump(module_exists('a')); // true
   var_dump(module_exists('b')); // false

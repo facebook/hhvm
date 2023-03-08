b374d06a5daf181ea2b238489dfcf3ecfda4bfa9
Kirill Sazonov <sazonovk@meta.com>
2023-03-07 18:03:55 -0800

    Simplify ResourcePoolHandle
    Summary: Simplify comments
    
    Differential Revision: D43827181
    
    fbshipit-source-id: d6667138fbfd28960ecc9b446b9a3b29d9364cb4

--- third-party/thrift/src/thrift/lib/cpp2/server/ResourcePoolHandle.h
+++ third-party/thrift/src/thrift/lib/cpp2/server/ResourcePoolHandle.h
@@ -23,14 +23,7 @@
 
 namespace apache::thrift {
 
-// This is used to identify a ResourcePool within a ResourcePoolSet. This
-// is just data. It is the responsibility of the code that creates and
-// then accepts these to do any validation.
-//
-// To help with that we provide an ownerId value which can be set on creation by
-// a class that returns ResourcePoolHandles to a unique value and then when that
-// class is passed a ResourcePoolHandle it can check that it is one of its own
-// using checkOwner().
+// Indentifies ResourcePool in a ResourcePoolSet by its name and index
 class ResourcePoolHandle {
  public:
   static constexpr std::size_t kDefaultSyncIndex = 0;
@@ -40,16 +33,11 @@ class ResourcePoolHandle {
   std::size_t index() const { return index_; }
   std::string_view name() const { return name_; }
 
-  // Returns pointer to a singleton default Sync handle. Users should cache the
-  // result of this.
   static const ResourcePoolHandle& defaultSync();
-
-  // Returns pointer to a singleton default aysnc handle. Users should cache the
-  // result of this.
   static const ResourcePoolHandle& defaultAsync();
 
-  // Create a new handle (cannot be used to create kDefaultSync or
-  // kDefaultAsync handles).
+  // Create a new handle for a custom (not default async or default async)
+  // resource pool
   static ResourcePoolHandle makeHandle(
       std::string_view name, std::size_t index) {
     CHECK(index != kDefaultSyncIndex && index != kDefaultAsyncIndex);

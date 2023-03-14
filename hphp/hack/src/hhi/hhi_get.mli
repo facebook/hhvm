afeb6ffc6fccdde2606f337475b64e85353097b7
Wilfred Hughes <wilfred@meta.com>
2023-03-14 11:35:58 -0700

    Only depend on hhi_get where it's needed
    Summary: Previously we used `hhi_get` indirectly via `hhi`, which was confusing. Also add a `.mli` to make the API explicit.
    
    Reviewed By: hgoldstein
    
    Differential Revision: D44037563
    
    fbshipit-source-id: fad7d2553b87ba4111358eb8d7cae12dba2c2bec

---
+++ hphp/hack/src/hhi/hhi_get.mli
@@ -0,0 +1,12 @@
+(*
+ * Copyright (c) 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the "flow" directory of this source tree.
+ *
+ *)
+
+val get_hhis_in_dir : string -> (string * string) list
+
+val get_hhis : string -> string -> (string * string) list

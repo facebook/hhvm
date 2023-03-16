7b12e4ac51e8ab7c162859eee33ba97d97c18789
Max Heiber <mheiber@meta.com>
2023-03-16 04:35:30 -0700

    less hardcoding in jsonl
    Summary:
    a noop
    
    Reducing hard-coding will make it easier to deserialize (later in the stack)
    
    Reviewed By: madgen
    
    Differential Revision: D43905253
    
    fbshipit-source-id: ff421f7b39199f0ac091979f2742f69675d85792

--- hphp/hack/src/sdt_analysis/sdt_analysis_summary_jsonl.ml
+++ hphp/hack/src/sdt_analysis/sdt_analysis_summary_jsonl.ml
@@ -9,37 +9,60 @@ open Hh_prelude
 open Sdt_analysis_types
 module J = Hh_json
 
+let entry_kind_label = "entry_kind"
+
 let stats_json_of_summary
     Summary.
       { id_cnt; syntactically_nadable_cnt; nadable_cnt; nadable_groups = _ } =
   J.JSON_Object
     [
-      ("entry_kind", J.string_ "stats");
+      (entry_kind_label, J.string_ "stats");
       ("id_cnt", J.int_ id_cnt);
       ("syntactically_nadable_cnt", J.int_ syntactically_nadable_cnt);
       ("nadable_cnt", J.int_ nadable_cnt);
     ]
 
-let stats_json_of_group nadables =
-  let of_nadable Summary.{ id; kind } =
+module Nad : sig
+  val json_of_nadables : Summary.nadable list -> J.json
+end = struct
+  module Names = struct
+    let add_no_auto_dynamic_attr = "add_no_auto_dynamic_attr"
+
+    let items = "items"
+
+    let sid = "sid"
+
+    let kind = "kind"
+
+    let function_ = "function"
+
+    let classish_kind_unknown = "classish_kind_unknown"
+  end
+
+  let json_obj_of_nadable Summary.{ id; kind } =
     let kind_str =
       match kind with
-      | Summary.Function -> "function"
+      | Summary.Function -> Names.function_
       | Summary.ClassLike classish_kind_opt ->
         Option.(
           classish_kind_opt
           >>| show_classish_kind
-          |> value ~default:"classish_kind_unknown")
+          |> value ~default:Names.classish_kind_unknown)
     in
     J.JSON_Object
-      [("sid", J.string_ @@ H.Id.sid_of_t id); ("kind", J.string_ kind_str)]
-  in
-  let nadables_arr = J.array_ of_nadable nadables in
-  J.JSON_Object
-    [
-      ("entry_kind", J.string_ "add_no_auto_dynamic_attr");
-      ("items", nadables_arr);
-    ]
+      [
+        (Names.sid, J.string_ @@ H.Id.sid_of_t id);
+        (Names.kind, J.string_ kind_str);
+      ]
+
+  let json_of_nadables nadables =
+    let nadables_arr = J.array_ json_obj_of_nadable nadables in
+    J.JSON_Object
+      [
+        (entry_kind_label, J.string_ Names.add_no_auto_dynamic_attr);
+        (Names.items, nadables_arr);
+      ]
+end
 
 let of_summary summary : string Sequence.t =
   let to_string = J.json_to_string ~sort_keys:true ~pretty:false in
@@ -48,7 +71,7 @@ let of_summary summary : string Sequence.t =
   in
   let groups =
     summary.Summary.nadable_groups
-    |> Sequence.map ~f:stats_json_of_group
+    |> Sequence.map ~f:Nad.json_of_nadables
     |> Sequence.map ~f:to_string
   in
   Sequence.append stats groups

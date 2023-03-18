f390b304308411475ae5cf86e474d6d0de1eee0b
Edwin Smith <smith@meta.com>
2023-03-18 12:35:33 -0700

    migrate autoload_user_symbol_to_path.php to native facts
    Summary: Continue testing explicit symbol->path lookups
    
    Reviewed By: hgoldstein
    
    Differential Revision: D44095713
    
    fbshipit-source-id: e53bda0a69fb6b85ea6e69d2b3eb359023c744da

---
+++ hphp/test/slow/autoload/autoload_user_symbol_to_path/autoload_user_symbol_to_path.php
@@ -0,0 +1,30 @@
+<?hh
+
+class A {}
+type T = A;
+const C = 10;
+
+<<__EntryPoint>>
+function main(): void {
+  var_dump(HH\autoload_type_to_path(
+    A::class,
+  ));
+  var_dump(HH\autoload_type_alias_to_path(
+    'T',
+  ));
+  var_dump(HH\autoload_type_to_path(
+    'notexisting',
+  ));
+  var_dump(HH\autoload_function_to_path(
+    'main',
+  ));
+  var_dump(HH\autoload_function_to_path(
+    'notexisting',
+  ));
+  var_dump(HH\autoload_constant_to_path(
+    'C',
+  ));
+  var_dump(HH\autoload_constant_to_path(
+    'notexisting',
+  ));
+}

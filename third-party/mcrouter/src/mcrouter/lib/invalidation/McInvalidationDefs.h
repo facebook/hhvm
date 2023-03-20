9a70103a6e3a62be2383ccb9c02099872cea9c4b
Stuart Clark <stuclar@meta.com>
2023-03-20 15:45:26 -0700

    Move invalidation definition to mcrouter
    Summary: As above
    
    Reviewed By: disylh
    
    Differential Revision: D44083135
    
    fbshipit-source-id: 055a2a87590619e2f5c2d072a20d29d719108b6b

---
+++ third-party/mcrouter/src/mcrouter/lib/invalidation/McInvalidationDefs.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <string>
+
+namespace facebook {
+namespace memcache {
+
+inline const std::string kMcDeleteReqAttrInvalidationTimeout =
+    "invalidation_timeout";
+inline const std::string kMcDeleteReqAttrDBLogTimestamp = "dblog_timestamp";
+inline const std::string kMcDeleteReqAttrSource = "source";
+inline const std::string kMcDeleteReqHlc = "hlc";
+
+enum class McDeleteRequestSource : uint8_t {
+  UNKNOWN,
+  INGESTION_SERVICE,
+  GLOSTIC,
+  FAILED_INVALIDATION,
+  BROADCAST_INVALIDATION,
+};
+
+} // namespace memcache
+} // namespace facebook

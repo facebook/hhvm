f2b3ecf941f579936d6d3d8217232590e1a904b8
Dave Kwon <dokwon@meta.com>
2023-03-21 16:05:14 -0700

    Clean up FieldMaskTest.MaskRefGetMask
    Summary: clean up duplicate test
    
    Reviewed By: vitaut
    
    Differential Revision: D44231456
    
    fbshipit-source-id: 26e546c0c904d454ba4a245ab8a94bdf147311bd

--- third-party/thrift/src/thrift/test/FieldMaskTest.cpp
+++ third-party/thrift/src/thrift/test/FieldMaskTest.cpp
@@ -164,32 +164,24 @@ TEST(FieldMaskTest, MaskRefGetMask) {
     Mask m;
     m.includes_ref().emplace()[5] = allMask();
     EXPECT_EQ(getFieldMask(m), &*m.includes_ref());
-    EXPECT_EQ(getFieldMask(m), &*m.includes_ref());
-    EXPECT_EQ(getMapMask(m), nullptr);
     EXPECT_EQ(getMapMask(m), nullptr);
   }
   {
     Mask m;
     m.excludes_ref().emplace()[5] = noneMask();
     EXPECT_EQ(getFieldMask(m), &*m.excludes_ref());
-    EXPECT_EQ(getFieldMask(m), &*m.excludes_ref());
-    EXPECT_EQ(getMapMask(m), nullptr);
     EXPECT_EQ(getMapMask(m), nullptr);
   }
   {
     Mask m;
     m.includes_map_ref().emplace()[5] = allMask();
     EXPECT_EQ(getMapMask(m), &*m.includes_map_ref());
-    EXPECT_EQ(getMapMask(m), &*m.includes_map_ref());
-    EXPECT_EQ(getFieldMask(m), nullptr);
     EXPECT_EQ(getFieldMask(m), nullptr);
   }
   {
     Mask m;
     m.excludes_map_ref().emplace()[5] = noneMask();
     EXPECT_EQ(getMapMask(m), &*m.excludes_map_ref());
-    EXPECT_EQ(getMapMask(m), &*m.excludes_map_ref());
-    EXPECT_EQ(getFieldMask(m), nullptr);
     EXPECT_EQ(getFieldMask(m), nullptr);
   }
 }

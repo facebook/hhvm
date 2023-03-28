1bf22f45854c767a11c3503c931b2e0eb09c13ca
TJ Yin <ytj@meta.com>
2023-03-28 15:42:54 -0700

    rebuild API doc
    Summary: This diff rebuilds api docs via `thrift/website/rebuild_api_docs`.
    
    Reviewed By: thedavekwon
    
    Differential Revision: D44470814
    
    fbshipit-source-id: 7da8499c9b56e0c5eefefbda656cd4539f5e9c53

--- third-party/thrift/src/thrift/website/src/components/Ref.js
+++ third-party/thrift/src/thrift/website/src/components/Ref.js
@@ -1,17 +1,5 @@
 /**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
  */
 
  import React from 'react';

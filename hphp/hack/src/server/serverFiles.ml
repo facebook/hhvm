fabd6db4ece412a4543a45e4418a294869aac5f1
Lucian Wischik <ljw@meta.com>
2023-03-28 09:02:00 -0700

    make integration_ml/server_progress_tests more robust
    Summary:
    Integration tests are hard! Our experience with other hack integration tests is that they take a while to get robust. They suffer from running on test machines that are doing multiple other things concurrently. Sometimes a simple "hh check" takes too long to start up a server, or connect to a monitor, and the test times out.
    
    For streaming-errors, I want to write several integration tests, and I want a robust platform upon which to build them. This diff makes "test/integration_ml/server_progress_tests.ml" more robust:
    1. Each test now runs in its own HH_TMPDIR to be sure that other concurrent tests don't interfere at all.
    2. (already prior to this diff) the target has the tag "heavyweight8_experimental" to reduce concurrency with other tests
    3. The test has a lot more stderr output so that, in case of failure, it's easy to track down what went wrong: it shows timestamps, command-lines of when we invoke hack, and upon test failure it prints server-log, monitor-log, client-log, and a directory listing of the test's private HH_TMPDIR. To make the stderr output easier to read, it now prints a banner separating each test's stderr.
    4. I bumped up the timeouts to 2mins
    5. We can never use "Unix.sleep" in tests to wait until something has happened, since that's racey. Instead this diff introduces `let process = hh_open () in let%lwt () = hh_await_substring ~substring:"txt"`. This lets us launch the process and await until a matching line has appeared on stdout. The functions are used in a subsequent diff.
    6. I changed the tests to use --custom-hhi-path pointing to an empty directory. None of the tests use HHIs, and by using an empty directory each test can start up quicker.
    
    Reviewed By: CatherineGasnier
    
    Differential Revision: D44408289
    
    fbshipit-source-id: 4c9fe8abb34f2ed13b77f74bf0d2d1581d0ee6f8

--- hphp/hack/src/server/serverFiles.ml
+++ hphp/hack/src/server/serverFiles.ml
@@ -10,17 +10,36 @@
 open Hh_prelude
 open Utils
 
+let tmp : Path.t option ref = ref None
+
+(** Normally, the ServerFiles.* functions use GlobalConfig.tmp_dir
+which is initialized at startup to be either HH_TMPDIR if defined
+or /tmp/hh_server otherwise.
+
+But some tests like to use these ServerFiles.* functions with a
+different tmpdir chosen at runtime. These tests can call
+ServerFiles.set_tmp_FOR_TESTING_ONLY, and all subsequent ServerFiles.*
+will refer to that new tmp directory.
+CARE! This is only to be used in limited circumstances. All the
+rest of the codebase will continue to use HH_TMPDIR-or-/tmp/hh_server.
+This only affects the ServerFiles.* functions; nothing else.
+You have been warned. *)
+let set_tmp_FOR_TESTING_ONLY (t : Path.t) : unit = tmp := Some t
+
+let get_tmp () : string =
+  Option.value_map !tmp ~f:Path.to_string ~default:GlobalConfig.tmp_dir
+
 (** Slash-escaped path in the system temp directory corresponding
     with this root directory for this extension. *)
 let path_of_root root extension =
   (* TODO: move this to places that write this file *)
-  Sys_utils.mkdir_no_fail GlobalConfig.tmp_dir;
+  Sys_utils.mkdir_no_fail (get_tmp ());
   let root_part = Path.slash_escaped_string_of_path root in
-  Filename.concat GlobalConfig.tmp_dir (spf "%s.%s" root_part extension)
+  Filename.concat (get_tmp ()) (spf "%s.%s" root_part extension)
 
 let is_of_root root fn =
   let root_part = Path.slash_escaped_string_of_path root in
-  String.is_prefix fn ~prefix:(Filename.concat GlobalConfig.tmp_dir root_part)
+  String.is_prefix fn ~prefix:(Filename.concat (get_tmp ()) root_part)
 
 (**
  * Lock on this file will be held after the server has finished initializing.
@@ -44,12 +63,10 @@ let client_ide_log root = path_of_root root "client_ide_log"
 let monitor_log_link root = path_of_root root "monitor_log"
 
 let server_finale_file (pid : int) : string =
-  Filename.concat GlobalConfig.tmp_dir (spf "%d.fin" pid)
+  Filename.concat (get_tmp ()) (spf "%d.fin" pid)
 
 let server_progress_file (root : Path.t) : string =
   path_of_root root "progress.json"
 
 let server_receipt_to_monitor_file (pid : int) : string =
-  Filename.concat
-    GlobalConfig.tmp_dir
-    (spf "server_receipt_to_monitor.%d.json" pid)
+  Filename.concat (get_tmp ()) (spf "server_receipt_to_monitor.%d.json" pid)
--- hphp/hack/test/integration_ml/server_progress_tests.ml
+++ hphp/hack/test/integration_ml/server_progress_tests.ml
@@ -14,53 +14,162 @@ let a_php = ("a.php", "<?hh\nfunction test_a(): void {}\n")
 let loop_php =
   ("loop.php", "<?hh\nfunction test_loop(): void {hh_loop_forever();}")
 
-(** Shell out to HH_CLIENT_PATH. Must have HH_SERVER_PATH defined too. *)
-let hh ~(root : Path.t) (args : string array) : string Lwt.t =
-  let hh_client_path = Sys.getenv "HH_CLIENT_PATH" in
-  let _ = Sys.getenv "HH_SERVER_PATH" in
-  (* just to assert that it's defined *)
+(** Shell out to HH_CLIENT_PATH and return its results once it's finished.
+In case of success return stdout; in case of failure return a human-readable
+representation that includes exit signal and stderr. *)
+let hh ~(tmp : Path.t) ~(root : Path.t) (args : string array) : string Lwt.t =
   Sys.chdir (Path.to_string root);
+  let hh_client_path = Sys.getenv "HH_CLIENT_PATH" in
+  let env =
+    Array.append [| "HH_TMPDIR=" ^ Path.to_string tmp |] (Unix.environment ())
+  in
+  Printf.eprintf
+    "[%s] EXECUTE: hh %s\n%!"
+    (Utils.timestring (Unix.gettimeofday ()))
+    (Array.to_list args |> String.concat ~sep:" ");
+  let (cancel, canceller) = Lwt.wait () in
+  let _ =
+    Lwt_unix.sleep 120.0 |> Lwt.map (fun () -> Lwt.wakeup_later canceller ())
+  in
   let%lwt result =
     Lwt_utils.exec_checked
       (Exec_command.For_use_in_testing_only hh_client_path)
-      ~timeout:30.0
+      ~cancel
+      ~env
       args
   in
   match result with
-  | Error e -> Lwt.return (Lwt_utils.Process_failure.to_string e)
+  | Error e ->
+    let e = Lwt_utils.Process_failure.to_string e in
+    Printf.eprintf "<error> %s\n%!" e;
+    Lwt.return e
   | Ok { Lwt_utils.Process_success.stdout; _ } -> Lwt.return stdout
+
+(** This spawns hh, so you can await output. *)
+let hh_open ~(tmp : Path.t) ~(root : Path.t) (args : string array) :
+    Lwt_process.process Lwt.t =
+  let hh_client_path = Sys.getenv "HH_CLIENT_PATH" in
+  let env =
+    Array.append [| "HH_TMPDIR=" ^ Path.to_string tmp |] (Unix.environment ())
+  in
+  Printf.eprintf
+    "[%s] EXECUTE: hh %s\n%!"
+    (Utils.timestring (Unix.gettimeofday ()))
+    (Array.to_list args |> String.concat ~sep:" ");
+  let process =
+    Lwt_process.open_process
+      ~env
+      ~cwd:(Path.to_string root)
+      (hh_client_path, Array.append [| hh_client_path |] args)
+  in
+  let%lwt () = Lwt_io.close process#stdin in
+  Lwt.return process
+
+(** This waits for substring to appear on a line of output.
+If the process dies, or if we've gone a long time without a matching line,
+then raises an exception. *)
+let hh_await_substring (process : Lwt_process.process) ~(substring : string) :
+    unit Lwt.t =
+  (* helper, returns Error after a timeout *)
+  let error_after_timeout () =
+    let%lwt () = Lwt_unix.sleep 120.0 in
+    Lwt.return_error ()
+  in
+  (* helper, reads a line and returns Ok *)
+  let ok_after_read_line_opt () =
+    try%lwt
+      let%lwt line_opt = Lwt_io.read_line_opt process#stdout in
+      Printf.eprintf
+        "[%s] [hh_client] %s\n%!"
+        (Utils.timestring (Unix.gettimeofday ()))
+        (Option.value line_opt ~default:"<eof>");
+      Lwt.return_ok line_opt
+    with
+    | exn ->
+      let e = Exception.wrap exn in
+      Printf.eprintf
+        "[%s] [hh_client] exception %s\n%!"
+        (Utils.timestring (Unix.gettimeofday ()))
+        (Exception.to_string e);
+      Lwt.return_ok None
+  in
+
+  let rec wait_for_substring () =
+    let%lwt line =
+      Lwt.pick [error_after_timeout (); ok_after_read_line_opt ()]
+    in
+    match line with
+    | Error () -> failwith "timeout"
+    | Ok None -> failwith "eof"
+    | Ok (Some s) when String.is_substring s ~substring -> Lwt.return_unit
+    | Ok (Some _) -> wait_for_substring ()
+  in
+  let%lwt () = wait_for_substring () in
+  Lwt.return_unit
+
+let dump_log ~(name : string) (file : string) : unit =
+  let contents =
+    try Sys_utils.cat file with
+    | _ -> "<No log>"
+  in
+  Printf.eprintf "\n\n--> hh %s:\n%s\n\n\n%!" name contents
 
 (** This creates a small repo, ready to run hh in it.
 (there'll be no watchman nor informant support). *)
 let try_with_server
-    (files : (string * string) list) (f : root:Path.t -> unit Lwt.t) :
-    unit Lwt.t =
+    (files : (string * string) list)
+    (f : tmp:Path.t -> root:Path.t -> hhi:Path.t -> unit Lwt.t) : unit Lwt.t =
   let root = Tempfile.mkdtemp ~skip_mocking:true in
+  let hhi = Tempfile.mkdtemp ~skip_mocking:true in
+  let tmp = Tempfile.mkdtemp ~skip_mocking:true in
   Lwt_utils.try_finally
     ~f:(fun () ->
-      Sys_utils.write_file
-        ~file:(Path.concat root ".hhconfig" |> Path.to_string)
-        "";
-      (* we'll put a dummy .hg directory in there to avoid the possibility of hg blowups *)
-      Sys_utils.mkdir_p
-        ~skip_mocking:true
-        (Path.concat root ".hg" |> Path.to_string);
-      List.iter files ~f:(fun (name, content) ->
-          Sys_utils.write_file
-            ~file:(Path.concat root name |> Path.to_string)
-            content);
-      ServerProgress.set_root root;
-      let%lwt () = f ~root in
-      Lwt.return_unit)
+      try%lwt
+        Sys_utils.write_file
+          ~file:(Path.concat root ".hhconfig" |> Path.to_string)
+          "";
+        (* we'll put a dummy .hg directory in there to avoid the possibility of hg blowups *)
+        Sys_utils.mkdir_p
+          ~skip_mocking:true
+          (Path.concat root ".hg" |> Path.to_string);
+        List.iter files ~f:(fun (name, content) ->
+            Sys_utils.write_file
+              ~file:(Path.concat root name |> Path.to_string)
+              content);
+        ServerProgress.set_root root;
+        ServerFiles.set_tmp_FOR_TESTING_ONLY tmp;
+        let%lwt () = f ~root ~hhi ~tmp in
+        Lwt.return_unit
+      with
+      | exn ->
+        let e = Exception.wrap exn in
+        dump_log ~name:"--logname" (ServerFiles.log_link root);
+        dump_log ~name:"--monitor-logname" (ServerFiles.monitor_log_link root);
+        dump_log ~name:"--client-logname" (ServerFiles.client_log root);
+        let%lwt ls =
+          Lwt_utils.exec_checked
+            Exec_command.Ls
+            ~timeout:5.0
+            [| "-l"; Path.to_string tmp |]
+        in
+        let ls =
+          match ls with
+          | Ok r -> r.Lwt_utils.Process_success.stdout
+          | Error e -> Lwt_utils.Process_failure.to_string e
+        in
+        Printf.eprintf "ls -l %s\n%s\n%!" (Path.to_string tmp) ls;
+        Exception.reraise e)
     ~finally:(fun () ->
       let%lwt () =
         try%lwt
-          let%lwt _ = hh ~root [| "stop" |] in
+          let%lwt _ = hh ~tmp ~root [| "stop" |] in
           Lwt.return_unit
         with
         | _ -> Lwt.return_unit
       in
       Sys_utils.rm_dir_tree ~skip_mocking:true (Path.to_string root);
+      Sys_utils.rm_dir_tree ~skip_mocking:true (Path.to_string hhi);
+      Sys_utils.rm_dir_tree ~skip_mocking:true (Path.to_string tmp);
       Lwt.return_unit)
 
 (** Looks for substring 'expected' in the ServerProgress message;
@@ -93,9 +202,19 @@ let get_server_pid ~(root : Path.t) : int Lwt.t =
 
 let test_start_stop () : bool Lwt.t =
   let%lwt () =
-    try_with_server [a_php] (fun ~root ->
+    try_with_server [a_php] (fun ~tmp ~root ~hhi ->
         let%lwt stdout =
-          hh ~root [| "start"; "--no-load"; "--config"; "max_workers=1" |]
+          hh
+            ~tmp
+            ~root
+            [|
+              "start";
+              "--no-load";
+              "--config";
+              "max_workers=1";
+              "--custom-hhi-path";
+              Path.to_string hhi;
+            |]
         in
         String_asserter.assert_equals "" stdout "hh start doesn't give output";
         (* status should eventually reach "ready" *)
@@ -104,7 +223,7 @@ let test_start_stop () : bool Lwt.t =
             ~deadline:(Unix.gettimeofday () +. 60.0)
             ~expected:"ready"
         in
-        let%lwt _ = hh ~root [| "stop" |] in
+        let%lwt _ = hh ~root ~tmp [| "stop" |] in
         (* The progress file should be cleanly deleted *)
         if Sys_utils.file_exists (ServerFiles.server_progress_file root) then
           failwith "expected progress file to be deleted";
@@ -117,9 +236,19 @@ let test_start_stop () : bool Lwt.t =
 
 let test_typechecking () : bool Lwt.t =
   let%lwt () =
-    try_with_server [a_php; loop_php] (fun ~root ->
+    try_with_server [a_php; loop_php] (fun ~tmp ~root ~hhi ->
         let%lwt stdout =
-          hh ~root [| "start"; "--no-load"; "--config"; "max_workers=1" |]
+          hh
+            ~root
+            ~tmp
+            [|
+              "start";
+              "--no-load";
+              "--config";
+              "max_workers=1";
+              "--custom-hhi-path";
+              Path.to_string hhi;
+            |]
         in
         String_asserter.assert_equals "" stdout "hh start doesn't give output";
         (* the file loop_php causes the typechecker to spin, typechecking, for 10mins! *)
@@ -134,9 +263,18 @@ let test_typechecking () : bool Lwt.t =
 
 let test_kill_server () : bool Lwt.t =
   let%lwt () =
-    try_with_server [a_php] (fun ~root ->
+    try_with_server [a_php] (fun ~tmp ~root ~hhi ->
         let%lwt stdout =
-          hh ~root [| "--no-load"; "--config"; "max_workers=1" |]
+          hh
+            ~root
+            ~tmp
+            [|
+              "--no-load";
+              "--config";
+              "max_workers=1";
+              "--custom-hhi-path";
+              Path.to_string hhi;
+            |]
         in
         String_asserter.assert_equals "No errors!\n" stdout "no errors";
         let%lwt server_pid = get_server_pid ~root in
@@ -156,9 +294,18 @@ let test_kill_server () : bool Lwt.t =
 
 let test_kill_monitor () : bool Lwt.t =
   let%lwt () =
-    try_with_server [a_php] (fun ~root ->
+    try_with_server [a_php] (fun ~tmp ~root ~hhi ->
         let%lwt stdout =
-          hh ~root [| "--no-load"; "--config"; "max_workers=1" |]
+          hh
+            ~root
+            ~tmp
+            [|
+              "--no-load";
+              "--config";
+              "max_workers=1";
+              "--custom-hhi-path";
+              Path.to_string hhi;
+            |]
         in
         String_asserter.assert_equals "No errors!\n" stdout "no errors";
         let%lwt server_pid = get_server_pid ~root in
@@ -181,10 +328,19 @@ let test_kill_monitor () : bool Lwt.t =
 let () =
   Printexc.record_backtrace true;
   EventLogger.init_fake ();
-  Unit_test.run_all
+  let tests =
     [
-      ("test_start_stop", (fun () -> Lwt_main.run (test_start_stop ())));
-      ("test_typechecking", (fun () -> Lwt_main.run (test_typechecking ())));
-      ("test_kill_server", (fun () -> Lwt_main.run (test_kill_server ())));
-      ("test_kill_monitor", (fun () -> Lwt_main.run (test_kill_monitor ())));
+      ("test_start_stop", test_start_stop);
+      ("test_typechecking", test_typechecking);
+      ("test_kill_server", test_kill_server);
+      ("test_kill_monitor", test_kill_monitor);
     ]
+    |> List.map ~f:(fun (name, f) ->
+           ( name,
+             fun () ->
+               Printf.eprintf
+                 "\n\n\n\n============================\n%s\n========================\n%!"
+                 name;
+               Lwt_main.run (f ()) ))
+  in
+  Unit_test.run_all tests

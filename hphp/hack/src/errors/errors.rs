ad1cf7d30ffa57af3b4a287d19001ff8df1ef0c1
Edwin Smith <smith@meta.com>
2023-03-29 04:22:29 -0700

    factor out error printing
    Summary:
    This diff separates how errors are generated from how they are printed in non-streaming modes.
    `hh_distc --json` is one of the ways errors are printed, and also prints other stats
    from the typecheck besides errors. For now, this includes dep_table_edges_added,
    so CheckResult also includes dep_edge_count.
    
    Reviewed By: nt591
    
    Differential Revision: D44398592
    
    fbshipit-source-id: c33cbfe90b7197ea9ea740b55dbefff4e51e694e

--- hphp/hack/src/errors/errors.rs
+++ hphp/hack/src/errors/errors.rs
@@ -2,11 +2,36 @@
 //
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
+#![feature(is_terminal)]
+
 mod fmt_plain;
 mod fmt_raw;
 mod user_error;
 
+use std::io::IsTerminal;
+
 pub use fmt_plain::*;
 pub use fmt_raw::*;
 pub use oxidized::errors::*;
+pub use relative_path::RelativePathCtx;
 pub use user_error::*;
+
+pub fn print_error_list<'e, I>(roots: &RelativePathCtx, error_format: Format, errs: I)
+where
+    I: IntoIterator<Item = &'e Error>,
+{
+    match error_format {
+        Format::Plain => {
+            for err in errs {
+                println!("{}", FmtPlain(err, roots));
+            }
+        }
+        Format::Raw => {
+            let is_term = std::io::stdout().is_terminal();
+            for err in errs {
+                println!("{}", FmtRaw(err, roots, is_term));
+            }
+        }
+        bad => todo!("{bad}"),
+    }
+}

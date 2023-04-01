d87cafb002705c9e5311d5cf1368bdbfc0ca232a
Edwin Smith <smith@meta.com>
2023-04-01 03:42:06 -0700

    Use tracing-indicatif for progress bars
    Summary:
    At this point, `check::Console` is only used to render progress bars.
    Hh_distc's parse, decl, and typing code has been converted to use `tracing`,
    which associates log events with spans, which are tree-structured units of work.
    
    Tracing-indicatif provides `tracing` listeners that render progress bars, allowing
    us to remove Console tendrils from hh_distc's core async code.
    tracing-indicatif renders this Span tree with spinners by default, and we
    provide custom bar styles for decl & typing, similar to our previous progress bars.
    
    I got rid of the separate progress bar for "Typing Start" it's not very important.
    There's no clean way to have 2 progress bars for 1 span, and it's not worth creating
    a nested span just for the sake of having 2 bars.
    
    Instead, the typing bar starts at length 0, increments length when each batch starts
    (subject to `--num-tasks` throttling), and increments position when each sub-batch
    completes.
    
    Reviewed By: nt591
    
    Differential Revision: D44459892
    
    fbshipit-source-id: c1f4e91592186fb3ad0cf1828861195bc784d448

--- hphp/hack/src/errors/errors.rs
+++ hphp/hack/src/errors/errors.rs
@@ -2,13 +2,11 @@
 //
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
-#![feature(is_terminal)]
-
 mod fmt_plain;
 mod fmt_raw;
 mod user_error;
 
-use std::io::IsTerminal;
+use std::io::Write;
 
 pub use fmt_plain::*;
 pub use fmt_raw::*;
@@ -16,22 +14,32 @@ pub use oxidized::errors::*;
 pub use relative_path::RelativePathCtx;
 pub use user_error::*;
 
-pub fn print_error_list<'e, I>(roots: &RelativePathCtx, error_format: Format, errs: I)
+/// Render this list of errors using the requested error_format.
+/// If `is_term` is true, ansi colors and styles will be used according to the format.
+/// Absolute paths are formed (at the discretion of the error format) from error
+/// positions using the provided RelativePathCtx.
+pub fn print_error_list<'e, I>(
+    mut w: impl Write,
+    is_term: bool,
+    roots: &RelativePathCtx,
+    error_format: Format,
+    errs: I,
+) -> std::io::Result<()>
 where
     I: IntoIterator<Item = &'e Error>,
 {
     match error_format {
         Format::Plain => {
             for err in errs {
-                println!("{}", FmtPlain(err, roots));
+                writeln!(w, "{}", FmtPlain(err, roots))?;
             }
         }
         Format::Raw => {
-            let is_term = std::io::stdout().is_terminal();
             for err in errs {
-                println!("{}", FmtRaw(err, roots, is_term));
+                writeln!(w, "{}", FmtRaw(err, roots, is_term))?;
             }
         }
         bad => todo!("{bad}"),
     }
+    Ok(())
 }

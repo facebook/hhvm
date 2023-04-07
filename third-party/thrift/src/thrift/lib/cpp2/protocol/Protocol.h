f9797fe98479c2582c8d9fb9512429e0243ce5c2
Dave Kwon <dokwon@meta.com>
2023-04-06 18:36:26 -0700

    Add return false for invalid path for `validate_bool`
    Reviewed By: Mizuchi
    
    Differential Revision: D44758927
    
    fbshipit-source-id: 4f323879a0958911b683d73b06517c73561abb0a

--- third-party/thrift/src/thrift/lib/cpp2/protocol/Protocol.h
+++ third-party/thrift/src/thrift/lib/cpp2/protocol/Protocol.h
@@ -136,6 +136,7 @@ inline bool validate_bool(uint8_t value) {
   return value;
 invalid:
   LOG(FATAL) << "invalid bool value";
+  return false;
 #else
   // Store in a volatile variable to prevent the compiler from optimizing the
   // check away.

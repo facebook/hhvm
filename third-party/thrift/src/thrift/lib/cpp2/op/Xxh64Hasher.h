4f578839503de9d6bf501fab94c047c263a1507a
Taras Tsugrii <ttsugrii@meta.com>
2023-04-07 17:40:53 -0700

    Add Xxh64 hasher.
    Summary: XXH3 64 bit is one of the fastest hashers.
    
    Reviewed By: thedavekwon
    
    Differential Revision: D44797954
    
    fbshipit-source-id: ca6aff607612442f1e138fdbd2d2af1e366ef70c

---
+++ third-party/thrift/src/thrift/lib/cpp2/op/Xxh64Hasher.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <type_traits>
+
+#include <folly/Hash.h>
+#include <folly/Optional.h>
+#include <folly/Range.h>
+#include <folly/io/IOBuf.h>
+
+#include <xxhash.h>
+
+namespace apache {
+namespace thrift {
+namespace op {
+
+class Xxh64Hasher {
+ public:
+  Xxh64Hasher() : state_{XXH3_createState(), &XXH3_freeState} {
+    XXH3_64bits_reset(state_.get());
+  }
+  XXH64_hash_t getResult() const {
+    if (!result_.has_value())
+      throw std::runtime_error("getResult called on non finalized hasher");
+    return *result_;
+  }
+
+  template <typename T>
+  constexpr std::enable_if_t<std::is_arithmetic_v<T>> combine(const T& val) {
+    XXH3_64bits_update(state_.get(), (const unsigned char*)&val, sizeof(val));
+  }
+  void combine(const folly::IOBuf& value) {
+    for (const auto& buf : value) {
+      combine(buf);
+    }
+  }
+  void combine(folly::ByteRange value) {
+    XXH3_64bits_update(state_.get(), value.data(), value.size());
+  }
+  void combine(const Xxh64Hasher& other) {
+    if (!other.result_.has_value())
+      throw std::runtime_error("cannot combine non finalized hasher");
+    combine(*other.result_);
+  }
+
+  void finalize() { result_ = XXH3_64bits_digest(state_.get()); }
+
+  bool operator<(const Xxh64Hasher& other) const {
+    if (!result_.has_value())
+      throw std::runtime_error("less then called on non finalized hasher");
+    if (!other.result_.has_value())
+      throw std::runtime_error("non finalized hasher passed to less then");
+    return *result_ < *other.result_;
+  }
+
+ private:
+  folly::Optional<XXH64_hash_t> result_;
+  std::unique_ptr<XXH3_state_t, decltype(&XXH3_freeState)> state_;
+};
+
+} // namespace op
+} // namespace thrift
+} // namespace apache
---
+++ third-party/thrift/src/thrift/lib/cpp2/op/Xxh64HasherTest.cpp
@@ -0,0 +1,240 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <thrift/lib/cpp2/hash/DeterministicHash.h>
+#include <thrift/lib/cpp2/op/Xxh64Hasher.h>
+#include <thrift/lib/rust/deterministic_hash/src/tests/gen-cpp2/test_structs_constants.h>
+#include <thrift/lib/rust/deterministic_hash/src/tests/gen-cpp2/test_structs_types.h>
+#include <thrift/lib/rust/deterministic_hash/src/tests/gen-cpp2/test_structs_types_custom_protocol.h>
+
+#include <cstdint>
+
+#include <folly/Range.h>
+#include <folly/io/IOBuf.h>
+#include <folly/portability/GTest.h>
+
+using apache::thrift::hash::deterministic_hash;
+
+namespace apache {
+namespace thrift {
+namespace op {
+
+TEST(Xxh64HasherTest, checkCombineBool) {
+  Xxh64Hasher hasher1;
+  hasher1.combine(true);
+  hasher1.finalize();
+  Xxh64Hasher hasher2;
+  hasher2.combine(false);
+  hasher2.finalize();
+  EXPECT_NE(hasher1.getResult(), hasher2.getResult());
+}
+
+TEST(Xxh64HasherTest, checkCombineInt8) {
+  Xxh64Hasher hasher1;
+  hasher1.combine(static_cast<std::int8_t>(-1));
+  hasher1.finalize();
+  Xxh64Hasher hasher2;
+  hasher2.combine(static_cast<std::int8_t>(0));
+  hasher2.finalize();
+  EXPECT_NE(hasher1.getResult(), hasher2.getResult());
+  Xxh64Hasher hasher3;
+  hasher3.combine(static_cast<std::int8_t>(+1));
+  hasher3.finalize();
+  EXPECT_NE(hasher2.getResult(), hasher3.getResult());
+}
+
+TEST(Xxh64HasherTest, checkCombineInt16) {
+  Xxh64Hasher hasher1;
+  hasher1.combine(static_cast<std::int16_t>(-1));
+  hasher1.finalize();
+  Xxh64Hasher hasher2;
+  hasher2.combine(static_cast<std::int16_t>(0));
+  hasher2.finalize();
+  EXPECT_NE(hasher1.getResult(), hasher2.getResult());
+  Xxh64Hasher hasher3;
+  hasher3.combine(static_cast<std::int16_t>(+1));
+  hasher3.finalize();
+  EXPECT_NE(hasher2.getResult(), hasher3.getResult());
+}
+
+TEST(Xxh64HasherTest, checkCombineInt32) {
+  Xxh64Hasher hasher1;
+  hasher1.combine(static_cast<std::int32_t>(-1));
+  hasher1.finalize();
+  Xxh64Hasher hasher2;
+  hasher2.combine(static_cast<std::int32_t>(0));
+  hasher2.finalize();
+  EXPECT_NE(hasher1.getResult(), hasher2.getResult());
+  Xxh64Hasher hasher3;
+  hasher3.combine(static_cast<std::int32_t>(+1));
+  hasher3.finalize();
+  EXPECT_NE(hasher2.getResult(), hasher3.getResult());
+}
+
+TEST(Xxh64HasherTest, checkCombineInt64) {
+  Xxh64Hasher hasher1;
+  hasher1.combine(static_cast<std::int64_t>(-1));
+  hasher1.finalize();
+  Xxh64Hasher hasher2;
+  hasher2.combine(static_cast<std::int64_t>(0));
+  hasher2.finalize();
+  EXPECT_NE(hasher1.getResult(), hasher2.getResult());
+  Xxh64Hasher hasher3;
+  hasher3.combine(static_cast<std::int64_t>(+1));
+  hasher3.finalize();
+  EXPECT_NE(hasher2.getResult(), hasher3.getResult());
+}
+
+TEST(Xxh64HasherTest, checkCombineFloat) {
+  Xxh64Hasher hasher1;
+  hasher1.combine(static_cast<float>(-1.0));
+  hasher1.finalize();
+  Xxh64Hasher hasher2;
+  hasher2.combine(static_cast<float>(0.0));
+  hasher2.finalize();
+  EXPECT_NE(hasher1.getResult(), hasher2.getResult());
+  Xxh64Hasher hasher3;
+  hasher3.combine(static_cast<float>(+1.0));
+  hasher3.finalize();
+  EXPECT_NE(hasher2.getResult(), hasher3.getResult());
+}
+
+TEST(Xxh64HasherTest, checkCombineDouble) {
+  Xxh64Hasher hasher1;
+  hasher1.combine(static_cast<double>(-1.0));
+  hasher1.finalize();
+  Xxh64Hasher hasher2;
+  hasher2.combine(static_cast<double>(0.0));
+  hasher2.finalize();
+  EXPECT_NE(hasher1.getResult(), hasher2.getResult());
+  Xxh64Hasher hasher3;
+  hasher3.combine(static_cast<double>(+1.0));
+  hasher3.finalize();
+  EXPECT_NE(hasher2.getResult(), hasher3.getResult());
+}
+
+TEST(Xxh64HasherTest, checkCombineIOBuf) {
+  Xxh64Hasher hasher1;
+  hasher1.finalize();
+  Xxh64Hasher hasher2;
+  auto bufA = folly::IOBuf::wrapBuffer(folly::range("abc"));
+  hasher2.combine(*bufA);
+  hasher2.finalize();
+  EXPECT_NE(hasher1.getResult(), hasher2.getResult());
+
+  Xxh64Hasher hasher3;
+  Xxh64Hasher hasherCopy;
+  auto bufB = folly::IOBuf::wrapBuffer(folly::range("def"));
+  hasherCopy.combine(*bufA);
+  hasherCopy.combine(*bufB);
+  hasherCopy.finalize();
+  bufA->prependChain(std::move(bufB));
+  hasher3.combine(*bufA);
+  hasher3.finalize();
+  EXPECT_EQ(hasherCopy.getResult(), hasher3.getResult());
+}
+
+TEST(Xxh64HasherTest, checkCombineByteRange) {
+  Xxh64Hasher hasher1;
+  hasher1.finalize();
+  Xxh64Hasher hasher2;
+  hasher2.combine(folly::range("abc"));
+  hasher2.finalize();
+  EXPECT_NE(hasher1.getResult(), hasher2.getResult());
+  Xxh64Hasher hasher3;
+  hasher3.combine(folly::range("abc"));
+  hasher3.combine(folly::range(""));
+  hasher3.finalize();
+  EXPECT_NE(hasher3.getResult(), hasher2.getResult());
+}
+
+TEST(Xxh64HasherTest, checkCombineXxh64Hasher) {
+  Xxh64Hasher hasher1;
+  hasher1.finalize();
+
+  Xxh64Hasher hasher4;
+  hasher4.combine(folly::range("abc"));
+  hasher4.finalize();
+  Xxh64Hasher hasher5;
+  hasher5.combine(hasher4);
+  hasher5.finalize();
+  EXPECT_NE(hasher1.getResult(), hasher5.getResult());
+  EXPECT_NE(hasher4.getResult(), hasher5.getResult());
+}
+
+TEST(Xxh64HasherTest, checkLess) {
+  Xxh64Hasher hasher1;
+  hasher1.combine(folly::range("abc"));
+  hasher1.finalize();
+  Xxh64Hasher hasher2;
+  hasher2.finalize();
+  EXPECT_TRUE(hasher2 < hasher1);
+  Xxh64Hasher hasher3;
+  hasher3.combine(folly::range("abc"));
+  hasher3.finalize();
+  EXPECT_FALSE(hasher1 < hasher3);
+  EXPECT_FALSE(hasher3 < hasher1);
+}
+
+TEST(Xxh64HasherTest, e2eSimpleStruct) {
+  SimpleStruct1 s;
+  s.byte_() = 8;
+  s.i_16() = 12;
+  s.i_32() = 1;
+  s.i_64() = 2;
+  s.f_32() = 1.0;
+  s.f_64() = 2.0;
+  s.str_with_value() = "abc";
+  s.str_empty() = "";
+  s.bool_true() = true;
+  s.bool_false() = false;
+  s.binary_() = {{1, 2, 3}};
+  auto result = hash::deterministic_hash<Xxh64Hasher>(s);
+  EXPECT_EQ(result, 3750621144346093671UL);
+}
+
+TEST(Xxh64HasherTest, e2eComplexStruct) {
+  ComplexStruct s;
+
+  s.l() = {"a", "ab", "abc"};
+  s.s() = {"a", "ab", "abc"};
+  s.m() = {{"aa", "bb"}, {"cc", "dd"}, {"ee", "ff"}};
+  s.ml() = {
+      {"aa", {1, 2, 3}},
+      {"bb", {}},
+      {"cc", {3, 2}},
+      {"ee",
+       {
+           1,
+           2,
+           3,
+       }},
+      {"ff", {}}};
+
+  s.mm() = {
+      {"aa", {{1, 2}}},
+      {"bb", {}},
+      {"cc", {{2, 3}, {3, 2}}},
+      {"ee", {{3, 4}}},
+      {"ff", {}}};
+
+  auto result = hash::deterministic_hash<Xxh64Hasher>(s);
+  EXPECT_EQ(result, 2487349242360451610UL);
+}
+
+} // namespace op
+} // namespace thrift
+} // namespace apache

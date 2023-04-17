991e33b196e66875eb2e676b8abae2f3704198e0
Oguz Ulgen <oulgen@meta.com>
2023-04-16 23:52:02 -0700

    Make hphp/test/slow/packages/basic-1.php not order dependant
    Summary: This test's output depended on insertion order. Any kind of shuffling breaks the output. Lets clean that.
    
    Reviewed By: ricklavoie
    
    Differential Revision: D45033669
    
    fbshipit-source-id: 1995f585c8a0ac88e9e23a7bc4c2217a3fc3adff

--- hphp/test/slow/packages/basic-1.php
+++ hphp/test/slow/packages/basic-1.php
@@ -1,7 +1,29 @@
 <?hh
 
+function get_vec($vec) {
+  $vec = HH\Lib\Vec\sort($vec);
+  return "[".HH\Lib\Str\join($vec, ", ")."]";
+}
+
+function print_dict($dict, $level = 1) {
+  $dict = HH\Lib\Dict\sort_by_key($dict);
+  foreach ($dict as $k => $v) {
+    for ($i = 0; $i < $level; $i++) {
+      echo "  ";
+    }
+    if ($v is vec) {
+      echo "$k => ". get_vec($v) . "\n";
+    } else {
+      echo "$k\n";
+      print_dict($v, $level + 1);
+    }
+  }
+}
+
 <<__EntryPoint>>
 function main() {
-  var_dump(HH\get_all_packages());
-  var_dump(HH\get_all_deployments());
+  echo "Packages\n";
+  print_dict(HH\get_all_packages());
+  echo "Deployments\n";
+  print_dict(HH\get_all_deployments());
 }

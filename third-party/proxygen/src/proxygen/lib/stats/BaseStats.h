e3d14bdfee18d8334b06e973ee4effcaa695404a
Jerry Liu <jerryliu55@meta.com>
2023-04-26 12:20:34 -0700

    Delete all-time aggregations for non-histogram timeseries
    Summary: All-time timeseries are not as useful as .60 windowed one in production.
    
    Reviewed By: kwaugh
    
    Differential Revision: D44987059
    
    fbshipit-source-id: e9f4dd86accb20f14fa0d6d815de9aa1cd681632

--- third-party/proxygen/src/proxygen/lib/stats/BaseStats.h
+++ third-party/proxygen/src/proxygen/lib/stats/BaseStats.h
@@ -10,6 +10,8 @@
 
 #include <fb303/ThreadCachedServiceData.h>
 
+DECLARE_bool(basestats_all_time_timeseries);
+
 namespace proxygen {
 
 /*
@@ -38,7 +40,32 @@ class BaseStats {
   // therefore aliasing it to TLTTimeSeriesMinute which only
   // exports .60 counters
   using TLTimeseries = TLTimeseriesMinute;
-  using TLTimeseriesMinuteAndAllTime = facebook::fb303::MinuteTimeseriesWrapper;
+  // Wrapper which constructs windowed 60s and conditionally all-time timeseries
+  // based on gflag. All-time timeseries' only use case is in prospect tests
+  // where windowed timeseries cause test flakiness if the proxygend instance is
+  // alive for >60s. The underlying timeseries object is constructed on the heap
+  // to work around compiler error of the fb303 objects having no default
+  // constructor.
+  class TLTimeseriesMinuteAndAllTime final {
+   public:
+    template <typename... Args>
+    explicit TLTimeseriesMinuteAndAllTime(std::string name,
+                                          const Args&... args) {
+      if (FLAGS_basestats_all_time_timeseries) {
+        impl_ = std::make_unique<facebook::fb303::MinuteTimeseriesWrapper>(
+            std::move(name), args...);
+      } else {
+        impl_ = std::make_unique<facebook::fb303::MinuteOnlyTimeseriesWrapper>(
+            std::move(name), args...);
+      }
+    }
+    void add(int64_t value = 1) {
+      impl_->add(value);
+    }
+
+   private:
+    std::unique_ptr<facebook::fb303::TimeseriesWrapperBase> impl_;
+  };
 
   using TLHistogram = facebook::fb303::MinuteOnlyHistogram;
   using TLHistogramMinuteAndAllTime = facebook::fb303::HistogramWrapper;

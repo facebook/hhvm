db30e442eb0ddeed6b0d97d0c1fee7e4e327e425
Lucian Wischik <ljw@meta.com>
2023-05-01 11:15:40 -0700

    remove layers of indirection in naming_errors
    Summary:
    This diff is an RFC. I'd be grateful for your opinion on whether you think the new form of this test is easier to read.
    
    I was struggling too hard to read the test. It had too many layers of indirection. I think my brain prefers to see a test just as a straightforward sequence of operations, with everything explicit.
    
    Reviewed By: Wilfred
    
    Differential Revision: D45419884
    
    fbshipit-source-id: c6a09691c30dae144c042f8bfd66647342bf8fb0

--- hphp/hack/test/integration_ml/ide/test_naming_errors.ml
+++ hphp/hack/test/integration_ml/ide/test_naming_errors.ml
@@ -10,151 +10,98 @@
 
 module Test = Integration_test_base
 
-let foo_returns_int_name = "foo_returns_int.php"
-
-let foo_returns_string_name = "foo_returns_string_name.php"
-
-let foo_contents =
-  Printf.sprintf "<?hh // strict
-/* HH_FIXME[4336] */
-function foo() : %s {
-
-}
-"
-
-let foo_returns_int_contents = foo_contents "int"
-
-let foo_returns_string_contents = foo_contents "string"
-
-let bar_expects_int_name = "bar_expects_int.php"
-
-let bar_expects_string_name = "bar_expects_string.php"
-
-let bar_contents x =
-  Printf.sprintf
-    "<?hh // strict
-
-function bar_%s(): %s {
-  return foo();
-}
-"
-    x
-    x
-
-let foo_unbound_diagnostics =
-  SMap.of_list
-    [
-      ( "/bar_expects_int.php",
-        SSet.of_list
-          [
-            "File \"/bar_expects_int.php\", line 4, characters 10-12:
-Unbound name: `foo` (a global function) (Naming[2049])";
-            "File \"/bar_expects_int.php\", line 4, characters 10-12:
-Unbound name (typing): `foo` (Typing[4107])";
-          ] );
-      ( "/bar_expects_string.php",
-        SSet.of_list
-          [
-            "File \"/bar_expects_string.php\", line 4, characters 10-12:
-Unbound name: `foo` (a global function) (Naming[2049])";
-            "File \"/bar_expects_string.php\", line 4, characters 10-12:
-Unbound name (typing): `foo` (Typing[4107])";
-          ] );
-    ]
-
-let foo_returns_string_diagnostics =
-  "
-/bar_expects_int.php:
-File \"/bar_expects_int.php\", line 4, characters 10-14:
-Invalid return type (Typing[4110])
-  File \"/bar_expects_int.php\", line 3, characters 21-23:
-  Expected `int`
-  File \"/foo_returns_string_name.php\", line 3, characters 18-23:
-  But got `string`
-
-/bar_expects_string.php:
-"
-
-let foo_duplicate_diagnostics =
-  "
-/foo_returns_int.php:
-File \"/foo_returns_int.php\", line 3, characters 10-12:
-Name already bound: `foo` (Naming[2012])
-  File \"/foo_returns_string_name.php\", line 3, characters 10-12:
-  Previous definition is here
-"
-
-let foo_returns_int_diagnostics =
-  "
-/bar_expects_int.php:
-/bar_expects_string.php:
-File \"/bar_expects_string.php\", line 4, characters 10-14:
-Invalid return type (Typing[4110])
-  File \"/bar_expects_string.php\", line 3, characters 24-29:
-  Expected `string`
-  File \"/foo_returns_int.php\", line 3, characters 18-20:
-  But got `int`
-
-/foo_returns_int.php:
-"
-
-let root = "/"
-
-let hhconfig_filename = Filename.concat root ".hhconfig"
-
-let hhconfig_contents =
-  "
-allowed_fixme_codes_strict = 4336
-allowed_decl_fixme_codes = 4336
-"
-
-let load_config () =
+let test () =
+  let root = "/" in
+  Relative_path.set_path_prefix Relative_path.Root (Path.make root);
+  TestDisk.set (Filename.concat root ".hhconfig") "";
   let options = ServerArgs.default_options ~root in
   let (custom_config, _) = ServerConfig.load ~silent:false options in
-  custom_config
-
-let test () =
-  Relative_path.set_path_prefix Relative_path.Root (Path.make root);
-  TestDisk.set hhconfig_filename hhconfig_contents;
-  let custom_config = load_config () in
   let env = Test.setup_server ~custom_config () in
   let env = Test.connect_persistent_client env in
 
   (* Two bar files use `foo` which is still unbound. *)
   let env =
-    Test.open_file env bar_expects_int_name ~contents:(bar_contents "int")
+    Test.open_file
+      env
+      "bar_expects_int.php"
+      ~contents:"<?hh\nfunction bar_int(): int { return foo(); }\n"
   in
   let env =
-    Test.open_file env bar_expects_string_name ~contents:(bar_contents "string")
+    Test.open_file
+      env
+      "bar_expects_string.php"
+      ~contents:"<?hh\nfunction bar_string(): string { return foo(); }\n"
   in
   let env = Test.wait env in
   let (env, loop_outputs) = Test.(run_loop_once env default_loop_input) in
-  Test.assert_diagnostics loop_outputs foo_unbound_diagnostics;
+  Test.assert_diagnostics
+    loop_outputs
+    (SMap.of_list
+       [
+         ( "/bar_expects_int.php",
+           SSet.of_list
+             [
+               "File \"/bar_expects_int.php\", line 2, characters 34-36:\nUnbound name: `foo` (a global function) (Naming[2049])";
+               "File \"/bar_expects_int.php\", line 2, characters 34-36:\nUnbound name (typing): `foo` (Typing[4107])";
+             ] );
+         ( "/bar_expects_string.php",
+           SSet.of_list
+             [
+               "File \"/bar_expects_string.php\", line 2, characters 40-42:\nUnbound name: `foo` (a global function) (Naming[2049])";
+               "File \"/bar_expects_string.php\", line 2, characters 40-42:\nUnbound name (typing): `foo` (Typing[4107])";
+             ] );
+       ]);
 
   (* Now create `foo` in foo_string.php which returns string. *)
   let env =
     Test.open_file
       env
-      foo_returns_string_name
-      ~contents:foo_returns_string_contents
+      "foo_returns_string_name.php"
+      ~contents:"<?hh\nfunction foo() : string { return \"\"; }\n"
   in
   let env = Test.wait env in
   let (env, loop_output) = Test.(run_loop_once env default_loop_input) in
-  Test.assert_diagnostics_string loop_output foo_returns_string_diagnostics;
+  Test.assert_diagnostics_string
+    loop_output
+    "
+/bar_expects_int.php:
+File \"/bar_expects_int.php\", line 2, characters 34-38:\nInvalid return type (Typing[4110])
+  File \"/bar_expects_int.php\", line 2, characters 21-23:\n  Expected `int`
+  File \"/foo_returns_string_name.php\", line 2, characters 18-23:\n  But got `string`
+
+/bar_expects_string.php:
+";
 
   (* Create another `foo` in foo_int.php which returns int. *)
   let env =
-    Test.open_file env foo_returns_int_name ~contents:foo_returns_int_contents
+    Test.open_file
+      env
+      "foo_returns_int.php"
+      ~contents:"<?hh\nfunction foo() : int { return 1; }\n"
   in
   let env = Test.wait env in
   let (env, loop_output) = Test.(run_loop_once env default_loop_input) in
   Test.assert_diagnostics_in
     loop_output
-    ~filename:foo_returns_int_name
-    foo_duplicate_diagnostics;
+    ~filename:"foo_returns_int.php"
+    "
+/foo_returns_int.php:
+File \"/foo_returns_int.php\", line 2, characters 10-12:\nName already bound: `foo` (Naming[2012])
+  File \"/foo_returns_string_name.php\", line 2, characters 10-12:\n  Previous definition is here
+";
 
   (* Erase foo_string.php. *)
-  let env = Test.open_file env foo_returns_string_name ~contents:"" in
+  let env = Test.open_file env "foo_returns_string_name.php" ~contents:"" in
   let env = Test.wait env in
   let (_, loop_output) = Test.(run_loop_once env default_loop_input) in
-  Test.assert_diagnostics_string loop_output foo_returns_int_diagnostics
+  Test.assert_diagnostics_string
+    loop_output
+    "
+/bar_expects_int.php:
+/bar_expects_string.php:
+File \"/bar_expects_string.php\", line 2, characters 40-44:\nInvalid return type (Typing[4110])
+  File \"/bar_expects_string.php\", line 2, characters 24-29:\n  Expected `string`
+  File \"/foo_returns_int.php\", line 2, characters 18-20:\n  But got `int`
+
+/foo_returns_int.php:
+"

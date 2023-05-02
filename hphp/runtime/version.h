652dd2e7e0c61121d16aded83f3906d86923e098
Wilfred Hughes <wilfred@meta.com>
2023-05-02 15:50:45 -0700

    Targetting 4.172.4

--- hphp/runtime/version.h
+++ hphp/runtime/version.h
@@ -11,8 +11,8 @@
 #ifndef HHVM_VERSION_OVERRIDE
 # define HHVM_VERSION_MAJOR 4
 # define HHVM_VERSION_MINOR 172
-# define HHVM_VERSION_PATCH 3
-# define HHVM_VERSION_SUFFIX ""
+# define HHVM_VERSION_PATCH 4
+# define HHVM_VERSION_SUFFIX "-dev"
 #endif
 
 /* HHVM_VERSION_ID minus the patch number

052d63588015439ac3400d6e7261cc07a39006a0
Cameron Pickett <pickett@meta.com>
2023-05-03 15:23:20 -0700

    Fix UB in shardalloc
    Summary:
    D44476382 enables building rust std library from source, which allows us to enable debug assertions in Rust. This caught an issue with undefined behaviour in Hack, where we were calling `ptr::write` with a misaligned pointer. With debugger attached, the bad address was aligned to alignment = 1, which is too small for `AllocHeader`.
    
    This change proposes aligning the pointer to at *least* the alignment of AllocHeader, in the case where the provided `l` is too small.
    
    Reviewed By: dtolnay
    
    Differential Revision: D45539401
    
    fbshipit-source-id: 51945d1f80dab24c6b9a583afe708282b2a3be8a

--- hphp/hack/src/shmrs/shardalloc.rs
+++ hphp/hack/src/shmrs/shardalloc.rs
@@ -212,9 +212,14 @@ impl ShardAllocControlData {
     fn alloc_slice_within_chunk(&mut self, l: Layout) -> Option<NonNull<[u8]>> {
         let size = l.size();
         let header = AllocHeader::new(size);
+        let header_align = std::mem::align_of::<AllocHeader>();
         let header_size = std::mem::size_of::<AllocHeader>();
 
-        let align_offset = self.current_next.align_offset(l.align());
+        // We must align to both the AllocHeader and provided layout, so that we can
+        // safely `ptr::write` the header at the address of `pointer` below.
+        let align_offset = self
+            .current_next
+            .align_offset(std::cmp::max(l.align(), header_align));
         let mut pointer = unsafe { self.current_next.add(align_offset) };
         let new_current = unsafe { pointer.add(header_size).add(size) };
 
@@ -223,6 +228,7 @@ impl ShardAllocControlData {
         }
 
         debug_assert!(!new_current.is_null());
+        debug_assert_eq!(pointer.align_offset(std::mem::align_of::<AllocHeader>()), 0);
         self.current_next = new_current;
         // Write header into memory
         unsafe { std::ptr::write(pointer as *mut AllocHeader, header) };

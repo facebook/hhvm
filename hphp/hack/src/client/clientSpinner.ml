777e39509c0694978815f65a82a7cb729b3ddfe4
Lucian Wischik <ljw@meta.com>
2023-05-05 22:25:58 -0700

    rename
    Summary:
    This diff purely renames two fields, to better reflect their nature.
    
    BEFORE
    ```
      message: string option;  (** "hh_server is busy: [<message>] <spinner>" *)
      non_null_message: string option;
          (** the most recent non-null message to have been reported *)
    ```
    
    AFTER
    ```
      message_or_hidden: string option;
          (** "hh_server is busy: [<message>] <spinner>".
          This may be [None] if it has been temporarily suppressed (by [report None]). *)
      message: string option;
          (** the most recent non-None message to have been reported *)
    ```
    
    What I came to realize is that when someone calls `ClientSpinner.report (Some x)` it means to update the spinner message, but when they call `ClientSpinner.report None` then it means to *temporarily hide the spinner*.
    
    Reviewed By: CatherineGasnier
    
    Differential Revision: D45597992
    
    fbshipit-source-id: 1ca5d4062a5f659edeebd437cba678e21e49f763

--- hphp/hack/src/client/clientSpinner.ml
+++ hphp/hack/src/client/clientSpinner.ml
@@ -25,10 +25,21 @@ let emoji_chars =
     (* Hundred Points *)
   |]
 
+type message =
+  | Init
+      (** There has not yet been a call to [report (Some s)], so there's nothing to show *)
+  | Message of {
+      text: string;
+          (** This reflects the most recent call to [report (Some text)].
+          It is displayed as "hh_server is busy: [<s>] <spinner>". *)
+      is_hidden: bool;
+          (** This is true when [report None] has been called more recently than [report Some].
+          It means that the text won't be displayed on stderr.
+          (However, the text is still used for heartbeat and logging). *)
+    }
+
 type state = {
-  message: string option;  (** "hh_server is busy: [<message>] <spinner>" *)
-  non_null_message: string option;
-      (** the most recent non-null message to have been reported *)
+  message: message;
   to_stderr: bool;  (** should this be displayed on stderr? *)
   angery_reaccs_only: bool; (* do we use emoji_chars or ascii_chars? *)
   index: int;  (** which of the four chars is used as spinner *)
@@ -38,20 +49,15 @@ type state = {
 is reflected in stderr -- either some message, or nothing. *)
 let state : state ref =
   ref
-    {
-      message = None;
-      non_null_message = None;
-      to_stderr = false;
-      angery_reaccs_only = false;
-      index = 0;
-    }
+    { message = Init; to_stderr = false; angery_reaccs_only = false; index = 0 }
 
 let get_latest_report () : string option =
-  match (!state.message, !state.non_null_message) with
-  | (Some message, _) -> Some message
-  | (None, Some message) -> Some ("[hidden] " ^ message)
-  | (None, None) -> None
+  match !state.message with
+  | Message { text; is_hidden = false } -> Some text
+  | Message { text; is_hidden = true } -> Some ("[hidden] " ^ text)
+  | Init -> None
 
+(** Used so that when we Hh_logger.log a spinner change, we can show wall-time. *)
 let start_time : float = Unix.gettimeofday ()
 
 let start_heartbeat_telemetry () : unit =
@@ -67,10 +73,14 @@ let start_heartbeat_telemetry () : unit =
 the old stderr (if there was anything on it), and displaying a new thing
 on stderr (if needed). *)
 let update_stderr (old_state : state) ~(new_state : state) : unit =
-  let was_present = old_state.to_stderr && Option.is_some old_state.message in
+  let was_present =
+    match old_state.message with
+    | Message { is_hidden = false; _ } when old_state.to_stderr -> true
+    | _ -> false
+  in
   if was_present then Tty.print_clear_line stderr;
   match new_state.message with
-  | Some message when new_state.to_stderr ->
+  | Message { is_hidden = false; text } when new_state.to_stderr ->
     let chars =
       if new_state.angery_reaccs_only then
         emoji_chars
@@ -78,7 +88,7 @@ let update_stderr (old_state : state) ~(new_state : state) : unit =
         ascii_chars
     in
     let spinner = Array.get chars new_state.index in
-    Tty.eprintf "hh_server is busy: [%s] %s%!" message spinner
+    Tty.eprintf "hh_server is busy: [%s] %s%!" text spinner
   | _ -> ()
 
 (** Never-ending background process to update the spinner on stderr once a second.
@@ -95,20 +105,28 @@ let animator : unit Lwt.t option ref = ref None
 
 let report ~(to_stderr : bool) ~(angery_reaccs_only : bool) :
     string option -> unit =
- fun message ->
+ fun next ->
   if Option.is_none !animator then animator := Some (animate ());
-  if
-    Option.is_some message
-    && not (Option.equal String.equal !state.message message)
-  then
-    Hh_logger.log
-      "spinner %0.1fs: [%s]"
-      (Unix.gettimeofday () -. start_time)
-      (Option.value_exn message);
-  let non_null_message = Option.first_some message !state.non_null_message in
-  let new_state =
-    { !state with message; non_null_message; to_stderr; angery_reaccs_only }
+  let message =
+    match (!state.message, next) with
+    | (Message { text = prev; _ }, Some next) when String.equal prev next ->
+      (* text is unchanged. But if it was hidden before, now let it be shown *)
+      Message { text = next; is_hidden = false }
+    | (_, Some next) ->
+      (* text has changed *)
+      Hh_logger.log
+        "spinner %0.1fs: [%s]"
+        (Unix.gettimeofday () -. start_time)
+        next;
+      Message { text = next; is_hidden = false }
+    | (Message { text; _ }, None) ->
+      (* pre-existing text gets hidden *)
+      Message { text; is_hidden = true }
+    | (Init, None) ->
+      (* no-op, because we're hiding when no text has yet been reported *)
+      Init
   in
+  let new_state = { !state with message; to_stderr; angery_reaccs_only } in
   update_stderr !state ~new_state;
   state := new_state;
   ()

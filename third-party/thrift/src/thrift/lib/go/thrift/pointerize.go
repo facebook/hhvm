1309982a74c58e5f7278dc742338b2461e9970e3
Jack Chistyakov <jchistyakov@meta.com>
2023-05-08 12:39:31 -0700

    Complete pointerize support for all Thrift primitive types
    Summary: Add pointerize support for a few missing types.
    
    Reviewed By: leoleovich
    
    Differential Revision: D45636737
    
    fbshipit-source-id: c68b09fcb6d89da8141cecc7270e4de70d0adb46

--- third-party/thrift/src/thrift/lib/go/thrift/pointerize.go
+++ third-party/thrift/src/thrift/lib/go/thrift/pointerize.go
@@ -38,11 +38,15 @@ package thrift
 func Float32Ptr(v float32) *float32 { return &v }
 func Float64Ptr(v float64) *float64 { return &v }
 func IntPtr(v int) *int             { return &v }
+func Int8Ptr(v int8) *int8          { return &v }
 func Int16Ptr(v int16) *int16       { return &v }
 func Int32Ptr(v int32) *int32       { return &v }
 func Int64Ptr(v int64) *int64       { return &v }
 func StringPtr(v string) *string    { return &v }
+func Uint8Ptr(v uint8) *uint8       { return &v }
+func Uint16Ptr(v uint16) *uint16    { return &v }
 func Uint32Ptr(v uint32) *uint32    { return &v }
 func Uint64Ptr(v uint64) *uint64    { return &v }
 func BoolPtr(v bool) *bool          { return &v }
+func BytePtr(v byte) *byte          { return &v }
 func ByteSlicePtr(v []byte) *[]byte { return &v }

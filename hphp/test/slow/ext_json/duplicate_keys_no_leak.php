76de78ffa36ab46bbcc60ac42bddcbfe8aaea010
Aaron Orenstein <aorenste@meta.com>
2023-05-09 19:24:07 -0700

    Fix duplicate_keys_no_leak test to avoid memory variance
    Reviewed By: ricklavoie
    
    Differential Revision: D45706679
    
    fbshipit-source-id: a256acbcb552170467ad518dd7f05e69a7f597bc

--- hphp/test/slow/ext_json/duplicate_keys_no_leak.php
+++ hphp/test/slow/ext_json/duplicate_keys_no_leak.php
@@ -2,13 +2,21 @@
 
 <<__EntryPoint>>
 function main_duplicate_keys_no_leak() {
+  // Prime the JSON engine.
   json_decode('{"a":"1","a":"2"}', true, 512, JSON_FB_LOOSE);
-  $limit = memory_get_usage(true) + 100000;
+
+  // This test is really noisy.  The intent is to look for leaks in the JSON
+  // decoding. The current min limit at this time is 215744 so we'll make it
+  // somewhat higher to account for variance.
+  $limit = memory_get_usage(true) + 300000;
   ini_set('memory_limit', $limit);
-  for ($i = 0; $i < 100000; $i++) {
+
+  // Run enough iterations that if we leak even 1 byte per loop we'll break the
+  // limit.
+  for ($i = 0; $i < 1000000; $i++) {
     $x = json_decode('{"a":"1","a":"2"}', true, 512, JSON_FB_LOOSE);
-    __hhvm_intrinsics\memory_manager_stats(); // Force memory manager
-                                              // to update accounting.
+    // Force memory manager to update accounting.
+    __hhvm_intrinsics\memory_manager_stats();
     __hhvm_intrinsics\launder_value($x);
   }
   var_dump($x);

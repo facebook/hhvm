35d79976698bb17cb1e97a68a4bd2c61104fbbcf
Lucian Wischik <ljw@meta.com>
2023-05-30 12:47:11 -0700

    6/19 - remove test_non_blocking
    Summary:
    [Diffstack overview: my plan is (1) one by one, port all the tests over to ide_standalone, doing them gradually because different ones require different levels of detail. (2) once that is done, hard-code test_lsp.py to always use ide_standalone, and clean it up. This diff is part of phase 1.]
    
    The "test_non_blocking" test was introduced in D7923548 (May 2018) to prove that hh_server is able to interrupt an ongoing typecheck in order to handle a priority request from the IDE over the persistent connection.
    
    None of this has any meaning any longer, now that we're switch to ide_standalone. I'm therefore deleting the test.
    
    Reviewed By: mheiber
    
    Differential Revision: D46214004
    
    fbshipit-source-id: 211d2c8343d0184afaeaed5d282fcca4bfb7c897

--- hphp/hack/test/integration/test_lsp.py
+++ hphp/hack/test/integration/test_lsp.py
@@ -8317,57 +8317,6 @@ function call_method(ClassWithFooBar $mc): void {
         )
         self.run_spec(spec, variables, wait_for_server=False, use_serverless_ide=True)
 
-    def test_non_blocking(self) -> None:
-        self.prepare_server_environment()
-        variables = self.setup_php_file("non_blocking.php")
-        self.test_driver.start_hh_loop_forever_assert_timeout()
-        spec = (
-            self.initialize_spec(LspTestSpec("non_blocking"), use_serverless_ide=False)
-            .ignore_notifications(method="textDocument/publishDiagnostics")
-            .wait_for_hh_server_ready()
-            .request(
-                line=line(),
-                method="textDocument/definition",
-                params={
-                    "textDocument": {"uri": "${php_file_uri}"},
-                    "position": {"line": 7, "character": 11},
-                },
-                result=[
-                    {
-                        "uri": "file://${root_path}/non_blocking.php",
-                        "range": {
-                            "start": {"line": 2, "character": 9},
-                            "end": {"line": 2, "character": 32},
-                        },
-                        "title": "non_blocking_definition",
-                    }
-                ],
-                wait_id="definition request",
-            )
-            .notification(
-                comment="remove hh_loop_forever() invocation to break the infinite loop",
-                method="textDocument/didOpen",
-                params={
-                    "textDocument": {
-                        "uri": "${root_path}/__hh_loop_forever_foo.php",
-                        "languageId": "hack",
-                        "version": 1,
-                        "text": """\
-<?hh // strict
-
-function __hh_loop_forever_foo(): int {
-  return 4;
-}
-""",
-                    }
-                },
-            )
-            .wait_for_response(wait_id="definition request")
-            .request(line=line(), method="shutdown", params={}, result=None)
-            .notification(method="exit", params={})
-        )
-        self.run_spec(spec, variables, wait_for_server=True, use_serverless_ide=False)
-
     def test_serverless_ide_hierarchy_file_change_on_disk(self) -> None:
         variables = dict(self.prepare_serverless_ide_environment())
         variables.update(self.setup_php_file("incremental_derived.php"))

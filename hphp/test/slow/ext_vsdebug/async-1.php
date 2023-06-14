c3e8d71b0071da679cbb4b88c9676e43b0978616
Oguz Ulgen <oulgen@meta.com>
2023-06-14 09:43:56 -0700

    Clean up vscode tests using a simple repl DSL
    Summary: For writing vscode debugger tests, I wrote a simple DSL. Lets move it to common and use it on other tests to reduce passing around json.
    
    Reviewed By: mdko
    
    Differential Revision: D46703495
    
    fbshipit-source-id: b86167632e75bc2e759c605f7146dfe133691eed

--- hphp/test/slow/ext_vsdebug/async-1.php
+++ hphp/test/slow/ext_vsdebug/async-1.php
@@ -1,52 +1,7 @@
 <?hh
 
-function skipEvents() {
-  $msg = json_decode(getNextVsDebugMessage(), true);
-  while ($msg['type'] == 'event') {
-    if ($msg['event'] === 'output') {
-      $body = $msg['body'];
-      printf("OUT: %s", $body['output']);
-    }
-    $msg = json_decode(getNextVsDebugMessage(), true);
-  }
-  return $msg;
-}
-
-function send($expr) {
-  return sendVsCommand(darray[
-    "command" => "evaluate",
-    "type" => "request",
-    "arguments" =>
-    darray[
-      'expression' => $expr,
-      'context' => 'repl',
-      'frameId' => 2]]);
-}
-
-function expect_int($seq, $i) {
-  $msg = skipEvents();
-  checkObjEqualRecursively($msg, darray[
-    "type" => "response",
-    "command" => "evaluate",
-    "success" => true,
-    'body' => dict[
-      'type' => 'int',
-      'result' => $i,
-    ],
-    "request_seq" => $seq]);
-}
-
-function expect_success($seq) {
-  $msg = skipEvents();
-  checkObjEqualRecursively($msg, darray[
-    "type" => "response",
-    "command" => "evaluate",
-    "success" => true,
-    "request_seq" => $seq]);
-}
-
 <<__EntryPoint>>
-function main(): void {
+function main_async_1(): void {
   chdir(__DIR__);
   require('./common.inc');
   $path = __FILE__ . ".test";
@@ -57,21 +12,21 @@ function main(): void {
     'type' => 'request',
     'arguments' => darray['threadId' => 1],
   ]);
-  skipEvents();
+  skip_events();
 
-  send('await foo_async();') |> expect_int($$, '5');
-  send('await bar_async();') |> expect_int($$, '15');
+  send_eval('await foo_async();') |> expect_int($$, '5');
+  send_eval('await bar_async();') |> expect_int($$, '15');
 
-  send('await foo_async() + 5;') |> expect_int($$, '10');
-  send('5 + await foo_async();') |> expect_int($$, '10');
-  send('await foo_async() + await bar_async();') |> expect_int($$, '20');
+  send_eval('await foo_async() + 5;') |> expect_int($$, '10');
+  send_eval('5 + await foo_async();') |> expect_int($$, '10');
+  send_eval('await foo_async() + await bar_async();') |> expect_int($$, '20');
 
-  send('return await foo_async() + 5;') |> expect_int($$, '10');
-  send('return 5 + await foo_async();') |> expect_int($$, '10');
-  send('return await foo_async() + await bar_async();') |> expect_int($$, '20');
+  send_eval('return await foo_async() + 5;') |> expect_int($$, '10');
+  send_eval('return 5 + await foo_async();') |> expect_int($$, '10');
+  send_eval('return await foo_async() + await bar_async();') |> expect_int($$, '20');
 
-  send('concurrent { $x = await foo_async(); $y = await bar_async(); };') |> expect_success($$);
-  send('$x + $y') |> expect_int($$, '20');
+  send_eval('concurrent { $x = await foo_async(); $y = await bar_async(); };') |> expect_success($$);
+  send_eval('$x + $y') |> expect_int($$, '20');
 
   resumeTarget();
   vsDebugCleanup($testProcess);
--- hphp/test/slow/ext_vsdebug/repl_lambda.php
+++ hphp/test/slow/ext_vsdebug/repl_lambda.php
@@ -1,136 +1,23 @@
 <?hh
 
-function skipEvents() {
-  $msg = json_decode(getNextVsDebugMessage(), true);
-  while ($msg['type'] == 'event') {
-    if ($msg['event'] === 'output') {
-      $body = $msg['body'];
-      if ($body['category'] === 'stdout') {
-        printf("STDOUT: %s", $body['output']);
-      }
-    }
-    $msg = json_decode(getNextVsDebugMessage(), true);
-
-  }
-
-  return $msg;
-}
 <<__EntryPoint>> function main(): void {
   chdir(__DIR__);
   require('./common.inc');
   $path = __FILE__ . ".test";
   $testProcess = vsDebugLaunch($path, true, varray[]);
 
-  $seq = sendVsCommand(darray[
-    "command" => "evaluate",
-    "type" => "request",
-    "arguments" =>
-    darray[
-      'expression' => '$a = 76;',
-      'context' => 'repl']]);
-
-  $msg = skipEvents();
-  checkObjEqualRecursively($msg, darray[
-    "type" => "response",
-    "command" => "evaluate",
-    "success" => true,
-    'body' => dict[
-      'type' => 'int',
-      'result' => '76',
-    ],
-    "request_seq" => $seq]);
+  send_eval('$a = 76;') |> expect_int($$, '76');
 
   echo "---- NEW LAMBDA -----\n";
-
-  $seq = sendVsCommand(darray[
-    "command" => "evaluate",
-    "type" => "request",
-    "arguments" =>
-    darray[
-      'expression' => '(() ==> $a)();',
-      'context' => 'repl']]);
-
-  $msg = skipEvents();
-  checkObjEqualRecursively($msg, darray[
-    "type" => "response",
-    "command" => "evaluate",
-    "success" => true,
-    'body' => dict[
-      'type' => 'int',
-      'result' => '76',
-    ],
-    "request_seq" => $seq]);
-
-  $seq = sendVsCommand(darray[
-    "command" => "evaluate",
-    "type" => "request",
-    "arguments" =>
-    darray[
-      'expression' => '$l1 = () ==> $a;',
-      'context' => 'repl']]);
-
-  $msg = skipEvents();
-  checkObjEqualRecursively($msg, darray[
-    "type" => "response",
-    "command" => "evaluate",
-    "success" => true,
-    "request_seq" => $seq]);
-
-  $seq = sendVsCommand(darray[
-    "command" => "evaluate",
-    "type" => "request",
-    "arguments" =>
-    darray[
-      'expression' => '$l1();',
-      'context' => 'repl']]);
-
-  $msg = skipEvents();
-  checkObjEqualRecursively($msg, darray[
-    "type" => "response",
-    "command" => "evaluate",
-    "success" => true,
-    'body' => dict[
-      'type' => 'int',
-      'result' => '76',
-    ],
-    "request_seq" => $seq]);
+  send_eval('(() ==> $a)();') |> expect_int($$, '76');
+  send_eval('$l1 = () ==> $a;') |> expect_success($$);
+  send_eval('$l1();') |> expect_int($$, '76');
 
   echo "---- OLD LAMBDA -----\n";
+  send_eval('(function () use ($a) { return $a; })()') |> expect_int($$, '76');
+  send_eval('$l2 = function () use ($a) { return $a; };') |> expect_success($$);
 
-  $seq = sendVsCommand(darray[
-    "command" => "evaluate",
-    "type" => "request",
-    "arguments" =>
-    darray[
-      'expression' => '(function () use ($a) { return $a; })()',
-      'context' => 'repl']]);
-
-  $msg = skipEvents();
-  checkObjEqualRecursively($msg, darray[
-    "type" => "response",
-    "command" => "evaluate",
-    "success" => true,
-    'body' => dict[
-      'type' => 'int',
-      'result' => '76',
-    ],
-    "request_seq" => $seq]);
-
-  $seq = sendVsCommand(darray[
-    "command" => "evaluate",
-    "type" => "request",
-    "arguments" =>
-    darray[
-      'expression' => '$l2 = function () use ($a) { return $a; };',
-      'context' => 'repl']]);
-
-  $msg = skipEvents();
-  checkObjEqualRecursively($msg, darray[
-    "type" => "response",
-    "command" => "evaluate",
-    "success" => true,
-    "request_seq" => $seq]);
-
+  // This one requires frame id to be null, so leave it old style
   $seq = sendVsCommand(darray[
     "command" => "evaluate",
     "type" => "request",
@@ -139,7 +26,7 @@ function skipEvents() {
       'expression' => '$l2();',
       'context' => 'repl']]);
 
-  $msg = skipEvents();
+  $msg = skip_events();
   checkObjEqualRecursively($msg, darray[
     "type" => "response",
     "command" => "evaluate",

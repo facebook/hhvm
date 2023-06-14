813b4573d4eab23624098507b2311658dfe6fdb6
Oguz Ulgen <oulgen@meta.com>
2023-06-14 14:12:08 -0700

    Add package_exists tests for the debugger
    Reviewed By: mdko
    
    Differential Revision: D46730246
    
    fbshipit-source-id: 3f163a6e3d69f9392059b636393a8e56369d3f19

---
+++ hphp/test/slow/ext_vsdebug/packages/packages-1.php
@@ -0,0 +1,24 @@
+<?hh
+
+<<__EntryPoint>>
+function main_packages(): void {
+  chdir(__DIR__);
+  require('./../common.inc');
+  $path = __FILE__ . ".test";
+  $testProcess = vsDebugLaunch($path, true, varray[]);
+
+  sendVsCommand(darray[
+    'command' => 'stackTrace',
+    'type' => 'request',
+    'arguments' => darray['threadId' => 1],
+  ]);
+  skip_events();
+
+  send_eval('(int)package_exists(\'foo\');') |> expect_int($$, "1");
+  send_eval('(int)package_exists(\'bar\');') |> expect_int($$, "1");
+  send_eval('(int)package_exists(\'nope\');') |> expect_int($$, "0");
+
+  resumeTarget();
+  vsDebugCleanup($testProcess);
+  echo "Ok!\n";
+}

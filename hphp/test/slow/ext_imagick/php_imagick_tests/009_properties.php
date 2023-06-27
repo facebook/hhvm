217c39a0185c3c543563945f9499d1037c93c350
Gilbert Liu <zpak@meta.com>
2023-06-27 16:39:47 -0700

    build fontconfig-2.14.2 and deps
    Summary:
    build fontconfig-2.14.2 and deps
    upgrade ImageMagick to 6.9.12.89
    
    Reviewed By: dmm-fb
    
    Differential Revision: D46383788
    
    fbshipit-source-id: bf00f30c49dc3c1410cf244168d7266d0f5f8cca

--- hphp/test/slow/ext_imagick/php_imagick_tests/009_properties.php
+++ hphp/test/slow/ext_imagick/php_imagick_tests/009_properties.php
@@ -2,7 +2,7 @@
 <<__EntryPoint>> function main(): void {
 $im = new Imagick();
 $im->newPseudoImage(100, 100, "XC:red");
-$im->setImageFormat("png");
+$im->setImageFormat("PNG");
 
 echo $im->width . "x" . $im->height . "\n";
 echo $im->format;

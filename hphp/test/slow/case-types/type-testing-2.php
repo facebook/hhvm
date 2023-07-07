c205cbeb93405efa577ac07c81a52e94bc642b81
Oguz Ulgen <oulgen@meta.com>
2023-07-06 18:09:41 -0700

    Add more test cases to case types to clarify behavior
    Reviewed By: aorenste
    
    Differential Revision: D47274375
    
    fbshipit-source-id: d675826181264dfd274dffcc9232942615778144

--- hphp/test/slow/case-types/type-testing-2.php
+++ hphp/test/slow/case-types/type-testing-2.php
@@ -7,9 +7,21 @@ class A<reify T> {}
 case type C1 = int | string;
 case type C2 = int | string;
 
+type T1 = C1;
+newtype NT1 = C1;
+case type C3 = C1;
+
 <<__EntryPoint>>
 function main() :mixed{
-  var_dump((new A<C1>) is A<C1>); // true
-  // false because it should use the type as witness
-  var_dump((new A<C1>) is A<C2>);
+  $a = new A<C1>();
+  // True: Identical
+  var_dump($a is A<C1>);
+  // False: It should use the type as witness
+  var_dump($a is A<C2>);
+  // True: Identical through an indirection
+  var_dump($a is A<T1>);
+  // True: Same as regular alias since HHVM does not see newtype
+  var_dump($a is A<NT1>);
+  // False: Same reason as why C2 is not same as C1
+  var_dump($a is A<C3>);
 }

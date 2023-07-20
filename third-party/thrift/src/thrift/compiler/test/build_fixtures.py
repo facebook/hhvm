d677251128e66dc9fe6b163f8d8bcac04f76c5eb
Jun Zhou <junz@meta.com>
2023-07-20 14:31:01 -0700

    fix DeprecationWarning in build_fixtures.py
    Summary:
    > asyncio.get_event_loop()
    > ...
    > Note: In Python versions 3.10.0–3.10.8 and 3.11.0 this function (and other functions which use it implicitly) emitted a DeprecationWarning if there was no running event loop, even if the current loop was set on the policy. In Python versions 3.10.9, 3.11.1 and 3.12 they emit a DeprecationWarning if there is no running event loop and no current loop is set. In some future Python release this will become an error.
    
    https://docs.python.org/3.10/library/asyncio-eventloop.html#asyncio.get_event_loop
    
    Reviewed By: iahs
    
    Differential Revision: D47641635
    
    fbshipit-source-id: fa65df8a7636bf572ff404a7f8eca9e8c1c6f725

--- third-party/thrift/src/thrift/compiler/test/build_fixtures.py
+++ third-party/thrift/src/thrift/compiler/test/build_fixtures.py
@@ -140,57 +140,60 @@ async def run_subprocess(cmd, *, cwd):
         sys.stderr.write(err.decode(sys.stderr.encoding))
 
 
-processes = []
+async def main():
+    processes = []
 
-msg_format = "Building fixture {{0:>{w}}}/{{1}}: {{2}}".format(
-    w=len(str(len(fixture_names)))
-)
-for name, index in zip(fixture_names, range(len(fixture_names))):
-    msg = msg_format.format(index + 1, len(fixture_names), name)
-    print(msg, file=sys.stderr)
-    fixture_src = os.path.join(fixture_dir, name)
-    for fn in set(os.listdir(fixture_src)) - {"cmd", "src"}:
-        if fn.startswith("."):
-            continue
-        shutil.rmtree(os.path.join(fixture_src, fn))
-    cmds = read_lines(os.path.join(fixture_src, "cmd"))
-    for cmd in cmds:
-        if re.match(r"^\s*#", cmd):
-            continue
-        args = shlex.split(cmd.strip())
-        args[1] = os.path.relpath(os.path.join(fixture_src, args[1]), fixture_root)
-        base_args = [
-            thrift,
-            "-r",
-            "-I",
-            os.path.abspath(fixture_src),
-            "-I",
-            os.path.abspath(fixture_root),
-            "-o",
-            os.path.abspath(fixture_src),
-            "--gen",
-        ]
-        if "mstch_cpp" in args[0]:
-            path = os.path.join("thrift/compiler/test/fixtures", name)
-            extra = "include_prefix=" + path
-            join = "," if ":" in args[0] else ":"
-            args[0] = args[0] + join + extra
-        if (
-            "cpp2" in args[0]
-            or "schema" in args[0]
-            or "mstch_java" in args[0]
-            or "mstch_python" in args[0]
-        ):
-            # TODO: (yuhanhao) T41937765 When use generators that use
-            # `mstch_objects` in recursive mode, if included thrift file
-            # contains const structs or const union, generater will attempt to
-            # de-reference a nullptr in `mstch_const_value::const_struct()`.
-            # This is a hack before this is resolved.
-            base_args.remove("-r")
-        xargs = base_args + args
-        processes.append(run_subprocess(xargs, cwd=fixture_root))
+    msg_format = "Building fixture {{0:>{w}}}/{{1}}: {{2}}".format(
+        w=len(str(len(fixture_names)))
+    )
+    for name, index in zip(fixture_names, range(len(fixture_names))):
+        msg = msg_format.format(index + 1, len(fixture_names), name)
+        print(msg, file=sys.stderr)
+        fixture_src = os.path.join(fixture_dir, name)
+        for fn in set(os.listdir(fixture_src)) - {"cmd", "src"}:
+            if fn.startswith("."):
+                continue
+            shutil.rmtree(os.path.join(fixture_src, fn))
+        cmds = read_lines(os.path.join(fixture_src, "cmd"))
+        for cmd in cmds:
+            if re.match(r"^\s*#", cmd):
+                continue
+            args = shlex.split(cmd.strip())
+            args[1] = os.path.relpath(os.path.join(fixture_src, args[1]), fixture_root)
+            base_args = [
+                thrift,
+                "-r",
+                "-I",
+                os.path.abspath(fixture_src),
+                "-I",
+                os.path.abspath(fixture_root),
+                "-o",
+                os.path.abspath(fixture_src),
+                "--gen",
+            ]
+            if "mstch_cpp" in args[0]:
+                path = os.path.join("thrift/compiler/test/fixtures", name)
+                extra = "include_prefix=" + path
+                join = "," if ":" in args[0] else ":"
+                args[0] = args[0] + join + extra
+            if (
+                "cpp2" in args[0]
+                or "schema" in args[0]
+                or "mstch_java" in args[0]
+                or "mstch_python" in args[0]
+            ):
+                # TODO: (yuhanhao) T41937765 When use generators that use
+                # `mstch_objects` in recursive mode, if included thrift file
+                # contains const structs or const union, generater will attempt to
+                # de-reference a nullptr in `mstch_const_value::const_struct()`.
+                # This is a hack before this is resolved.
+                base_args.remove("-r")
+            xargs = base_args + args
+            processes.append(run_subprocess(xargs, cwd=fixture_root))
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(asyncio.gather(*processes))
+    await asyncio.gather(*processes)
+
+
+asyncio.run(main())
 
 sys.exit(int(has_errors))

41f2f1f59ea485d171d6a81fda57c1fa6437c51e
Lucian Wischik <ljw@meta.com>
2023-07-24 08:02:06 -0700

    autocomplete integration tests
    Summary:
    Here are glean autocomplete integration tests.
    
    A few exected scenarios just don't work, e.g. Ve|" doesn't complete to namespace "Vec", and "\Vec\ch|" doesn't complete to function "Vec\chunk"
    
    I wrote these as lit tests using FileCheck, instead of hack's more common .exp format, because glean will likely return lots of results and I want the tests to be robust against glean's results changing.
    
    For now I'll let the tests run in phabricator, and see whether they work. But I also set up a chronos job to run them hourly in case I need to mark them as local-only.
    
    Reviewed By: mheiber
    
    Differential Revision: D47699620
    
    fbshipit-source-id: 283c063e07f1a2cd59fea38dcae352694b4ed031

---
+++ hphp/hack/test/integration/glean/glean1.php
@@ -0,0 +1,59 @@
+//// main.php
+<?hh
+// RUN: %hh_single_complete --auto-complete --glean-reponame www.autocomplete --auto-namespace-map '{"Vec": "FlibSL\\Vec"}' %s | FileCheck %s
+
+//// glean1_vec.php
+<?hh
+function glean1_vec(): void { VecAUTO332
+// CHECK: //// glean1_vec.php
+// **not_yet_working**: INSERT Vec
+// **not_yet_working-next**: namespace
+
+//// glean1_bvec.php
+<?hh
+function glean1_bvec(): void { \VecAUTO332
+// CHECK: //// glean1_bvec.php
+// **not_yet_working**: INSERT Vec
+// **not_yet_working-next**: namespace
+
+//// glean1_vec_chunk.php
+<?hh
+function glean1_vec_chunk(): void { Vec\chAUTO332
+// CHECK: //// glean1_vec_chunk.php
+// CHECK: INSERT chunk
+// CHECK-NEXT: function
+
+//// glean1_bvec_chunk.php
+<?hh
+function glean1_bvec_chunk(): void { \Vec\chAUTO332
+// CHECK: //// glean1_bvec_chunk.php
+// **not_yet_working**: INSERT chunk
+// **not_yet_working-next**: function
+
+//// glean1_chessbot.php
+<?hh
+function glean1_chessbot(): void { ChessBoAUTO332
+// CHECK: //// glean1_chessbot.php
+// CHECK: INSERT ChessBot
+// CHECK-NEXT: class
+
+//// glean1_bchessbot.php
+<?hh
+function glean1_bchessbot(): void { \ChessBoAUTO332
+// CHECK: //// glean1_bchessbot.php
+// CHECK: INSERT ChessBot
+// CHECK-NEXT: class
+
+//// glean1_utf8.php
+<?hh
+function glean1_utf8(): void { utf8_strleAUTO332
+// CHECK: //// glean1_utf8.php
+// CHECK: INSERT utf8_strlen
+// CHECK-NEXT: function
+
+//// glean1_butf8.php
+<?hh
+function glean1_butf8(): void { \utf8_strleAUTO332
+// CHECK: //// glean1_butf8.php
+// CHECK: INSERT utf8_strlen
+// CHECK-NEXT: function

223f6d03b75f46a4bc4893ee601dcf05f9398c17
Yuhan Hao <yuhanhao@meta.com>
2023-07-28 16:36:06 -0700

    rebuild fixtures
    Summary: rebuild fixtures to fix test failure under `thrift/compiler`
    
    Differential Revision: D47889528
    
    fbshipit-source-id: 51d008835a15dc58548656f3ae7ef611d40844a7

--- third-party/thrift/src/thrift/compiler/test/fixtures/basic-annotations/gen-java/test/fixtures/basicannotations/FooBarBazServiceRpcServerHandler.java
+++ third-party/thrift/src/thrift/compiler/test/fixtures/basic-annotations/gen-java/test/fixtures/basicannotations/FooBarBazServiceRpcServerHandler.java
@@ -94,19 +94,10 @@ public class FooBarBazServiceRpcServerHandler
 
           _chain.postRead(_data);
 
-          reactor.core.publisher.Mono<Void> _delegateResponse = null;
-
-          if (com.facebook.thrift.util.resources.RpcResources.isForceExecutionOffEventLoop()) {
-            _delegateResponse = reactor.core.publisher.Mono.defer(() -> {
-                com.facebook.nifty.core.RequestContexts.setCurrentContext(_payload.getRequestContext());
-                return _delegate.foo();
-              });
-            _delegateResponse = _delegateResponse.publishOn(com.facebook.thrift.util.resources.RpcResources.getOffLoopScheduler());
-          } else {
-            _delegateResponse = _delegate
+          reactor.core.publisher.Mono<Void> _delegateResponse =
+            _delegate
               .foo()
               .doFirst(() -> com.facebook.nifty.core.RequestContexts.setCurrentContext(_payload.getRequestContext()));
-          }
 
           reactor.core.publisher.Mono<com.facebook.thrift.payload.ServerResponsePayload> _internalResponse =
             _delegateResponse.map(_response -> {
@@ -140,9 +131,6 @@ public class FooBarBazServiceRpcServerHandler
 
                 return reactor.core.publisher.Mono.just(_serverResponsePayload);
             });
-          if (com.facebook.thrift.util.resources.RpcResources.isForceExecutionOffEventLoop()) {
-            _internalResponse = _internalResponse.subscribeOn(com.facebook.thrift.util.resources.RpcResources.getOffLoopScheduler());
-          }
 
           return _internalResponse;
   }
@@ -188,19 +176,10 @@ public class FooBarBazServiceRpcServerHandler
 
           _chain.postRead(_data);
 
-          reactor.core.publisher.Mono<Void> _delegateResponse = null;
-
-          if (com.facebook.thrift.util.resources.RpcResources.isForceExecutionOffEventLoop()) {
-            _delegateResponse = reactor.core.publisher.Mono.defer(() -> {
-                com.facebook.nifty.core.RequestContexts.setCurrentContext(_payload.getRequestContext());
-                return _delegate.bar();
-              });
-            _delegateResponse = _delegateResponse.publishOn(com.facebook.thrift.util.resources.RpcResources.getOffLoopScheduler());
-          } else {
-            _delegateResponse = _delegate
+          reactor.core.publisher.Mono<Void> _delegateResponse =
+            _delegate
               .bar()
               .doFirst(() -> com.facebook.nifty.core.RequestContexts.setCurrentContext(_payload.getRequestContext()));
-          }
 
           reactor.core.publisher.Mono<com.facebook.thrift.payload.ServerResponsePayload> _internalResponse =
             _delegateResponse.map(_response -> {
@@ -234,9 +213,6 @@ public class FooBarBazServiceRpcServerHandler
 
                 return reactor.core.publisher.Mono.just(_serverResponsePayload);
             });
-          if (com.facebook.thrift.util.resources.RpcResources.isForceExecutionOffEventLoop()) {
-            _internalResponse = _internalResponse.subscribeOn(com.facebook.thrift.util.resources.RpcResources.getOffLoopScheduler());
-          }
 
           return _internalResponse;
   }
@@ -282,19 +258,10 @@ public class FooBarBazServiceRpcServerHandler
 
           _chain.postRead(_data);
 
-          reactor.core.publisher.Mono<Void> _delegateResponse = null;
-
-          if (com.facebook.thrift.util.resources.RpcResources.isForceExecutionOffEventLoop()) {
-            _delegateResponse = reactor.core.publisher.Mono.defer(() -> {
-                com.facebook.nifty.core.RequestContexts.setCurrentContext(_payload.getRequestContext());
-                return _delegate.baz();
-              });
-            _delegateResponse = _delegateResponse.publishOn(com.facebook.thrift.util.resources.RpcResources.getOffLoopScheduler());
-          } else {
-            _delegateResponse = _delegate
+          reactor.core.publisher.Mono<Void> _delegateResponse =
+            _delegate
               .baz()
               .doFirst(() -> com.facebook.nifty.core.RequestContexts.setCurrentContext(_payload.getRequestContext()));
-          }
 
           reactor.core.publisher.Mono<com.facebook.thrift.payload.ServerResponsePayload> _internalResponse =
             _delegateResponse.map(_response -> {
@@ -328,9 +295,6 @@ public class FooBarBazServiceRpcServerHandler
 
                 return reactor.core.publisher.Mono.just(_serverResponsePayload);
             });
-          if (com.facebook.thrift.util.resources.RpcResources.isForceExecutionOffEventLoop()) {
-            _internalResponse = _internalResponse.subscribeOn(com.facebook.thrift.util.resources.RpcResources.getOffLoopScheduler());
-          }
 
           return _internalResponse;
   }
@@ -339,7 +303,7 @@ public class FooBarBazServiceRpcServerHandler
   @java.lang.Override
   public reactor.core.publisher.Flux<com.facebook.thrift.payload.ServerResponsePayload> singleRequestStreamingResponse(com.facebook.thrift.payload.ServerRequestPayload _payload) {
     final String _name = _payload.getRequestRpcMetadata().getName();
-    return reactor.core.publisher.Flux.defer(() -> {
+    reactor.core.publisher.Flux<com.facebook.thrift.payload.ServerResponsePayload> _retVal = reactor.core.publisher.Flux.defer(() -> {
     com.facebook.swift.service.ContextChain _chain;
     try {
       _chain = new com.facebook.swift.service.ContextChain(_eventHandlers, _name, _payload.getRequestContext());
@@ -367,12 +331,16 @@ public class FooBarBazServiceRpcServerHandler
     }
     return _result;
     });
+    if (com.facebook.thrift.util.resources.RpcResources.isForceExecutionOffEventLoop()) {
+      _retVal = _retVal.subscribeOn(com.facebook.thrift.util.resources.RpcResources.getOffLoopScheduler());
+    }
+    return _retVal;
   }
 
   @java.lang.Override
   public reactor.core.publisher.Mono<com.facebook.thrift.payload.ServerResponsePayload> singleRequestSingleResponse(com.facebook.thrift.payload.ServerRequestPayload _payload) {
     final String _name = _payload.getRequestRpcMetadata().getName();
-    return reactor.core.publisher.Mono.defer(() -> {
+    reactor.core.publisher.Mono<com.facebook.thrift.payload.ServerResponsePayload> _retVal = reactor.core.publisher.Mono.defer(() -> {
     com.facebook.swift.service.ContextChain _chain;
     try {
       _chain = new com.facebook.swift.service.ContextChain(_eventHandlers, _name, _payload.getRequestContext());
@@ -410,13 +378,18 @@ public class FooBarBazServiceRpcServerHandler
 
     return _result;
     });
+
+    if (com.facebook.thrift.util.resources.RpcResources.isForceExecutionOffEventLoop()) {
+      _retVal = _retVal.subscribeOn(com.facebook.thrift.util.resources.RpcResources.getOffLoopScheduler());
+    }
+    return _retVal;
   }
 
   @java.lang.Override
   public reactor.core.publisher.Mono<Void> singleRequestNoResponse(com.facebook.thrift.payload.ServerRequestPayload _payload) {
     final String _name = _payload.getRequestRpcMetadata().getName();
 
-    return reactor.core.publisher.Mono.defer(() -> {
+    reactor.core.publisher.Mono<Void> _retVal = reactor.core.publisher.Mono.defer(() -> {
     com.facebook.swift.service.ContextChain _chain;
     try {
       _chain = new com.facebook.swift.service.ContextChain(_eventHandlers, _name, _payload.getRequestContext());
@@ -437,6 +410,11 @@ public class FooBarBazServiceRpcServerHandler
 
     return _result;
     });
+
+    if (com.facebook.thrift.util.resources.RpcResources.isForceExecutionOffEventLoop()) {
+      _retVal = _retVal.subscribeOn(com.facebook.thrift.util.resources.RpcResources.getOffLoopScheduler());
+    }
+    return _retVal;
   }
 
   public java.util.Map<String, com.facebook.thrift.server.RpcServerHandler> getMethodMap() {

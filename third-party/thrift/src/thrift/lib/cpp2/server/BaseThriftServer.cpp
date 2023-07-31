e03e11389f27046aaa3ba919d6c0bcfdfb3c5986
Kirill Sazonov <sazonovk@meta.com>
2023-07-31 11:58:35 -0700

    Add a way to set a mock CPU-CC config in code
    Reviewed By: jdonald
    
    Differential Revision: D47880820
    
    fbshipit-source-id: 52968725982cdf176e36d772377dd141cd387266

--- third-party/thrift/src/thrift/lib/cpp2/server/BaseThriftServer.cpp
+++ third-party/thrift/src/thrift/lib/cpp2/server/BaseThriftServer.cpp
@@ -42,10 +42,22 @@ THRIFT_PLUGGABLE_FUNC_REGISTER(
   return folly::observer::makeStaticObserver(
       AdaptiveConcurrencyController::Config{});
 }
-
 } // namespace detail
 
 using namespace std;
+
+folly::observer::Observer<CPUConcurrencyController::Config>
+BaseThriftServer::makeCPUConcurrencyControllerConfigInternal() {
+  return folly::observer::makeObserver(
+      [mockConfig = mockCPUConcurrencyControllerConfig_.getObserver(),
+       config = detail::makeCPUConcurrencyControllerConfig(
+           this)]() -> CPUConcurrencyController::Config {
+        if (auto config = **mockConfig) {
+          return *config;
+        }
+        return **config;
+      });
+}
 
 BaseThriftServer::BaseThriftServer()
     : thriftConfig_(),
@@ -54,7 +66,7 @@ BaseThriftServer::BaseThriftServer()
           thriftConfig_.getMaxRequests().getObserver(),
           detail::getThriftServerConfig(*this)},
       cpuConcurrencyController_{
-          detail::makeCPUConcurrencyControllerConfig(this),
+          makeCPUConcurrencyControllerConfigInternal(),
           *this,
           detail::getThriftServerConfig(*this)},
       addresses_(1) {}

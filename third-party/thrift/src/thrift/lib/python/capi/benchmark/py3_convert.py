6ed8f07797396b3866ecd52364996fde97a09b0d
Andrew Hilger <ahilger@meta.com>
2023-08-16 17:45:08 -0700

    benchmark capi conversion
    Summary:
    Using serialization-based capi (on trunk)
    ```
    | Benchmark               | reflection   | capi     |
    |-------------------------|--------------|----------|
    | Simple                  | 0.008 ms     | 0.006 ms |
    | Simple with large list  | 1.066 ms     | 0.140 ms |
    | Simple with long string | 4.673 ms     | 8.751 ms |
    | Nested                  | 0.013 ms     | 0.006 ms |
    | Nested with large list  | 12.321 ms    | 0.621 ms |
    | Union                   | 0.002 ms     | 0.006 ms |
    ```
    Generally better for containers and structs with many fields, but serialization adds an extra string copy that makes it worse for long string case.
    
    With marshal-based capi (on separate branch), copies are reduced and performance is much better than reflection and better than serialization.
    ```
    | Benchmark               | reflection   | capi     |
    |-------------------------|--------------|----------|
    | Simple                  | 0.008 ms     | 0.001 ms |
    | Simple with large list  | 1.005 ms     | 0.061 ms |
    | Simple with long string | 4.600 ms     | 1.733 ms |
    | Nested                  | 0.012 ms     | 0.001 ms |
    | Nested with large list  | 12.008 ms    | 0.157 ms |
    | Union                   | 0.002 ms     | 0.001 ms |
    ```
    
    Reviewed By: junzh0u, Filip-F
    
    Differential Revision: D48240003
    
    fbshipit-source-id: 83fc7f435b71f21ca245adc3766fc78e6298b057

---
+++ third-party/thrift/src/thrift/lib/python/capi/benchmark/py3_convert.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env fbpython
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import timeit
+from collections import defaultdict
+
+import click
+
+import convertible.thrift_types as python_types  # noqa: F401
+import convertible.types as py3_types  # noqa: F401
+from tabulate import tabulate
+from thrift.python.converter import to_python_struct  # noqa: F401
+
+cost_in_ms = defaultdict(list)
+
+
+@click.pass_context
+def benchmark(ctx, klass_name: str, setup: str, qualifier: str = "") -> None:
+    title = f"{klass_name} {qualifier}" if qualifier else klass_name
+    ctrl_stmt = f"to_python_struct(python_types.{klass_name}, PY3)"
+    test_stmt = "PY3._to_python()"
+    for stmt in (ctrl_stmt, test_stmt):
+        timer = timeit.Timer(
+            stmt=stmt,
+            setup=setup,
+            globals={
+                "python_types": python_types,
+                "py3_types": py3_types,
+                "to_python_struct": to_python_struct,
+            },
+        )
+        costs = []
+        for _ in range(ctx.params["repeat"]):
+            (n, total) = timer.autorange()
+            costs.append(total * 1000.0 / n)
+        cost_in_ms[title].append(min(costs))
+
+
+@click.command()
+@click.option(
+    "--repeat",
+    default=3,
+    help="Repeat each benchmark N times and take the minimum.",
+)
+def main(repeat: int) -> None:
+    benchmark(
+        "Simple",
+        setup="PY3=py3_types.Simple()",
+    )
+    benchmark(
+        "Simple",
+        qualifier="with large list",
+        setup="PY3=py3_types.Simple(intList=[1] * 10_000)",
+    )
+    benchmark(
+        "Simple",
+        qualifier="with long string",
+        setup="PY3=py3_types.Simple(strField='a' * 10_000_000)",
+    )
+    benchmark(
+        "Nested",
+        setup="PY3=py3_types.Nested()",
+    )
+    benchmark(
+        "Nested",
+        qualifier="with large list",
+        setup="PY3=py3_types.Nested(simpleList=[py3_types.Simple()] * 1_000)",
+    )
+    benchmark(
+        "Union",
+        setup="PY3=py3_types.Union(intField=42)",
+    )
+
+    headers = ["thrift-py3 to thrift-python", "reflection", "capi"]
+    table = [
+        ([name] + [f"{cost:.3f} ms" for cost in costs])
+        for (name, costs) in cost_in_ms.items()
+    ]
+    print(
+        tabulate(
+            table,
+            headers=headers,
+            tablefmt="github",
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()

ea237587ea17979b54644da06ea3165864180f70
Fred Qiu <qiu@meta.com>
2023-09-01 08:43:22 -0700

    Fix fizz copyright headers
    Summary: Fix fizz copyright headers. See https://www.internalfb.com/phabricator/paste/view/P814551381 for details.
    
    Reviewed By: knekritz
    
    Differential Revision: D48736465
    
    fbshipit-source-id: d4585406ea6156d3e02b08da584b6f4aac018431

--- third-party/fizz/src/fizz/crypto/aead/CryptoUtil.h
+++ third-party/fizz/src/fizz/crypto/aead/CryptoUtil.h
@@ -1,4 +1,10 @@
-// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+/*
+ *  Copyright (c) 2023-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree.
+ */
 
 #pragma once
 

7a886c90d1799ca436082966fc53581d81db4881
Dmitrii Senin <kairo5@meta.com>
2023-09-06 04:49:42 -0700

    Add unit-tests for HH\Vector compact thrift deserialization
    Summary: Added unit-tests for HH\Vector compact thrift deserialization use case.
    
    Reviewed By: elalfer
    
    Differential Revision: D48717019
    
    fbshipit-source-id: 05c1a4c484a18e73b951304e6caa93bd3ef1b8d4

--- hphp/test/slow/thrift/fast_compact_read_list.php
+++ hphp/test/slow/thrift/fast_compact_read_list.php
@@ -1,16 +1,6 @@
 <?hh
 
-class ListI64 {
-  const SPEC = darray[
-    1 => darray[
-      'var' => 'data',
-      'format' => 'harray',
-      'type' => TType::LST,
-      'etype' => TType::I64,
-      'elem' => darray[ 'type' => TType::I64 ],
-    ],
-  ];
-
+class ThriftData {
   public $data = null;
 
   public static function withDefaultValues()[]: this {
@@ -20,7 +10,19 @@ class ListI64 {
   public function clearTerseFields()[write_props]: void {}
 }
 
-class ListI08 {
+class ListI64 extends ThriftData {
+  const SPEC = darray[
+    1 => darray[
+      'var' => 'data',
+      'format' => 'harray',
+      'type' => TType::LST,
+      'etype' => TType::I64,
+      'elem' => darray[ 'type' => TType::I64 ],
+    ],
+  ];
+}
+
+class ListI08 extends ThriftData {
   const SPEC = darray[
     1 => darray[
       'var' => 'data',
@@ -30,14 +32,30 @@ class ListI08 {
       'elem' => darray[ 'type' => TType::I08 ],
     ],
   ];
+}
 
-  public $data = null;
+class VectorI64 extends ThriftData {
+  const SPEC = darray[
+    1 => darray[
+      'var' => 'data',
+      'format' => 'collection',
+      'type' => TType::LST,
+      'etype' => TType::I64,
+      'elem' => darray[ 'type' => TType::I64 ],
+    ],
+  ];
+}
 
-  public static function withDefaultValues()[]: this {
-    return new static();
-  }
-
-  public function clearTerseFields()[write_props]: void {}
+class VectorI08 extends ThriftData {
+  const SPEC = darray[
+    1 => darray[
+      'var' => 'data',
+      'format' => 'collection',
+      'type' => TType::LST,
+      'etype' => TType::I08,
+      'elem' => darray[ 'type' => TType::I08 ],
+    ],
+  ];
 }
 
 function test($name, $classname, $data) :mixed{
@@ -51,9 +69,13 @@ function test($name, $classname, $data) :mixed{
 }
 
 function main(): mixed {
-  test("empty vec", ListI64::class, vec[]);
-  test("fast decoded vec", ListI64::class, vec[0x7FFFFFFFFFFFFF00, 0x7FFFFFFFFFFFFF01]);
-  test("fast decoded vec", ListI08::class, vec[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  test("empty ListI64", ListI64::class, vec[]);
+  test("fast decoded ListI64", ListI64::class, vec[0x7FFFFFFFFFFFFF00, 0x7FFFFFFFFFFFFF01]);
+  test("fast decoded ListI08", ListI08::class, vec[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+  test("empty VectorI64", VectorI64::class, new Vector(null));
+  test("fast decoded VectorI64", VectorI64::class, Vector{0x7FFFFFFFFFFFFF00, 0x7FFFFFFFFFFFFF01});
+  test("fast decoded VectorI08", VectorI08::class, Vector{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
 }
 
 <<__EntryPoint>>

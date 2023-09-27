cdc10af97502a65d29471aaeb8fb218eb6dd485b
Lucian Wischik <ljw@meta.com>
2023-09-27 15:36:49 -0700

    baseline tests for namespaces
    Summary:
    This adds autocomplete tests for namespaces.
    
    (I'm adding it after realizing that I wrote a diff which broke namespace autocomplete, but it wasn't caught in any tests. Now it will be).
    
    Reviewed By: mheiber
    
    Differential Revision: D49618462
    
    fbshipit-source-id: 93990ea9bc44819db623de1e247087d6181a88df

---
+++ hphp/hack/test/autocomplete/namespaces/namespaces.php
@@ -0,0 +1,29 @@
+//// SecureRando.php
+<?hh
+function test(): void { SecureRandoAUTO332
+
+//// SecureRandom.php
+function test(): void { SecureRandomAUTO332
+
+//// bSecureRando.php
+function test(): void { \SecureRandoAUTO332
+
+//// Ve.php
+<?hh
+function test(): void { VeAUTO332
+
+//// Vec.php
+<?hh
+function test(): void { VecAUTO332
+
+//// bVe.php
+<?hh
+function test(): void { \VeAUTO332
+
+//// C.php
+<?hh
+function test(): void { CAUTO332
+
+//// bC.php
+<?hh
+function test(): void { \CAUTO332

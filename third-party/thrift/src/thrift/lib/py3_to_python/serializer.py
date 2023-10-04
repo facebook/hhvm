2908647e3adfca97f38612e165d5e516f46ab11f
Alvaro Leiva Geisse <aleivag@meta.com>
2023-10-03 20:51:25 -0700

    generic and simple multi py serializer
    Summary:
    I should probably build this in cython or c, but python was a bit
    
    faster
    
    this provides a very simple and nice interface for a universal py* thrift serializer, see the diff up in the stack for how its used
    
    Reviewed By: zsol
    
    Differential Revision: D49674520
    
    fbshipit-source-id: f42e40720ee9070941927b80647642bd2b09e177

---
+++ third-party/thrift/src/thrift/lib/py3_to_python/serializer.py
@@ -0,0 +1,52 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Intermediate solution to be able to have a generic serializer from
+"""
+
+
+from enum import Enum
+from functools import singledispatch
+
+import thrift.py3.types
+import thrift.python.types
+
+
+class Protocol(Enum):
+    COMPACT = 0
+    BINARY = 1
+    JSON = 2
+
+
+GENERIC_THRIFT_STRUCT = (thrift.py3.types.Struct, thrift.python.types.StructOrUnion)
+
+
+@singledispatch
+def serialize(obj, protocol=Protocol.COMPACT) -> bytes:
+    from thrift.py3 import Protocol as py3_Protocol, serialize as py3_serialize
+
+    return py3_serialize(obj, protocol=py3_Protocol[protocol.name])
+
+
+@serialize.register(thrift.python.types.StructOrUnion)
+def _(
+    obj: thrift.python.types.StructOrUnion, protocol: Protocol = Protocol.COMPACT
+) -> bytes:
+    from thrift.python.serializer import (
+        Protocol as python_Protocol,
+        serialize as python_serialize,
+    )
+
+    return python_serialize(obj, protocol=python_Protocol[protocol.name])

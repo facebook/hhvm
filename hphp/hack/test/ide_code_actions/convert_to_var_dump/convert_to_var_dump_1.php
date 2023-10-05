506f3429607a5b9fb560e44737a9b34454bd5564
Max Heiber <mheiber@meta.com>
2023-10-05 11:03:07 -0700

    repro quickfix not found
    Summary:
    Sometimes we generate quickfixes but the user never sees them, due to a bug in our logic for calculating whether a quickfix is relevant to a range.
    
    Repro the bug. Fixed in next diff: {D49772308}
    
    Differential Revision: D49773396
    
    fbshipit-source-id: 4a2e9b7642bac9ffc50b18e78030b64eff615275

---
+++ hphp/hack/test/ide_code_actions/convert_to_var_dump/convert_to_var_dump_1.php
@@ -0,0 +1,7 @@
+<?hh
+
+// This tests the logic for finding relevant quickfixes for a range.
+// The tests in //hphp/hack/test/quickfixes exercise quickfixes more.
+function f(int $x): void{
+    =/*range-start*/$x/*range-end*/;
+}

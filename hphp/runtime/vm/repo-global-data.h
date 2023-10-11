cd3780b6757d0c71f6ff74447aca5cb936783156
Vassil Mladenov <vmladenov@meta.com>
2023-10-11 00:00:28 -0700

    Fix type of repo mode option
    Summary: Fix mistake I made in D49935123
    
    Reviewed By: ricklavoie
    
    Differential Revision: D50160424
    
    fbshipit-source-id: 7616cb8dea15294e3935c7cd017a1f3dd2ff5004

--- hphp/runtime/vm/repo-global-data.h
+++ hphp/runtime/vm/repo-global-data.h
@@ -129,7 +129,7 @@ struct RepoGlobalData {
   bool IsVecNotices = false;
 
   /* Whether implicit class conversions can raise a notice */
-  bool RaiseClassConversionNoticeSampleRate = 0;
+  uint32_t RaiseClassConversionNoticeSampleRate = 0;
 
   /* Whether classname type-hint accepts (lazy) classes */
   bool ClassPassesClassname = false;

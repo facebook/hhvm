bca9ee3a96d4b452e315ba9e9aa38d8f53090494
Catherine Gasnier <catg@meta.com>
2023-10-18 09:42:22 -0700

    More ServerProgress.with_message, requiring refactoring
    Summary:
    For now the progress message "figuring out files to recheck" shows up for a long time.
    I want to show it with `ServerProgress.with_message` instead of `ServerProgress.write` (see previous diff) to see if it really takes a long time to compute fanout or if it's just because we don't update the progress message once we're done.
    For the code to be more readable, I also bread down `post_saved_state_initialization` into smaller functions.
    
    Reviewed By: ljw1004
    
    Differential Revision: D50344990
    
    fbshipit-source-id: b0283697ac17c8984864961fda6a36cf047717c9

--- hphp/hack/src/server/serverLazyInit.ml
+++ hphp/hack/src/server/serverLazyInit.ml
@@ -1047,13 +1047,37 @@ let parse_only_init
     env
     cgroup_steps
 
-(** Among other things, updates the naming table and compute fanout. *)
-let post_saved_state_initialization
+(** We'll adjust the forward and reverse naming table to reflect
+    changes to files which changed since the saved-state. We'll also
+    reflect changes in files which had [phase=Errors.(Naming|Parsing)]
+    as well. Notionally that's because our current mechanism for handling
+    duplicate-name-errors requires all affected files to go through
+    the "update reverse naming table" procedure. (but it's redundant
+    because, elsewhere, we don't allow saved-state-generation in case
+    of naming errors...)
+
+    The actual implementation is confusing because it stores fragments
+    of naming-table in places you wouldn't expect:
+
+    1. [old_naming_table], which we got from the saved-state, is a [NamingTable.t]
+       that's "backed" i.e. it reflects just the sqlite file plus a delta, initially empty.
+    2. [env.naming_table] starts out as [Naming_table.empty] as it was created in
+       [ServerMain.setup_server]. We will add to it the forward-naming-table FileInfo.t
+       for all files discussed above, [parsing_files]
+    3. The reverse naming-table is made up of global mutable shmem delta with
+       eventual fallback to sqlite. We will write into that delta the reverse-names
+       that arise from the [parsing_files]. The same step also gathers any
+       duplicate-name errors.
+    4. Finally we'll merge the [env.naming_table] (which is only [parsed_files] at the moment)
+       into [old_naming_table] (which represents the sqlite file), and store the result
+       back in [env.naming_table]. At this point the naming-table, forward and reverse,
+       is complete. *)
+let update_naming_table
+    env
+    genv
     ~(do_indexing : bool)
-    ~(genv : ServerEnv.genv)
-    ~(env : ServerEnv.env)
     ~(state_result : loaded_info * Relative_path.Set.t * Watchman.clock option)
-    (cgroup_steps : CgroupProfiler.step_group) : ServerEnv.env * float =
+    (cgroup_steps : CgroupProfiler.step_group) =
   let ((loaded_info : ServerInitTypes.loaded_info), changed_while_parsing, clock)
       =
     state_result
@@ -1062,6 +1086,170 @@ let post_saved_state_initialization
   let {
     naming_table_fallback_fn;
     dirty_naming_files;
+    dirty_local_files;
+    dirty_master_files;
+    old_naming_table;
+    mergebase_rev = _;
+    mergebase = _;
+    old_errors;
+    deptable_fn = _;
+    naming_table_fn = _;
+    corresponding_rev = _;
+    saved_state_delta = _;
+    naming_table_manifold_path = _;
+  } =
+    loaded_info
+  in
+  ServerProgress.with_message "updating file-to-symbol table" @@ fun () ->
+  let t = Unix.gettimeofday () in
+  let naming_files =
+    List.fold
+      ~init:Relative_path.Set.empty
+      ~f:Relative_path.Set.union
+      [
+        dirty_naming_files;
+        dirty_master_files;
+        dirty_local_files;
+        changed_while_parsing;
+      ]
+    |> Relative_path.Set.filter ~f:FindUtils.path_filter
+  in
+  ( CgroupProfiler.step_start_end cgroup_steps "remove fixmes"
+  @@ fun _cgroup_step -> Fixme_provider.remove_batch naming_files );
+  (* Parse dirty files only *)
+  let (env, t) =
+    ServerInitCommon.parse_files_and_update_forward_naming_table
+      genv
+      env
+      ~get_next:
+        (MultiWorker.next
+           genv.workers
+           (Relative_path.Set.elements naming_files))
+      ~count:(Relative_path.Set.cardinal naming_files)
+      t
+      ~trace
+      ~cache_decls:false (* Don't overwrite old decls loaded from saved state *)
+      ~telemetry_label:"post_ss1.parsing"
+      ~cgroup_steps
+      ~worker_call:MultiWorker.wrapper
+  in
+  let ctx = Provider_utils.ctx_from_server_env env in
+  let t =
+    remove_items_from_reverse_naming_table_or_build_new_reverse_naming_table
+      ctx
+      old_naming_table
+      naming_files
+      naming_table_fallback_fn
+      t
+      ~cgroup_steps
+  in
+  if do_indexing then
+    let (env, t) = write_symbol_info genv env cgroup_steps t in
+    (env, t, None)
+  else
+    (* Do global naming on all dirty files *)
+    let (env, t) =
+      ServerInitCommon
+      .update_reverse_naming_table_from_env_and_get_duplicate_name_errors
+        env
+        t
+        ~telemetry_label:"post_ss1.naming"
+        ~cgroup_steps
+    in
+    ServerInitCommon.validate_no_errors env.errorl;
+
+    let new_naming_table = env.naming_table in
+    let env =
+      {
+        env with
+        clock;
+        naming_table = Naming_table.combine old_naming_table env.naming_table;
+        disk_needs_parsing = Relative_path.Set.empty;
+        needs_recheck = Relative_path.Set.union env.needs_recheck old_errors;
+      }
+    in
+    (env, t, Some new_naming_table)
+
+(** What files should be checked?
+
+  We've already said that files-with-errors from the dirty saved state must be
+  rechecked. And we've already produced "duplicate name" errors if needed from
+  all the changed files. The question remaining is, what fanout should be checked?
+
+  Here, for each changed file, we compare its hash to the one saved
+  in the saved state. If the hashes are the same, then the declarations
+  on the file have not changed and we only need to retypecheck that file,
+  not all of its dependencies.
+  We call these files "similar" to their previous versions.
+
+  A similar check is also made later, inside [calculate_fanout_and_defer_or_do_type_check]
+  when it calls [get_files_to_recheck] which calls [Decl_redecl_service.redo_type_decl].
+  That obtains old decls from an online service and uses that for decl-diffing.
+
+  Anyway, the effect of this phase is to calculate fanout, techniques to determine
+  decl-diffing, using the prechecked algorithm too. Then, the fanout files are
+  combined into [env.needs_recheck], as a way of deferring the check until
+  the first iteration of ServerTypeCheck. *)
+let compute_fanout
+    env
+    genv
+    t
+    ~cgroup_steps
+    ~old_naming_table
+    ~new_naming_table
+    ~dirty_master_files
+    ~dirty_local_files =
+  ServerProgress.with_message "figuring out files to recheck" @@ fun () ->
+  let partition_unchanged_hash dirty_files =
+    Relative_path.Set.partition
+      (fun f ->
+        let old_info = Naming_table.get_file_info old_naming_table f in
+        let new_info = Naming_table.get_file_info env.naming_table f in
+        match (old_info, new_info) with
+        | (Some x, Some y) ->
+          (match (x.FileInfo.hash, y.FileInfo.hash) with
+          | (Some x, Some y) -> Int64.equal x y
+          | _ -> false)
+        | _ -> false)
+      dirty_files
+  in
+  let (dirty_master_files_unchanged_decls, dirty_master_files_changed_decls) =
+    partition_unchanged_hash dirty_master_files
+  in
+  let (dirty_local_files_unchanged_decls, dirty_local_files_changed_decls) =
+    partition_unchanged_hash dirty_local_files
+  in
+
+  let (env, t) =
+    calculate_fanout_and_defer_or_do_type_check
+      genv
+      env
+      ~old_naming_table
+      ~new_naming_table
+      ~dirty_master_files_unchanged_decls
+      ~dirty_master_files_changed_decls
+      ~dirty_local_files_unchanged_decls
+      ~dirty_local_files_changed_decls
+      t
+      cgroup_steps
+  in
+  (env, t)
+
+(** Among other things, updates the naming table and compute fanout. *)
+let post_saved_state_initialization
+    ~(do_indexing : bool)
+    ~(genv : ServerEnv.genv)
+    ~(env : ServerEnv.env)
+    ~(state_result : loaded_info * Relative_path.Set.t * Watchman.clock option)
+    (cgroup_steps : CgroupProfiler.step_group) : ServerEnv.env * float =
+  let ( (loaded_info : ServerInitTypes.loaded_info),
+        _changed_while_parsing,
+        _clock ) =
+    state_result
+  in
+  let {
+    naming_table_fallback_fn = _;
+    dirty_naming_files = _;
     dirty_local_files;
     dirty_master_files;
     old_naming_table;
@@ -1154,157 +1342,28 @@ let post_saved_state_initialization
 
   (***********************************************************
     NAMING TABLE.
-    Plan: we'll adjust the forward and reverse naming table to reflect
-    changes to files which changed since the saved-state. We'll also
-    reflect changes in files which had [phase=Errors.(Naming|Parsing)]
-    as well. Notionally that's because our current mechanism for handling
-    duplicate-name-errors requires all affected files to go through
-    the "update reverse naming table" procedure. (but it's redundant
-    because, elsewhere, we don't allow saved-state-generation in case
-    of naming errors...)
-
-    The actual implementation is confusing because it stores fragments
-    of naming-table in places you wouldn't expect:
-
-    1. [old_naming_table], which we got from the saved-state, is a [NamingTable.t]
-       that's "backed" i.e. it reflects just the sqlite file plus a delta, initially empty.
-    2. [env.naming_table] starts out as [Naming_table.empty] as it was created in
-       [ServerMain.setup_server]. We will add to it the forward-naming-table FileInfo.t
-       for all files discussed above, [parsing_files]
-    3. The reverse naming-table is made up of global mutable shmem delta with
-       eventual fallback to sqlite. We will write into that delta the reverse-names
-       that arise from the [parsing_files]. The same step also gathers any
-       duplicate-name errors.
-    4. Finally we'll merge the [env.naming_table] (which is only [parsed_files] at the moment)
-       into [old_naming_table] (which represents the sqlite file), and store the result
-       back in [env.naming_table]. At this point the naming-table, forward and reverse,
-       is complete. *)
-  ServerProgress.write "updating file-to-symbol table";
-  let t = Unix.gettimeofday () in
-  let naming_files =
-    List.fold
-      ~init:Relative_path.Set.empty
-      ~f:Relative_path.Set.union
-      [
-        dirty_naming_files;
-        dirty_master_files;
-        dirty_local_files;
-        changed_while_parsing;
-      ]
-    |> Relative_path.Set.filter ~f:FindUtils.path_filter
+    See doc of update_naming_table.
+  *)
+  let (env, t, new_naming_table) =
+    update_naming_table env genv ~do_indexing ~state_result cgroup_steps
   in
-  ( CgroupProfiler.step_start_end cgroup_steps "remove fixmes"
-  @@ fun _cgroup_step -> Fixme_provider.remove_batch naming_files );
-  (* Parse dirty files only *)
-  let (env, t) =
-    ServerInitCommon.parse_files_and_update_forward_naming_table
-      genv
+
+  (***********************************************************
+    FANOUT.
+    See doc of compute_fanout
+  *)
+  match new_naming_table with
+  | None -> (env, t)
+  | Some new_naming_table ->
+    compute_fanout
       env
-      ~get_next:
-        (MultiWorker.next
-           genv.workers
-           (Relative_path.Set.elements naming_files))
-      ~count:(Relative_path.Set.cardinal naming_files)
-      t
-      ~trace
-      ~cache_decls:false (* Don't overwrite old decls loaded from saved state *)
-      ~telemetry_label:"post_ss1.parsing"
-      ~cgroup_steps
-      ~worker_call:MultiWorker.wrapper
-  in
-  let ctx = Provider_utils.ctx_from_server_env env in
-  let t =
-    remove_items_from_reverse_naming_table_or_build_new_reverse_naming_table
-      ctx
-      old_naming_table
-      naming_files
-      naming_table_fallback_fn
+      genv
       t
       ~cgroup_steps
-  in
-  if do_indexing then
-    write_symbol_info genv env cgroup_steps t
-  else
-    (* Do global naming on all dirty files *)
-    let (env, t) =
-      ServerInitCommon
-      .update_reverse_naming_table_from_env_and_get_duplicate_name_errors
-        env
-        t
-        ~telemetry_label:"post_ss1.naming"
-        ~cgroup_steps
-    in
-    ServerInitCommon.validate_no_errors env.errorl;
-
-    let new_naming_table = env.naming_table in
-    let env =
-      {
-        env with
-        clock;
-        naming_table = Naming_table.combine old_naming_table env.naming_table;
-        disk_needs_parsing = Relative_path.Set.empty;
-        needs_recheck = Relative_path.Set.union env.needs_recheck old_errors;
-      }
-    in
-
-    (***********************************************************
-      FANOUT.
-      What files should be checked?
-
-      We've already said that files-with-errors from the dirty saved state must be
-      rechecked. And we've already produced "duplicate name" errors if needed from
-      all the changed files. The question remaining is, what fanout should be checked?
-
-      Here, for each changed file, we compare its hash to the one saved
-      in the saved state. If the hashes are the same, then the declarations
-      on the file have not changed and we only need to retypecheck that file,
-      not all of its dependencies.
-      We call these files "similar" to their previous versions.
-
-      A similar check is also made later, inside [calculate_fanout_and_defer_or_do_type_check]
-      when it calls [get_files_to_recheck] which calls [Decl_redecl_service.redo_type_decl].
-      That obtains old decls from an online service and uses that for decl-diffing.
-
-      Anyway, the effect of this phase is to calculate fanout, techniques to determine
-      decl-diffing, using the prechecked algorithm too. Then, the fanout files are
-      combined into [env.needs_recheck], as a way of deferring the check until
-      the first iteration of ServerTypeCheck.
-    *)
-    ServerProgress.write "figuring out files to recheck";
-    let partition_unchanged_hash dirty_files =
-      Relative_path.Set.partition
-        (fun f ->
-          let old_info = Naming_table.get_file_info old_naming_table f in
-          let new_info = Naming_table.get_file_info env.naming_table f in
-          match (old_info, new_info) with
-          | (Some x, Some y) ->
-            (match (x.FileInfo.hash, y.FileInfo.hash) with
-            | (Some x, Some y) -> Int64.equal x y
-            | _ -> false)
-          | _ -> false)
-        dirty_files
-    in
-    let (dirty_master_files_unchanged_decls, dirty_master_files_changed_decls) =
-      partition_unchanged_hash dirty_master_files
-    in
-    let (dirty_local_files_unchanged_decls, dirty_local_files_changed_decls) =
-      partition_unchanged_hash dirty_local_files
-    in
-
-    let (env, t) =
-      calculate_fanout_and_defer_or_do_type_check
-        genv
-        env
-        ~old_naming_table
-        ~new_naming_table
-        ~dirty_master_files_unchanged_decls
-        ~dirty_master_files_changed_decls
-        ~dirty_local_files_unchanged_decls
-        ~dirty_local_files_changed_decls
-        t
-        cgroup_steps
-    in
-    (env, t)
+      ~old_naming_table
+      ~new_naming_table
+      ~dirty_master_files
+      ~dirty_local_files
 
 let saved_state_init
     ~(do_indexing : bool)

b087f589868a1778415012eb2c643e7fdb86cb6f
Catherine Gasnier <catg@meta.com>
2023-10-31 07:27:00 -0700

    add mli file for Sqlite_utils
    Reviewed By: vsiles
    
    Differential Revision: D50835060
    
    fbshipit-source-id: bf0f6d637e633f1086ddc219352cac3ba64a8f1b

--- hphp/hack/src/utils/sqlite/sqlite_utils.ml
+++ hphp/hack/src/utils/sqlite/sqlite_utils.ml
@@ -8,7 +8,7 @@
  *)
 open Core
 
-(* Check a sqlite result, and crash if it is invalid *)
+(** Check a sqlite result, and crash if it is invalid *)
 let check_rc (db : Sqlite3.db) (rc : Sqlite3.Rc.t) : unit =
   match rc with
   | Sqlite3.Rc.OK
@@ -20,24 +20,6 @@ let check_rc (db : Sqlite3.db) (rc : Sqlite3.Rc.t) : unit =
          "SQLite operation failed: %s (%s)"
          (Sqlite3.Rc.to_string rc)
          (Sqlite3.errmsg db))
-
-(* Gather a database and prepared statement into a tuple *)
-let prepare_or_reset_statement
-    (db_opt_ref : Sqlite3.db option ref)
-    (stmt_ref : Sqlite3.stmt option ref)
-    (sql_command_text : string) : Sqlite3.stmt =
-  let db = Option.value_exn !db_opt_ref in
-  let stmt =
-    match !stmt_ref with
-    | Some s ->
-      Sqlite3.reset s |> check_rc db;
-      s
-    | None ->
-      let s = Sqlite3.prepare db sql_command_text in
-      stmt_ref := Some s;
-      s
-  in
-  stmt
 
 let to_str_exn (value : Sqlite3.Data.t) : string =
   match value with
@@ -59,7 +41,7 @@ let to_blob_exn (value : Sqlite3.Data.t) : string =
             "Expected a blob, but was %s"
             (Sqlite3.Data.to_string_debug value)))
 
-(* Convert a sqlite data value to an Int64, or raise an exception *)
+(** Convert a sqlite data value to an Int64, or raise an exception *)
 let to_int64_exn (value : Sqlite3.Data.t) : int64 =
   match value with
   | Sqlite3.Data.INT (i : int64) -> i
@@ -69,45 +51,6 @@ let to_int64_exn (value : Sqlite3.Data.t) : int64 =
          (Printf.sprintf
             "Expected an int, but was %s"
             (Sqlite3.Data.to_string_debug value)))
-
-(* Coerce a value to an Int64, and ignore errors *)
-let to_int64 (value : Sqlite3.Data.t) : int64 =
-  try to_int64_exn value with
-  | Invalid_argument _ -> 0L
-
-(* Convert a sqlite data value to an ocaml int, or raise an exception *)
-let to_int_exn (value : Sqlite3.Data.t) : int =
-  match value with
-  | Sqlite3.Data.INT i -> begin
-    match Int64.to_int i with
-    | Some num -> num
-    | None ->
-      raise (Invalid_argument "Attempt to coerce sqlite value to ocaml int")
-  end
-  | _ -> raise (Invalid_argument "Attempt to coerce sqlite value to ocaml int")
-
-(* Convert a sqlite data value to an ocaml int, and ignore errors *)
-let to_int (value : Sqlite3.Data.t) : int =
-  try to_int_exn value with
-  | Invalid_argument _ -> 0
-
-(* To save a bool to sqlite have to convert it to int64 *)
-let bool_to_sqlite (value : bool) : Sqlite3.Data.t =
-  match value with
-  | true -> Sqlite3.Data.INT 1L
-  | false -> Sqlite3.Data.INT 0L
-
-(* Convert a sqlite value to a bool *)
-let to_bool_exn (value : Sqlite3.Data.t) : bool =
-  match value with
-  | Sqlite3.Data.INT 0L -> false
-  | Sqlite3.Data.INT 1L -> true
-  | _ -> raise (Invalid_argument "Attempt to coerce sqlite value to ocaml bool")
-
-(* Convert a sqlite data value to an ocaml int, and ignore errors *)
-let to_bool (value : Sqlite3.Data.t) : bool =
-  try to_bool_exn value with
-  | Invalid_argument _ -> false
 
 let column_str stmt idx = to_str_exn (Sqlite3.column stmt idx)
 

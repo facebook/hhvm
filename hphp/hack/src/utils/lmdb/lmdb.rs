6b63ebc9799ba2b005a2f971bdd470e4f16197c8
Edwin Smith <smith@meta.com>
2023-10-31 09:51:35 -0700

    lmdb options builder
    Summary:
    File::options() returns an Options object with builder-pattern
    methods for configuring Env settings that must be set before
    an lmdb::Env (database file) is opened.
    
    Reviewed By: rjbailey
    
    Differential Revision: D50828598
    
    fbshipit-source-id: 7ee9a85ef17f7ea6da566a27b0dfdbfe789cc094

--- hphp/hack/src/utils/lmdb/lmdb.rs
+++ hphp/hack/src/utils/lmdb/lmdb.rs
@@ -7,6 +7,7 @@ use std::ffi::CStr;
 use std::ffi::CString;
 use std::marker::PhantomData;
 use std::os::raw::c_int;
+use std::os::raw::c_uint;
 use std::os::unix::ffi::OsStrExt;
 use std::path::Path;
 use std::ptr;
@@ -17,12 +18,63 @@ use lmdb_sys::*;
 
 // LMDB docs: http://www.lmdb.tech/doc/index.html
 
+/// A builder wrapping an unopened lmdb::Env, used for setting configuration
+/// items that must be set before the environment is opened. Call Env::options()
+/// to create one.
+pub struct Options {
+    env: *mut MDB_env,
+    flags: c_uint,
+}
+
 #[derive(Debug)]
 pub struct Env {
     env: *mut MDB_env,
 }
 
-impl Env {
+// TODO Available Env flags:
+// MDB_RDONLY - change api to open/create, ala File. write txn will fail
+// MDB_WRITEMAP - for writable db, use rw mmap (faster but less corruption proof)
+// MDB_NOTLS - allow Txn that are Send. also Sync if we provide locks.
+//           Maybe a different Env type with different Txn types
+// MDB_NORDAHEAD - disable OS readahead for huge dbs & random read access
+// MDB_NOMEMINIT - dont zero blocks first. (can change in live env). nop for MEM_WRITEMAP
+
+// TODO available env settings
+// mdb_env_set_mapsize() - may be called after mdb_env_open() if no live txns
+//   mdb_txn_begin() can fail with MDB_MAP_RESIZED if another process grew the data
+//   beyond this process map size
+// mdb_env_set_maxreaders() - (for all procs). only before open. should be >= max threads
+// mdb_env_set_maxdbs() - only before open
+
+impl Options {
+    pub fn set_mapsize(self, mapsize: usize) -> Result<Self> {
+        unsafe {
+            check(mdb_env_set_mapsize(self.env, mapsize))?;
+        }
+        Ok(self)
+    }
+
+    pub fn set_max_named_dbs(self, n: u32) -> Result<Self> {
+        unsafe {
+            check(mdb_env_set_maxdbs(self.env, n))?;
+        }
+        Ok(self)
+    }
+
+    /// Use a writable memory map if the Env is opened as writable.
+    /// This is faster and uses fewer mallocs, but loses protection from
+    /// application bugs like wild pointer writes and other bad updates
+    /// into the database. Incompatible with nested transactions.
+    /// Do not mix processes with and without MDB_WRITEMAP on the same environment.
+    /// This can defeat durability (mdb_env_sync etc).
+    pub fn set_write_mmap(mut self, enable: bool) -> Self {
+        match enable {
+            false => self.flags &= !MDB_WRITEMAP,
+            true => self.flags |= MDB_WRITEMAP,
+        }
+        self
+    }
+
     /// Create a database in the directory specified by `path`.
     /// `path`: This directory must already exist and be writable.
     /// `size`: The size of the memory map to use for this environment,
@@ -34,16 +86,38 @@ impl Env {
     /// SAFETY: Only one instance per canonical path may exist process-wide.
     /// Do not have open an LMDB database twice in the same process at the same time.
     /// Not even from a plain open() call - close()ing it breaks flock() advisory locking.
-    pub unsafe fn create(path: &Path, mapsize: usize, max_dbs: u32) -> Result<Self> {
+    pub fn create_file(self, path: impl AsRef<Path>) -> Result<Env> {
+        let Self { env, flags } = self;
+        unsafe {
+            let path = CString::new(path.as_ref().as_os_str().as_bytes())?;
+            check(mdb_env_open(
+                env,
+                path.as_ptr(),
+                flags | MDB_NOSUBDIR,
+                0o644,
+            ))?;
+            std::mem::forget(self);
+            Ok(Env { env })
+        }
+    }
+
+    pub fn open_file(self, path: impl AsRef<Path>) -> Result<Env> {
+        let Self { env, flags } = self;
+        unsafe {
+            let path = CString::new(path.as_ref().as_os_str().as_bytes())?;
+            check(mdb_env_open(env, path.as_ptr(), flags | MDB_RDONLY, 0o444))?;
+            std::mem::forget(self);
+            Ok(Env { env })
+        }
+    }
+}
+
+impl Env {
+    pub fn options() -> Result<Options> {
         unsafe {
             let mut env = ptr::null_mut();
             check(mdb_env_create(&mut env))?;
-            let env = Self { env };
-            check(mdb_env_set_mapsize(env.env, mapsize))?;
-            check(mdb_env_set_maxdbs(env.env, max_dbs))?;
-            let path = CString::new(path.as_os_str().as_bytes())?;
-            check(mdb_env_open(env.env, path.as_ptr(), 0, 0o644))?;
-            Ok(env)
+            Ok(Options { env, flags: 0 })
         }
     }
 
@@ -66,7 +140,15 @@ impl Env {
         }
     }
 
+    // TODO: Available DB options
+    // MDB_REVERSEKEY    compare keys as reversed strings
+    // MDB_DUPSORT       treat (key,value) as key, subject to max_key_size
+    // MDB_INTEGERKEY    keys are binary size_t
+    // MDB_DUPFIXED      fixed-size (key,value) items. requires DUPSORT
+    // MDB_INTEGERDUP    (key,value) are both size_t
+    // MDB_REVERSEDUP    compare data items as reversed strings
     pub fn create_db(&self, name: Option<&str>) -> Result<Db> {
+        // TODO mutex around this
         let txn = self.rw_begin()?;
         let c_name = name.map(|n| CString::new(n).unwrap());
         let c_name = c_name.as_ref().map_or(ptr::null(), |n| n.as_ptr());
@@ -79,12 +161,35 @@ impl Env {
         Ok(Db(dbi))
     }
 
+    pub fn open_db(&self, name: Option<&str>) -> Result<Db> {
+        // TODO mutex around this
+        let txn = self.ro_begin()?;
+        let c_name = name.map(|n| CString::new(n).unwrap());
+        let c_name = c_name.as_ref().map_or(ptr::null(), |n| n.as_ptr());
+        let mut dbi: MDB_dbi = 0;
+        unsafe {
+            check(mdb_dbi_open(txn.txn, c_name, 0, &mut dbi))
+                .with_context(|| name.unwrap_or("").to_owned())?
+        }
+        Ok(Db(dbi))
+    }
+
     pub fn page_size(&self) -> Result<usize> {
         let mut stat = std::mem::MaybeUninit::uninit();
         unsafe {
             check(mdb_env_stat(self.env, stat.as_mut_ptr()))?;
             Ok(stat.assume_init().ms_psize as usize)
         }
+    }
+
+    pub fn max_key_size(&self) -> usize {
+        unsafe { mdb_env_get_maxkeysize(self.env) as u32 as usize }
+    }
+}
+
+impl Drop for Options {
+    fn drop(&mut self) {
+        unsafe { mdb_env_close(self.env) }
     }
 }
 
@@ -144,10 +249,23 @@ impl<'e> Txn for RwTxn<'e> {
 }
 
 impl<'e> RwTxn<'e> {
-    pub fn put(&self, db: Db, key: impl AsRef<[u8]>, value: impl AsRef<[u8]>) -> Result<()> {
+    /// Fail with MDB_BAD_VALSIZE if a key length is outside [1,max_key_size()]
+    /// or value exceeds 4GiB. Zero length keys are not allowed.
+    pub fn put(&mut self, db: Db, key: impl AsRef<[u8]>, value: impl AsRef<[u8]>) -> Result<()> {
         let mut k = slice_to_val(key);
         let mut v = slice_to_val(value);
         unsafe { check(mdb_put(self.txn, db.0, &mut k, &mut v, 0)) }
+    }
+
+    pub fn append(&mut self, db: Db, key: impl AsRef<[u8]>, value: impl AsRef<[u8]>) -> Result<()> {
+        let mut k = slice_to_val(key);
+        let mut v = slice_to_val(value);
+        unsafe { check(mdb_put(self.txn, db.0, &mut k, &mut v, MDB_APPEND)) }
+    }
+
+    pub fn del(&mut self, db: Db, key: impl AsRef<[u8]>) -> Result<()> {
+        let mut k = slice_to_val(key);
+        unsafe { check(mdb_del(self.txn, db.0, &mut k, ptr::null_mut())) }
     }
 }
 

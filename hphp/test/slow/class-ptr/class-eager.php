ee3bd516b5c6e5c814eafb5af2b7c4e25e73b345
Vassil Mladenov <vmladenov@meta.com>
2023-11-01 22:47:26 -0700

    Commit copies for eager tests
    Reviewed By: ricklavoie
    
    Differential Revision: D50917289
    
    fbshipit-source-id: 2a37b25b70e18de898f3762045f8930a6ad51481

---
+++ hphp/test/slow/class-ptr/class-eager.php
@@ -0,0 +1,33 @@
+<?hh
+
+class Foo {}
+class Bar {
+  const FOO = Foo::class;
+  public static function sm() :mixed{
+    var_dump("sm");
+  }
+  public static int $sp = 10;
+  public function m() :mixed{
+    var_dump("m");
+  }
+}
+
+<<__EntryPoint>>
+function main() :mixed{
+  $c = __hhvm_intrinsics\create_class_pointer(Bar::class);
+  var_dump($c);
+  var_dump(is_scalar($c));
+  var_dump(HH\class_get_class_name($c));
+  $v = vec[
+    __hhvm_intrinsics\create_class_pointer(Bar::class),
+    __hhvm_intrinsics\create_class_pointer(Fizz::class)
+  ]; // Fizz is not a class
+  var_dump($v);
+  var_dump($c::FOO);
+  $c::sm();
+  var_dump($c::$sp);
+  $o = new $c;
+  $o->m();
+  $f = __hhvm_intrinsics\create_class_pointer(Fizz::class);
+  $o = new $f;  // should error as Fizz is not a class
+}

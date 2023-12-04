5f7e9eacd3a42ad44d9e5c8d4250d7d9ad0963ed
David Goldblatt <davidgoldblatt@meta.com>
2023-12-04 10:25:19 -0800

    Add more fields to ShallowData benchmark.
    Summary:
    Thrift struct boundaries are in a sense "hard" and have not historically been
    the target of optimizations. Fields within a struct, though, have been. Have
    some more of the latter to make small performance changes easier to identify.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D46992085
    
    fbshipit-source-id: f2abf1ec0ab955e20e3d3dfbb33c289892a18a2f

--- third-party/thrift/src/thrift/lib/cpp2/test/CompactProtocolBenchData.thrift
+++ third-party/thrift/src/thrift/lib/cpp2/test/CompactProtocolBenchData.thrift
@@ -17,8 +17,26 @@
 struct Empty {}
 
 struct Shallow {
-  1: i64 one;
-  2: i64 two;
+  1: i64 var1;
+  2: i64 var2;
+  3: i64 var3;
+  4: i64 var4;
+  5: i64 var5;
+  6: i64 var6;
+  7: i64 var7;
+  8: i64 var8;
+  9: i64 var9;
+  10: i64 var10;
+  11: i64 var11;
+  12: i64 var12;
+  13: i64 var13;
+  14: i64 var14;
+  15: i64 var15;
+  16: i64 var16;
+  17: i64 var17;
+  18: i64 var18;
+  19: i64 var19;
+  20: i64 var20;
 }
 
 struct Deep2 {

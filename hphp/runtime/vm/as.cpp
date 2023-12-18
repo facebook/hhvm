977fee404731f32fde8185bd7666cb231b6935ac
Edwin Smith <smith@meta.com>
2023-12-18 03:20:12 -0800

    strcasecmp -> icasecmp for case-collision logging (take 2)
    Summary:
    Convert a few strcasecmp() call sites in HHVM to the istrcmp() logging
    versions, to log case collisions. These weren't using the existing isame()
    helper that already logs.
    
    Reviewed By: ricklavoie
    
    Differential Revision: D52227711
    
    fbshipit-source-id: 8760d213d72741bfb0a9c9a717831f778813b308

--- hphp/runtime/vm/as.cpp
+++ hphp/runtime/vm/as.cpp
@@ -3321,12 +3321,12 @@ void parse_default_value(FuncEmitter::ParamInfo& param, const StringData* str) {
   TypedValue tv;
   tvWriteUninit(tv);
   if (str->size() == 4) {
-    if (!strcasecmp("null", str->data())) {
+    if (!istrcmp("null", str->data())) {
       tvWriteNull(tv);
-    } else if (!strcasecmp("true", str->data())) {
+    } else if (!istrcmp("true", str->data())) {
       tv = make_tv<KindOfBoolean>(true);
     }
-  } else if (str->size() == 5 && !strcasecmp("false", str->data())) {
+  } else if (str->size() == 5 && !istrcmp("false", str->data())) {
     tv = make_tv<KindOfBoolean>(false);
   }
   auto utype = param.typeConstraint.underlyingDataType();

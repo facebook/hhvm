f40daaee827ffcdf8ab6134644a3d80e4356b6dc
Aaron Orenstein <aorenste@meta.com>
2023-12-23 14:52:24 -0800

    Fix apc_expire test
    Summary: There's a race condition here - APC TTLs are based on second-resolution accuracy so if we set a TTL of 1s and happen to cross the second boundary before the fetch (unlikely * runs a lot == it happens) it will have already timed out. Since we're running 10 times with a delay of 1s on each iteration then a 3s TTL should be plenty.
    
    Reviewed By: ricklavoie
    
    Differential Revision: D52398509
    
    fbshipit-source-id: b580565de0d7b3a9068a19b7f051642148ac38c1

--- hphp/test/slow/ext_apc/apc_expire.php
+++ hphp/test/slow/ext_apc/apc_expire.php
@@ -1,5 +1,7 @@
 <?hh
 
+const int TEST_TTL = 3;
+
 function apc_fetch_null<T>(string $key): ?T {
   $success = false;
   $ret = apc_fetch($key, inout $success);
@@ -15,18 +17,24 @@ async function test(string $prefix, int $times): Awaitable<vec<(string, string)>
 
   switch ($times) {
     case 0:
-      apc_add($prefix.'add', makestring('add'), 1);
+      // There's a race condition here - APC TTLs are based on second-resolution
+      // accuracy so if we set a TTL of 1s and happen to cross the second
+      // boundary before the fetch (unlikely * runs a lot == it happens) it will
+      // have already timed out. Since we're running 10 times with a delay of 1s
+      // then a 3s TTL should be plenty.
+      $x = apc_add($prefix.'add', makestring('add'), TEST_TTL);
       $results[] = $prefix.'add: '.apc_fetch_null($prefix.'add');
-      apc_add($prefix.'add', makestring('add'), 3);
+      apc_add($prefix.'add', makestring('add'), TEST_TTL);
       $results[] = $prefix.'add: '.apc_fetch_null($prefix.'add');
-      apc_store($prefix.'store', makestring('store'), 1);
+      apc_store($prefix.'store', makestring('store'), TEST_TTL);
       $results[] = $prefix.'store: '.apc_fetch_null($prefix.'store');
-      apc_store($prefix.'store', makestring('store'), 3);
+      apc_store($prefix.'store', makestring('store'), TEST_TTL);
       $results[] = $prefix.'store: '.apc_fetch_null($prefix.'store');
-      apc_store($prefix.'extend', makestring('extend'), 1);
+      apc_store($prefix.'extend', makestring('extend'), TEST_TTL);
       $results[] = $prefix.'extend: '.apc_fetch_null($prefix.'extend');
-      apc_extend_ttl($prefix.'extend', 3);
+      apc_extend_ttl($prefix.'extend', TEST_TTL);
       $results[] = $prefix.'extend: '.apc_fetch_null($prefix.'extend');
+      // Sleep for 1s
       await SleepWaitHandle::create(1000000);
       break;
     case 9:
@@ -36,9 +44,10 @@ async function test(string $prefix, int $times): Awaitable<vec<(string, string)>
       break;
     default:
       if ($times < 9) {
+        // Sleep for 1s
         await SleepWaitHandle::create(1000000);
       } else {
-        throw new Exception('To big times '.$times);
+        throw new Exception('Test ran too many times: '.$times);
       }
       break;
   }
@@ -48,6 +57,9 @@ async function test(string $prefix, int $times): Awaitable<vec<(string, string)>
 
 <<__EntryPoint>>
 async function main(): Awaitable<void> {
+  // The test runner runs this test 10 times (because of the opts file) - the
+  // first iteration sets up APC. Middle iterations just sleep. The final
+  // iteration checks that APC values have expired.
   $success = false;
   $count = apc_fetch('times', inout $success) ?: 0;
   concurrent {

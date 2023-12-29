e5e0bd65ae64fd84ebaa0964491065bca119b5aa
Yedidya Feldblum <yfeldblum@meta.com>
2023-12-29 09:30:29 -0800

    migrate from legacy boost::filesystem::ofstream when via helper namespace fs
    Summary: Migrate from `boost::filesystem::ofstream`, spelled as `fs::ofstream`, to `std::ofstream`.
    
    Differential Revision: D52437152
    
    fbshipit-source-id: 8461af15b036d37eb8eb0c21bc4d414e08a3005b

--- third-party/watchman/src/watchman/integration/cppclient.cpp
+++ third-party/watchman/src/watchman/integration/cppclient.cpp
@@ -88,7 +88,7 @@ int main(int argc, char** argv) {
     auto empty_file_relative_path =
         fs::relative(empty_file_path, subdir.path());
     {
-      auto empty_file = fs::ofstream{empty_file_path};
+      auto empty_file = std::ofstream{empty_file_path};
       if (!empty_file) {
         LOG(ERROR) << "Failed to create " << empty_file_path;
         return 1;

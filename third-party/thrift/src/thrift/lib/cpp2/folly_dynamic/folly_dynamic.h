665a829999b3469f16321319b2ab989a8c43bb9e
Pavel Iatchenii <operasfantom@meta.com>
2024-01-11 10:25:57 -0800

    remove @author tags
    Summary:
    As [suggested](https://www.internalfb.com/diff/D52515797?dst_version_fbid=344668185182531&transaction_fbid=924681979242206),
    
    > Given the implementation is completely different, might be a good time to drop the author tags (anti-pattern that we don't use in other code for this reason).
    
    Reviewed By: thedavekwon
    
    Differential Revision: D52692005
    
    fbshipit-source-id: 3b1596d19ffca36380d8156435ba790bf3aa32ba

--- third-party/thrift/src/thrift/lib/cpp2/folly_dynamic/folly_dynamic.h
+++ third-party/thrift/src/thrift/lib/cpp2/folly_dynamic/folly_dynamic.h
@@ -29,46 +29,34 @@ namespace thrift {
 
 /**
  * Describes the format of the data contained in the `folly::dynamic`.
- *
- * @author: Marcelo Juchem <marcelo@fb.com>
  */
 enum class dynamic_format {
   /**
    * A data format that's aimed at being more portable and easier to read by
    * humans.
-   *
-   * @author: Marcelo Juchem <marcelo@fb.com>
    */
   PORTABLE,
 
   /**
    * A data format that's compatible with `readFromJson()` and
    * `TSimpleJSONProtocol` from Thrift 1.
-   *
-   * @author: Marcelo Juchem <marcelo@fb.com>
    */
   JSON_1
 };
 
 /**
  * Tells how much a decoder should adhere to the data format specification.
- *
- * @author: Marcelo Juchem <marcelo@fb.com>
  */
 enum class format_adherence {
   /**
    * Demands the data to strictly follow the given format. Any deviation from
    * the format will be rejected.
-   *
-   * @author: Marcelo Juchem <marcelo@fb.com>
    */
   STRICT,
 
   /**
    * Accepts data that deviates from the format, as long as the deviation is not
    * ambiguous and can be safely interpreted by the decoder.
-   *
-   * @author: Marcelo Juchem <marcelo@fb.com>
    */
   LENIENT
 };
@@ -78,8 +66,6 @@ enum class format_adherence {
  * always-on reflection support.
  *
  * The root object is output to the given `folly::dynamic` output parameter.
- *
- * @author: Marcelo Juchem <marcelo@fb.com>
  */
 template <typename Tag, typename T>
 void to_dynamic(folly::dynamic& out, T&& input, dynamic_format format) {
@@ -94,8 +80,6 @@ void to_dynamic(folly::dynamic& out, T&& input, dynamic_format format) {
 /**
  * Converts an object to its `folly::dynamic` representation using Thrift's
  * always-on reflection support.
- *
- * @author: Marcelo Juchem <marcelo@fb.com>
  */
 template <typename Tag, typename T>
 folly::dynamic to_dynamic(T&& input, dynamic_format format) {
@@ -122,8 +106,6 @@ folly::dynamic to_dynamic(T&& input, dynamic_format format) {
  requires a cleared object to be passed in
  *
  * The decoded object is output to the given `out` parameter.
- *
- * @author: Marcelo Juchem <marcelo@fb.com>
  */
 template <typename Tag, typename T>
 void from_dynamic(
@@ -158,8 +140,6 @@ void from_dynamic(
 /**
  * Converts an object from its `folly::dynamic` representation using Thrift's
  * always-on reflection support.
- *
- * @author: Marcelo Juchem <marcelo@fb.com>
  */
 template <typename Tag, typename T>
 T from_dynamic(

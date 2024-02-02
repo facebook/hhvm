8b6bdb61c34fad4ef0bc7f09ef5a85d629e6b74a
Scott Owens <sowens@meta.com>
2024-02-02 06:11:19 -0800

    Add some expression tree hover tests
    Summary: ^
    
    Reviewed By: dlreeves
    
    Differential Revision: D53316913
    
    fbshipit-source-id: e27bd612ad35426a26210301385080bd260878a5

---
+++ hphp/hack/test/hover/expression_tree_splice_expr.php
@@ -0,0 +1,11 @@
+<?hh
+
+<<file:__EnableUnstableFeatures('expression_trees')>>
+
+function foo(): void {
+  // I am a comment associated with $one.
+  $one = ExampleDsl`1`;
+
+  $one_splice = ExampleDsl`${$one}`;
+  //                       ^ hover-at-caret
+}

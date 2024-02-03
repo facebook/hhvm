5584b958251d85af4be01bd294f77c606537e1d0
David Tolnay <dtolnay@meta.com>
2024-02-03 09:51:55 -0800

    Fix "should have a snake case name" warning on Thrift libraries with no services
    Summary: New warnings got introduced inadvertently by {D53166614} and {D53166618}.
    
    Reviewed By: zertosh
    
    Differential Revision: D53382363
    
    fbshipit-source-id: 9c9a5ade978445d14e8b69fa22f6535a13a19361

--- third-party/thrift/src/thrift/compiler/test/fixtures/rust-include_src/gen-rust/client.rs
+++ third-party/thrift/src/thrift/compiler/test/fixtures/rust-include_src/gen-rust/client.rs
@@ -2,7 +2,7 @@
 // This file is probably not the place you want to edit!
 
 #![doc(hidden)]
-#![allow(unused_crate_dependencies)]
+#![allow(non_snake_case, unused_crate_dependencies)]
 
 #[doc(inline)]
 pub use :: as types;

377200886fecd22a8658562f33976f4b57737a55
Nikhil Thomas <nzthomas@meta.com>
2024-02-08 07:57:10 -0800

    add test case for parsePath concurrency
    Summary: ensures that we can parse a ton of files without locking.
    
    Reviewed By: edwinsmith
    
    Differential Revision: D53531251
    
    fbshipit-source-id: 61a3682ab3f246f2c297df2b75df0c94b00bad85

---
+++ hphp/test/slow/decl/FileDecls_from_path_repeated.php
@@ -0,0 +1,28 @@
+<?hh
+
+function do_a_parse($f) {
+  HH\FileDecls::parsePath($f);
+}
+<<__EntryPoint>>
+function main() {
+  // Get the autoloader to initialize
+  HH\autoload_is_native();
+  if (HH\execution_context() === 'xbox') return;
+  $r = vec[];
+  echo "starting\n";
+  $tmpdir = sys_get_temp_dir();
+    for ($i = 0; $i < 500; $i++) {
+    $f = "$tmpdir/foo_$i.php";
+    file_put_contents($f, "<?hh class Foo {}\nclass Bar extends Foo {}");
+    $r[] = fb_call_user_func_async(__FILE__, 'do_a_parse', $f);
+  }
+  $start = new DateTime();
+  foreach ($r as $xbox) {
+    fb_end_user_func_async($xbox);
+  }
+  $end = new DateTime();
+  // This is ideally to catch either deadlocks (test will hang) or
+  // perf regressions (taking forever to parse files due to lock contention).
+  echo "took less than 2 seconds? ". ($end->diff($start)->s < 2 ? 'yes' : 'no')."\n";
+  echo "finished all runs\n";
+}

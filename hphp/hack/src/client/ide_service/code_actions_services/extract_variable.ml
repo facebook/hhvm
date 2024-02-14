8f2e2fa5d483e196a7b74610bc63167dfbdc99e4
Maxwell Heiber <mheiber@meta.com>
2024-02-14 13:11:00 -0800

    Provide the "extract into variable" refactoring even in braceless lambdas
    Summary:
    ## What
    
    Provide the "Extract into variable" refactoring for code inside bracless lambdas (Example: `() ==> 3 + /*range-start*/5 + 2/*range-end*/`). Before we did not provide the refactoring in such cases because the logic for doing so is kind of fiddly.
    
    https://pxl.cl/4lvF4
    
    ## Why now
    
    - Removes a blocker for adapting the "Extract into variable" refactoring as a quickfix for the hard-to-action "`await` cannot be used as an expression in this location because it's conditionally executed" error: {D53757680}
    - Several users asked why the "Extract into variable" refactoring wasn't available in cases like this. The behavior was confusing.
    
    ## Notes on snapshot tests
    
    You'll see the following changes:
    - Main change: we provide the "Extract into variable" refactoring in cases where we previously did not. The tests introduced in {D53756767} are all updated: see `inline_variable_in_bracless_lambda_*.php.exp`.
    - Some code actions now look different at the LSP protocol level, but applying the code action produces the same code. This is because we now represent that "Extract into variable" refactoring as a single big edit rather than several small edits. It was easier to implement this way and afaict makes no user-perceptible difference.
    - Our tests for unrelated refactorings now record that the "Extract into variable" refactoring is available in more places.
    
    Differential Revision: D53756768
    
    fbshipit-source-id: d65204e57a1a90a301969dfeb14545ebd666975b

--- hphp/hack/src/client/ide_service/code_actions_services/extract_variable.ml
+++ hphp/hack/src/client/ide_service/code_actions_services/extract_variable.ml
@@ -10,31 +10,24 @@ open Hh_prelude
 let placeholder_regexp = Str.regexp {|\$placeholder\([0-9]+\)|}
 
 type candidate = {
-  stmt_pos: Pos.t;
+  prev_stmt_pos: Pos.t option;  (** used for calculating indentation *)
+  stmt_pos: Pos.t;  (** used for calculating indentation *)
   pos: Pos.t;
+      (** position of expr or statement corresponding to the selection *)
   placeholder_n: int;
+      (** used for naming the placeholder of the generated variable *)
 }
 
-(**
-We don't want to extract variables for lambdas like this: `() ==> 200`.
-The AST of such a lambda is indistinguishable from `() ==> { return 200; }`
-so we peek at the source *)
-let might_be_expression_lambda ~f_body:Aast.{ fb_ast } ~pos ~source_text =
-  match fb_ast with
-  | [(stmt_pos, _)] ->
-    let length = Pos.start_offset stmt_pos - Pos.start_offset pos in
-    if length > 0 then
-      let src = Full_fidelity_source_text.sub_of_pos source_text pos ~length in
-      not @@ String.is_substring ~substring:"{" src
-    else
-      (*  length can be negative to curlies in default params: `(($a = () ==> {}) ==> ...` *)
-      true
-  | _ -> false
-
-let visitor (selection : Pos.t) ~source_text =
+let visitor (selection : Pos.t) =
   let should_traverse outer = Pos.contains outer selection in
-  let stmt_pos = ref Pos.none in
-  let expression_lambda_pos = ref None in
+  let prev_stmt_pos = ref None in
+  let stmt_pos = ref None in
+  let set_stmt_pos pos =
+    if not Pos.(equal none pos) then begin
+      prev_stmt_pos := !stmt_pos;
+      stmt_pos := Some pos
+    end
+  in
   let placeholder_n = ref 0 in
   let expr_positions_overlapping_selection = ref [] in
   let ensure_selection_common_root =
@@ -83,14 +76,14 @@ let visitor (selection : Pos.t) ~source_text =
     method! on_func_body env fb =
       let acc = super#on_func_body env fb in
       (match List.hd fb.Aast.fb_ast with
-      | Some (pos, _) -> stmt_pos := pos
+      | Some (pos, _) -> set_stmt_pos pos
       | _ -> ());
       match acc with
       | Some acc -> Some { acc with placeholder_n = !placeholder_n }
       | None -> None
 
     method! on_stmt env stmt =
-      stmt_pos := fst stmt;
+      set_stmt_pos (fst stmt);
       super#on_stmt env stmt
 
     method! on_expr env expr =
@@ -99,37 +92,22 @@ let visitor (selection : Pos.t) ~source_text =
         expr_positions_overlapping_selection :=
           pos :: !expr_positions_overlapping_selection;
 
-      match expr_ with
-      | Aast.(Binop { bop = Ast_defs.Eq _; lhs = (_, lhs_pos, _); rhs = _ }) ->
+      match (!stmt_pos, expr_) with
+      | (Some stmt_pos, _) when Pos.contains selection pos ->
+        Some
+          {
+            prev_stmt_pos = !prev_stmt_pos;
+            stmt_pos;
+            pos;
+            placeholder_n = 0 (* will be adjusted on the way up *);
+          }
+      | (_, Aast.(Binop { bop = Ast_defs.Eq _; lhs = (_, lhs_pos, _); rhs = _ }))
+        ->
         let acc = super#on_expr env expr in
         Option.filter acc ~f:(fun candidate ->
             not @@ Pos.contains lhs_pos candidate.pos)
-      | Aast.Lfun (Aast.{ f_body; _ }, _) ->
-        expression_lambda_pos :=
-          Option.some_if
-            (might_be_expression_lambda ~f_body ~pos ~source_text)
-            pos;
-        super#on_expr env expr
-      | Aast.Efun _ ->
-        expression_lambda_pos := None;
-        super#on_expr env expr
-      | _ ->
-        if
-          Pos.contains selection pos
-          && (not @@ Pos.equal !stmt_pos Pos.none)
-          && not
-               (Option.map !expression_lambda_pos ~f:(fun lpos ->
-                    Pos.contains lpos pos)
-               |> Option.value ~default:false)
-        then
-          Some
-            {
-              stmt_pos = !stmt_pos;
-              pos;
-              placeholder_n = 0 (* will be adjusted on the way up *);
-            }
-        else
-          super#on_expr env expr
+      | (_, Aast.Efun _) -> super#on_expr env expr
+      | _ -> super#on_expr env expr
   end
 
 (** Generate a snippet from the placeholder number.
@@ -138,38 +116,104 @@ https://fburl.com/code/0vzkqds8. We can implement non-hackily if LSP is updated:
 https://github.com/microsoft/language-server-protocol/issues/592 *)
 let placeholder_name_of_n (n : int) = Format.sprintf "$${0:placeholder%d}" n
 
-let refactor_of_candidate ~source_text ~path { stmt_pos; pos; placeholder_n } =
-  let placeholder = placeholder_name_of_n placeholder_n in
-  let exp_string = Full_fidelity_source_text.sub_of_pos source_text pos in
-  let change_expression = Code_action_types.{ pos; text = placeholder } in
-  let change_add_assignment =
-    let indent =
-      let (_, character) = Pos.line_column stmt_pos in
-      String.make character ' '
-    in
-    let pos = Pos.shrink_to_start stmt_pos in
-    Code_action_types.
-      {
-        pos;
-        text = Printf.sprintf "%s = %s;\n%s" placeholder exp_string indent;
-      }
+let offset_of_pos source_text pos =
+  let (line, start, _) = Pos.info_pos pos in
+  Full_fidelity_source_text.position_to_offset source_text (line, start)
+
+(**
+  true iff the expr or stmt at `pos` is in a lambda where the body is not wrapped in curly braces
+  (single expression with an implicit return).  For example:
+    - `true` for `() ==>  ...   expr  ... `
+    - `false` for `() ==>  { ... expr ... }`
+*)
+let is_in_braceless_lambda source_text positioned_tree pos =
+  let offset = offset_of_pos source_text pos in
+  let nodes =
+    let root = Provider_context.PositionedSyntaxTree.root positioned_tree in
+    Full_fidelity_positioned_syntax.parentage root offset
   in
+  let matching_lambda_body = function
+    | Full_fidelity_positioned_syntax.LambdaExpression { lambda_body; _ }
+      when Full_fidelity_positioned_syntax.start_offset lambda_body = offset ->
+      Some lambda_body
+    | _ -> None
+  in
+  nodes
+  |> List.map ~f:Full_fidelity_positioned_syntax.syntax
+  |> List.find_map ~f:matching_lambda_body
+  |> Option.map
+       ~f:(Fn.non Full_fidelity_positioned_syntax.is_compound_statement)
+  |> Option.value ~default:false
+
+let indent_of_pos pos =
+  let (_, character) = Pos.line_column pos in
+  String.make character ' '
+
+let slice
+    (source_text : Full_fidelity_source_text.t) (start : Pos.t) (end_ : Pos.t) :
+    string =
+  let start_offset = offset_of_pos source_text start in
+  let end_offset = offset_of_pos source_text end_ in
+  Full_fidelity_source_text.sub
+    source_text
+    start_offset
+    (end_offset - start_offset)
+
+let edits_of_candidate ctx entry { prev_stmt_pos; stmt_pos; pos; placeholder_n }
+    : Code_action_types.edit list =
+  let positioned_tree = Ast_provider.compute_cst ~ctx ~entry in
+  let source_text = Ast_provider.compute_source_text ~entry in
+
+  let placeholder = placeholder_name_of_n placeholder_n in
+  let stmt_text =
+    let before = slice source_text stmt_pos pos in
+    let after =
+      slice source_text (Pos.shrink_to_end pos) (Pos.shrink_to_end stmt_pos)
+    in
+    before ^ placeholder ^ after
+  in
+  let stmt_indent = indent_of_pos stmt_pos in
+  let assignment_text =
+    let exp_string = Full_fidelity_source_text.sub_of_pos source_text pos in
+    Printf.sprintf "%s = %s;\n" placeholder exp_string
+  in
+  let text =
+    if is_in_braceless_lambda source_text positioned_tree stmt_pos then
+      let prev_indent =
+        prev_stmt_pos
+        |> Option.map ~f:indent_of_pos
+        |> Option.value ~default:stmt_indent
+      in
+      let body_indent =
+        let default_indent =
+          String.make Format_env.(default.indent_width) ' '
+        in
+        prev_indent ^ default_indent
+      in
+      Format.sprintf
+        "{\n%sreturn %s;\n%s}"
+        (body_indent ^ assignment_text ^ body_indent)
+        stmt_text
+        prev_indent
+    else
+      assignment_text ^ stmt_indent ^ stmt_text
+  in
+  [Code_action_types.{ pos = stmt_pos; text }]
+
+let refactor_of_candidate ctx entry path candidate =
   let edits =
     lazy
       (Relative_path.Map.singleton
          path
-         [change_add_assignment; change_expression])
+         (edits_of_candidate ctx entry candidate))
   in
   Code_action_types.Refactor.{ title = "Extract into variable"; edits }
 
 let find ~entry selection ctx =
   let path = entry.Provider_context.path in
-  let source_text = Ast_provider.compute_source_text ~entry in
   let { Tast_provider.Compute_tast.tast; _ } =
     Tast_provider.compute_tast_quarantined ~ctx ~entry
   in
-  (visitor selection ~source_text)#go
-    ctx
-    tast.Tast_with_dynamic.under_normal_assumptions
-  |> Option.map ~f:(refactor_of_candidate ~source_text ~path)
+  (visitor selection)#go ctx tast.Tast_with_dynamic.under_normal_assumptions
+  |> Option.map ~f:(refactor_of_candidate ctx entry path)
   |> Option.to_list
--- hphp/hack/test/ide_code_actions/extract_variable/extract_variable_lambda_6.php
+++ hphp/hack/test/ide_code_actions/extract_variable/extract_variable_lambda_6.php
@@ -1,8 +1,6 @@
 <?hh
 
 function foo(): void {
-  // The curly braces in the param complicate our expression lambda check.
-  // Make sure we don't crash.
   (($a = () ==> {}) ==>
     /*range-start*/3 + 3/*range-end*/
   );

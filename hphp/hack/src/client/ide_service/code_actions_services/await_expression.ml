02ab77e706a773b16e5cad42ac2d9ce5ff324374
Maxwell Heiber <mheiber@meta.com>
2024-02-20 05:02:40 -0800

    'Await expression' quickfix/refactor: change lambda return type to 'Awaitable' where needed
    Summary:
    For the refactor that adds `await`, if the current lambda has a hint that is not an awaitable, correct the hint to be an `Awaitable`. This avoids the case where applying the refactoring/quickfix would introduce a type error and will save users time.
    
     https://pxl.cl/4mjDZ
    
    Differential Revision: D53858648
    
    fbshipit-source-id: 495c820a7108c2d9155cf5286152fabb05c38f04

--- hphp/hack/src/client/ide_service/code_actions_services/await_expression.ml
+++ hphp/hack/src/client/ide_service/code_actions_services/await_expression.ml
@@ -76,6 +76,8 @@ let find_candidate ctx tast selection : candidate option =
 
 let find_modifier_and_signature_edits path source_text positioned_tree pos :
     Code_action_types.edit list option =
+  let module Syn = Full_fidelity_positioned_syntax in
+  let open Option.Let_syntax in
   let pos_of_offset offset : Pos.t =
     let (line, col) =
       Full_fidelity_source_text.offset_to_position source_text offset
@@ -88,7 +90,26 @@ let find_modifier_and_signature_edits path source_text positioned_tree pos :
       ~pos_end:triple
   in
 
-  let module Syn = Full_fidelity_positioned_syntax in
+  let signature_edit_of_type_node (type_node : Syn.t) :
+      Code_action_types.edit option =
+    let return_type_string = Syn.text type_node in
+    if
+      (not @@ Syn.is_missing type_node)
+      && (not @@ String.is_prefix return_type_string ~prefix:"Awaitable")
+      && (not @@ String.is_prefix return_type_string ~prefix:"~Awaitable")
+    then
+      let+ return_type_offset = Syn.offset type_node in
+      let pos =
+        let start_pos = pos_of_offset return_type_offset in
+        let end_pos = pos_of_offset (Syn.end_offset type_node + 1) in
+        Pos.merge start_pos end_pos
+      in
+      let text = Printf.sprintf "Awaitable<%s>" return_type_string in
+      Code_action_types.{ pos; text }
+    else
+      None
+  in
+
   let parents =
     let root = Provider_context.PositionedSyntaxTree.root positioned_tree in
     let offset =
@@ -101,15 +122,31 @@ let find_modifier_and_signature_edits path source_text positioned_tree pos :
   |> List.map ~f:Syn.syntax
   |> List.find_map ~f:(function
          | Syn.LambdaExpression
-             { lambda_async = Syn.{ syntax = Missing; _ } as node; _ } -> begin
-           Syn.offset node
-           |> Option.map ~f:(fun offset ->
-                  [
-                    Code_action_types.
-                      { pos = pos_of_offset offset; text = "async " };
-                  ])
+             {
+               lambda_async = Syn.{ syntax = modifier; _ } as node;
+               lambda_signature = Syn.{ syntax = signature; _ };
+               _;
+             } -> begin
+           let signature_edit_opt =
+             begin
+               match signature with
+               | Syn.LambdaSignature { lambda_type; _ } ->
+                 signature_edit_of_type_node lambda_type
+               | _ -> None
+             end
+           in
+           let modifier_edit_opt =
+             begin
+               match modifier with
+               | Syn.Missing ->
+                 let+ offset = Syn.offset node in
+                 Code_action_types.
+                   { pos = pos_of_offset offset; text = "async " }
+               | _ -> None
+             end
+           in
+           Some (List.filter_opt [signature_edit_opt; modifier_edit_opt])
          end
-         | Syn.LambdaExpression _ -> Some []
          | Syn.FunctionDeclaration
              {
                function_declaration_header =
@@ -144,27 +181,7 @@ let find_modifier_and_signature_edits path source_text positioned_tree pos :
                    };
                _;
              } ->
-           let open Option.Let_syntax in
-           let signature_edit_opt =
-             let return_type_string = Syn.text function_type in
-             if
-               (not @@ String.is_prefix return_type_string ~prefix:"Awaitable")
-               && not
-                  @@ String.is_prefix return_type_string ~prefix:"~Awaitable"
-             then
-               let+ return_type_offset = Syn.offset function_type in
-               let pos =
-                 let start_pos = pos_of_offset return_type_offset in
-                 let end_pos =
-                   pos_of_offset (Syn.end_offset function_type + 1)
-                 in
-                 Pos.merge start_pos end_pos
-               in
-               let text = Printf.sprintf "Awaitable<%s>" return_type_string in
-               Code_action_types.{ pos; text }
-             else
-               None
-           in
+           let signature_edit_opt = signature_edit_of_type_node function_type in
            let modifier_edit_opt =
              let has_async =
                match Syn.syntax function_modifiers with

12fe3e1a8e98269db1fbab2fc5e550aa59416631
Conner Nilsen <connernilsen@meta.com>
2024-03-04 18:30:37 -0800

    Pyre Configurationless migration for] [batch:85/112] [shard:6/N]
    Reviewed By: inseokhwang
    
    Differential Revision: D54438157
    
    fbshipit-source-id: a6acfe146ed29fff82123b5e458906d4b4cee6a2

--- third-party/watchman/src/watchman/integration/lib/__init__.py
+++ third-party/watchman/src/watchman/integration/lib/__init__.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-unsafe
+
 import os.path
 
 if "WATCHMAN_INTEGRATION_HELPERS" in os.environ:

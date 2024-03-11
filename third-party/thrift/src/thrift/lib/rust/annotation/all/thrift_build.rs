5ce8ec26c8c5dde20cc1db16cdca9f6378541f41
David Tolnay <dtolnay@meta.com>
2024-03-11 13:55:15 -0700

    Update autocargo component on FBS:master
    Reviewed By: zertosh
    
    Differential Revision: D54770836
    
    fbshipit-source-id: 2417729ed8d0e2a7bdfca98aeed106fd30d7ab36

--- third-party/thrift/src/thrift/lib/rust/annotation/all/thrift_build.rs
+++ third-party/thrift/src/thrift/lib/rust/annotation/all/thrift_build.rs
@@ -29,7 +29,7 @@ thrift thrift //thrift/annotation:thrift-rust",
     ).expect("Failed to write cratemap");
 
     let conf = {
-        let mut conf = Config::from_env(GenContext::Lib).expect("Failed to instantiate thrift_compiler::Config");
+        let mut conf = Config::from_env(GenContext::Types).expect("Failed to instantiate thrift_compiler::Config");
 
         let path_from_manifest_to_base: &Path = "../../../../..".as_ref();
         let cargo_manifest_dir =

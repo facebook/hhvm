9c8772d10bd6f96a0fd2d57a45838b1d9f69f820
Edwin Smith <smith@meta.com>
2024-03-12 07:39:15 -0700

    simplify AdataState by not cloning TypedValues
    Summary:
    Why store two copies of every array when one will do?
    
    A long time ago when hackc supported array provenance, two identical
    array literals could have different provenance information. Now they can't.
    
    Reviewed By: aorenste
    
    Differential Revision: D54713577
    
    fbshipit-source-id: 0fa084804ce78af04b64b5b20eb01bad55728784

--- hphp/hack/src/hackc/emitter/adata_state.rs
+++ hphp/hack/src/hackc/emitter/adata_state.rs
@@ -2,33 +2,30 @@
 //
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
-use hash::HashMap;
+use hash::IndexSet;
 use hhbc::Adata;
 use hhbc::AdataId;
 use hhbc::TypedValue;
 
 #[derive(Debug, Default)]
 pub struct AdataState {
-    shared: HashMap<TypedValue, AdataId>,
-    adata: Vec<Adata>,
+    shared: IndexSet<TypedValue>,
 }
 
 impl AdataState {
     pub fn intern(&mut self, tv: TypedValue) -> AdataId {
-        *self
-            .shared
-            .entry(tv)
-            .or_insert_with_key(|tv| push(&mut self.adata, tv.clone()))
+        let (i, _) = self.shared.insert_full(tv);
+        AdataId::new(i)
     }
 
     pub fn take_adata(&mut self) -> Vec<Adata> {
-        self.shared = Default::default();
-        std::mem::take(&mut self.adata)
+        self.shared
+            .drain(..)
+            .enumerate()
+            .map(|(i, value)| Adata {
+                id: AdataId::new(i),
+                value,
+            })
+            .collect()
     }
-}
-
-fn push(adata: &mut Vec<Adata>, value: TypedValue) -> AdataId {
-    let id = AdataId::new(adata.len());
-    adata.push(Adata { id, value });
-    id
 }

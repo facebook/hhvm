7d6413d171523a22b9a9ac2ac99b928162132967
Aristidis Papaioannou <aristidis@meta.com>
2024-03-22 18:21:59 -0700

    First (partial) implementation, using dataclasses
    Summary:
    Basic and partial implementation, using Python's `dataclass`.
    
    Features:
    * Creates a (data)class per thrift `struct`, with attributes for each field.
    * Prevents inheriting from the generated (thrift) class.
    
    Known limitations:
    * Does not support adapted fields (will raise error)
    * Does not honor default values (custom or otherwise) or qualifiers (`optional`, `required`, etc.): all fields are `None` by default.
    
    Reminder: this is only enabled if the corresponding experimental flag is explicitly given to the thrift compiler
    
    Reviewed By: yoney
    
    Differential Revision: D55156752
    
    fbshipit-source-id: 6f26cddce77214a00e1bc4771ed4f7d389e44a42

--- third-party/thrift/src/thrift/lib/python/mutable_types.py
+++ third-party/thrift/src/thrift/lib/python/mutable_types.py
@@ -12,20 +12,97 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import dataclasses
+import typing
+
+
+class MutableStructOrUnion:
+    pass
+
+
+class MutableStruct(MutableStructOrUnion):
+    pass
+
+
+def _gen_subclass_forbidden(thrift_type_name: str):
+    """
+    Returns a callable suitable for usage as a '__init_subclass__' classmethod,
+    which always raises a `TypeError` (effectively disabling inheritance).
+
+    Args:
+        thrift_type_name: the (human-readable) name of the Thrift type (struct,
+            union, etc.) for which the returned lambda will prevent
+            inheritance. Used for error messaging.
+    """
+
+    def _subclass_forbidden(cls):
+        raise TypeError(
+            "Inheriting from thrift-python data types is forbidden: "
+            f"'{cls.__name__}' cannot inherit from '{thrift_type_name}'."
+        )
+
+    return _subclass_forbidden
+
 
 class MutableStructMeta(type):
     """Metaclass for all generated (mutable) thrift-python Struct types."""
 
-    def __new__(metacls, cls_name, bases, dct):
+    def __new__(metacls, cls_name: str, bases: tuple, dct: dict):
         """
         Returns a new mutable Thrift Struct class with the given name and
         members.
 
-        Raises:
-            NotImplementedError: always (not implemented YET).
+        Args:
+            cls_name: Name of the Thrift Struct, as specified in the Thrift IDL.
+
+            bases: unused, MUST be empty.
+
+            dct: class members, including the SPEC for this class under
+                the key '_fbthrift_SPEC'.
         """
-        raise NotImplementedError(
-            "MutableStructMeta: thrift-python mutable types are not "
-            "implemented yet, cannot create class for Thrift Struct: "
-            f"{cls_name}"
+        if bases:
+            raise ValueError(
+                f"New mutable thrift-python struct class ('{cls_name}') must "
+                "not have base types, but had: {bases}"
+            )
+
+        # Set[Tuple (field spec)]. See `StructInfo` class docstring for the
+        # contents of the field spec tuples.
+        fields = dct.pop("_fbthrift_SPEC")
+
+        dataclass_fields = []
+        for field_spec in fields:
+            (
+                id,
+                qualifier,
+                name,
+                type_info,
+                default_value,
+                adapter_info,
+                is_primitive,
+            ) = field_spec
+
+            if adapter_info is not None:
+                raise NotImplementedError(
+                    "MutableStructMeta: adapted fields are not yet supported "
+                    f"for mutable thrift-python struct (struct={cls_name}, "
+                    f"field_name={name})."
+                )
+
+            field_type = typing.Any
+            dataclass_field = dataclasses.field(
+                default=None, metadata={"__fbthrift_field_spec": field_spec}
+            )
+            dataclass_fields.append((name, field_type, dataclass_field))
+
+        return dataclasses.make_dataclass(
+            cls_name,
+            dataclass_fields,
+            bases=(MutableStruct,),
+            namespace={
+                "__init_subclass__": classmethod(_gen_subclass_forbidden(cls_name)),
+            },
+            match_args=False,
+            kw_only=True,
+            slots=True,
         )

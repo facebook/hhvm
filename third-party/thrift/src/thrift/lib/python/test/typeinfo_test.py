050547de8785379a0bdc600e4c48bc2b7ad2e049
Alper Yoney <alperyoney@meta.com>
2024-03-27 13:18:37 -0700

    Add TypeInfo tests
    Summary: Add unit-tests for `{List|Set|Integer|String}TypeInfo`.
    
    Reviewed By: ahilger
    
    Differential Revision: D55321837
    
    fbshipit-source-id: d829ef106f1244da87bbd0d50d28b7c3d1e4659d

---
+++ third-party/thrift/src/thrift/lib/python/test/typeinfo_test.py
@@ -0,0 +1,37 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pyre-strict
+
+import unittest
+
+from thrift.python.test.typeinfo_test import TypeInfoTests as CTests
+
+# This python file serves as a boilerplate code for executing tests written
+# in a Cython module. Simply import the Cython module containing the tests,
+# and call the appropriate test functions within the TestCase class.
+
+
+class TypeInfoTests(unittest.TestCase):
+    def test_IntegerTypeInfo(self) -> None:
+        CTests(self).test_IntegerTypeInfo()
+
+    def test_StringTypeInfo(self) -> None:
+        CTests(self).test_StringTypeInfo()
+
+    def test_ListTypeInfo(self) -> None:
+        CTests(self).test_ListTypeInfo()
+
+    def test_SetTypeInfo(self) -> None:
+        CTests(self).test_SetTypeInfo()
---
+++ third-party/thrift/src/thrift/lib/python/test/typeinfo_test.pyx
@@ -0,0 +1,62 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# cython: c_string_type=unicode, c_string_encoding=utf8
+
+from thrift.python.types import ListTypeInfo, SetTypeInfo, typeinfo_i64, typeinfo_string
+
+cdef class TypeInfoTests():
+    def __cinit__(self, unit_test):
+        self.ut = unit_test
+
+    def test_ListTypeInfo(self) -> None:
+        list_type_info = ListTypeInfo(typeinfo_i64)
+
+        with self.ut.assertRaises(TypeError):
+            list_type_info.to_internal_data(None)
+
+        data = list_type_info.to_internal_data([1, 2, 3])
+        self.ut.assertEqual(data, (1, 2, 3))
+
+    def test_SetTypeInfo(self) -> None:
+        set_type_info = SetTypeInfo(typeinfo_i64)
+
+        with self.ut.assertRaises(TypeError):
+            set_type_info.to_internal_data(None)
+
+        data = set_type_info.to_internal_data([1, 2, 3])
+        self.ut.assertEqual(data, frozenset([1, 2, 3]))
+
+        data = set_type_info.to_internal_data([1, 2, 3, 1, 2])
+        self.ut.assertEqual(data, frozenset([1, 2, 3]))
+
+    def test_IntegerTypeInfo(self) -> None:
+        with self.ut.assertRaises(TypeError):
+            typeinfo_i64.to_internal_data(None)
+
+        with self.ut.assertRaises(OverflowError):
+            typeinfo_i64.to_internal_data(2**63)
+
+        data = typeinfo_i64.to_internal_data(2**63 - 1)
+        self.ut.assertEqual(data, 2**63 - 1)
+
+    def test_StringTypeInfo(self) -> None:
+        with self.ut.assertRaises(TypeError):
+            typeinfo_string.to_internal_data(None)
+
+        with self.ut.assertRaises(TypeError):
+            typeinfo_string.to_internal_data(b"AbC")
+
+        data = typeinfo_string.to_internal_data("AbC")
+        self.ut.assertEqual(data, b"AbC")

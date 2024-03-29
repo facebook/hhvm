6b0f4f14a1afa02f28c552ddbb4daa94e053fd77
Duc Nguyen <dvn@meta.com>
2024-03-28 17:00:04 -0700

    Add onBytesEvent() to HTTPTransactionObserverInterface
    Summary:
    * Added `TxnBytesEvent` containing timestamp, used for recording timestamp at various points in the lifetime of a transaction
    * Added `onBytesEvent()` to HTTPTransactionObserverInterface to observe the timestamp at various point in the lifetime of an HTTPTransaction.
    
    Differential Revision: D48589676
    
    fbshipit-source-id: 66b5c46f10c80796aa20310c5aff942c7ddb7b40

--- third-party/proxygen/src/proxygen/lib/http/observer/HTTPTransactionObserverInterface.cpp
+++ third-party/proxygen/src/proxygen/lib/http/observer/HTTPTransactionObserverInterface.cpp
@@ -6,8 +6,34 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+#include <glog/logging.h>
 #include <proxygen/lib/http/observer/HTTPTransactionObserverInterface.h>
+#include <utility>
 
 namespace proxygen {
+
+HTTPTransactionObserverInterface::TxnBytesEvent::Builder&&
+HTTPTransactionObserverInterface::TxnBytesEvent::Builder::setTimestamp(
+    const proxygen::TimePoint& timestamp) {
+  maybeTimestampRef = timestamp;
+  return std::move(*this);
+}
+
+HTTPTransactionObserverInterface::TxnBytesEvent::Builder&&
+HTTPTransactionObserverInterface::TxnBytesEvent::Builder::setType(Type typeIn) {
+  type = typeIn;
+  return std::move(*this);
+}
+
+HTTPTransactionObserverInterface::TxnBytesEvent
+HTTPTransactionObserverInterface::TxnBytesEvent::Builder::build() && {
+  return TxnBytesEvent(*this);
+}
+
+HTTPTransactionObserverInterface::TxnBytesEvent::TxnBytesEvent(
+    const TxnBytesEvent::BuilderFields& builderFields)
+    : timestamp(*CHECK_NOTNULL(builderFields.maybeTimestampRef.get_pointer())),
+      type(builderFields.type) {
+}
 
 } // namespace proxygen
--- third-party/proxygen/src/proxygen/lib/http/observer/HTTPTransactionObserverInterface.h
+++ third-party/proxygen/src/proxygen/lib/http/observer/HTTPTransactionObserverInterface.h
@@ -8,6 +8,9 @@
 
 #pragma once
 
+#include <folly/Optional.h>
+#include <proxygen/lib/utils/Time.h>
+
 namespace proxygen {
 
 class HTTPTransactionObserverAccessor;
@@ -17,9 +20,50 @@ class HTTPTransactionObserverAccessor;
  */
 class HTTPTransactionObserverInterface {
  public:
-  enum class Events {};
-
   virtual ~HTTPTransactionObserverInterface() = default;
+
+  enum class Events {
+    TxnBytes = 1,
+  };
+
+  struct TxnBytesEvent {
+    enum class Type : uint8_t {
+      FIRST_HEADER_BYTE_WRITE,
+      FIRST_BODY_BYTE_WRITE,
+      FIRST_BODY_BYTE_ACK,
+      LAST_BODY_BYTE_WRITE,
+      LAST_BODY_BYTE_ACK,
+    };
+
+    const proxygen::TimePoint timestamp;
+    const Type type;
+
+    TxnBytesEvent(TxnBytesEvent&&) = delete;
+    TxnBytesEvent& operator=(const TxnBytesEvent&) = delete;
+    TxnBytesEvent& operator=(TxnBytesEvent&& rhs) = delete;
+    TxnBytesEvent(const TxnBytesEvent&) = delete;
+
+    struct BuilderFields {
+      folly::Optional<std::reference_wrapper<const proxygen::TimePoint>>
+          maybeTimestampRef;
+      Type type;
+      explicit BuilderFields() = default;
+    };
+
+    struct Builder : public BuilderFields {
+      Builder&& setTimestamp(const proxygen::TimePoint& timestamp);
+      Builder&& setType(Type type);
+      TxnBytesEvent build() &&;
+      explicit Builder() = default;
+    };
+
+    // Use builder to construct
+    explicit TxnBytesEvent(const BuilderFields& builderFields);
+  };
+
+  virtual void onBytesEvent(HTTPTransactionObserverAccessor* /* txn */,
+                            const TxnBytesEvent& /* event */) noexcept {
+  }
 };
 
 } // namespace proxygen

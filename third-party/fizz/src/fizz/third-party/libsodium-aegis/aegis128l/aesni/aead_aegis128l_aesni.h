7a5f5e1c1b909aa494cf2ec358a50a86822474a5
Huilin Chen <huilinc@meta.com>
2024-04-04 16:13:10 -0700

    Removing libaegis under fizz/third-party
    Differential Revision: D55599037
    
    fbshipit-source-id: e02e3c8a895d054a1b6b640c54e42a5e762c1e4e

--- third-party/fizz/src/fizz/third-party/libsodium-aegis/aegis128l/aesni/aead_aegis128l_aesni.h
+++
@@ -1,6 +0,0 @@
-#include <stdint.h>
-
-#include <fizz/third-party/libsodium-aegis/aegis128l/aead_aegis128l.h>
-
-extern struct crypto_aead_aegis128l_implementation fizz_crypto_aead_aegis128l_aesni_implementation;
-extern struct aegis128l_evp aegis128l_aesni_evp;
--- third-party/fizz/src/fizz/third-party/libsodium-aegis/aegis128l/soft/aead_aegis128l_soft.h
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-
-#include <fizz/third-party/libsodium-aegis/aegis128l/aead_aegis128l.h>
-
-extern struct crypto_aead_aegis128l_implementation
-    fizz_crypto_aead_aegis128l_soft_implementation;
-extern struct aegis128l_evp aegis128l_soft_evp;

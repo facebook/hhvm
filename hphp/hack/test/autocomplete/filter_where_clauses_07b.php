f0a74b2ec70620b24adfd76db8109525ab1cae91
Francesco Zappa Nardelli <fzn@meta.com>
2024-04-29 09:00:07 -0700

    more tests for where clause filtering
    Summary: Investigation of https://fb.workplace.com/groups/hackforhiphop/permalink/26134223119532956/
    
    Reviewed By: madgen
    
    Differential Revision: D56699578
    
    fbshipit-source-id: 8826b144394e29a9c0d5526d51c9c793dc78a52d

---
+++ hphp/hack/test/autocomplete/filter_where_clauses_07b.php
@@ -0,0 +1,35 @@
+<?hh
+
+class ExpectObj<T> {
+  final public function toBeNull(
+    ?Str\SprintfFormatString $msg = null,
+    mixed ...$args
+  )[]: void { }
+
+  final public function toNotBeNull<Tu>(
+    ?Str\SprintfFormatString $msg = null,
+    mixed ...$args
+  )[]: Tu where T = ?Tu {
+    if ($this->obj is null) {
+      assert false;
+    }
+    return $this->obj;
+  }
+
+  final public function __construct(
+    protected T $obj;
+  }
+
+}
+
+function expect<T>(T $obj): ExpectObj<T> {
+  return new ExpectObj<T>();
+}
+
+function nonOptValue(): string {
+  return "hello";
+}
+
+function myTest() {
+  expect(nonOptValue())->AUTO332
+}

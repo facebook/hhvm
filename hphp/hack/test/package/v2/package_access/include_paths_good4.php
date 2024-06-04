24363303cd359c10360dedc2d7e22b091443f25f
Millie Chen <milliechen@meta.com>
2024-06-04 14:58:22 -0700

    Add tests for PackageOverride on classes
    Summary: Now that package overrides have been applied to classes as well in D57978893, add a test for them similar to those for funcs & typedefs in D57951545
    
    Reviewed By: DavidSnider
    
    Differential Revision: D57983207
    
    fbshipit-source-id: 3a3c45ca699e8d6f0c96ce71fb0a46c3fc219b44

---
+++ hphp/hack/test/package/v2/package_access/include_paths_good4.php
@@ -0,0 +1,13 @@
+//// foo.php
+<?hh
+// package pkg2
+<<file: __EnableUnstableFeatures('package_v2'), __PackageOverride('pkg2')>>
+class Foo {}
+
+//// bar.php
+<?hh
+// package pkg2
+<<file: __EnableUnstableFeatures('package_v2'), __PackageOverride('pkg2')>>
+function bar(): void {
+  $foo = new Foo();
+}

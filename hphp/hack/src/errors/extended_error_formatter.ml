3e5885f98e173b9a14986fae134c466839e759a1
Michael Thomas <mjt@meta.com>
2024-06-05 03:44:11 -0700

    Handle empty positions
    Summary: Handle empty positions when printing context
    
    Differential Revision: D58181752
    
    fbshipit-source-id: da1973f02540d3d1aac9db684e6aca2f0367b574

--- hphp/hack/src/errors/extended_error_formatter.ml
+++ hphp/hack/src/errors/extended_error_formatter.ml
@@ -57,79 +57,89 @@ let merge ps =
 let mark_with_context (pos : Pos.absolute) ~buf ~spans =
   (* Find the containing span for our position *)
   let path = Pos.filename pos in
-  let ctxt_pos =
-    List.find_exn ~f:(fun spos -> Pos.contains spos @@ Pos.to_relative pos)
-    @@ SMap.find path spans
+  let ctxt_pos_opt =
+    Option.bind
+      (SMap.find_opt path spans)
+      ~f:
+        (List.find ~f:(fun spos ->
+             try Pos.contains spos @@ Pos.to_relative pos with
+             | _ -> false))
   in
-  (* Determine start and end lines and columns and reindex to 0 *)
-  let (ctxt_start_ln, ctxt_end_ln, start_ln, end_ln, start_col, end_col) =
-    let ctxt_start_ln = Pos.line ctxt_pos
-    and ctxt_end_ln = Pos.end_line ctxt_pos in
-    let (start_ln, end_ln, start_col, end_col) = Pos.info_pos_extended pos in
-    ( ctxt_start_ln - pad_before - 1,
-      ctxt_end_ln + pad_after - 1,
-      start_ln - 1,
-      end_ln - 1,
-      start_col - 1,
-      end_col - 1 )
-  in
-  (* We'll use the same number of characters for each line number so find how
-     many chars the longest line number needs *)
-  let line_num_width = 1 + (String.length @@ string_of_int end_ln) in
-  (* Write to buffer *)
-  List.iteri (Errors.read_lines path) ~f:(fun ln str ->
-      if ln < ctxt_start_ln || ln > ctxt_end_ln then
-        (* This line isn't in our containing span so skip it *)
-        ()
-      else if
-        (ln = ctxt_start_ln || ln = ctxt_end_ln)
-        && String.(is_empty @@ lstrip str)
-      then
-        (* If the line is blank and is either the first of last line, skip it *)
-        ()
-      else begin
-        (* First write the line number and gutter marker  *)
-        let ln_num =
-          String.pad_left (Int.to_string (ln + 1)) ~char:' ' ~len:line_num_width
-        in
-        let (_ : unit) =
-          Buffer.add_string buf ln_num;
-          Buffer.add_string buf " | "
-        in
-        let is_start = ln = start_ln and is_end = ln = end_ln in
-        let (_ : unit) =
-          if is_start && is_end then (
-            (* If our target pos starts and ends on the line..
-               - write the prefix before the start of the marked pos
-               - write the lhs cursor marker
-               - write the marked string
-               - write the rhs cursor marker
-               - write the suffix after the end of the marked pos
-            *)
-            Buffer.add_string buf (String.prefix str start_col);
-            Buffer.add_string buf cursor_l;
-            Buffer.add_string buf (String.slice str start_col (end_col + 1));
-            Buffer.add_string buf cursor_r;
-            Buffer.add_string buf (String.drop_prefix str (end_col + 1))
-          ) else if is_start then (
-            (* If this is the start line, write the prefix, the lhs cursor and
-               substring of the marked pos that is on this line *)
-            Buffer.add_string buf (String.prefix str start_col);
-            Buffer.add_string buf cursor_l;
-            Buffer.add_string buf (String.drop_prefix str start_col)
-          ) else if is_end then (
-            (* If this is the end line, write the substring of the marked pos
-               that is on the line, the rhs cursor and the suffix *)
-            Buffer.add_string buf (String.prefix str end_col);
-            Buffer.add_string buf cursor_r;
-            Buffer.add_string buf (String.drop_prefix str end_col)
-          ) else
-            (* Either the whole line is part of the target pos or the target
-               is not on the line at all; either way, write the entire line *)
-            Buffer.add_string buf str
-        in
-        Buffer.add_string buf "\n"
-      end)
+  match ctxt_pos_opt with
+  | Some ctxt_pos ->
+    (* Determine start and end lines and columns and reindex to 0 *)
+    let (ctxt_start_ln, ctxt_end_ln, start_ln, end_ln, start_col, end_col) =
+      let ctxt_start_ln = Pos.line ctxt_pos
+      and ctxt_end_ln = Pos.end_line ctxt_pos in
+      let (start_ln, end_ln, start_col, end_col) = Pos.info_pos_extended pos in
+      ( ctxt_start_ln - pad_before - 1,
+        ctxt_end_ln + pad_after - 1,
+        start_ln - 1,
+        end_ln - 1,
+        start_col - 1,
+        end_col - 1 )
+    in
+    (* We'll use the same number of characters for each line number so find how
+       many chars the longest line number needs *)
+    let line_num_width = 1 + (String.length @@ string_of_int end_ln) in
+    (* Write to buffer *)
+    List.iteri (Errors.read_lines path) ~f:(fun ln str ->
+        if ln < ctxt_start_ln || ln > ctxt_end_ln then
+          (* This line isn't in our containing span so skip it *)
+          ()
+        else if
+          (ln = ctxt_start_ln || ln = ctxt_end_ln)
+          && String.(is_empty @@ lstrip str)
+        then
+          (* If the line is blank and is either the first of last line, skip it *)
+          ()
+        else begin
+          (* First write the line number and gutter marker  *)
+          let ln_num =
+            String.pad_left
+              (Int.to_string (ln + 1))
+              ~char:' '
+              ~len:line_num_width
+          in
+          let (_ : unit) =
+            Buffer.add_string buf ln_num;
+            Buffer.add_string buf " | "
+          in
+          let is_start = ln = start_ln and is_end = ln = end_ln in
+          let (_ : unit) =
+            if is_start && is_end then (
+              (* If our target pos starts and ends on the line..
+                 - write the prefix before the start of the marked pos
+                 - write the lhs cursor marker
+                 - write the marked string
+                 - write the rhs cursor marker
+                 - write the suffix after the end of the marked pos
+              *)
+              Buffer.add_string buf (String.prefix str start_col);
+              Buffer.add_string buf cursor_l;
+              Buffer.add_string buf (String.slice str start_col (end_col + 1));
+              Buffer.add_string buf cursor_r;
+              Buffer.add_string buf (String.drop_prefix str (end_col + 1))
+            ) else if is_start then (
+              (* If this is the start line, write the prefix, the lhs cursor and
+                 substring of the marked pos that is on this line *)
+              Buffer.add_string buf (String.prefix str start_col);
+              Buffer.add_string buf cursor_l;
+              Buffer.add_string buf (String.drop_prefix str start_col)
+            ) else if is_end then (
+              (* If this is the end line, write the substring of the marked pos
+                 that is on the line, the rhs cursor and the suffix *)
+              Buffer.add_string buf (String.prefix str end_col);
+              Buffer.add_string buf cursor_r;
+              Buffer.add_string buf (String.drop_prefix str end_col)
+            ) else
+              (* Either the whole line is part of the target pos or the target
+                 is not on the line at all; either way, write the entire line *)
+              Buffer.add_string buf str
+          in
+          Buffer.add_string buf "\n"
+        end)
+  | _ -> Buffer.add_string buf "No source found.\n\n"
 
 let to_string User_error.{ code; claim = (pos, msg); reasons; severity; _ } =
   (* First fold over all positions to get a map from filename to the list of all
@@ -142,13 +152,16 @@ let to_string User_error.{ code; claim = (pos, msg); reasons; severity; _ } =
         merge @@ List.sort ps ~compare:(fun x y -> Pos.compare y x))
     @@ List.fold_left reasons ~init ~f:(fun acc ((p : Pos.absolute), _) ->
            let file_checked = Pos.filename p in
-           let p = Pos.to_relative p in
-           SMap.update
-             file_checked
-             (function
-               | Some ps -> Some (p :: ps)
-               | None -> Some [p])
-             acc)
+           try
+             let p = Pos.to_relative p in
+             SMap.update
+               file_checked
+               (function
+                 | Some ps -> Some (p :: ps)
+                 | None -> Some [p])
+               acc
+           with
+           | _ -> acc)
   in
   let buf = Buffer.create 500 in
   (* Next, write the primary claim to the buffer *)

c88c2c91e7ec4e083c43e42c3a2491701be11c32
Dave Kwon <dokwon@meta.com>
2024-06-05 14:43:00 -0700

    Remove optimization for MapPatch::Remove in extractMaskFromPatch
    Summary: The optimization added for both set and map since we cannot distinguish them. This is causing issue for `extractMaskFromPatch`. Until we have proper fix for `extractMaskFromPatch` disable this for non-view and only enable optimization for `extractMaskViewFromPatch`.
    
    Reviewed By: Mizuchi
    
    Differential Revision: D58196563
    
    fbshipit-source-id: 6eec96aeb0cb00d7cf4be5f53437bc70cdf64d12

--- third-party/thrift/src/thrift/lib/cpp2/protocol/Patch.cpp
+++ third-party/thrift/src/thrift/lib/cpp2/protocol/Patch.cpp
@@ -760,13 +760,18 @@ ExtractedMasksFromPatch extractMaskFromPatch(
   }
   // We can only distinguish struct. For struct, add removed fields to write
   // mask. Both set and map use a set for Remove, so they are indistinguishable.
-  // Therefore, we always add removed keys to write map mask.
+  // For set and map, we cannot distinguish them. For view, we always add
+  // removed keys to write map mask. For non-view, it is a read-write operation
+  // if not intristic default.
   if (auto* value = findOp(patch, PatchOp::Remove)) {
     if (value->is_list()) {
       // struct patch
       insertRemoveWriteFieldsToMask(masks.write, value->as_list());
     } else if (!isIntrinsicDefault(*value)) {
       // set/map patch
+      if (!view) {
+        return {allMask(), allMask()};
+      }
       insertWriteKeysToMapMask(masks.write, value->as_set(), view);
     }
   }

67328c1ec92b082bf830725a06c22e5f8dff8757
Michael Thomas <mjt@meta.com>
2024-06-10 03:39:42 -0700

    Print primary claim in extended formatter
    Summary: Include snippet and position of primary claim
    
    Differential Revision: D58191719
    
    fbshipit-source-id: 2227a5c1cd727fc6750d0d1c98479eef209c34ca

--- hphp/hack/src/errors/extended_error_formatter.ml
+++ hphp/hack/src/errors/extended_error_formatter.ml
@@ -171,7 +171,11 @@ let to_string User_error.{ code; claim = (pos, msg); reasons; severity; _ } =
          "%s: %s %s\n\n"
          (User_error.Severity.to_string severity)
          (User_error.error_code_to_string code)
-         msg
+         msg;
+    mark_with_context pos ~buf ~spans;
+    Buffer.add_string buf "\n";
+    Buffer.add_string buf (Pos.multiline_string pos);
+    Buffer.add_string buf "\n\n"
   in
   let (_ : unit) =
     List.iter reasons ~f:(fun (pos, msg) ->

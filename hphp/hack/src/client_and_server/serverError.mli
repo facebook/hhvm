15b1361ec29264501db5da7e9ca740a3adfad167
Henri Verroken <hverr@meta.com>
2024-06-10 04:31:10 -0700

    Add serverError.mli
    Summary: Just adds an mli file for serverError to make it's interface a bit more clear
    
    Differential Revision: D58354849
    
    fbshipit-source-id: 47877c4525e4401e41638ad05edcf8de0ad0f06b

---
+++ hphp/hack/src/client_and_server/serverError.mli
@@ -0,0 +1,19 @@
+(*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the "hack" directory of this source tree.
+ *
+ *)
+
+val get_save_state_result_props_json :
+  SaveStateServiceTypes.save_state_result -> (string * Hh_json.json) list
+
+val print_error_list :
+  Out_channel.t ->
+  stale_msg:string option ->
+  output_json:bool ->
+  error_list:Errors.finalized_error list ->
+  save_state_result:SaveStateServiceTypes.save_state_result option ->
+  recheck_stats:Telemetry.t option ->
+  unit

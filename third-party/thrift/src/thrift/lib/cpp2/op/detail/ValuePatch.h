d2627e2fd5f0100fab7a1cf1f021941e15168f07
Dave Kwon <dokwon@meta.com>
2024-06-12 12:19:08 -0700

    Add AssignPatch::empty override
    Reviewed By: Mizuchi
    
    Differential Revision: D58368274
    
    fbshipit-source-id: 4aadb9efa09da752d50b564deaf369e1dd612697

--- third-party/thrift/src/thrift/lib/cpp2/op/detail/ValuePatch.h
+++ third-party/thrift/src/thrift/lib/cpp2/op/detail/ValuePatch.h
@@ -130,6 +130,8 @@ class AssignPatch : public BaseAssignPatch<Patch, AssignPatch<Patch>> {
         protocol::detail::parseValue(prot, TType::T_STRUCT).as_object());
   }
 
+  bool empty() const { return !dynPatch_.has_value() && Base::empty(); }
+
  private:
   using Base::data_;
   std::optional<protocol::Object> dynPatch_;

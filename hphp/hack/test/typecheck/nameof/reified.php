6f20b1e44bc68532cad9bd4141613943359ca76e
Vassil Mladenov <vmladenov@meta.com>
2024-06-20 18:54:00 -0700

    Explicitly enumerate valid nameof targets in lowerer
    Summary: Expecting the result to be an `Aast.Id` is insufficient because it does not see parens e.g. `nameof (Foo)`. This is tricky in the parser because xhp names, self, static, parent, and (qualified) names are heterogenous in their parsing. Let's just be explicit about what Syntax nodes are supported.
    
    Reviewed By: viratyosin
    
    Differential Revision: D58843483
    
    fbshipit-source-id: 383ed114a8f1dd7bff94879fc05e96d0efc05a3f

--- hphp/hack/test/typecheck/nameof/reified.php
+++ hphp/hack/test/typecheck/nameof/reified.php
@@ -6,12 +6,3 @@ function f<reify T as C>(): void {
   expect_string(T::class);
   expect_string(nameof T);
 }
-class R<reify Tr as C> {
-  public function m<reify Tm as C>(): void {
-    expect_string(Tr::class);
-    expect_string(nameof Tr);
-
-    expect_string(Tm::class);
-    expect_string(nameof Tm);
-  }
-}

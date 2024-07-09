cbde34ec5adba122d36c26f8e67b0a25a989153d
TJ Yin <ytj@meta.com>
2024-07-09 10:25:02 -0700

    Remove unnecessary files
    Reviewed By: vitaut
    
    Differential Revision: D59524639
    
    fbshipit-source-id: 03d0629860ee6c08f8363467cef849a32da8b00c

--- third-party/thrift/src/thrift/lib/thrift/detail/protocol_patch.cpp
+++
@@ -1,7 +0,0 @@
-// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
-
-#include <thrift/lib/thrift/gen-cpp2/gen_patch_protocol_detail_types.h>
-
-namespace apache::thrift::protocol::detail {
-
-} // namespace apache::thrift::protocol::detail

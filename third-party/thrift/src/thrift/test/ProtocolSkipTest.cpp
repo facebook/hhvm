3e41a8c1bff94104e5ed42b9076cef8cf5c750fa
Shai Szulanski <iahs@meta.com>
2024-07-12 11:03:39 -0700

    Avoid instantiating testset in basic thrift tests
    Summary:
    These generated structs all constitute the bulk in the bulk/bound model of test cases and result in our test suite being impracticably slow
    The tested behavior is so fundamental that it's covered by other tests throughout the suite, and the commit history shows these were added primarily to demonstrate the testset capability rather than prevent a specific regression.
    
    Reviewed By: yoney
    
    Differential Revision: D59658004
    
    fbshipit-source-id: d5e10e0ad058e83f3baa90e1e2fbb0a3675ca8c6

--- third-party/thrift/src/thrift/test/ProtocolSkipTest.cpp
+++ third-party/thrift/src/thrift/test/ProtocolSkipTest.cpp
@@ -17,8 +17,6 @@
 #include <folly/portability/GTest.h>
 #include <thrift/lib/cpp2/protocol/ProtocolReaderStructReadState.h>
 #include <thrift/lib/cpp2/protocol/Serializer.h>
-#include <thrift/test/testset/Populator.h>
-#include <thrift/test/testset/gen-cpp2/testset_types_custom_protocol.h>
 
 namespace apache::thrift::test {
 namespace {
@@ -171,69 +169,6 @@ TEST(ProtocolSkipTest, SkipStopInContainer) {
   }
   EXPECT_TRUE(thrown);
 }
-
-template <typename T>
-class SpecializedSkipTest : public testing::Test {
- private:
-  template <typename Reader>
-  static auto serialized() {
-    std::mt19937 rng;
-    using Writer = typename Reader::ProtocolWriter;
-    auto buf = Serializer<Reader, Writer>::template serialize<std::string>(
-        populated_if_not_adapted<T>(rng));
-    return folly::IOBuf::copyBuffer(buf);
-  }
-
- public:
-  template <typename Reader>
-  void test() {
-    struct SlowReader : Reader {
-      /* implicit */ SlowReader(const Reader& reader) : Reader(reader) {}
-      static size_t fixedSizeInContainer(TType) { return 0; }
-    };
-
-    auto buf = serialized<Reader>();
-
-    Reader prot;
-    prot.setInput(buf.get());
-    apache::thrift::detail::ProtocolReaderStructReadState<Reader> state;
-
-    state.readStructBegin(&prot);
-    state.readFieldBeginNoInline(&prot);
-    while (!state.atStop()) {
-      SlowReader protCopy = prot;
-      protCopy.skip(state.fieldType);
-      prot.skip(state.fieldType);
-      EXPECT_EQ(prot.getCursor(), protCopy.getCursor());
-      state.readFieldEnd(&prot);
-      state.readFieldBeginNoInline(&prot);
-    }
-
-    state.readStructEnd(&prot);
-  }
-};
-
-TYPED_TEST_CASE_P(SpecializedSkipTest);
-
-TYPED_TEST_P(SpecializedSkipTest, Compact) {
-  this->template test<CompactProtocolReader>();
-}
-
-TYPED_TEST_P(SpecializedSkipTest, Binary) {
-  this->template test<BinaryProtocolReader>();
-}
-
-TYPED_TEST_P(SpecializedSkipTest, JSON) {
-  this->template test<JSONProtocolReader>();
-}
-
-TYPED_TEST_P(SpecializedSkipTest, SimpleJSON) {
-  this->template test<SimpleJSONProtocolReader>();
-}
-
-REGISTER_TYPED_TEST_CASE_P(
-    SpecializedSkipTest, Compact, Binary, JSON, SimpleJSON);
-THRIFT_INST_TESTSET_ALL(SpecializedSkipTest);
 
 } // namespace
 } // namespace apache::thrift::test

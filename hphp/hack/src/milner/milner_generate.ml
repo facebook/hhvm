132f51ca23efa8d256f56103261187e5863c7617
Mistral Contrastin <mojpc2@meta.com>
2024-07-17 07:46:22 -0700

    Support case types
    Summary:
    Adds support for case type generation.
    
    Needed to implement a notion of disjointness of `milner` types. I'd have liked to reuse the predicate case types use, but it requires a typing environment. Hence, the new disjointness predicate.
    
    Reviewed By: enetsee
    
    Differential Revision: D59410473
    
    fbshipit-source-id: 83a7888b4a8a62e732e466be4e5c2a6934721846

--- hphp/hack/src/milner/milner_generate.ml
+++ hphp/hack/src/milner/milner_generate.ml
@@ -17,13 +17,14 @@ type primitive =
   | PBool
   | PArraykey
   | PNum
-[@@deriving enum]
+[@@deriving enum, ord]
 
 type kind =
   | KPrimitive
   | KClass
   | KAlias
   | KNewtype
+  | KCase
 [@@deriving enum]
 
 type ty =
@@ -37,6 +38,10 @@ type ty =
       name: string;
       producer: string;
     }
+  | TCase of {
+      name: string;
+      disjuncts: ty list;
+    }
 
 let name_ctr = ref 0
 
@@ -44,6 +49,8 @@ let fresh prefix =
   let n = !name_ctr in
   name_ctr := !name_ctr + 1;
   prefix ^ string_of_int n
+
+let pick l = List.length l - 1 |> Random.int_incl 0 |> List.nth_exn l
 
 let show_ty ty =
   match ty with
@@ -59,6 +66,7 @@ let show_ty ty =
   | TClass info -> info.name
   | TAlias info -> info.name
   | TNewtype info -> info.name
+  | TCase info -> info.name
 
 let rec expr_for = function
   | TPrimitive prim -> begin
@@ -68,15 +76,49 @@ let rec expr_for = function
     | PFloat -> "42.0"
     | PBool -> "true"
     | PArraykey ->
-      let prim = List.nth_exn [PInt; PString] (Random.int_incl 0 1) in
+      let prim = pick [PInt; PString] in
       expr_for (TPrimitive prim)
     | PNum ->
-      let prim = List.nth_exn [PInt; PFloat] (Random.int_incl 0 1) in
+      let prim = pick [PInt; PFloat] in
       expr_for (TPrimitive prim)
   end
   | TClass info -> "new " ^ info.name ^ "()"
   | TAlias info -> expr_for info.aliased
   | TNewtype info -> info.producer ^ "()"
+  | TCase info -> expr_for (pick info.disjuncts)
+
+let are_disjoint_prims prim prim' =
+  let expand_prim = function
+    | PArraykey -> [PInt; PString]
+    | PNum -> [PInt; PFloat]
+    | prim -> [prim]
+  in
+  let prims = expand_prim prim @ expand_prim prim' in
+  let raw_length = List.length prims in
+  let dedup_length =
+    List.dedup_and_sort ~compare:compare_primitive prims |> List.length
+  in
+  raw_length = dedup_length
+
+let rec are_disjoint_tys ty ty' =
+  match (ty, ty') with
+  | (TNewtype _, _)
+  | (_, TNewtype _) ->
+    (* Opaque newtypes (which is all we represent at the moment) cannot be
+       statically checked to be disjoint since the typechecker cannot see the
+       underlying type *)
+    false
+  | (TAlias info, ty') -> are_disjoint_tys info.aliased ty'
+  | (ty, TAlias info) -> are_disjoint_tys ty info.aliased
+  | (TCase info, ty)
+  | (ty, TCase info) ->
+    List.for_all info.disjuncts ~f:(are_disjoint_tys ty)
+  | (TPrimitive prim, TPrimitive prim') -> are_disjoint_prims prim prim'
+  | (TClass _, _)
+  | (_, TClass _) ->
+    (* Each class is distinct from any other type because we generate a fresh
+       class definition each time we generate a new type. *)
+    true
 
 let rec ty () =
   let kind =
@@ -118,3 +160,25 @@ let rec ty () =
     in
     defs := def :: !defs;
     TNewtype { name; producer }
+  | KCase ->
+    let name = fresh "CT" in
+    let rec add_disjuncts disjuncts =
+      if Random.bool () then
+        let disjunct = ty () in
+        if List.for_all disjuncts ~f:(are_disjoint_tys disjunct) then
+          add_disjuncts @@ (disjunct :: disjuncts)
+        else
+          add_disjuncts disjuncts
+      else
+        disjuncts
+    in
+    let disjuncts = add_disjuncts [ty ()] in
+    let def =
+      "case type "
+      ^ name
+      ^ " = "
+      ^ String.concat ~sep:" | " (List.map ~f:show_ty disjuncts)
+      ^ ";"
+    in
+    defs := def :: !defs;
+    TCase { name; disjuncts }

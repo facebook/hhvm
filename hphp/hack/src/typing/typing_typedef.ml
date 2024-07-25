5da76868c3e69b6feb937580d248a52d2d0e0bdb
Catherine Gasnier <catg@meta.com>
2024-07-25 03:18:16 -0700

    doc and refactor in Typing_typedef
    Reviewed By: dlreeves
    
    Differential Revision: D60174686
    
    fbshipit-source-id: a84d76d54b91addf1dafe80fa1a66f92dd8824d4

--- hphp/hack/src/typing/typing_typedef.ml
+++ hphp/hack/src/typing/typing_typedef.ml
@@ -17,7 +17,10 @@ module SN = Naming_special_names
 module Phase = Typing_phase
 module EnvFromDef = Typing_env_from_def
 
-let get_cnstr_errs env tcstr reverse t_pos ty =
+(** [get_cnstr_errs env tcstr super t_pos ty] checks that
+  `ty <: tcstr` if not [super], else if [super] that `ty :> tcstr` *)
+let get_cnstr_errs env tcstr reverse t_pos ty :
+    Typing_env_types.env * Typing_error.t option =
   match tcstr with
   | Some tcstr ->
     let ((env, ty_err_opt1), cstr) =
@@ -41,6 +44,7 @@ let get_cnstr_errs env tcstr reverse t_pos ty =
     (env, Option.merge ~f:Typing_error.both ty_err_opt1 ty_err_opt2)
   | _ -> (env, None)
 
+(** Checks that variants of a case type are disjoint *)
 let casetype_def env typedef =
   let {
     t_annotation = ();
@@ -100,18 +104,39 @@ let casetype_def env typedef =
 
 let typedef_def ctx typedef =
   let env = EnvFromDef.typedef_env ~origin:Decl_counters.TopLevel ctx typedef in
-  let env = Env.set_current_module env typedef.t_module in
-  let env = Env.set_internal env typedef.t_internal in
+  let {
+    t_annotation = ();
+    t_name;
+    t_tparams;
+    t_as_constraint;
+    t_super_constraint;
+    t_kind;
+    t_user_attributes;
+    t_vis;
+    t_mode;
+    t_namespace;
+    t_span;
+    t_emit_id;
+    t_is_ctx;
+    t_file_attributes;
+    t_internal;
+    t_module;
+    t_docs_url;
+    t_doc_comment;
+  } =
+    typedef
+  in
+  let (t_pos, _) = t_name in
+  let env = Env.set_current_module env t_module in
+  let env = Env.set_internal env t_internal in
   let env =
-    Env.set_current_package_override_from_file_attributes
-      env
-      typedef.t_file_attributes
+    Env.set_current_package_override_from_file_attributes env t_file_attributes
   in
   let (env, ty_err_opt1) =
     Phase.localize_and_add_ast_generic_parameters_and_where_constraints
       env
       ~ignore_errors:false
-      typedef.t_tparams
+      t_tparams
       []
   in
   Option.iter ~f:(Typing_error_utils.add_typing_error ~env) ty_err_opt1;
@@ -119,28 +144,6 @@ let typedef_def ctx typedef =
   |> List.iter ~f:(Typing_error_utils.add_typing_error ~env);
   Env.make_depend_on_current_module env;
   Typing_variance.typedef env typedef;
-  let {
-    t_annotation = ();
-    t_name = (t_pos, t_name);
-    t_tparams = _;
-    t_as_constraint = tascstr;
-    t_super_constraint = tsupercstr;
-    t_kind = hint;
-    t_user_attributes = _;
-    t_vis = _;
-    t_mode = _;
-    t_namespace = _;
-    t_span = _;
-    t_emit_id = _;
-    t_is_ctx = _;
-    t_file_attributes = _;
-    t_internal = _;
-    t_module = _;
-    t_docs_url = _;
-    t_doc_comment = _;
-  } =
-    typedef
-  in
 
   let env =
     let ((env, ty_err_opt2), ty) =
@@ -148,14 +151,18 @@ let typedef_def ctx typedef =
       Phase.localize_hint_no_subst
         env
         ~ignore_errors:false
-        ~report_cycle:(t_pos, t_name)
-        hint
+        ~report_cycle:t_name
+        t_kind
     in
     let env = casetype_def env typedef in
     Option.iter ~f:(Typing_error_utils.add_typing_error ~env) ty_err_opt2;
 
-    let (env, ty_err_opt3) = get_cnstr_errs env tascstr false t_pos ty in
-    let (env, ty_err_opt4) = get_cnstr_errs env tsupercstr true t_pos ty in
+    let (env, ty_err_opt3) =
+      get_cnstr_errs env t_as_constraint false t_pos ty
+    in
+    let (env, ty_err_opt4) =
+      get_cnstr_errs env t_super_constraint true t_pos ty
+    in
     Option.iter
       ~f:(Typing_error_utils.add_typing_error ~env)
       (Option.merge ~f:Typing_error.both ty_err_opt3 ty_err_opt4);
@@ -163,7 +170,7 @@ let typedef_def ctx typedef =
   in
 
   let env =
-    match hint with
+    match t_kind with
     | (_pos, Hshape { nsi_allows_unknown_fields = _; nsi_field_map }) ->
       let get_name sfi = sfi.sfi_name in
       Typing_shapes.check_shape_keys_validity
@@ -175,31 +182,27 @@ let typedef_def ctx typedef =
     Typing.attributes_check_def
       env
       SN.AttributeKinds.typealias
-      typedef.t_user_attributes
+      t_user_attributes
   in
-  let (env, tparams) =
-    List.map_env env typedef.t_tparams ~f:Typing.type_param
-  in
-  let (env, file_attributes) =
-    Typing.file_attributes env typedef.t_file_attributes
-  in
+  let (env, tparams) = List.map_env env t_tparams ~f:Typing.type_param in
+  let (env, file_attributes) = Typing.file_attributes env t_file_attributes in
   {
     Aast.t_annotation = Env.save (Env.get_tpenv env) env;
-    Aast.t_name = typedef.t_name;
-    Aast.t_mode = typedef.t_mode;
-    Aast.t_vis = typedef.t_vis;
+    Aast.t_name;
+    Aast.t_mode;
+    Aast.t_vis;
     Aast.t_user_attributes = user_attributes;
-    Aast.t_as_constraint = typedef.t_as_constraint;
-    Aast.t_super_constraint = typedef.t_super_constraint;
-    Aast.t_kind = typedef.t_kind;
+    Aast.t_as_constraint;
+    Aast.t_super_constraint;
+    Aast.t_kind;
     Aast.t_tparams = tparams;
-    Aast.t_namespace = typedef.t_namespace;
-    Aast.t_span = typedef.t_span;
-    Aast.t_emit_id = typedef.t_emit_id;
-    Aast.t_is_ctx = typedef.t_is_ctx;
+    Aast.t_namespace;
+    Aast.t_span;
+    Aast.t_emit_id;
+    Aast.t_is_ctx;
     Aast.t_file_attributes = file_attributes;
-    Aast.t_internal = typedef.t_internal;
-    Aast.t_module = typedef.t_module;
-    Aast.t_docs_url = typedef.t_docs_url;
-    Aast.t_doc_comment = typedef.t_doc_comment;
+    Aast.t_internal;
+    Aast.t_module;
+    Aast.t_docs_url;
+    Aast.t_doc_comment;
   }

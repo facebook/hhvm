2f9d2d30df2dd09456a9d634b83b3ff300a81d42
Pranav Thulasiram Bhat <pranavtbhat@meta.com>
2024-07-29 18:07:05 -0700

    Reland [thrift][mask] Support unions in compare(a, b)
    Summary:
    Reland D59918551
    
    We've decided not to use a separate mask variant for unions
    
    Reviewed By: thedavekwon
    
    Differential Revision: D60266930
    
    fbshipit-source-id: d991e8dc017ab794b411072157d8263cd5e2a885

--- third-party/thrift/src/thrift/lib/cpp2/protocol/detail/FieldMask.h
+++ third-party/thrift/src/thrift/lib/cpp2/protocol/detail/FieldMask.h
@@ -100,10 +100,9 @@ void moveObject(T& field, U&& object) {
 // Throws a runtime error if the mask contains a map mask.
 void throwIfContainsMapMask(const Mask& mask);
 
-template <typename Struct>
-void compare_impl(
-    const Struct& original, const Struct& modified, FieldIdToMask& mask) {
-  op::for_each_field_id<Struct>([&](auto id) {
+template <typename T>
+void compare_impl(const T& original, const T& modified, FieldIdToMask& mask) {
+  op::for_each_field_id<T>([&](auto id) {
     using Id = decltype(id);
     int16_t fieldId = folly::to_underlying(id());
     auto&& original_field = op::get<Id>(original);
@@ -122,8 +121,8 @@ void compare_impl(
       return;
     }
     // check if nested fields need to be added to mask
-    using FieldType = op::get_native_type<Struct, Id>;
-    if constexpr (is_thrift_struct_v<FieldType>) {
+    using FieldType = op::get_native_type<T, Id>;
+    if constexpr (is_thrift_class_v<FieldType>) {
       compare_impl(
           *original_ptr, *modified_ptr, mask[fieldId].includes_ref().emplace());
       return;

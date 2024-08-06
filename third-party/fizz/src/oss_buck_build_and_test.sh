6a8f9c0af367a00de7f1f46ecf07da324d007f28
Fred Qiu <qiu@meta.com>
2024-08-06 10:46:12 -0700

    Fix copyright headers
    Summary: Fix copyright headers.
    
    Reviewed By: NickR23
    
    Differential Revision: D60831082
    
    fbshipit-source-id: 1a94aa84361d308fb6f0d9fb8d0a6d96c9f34bdc

--- third-party/fizz/src/oss_buck_build_and_test.sh
+++ third-party/fizz/src/oss_buck_build_and_test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
 
 
 if [ "$GITHUB_ACTIONS" == "true" ]; then

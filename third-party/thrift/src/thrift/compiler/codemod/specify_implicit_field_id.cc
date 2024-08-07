4f4ad4fc67c22e67f75cf99ada9bf0f8dd8b6094
Shai Szulanski <iahs@meta.com>
2024-08-07 15:47:51 -0700

    Add codemod to fill in implicit field ids
    Reviewed By: yoney
    
    Differential Revision: D60919266
    
    fbshipit-source-id: 8a97f5f3601843b07ae5ebcb1095d93f14097141

---
+++ third-party/thrift/src/thrift/compiler/codemod/specify_implicit_field_id.cc
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <algorithm>
+#include <map>
+#include <string>
+
+#include <fmt/core.h>
+
+#include <thrift/compiler/ast/ast_visitor.h>
+#include <thrift/compiler/ast/t_program_bundle.h>
+#include <thrift/compiler/ast/t_struct.h>
+#include <thrift/compiler/codemod/file_manager.h>
+#include <thrift/compiler/compiler.h>
+#include <thrift/compiler/lib/cpp2/util.h>
+
+using namespace apache::thrift::compiler;
+
+namespace {
+class specify_implicit_field_id {
+ public:
+  specify_implicit_field_id(source_manager& sm, t_program& program)
+      : fm_(sm, program), sm_(sm), prog_(program) {}
+
+  void run() {
+    const_ast_visitor visitor;
+
+    visitor.add_field_visitor([=](const auto& f) { visit_field(f); });
+
+    visitor(prog_);
+
+    fm_.apply_replacements();
+  }
+
+  void visit_field(const t_field& f) {
+    if (f.explicit_id()) {
+      return;
+    }
+    fm_.add(
+        {f.src_range().begin.offset(),
+         f.src_range().begin.offset(),
+         fmt::format("{}\n    {}: ", kComment, f.id())});
+  }
+
+ private:
+  codemod::file_manager fm_;
+  source_manager sm_;
+  t_program& prog_;
+
+  constexpr static std::string_view kComment =
+      "// @lint-ignore thrift-compiler-warning Negative field id is deprecated, don't add new ones.";
+};
+} // namespace
+
+int main(int argc, char** argv) {
+  auto source_mgr = source_manager();
+  auto program_bundle = parse_and_get_program(
+      source_mgr, std::vector<std::string>(argv, argv + argc));
+  if (!program_bundle) {
+    return 1;
+  }
+  auto program = program_bundle->root_program();
+  specify_implicit_field_id(source_mgr, *program).run();
+
+  return 0;
+}
---
+++ third-party/thrift/src/thrift/compiler/codemod/specify_implicit_field_id_test.py
@@ -0,0 +1,112 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import shutil
+import tempfile
+import textwrap
+import unittest
+
+import pkg_resources
+
+from thrift.compiler.codemod.test_utils import read_file, run_binary, write_file
+
+
+class HoistAnnotatedTypes(unittest.TestCase):
+    def setUp(self):
+        tmp = tempfile.mkdtemp()
+        self.addCleanup(shutil.rmtree, tmp, True)
+        self.tmp = tmp
+        self.addCleanup(os.chdir, os.getcwd())
+        os.chdir(self.tmp)
+        self.maxDiff = None
+
+    def test_basic_replace(self):
+        write_file(
+            "foo.thrift",
+            textwrap.dedent(
+                """\
+                struct A {
+                    1: string foo;
+                }
+                struct B {
+                    string foo;
+                }
+                struct C {
+                    string foo; // comment
+                    string bar; /// docblock
+                }
+                struct D {
+                    string foo;
+                    1: string bar;
+                }
+                struct E {
+                    // comment
+                    string foo;
+                    /// docblock
+                    string bar;
+                }
+                struct F {
+                    // @lint-ignore thrift-compiler-warning Negative field id is deprecated, don't add new ones.
+                    -1: string foo;
+                    1: string bar;
+                    string added_after_codemod;
+                }
+                """
+            ),
+        )
+
+        binary = pkg_resources.resource_filename(__name__, "codemod")
+        run_binary(binary, "foo.thrift")
+
+        self.assertEqual(
+            read_file("foo.thrift"),
+            textwrap.dedent(
+                """\
+                struct A {
+                    1: string foo;
+                }
+                struct B {
+                    // @lint-ignore thrift-compiler-warning Negative field id is deprecated, don't add new ones.
+                    -1: string foo;
+                }
+                struct C {
+                    // @lint-ignore thrift-compiler-warning Negative field id is deprecated, don't add new ones.
+                    -1: string foo; // comment
+                    // @lint-ignore thrift-compiler-warning Negative field id is deprecated, don't add new ones.
+                    -2: string bar; /// docblock
+                }
+                struct D {
+                    // @lint-ignore thrift-compiler-warning Negative field id is deprecated, don't add new ones.
+                    -1: string foo;
+                    1: string bar;
+                }
+                struct E {
+                    // comment
+                    // @lint-ignore thrift-compiler-warning Negative field id is deprecated, don't add new ones.
+                    -1: string foo;
+                    /// docblock
+                    // @lint-ignore thrift-compiler-warning Negative field id is deprecated, don't add new ones.
+                    -2: string bar;
+                }
+                struct F {
+                    // @lint-ignore thrift-compiler-warning Negative field id is deprecated, don't add new ones.
+                    -1: string foo;
+                    1: string bar;
+                    // @lint-ignore thrift-compiler-warning Negative field id is deprecated, don't add new ones.
+                    -2: string added_after_codemod;
+                }
+                """
+            ),
+        )

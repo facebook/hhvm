e9dd621f2122f2dbb0addfa65a970741132a87ec
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2024-08-09 16:16:54 -0700

    removing base_module] fbcode/thrift/lib/python/conformance/TARGETS
    Reviewed By: avalonalex
    
    Differential Revision: D61015708
    
    fbshipit-source-id: f3993b12abd3aed55af6ac14dc24060740bd3033

--- third-party/thrift/src/thrift/lib/python/conformance/test/any_registry.py
+++ third-party/thrift/src/thrift/lib/python/conformance/test/any_registry.py
@@ -20,7 +20,7 @@ from __future__ import annotations
 import unittest
 
 from thrift.conformance.protocol.thrift_types import StandardProtocol
-from thrift.python.conformance.any_registry import AnyRegistry
+from thrift.lib.python.conformance.any_registry import AnyRegistry
 
 # @manual=//thrift/test/testset:testset-python-types
 from thrift.test.testset import thrift_types

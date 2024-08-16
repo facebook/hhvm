c2e2784426f85a6e59e46b995713f748d528b5d9
Mingtao Yang <mingtao@meta.com>
2024-08-16 12:18:31 -0700

    Wire up AEGIS support in oss build
    Summary: As titled
    
    Differential Revision: D61236352
    
    fbshipit-source-id: 332ccaf7e043d66b010be8ba244fc58caaabb231

--- third-party/fizz/src/fizz/cmake/FizzOptions.cmake
+++ third-party/fizz/src/fizz/cmake/FizzOptions.cmake
@@ -1,5 +1,4 @@
 #  Copyright (c) 2018, Facebook, Inc.
 #  All rights reserved.
 
-option(FIZZ_BUILD_AEGIS "Compiles Fizz with AEGIS support (experimental)" OFF)
 option(FIZZ_CERTIFICATE_USE_OPENSSL_CERT "Compiles Fizz Certificate with OpenSSL dependency" ON)

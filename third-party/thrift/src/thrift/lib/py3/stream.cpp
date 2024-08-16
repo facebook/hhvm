d0dbab7ee6fde40e6e67ea033c4f3a2958261c44
Victor Zverovich <viz@meta.com>
2024-08-16 14:32:36 -0700

    Unify py3 runtime library targets
    Summary:
    We already unified fbcode and xplat Thrift runtime library targets for C++ and Java, now do the same for py3. To this end:
    
    * Add `thrift_cython_library` that handles differences between xplat and fbcode.
    * Add proper xplat support to `thrift_python_library`.
    * Reduce the scope of the stream hack by moving it from the build config to the source code where we can reuse existing conditional compilation logic.
    
    This improves consistency between fbcode and xplat and removes a bunch of manually maintained and incomplete xplat targets.
    
    Reviewed By: yoney
    
    Differential Revision: D61388824
    
    fbshipit-source-id: 5f28f9bfc0a360b234b011dd9a56085662435240

--- third-party/thrift/src/thrift/lib/py3/stream.cpp
+++ third-party/thrift/src/thrift/lib/py3/stream.cpp
@@ -19,9 +19,9 @@
 #include <folly/CppAttributes.h>
 #include <folly/Portability.h>
 #include <thrift/lib/py3/stream.h>
-#include <thrift/lib/py3/stream_api.h> // @manual
 
-#if FOLLY_HAS_COROUTINES
+#if __has_include(<thrift/lib/py3/stream_api.h>) && FOLLY_HAS_COROUTINES
+#include <thrift/lib/py3/stream_api.h> // @manual
 
 namespace thrift {
 namespace py3 {

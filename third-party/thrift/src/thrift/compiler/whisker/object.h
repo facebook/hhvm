ca4378f03b7ae4639d089bc7456d4820e34da5cb
Pranjal Raihan <praihan@meta.com>
2024-08-21 19:53:30 -0700

    Implement whisker::object
    Summary:
    This diff adds `whisker::object` — *the* type representing any possible value in the Whisker templating language. The doc block for `whisker::object` and other comments within `object.h` explains much of the details.
    
    The `native_object` type is currently incomplete. The plan in future diffs is to add functions like `lookup_property` etc. when we start building the Whisker evaluation engine.
    
    The `native_object` is necessary not only for glueing with C++ code but also enables us to shim existing `mstch::object` hierarchy into the Whisker templating engine. This will let us slowly migrate our existing generator code in C++ without boiling the ocean.
    
    Reviewed By: vitaut
    
    Differential Revision: D61323564
    
    fbshipit-source-id: fcc87592c273ed63a70532e1bb57b6d54ee820ea

---
+++ third-party/thrift/src/thrift/compiler/whisker/object.h
@@ -0,0 +1,508 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <functional>
+#include <map>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <utility>
+#include <variant>
+#include <vector>
+
+#include <thrift/compiler/whisker/detail/overload.h>
+
+namespace whisker {
+
+// Whisker supports a small set of types.
+// They are all represented by the whisker::object sum type, which is one of:
+//   * i64     — 64-bit two’s complement signed integer
+//   * f64     — IEEE 754 binary64 floating point number
+//   * string  — UTF-8 encoded range of Unicode characters
+//   * boolean — 1-bit (true or false)
+//   * null    — The absence of a value
+//   * array   — An ordered list of `whisker::object`s (recursive)
+//   * map     — An unordered list of key-value pairs, where the key is a valid
+//               identifier, and the value is a `whisker::object` (recursive)
+//   * native_object —
+//               User-defined (C++) type with lazily evaluated properties.
+//
+// Currently, the Whisker templating language only supports i64, null, string,
+// and boolean as literals. However, the evaluation engine can recognize all of
+// the types above as they are required for compatibility with Mustache /
+// Handlebars.
+
+using i64 = std::int64_t;
+using f64 = double;
+using string = std::string;
+using boolean = bool;
+using null = std::monostate;
+
+class object;
+/**
+ * There are two main reasons why we are choosing std::map (rather than
+ * std::unordered_map):
+ *   1. Before C++20, std::unordered_map does not support heterogenous lookups.
+ *   2. *Technically* neither std::map or std::unordered_map are supposed to be
+ *      instantiate with an incomplete type. However, the standard library
+ *      implementations we use do support it. We know this because mstch has
+ *      been doing this for years in production at this point.
+ *      std::unordered_map actually breaks for incomplete types in practice.
+ */
+using map = std::map<std::string, object, std::less<>>;
+using array = std::vector<object>;
+
+/**
+ * A native_object is the most powerful type in Whisker. Its properties and
+ * behavior are fully defined by user C++ code.
+ *
+ * A native_object implementation may perform arbitrary computation in C++ to
+ * resolve a property name as long as the produced result can be marshaled back
+ * to a whisker::object type (including possibly another native_object!).
+ *
+ * Whisker also does not require that all property names exposed by a
+ * native_object are statically (or finitely) enumerable.
+ */
+class native_object {
+ public:
+  using ptr = std::shared_ptr<native_object>;
+  virtual ~native_object() = default;
+};
+
+namespace detail {
+// This only exists to form a kind-of recursive std::variant with the help of
+// forward declared types.
+template <typename Self>
+using object_base = std::variant<
+    null,
+    i64,
+    f64,
+    string,
+    boolean,
+    native_object::ptr,
+    std::vector<Self>,
+    std::map<std::string, Self, std::less<>>>;
+
+} // namespace detail
+
+/**
+ * A whisker::object represents any possible type of data that is recognized by
+ * the Whisker templating language and its evaluation engine.
+ *
+ * Ergonomically, a whisker::object looks and feels very much like a
+ * std::variant when interacting with an instance in C++. There are convenience
+ * accessors and methods that make it easier to interact with its possible
+ * alternatives.
+ *
+ * The contained data in a whisker::object is immutable through the instance.
+ * That is, once a value is emplaced within whisker::object, the only way to
+ * change the value is to assign a new value to the whisker::object instance
+ * (for example, std::move-out or operator=). Therefore, whisker::object is not
+ * suitable for general data manipulation — it should be the terminal store of
+ * value after computation.
+ *
+ * A default constructed whisker::object has the alternative whisker::null.
+ *
+ * One important difference from std::variant is the behavior on move-construct
+ * or move-assign — the moved from whisker::object is left in a valid state
+ * whose value is whisker::null.
+ */
+class object final : private detail::object_base<object> {
+ private:
+  using base = detail::object_base<object>;
+  base& as_variant() & { return *this; }
+  const base& as_variant() const& { return *this; }
+
+  template <typename T>
+  bool holds_alternative() const noexcept {
+    assert(!valueless_by_exception());
+    return std::holds_alternative<T>(*this);
+  }
+
+  template <typename T>
+  decltype(auto) as() const {
+    assert(!valueless_by_exception());
+    return std::get<T>(*this);
+  }
+
+  // This function moves out the base variant object which would normally leave
+  // the object in a partially moved-from state. However, moving a variant
+  // performs a move on the contained alternative, meaniong that assigning to
+  // the variant after a move is safe. We have this steal_variant() function
+  // hide this detail so we don't trip clang-tidy use-after-move linter.
+  base&& steal_variant() & { return std::move(*this); }
+
+ public:
+  /* implicit */ object(null = {}) : base(std::in_place_type<null>) {}
+  explicit object(boolean value) : base(bool(value)) {}
+  explicit object(i64 value) : base(value) {}
+  explicit object(f64 value) : base(value) {}
+  explicit object(string&& value) : base(std::move(value)) {}
+  explicit object(native_object::ptr&& value) : base(std::move(value)) {
+    assert(as_native_object() != nullptr);
+  }
+  explicit object(map&& value) : base(std::move(value)) {}
+  explicit object(array&& value) : base(std::move(value)) {}
+
+  object(const object&) = default;
+  object& operator=(const object&) = default;
+
+  object(object&& other) noexcept : base(other.steal_variant()) {
+    other = null();
+  }
+
+  object& operator=(object&& other) noexcept {
+    as_variant() = other.steal_variant();
+    other = null();
+    return *this;
+  }
+  // Pull in all the variant alternative operator= overloads
+  using base::operator=;
+
+  void swap(object& other) noexcept(noexcept(base::swap(other))) {
+    base::swap(other);
+  }
+
+  template <typename... Visitors>
+  decltype(auto) visit(Visitors&&... visitors) const {
+    return detail::variant_match(
+        as_variant(), std::forward<Visitors>(visitors)...);
+  }
+
+  const i64& as_i64() const { return as<i64>(); }
+  bool is_i64() const noexcept { return holds_alternative<i64>(); }
+
+  const f64& as_f64() const { return as<f64>(); }
+  bool is_f64() const noexcept { return holds_alternative<f64>(); }
+
+  const string& as_string() const { return as<string>(); }
+  bool is_string() const noexcept { return holds_alternative<string>(); }
+
+  const boolean& as_boolean() const { return as<boolean>(); }
+  bool is_boolean() const noexcept { return holds_alternative<boolean>(); }
+
+  bool is_null() const noexcept { return holds_alternative<null>(); }
+
+  const native_object::ptr& as_native_object() const {
+    return as<native_object::ptr>();
+  }
+  bool is_native_object() const noexcept {
+    return holds_alternative<native_object::ptr>();
+  }
+
+  const array& as_array() const { return as<array>(); }
+  bool is_array() const noexcept { return holds_alternative<array>(); }
+
+  const map& as_map() const { return as<map>(); }
+  bool is_map() const noexcept { return holds_alternative<map>(); }
+
+  friend bool operator==(const object& lhs, null) noexcept {
+    return lhs.is_null();
+  }
+  friend bool operator==(null, const object& rhs) noexcept {
+    return rhs.is_null();
+  }
+
+  // Prevent implicit conversion from const char* to bool
+  template <
+      typename T = boolean,
+      typename = std::enable_if_t<std::is_same_v<T, boolean>>>
+  friend bool operator==(const object& lhs, T rhs) noexcept {
+    return lhs.is_boolean() && lhs.as_boolean() == rhs;
+  }
+  template <
+      typename T = boolean,
+      typename = std::enable_if_t<std::is_same_v<T, boolean>>>
+  friend bool operator==(T lhs, const object& rhs) noexcept {
+    return rhs == lhs;
+  }
+
+  friend bool operator==(const object& lhs, i64 rhs) noexcept {
+    return lhs.is_i64() && lhs.as_i64() == rhs;
+  }
+  friend bool operator==(i64 lhs, const object& rhs) noexcept {
+    return rhs == lhs;
+  }
+
+  friend bool operator==(const object& lhs, f64 rhs) noexcept {
+    return lhs.is_f64() && lhs.as_f64() == rhs;
+  }
+  friend bool operator==(f64 lhs, const object& rhs) noexcept {
+    return rhs == lhs;
+  }
+
+  friend bool operator==(const object& lhs, std::string_view rhs) noexcept {
+    return lhs.is_string() && lhs.as_string() == rhs;
+  }
+  friend bool operator==(std::string_view lhs, const object& rhs) noexcept {
+    return rhs == lhs;
+  }
+
+  friend bool operator==(
+      const object& lhs, const native_object::ptr& rhs) noexcept {
+    return lhs.is_native_object() && lhs.as_native_object() == rhs;
+  }
+  friend bool operator==(
+      const native_object::ptr& lhs, const object& rhs) noexcept {
+    return rhs == lhs;
+  }
+  // whisker::object is not allowed to have a nullptr native_object
+  friend bool operator==(const object&, std::nullptr_t) = delete;
+  friend bool operator==(std::nullptr_t, const object&) = delete;
+
+  friend bool operator==(const object& lhs, const array& rhs) noexcept {
+    return lhs.is_array() && lhs.as_array() == rhs;
+  }
+  friend bool operator==(const array& lhs, const object& rhs) noexcept {
+    return rhs == lhs;
+  }
+
+  friend bool operator==(const object& lhs, const map& rhs) noexcept {
+    return lhs.is_map() && lhs.as_map() == rhs;
+  }
+  friend bool operator==(const map& lhs, const object& rhs) noexcept {
+    return rhs == lhs;
+  }
+
+  friend bool operator==(const object& lhs, const object& rhs) noexcept {
+    return lhs.visit([&rhs](auto&& value) { return rhs == value; });
+  }
+
+  // Before C++20, operator!= is not synthesized from operator==.
+
+  friend bool operator!=(const object& lhs, null) noexcept {
+    return !lhs.is_null();
+  }
+  friend bool operator!=(null, const object& rhs) noexcept {
+    return !rhs.is_null();
+  }
+
+  // Prevent implicit conversion from const char* to bool
+  template <
+      typename T = boolean,
+      typename = std::enable_if_t<std::is_same_v<T, boolean>>>
+  friend bool operator!=(const object& lhs, T rhs) noexcept {
+    return !(lhs == rhs);
+  }
+  template <
+      typename T = boolean,
+      typename = std::enable_if_t<std::is_same_v<T, boolean>>>
+  friend bool operator!=(T lhs, const object& rhs) noexcept {
+    return !(lhs == rhs);
+  }
+
+  friend bool operator!=(const object& lhs, i64 rhs) noexcept {
+    return !(lhs == rhs);
+  }
+  friend bool operator!=(i64 lhs, const object& rhs) noexcept {
+    return !(lhs == rhs);
+  }
+
+  friend bool operator!=(const object& lhs, f64 rhs) noexcept {
+    return !(lhs == rhs);
+  }
+  friend bool operator!=(f64 lhs, const object& rhs) noexcept {
+    return !(lhs == rhs);
+  }
+
+  friend bool operator!=(const object& lhs, std::string_view rhs) noexcept {
+    return !(lhs == rhs);
+  }
+  friend bool operator!=(std::string_view lhs, const object& rhs) noexcept {
+    return !(lhs == rhs);
+  }
+
+  friend bool operator!=(
+      const object& lhs, const native_object::ptr& rhs) noexcept {
+    return !(lhs == rhs);
+  }
+  friend bool operator!=(
+      const native_object::ptr& lhs, const object& rhs) noexcept {
+    return !(lhs == rhs);
+  }
+  // whisker::object is not allowed to have a nullptr native_object
+  friend bool operator!=(const object&, std::nullptr_t) = delete;
+  friend bool operator!=(std::nullptr_t, const object&) = delete;
+
+  friend bool operator!=(const object& lhs, const array& rhs) noexcept {
+    return !(lhs == rhs);
+  }
+  friend bool operator!=(const array& lhs, const object& rhs) noexcept {
+    return !(lhs == rhs);
+  }
+
+  friend bool operator!=(const object& lhs, const map& rhs) noexcept {
+    return !(lhs == rhs);
+  }
+  friend bool operator!=(const map& lhs, const object& rhs) noexcept {
+    return !(lhs == rhs);
+  }
+
+  friend bool operator!=(const object& lhs, const object& rhs) noexcept {
+    return !(lhs == rhs);
+  }
+};
+
+// The make::* functions are syntactic sugar for constructing whisker::object
+namespace make {
+
+namespace detail {
+
+/**
+ * The set of integral types that are supported by whisker::object (and are
+ * therefore also implicitly convertible from).
+ */
+template <typename T>
+static constexpr bool is_supported_integral_type =
+    std::is_integral_v<T> && std::is_signed_v<T> && sizeof(T) <= sizeof(i64);
+
+static_assert(is_supported_integral_type<i64>);
+static_assert(is_supported_integral_type<char>);
+static_assert(is_supported_integral_type<short>);
+static_assert(is_supported_integral_type<int>);
+static_assert(is_supported_integral_type<long>);
+
+/**
+ * The set of floating point types that are supported by whisker::object (and
+ * are therefore also implicitly convertible from).
+ */
+template <typename T>
+static constexpr bool is_supported_floating_point_type =
+    std::is_same_v<T, float> || std::is_same_v<T, double>;
+
+} // namespace detail
+
+/**
+ * An empty whisker::object. null.is_null() == true.
+ */
+inline const object null;
+
+/**
+ * Creates whisker::object with boolean type.
+ *
+ * Postconditions:
+ *   object::is_boolean() == true
+ *   object::as_boolean() == value
+ */
+inline object boolean(boolean value) {
+  return object(value);
+}
+
+/**
+ * Creates whisker::object with integral type.
+ *
+ * Postconditions:
+ *   object::is_i64() == true
+ *   object::as_i64() == value
+ */
+template <
+    typename T = i64,
+    typename = std::enable_if_t<detail::is_supported_integral_type<T>>>
+object i64(T value) {
+  // Smaller signed integrals types are implicitly converted to i64.
+  return object(whisker::i64(value));
+}
+
+/**
+ * Creates whisker::object with floating point type.
+ *
+ * Postconditions:
+ *   object::is_f64() == true
+ *   object::as_f64() == value
+ */
+template <
+    typename T = f64,
+    typename = std::enable_if_t<detail::is_supported_floating_point_type<T>>>
+object f64(T value) {
+  return object(whisker::f64(value));
+}
+
+/**
+ * Creates whisker::object with string type. This function takes ownership of
+ * the provided string.
+ *
+ * Postconditions:
+ *   object::is_string() == true
+ *   object::as_string() == value
+ */
+inline object string(whisker::string&& value) {
+  return object(std::move(value));
+}
+/**
+ * Creates whisker::object with string type from a (possibly not NUL-terminated)
+ * string.
+ *
+ * Postconditions:
+ *   object::is_string() == true
+ *   object::as_string() == value
+ */
+inline object string(std::string_view value) {
+  return object(whisker::string(value));
+}
+/**
+ * Creates whisker::object with string type from a NUL-terminated C string.
+ *
+ * Postconditions:
+ *   object::is_string() == true
+ *   object::as_string() == value
+ */
+inline object string(const char* value) {
+  return object(whisker::string(value));
+}
+
+/**
+ * Creates whisker::object with map type. This function takes ownership of the
+ * provided map.
+ *
+ * Postconditions:
+ *   object::is_map() == true
+ *   object::as_map() == value
+ */
+inline object map(map&& value) {
+  return object(std::move(value));
+}
+
+/**
+ * Creates whisker::object with array type. This function takes ownership of the
+ * provided array.
+ *
+ * Postconditions:
+ *   object::is_array() == true
+ *   object::as_array() == value
+ */
+inline object array(array&& value) {
+  return object(std::move(value));
+}
+
+/**
+ * Creates whisker::object with a backing native_object.
+ *
+ * Postconditions:
+ *   object::is_native_object() == true
+ *   object::as_native_object() == value
+ */
+inline object native_object(native_object::ptr value) {
+  return object(std::move(value));
+}
+
+} // namespace make
+
+} // namespace whisker
---
+++ third-party/thrift/src/thrift/compiler/whisker/test/object_test.cc
@@ -0,0 +1,288 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <folly/portability/GMock.h>
+#include <folly/portability/GTest.h>
+
+#include <thrift/compiler/whisker/object.h>
+
+namespace w = whisker::make;
+
+namespace whisker {
+
+TEST(ObjectTest, empty) {
+  object o;
+  EXPECT_TRUE(o.is_null());
+  EXPECT_EQ(o, w::null);
+  EXPECT_EQ(w::null, o);
+  EXPECT_EQ(o, object());
+  EXPECT_EQ(object(), o);
+  EXPECT_EQ(o, o);
+}
+
+TEST(ObjectTest, i64) {
+  std::vector<object> objects = {
+      w::i64(1),
+      w::i64(0L),
+      w::i64(short(-1)),
+      w::i64(std::int32_t(4)),
+      w::i64(char(5)),
+  };
+  for (auto& o : objects) {
+    EXPECT_TRUE(o.is_i64());
+    EXPECT_FALSE(o.is_f64());
+    EXPECT_EQ(o, o);
+  }
+  EXPECT_EQ(objects.front(), w::i64(1));
+  EXPECT_EQ(i64(1), objects.front());
+  EXPECT_NE(objects.front(), w::i64(2));
+  EXPECT_NE(objects.front(), w::f64(2.));
+}
+
+TEST(ObjectTest, f64) {
+  object o = w::f64(2.);
+  EXPECT_TRUE(o.is_f64());
+  EXPECT_FALSE(o.is_string());
+  EXPECT_EQ(o, 2.);
+  EXPECT_EQ(2., o);
+  EXPECT_NE(o, 3.);
+  EXPECT_NE(o, std::string("bar"));
+  EXPECT_EQ(o, o);
+}
+
+TEST(ObjectTest, string) {
+  object o = w::string("foo");
+  EXPECT_TRUE(o.is_string());
+  EXPECT_FALSE(o.is_boolean());
+  EXPECT_EQ(o, std::string("foo"));
+  EXPECT_EQ(std::string("foo"), o);
+  EXPECT_NE(o, std::string("bar"));
+  EXPECT_NE(o, true);
+  EXPECT_EQ(o, o);
+}
+
+TEST(ObjectTest, boolean) {
+  object o = w::boolean(true);
+  EXPECT_TRUE(o.is_boolean());
+  EXPECT_FALSE(o.is_null());
+  EXPECT_EQ(o, true);
+  EXPECT_EQ(true, o);
+  EXPECT_NE(o, false);
+  EXPECT_EQ(o, o);
+}
+
+TEST(ObjectTest, null) {
+  object o = null();
+  EXPECT_TRUE(o.is_null());
+  EXPECT_FALSE(o.is_array());
+  EXPECT_EQ(o, null());
+  EXPECT_EQ(null(), o);
+  EXPECT_NE(o, array());
+  EXPECT_EQ(o, o);
+}
+
+TEST(ObjectTest, array) {
+  object o =
+      w::array({w::i64(1), w::array({w::string("foo")}), w::boolean(true)});
+  EXPECT_TRUE(o.is_array());
+  EXPECT_FALSE(o.is_map());
+  EXPECT_EQ(
+      o, w::array({w::i64(1), w::array({w::string("foo")}), w::boolean(true)}));
+  EXPECT_EQ(
+      w::array({w::i64(1), w::array({w::string("foo")}), w::boolean(true)}), o);
+  EXPECT_NE(o, w::array({w::string("foo"), w::i64(1), w::boolean(true)}));
+  EXPECT_NE(o, w::array({w::i64(0), w::string("foo"), w::boolean(true)}));
+  EXPECT_NE(o, map());
+  EXPECT_EQ(o, o);
+}
+
+TEST(ObjectTest, map) {
+  object o = w::map(
+      {{"foo", w::i64(1)},
+       {"bar", w::array({w::string("foo"), w::boolean(true)})}});
+  EXPECT_TRUE(o.is_map());
+  EXPECT_FALSE(o.is_native_object());
+  EXPECT_EQ(
+      o,
+      w::map(
+          {{"foo", w::i64(1)},
+           {"bar", w::array({w::string("foo"), w::boolean(true)})}}));
+  EXPECT_NE(
+      o,
+      w::map(
+          {{"foo-bar", w::i64(1)},
+           {"bar", w::array({w::string("foo"), w::boolean(true)})}}));
+  EXPECT_NE(o, native_object::ptr(nullptr));
+  EXPECT_EQ(o, o);
+}
+
+TEST(ObjectTest, native_object) {
+  native_object::ptr ptr = std::make_shared<native_object>();
+
+  object o = w::native_object(ptr);
+  EXPECT_TRUE(o.is_native_object());
+  EXPECT_FALSE(o.is_i64());
+  EXPECT_EQ(o, ptr);
+  EXPECT_EQ(ptr, o);
+  EXPECT_NE(o, native_object::ptr(nullptr));
+  EXPECT_NE(native_object::ptr(nullptr), o);
+  EXPECT_NE(o, i64(1));
+  EXPECT_EQ(o, o);
+}
+
+TEST(ObjectTest, copy) {
+  native_object::ptr ptr = std::make_shared<native_object>();
+
+  object o1 = w::array(
+      {w::i64(1),
+       w::array({w::string("foo"), w::f64(4.)}),
+       w::map({{"bar", w::null}, {"baz", w::native_object(ptr)}})});
+  object o2 = o1;
+
+  EXPECT_EQ(
+      o2,
+      w::array(
+          {w::i64(1),
+           w::array({w::string("foo"), w::f64(4.)}),
+           w::map({{"bar", w::null}, {"baz", w::native_object(ptr)}})}));
+  EXPECT_EQ(o1, o2);
+
+  o1 = "foo";
+  EXPECT_EQ(o1, "foo");
+  EXPECT_NE(o1, o2);
+
+  o1 = o2;
+  EXPECT_EQ(o2, o1);
+  EXPECT_EQ(
+      o2,
+      w::array(
+          {w::i64(1),
+           w::array({w::string("foo"), w::f64(4.)}),
+           w::map({{"bar", w::null}, {"baz", w::native_object(ptr)}})}));
+}
+
+TEST(ObjectTest, move) {
+  native_object::ptr ptr = std::make_shared<native_object>();
+
+  object o1 = w::array(
+      {w::i64(1),
+       w::array({w::string("foo")}),
+       w::map({{"bar", w::boolean(true)}, {"baz", w::native_object(ptr)}})});
+  object o2 = std::move(o1);
+
+  EXPECT_EQ(
+      o2,
+      w::array(
+          {w::i64(1),
+           w::array({w::string("foo")}),
+           w::map(
+               {{"bar", w::boolean(true)}, {"baz", w::native_object(ptr)}})}));
+  // @lint-ignore CLANGTIDY bugprone-use-after-move
+  EXPECT_EQ(o1, null());
+
+  // @lint-ignore CLANGTIDY bugprone-use-after-move
+  o1 = std::move(o2);
+  // @lint-ignore CLANGTIDY bugprone-use-after-move
+  EXPECT_EQ(
+      o1,
+      w::array(
+          {w::i64(1),
+           w::array({w::string("foo")}),
+           w::map(
+               {{"bar", w::boolean(true)}, {"baz", w::native_object(ptr)}})}));
+  // @lint-ignore CLANGTIDY bugprone-use-after-move
+  EXPECT_EQ(o2, null());
+}
+
+TEST(ObjectTest, swap) {
+  native_object::ptr ptr1 = std::make_shared<native_object>();
+  native_object::ptr ptr2 = std::make_shared<native_object>();
+
+  object o1 = w::array(
+      {w::i64(1),
+       w::array({w::string("foo")}),
+       w::map({{"bar", w::boolean(true)}, {"baz", w::native_object(ptr1)}})});
+  object o2 = w::array(
+      {w::array({w::boolean(true), w::string("foo"), w::f64(4.)}),
+       w::map({{"bar", w::string("xyz")}, {"baz", w::native_object(ptr1)}})});
+  EXPECT_NE(o1, o2);
+
+  o1.swap(o2);
+  EXPECT_EQ(
+      o1,
+      w::array(
+          {w::array({w::boolean(true), w::string("foo"), w::f64(4.)}),
+           w::map(
+               {{"bar", w::string("xyz")}, {"baz", w::native_object(ptr1)}})}));
+  EXPECT_EQ(
+      o2,
+      w::array(
+          {w::i64(1),
+           w::array({w::string("foo")}),
+           w::map(
+               {{"bar", w::boolean(true)}, {"baz", w::native_object(ptr1)}})}));
+}
+
+TEST(ObjectTest, assign_copy_alternatives) {
+  object o;
+  EXPECT_EQ(o, null());
+  {
+    i64 i = 1;
+    o = i;
+    EXPECT_EQ(i64(1), o);
+  }
+  {
+    f64 f = 2.;
+    o = f;
+    EXPECT_EQ(f64(2.), o);
+  }
+  {
+    string str = "foobar";
+    o = str;
+    EXPECT_EQ("foobar", o);
+    EXPECT_EQ("foobar", str);
+  }
+  {
+    boolean b = true;
+    o = b;
+    EXPECT_EQ(true, o);
+  }
+  {
+    null n = null{};
+    o = n;
+    EXPECT_EQ(null(), o);
+  }
+  {
+    array a = {w::i64(1), w::string("2"), w::i64(3)};
+    o = a;
+    EXPECT_EQ(w::array({w::i64(1), w::string("2"), w::i64(3)}), o);
+    EXPECT_EQ(a, w::array({w::i64(1), w::string("2"), w::i64(3)}));
+  }
+  {
+    map m = {{"foo", w::i64(3)}, {"bar", w::string("baz")}};
+    o = m;
+    EXPECT_EQ((w::map({{"foo", w::i64(3)}, {"bar", w::string("baz")}})), o);
+    EXPECT_EQ(m, (w::map({{"foo", w::i64(3)}, {"bar", w::string("baz")}})));
+  }
+  {
+    native_object::ptr ptr = std::make_shared<native_object>();
+    o = ptr;
+    EXPECT_EQ(ptr, o);
+    EXPECT_NE(ptr, nullptr);
+  }
+}
+
+} // namespace whisker

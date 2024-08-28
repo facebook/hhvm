f9dbbc6284666e0641cf2a1fce170581b24d4404
Nick Richardson <richardsonnick@meta.com>
2024-08-28 14:48:08 -0700

    Add explicit values to KeyLogWriter::Label
    Summary: Adds explicit values to the KeyLogWriter::Label enum. To improve ABI consistency.
    
    Reviewed By: knekritz
    
    Differential Revision: D61864674
    
    fbshipit-source-id: b3c43fc5e9d1267e1615c766924bab6868dcbf34

--- third-party/fizz/src/fizz/util/KeyLogWriter.h
+++ third-party/fizz/src/fizz/util/KeyLogWriter.h
@@ -20,29 +20,35 @@ namespace fizz {
  * More details can be found here:
  * https://developer.mozilla.org/en-US/docs/Mozilla/Projects/NSS/Key_Log_Format.
  * The syntax: <Label> <space> <ClientRandom> <space> <Secret>.
+ *
+ * The values here map with values in a tracepoint script (fizztrace.py).
+ * They have no correlation with the NSS standard.
  */
 class KeyLogWriter {
  public:
-  enum class Label {
-    RSA, // 48 bytes for the premaster secret, encoded as 96 hexadecimal
-         // characters
-    CLIENT_RANDOM, // 48 bytes for the master secret, encoded as 96 hexadecimal
-                   // characters (for SSL 3.0, TLS 1.0, 1.1 and 1.2)
-    CLIENT_EARLY_TRAFFIC_SECRET, // the hex-encoded early traffic secret for the
-                                 // client side (for TLS 1.3)
-    CLIENT_HANDSHAKE_TRAFFIC_SECRET, // the hex-encoded handshake traffic secret
-                                     // for the client side (for TLS 1.3)
-    SERVER_HANDSHAKE_TRAFFIC_SECRET, // the hex-encoded handshake traffic secret
-                                     // for the server side (for TLS 1.3)
-    CLIENT_TRAFFIC_SECRET_0, // the first hex-encoded application traffic secret
-                             // for the client side (for TLS 1.3)
-    SERVER_TRAFFIC_SECRET_0, // the first hex-encoded application traffic secret
-                             // for the server side (for TLS 1.3)
-    EARLY_EXPORTER_SECRET, // the hex-encoded early exporter secret (for
-                           // TLS 1.3, used for 0-RTT keys in older QUIC
-                           // drafts).
-    EXPORTER_SECRET // the hex-encoded exporter secret (for TLS 1.3, used for
-                    // 1-RTT keys in older QUIC drafts)
+  enum class Label : uint64_t {
+    RSA = 0, // 48 bytes for the premaster secret, encoded as 96 hexadecimal
+             // characters
+    CLIENT_RANDOM =
+        1, // 48 bytes for the master secret, encoded as 96 hexadecimal
+           // characters (for SSL 3.0, TLS 1.0, 1.1 and 1.2)
+    CLIENT_EARLY_TRAFFIC_SECRET = 2, // the hex-encoded early traffic secret for
+                                     // the client side (for TLS 1.3)
+    CLIENT_HANDSHAKE_TRAFFIC_SECRET =
+        3, // the hex-encoded handshake traffic secret
+           // for the client side (for TLS 1.3)
+    SERVER_HANDSHAKE_TRAFFIC_SECRET =
+        4, // the hex-encoded handshake traffic secret
+           // for the server side (for TLS 1.3)
+    CLIENT_TRAFFIC_SECRET_0 = 5, // the first hex-encoded application traffic
+                                 // secret for the client side (for TLS 1.3)
+    SERVER_TRAFFIC_SECRET_0 = 6, // the first hex-encoded application traffic
+                                 // secret for the server side (for TLS 1.3)
+    EARLY_EXPORTER_SECRET = 7, // the hex-encoded early exporter secret (for
+                               // TLS 1.3, used for 0-RTT keys in older QUIC
+                               // drafts).
+    EXPORTER_SECRET = 8 // the hex-encoded exporter secret (for TLS 1.3, used
+                        // for 1-RTT keys in older QUIC drafts)
   };
 
   /**

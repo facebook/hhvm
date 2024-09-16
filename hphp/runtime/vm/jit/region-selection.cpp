87a1a16d10cc9a66d6834b472d206aa6b2686f35
Richard Barnes <rbarnes@meta.com>
2024-09-16 10:43:55 -0700

    Fix `-Wunused-but-set-variable` in hphp/
    Summary:
    #buildsonlynotests - No runtime effects!
    
     - If you approve of this diff, please use the "Accept & Ship" button
    
    ig-no-test - Doesn't require Instagram testing.
    
    allow-large-files
    
    Reviewed By: dmm-fb
    
    Differential Revision: D62756783
    
    fbshipit-source-id: 2e341b3690d9f8465ff88dc760cd56db215392c1

--- hphp/runtime/vm/jit/region-selection.cpp
+++ hphp/runtime/vm/jit/region-selection.cpp
@@ -1132,10 +1132,8 @@ bool check(const RegionDesc& region, std::string& error) {
   auto& blocks = region.blocks();
   for (unsigned i = 0; i < blocks.size(); i++) {
     auto bid = blocks[i]->id();
-    DEBUG_ONLY unsigned nAllPreds = region.preds(bid).size();
     unsigned nVisited = 0;
     if (auto prevRetrans = region.prevRetrans(bid)) {
-      nAllPreds++;
       nVisited += visited.count(prevRetrans.value());
     }
     for (auto pred : region.preds(bid)) {

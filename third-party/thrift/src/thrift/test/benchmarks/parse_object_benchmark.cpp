c6add2ed5fd887a687b920f124284dbd89bfd3fb
Dave Kwon <dokwon@meta.com>
2024-09-25 15:01:30 -0700

    ParseObject benchmarks
    Summary:
    Add a benchmark to demonstrate partial parse object performance.
    
    ```
    ============================================================================
    [...]benchmarks/parse_object_benchmark.cpp     relative  time/iter   iters/s
    ============================================================================
    OpDecodeComplexStruct_Compact                             147.66ms      6.77
    ParseObjectComplexStruct_Compact                20.895%   706.69ms      1.42
    OpDecodeMixed_Compact                                      35.34ns    28.29M
    ParseObjectMixed_Compact                        18.098%   195.29ns     5.12M
    ParseObjectMixedWithMaskFromComplexStruct_Compa6.6622e-05%    53.05ms     18.85
    OpDecodeComplexStruct_Binary                              115.66ms      8.65
    ParseObjectComplexStruct_Binary                 16.908%   684.07ms      1.46
    OpDecodeMixed_Binary                                       30.91ns    32.36M
    ParseObjectMixed_Binary                         17.550%   176.10ns     5.68M
    ParseObjectMixedWithMaskFromComplexStruct_Binar0.00010967%    28.18ms     35.49
    ```
    
    Few interesting notes:
    1. `ParseObjectMixedWithMaskFromComplexStruct` is about twice faster in binary protocol as skipping is much more efficient.
    2. ParseObjectMixedWithMaskFromComplexStruct takes significant times in skipping ~99% time as we want to extract tiny portion of the data from the mask. [strobelight](https://www.internalfb.com/intern/strobelight/ondemand/?dataset=strobelight_services%2Fon_demand&drillstate=%7B%22dimensions%22%3A%5B%22process%22%5D%2C%22param_dimensions%22%3A%5B%7B%22anchor%22%3A%220%22%2C%22param%22%3A%220%22%2C%22op%22%3A%22all%22%2C%22dim%22%3A%22stack_combined%22%7D%5D%2C%22constraints%22%3A%5B%5B%7B%22value%22%3A%5B%22%5B%5C%227083985589358813%5C%22%5D%22%5D%2C%22op%22%3A%22eq%22%2C%22column%22%3A%22run_id%22%7D%2C%7B%22value%22%3A%5B%22%5B%5C%22dokwon%5C%22%5D%22%5D%2C%22op%22%3A%22eq%22%2C%22column%22%3A%22run_user%22%7D%2C%7B%22value%22%3A%5B%22%5B%5C%22strobelight_cli%5C%22%5D%22%5D%2C%22op%22%3A%22all%22%2C%22column%22%3A%22sample_tags%22%7D%5D%5D%2C%22top%22%3A100000%2C%22end%22%3A%221727138641%22%2C%22start%22%3A%221727131441%22%7D&view=Icicle3&=&icicle_state=%7B%22deltaMode%22%3A%22none%22%2C%22colorMode%22%3A%22total%22%2C%22valueMode%22%3A%22selection%22%2C%22formatMode%22%3A%22percentage%22%2C%22codeHubState%22%3A%7B%22aggregationField%22%3A%22%22%2C%22scubaParams%22%3A%22%22%2C%22symbolPath%22%3Anull%7D%2C%22showCodeProfileModal%22%3Afalse%2C%22colorPatterns%22%3A%5B%5D%7D)
    
    Reviewed By: pranavtbhat
    
    Differential Revision: D63296993
    
    fbshipit-source-id: 492593734fa24db1e034dec633667fd561250541

---
+++ third-party/thrift/src/thrift/test/benchmarks/parse_object_benchmark.cpp
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <folly/Benchmark.h>
+#include <folly/init/Init.h>
+#include <thrift/lib/cpp2/protocol/Object.h>
+#include <thrift/lib/cpp2/protocol/Serializer.h>
+#include <thrift/lib/cpp2/test/Structs.h>
+
+namespace apache::thrift::protocol {
+
+using ::thrift::benchmark::ComplexStruct;
+using ::thrift::benchmark::Mixed;
+
+template <typename Serializer, typename T>
+folly::IOBuf getSerializedCoalscedBuf(const T& t) {
+  folly::IOBufQueue queue;
+  Serializer::serialize(t, &queue);
+  auto buf = queue.moveAsValue();
+  buf.coalesce();
+  return buf;
+}
+
+const auto complexStruct = create<ComplexStruct>();
+const auto mixed = create<Mixed>();
+const auto complexStructCompactBuf =
+    getSerializedCoalscedBuf<CompactSerializer>(complexStruct);
+const auto complexStructBinaryBuf =
+    getSerializedCoalscedBuf<BinarySerializer>(complexStruct);
+const auto mixedCompactBuf = getSerializedCoalscedBuf<CompactSerializer>(mixed);
+const auto mixedBinaryBuf = getSerializedCoalscedBuf<BinarySerializer>(mixed);
+const auto mixedBufMask = []() {
+  Mask m;
+  m.includes_ref().emplace()[16] = allMask();
+  return m;
+}();
+
+BENCHMARK(OpDecodeComplexStruct_Compact) {
+  ComplexStruct obj;
+  CompactProtocolReader reader;
+  reader.setInput(&complexStructCompactBuf);
+  op::decode<type::struct_t<ComplexStruct>>(reader, obj);
+  folly::doNotOptimizeAway(obj);
+}
+
+BENCHMARK_RELATIVE(ParseObjectComplexStruct_Compact) {
+  auto obj =
+      protocol::parseObject<CompactProtocolReader>(complexStructCompactBuf);
+  folly::doNotOptimizeAway(obj);
+}
+
+BENCHMARK(OpDecodeMixed_Compact) {
+  Mixed obj;
+  CompactProtocolReader reader;
+  reader.setInput(&mixedCompactBuf);
+  op::decode<type::struct_t<Mixed>>(reader, obj);
+  folly::doNotOptimizeAway(obj);
+}
+
+BENCHMARK_RELATIVE(ParseObjectMixed_Compact) {
+  auto obj = protocol::parseObject<CompactProtocolReader>(mixedCompactBuf);
+  folly::doNotOptimizeAway(obj);
+}
+
+BENCHMARK_RELATIVE(ParseObjectMixedWithMaskFromComplexStruct_Compact) {
+  auto obj = protocol::parseObject<CompactProtocolReader>(
+      complexStructCompactBuf, mixedBufMask);
+  folly::doNotOptimizeAway(obj);
+}
+
+BENCHMARK(OpDecodeComplexStruct_Binary) {
+  ComplexStruct obj;
+  BinaryProtocolReader reader;
+  reader.setInput(&complexStructBinaryBuf);
+  op::decode<type::struct_t<ComplexStruct>>(reader, obj);
+  folly::doNotOptimizeAway(obj);
+}
+
+BENCHMARK_RELATIVE(ParseObjectComplexStruct_Binary) {
+  auto obj =
+      protocol::parseObject<BinaryProtocolReader>(complexStructBinaryBuf);
+  folly::doNotOptimizeAway(obj);
+}
+
+BENCHMARK(OpDecodeMixed_Binary) {
+  Mixed obj;
+  BinaryProtocolReader reader;
+  reader.setInput(&mixedBinaryBuf);
+  op::decode<type::struct_t<Mixed>>(reader, obj);
+  folly::doNotOptimizeAway(obj);
+}
+
+BENCHMARK_RELATIVE(ParseObjectMixed_Binary) {
+  auto obj = protocol::parseObject<BinaryProtocolReader>(mixedBinaryBuf);
+  folly::doNotOptimizeAway(obj);
+}
+
+BENCHMARK_RELATIVE(ParseObjectMixedWithMaskFromComplexStruct_Binary) {
+  auto obj = protocol::parseObject<BinaryProtocolReader>(
+      complexStructBinaryBuf, mixedBufMask);
+  folly::doNotOptimizeAway(obj);
+}
+
+} // namespace apache::thrift::protocol
+
+int main(int argc, char** argv) {
+  folly::Init init(&argc, &argv);
+  folly::runBenchmarks();
+}

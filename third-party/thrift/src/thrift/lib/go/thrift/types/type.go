7054b8deaa8d2b763f5915e509df0a2107e0de81
Jack Chistyakov <jchistyakov@meta.com>
2024-09-27 17:12:29 -0700

    Remove non-existent types
    Summary:
    `I08` and `UTF7` are not valid Thrift types. Not used and not needed.
    
    https://internalfb.com/intern/staticdocs/thrift/docs/idl/#primitive-types
    
    Reviewed By: podtserkovskiy
    
    Differential Revision: D63548836
    
    fbshipit-source-id: 3a3ff9ef1591c9b4dfe6afd4621710770e8c2323

--- third-party/thrift/src/thrift/lib/go/thrift/types/type.go
+++ third-party/thrift/src/thrift/lib/go/thrift/types/type.go
@@ -24,13 +24,11 @@ const (
 	VOID   = 1
 	BOOL   = 2
 	BYTE   = 3
-	I08    = 3
 	DOUBLE = 4
 	I16    = 6
 	I32    = 8
 	I64    = 10
 	STRING = 11
-	UTF7   = 11
 	STRUCT = 12
 	MAP    = 13
 	SET    = 14

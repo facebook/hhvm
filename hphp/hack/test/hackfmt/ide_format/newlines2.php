aefc0eb71d5ec2889dc853ee55ac9885320720f5
Max Heiber <mheiber@meta.com>
2024-10-09 10:31:52 -0700

    regression test for IDE formatting
    Summary:
    Based on a user report, this is a case it's important for formatting to get right.
    
    This test:
    - exercises the code path used by Hack IDE when formatting a file
    - shows the output
    - verifies that the output is the same as what we would get from running hackfmt directly
    
    > more context on this kind of test: D61022050
    
    Reviewed By: madgen
    
    Differential Revision: D64111922
    
    fbshipit-source-id: 0a521ed1a74b85a4f4178f76a3435377d79ba8ad

---
+++ hphp/hack/test/hackfmt/ide_format/newlines2.php
@@ -0,0 +1,9 @@
+<?hh
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+final class ExampleFormatBreak {
+  public static function formatBreak(): void {
+  }
+
+  public static function formatBreak2(): void {
+  }
+}

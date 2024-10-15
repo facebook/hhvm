895eb1b686524d433460a4126a028a8a3448a697
generatedunixname89002005232357 <generatedunixname89002005232357@meta.com>
2024-10-14 20:56:50 -0700

    Revert D64220843 to fix build failures
    Summary:
    This diff reverts D64220843
    t204658457
    
    Reviewed By: Mizuchi
    
    Differential Revision: D64366467
    
    fbshipit-source-id: 5d6531f0a98f8007a5ff256fddbc0eda1f316c74

--- third-party/thrift/src/thrift/compiler/test/fixtures/mcpp2-compare/out/cpp2_enums/gen-cpp2/enums_fatal_enum.h
+++ third-party/thrift/src/thrift/compiler/test/fixtures/mcpp2-compare/out/cpp2_enums/gen-cpp2/enums_fatal_enum.h
@@ -13,6 +13,182 @@
 
 #include <type_traits>
 
+namespace facebook::ns::qwerty {
+
+namespace __fbthrift_refl {
+namespace __fbthrift_refl_impl = ::apache::thrift::detail::reflection_impl;
+
+class AnEnumA_enum_traits {
+ public:
+  using type = ::facebook::ns::qwerty::AnEnumA;
+
+ private:
+  struct __fbthrift_value_FIELDA {
+    using name = __fbthrift_strings_enums::__fbthrift_hash_add540b98778029e7e62e524687274b838fd038b831b1885b93d5ab01ef056e6;
+    using value = std::integral_constant<type, type::FIELDA>;
+    using annotations = __fbthrift_refl_impl::no_annotations;
+  };
+
+  struct __fbthrift_member {
+    using FIELDA = __fbthrift_value_FIELDA;
+  };
+
+ public:
+  using name = __fbthrift_strings_enums::AnEnumA;
+  using member = __fbthrift_member;
+  using fields = ::fatal::list<
+      member::FIELDA
+  >;
+
+  using annotations = __fbthrift_refl_impl::no_annotations;
+
+  static char const *to_string(type e, char const *fallback) {
+    switch (e) {
+      case type::FIELDA: return "FIELDA";
+      default: return fallback;
+    }
+  }
+};
+
+class AnEnumB_enum_traits {
+ public:
+  using type = ::facebook::ns::qwerty::AnEnumB;
+
+ private:
+  struct __fbthrift_value_FIELDA {
+    using name = __fbthrift_strings_enums::__fbthrift_hash_add540b98778029e7e62e524687274b838fd038b831b1885b93d5ab01ef056e6;
+    using value = std::integral_constant<type, type::FIELDA>;
+    using annotations = __fbthrift_refl_impl::no_annotations;
+  };
+
+  struct __fbthrift_value_FIELDB {
+    using name = __fbthrift_strings_enums::__fbthrift_hash_148e782df34993fa930a9b389d5a4e2e92f13610cf6adbeab2abf6e095f5ef89;
+    using value = std::integral_constant<type, type::FIELDB>;
+    using annotations = __fbthrift_refl_impl::no_annotations;
+  };
+
+  struct __fbthrift_member {
+    using FIELDA = __fbthrift_value_FIELDA;
+    using FIELDB = __fbthrift_value_FIELDB;
+  };
+
+ public:
+  using name = __fbthrift_strings_enums::AnEnumB;
+  using member = __fbthrift_member;
+  using fields = ::fatal::list<
+      member::FIELDA,
+      member::FIELDB
+  >;
+
+  using annotations = __fbthrift_refl_impl::no_annotations;
+
+  static char const *to_string(type e, char const *fallback) {
+    switch (e) {
+      case type::FIELDA: return "FIELDA";
+      case type::FIELDB: return "FIELDB";
+      default: return fallback;
+    }
+  }
+};
+
+class AnEnumC_enum_traits {
+ public:
+  using type = ::facebook::ns::qwerty::AnEnumC;
+
+ private:
+  struct __fbthrift_value_FIELDC {
+    using name = __fbthrift_strings_enums::__fbthrift_hash_c907b51dbf720f8a71d2fe895168681ecaffaeebee9402b24e9da11816eda10e;
+    using value = std::integral_constant<type, type::FIELDC>;
+    using annotations = __fbthrift_refl_impl::no_annotations;
+  };
+
+  struct __fbthrift_member {
+    using FIELDC = __fbthrift_value_FIELDC;
+  };
+
+ public:
+  using name = __fbthrift_strings_enums::AnEnumC;
+  using member = __fbthrift_member;
+  using fields = ::fatal::list<
+      member::FIELDC
+  >;
+
+  using annotations = __fbthrift_refl_impl::no_annotations;
+
+  static char const *to_string(type e, char const *fallback) {
+    switch (e) {
+      case type::FIELDC: return "FIELDC";
+      default: return fallback;
+    }
+  }
+};
+
+class AnEnumD_enum_traits {
+ public:
+  using type = ::facebook::ns::qwerty::AnEnumD;
+
+ private:
+  struct __fbthrift_value_FIELDD {
+    using name = __fbthrift_strings_enums::__fbthrift_hash_40d9038a74e2f4da3d23c9054a5c003163af248d9a0c6133ac130acfc5d4122d;
+    using value = std::integral_constant<type, type::FIELDD>;
+    using annotations = __fbthrift_refl_impl::no_annotations;
+  };
+
+  struct __fbthrift_member {
+    using FIELDD = __fbthrift_value_FIELDD;
+  };
+
+ public:
+  using name = __fbthrift_strings_enums::AnEnumD;
+  using member = __fbthrift_member;
+  using fields = ::fatal::list<
+      member::FIELDD
+  >;
+
+  using annotations = __fbthrift_refl_impl::no_annotations;
+
+  static char const *to_string(type e, char const *fallback) {
+    switch (e) {
+      case type::FIELDD: return "FIELDD";
+      default: return fallback;
+    }
+  }
+};
+
+class AnEnumE_enum_traits {
+ public:
+  using type = ::facebook::ns::qwerty::AnEnumE;
+
+ private:
+  struct __fbthrift_value_FIELDA {
+    using name = __fbthrift_strings_enums::__fbthrift_hash_add540b98778029e7e62e524687274b838fd038b831b1885b93d5ab01ef056e6;
+    using value = std::integral_constant<type, type::FIELDA>;
+    using annotations = __fbthrift_refl_impl::no_annotations;
+  };
+
+  struct __fbthrift_member {
+    using FIELDA = __fbthrift_value_FIELDA;
+  };
+
+ public:
+  using name = __fbthrift_strings_enums::AnEnumE;
+  using member = __fbthrift_member;
+  using fields = ::fatal::list<
+      member::FIELDA
+  >;
+
+  using annotations = __fbthrift_refl_impl::no_annotations;
+
+  static char const *to_string(type e, char const *fallback) {
+    switch (e) {
+      case type::FIELDA: return "FIELDA";
+      default: return fallback;
+    }
+  }
+};
+
+} // __fbthrift_refl
+} // namespace facebook::ns::qwerty
 namespace apache::thrift::detail {
 template <>
 struct ExtraEnumTraits<::facebook::ns::qwerty::AnEnumA> {
--- third-party/thrift/src/thrift/compiler/test/fixtures/mcpp2-compare/out/cpp2_reflection/gen-cpp2/reflection_fatal_enum.h
+++ third-party/thrift/src/thrift/compiler/test/fixtures/mcpp2-compare/out/cpp2_reflection/gen-cpp2/reflection_fatal_enum.h
@@ -13,5 +13,12 @@
 
 #include <type_traits>
 
+namespace cpp2 {
+
+namespace __fbthrift_refl {
+namespace __fbthrift_refl_impl = ::apache::thrift::detail::reflection_impl;
+
+} // __fbthrift_refl
+} // namespace cpp2
 namespace apache::thrift::detail {
 }

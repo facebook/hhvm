5065767a710c2c7c2053164c3ce47a00812da056
Catherine Gasnier <catg@meta.com>
2024-10-18 07:45:54 -0700

    tests with cyclic bounds on type params
    Differential Revision: D64533537
    
    fbshipit-source-id: 070b95090ea84430ccb160c2c6327d2c6436aae4

---
+++ hphp/hack/test/typecheck/case_type/recursive/recursive_tparam_bounds1.php
@@ -0,0 +1,4 @@
+<?hh
+<<file: __EnableUnstableFeatures('case_types')>>
+
+case type A<T as A, T2 as vec<A>> = (T, T2);
---
+++ hphp/hack/test/typecheck/case_type/recursive/recursive_tparam_bounds2.php
@@ -0,0 +1,11 @@
+<?hh
+
+type A<T as A<T>> = T;
+
+function f<T as A<T>, T2 as A<T2> super A<T2>>(A<T> $x): void {
+  hh_expect<T>($x);
+  hh_expect<A<T>>($x);
+  hh_expect<T2>($x);
+  hh_expect<A<T2>>($x);
+  hh_expect<string>($x);
+}

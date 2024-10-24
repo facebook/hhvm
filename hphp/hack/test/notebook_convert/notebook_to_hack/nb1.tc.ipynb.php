ebc43943ddd49adc64004da8b12fd9f8b37169fa
Max Heiber <mheiber@meta.com>
2024-10-24 07:18:21 -0700

    use single-line comment syntax
    Summary:
    ## What
    
     use single-line comment syntax in our format for source-controlled hack notebooks
    
    ## Why
    
    It's easiest to parse the metadata in comments if we don't have to consider multi-line comments.
    
    ## Context
    
    fb-only, see below
    
    Reviewed By: patriciamckenzie
    
    Differential Revision: D64711055
    
    fbshipit-source-id: 988c1af059ce6738aab3ad31a11ec6c9b13baab0

--- hphp/hack/test/notebook_convert/notebook_to_hack/nb1.tc.ipynb.php
+++ hphp/hack/test/notebook_convert/notebook_to_hack/nb1.tc.ipynb.php
@@ -1,25 +1,25 @@
 <?hh
 
-/*@bento-cell:{"id": 2, "cell_type": "markdown"}*/
+//@bento-cell:{"id": 2, "cell_type": "markdown"}
 /*
 # Check it out
 
 I am a *markdown* **cell**
 */
 
-/*@bento-cell:{"id": 4, "cell_type": "code"}*/
+//@bento-cell:{"id": 4, "cell_type": "code"}
 class MyClass {}
 
 
 function notebook_main_n1234(): void {
-    /*@bento-cell:{"id": 1, "cell_type": "code"}*/
+    //@bento-cell:{"id": 1, "cell_type": "code"}
     new MyClass();
     echo "hi3";
-    /*@bento-cell:{"id": 4, "cell_type": "code"}*/
+    //@bento-cell:{"id": 4, "cell_type": "code"}
     echo "hi2";
     $x = 3;
     echo $x + 1;
     echo "hi0";
-    /*@bento-cell:{"id": 4, "cell_type": "code"}*/
+    //@bento-cell:{"id": 4, "cell_type": "code"}
     echo "hi1";
 }

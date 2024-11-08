0a80a825443843cd2a6bf4899d76d1f7b40bd692
Dave Kwon <dokwon@meta.com>
2024-11-08 13:52:32 -0800

    Add roundtrip serialization test for py3 custom test
    Summary: Add more test in preparation for adding limited adapter support for py3.
    
    Reviewed By: ahilger
    
    Differential Revision: D65608478
    
    fbshipit-source-id: af3e1324ab6f310ee9b1c2c2860cb53bcf68e1f8

--- third-party/thrift/src/thrift/lib/py3/test/auto_migrate/custom.py
+++ third-party/thrift/src/thrift/lib/py3/test/auto_migrate/custom.py
@@ -18,18 +18,23 @@
 import unittest
 
 from testing.types import customized
+from thrift.py3.serializer import deserialize, serialize
 
 
 # Tests for customized alternate implementations of the data structures:
 
 
 class CustomTests(unittest.TestCase):
+    def roundTrip(self, obj: customized) -> None:
+        self.assertEqual(obj, deserialize(customized, serialize(obj)))
+
     def test_list_template(self) -> None:
         c = customized(list_template=[1, 2, 3])
         res = []
         for v in c.list_template:
             res.append(v)
         self.assertEqual(res, [1, 2, 3])
+        self.roundTrip(c)
 
     def test_set_template(self) -> None:
         c = customized(set_template={1, 2, 3})
@@ -37,6 +42,7 @@ class CustomTests(unittest.TestCase):
         for v in c.set_template:
             res.add(v)
         self.assertEqual(res, {1, 2, 3})
+        self.roundTrip(c)
 
     def test_map_template(self) -> None:
         c = customized(map_template={1: 2, 3: 6, 5: 10})
@@ -44,6 +50,7 @@ class CustomTests(unittest.TestCase):
         for k, v in c.map_template.items():
             res[k] = v
         self.assertEqual(res, {1: 2, 3: 6, 5: 10})
+        self.roundTrip(c)
 
     def test_list_type(self) -> None:
         c = customized(list_type=[1, 2, 3])
@@ -51,6 +58,7 @@ class CustomTests(unittest.TestCase):
         for v in c.list_type:
             res.append(v)
         self.assertEqual(res, [1, 2, 3])
+        self.roundTrip(c)
 
     def test_set_type(self) -> None:
         c = customized(set_type={1, 2, 3})
@@ -58,6 +66,7 @@ class CustomTests(unittest.TestCase):
         for v in c.set_type:
             res.add(v)
         self.assertEqual(res, {1, 2, 3})
+        self.roundTrip(c)
 
     def test_map_type(self) -> None:
         c = customized(map_type={1: 2, 3: 6, 5: 10})
@@ -65,15 +74,19 @@ class CustomTests(unittest.TestCase):
         for k, v in c.map_type.items():
             res[k] = v
         self.assertEqual(res, {1: 2, 3: 6, 5: 10})
+        self.roundTrip(c)
 
     def test_string_type(self) -> None:
         c = customized(string_type="hello")
         self.assertEqual(c.string_type, "hello")
+        self.roundTrip(c)
 
     def test_cpp_name(self) -> None:
         c = customized(foo=3)
         self.assertEqual(c.foo, 3)
+        self.roundTrip(c)
 
     def test_list_of_uint32(self) -> None:
         c = customized(list_of_uint32=[1, 2, 3])
         self.assertEqual(c.list_of_uint32, [1, 2, 3])
+        self.roundTrip(c)

c3be0c29d3208be9e7564d651006c162355c5cc4
Jack Chistyakov <jchistyakov@meta.com>
2024-11-15 05:57:54 -0800

    Add minimal dummy handler
    Summary:
    Handler to go with the Dummy processor.
    Used downstream.
    
    Reviewed By: awalterschulze
    
    Differential Revision: D65959970
    
    fbshipit-source-id: a1377dc38cafe8ca2826e3d2ff019bb1660a189e

---
+++ third-party/thrift/src/thrift/lib/go/thrift/dummy/handler.go
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dummy
+
+import (
+	"context"
+)
+
+type DummyHandler struct {
+	// Testing helper to verify that oneway RPCs get delivered
+	OnewayRPCRequests chan<- string
+}
+
+// Compile time interface enforcer
+var _ Dummy = (*DummyHandler)(nil)
+
+func (h *DummyHandler) Echo(ctx context.Context, value string) (string, error) {
+	return value, nil
+}
+
+func (h *DummyHandler) OnewayRPC(ctx context.Context, value string) error {
+	if h.OnewayRPCRequests != nil {
+		h.OnewayRPCRequests <- value
+	}
+	return nil
+}

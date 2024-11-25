5674ee32b36fb033dcb6aa4416b92ee8e08bfacc
Max Heiber <mheiber@meta.com>
2024-11-25 14:20:04 -0800

    ensure we handle empty notebooks gracefully
    Summary:
    Add tests to make sure we handle emptiness gracefully.
    
    context: https://fburl.com/source-controlled-notebooks-for-hack-project-2024
    
    Reviewed By: tsurupin
    
    Differential Revision: D66455271
    
    fbshipit-source-id: 6e0a5bd8079dea1bf9db0cc2b4d9823b8fb2c0bd

---
+++ hphp/hack/test/notebook_convert/notebook_to_hack/nb6_no_hack_cells.ipynb.php
@@ -0,0 +1,13 @@
+<?hh
+// copyright header here
+//@bento-notebook:{"notebook_number":"N1234","kernelspec":{"display_name":"hack","language":"hack","name":"bento_kernel_hack"}}
+//@bento-cell:{"cell_bento_metadata":{},"cell_type":"markdown","id":1}
+/*
+## Markdown cell (cell 1)
+
+I am a *markdown* **cell**
+*/
+
+function notebook_main_N1234(): void {
+
+}
---
+++ hphp/hack/test/notebook_convert/notebook_to_hack/nb8_empty_cell_contents.ipynb.php
@@ -0,0 +1,7 @@
+<?hh
+// copyright header here
+//@bento-notebook:{"notebook_number":"N1234","kernelspec":{"display_name":"hack","language":"hack","name":"bento_kernel_hack"}}
+
+function notebook_main_N1234(): void {
+
+}

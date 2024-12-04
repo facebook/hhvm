33bd2e072e4b54018b83b0093e86533fe03a05e7
Jordi Cenzano Ferret <jcenzano@meta.com>
2024-12-04 10:56:38 -0800

    Fix moxygen OSS build
    Summary:
    Fixes a breakage in the open source build of moxygen, see:
    
    ```
    /Users/jcenzano/MetaOpenSource/moxygen/_build/deps/fizz/fizz/test/BogoShim.cpp:396:3: error: reference to 'gflags' is ambiguous
      396 |   gflags::ParseCommandLineFlags(&argc, &argv, true);
          |   ^
    /Users/jcenzano/homebrew/include/gflags/gflags_gflags.h:41:11: note: candidate found by name lookup is 'gflags'
       41 | namespace gflags {
          |           ^
    /Users/jcenzano/MetaOpenSource/moxygen/_build/deps/include/folly/portability/GFlags.h:124:11: note: candidate found by name lookup is 'folly::gflags'
      124 | namespace gflags = ::GFLAGS_NAMESPACE;
          |           ^
    ```
    
    Reviewed By: afrind
    
    Differential Revision: D66572281
    
    fbshipit-source-id: fe02d944e07cbf4daf7b93bf4dc410226e99d10a

--- third-party/fizz/src/fizz/test/BogoShim.cpp
+++ third-party/fizz/src/fizz/test/BogoShim.cpp
@@ -393,7 +393,7 @@ int main(int argc, char** argv) {
     }
   }
 
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  folly::gflags::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
   CryptoUtils::init();
 

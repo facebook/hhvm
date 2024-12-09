69000580f6279cbdf6dec7395eab073a98335fe3
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2024-12-09 13:56:45 -0800

    Add missing Pyre mode headers] [batch:35/1460] [shard:23/N]
    Differential Revision: D66952720
    
    fbshipit-source-id: 797e7c1f76ee280c08b3699ecdc2e62f719dab10

--- third-party/mcrouter/src/mcrouter/test/test_custom_failover.py
+++ third-party/mcrouter/src/mcrouter/test/test_custom_failover.py
@@ -4,6 +4,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-unsafe
+
 
 from mcrouter.test.MCProcess import Memcached
 from mcrouter.test.McrouterTestCase import McrouterTestCase

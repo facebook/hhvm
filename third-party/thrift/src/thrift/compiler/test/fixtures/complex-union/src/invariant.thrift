b28979640fb2736fe7d9b8c86a4771b738340ce2
Satish Kumar <satishvk@meta.com>
2025-01-16 18:56:13 -0800

    Add invariant types to complex union
    Summary:
    To provide the ability for the diff to show `fbthrift_current_value` removed from generated unions with invariant types in `thrift_abstract_types`, add invariant types to union fixtures.
    
    Rebuild fixtures with `buck2 run fbcode//thrift/compiler/test:build_fixtures`.
    
    Reviewed By: yoney
    
    Differential Revision: D68270121
    
    fbshipit-source-id: 5924427ea77a569cbe158e62a45576fafa59e806

---
+++ third-party/thrift/src/thrift/compiler/test/fixtures/complex-union/src/invariant.thrift
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace java.swift test.fixtures.complex_union
+
+struct StructForInvariantTypes {
+  1: i64 num;
+}
+
+struct UnionForInvariantTypes {
+  1: i32 num32;
+  2: i64 num64;
+}
+
+union InvariantTypes {
+  1: map<StructForInvariantTypes, i64> struct_map;
+  2: map<UnionForInvariantTypes, i64> union_map;
+}

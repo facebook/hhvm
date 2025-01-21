759c1c246bea63983f01393fbce0fa50e7407eb7
George Giorgidze <giorgidze@meta.com>
2025-01-21 09:44:21 -0800

    Add set_nosync function
    Summary: This function sets the nosync flag on the lmdb environment, which disables the automatic flushing of system buffers to disk when committing a transaction.
    
    Reviewed By: nt591
    
    Differential Revision: D68428163
    
    fbshipit-source-id: f0f8c0cfab67266c0621c562694718fa613b08f0

--- hphp/hack/src/utils/lmdb/lmdb.rs
+++ hphp/hack/src/utils/lmdb/lmdb.rs
@@ -130,6 +130,11 @@ impl Options {
         self.set_flag(enable, MDB_RDONLY)
     }
 
+    /// Don't flush system buffers to disk when committing a transaction. This optimization means a system crash can corrupt the database or lose the last transactions if buffers are not yet flushed to disk. The risk is governed by how often the system flushes dirty buffers to disk and how often mdb_env_sync() is called. However, if the filesystem preserves write order and the MDB_WRITEMAP flag is not used, transactions exhibit ACI (atomicity, consistency, isolation) properties and only lose D (durability). I.e. database integrity is maintained, but a system crash may undo the final transactions. Note that (MDB_NOSYNC | MDB_WRITEMAP) leaves the system with no hint for when to write transactions to disk, unless mdb_env_sync() is called. (MDB_MAPASYNC | MDB_WRITEMAP) may be preferable. This flag may be changed at any time using mdb_env_set_flags().
+    pub fn set_nosync(self, enable: bool) -> Self {
+        self.set_flag(enable, MDB_NOSYNC)
+    }
+
     /// Flush system buffers to disk only once per transaction, omit the
     /// metadata flush. Defer that until the system flushes files to disk,
     /// or next non-MDB_RDONLY commit or mdb_env_sync(). This optimization

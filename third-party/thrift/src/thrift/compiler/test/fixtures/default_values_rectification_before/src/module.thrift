1c14921ee6ffdfa8c18d59fd84850e9b4a2b5e33
Aristidis Papaioannou <aristidis@meta.com>
2025-01-23 22:32:27 -0800

    Add fixtures default_values_{before,after}_rectification
    Summary: To compare generated output in preparation of custom value cleanup: the "before" module.thrift shows the possible custom default values today, and the "after" shows what the desired corresponding file would look like without these values.
    
    Reviewed By: praihan, createdbysk
    
    Differential Revision: D68504535
    
    fbshipit-source-id: c015cb74fc4aa35de19ad4ceeb3863f0e2e17a08

---
+++ third-party/thrift/src/thrift/compiler/test/fixtures/default_values_rectification_before/src/module.thrift
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package "facebook.com/thrift/compiler/test/fixtures/default_values_rectification"
+
+struct EmptyStruct {}
+
+struct TestStruct {
+  1: i32 unqualified_int_field = 0;
+
+  2: bool unqualified_bool_field = false;
+
+  3: list<i32> unqualified_list_field = [];
+
+  4: EmptyStruct unqualified_struct_field = {};
+
+  5: optional i32 optional_int_field = 42;
+
+  6: optional bool optional_bool_field = true;
+
+  7: optional list<i32> optional_list_field = [1, 2];
+
+  8: optional EmptyStruct optional_struct_field = {};
+}

3ba2e26b90d26899fac2cd70e2dfa5d63b568e11
Dave Kwon <dokwon@meta.com>
2025-01-30 16:10:45 -0800

    Add @cpp.UseOpEncode fixtures
    Summary: Add missing fixtures
    
    Reviewed By: Filip-F
    
    Differential Revision: D68918587
    
    fbshipit-source-id: fb48b6bcfa0430a23d84d2d78d715205bfe88cbf

---
+++ third-party/thrift/src/thrift/compiler/test/fixtures/use_op_encode/src/module.thrift
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+include "thrift/annotation/cpp.thrift"
+include "thrift/annotation/thrift.thrift"
+
+@cpp.UseOpEncode
+package "facebook.com/thrift/compiler/test"
+
+struct MyStruct {
+    1: i32 def_field;
+    2: optional i32 opt_field;
+    3: required i32 req_field;
+    @thrift.TerseWrite
+    4: i32 terse_field;
+}

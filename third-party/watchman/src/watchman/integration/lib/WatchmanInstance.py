baca1840b11390b5e2199a5bb331cf7e21b7c44a
Muir Manders <muirdm@meta.com>
2025-02-04 09:14:55 -0800

    pywatchman: kill a couple more references to "compat"
    Summary: I removed the compat module in D68741983, but missed a couple spots.
    
    Reviewed By: singhsrb, zzl0
    
    Differential Revision: D69121318
    
    fbshipit-source-id: fb647dbe1846654d50d490286c81bfe8faa1dbef

--- third-party/watchman/src/watchman/integration/lib/WatchmanInstance.py
+++ third-party/watchman/src/watchman/integration/lib/WatchmanInstance.py
@@ -23,7 +23,6 @@ import pywatchman
 
 from . import TempDir
 
-
 try:
     import pwd
 except ImportError:
@@ -211,12 +210,7 @@ class _Instance:
             )
         if self.debug_watchman:
             print("Watchman instance PID: " + str(self.proc.pid))
-            # pyre-fixme[16]: Module `pywatchman` has no attribute `compat`.
-            if pywatchman.compat.PYTHON3:
-                user_input = input
-            else:
-                # pyre-fixme[10]: Name `raw_input` is used but not defined.
-                user_input = raw_input  # noqa:F821
+            user_input = input
             user_input("Press Enter to continue...")
 
         # wait for it to come up

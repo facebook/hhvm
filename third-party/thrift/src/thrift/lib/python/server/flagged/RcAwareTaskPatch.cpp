922daf99f904d929876a12492da93054d0beef57
Wendi Liu <wdliu@meta.com>
2025-02-04 23:26:47 -0800

    replace JK with thrift flag for RequestContextAwareTask patch rollout
    Summary: JK caused quite a few errors in both tests that requires explicit mocking, and in thrift services that don't have access to configerator. change it to thrift flag.
    
    Differential Revision: D69160650
    
    fbshipit-source-id: cd29d0ef1b3b6d86eeea349d7ff549e66e554377

---
+++ third-party/thrift/src/thrift/lib/python/server/flagged/RcAwareTaskPatch.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <thrift/lib/cpp2/Flags.h>
+
+#include <thrift/lib/python/server/flagged/RcAwareTaskPatch.h>
+
+THRIFT_FLAG_DEFINE_bool(rc_aware_task_patch_enabled, false);
+
+namespace thrift::python::detail {
+
+bool isRcAwareTaskPatchEnabled() {
+  return THRIFT_FLAG(rc_aware_task_patch_enabled);
+}
+
+void mockRcAwareTaskPatchEnabled(bool enabled) {
+  THRIFT_FLAG_SET_MOCK(rc_aware_task_patch_enabled, enabled);
+}
+
+} // namespace thrift::python::detail

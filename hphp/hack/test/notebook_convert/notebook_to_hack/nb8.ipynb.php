6fe0823a869f1337f085ae1b363a057e595c0627
Max Heiber <mheiber@meta.com>
2025-02-25 08:33:22 -0800

    Repro bug
    Summary:
    Repro bug where we lose newlines when converting from notebook JSON to Hack (.php).
    
    [context](https://fburl.com/source-controlled-notebooks-for-hack-plan).
    
    The user-facing effect of this bug was for notebook creation to spin indefinitely (though we plan to have better error handling in the place that hung)
    
    Fixed in next diff
    
    Reviewed By: nt591
    
    Differential Revision: D70166475
    
    fbshipit-source-id: e36043027843e4e5465cbacede112f721e833c7d

---
+++ hphp/hack/test/notebook_convert/notebook_to_hack/nb8.ipynb.php
@@ -0,0 +1,10 @@
+<?hh
+// copyright header here
+//@bento-notebook:{"notebook_number":"N1234","kernelspec":{"display_name":"hack","language":"hack","metadata":{"cinder_runtime":false,"default_tagged_version":"2093","download_status":"downloaded","fbpkg_supported":true,"is_prebuilt":true,"kernel_name":"bento_kernel_hack","kernel_version":"2093","nightly_builds":true},"name":"bento_kernel_hack"}}
+//@bento-cell:{"cell_bento_metadata":{"customInput":"foo_plugin","extensionName":"FOO_EXTENSION","isAgentGenerated":false,"language":"hack","originalKey":"32948q24","outputsInitialized":false},"cell_type":"code","id":1}
+final class MyClass {  // This is a comment  const int METADATA_ID = 219481;}
+
+
+async function gen_notebook_main_N1234(): Awaitable<void> {
+
+}

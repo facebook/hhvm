9fb4155372f7dc406b6f15a04c4f65deddecff43
Sam De Roeck <sadroeck@meta.com>
2025-03-04 09:29:54 -0800

    Simplify protocol object benchmark & re-use statics as much as possible
    Summary:
    Each benchmark case now creates a static of `TestingObject<T>`, which provides both the value, a serialized buffer containing that object (for each protocol) & a `protocol.Object` representation.
    
    These are constructed once & re-used as much as possible. The overall outcome of the results is the same, but it significantly reduces the total time required to run the benchmarks.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D70534018
    
    fbshipit-source-id: d345e27954dbf946d8cdcc6594df90d014cb0c06

--- third-party/thrift/src/thrift/lib/cpp2/test/ThriftObjectBench.cpp
+++ third-party/thrift/src/thrift/lib/cpp2/test/ThriftObjectBench.cpp
@@ -31,12 +31,7 @@ using namespace apache::thrift;
 using namespace thrift::benchmark;
 using namespace apache::thrift::test::utils;
 
-constexpr std::size_t QUEUE_ALLOC_SIZE = 4 * 1024 * 1024;
-
-folly::IOBufQueue& get_queue() {
-  static folly::IOBufQueue queue(folly::IOBufQueue::cacheChainLength());
-  return queue;
-}
+constexpr std::size_t QUEUE_ALLOC_SIZE = 16 * 1024 * 1024;
 
 template <typename ProtocolWriter, typename T>
 std::unique_ptr<folly::IOBuf> serialize(T& s) {
@@ -44,95 +39,105 @@ std::unique_ptr<folly::IOBuf> serialize(T& s) {
   ProtocolWriter writer;
   writer.setOutput(&iobufQueue);
   s.write(&writer);
-  return iobufQueue.move();
-}
-
-template <typename T, typename ProtocolWriter, typename F>
-void withSerialized(F&& f) {
-  folly::BenchmarkSuspender suspender;
-  auto t = create<T>();
-  auto buf = serialize<ProtocolWriter>(t);
+  auto buf = iobufQueue.move();
   buf->coalesce();
-  suspender.dismiss();
-  const auto res = f(*buf);
-  // Destructors & clean-up are excluded from benchmarking
-  suspender.rehire();
-  folly::doNotOptimizeAway(res);
-  get_queue().clearAndTryReuseLargestBuffer();
+  return buf;
 }
 
-template <typename T, typename ProtocolWriter, typename ParseF, typename F>
-void withParsed(ParseF&& parseF, F&& f) {
-  folly::BenchmarkSuspender suspender;
-  auto t = create<T>();
-  auto buf = serialize<ProtocolWriter>(t);
-  buf->coalesce();
-  const auto obj = parseF(*buf);
-  suspender.dismiss();
-  const auto res = f(obj);
-  // Destructors & clean-up are excluded from benchmarking
-  suspender.rehire();
-  folly::doNotOptimizeAway(res);
-  get_queue().clearAndTryReuseLargestBuffer();
-}
+template <typename T>
+struct TestingObject {
+  T val;
+  std::unique_ptr<folly::IOBuf> buf;
+  protocol::Object obj;
 
-template <typename ProtocolWriter, typename ProtocolReader, typename T>
-void bench_decode_protocol() {
-  withSerialized<T, ProtocolWriter>([](folly::IOBuf& buf) {
-    return protocol::parseObject<ProtocolReader>(buf);
-  });
-}
-
-template <typename ProtocolWriter, typename ProtocolReader, typename T>
-void bench_encode_protocol() {
-  withParsed<T, ProtocolWriter>(
-      [](auto& buf) { return protocol::parseObject<ProtocolReader>(buf); },
-      [](const auto& obj) {
-        protocol::serializeObject<ProtocolWriter>(obj, get_queue());
-        return 0;
-      });
-}
-
-template <typename ProtocolWriter, typename ProtocolReader, typename T>
-void bench_read_all_protocol() {
-  withParsed<T, ProtocolWriter>(
-      [](auto& buf) { return protocol::parseObject<ProtocolReader>(buf); },
-      [](const auto& obj) { return read_all(obj); });
-}
-
-template <typename ProtocolWriter, typename ProtocolReader, typename T>
-void bench_read_half_protocol() {
-  withParsed<T, ProtocolWriter>(
-      [](auto& buf) { return protocol::parseObject<ProtocolReader>(buf); },
-      [](const auto& obj) { return read_some(SparseAccess::Half, obj); });
-}
-
-template <typename ProtocolWriter, typename ProtocolReader, typename T>
-void bench_read_first_protocol() {
-  withParsed<T, ProtocolWriter>(
-      [](auto& buf) { return protocol::parseObject<ProtocolReader>(buf); },
-      [](const auto& obj) {
-        return read_some(SparseAccess::SingleRandom, obj);
-      });
-}
-
-#define BENCH_OP(PROT_NAME, OP, WRITER, READER, T) \
-  BENCHMARK(PROT_NAME##_protocol_##T##_##OP) {     \
-    bench_##OP##_protocol<WRITER, READER, T>();    \
+  template <typename ProtocolWriter, typename ProtocolReader>
+  static TestingObject<T> make() {
+    T value = create<T>();
+    std::unique_ptr<folly::IOBuf> buf = serialize<ProtocolWriter>(value);
+    protocol::Object obj = protocol::parseObject<ProtocolReader>(*buf);
+    return TestingObject<T>{std::move(value), std::move(buf), std::move(obj)};
   }
 
-// Define a list of protocols to benchmark each operation + struct
-#define FOR_EACH_PROTOCOL(OP, T)                                      \
-  BENCH_OP(Binary, OP, BinaryProtocolWriter, BinaryProtocolReader, T) \
-  BENCH_OP(Compact, OP, CompactProtocolWriter, CompactProtocolReader, T)
+ private:
+  TestingObject(
+      T&& val, std::unique_ptr<folly::IOBuf>&& buf, protocol::Object&& obj)
+      : val{std::move(val)}, buf{std::move(buf)}, obj{std::move(obj)} {}
+};
 
-// Define a list of operations to benchmark for each struct
-#define BENCH_T(T)                \
-  FOR_EACH_PROTOCOL(decode, T)    \
-  FOR_EACH_PROTOCOL(encode, T)    \
-  FOR_EACH_PROTOCOL(read_all, T)  \
-  FOR_EACH_PROTOCOL(read_half, T) \
-  FOR_EACH_PROTOCOL(read_first, T)
+folly::IOBufQueue& get_queue() {
+  static folly::IOBufQueue queue(folly::IOBufQueue::cacheChainLength());
+  return queue;
+}
+
+// Constructs a serialized buffer for the given type encoded with a given
+// Protocol(Writer)
+template <typename T, typename ProtocolWriter, typename ProtocolReader>
+const TestingObject<T>& get_serde();
+
+template <typename ProtocolWriter, typename ProtocolReader, typename T>
+void bench_decode_protocol(const TestingObject<T>& input) {
+  const auto obj = protocol::parseObject<ProtocolReader>(*input.buf);
+  folly::doNotOptimizeAway(obj);
+  BENCHMARK_SUSPEND {
+    std::destroy_at(&obj);
+  }
+}
+
+template <typename ProtocolWriter, typename ProtocolReader, typename T>
+void bench_encode_protocol(const TestingObject<T>& input) {
+  auto& queue = get_queue();
+  protocol::serializeObject<ProtocolWriter>(input.obj, get_queue());
+  folly::doNotOptimizeAway(queue);
+  BENCHMARK_SUSPEND {
+    queue.clearAndTryReuseLargestBuffer();
+  }
+}
+
+template <typename ProtocolWriter, typename ProtocolReader, typename T>
+void bench_read_all_protocol(const TestingObject<T>& input) {
+  const auto val = read_all(input.obj);
+  folly::doNotOptimizeAway(val);
+}
+
+template <typename ProtocolWriter, typename ProtocolReader, typename T>
+void bench_read_half_protocol(const TestingObject<T>& input) {
+  const auto val = read_some(SparseAccess::Half, input.obj);
+  folly::doNotOptimizeAway(val);
+}
+
+template <typename ProtocolWriter, typename ProtocolReader, typename T>
+void bench_read_first_protocol(const TestingObject<T>& input) {
+  const auto val = read_some(SparseAccess::SingleRandom, input.obj);
+  folly::doNotOptimizeAway(val);
+}
+
+#define BENCH_OP(PROT_NAME, OP, WRITER, READER, T)                            \
+  BENCHMARK(PROT_NAME##_protocol_##T##_##OP) {                                \
+    bench_##OP##_protocol<WRITER, READER, T>(get_serde<T, WRITER, READER>()); \
+  }
+
+// Define prototype serialization & deserialization functions for each struct &
+// protocol
+#define DEFINE_SERDE(T, WRITER, READER)                                     \
+  template <>                                                               \
+  const TestingObject<T>& get_serde<T, WRITER, READER>() {                  \
+    static TestingObject<T> obj = TestingObject<T>::make<WRITER, READER>(); \
+    return obj;                                                             \
+  }
+
+// Define a list of operations to benchmark for each struct + protocol
+#define FOR_EACH_OP(PROT_NAME, T, WRITER, READER)   \
+  DEFINE_SERDE(T, WRITER, READER)                   \
+  BENCH_OP(PROT_NAME, decode, WRITER, READER, T)    \
+  BENCH_OP(PROT_NAME, encode, WRITER, READER, T)    \
+  BENCH_OP(PROT_NAME, read_all, WRITER, READER, T)  \
+  BENCH_OP(PROT_NAME, read_half, WRITER, READER, T) \
+  BENCH_OP(PROT_NAME, read_first, WRITER, READER, T)
+
+// Define a list of protocols to benchmark for each struct
+#define BENCH_T(T)                                                   \
+  FOR_EACH_OP(Binary, T, BinaryProtocolWriter, BinaryProtocolReader) \
+  FOR_EACH_OP(Compact, T, CompactProtocolWriter, CompactProtocolReader)
 
 // Define benchmarks for each struct
 BENCH_T(Empty)

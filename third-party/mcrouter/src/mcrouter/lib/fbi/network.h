8d7c0669b49503356abbffaf70a006a837662fd2
Alper Yoney <alperyoney@meta.com>
2025-03-05 18:35:48 -0800

    Refactor ipv6_addr_loopback function
    Summary:
    Use `IN6_IS_ADDR_LOOPBACK` macro to fix the macos build.
    
    ```
    The <netinet/in.h> header shall define the following macros that test for special IPv6 addresses. Each macro is of type int and takes a single argument of type const struct in6_addr *:
    ...
    IN6_IS_ADDR_LOOPBACK
        Loopback address
    ...
    ```
    
    Ref: https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/netinet_in.h.html
    
    Reviewed By: iahs
    
    Differential Revision: D70647266
    
    fbshipit-source-id: fe5c6d68e9651a8cb66d7d076f28b425550db77a

--- third-party/mcrouter/src/mcrouter/lib/fbi/network.h
+++ third-party/mcrouter/src/mcrouter/lib/fbi/network.h
@@ -25,8 +25,7 @@ static inline bool ipv4_addr_loopback(const struct in_addr* a) {
 }
 
 static inline bool ipv6_addr_loopback(const struct in6_addr* a) {
-  return (a->s6_addr32[0] | a->s6_addr32[1] | a->s6_addr32[2] |
-          (a->s6_addr32[3] ^ htonl(1))) == 0;
+  return IN6_IS_ADDR_LOOPBACK(a);
 }
 
 /**

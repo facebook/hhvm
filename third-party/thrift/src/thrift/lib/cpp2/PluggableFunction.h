0cafcac5df3bd6b9a94b88c7439edafd79a591c5
Matthew Hillman <mhillman@meta.com>
2025-03-11 04:07:15 -0700

    Reinstate 'Add late override support to Thrift pluggable functions'
    Summary: I believe the auto-revert of the previous attempt to have been a result of a now-resolved unrelated issue.
    
    Reviewed By: andriigrynenko
    
    Differential Revision: D70704513
    
    fbshipit-source-id: 28cf3e35f5c891c538069f06c89e4e0adafde15e

--- third-party/thrift/src/thrift/lib/cpp2/PluggableFunction.h
+++ third-party/thrift/src/thrift/lib/cpp2/PluggableFunction.h
@@ -67,16 +67,28 @@ class PluggableFunction<Ret(Args...)> {
   using signature = Ret(Args...);
 
   constexpr explicit PluggableFunction(
-      signature& init, const char* name) noexcept
-      : init_{init}, name_{name} {}
+      signature& init, const char* name, bool allowLateOverride) noexcept
+      : init_{init}, name_{name}, allowLateOverride_(allowLateOverride) {}
 
   const PluggableFunction& operator=(signature& next) const noexcept {
     if (auto prev = impl_.exchange(&next)) {
-      auto reason = prev == &init_
-          ? " pluggable function: override after invocation"
-          : " pluggable function: override after override";
-      std::string msg = fmt::format("[{}] {}", name_, reason);
-      folly::terminate_with<std::logic_error>(msg);
+      enum class Reason { OVERRIDE_AFTER_INVOCATION, OVERRIDE_AFTER_OVERRIDE };
+      Reason reason = prev == &init_ ? Reason::OVERRIDE_AFTER_INVOCATION
+                                     : Reason::OVERRIDE_AFTER_OVERRIDE;
+      const char* reasonText = [&]() {
+        switch (reason) {
+          case Reason::OVERRIDE_AFTER_INVOCATION:
+            return " pluggable function: override after invocation";
+          case Reason::OVERRIDE_AFTER_OVERRIDE:
+            return " pluggable function: override after override";
+        }
+      }();
+
+      if (!(allowLateOverride_ &&
+            reason == Reason::OVERRIDE_AFTER_INVOCATION)) {
+        std::string msg = fmt::format("[{}] {}", name_, reasonText);
+        folly::terminate_with<std::logic_error>(msg);
+      }
     }
     return *this;
   }
@@ -107,6 +119,7 @@ class PluggableFunction<Ret(Args...)> {
   mutable folly::relaxed_atomic<signature*> impl_{nullptr};
   signature& init_;
   const char* name_;
+  const bool allowLateOverride_;
 };
 
 } // namespace apache::thrift::detail
@@ -119,7 +132,13 @@ class PluggableFunction<Ret(Args...)> {
 #define THRIFT_PLUGGABLE_FUNC_REGISTER(_ret, _name, ...)                     \
   FOLLY_STORAGE_CONSTEXPR const ::apache::thrift::detail::PluggableFunction< \
       _ret(__VA_ARGS__)>                                                     \
-      _name{THRIFT__PLUGGABLE_FUNC_DEFAULT_##_name, #_name};                 \
+      _name{THRIFT__PLUGGABLE_FUNC_DEFAULT_##_name, #_name, false};          \
+  _ret THRIFT__PLUGGABLE_FUNC_DEFAULT_##_name(__VA_ARGS__)
+
+#define THRIFT_PLUGGABLE_FUNC_REGISTER_ALLOW_LATE_OVERRIDE(_ret, _name, ...) \
+  FOLLY_STORAGE_CONSTEXPR const ::apache::thrift::detail::PluggableFunction< \
+      _ret(__VA_ARGS__)>                                                     \
+      _name{THRIFT__PLUGGABLE_FUNC_DEFAULT_##_name, #_name, true};           \
   _ret THRIFT__PLUGGABLE_FUNC_DEFAULT_##_name(__VA_ARGS__)
 
 #define THRIFT_PLUGGABLE_FUNC_SET(_ret, _name, ...)      \

da3ce029fa6ab0698fed2087e3b8c5b9a1a5cfd5
TJ Yin <ytj@meta.com>
2025-03-12 14:51:37 -0700

    Convert Union's Type to integer in CHECK_EQ(...)
    Summary: CHECK_EQ will use `ostream::operator<<` to log the value, which won't work with scoped enum. This diff converts the enum to the underlying integer first.
    
    Reviewed By: iahs
    
    Differential Revision: D71023223
    
    fbshipit-source-id: 505885e3a0a0715e01f6c9230e0e23c0a83fd29d

--- third-party/thrift/src/thrift/lib/cpp2/GeneratedCodeHelper.h
+++ third-party/thrift/src/thrift/lib/cpp2/GeneratedCodeHelper.h
@@ -1430,8 +1430,8 @@ folly::exception_wrapper decode_stream_exception(folly::exception_wrapper ew) {
         auto& payload = err.encoded;
         DCHECK_EQ(payload.metadata.payloadMetadata().has_value(), true);
         DCHECK_EQ(
-            payload.metadata.payloadMetadata()->getType(),
-            PayloadMetadata::Type::exceptionMetadata);
+            folly::to_underlying(payload.metadata.payloadMetadata()->getType()),
+            folly::to_underlying(PayloadMetadata::Type::exceptionMetadata));
         auto& exceptionMetadataBase =
             payload.metadata.payloadMetadata()->get_exceptionMetadata();
         if (auto exceptionMetadataRef = exceptionMetadataBase.metadata()) {

a6afde13558b99163b457ce20a2370a17e9f6db1
Jack Chistyakov <jchistyakov@meta.com>
2025-03-14 10:28:44 -0700

    Merge serializer and deserializer in serdes.go file
    Summary: Marry them together.
    
    Reviewed By: podtserkovskiy
    
    Differential Revision: D71171800
    
    fbshipit-source-id: 1f2c3fa4500c1d4ca3a4ff3ee0ae037fa9d33646

--- third-party/thrift/src/thrift/lib/go/thrift/serdes.go
+++ third-party/thrift/src/thrift/lib/go/thrift/serdes.go
@@ -102,3 +102,57 @@ func (s *Serializer) Encode(msg types.WritableStruct) error {
 	}
 	return s.format.Flush()
 }
+
+// A Deserializer is used to turn a byte stream into a Struct
+type Deserializer struct {
+	format types.Format
+}
+
+// NewBinaryDeserializer creates a new deserializer using the binary format
+func NewBinaryDeserializer(readWriter types.ReadWriteSizer) *Deserializer {
+	return &Deserializer{format: NewBinaryFormat(readWriter)}
+}
+
+// NewCompactDeserializer creates a new deserializer using the compact format
+func NewCompactDeserializer(readWriter types.ReadWriteSizer) *Deserializer {
+	return &Deserializer{format: NewCompactFormat(readWriter)}
+}
+
+// NewCompactJSONDeserializer creates a new deserializer using the JSON format
+func NewCompactJSONDeserializer(readWriter io.ReadWriter) *Deserializer {
+	return &Deserializer{format: NewCompactJSONFormat(readWriter)}
+}
+
+// NewSimpleJSONDeserializer creates a new deserializer using the simple JSON format
+func NewSimpleJSONDeserializer(readWriter io.ReadWriter) *Deserializer {
+	return &Deserializer{format: NewSimpleJSONFormat(readWriter)}
+}
+
+// DecodeCompact deserializes a compact format message
+func DecodeCompact(data []byte, msg types.ReadableStruct) error {
+	reader := bytes.NewBuffer(data)
+	return NewCompactDeserializer(reader).Decode(msg)
+}
+
+// DecodeBinary deserializes a binary format message
+func DecodeBinary(data []byte, msg types.ReadableStruct) error {
+	reader := bytes.NewBuffer(data)
+	return NewBinaryDeserializer(reader).Decode(msg)
+}
+
+// DecodeCompactJSON deserializes a compact JSON format message
+func DecodeCompactJSON(data []byte, msg types.ReadableStruct) error {
+	reader := bytes.NewBuffer(data)
+	return NewCompactJSONDeserializer(reader).Decode(msg)
+}
+
+// DecodeSimpleJSON deserializes a simple JSON format message
+func DecodeSimpleJSON(data []byte, msg types.ReadableStruct) error {
+	reader := bytes.NewBuffer(data)
+	return NewSimpleJSONDeserializer(reader).Decode(msg)
+}
+
+// Decode deserializes a Thrift struct from the underlying format
+func (d *Deserializer) Decode(msg types.ReadableStruct) error {
+	return msg.Read(d.format)
+}

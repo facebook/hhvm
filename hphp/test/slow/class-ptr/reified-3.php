20bd997306ebd794daedac0836e3afd54a82e630
Vassil Mladenov <vmladenov@meta.com>
2025-03-18 14:48:56 -0700

    Commit reified test cases showing implicit string to class conversion
    Reviewed By: jano
    
    Differential Revision: D71418381
    
    fbshipit-source-id: 73d8cfac6076427e98df73d340721f4509bdc4b0

---
+++ hphp/test/slow/class-ptr/reified-3.php
@@ -0,0 +1,55 @@
+<?hh
+
+class X {}
+
+class C {
+  public static function m<Te>(): void { echo "C::m\n"; }
+  public static function r<reify Tr>(): void { var_dump(HH\ReifiedGenerics\get_classname<Tr>()); }
+  const string S = "hi";
+}
+
+class R<reify T> {
+  public static function m<Te>(): void { echo "R::m\n"; }
+  public static function r<reify Tr>(): void { var_dump(HH\ReifiedGenerics\get_classname<Tr>()); }
+  const string S = "hi";
+}
+
+
+function f<reify Treify, Terase>(): void {
+  echo ">> T::cm\n";
+  Treify::m();
+  Treify::m<Terase>();
+  Treify::m<X>();
+  Treify::m<Tr>();
+
+  // TODO(T218390557)
+  // echo ">> T::r\n";
+  // Treify::r<X>();
+  // Treify::r<Tr>();
+
+  echo ">> T::TS\n";
+  var_dump(Treify::S);
+  echo ">> T::cm<>\n";
+  $m1 = Treify::m<>;
+  $m2 = Treify::m<Terase>;
+  $m3 = Treify::m<X>;
+  $m4 = Treify::m<Treify>;
+  $m1();
+  $m2();
+  $m3();
+  $m4();
+  echo ">> T::cr<>\n";
+  $r1 = Treify::r<X>;
+  $r2 = Treify::r<Treify>;
+  $r1();
+  $r2();
+}
+
+<<__EntryPoint>>
+function main(): void {
+  echo "=== C ===\n";
+  f<C, X>();
+
+  echo "\n\n=== R<int> ===\n";
+  f<R<X>, X>();
+}

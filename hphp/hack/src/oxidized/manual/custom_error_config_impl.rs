f25818f8440f67492ff7945fda748904b7d382d5
Cj Longoria <cjlongoria@meta.com>
2025-03-26 13:59:42 -0700

    update platform010 & platform010-aarch64 symlinks
    Summary:
    Updates that are folded in include:
    - Removed the unstable feature async_closure - D71122628
    - Add extra param to `extract_if` for vec - D71447259
    - unstable format options - D71642978
    - remove no_mangle attribute - D71646078
    - misc changes - D71643626
    - clippy updates - D71766631
    - updated nightly version from 2024-11-22 to 2025-01-03 where applicable to match new stable release - P1764978047
        - Except for OCaml. That is show in the paste above, but was moved to a follow up diff (D71817119)
    
    allow-large-files
    
    Reviewed By: dtolnay
    
    Differential Revision: D70942192
    
    fbshipit-source-id: 41d942448f1475c855daf6fe3dd36ea22abea86b

--- hphp/hack/src/oxidized/manual/custom_error_config_impl.rs
+++ hphp/hack/src/oxidized/manual/custom_error_config_impl.rs
@@ -39,7 +39,7 @@ use crate::validation_err::ValidationErr;
 impl CustomErrorConfig {
     pub fn new(mut errors: Vec<CustomError>) -> Self {
         let invalid = errors
-            .extract_if(|e| {
+            .extract_if(.., |e| {
                 let mut env = ValidationEnv::default();
                 !e.validate(&mut env)
             })

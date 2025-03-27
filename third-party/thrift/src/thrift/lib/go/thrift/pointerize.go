5e5582bca6cd97723d5dc95ed745b11cfde25347
Jack Chistyakov <jchistyakov@meta.com>
2025-03-27 11:57:42 -0700

    Deprecate Int32Ptr function
    Summary: Superceded by Pointerize().
    
    Reviewed By: leoleovich
    
    Differential Revision: D71930002
    
    fbshipit-source-id: de0a0b9f12129518b6a38d6082e60204721c567f

--- third-party/thrift/src/thrift/lib/go/thrift/pointerize.go
+++ third-party/thrift/src/thrift/lib/go/thrift/pointerize.go
@@ -35,6 +35,5 @@ package thrift
 // exported thrift structs, we factor such helpers here.
 ///////////////////////////////////////////////////////////////////////////////
 
-func Int32Ptr(v int32) *int32    { return &v }
 func Int64Ptr(v int64) *int64    { return &v }
 func StringPtr(v string) *string { return &v }

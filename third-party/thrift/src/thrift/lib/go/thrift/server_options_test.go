db1c73c5c88a75bd0473852b6babe71d59596e0b
Jack Chistyakov <jchistyakov@meta.com>
2025-04-08 10:35:09 -0700

    Server options unittest coverage
    Summary: Raise coverage to 100%
    
    Reviewed By: mahmednabil109
    
    Differential Revision: D72584333
    
    fbshipit-source-id: 34d9ff0b2d19e5f8d403cd68163d186340d87176

--- third-party/thrift/src/thrift/lib/go/thrift/server_options_test.go
+++ third-party/thrift/src/thrift/lib/go/thrift/server_options_test.go
@@ -17,9 +17,13 @@
 package thrift
 
 import (
+	"context"
 	"crypto/tls"
+	"net"
+	"runtime"
 	"testing"
 
+	"github.com/facebook/fbthrift/thrift/lib/go/thrift/stats"
 	"github.com/stretchr/testify/require"
 )
 
@@ -41,4 +45,67 @@ func TestListenerALPNOptions(t *testing.T) {
 			require.Equal(t, tc.expectedNextProtos, tlsConfig.NextProtos)
 		})
 	}
+}
+
+func TestWithoutPipelining(t *testing.T) {
+	defaultOptions := newServerOptions()
+	require.True(t, defaultOptions.pipeliningEnabled)
+
+	customOptions := newServerOptions(WithoutPipelining())
+	require.False(t, customOptions.pipeliningEnabled)
+}
+
+func TestWithNumWorkers(t *testing.T) {
+	defaultOptions := newServerOptions()
+	require.Equal(t, runtime.NumCPU(), defaultOptions.numWorkers)
+
+	customOptions := newServerOptions(WithNumWorkers(12345))
+	require.Equal(t, 12345, customOptions.numWorkers)
+}
+
+func TestWithConnContext(t *testing.T) {
+	dummyConn, _ := net.Pipe()
+
+	defaultOptions := newServerOptions()
+	require.NotNil(t, defaultOptions.connContext)
+	defaultOptions.connContext(context.TODO(), dummyConn)
+
+	invoked := false
+	customConnContextFn := func(ctx context.Context, conn net.Conn) context.Context { invoked = true; return ctx }
+	customOptions := newServerOptions(WithConnContext(customConnContextFn))
+	require.NotNil(t, customOptions.connContext)
+	customOptions.connContext(context.TODO(), dummyConn)
+	require.True(t, invoked)
+}
+
+func TestWithLog(t *testing.T) {
+	defaultOptions := newServerOptions()
+	require.NotNil(t, defaultOptions.log)
+	defaultOptions.log("test")
+
+	invoked := false
+	customLogFn := func(format string, args ...any) { invoked = true }
+	customOptions := newServerOptions(WithLog(customLogFn))
+	require.NotNil(t, customOptions.log)
+	customOptions.log("test")
+	require.True(t, invoked)
+}
+
+func TestWithServerStats(t *testing.T) {
+	defaultOptions := newServerOptions()
+	require.NotNil(t, defaultOptions.serverStats)
+
+	customServerStats := stats.NewServerStats(stats.NewTimingConfig(60), 60)
+	customOptions := newServerOptions(WithServerStats(customServerStats))
+	require.Equal(t, customServerStats, customOptions.serverStats)
+}
+
+func TestWithProcessorStats(t *testing.T) {
+	defaultOptions := newServerOptions()
+	require.NotNil(t, defaultOptions.processorStats)
+	require.Len(t, defaultOptions.processorStats, 0)
+
+	customProcessorStats := map[string]*stats.TimingSeries{"test": {}}
+	customOptions := newServerOptions(WithProcessorStats(customProcessorStats))
+	require.Equal(t, customProcessorStats, customOptions.processorStats)
 }

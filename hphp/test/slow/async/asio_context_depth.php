3efb4e068fc4d5733c6b3769cfe49975b41236d6
Jeffrey Warren <jtwarren@meta.com>
2025-04-16 17:00:57 -0700

    Drop HH namespace from asio context_depth funcs
    Summary: As titled.
    
    Reviewed By: jano
    
    Differential Revision: D73076336
    
    fbshipit-source-id: 03a46de4fff9b2a62260016ccbac16cd5de21a65

--- hphp/test/slow/async/asio_context_depth.php
+++ hphp/test/slow/async/asio_context_depth.php
@@ -2,19 +2,19 @@
 
 async function bar(): Awaitable<void> {
   await RescheduleWaitHandle::create(RescheduleWaitHandle::QUEUE_DEFAULT, 0);
-  echo(HH\asio_get_current_context_depth()."\n"); // 2
+  echo(asio_get_current_context_depth()."\n"); // 2
 }
 
 async function foo(): Awaitable<void> {
   await RescheduleWaitHandle::create(RescheduleWaitHandle::QUEUE_DEFAULT, 0);
-  echo(HH\asio_get_current_context_depth()."\n"); // 1
+  echo(asio_get_current_context_depth()."\n"); // 1
   HH\Asio\join(bar());
-  echo(HH\asio_get_current_context_depth()."\n"); // 1
+  echo(asio_get_current_context_depth()."\n"); // 1
 }
 
 <<__EntryPoint>>
 function main(): void {
-  echo(HH\asio_get_current_context_depth()."\n"); // 0
+  echo(asio_get_current_context_depth()."\n"); // 0
   HH\Asio\join(foo());
-  echo(HH\asio_get_current_context_depth()."\n"); // 0
+  echo(asio_get_current_context_depth()."\n"); // 0
 }

11f8711b19f5ae0b96bcf50b7dbb5344e7de7cc6
Fred Qiu <qiu@meta.com>
2025-04-30 12:31:04 -0700

    Add copyright info
    Summary: Add copyright info for files mentioned in P1793756698
    
    Differential Revision: D73926385
    
    fbshipit-source-id: 5e17f4c0a3df735bf2f36f9c69c7ee64f4f0addc

--- third-party/fizz/src/fizz/backend/libaegis/LibAEGIS.h
+++ third-party/fizz/src/fizz/backend/libaegis/LibAEGIS.h
@@ -1,3 +1,11 @@
+/*
+ *  Copyright (c) Meta Platforms, Inc. and affiliates.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree.
+ */
+
 #pragma once
 
 #include <fizz/crypto/Crypto.h>
--- third-party/fizz/src/fizz/extensions/clientpadding/PaddingClientExtension.cpp
+++ third-party/fizz/src/fizz/extensions/clientpadding/PaddingClientExtension.cpp
@@ -1,4 +1,10 @@
-// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+/*
+ *  Copyright (c) Meta Platforms, Inc. and affiliates.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree.
+ */
 
 #include <fizz/extensions/clientpadding/PaddingClientExtension.h>
 #include <fizz/extensions/clientpadding/Types.h>

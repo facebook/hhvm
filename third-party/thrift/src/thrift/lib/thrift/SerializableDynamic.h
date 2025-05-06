6cff28942452d995678b566d142757342da071bf
David Devecsery <ddevec@meta.com>
2025-05-06 16:48:07 -0700

    Remove unneeded copies from `SerializableDynamic` serilization.
    Summary:
    These functions previously used `asString` instead of `getString`, despite
    checking the type of the dynamic is `Type::STRING`.  The `as` methods must
    return a copy (as they will create a new string on-type-conversion), but the
    copy here is unneeded.
    
    Reviewed By: iahs
    
    Differential Revision: D74263988
    
    fbshipit-source-id: e0857d54e0aca3eb0feb2e23d475377633f23dac

--- third-party/thrift/src/thrift/lib/thrift/SerializableDynamic.h
+++ third-party/thrift/src/thrift/lib/thrift/SerializableDynamic.h
@@ -191,7 +191,7 @@ class SerializableDynamic {
 
       case folly::dynamic::Type::STRING:
         xfer += p->writeFieldBegin("str", protocol::T_STRING, 4);
-        xfer += p->writeString(value_.asString());
+        xfer += p->writeString(value_.getString());
         xfer += p->writeFieldEnd();
         break;
 
@@ -273,7 +273,7 @@ class Cpp2Ops<SerializableDynamic> {
 
       case folly::dynamic::Type::STRING:
         xfer += p->serializedFieldSize("str", protocol::T_STRING, 4);
-        xfer += p->serializedSizeString(obj->value_.asString());
+        xfer += p->serializedSizeString(obj->value_.getString());
         break;
 
       case folly::dynamic::Type::ARRAY:

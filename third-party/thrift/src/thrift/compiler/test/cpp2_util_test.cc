8d7bcc7db5ed7385e3c558883a0369769e3166b1
TJ Yin <ytj@meta.com>
2025-05-09 11:47:02 -0700

    Add unit-test for orderability
    Summary:
    D74267193's diff summary described when `operator<` is defined. This diff added unit-test for it.
    
    The main reason is that I want to make sure the codemod program (next diff) considered most (if not all) corner cases.
    
    Reviewed By: thedavekwon
    
    Differential Revision: D74364242
    
    fbshipit-source-id: c517940a033fb1335694a15fa8f87af491e67445

--- third-party/thrift/src/thrift/compiler/test/cpp2_util_test.cc
+++ third-party/thrift/src/thrift/compiler/test/cpp2_util_test.cc
@@ -42,6 +42,7 @@
 #include <thrift/compiler/test/parser_test_helpers.h>
 
 namespace apache::thrift::compiler {
+using cpp2::is_orderable;
 namespace {
 
 class UtilTest : public ::testing::Test {};
@@ -52,21 +53,21 @@ TEST_F(UtilTest, is_orderable_set_template) {
   t_program p("path/to/program.thrift", "path/to/program.thrift");
   t_struct s(&p, "struct_name");
   s.append(std::make_unique<t_field>(&t, "set_field", 1));
-  EXPECT_FALSE(cpp2::is_orderable(t, true));
-  EXPECT_FALSE(cpp2::is_orderable(s, true));
+  EXPECT_FALSE(is_orderable(t, true));
+  EXPECT_FALSE(is_orderable(s, true));
 
   s.set_uri("facebook.com/path/to/struct_name");
-  EXPECT_TRUE(cpp2::is_orderable(s, true));
-  EXPECT_FALSE(cpp2::is_orderable(s, false));
+  EXPECT_TRUE(is_orderable(s, true));
+  EXPECT_FALSE(is_orderable(s, false));
 
   s.set_uri(""); // remove uri
-  EXPECT_FALSE(cpp2::is_orderable(s, true));
-  EXPECT_FALSE(cpp2::is_orderable(s, false));
+  EXPECT_FALSE(is_orderable(s, true));
+  EXPECT_FALSE(is_orderable(s, false));
 
   auto builder = gen::cpp_annotation_builder::EnableCustomTypeOrdering(p);
   s.add_structured_annotation(builder.make());
-  EXPECT_TRUE(cpp2::is_orderable(s, true));
-  EXPECT_TRUE(cpp2::is_orderable(s, false));
+  EXPECT_TRUE(is_orderable(s, true));
+  EXPECT_TRUE(is_orderable(s, false));
 }
 
 TEST_F(UtilTest, is_orderable_struct) {
@@ -74,7 +75,7 @@ TEST_F(UtilTest, is_orderable_struct) {
   t_struct s(&p, "struct_name");
   s.append(std::make_unique<t_field>(
       &t_primitive_type::t_string(), "field_name", 1));
-  EXPECT_TRUE(cpp2::is_orderable(s, true));
+  EXPECT_TRUE(is_orderable(s, true));
 }
 
 TEST_F(UtilTest, is_orderable_struct_self_reference) {
@@ -85,7 +86,7 @@ TEST_F(UtilTest, is_orderable_struct_self_reference) {
 
   t_struct c(&p, "C");
   c.append(std::make_unique<t_field>(&t, "set_field", 1));
-  EXPECT_FALSE(cpp2::is_orderable(c, true));
+  EXPECT_FALSE(is_orderable(c, true));
 
   t_struct b(&p, "B");
   t_struct a(&p, "A");
@@ -94,13 +95,13 @@ TEST_F(UtilTest, is_orderable_struct_self_reference) {
   a.append(std::make_unique<t_field>(&b, "b", 1));
   a.append(std::make_unique<t_field>(&c, "c", 2));
 
-  EXPECT_FALSE(cpp2::is_orderable(a, true));
-  EXPECT_FALSE(cpp2::is_orderable(b, true));
+  EXPECT_FALSE(is_orderable(a, true));
+  EXPECT_FALSE(is_orderable(b, true));
 
   std::unordered_map<t_type const*, bool> memo;
-  EXPECT_FALSE(cpp2::is_orderable(memo, a, true));
-  EXPECT_FALSE(cpp2::is_orderable(memo, b, true));
-  EXPECT_FALSE(cpp2::is_orderable(memo, c, true));
+  EXPECT_FALSE(is_orderable(memo, a, true));
+  EXPECT_FALSE(is_orderable(memo, b, true));
+  EXPECT_FALSE(is_orderable(memo, c, true));
 }
 
 TEST_F(UtilTest, is_eligible_for_constexpr) {
@@ -604,6 +605,185 @@ TEST_F(UtilTest, structs_and_typedefs_dependency_graph) {
       FAIL() << "Wrong graph node: " << name;
     }
   }
+}
+
+const std::string kHeaderNoUri = R"(
+include "thrift/annotation/cpp.thrift"
+include "thrift/annotation/thrift.thrift"
+)";
+
+const std::string kHeaderWithUri =
+    kHeaderNoUri + "package \"apache.org/thrift/test\"\n";
+
+const std::string kOrderabilityTestProgram = R"(
+// There are 3 categories:
+// * Foo*: With custom set/map
+// * Bar*: Without custom set/map
+// * Baz*: Special cases
+
+@cpp.Type{template = "std::unordered_set"}
+typedef set<i32> CustomSet1;
+@cpp.Type{name = "std::unordered_set<int32_t>"}
+typedef set<i32> CustomSet2;
+@cpp.Adapter{name = "::apache::thrift::test::TemplatedTestAdapter"}
+typedef set<i32> CustomSet3;
+typedef set<i32> CustomSet4 (cpp.template = "std::unordered_set");
+typedef set<i32> CustomSet5 (cpp.type = "std::unordered_set<int32_t>");
+
+struct Foo1 { 1: CustomSet1 foo; }
+struct Foo2 { 1: CustomSet2 foo; }
+struct Foo3 { 1: CustomSet3 foo; }
+struct Foo4 { 1: CustomSet4 foo; }
+struct Foo5 { 1: CustomSet5 foo; }
+struct Foo6 { 1: list<CustomSet1> foo; }
+struct Foo7 {
+  @cpp.Type{template = "std::unordered_set"}
+  1: set<i32> foo;
+}
+struct Foo8 {
+  @cpp.Type{template = "std::unordered_map"}
+  1: map<i32, i32> foo;
+}
+struct Foo9 {
+  1: i32 field1;
+  2: CustomSet1 field2;
+}
+struct Foo10 {
+  @cpp.Ref{type = cpp.RefType.Unique}
+  1: optional CustomSet1 foo;
+}
+struct Foo11 {
+  @thrift.Box
+  1: optional CustomSet1 foo;
+}
+
+@cpp.Type{template = "std::deque"}
+typedef list<i32> CustomList1
+
+struct Bar1 { 1: set<i32> foo; }
+struct Bar2 { 1: CustomList1 foo; }
+struct Bar3 { 1: Foo1 foo; }
+struct Bar4 { 1: map<i32, Foo4> foo; }
+
+// Weirdly field adapter doesn't count as custom type, probably a bug.
+struct Baz1 {
+  @cpp.Adapter{name = "::apache::thrift::test::TemplatedTestAdapter"}
+  1: set<i32> baz;
+}
+
+// Always unorderable due to the lack of URI
+@thrift.Uri{value = ""}
+struct Baz2 {
+  @cpp.Type{template = "std::unordered_set"}
+  1: set<i32> foo;
+}
+
+// Has URI, but still unorderable since it contains unorderable field.
+@thrift.Uri{value = "apache.org/thrift/Baz3"}
+struct Baz3 {
+  1: Baz2 foo;
+}
+
+// Already enabled custom type ordering
+@cpp.EnableCustomTypeOrdering
+struct Baz4 {
+  @cpp.Type{template = "std::unordered_set"}
+  1: set<i32> foo;
+}
+)";
+
+const t_structured& get(const t_program& p, std::string_view name) {
+  for (auto i : p.structured_definitions()) {
+    if (i->name() == name) {
+      return *i;
+    }
+  }
+  throw std::logic_error("Foo not found");
+}
+
+TEST_F(UtilTest, CustomSetOrderabilityWithoutUri) {
+  static auto source_mgr = source_manager();
+  auto program = dedent_and_parse_to_program(
+      source_mgr, kHeaderNoUri + kOrderabilityTestProgram);
+
+  EXPECT_FALSE(is_orderable(get(*program, "Foo1"), true));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo2"), true));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo3"), true));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo4"), true));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo5"), true));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo6"), true));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo7"), true));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo8"), true));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo9"), true));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo10"), true));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo11"), true));
+
+  EXPECT_TRUE(is_orderable(get(*program, "Bar1"), true));
+  EXPECT_TRUE(is_orderable(get(*program, "Bar2"), true));
+  EXPECT_FALSE(is_orderable(get(*program, "Bar3"), true));
+  EXPECT_FALSE(is_orderable(get(*program, "Bar4"), true));
+
+  EXPECT_TRUE(is_orderable(get(*program, "Baz1"), true));
+  EXPECT_FALSE(is_orderable(get(*program, "Baz2"), true));
+  EXPECT_FALSE(is_orderable(get(*program, "Baz3"), true));
+  EXPECT_TRUE(is_orderable(get(*program, "Baz4"), true));
+}
+
+// The orderability should be the same as previous test case
+TEST_F(UtilTest, CustomSetOrderabilityWithUriButPreservedOldBehavior) {
+  static auto source_mgr = source_manager();
+  auto program = dedent_and_parse_to_program(
+      source_mgr, kHeaderWithUri + kOrderabilityTestProgram);
+
+  EXPECT_FALSE(is_orderable(get(*program, "Foo1"), false));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo2"), false));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo3"), false));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo4"), false));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo5"), false));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo6"), false));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo7"), false));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo8"), false));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo9"), false));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo10"), false));
+  EXPECT_FALSE(is_orderable(get(*program, "Foo11"), false));
+
+  EXPECT_TRUE(is_orderable(get(*program, "Bar1"), false));
+  EXPECT_TRUE(is_orderable(get(*program, "Bar2"), false));
+  EXPECT_FALSE(is_orderable(get(*program, "Bar3"), false));
+  EXPECT_FALSE(is_orderable(get(*program, "Bar4"), false));
+
+  EXPECT_TRUE(is_orderable(get(*program, "Baz1"), false));
+  EXPECT_FALSE(is_orderable(get(*program, "Baz2"), false));
+  EXPECT_FALSE(is_orderable(get(*program, "Baz3"), false));
+  EXPECT_TRUE(is_orderable(get(*program, "Baz4"), false));
+}
+
+TEST_F(UtilTest, CustomSetOrderabilityWithUri) {
+  static auto source_mgr = source_manager();
+  auto program = dedent_and_parse_to_program(
+      source_mgr, kHeaderWithUri + kOrderabilityTestProgram);
+
+  EXPECT_TRUE(is_orderable(get(*program, "Foo1"), true));
+  EXPECT_TRUE(is_orderable(get(*program, "Foo2"), true));
+  EXPECT_TRUE(is_orderable(get(*program, "Foo3"), true));
+  EXPECT_TRUE(is_orderable(get(*program, "Foo4"), true));
+  EXPECT_TRUE(is_orderable(get(*program, "Foo5"), true));
+  EXPECT_TRUE(is_orderable(get(*program, "Foo6"), true));
+  EXPECT_TRUE(is_orderable(get(*program, "Foo7"), true));
+  EXPECT_TRUE(is_orderable(get(*program, "Foo8"), true));
+  EXPECT_TRUE(is_orderable(get(*program, "Foo9"), true));
+  EXPECT_TRUE(is_orderable(get(*program, "Foo10"), true));
+  EXPECT_TRUE(is_orderable(get(*program, "Foo11"), true));
+
+  EXPECT_TRUE(is_orderable(get(*program, "Bar1"), true));
+  EXPECT_TRUE(is_orderable(get(*program, "Bar2"), true));
+  EXPECT_TRUE(is_orderable(get(*program, "Bar3"), true));
+  EXPECT_TRUE(is_orderable(get(*program, "Bar4"), true));
+
+  EXPECT_TRUE(is_orderable(get(*program, "Baz1"), true));
+  EXPECT_FALSE(is_orderable(get(*program, "Baz2"), true));
+  EXPECT_FALSE(is_orderable(get(*program, "Baz3"), true));
+  EXPECT_TRUE(is_orderable(get(*program, "Baz4"), true));
 }
 
 } // namespace

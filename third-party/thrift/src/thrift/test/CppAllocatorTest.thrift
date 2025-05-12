8482a411a85e408eaa20fff837ee2aabe11b7146
Dave Kwon <dokwon@meta.com>
2025-05-12 16:04:25 -0700

    Run structured annot codemod in CppAllocatorTest.thrift
    Summary: codemod
    
    Reviewed By: iahs
    
    Differential Revision: D74592530
    
    fbshipit-source-id: e5438f6cc57851d65d8b069e5fe0ccea1a9e9018

--- third-party/thrift/src/thrift/test/CppAllocatorTest.thrift
+++ third-party/thrift/src/thrift/test/CppAllocatorTest.thrift
@@ -21,6 +21,9 @@ namespace cpp2 apache.thrift.test
 
 cpp_include "thrift/test/CppAllocatorTest.h"
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::ScopedAlwaysThrowAlloc<>"},
+}
 struct AlwaysThrowChild {
   1: list_i32_1528 aa_list;
   2: set_i32_2716 aa_set;
@@ -31,12 +34,18 @@ struct AlwaysThrowChild {
   7: set<i32> not_aa_set;
   8: map<i32, i32> not_aa_map;
   9: string not_aa_string;
-} (cpp.allocator = "::ScopedAlwaysThrowAlloc<>")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::ScopedAlwaysThrowAlloc<>"},
+}
 struct AlwaysThrowParent {
   1: AlwaysThrowChild_4275 child;
-} (cpp.allocator = "::ScopedAlwaysThrowAlloc<>")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "PmrByteAlloc"},
+}
 struct ChildPmr {
   1: list_i32_8699 aa_list;
   2: set_i32_4098 aa_set;
@@ -47,99 +56,158 @@ struct ChildPmr {
   7: set<i32> not_aa_set;
   8: map<i32, i32> not_aa_map;
   9: string not_aa_string;
-} (cpp.allocator = "PmrByteAlloc")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "PmrByteAlloc"},
+}
 struct ParentPmr {
   1: ChildPmr_7470 child;
   2: list_ChildPmr_9632 aa_child_list;
   3: list<ChildPmr> not_aa_child_list;
-} (cpp.allocator = "PmrByteAlloc")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::ScopedStatefulAlloc<>"},
+}
 struct AAStruct {
   1: i32 foo;
-} (cpp.allocator = "::ScopedStatefulAlloc<>")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::ScopedStatefulAlloc<>"},
+}
 struct NoAllocatorVia {
   1: AAStruct_7495 foo;
-} (cpp.allocator = "::ScopedStatefulAlloc<>")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {
+    "cpp.allocator": "::ScopedStatefulAlloc<>",
+    "cpp.allocator_via": "bar",
+  },
+}
 struct YesAllocatorVia {
   @cpp.Name{value = "bar"}
   1: AAStruct_7495 foo;
-} (cpp.allocator = "::ScopedStatefulAlloc<>", cpp.allocator_via = "bar")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "PmrByteAlloc"},
+}
 struct AAStructPmr {
   1: i32 foo;
-} (cpp.allocator = "PmrByteAlloc")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "PmrByteAlloc"},
+}
 struct NoAllocatorViaPmr {
   1: AAStructPmr_1852 foo;
-} (cpp.allocator = "PmrByteAlloc")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "PmrByteAlloc", "cpp.allocator_via": "bar"},
+}
 struct YesAllocatorViaPmr {
   @cpp.Name{value = "bar"}
   1: AAStructPmr_1852 foo;
-} (cpp.allocator = "PmrByteAlloc", cpp.allocator_via = "bar")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::ScopedStatefulAlloc<>"},
+}
 struct HasContainerFields {
   1: list_i32_4073 aa_list;
   2: set_i32_8876 aa_set;
   3: map_i32_i32_3938 aa_map;
-} (cpp.allocator = "::ScopedStatefulAlloc<>")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "PmrByteAlloc"},
+}
 struct HasContainerFieldsPmr {
   1: list_i32_8699 aa_list;
   2: set_i32_4098 aa_set;
   3: map_i32_i32_7355 aa_map;
-} (cpp.allocator = "PmrByteAlloc")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "::StatefulAllocMap"}
-typedef map<i32, i32> (cpp.use_allocator) StatefulAllocIntMap
+typedef map<i32, i32> StatefulAllocIntMap
 
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "::StatefulAllocMap"}
-typedef map<i32, StatefulAllocIntMap> (cpp.use_allocator) StatefulAllocMapMap
+typedef map<i32, StatefulAllocIntMap> StatefulAllocMapMap
 
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "::StatefulAllocSet"}
-typedef set<i32> (cpp.use_allocator) StatefulAllocIntSet
+typedef set<i32> StatefulAllocIntSet
 
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "::StatefulAllocMap"}
-typedef map<i32, StatefulAllocIntSet> (cpp.use_allocator) StatefulAllocMapSet
+typedef map<i32, StatefulAllocIntSet> StatefulAllocMapSet
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {
+    "cpp.allocator": "::ScopedStatefulAlloc<>",
+    "cpp.allocator_via": "aa_map",
+  },
+}
 struct UsesTypedef {
   1: StatefulAllocIntMap aa_map;
-} (cpp.allocator = "::ScopedStatefulAlloc<>", cpp.allocator_via = "aa_map")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::ScopedStatefulAlloc<>"},
+}
 struct HasNestedContainerFields {
   1: StatefulAllocMapMap aa_map_of_map;
   2: StatefulAllocMapSet aa_map_of_set;
-} (cpp.allocator = "::ScopedStatefulAlloc<>")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "std::pmr::map"}
-typedef map<i32, i32> (cpp.use_allocator) PmrIntMap
+typedef map<i32, i32> PmrIntMap
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "PmrByteAlloc", "cpp.allocator_via": "aa_map"},
+}
 struct UsesTypedefPmr {
   1: PmrIntMap aa_map;
-} (cpp.allocator = "PmrByteAlloc", cpp.allocator_via = "aa_map")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "std::pmr::map"}
-typedef map<i32, PmrIntMap> (cpp.use_allocator) PmrMapMap
+typedef map<i32, PmrIntMap> PmrMapMap
 
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "std::pmr::set"}
-typedef set<i32> (cpp.use_allocator) PmrIntSet
+typedef set<i32> PmrIntSet
 
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "std::pmr::map"}
-typedef map<i32, PmrIntSet> (cpp.use_allocator) PmrMapSet
+typedef map<i32, PmrIntSet> PmrMapSet
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "PmrByteAlloc"},
+}
 struct HasNestedContainerFieldsPmr {
   1: PmrMapMap aa_map_of_map;
   2: PmrMapSet aa_map_of_set;
-} (cpp.allocator = "PmrByteAlloc")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::ScopedStatefulAlloc<>"},
+}
 struct HasSortedUniqueConstructibleFields {
   1: set_i32_992 aa_set;
   2: map_i32_i32_4068 aa_map;
-} (cpp.allocator = "::ScopedStatefulAlloc<>")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::ScopedCountingAlloc<>"},
+}
 struct CountingChild {
   1: list_i32_8474 aa_list;
   2: set_i32_9206 aa_set;
@@ -150,62 +218,101 @@ struct CountingChild {
   7: set<i32> not_aa_set;
   8: map<i32, i32> not_aa_map;
   9: string not_aa_string;
-} (cpp.allocator = "::ScopedCountingAlloc<>")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::ScopedCountingAlloc<>"},
+}
 struct CountingParent {
   1: list_CountingChild_2391 aa_child_list;
   2: list<CountingChild> not_aa_child_list;
-} (cpp.allocator = "::ScopedCountingAlloc<>")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::PropagateAllAlloc"},
+}
 struct PropagateAllAllocStruct {
   1: i32 foo;
-} (cpp.allocator = "::PropagateAllAlloc")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::PropagateNoneAlloc"},
+}
 struct PropagateNoneAllocStruct {
   1: i32 foo;
-} (cpp.allocator = "::PropagateNoneAlloc")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::PropagateOnlyCopyAlloc"},
+}
 struct PropagateOnlyCopyAllocStruct {
   1: i32 foo;
-} (cpp.allocator = "::PropagateOnlyCopyAlloc")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::PropagateOnlyMoveAlloc"},
+}
 struct PropagateOnlyMoveAllocStruct {
   1: i32 foo;
-} (cpp.allocator = "::PropagateOnlyMoveAlloc")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::PropagateOnlySwapAlloc"},
+}
 struct PropagateOnlySwapAllocStruct {
   1: i32 foo;
-} (cpp.allocator = "::PropagateOnlySwapAlloc")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::PropagateCopyMoveAlloc"},
+}
 struct PropagateCopyMoveAllocStruct {
   1: i32 foo;
-} (cpp.allocator = "::PropagateCopyMoveAlloc")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::PropagateCopySwapAlloc"},
+}
 struct PropagateCopySwapAllocStruct {
   1: i32 foo;
-} (cpp.allocator = "::PropagateCopySwapAlloc")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::PropagateMoveSwapAlloc"},
+}
 struct PropagateMoveSwapAllocStruct {
   1: i32 foo;
-} (cpp.allocator = "::PropagateMoveSwapAlloc")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::ScopedAlwaysThrowAlloc<>"},
+}
 struct AlwaysThrowCppRefChild {
   1: i32 value1;
   2: i64 value2;
-} (cpp.allocator = "::ScopedAlwaysThrowAlloc<>")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::ScopedAlwaysThrowAlloc<>"},
+}
 struct AlwaysThrowCppRefParent {
   @cpp.Ref{type = cpp.RefType.SharedMutable}
   @cpp.AllowLegacyNonOptionalRef
   2: AlwaysThrowCppRefChild_9703 sharedChild;
   3: i32 no_alloc;
-} (cpp.allocator = "::ScopedAlwaysThrowAlloc<>")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::ScopedCountingAlloc<>"},
+}
 struct CountingCppRefChild {
   1: i32 value1;
   2: i64 value2;
-} (cpp.allocator = "::ScopedCountingAlloc<>")
+}
 
+@thrift.DeprecatedUnvalidatedAnnotations{
+  items = {"cpp.allocator": "::ScopedCountingAlloc<>"},
+}
 struct CountingCppRefParent {
   @cpp.Ref{type = cpp.RefType.SharedMutable}
   @cpp.AllowLegacyNonOptionalRef
@@ -222,52 +329,75 @@ struct CountingCppRefParent {
   6: list_i32_8474 allocVector;
   @thrift.Box
   7: optional CountingCppRefChild noAllocBoxedChild;
-} (cpp.allocator = "::ScopedCountingAlloc<>")
+}
 
 // The following were automatically generated and may benefit from renaming.
-typedef AAStructPmr (cpp.use_allocator = "1") AAStructPmr_1852
-typedef AAStruct (cpp.use_allocator = "1") AAStruct_7495
-typedef AlwaysThrowChild (cpp.use_allocator = "1") AlwaysThrowChild_4275
-typedef AlwaysThrowCppRefChild (
-  cpp.use_allocator = "1",
-) AlwaysThrowCppRefChild_9703
-typedef ChildPmr (cpp.use_allocator = "1") ChildPmr_7470
-typedef CountingCppRefChild (cpp.use_allocator = "1") CountingCppRefChild_4892
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
+typedef AAStructPmr AAStructPmr_1852
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
+typedef AAStruct AAStruct_7495
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
+typedef AlwaysThrowChild AlwaysThrowChild_4275
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
+typedef AlwaysThrowCppRefChild AlwaysThrowCppRefChild_9703
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
+typedef ChildPmr ChildPmr_7470
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
+typedef CountingCppRefChild CountingCppRefChild_4892
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "std::pmr::vector"}
-typedef list<ChildPmr> (cpp.use_allocator = "1") list_ChildPmr_9632
+typedef list<ChildPmr> list_ChildPmr_9632
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "::CountingVector"}
-typedef list<CountingChild> (cpp.use_allocator = "1") list_CountingChild_2391
+typedef list<CountingChild> list_CountingChild_2391
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "::AlwaysThrowVector"}
-typedef list<i32> (cpp.use_allocator = "1") list_i32_1528
+typedef list<i32> list_i32_1528
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "::StatefulAllocVector"}
-typedef list<i32> (cpp.use_allocator = "1") list_i32_4073
+typedef list<i32> list_i32_4073
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "::CountingVector"}
-typedef list<i32> (cpp.use_allocator = "1") list_i32_8474
+typedef list<i32> list_i32_8474
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "std::pmr::vector"}
-typedef list<i32> (cpp.use_allocator = "1") list_i32_8699
+typedef list<i32> list_i32_8699
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "::CountingMap"}
-typedef map<i32, i32> (cpp.use_allocator = "1") map_i32_i32_1496
+typedef map<i32, i32> map_i32_i32_1496
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "::StatefulAllocMap"}
-typedef map<i32, i32> (cpp.use_allocator = "1") map_i32_i32_3938
+typedef map<i32, i32> map_i32_i32_3938
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "::StatefulAllocSortedVectorMap"}
-typedef map<i32, i32> (cpp.use_allocator = "1") map_i32_i32_4068
+typedef map<i32, i32> map_i32_i32_4068
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "::AlwaysThrowMap"}
-typedef map<i32, i32> (cpp.use_allocator = "1") map_i32_i32_4816
+typedef map<i32, i32> map_i32_i32_4816
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "std::pmr::map"}
-typedef map<i32, i32> (cpp.use_allocator = "1") map_i32_i32_7355
+typedef map<i32, i32> map_i32_i32_7355
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "::AlwaysThrowSet"}
-typedef set<i32> (cpp.use_allocator = "1") set_i32_2716
+typedef set<i32> set_i32_2716
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "std::pmr::set"}
-typedef set<i32> (cpp.use_allocator = "1") set_i32_4098
+typedef set<i32> set_i32_4098
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "::StatefulAllocSet"}
-typedef set<i32> (cpp.use_allocator = "1") set_i32_8876
+typedef set<i32> set_i32_8876
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "::CountingSet"}
-typedef set<i32> (cpp.use_allocator = "1") set_i32_9206
+typedef set<i32> set_i32_9206
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{template = "::StatefulAllocSortedVectorSet"}
-typedef set<i32> (cpp.use_allocator = "1") set_i32_992
+typedef set<i32> set_i32_992
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{name = "::CountingString"}
-typedef string (cpp.use_allocator = "1") string_186
+typedef string string_186
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{name = "::AlwaysThrowString"}
-typedef string (cpp.use_allocator = "1") string_4597
+typedef string string_4597
+@thrift.DeprecatedUnvalidatedAnnotations{items = {"cpp.use_allocator": "1"}}
 @cpp.Type{name = "std::pmr::string"}
-typedef string (cpp.use_allocator = "1") string_8090
+typedef string string_8090

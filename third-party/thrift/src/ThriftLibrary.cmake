1db3663cce381665b3016e3afe09a2c314b139da
TJ Yin <ytj@meta.com>
2025-05-16 11:38:03 -0700

    Migrate OSS project to handle protocol split
    Summary: fbthrift has moved compact/binary protocol instantiation outside _types.cpp (to _types_compact.cpp and _types_binary.cpp). We need to add these two files to the build system.
    
    Reviewed By: vitaut
    
    Differential Revision: D74850941
    
    fbshipit-source-id: 2965fb24d3a082b138550c02776dd4555afd71c3

--- third-party/thrift/src/ThriftLibrary.cmake
+++ third-party/thrift/src/ThriftLibrary.cmake
@@ -229,6 +229,8 @@ macro(thrift_generate
     ${output_path}/gen-${language}/${source_file_name}_constants.cpp
     ${output_path}/gen-${language}/${source_file_name}_data.cpp
     ${output_path}/gen-${language}/${source_file_name}_types.cpp
+    ${output_path}/gen-${language}/${source_file_name}_types_compact.cpp
+    ${output_path}/gen-${language}/${source_file_name}_types_binary.cpp
   )
   if("${options}" MATCHES "layouts")
     set("${target_file_name}-${language}-SOURCES"

cf6052ee6ea523e4a768b94d909d0e401c6ca235
Jack Chistyakov <jchistyakov@meta.com>
2025-05-21 15:00:15 -0700

    Add rocket stream payload
    Summary:
    Stream payload wrapper class.
    Akin to `responsePayload`:
    https://www.internalfb.com/code/fbsource/[69280ce79725]/fbcode/thrift/lib/go/thrift/rocket/rocket_payload_response.go?lines=28
    
    Reviewed By: podtserkovskiy
    
    Differential Revision: D75160870
    
    fbshipit-source-id: 86298018f7a522c5acb700846b2e11654e61b7df

---
+++ third-party/thrift/src/thrift/lib/go/thrift/rocket/rocket_payload_stream.go
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package rocket
+
+import (
+	"fmt"
+
+	"github.com/facebook/fbthrift/thrift/lib/go/thrift/format"
+	"github.com/facebook/fbthrift/thrift/lib/go/thrift/types"
+	"github.com/facebook/fbthrift/thrift/lib/thrift/rpcmetadata"
+	"github.com/rsocket/rsocket-go/payload"
+)
+
+type streamPayload struct {
+	metadata  *rpcmetadata.StreamPayloadMetadata
+	exception *rocketException
+	data      []byte
+}
+
+func (r *streamPayload) Headers() map[string]string {
+	if r.metadata == nil {
+		return nil
+	}
+	return r.metadata.OtherMetadata
+}
+
+func (r *streamPayload) Data() []byte {
+	return r.data
+}
+
+func (r *streamPayload) Error() error {
+	if r.exception != nil && !r.exception.IsDeclared() {
+		return r.exception
+	}
+	return nil
+}
+
+// DecodeStreamPayload decodes a stream payload.
+func DecodeStreamPayload(msg payload.Payload) (*streamPayload, error) {
+	msg = payload.Clone(msg)
+	if msg == nil {
+		return &streamPayload{metadata: &rpcmetadata.StreamPayloadMetadata{}, data: []byte{}}, nil
+	}
+	res := &streamPayload{metadata: &rpcmetadata.StreamPayloadMetadata{}, data: msg.Data()}
+	var err error
+	metadataBytes, ok := msg.Metadata()
+	if ok {
+		if err := format.DecodeCompact(metadataBytes, res.metadata); err != nil {
+			return nil, err
+		}
+		res.data, err = maybeDecompress(res.data, res.metadata.GetCompression())
+		if err != nil {
+			return nil, fmt.Errorf("stream payload decompression failed: %w", err)
+		}
+		if res.metadata.PayloadMetadata != nil && res.metadata.PayloadMetadata.ExceptionMetadata != nil {
+			res.exception = newRocketException(res.metadata.PayloadMetadata.ExceptionMetadata)
+		}
+	}
+	return res, res.Error()
+}
+
+// EncodeStreamPayload encodes a stream payload.
+func EncodeStreamPayload(
+	name string,
+	messageType types.MessageType,
+	headers map[string]string,
+	compression rpcmetadata.CompressionAlgorithm,
+	dataBytes []byte,
+) (payload.Payload, error) {
+	metadata := rpcmetadata.NewStreamPayloadMetadata().
+		SetOtherMetadata(headers).
+		SetCompression(&compression)
+
+	if messageType == types.EXCEPTION {
+		excpetionMetadata := newUnknownPayloadExceptionMetadataBase(name, string(dataBytes))
+		metadata.SetPayloadMetadata(rpcmetadata.NewPayloadMetadata().SetExceptionMetadata(excpetionMetadata))
+	}
+	metadataBytes, err := format.EncodeCompact(metadata)
+	if err != nil {
+		return nil, err
+	}
+	if messageType == types.EXCEPTION {
+		return payload.New(nil, metadataBytes), nil
+	}
+
+	dataBytes, err = maybeCompress(dataBytes, compression)
+	if err != nil {
+		return nil, err
+	}
+
+	pay := payload.New(dataBytes, metadataBytes)
+	return pay, nil
+}

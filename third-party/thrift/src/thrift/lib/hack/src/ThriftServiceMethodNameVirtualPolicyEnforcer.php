615275ac58bafc394ccfab9b099dab0375cfe662
Zhengyu Duan <duan@meta.com>
2025-06-12 13:09:05 -0700

    setup thrift shadow testing
    Summary:
    # What
    
    setup thrift shadow testing
    
    NOTE: need to log extra is_privacy_safe field, this is affecting external results
    
    Differential Revision: D76453322
    
    fbshipit-source-id: bdbd54525866d0659852c94bf3f3fddeffb35b9d

--- third-party/thrift/src/thrift/lib/hack/src/ThriftServiceMethodNameVirtualPolicyEnforcer.php
+++ third-party/thrift/src/thrift/lib/hack/src/ThriftServiceMethodNameVirtualPolicyEnforcer.php
@@ -1,12 +1,26 @@
 <?hh
 // (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
 
+module privacylib;
+
 <<Oncalls('www_privacy_frameworks')>>
 final class ThriftServiceMethodNameVirtualPolicyEnforcer
   implements IPolicyEnforcerAtThrift {
   const type TPrivacyLib = ThriftServiceMethodNamePrivacyLib;
   const PolicyEnforcerAssetClass ASSET_CLASS =
     PolicyEnforcerAssetClass::THRIFT_CLIENT;
+
+  <<__Memoize>>
+  private static function getPrivacyLibObject(
+    ThriftServiceMethodNameAssetXID $asset_xid,
+    string $sr_config_service_name,
+  ): ThriftServiceMethodNamePrivacyLib {
+    $privacy_lib = ThriftServiceMethodNamePrivacyLib::get(
+      $asset_xid,
+      $sr_config_service_name,
+    );
+    return $privacy_lib;
+  }
 
   public static function getCallerName(
     string $asset_type, // This is actually the service name in this context
@@ -39,6 +53,14 @@ final class ThriftServiceMethodNameVirtualPolicyEnforcer
     return ThriftPolicyEnforcer::isConditionallyAllowed($caller, $asset_type);
   }
 
+  // TODO(duan): special usecase for this policyEnforcer
+  private static function isTrusted(
+    PolicyEnforcerCallerIdentity $caller,
+    string $asset_type,
+  )[zoned_shallow]: bool {
+    return ThriftPolicyEnforcer::isTrusted($caller, $asset_type);
+  }
+
   public static function extractEnforcableAssets(
     string $func_name,
     ?IThriftStruct $args,
@@ -69,6 +91,68 @@ final class ThriftServiceMethodNameVirtualPolicyEnforcer
       $old_exception = $e;
     }
 
+    if (!PrivacyLibKS::isKilled(PLKS::PAAL_CONSOLIDATION_THRIFT)) {
+      // Run shadow traffic to validate results
+      try {
+        list($result, $pl_failure) = await self::genPrivacyLibResults(
+          $asset_type,
+          $policy_enforcer_api,
+          $caller,
+          $context,
+        );
+
+        $old_allow = $old_result['allow'];
+        $new_allow = $result['allow'];
+
+        $old_safe = $old_result['is_privacy_safe'];
+        $new_safe = $result['is_privacy_safe'];
+
+        if ($old_allow != $new_allow) {
+          CategorizedOBC::typedBumpKey(
+            ODSCategoryID::ODS_PRIVACYLIB,
+            'www.privacylib.thrift.consolidation_mismatch.result',
+          );
+          PrivacyLibPaalConsolidationLogging::logResultMismatch(
+            PaalConsolidationAssetClassName::THRIFT,
+            $asset_type,
+            $policy_enforcer_api,
+            $caller,
+            $old_allow,
+            $new_allow,
+            $pl_failure,
+            $old_exception,
+          );
+        }
+
+        if ($old_safe != $new_safe) {
+          CategorizedOBC::typedBumpKey(
+            ODSCategoryID::ODS_PRIVACYLIB,
+            'www.privacylib.thrift.consolidation_mismatch.privacy_safe',
+          );
+          PrivacyLibPaalConsolidationLogging::logResultMismatch(
+            PaalConsolidationAssetClassName::THRIFT_PRIVACY_SAFE,
+            $asset_type,
+            $policy_enforcer_api,
+            $caller,
+            $old_safe,
+            $new_safe,
+          );
+        }
+      } catch (Exception $e) {
+        CategorizedOBC::typedBumpKey(
+          ODSCategoryID::ODS_PRIVACYLIB,
+          'www.privacylib.thrift.consolidation_exception',
+        );
+        PrivacyLibPaalConsolidationLogging::logEvaluationError(
+          PaalConsolidationAssetClassName::THRIFT,
+          $asset_type,
+          $policy_enforcer_api,
+          $caller,
+          $e,
+        );
+      }
+    }
+
     if ($old_exception is nonnull) {
       throw $old_exception;
     }
@@ -87,12 +171,62 @@ final class ThriftServiceMethodNameVirtualPolicyEnforcer
     return $old_result;
   }
 
+  <<StringMetadataExtractor>>
+  private static async function genPrivacyLibResults(
+    string $asset_type,
+    string $policy_enforcer_api,
+    PolicyEnforcerCallerIdentity $caller,
+    PolicyEnforcerContext $context,
+  )[zoned_local]: Awaitable<(TPolicyEnforcerResult, ?PrivacyLibModuleFailure)> {
+    HH\set_frame_metadata('PAALC_NEW:THRIFT');
+    // Thrift endpoint has this context set
+    $asset_xid = $context->get(#ThriftPolicyZones)
+      ?->getXID() ??
+      // DeletionFrameworkNodeServiceAsyncHandler do not have context set,
+      // but $api is the method name
+      ThriftServiceMethodNameAssetXID::unsafeGet(
+        $asset_type,
+        $policy_enforcer_api,
+      );
+
+    /**
+     * Incorrect sr_config_service_name will ONLY affect service with granular
+     * annotation. We are not expecting null $sr_config_service_name here.
+     * As a short-term solution, it will be default to ''. This ensure it
+     * has minimum blast radius regarding infra change.
+     */
+    $sr_config_service_name =
+      $context->get(#ThriftPolicyZones)?->getSmcServiceName() ?? '';
+
+    $privacy_lib =
+      self::getPrivacyLibObject($asset_xid, $sr_config_service_name);
+    $privacylib_failure = await $privacy_lib->genClientRPCWithTAE(
+      null,
+      $context,
+      $caller,
+      $policy_enforcer_api,
+    );
+
+    return tuple(
+      shape(
+        'allow' => $privacylib_failure is null,
+        'is_privacy_safe' => self::isTrusted($caller, $asset_type),
+        // Currently, Thrift is integrated with PrivacyLib for Zone2, which is not enforced yet globally.
+        'ran_enforcing_policy_zone_check' => false,
+        'logging_sampling_rate' => 0, // does not matter
+      ),
+      $privacylib_failure,
+    );
+  }
+
+  <<StringMetadataExtractor>>
   private static async function genPolicyEnforcerResults(
     string $asset_type,
     string $policy_enforcer_api,
     PolicyEnforcerCallerIdentity $caller,
     PolicyEnforcerContext $context,
   )[zoned_local]: Awaitable<TPolicyEnforcerResult> {
+    HH\set_frame_metadata('PAALC_OLD:THRIFT');
     // This may throw exceptions
     return await ThriftPolicyEnforcer::genEnforcePolicyEnforcerAndPolicyZone(
       $asset_type,

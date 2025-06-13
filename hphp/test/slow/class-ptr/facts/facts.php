1881ae300be9fedb5e06f9caebc6f4f673b320a8
Vassil Mladenov <vmladenov@meta.com>
2025-06-13 03:58:54 -0700

    Add native Facts API test showing class-to-string and vice versa
    Summary: The goal of this stack is to make ext_facts etc be less string-focused.
    
    Reviewed By: nt591
    
    Differential Revision: D76565975
    
    fbshipit-source-id: 128b8f60b9d89abfca515d8cee51a7b9b7d932b3

---
+++ hphp/test/slow/class-ptr/facts/facts.php
@@ -0,0 +1,27 @@
+<?hh
+
+class C { public function __construct() { echo nameof static."\n"; }}
+class D extends C {}
+class E extends C {}
+class F extends D {}
+
+function v($a): void {
+  foreach ($a as $c) {
+    new $c();
+  }
+}
+
+<<__EntryPoint>>
+function main(): void {
+  echo "===== subtypes =====\n";
+  $children = HH\Facts\subtypes(C::class);
+  v($children);
+
+  echo "===== transitive_subtypes =====\n";
+  $all_children = HH\Facts\transitive_subtypes(C::class);
+  v($all_children);
+
+  echo "===== supertypes =====\n";
+  $parents = HH\Facts\supertypes(F::class);
+  v($parents);
+}

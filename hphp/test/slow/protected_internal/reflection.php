326986690562fcab3eb793f309daf7b9843dd265
Ilya Zorin <izorin@meta.com>
2025-06-13 02:07:10 -0700

    Extend protected internal reflection tests to cover visibility as well
    Summary: Extend protected internal reflection tests to cover visibility as well
    
    Reviewed By: francesco-zappa-nardelli
    
    Differential Revision: D76427992
    
    fbshipit-source-id: 7a615ac3563929feca15755b51210e77128be8dc

--- hphp/test/slow/protected_internal/reflection.php
+++ hphp/test/slow/protected_internal/reflection.php
@@ -6,15 +6,44 @@ module A;
 class A {
   protected internal function foo(): void {}
   public function bar(): void {}
+  internal function baz(): void {}
 
-  protected internal int $baz;
-  public int $qux;
+  protected internal int $qux;
+  public int $quux;
+  internal int $corge;
 }
 
 <<__EntryPoint>>
 function main() {
-   var_dump((new ReflectionMethod('A::foo')->isInternalToModule()));
-   var_dump((new ReflectionMethod('A::bar')->isInternalToModule()));
-   var_dump((new ReflectionProperty('A', 'baz')->isInternalToModule()));
-   var_dump((new ReflectionProperty('A', 'qux')->isInternalToModule()));
+  echo "is A::foo internal: ";
+  var_dump((new ReflectionMethod('A::foo')->isInternalToModule()));
+  echo "is A::bar internal: ";
+  var_dump((new ReflectionMethod('A::bar')->isInternalToModule()));
+  echo "is A::baz internal: ";
+  var_dump((new ReflectionMethod('A::baz')->isInternalToModule()));
+
+  echo "is A::qux internal: ";
+  var_dump((new ReflectionProperty('A', 'qux')->isInternalToModule()));
+  echo "is A::quux internal: ";
+  var_dump((new ReflectionProperty('A', 'quux')->isInternalToModule()));
+  echo "is A::corge internal: ";
+  var_dump((new ReflectionProperty('A', 'corge')->isInternalToModule()));
+
+  echo "is A::foo protected: ";
+  var_dump((new ReflectionMethod('A::foo')->isProtected()));
+  echo "is A::foo public: ";
+  var_dump((new ReflectionMethod('A::foo')->isPublic()));
+  echo "is A::baz public: ";
+  var_dump((new ReflectionMethod('A::baz')->isPublic()));
+  echo "is A::baz protected: ";
+  var_dump((new ReflectionMethod('A::baz')->isProtected()));
+
+  echo "is A::qux protected: ";
+  var_dump((new ReflectionProperty('A', 'qux')->isProtected()));
+  echo "is A::qux public: ";
+  var_dump((new ReflectionProperty('A', 'qux')->isPublic()));
+  echo "is A::corge public: ";
+  var_dump((new ReflectionProperty('A', 'corge')->isPublic()));
+  echo "is A::corge protected: ";
+  var_dump((new ReflectionProperty('A', 'corge')->isProtected()));
 }

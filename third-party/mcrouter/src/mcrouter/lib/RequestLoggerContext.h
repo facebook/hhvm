dc954485886b9f297622edfd36d7dcf56619f92e
Ivan Topolcic <ivantopolcic@meta.com>
2025-06-25 17:10:48 -0700

    Add original size and chunk logging for BigValueRoute set requests
    Summary: This diff adds logging the original payload size as well as the number of chunks for big value requests that pass through big value route.
    
    Reviewed By: stuclar
    
    Differential Revision: D76844605
    
    fbshipit-source-id: 82cd202f05b696946c5021d7c9b3c060697d7e4f

--- third-party/mcrouter/src/mcrouter/lib/RequestLoggerContext.h
+++ third-party/mcrouter/src/mcrouter/lib/RequestLoggerContext.h
@@ -120,7 +120,8 @@ struct RequestLoggerContext {
       std::optional<int64_t> productId_ = std::nullopt,
       std::optional<int32_t> regionalizationEntity_ = std::nullopt,
       std::optional<int64_t> usecaseId_ = std::nullopt,
-      uint32_t replySourceBitMask_ = 0)
+      uint32_t replySourceBitMask_ = 0,
+      std::optional<BigValueContext> bigValueContext_ = std::nullopt)
       : strippedRoutingPrefix(strippedRoutingPrefix_),
         requestClass(requestClass_),
         poolName(poolName_),
@@ -140,7 +141,8 @@ struct RequestLoggerContext {
         productId(productId_),
         regionalizationEntity(regionalizationEntity_),
         usecaseId(usecaseId_),
-        replySourceBitMask(replySourceBitMask_) {}
+        replySourceBitMask(replySourceBitMask_),
+        bigValueContext(bigValueContext_) {}
 
   RequestLoggerContext(const RequestLoggerContext&) = delete;
   RequestLoggerContext& operator=(const RequestLoggerContext&) = delete;
@@ -165,6 +167,7 @@ struct RequestLoggerContext {
   std::optional<int32_t> regionalizationEntity;
   std::optional<uint64_t> usecaseId;
   const uint32_t replySourceBitMask;
+  std::optional<BigValueContext> bigValueContext;
 };
 
 } // namespace mcrouter

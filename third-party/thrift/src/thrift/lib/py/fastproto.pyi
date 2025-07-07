fbecbd104d36f6de8175826cc21a1bda80aefae8
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2025-07-07 14:47:52 -0700

    Add missing Pyre mode headers] [batch:15/1868] [shard:48/N]
    Differential Revision: D77855112
    
    fbshipit-source-id: 583d3212c6687c301dd995057fbb68f29a480e7e

--- third-party/thrift/src/thrift/lib/py/fastproto.pyi
+++ third-party/thrift/src/thrift/lib/py/fastproto.pyi
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pyre-unsafe
+
 def decode(
     self, args, dec=None, transport=None, spec=None, utf8strings=0, protoid=0
 ) -> None: ...

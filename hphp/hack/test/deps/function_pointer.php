8de295b3fc6fb0e64874034bdd3ca167bfd0214c
Michael Thomas <mjt@meta.com>
2025-07-07 02:04:04 -0700

    Add test case for abstract type constant deps
    Summary: Adds additional tests to ensure all type constant depdencies are recorded for meth_caller and function refs
    
    Reviewed By: geralt-encore
    
    Differential Revision: D77732552
    
    fbshipit-source-id: 915d881f6ffc1de8c55ea59afa5dbf930edf7475

--- hphp/hack/test/deps/function_pointer.php
+++ hphp/hack/test/deps/function_pointer.php
@@ -1,6 +1,9 @@
 <?hh
 
+class A {}
+
 class C {
+  const type TC = A;
   public static function foo<T>(T $_): void {}
   public function bar<T>(T $_): void {}
 }
@@ -17,4 +20,24 @@ function refBar() : void {
 
 function refQux() : void {
   $_ =  qux<>;
+}
+
+
+abstract class B {
+  abstract const type TC1 as C;
+  const type TC2 = arraykey;
+
+  public function buz(this::TC1 $_, this::TC1::TC $_ , this::TC2 $_): void {}
+
+  public static function baz(this $_, this::TC1 $_, this::TC1::TC $_ , this::TC2 $_): void {}
+}
+
+
+function refBuz(): void {
+  $_ = meth_caller(B::class, 'buz');
+}
+
+
+function refBaz(): void {
+  $_ = B::baz<>;
 }

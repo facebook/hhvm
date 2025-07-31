acad9ac419da66f23efae6fe01398307731b0039
Cameron Evans <camerone@meta.com>
2025-07-30 23:04:49 -0700

    Remove unused includes from AsyncProcessor.
    Summary:
    # This diff
    
    This removes all includes from the original AsyncProcessor.h file, leaving only includes for headers coming from the `processor` folder.
    
    # Diff chain overview
    
    This is part of our effort to simplify Thrift's AsyncProcessor. At the end of this diff chain, the following will be true.
    
    - AsyncProcessor implementation will live in its own folder next to where the original `AsyncProcessor.{h|cpp}` lived. That folder will be called `processor`.
    - Wherever classes can be placed into their own file and build rule, they will be.
    - Cyclic dependencies will be ironed out and be made acyclic.
    - The original `AsyncProcessor.{h.cpp}` will just be a glob of headers for the implementation that has been moved to the `processor` folder.
    
    Reviewed By: robertroeser
    
    Differential Revision: D77137474
    
    fbshipit-source-id: 4d4aca585c20e5563ca73ef29008f4791777816d

--- third-party/thrift/src/thrift/lib/cpp2/async/AsyncProcessor.h
+++ third-party/thrift/src/thrift/lib/cpp2/async/AsyncProcessor.h
@@ -16,41 +16,6 @@
 
 #pragma once
 
-#include <exception>
-#include <mutex>
-#include <string>
-#include <string_view>
-#include <variant>
-
-#include <folly/ExceptionWrapper.h>
-#include <folly/Portability.h>
-#include <folly/String.h>
-#include <folly/Synchronized.h>
-#include <folly/Unit.h>
-#include <folly/concurrency/memory/PrimaryPtr.h>
-#include <folly/container/F14Map.h>
-#include <folly/futures/Future.h>
-#include <folly/io/async/EventBase.h>
-#include <folly/lang/Badge.h>
-
-#include <thrift/lib/cpp/TApplicationException.h>
-#include <thrift/lib/cpp/TProcessor.h>
-#include <thrift/lib/cpp/Thrift.h>
-#include <thrift/lib/cpp/concurrency/Thread.h>
-#include <thrift/lib/cpp/concurrency/ThreadManager.h>
-#include <thrift/lib/cpp/protocol/TProtocolTypes.h>
-#include <thrift/lib/cpp/transport/THeader.h>
-#include <thrift/lib/cpp2/SerializationSwitch.h>
-#include <thrift/lib/cpp2/Thrift.h>
-#include <thrift/lib/cpp2/TrustedServerException.h>
-#include <thrift/lib/cpp2/async/AsyncProcessorFactory.h>
-#include <thrift/lib/cpp2/async/Interaction.h>
-#include <thrift/lib/cpp2/async/ReplyInfo.h>
-#include <thrift/lib/cpp2/async/ResponseChannel.h>
-#include <thrift/lib/cpp2/async/RpcTypes.h>
-#include <thrift/lib/cpp2/async/ServerRequestData.h>
-#include <thrift/lib/cpp2/async/ServerStream.h>
-#include <thrift/lib/cpp2/async/Sink.h>
 #include <thrift/lib/cpp2/async/processor/AsyncProcessor.h>
 #include <thrift/lib/cpp2/async/processor/AsyncProcessorFunc.h>
 #include <thrift/lib/cpp2/async/processor/EventTask.h>
@@ -65,15 +30,3 @@
 #include <thrift/lib/cpp2/async/processor/ServerRequestHelper.h>
 #include <thrift/lib/cpp2/async/processor/ServerRequestTask.h>
 #include <thrift/lib/cpp2/async/processor/ServiceHandlerBase.h>
-#include <thrift/lib/cpp2/protocol/Protocol.h>
-#include <thrift/lib/cpp2/schema/SchemaV1.h>
-#include <thrift/lib/cpp2/server/ConcurrencyControllerInterface.h>
-#include <thrift/lib/cpp2/server/Cpp2ConnContext.h>
-#include <thrift/lib/cpp2/server/IOWorkerContext.h>
-#include <thrift/lib/cpp2/server/RequestPileInterface.h>
-#include <thrift/lib/cpp2/server/ResourcePoolHandle.h>
-#include <thrift/lib/cpp2/util/Checksum.h>
-#include <thrift/lib/cpp2/util/IntrusiveSharedPtr.h>
-#include <thrift/lib/cpp2/util/TypeErasedValue.h>
-#include <thrift/lib/thrift/gen-cpp2/RpcMetadata_types.h>
-#include <thrift/lib/thrift/gen-cpp2/metadata_types.h>

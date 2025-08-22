71820d696494030b2b7c4860a3d4bae607697462
Jack Chistyakov <jchistyakov@meta.com>
2025-08-22 15:38:57 -0700

    Move GoroutinePerRequest UT to Thrift lib
    Summary: Appropriate location.
    
    Reviewed By: podtserkovskiy
    
    Differential Revision: D80830928
    
    fbshipit-source-id: ea031bbfb3340b59284f4c1e76b67f63b6601cb8

--- third-party/thrift/src/thrift/lib/go/thrift/server_test.go
+++ third-party/thrift/src/thrift/lib/go/thrift/server_test.go
@@ -23,6 +23,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"sync"
 	"testing"
 	"time"
 
@@ -30,6 +31,7 @@ import (
 
 	"github.com/facebook/fbthrift/thrift/lib/go/thrift/dummy"
 	dummyif "github.com/facebook/fbthrift/thrift/test/go/if/dummy"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -443,5 +445,85 @@ func TestPanics(t *testing.T) {
 	})
 	t.Run("NewServer/Rocket", func(t *testing.T) {
 		runPanicTestFunc(t, TransportIDRocket)
+	})
+}
+
+func TestGoroutinePerRequest(t *testing.T) {
+	runTestFunc := func(t *testing.T, serverTransport TransportID) {
+		var clientTransportOption ClientOption
+		switch serverTransport {
+		case TransportIDHeader:
+			clientTransportOption = WithHeader()
+		case TransportIDUpgradeToRocket:
+			clientTransportOption = WithUpgradeToRocket()
+		case TransportIDRocket:
+			clientTransportOption = WithRocket()
+		default:
+			panic("unsupported transport!")
+		}
+
+		listener, err := net.Listen("tcp", "[::]:0")
+		require.NoError(t, err)
+		addr := listener.Addr()
+		t.Logf("Server listening on %v", addr)
+
+		processor := dummyif.NewDummyProcessor(&dummy.DummyHandler{})
+		server := NewServer(processor, listener, serverTransport, WithNumWorkers(GoroutinePerRequest))
+
+		serverCtx, serverCancel := context.WithCancel(context.Background())
+		var serverEG errgroup.Group
+		serverEG.Go(func() error {
+			return server.ServeContext(serverCtx)
+		})
+
+		channel, err := NewClient(
+			clientTransportOption,
+			WithIoTimeout(5*time.Second),
+			WithDialer(func() (net.Conn, error) {
+				return net.DialTimeout(addr.Network(), addr.String(), 5*time.Second)
+			}),
+		)
+		require.NoError(t, err)
+		client := dummyif.NewDummyChannelClient(channel)
+
+		var clientsWG sync.WaitGroup
+		startTime := time.Now()
+		for range 1000 {
+			clientsWG.Go(
+				func() {
+					err := client.Sleep(context.Background(), 500 /* ms */)
+					assert.NoError(t, err)
+				},
+			)
+		}
+		clientsWG.Wait()
+		timeElapsed := time.Since(startTime)
+
+		err = client.Close()
+		require.NoError(t, err)
+
+		// The core assertion of this test!!!
+		// We just made 1000 sleep requests in parallel (with 500ms sleep each).
+		// That's 500s (seconds!!!) of total sleep time - quite a long duration.
+		// If GoroutinePerRequest is working properly - all these sleeps would have been
+		// done in parallel, taking about 500ms of real world time.
+		// To avoid flakiness - use a 3 second buffer in the assertion.
+		require.Less(t, timeElapsed, 3*time.Second)
+
+		// Shut down server.
+		serverCancel()
+		err = serverEG.Wait()
+		require.ErrorIs(t, err, context.Canceled)
+	}
+
+	// Only supported in Rocket
+	// t.Run("NewServer/Header", func(t *testing.T) {
+	// 	runTestFunc(t, TransportIDHeader)
+	// })
+	// t.Run("NewServer/UpgradeToRocket", func(t *testing.T) {
+	// 	runTestFunc(t, TransportIDUpgradeToRocket)
+	// })
+	t.Run("NewServer/Rocket", func(t *testing.T) {
+		runTestFunc(t, TransportIDRocket)
 	})
 }

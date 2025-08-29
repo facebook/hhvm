f5aef7faf7df0acddfd45f7981d8ce894bf425cc
Vassil Mladenov <vmladenov@meta.com>
2025-08-29 10:28:17 -0700

    Extend unused generic case to show regular alias comparison
    Reviewed By: DavidSnider
    
    Differential Revision: D81268706
    
    fbshipit-source-id: 2059e98af321bf14898c1f92ea1e09040e0eb739

--- hphp/test/slow/case-types/unused_generic.php
+++ hphp/test/slow/case-types/unused_generic.php
@@ -2,12 +2,18 @@
 
 <<file: __EnableUnstableFeatures('case_types')>>
 
-case type HasGenericAndUsesIt<T> = T;
+case type CaseHasGenericAndUsesIt<T> = T;
 
-case type HasGenericAndDoesNotUseIt<T> = int;
+type HasGenericAndUsesIt<T> = T;
+
+case type CaseHasGenericAndDoesNotUseIt<T> = int;
+
+type HasGenericAndDoesNotUseIt<T> = int;
 
 <<__EntryPoint>>
 function main(): void {
   var_dump(type_structure_for_alias(nameof HasGenericAndUsesIt));
+  var_dump(type_structure_for_alias(nameof CaseHasGenericAndUsesIt));
   var_dump(type_structure_for_alias(nameof HasGenericAndDoesNotUseIt));
+  var_dump(type_structure_for_alias(nameof CaseHasGenericAndDoesNotUseIt));
 }

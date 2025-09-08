2cca5d220d8edd3adc71b81b7eff3e6eebf4fbad
Jack Chistyakov <jchistyakov@meta.com>
2025-09-08 13:46:15 -0700

    Replace ClientInterface -> Client
    Summary:
    They are aliases.
    
    https://www.internalfb.com/code/fbsource/[de6eef85e176]/xplat/thrift/compiler/generate/templates/go/svc/interface.mustache?lines=46-47
    
    Differential Revision: D81748655
    
    fbshipit-source-id: e284769b0ecf44378f71f3974d04de96eede2f8f

--- third-party/thrift/src/thrift/lib/go/thrift/server_test.go
+++ third-party/thrift/src/thrift/lib/go/thrift/server_test.go
@@ -283,7 +283,7 @@ func TestALPN(t *testing.T) {
 		return net.Dial("tcp", addr.String())
 	})
 
-	createAlpnClient := func(opts ...ClientOption) (dummyif.DummyClientInterface, error) {
+	createAlpnClient := func(opts ...ClientOption) (dummyif.DummyClient, error) {
 		channel, err := NewClient(opts...)
 		if err != nil {
 			return nil, fmt.Errorf("could not create client: %w", err)

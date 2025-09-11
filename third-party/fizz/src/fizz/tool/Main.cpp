1d026be0ab118e0073154b0d1eb0f580d19c475d
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2025-09-10 17:26:24 -0700

    Fix CQS signal modernize-use-emplace in fbcode/fizz/tool
    Reviewed By: zxjtan
    
    Differential Revision: D82093204
    
    fbshipit-source-id: 502ec80b35e1c644d79ef2d63896c146860a1da9

--- third-party/fizz/src/fizz/tool/Main.cpp
+++ third-party/fizz/src/fizz/tool/Main.cpp
@@ -51,7 +51,7 @@ void parseVerbosityFlags(const std::vector<std::string>& arguments) {
 int main(int argc, char** argv) {
   std::vector<std::string> arguments;
   for (int i = 0; i < argc; i++) {
-    arguments.push_back(argv[i]);
+    arguments.emplace_back(argv[i]);
   }
 
   try {

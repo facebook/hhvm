04e27c40a855882ec1a01042ab07cbaa5c6943bb
Scott Owens <sowens@meta.com>
2025-09-16 03:35:31 -0700

    Complete removal of oxidized-by-ref
    Reviewed By: vassilmladenov
    
    Differential Revision: D80171861
    
    fbshipit-source-id: b23c9f3d6793c3909a8d18be588d687160c9311e

--- hphp/hack/src/oxidized_by_ref/manual/i_set.rs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Facebook, Inc. and its affiliates.
-//
-// This source code is licensed under the MIT license found in the
-// LICENSE file in the "hack" directory of this source tree.
-
-pub type ISet<'a> = arena_collections::set::Set<'a, isize>;

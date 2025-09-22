3dd34916bb3603d321842a85eb99a4d88a3102e5
generatedunixname537391475639613 <noreply+537391475639613@fb.com>
2025-09-22 10:44:51 -0700

    Fix for T238840860 ("Your diff, D82825549, broke one test")
    Reviewed By: quark-zju
    
    Differential Revision: D82905618
    
    fbshipit-source-id: 6d06a9a9817e12a6f29cccd5124371b54e7b6bf4

--- third-party/watchman/src/watchman/main.cpp
+++ third-party/watchman/src/watchman/main.cpp
@@ -17,6 +17,7 @@
 #include <folly/system/Shell.h>
 
 #include <stdio.h>
+#include <optional>
 
 #include "watchman/ChildProcess.h"
 #include "watchman/Client.h"
@@ -1039,8 +1040,9 @@ static int inner_main(int argc, char** argv) {
   // TODO: We used to avoid folly::init so it didn't interfere with our own
   // signal handling. We want to swap to folly signal handling, so we'll do a
   // full init on Linux to test it. We should remove this if in the future.
+  std::optional<folly::Init> folly_init;
   if (kUseFollySignalHandler) {
-    const folly::Init init(&argc, &argv, folly::InitOptions().useGFlags(false));
+    folly_init.emplace(&argc, &argv, folly::InitOptions().useGFlags(false));
   } else {
     folly::SingletonVault::singleton()->registrationComplete();
   }

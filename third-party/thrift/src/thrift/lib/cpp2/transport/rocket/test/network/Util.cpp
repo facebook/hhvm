aafa2286a2fadd8a04dbde81872f53a362ca475e
Yedidya Feldblum <yfeldblum@meta.com>
2025-09-24 14:01:35 -0700

    use template get_exception in rocket test utils
    Summary:
    Saves a `dynamic_cast`, since `get_exception` must effectively do a `dynamic_cast` internally.
    
    From this pattern:
    ```
    const auto* const exn = dynamic_cast<Exn*>(ew.get_exception());
    ```
    To this pattern:
    ```
    const auto* const exn = ew.get_exception<Exn>();
    ```
    
    Reviewed By: iahs
    
    Differential Revision: D83158163
    
    fbshipit-source-id: d5367b23d257f692200fe1574d20c374cd206b6c

--- third-party/thrift/src/thrift/lib/cpp2/transport/rocket/test/network/Util.cpp
+++ third-party/thrift/src/thrift/lib/cpp2/transport/rocket/test/network/Util.cpp
@@ -45,22 +45,20 @@ std::string repeatPattern(folly::StringPiece pattern, size_t nbytes) {
 void expectTransportExceptionType(
     transport::TTransportException::TTransportExceptionType expectedType,
     folly::exception_wrapper ew) {
-  const auto* const tex =
-      dynamic_cast<transport::TTransportException*>(ew.get_exception());
+  const auto* const tex = ew.get_exception<transport::TTransportException>();
   ASSERT_NE(nullptr, tex);
   EXPECT_EQ(expectedType, tex->getType());
 }
 
 void expectRocketExceptionType(
     ErrorCode expectedCode, folly::exception_wrapper ew) {
-  const auto* const rex = dynamic_cast<RocketException*>(ew.get_exception());
+  const auto* const rex = ew.get_exception<RocketException>();
   ASSERT_NE(nullptr, rex);
   EXPECT_EQ(expectedCode, rex->getErrorCode());
 }
 
 void expectEncodedError(folly::exception_wrapper ew) {
-  const auto* const rex =
-      dynamic_cast<thrift::detail::EncodedError*>(ew.get_exception());
+  const auto* const rex = ew.get_exception<thrift::detail::EncodedError>();
   ASSERT_NE(nullptr, rex);
 }
 

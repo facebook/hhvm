984577fc874ad0e1a8dd0768d26b6b49843e4181
Kirill Sazonov <sazonovk@meta.com>
2025-09-24 11:38:50 -0700

    Get rid of ServerGeneratorStreamBridge-inl.h
    Summary:
    Meta:
    Our existing coding guidelines recommend against using `-inl.h` files.
    
    Reviewed By: evanjzou
    
    Differential Revision: D82692299
    
    fbshipit-source-id: 1d704497c378a8f1ab794c8cbad737642e757c10

--- third-party/thrift/src/thrift/lib/cpp2/async/ServerGeneratorStreamBridge.h
+++ third-party/thrift/src/thrift/lib/cpp2/async/ServerGeneratorStreamBridge.h
@@ -77,13 +77,149 @@ class ServerGeneratorStreamBridge : public TwoWayBridge<
       StreamElementEncoder<T>* encode,
       folly::coro::AsyncGenerator<
           std::conditional_t<WithHeader, MessageVariant<T>, T>&&> gen,
-      TileStreamGuard interaction);
+      TileStreamGuard interaction) {
+    class ReadyCallback final : public QueueConsumer {
+     public:
+      void consume() override { baton.post(); }
+      void canceled() override { LOG(WARNING) << "Server stream is cancelled"; }
+
+      folly::coro::Task<void> wait() { co_await baton; }
+
+      folly::coro::Baton baton;
+    };
+
+    bool pauseStream = false;
+    int64_t credits = 0;
+    SCOPE_EXIT {
+      stream->serverClose();
+    };
+
+    stream->notifyStreamSubscribe(interaction);
+
+    // ensure the generator is destroyed before the interaction TimeStreamGuard
+    auto gen_ = std::move(gen);
+
+    while (true) {
+      if (credits == 0 || pauseStream) {
+        ReadyCallback ready;
+        if (stream->wait(&ready)) {
+          co_await ready.baton;
+        }
+      }
+
+      {
+        auto queue = stream->getMessages();
+        while (!queue.empty()) {
+          auto next = queue.front();
+          queue.pop();
+          switch (next) {
+            case detail::StreamControl::CANCEL:
+              stream->notifyStreamFinally(stream->cancelSource_);
+              co_return;
+            case detail::StreamControl::PAUSE:
+              stream->notifyStreamPause(
+                  details::STREAM_PAUSE_REASON::EXPLICIT_PAUSE);
+              pauseStream = true;
+              break;
+            case detail::StreamControl::RESUME:
+              stream->notifyStreamResumeReceive();
+              pauseStream = false;
+              break;
+            default:
+              stream->notifyStreamCredit(next);
+              credits += next;
+              break;
+          }
+        }
+      }
+
+      if (UNLIKELY(pauseStream || credits == 0)) {
+        continue;
+      }
+
+      auto next =
+          co_await folly::coro::co_awaitTry(folly::coro::co_withCancellation(
+              stream->cancelSource_.getToken(), gen_.next()));
+      if (next.hasException()) {
+        stream->notifyStreamFinally(stream->cancelSource_, next.exception());
+        stream->publish((*encode)(std::move(next.exception())));
+        co_return;
+      }
+      if (!next->has_value()) {
+        stream->publish({});
+        stream->notifyStreamFinally(stream->cancelSource_);
+        co_return;
+      }
+
+      auto&& item = **next;
+      if constexpr (WithHeader) {
+        folly::Try<StreamPayload> sp =
+            encodeMessageVariant(encode, std::move(item));
+        bool hasPayload = sp->payload || sp->isOrderedHeader;
+        stream->publish(std::move(sp));
+        if (hasPayload) {
+          --credits;
+        }
+      } else {
+        stream->publish((*encode)(std::move(item)));
+        --credits;
+      }
+
+      stream->notifyStreamNext();
+      if (credits == 0) {
+        stream->notifyStreamPause(details::STREAM_PAUSE_REASON::NO_CREDITS);
+      }
+    }
+  }
 
  public:
   template <bool WithHeader, typename T>
   static ServerStreamFn<T> fromAsyncGenerator(
       folly::coro::AsyncGenerator<
-          std::conditional_t<WithHeader, MessageVariant<T>, T>&&>&& gen);
+          std::conditional_t<WithHeader, MessageVariant<T>, T>&&>&& gen) {
+    return [gen = std::move(gen)](
+               folly::Executor::KeepAlive<> serverExecutor,
+               StreamElementEncoder<T>* encode) mutable {
+      return ServerStreamFactory([gen = std::move(gen),
+                                  serverExecutor = std::move(serverExecutor),
+                                  encode](
+                                     FirstResponsePayload&& payload,
+                                     StreamClientCallback* callback,
+                                     folly::EventBase* clientEb,
+                                     TilePtr&& interaction,
+                                     std::shared_ptr<ContextStack>
+                                         contextStack) mutable {
+        DCHECK(clientEb->isInEventBaseThread());
+
+        // For Stream, ContextStack is co-owned by Several Steam components
+        if (contextStack && callback) {
+          callback->setContextStack(contextStack);
+        }
+
+        auto stream = new ServerGeneratorStreamBridge(
+            callback, clientEb, std::move(contextStack));
+        auto streamPtr = stream->copy();
+        fromAsyncGeneratorImpl<WithHeader>(
+            std::move(streamPtr),
+            encode,
+            std::move(gen),
+            TileStreamGuard::transferFrom(std::move(interaction)))
+            .scheduleOn(std::move(serverExecutor))
+            .start([sp = stream->copy()](folly::Try<folly::Unit> t) {
+              if (t.hasException()) {
+                LOG(ERROR)
+                    << "Closing Stream because it received an exception before it started: "
+                    << std::endl
+                    << t.exception();
+                sp->close();
+              }
+            });
+        std::ignore =
+            callback->onFirstResponse(std::move(payload), clientEb, stream);
+        stream->processPayloads();
+      });
+    };
+  }
 #endif // FOLLY_HAS_COROUTINES
 
   void consume();
@@ -138,5 +274,3 @@ class ServerGeneratorStreamBridge : public TwoWayBridge<
 };
 
 } // namespace apache::thrift::detail
-
-#include <thrift/lib/cpp2/async/ServerGeneratorStreamBridge-inl.h>

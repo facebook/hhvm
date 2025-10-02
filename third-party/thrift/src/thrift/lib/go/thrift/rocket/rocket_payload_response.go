232820f50c810f855f131b37069b3f114999bc1a
Jack Chistyakov <jchistyakov@meta.com>
2025-10-02 08:48:22 -0700

    Simplify DecodeResponsePayload
    Summary: Simplify.
    
    Reviewed By: podtserkovskiy
    
    Differential Revision: D83724059
    
    fbshipit-source-id: 910918dee3d6269895b28a4be44cdf317a623fd6

--- third-party/thrift/src/thrift/lib/go/thrift/rocket/rocket_payload_response.go
+++ third-party/thrift/src/thrift/lib/go/thrift/rocket/rocket_payload_response.go
@@ -47,29 +47,26 @@ func DecodeResponsePayload(msg payload.Payload) (*responsePayload, error) {
 	if msg == nil {
 		return &responsePayload{metadata: &rpcmetadata.ResponseRpcMetadata{}, data: []byte{}}, nil
 	}
-	res := &responsePayload{metadata: &rpcmetadata.ResponseRpcMetadata{}, data: msg.Data()}
 
-	metadataBytes, ok := msg.Metadata()
-	if !ok {
-		return res, nil
+	metadata := &rpcmetadata.ResponseRpcMetadata{}
+	err := DecodePayloadMetadata(msg, metadata)
+	if err != nil {
+		return nil, fmt.Errorf("unable to decode ResponseRpcMetadata: %w", err)
 	}
 
-	var err error
-	if err := format.DecodeCompact(metadataBytes, res.metadata); err != nil {
-		return nil, err
-	}
-	res.data, err = MaybeDecompress(res.data, res.metadata.GetCompression())
+	dataBytes, err := MaybeDecompress(msg.Data(), metadata.GetCompression())
 	if err != nil {
 		return nil, fmt.Errorf("response payload decompression failed: %w", err)
 	}
-	if res.metadata.PayloadMetadata != nil && res.metadata.PayloadMetadata.ExceptionMetadata != nil {
-		exception := newRocketException(res.metadata.PayloadMetadata.ExceptionMetadata)
+	result := &responsePayload{metadata: metadata, data: dataBytes}
+	if metadata.PayloadMetadata != nil && metadata.PayloadMetadata.ExceptionMetadata != nil {
+		exception := newRocketException(metadata.PayloadMetadata.ExceptionMetadata)
 		if !exception.IsDeclared() {
-			exception.SerializedException = res.data
-			return res, exception
+			exception.SerializedException = dataBytes
+			return result, exception
 		}
 	}
-	return res, nil
+	return result, nil
 }
 
 // EncodeResponsePayload encodes a response payload.

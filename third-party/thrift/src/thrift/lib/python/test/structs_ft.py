f5ca6c5cbe2dc1b2a3eec8076a70559878bfff93
Alper Yoney <alperyoney@meta.com>
2025-10-08 07:10:06 -0700

    Add more ft-python tests
    Summary: Add `thrift.serialize`, `thrift.deserialize`, `copy`, `deepcopy`, `__call__`, `pickle` tests for ft-python.
    
    Reviewed By: fried
    
    Differential Revision: D84033496
    
    fbshipit-source-id: 8939906c3db9fd48bc628a38e913c1f17f9c4577

--- third-party/thrift/src/thrift/lib/python/test/structs_ft.py
+++ third-party/thrift/src/thrift/lib/python/test/structs_ft.py
@@ -14,13 +14,15 @@
 
 # pyre-strict
 
+import copy
+import pickle
 import threading
 import unittest
 from contextlib import contextmanager
 
 from typing import Callable, TypeVar
 
-from testing.thrift_types import easy, Nested1, Nested2, Nested3
+from testing.thrift_types import easy, Integers, Nested1, Nested2, Nested3
 
 from thrift.python.serializer import deserialize, serialize
 from thrift.python.types import StructOrUnion
@@ -100,6 +102,11 @@ def serialization_roundtrip(struct: T) -> T:
     return deserialize(type(struct), data)
 
 
+def pickle_roundtrip(struct: T) -> T:
+    data = pickle.dumps(struct)
+    return pickle.loads(data)
+
+
 class FreeThreading_StructTests(unittest.TestCase):
     def test_struct_init_and_serde(self) -> None:
         def worker() -> None:
@@ -108,3 +115,39 @@ class FreeThreading_StructTests(unittest.TestCase):
             self.assertEqual(s1, s2)
 
         run_concurrently(worker_func=worker, nthreads=10)
+
+    def test_serde_same_struct_from_multiple_threads(self) -> None:
+        def worker(s1: Nested1) -> None:
+            s2 = serialization_roundtrip(s1)
+            self.assertEqual(s1, s2)
+
+        s1 = Nested1(a=Nested2(b=Nested3(c=easy(val=42, name="foo"))))
+        run_concurrently(worker_func=worker, nthreads=10, args=(s1,))
+
+    def test_copy_deepcopy(self) -> None:
+        def worker(s1: Nested1) -> None:
+            s2 = copy.copy(s1)
+            self.assertEqual(s1, s2)
+            s3 = copy.deepcopy(s1)
+            self.assertEqual(s1, s3)
+
+        s1 = Nested1(a=Nested2(b=Nested3(c=easy(val=42, name="foo"))))
+        run_concurrently(worker_func=worker, nthreads=10, args=(s1,))
+
+    def test_call_replace(self) -> None:
+        def worker(s1: easy) -> None:
+            tid = threading.get_ident() % 1000000000
+            s2 = s1(val=tid, name=f"{tid}")
+            self.assertEqual(s2.val, tid)
+            self.assertEqual(s2.name, f"{tid}")
+
+        s1 = easy(val=1, an_int=Integers(small=300), name="foo")
+        run_concurrently(worker_func=worker, nthreads=10, args=(s1,))
+
+    def test_pickle_same_struct_from_multiple_threads(self) -> None:
+        def worker(s1: Nested1) -> None:
+            s2 = pickle_roundtrip(s1)
+            self.assertEqual(s1, s2)
+
+        s1 = Nested1(a=Nested2(b=Nested3(c=easy(val=42, name="foo"))))
+        run_concurrently(worker_func=worker, nthreads=10, args=(s1,))

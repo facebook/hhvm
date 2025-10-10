a52652bb95947a220f84baa8eed6beb95b3150ed
Dmitry Polukhin <dpolukhin@meta.com>
2025-10-10 11:20:35 -0700

    xplat/thrift/compiler/generate/t_whisker_generator.cc
    Reviewed By: iahs
    
    Differential Revision: D83637990
    
    fbshipit-source-id: 919644ec14fca1e4072fbccc8e0d8ff95a724123

--- third-party/thrift/src/thrift/compiler/generate/t_whisker_generator.cc
+++ third-party/thrift/src/thrift/compiler/generate/t_whisker_generator.cc
@@ -597,14 +597,14 @@ prototype<t_program>::ptr t_whisker_generator::make_prototype_for_program(
   });
   def.property("uris", [](const t_program& self) {
     array::raw result;
-    for (const auto& def : self.structured_definitions()) {
-      if (!def->uri().empty()) {
-        result.emplace_back(def->uri());
+    for (const auto& struct_def : self.structured_definitions()) {
+      if (!struct_def->uri().empty()) {
+        result.emplace_back(struct_def->uri());
       }
     }
-    for (const auto& def : self.enums()) {
-      if (!def->uri().empty()) {
-        result.emplace_back(def->uri());
+    for (const auto& enum_def : self.enums()) {
+      if (!enum_def->uri().empty()) {
+        result.emplace_back(enum_def->uri());
       }
     }
     return array::of(std::move(result));

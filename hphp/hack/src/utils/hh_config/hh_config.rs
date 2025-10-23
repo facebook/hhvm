1633db7f838643f460024ac54fd9a5b1223bdb79
Ilya Zorin <izorin@meta.com>
2025-10-23 13:13:26 -0700

    Per-attempt decl directories
    Summary:
    Change input structure from providing decls as a flat list of files in the root directory into providing a separate directory for decls necessary for each typecheck attempt. The former format is not cache friendly since we keep re-uploading everything since directory structure is flat while the latter one makes sure that we upload only new decls since the old ones were already uploaded and sub-directories that contain them are cached.
    The change is hidden by JK which is fully disabled at the moment.
    
    Reviewed By: nt591
    
    Differential Revision: D85340121
    
    fbshipit-source-id: b59384ca926c29ff2e6e51e62cb2a1a270ecf695

--- hphp/hack/src/utils/hh_config/hh_config.rs
+++ hphp/hack/src/utils/hh_config/hh_config.rs
@@ -62,6 +62,7 @@ pub struct HhConfig {
     pub use_distc_crawl_dircache: bool,
     pub distc_no_unnecessary_saved_state_work: bool,
     pub distc_write_trace_during_save_state_creation_only: bool,
+    pub distc_per_attempt_decls_directory_upload: bool,
 }
 
 impl HhConfig {
@@ -662,6 +663,9 @@ impl HhConfig {
                 "distc_write_trace_during_save_state_creation_only" => {
                     c.distc_write_trace_during_save_state_creation_only = parse_json(&value)?;
                 }
+                "distc_per_attempt_decls_directory_upload" => {
+                    c.distc_per_attempt_decls_directory_upload = parse_json(&value)?;
+                }
                 _ => {}
             }
         }
@@ -673,7 +677,8 @@ impl HhConfig {
             "eden_fetch_parallelism": self.eden_fetch_parallelism,
             "use_distc_crawl_dircache": self.use_distc_crawl_dircache,
             "distc_no_unnecessary_saved_state_work": self.distc_no_unnecessary_saved_state_work,
-            "distc_write_trace_during_save_state_creation_only": self.distc_write_trace_during_save_state_creation_only
+            "distc_write_trace_during_save_state_creation_only": self.distc_write_trace_during_save_state_creation_only,
+            "distc_per_attempt_decls_directory_upload": self.distc_per_attempt_decls_directory_upload
         });
         experiments.to_string()
     }

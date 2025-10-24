bf8d50e5e075b447c864a29b34db34c9d4da89c6
Yedidya Feldblum <yfeldblum@meta.com>
2025-10-24 08:41:37 -0700

    stress test for json ascii string decoding
    Summary: Stress test for json ascii string decoding, specifically roundtripping random strings via `JSONProtocolWriterCommon::writeJSONString` and `JSONProtocolReaderCommon::readJSONString`.
    
    Reviewed By: iahs
    
    Differential Revision: D21609288
    
    fbshipit-source-id: dfe5139fc0ea669c465791c9c23d064dd8d7f01c

--- third-party/thrift/src/thrift/lib/cpp2/protocol/test/JSONProtocolCommonTest.cpp
+++ third-party/thrift/src/thrift/lib/cpp2/protocol/test/JSONProtocolCommonTest.cpp
@@ -23,6 +23,8 @@
 #include <folly/String.h>
 #include <folly/io/IOBuf.h>
 
+using namespace std::literals;
+
 class JSONProtocolCommonTest : public testing::Test {};
 
 namespace {
@@ -154,5 +156,116 @@ TEST_F(JSONProtocolCommonTest, whitespace_combinatorics) {
       EXPECT_EQ(i + 1, read(vec))
           << "split[" << split << "] input: " << quote(s);
     });
+  }
+}
+
+TEST_F(JSONProtocolCommonTest, string_ascii) {
+  struct Reader : apache::thrift::JSONProtocolReaderCommon {
+    using apache::thrift::JSONProtocolReaderCommon::readJSONString;
+  };
+  struct Writer : apache::thrift::JSONProtocolWriterCommon {
+    using apache::thrift::JSONProtocolWriterCommon::writeJSONString;
+  };
+
+  auto write = [](std::string_view sv) {
+    folly::IOBufQueue q;
+    Writer w;
+    w.setOutput(&q);
+    w.writeJSONString(sv);
+    return std::string(folly::StringPiece(q.move()->coalesce()));
+  };
+
+  auto read = [](auto const& svs) {
+    auto const buf = from_split(svs);
+    Reader r;
+    r.setInput(buf.get());
+    std::string out;
+    r.readJSONString(out);
+    // NOLINTNEXTLINE(clang-diagnostic-nrvo) false-positive?
+    return out;
+  };
+
+  for (size_t i = 0; i < 40; ++i) {
+    auto const s = std::invoke([&] {
+      std::string r;
+      r += std::string(i, 'x');
+      return r;
+    });
+    EXPECT_EQ(i, s.size());
+    auto v = write(s);
+    EXPECT_EQ(i + 2, v.size());
+    each_split(v, [&](auto const& split, auto const& vec) {
+      EXPECT_EQ(s, read(vec)) //
+          << "split[" << split << "] input: " << quote(s);
+    });
+  }
+
+  for (size_t i = 0; i < 40; ++i) {
+    auto const s = std::invoke([&] {
+      std::string r;
+      for (size_t j = 0; j < i; ++j) {
+        r += std::string("x\"y\"z");
+      }
+      return r;
+    });
+    EXPECT_EQ(5 * i, s.size());
+    auto v = write(s);
+    EXPECT_EQ(7 * i + 2, v.size());
+    each_split(v, [&](auto const& split, auto const& vec) {
+      EXPECT_EQ(s, read(vec)) //
+          << "split[" << split << "] input: " << quote(s);
+    });
+  }
+}
+
+TEST_F(JSONProtocolCommonTest, string_ascii_underflow) {
+  struct Reader : apache::thrift::JSONProtocolReaderCommon {
+    using apache::thrift::JSONProtocolReaderCommon::readJSONString;
+  };
+
+  auto read = [](auto const& svs) {
+    auto const buf = from_split(svs);
+    Reader r;
+    r.setInput(buf.get());
+    std::string out;
+    r.readJSONString(out);
+    // NOLINTNEXTLINE(clang-diagnostic-nrvo) false-positive?
+    return out;
+  };
+
+  {
+    auto v = "\"hello"s; // no terminal "
+    each_split(v, [&](auto, auto const& vec) {
+      EXPECT_THROW(read(vec), std::out_of_range);
+    });
+  }
+}
+
+TEST_F(JSONProtocolCommonTest, string_ascii_stress) {
+  struct Reader : apache::thrift::JSONProtocolReaderCommon {
+    using apache::thrift::JSONProtocolReaderCommon::readJSONString;
+  };
+  struct Writer : apache::thrift::JSONProtocolWriterCommon {
+    using apache::thrift::JSONProtocolWriterCommon::writeJSONString;
+  };
+
+  std::mt19937 rng;
+  for (size_t i = 0; i < 1024; ++i) {
+    std::uniform_int_distribution<size_t> length_d{0, 16};
+    std::uniform_int_distribution<uint16_t> char_d{0x00, 0x7f};
+    std::string e(length_d(rng), 0);
+    std::generate(e.begin(), e.end(), [&] { return char(char_d(rng)); });
+
+    folly::IOBufQueue q;
+    Writer w;
+    w.setOutput(&q);
+    w.writeJSONString(e);
+
+    std::string f;
+    Reader r;
+    r.setInput(q.front());
+    r.readJSONString(f);
+
+    EXPECT_EQ(e, f);
   }
 }

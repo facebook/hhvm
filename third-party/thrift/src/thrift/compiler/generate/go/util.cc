a885faec2d70eddb025d57218e548a767ff4f35f
Jack Chistyakov <jchistyakov@meta.com>
2025-10-24 21:57:34 -0700

    State explicitly that interaction constructors are not supported
    Summary: And never will be.
    
    Reviewed By: RollerMatic
    
    Differential Revision: D85465418
    
    fbshipit-source-id: a1ecb62014d7d588480de477f526a94d4ec3bebb

--- third-party/thrift/src/thrift/compiler/generate/go/util.cc
+++ third-party/thrift/src/thrift/compiler/generate/go/util.cc
@@ -518,11 +518,15 @@ std::string snakecase(const std::string& name) {
 }
 
 bool is_func_go_client_supported(const t_function* func) {
-  return !func->sink() && !func->interaction();
+  // "Interaction constructor" is a legacy API, which we will not support.
+  return !func->sink() && !func->interaction() &&
+      !func->is_interaction_constructor();
 }
 
 bool is_func_go_server_supported(const t_function* func) {
-  return !func->sink() && !func->interaction();
+  // "Interaction constructor" is a legacy API, which we will not support.
+  return !func->sink() && !func->interaction() &&
+      !func->is_interaction_constructor();
 }
 
 bool is_go_reserved_word(const std::string& value) {

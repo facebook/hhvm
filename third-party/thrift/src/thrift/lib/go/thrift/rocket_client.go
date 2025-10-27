1be0ad9670aef5254a8f7628a019f6859e73cdb5
Jack Chistyakov <jchistyakov@meta.com>
2025-10-27 16:07:53 -0700

    Remove dead protocol code from Rocket client
    Summary: It is now fully converted to RequestChannel.
    
    Reviewed By: siutsin
    
    Differential Revision: D85481005
    
    fbshipit-source-id: c9ac154f94fc27e89a521dd00052ea8ca07a2b91

--- third-party/thrift/src/thrift/lib/go/thrift/rocket_client.go
+++ third-party/thrift/src/thrift/lib/go/thrift/rocket_client.go
@@ -32,9 +32,6 @@ import (
 )
 
 type rocketClient struct {
-	types.Encoder
-	types.Decoder
-
 	// rsocket client state
 	client RSocketClient
 	// Handle containing the cleanup (.Close call) for the 'client' (RSocketClient) above,
@@ -48,20 +45,8 @@ type rocketClient struct {
 	protoID types.ProtocolID
 
 	persistentHeaders map[string]string
-
-	// NOTE: all variables below are used per-request.
-	messageName string
-	writeType   types.MessageType
-	seqID       int32
-	resultData  []byte
-	resultErr   error
-	reqHeaders  map[string]string
-	respHeaders map[string]string
-	rbuf        *bytes.Buffer
-	wbuf        *bytes.Buffer
 }
 
-var _ Protocol = (*rocketClient)(nil)
 var _ RequestChannel = (*rocketClient)(nil)
 
 func newRocketClient(
@@ -92,63 +77,13 @@ func newRocketClientFromRsocket(
 		client:            client,
 		protoID:           protoID,
 		persistentHeaders: persistentHeaders,
-		rbuf:              new(bytes.Buffer),
-		wbuf:              new(bytes.Buffer),
 		ioTimeout:         ioTimeout,
-		reqHeaders:        make(map[string]string),
-	}
-	switch p.protoID {
-	case types.ProtocolIDBinary:
-		p.Decoder = format.NewBinaryDecoder(p.rbuf)
-		p.Encoder = format.NewBinaryEncoder(p.wbuf)
-	case types.ProtocolIDCompact:
-		p.Decoder = format.NewCompactDecoder(p.rbuf)
-		p.Encoder = format.NewCompactEncoder(p.wbuf)
-	default:
-		return nil, types.NewProtocolException(fmt.Errorf("Unknown protocol id: %d", p.protoID))
 	}
 	p.clientCleanup = runtime.AddCleanup(p,
 		func(underlyingClient RSocketClient) {
 			underlyingClient.Close()
 		}, client)
 	return p, nil
-}
-
-func (p *rocketClient) WriteMessageBegin(name string, typeID types.MessageType, seqid int32) error {
-	p.wbuf.Reset()
-	p.seqID = seqid
-	p.writeType = typeID
-	p.messageName = name
-	return nil
-}
-
-func (p *rocketClient) WriteMessageEnd() error {
-	return nil
-}
-
-func (p *rocketClient) Flush() error {
-	dataBytes := p.wbuf.Bytes()
-
-	ctx := context.Background()
-	if p.ioTimeout > 0 {
-		var cancel context.CancelFunc
-		ctx, cancel = context.WithTimeout(ctx, p.ioTimeout)
-		defer cancel()
-	}
-
-	if err := p.client.SendSetup(ctx); err != nil {
-		return err
-	}
-	headers := unionMaps(p.reqHeaders, p.persistentHeaders)
-	if p.writeType == types.ONEWAY {
-		return p.client.FireAndForget(ctx, p.messageName, headers, dataBytes)
-	}
-	if p.writeType != types.CALL {
-		return nil
-	}
-	p.respHeaders, p.resultData, p.resultErr = p.client.RequestResponse(ctx, p.messageName, headers, dataBytes)
-	clear(p.reqHeaders)
-	return nil
 }
 
 func (p *rocketClient) SendRequestNoResponse(ctx context.Context, messageName string, request WritableStruct) error {
@@ -322,38 +257,6 @@ func unionMaps(args ...map[string]string) map[string]string {
 		maps.Copy(unifiedMap, arg)
 	}
 	return unifiedMap
-}
-
-func (p *rocketClient) ReadMessageBegin() (string, types.MessageType, int32, error) {
-	name := p.messageName
-	if p.resultErr != nil {
-		return name, types.EXCEPTION, p.seqID, p.resultErr
-	}
-
-	// Clear the buffer, but keep the underlying storage.
-	p.rbuf.Reset()
-	// Write the result data into the buffer.
-	_, err := p.rbuf.Write(p.resultData)
-	if err != nil {
-		return name, types.EXCEPTION, p.seqID, err
-	}
-	return name, types.REPLY, p.seqID, nil
-}
-
-func (p *rocketClient) ReadMessageEnd() error {
-	return nil
-}
-
-func (p *rocketClient) Skip(fieldType types.Type) error {
-	return types.SkipDefaultDepth(p, fieldType)
-}
-
-func (p *rocketClient) setRequestHeader(key, value string) {
-	p.reqHeaders[key] = value
-}
-
-func (p *rocketClient) getResponseHeaders() map[string]string {
-	return p.respHeaders
 }
 
 func (p *rocketClient) Close() error {

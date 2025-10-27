241c8487ace0c741804a78e6b55a484c6f574308
Vishwa Prakash Gayasen <vishwagayasen@meta.com>
2025-10-27 15:22:09 -0700

    Add more tests
    Summary:
    - Add more integration tests for bidi
          - Client cancellation
          - Bidi of different primitive types
          - Structs for bidi types
    
    Reviewed By: ahilger
    
    Differential Revision: D85574957
    
    fbshipit-source-id: f2a5c44cba76f58ebeaa5d3fef91784178d09582

--- third-party/thrift/src/thrift/lib/python/test/bidi.py
+++ third-party/thrift/src/thrift/lib/python/test/bidi.py
@@ -17,12 +17,20 @@
 
 from __future__ import annotations
 
+import asyncio
+
 from typing import AsyncGenerator, Callable, Tuple
 from unittest import IsolatedAsyncioTestCase
 
 from thrift.lib.python.test.test_server import TestServer
 from thrift.python.bidi_service.thrift_clients import TestBidiService
 from thrift.python.bidi_service.thrift_services import TestBidiServiceInterface
+from thrift.python.bidi_service.thrift_types import (
+    FirstRequest,
+    FirstResponse,
+    SinkChunk,
+    StreamChunk,
+)
 
 from thrift.python.client import ClientType, get_client
 
@@ -33,9 +41,20 @@ def local_server() -> TestServer:
     return TestServer(handler=BidiHandler(), ip="::1")
 
 
-async def yield_strs(start: int, stop: int) -> AsyncGenerator[str, None]:
+async def yield_strs(
+    start: int, stop: int, delay: float = 0.0
+) -> AsyncGenerator[str, None]:
     for i in range(start, stop):
+        await asyncio.sleep(delay)
         yield str(i)
+
+
+async def yield_structs(
+    start: int, stop: int, delay: float = 0.0
+) -> AsyncGenerator[SinkChunk, None]:
+    for i in range(start, stop):
+        await asyncio.sleep(delay)
+        yield SinkChunk(value=str(i))
 
 
 class BidiTests(IsolatedAsyncioTestCase):
@@ -84,6 +103,65 @@ class BidiTests(IsolatedAsyncioTestCase):
                     self.assertEqual(item, str(i))
                     i += 1
 
+    async def test_bidi_cancellation(self) -> None:
+        async with local_server() as sa:
+            ip, port = sa.ip, sa.port
+            assert ip and port
+            async with get_client(
+                TestBidiService,
+                host=ip,
+                port=port,
+                client_type=ClientType.THRIFT_ROCKET_CLIENT_TYPE,
+            ) as client:
+                first_response, bidi = await client.echoWithResponse("start")
+                self.assertEqual("start", first_response)
+                client_sink = bidi.sink
+                with self.assertRaises(asyncio.TimeoutError):
+                    await asyncio.wait_for(client_sink.sink(yield_strs(1, 3, 0.1)), 0.2)
+                # ensure no weird segfault
+                await asyncio.sleep(1)
+
+    async def test_bidi_different_datatype(self) -> None:
+        async with local_server() as sa:
+            ip, port = sa.ip, sa.port
+            assert ip and port
+            async with get_client(
+                TestBidiService,
+                host=ip,
+                port=port,
+                client_type=ClientType.THRIFT_ROCKET_CLIENT_TYPE,
+            ) as client:
+                bidi = await client.intStream()
+                client_sink = bidi.sink
+                client_stream = bidi.stream
+                await client_sink.sink(yield_strs(1, 3))
+                i: int = 1
+                async for item in client_stream:
+                    self.assertEqual(item, i)
+                    i += 1
+
+    async def test_struct_bidi(self) -> None:
+        async with local_server() as sa:
+            ip, port = sa.ip, sa.port
+            assert ip and port
+            async with get_client(
+                TestBidiService,
+                host=ip,
+                port=port,
+                client_type=ClientType.THRIFT_ROCKET_CLIENT_TYPE,
+            ) as client:
+                first_response, bidi = await client.structBidi(
+                    FirstRequest(value="start")
+                )
+                self.assertEqual("start", first_response.value)
+                client_sink = bidi.sink
+                client_stream = bidi.stream
+                await client_sink.sink(yield_structs(1, 3))
+                i: int = 1
+                async for item in client_stream:
+                    self.assertEqual(item.value, str(i))
+                    i += 1
+
 
 class BidiHandler(TestBidiServiceInterface):
     async def echo(
@@ -116,3 +194,34 @@ class BidiHandler(TestBidiServiceInterface):
                 yield item
 
         return initial, callback
+
+    async def intStream(
+        self,
+    ) -> Callable[
+        [AsyncGenerator[str, None]],
+        AsyncGenerator[int, None],
+    ]:
+        async def callback(
+            agen: AsyncGenerator[str, None],
+        ) -> AsyncGenerator[int, None]:
+            async for item in agen:
+                yield int(item)
+
+        return callback
+
+    async def structBidi(
+        self, request: FirstRequest
+    ) -> Tuple[
+        FirstResponse,
+        Callable[
+            [AsyncGenerator[SinkChunk, None]],
+            AsyncGenerator[StreamChunk, None],
+        ],
+    ]:
+        async def callback(
+            agen: AsyncGenerator[SinkChunk, None],
+        ) -> AsyncGenerator[StreamChunk, None]:
+            async for item in agen:
+                yield StreamChunk(value=item.value)
+
+        return FirstResponse(value=request.value), callback
--- third-party/thrift/src/thrift/lib/python/test/bidi_service.thrift
+++ third-party/thrift/src/thrift/lib/python/test/bidi_service.thrift
@@ -18,6 +18,38 @@ namespace cpp2 apache.thrift.test
 namespace py3 thrift.python
 
 include "thrift/annotation/cpp.thrift"
+include "thrift/annotation/thrift.thrift"
+
+struct SinkChunk {
+  1: string value;
+}
+
+struct StreamChunk {
+  1: string value;
+}
+
+struct FirstRequest {
+  1: string value;
+}
+
+struct FirstResponse {
+  1: string value;
+}
+
+exception SinkException {
+  @thrift.ExceptionMessage
+  1: string message;
+}
+
+exception StreamException {
+  @thrift.ExceptionMessage
+  1: string message;
+}
+
+exception MethodException {
+  @thrift.ExceptionMessage
+  1: string message;
+}
 
 service TestBidiService {
   // @lint-ignore THRIFTCHECKS new unreleased feature
@@ -25,4 +57,20 @@ service TestBidiService {
 
   // @lint-ignore THRIFTCHECKS new unreleased feature
   string, sink<string>, stream<string> echoWithResponse(1: string initial);
+
+  // @lint-ignore THRIFTCHECKS new unreleased feature
+  sink<string>, stream<i32> intStream();
+
+  // @lint-ignore THRIFTCHECKS new unreleased feature
+  FirstResponse, sink<SinkChunk>, stream<StreamChunk> structBidi(
+    1: FirstRequest request,
+  );
+
+  // Method that throws an exception:
+  // if where is 0, throws on the method
+  // if where is 1, throws on the stream
+  // @lint-ignore THRIFTCHECKS new unreleased feature
+  sink<i64 throws (1: SinkException sinkEx)>, stream<
+    i64 throws (1: StreamException sinkEx)
+  > canThrow(1: i32 where) throws (1: MethodException methodEx);
 }

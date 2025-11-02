23a7cec6d05916d5cda4157ae0831d17b6e13d22
TJ Yin <ytj@meta.com>
2025-11-01 23:12:25 -0700

    change findStructuredAnnotation(...)'s input to LimitedVictor
    Summary:
    We switched structured_annotations to `LimitedVector`
    
    This code works today since `const LimitedVector&` can be converted to `const std::vector<ThriftConstStruct>&` (since std::vector is the base class).
    
    Though it's better to make it more explicit and change the function to accept `LimitedVector`.
    
    Reviewed By: praihan
    
    Differential Revision: D85866372
    
    fbshipit-source-id: 4e98dd95d9378346e5dcc57822ad148ad5889157

--- third-party/thrift/src/thrift/lib/thrift/detail/MetadataAdapter.cpp
+++ third-party/thrift/src/thrift/lib/thrift/detail/MetadataAdapter.cpp
@@ -1,4 +1,18 @@
-// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "thrift/lib/thrift/detail/MetadataAdapter.h"
 #include "thrift/lib/thrift/gen-cpp2/metadata_types.h"
@@ -6,7 +20,8 @@
 namespace apache::thrift::metadata {
 
 const ThriftConstStruct* findStructuredAnnotation(
-    const std::vector<ThriftConstStruct>& annotations, std::string_view name) {
+    const detail::LimitedVector<ThriftConstStruct>& annotations,
+    std::string_view name) {
   for (const auto& i : annotations) {
     if (i.type()->name() == name) {
       return &i;
@@ -15,7 +30,8 @@ const ThriftConstStruct* findStructuredAnnotation(
   return nullptr;
 }
 const ThriftConstStruct& findStructuredAnnotationOrThrow(
-    const std::vector<ThriftConstStruct>& annotations, std::string_view name) {
+    const detail::LimitedVector<ThriftConstStruct>& annotations,
+    std::string_view name) {
   if (const auto* p = findStructuredAnnotation(annotations, name)) {
     return *p;
   }
--- third-party/thrift/src/thrift/lib/thrift/detail/MetadataAdapter.h
+++ third-party/thrift/src/thrift/lib/thrift/detail/MetadataAdapter.h
@@ -49,7 +49,9 @@ class LimitedVector : public std::vector<T> {
 } // namespace detail
 
 const ThriftConstStruct* findStructuredAnnotation(
-    const std::vector<ThriftConstStruct>& annotations, std::string_view name);
+    const detail::LimitedVector<ThriftConstStruct>& annotations,
+    std::string_view name);
 const ThriftConstStruct& findStructuredAnnotationOrThrow(
-    const std::vector<ThriftConstStruct>& annotations, std::string_view name);
+    const detail::LimitedVector<ThriftConstStruct>& annotations,
+    std::string_view name);
 } // namespace apache::thrift::metadata

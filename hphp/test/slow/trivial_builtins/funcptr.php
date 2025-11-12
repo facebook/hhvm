df56c5d0fc0a1acc962903325964c4b2ae3b48c3
Vassil Mladenov <vmladenov@meta.com>
2025-11-11 17:32:36 -0800

    Record JIT bug with TrivialHHVMBuiltinWrapper
    Summary: Will explain the problem in the following diff; just saving the test case here for clarity.
    
    Reviewed By: jano
    
    Differential Revision: D86724846
    
    fbshipit-source-id: 14ec7116ab8038c9e49d39866154e5a3c55fd7e3

---
+++ hphp/test/slow/trivial_builtins/funcptr.php
@@ -0,0 +1,19 @@
+<?hh
+
+// The divergent implementations are intentional to illustrate
+// which function is being called
+
+<<\TrivialHHVMBuiltinWrapper('g')>>
+function f(): void {
+  echo "f\n";
+}
+
+function g(): void {
+  echo "g\n";
+}
+
+<<__EntryPoint>>
+function main(): void {
+  $f = f<>;
+  $f();
+}

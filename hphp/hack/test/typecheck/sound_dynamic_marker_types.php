5edcde72417f33bab549446152c569bc108196ff
Andrew Kennedy <akenn@meta.com>
2025-12-01 10:54:46 -0800

    Fix for hh --no-load regression caused by D87918907
    Summary: For some unknown reason, D87918907 caused a massive regression in `hh --no-load` but not in `hh --re`.
    
    Differential Revision: D88076204
    
    fbshipit-source-id: 183376b2172c535c14191ea3b8b2e3e0609cb03e

--- hphp/hack/test/typecheck/sound_dynamic_marker_types.php
+++ hphp/hack/test/typecheck/sound_dynamic_marker_types.php
@@ -1,11 +1,18 @@
 <?hh
 
-function f(HH\FIXME\POISON_MARKER<string> $poison): void {
-  hh_show($poison);
+namespace HH_FIXME {
+  type MISSING_TYPE_IN_HIERARCHY = mixed;
 }
 
-function g(\HH_FIXME\MISSING_TYPE_IN_HIERARCHY $missing): int {
-  hh_show($missing);
-  // Check that this is unsound!
-  return $missing;
+namespace Test {
+
+  function f(\HH\FIXME\POISON_MARKER<string> $poison): void {
+    hh_show($poison);
+  }
+
+  function g(\HH_FIXME\MISSING_TYPE_IN_HIERARCHY $missing): int {
+    hh_show($missing);
+    // Check that this is unsound!
+    return $missing;
+  }
 }

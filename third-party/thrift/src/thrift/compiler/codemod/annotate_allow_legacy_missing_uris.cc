5adba870f774a13811044e7bef6b403e13666edb
Aristidis Papaioannou <aristidis@meta.com>
2025-12-02 20:12:26 -0800

    Add: annotate_allow_legacy_missing_uris
    Reviewed By: hchokshi
    
    Differential Revision: D88031047
    
    fbshipit-source-id: b4f956f1ccc4cc30be3caa13d695fc303f78af41

---
+++ third-party/thrift/src/thrift/compiler/codemod/annotate_allow_legacy_missing_uris.cc
@@ -0,0 +1,192 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <fmt/core.h>
+#include <glog/logging.h>
+#include <thrift/compiler/ast/ast_visitor.h>
+#include <thrift/compiler/ast/t_program_bundle.h>
+#include <thrift/compiler/ast/uri.h>
+#include <thrift/compiler/codemod/codemod.h>
+#include <thrift/compiler/codemod/file_manager.h>
+#include <thrift/compiler/sema/ast_uri_utils.h>
+
+using apache::thrift::compiler::source_manager;
+using apache::thrift::compiler::t_program_bundle;
+
+namespace apache::thrift::compiler {
+namespace {
+
+constexpr std::string_view kAnnotation = "@thrift.AllowLegacyMissingUris";
+constexpr const char* const kAnnotationUri = kAllowLegacyMissingUris;
+
+/**
+ * Returns `true` if the given program needs the
+ * `@thrift.AllowLegacyMissingThriftUris` annotation to be added at the package
+ * level.
+ */
+bool needs_new_package_level_annotation(const t_program& program) {
+  const t_package& package = program.package();
+
+  // A non-empty package cannot require a (new) annotation, since all types will
+  // have a URI by definition.
+  if (!package.empty()) {
+    return false;
+  }
+
+  // package is empty - it is either missing or explicitly set to ""
+
+  // If the package (i.e., program) already has the annotation => nothing to do.
+  if (program.has_structured_annotation(kAnnotationUri)) {
+    return false;
+  }
+
+  // Check if any root definition is missing a URI and does not have the
+  // annotation
+
+  bool needs_annotation = false;
+  const_ast_visitor visitor;
+  visitor.add_root_definition_visitor([&](const t_named& node) {
+    if (needs_annotation) {
+      // Already found that the annotation is needed, short-circuit subsequent
+      // checks.
+      return;
+    }
+
+    if (!AstUriUtils::shouldHaveUri(node)) {
+      // Node does not require a URI => skip
+      return;
+    }
+
+    if (!node.uri().empty()) {
+      // Node has a URI => skip
+      return;
+    }
+
+    // Node requires a URI, but does not have one.
+
+    if (node.has_structured_annotation(kAnnotationUri)) {
+      // Node is already annotated with @thrift.AllowLegacyMissingUris => skip
+      return;
+    }
+
+    needs_annotation = true;
+  });
+  visitor(program);
+
+  return needs_annotation;
+}
+
+/**
+ * Applies this codemod to the given Thrift file (`program`).
+ *
+ * The logic of this codemod is as follows:
+ *
+ * 1. First, determine if a package-level @thrift.AllowLegacyMissingThriftUris
+ *    must be added, using the following logic:
+ *    a. If there is an (explicit) `package` directive:
+ *      i. If it is not empty => false
+ *      b. If (it is empty, and) already has the annotation => false
+ *    b. If any of the definitions in `program` that require a URI (struct,
+ *       union, exception, enum) do not have one, and are NOT annotated with
+ *       @thrift.AllogLegacyMissingThriftUris => return true c. (Else, ) =>
+ *       return false
+ *
+ * 2. If the answer above is false => return (nothing to do)
+ *
+ * 3. (Else) If the answer above is true (i.e., the annotation must be added),
+ *    do the followinmg:
+ *    a. If there is no package directive, add one with an explicitly empty
+ *       name (i.e., `package;`)
+ *    b. Annotate the package directive with the annotation (including the
+ *       thrift.thrift file as needed).
+ *    c. Remve the annotation from any root definition that has it.
+ */
+class AnnotateAllowLegacyMissingUris final {
+ public:
+  AnnotateAllowLegacyMissingUris(
+      source_manager& src_manager, t_program& program)
+      : source_manager_(src_manager),
+        program_(program),
+        file_manager_(source_manager_, program) {}
+
+  void run() {
+    if (!needs_new_package_level_annotation(program_)) {
+      return;
+    }
+
+    annotate_package();
+    file_manager_.apply_replacements();
+  }
+
+ private:
+  source_manager& source_manager_;
+  t_program& program_;
+  codemod::file_manager file_manager_;
+
+  /**
+   * Adds the `@thrift.AllowLegacyMissingUris` annotation at the package level.
+   *
+   * If the file has an explicit package declaration (including empty package
+   * `package;`), the annotation is added before it. Otherwise, a new empty
+   * package with the annotation is created.
+   *
+   * @return `true` if the annotation was added, `false` otherwise
+   */
+  bool annotate_package() {
+    file_manager_.add_include("thrift/annotation/thrift.thrift");
+
+    const t_package& package = program_.package();
+    if (package.is_explicit()) {
+      // There is already a `package` directive: pre-pend annotation.
+      const uint_least32_t package_offset = package.src_range().begin.offset();
+      file_manager_.add(
+          {.begin_pos = package_offset,
+           .end_pos = package_offset,
+           .new_content = fmt::format("{}\n", kAnnotation)});
+    } else {
+      // No explicit package - create empty package with annotation
+
+      const node_list_view<const t_named>& definitions = program_.definitions();
+      CHECK(!definitions.empty())
+          << "Invariant failure: program cannot require "
+          << "@thrift.AllowLegacyMissingUris annotation without any "
+          << "definition!";
+
+      const uint_least32_t definitions_offset =
+          definitions.front().src_range().begin.offset();
+      file_manager_.add(
+          {.begin_pos = definitions_offset,
+           .end_pos = definitions_offset,
+           .new_content = fmt::format("{}\npackage;\n\n", kAnnotation)});
+    }
+    return true;
+  }
+};
+
+} // namespace
+} // namespace apache::thrift::compiler
+
+int main(int argc, char** argv) {
+  return apache::thrift::compiler::run_codemod(
+      argc, argv, [](source_manager& sm, t_program_bundle& pb) {
+        apache::thrift::compiler::AnnotateAllowLegacyMissingUris(
+            sm, *pb.root_program())
+            .run();
+      });
+}
---
+++ third-party/thrift/src/thrift/compiler/codemod/annotate_allow_legacy_missing_uris_test.py
@@ -0,0 +1,253 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pyre-unsafe
+
+import os
+import shutil
+import tempfile
+import textwrap
+import unittest
+
+import pkg_resources
+
+from xplat.thrift.compiler.codemod.test_utils import read_file, run_binary, write_file
+
+
+class AnnotateAllowLegacyMissingUrisTest(unittest.TestCase):
+    def setUp(self):
+        tmp = tempfile.mkdtemp()
+        self.addCleanup(shutil.rmtree, tmp, True)
+        self.tmp = tmp
+        self.addCleanup(os.chdir, os.getcwd())
+        os.chdir(self.tmp)
+        self.maxDiff = None
+
+    def test_adds_package_annotation_for_missing_uris(self):
+        """Test that package-level annotation is added when types are missing URIs."""
+        write_file(
+            "foo.thrift",
+            textwrap.dedent(
+                """\
+                struct TestStruct {
+                  1: i32 a;
+                  2: i32 b;
+                }
+
+                enum TestEnum {
+                  VALUE1 = 1,
+                  VALUE2 = 2,
+                }
+
+                union TestUnion {
+                  1: i32 x;
+                  2: string y;
+                }
+                """
+            ),
+        )
+
+        binary = pkg_resources.resource_filename(__name__, "codemod")
+        run_binary(binary, "foo.thrift")
+
+        self.assertEqual(
+            read_file("foo.thrift"),
+            textwrap.dedent(
+                """\
+                include "thrift/annotation/thrift.thrift"
+
+                @thrift.AllowLegacyMissingUris
+                package;
+
+                struct TestStruct {
+                  1: i32 a;
+                  2: i32 b;
+                }
+
+                enum TestEnum {
+                  VALUE1 = 1,
+                  VALUE2 = 2,
+                }
+
+                union TestUnion {
+                  1: i32 x;
+                  2: string y;
+                }
+                """
+            ),
+        )
+
+    def test_no_change_when_package_has_uri(self):
+        """Test that no annotation is added when package already provides URIs."""
+        write_file(
+            "foo.thrift",
+            textwrap.dedent(
+                """\
+                package "facebook.com/thrift/test"
+
+                struct TestStruct {
+                  1: i32 a;
+                }
+                """
+            ),
+        )
+
+        binary = pkg_resources.resource_filename(__name__, "codemod")
+        run_binary(binary, "foo.thrift")
+
+        # Should remain unchanged
+        self.assertEqual(
+            read_file("foo.thrift"),
+            textwrap.dedent(
+                """\
+                package "facebook.com/thrift/test"
+
+                struct TestStruct {
+                  1: i32 a;
+                }
+                """
+            ),
+        )
+
+    def test_no_change_when_annotation_already_present(self):
+        """Test that no duplicate annotation is added if already present."""
+        write_file(
+            "foo.thrift",
+            textwrap.dedent(
+                """\
+                include "thrift/annotation/thrift.thrift"
+
+                @thrift.AllowLegacyMissingUris
+                package ""
+
+                struct TestStruct {
+                  1: i32 a;
+                }
+                """
+            ),
+        )
+
+        binary = pkg_resources.resource_filename(__name__, "codemod")
+        run_binary(binary, "foo.thrift")
+
+        # Should remain unchanged
+        self.assertEqual(
+            read_file("foo.thrift"),
+            textwrap.dedent(
+                """\
+                include "thrift/annotation/thrift.thrift"
+
+                @thrift.AllowLegacyMissingUris
+                package ""
+
+                struct TestStruct {
+                  1: i32 a;
+                }
+                """
+            ),
+        )
+
+    def test_no_change_when_types_have_explicit_uris(self):
+        """Test that no annotation is added when all types have explicit URIs."""
+        write_file(
+            "foo.thrift",
+            textwrap.dedent(
+                """\
+                include "thrift/annotation/thrift.thrift"
+
+                @thrift.Uri{value = "facebook.com/thrift/test/TestStruct"}
+                struct TestStruct {
+                  1: i32 a;
+                }
+                """
+            ),
+        )
+
+        binary = pkg_resources.resource_filename(__name__, "codemod")
+        run_binary(binary, "foo.thrift")
+
+        # Should remain unchanged (no package annotation needed)
+        self.assertEqual(
+            read_file("foo.thrift"),
+            textwrap.dedent(
+                """\
+                include "thrift/annotation/thrift.thrift"
+
+                @thrift.Uri{value = "facebook.com/thrift/test/TestStruct"}
+                struct TestStruct {
+                  1: i32 a;
+                }
+                """
+            ),
+        )
+
+    def test_no_annotation_for_typedef(self):
+        """Test that typedefs don't trigger package annotation (they don't need URIs)."""
+        write_file(
+            "foo.thrift",
+            textwrap.dedent(
+                """\
+                typedef i32 MyInt
+                typedef string MyString
+                """
+            ),
+        )
+
+        binary = pkg_resources.resource_filename(__name__, "codemod")
+        run_binary(binary, "foo.thrift")
+
+        # Should remain unchanged (typedefs don't need URIs)
+        self.assertEqual(
+            read_file("foo.thrift"),
+            textwrap.dedent(
+                """\
+                typedef i32 MyInt
+                typedef string MyString
+                """
+            ),
+        )
+
+    def test_adds_annotation_before_existing_empty_package(self):
+        """Test that annotation is added before existing empty package declaration."""
+        write_file(
+            "foo.thrift",
+            textwrap.dedent(
+                """\
+                package;
+
+                struct TestStruct {
+                  1: i32 a;
+                }
+                """
+            ),
+        )
+
+        binary = pkg_resources.resource_filename(__name__, "codemod")
+        run_binary(binary, "foo.thrift")
+
+        self.assertEqual(
+            read_file("foo.thrift"),
+            textwrap.dedent(
+                """\
+                include "thrift/annotation/thrift.thrift"
+
+                @thrift.AllowLegacyMissingUris
+                package;
+
+                struct TestStruct {
+                  1: i32 a;
+                }
+                """
+            ),
+        )

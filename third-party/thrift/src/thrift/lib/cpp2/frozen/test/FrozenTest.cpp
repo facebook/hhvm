f8a090a746cb852c7824743e76646b5650f798b6
generatedunixname89002005287564 <noreply+89002005287564@fb.com>
2025-12-04 11:21:06 -0800

    Fix CQS signal facebook-hte-StringPiecePassByReference in fbcode/thrift/lib
    Differential Revision: D88244502
    
    fbshipit-source-id: 29cb094c1e338502acd7737178de7dfe483b6ff6

--- third-party/thrift/src/thrift/lib/cpp2/frozen/test/FrozenTest.cpp
+++ third-party/thrift/src/thrift/lib/cpp2/frozen/test/FrozenTest.cpp
@@ -176,7 +176,7 @@ TEST(Frozen, HashCompatibility) {
   using StrLayout = Layout<std::string>;
   using View = StrLayout::View;
 
-  auto follyHash = [](const View& v) {
+  auto follyHash = [](const View v) {
     return folly::hash::fnv64_buf_BROKEN(v.begin(), v.size());
   };
 

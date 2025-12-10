c53454b16f82ecaeb52e2f21b9c433a6659b9132
Emil Hesslow <emil@meta.com>
2025-12-09 23:55:32 -0800

    Line table start from offset 0
    Reviewed By: jano
    
    Differential Revision: D86798956
    
    fbshipit-source-id: ef0ed28e797e30b94786232c6a015794c16d3e9c

--- hphp/runtime/vm/source-location.cpp
+++ hphp/runtime/vm/source-location.cpp
@@ -130,7 +130,7 @@ LineInfo getLineInfo(const LineTable& table, Offset pc) {
     if (line > 0) {
       auto const pastOff = it->pastOffset();
       auto const baseOff = it == begin(table) ?
-        pc : std::prev(it)->pastOffset();
+        0 : std::prev(it)->pastOffset();
       assertx(baseOff <= pc && pc < pastOff);
       return { { baseOff, pastOff }, line };
     }

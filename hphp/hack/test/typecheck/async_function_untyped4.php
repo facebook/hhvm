fd3a6ebbd24f45bdec44f11fc66fe94d9b9235a1
Andrew Kennedy <akenn@meta.com>
2025-12-17 09:07:43 -0800

    Remove some untyped tests
    Summary: Tests that have missing return types intended to produce Tany don't make any sense now that we don't support Tany.
    
    Reviewed By: geralt-encore
    
    Differential Revision: D89374203
    
    fbshipit-source-id: 1010a9b0c0e0b5c9f5acd6d7c877c319fc183a69

--- hphp/hack/test/typecheck/async_function_untyped4.php
+++
@@ -1,9 +0,0 @@
-<?hh
-
-interface I1 {
-  public function f(): int;
-}
-
-class C1 implements I1 {
-  public async function f() /* : TAny */ { return 10; }
-}
--- hphp/hack/test/typecheck/async_function_untyped5.php
+++
@@ -1,9 +0,0 @@
-<?hh
-
-class C1 {
-  public async function f() /* : TAny */ { return 10; }
-}
-
-class C2 extends C1 {
-  public function f(): int { return 10; }
-}

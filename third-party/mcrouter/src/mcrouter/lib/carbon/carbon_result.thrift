81a96e4825b91abc8f52f92385aadeb58ce68c86
generatedunixname226714639793621 <noreply+226714639793621@fb.com>
2025-12-17 11:13:09 -0800

    fbcode/mcrouter/
    Reviewed By: aristidisp
    
    Differential Revision: D89334763
    
    fbshipit-source-id: e2b4421bad8a09d3de7b116dc280819b2929f6b5

--- third-party/mcrouter/src/mcrouter/lib/carbon/carbon_result.thrift
+++ third-party/mcrouter/src/mcrouter/lib/carbon/carbon_result.thrift
@@ -5,6 +5,11 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+include "thrift/annotation/thrift.thrift"
+
+@thrift.AllowLegacyMissingUris
+package;
+
 namespace cpp2 carbon
 namespace py3 carbon
 namespace hack carbon

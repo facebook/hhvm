a5a9e10d238be6d1d78f130a010123f127b2b678
Max Heiber <mheiber@meta.com>
2025-12-17 15:48:19 -0800

    Repro named parameters subtyping completeness issue
    Summary:
    ## Description
    
    Repro completeness bug with named parameters subtyping, fixed in next diff {D89399796}
    See test case.
    
    For more details, see the explanation in the next diff (D89399796)
    
    Differential Revision: D89399795
    
    fbshipit-source-id: c8c67dc4e69495915b7a413660d1df8f4e35e828

---
+++ hphp/hack/test/typecheck/named_parameters/named_params_optional_subtyping.php
@@ -0,0 +1,13 @@
+<?hh
+
+function my_fun((function(int, optional named bool $b): void) $f): (function(int): void) {
+  return $f; // Should be OK
+}
+
+class C1 {
+  public function m(int $_): void {}
+}
+
+class C2 extends C1 {
+  public function m(int $_, named bool $b = true): void {} // Should be OK
+}

f4eb0f648c5ec114f1c6c94a98d34fdef5303d24
Danfeng Wang <sunniewang@meta.com>
2026-01-13 13:49:48 -0800

    Add method metadata for async processor
    Summary: Introduces a new Thrift processor architecture that uses getMethodMetadata* functions to separate IDL-dependent generated code from processAsync logic.
    
    Reviewed By: rmakheja
    
    Differential Revision: D90211895
    
    fbshipit-source-id: 5570403d101afb149b3812479f48a351be35f758

--- third-party/thrift/src/thrift/lib/hack/src/ThriftAsyncProcessor.php
+++ third-party/thrift/src/thrift/lib/hack/src/ThriftAsyncProcessor.php
@@ -18,6 +18,225 @@
 
 use namespace FlibSL\{C, Math, Str, Vec}; // @oss-enable
 
+/**
+ * Base class for method metadata returned by getMethodMetadata* functions.
+ * Provides common interface for accessing args/result classes and handler methods.
+ */
+abstract class ThriftServiceMethod<
+  TThriftIf as IThriftAsyncIf,
+  TArgs as IThriftStruct,
+  TResultStruct as IResultThriftStruct,
+  THandlerResult,
+> {
+  abstract const Thrift_RpcMetadata_RpcKind RPC_KIND;
+
+  public function __construct(
+    protected classname<TArgs> $args,
+    protected classname<TResultStruct> $ret,
+    protected (function(
+      TThriftIf,
+      TArgs,
+    ): Awaitable<THandlerResult>) $handlerMethod,
+  )[] {}
+
+  abstract public function genResult(
+    TThriftIf $handler,
+    TArgs $args,
+    TResultStruct $res,
+  ): Awaitable<THandlerResult>;
+
+  public function getArgsClass()[]: classname<TArgs> {
+    return $this->args;
+  }
+
+  public function getResultClass()[]: classname<TResultStruct> {
+    return $this->ret;
+  }
+
+  public function isStreaming()[]: bool {
+    return false;
+  }
+}
+
+/**
+ * Method metadata for standard request-response methods.
+ */
+final class ThriftServiceRequestResponseMethod<
+  TThriftIf as IThriftAsyncIf,
+  TArgs as IThriftStruct,
+  TResultStruct as IResultThriftStruct with { type TResult = TRet },
+  TRet,
+> extends ThriftServiceMethod<TThriftIf, TArgs, TResultStruct, TRet> {
+  const Thrift_RpcMetadata_RpcKind RPC_KIND =
+    Thrift_RpcMetadata_RpcKind::SINGLE_REQUEST_SINGLE_RESPONSE;
+
+  <<__Override>>
+  public async function genResult(
+    TThriftIf $handler,
+    TArgs $args,
+    TResultStruct $result,
+  ): Awaitable<TRet> {
+    $response = await ($this->handlerMethod)($handler, $args);
+    if ($result is ThriftSyncStructWithResult) {
+      $result->success = $response;
+    } else if ($result is ThriftAsyncStructWithResult) {
+      $result->success = $response;
+    }
+    return $response;
+  }
+}
+
+/**
+ * Method metadata for oneway (fire-and-forget) methods.
+ * Oneway methods do not send any response back to the client.
+ * Unlike request-response methods, oneway methods don't need a result type
+ * since they never send responses back to the client.
+ */
+final class ThriftServiceOnewayMethod<
+  TThriftIf as IThriftAsyncIf,
+  TArgs as IThriftStruct,
+> {
+  const Thrift_RpcMetadata_RpcKind RPC_KIND =
+    Thrift_RpcMetadata_RpcKind::SINGLE_REQUEST_NO_RESPONSE;
+
+  public function __construct(
+    protected classname<TArgs> $args,
+    protected (function(TThriftIf, TArgs): Awaitable<void>) $handlerMethod,
+  )[] {}
+
+  public function getArgsClass()[]: classname<TArgs> {
+    return $this->args;
+  }
+
+  public async function genExecute(
+    TThriftIf $handler,
+    TArgs $args,
+  ): Awaitable<void> {
+    await ($this->handlerMethod)($handler, $args);
+  }
+}
+
+/**
+ * Method metadata for streaming response methods.
+ */
+final class ThriftServiceStreamingResponseMethod<
+  TThriftIf as IThriftAsyncIf,
+  TArgs as IThriftStruct,
+  TResultStruct as IResultThriftStruct with { type TResult = TRet },
+  TRet,
+  TStreamPayloadStruct as IResultThriftStruct with {
+    type TResult = TStreamType },
+  TStreamType,
+>
+  extends ThriftServiceMethod<
+    TThriftIf,
+    TArgs,
+    TResultStruct,
+    ResponseAndStream<TRet, TStreamType>,
+  > {
+  const Thrift_RpcMetadata_RpcKind RPC_KIND =
+    Thrift_RpcMetadata_RpcKind::SINGLE_REQUEST_STREAMING_RESPONSE;
+
+  public function __construct(
+    protected classname<TArgs> $args,
+    protected classname<TResultStruct> $ret,
+    protected (function(
+      TThriftIf,
+      TArgs,
+    ): Awaitable<ResponseAndStream<TRet, TStreamType>>) $handlerMethod,
+    protected classname<TStreamPayloadStruct> $streamPayload,
+  )[] {
+    parent::__construct($args, $ret, $handlerMethod);
+  }
+
+  <<__Override>>
+  public async function genResult(
+    TThriftIf $handler,
+    TArgs $args,
+    TResultStruct $result,
+  ): Awaitable<ResponseAndStream<TRet, TStreamType>> {
+    $res_and_stream = await ($this->handlerMethod)($handler, $args);
+    if ($result is ThriftSyncStructWithResult) {
+      $result->success = $res_and_stream->response;
+    } else if ($result is ThriftAsyncStructWithResult) {
+      $result->success = $res_and_stream->response;
+    }
+    return $res_and_stream;
+  }
+
+  public function getStreamPayloadClass()[]: classname<IResultThriftStruct> {
+    return $this->streamPayload;
+  }
+
+  <<__Override>>
+  public function isStreaming()[]: bool {
+    return true;
+  }
+}
+
+/**
+ * Method metadata for sink methods.
+ */
+final class ThriftServiceSinkMethod<
+  TThriftIf as IThriftAsyncIf,
+  TArgs as IThriftStruct,
+  TResultStruct as IResultThriftStruct with { type TResult = TRet },
+  TRet,
+  TSinkPayloadStruct as IResultThriftStruct with {
+    type TResult = TSinkPayloadType },
+  TSinkPayloadType,
+  TFinalResponseStruct as IResultThriftStruct with {
+    type TResult = TFinalResponseType },
+  TFinalResponseType,
+>
+  extends ThriftServiceMethod<
+    TThriftIf,
+    TArgs,
+    TResultStruct,
+    ResponseAndSink<TRet, TSinkPayloadType, TFinalResponseType>,
+  > {
+  const Thrift_RpcMetadata_RpcKind RPC_KIND = Thrift_RpcMetadata_RpcKind::SINK;
+
+  public function __construct(
+    protected classname<TArgs> $args,
+    protected classname<TResultStruct> $ret,
+    protected (function(TThriftIf, TArgs): Awaitable<
+      ResponseAndSink<TRet, TSinkPayloadType, TFinalResponseType>,
+    >) $handlerMethod,
+    protected classname<TSinkPayloadStruct> $sinkPayload,
+    protected classname<TFinalResponseStruct> $finalResponse,
+  )[] {
+    parent::__construct($args, $ret, $handlerMethod);
+  }
+
+  <<__Override>>
+  public async function genResult(
+    TThriftIf $handler,
+    TArgs $args,
+    TResultStruct $result,
+  ): Awaitable<ResponseAndSink<TRet, TSinkPayloadType, TFinalResponseType>> {
+    $response_and_sink = await ($this->handlerMethod)($handler, $args);
+    if ($result is ThriftSyncStructWithResult) {
+      $result->success = $response_and_sink->response;
+    } else if ($result is ThriftAsyncStructWithResult) {
+      $result->success = $response_and_sink->response;
+    }
+    return $response_and_sink;
+  }
+
+  public function getSinkPayloadClass()[]: classname<IResultThriftStruct> {
+    return $this->sinkPayload;
+  }
+
+  public function getFinalResponseClass()[]: classname<IResultThriftStruct> {
+    return $this->finalResponse;
+  }
+
+  public function isSink()[]: bool {
+    return true;
+  }
+}
+
 // @oss-disable: <<Oncalls('thrift')>>
 abstract class ThriftAsyncProcessor
   extends ThriftProcessorBase

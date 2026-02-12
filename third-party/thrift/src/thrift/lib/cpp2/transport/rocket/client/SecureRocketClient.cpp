aa121e4a87c45044c8ce7e2621006dd069673936
Volodymyr Ferniuk <volodymyrf@meta.com>
2026-02-12 11:50:32 -0800

    Use for_each_field_id to iterate SecurityPolicy fields
    Summary:
    Refactor `validateSecurityPolicy` in `SecureRocketClient` to use `apache::thrift::op::for_each_field_id` instead of manually enumerating each field of `SecurityPolicy`.
    
    This change improves maintainability by automatically adapting to any future fields added to the `SecurityPolicy` struct, eliminating the need to update this code when the schema changes.
    
    The `for_each_field_id` API is compile-time optimized and has zero runtime overhead compared to direct field access - the lambda is instantiated for each field at compile time, making it equivalent to writing out each field access manually.
    
    Differential Revision: D92560318
    
    fbshipit-source-id: aa079b90a4f109d1a63d8b2d1c87dea56d3f7baf

--- third-party/thrift/src/thrift/lib/cpp2/transport/rocket/client/SecureRocketClient.cpp
+++ third-party/thrift/src/thrift/lib/cpp2/transport/rocket/client/SecureRocketClient.cpp
@@ -17,14 +17,13 @@
 #include <thrift/lib/cpp2/transport/rocket/client/SecureRocketClient.h>
 
 #include <folly/container/F14Map.h>
+#include <thrift/lib/cpp2/op/Get.h>
 #include <thrift/lib/thrift/gen-cpp2/RpcMetadata_types.h>
 
 THRIFT_FLAG_DEFINE_bool(client_authwall_client_enabled, false);
 THRIFT_FLAG_DEFINE_bool(client_authwall_client_enforced, false);
 
 namespace {
-const std::string kAuthorization = "authorization";
-const std::string kAuthWall = "authWall";
 const std::string kUndefined = "UNDEFINED";
 } // namespace
 
@@ -64,18 +63,16 @@ bool SecureRocketClient::validateSecurityPolicy(
   }
 
   folly::F14FastMap<std::string, std::string> policies;
-  if (auto authorization = securityPolicy->authorization()) {
-    policies[kAuthorization] =
-        apache::thrift::util::enumNameSafe(*authorization);
-  } else {
-    policies[kAuthorization] = kUndefined;
-  }
-
-  if (auto authWall = securityPolicy->authWall()) {
-    policies[kAuthWall] = apache::thrift::util::enumNameSafe(*authWall);
-  } else {
-    policies[kAuthWall] = kUndefined;
-  }
+  apache::thrift::op::for_each_field_id<SecurityPolicy>([&](auto id) {
+    using Id = decltype(id);
+    auto fieldRef = apache::thrift::op::get<Id>(*securityPolicy);
+    std::string fieldName{apache::thrift::op::get_name_v<SecurityPolicy, Id>};
+    if (fieldRef.has_value()) {
+      policies[fieldName] = apache::thrift::util::enumNameSafe(*fieldRef);
+    } else {
+      policies[fieldName] = kUndefined;
+    }
+  });
 
   logger_->logSecurityPolicies(policies);
 

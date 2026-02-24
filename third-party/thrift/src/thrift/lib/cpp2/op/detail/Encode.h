9b6545394cda6d3b7d02afa93338a8f82a0f46b4
TJ Yin <ytj@meta.com>
2026-02-24 03:20:03 -0800

    Add Json5ProtocolWriter (1): Implement the main functionalities
    Summary: Add JSON5ProtocolWriter class that wraps JsonWriter, plus toJsonString/toJson5String public template functions.
    
    Reviewed By: iahs, aristidisp
    
    Differential Revision: D93834775
    
    fbshipit-source-id: a7da907ce07c9864574d880f693b90bbb0cea91d

--- third-party/thrift/src/thrift/lib/cpp2/op/detail/Encode.h
+++ third-party/thrift/src/thrift/lib/cpp2/op/detail/Encode.h
@@ -606,7 +606,16 @@ template <typename T>
 struct Encode<type::enum_t<T>> {
   template <typename Protocol>
   uint32_t operator()(Protocol& prot, const T& s) const {
-    return prot.writeI32(static_cast<int32_t>(s));
+    const auto value = static_cast<std::int32_t>(s);
+    if constexpr (requires {
+                    prot.writeEnum(std::string_view{}, std::int32_t{});
+                  }) {
+      const char* name = util::enumName(s);
+      return prot.writeEnum(name ? name : "", value);
+    } else {
+      // TODO: add writeEnum to all protocols and delete this branch
+      return prot.writeI32(value);
+    }
   }
 };
 

d1013d6357394bc9abf489f411e8a0bb81f8f52c
Myungho Jung <myjung@meta.com>
2026-05-22 13:24:40 -0700

    Add codemod for php namespace to hack NamePrefix
    Summary: Introduces a Thrift codemod to safely migrate legacy `namespace php` declarations to modern `edward-io.NamePrefix` annotations. This ensures compatibility with Hack codegen and preserves generated code behavior by automatically handling required scaffolding, constant class names, and stable service names during the transition.
    
    Reviewed By: rmakheja
    
    Differential Revision: D104135488
    
    fbshipit-source-id: 2136a504516389f851a348c8658aad637414cb00

---
+++ third-party/thrift/src/thrift/compiler/codemod/migrate_php_namespace_to_hack_name_prefix.cc
@@ -0,0 +1,253 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <algorithm>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include <fmt/core.h>
+
+#include <thrift/compiler/ast/t_program.h>
+#include <thrift/compiler/ast/t_program_bundle.h>
+#include <thrift/compiler/ast/uri.h>
+#include <thrift/compiler/codemod/codemod.h>
+#include <thrift/compiler/codemod/file_manager.h>
+
+using apache::thrift::compiler::kHackConstantsClassUri;
+using apache::thrift::compiler::kHackNamePrefixUri;
+using apache::thrift::compiler::source_manager;
+using apache::thrift::compiler::t_package;
+using apache::thrift::compiler::t_program;
+using apache::thrift::compiler::t_program_bundle;
+using apache::thrift::compiler::codemod::file_manager;
+
+namespace {
+
+constexpr std::string_view kHackAnnotationInclude =
+    "thrift/annotation/hack.thrift";
+constexpr std::string_view kHackAnnotationIncludeFbcode =
+    "fbcode/thrift/annotation/hack.thrift";
+
+std::string get_php_namespace_prefix(const t_program& program) {
+  std::string php_ns = program.get_namespace("php");
+  std::replace(php_ns.begin(), php_ns.end(), '.', '_');
+  php_ns.push_back('_');
+  return php_ns;
+}
+
+class MigratePhpNamespaceToHackNamePrefix final {
+ public:
+  MigratePhpNamespaceToHackNamePrefix(
+      source_manager& sm,
+      t_program& program,
+      bool mangled_services,
+      bool remove_only)
+      : fm_(sm, program),
+        program_(program),
+        mangled_services_(mangled_services),
+        remove_only_(remove_only) {}
+
+  void run() {
+    if (!should_migrate()) {
+      return;
+    }
+
+    if (remove_only_) {
+      // For thrift files that are not consumed by Hack codegen (e.g. fbsource
+      // or configerator files that are not synced to www), strip the now-unused
+      // `namespace php` declaration without inserting any Hack-specific
+      // annotations or scaffolding.
+      remove_php_namespace();
+      fm_.apply_replacements();
+      return;
+    }
+
+    const std::optional<size_t> maybe_new_include_offset = maybe_add_include();
+    add_program_annotations(maybe_new_include_offset);
+    add_empty_hack_namespace_for_named_package();
+    remove_php_namespace();
+    fm_.apply_replacements();
+  }
+
+ private:
+  file_manager fm_;
+  const t_program& program_;
+  bool mangled_services_;
+  bool remove_only_;
+
+  bool should_migrate() const {
+    const auto& namespaces = program_.namespaces();
+    auto php_it = namespaces.find("php");
+    if (php_it == namespaces.end() || php_it->second->ns().empty()) {
+      return false;
+    }
+    if (remove_only_) {
+      // In remove-only mode we only care that there is a non-empty
+      // `namespace php` to strip. Hack-side state (existing `namespace hack`
+      // or `@hack.NamePrefix`) is irrelevant since we never write any.
+      return true;
+    }
+    if (namespaces.count("hack") != 0) {
+      return false;
+    }
+    return !program_.find_structured_annotation_or_null(kHackNamePrefixUri);
+  }
+
+  std::optional<size_t> maybe_add_include() {
+    if (fm_.has_thrift_include(std::string(kHackAnnotationInclude)) ||
+        fm_.has_thrift_include(std::string(kHackAnnotationIncludeFbcode))) {
+      return std::nullopt;
+    }
+    return fm_.add_include(std::string(kHackAnnotationInclude));
+  }
+
+  void remove_php_namespace() {
+    const auto php_it = program_.namespaces().find("php");
+    if (php_it == program_.namespaces().end()) {
+      return;
+    }
+
+    size_t begin_offset = fm_.to_offset(php_it->second->src_range().begin);
+    size_t end_offset = fm_.old_content().find(
+        '\n', fm_.to_offset(php_it->second->src_range().end));
+    if (end_offset == std::string::npos) {
+      end_offset = fm_.old_content().size();
+    } else {
+      ++end_offset;
+    }
+
+    if (begin_offset == 0 && end_offset < fm_.old_content().size() &&
+        fm_.old_content()[end_offset] == '\n') {
+      ++end_offset;
+    }
+    fm_.add({begin_offset, end_offset, ""});
+  }
+
+  void add_program_annotations(std::optional<size_t> maybe_new_include_offset) {
+    const std::string php_prefix = get_php_namespace_prefix(program_);
+    // Only emit `skip_services` when the program actually defines services;
+    // otherwise the field is meaningless and just clutters the annotation.
+    std::string annotations;
+    if (program_.services().empty()) {
+      annotations = fmt::format(
+          "@hack.NamePrefix{{prefix = \"{}\", apply_on_getName = false}}\n",
+          php_prefix);
+    } else {
+      annotations = fmt::format(
+          "@hack.NamePrefix{{prefix = \"{}\", apply_on_getName = false, "
+          "skip_services = {}}}\n",
+          php_prefix,
+          mangled_services_ ? "false" : "true");
+    }
+
+    if (!program_.consts().empty() &&
+        !program_.find_structured_annotation_or_null(kHackConstantsClassUri)) {
+      annotations =
+          fmt::format(
+              "@hack.ConstantsClass{{name = \"{}CONSTANTS\"}}\n", php_prefix) +
+          annotations;
+    }
+
+    const t_package& package = program_.package();
+    if (package.is_explicit()) {
+      const size_t offset = package.src_range().begin.offset();
+      fm_.add({offset, offset, annotations});
+      return;
+    }
+
+    size_t offset = 0;
+    if (maybe_new_include_offset.has_value()) {
+      offset = *maybe_new_include_offset;
+    } else if (!program_.includes().empty()) {
+      offset = fm_.to_offset(program_.includes().back()->src_range().end) + 1;
+    } else {
+      offset = fm_.get_namespace_offset();
+    }
+
+    std::string new_content = annotations + "package;\n\n";
+    if (!maybe_new_include_offset.has_value() && !program_.includes().empty()) {
+      new_content = "\n" + new_content;
+    }
+    fm_.add({offset, offset, new_content});
+  }
+
+  void add_empty_hack_namespace_for_named_package() {
+    const t_package& package = program_.package();
+    if (!package.is_explicit() || package.empty()) {
+      return;
+    }
+
+    size_t offset = fm_.to_offset(package.src_range().end);
+    offset = fm_.old_content().find('\n', offset);
+    if (offset == std::string::npos) {
+      offset = fm_.old_content().size();
+    } else {
+      ++offset;
+    }
+    fm_.add({offset, offset, "\nnamespace hack \"\"\n"});
+  }
+};
+
+} // namespace
+
+int main(int argc, char** argv) {
+  bool mangled_services = false;
+  bool remove_only = false;
+  std::vector<char*> filtered_argv;
+  filtered_argv.reserve(argc);
+  filtered_argv.push_back(argv[0]);
+
+  for (int i = 1; i < argc; ++i) {
+    std::string_view arg = argv[i];
+    if (arg == "--mangledsvcs") {
+      mangled_services = true;
+      continue;
+    }
+    if (arg == "--remove-only") {
+      remove_only = true;
+      continue;
+    }
+    filtered_argv.push_back(argv[i]);
+  }
+
+  if (filtered_argv.size() <= 1) {
+    fmt::print(
+        stderr,
+        "Usage: {} [--mangledsvcs] [--remove-only] <thrift-file>\n",
+        filtered_argv.front());
+    return 1;
+  }
+
+  if (remove_only && mangled_services) {
+    fmt::print(
+        stderr,
+        "--remove-only and --mangledsvcs are mutually exclusive: "
+        "--remove-only does not emit @hack.NamePrefix, so service "
+        "naming is not preserved by this tool in that mode.\n");
+    return 1;
+  }
+
+  return apache::thrift::compiler::run_codemod(
+      static_cast<int>(filtered_argv.size()),
+      filtered_argv.data(),
+      [mangled_services, remove_only](
+          source_manager& sm, t_program_bundle& pb) {
+        MigratePhpNamespaceToHackNamePrefix(
+            sm, *pb.root_program(), mangled_services, remove_only)
+            .run();
+      });
+}
---
+++ third-party/thrift/src/thrift/compiler/codemod/migrate_php_namespace_to_hack_name_prefix_test.py
@@ -0,0 +1,377 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pyre-unsafe
+
+import difflib
+import filecmp
+import os
+import shutil
+import subprocess
+import tempfile
+import textwrap
+import unittest
+from pathlib import Path
+
+import pkg_resources
+from xplat.thrift.compiler.codemod.test_utils import read_file, run_binary, write_file
+
+
+class MigratePhpNamespaceToHackNamePrefixTest(unittest.TestCase):
+    def setUp(self):
+        tmp = tempfile.mkdtemp()
+        self.addCleanup(shutil.rmtree, tmp, True)
+        self.tmp = tmp
+        self.addCleanup(os.chdir, os.getcwd())
+        os.chdir(self.tmp)
+        self.maxDiff = None
+        self.binary = pkg_resources.resource_filename(__name__, "codemod")
+        self.thrift = pkg_resources.resource_filename(__name__, "thrift_bin")
+        self.hack_annotation = pkg_resources.resource_filename(
+            __name__, "thrift/annotation/hack.thrift"
+        )
+        os.makedirs("thrift/annotation", exist_ok=True)
+        shutil.copyfile(self.hack_annotation, "thrift/annotation/hack.thrift")
+
+    def test_rewrites_named_package_for_mangled_services(self):
+        write_file(
+            "foo.thrift",
+            textwrap.dedent(
+                """\
+                namespace php foo.php.ns
+
+                package "meta.com/foo"
+
+                const i32 ANSWER = 42;
+
+                struct S {
+                  1: string name;
+                }
+
+                service MyService {
+                  S ping(1: S request);
+                }
+                """
+            ),
+        )
+
+        run_binary(self.binary, "foo.thrift", "--mangledsvcs")
+
+        self.assertEqual(
+            read_file("foo.thrift"),
+            textwrap.dedent(
+                """\
+                include "thrift/annotation/hack.thrift"
+
+                @hack.ConstantsClass{name = "foo_php_ns_CONSTANTS"}
+                @hack.NamePrefix{prefix = "foo_php_ns_", apply_on_getName = false, skip_services = false}
+                package "meta.com/foo"
+
+                namespace hack ""
+
+                const i32 ANSWER = 42;
+
+                struct S {
+                  1: string name;
+                }
+
+                service MyService {
+                  S ping(1: S request);
+                }
+                """
+            ),
+        )
+
+    def test_adds_empty_package_for_non_mangled_services(self):
+        write_file(
+            "foo.thrift",
+            textwrap.dedent(
+                """\
+                namespace php Foo.Bar
+
+                struct S {
+                  1: i32 x;
+                }
+
+                service MyService {
+                  S ping(1: S request);
+                }
+                """
+            ),
+        )
+
+        run_binary(self.binary, "foo.thrift")
+
+        self.assertEqual(
+            read_file("foo.thrift"),
+            textwrap.dedent(
+                """\
+                include "thrift/annotation/hack.thrift"
+
+                @hack.NamePrefix{prefix = "Foo_Bar_", apply_on_getName = false, skip_services = true}
+                package;
+
+                struct S {
+                  1: i32 x;
+                }
+
+                service MyService {
+                  S ping(1: S request);
+                }
+                """
+            ),
+        )
+
+    def test_omits_skip_services_when_no_service(self):
+        write_file(
+            "foo.thrift",
+            textwrap.dedent(
+                """\
+                namespace php Foo.Bar
+
+                struct S {
+                  1: i32 x;
+                }
+                """
+            ),
+        )
+
+        run_binary(self.binary, "foo.thrift")
+
+        self.assertEqual(
+            read_file("foo.thrift"),
+            textwrap.dedent(
+                """\
+                include "thrift/annotation/hack.thrift"
+
+                @hack.NamePrefix{prefix = "Foo_Bar_", apply_on_getName = false}
+                package;
+
+                struct S {
+                  1: i32 x;
+                }
+                """
+            ),
+        )
+
+    def test_keeps_generated_php_identical_for_mangled_services(self):
+        original = textwrap.dedent(
+            """\
+            namespace php foo.php.ns
+
+            package "meta.com/foo"
+
+            const i32 ANSWER = 42;
+
+            struct S {
+              1: string name;
+            }
+
+            service MyService {
+              S ping(1: S request);
+            }
+            """
+        )
+        self._assert_generated_php_is_identical(
+            original,
+            before_gen="hack:mangledsvcs=1",
+            after_gen="hack",
+            codemod_args=["--mangledsvcs"],
+        )
+
+    def test_keeps_generated_php_identical_for_non_mangled_services(self):
+        original = textwrap.dedent(
+            """\
+            namespace php Foo.Bar
+
+            struct S {
+              1: string name;
+            }
+
+            service MyService {
+              S ping(1: S request);
+            }
+            """
+        )
+        self._assert_generated_php_is_identical(
+            original,
+            before_gen="hack",
+            after_gen="hack",
+            codemod_args=[],
+        )
+
+    def test_remove_only_strips_php_namespace_without_annotations(self):
+        write_file(
+            "foo.thrift",
+            textwrap.dedent(
+                """\
+                namespace php Foo.Bar
+
+                struct S {
+                  1: i32 x;
+                }
+
+                service MyService {
+                  S ping(1: S request);
+                }
+                """
+            ),
+        )
+
+        run_binary(self.binary, "foo.thrift", "--remove-only")
+
+        self.assertEqual(
+            read_file("foo.thrift"),
+            textwrap.dedent(
+                """\
+                struct S {
+                  1: i32 x;
+                }
+
+                service MyService {
+                  S ping(1: S request);
+                }
+                """
+            ),
+        )
+
+    def test_remove_only_is_a_no_op_when_php_namespace_absent(self):
+        original = textwrap.dedent(
+            """\
+            struct S {
+              1: i32 x;
+            }
+            """
+        )
+        write_file("foo.thrift", original)
+
+        run_binary(self.binary, "foo.thrift", "--remove-only")
+
+        self.assertEqual(read_file("foo.thrift"), original)
+
+    def test_remove_only_strips_php_namespace_even_when_hack_present(self):
+        # Unlike full migration, --remove-only doesn't care about existing
+        # hack namespace or @hack.NamePrefix; it only strips `namespace php`.
+        write_file(
+            "foo.thrift",
+            textwrap.dedent(
+                """\
+                namespace php Foo.Bar
+                namespace hack ""
+
+                struct S {
+                  1: i32 x;
+                }
+                """
+            ),
+        )
+
+        run_binary(self.binary, "foo.thrift", "--remove-only")
+
+        self.assertEqual(
+            read_file("foo.thrift"),
+            textwrap.dedent(
+                """\
+                namespace hack ""
+
+                struct S {
+                  1: i32 x;
+                }
+                """
+            ),
+        )
+
+    def test_remove_only_and_mangledsvcs_are_mutually_exclusive(self):
+        write_file(
+            "foo.thrift",
+            textwrap.dedent(
+                """\
+                namespace php Foo.Bar
+
+                struct S {
+                  1: i32 x;
+                }
+                """
+            ),
+        )
+
+        with self.assertRaises(subprocess.CalledProcessError):
+            run_binary(self.binary, "foo.thrift", "--remove-only", "--mangledsvcs")
+
+    def _assert_generated_php_is_identical(
+        self,
+        thrift_source,
+        *,
+        before_gen,
+        after_gen,
+        codemod_args,
+    ):
+        write_file("foo.thrift", thrift_source)
+
+        before_dir = Path(self.tmp) / "before"
+        self._run_thrift(before_gen, before_dir)
+        run_binary(self.binary, "foo.thrift", *codemod_args)
+        after_dir = Path(self.tmp) / "after"
+        self._run_thrift(after_gen, after_dir)
+
+        self._assert_directory_tree_equal(
+            before_dir / "gen-hack",
+            after_dir / "gen-hack",
+        )
+
+    def _run_thrift(self, gen, output_dir):
+        output_dir.mkdir(parents=True, exist_ok=True)
+        subprocess.check_call(
+            [
+                self.thrift,
+                "-I",
+                self.tmp,
+                "-o",
+                output_dir,
+                "--gen",
+                gen,
+                "foo.thrift",
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+    def _assert_directory_tree_equal(self, expected_dir, actual_dir):
+        comparison = filecmp.dircmp(expected_dir, actual_dir)
+        self.assertEqual(comparison.left_only, [])
+        self.assertEqual(comparison.right_only, [])
+        self.assertEqual(comparison.funny_files, [])
+
+        for filename in comparison.common_files:
+            expected_contents = read_file(os.path.join(expected_dir, filename))
+            actual_contents = read_file(os.path.join(actual_dir, filename))
+            if expected_contents == actual_contents:
+                continue
+
+            diff = "\n".join(
+                difflib.unified_diff(
+                    expected_contents.splitlines(),
+                    actual_contents.splitlines(),
+                    fromfile=str(Path(expected_dir) / filename),
+                    tofile=str(Path(actual_dir) / filename),
+                    lineterm="",
+                )
+            )
+            self.fail(diff)
+
+        for subdir in comparison.common_dirs:
+            self._assert_directory_tree_equal(
+                Path(expected_dir) / subdir,
+                Path(actual_dir) / subdir,
+            )

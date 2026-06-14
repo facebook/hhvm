05fb3007cfb1fea2c1e4dc5da78dd2a1d5fad0d0
David Tolnay <dtolnay@meta.com>
2026-06-13 18:42:07 -0700

    Update platform010 & platform010-aarch64 symlinks
    Summary:
    Release notes: https://blog.rust-lang.org/2026/05/28/Rust-1.96.0/
    
    - Stabilized features:
        - `feature(assert_matches)`
    
    - Wasm targets no longer pass `--allow-undefined` to the linker ([rust#149868](https://github.com/rust-lang/rust/pull/149868))
    
    - Changes in the computation of *"private type in public interface"*
    
    - New ambiguities surfaced by `ambiguous_glob_imports` lint ([rust#114095](https://github.com/rust-lang/rust/issues/114095))
    
    - New warning on unneeded `#![feature(…)]`
    
    - New error on mismatched `-Zbranch-protection` between dependencies
    
    Reviewed By: ship-it-ship-it, diliop, opsound
    
    Differential Revision: D108384430
    
    fbshipit-source-id: 60c3a5a98e7eabdff47eb1db213f28a29111c11d

--- hphp/hack/src/utils/rust/file_rwlock.rs
+++ hphp/hack/src/utils/rust/file_rwlock.rs
@@ -3,8 +3,6 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the "hack" directory of this source tree.
 
-#![feature(assert_matches)]
-
 use std::os::unix::prelude::FileExt;
 use std::path::Path;
 use std::path::PathBuf;

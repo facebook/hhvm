6c7abc35722d8f61dbc4c8523c705ade44d36fa7
Sam De Roeck <sadroeck@meta.com>
2026-07-23 18:30:57 -0700

    Compare DynamicValue benchmark roundtrips
    Summary:
    Add a roundtrip benchmark baseline using `DynamicValue` as the Document model. This provides the comparison for non-static schema encoding/decoding, e.g. as currently used by thrift-proxy.
    
    ___
    
    Differential Revision: D113449676
    
    fbshipit-source-id: 043947205cab9a0ec27ae85be6af5384255ee2af

--- third-party/thrift/src/thrift/lib/cpp2/transcode/test/TranscodeBenchmark.cpp
+++ third-party/thrift/src/thrift/lib/cpp2/transcode/test/TranscodeBenchmark.cpp
@@ -18,6 +18,7 @@
 
 #include <thrift/lib/cpp2/transcode/test/TranscodeBenchmarkData.h>
 
+#include <thrift/lib/cpp2/dynamic/Serialization.h>
 #include <thrift/lib/cpp2/protocol/Serializer.h>
 #include <thrift/lib/cpp2/runtime/SchemaRegistry.h>
 #include <thrift/lib/cpp2/transcode/CodecFactory.h>
@@ -26,15 +27,18 @@
 #include <folly/Benchmark.h>
 #include <folly/init/Init.h>
 #include <folly/io/IOBuf.h>
+#include <folly/io/IOBufQueue.h>
 
 #include <array>
 #include <cstddef>
+#include <cstdint>
 #include <memory>
 #include <optional>
 #include <stdexcept>
 #include <string>
 #include <string_view>
 #include <utility>
+#include <vector>
 
 namespace apache::thrift::transcode {
 namespace {
@@ -52,6 +56,7 @@ constexpr auto kCompact = ProtocolKey::Compact;
 constexpr auto kBinary = ProtocolKey::Binary;
 constexpr auto kJson = ProtocolKey::Json;
 constexpr auto kProtocolCount = static_cast<std::size_t>(ProtocolKey::Count);
+constexpr std::size_t kOutputBufferSize = 1024 * 1024;
 
 constexpr std::size_t protocolIndex(ProtocolKey protocol) {
   return static_cast<std::size_t>(protocol);
@@ -78,7 +83,8 @@ struct ProtocolPair {
   std::string_view name;
   ProtocolKey source;
   ProtocolKey target;
-  void (*benchRoundtrip)(std::size_t);
+  void (*benchCpp2Roundtrip)(std::size_t);
+  void (*benchDynamicValueRoundtrip)(std::size_t);
 };
 
 const std::array<ProtocolPair, 4>& protocolPairs();
@@ -131,8 +137,9 @@ std::unique_ptr<folly::IOBuf> serializeInput(
 
 template <typename T>
 struct BenchFixture {
-  explicit BenchFixture(T value) {
-    const auto& node = SchemaRegistry::get().getTypeSystemNode<T>();
+  explicit BenchFixture(T value)
+      : type_(SchemaRegistry::get().getTypeSystemNode<T>().asRef()) {
+    const auto& node = type_.asStruct();
     for (const auto& spec : kProtocolSpecs) {
       codecs_[protocolIndex(spec.key)] = spec.makeCodec(node);
       inputs_[protocolIndex(spec.key)] = serializeInput(spec.key, value);
@@ -142,6 +149,8 @@ struct BenchFixture {
           makeInterpreter(codec(pair.source), codec(pair.target));
     }
   }
+
+  type_system::TypeRef type() const { return type_; }
 
   const folly::IOBuf& input(ProtocolKey protocol) const {
     const auto& value = inputs_[protocolIndex(protocol)];
@@ -168,6 +177,7 @@ struct BenchFixture {
     return *value;
   }
 
+  type_system::TypeRef type_;
   std::array<std::optional<Codec>, kProtocolCount> codecs_;
   std::array<std::unique_ptr<folly::IOBuf>, kProtocolCount> inputs_;
   std::array<std::unique_ptr<ITranscoder>, kProtocolCount * kProtocolCount>
@@ -180,24 +190,48 @@ BenchFixture<fixture::GoldenStruct>& goldenFixture() {
   return fixture;
 }
 
+void reserveOutput(folly::IOBufQueue& queue, std::size_t size) {
+  auto [data, capacity] = queue.preallocate(size, size, size);
+  if (data == nullptr || capacity < size) {
+    throw std::runtime_error("failed to preallocate benchmark output buffer");
+  }
+}
+
+void observeOutput(const folly::IOBufQueue& queue) {
+  auto length = queue.chainLength();
+  const auto* output = queue.front();
+  folly::doNotOptimizeAway(length);
+  folly::doNotOptimizeAway(output);
+}
+
 void benchTranscode(std::size_t iters, ProtocolKey source, ProtocolKey target) {
   const auto& fixture = goldenFixture();
   const auto& transcoder = fixture.transcoder(source, target);
   const auto& input = fixture.input(source);
+  std::vector<std::uint8_t> output(kOutputBufferSize);
   while (iters-- > 0) {
-    auto output = transcoder.transcode(input);
-    folly::doNotOptimizeAway(output);
+    auto written =
+        transcoder.transcodeInto(input, output.data(), output.size());
+    if (written.hasError()) {
+      throw std::runtime_error(written.error().message);
+    }
+    folly::doNotOptimizeAway(*written);
+    folly::doNotOptimizeAway(output.data());
   }
 }
 
 template <typename SourceSerializer, typename TargetSerializer>
 void benchStructRoundtrip(std::size_t iters, ProtocolKey source) {
-  const auto& input = goldenFixture().input(source);
+  const auto& fixture = goldenFixture();
+  const auto& input = fixture.input(source);
+  folly::IOBufQueue output(folly::IOBufQueue::cacheChainLength());
+  reserveOutput(output, kOutputBufferSize);
   while (iters-- > 0) {
     auto value =
         SourceSerializer::template deserialize<fixture::GoldenStruct>(&input);
-    auto output = TargetSerializer::template serialize<std::string>(value);
-    folly::doNotOptimizeAway(output);
+    TargetSerializer::serialize(value, &output);
+    observeOutput(output);
+    output.clearAndTryReuseLargestBuffer();
   }
 }
 
@@ -209,23 +243,66 @@ void benchPairRoundtrip(std::size_t iters) {
   benchStructRoundtrip<SourceSerializer, TargetSerializer>(iters, Source);
 }
 
+template <typename SourceReader, typename TargetWriter>
+void benchDynamicValueRoundtrip(std::size_t iters, ProtocolKey source) {
+  const auto& fixture = goldenFixture();
+  const auto& input = fixture.input(source);
+  const auto type = fixture.type();
+  folly::IOBufQueue output(folly::IOBufQueue::cacheChainLength());
+  reserveOutput(output, kOutputBufferSize);
+  while (iters-- > 0) {
+    SourceReader reader;
+    reader.setInput(&input);
+    auto value = dynamic::deserializeValue(reader, type, nullptr);
+
+    {
+      TargetWriter writer;
+      writer.setOutput(&output, kOutputBufferSize);
+      dynamic::serializeValue(writer, value);
+    }
+    observeOutput(output);
+    output.clearAndTryReuseLargestBuffer();
+  }
+}
+
+template <typename SourceReader, typename TargetWriter, ProtocolKey Source>
+void benchPairDynamicValueRoundtrip(std::size_t iters) {
+  benchDynamicValueRoundtrip<SourceReader, TargetWriter>(iters, Source);
+}
+
 const std::array<ProtocolPair, 4> kProtocolPairs{{
     {"Golden_JsonToCompact",
      kJson,
      kCompact,
-     benchPairRoundtrip<SimpleJSONSerializer, CompactSerializer, kJson>},
+     benchPairRoundtrip<SimpleJSONSerializer, CompactSerializer, kJson>,
+     benchPairDynamicValueRoundtrip<
+         SimpleJSONProtocolReader,
+         CompactProtocolWriter,
+         kJson>},
     {"Golden_CompactToJson",
      kCompact,
      kJson,
-     benchPairRoundtrip<CompactSerializer, SimpleJSONSerializer, kCompact>},
+     benchPairRoundtrip<CompactSerializer, SimpleJSONSerializer, kCompact>,
+     benchPairDynamicValueRoundtrip<
+         CompactProtocolReader,
+         SimpleJSONProtocolWriter,
+         kCompact>},
     {"Golden_JsonToBinary",
      kJson,
      kBinary,
-     benchPairRoundtrip<SimpleJSONSerializer, BinarySerializer, kJson>},
+     benchPairRoundtrip<SimpleJSONSerializer, BinarySerializer, kJson>,
+     benchPairDynamicValueRoundtrip<
+         SimpleJSONProtocolReader,
+         BinaryProtocolWriter,
+         kJson>},
     {"Golden_BinaryToJson",
      kBinary,
      kJson,
-     benchPairRoundtrip<BinarySerializer, SimpleJSONSerializer, kBinary>},
+     benchPairRoundtrip<BinarySerializer, SimpleJSONSerializer, kBinary>,
+     benchPairDynamicValueRoundtrip<
+         BinaryProtocolReader,
+         SimpleJSONProtocolWriter,
+         kBinary>},
 }};
 
 const std::array<ProtocolPair, 4>& protocolPairs() {
@@ -233,21 +310,32 @@ const std::array<ProtocolPair, 4>& protocolPairs() {
 }
 
 bool registerProtocolPairBenchmarks() {
-  for (const auto& pair : kProtocolPairs) {
+  for (std::size_t i = 0; i < kProtocolPairs.size(); ++i) {
+    const auto& pair = kProtocolPairs[i];
     folly::addBenchmark(
         __FILE__,
-        std::string{pair.name} + "_KernelInterpreter",
+        std::string{pair.name} + "_DynamicValueRoundtrip",
         [pair](unsigned iters) {
-          benchTranscode(iters, pair.source, pair.target);
+          pair.benchDynamicValueRoundtrip(iters);
           return iters;
         });
     folly::addBenchmark(
         __FILE__,
         "%" + std::string{pair.name} + "_Cpp2Roundtrip",
         [pair](unsigned iters) {
-          pair.benchRoundtrip(iters);
+          pair.benchCpp2Roundtrip(iters);
           return iters;
         });
+    folly::addBenchmark(
+        __FILE__,
+        "%" + std::string{pair.name} + "_Kernel",
+        [pair](unsigned iters) {
+          benchTranscode(iters, pair.source, pair.target);
+          return iters;
+        });
+    if (i + 1 < kProtocolPairs.size()) {
+      folly::addBenchmark(__FILE__, "-", []() -> unsigned { return 0; });
+    }
   }
   return true;
 }
@@ -263,6 +351,7 @@ FOLLY_POP_WARNING
 
 int main(int argc, char** argv) {
   folly::Init init(&argc, &argv);
+  (void)apache::thrift::transcode::goldenFixture();
   folly::runBenchmarks();
   return 0;
 }

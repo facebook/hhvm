da2fb1e9a90f99b168bfff588aa122e309448293
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2026-08-18 19:22:19 -0700

    Remove unused type error suppressions - thrift
    Summary:
    This diff was automatically generated by the Pyre per-target upgrade tool.
    
    It removes `# pyre-fixme` or `pyrefly: ignore` comments that are no longer needed because the underlying type errors have been resolved.
    Note that it will also aim to ensure type checking runs cleanly, and will add suppressions to existing type errors.
    
    #pyreupgrade
    
    Differential Revision: D116538124
    
    fbshipit-source-id: 5acc7ea5e3851dd4786c5d6fa46af5680e3ccf53

--- third-party/thrift/src/thrift/test/py/adapter_for_tests.py
+++ third-party/thrift/src/thrift/test/py/adapter_for_tests.py
@@ -33,5 +33,4 @@ class AdapterTestStructToDict:
     # pyre-fixme[3]: Return type must be annotated.
     # pyre-fixme[2]: Parameter must be annotated.
     def to_thrift(py_value):
-        # pyre-fixme[16]: Module `thrift.test.py` has no attribute `adapter_bar`.
         return Bar(**py_value)

a21a5434102e556b5e8d09593943d2e40cc046b6
generatedunixname896621659824011 <noreply+896621659824011@fb.com>
2026-09-16 02:52:01 -0700

    fbcode/hphp/runtime/ext/core/php/lang/KeyedIterable.ns.php
    Differential Revision: D119937478
    
    fbshipit-source-id: 7dbe711d648fc01e4f6275d833719cd664ac158d

--- hphp/runtime/ext/core/php/lang/KeyedIterable.ns.php
+++ hphp/runtime/ext/core/php/lang/KeyedIterable.ns.php
@@ -3,8 +3,6 @@
 namespace HH {
 
 interface KeyedIterable<+Tk, +Tv> extends Iterable<Tv>, KeyedTraversable<Tk, Tv> {
-  // TODO(T121800572) We should add in the methods defined in the version of
-  // this class in `hphp/hack/hhi/interfaces.hhi`
   // TODO(T125922750) This file *used* to contain a definition for `mapWithKeys`
   // but it has been removed for now to unblock typechecking `Set`.
 
